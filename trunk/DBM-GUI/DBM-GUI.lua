4a58a6859b2f156a144bfb1ae3b19c23279a4677
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-08-14 17:58:58 +0000

    first working DBM GUI

--- trunk/DBM-GUI/DBM-GUI.lua
+++ trunk/DBM-GUI/DBM-GUI.lua
@@ -6,58 +6,66 @@ DBM_GUI_Language.General = "General Options"
 DBM_GUI_Language.Enable = "enable"
 DBM_GUI_Language.Disable = "disable"
 
-
+local framecount = 0
+local PanelPrototype = {}
 DBM_GUI = {}
 DBM_GUI.panels = {}
-DBM_GUI.areas = {}
 
-function DBM_GUI:CreateNewPanel(FrameName, ParentFrame, OkButton, CancelButton, DefaultButton) 
-	local panel = CreateFrame('Frame')
+function DBM_GUI:CreateNewPanel(FrameName, OkButton, CancelButton, DefaultButton) 
+	local panel = CreateFrame('Frame', "DBM_GUI_Option_"..self:GetNewID())
 	panel.name = FrameName
-	if ParentFrame then 
-		panel.parent = ParentFrame 
-	elseif ParentFrame == false then
-		-- do nothing, this is the first declaration
+	if self == DBM_GUI then
+		-- no panel.parent is need
 	else
-		panel.parent = DBM_GUI_Language.MainFrame
+		panel.parent = self.frame.name
 	end
 	if type(OkButton) == "function" then
 		panel.okay = OkButton
 	end
 	if type(CancelButton) == "function" then
-		panel.cancel = OkButton
+		panel.cancel = CancelButton 
 	end
 	if type(DefaultButton) == "function" then
-		panel.default = OkButton
+		panel.default = DefaultButton
 	end
 	InterfaceOptions_AddCategory(panel)
 
-	table.insert(self.panels, {frame = panel})
+	table.insert(self.panels, {frame = panel, parent = self, name = "DBM_GUI_Option_"..framecount})
 	local obj = self.panels[#self.panels]
-	return setmetatable(obj, {__index = panelPrototype})
+	return setmetatable(obj, {__index = PanelPrototype})
+end
+function DBM_GUI:GetNewID() 
+	framecount = framecount + 1
+	return framecount
+end
+function DBM_GUI:GetCurrentID()
+	return framecount
 end
 
 -- BEGIN - Basic GUI Items
-local PanelPrototype = {}
+PanelPrototype.panels = {}
+PanelPrototype.areas = {}
+PanelPrototype.CreateNewPanel 	= DBM_GUI.CreateNewPanel
+PanelPrototype.GetNewID 	= DBM_GUI.GetNewID
 function PanelPrototype:CreateArea(name, width, height)
-	local panel = CreateFrame('Frame', self:GetName() .. name, self, 'OptionFrameBoxTemplate')
+	local panel = CreateFrame('Frame', "DBM_GUI_Option_"..self:GetNewID(), self.frame, 'OptionFrameBoxTemplate')
 	panel:SetBackdropBorderColor(0.4, 0.4, 0.4)
 	panel:SetBackdropColor(0.15, 0.15, 0.15, 0.5)
 	getglobal(panel:GetName() .. 'Title'):SetText(name)
 	panel:SetWidth(width)
 	panel:SetHeight(height)
 
-	table.insert(self.areas, {frame = panel})
+	table.insert(self.areas, {frame = panel, parent = self, name = "DBM_GUI_Option_"..framecount})
 	local obj = self.areas[#self.areas]
 	return setmetatable(obj, {__index = panelPrototype})
 end
 function PanelPrototype:CreateCheckButton(name)
-	local button = CreateFrame('CheckButton', parent:GetName() .. name, self, 'OptionsCheckButtonTemplate')
+	local button = CreateFrame('CheckButton', "DBM_GUI_Option_"..self:GetNewID(), self.frame, 'OptionsCheckButtonTemplate')
 	getglobal(button:GetName() .. 'Text'):SetText(name)
 	return button
 end
 function PanelPrototype:CreateDropdown(name)
-	local frame = CreateFrame('Frame', parent:GetName() .. name, self, 'UIDropDownMenuTemplate')
+	local frame = CreateFrame('Frame', "DBM_GUI_Option_"..self:GetNewID(), self.frame, 'UIDropDownMenuTemplate')
 	local text = frame:CreateFontString(nil, 'BACKGROUND')
 	text:SetPoint('BOTTOMLEFT', frame, 'TOPLEFT', 21, 0)
 	text:SetFontObject('GameFontNormalSmall')
@@ -65,10 +73,12 @@ function PanelPrototype:CreateDropdown(name)
 	return frame
 end
 
-
 -- END - Basic GUI Items
 
 mainpanel = DBM_GUI:CreateNewPanel("Deadly Boss Mods", false)
---mainpanel.frame:CreateArea(DBM_GUI_Language.General, 180, 200)
+mainpanel:CreateArea(DBM_GUI_Language.General, 180, 200)
+subpanel = mainpanel:CreateNewPanel("Aggro Alert")
+
+
 
 
