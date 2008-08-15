17f4619a867d794d00c1b88bbdc8ed84fe301c1a
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-08-15 12:01:08 +0000

    added editbox .. (buggy)

--- DBM-GUI/DBM-GUI.lua
+++ DBM-GUI/DBM-GUI.lua
@@ -6,12 +6,16 @@ DBM_GUI_Language.General = "General Options"
 DBM_GUI_Language.Enable = "enable"
 DBM_GUI_Language.Disable = "disable"
 
+
+
+local FrameTitle = "DBM_GUI_Option_"	-- all GUI Frames get automatical a name FrameTitle..ID
+
 local PanelPrototype = {}
 DBM_GUI = {}
 setmetatable(PanelPrototype, {__index = DBM_GUI})
 
 function DBM_GUI:CreateNewPanel(FrameName, OkButton, CancelButton, DefaultButton) 
-	local panel = CreateFrame('Frame', "DBM_GUI_Option_"..self:GetNewID())
+	local panel = CreateFrame('Frame', FrameTitle..self:GetNewID())
 	panel.name = FrameName
 	if self == DBM_GUI then
 		-- no panel.parent is need
@@ -30,7 +34,7 @@ function DBM_GUI:CreateNewPanel(FrameName, OkButton, CancelButton, DefaultButton
 	InterfaceOptions_AddCategory(panel)
 	
 	self.panels = self.panels or {}
-	table.insert(self.panels, {frame = panel, parent = self, framename = "DBM_GUI_Option_"..self:GetCurrentID()})
+	table.insert(self.panels, {frame = panel, parent = self, framename = FrameTitle..self:GetCurrentID()})
 	local obj = self.panels[#self.panels]
 	return setmetatable(obj, {__index = PanelPrototype})
 end
@@ -47,39 +51,86 @@ do
 end
 
 -- BEGIN - Basic GUI Items
-
 function PanelPrototype:CreateArea(name, width, height)
-	local panel = CreateFrame('Frame', "DBM_GUI_Option_"..self:GetNewID(), self.frame, 'OptionFrameBoxTemplate')
-	panel:SetBackdropBorderColor(0.4, 0.4, 0.4)
-	panel:SetBackdropColor(0.15, 0.15, 0.15, 0.5)
-	getglobal(panel:GetName() .. 'Title'):SetText(name)
-	panel:SetWidth(width)
-	panel:SetHeight(height)
-	panel:Show()
-	
+	local area = CreateFrame('Frame', FrameTitle..self:GetNewID(), self.frame, 'OptionFrameBoxTemplate')
+	area:SetBackdropBorderColor(0.4, 0.4, 0.4)
+	area:SetBackdropColor(0.15, 0.15, 0.15, 0.5)
+	getglobal(area:GetName() .. 'Title'):SetText(name)
+	area:SetWidth(width)
+	area:SetHeight(height)
+		
 	self.areas = self.areas or {}
-	table.insert(self.areas, {frame = panel, parent = self, framename = "DBM_GUI_Option_"..self:GetCurrentID()})
+	table.insert(self.areas, {frame = area, parent = self, framename = FrameTitle..self:GetCurrentID()})
 	local obj = self.areas[#self.areas]
-	return setmetatable(obj, {__index = panelPrototype})
+	return setmetatable(obj, {__index = PanelPrototype})
 end
 function PanelPrototype:CreateCheckButton(name)
-	local button = CreateFrame('CheckButton', "DBM_GUI_Option_"..self:GetNewID(), self.parent.frame, 'OptionsCheckButtonTemplate')
+	local button = CreateFrame('CheckButton', FrameTitle..self:GetNewID(), self.frame, 'OptionsCheckButtonTemplate')
 	getglobal(button:GetName() .. 'Text'):SetText(name)
 	return button
 end
 function PanelPrototype:CreateDropdown(name)
-	local frame = CreateFrame('Frame', "DBM_GUI_Option_"..self:GetNewID(), self.parent.frame, 'UIDropDownMenuTemplate')
-	local text = frame:CreateFontString(nil, 'BACKGROUND')
-	text:SetPoint('BOTTOMLEFT', frame, 'TOPLEFT', 21, 0)
+	local dropdown = CreateFrame('Frame', FrameTitle..self:GetNewID(), self.frame, 'UIDropDownMenuTemplate')
+	local text = frame:CreateFontString(FrameTitle..self:GetCurrentID().."Text", 'BACKGROUND')
+	text:SetPoint('BOTTOMLEFT', dropdown, 'TOPLEFT', 21, 0)
 	text:SetFontObject('GameFontNormalSmall')
 	text:SetText(name)
-	return frame
+	return dropdown
 end
+function PanelPrototype:CreateEditBox(text)
+	local textbox = CreateFrame('EditBox', FrameTitle..self:GetNewID(), self.frame, 'DBM_GUI_FrameEditBoxTemplate')
+	getglobal(FrameTitle..self:GetNewID().."Text"):SetText(text)
+
+	return textbox
+end
+function PanelPrototype:CreateSlider(text, low, high, step)
+	local slider = CreateFrame('Slider', FrameTitle..self:GetNewID(), self.frame, 'OptionsSliderTemplate')
+	--slider:SetScript('OnMouseWheel', Slider_OnMouseWheel)
+	--slider:EnableMouseWheel(true)
+	slider:SetMinMaxValues(low, high)
+	slider:SetValueStep(step)
+
+	getglobal(FrameTitle..self:GetCurrentID()..'Text'):SetText(text)
+	getglobal(FrameTitle..self:GetCurrentID()..'Low'):SetText()
+	getglobal(FrameTitle..self:GetCurrentID()..'High'):SetText()
+
+	--local text = slider:CreateFontString(nil, 'BACKGROUND')
+	--text:SetFontObject('GameFontHighlightSmall')
+	--text:SetPoint('LEFT', slider, 'RIGHT', 7, 0)
+	--slider.valText = text 
+	return slider
+end
+
 -- END - Basic GUI Items
 
-mainpanel = DBM_GUI:CreateNewPanel("Deadly Boss Mods", false)
-mainpanel:CreateArea(DBM_GUI_Language.General, 180, 200)
-subpanel = mainpanel:CreateNewPanel("Aggro Alert")
+
+do 
+	DBM_GUI_Language.PizzaTimer = 'Create "Pizza" Timer'
+
+
+	DBM_GUI_Frame = DBM_GUI:CreateNewPanel("Deadly Boss Mods", false)
+
+	part1 = DBM_GUI_Frame:CreateArea(DBM_GUI_Language.General, 180, 180)
+	part1.frame:SetPoint('TOPLEFT', 10, -20)
+
+	part2 = DBM_GUI_Frame:CreateArea(DBM_GUI_Language.General, 180, 180)
+	part2.frame:SetPoint('TOPLEFT', 195, -20)
+
+	pizzaarea = DBM_GUI_Frame:CreateArea(DBM_GUI_Language.PizzaTimer, 180, 180)
+	pizzaarea.frame:SetPoint('TOPLEFT', 10, -215)
+
+	textbox = pizzaarea:CreateEditBox("lol")
+	textbox:SetPoint('TOPLEFT', 10, -10)
+
+
+	--part4 = DBM_GUI_Frame:CreateArea(DBM_GUI_Language.General, 180, 180)
+	--part4.frame:SetPoint('TOPLEFT', 195, -215)
+
+--	DBM_GUI_Frame:CreateSlider
+
+	DBM_GUI_Aggro = DBM_GUI_Frame:CreateNewPanel("Aggro Alert")
+
+end
 
 
 
