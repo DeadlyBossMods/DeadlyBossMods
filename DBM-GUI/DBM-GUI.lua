f56444b734d8bc3f17b39b6bb1a5aa4b6b5bfd5e
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-08-15 22:46:21 +0000

    First GUI with Pizza Timer and localization files

--- DBM-GUI/DBM-GUI.lua
+++ DBM-GUI/DBM-GUI.lua
@@ -1,14 +1,42 @@
+--
+-- ***********************************************************
+-- This Software and all parts of it is fully written by 
+--
+-- 	Nitram @ EU-Azshara & Tandanu @ EU-Aegwin
+--
+-- Its not allowed to remove, modify this Author Information.
+-- ***********************************************************
+-- 
+-- The Deadly Boss Mods are formerly known as La Vendetta BossMods, 
+-- written by the Authors with all the Informations we got while 
+-- playing by La Vendetta (RIP).
+-- 
+-- Translations are written by some very helpfull guys, thanks to you.
+-- The Translators Information can be found in the Localisation file.
+--
+-- This DBM 4 Release is written for WOTLK (Lich King)
+-- 
+-- ***********************************************************
+-- Copyright Informations:
+--
+--   DBM is free of use without any warrenty. You can use it
+--   on each WoW Client you like, but it's not allowed to 
+--   copy&paste code from our Software. So please ask for 
+--   permisson if you want to use parts of this software for
+--   your own developments. 
+--   If you wan't to add DBM 4 for download, feel free to add 
+--   the Files on your website but please include a link to 
+--   www.deadlybossmods.com and leave the Authors information.
+--
+-- ***********************************************************
+-- Version History:
+--
+--   v4.00:  First Release for WOTLK Beta
+--
+--
 
 
-local DBM_GUI_Language = {}
-DBM_GUI_Language.MainFrame = "Deadly Boss Mods"
-DBM_GUI_Language.General = "General Options"
-DBM_GUI_Language.Enable = "enable"
-DBM_GUI_Language.Disable = "disable"
-
-
-
-local FrameTitle = "DBM_GUI_Option_"	-- all GUI Frames get automatically a name FrameTitle..ID
+local FrameTitle = "DBM_GUI_Option_"	-- all GUI Frames get automatical a name FrameTitle..ID
 
 local PanelPrototype = {}
 DBM_GUI = {}
@@ -55,7 +83,7 @@ function PanelPrototype:CreateArea(name, width, height)
 	local area = CreateFrame('Frame', FrameTitle..self:GetNewID(), self.frame, 'OptionFrameBoxTemplate')
 	area:SetBackdropBorderColor(0.4, 0.4, 0.4)
 	area:SetBackdropColor(0.15, 0.15, 0.15, 0.5)
-	getglobal(area:GetName() .. 'Title'):SetText(name)
+	getglobal(FrameTitle..self:GetCurrentID()..'Title'):SetText(name)
 	area:SetWidth(width)
 	area:SetHeight(height)
 		
@@ -77,10 +105,11 @@ function PanelPrototype:CreateDropdown(name)
 	text:SetText(name)
 	return dropdown
 end
-function PanelPrototype:CreateEditBox(text)
+function PanelPrototype:CreateEditBox(text, width, height)
 	local textbox = CreateFrame('EditBox', FrameTitle..self:GetNewID(), self.frame, 'DBM_GUI_FrameEditBoxTemplate')
 	getglobal(FrameTitle..self:GetCurrentID().."Text"):SetText(text)
-
+	textbox:SetWidth(width or 100)
+	textbox:SetHeight(height or 20)
 	return textbox
 end
 function PanelPrototype:CreateSlider(text, low, high, step)
@@ -100,39 +129,48 @@ function PanelPrototype:CreateSlider(text, low, high, step)
 	--slider.valText = text 
 	return slider
 end
+function PanelPrototype:CreateButton(title, width, height, onclick)
+	local button = CreateFrame('Button', FrameTitle..self:GetNewID(), self.frame, 'UIPanelButtonTemplate')
+	button:SetText(title)
+	button:SetWidth(width or 100)
+	button:SetHeight(height or 20)
 
+	return button
+end
 -- END - Basic GUI Items
 
 
-do 
-	DBM_GUI_Language.PizzaTimer = 'Create "Pizza" Timer'
-
-
+do
 	DBM_GUI_Frame = DBM_GUI:CreateNewPanel("Deadly Boss Mods", false)
 
-	part1 = DBM_GUI_Frame:CreateArea(DBM_GUI_Language.General, 180, 180)
-	part1.frame:SetPoint('TOPLEFT', 10, -20)
-
-	part2 = DBM_GUI_Frame:CreateArea(DBM_GUI_Language.General, 180, 180)
-	part2.frame:SetPoint('TOPLEFT', 195, -20)
-
-	pizzaarea = DBM_GUI_Frame:CreateArea(DBM_GUI_Language.PizzaTimer, 180, 180)
-	pizzaarea.frame:SetPoint('TOPLEFT', 10, -215)
-
-	textbox = pizzaarea:CreateEditBox("lol")
-	textbox:SetPoint('TOPLEFT', 10, -10)
+	local generaloptions = DBM_GUI_Frame:CreateArea(L.General, 180, 180)
+	generaloptions.frame:SetPoint('TOPLEFT', 10, -20)
 
 
-	--part4 = DBM_GUI_Frame:CreateArea(DBM_GUI_Language.General, 180, 180)
-	--part4.frame:SetPoint('TOPLEFT', 195, -215)
+
+
+	-- Pizza Timer (create your own timer menue)
+	local pizzaarea = DBM_GUI_Frame:CreateArea(L.PizzaTimer_Headline, 365, 85)
+	pizzaarea.frame:SetPoint('TOPLEFT', 10, -315)
+
+	local textbox = pizzaarea:CreateEditBox(L.PizzaTimer_Title, 140)
+	local hourbox = pizzaarea:CreateEditBox(L.PizzaTimer_Hours, 25)
+	local minbox  = pizzaarea:CreateEditBox(L.PizzaTimer_Mins, 25)
+	local secbox  = pizzaarea:CreateEditBox(L.PizzaTimer_Secs, 25)
+	local okbttn  = pizzaarea:CreateButton("OK", 53, 30);
+
+	textbox:SetPoint('TOPLEFT', 20, -20)
+	hourbox:SetPoint('TOPLEFT', textbox, "TOPRIGHT", 20, 0)
+	minbox:SetPoint('TOPLEFT', hourbox, "TOPRIGHT", 20, 0)
+	secbox:SetPoint('TOPLEFT', minbox, "TOPRIGHT", 20, 0)
+	okbttn:SetPoint('TOPLEFT', secbox, "TOPRIGHT", 7, 5)
+	-- END Pizza Timer
+
 
 --	DBM_GUI_Frame:CreateSlider
 
 	DBM_GUI_Aggro = DBM_GUI_Frame:CreateNewPanel("Aggro Alert")
 
 end
-
-
-
 
 
---
+++ DBM-GUI/localization.en.lua
@@ -0,0 +1,28 @@
+
+L = {}
+
+L.MainFrame = "Deadly Boss Mods"
+L.General = "General Options"
+L.Enable = "enable"
+L.Disable = "disable"
+
+L.PizzaTimer_Headline = 'Create a "Pizza" Timer'
+L.PizzaTimer_Title = 'Name (like "pizza is done")'
+L.PizzaTimer_Hours = "Hours"
+L.PizzaTimer_Mins = "Min"
+L.PizzaTimer_Secs = "Sec"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
