7c259e78d6060c8e54703a36b9290ca4db676a9c
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-09-04 01:28:27 +0000

    changed scale

--- trunk/DBM-TestMod/TestBoss.lua
+++ trunk/DBM-TestMod/TestBoss.lua
@@ -1,7 +1,7 @@
 local mod = DBM:NewMod("The Test Mod", "DBM-TestMod")
 local L = mod:GetLocalizedStrings()
 
-mod:SetModelScale(0.75)
+mod:SetModelScale(0.25)
 mod:SetModelOffset(0, 0, 0)
 mod:SetModelWalkSequence(4)
 mod:SetModelIdleSquences(16, 18, 2, 1)
