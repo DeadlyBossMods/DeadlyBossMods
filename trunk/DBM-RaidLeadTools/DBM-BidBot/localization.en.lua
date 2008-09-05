96b9db98898282e0516fb34fc57555f9dee36ce9
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-09-05 19:21:21 +0000

    Added first RaidLead Tool - BidBot

---
+++ trunk/DBM-RaidLeadTools/DBM-BidBot/localization.en.lua
@@ -0,0 +1,24 @@
+
+
+if not DBM_BidBot_Translations then DBM_BidBot_Translations = {} end
+if not DBM_BidBot_Translations["enGB"] then DBM_BidBot_Translations["enGB"] = {} end
+
+local L = DBM_GUI_Translation["enGB"]
+
+L.Prefix = "[BidBot]: "
+
+L.Whisper_Queue 		= "An other Auction is currenlty running. Your Item has been queued."
+L.Whisper_Bid_OK 		= "Your Bid of %d DKP was accepted."
+L.Whisper_InUse 		= "Currently you can Bid on %s."
+
+L.Message_StartBidding		= "Please Bid NOW for %s via whisper to %s! MinPrice: %d"
+L.Message_DoBidding		= "Time remaining for %s: %d Seconds."
+
+L.Message_ItemGoesTo		= "%s goes for %d Points to %s. Gz!"
+L.Message_NoBidMade		= "There where no Bid on %s."
+
+L.Message_Biddings		= "%d. %s bids %d DKP."
+L.Message_BiddingsVisible	= "%d of %d Bidders are visible."
+
+L.Disenchant			= "Disenchant"
+
