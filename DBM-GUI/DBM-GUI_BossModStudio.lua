adcf74d0cba75f9dd6771c308018084a2f6d511a
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-09-06 15:32:12 +0000

    start of BossModstudio

--- DBM-GUI/DBM-GUI_BossModStudio.lua
+++ DBM-GUI/DBM-GUI_BossModStudio.lua
@@ -28,12 +28,62 @@
 local L = {}
 
 L.TabCategory_BossModStudio = "BossMod Studio"
+L.AreaHead_CreateBossMod = "Main Informations of this new BossMod"
+L.BossName = "Name of the BossMod - like 'Hogger'"
+L.BossID = "Creature ID"
+L.BossLookup = "Take ID from Target"
 
+L.AreaHead_Pull = "Pull / Combat Detection"
+L.CombatFromYell = "Combat starts with a Yell"
+L.CombatAutoDetect = "Automatic Combat Detection"
+
+L.Min = "Min"
+L.Sec = "Sec"
+
+CurrentBossSetup = {
+	BossName = "",
+	CombatFromYell = false,
+	CombatAutoDetect = true,
+
+}
 
 BMS_Panel = DBM_GUI:CreateNewPanel(L.TabCategory_BossModStudio, "option")
 
---BMS_Panel
+do
+	-------------------------------------------------
+	--         Create a new BossMod                --
+	-------------------------------------------------
+	local BMS_Create = BMS_Panel:CreateArea(L.AreaHead_CreateBossMod, nil, 130, true)
 
+	local BossName = BMS_Create:CreateEditBox(L.BossName, "", 230)
+	BossName:SetPoint('TOPLEFT', 40, -25)
+	
+	local BossID = BMS_Create:CreateEditBox(L.BossID, "0", 75)
+	BossID:SetPoint('TOPLEFT', BossName, "BOTTOMLEFT", 0, -20)
+
+	local BossLookup = BMS_Create:CreateButton(L.BossLookup, 160, nil, function()
+		local guid = UnitGUID("target")
+		local cId = tonumber(guid:sub(9, 12), 16)
+		CurrentBossSetup.BossName = cId
+		BossID:SetText(cId)
+	end);
+	BossLookup:SetPoint('TOPLEFT', BossID, "TOPRIGHT", 5, 0)
+
+
+
+	local BMS_Pull = BMS_Panel:CreateArea(L.AreaHead_Pull, nil, 130, true)
+	
+	local CombatFromYell = BMS_Pull:CreateCheckButton(L.CombatFromYell, true, nil, CurrentBossSetup.CombatFromYell)
+	
+	local CombatAutoDetect = BMS_Pull:CreateCheckButton(L.CombatAutoDetect, true, nil, CurrentBossSetup.CombatAutoDetect)
+
+	local EnrageMin = BMS_Pull:CreateEditBox(L.Min, "", 30)
+	EnrageMin:SetPoint('TOPLEFT', CombatAutoDetect, "BOTTOMLEFT", 15, -20)
+
+	local EnrageSec = BMS_Pull:CreateEditBox(L.Sec, "", 30)
+	EnrageSec:SetPoint('TOPLEFT', EnrageMin, "TOPRIGHT", 20, 0)
+
+end
 
 
 
