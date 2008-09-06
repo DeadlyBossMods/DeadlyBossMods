62bdd7f0f7b08b10e14c31f61a640111368c36f3
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-09-06 22:21:13 +0000

    naxx update

--- trunk/DBM-Naxx-10/ConstructQuarter/Patchwerk.lua
+++ trunk/DBM-Naxx-10/ConstructQuarter/Patchwerk.lua
@@ -1,1 +1,37 @@
 local mod = DBM:NewMod("Patchwerk 10", "DBM-Naxx-10", 2)
+local L = mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision: 84 $"):sub(12, -3))
+mod:SetCreatureID(16028)
+mod:SetZone(GetAddOnMetadata("DBM-Naxx-10", "X-DBM-Mod-LoadZone"))
+
+mod:RegisterCombat("yell", L.yell1, L.yell2)
+
+mod:RegisterEvents(
+	"SPELL_DAMAGE",
+	"SPELL_MISSED"
+)
+
+mod:AddBoolOption("WarningHateful", false, "announce")
+local enrageTimer = mod:NewEnrageTimer(420)
+
+local function announceStrike(target, damage)
+	SendChatMessage(L.HatefulStrike:format(target, damage), "RAID")
+end
+
+function mod:OnCombatStart(delay)
+	enrageTimer:Start()
+end
+
+function mod:SPELL_DAMAGE(args)
+	if args.spellId == 28308 and self.Options.WarningHateful and DBM:GetRaidRank() >= 1 then
+		announceStrike(args.destName, args.amount or 0)
+	end
+end
+
+function mod:SPELL_MISSED(args)
+	if args.spellId == 28308 and self.Options.WarningHateful and DBM:GetRaidRank() >= 1 then
+		announceStrike(args.destName, getglobal("ACTION_SPELL_MISSED_"..(args.missType)) or "")
+	end	
+end
+
