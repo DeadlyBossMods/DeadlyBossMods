75a38d8e36b69ba9bbc920a5c0ef2cbd740844ab
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-09-09 14:23:17 +0000

    sapphiron adjustments

--- trunk/DBM-Naxx/ArachnidQuarter/Faerlina.lua
+++ trunk/DBM-Naxx/ArachnidQuarter/Faerlina.lua
@@ -20,7 +20,7 @@ local timerEmbrace			= mod:NewTimer(30, "TimerEmbrace", 28732)
 local embraceSpam = 0
 function mod:SPELL_CAST_SUCCESS(args)
 	if (args.spellId == 28732				-- Widow's Embrace (10)
-	or args.spellId == 54097)				-- Widow's Embrace (20)
+	or args.spellId == 54097)				-- Widow's Embrace (25)
 	and (GetTime() - embraceSpam) > 5 then  -- This spell is casted twice in Naxx 25 (bug?)
 		embraceSpam = GetTime()
 		warnEmbraceExpire:Cancel()
