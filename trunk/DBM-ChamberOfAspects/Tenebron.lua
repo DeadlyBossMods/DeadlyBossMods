b8418486a7e31e61a24f908d51407217bc75d58a
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-09-12 19:45:25 +0000

    chamber: added

---
+++ trunk/DBM-ChamberOfAspects/Tenebron.lua
@@ -0,0 +1,6 @@
+local mod = DBM:NewMod("Tenebron", "DBM-ChamberOfAspects")
+local L = mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision: 122 $"):sub(12, -3))
+mod:SetCreatureID(30452)
+mod:SetZone()
---
+++ trunk/DBM-ChamberOfAspects/Vesperon.lua
@@ -0,0 +1,6 @@
+local mod = DBM:NewMod("Vesperon", "DBM-ChamberOfAspects")
+local L = mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision: 122 $"):sub(12, -3))
+mod:SetCreatureID(30449)
+mod:SetZone()
