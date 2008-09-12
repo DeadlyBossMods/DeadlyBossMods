532e460950e6d4500776349ae4cc04433becb18f
Paul Emmerich <emmericp@net.in.tum.de>
2008-09-12 19:45:25 +0000

    chamber: added

---
+++ DBM-ChamberOfAspects/localization.en.lua
@@ -0,0 +1,40 @@
+local L
+
+---------------
+--  Shadron  --
+---------------
+L = DBM:GetModLocalization("Shadron")
+
+L:SetGeneralLocalization({
+	name = "Shadron"
+})
+
+
+---------------
+--  Tenebron  --
+---------------
+L = DBM:GetModLocalization("Tenebron")
+
+L:SetGeneralLocalization({
+	name = "Tenebron"
+})
+
+
+---------------
+--  Vesperon  --
+---------------
+L = DBM:GetModLocalization("Vesperon")
+
+L:SetGeneralLocalization({
+	name = "Vesperon"
+})
+
+
+---------------
+--  Sartharion  --
+---------------
+L = DBM:GetModLocalization("Sartharion")
+
+L:SetGeneralLocalization({
+	name = "Sartharion"
+})
