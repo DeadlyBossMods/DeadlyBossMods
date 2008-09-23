fa33afcf0255b0d2f279ae787bd7d09889c8871f
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-09-23 01:45:47 +0000

    stats update

--- trunk/DBM-GUI/localization.en.lua
+++ trunk/DBM-GUI/localization.en.lua
@@ -6,77 +6,76 @@ L.MainFrame = "Deadly Boss Mods"
 
 L.TabCategory_Options 	= "General Options"
 L.TabCategory_WOTLK 	= "Wrath of the Lich King"
-L.TabCategory_BC 		= "The Burning Crusade"
+L.TabCategory_BC 	= "The Burning Crusade"
 L.TabCategory_CLASSIC 	= "WoW Classic Bosses"
 L.TabCategory_OTHER     = "Other Boss Mods"
 
-L.BossModLoaded = "Please choose a Boss Mod from the list to the left"
-L.BossModLoad_now = "This Boss Mod is not loaded; it will be loaded when you enter the instance. You can also click the button to load the mod manually."
+L.BossModLoaded 	= "Please choose a Boss Mod from the list to the left"
+L.BossModLoad_now 	= "This Boss Mod is not loaded. It will be loaded when you enter the instance. You can also click the button to load the mod manually."
 
 L.PosX = 'Position X'
 L.PosY = 'Position Y'
 
-L.MoveMe = 'move me'
-L.Button_OK = 'OK'
-L.Button_Cancel = 'Cancel'
-L.Button_LoadMod = 'Load AddOn'
+L.MoveMe 		= 'move me'
+L.Button_OK 		= 'OK'
+L.Button_Cancel 	= 'Cancel'
+L.Button_LoadMod 	= 'Load AddOn'
+L.Reset 		= "reset"
 
-L.Enable = "enable"
+L.Enable  = "enable"
 L.Disable = "disable"
 
--- Tab: General Options
-L.General = "General Options"
-L.EnableDBM = "Enable DBM"
-L.EnableStatus = "Reply to 'status' whispers"
-L.AutoRespond = "Enable auto-respond while fighting"
+-- Tab: Boss Statistics
+L.BossStatistics	= "Boss Statistics"
+L.Statistic_Kills	= "Kills:"
+L.Statistic_Wipes	= "Wipes:"
+L.Statistic_BestKill	= "Best kill:"
+L.Statistic_Heroic	= "heroic"
 
-L.PizzaTimer_Headline = 'Create a "Pizza Timer"'
-L.PizzaTimer_Title = 'Name (e.g. "Pizza!")'
-L.PizzaTimer_Hours = "Hours"
-L.PizzaTimer_Mins = "Min"
-L.PizzaTimer_Secs = "Sec"
+-- Tab: General Options
+L.General 		= "General Options"
+L.EnableDBM 		= "Enable DBM"
+L.EnableStatus 		= "Reply to 'status' whispers"
+L.AutoRespond 		= "Enable auto-respond while fighting"
+
+L.PizzaTimer_Headline 	= 'Create a "Pizza Timer"'
+L.PizzaTimer_Title	= 'Name (e.g. "Pizza!")'
+L.PizzaTimer_Hours 	= "Hours"
+L.PizzaTimer_Mins 	= "Min"
+L.PizzaTimer_Secs 	= "Sec"
 L.PizzaTimer_ButtonStart = "Start Timer"
-L.PizzaTimer_BroadCast = "Broadcast to Raid"
+L.PizzaTimer_BroadCast	= "Broadcast to Raid"
 
 -- Tab: Raidwarning
-L.Tab_RaidWarning = "Raid Warnings"
-L.RaidWarnColors = "Raid Warning Colors"
-L.RaidWarnColor_1 = "Color 1"
-L.RaidWarnColor_2 = "Color 2"
-L.RaidWarnColor_3 = "Color 3"
-L.RaidWarnColor_4 = "Color 4"
-L.InfoRaidWarning = [[You can specify the position and colors of the raid warning frame. 
+L.Tab_RaidWarning 	= "Raid Warnings"
+L.RaidWarnColors 	= "Raid Warning Colors"
+L.RaidWarnColor_1 	= "Color 1"
+L.RaidWarnColor_2 	= "Color 2"
+L.RaidWarnColor_3 	= "Color 3"
+L.RaidWarnColor_4 	= "Color 4"
+L.InfoRaidWarning	= [[You can specify the position and colors of the raid warning frame. 
 This frame is used for messages like "Player X is afflicted by Y" messages]]
-
-L.ColorResetted = "The Color Setting of this field has been resetted"
-
-L.ShowWarningsInChat = "Show warnings in chatframe"
+L.ColorResetted 	= "The Color Setting of this field has been resetted"
+L.ShowWarningsInChat 	= "Show warnings in chatframe"
 L.ShowFakedRaidWarnings = "Show warnings as faked Raid Warning messages"
-L.WarningIconLeft = "Show icon on left side"
-L.WarningIconRight = "Show icon on right side"
-L.RaidWarnMessage = "Thanks for using DeadlyBossMods"
+L.WarningIconLeft 	= "Show icon on left side"
+L.WarningIconRight 	= "Show icon on right side"
+L.RaidWarnMessage 	= "Thanks for using DeadlyBossMods"
+L.BarWhileMove 		= "Raidwarn movable"
 
 -- Tab: Barsetup
-L.BarSetup = "Bar Style"
+L.BarSetup   = "Bar Style"
 L.BarTexture = "Bar Texture"
 
-L.Slider_BarOffSetX = "OffSet X"
-L.Slider_BarOffSetY = "OffSet Y"
-L.Slider_BarWidth = "Bar width"
-L.Slider_BarScale = "Bar scale"
-L.AreaTitle_BarSetup = "General Bar Options"
+L.Slider_BarOffSetX 	= "OffSet X"
+L.Slider_BarOffSetY 	= "OffSet Y"
+L.Slider_BarWidth 	= "Bar width"
+L.Slider_BarScale 	= "Bar scale"
+L.AreaTitle_BarSetup 	= "General Bar Options"
 L.AreaTitle_BarSetupSmall = "Small Bar Options"
 L.AreaTitle_BarSetupHuge = "Huge Bar Options"
-L.BarIconLeft = "Left Icon"
-L.BarIconRight = "Right Icon"
-L.Reset = "reset"
-
-
-L.TabInfoText_WOTLK = [[Wrath of the Lich King Boss Mods
-in this category you can find all supported instances starting
-with Utgarde and ending with all WotLK raid instances.
-If you have any wishes (...?)
-Best Regards and a lot of fun
-Your Deadly Boss Mods Team]]
+L.BarIconLeft 		= "Left Icon"
+L.BarIconRight 		= "Right Icon"
+L.EnableHugeBar 	= "Enable Huge Bar (Bar 2)"
 
 
