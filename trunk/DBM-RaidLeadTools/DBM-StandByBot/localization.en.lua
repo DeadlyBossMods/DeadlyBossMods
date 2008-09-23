e0adcf734a1f13bc58a451eff694974ea6e41612
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-09-23 22:25:27 +0000

    Standbybot grundzug entwickelt. Feintuning + Debugging muss noch gemacht werden

--- trunk/DBM-RaidLeadTools/DBM-StandByBot/localization.en.lua
+++ trunk/DBM-RaidLeadTools/DBM-StandByBot/localization.en.lua
@@ -2,5 +2,24 @@ DBM_StandbyBot_Translations = {}
 
 local L = DBM_StandbyBot_Translations
 
-L.Prefix = "[SB]: "
+L.LeftRaidGroup 	= "You have left our RaidGroup. Please don't forget to whisper me !sb if you wan't to be StandyBy."
+L.AddedSBUser		= "You are now Standby. Please stay available until required or removed from the SB List."
+L.UserIsAllreadySB	= "Sorry, but you are already Standby. To remove yourself from the List please type '!sb off' in reply."
+L.NotStandby		= "Sorry, you are currently not registerd as a Standby Member. Please type '!sb' in reply."
+L.NoLongerStandby	= "You are no longer Standby. Your were Standby for %d Hour(s) and %02d Minutes."
+L.PostStandybyList	= "Currently Standby:"
+
+L.Local_AddedPlayer	= "[SB]: %s is now Standby."
+L.Local_RemovedPlayer	= "[SB]: %s is no longer Standby."
+L.Local_CantRemove	= "Sorry, can't add player."
+
+L.Current_StandbyTime	= "StandbyTimes from %s:"
+L.DateTimeFormat	= "%c"
+
+-- GUI
+L.TabCategory_Standby	= "StandbyBot"
+L.AreaGeneral		= "Gernal StandbyBot Settings"
+L.Enable		= "Enable StandbyBot (!sb)"
+L.AreaStandbyHistory	= "Standby History"
+L.NoHistoryAvailable	= "Sorry, no saved Raids with Standby Players"
 
