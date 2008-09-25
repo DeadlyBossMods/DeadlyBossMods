e1576299011318026ab3a06178deac7a7eab9d65
Paul Emmerich <emmericp@net.in.tum.de>
2008-09-25 23:04:14 +0000

    more revision keywords...

--- DBM-ChamberOfAspects/Shadron.lua
+++ DBM-ChamberOfAspects/Shadron.lua
@@ -1,6 +1,6 @@
 local mod = DBM:NewMod("Shadron", "DBM-ChamberOfAspects")
 local L = mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 122 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(30451)
 mod:SetZone()
