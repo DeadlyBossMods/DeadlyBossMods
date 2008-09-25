fd541aad2f567cbc65258e089125b95aec409873
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-09-25 23:04:14 +0000

    more revision keywords...

--- trunk/DBM-ChamberOfAspects/Sartharion.lua
+++ trunk/DBM-ChamberOfAspects/Sartharion.lua
@@ -1,6 +1,6 @@
 local mod = DBM:NewMod("Sartharion", "DBM-ChamberOfAspects")
 local L = mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 122 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(28860)
 mod:SetZone()
