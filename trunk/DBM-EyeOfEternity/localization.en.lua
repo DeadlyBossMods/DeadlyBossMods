721fb92cf4e7759648abca4b788b9444e575b1bd
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-09-27 00:07:59 +0000

    eye of eternity warnings

--- trunk/DBM-EyeOfEternity/localization.en.lua
+++ trunk/DBM-EyeOfEternity/localization.en.lua
@@ -9,6 +9,29 @@ L:SetGeneralLocalization({
 	name = "Malygos"
 })
 
+L:SetWarningLocalization({
+	WarningSpark		= "Power Spark spawned",
+	WarningVortex		= "Vortex",
+	WarningBreathSoon	= "Deep Breath soon",
+	WarningBreath		= "Deep Breath!",
+})
+
+L:SetTimerLocalization({
+	TimerSpark	= "Next Spark",
+	TimerVortex	= "Vortex",
+	TimerBreath	= "Deep Breath",
+})
+
+L:SetOptionLocalization({
+	WarningSpark		= "Show Power Spark warning",
+	WarningVortex		= "Show Vortex warning",
+	WarningBreathSoon	= "Show Deep Breath pre-warning",
+	WarningBreath		= "Show Deep Breath warning",
+	TimerSpark			= "Show Power Spark timer",
+	TimerVortex			= "Show Vortex timer",
+	TimerBreath			= "Show Deep Breath timer",
+})
+
 L:SetMiscLocalization({
 	YellPull		= "My patience has reached its limit. I will be rid of you!",
 	EmoteSpark		= "A Power Spark forms from a nearby rift!",
