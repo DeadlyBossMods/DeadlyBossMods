5efed6726e87e3506c2fe284cd1b360903c9ded5
Paul Emmerich <emmericp@net.in.tum.de>
2008-11-17 23:10:09 +0000

    malygos fixes

--- DBM-EyeOfEternity/localization.en.lua
+++ DBM-EyeOfEternity/localization.en.lua
@@ -14,12 +14,15 @@ L:SetWarningLocalization({
 	WarningVortex		= "Vortex",
 	WarningBreathSoon	= "Deep Breath soon",
 	WarningBreath		= "Deep Breath!",
+	WarningSurge		= "Surge on >%s<",
+	WarningVortexSoon	= "Vortex soon"
 })
 
 L:SetTimerLocalization({
-	TimerSpark	= "Next Spark",
-	TimerVortex	= "Vortex",
-	TimerBreath	= "Deep Breath",
+	TimerSpark		= "Next Spark",
+	TimerVortex		= "Vortex",
+	TimerBreath		= "Deep Breath",
+	TimerVortexCD	= "Vortex Cooldown"
 })
 
 L:SetOptionLocalization({
@@ -27,9 +30,12 @@ L:SetOptionLocalization({
 	WarningVortex		= "Show Vortex warning",
 	WarningBreathSoon	= "Show Deep Breath pre-warning",
 	WarningBreath		= "Show Deep Breath warning",
+	WarningSurge		= "Show Surge of Power warning",
 	TimerSpark			= "Show Power Spark timer",
 	TimerVortex			= "Show Vortex timer",
 	TimerBreath			= "Show Deep Breath timer",
+	TimerVortexCD		= "Show Vortex Cooldown timer (inaccurate)",
+	WarningVortexSoon	= "Show Vortex pre-warning (inaccurate)"
 })
 
 L:SetMiscLocalization({
@@ -38,5 +44,6 @@ L:SetMiscLocalization({
 	YellPhase2		= "I had hoped to end your lives quickly",
 	EmoteBreath		= "%s takes a deep breath.",
 	YellBreath		= "You will not succeed while I draw breath!",
+	YellPhase3		= "Now your benefactors make their"
 })
 
