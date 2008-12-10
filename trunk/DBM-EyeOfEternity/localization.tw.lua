da945530e70151de72c9ffde6ac4748bd1457ba5
Azael <Azael@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-12-10 15:01:01 +0000

    fix typo mistake

--- trunk/DBM-EyeOfEternity/localization.tw.lua
+++ trunk/DBM-EyeOfEternity/localization.tw.lua
@@ -45,7 +45,7 @@ L:SetOptionLocalization({
 L:SetMiscLocalization({
 	YellPull		= "我的耐心到此為止了。我要親自消滅你們!",
 	EmoteSpark		= "一個力量火花從附近的裂縫中形成。",
-	YellPhase2		= "我原來只是想盡快結束你們的生命",
+	YellPhase2		= "我原本只是想盡快結束你們的生命",
 	EmoteBreath		= "只要我的龍息尚存，你們就毫無機會!",
 	YellPhase3		= "現在你們幕後的主使終於出現"
 })
