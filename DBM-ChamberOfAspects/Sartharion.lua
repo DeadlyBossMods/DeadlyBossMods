6529927d48780ae2cc878536cd86a3ae4d598442
Arta <Arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-12-12 20:52:11 +0000

    Sartharion.lua
    Fire Wall cooldown timer + 2sec pre-warning   (~30 sec cooldown)
    Add incoming timers (based on UNIT_DIED, the timer will start or not) -- start -> attackble = timer

--- DBM-ChamberOfAspects/Sartharion.lua
+++ DBM-ChamberOfAspects/Sartharion.lua
@@ -4,3 +4,62 @@ local L = mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(28860)
 mod:SetZone()
+
+mod:RegisterCombat("yell", L.Pull)
+
+mod:EnableModel()
+
+mod:RegisterEvents(
+	"CHAT_MSG_RAID_BOSS_EMOTE",
+	"CHAT_MSG_MONSTER_YELL",
+	"UNIT_DIED"
+)
+
+local warningWallSoon		= mod:NewAnnounce("WarningWallSoon", 3, 43113)
+local warningAdd		= mod:NewAnnounce("WarningAdd", 4, 59571)
+local timerAdd			= mod:NewTimer(15, "TimerAdd", 59571)  	
+local timerWall			= mod:NewTimer(30, "TimerWall", 43113)		
+
+local vesperon = 1
+local shadron = 1
+local tenebron = 1
+
+function mod:OnCombatStart(delay)
+	if vesperon then timerAdd:Start(30 - delay, "Vesperon") end
+	if shadron then timerAdd:Start(90 - delay, "Shadron") end
+	if tenebron then timerAdd:Start(120 - delay, "Tenebron") end
+	timerWall:Start(30 - delay)
+	warningWallSoon:Schedule(28 - delay)
+end
+
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, sender)
+	if sender == L.name then
+		warningWallSoon:Unschedule()
+		timerWall:Stop()
+		warningWallSoon:Schedule(28)
+		timerWall:Start()
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg, sender)
+	if msg:find(L.Vesperon) then
+		warningAdd:Show("Vesperon")
+	elseif msg:find(L.Shadron) then
+		warningAdd:Show("Shadron")
+	elseif msg:find(L.Tenebron) then
+		warningAdd:Show("Tenebron")
+	end
+end
+
+function mod:UNIT_DIED(args)
+	if bit.band(args.destGUID:sub(0, 5), 0x00F) == 3 then
+		local z = tonumber(args.destGUID:sub(9, 12), 16)
+		if z == 30449 then
+			vesperon = nil
+		elseif z == 30451 then
+			shadron = nil
+		elseif z == 30452 then
+			tenebron = nil
+		end
+	end
+end
--- DBM-ChamberOfAspects/localization.en.lua
+++ DBM-ChamberOfAspects/localization.en.lua
@@ -38,3 +38,28 @@ L = DBM:GetModLocalization("Sartharion")
 L:SetGeneralLocalization({
 	name = "Sartharion"
 })
+
+L:SetWarningLocalization({
+	WarningWallSoon		= "Fire Wall soon",
+	WarningAdd		= "%s incoming"
+})
+
+L:SetTimerLocalization({
+	TimerWall	= "Fire Wall cooldown",
+	TimerAdd	= "%s"
+})
+
+L:SetOptionLocalization({
+	WarningWallSoon		= "Show \"Fire Wall\" pre-warning",
+	WarningAdd		= "Shows a warning when Vesperon\Shadron\Tenebron is incoming",
+	TimerWall		= "Show \"Fire Wall\" cooldown timer",
+	TimerAdd		= "Shows an incoming timer for Vesperon\Shadron\Tenebron"
+})
+
+L:SetMiscLocalization({
+	Pull		= "It is my charge to watch over these eggs. I will see you burn before any harm comes to them!",
+	Wall		= "The lava surrounding %s churns!",
+	Vesperon	= "Vesperon, the clutch is in danger! Assist me!",
+	Shadron		= "Shadron! Come to me! All is at risk!",
+	Tenebron	= "Tenebron! The eggs are yours to protect as well!"
+})
