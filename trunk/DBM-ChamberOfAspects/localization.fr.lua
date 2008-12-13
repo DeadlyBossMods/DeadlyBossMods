7eb8d8a61449b5ce1771e731c767b8f521d125b3
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-12-13 22:20:35 +0000

    frFR update

---
+++ trunk/DBM-ChamberOfAspects/localization.fr.lua
@@ -0,0 +1,40 @@
+local L
+
+---------------
+--  Shadron  --
+---------------
+L = DBM:GetModLocalization("Shadron")
+
+L:SetGeneralLocalization({
+	name = "Obscuron"
+})
+
+
+---------------
+--  Tenebron  --
+---------------
+L = DBM:GetModLocalization("Tenebron")
+
+L:SetGeneralLocalization({
+	name = "Ténébron"
+})
+
+
+---------------
+--  Vesperon  --
+---------------
+L = DBM:GetModLocalization("Vesperon")
+
+L:SetGeneralLocalization({
+	name = "Vespéron"
+})
+
+
+---------------
+--  Sartharion  --
+---------------
+L = DBM:GetModLocalization("Sartharion")
+
+L:SetGeneralLocalization({
+	name = "Sartharion"
+})
