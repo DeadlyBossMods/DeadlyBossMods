90f807f74db5770037b69ee688a869bc4c2a886b
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-12-22 23:00:12 +0000

    ruRU update

--- trunk/DBM-EyeOfEternity/localization.ru.lua
+++ trunk/DBM-EyeOfEternity/localization.ru.lua
@@ -14,40 +14,40 @@ L:SetGeneralLocalization({
 L:SetWarningLocalization({
 	WarningSpark		= "Призыв Искры мощи",
 	WarningVortex		= "Воронка",
-	WarningBreathSoon	= "Скоро Дыхание Чар",
-	WarningBreath		= "Дыхание Чар!",
+	WarningBreathSoon	= "Скоро Дыхание чар",
+	WarningBreath		= "Дыхание чар!",
 	WarningSurge		= ">%s< в поле сверкающей энергии",
 	WarningVortexSoon	= "Скоро Воронка",
 	WarningSurgeYou		= "На вас аура Прилива мощи!"
 })
 
 L:SetTimerLocalization({
-	TimerSpark			= "Следующий призыв Искры мощи",
+	TimerSpark			= "Следующая Искра мощи",
 	TimerVortex			= "Воронка",
-	TimerBreath			= "Дыхание Чар",
+	TimerBreath			= "Дыхание чар",
 	TimerVortexCD		= "Перезарядка Воронки"
 })
 
 L:SetOptionLocalization({
-	WarningSpark		= "Показать предупреждение для призыва Искры мощи",
-	WarningVortex		= "Показать предупреждение для Воронки",
-	WarningBreathSoon	= "Показать пред-предупреждение Дыхания Чар",
-	WarningBreath		= "Показать предупреждение для Дыхания Чар",
-	WarningSurge		= "Показать предупреждение для Прилива мощи",
-	TimerSpark			= "Показать отсчет времени до призыва Искры мощи",
-	TimerVortex			= "Показать отсчет времени до Воронки",
-	TimerBreath			= "Показать отсчет времени до Дыхания Чар",
-	TimerVortexCD		= "Показать отсчет времени до перезарядки Воронки (неточно)",
-	WarningVortexSoon	= "Показать пред-предупреждение Воронки (неточно)",
+	WarningSpark		= "Объявлять появление Искры мощи",
+	WarningVortex		= "Объявлять появление Воронки",
+	WarningBreathSoon	= "Предупреждать о приближающемся Дыхании чар",
+	WarningBreath		= "Объявлять Дыхание чар",
+	WarningSurge		= "Объявлять Прилив мощи",
+	TimerSpark			= "Показать таймер призыва Искры мощи",
+	TimerVortex			= "Показать таймер Воронки",
+	TimerBreath			= "Показать таймер Дыхания чар",
+	TimerVortexCD		= "Показать таймер перезарядки Воронки (неточно)",
+	WarningVortexSoon	= "Предупреждать о скором появлении Воронки (неточно)",
 	WarningSurgeYou		= "Показать спец-предупреждение для Прилива мощи"
 })
 
 L:SetMiscLocalization({
-	YellPull			= "My patience has reached its limit. I will be rid of you!", -- correct this
-	EmoteSpark			= "A Power Spark forms from a nearby rift!", -- correct this
-	YellPhase2			= "I had hoped to end your lives quickly", -- correct this
-	EmoteBreath			= "%s takes a deep breath.", -- correct this
-	YellBreath			= "You will not succeed while I draw breath!", -- correct this
-	YellPhase3			= "Now your benefactors make their" -- correct this
+	YellPull			= "Все, моему терпению пришел конец. Я от тебя избавлюсь!",
+	EmoteSpark			= "Искра мощи появляется из ближайшей расселины!",
+	YellPhase2			= "Я рассчитывал быстро покончить с вами",
+	EmoteBreath			= "Малигос глубоко вздыхает.",
+	YellBreath			= "Тебе не добиться своего, пока бьется мое сердце!",
+	YellPhase3			= "А-а, вот и твои благодетели!"
 })
 
