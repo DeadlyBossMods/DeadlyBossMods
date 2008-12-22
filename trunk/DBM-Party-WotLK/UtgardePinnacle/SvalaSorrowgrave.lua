be0ce22f12a270a284a6441e90c215c1b04c08a4
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2008-12-22 23:38:09 +0000

    party mod fixes

--- trunk/DBM-Party-WotLK/UtgardePinnacle/SvalaSorrowgrave.lua
+++ trunk/DBM-Party-WotLK/UtgardePinnacle/SvalaSorrowgrave.lua
@@ -11,10 +11,12 @@ mod:RegisterEvents(
 	"SPELL_CAST_START"
 )
 
-local warningSword	= mod:NewAnnounce("WarningSword", 2, 48276)
+--local warningSword	= mod:NewAnnounce("WarningSword", 2, 48276) -- seems to be broken
+local warningSword	= mod:NewAnnounce((GetSpellInfo(48276)), 2, 48276) -- temporary replacement...just a simple warnung with the spell's name is better than "Unknown: Unknown!"
 
 function mod:SPELL_CAST_START(args)
 	if args.spellId == 48276 then
-		warningSword:Show(args.spellName, args.destName)
+--		warningSword:Show(args.spellName, args.destName) -- doesn't work?!
+		warningSword:Show()
 	end
 end
