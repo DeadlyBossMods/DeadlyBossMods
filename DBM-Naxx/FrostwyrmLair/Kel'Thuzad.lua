2057e69080f00cde5a7e72a2a2c3629cb1078fc6
Paul Emmerich <emmericp@net.in.tum.de>
2008-12-23 00:11:26 +0000

    fix-.-

--- DBM-Naxx/FrostwyrmLair/Kel'Thuzad.lua
+++ DBM-Naxx/FrostwyrmLair/Kel'Thuzad.lua
@@ -43,7 +43,7 @@ end
 function mod:AnnounceBlastTargets()
 	warnBlastTargets:Show(table.concat(frostBlastTargets, "< >"))
 	for i = #frostBlastTargets, 1, -1 do
-		self:SetIcon(args.destName, 9 - i, 4.5) 
+		self:SetIcon(frostBlastTargets[i], 9 - i, 4.5) 
 		frostBlastTargets[i] = nil
 	end
 end
