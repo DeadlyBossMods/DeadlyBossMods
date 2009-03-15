55fbb12b0e5f53e175ee541d382efadf1bfa65e5
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-03-15 22:09:08 +0000

    added basic bossmods

---
+++ DBM-Ulduar/Razorscale.lua
@@ -0,0 +1,21 @@
+local mod = DBM:NewMod("Razorscale", "DBM-Ulduar")
+local L = mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision: 679 $"):sub(12, -3))
+
+mod:SetZone()
+
+--mod:RegisterCombat("yell", L.YellPull)
+
+mod:RegisterEvents(
+)
+
+--mod:NewAnnounce("WarningSpark", 1, 59381)
+--mod:NewTimer(30, "TimerSpark", 59381)
+--mod:NewSpecialWarning("WarningSurgeYou")
+--mod:NewEnrageTimer(615)
+
+function mod:OnCombatStart(delay)
+end
+
+
