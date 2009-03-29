fcaf3ccfef35e8176809e5183e406d2830be71d4
Azael <Azael@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-03-29 09:55:34 +0000

    zhtw update

--- trunk/DBM-Battlegrounds/localization.tw.lua
+++ trunk/DBM-Battlegrounds/localization.tw.lua
@@ -10,17 +10,17 @@ local L
 L = DBM:GetModLocalization("Battlegrounds")
 
 L:SetGeneralLocalization({
-	name = "基本戰埸功能"
+	name 			= "基本戰埸功能"
 })
 
 L:SetTimerLocalization({
-	TimerInvite = "%s"
+	TimerInvite 		= "%s"
 })
 
 L:SetOptionLocalization({
-	ColorByClass	= "在得分視窗中設置玩家名為職業顏色",
-	ShowInviteTimer	= "顯示戰場組隊計時",
-	AutoSpirit	= "自動釋放靈魂"
+	ColorByClass		= "在得分視窗中設置玩家名為職業顏色",
+	ShowInviteTimer		= "顯示戰場組隊計時",
+	AutoSpirit		= "自動釋放靈魂"
 })
 
 
@@ -30,22 +30,22 @@ L:SetOptionLocalization({
 L = DBM:GetModLocalization("Arenas")
 
 L:SetGeneralLocalization({
-	name = "競技場"
+	name 			= "競技場"
 })
 
 L:SetTimerLocalization({
-	TimerStart	= "戰鬥即將開始",
-	TimerShadow	= "暗影之眼"
+	TimerStart		= "戰鬥即將開始",
+	TimerShadow		= "暗影之眼"
 })
 
 L:SetOptionLocalization({
-	TimerStart = "顯示開始計時器"
+	TimerStart 		= "顯示開始計時器"
 })
 
 L:SetMiscLocalization({
-	Start60 = "競技場戰鬥在1分鐘內開始!",
-	Start30 = "競技場戰鬥在30秒內開始!",
-	Start15 = "競技場戰鬥在30秒內開始!"
+	Start60 		= "競技場戰鬥在1分鐘內開始!",
+	Start30 		= "競技場戰鬥在30秒內開始!",
+	Start15 		= "競技場戰鬥在30秒內開始!"
 })
 
 ---------------
@@ -54,26 +54,26 @@ L:SetMiscLocalization({
 L = DBM:GetModLocalization("Alterac")
 
 L:SetGeneralLocalization({
-	name = "奧特蘭克山谷"
+	name 			= "奧特蘭克山谷"
 })
 
 L:SetTimerLocalization({
-	TimerStart = "戰鬥開始", 
-	TimerTower = "%s",
-	TimerGY = "%s",
+	TimerStart 		= "戰鬥開始", 
+	TimerTower 		= "%s",
+	TimerGY 		= "%s",
 })
 
 L:SetMiscLocalization({
-	BgStart60 = "奧特蘭克山谷一分鐘後開始戰鬥。",
-	BgStart30 = "奧特蘭克山谷30秒後開始戰鬥。",
-	ZoneName = "奧特蘭克山谷",
+	BgStart60 		= "奧特蘭克山谷一分鐘後開始戰鬥。",
+	BgStart30 		= "奧特蘭克山谷30秒後開始戰鬥。",
+	ZoneName 		= "奧特蘭克山谷",
 })
 
 L:SetOptionLocalization({
-	TimerStart  = "顯示開始計時器",
-	TimerTower = "顯示奪取哨塔計時器",
-	TimerGY = "顯示奪取墓地計時器",
-	AutoTurnIn = "自動繳交任務物品"
+	TimerStart  		= "顯示開始計時器",
+	TimerTower 		= "顯示奪取哨塔計時器",
+	TimerGY 		= "顯示奪取墓地計時器",
+	AutoTurnIn 		= "自動繳交任務物品"
 })
 
 ---------------
@@ -82,32 +82,32 @@ L:SetOptionLocalization({
 L = DBM:GetModLocalization("Arathi")
 
 L:SetGeneralLocalization({
-	name = "阿拉希盆地"
+	name 			= "阿拉希盆地"
 })
 
 L:SetMiscLocalization({
-	BgStart60 = "阿拉希盆地的戰鬥將在1分鐘後開始。",
-	BgStart30 = "阿拉希盆地的戰鬥將在30秒後開始。",
-	ZoneName = "阿拉希盆地",
-	ScoreExpr = "(%d+)/2000",
-	Alliance = "聯盟",
-	Horde = "部落",
-	WinBarText = "%s 勝利",
-	BasesToWin = "勝利需要基地: %d",
-	Flag = "旗幟"
+	BgStart60 		= "阿拉希盆地的戰鬥將在1分鐘後開始。",
+	BgStart30 		= "阿拉希盆地的戰鬥將在30秒後開始。",
+	ZoneName 		= "阿拉希盆地",
+	ScoreExpr 		= "(%d+)/2000",
+	Alliance 		= "聯盟",
+	Horde 			= "部落",
+	WinBarText 		= "%s 勝利",
+	BasesToWin 		= "勝利需要基地: %d",
+	Flag 			= "旗幟"
 })
 
 L:SetTimerLocalization({
-	TimerStart = "戰鬥開始", 
-	TimerCap = "%s",
+	TimerStart 		= "戰鬥開始", 
+	TimerCap 		= "%s",
 })
 
 L:SetOptionLocalization({
-	TimerStart  = "顯示開始計時器",
-	TimerWin = "顯示勝利計時器",
-	TimerCap = "顯示奪取計時器",
-	ShowAbEstimatedPoints = "顯示戰鬥結束時雙方資源預計值",
-	ShowAbBasesToWin = "顯示勝利需要的基地數量"
+	TimerStart  		= "顯示開始計時器",
+	TimerWin 		= "顯示勝利計時器",
+	TimerCap 		= "顯示奪取計時器",
+	ShowAbEstimatedPoints 	= "顯示戰鬥結束時雙方資源預計值",
+	ShowAbBasesToWin 	= "顯示勝利需要的基地數量"
 })
 
 -----------------------
@@ -116,34 +116,34 @@ L:SetOptionLocalization({
 L = DBM:GetModLocalization("EyeOfTheStorm")
 
 L:SetGeneralLocalization({
-	name = "暴風之眼"
+	name 			= "暴風之眼"
 })
 
 L:SetMiscLocalization({
-	BgStart60 = "戰鬥在1分鐘內開始!",
-	BgStart30 = "戰鬥在30秒內開始!",
-	ZoneName = "暴風之眼",
-	ScoreExpr = "(%d+)/2000",
-	Alliance = "聯盟",
-	Horde = "部落",
-	WinBarText = "%s 勝利",
-	FlagReset = "旗幟已重置!",
-	FlagTaken = "(.+)已經奪走了旗幟!",
-	FlagCaptured = "(.+)已奪得旗幟!",
-	FlagDropped = "旗幟已經掉落!",
+	BgStart60 		= "戰鬥在1分鐘內開始!",
+	BgStart30 		= "戰鬥在30秒內開始!",
+	ZoneName 		= "暴風之眼",
+	ScoreExpr 		= "(%d+)/2000",
+	Alliance 		= "聯盟",
+	Horde 			= "部落",
+	WinBarText 		= "%s 勝利",
+	FlagReset 		= "旗幟已重置!",
+	FlagTaken 		= "(.+)已經奪走了旗幟!",
+	FlagCaptured 		= "(.+)已奪得旗幟!",
+	FlagDropped 		= "旗幟已經掉落!",
 
 })
 
 L:SetTimerLocalization({
-	TimerStart = "戰鬥開始", 
-	TimerFlag = "旗幟重生",
+	TimerStart 		= "戰鬥開始", 
+	TimerFlag 		= "旗幟重生",
 })
 
 L:SetOptionLocalization({
-	TimerStart  = "顯示開始計時器",
-	TimerWin = "顯示勝利計時器",
-	TimerFlag = "顯示旗幟重生計時器",
-	ShowPointFrame = "顯示旗幟攜帶者和預計勝利方",
+	TimerStart  		= "顯示開始計時器",
+	TimerWin 		= "顯示勝利計時器",
+	TimerFlag 		= "顯示旗幟重生計時器",
+	ShowPointFrame 		= "顯示旗幟攜帶者和預計勝利方",
 })
 
 --------------------
@@ -152,34 +152,34 @@ L:SetOptionLocalization({
 L = DBM:GetModLocalization("Warsong")
 
 L:SetGeneralLocalization({
-	name = "戰歌峽谷"
+	name 			= "戰歌峽谷"
 })
 
 L:SetMiscLocalization({
-	BgStart60 = "戰歌峽谷戰鬥將在1分鐘內開始。",
-	BgStart30 = "戰歌峽谷戰鬥將在30秒鐘內開始。做好準備!",
-	ZoneName = "戰歌峽谷",
-	Alliance = "聯盟",
-	Horde = "部落",	
-	InfoErrorText = "當你離開戰鬥後，旗幟框架將會回復。",
-	ExprFlagPickUp = "(.+)的旗幟被(.+)拔掉了!",
-	ExprFlagCaptured = "(.+)佔據了(.+)的旗幟!",
-	ExprFlagReturn = "(.+)的旗幟被(.+)還到了它的基地",
-	FlagAlliance = "聯盟旗幟: ",
-	FlagHorde = "部落旗幟: ",
-	FlagBase = "基地",
+	BgStart60 		= "戰歌峽谷戰鬥將在1分鐘內開始。",
+	BgStart30 		= "戰歌峽谷戰鬥將在30秒鐘內開始。做好準備!",
+	ZoneName 		= "戰歌峽谷",
+	Alliance 		= "聯盟",
+	Horde 			= "部落",	
+	InfoErrorText 		= "當你離開戰鬥後，旗幟框架將會回復。",
+	ExprFlagPickUp 		= "(.+)的旗幟被(.+)拔掉了!",
+	ExprFlagCaptured 	= "(.+)佔據了(.+)的旗幟!",
+	ExprFlagReturn 		= "(.+)的旗幟被(.+)還到了它的基地",
+	FlagAlliance 		= "聯盟旗幟: ",
+	FlagHorde 		= "部落旗幟: ",
+	FlagBase 		= "基地",
 })
 
 L:SetTimerLocalization({
-	TimerStart = "戰鬥開始", 
-	TimerFlag = "旗幟重生",
+	TimerStart 		= "戰鬥開始", 
+	TimerFlag 		= "旗幟重生",
 })
 
 L:SetOptionLocalization({
-	TimerStart  = "顯示開始計時器",
-	TimerWin = "顯示勝利計時器",
-	TimerFlag = "顯示旗幟重生計時器",
-	ShowFlagCarrier = "顯示旗幟攜帶者",
+	TimerStart  		= "顯示開始計時器",
+	TimerWin 		= "顯示勝利計時器",
+	TimerFlag 		= "顯示旗幟重生計時器",
+	ShowFlagCarrier 	= "顯示旗幟攜帶者",
 	ShowFlagCarrierErrorNote = "當正在戰鬥時顯示旗幟錯誤提示。",
 })
 
@@ -192,24 +192,24 @@ L:SetOptionLocalization({
 L = DBM:GetModLocalization("Archavon")
 
 L:SetGeneralLocalization({
-	name = "亞夏梵"
+	name 			= "『石之看守者』亞夏梵"
 })
 
 L:SetWarningLocalization({
-	WarningShards	= "岩石裂片: >%s<",
+	WarningShards		= "岩石裂片: >%s<",
 	WarningGrab		= "亞夏梵撞擊 >%s<"
 })
 
 L:SetTimerLocalization({
-	TimerShards = "岩石裂片: %s"
+	TimerShards 		= "岩石裂片: %s"
 })
 
 L:SetMiscLocalization({
-	TankSwitch = "%%ss撲向(%S+)!"
+	TankSwitch 		= "%%ss撲向(%S+)!"
 })
 
 L:SetOptionLocalization({
-	TimerShards = "顯示岩石裂片計時器",
-	WarningShards = "顯示岩石裂片警告",
-	WarningGrab = "顯示坦克撞擊警告"
+	TimerShards 		= "顯示岩石裂片計時器",
+	WarningShards 		= "顯示岩石裂片警告",
+	WarningGrab 		= "顯示坦克撞擊警告"
 })
