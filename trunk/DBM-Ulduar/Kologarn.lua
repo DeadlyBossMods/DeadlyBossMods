7e7372508a995b58770d1dfdec923231ec2e8572
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-04-01 18:28:08 +0000

    fixed properties

--- trunk/DBM-Ulduar/Kologarn.lua
+++ trunk/DBM-Ulduar/Kologarn.lua
@@ -1,7 +1,7 @@
 local mod = DBM:NewMod("Kologarn", "DBM-Ulduar")
 local L = mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 679 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 
 mod:SetZone()
 
