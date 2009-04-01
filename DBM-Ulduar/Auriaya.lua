ffd9a6cbecfb34c0b8b6cdc79df9bc7ef998322b
Paul Emmerich <emmericp@net.in.tum.de>
2009-04-01 18:28:08 +0000

    fixed properties

--- DBM-Ulduar/Auriaya.lua
+++ DBM-Ulduar/Auriaya.lua
@@ -1,7 +1,7 @@
 local mod = DBM:NewMod("Auriaya", "DBM-Ulduar")
 local L = mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 679 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 
 mod:SetZone()
 
--- DBM-Ulduar/Mimiron.lua
+++ DBM-Ulduar/Mimiron.lua
@@ -1,7 +1,7 @@
 local mod = DBM:NewMod("Mimiron", "DBM-Ulduar")
 local L = mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 694 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(33350)
 mod:SetZone()
 
