c4a330149ae0980ff7816035f97448d3134463ca
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-04-06 02:19:56 +0000

    added Razorscale mod (quite simple, don't know what to add there ... its much to simple at all ^^

--- DBM-Ulduar/Razorscale.lua
+++ DBM-Ulduar/Razorscale.lua
@@ -5,17 +5,40 @@ mod:SetRevision(("$Revision$"):sub(12, -3))
 
 mod:SetZone()
 
---mod:RegisterCombat("yell", L.YellPull)
+-- freely created by provided informations from
+-- http://www.mmo-champion.com/index.php?page=840
+-- this mod issn't fully working and finished - please provide combatlogs
+
+mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_CAST_START",
+	"SPELL_AURA_APPLIED",
 )
 
---mod:NewAnnounce("WarningSpark", 1, 59381)
---mod:NewTimer(30, "TimerSpark", 59381)
---mod:NewSpecialWarning("WarningSurgeYou")
---mod:NewEnrageTimer(615)
+local specWarnDevouringFlame		= mod:NewSpecialWarning("SpecWarnDevouringFlame")
+local timerDeepBreathCooldown		= mod:NewTimer(21, "timerDeepBreathCooldown", 64021)
+local timerDeepBreathCast		= mod:NewTimer(2.5, "timerDeepBreathCast", 64021)
+
+mod:AddBoolOption("PlaySoundOnDevouringFlame", true, "announce")
+
 
 function mod:OnCombatStart(delay)
 end
 
+function mod:SPELL_AURA_APPLIED(args)
+	if args.spellId == 63816 and args.destName == UnitName("player") then		-- you stay in the Devouring Flame (some shit on the ground ^^)
+		specWarnDevouringFlame:Show()
+		if self.Options.PlaySoundOnDevouringFlame then
+			PlaySoundFile("Sound\\Creature\\HoodWolf\\HoodWolfTransformPlayer01.wav")
+		end		
+	end
+end
+
+function mod:SPELL_CAST_START(args)
+	if args.spellId == 64021 then	-- deep breath
+		timerDeepBreathCast:Start()
+		timerDeepBreathCooldown:Start()
+	end
+end
 
