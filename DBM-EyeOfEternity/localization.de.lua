6f97a7c27de84742697b23bfa89f0c4983171fe1
Paul Emmerich <emmericp@net.in.tum.de>
2009-04-14 14:23:28 +0000

    malygos deDE

--- DBM-EyeOfEternity/localization.de.lua
+++ DBM-EyeOfEternity/localization.de.lua
@@ -8,46 +8,45 @@ local L
 L = DBM:GetModLocalization("Malygos")
 
 L:SetGeneralLocalization({
-	name = "Malygos"
+   name = "Malygos"
 })
 
 L:SetWarningLocalization({
-	WarningSpark		= "Energiefunke da!",
-	WarningVortex		= "Vortex",
-	WarningBreathSoon	= "Tiefer Atem bald",
-	WarningBreath		= "Tiefer Atem!",
-	WarningSurge		= "Kraftsog auf >%s<",
-	WarningVortexSoon	= "Vortex bald",
-	WarningSurgeYou		= "Kraftsog auf dir!"
+   WarningSpark      = "Energiefunke da!",
+   WarningVortex      = "Vortex",
+   WarningBreathSoon   = "Tiefer Atem bald",
+   WarningBreath      = "Tiefer Atem!",
+   WarningSurge      = "Kraftsog auf >%s<",
+   WarningVortexSoon   = "Vortex bald",
+   WarningSurgeYou      = "Kraftsog auf dir!"
 })
 
 L:SetTimerLocalization({
-	TimerSpark		= "Energiefunke",
-	TimerVortex		= "Vortex",
-	TimerBreath		= "Tiefer Atem",
-	TimerVortexCD	= "Vortex Cooldown"
+   TimerSpark      = "Energiefunke",
+   TimerVortex      = "Vortex",
+   TimerBreath      = "Tiefer Atem",
+   TimerVortexCD   = "Vortex Cooldown"
 })
 
 L:SetOptionLocalization({
-	WarningSpark		= "Warnung fÃ¼r Energiefunke anzeigen",
-	WarningVortex		= "Warnung fÃ¼r Vortex anzeigen",
-	WarningBreathSoon	= "Vorwarnung fÃ¼r Tiefen Atem anzeigen",
-	WarningBreath		= "Warnung fÃ¼r Tiefen Atem anzeigen",
-	WarningSurge		= "Warnung fÃ¼r Kraftsog anzeigen",
-	TimerSpark			= "Timer fÃ¼r Energiefunke anzeigen",
-	TimerVortex			= "Timer fÃ¼r Vortex anzeigen",
-	TimerBreath			= "Timer fÃ¼r Tiefen Atem anzeigen",
-	TimerVortexCD		= "Timer fÃ¼r Vortex Cooldown anzeigen (ungenau)",
-	WarningVortexSoon	= "Vorwarnung fÃ¼r Vortex anzeigen (ungenau)",
-	WarningSurgeYou		= "Spezial Warnung anzeigen, wenn du von Kraftsog betroffen bist"
+   WarningSpark      = "Warnung für Energiefunke anzeigen",
+   WarningVortex      = "Warnung für Vortex anzeigen",
+   WarningBreathSoon   = "Vorwarnung für Tiefen Atem anzeigen",
+   WarningBreath      = "Warnung für Tiefen Atem anzeigen",
+   WarningSurge      = "Warnung für Kraftsog anzeigen",
+   TimerSpark         = "Timer für Energiefunke anzeigen",
+   TimerVortex         = "Timer für Vortex anzeigen",
+   TimerBreath         = "Timer für Tiefen Atem anzeigen",
+   TimerVortexCD      = "Timer für Vortex Cooldown anzeigen (ungenau)",
+   WarningVortexSoon   = "Vorwarnung für Vortex anzeigen (ungenau)",
+   WarningSurgeYou      = "Spezial Warnung anzeigen, wenn du von Kraftsog betroffen bist"
 })
 
-L:SetMiscLocalization({ -- todo!
-	YellPull		= "My patience has reached its limit. I will be rid of you!",
-	EmoteSpark		= "A Power Spark forms from a nearby rift!",
-	YellPhase2		= "I had hoped to end your lives quickly",
-	EmoteBreath		= "%s takes a deep breath.",
-	YellBreath		= "You will not succeed while I draw breath!",
-	YellPhase3		= "Now your benefactors make their"
+L:SetMiscLocalization({
+   YellPull      = "Meine Geduld ist am Ende. Ich werde mich eurer entledigen!",
+   EmoteSpark      = "Ein Energiefunke bildet sich aus einem nahegelegenen Graben!",
+   YellPhase2      = "Ich hatte gehofft, eure Leben schnell zu beenden, doch ihr zeigt euch... hartnäckiger als erwartet.",
+   EmoteBreath      = "%s holt tief Luft.",
+   YellBreath      = "Solange ich atme, werdet ihr nicht obsiegen!",
+   YellPhase3      = "Eure Wohltäter sind eingetroffen, doch sie kommen zu spät! Die hier gespeicherten Energien reichen aus, die Welt zehnmal zu zerstören. Was, denkt ihr, werden sie mit euch machen?"
 })
-
