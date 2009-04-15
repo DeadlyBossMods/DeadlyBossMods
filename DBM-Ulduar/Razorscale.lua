211da87076ad363653fb5021ba383291d97eae17
Paul Emmerich <emmericp@net.in.tum.de>
2009-04-15 18:15:20 +0000

    razorscale update

--- DBM-Ulduar/Razorscale.lua
+++ DBM-Ulduar/Razorscale.lua
@@ -14,9 +14,12 @@ mod:RegisterEvents(
 	"SPELL_AURA_APPLIED"
 )
 
-local specWarnDevouringFlame		= mod:NewSpecialWarning("SpecWarnDevouringFlame")
-local timerDeepBreathCooldown		= mod:NewTimer(21, "timerDeepBreathCooldown", 64021)
+local specWarnDevouringFlame	= mod:NewSpecialWarning("SpecWarnDevouringFlame")
+local timerDeepBreathCooldown	= mod:NewTimer(21, "timerDeepBreathCooldown", 64021)
 local timerDeepBreathCast		= mod:NewTimer(2.5, "timerDeepBreathCast", 64021)
+local timerAllTurretsReady		= mod:NewTimer(115, "timerAllTurretsReady") -- todo: icon?
+local warnTurretsReadySoon		= mod:NewAnnounce("warnTurretsReadySoon", 1)
+local warnTurretsReady			= mod:NewAnnounce("warnTurretsReady", 3)
 
 mod:AddBoolOption("PlaySoundOnDevouringFlame", false, "announce")
 
@@ -25,11 +28,19 @@ function mod:OnCombatStart(delay)
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 63816 and args.destName == UnitName("player") then		-- you are standing in Devouring Flame (some flame stuff on the ground)
+	if args.spellId == 63816 and args.destName == UnitName("player") then		-- you are standing in Devouring Flame
 		specWarnDevouringFlame:Show()
 		if self.Options.PlaySoundOnDevouringFlame then
 			PlaySoundFile("Sound\\Creature\\HoodWolf\\HoodWolfTransformPlayer01.wav")
 		end		
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg, mob)
+	if msg == L.YellAir then
+		timerAllTurretsReady:Start()
+		warnTurretsReadySoon:Schedule(95)
+		warnTurretsReady:Schedule(115)
 	end
 end
 
