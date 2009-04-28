aa8bec9356b5ceb5d0c280ee2d6a128b2dc0ca40
Paul Emmerich <emmericp@net.in.tum.de>
2009-04-28 10:58:04 +0000

    ruRU fix

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -33,6 +33,7 @@
 --    * Share Alike. If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one.
 --
 
+
 -------------------------------
 --  Globals/Default Options  --
 -------------------------------
