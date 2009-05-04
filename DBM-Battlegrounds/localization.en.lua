ff0ba59cc6d0f2bee5c7b48aae8d797df7944101
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-05-04 09:24:46 +0000

    fixed Arena Invite timer (Easter Kingdom bug, thanks to blizzard...)

--- DBM-Battlegrounds/localization.en.lua
+++ DBM-Battlegrounds/localization.en.lua
@@ -19,6 +19,10 @@ L:SetOptionLocalization({
 	AutoSpirit	= "Auto-release spirit"
 })
 
+L:SetMiscLocalization({
+	ArenaInvite	= "Arena invite"
+})
+
 
 --------------
 --  Arenas  --
@@ -35,13 +39,14 @@ L:SetTimerLocalization({
 })
 
 L:SetOptionLocalization({
-	TimerStart = "Show start timer"
+	TimerStart	= "Show start timer",
+	TimerShadow 	= "Show timer for Shadow Crystals"
 })
 
 L:SetMiscLocalization({
-	Start60 = "One minute until the Arena battle begins!",
-	Start30 = "Thirty seconds until the Arena battle begins!",
-	Start15 = "Fifteen seconds until the Arena battle begins!"
+	Start60		= "One minute until the Arena battle begins!",
+	Start30		= "Thirty seconds until the Arena battle begins!",
+	Start15		= "Fifteen seconds until the Arena battle begins!"
 })
 
 ---------------
@@ -54,22 +59,22 @@ L:SetGeneralLocalization({
 })
 
 L:SetTimerLocalization({
-	TimerStart = "Game starts", 
-	TimerTower = "%s",
-	TimerGY = "%s",
+	TimerStart		= "Game starts", 
+	TimerTower		= "%s",
+	TimerGY			= "%s",
 })
 
 L:SetMiscLocalization({
-	BgStart60 = "1 minute until the battle for Alterac Valley begins.",
-	BgStart30 = "30 seconds until the battle for Alterac Valley begins.",
-	ZoneName = "Alterac Valley",
+	BgStart60		= "1 minute until the battle for Alterac Valley begins.",
+	BgStart30		= "30 seconds until the battle for Alterac Valley begins.",
+	ZoneName		= "Alterac Valley",
 })
 
 L:SetOptionLocalization({
-	TimerStart  = "Show start timer",
-	TimerTower = "Show tower capture timer",
-	TimerGY = "Show graveyard capture timer",
-	AutoTurnIn = "Auto turn-in quests"
+	TimerStart		= "Show start timer",
+	TimerTower		= "Show tower capture timer",
+	TimerGY			= "Show graveyard capture timer",
+	AutoTurnIn		= "Auto turn-in quests"
 })
 
 ---------------
@@ -82,28 +87,28 @@ L:SetGeneralLocalization({
 })
 
 L:SetMiscLocalization({
-	BgStart60 = "The Battle for Arathi Basin will begin in 1 minute.",
-	BgStart30 = "The Battle for Arathi Basin will begin in 30 seconds.",
-	ZoneName = "Arathi Basin",
-	ScoreExpr = "(%d+)/2000",
-	Alliance = "Alliance",
-	Horde = "Horde",
-	WinBarText = "%s wins",
-	BasesToWin = "Bases to win: %d",
-	Flag = "Flag"
+	BgStart60		= "The Battle for Arathi Basin will begin in 1 minute.",
+	BgStart30		= "The Battle for Arathi Basin will begin in 30 seconds.",
+	ZoneName 		= "Arathi Basin",
+	ScoreExpr 		= "(%d+)/2000",
+	Alliance 		= "Alliance",
+	Horde 			= "Horde",
+	WinBarText 		= "%s wins",
+	BasesToWin 		= "Bases to win: %d",
+	Flag 			= "Flag"
 })
 
 L:SetTimerLocalization({
-	TimerStart = "Game starts", 
-	TimerCap = "%s",
+	TimerStart 		= "Game starts", 
+	TimerCap 		= "%s",
 })
 
 L:SetOptionLocalization({
-	TimerStart  = "Show start timer",
-	TimerWin = "Show win timer",
-	TimerCap = "Show capture timer",
-	ShowAbEstimatedPoints = "Show estimated points on win/lose",
-	ShowAbBasesToWin = "Show bases required to win"
+	TimerStart  		= "Show start timer",
+	TimerWin 		= "Show win timer",
+	TimerCap 		= "Show capture timer",
+	ShowAbEstimatedPoints	= "Show estimated points on win/lose",
+	ShowAbBasesToWin	= "Show bases required to win"
 })
 
 -----------------------
@@ -116,30 +121,30 @@ L:SetGeneralLocalization({
 })
 
 L:SetMiscLocalization({
-	BgStart60 = "The battle begins in 1 minute!",
-	BgStart30 = "The battle begins in 30 seconds!",
-	ZoneName = "Eye of the Storm",
-	ScoreExpr = "(%d+)/2000",
-	Alliance = "Alliance",
-	Horde = "Horde",
-	WinBarText = "%s wins",
-	FlagReset = "The flag has been reset!",
-	FlagTaken = "(.+) has taken the flag!",
-	FlagCaptured = "The .+ ha%w+ captured the flag!",
-	FlagDropped = "The flag has been dropped!",
+	BgStart60		= "The battle begins in 1 minute!",
+	BgStart30		= "The battle begins in 30 seconds!",
+	ZoneName		= "Eye of the Storm",
+	ScoreExpr		= "(%d+)/2000",
+	Alliance 		= "Alliance",
+	Horde 			= "Horde",
+	WinBarText 		= "%s wins",
+	FlagReset 		= "The flag has been reset!",
+	FlagTaken 		= "(.+) has taken the flag!",
+	FlagCaptured 		= "The .+ ha%w+ captured the flag!",
+	FlagDropped 		= "The flag has been dropped!",
 
 })
 
 L:SetTimerLocalization({
-	TimerStart = "Game starts", 
-	TimerFlag = "Flag respawn",
+	TimerStart 		= "Game starts", 
+	TimerFlag 		= "Flag respawn",
 })
 
 L:SetOptionLocalization({
-	TimerStart  = "Show start timer",
-	TimerWin = "Show win timer",
-	TimerFlag = "Show flag respawn timer",
-	ShowPointFrame = "Show flag carrier and estimated points",
+	TimerStart  		= "Show start timer",
+	TimerWin 		= "Show win timer",
+	TimerFlag 		= "Show flag respawn timer",
+	ShowPointFrame 		= "Show flag carrier and estimated points",
 })
 
 --------------------
@@ -152,31 +157,31 @@ L:SetGeneralLocalization({
 })
 
 L:SetMiscLocalization({
-	BgStart60 = "The battle for Warsong Gulch begins in 1 minute.",
-	BgStart30 = "The battle for Warsong Gulch begins in 30 seconds. Prepare yourselves!",
-	ZoneName = "Warsong Gulch",
-	Alliance = "Alliance",
-	Horde = "Horde",	
-	InfoErrorText = "The flag carrier targeting function will be restored when you are out of combat.",
-	ExprFlagPickUp = "The (%w+) .lag was picked up by (.+)!",
-	ExprFlagCaptured = "(.+) captured the (%w+) flag!",
-	ExprFlagReturn = "The (%w+) .lag was returned to its base by (.+)!",
-	FlagAlliance = "Alliance Flag: ",
-	FlagHorde = "Horde Flag: ",
-	FlagBase = "Base",
+	BgStart60 			= "The battle for Warsong Gulch begins in 1 minute.",
+	BgStart30 			= "The battle for Warsong Gulch begins in 30 seconds. Prepare yourselves!",
+	ZoneName 			= "Warsong Gulch",
+	Alliance 			= "Alliance",
+	Horde 				= "Horde",	
+	InfoErrorText 			= "The flag carrier targeting function will be restored when you are out of combat.",
+	ExprFlagPickUp 			= "The (%w+) .lag was picked up by (.+)!",
+	ExprFlagCaptured 		= "(.+) captured the (%w+) flag!",
+	ExprFlagReturn 			= "The (%w+) .lag was returned to its base by (.+)!",
+	FlagAlliance 			= "Alliance Flag: ",
+	FlagHorde			= "Horde Flag: ",
+	FlagBase			= "Base",
 })
 
 L:SetTimerLocalization({
-	TimerStart = "Game starts", 
-	TimerFlag = "Flag respawn",
+	TimerStart 			= "Game starts", 
+	TimerFlag 			= "Flag respawn",
 })
 
 L:SetOptionLocalization({
-	TimerStart  = "Show start timer",
-	TimerWin = "Show win timer",
-	TimerFlag = "Show flag respawn timer",
-	ShowFlagCarrier = "Show flag carrier",
-	ShowFlagCarrierErrorNote = "Shows flag carrier error message when in combat",
+	TimerStart  			= "Show start timer",
+	TimerWin 			= "Show win timer",
+	TimerFlag			= "Show flag respawn timer",
+	ShowFlagCarrier			= "Show flag carrier",
+	ShowFlagCarrierErrorNote 	= "Shows flag carrier error message when in combat",
 })
 
 
@@ -193,21 +198,21 @@ L:SetGeneralLocalization({
 
 L:SetWarningLocalization({
 	WarningShards	= "Rock Shards on >%s<",
-	WarningGrab		= "Archavon grabbed >%s<"
+	WarningGrab	= "Archavon grabbed >%s<"
 })
 
 L:SetTimerLocalization({
-	TimerShards = "Rock Shards: %s"
+	TimerShards 	= "Rock Shards: %s"
 })
 
 L:SetMiscLocalization({
-	TankSwitch = "%%s lunges for (%S+)!"
+	TankSwitch	 = "%%s lunges for (%S+)!"
 })
 
 L:SetOptionLocalization({
-	TimerShards = "Show Rock Shards timer",
-	WarningShards = "Show Rock Shards warning",
-	WarningGrab = "Show Tank Grab warning"
+	TimerShards 	= "Show Rock Shards timer",
+	WarningShards 	= "Show Rock Shards warning",
+	WarningGrab 	= "Show Tank Grab warning"
 })
 
 --------------
@@ -221,16 +226,13 @@ L:SetGeneralLocalization{
 }
 
 L:SetWarningLocalization{
-	specWarnNova	= "Lightning Nova",
+	specWarnNova		= "Lightning Nova",
 	warnNova 		= "Lightning Nova",
-	warnOverCharge	= "Overcharge"
-}
-
-L:SetTimerLocalization{
+	warnOverCharge		= "Overcharge"
 }
 
 L:SetOptionLocalization{
-	specWarnNova 	= ("Show special warning for |cff71d5ff|Hspell:%d|h%s|h|r"):format(64216, "Lightning Nova"),
+	specWarnNova 		= ("Show special warning for |cff71d5ff|Hspell:%d|h%s|h|r"):format(64216, "Lightning Nova"),
 	warnNova 		= ("Show warning for |cff71d5ff|Hspell:%d|h%s|h|r"):format(64216, "Lightning Nova"),
-	warnOverCharge 	= ("Show warning for |cff71d5ff|Hspell:%d|h%s|h|r"):format(64218, "Overcharge")
+	warnOverCharge 		= ("Show warning for |cff71d5ff|Hspell:%d|h%s|h|r"):format(64218, "Overcharge")
 }
