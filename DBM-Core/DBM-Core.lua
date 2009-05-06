6e91a93cbe53c51a1fd7ea0c3139898af4144b57
Paul Emmerich <emmericp@net.in.tum.de>
2009-05-06 20:22:28 +0000

    rss feed should work now!

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -31,7 +31,7 @@
 --    * Attribution. You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work).
 --    * Noncommercial. You may not use this work for commercial purposes.
 --    * Share Alike. If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one.
---
+
 
 -------------------------------
 --  Globals/Default Options  --
