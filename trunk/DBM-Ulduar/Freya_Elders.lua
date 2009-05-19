8748d7064293bc715d019fd7de75cc47d54ce5ac
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-05-19 01:47:00 +0000

    fixed TrashRespawnTimer

--- trunk/DBM-Ulduar/Freya_Elders.lua
+++ trunk/DBM-Ulduar/Freya_Elders.lua
@@ -4,9 +4,9 @@ local L = mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision: 1114 $"):sub(12, -3))
 mod:SetZone()
 
--- passive mod to provide informations over multible fight (trash respawn)
---mod:SetCreatureID(32914, 32915, 32913)
---mod:RegisterCombat("combat")
+-- passive mod to provide information for multiple fight (trash respawn)
+-- mod:SetCreatureID(32914, 32915, 32913)
+-- mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_CAST_START",
@@ -16,7 +16,7 @@ mod:RegisterEvents(
 mod:AddBoolOption("PlaySoundOnFistOfStone", false)
 local specWarnFistofStone	= mod:NewSpecialWarning("specWarnFistofStone", false)
 local specWarnGroundTremor	= mod:NewSpecialWarning("specWarnGroundTremor", true)
-local trashRespawnTimer		= mod:NewTimer(7200, "trashRespawnTimer")
+mod:AddBoolOption("TrashRespawnTimer", true, "timer")
 
 --
 -- Trash: 33430 Guardian Lasher (so ne blume)
@@ -46,10 +46,10 @@ function mod:SPELL_CAST_START(args)
 end
 
 function mod:UNIT_DIED(args)
-	if self.Options.trashRespawnTimer and not trashRespawnTimer:IsStarted() then
+	if self.Options.TrashRespawnTimer and not DBM.Bars:GetBar(L.TrashRespawnTimer) then
 		local guid = tonumber(args.destGUID:sub(9, 12), 16)
 		if guid == 33430 or guid == 33355 or guid == 33354 then		-- guardian lasher / nymphe / tree
-			trashRespawnTimer:Start()
+			DBM.Bars:CreateBar(7200, L.TrashRespawnTimer)
 		end
 	end
 end
