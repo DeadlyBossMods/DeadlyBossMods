7744c32945b83049a037c5ac9661f3eaeade3481
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-05-28 19:26:10 +0000

    GUI: localization.fr.lua update by Ezka via http://www.deadlybossmods.com/forum/viewtopic.php?t=1513

--- DBM-GUI/localization.fr.lua
+++ DBM-GUI/localization.fr.lua
@@ -1,4 +1,4 @@
-if GetLocale() ~= "frFR" then return end
+ï»¿if GetLocale() ~= "frFR" then return end
 
 if not DBM_GUI_Translations then DBM_GUI_Translations = {} end
 local L = DBM_GUI_Translations
@@ -6,51 +6,51 @@ local L = DBM_GUI_Translations
 
 L.MainFrame = "Deadly Boss Mods"
 
-L.TabCategory_Options 	= "Options Générales"
+L.TabCategory_Options 	= "Options GÃ©nÃ©rales"
 L.TabCategory_WOTLK 	= "Wrath of the Lich King"
 L.TabCategory_BC 	= "The Burning Crusade"
 L.TabCategory_CLASSIC 	= "WoW Classique"
 L.TabCategory_OTHER     = "Autres Boss Mods"
 
 L.BossModLoaded 	= "%s statistiques"
-L.BossModLoad_now 	= [[Ce boss mod n'est pas chargé.
-Il sera chargé quand vous entrerez dans l'instance. 
-Vous pouvez aussi cliquer sur le bouton pour chargé le mod manuellement.]]
+L.BossModLoad_now 	= [[Ce boss mod n'est pas chargÃ©.
+Il sera chargÃ© quand vous entrerez dans l'instance. 
+Vous pouvez aussi cliquer sur le bouton pour chargÃ© le mod manuellement.]]
 
 L.PosX = "Position X"
 L.PosY = "Position Y"
 
-L.MoveMe 		= "Déplacez moi"
+L.MoveMe 		= "DÃ©placez moi"
 L.Button_OK 		= "OK"
 L.Button_Cancel 	= "Annuler"
 L.Button_LoadMod 	= "Charger l'AddOn"
-L.Mod_Enabled		= "Activé boss mod"
+L.Mod_Enabled		= "ActivÃ© boss mod"
 L.Mod_EnableAnnounce	= "Annoncer au raid"
 L.Reset 		= "Reset"
 
 L.Enable  		= "Activer"
-L.Disable		= "Désactiver"
+L.Disable		= "DÃ©sactiver"
 
 L.NoSound		= "Pas de Son"
 
 -- Tab: Boss Statistics
 L.BossStatistics	= "Statistiques des boss"
-L.Statistic_Kills	= "Tués:"
+L.Statistic_Kills	= "TuÃ©s:"
 L.Statistic_Wipes	= "Wipes:"
 L.Statistic_BestKill	= "Meilleur down:"
-L.Statistic_Heroic	= "Héroique"
+L.Statistic_Heroic	= "HÃ©roique"
 
 -- Tab: General Options
-L.General 		= "Options Générales DBM"
+L.General 		= "Options GÃ©nÃ©rales DBM"
 L.EnableDBM 		= "Activer DBM"
 L.EnableStatus 		= "Envoie du 'status' au chuchotement"
-L.AutoRespond 		= "Activer la réponse automatique pendant les combats"
+L.AutoRespond 		= "Activer la rÃ©ponse automatique pendant les combats"
 L.EnableMiniMapIcon	= "Afficher le bouton sur minicarte "
 
-L.Button_RangeFrame	= "Afficher/Cacher la fenêtre de portée"
+L.Button_RangeFrame	= "Afficher/Cacher la fenÃªtre de portÃ©e"
 L.Button_TestBars	= "Lancer les barres de test"
 
-L.PizzaTimer_Headline 	= 'Créer un "Pizza Timer"'
+L.PizzaTimer_Headline 	= 'CrÃ©er un "Pizza Timer"'
 L.PizzaTimer_Title	= 'Nom (ex. "Pizza!")'
 L.PizzaTimer_Hours 	= "Heures"
 L.PizzaTimer_Mins 	= "Min"
@@ -65,22 +65,22 @@ L.RaidWarnColor_1 	= "Couleur 1"
 L.RaidWarnColor_2 	= "Couleur 2"
 L.RaidWarnColor_3 	= "Couleur 3"
 L.RaidWarnColor_4 	= "Couleur 4"
-L.InfoRaidWarning	= [[Vous pouvez spécifier la position et la couleurs de l'affichage des Alertes Raid.
-Cet affichage est utilisé pour des messages comme "Joueur X est affecté par Y"]]
-L.ColorResetted 	= "La couleur de ce champs a été remise à zéro."
-L.ShowWarningsInChat 	= "Afficher les alertes dans la fenétre de dialogue"
+L.InfoRaidWarning	= [[Vous pouvez spÃ©cifier la position et la couleurs de l'affichage des Alertes Raid.
+Cet affichage est utilisÃ© pour des messages comme "Joueur X est affectÃ© par Y"]]
+L.ColorResetted 	= "La couleur de ce champs a Ã©tÃ© remise Ã  zÃ©ro."
+L.ShowWarningsInChat 	= "Afficher les alertes dans la fenÃ©tre de dialogue"
 L.ShowFakedRaidWarnings = "Afficher les alertes comme de faux avertissements de raid"
-L.WarningIconLeft 	= "Afficher l'icone à gauche"
-L.WarningIconRight 	= "Afficher l'icone à droite"
+L.WarningIconLeft 	= "Afficher l'icone Ã  gauche"
+L.WarningIconRight 	= "Afficher l'icone Ã  droite"
 L.RaidWarnMessage 	= "Merci d'utiliser Deadly Boss Mods"
-L.BarWhileMove 		= "Alerte-raid déplacable"
+L.BarWhileMove 		= "Alerte-raid dÃ©placable"
 L.RaidWarnSound		= "Jouer un son pour les alertes raid"
-L.SpecialWarnSound	= "Jouer un son pour les alertes spéciales"
+L.SpecialWarnSound	= "Jouer un son pour les alertes spÃ©ciales"
 
 -- Tab: Barsetup
 L.BarSetup   = "Style des barres"
 L.BarTexture = "Texture des barres"
-L.BarStartColor = "Couleur de départ"
+L.BarStartColor = "Couleur de dÃ©part"
 L.BarEndColor = "Couleur de fin"
 L.ExpandUpwards		= "Nouvelles barres au-dessus"
 
@@ -88,17 +88,17 @@ L.Slider_BarOffSetX 	= "Position X: %d"
 L.Slider_BarOffSetY 	= "Position Y: %d"
 L.Slider_BarWidth 	= "Largeur: %d"
 L.Slider_BarScale 	= "Echelle: %0.2f"
-L.AreaTitle_BarSetup 	= "Options générales des barres"
+L.AreaTitle_BarSetup 	= "Options gÃ©nÃ©rales des barres"
 L.AreaTitle_BarSetupSmall = "Options des petites barres"
 L.AreaTitle_BarSetupHuge = "Options des grandes barres"
 L.BarIconLeft 		= "Icone gauche"
 L.BarIconRight 		= "Icone droit"
-L.EnableHugeBar 	= "Activé les grandes barres (Barre 2)"
+L.EnableHugeBar 	= "ActivÃ© les grandes barres (Barre 2)"
 L.FillUpBars		= "Remplir les barres"
 
 -- Tab: Spam Filter
 L.Panel_SpamFilter		= "Filtre anti-spam"
-L.Area_SpamFilter		= "Options générales du filtre anti-spam"
+L.Area_SpamFilter		= "Options gÃ©nÃ©rales du filtre anti-spam"
 L.HideBossEmoteFrame	= "Cacher la fenêtre des emotes de boss"
 L.SpamBlockRaidWarning	= "Filtrer les annonces venant d'autre boss mods"
 L.SpamBlockBossWhispers	= "Filtrer les alertes <DBM> chuchotement pendant les combats"
