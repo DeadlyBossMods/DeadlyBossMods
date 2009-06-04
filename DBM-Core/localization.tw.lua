965ed614673e84fd1eb14567eca874ca4b65097c
Azael <Azael@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-06-04 16:36:14 +0000

    zhtw update for r1205

--- DBM-Core/localization.tw.lua
+++ DBM-Core/localization.tw.lua
@@ -4,56 +4,57 @@ DBM_CORE_LOAD_MOD_SUCCESS			= "成功讀取%s模組!"
 DBM_CORE_LOAD_GUI_ERROR				= "無法讀取圖形介面：%s"
 
 DBM_CORE_COMBAT_STARTED				= "%s開戰. 祝好運與盡興! :)";
-DBM_CORE_BOSS_DOWN					= "%s 倒地, 經過 %s!"
+DBM_CORE_BOSS_DOWN				= "%s 倒地, 經過 %s!"
 DBM_CORE_BOSS_DOWN_LONG				= "%s倒地!本次經過%s，上次經過%s，最快一次%s。"
-DBM_CORE_BOSS_DOWN_NEW_RECORD		= "%s倒地! 經過	%s，這是一個新記錄!（舊紀錄為%s）"
+DBM_CORE_BOSS_DOWN_NEW_RECORD			= "%s倒地! 經過	%s，這是一個新記錄!（舊紀錄為%s）"
 DBM_CORE_COMBAT_ENDED				= "%s 的戰鬥經過 %s 結束."
 
 DBM_CORE_TIMER_FORMAT_SECS			= "%d秒"
 DBM_CORE_TIMER_FORMAT_MINS			= "%d分鐘"
 DBM_CORE_TIMER_FORMAT				= "%d分%d秒"
 
-DBM_CORE_MIN						= "分"
-DBM_CORE_SEC						= "秒"
-DBM_CORE_DEAD						= "死亡"
-DBM_CORE_OK							= "確定"
+DBM_CORE_MIN					= "分"
+DBM_CORE_SEC					= "秒"
+DBM_CORE_DEAD					= "死亡"
+DBM_CORE_OK						= "確定"
 
-DBM_CORE_GENERIC_WARNING_ENRAGE		= "%s%s後狂怒"
-DBM_CORE_GENERIC_TIMER_ENRAGE		= "狂怒"
-DBM_CORE_OPTION_TIMER_ENRAGE		= "顯示狂怒計時器"
-DBM_CORE_OPTION_HEALTH_FRAME		= "顯示首領血量框架"
+DBM_CORE_GENERIC_WARNING_ENRAGE			= "%s%s後狂怒"
+DBM_CORE_GENERIC_TIMER_ENRAGE			= "狂怒"
+DBM_CORE_OPTION_TIMER_ENRAGE			= "顯示狂怒計時器"
+DBM_CORE_OPTION_HEALTH_FRAME			= "顯示首領血量框架"
 
-DBM_CORE_OPTION_CATEGORY_TIMERS		= "計時條"
-DBM_CORE_OPTION_CATEGORY_WARNINGS	= "公佈"
-DBM_CORE_OPTION_CATEGORY_MISC		= "其它"
+DBM_CORE_OPTION_CATEGORY_TIMERS			= "計時條"
+DBM_CORE_OPTION_CATEGORY_WARNINGS		= "公佈"
+DBM_CORE_OPTION_CATEGORY_MISC			= "其它"
 
 DBM_CORE_AUTO_RESPONDED				= "已自動回復密語。"
 DBM_CORE_STATUS_WHISPER				= "%s：%s，%d/%d存活"
-DBM_CORE_AUTO_RESPOND_WHISPER		= "%s正在與%s交戰，（當前%s，%d/%d存活）"
+DBM_CORE_AUTO_RESPOND_WHISPER			= "%s正在與%s交戰，（當前%s，%d/%d存活）"
 
-DBM_CORE_VERSIONCHECK_HEADER		= "Deadly Boss Mods - 版本檢測"
+DBM_CORE_VERSIONCHECK_HEADER			= "Deadly Boss Mods - 版本檢測"
 DBM_CORE_VERSIONCHECK_ENTRY			= "%s：%s(r%d)"
-DBM_CORE_VERSIONCHECK_ENTRY_NO_DBM	= "%s：尚未安裝DBM"
-DBM_CORE_VERSIONCHECK_FOOTER		= "團隊中有%d名成員正在使用Deadly Boss Mods"
+DBM_CORE_VERSIONCHECK_ENTRY_NO_DBM		= "%s：尚未安裝DBM"
+DBM_CORE_VERSIONCHECK_FOOTER			= "團隊中有%d名成員正在使用Deadly Boss Mods"
 
-DBM_CORE_UPDATEREMINDER_HEADER		= "你的 Deadly Boss Mod 已經過期! \n你可以在如下位址下載到新版本%s(r%d)："
-DBM_CORE_UPDATEREMINDER_FOOTER		= "Ctrl-C：複製下載地址到剪貼本。"
+DBM_CORE_UPDATEREMINDER_HEADER			= "你的 Deadly Boss Mod 已經過期! \n你可以在如下位址下載到新版本%s(r%d)："
+DBM_CORE_UPDATEREMINDER_FOOTER			= "Ctrl-C：複製下載地址到剪貼本。"
+DBM_CORE_UPDATEREMINDER_NOTAGAIN		= "當有新版本時顯示彈出提示"
 
 DBM_CORE_MOVABLE_BAR				= "拖動我!"
 
-DBM_PIZZA_SYNC_INFO					= "|Hplayer:%1$s|h[%1$s]|h向你發送了一個倒計時：'%2$s'\n|HDBM:cancel:%2$s:nil|h|cff3588ff[取消該計時]|r|h  |HDBM:ignore:%2$s:%1$s|h|cff3588ff[忽略來自%1$s的計時]|r|h"
+DBM_PIZZA_SYNC_INFO				= "|Hplayer:%1$s|h[%1$s]|h向你發送了一個倒計時：'%2$s'\n|HDBM:cancel:%2$s:nil|h|cff3588ff[取消該計時]|r|h  |HDBM:ignore:%2$s:%1$s|h|cff3588ff[忽略來自%1$s的計時]|r|h"
 DBM_PIZZA_CONFIRM_IGNORE			= "是否要在該次遊戲連接中遮罩來自%s的計時？"
 DBM_PIZZA_ERROR_USAGE				= "命令：/dbm [broadcast] timer <時間（秒）> <文字>"
 
 DBM_CORE_ERROR_DBMV3_LOADED			= "目前有2個版本的Deadly Boss Mods正在運行：DBMv3和DBMv4。\n按一下“確定”按鈕可將DBMv3關閉並重載使用者介面。\n我們建議將外掛程式目錄下的DBMv3刪除。"
 
-DBM_CORE_MINIMAP_TOOLTIP_HEADER		= "Deadly Boss Mods"
-DBM_CORE_MINIMAP_TOOLTIP_FOOTER		= "Shift+按一下或右鍵點擊即可移動"
+DBM_CORE_MINIMAP_TOOLTIP_HEADER			= "Deadly Boss Mods"
+DBM_CORE_MINIMAP_TOOLTIP_FOOTER			= "Shift+按一下或右鍵點擊即可移動"
 
 DBM_CORE_RANGECHECK_HEADER			= "距離監視（%d碼）"
-DBM_CORE_RANGECHECK_SETRANGE		= "設置距離"
+DBM_CORE_RANGECHECK_SETRANGE			= "設置距離"
 DBM_CORE_RANGECHECK_HIDE			= "隱藏"
-DBM_CORE_RANGECHECK_SETRANGE_TO		= "%d碼"
+DBM_CORE_RANGECHECK_SETRANGE_TO			= "%d碼"
 
 DBM_CORE_SLASHCMD_HELP				= {
 	"可用命令：",
@@ -66,14 +67,14 @@ DBM_CORE_SLASHCMD_HELP				= {
 
 DBM_ERROR_NO_PERMISSION				= "無權進行該操作。"
 
-DBM_CORE_BOSSHEALTH_HIDE_FRAME		= "隱藏"
+DBM_CORE_BOSSHEALTH_HIDE_FRAME			= "隱藏"
 
-DBM_CORE_ALLIANCE					= "聯盟"
-DBM_CORE_HORDE						= "部落"
+DBM_CORE_ALLIANCE				= "聯盟"
+DBM_CORE_HORDE					= "部落"
 
-DBM_CORE_UNKNOWN					= "未知"
+DBM_CORE_UNKNOWN				= "未知"
 
-DBM_CORE_TIMER_PULL					= "戰鬥準備"
+DBM_CORE_TIMER_PULL				= "戰鬥準備"
 DBM_CORE_ANNOUNCE_PULL				= "%d秒後拉怪"
 DBM_CORE_ANNOUNCE_PULL_NOW			= "拉怪囉!"
 
