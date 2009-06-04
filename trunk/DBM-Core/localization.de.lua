d5b29dc1b00feaf5701099b96d750bf3216a191a
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-06-04 01:26:30 +0000

    DE: localization update

--- trunk/DBM-Core/localization.de.lua
+++ trunk/DBM-Core/localization.de.lua
@@ -5,56 +5,56 @@ DBM_CORE_LOAD_MOD_SUCCESS			= "Boss Mods für '%s' geladen!"
 DBM_CORE_LOAD_GUI_ERROR				= "Konnte das GUI nicht laden: %s"
 
 DBM_CORE_COMBAT_STARTED				= "Kampf gegen %s hat begonnen. Viel Glück! :)";
-DBM_CORE_BOSS_DOWN					= "%s tot nach %s!"
+DBM_CORE_BOSS_DOWN				= "%s tot nach %s!"
 DBM_CORE_BOSS_DOWN_LONG				= "%s tot nach %s! Euer letzter kill hat %s gedauert und der schnellste %s."
-DBM_CORE_BOSS_DOWN_NEW_RECORD		= "%s tot nach %s! Das ist ein neuer Rekord! (der alte Rekord war %s)"
+DBM_CORE_BOSS_DOWN_NEW_RECORD			= "%s tot nach %s! Das ist ein neuer Rekord! (der alte Rekord war %s)"
 DBM_CORE_COMBAT_ENDED				= "Kampf gegen %s hat nach %s aufgehört."
 
 DBM_CORE_TIMER_FORMAT_SECS			= "%d |4Sekunde:Sekunden;"
 DBM_CORE_TIMER_FORMAT_MINS			= "%d |4Minute:Minuten;"
 DBM_CORE_TIMER_FORMAT				= "%d |4Minute:Minuten; und %d |4Sekunde:Sekunden;"
 
-DBM_CORE_MIN						= "Min"
-DBM_CORE_SEC						= "Sek"
-DBM_CORE_DEAD						= "Tot"
-DBM_CORE_OK							= "Okay"
+DBM_CORE_MIN					= "Min"
+DBM_CORE_SEC					= "Sek"
+DBM_CORE_DEAD					= "Tot"
+DBM_CORE_OK					= "Okay"
 
-DBM_CORE_GENERIC_WARNING_ENRAGE		= "Enrage in %s %s"
-DBM_CORE_GENERIC_TIMER_ENRAGE		= "Enrage"
-DBM_CORE_OPTION_TIMER_ENRAGE		= "Enrage-Timer anzeigen"
-DBM_CORE_OPTION_HEALTH_FRAME		= "BossHealth-Frame anzeigen"
+DBM_CORE_GENERIC_WARNING_ENRAGE			= "Enrage in %s %s"
+DBM_CORE_GENERIC_TIMER_ENRAGE			= "Enrage"
+DBM_CORE_OPTION_TIMER_ENRAGE			= "Enrage-Timer anzeigen"
+DBM_CORE_OPTION_HEALTH_FRAME			= "BossHealth-Frame anzeigen"
 
-DBM_CORE_OPTION_CATEGORY_TIMERS		= "Timer"
-DBM_CORE_OPTION_CATEGORY_WARNINGS	= "Ansagen"
-DBM_CORE_OPTION_CATEGORY_MISC		= "Verschiedenes"
+DBM_CORE_OPTION_CATEGORY_TIMERS			= "Timer"
+DBM_CORE_OPTION_CATEGORY_WARNINGS		= "Ansagen"
+DBM_CORE_OPTION_CATEGORY_MISC			= "Verschiedenes"
 
 DBM_CORE_AUTO_RESPONDED				= "Automatisch geantwortet."
 DBM_CORE_STATUS_WHISPER				= "%s: %s, %d/%d Spieler am Leben"
-DBM_CORE_AUTO_RESPOND_WHISPER		= "%s ist damit beschäftigt gegen %s zu kämpfen! (%s, %d/%d Spieler am Leben)"
+DBM_CORE_AUTO_RESPOND_WHISPER			= "%s ist damit beschäftigt gegen %s zu kämpfen! (%s, %d/%d Spieler am Leben)"
 
-DBM_CORE_VERSIONCHECK_HEADER		= "Deadly Boss Mods - Versionen"
+DBM_CORE_VERSIONCHECK_HEADER			= "Deadly Boss Mods - Versionen"
 DBM_CORE_VERSIONCHECK_ENTRY			= "%s: %s (r%d)"
-DBM_CORE_VERSIONCHECK_ENTRY_NO_DBM	= "%s: DBM nicht installiert"
-DBM_CORE_VERSIONCHECK_FOOTER		= "%d Spieler mit Deadly Boss Mods gefunden"
+DBM_CORE_VERSIONCHECK_ENTRY_NO_DBM		= "%s: DBM nicht installiert"
+DBM_CORE_VERSIONCHECK_FOOTER			= "%d Spieler mit Deadly Boss Mods gefunden"
 
-DBM_CORE_UPDATEREMINDER_HEADER		= "Deine Version von Deadly Boss Mods ist veraltet.\n Version %s (r%d) ist hier zum Download verfügbar:"
-DBM_CORE_UPDATEREMINDER_FOOTER		= "Drückte Strg+C um den Download-Link in die Zwischenablage zu kopieren"
+DBM_CORE_UPDATEREMINDER_HEADER			= "Deine Version von Deadly Boss Mods ist veraltet.\n Version %s (r%d) ist hier zum Download verfügbar:"
+DBM_CORE_UPDATEREMINDER_FOOTER			= "Drücke Strg+C um den Download-Link in die Zwischenablage zu kopieren"
 
 DBM_CORE_MOVABLE_BAR				= "Zieh mich!"
 
-DBM_PIZZA_SYNC_INFO					= "|Hplayer:%1$s|h[%1$s]|h hat dir einen Pizza-Timer geschickt: '%2$s'\n|HDBM:cancel:%2$s:nil|h|cff3588ff[Diesen Timer abbrechen]|r|h  |HDBM:ignore:%2$s:%1$s|h|cff3588ff[Pizza-Timer von %1$s ignorieren]|r|h"
+DBM_PIZZA_SYNC_INFO				= "|Hplayer:%1$s|h[%1$s]|h hat dir einen Pizza-Timer geschickt: '%2$s'\n|HDBM:cancel:%2$s:nil|h|cff3588ff[Diesen Timer abbrechen]|r|h  |HDBM:ignore:%2$s:%1$s|h|cff3588ff[Pizza-Timer von %1$s ignorieren]|r|h"
 DBM_PIZZA_CONFIRM_IGNORE			= "Willst du wirklich Pizza-Timer von %s für diese Session ignorieren?"
 DBM_PIZZA_ERROR_USAGE				= "Benutzung: /dbm [broadcast] timer <time> <text>"
 
 DBM_CORE_ERROR_DBMV3_LOADED			= "Deadly Boss Mods läuft doppelt, da du DBMv3 und DBMv4 installiert und aktiviert hast!\nKlick auf \"Okay\" um DBMv3 zu deaktivieren und dein Interface neu zu laden.\nAußerdem solltest du deinen AddOn-Ordner aufräumen indem du alle DBMv3-Mods löschst."
 
-DBM_CORE_MINIMAP_TOOLTIP_HEADER		= "Deadly Boss Mods"
-DBM_CORE_MINIMAP_TOOLTIP_FOOTER		= "Shift+Klick oder Rechtsklick zum bewegen"
+DBM_CORE_MINIMAP_TOOLTIP_HEADER			= "Deadly Boss Mods"
+DBM_CORE_MINIMAP_TOOLTIP_FOOTER			= "Shift+Klick oder Rechtsklick zum bewegen"
 
 DBM_CORE_RANGECHECK_HEADER			= "Abstandscheck (%d m)"
-DBM_CORE_RANGECHECK_SETRANGE		= "Abstand Einstellen"
+DBM_CORE_RANGECHECK_SETRANGE			= "Abstand Einstellen"
 DBM_CORE_RANGECHECK_HIDE			= "Verstecken"
-DBM_CORE_RANGECHECK_SETRANGE_TO		= "%d m"
+DBM_CORE_RANGECHECK_SETRANGE_TO			= "%d m"
 
 DBM_CORE_SLASHCMD_HELP				= {
 	"Verfügbare Slash-Commands:",
@@ -67,10 +67,10 @@ DBM_CORE_SLASHCMD_HELP				= {
 
 DBM_ERROR_NO_PERMISSION				= "Du hast nicht die benötigte Berechtigung für diesen Befehl!"
 
-DBM_CORE_BOSSHEALTH_HIDE_FRAME		= "Verstecken"
+DBM_CORE_BOSSHEALTH_HIDE_FRAME			= "Verstecken"
 
-DBM_CORE_ALLIANCE					= "Allianz"
-DBM_CORE_HORDE						= "Horde"
+DBM_CORE_ALLIANCE				= "Allianz"
+DBM_CORE_HORDE					= "Horde"
 
 -- Auto-generated Timer Localizations
 DBM_CORE_AUTO_TIMER_TEXTS = {
@@ -78,16 +78,19 @@ DBM_CORE_AUTO_TIMER_TEXTS = {
 	cast = "%s",
 	active = "%s",
 	cd = "%s CD",
-	next = "N�chster %s",
+	next = "Nächster %s",
 	achievement = "%s"
 }
 
 
 DBM_CORE_AUTO_TIMER_OPTIONS = {
-	target = "Debufftimer f�r |cff71d5ff|Hspell:%d|h%s|h|r anzeigen",
-	cast = "Casttimer f�r |cff71d5ff|Hspell:%d|h%s|h|r anzeigen",
-	active = "Timer w�hrend |cff71d5ff|Hspell:%d|h%s|h|r aktiv ist anzeigen",
-	cd = "Cooldowntimer f�r |cff71d5ff|Hspell:%d|h%s|h|r anzeigen",
-	next = "Timer f�r n�chstes |cff71d5ff|Hspell:%d|h%s|h|r anzeigen",
-	achievement = "Timer f�r %s anzeigen"
+	target = "Debufftimer für |cff71d5ff|Hspell:%d|h%s|h|r anzeigen",
+	cast = "Casttimer für |cff71d5ff|Hspell:%d|h%s|h|r anzeigen",
+	active = "Timer während |cff71d5ff|Hspell:%d|h%s|h|r aktiv ist anzeigen",
+	cd = "Cooldowntimer für |cff71d5ff|Hspell:%d|h%s|h|r anzeigen",
+	next = "Timer für nächstes |cff71d5ff|Hspell:%d|h%s|h|r anzeigen",
+	achievement = "Timer für %s anzeigen"
 }
+
+
+
