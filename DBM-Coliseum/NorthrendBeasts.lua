ece374ef14c8c0c37ccc705ace44f0a1ea878b0e
Paul Emmerich <emmericp@net.in.tum.de>
2009-08-04 20:32:32 +0000

    update

--- DBM-Coliseum/NorthrendBeasts.lua
+++ DBM-Coliseum/NorthrendBeasts.lua
@@ -15,13 +15,20 @@ mod:RegisterEvents(
 local timerBreath		= mod:NewCastTimer(5, 67650)
 
 local warnImpaleOn		= mod:NewAnnounce("WarningImpale", 2, 67478)
+local warnSpray			= mod:NewAnnounce("WarningSpray", 2, 67616)
 local warnBreath		= mod:NewAnnounce("WarningBreath", 1, 67650)
 local warnRage			= mod:NewAnnounce("WarningRage", 3, 67657)
 
+local specWarnSpray		= mod:NewSpecialWarning("SpecialWarningSpray")
 local specWarnSilence	= mod:NewSpecialWarning("SpecialWarningSilence")
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 67478 then
+	if args.spellId == 67616 then
+		warnSpray:Show(args.spellName, args.destName)
+		if args.destName == UnitName("player") then
+			specWarnSpray:Show()
+		end
+	elseif args.spellId == 67478 then
 		warnImpaleOn:Show(args.spellName, args.destName)
 	elseif args.spellId == 67657 then
 		warnRage:Show()
