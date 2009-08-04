8127e3ecf098668b50b2c59c3e78f1bf6e320d76
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-08-04 17:56:56 +0000

    frFR update

--- trunk/DBM-ChamberOfAspects/localization.fr.lua
+++ trunk/DBM-ChamberOfAspects/localization.fr.lua
@@ -40,3 +40,58 @@ L = DBM:GetModLocalization("Sartharion")
 L:SetGeneralLocalization({
 	name = "Sartharion"
 })
+
+L:SetWarningLocalization({
+	WarningTenebron			= "Ténébron Arrive",
+	WarningShadron			= "Obscuron Arrive",
+	WarningVesperon			= "Vespéron Arrive",
+	WarningFireWall			= "Vague de Flammes",
+	WarningVesperonPortal	= "Portal de Vespéron",
+	WarningTenebronPortal	= "Portail de Ténébron",
+	WarningShadronPortal	= "Portail d'Obscuron",
+})
+
+L:SetTimerLocalization({
+	TimerWall	= "Cooldown Vague de Flammes",
+	TimerTenebron	= "Ténébron Arrive",
+	TimerShadron	= "Obscuron Arrive",
+	TimerVesperon	= "Vespéron Arrive"
+})
+
+L:SetOptionLocalization({
+	PlaySoundOnFireWall	= "Joue un sons pour les Vague de Flammes",
+	AnnounceFails		= "Ecris dans le chatt les joueurs qui n'ont pas éviter les zones noir / Vague de Flammes (requires announce enabled and promoted/leader status)",
+
+	TimerWall		= "Montre le timer pour les Vague de Flammes",
+	TimerTenebron		= "Montre le timer pour Ténébron",
+	TimerShadron		= "Montre le timer pour Obscuron",
+	TimerVesperon		= "Montre le timer pour Vespéron",
+
+	WarningFireWall		= "Montre une alerte spécial pour les Vague de Flammes",
+	WarningTenebron		= "Montre le timer avant que Ténébron arrive",
+	WarningShadron		= "Montre le timer avant qu'Obscuron arrive",
+	WarningVesperon		= "Montre le timer avant que Vespéron arrive",
+
+	WarningTenebronPortal	= "Montre une alerte spécial pour les portails de Ténébron",
+	WarningShadronPortal	= "Montre une alerte spécial pour les portails d'Obscuron",
+	WarningVesperonPortal	= "Montre une alerte spécial pour les portails de Vespéron",
+})
+
+L:SetMiscLocalization({
+	Wall			= "La lave qui entoure %s bouillonne !",
+	Portal			= "%s Commance a incanter un portail!",
+	NameTenebron	= "Ténébron",
+	NameShadron		= "Obscuron",
+	NameVesperon	= "Vespéron",
+	FireWallOn		= "Vague de Flammes: %s",
+	VoidZoneOn		= "Void Zone: %s",
+	VoidZones		= "Void Zone raté (Ce try): %s",
+	FireWalls		= "Vague de Flammes raté (Ce try): %s",
+	--[[ not in use; don't translate.
+	Vesperon	= "Vesperon, the clutch is in danger! Assist me!",
+	Shadron		= "Shadron! Come to me! All is at risk!",
+	Tenebron	= "Tenebron! The eggs are yours to protect as well!"
+	--]]
+})
+
+
