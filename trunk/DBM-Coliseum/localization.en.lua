f317a8fa6a2535a496da6ae0ce0f8c3bf0a34881
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-08-04 20:32:32 +0000

    update

--- trunk/DBM-Coliseum/localization.en.lua
+++ trunk/DBM-Coliseum/localization.en.lua
@@ -12,13 +12,17 @@ L:SetGeneralLocalization{
 L:SetOptionLocalization{
 	WarningImpale			= "Show warning for Impale",
 	WarningBreath			= "Show warning for Arctic Breath",
+	WarningSpray			= "Show warning for Paralytic Spray",
 	WarningRage				= "Show warning for Frothing Rages",
-	SpecialWarningSilence	= "Show special warning for Spell Block"
+	SpecialWarningSilence	= "Show special warning for Spell Block",
+	SpecialWarningSpray		= "Show special warning if you have Paralytic Spray"
 }
 
 L:SetWarningLocalization{
 	WarningImpale			= "%s on >%s<",
+	WarningSpray			= "%s on >%s<",
 	WarningBreath			= "Arctic Breath",
 	WarningRage				= "Frothing Rages",
-	SpecialWarningSilence	= "Spell Block in 0.5 Seconds!"
+	SpecialWarningSilence	= "Spell Block in 0.5 Seconds!",
+	SpecialWarningSpray		= "Paralytic Spray on You"
 }
