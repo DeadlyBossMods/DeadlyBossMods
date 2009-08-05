dbf240cd579c54a00d1841c8dab4407dbb2f67b7
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-08-05 17:41:06 +0000

    updated version number for alpha build

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -38,8 +38,8 @@
 -------------------------------
 DBM = {
 	Revision = ("$Revision$"):sub(12, -3),
-	Version = "4.20",
-	DisplayVersion = "4.21 alpha"
+	Version = "4.21",
+	DisplayVersion = "4.22 alpha"
 }
 
 DBM_SavedOptions = {}
