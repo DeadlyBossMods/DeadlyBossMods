0aa33dafd00d8a1e9d3e41f25c27a9cd2cfd7a1f
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-08-06 09:28:29 +0000

    updated coliseum

--- trunk/DBM-Coliseum/NorthrendBeasts.lua
+++ trunk/DBM-Coliseum/NorthrendBeasts.lua
@@ -5,21 +5,30 @@ mod:SetRevision(("$Revision: 599 $"):sub(12, -3))
 mod:SetCreatureID(34797)
 mod:SetZone()
 
-mod:RegisterCombat("combat", 34796)
+-- 34816 npc to talk to
+-- 34797 npc icehowl died
+
+--mod:RegisterCombat("combat", 34796)
+mod:RegisterCombat("yell", "Hailing from the deepest, darkest caverns of the Storm Peaks, Gormok the Impaler! Battle on, heroes!")
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
 	"SPELL_CAST_START",
-	"CHAT_MSG_RAID_BOSS_EMOTE"
+	"SPELL_DAMAGE",
+	"CHAT_MSG_RAID_BOSS_EMOTE",
+	"SPELL_AURA_APPLIED_DOSE"
 )
 
 local timerBreath		= mod:NewCastTimer(5, 67650)
 
 local warnImpaleOn		= mod:NewAnnounce("WarningImpale", 2, 67478)
+local warnFireBomb		= mod:NewAnnounce("WarningFireBomb", 4, 66317)
 local warnSpray			= mod:NewAnnounce("WarningSpray", 2, 67616)
 local warnBreath		= mod:NewAnnounce("WarningBreath", 1, 67650)
 local warnRage			= mod:NewAnnounce("WarningRage", 3, 67657)
 
+local specWarnImpale3	= mod:NewSpecialWarning("SpecialWarningImpale3", false)
+local specWarnFireBomb	= mod:NewSpecialWarning("SpecialWarningFireBomb")
 local specWarnSpray		= mod:NewSpecialWarning("SpecialWarningSpray")
 local specWarnSilence	= mod:NewSpecialWarning("SpecialWarningSilence")
 local specWarnCharge	= mod:NewSpecialWarning("SpecialWarningCharge")
@@ -30,8 +39,8 @@ function mod:SPELL_AURA_APPLIED(args)
 		if args.destName == UnitName("player") then
 			specWarnSpray:Show()
 		end
-	elseif args.spellId == 67478 then
-		warnImpaleOn:Show(args.spellName, args.destName)
+--	elseif args.spellId == 67478 then
+--		warnImpaleOn:Show(args.spellName, args.destName)
 	elseif args.spellId == 67657 then
 		warnRage:Show()
 	end
@@ -40,9 +49,11 @@ end
 function mod:SPELL_CAST_START(args)
 	if args.spellId == 67648 then
 		specWarnSilence:Show()
-	elseif args.spellId == 67650 then
+	elseif args.spellId == 67650 then		
 		timerBreath:Start()
 		warnBreath:Show()
+	elseif args.spellId == 66313 then		-- FireBomb (Impaler)
+		warnFireBomb:Show()
 	end
 end
 
@@ -51,5 +62,22 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 	if target and target == UnitName("player") then
 		specWarnCharge:Show()
 	end
+end
+
+function mod:SPELL_AURA_APPLIED_DOSE(args)
+	if (args.spellId == 67477 or args.spellId == 66331) and args.amount >= 3 then
+		if UnitName("player") == args.destName then
+			specWarnImpale3:Show()
+		end
+		warnImpaleOn:Show(args.spellName, args.destName)
+	end
+end
+
+function mod:SPELL_DAMAGE(args)
+		if args.spellId == 66320 or args.spellId == 66317 then
+			if UnitName("player") == args.destName then
+				specWarnFireBomb:Show()
+			end
+		end
 end
 
