cdde7cb893e2ca663fdf0eff08fda6cd3e3df062
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-08-06 09:28:29 +0000

    updated coliseum

--- DBM-Coliseum/localization.en.lua
+++ DBM-Coliseum/localization.en.lua
@@ -15,9 +15,12 @@ L:SetMiscLocalization{
 
 L:SetOptionLocalization{
 	WarningImpale			= "Show warning for Impale",
+	WarningFireBomb			= "Show warning for Fire Bomb",
 	WarningBreath			= "Show warning for Arctic Breath",
 	WarningSpray			= "Show warning for Paralytic Spray",
 	WarningRage				= "Show warning for Frothing Rages",
+	SpecialWarningImpale3	= "Show special warning for Impale (>=3 Stacks)",
+	SpecialWarningFireBomb	= "Show special warning for Fire Bomb on You",
 	SpecialWarningSilence	= "Show special warning for Spell Block",
 	SpecialWarningSpray		= "Show special warning if you have Paralytic Spray",
 	SpecialWarningCharge	= "Show special warning when Icehowl is about to charge you"
@@ -25,9 +28,12 @@ L:SetOptionLocalization{
 
 L:SetWarningLocalization{
 	WarningImpale			= "%s on >%s<",
+	WarningFireBomb			= "Fire Bomb",
 	WarningSpray			= "%s on >%s<",
 	WarningBreath			= "Arctic Breath",
 	WarningRage				= "Frothing Rages",
+	SpecialWarningImpale3	= "Impale on You",
+	SpecialWarningFireBomb	= "Fire Bomb on You",
 	SpecialWarningSilence	= "Spell Block in 0.5 Seconds!",
 	SpecialWarningSpray		= "Paralytic Spray on You",
 	SpecialWarningCharge	= "Charge on You! Run!"
