da066ade45395d71c540734a0976b2ae7e0ac708
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-08-10 21:56:39 +0000

    Updated Northend Beasts
    Icon on Charge Target and new options
    removed spray (very unusable ^^)

--- trunk/DBM-Coliseum/localization.en.lua
+++ trunk/DBM-Coliseum/localization.en.lua
@@ -14,33 +14,38 @@ L:SetMiscLocalization{
 }
 
 L:SetOptionLocalization{
-	WarningImpale			= "Show warning for Impale",
-	WarningFireBomb			= "Show warning for Fire Bomb",
-	WarningBreath			= "Show warning for Arctic Breath",
-	WarningSpray			= "Show warning for Paralytic Spray",
-	WarningRage				= "Show warning for Frothing Rages",
-	SpecialWarningImpale3	= "Show special warning for Impale (>=3 Stacks)",
-	SpecialWarningFireBomb	= "Show special warning for Fire Bomb on You",
-	SpecialWarningSlimePool	= "Show special warning for Slime Pool",
-	SpecialWarningSilence	= "Show special warning for Spell Block",
-	SpecialWarningSpray		= "Show special warning if you have Paralytic Spray",
-	SpecialWarningToxin		= "Show special warning if you have Paralytic Toxin",
-	SpecialWarningCharge	= "Show special warning when Icehowl is about to charge You"
+	WarningImpale				= "Show warning for Impale",
+	WarningFireBomb				= "Show warning for Fire Bomb",
+	WarningBreath				= "Show warning for Arctic Breath",
+--	WarningSpray				= "Show warning for Paralytic Spray",
+	WarningRage					= "Show warning for Frothing Rages",
+	WarningCharge				= "Show warning for Charge Target",
+	SpecialWarningImpale3		= "Show special warning for Impale (>=3 Stacks)",
+	SpecialWarningFireBomb		= "Show special warning for Fire Bomb on You",
+	SpecialWarningSlimePool		= "Show special warning for Slime Pool",
+	SpecialWarningSilence		= "Show special warning for Spell Block",
+	SpecialWarningSpray			= "Show special warning if you have Paralytic Spray",
+	SpecialWarningToxin			= "Show special warning if you have Paralytic Toxin",
+	SpecialWarningCharge		= "Show special warning when Icehowl is about to charge You",
+	SpecialWarningChargeNear	= "Show special warning when Icehowl charges near You",
+	SetIconOnChargeTarget		= "Set Icon on Charge Target (skull)"
 }
 
 L:SetWarningLocalization{
-	WarningImpale			= "%s on >%s<",
-	WarningFireBomb			= "Fire Bomb",
-	WarningSpray			= "%s on >%s<",
-	WarningBreath			= "Arctic Breath",
-	WarningRage				= "Frothing Rages",
-	SpecialWarningImpale3	= "Impale on You",
-	SpecialWarningFireBomb	= "Fire Bomb on You",
-	SpecialWarningSlimePool	= "Slime Pool, Move out!",
-	SpecialWarningSilence	= "Spell Block in 0.5 Seconds!",
-	SpecialWarningSpray		= "Paralytic Spray on You",
-	SpecialWarningToxin		= "Toxin on You! Move!",
-	SpecialWarningCharge	= "Charge on You! Run!"
+	WarningImpale				= "%s on >%s<",
+	WarningFireBomb				= "Fire Bomb",
+--	WarningSpray				= "%s on >%s<",
+	WarningBreath				= "Arctic Breath",
+	WarningRage					= "Frothing Rages",
+	WarningCharge				= "Charge on >%s<",
+	SpecialWarningImpale3		= "Impale >%d< on You",
+	SpecialWarningFireBomb		= "Fire Bomb on You",
+	SpecialWarningSlimePool		= "Slime Pool, Move out!",
+	SpecialWarningSilence		= "Spell Block in 0.5 Seconds!",
+	SpecialWarningSpray			= "Paralytic Spray on You",
+	SpecialWarningToxin			= "Toxin on You! Move!",
+	SpecialWarningCharge		= "Charge on You! Run!",
+	SpecialWarningChargeNear	= "Charge near You! Run!"
 }
 
 
