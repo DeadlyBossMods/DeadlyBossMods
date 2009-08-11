e2f23d311e0a1c5cf2abda0f3d569260c2dc7d7f
Azael <paul.poon.kw@gmail.com>
2009-08-11 09:37:34 +0000

    zhtw update

--- DBM-Coliseum/localization.tw.lua
+++ DBM-Coliseum/localization.tw.lua
@@ -12,15 +12,18 @@ L:SetGeneralLocalization{
 }
 
 L:SetMiscLocalization{
-	Charge	= "^%%s怒視著(%S+)，並發出震耳的咆哮!",
+	Charge				= "^%%s怒視著(%S+)，並發出震耳的咆哮!",
+	CombatStart			= "來自風暴群山最深邃，最黑暗的洞穴。歡迎『穿刺者』戈莫克!戰鬥吧，英雄們!";
 }
 
 L:SetOptionLocalization{
 	WarningImpale			= "為刺穿顯示警告",
 	WarningFireBomb			= "為燃燒彈顯示警告",
 	WarningBreath			= "為寒地之息顯示警告",
-	WarningSpray			= "為痲痺噴霧顯示警告",
+--	WarningSpray			= "為痲痺噴霧顯示警告",
 	WarningRage			= "為泡沫之怒顯示警告",
+	WarningCharge			= "為撞擊的目標顯示警告",
+	WarningToxin			= "為痲痺劇毒的目標顯示警告",
 	SpecialWarningImpale3		= "為刺穿(大於3層)顯示特別警告",
 	SpecialWarningFireBomb		= "當你中了燃燒彈時顯示特別警告",
 	SpecialWarningSlimePool		= "為泥漿池顯示特別警告",
@@ -28,21 +31,27 @@ L:SetOptionLocalization{
 	SpecialWarningSpray		= "當你中了痲痺噴霧時顯示特別警告",
 	SpecialWarningToxin		= "當你中了痲痺劇毒時顯示特別警告",
 	SpecialWarningCharge		= "當冰嚎即將撞擊你時顯示特別警告"
+	SpecialWarningChargeNear	= "當冰嚎的撞擊接近你時顯示特別警告",
+	SetIconOnChargeTarget		= "為撞擊的目標設置標記(頭顱)",
+	SetIconOnToxinTarget		= "為痲痺劇毒的目標設置標記(頭顱)",
 }
 
 L:SetWarningLocalization{
 	WarningImpale			= "%s: >%s<",
 	WarningFireBomb			= "燃燒彈",
-	WarningSpray			= "%s: >%s<",
+--	WarningSpray			= "%s: >%s<",
 	WarningBreath			= "寒地之息",
 	WarningRage			= "泡沫之怒",
-	SpecialWarningImpale3		= "你中了刺穿!",
+	WarningCharge			= "撞擊: >%s<",
+	WarningToxin			= "痲痺劇毒: >%s<",
+	SpecialWarningImpale3		= "你中了刺穿>%d<!",
 	SpecialWarningFireBomb		= "你中了燃燒彈!",
 	SpecialWarningSlimePool		= "泥漿池, 跑開!",
 	SpecialWarningSilence		= "0.5秒後 法術沉默!",
 	SpecialWarningSpray		= "你中了痲痺噴霧!",
 	SpecialWarningToxin		= "痲痺劇毒! 跑開!",
-	SpecialWarningCharge		= "你中了撞擊! 跑開!"
+	SpecialWarningCharge		= "你中了撞擊! 跑開!",
+	SpecialWarningChargeNear	= "撞擊接近你! 跑開!"
 }
 
 
