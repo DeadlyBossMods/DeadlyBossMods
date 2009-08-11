ab43822f82e24222337263c7ca7bc17759cef4c6
Azael <Azael@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-08-11 10:51:17 +0000

    zhtw update

--- DBM-Coliseum/localization.tw.lua
+++ DBM-Coliseum/localization.tw.lua
@@ -24,6 +24,7 @@ L:SetOptionLocalization{
 	WarningRage			= "為泡沫之怒顯示警告",
 	WarningCharge			= "為撞擊的目標顯示警告",
 	WarningToxin			= "為痲痺劇毒的目標顯示警告",
+	WarningBile			= "為燃燒膽汁的目標顯示警告",
 	SpecialWarningImpale3		= "為刺穿(大於3層)顯示特別警告",
 	SpecialWarningFireBomb		= "當你中了燃燒彈時顯示特別警告",
 	SpecialWarningSlimePool		= "為泥漿池顯示特別警告",
@@ -33,7 +34,7 @@ L:SetOptionLocalization{
 	SpecialWarningCharge		= "當冰嚎即將撞擊你時顯示特別警告"
 	SpecialWarningChargeNear	= "當冰嚎的撞擊接近你時顯示特別警告",
 	SetIconOnChargeTarget		= "為撞擊的目標設置標記(頭顱)",
-	SetIconOnToxinTarget		= "為痲痺劇毒的目標設置標記(頭顱)",
+	SetIconOnBileTarget		= "為燃燒膽汁的目標設置標記",
 }
 
 L:SetWarningLocalization{
@@ -44,6 +45,7 @@ L:SetWarningLocalization{
 	WarningRage			= "泡沫之怒",
 	WarningCharge			= "撞擊: >%s<",
 	WarningToxin			= "痲痺劇毒: >%s<",
+	WarningBile			= "燃燒膽汁: >%s<",
 	SpecialWarningImpale3		= "你中了刺穿>%d<!",
 	SpecialWarningFireBomb		= "你中了燃燒彈!",
 	SpecialWarningSlimePool		= "泥漿池, 跑開!",
@@ -68,22 +70,22 @@ L:SetTimerLocalization{
 }
 
 L:SetWarningLocalization{
-	PortalSoonWarning	= "虛空傳送門 即將到來!",
-	VolcanoSoonWarning	= "煉獄爆發 即將到來!",
-	WarnFlame		= "聚合烈焰: >%s<!"
+	PortalSoonWarning		= "虛空傳送門 即將到來!",
+	VolcanoSoonWarning		= "煉獄爆發 即將到來!",
+	WarnFlame			= "聚合烈焰: >%s<!"
 }
 
 L:SetMiscLocalization{
-	SpecWarnFlame		= "你中了聚合烈焰!",
-	SpecWarnFlesh		= "你中了焚化血肉!"
+	SpecWarnFlame			= "你中了聚合烈焰!",
+	SpecWarnFlesh			= "你中了焚化血肉!"
 }
 
 L:SetOptionLocalization{
-	PortalSoonWarning	= "預先提示虛空傳送門的出現",
-	VolcanoSoonWarning	= "預先提示煉獄爆發的出現",
-	WarnFlame		= "為聚合烈焰顯示警告",
-	SpecWarnFlame		= "當你中了聚合烈焰時顯示特別警告",
-	SpecWarnFlesh		= "當你中了焚化血肉時顯示特別警告"
+	PortalSoonWarning		= "預先提示虛空傳送門的出現",
+	VolcanoSoonWarning		= "預先提示煉獄爆發的出現",
+	WarnFlame			= "為聚合烈焰顯示警告",
+	SpecWarnFlame			= "當你中了聚合烈焰時顯示特別警告",
+	SpecWarnFlesh			= "當你中了焚化血肉時顯示特別警告"
 }
 
 
@@ -103,20 +105,20 @@ L:SetWarningLocalization{
 }
 
 L:SetMiscLocalization{
-	Gorgrim		= "DK - 高葛林·影斬",	-- 34458
-	Birana 		= "D - 碧菈娜·風暴之蹄",-- 34451
-	Erin		= "D - 艾琳·霧蹄",	-- 34459
-	Rujkah		= "H - 茹卡",		-- 34448
-	Ginselle	= "M - 金賽兒·凋擲",	-- 34449
-	Liandra		= "P - 黎安卓·喚日",	-- 45
-	Malithas	= "P - 瑪力薩·亮刃",	-- 56
-	Caiphus		= "PR - 嚴厲的凱普司",	-- 47
-	Vivienne	= "PR - 薇薇安·黑語",	-- 41
-	Mazdinah	= "R - 馬茲迪娜",	-- 54
-	Thrakgar	= "S - 瑟瑞克加爾",	--  44
-	Broln		= "S - 伯洛連·頑角",	-- 55
-	Harkzog		= "WL - 哈克佐格",	-- 50
-	Narrhok		= "W - 納霍克·破鋼者"	-- 53
+	Gorgrim				= "DK - 高葛林·影斬",	-- 34458
+	Birana 				= "D - 碧菈娜·風暴之蹄",-- 34451
+	Erin				= "D - 艾琳·霧蹄",	-- 34459
+	Rujkah				= "H - 茹卡",		-- 34448
+	Ginselle			= "M - 金賽兒·凋擲",	-- 34449
+	Liandra				= "P - 黎安卓·喚日",	-- 45
+	Malithas			= "P - 瑪力薩·亮刃",	-- 56
+	Caiphus				= "PR - 嚴厲的凱普司",	-- 47
+	Vivienne			= "PR - 薇薇安·黑語",	-- 41
+	Mazdinah			= "R - 馬茲迪娜",	-- 54
+	Thrakgar			= "S - 瑟瑞克加爾",	--  44
+	Broln				= "S - 伯洛連·頑角",	-- 55
+	Harkzog				= "WL - 哈克佐格",	-- 50
+	Narrhok				= "W - 納霍克·破鋼者"	-- 53
 } 
 
 L:SetOptionLocalization{
@@ -133,24 +135,24 @@ L:SetGeneralLocalization{
 }
 
 L:SetTimerLocalization{
-	TimerSpecialSpell	= "下一次 特別技能"	
+	TimerSpecialSpell		= "下一次 特別技能"	
 }
 
 L:SetWarningLocalization{
-	WarnSpecialSpellSoon	= "特別技能 即將到來!",
-	SpecWarnSpecial		= "顏色變換!"
+	WarnSpecialSpellSoon		= "特別技能 即將到來!",
+	SpecWarnSpecial			= "顏色變換!"
 }
 
 L:SetMiscLocalization{
-	YellPull 	= "以我們的黑暗君王之名。為了巫妖王。你‧得‧死。",
-	Fjola 		= "菲歐拉·光寂",
-	Eydis		= "艾狄絲·暗寂"
+	YellPull 			= "以我們的黑暗君王之名。為了巫妖王。你‧得‧死。",
+	Fjola 				= "菲歐拉·光寂",
+	Eydis				= "艾狄絲·暗寂"
 }
 
 L:SetOptionLocalization{
-	TimerSpecialSpell	= "為下一次 特別技能顯示計時器",
-	WarnSpecialSpellSoon	= "為下一次 特別技能預先提示",
-	SpecWarnSpecial		= "當你需要變換焚化血肉時顯示特別警告"
+	TimerSpecialSpell		= "為下一次 特別技能顯示計時器",
+	WarnSpecialSpellSoon		= "為下一次 特別技能預先提示",
+	SpecWarnSpecial			= "當你需要變換焚化血肉時顯示特別警告"
 }
 
 
@@ -167,17 +169,17 @@ L:SetTimerLocalization{
 }
 
 L:SetWarningLocalization{
-	WarnBurrow	= "鑽地!",
-	WarnPursue	= "追擊尖刺: >%s<",
-	SpecWarnPursue	= "你中了追擊尖刺!"
+	WarnBurrow			= "鑽地!",
+	WarnPursue			= "追擊尖刺: >%s<",
+	SpecWarnPursue			= "你中了追擊尖刺!"
 }
 
 L:SetMiscLocalization{
-	YellPull	= "這裡將會是你們的墳墓!"
+	YellPull			= "這裡將會是你們的墳墓!"
 }
 
 L:SetOptionLocalization{
-	WarnBurrow	= "為隱沒(鑽地)顯示警告",
-	WarnPursue	= "警告誰受到了追擊",
-	SpecWarnPursue	= "當你受到追擊時顯示特別警告"
+	WarnBurrow			= "為隱沒(鑽地)顯示警告",
+	WarnPursue			= "警告誰受到了追擊",
+	SpecWarnPursue			= "當你受到追擊時顯示特別警告"
 }
