033841b0bb4a870e7526f6c6e79707b8a293d448
Azael <paul.poon.kw@gmail.com>
2009-08-12 12:13:21 +0000

    zhtw update

--- DBM-Coliseum/localization.tw.lua
+++ DBM-Coliseum/localization.tw.lua
@@ -73,12 +73,17 @@ L:SetTimerLocalization{
 L:SetWarningLocalization{
 	PortalSoonWarning		= "虛空傳送門 即將到來!",
 	VolcanoSoonWarning		= "煉獄爆發 即將到來!",
-	WarnFlame			= "聚合烈焰: >%s<!"
+	WarnFlame			= "聚合烈焰: >%s<",
+	WarnTouch			= "賈拉克瑟斯之觸: >%s<"
 }
 
 L:SetMiscLocalization{
 	SpecWarnFlame			= "你中了聚合烈焰!",
-	SpecWarnFlesh			= "你中了焚化血肉!"
+	SpecWarnFlesh			= "你中了焚化血肉!",
+	WhisperFlame			= "你中了聚合烈焰!",
+	SpecWarnTouch			= "你中了賈拉克瑟斯之觸!",
+	SpecWarnKiss			= "仕女之吻",
+	SpecWarnTouchNear		= "接近你的%s中了賈拉克瑟斯之觸!" 
 }
 
 L:SetOptionLocalization{
@@ -86,7 +91,15 @@ L:SetOptionLocalization{
 	VolcanoSoonWarning		= "預先提示煉獄爆發的出現",
 	WarnFlame			= "為聚合烈焰顯示警告",
 	SpecWarnFlame			= "當你中了聚合烈焰時顯示特別警告",
-	SpecWarnFlesh			= "當你中了焚化血肉時顯示特別警告"
+	SpecWarnFlesh			= "當你中了焚化血肉時顯示特別警告",
+	IncinerateFleshIcon		= "為焚化血肉的目標設置標記",
+	LegionFlameIcon			= "為聚合烈焰的目標設置標記",
+	LegionFlameWhisper		= "密語提示中了聚合烈焰的人",
+	TouchJaraxxusIcon		= "為賈拉克瑟斯之觸的目標設置標記",
+	WarnTouch			= "為賈拉克瑟斯之觸顯示警告",
+	SpecWarnTouch			= "當你中了賈拉克瑟斯之觸時顯示特別警告",
+	SpecWarnTouchNear		= "當你附近的人中了賈拉克瑟斯之觸時顯示特別警告",
+	SpecWarnKiss			= "當你中了仕女之吻時顯示特別警告"
 }
 
 
