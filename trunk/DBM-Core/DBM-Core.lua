41ac1c5f752328d9499ae92d5d8898d262a97e77
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-08-12 15:06:00 +0000

    incremented version number (4.24 alpha, 4.23 release)

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -38,8 +38,8 @@
 -------------------------------
 DBM = {
 	Revision = ("$Revision$"):sub(12, -3),
-	Version = "4.22",
-	DisplayVersion = "4.23 alpha"
+	Version = "4.23",
+	DisplayVersion = "4.24 alpha"
 }
 
 DBM_SavedOptions = {}
