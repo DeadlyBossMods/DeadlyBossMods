36b37e259407718e9769ff2e1dd82a9b374276dc
bluenyx <bluenyx@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-08-12 16:19:21 +0000

    koKR update (for DBM-Core r1343, Jaraxxus r1346)

--- DBM-Coliseum/localization.kr.lua
+++ DBM-Coliseum/localization.kr.lua
@@ -50,7 +50,7 @@ L:SetWarningLocalization{
 	SpecialWarningImpale3		= "당신에게 꿰뚫기(3중첩 이상)",
 	SpecialWarningFireBomb		= "당신에게 화염 폭탄!",
 	SpecialWarningSlimePool		= "독 구름, 벗어나세요!",
-	SpecialWarningSilence		= "주문 차단 0.5 초 전!!!",
+	SpecialWarningSilence		= "1.5 초이내 주문 차단!!!",
 	SpecialWarningSpray			= "당신에게 마비의 이빨!",
 	SpecialWarningToxin			= "당신에게 마비독! 이동하세요!",
 	SpecialWarningCharge		= "당신에게 사나운 돌진! 뛰세요!",
@@ -67,39 +67,42 @@ L:SetGeneralLocalization{
 	name = "군주 자락서스"
 }
 
-L:SetTimerLocalization{
-}
-
 L:SetWarningLocalization{
-	PortalSoonWarning	= "곧 황천의 차원문!",
-	VolcanoSoonWarning	= "곧 지옥불 정령 분출!",
-	WarnFlame			= "군단의 불꽃 : >%s<!",
-	WarnTouch			= "군단의 불길 : >%s<"
+	WarnFlame			= "군단 불꽃 : >%s<!",
+	WarnTouch			= "자락서스의 손길 : >%s<",
+	WarnNetherPower			= "자락서스가 황천의 힘을 얻었습니다! 해제하세요!",
+	WarnPortalSoon			= "곧 황천의 차원문!",
+	WarnVolcanoSoon			= "곧 지옥불 정령 분출!",
+	SpecWarnFlesh			= "당신에게 살점 소각!",
+	SpecWarnTouch			= "당신에게 자락서스의 손길!",
+	SpecWarnKiss			= "여군주의 키스",
+	SpecWarnTouchNear		= "당신 주변에 자락서스의 손길 : >%s<",
+	SpecWarnFlame			= "군단 불꽃! 뛰세요!",
+	SpecWarnNetherPower		= "지금 해제!",
+	SpecWarnFelInferno		= "군단 불꽃! 움직이세요!"	
 }
 
 L:SetMiscLocalization{
-	SpecWarnFlame		= "당신에게 군단의 불꽃!",
-	SpecWarnFlesh		= "당신에게 살점 소각!",
 	WhisperFlame		= "당신에게 군단의 불꽃!",
-	SpecWarnTouch		= "당신에게 군단의 불길!",
-	SpecWarnKiss		= "여군주의 키스",
-	SpecWarnTouchNear	= "당신 주변의 군단의 불길 : >%s<"
 }
 
 L:SetOptionLocalization{
-	PortalSoonWarning	= "황천의 차원문 사전 경고 알리기",
-	VolcanoSoonWarning	= "지옥불 정령 분출 사전 경고 알리기",
-	WarnFlame			= "군단의 불꽃 경고하기",
-	SpecWarnFlame		= "군단의 불꽃에 걸렸을 경우 특수 경보 알리기",
-	SpecWarnFlesh		= "살점 소각에 걸렸을 경우 특수 경보 알리기",
-	IncinerateFleshIcon	= "살점 소각의 공격대 대상 아이콘 설정",
-	LegionFlameIcon		= "군단의 불꽃의 공격대 대상 아이콘 설정",
-	LegionFlameWhisper	= "군단의 불꽃 대상에게 귓속말 보내기",
-	TouchJaraxxusIcon	= "군단의 불길의 공격대 대상 아이콘 설정",
-	WarnTouch			= "군단의 불길 경보 알리기",
-	SpecWarnTouch		= "당신이 군단의 불길에 걸렸을 경우 특수 경고 알리기",
-	SpecWarnTouchNear	= "당신 주변에 군단의 불길이 존재할 경우 특수 경고 알리기",
-	SpecWarnKiss		= "당신이 여군주의 키스에 걸렸을 경우 특수 경고 알리기"
+	WarnFlame				= "군단 불꽃 경고 알리기",
+	WarnTouch				= "자락서스의 손길 경고 알리기",
+	WarnNetherPower			= "자락서스가 황천의 힘을 얻었을 경우 경고 하기 (해제를 위한 경고)",
+	WarnPortalSoon			= "황천의 차원문 생성 사전 경고 알리기",
+	WarnVolcanoSoon			= "지옥불 정령 분출 사전 경고 알리기",
+	SpecWarnFlame			= "군단 불꽃에 걸렸을 경우 특수 경보 알리기",
+	SpecWarnFlesh			= "살점 소각에 걸렸을 경우 특수 경보 알리기",
+	SpecWarnTouch			= "당신이 자락서스의 손길에 걸렸을 경우 특수 경고 알리기",
+	SpecWarnTouchNear		= "당신 주변의 플레이어가 자락서스의 손길에 걸렸을 경우 특수 경고 알리기",
+	SpecWarnKiss			= "당신이 여군주의 키스에 걸렸을 경우 특수 경고 알리기",
+	SpecWarnNetherPower		= "황천의 힘 특수 경고 알리기 (자락서스 해제클래스-법사/사제/주술사)",
+	SpecWarnFelInferno		= "당신 주변에 군단 불꽃이 있을 경우 특수 경고 알리기(바닥)",
+	TouchJaraxxusIcon		= "자락서스의 손길 대상 공격대 아이콘 설정 (엑스)",
+	IncinerateFleshIcon		= "살점 소각의 공격대 대상 아이콘 설정 (해골)",
+	LegionFlameIcon			= "군단의 불꽃의 공격대 대상 아이콘 설정 (네모)",
+	LegionFlameWhisper		= "군단의 불꽃 대상에게 귓속말 보내기",
 }
 
 
