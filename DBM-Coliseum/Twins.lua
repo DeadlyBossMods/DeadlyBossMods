3e25aa278d5e7108afd9bb1bc8c7fb45a27567d2
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-08-26 10:42:23 +0000

    Twins update (10ppl)

--- DBM-Coliseum/Twins.lua
+++ DBM-Coliseum/Twins.lua
@@ -8,7 +8,8 @@ mod:RegisterCombat("yell", L.YellPull)
 
 mod:RegisterEvents(
 	"SPELL_CAST_START",
-	"CHAT_MSG_MONSTER_YELL"
+	"CHAT_MSG_MONSTER_YELL",
+	"SPELL_AURA_APPLIED"
 )
 
 mod:SetBossHealthInfo(
@@ -16,9 +17,17 @@ mod:SetBossHealthInfo(
 	34496, L.Eydis
 )
 
+mod:AddBoolOption("HealthFrame", true)
+
+
 local warnSpecial			= mod:NewAnnounce("WarnSpecialSpellSoon", 2)	
 local timerSpecial			= mod:NewTimer(45, "TimerSpecialSpell")
-local specWarnSpecial			= mod:NewSpecialWarning("SpecWarnSpecial")
+local specWarnSpecial		= mod:NewSpecialWarning("SpecWarnSpecial")
+
+
+local timerHeal						= mod:NewCastTimer(15, 65875)
+local specWarnEmpoweredDarkness		= mod:NewSpecialWarning("SpecWarnEmpoweredDarkness")
+local specWarnEmpoweredLight		= mod:NewSpecialWarning("SpecWarnEmpoweredLight")
 
 function mod:OnCombatStart(delay)
 	timerSpecial:Start(-delay)
@@ -29,18 +38,21 @@ local LightEssence = GetSpellInfo(67223)
 local DarkEssence = GetSpellInfo(67176)
 
 function mod:SPELL_CAST_START(args)
-	if args.spellId == 67206 then 		-- Light Vortex
+	if args.spellId == 66046 or args.spellId == 67206 then 		-- Light Vortex
 		local debuff = UnitDebuff("player", LightEssence)
 		self:SpecialAbility(debuff)
-	elseif args.spellId == 67192 then	-- Dark Vortex
+	elseif args.spellId == 66058 or args.spellId == 67192 then	-- Dark Vortex
 		local debuff = UnitDebuff("player", DarkEssence)
 		self:SpecialAbility(debuff)
-	elseif args.spellId == 67303 then 	-- Dark Pact
+	elseif args.spellId == 67303 then 	-- Twin's Pact
 		local debuff = true
 		self:SpecialAbility(debuff)
 	elseif args.spellId == 67306 then	-- Light Pact
 		local debuff = true
 		self:SpecialAbility(debuff)
+
+	elseif args.spellId == 65875 or args.spellId == 65876 then		-- Heal
+		timerHeal:Start()
 	end
 end
 
@@ -51,3 +63,12 @@ function mod:SpecialAbility(debuff)
 	timerSpecial:Start()
 	warnSpecial:Schedule(40)
 end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args.spellId == 65724 and args.destName == UnitName("player") then 		-- Empowered Darkness
+		specWarnEmpowered:Show()
+	elseif args.spellId == 65748 and args.destName == UnitName("player") then	-- Empowered Light
+		specWarnEmpoweredLight:Show()
+	end
+end
+
