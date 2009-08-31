9abbdb22247d240e11d64528f7a0f6b5bf0805f5
1nn7erplay <1nn7erplay@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-08-31 11:49:46 +0000

    Spanish Localizations for GUI

--- DBM-GUI/DBM-GUI.lua
+++ DBM-GUI/DBM-GUI.lua
@@ -13,6 +13,7 @@
 --    * zhCN: Diablohu                   http://wow.gamespot.com.cn
 --    * zhTW: Hman			 herman_c1@hotmail.com
 --    * zhTW: Azael/kc10577				kc10577@hotmail.com
+--    * esES: Interplay/1nn7erpLaY      http://www.1nn7erpLaY.com
 --    * (add your names here!)
 --
 -- Special thanks to:
