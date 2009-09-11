71eeb64ac524ec1cb56493ddbfcb072053926a79
Paul Emmerich <emmericp@net.in.tum.de>
2009-09-11 16:07:49 +0000

    fixed anub kill detection

--- DBM-Coliseum/Anub'arak.lua
+++ DBM-Coliseum/Anub'arak.lua
@@ -2,7 +2,7 @@
 local L = mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(34660)  
+mod:SetCreatureID(34564)  
 
 mod:RegisterCombat("yell", L.YellPull)
 
