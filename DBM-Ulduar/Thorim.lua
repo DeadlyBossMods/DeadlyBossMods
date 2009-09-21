d7d8d26bbd19883ac2546136c2170ae955143fce
Paul Emmerich <emmericp@net.in.tum.de>
2009-09-21 23:40:54 +0000

    fixed Thorim Unbalancing Strike timer

--- DBM-Ulduar/Thorim.lua
+++ DBM-Ulduar/Thorim.lua
@@ -18,7 +18,7 @@ local enrageTimer		= mod:NewEnrageTimer(369)
 
 local timerStormhammer		= mod:NewCastTimer(16, 62042)
 local timerLightningCharge 	= mod:NewCDTimer(16, 62466) 
-local timerUnbalancingStrike	= mod:NewCastTimer(21, 62130)
+local timerUnbalancingStrike	= mod:NewCastTimer(26, 62130)
 local timerHardmode		= mod:NewTimer(175, "TimerHardmode", 62042)
 
 local warnPhase2		= mod:NewAnnounce("WarningPhase2", 1)
