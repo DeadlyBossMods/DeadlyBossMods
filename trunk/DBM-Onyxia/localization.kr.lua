d92f4bc72b2fa9fd394fb38881a86978c9621182
bluenyx <bluenyx@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-09-24 14:16:10 +0000

    koKR update(for Onyxia sound -_-;;)

--- trunk/DBM-Onyxia/localization.kr.lua
+++ trunk/DBM-Onyxia/localization.kr.lua
@@ -13,12 +13,12 @@ L:SetGeneralLocalization{
 
 L:SetWarningLocalization{
 	SpecWarnBreath	= "깊은 숨",
-	WarnWhelpsSoon	= "곧 새끼용(파수병) 등장",
+	WarnWhelpsSoon	= "곧 새끼용 등장",
 }
 
 L:SetTimerLocalization{
 	TimerBreath		= "깊은 숨",
-	TimerWhelps 	= "새끼용(파수병)"
+	TimerWhelps 	= "새끼용"
 }
 
 L:SetOptionLocalization{
@@ -26,11 +26,12 @@ L:SetOptionLocalization{
 	TimerBreath		= "깊은 숨의 타이머 보기",
 	TimerWhelps		= "새끼용 등장 타이머 보기",
 	WarnWhelpsSoon	= "새끼용 등장의 사전 경고 보기",
-	SoundBreath		= "깊은 숨의 경고 소리 재생"
+	SoundBreath		= "깊은 숨의 경고 소리 재생",
+	SoundWTF		= "독특한 레이드를 즐기기위한 웃긴 소리 재생.(가급적 안하시길 권장)"
 }
 
 L:SetMiscLocalization{ -- these yells are copy and pasted from our old onyxia mod...I don't know if they still work ;)
-	Breath = "%s 숨을 깊게 들이쉽니다...",
-	YellP2 = "This meaningless exertion bores me. I'll incinerate you all from above!",
-	YellP3 = "It seems you'll need another lesson, mortals!"
+	Breath = "%s 숨을 깊게 들이쉽니다.",
+	YellP2 = "쓸데없이 힘을 쓰는 것도 지루하군. 네 녀석들 머리 위에서 모조리 불살라 주마!",
+	YellP3 = "혼이 더 나야 정신을 차리겠구나!"
 }
