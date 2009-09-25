971c594e593f440c5fcdd239608a2da7f9b8aefc
Azael <paul.poon.kw@gmail.com>
2009-09-25 15:09:19 +0000

    zhtw update

--- DBM-Onyxia/localization.tw.lua
+++ DBM-Onyxia/localization.tw.lua
@@ -30,8 +30,8 @@ L:SetOptionLocalization{
 	SoundWTF		= "為經典傳奇式奧妮克希亞副本播放一些有趣的音效"
 }
 
-L:SetMiscLocalization{ -- these yells are copy and pasted from our old onyxia mod...I don't know if they still work ;)
-	Breath 			= "%s深深地吸了一口氣...",
+L:SetMiscLocalization{
+	Breath 			= "%s深深地吸了一口氣……",
 	YellP2 			= "這毫無意義的行動讓我很厭煩。我會從上空把你們都燒成灰!",
 	YellP3 			= "看起來需要再給你一次教訓，凡人!"
 }
