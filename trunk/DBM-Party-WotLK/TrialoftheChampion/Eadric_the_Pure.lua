5a7fcb5db26418283b2c93c92777969496f890e9
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-09-29 10:34:58 +0000

    Added "SetUsedIcons" to match the new icon feature and maintain consistency with Tandanu's hard work :)

--- trunk/DBM-Party-WotLK/TrialoftheChampion/Eadric_the_Pure.lua
+++ trunk/DBM-Party-WotLK/TrialoftheChampion/Eadric_the_Pure.lua
@@ -1,8 +1,9 @@
 local mod = DBM:NewMod("EadricthePure", "DBM-Party-WotLK", 13)
 local L = mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 1663 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 1664 $"):sub(12, -3))
 mod:SetCreatureID(35119)
+mod:SetUsedIcons(8)
 --mod:SetZone()
 
 mod:RegisterCombat("combat")
