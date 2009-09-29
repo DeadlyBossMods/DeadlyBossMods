161ad8a48b38600a739fc296f65b476d61f0770d
Adam <MysticalOS@users.noreply.github.com>
2009-09-29 10:09:03 +0000

    As deadly as Loken's "Mystic Autumn's Glow" spell was, I thought Lightning Nova made more sense in english local ;) (fixed spellid typo that's been present since probably wow 3.0.2). Changed Kronus local string to "Loken" for english since that is his bossname in English Client.
    Added speed kill achievement timers to Loken, Anub, Maiden of Grief, Volazj.
    Touched Black knight and Eadric the Pure with seticon options (off by default)

--- DBM-Party-WotLK/TrialoftheChampion/Eadric_the_Pure.lua
+++ DBM-Party-WotLK/TrialoftheChampion/Eadric_the_Pure.lua
@@ -1,7 +1,7 @@
 local mod = DBM:NewMod("EadricthePure", "DBM-Party-WotLK", 13)
 local L = mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 559 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 1663 $"):sub(12, -3))
 mod:SetCreatureID(35119)
 --mod:SetZone()
 
@@ -20,6 +20,8 @@ local warnHammerofJustice		= mod:NewTargetAnnounce(66940)
 local specwarnRadiance	= mod:NewSpecialWarning("specwarnRadiance")
 local specwarnHammerofJustice	= mod:NewSpecialWarning("specwarnHammerofJustice", isDispeller)
 
+mod:AddBoolOption("SetIconOnHammerTarget", false)
+
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(66935, 66862, 67681) then							-- Radiance Look Away!
 		specwarnRadiance:Show()
@@ -30,6 +32,9 @@ end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(66940) then							-- Hammer of Justice on <Player>
+		if self.Options.SetIconOnHammerTarget then
+			self:SetIcon(args.destName, 8, 6)
+		end
 		warnHammerofJustice:Show(args.destName)
 		specwarnHammerofJustice:Show(args.destName)
 	end
