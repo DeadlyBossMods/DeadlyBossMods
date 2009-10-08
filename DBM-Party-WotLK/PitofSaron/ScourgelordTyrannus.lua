543cf0e357ad8ddf900c0d41a18fcbe45902bb74
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-10-08 10:34:48 +0000

    update for Omegal - changed Hoarfrost detection

--- DBM-Party-WotLK/PitofSaron/ScourgelordTyrannus.lua
+++ DBM-Party-WotLK/PitofSaron/ScourgelordTyrannus.lua
@@ -3,7 +3,7 @@ local L = mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 1665 $"):sub(12, -3))
 mod:SetCreatureID(12800)
---mod:SetUsedIcons(8)
+mod:SetUsedIcons(8)
 
 mod:RegisterCombat("combat")
 
@@ -14,23 +14,27 @@ mod:RegisterEvents(
 	"SPELL_PERIODIC_DAMAGE"
 )
 
-local timerForcefulSmash	= mod:NewNextTimer(40, 69627) --Experimental, Timer may not be exact. Aso needs to be added to boss landing (jumpping off drake) event. But need BOSS emote for it first.
-local warnUnholyPower				= mod:NewAnnounce("warnUnholyPower")
+local timerForcefulSmash			= mod:NewNextTimer(40, 69627) --Experimental, Timer may not be exact. Aso needs to be added to boss landing (jumpping off drake) event. But need BOSS emote for it first.
+local warnUnholyPower				= mod:NewSpellAnnounce(69167)
 local timerUnholyPower				= mod:NewBuffActiveTimer(10, 69629)
 local warnOverlordsBrand			= mod:NewTargetAnnounce(69172)
-local timerOverlordsBrand				= mod:NewTargetTimer(8, 69172)
-local warnHoarfrost				= mod:NewAnnounce("warnHoarfrost")
-local specWarnIcyBlast		= mod:NewSpecialWarning("specWarnIcyBlast")
+local timerOverlordsBrand			= mod:NewTargetTimer(8, 69172)
+local specWarnIcyBlast				= mod:NewSpecialWarning("specWarnIcyBlast")
 
---mod:AddBoolOption("SetIconOnHoarfrostTarget", true) --Needs chatlog data to finish implimentation
+-- Hoarfrost ID 69246
+local warnHoarfrost					= mod:NewSpellAnnounce(69246)
+local specWarnHoarfrost				= mod:NewSpecialWarning("specWarnHoarfrost")
+local specWarnHoerfrostNear			= mod:NewSpecialWarning("specWarnHoarfrostNear")
+
+
+mod:AddBoolOption("SetIconOnHoarfrostTarget", true, "announce")
+
 
 function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(69629, 69167) then							-- Unholy Power
+	if args:IsSpellID(69629, 69167) then						-- Unholy Power
 		warnUnholyPower:Show(args.spellName)
---	elseif args:IsSpellID(69246) then							-- Trying a new method for this and using BOSS EMOTE instead, this method just wasn't working.
---		warnHoarfrost:Show(args.spellName)
-	elseif args:IsSpellID(69155, 69627) then							-- Forceful Smash
-	timerForcefulSmash:Start()
+	elseif args:IsSpellID(69155, 69627) then					-- Forceful Smash
+		timerForcefulSmash:Start()
 	end
 end
 
@@ -44,18 +48,40 @@ do
 	end
 end
 
-	function mod:SPELL_AURA_APPLIED(args)
-		if args:IsSpellID(69629, 69167) then							-- Unholy Power
-			timerUnholyPower:Show(args.destName)
-		elseif args:IsSpellID(69172) then							-- Overlord's Brand
-			warnOverlordsBrand:Show(args.destName)
-			timerOverlordsBrand:Show(args.destName)
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(69629, 69167) then						-- Unholy Power
+		timerUnholyPower:Show(args.destName)
+	elseif args:IsSpellID(69172) then							-- Overlord's Brand
+		warnOverlordsBrand:Show(args.destName)
+		timerOverlordsBrand:Show(args.destName)
 	end
 end
 
 function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 	local target = msg and msg:match(L.HoarfrostTarget)
 	if target then
-		warnHoarfrost:Show(target)
+		self:SendSync("Hoarfrost", target)
 	end
 end
+
+function mod:OnSync(msg, arg)
+	if msg == "Hoarfrost" then
+		warnHoarfrost:Show(arg)
+		if arg and arg == UnitName("player") then
+			specWarnHoarfrost:Show()
+		elseif arg then
+			local uId = DBM:GetRaidUnitId(arg)
+			if uId then
+				local inRange = CheckInteractDistance(uId, 2)
+				if inRange then
+					specWarnHoerfrostNear:Show()
+				end
+			end
+		end
+		if self.Options.SetIconOnHoarfrostTarget then
+			self:SetIcon(arg, 8, 5)
+		end
+	end
+end
+
+
