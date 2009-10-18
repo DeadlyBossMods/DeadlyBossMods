4b3e0d39743ecc37d39a18fd7c210b981311e5cc
Adam <MysticalOS@users.noreply.github.com>
2009-10-18 00:53:47 +0000

    Attempt some cleanup of the Toc5 mods for some issues that still remained. Hopefully will no longer announce haste if it's on player (mage spellsteal). Hopefuly will announce actual renew target instead of "unknown". Will no longer spam dispellers to dispel for time being as the shield can prevent it and a dispel warning while shielded made no sense. (needs more work to be reimplemented). Changed some Newannounce functions to newspellannounce and removed some locals that weren't needed since newannounce is depreciated for simple announcements.

--- DBM-Party-WotLK/TrialoftheChampion/Confessor.lua
+++ DBM-Party-WotLK/TrialoftheChampion/Confessor.lua
@@ -9,7 +9,6 @@ mod:RegisterCombat("combat")
 --mod:RegisterKill("yell", L.YellCombatEnd)
 
 mod:RegisterEvents(
-	"SPELL_CAST_START",
 	"SPELL_AURA_APPLIED"
 )
 
@@ -17,20 +16,16 @@ local isDispeller = select(2, UnitClass("player")) == "MAGE"
 				 or select(2, UnitClass("player")) == "PRIEST"
 				 or select(2, UnitClass("player")) == "SHAMAN"
 
-local warnReflectiveShield	= mod:NewAnnounce("warnReflectiveShield")
+local warnReflectiveShield	= mod:NewSpellAnnounce(66515)
 local warnRenew				= mod:NewSpellAnnounce(66537)
-local specwarnRenew			= mod:NewSpecialWarning("specwarnRenew", isDispeller)
-
-function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(66537, 67675) then					-- Renew
-		warnRenew:Show(args.destName)
-		specwarnRenew:Show(args.destName)
-	end
-end
+--local specwarnRenew			= mod:NewSpecialWarning("specwarnRenew", isDispeller) Commented out for time being do to it's spaming when it's wrong. Still needs to be redone to make it only warn for dispel if it's on a target that is NOT shielded by 66515. Currently it spams for dispels even when it's not dispelable.
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(66515) then							-- Shield Gained
-		warnReflectiveShield:Show(args.spellName)
+		warnReflectiveShield:Show(args.destName)
+	elseif args:IsSpellID(66537, 67675) then					-- Renew
+		warnRenew:Show(args.destName)
+--		specwarnRenew:Show(args.destName)
 	end
 end
 
