d53326f218a58981f7958019d2e16c170bd5f7ec
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-10-18 00:53:47 +0000

    Attempt some cleanup of the Toc5 mods for some issues that still remained. Hopefully will no longer announce haste if it's on player (mage spellsteal). Hopefuly will announce actual renew target instead of "unknown". Will no longer spam dispellers to dispel for time being as the shield can prevent it and a dispel warning while shielded made no sense. (needs more work to be reimplemented). Changed some Newannounce functions to newspellannounce and removed some locals that weren't needed since newannounce is depreciated for simple announcements.

--- trunk/DBM-Party-WotLK/TrialoftheChampion/Champions.lua
+++ trunk/DBM-Party-WotLK/TrialoftheChampion/Champions.lua
@@ -10,6 +10,7 @@ mod:RegisterKill("yell", L.YellCombatEnd)
 
 mod:RegisterEvents(
 	"SPELL_CAST_START",
+	"SPELL_CAST_SUCCESS",
 	"SPELL_AURA_APPLIED"
 )
 
@@ -17,10 +18,10 @@ local isDispeller = select(2, UnitClass("player")) == "MAGE"
 				 or select(2, UnitClass("player")) == "PRIEST"
 				 or select(2, UnitClass("player")) == "SHAMAN"
 
-local warnHealingWave		= mod:NewAnnounce("warnHealingWave")
+local warnHealingWave		= mod:NewSpellAnnounce(68318)
+local warnHaste				= mod:NewSpellAnnounce(66045)
 local warnPolymorph			= mod:NewTargetAnnounce(66043)
 local warnHexOfMending		= mod:NewTargetAnnounce(67534)
-local warnHaste				= mod:NewTargetAnnounce(66045)
 local specWarnPoison		= mod:NewSpecialWarning("specWarnPoison")
 local specWarnHaste			= mod:NewSpecialWarning("specWarnHaste", isDispeller)
 
@@ -30,14 +31,18 @@ function mod:SPELL_CAST_START(args)
 	end
 end
 
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(66045) then								-- Haste
+		warnHaste:Show(args.destName)
+		specWarnHaste:Show(args.destName)
+	end
+end
+
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(66043, 68311) then								-- Polymorph on <x>
 		warnPolymorph:Show(args.destName)
 	elseif args:IsSpellID(67534) then									-- Hex of Mending on <x>
 		warnHexOfMending:Show(args.destName)
-	elseif args:IsSpellID(66045) then									-- Haste on <x> Warns when on player too, don't know how to fix this.
-		warnHaste:Show(args.destName)
-		specWarnHaste:Show(args.destName)
 	elseif args:IsSpellID(67594, 68316) and args:IsPlayer() then		-- Standing in Poison Bottle.
 		specWarnPoison:Show()
 	end
