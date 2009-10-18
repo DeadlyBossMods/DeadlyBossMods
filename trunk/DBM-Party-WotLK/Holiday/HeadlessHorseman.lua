9a517828864f41aa5166d893541d737663b13864
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-10-18 07:00:59 +0000

    And done with Headless Horseman. A stupid easy and undertuned encounter just like coren direbrew. He has same HP as last year it's sad. But non the less the warnings help if you are soloing or 2 manning in blues or something ;)

--- trunk/DBM-Party-WotLK/Holiday/HeadlessHorseman.lua
+++ trunk/DBM-Party-WotLK/Holiday/HeadlessHorseman.lua
@@ -1,13 +1,32 @@
 local mod = DBM:NewMod("HeadlessHorseman", "DBM-Party-WotLK", 14)
 local L = mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 559 $"):sub(12, -3))
-mod:SetCreatureID(23682)
+mod:SetRevision(("$Revision: 1822 $"):sub(12, -3))
+mod:SetCreatureID(23682, 23775)
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
-	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS"
+	"CHAT_MSG_MONSTER_SAY"
 )
+
+local warnConflag					= mod:NewTargetAnnounce(42380)
+local timerConflag					= mod:NewTargetTimer(4, 42380)
+local specWarnHorsemanHead			= mod:NewSpecialWarning("specWarnHorsemanHead")
+local specWarnHorsemanSoldiers		= mod:NewSpecialWarning("specWarnHorsemanSoldiers")
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(42380) then											-- Conflagration
+		warnConflag:Show(args.destName)
+		timerConflag:Start(args.destName)
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_SAY(msg)
+	if msg == L.HorsemanHead then											-- No combatlog event for head spawning, Emote works iffy(head doesn't always emote right away, or at all when seperated)
+		specWarnHorsemanHead:Show()
+	elseif msg == L.HorsemanSoldiers then									-- Warning for adds spawning.
+		specWarnHorsemanSoldiers:Show()
+	end
+end
