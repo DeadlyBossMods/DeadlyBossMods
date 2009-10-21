da364c794ddd27374d242df70dec40f4f66a41b7
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-10-21 09:19:38 +0000

    Added basic bossmods for Icecrown PTR Testing 21/22 Oct - NO FUNCTION CURRENTLY! ;)

---
+++ DBM-Icecrown/Festergut.lua
@@ -0,0 +1,18 @@
+local mod = DBM:NewMod("Festergut", "DBM-Icecrown")
+local L = mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision: 1799 $"):sub(12, -3))
+mod:SetCreatureID(10)
+--mod:SetUsedIcons(3, 4, 5, 6, 7, 8)
+mod:RegisterCombat("combat")
+
+mod:RegisterEvents(
+	"SPELL_AURA_APPLIED"
+)
+
+
+
+
+
+
+
