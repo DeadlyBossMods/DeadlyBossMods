4640f8cc2fd06860d61e41b65ba60d9797509fff
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-10-25 13:16:19 +0000

    updated rotface creature ID and deDE zonename

--- trunk/DBM-Icecrown/Rotface.lua
+++ trunk/DBM-Icecrown/Rotface.lua
@@ -2,7 +2,7 @@ local mod = DBM:NewMod("Rotface", "DBM-Icecrown")
 local L = mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 1799 $"):sub(12, -3))
-mod:SetCreatureID(10)
+mod:SetCreatureID(36627)
 --mod:SetUsedIcons(3, 4, 5, 6, 7, 8)
 mod:RegisterCombat("combat")
 
