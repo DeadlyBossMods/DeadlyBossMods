fc1d698bdd726df6f7127b32327995cbc4160d26
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-10-26 22:40:44 +0000

    Void zone warnings for ChamberofAspects mod.

--- trunk/DBM-ChamberOfAspects/Vesperon.lua
+++ trunk/DBM-ChamberOfAspects/Vesperon.lua
@@ -4,3 +4,30 @@ local L = mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(30449)
 mod:SetZone()
+
+mod:RegisterCombat("combat")
+
+mod:RegisterEvents(
+    "SPELL_CAST_SUCCESS"
+)
+
+local warnShadowFissure	= mod:NewSpellAnnounce(59127)
+local timerShadowFissure = mod:NewCastTimer(5, 59128)
+local isInCombatWithVesperon = false
+
+function mod:OnCombatStart(delay)
+    isInCombatWithVesperon = true
+end
+
+function mod:OnCombatEnd()
+    isInCombatWithVesperon = false
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+    if (isInCombatWithVesperon) then
+        if (args.spellId == 57579 or args.spellId == 59127) then
+            warnShadowFissure:Show()
+            timerShadowFissure:Start
+        end
+    end
+end
