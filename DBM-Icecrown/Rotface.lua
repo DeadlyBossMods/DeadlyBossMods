9a969b8b37e4feeb6d9491f39cd39d18d64319d4
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-10-27 14:53:41 +0000

    icecrown: Rotface first test

--- DBM-Icecrown/Rotface.lua
+++ DBM-Icecrown/Rotface.lua
@@ -7,11 +7,75 @@ mod:SetCreatureID(36627)
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
-	"SPELL_AURA_APPLIED"
+	"SPELL_CAST_START",
+	"SPELL_AURA_APPLIED",
+	"SPELL_DAMAGE",
+	"CHAT_MSG_MONSTER_YELL"
 )
 
 
+local nextWallSlime			= mod:NewTimer(20, "NextPoisonSlimePipes")
+local nextSlimeSpray		= mod:NewNextTimer(21, 69508)
+local warnSlimeSpray		= mod:NewSpellAnnounce(69508)
 
+local nextStickyOoze		= mod:NewNextTimer(16, 69774)
+local warnStickyOoze		= mod:NewSpellAnnounce(69774)
 
+local specWarnStickyOoze	= mod:NewSpecialWarning("SpecWarnStickyOoze")
+mod:AddBoolOption("PlaySoundOnStickyOoze", true, "announce")
 
+local warnRadiatingOoze		= mod:NewSpellAnnounce(69760)
+local specWarnRadiatingOoze	= mod:NewSpecialWarning("SpecWarnRadiationOoze")
+
+function mod:OnCombatStart(delay)
+	nextWallSlime:Start(25-delay)
+	self:ScheduleMethod(25-delay, "WallSlime")
+end
+
+function mod:WallSlime()
+	if self:IsInCombat() then
+		nextWallSlime:Start()
+		self:UnscheduleMethod("WallSlime")
+		self:ScheduleMethod(20, "WallSlime")
+	end
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:SpellID(69508) then
+		nextSlimeSpray:Start()
+		warnSlimeSpray:Show()
+	elseif args:SpellID(69774) then
+		nextStickyOoze:Start()
+		warnStickyOoze:Show()
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsPlayer() and args:SpellID(71208) then
+		specWarnOoze:Show()
+		if self.Options.PlaySoundOnStickyOoze then
+			PlaySoundFile("Sound\\Creature\\HoodWolf\\HoodWolfTransformPlayer01.wav")
+		end
+	elseif args:SpellID(69760) then
+		warnRadiatingOoze:Show()
+	end
+end
+
+function mod:SPELL_DAMAGE(args)
+	if args:SpellID(69761, 71212) and args:IsPlayer() then
+		specWarnRadiatingOoze:Show()
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg == L.YellSlimePipes then
+		self:SendSync("PoisonSlimePipes")
+	end
+end
+
+function mod:OnSync(msg, arg)
+	if msg == "PoisonSlimePipes" then
+		self:WallSlime()
+	end
+end
 
