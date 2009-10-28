0c79c700c5c14951ceaa5603c0120b4dfe6473a7
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-10-28 14:30:18 +0000

    tag as 4.29 release

---
+++ tags/4.29r1989-stable/localization.kr.lua
@@ -0,0 +1,136 @@
+if (GetLocale() == "koKR") then
+
+DBM_CORE_LOAD_MOD_ERROR				= "%s 보스 모드 로딩중 에러 발생: %s"
+DBM_CORE_LOAD_MOD_SUCCESS			= "'%s' 보스 모드가 로드되었습니다.!"
+DBM_CORE_LOAD_GUI_ERROR				= "GUI를 로드할 수 없습니다: %s"
+
+DBM_CORE_COMBAT_STARTED				= "%s 작동됨. 행운을 빕니다! :)";
+DBM_CORE_BOSS_DOWN					= "%s (을)를 쓰러트렸습니다. (%s 소요 되었습니다!)"
+DBM_CORE_BOSS_DOWN_LONG				= "%s (을)를 쓰러트렸습니다. (%s 소요 되었습니다!) 당신의 최종 킬 타임은, %s 입니다. 그리고 당신의 제일 빠른 킬 타임은 %s 입니다.."
+DBM_CORE_BOSS_DOWN_NEW_RECORD		= "%s (을)를 쓰러뜨렸습니다. (%s 소요 되었습니다!) 이 기록은 새로운 기록이군요! (과거의 기록: %s)"
+DBM_CORE_COMBAT_ENDED				= "%s 전투가 종료되었습니다. (%s 소요 되었습니다.)"
+
+DBM_CORE_TIMER_FORMAT_SECS			= "%d |4초:초;"
+DBM_CORE_TIMER_FORMAT_MINS			= "%d |4분:분;"
+DBM_CORE_TIMER_FORMAT				= "%d |4분:분; and %d |4초:초;"
+
+DBM_CORE_MIN						= "분"
+DBM_CORE_MIN_FMT					= "%d 분"
+DBM_CORE_SEC						= "초"
+DBM_CORE_SEC_FMT					= "%d 초"
+DBM_CORE_DEAD						= "죽음"
+DBM_CORE_OK							= "수락"
+
+DBM_CORE_GENERIC_WARNING_ENRAGE		= "%s %s 후에 광폭화"
+DBM_CORE_GENERIC_TIMER_ENRAGE		= "광폭화"
+DBM_CORE_OPTION_TIMER_ENRAGE		= "광폭화 타이머 보기"
+DBM_CORE_OPTION_HEALTH_FRAME		= "보스 HP 프레임 보기"
+
+DBM_CORE_OPTION_CATEGORY_TIMERS		= "바"
+DBM_CORE_OPTION_CATEGORY_WARNINGS	= "알림"
+DBM_CORE_OPTION_CATEGORY_MISC		= "기타 추가 기능"
+
+DBM_CORE_AUTO_RESPONDED				= "자동-부활."
+DBM_CORE_STATUS_WHISPER				= "%s: %s, %d/%d 공대원이 생존해 있습니다."
+DBM_CORE_AUTO_RESPOND_WHISPER		= "%s 님은 현재 바쁩니다. %s 와 사투를 벌이고 있습니다. (%s, %d/%d 공대원 생존중) 전투가 끝난 후 통보를 받게 될 것입니다."
+
+DBM_CORE_VERSIONCHECK_HEADER		= "죽이는 보스 모드 - 버젼"
+DBM_CORE_VERSIONCHECK_ENTRY			= "%s: %s (r%d)"
+DBM_CORE_VERSIONCHECK_ENTRY_NO_DBM	= "%s: 죽이는 보스 모드가 설치 되어있지 않습니다."
+DBM_CORE_VERSIONCHECK_FOOTER		= "%d 명의 플레이어가 설치되어 있는 것을 찾았습니다."
+
+DBM_CORE_UPDATEREMINDER_HEADER		= "당신의 죽이는 보스 모드 버전이 옛날 버전입니다.\n Version %s (r%d) 가 새로운 버전이며, 최신 버전 위치:"
+DBM_CORE_UPDATEREMINDER_FOOTER		= "Ctrl-C 를 누르면 클립보드에 복사가 됩니다."
+DBM_CORE_UPDATEREMINDER_NOTAGAIN	= "새로운 버전이 검색되면 관련 창을 띄웁니다."
+
+DBM_CORE_MOVABLE_BAR				= "나를 움직이세요~_~"
+
+DBM_PIZZA_SYNC_INFO					= "|Hplayer:%1$s|h[%1$s]|h sent you a pizza timer: '%2$s'\n|HDBM:cancel:%2$s:nil|h|cff3588ff[Cancel this timer]|r|h  |HDBM:ignore:%2$s:%1$s|h|cff3588ff[Ignore timers from %1$s]|r|h"
+DBM_PIZZA_CONFIRM_IGNORE			= "Do you really want to ignore pizza timers from %s for this session?"
+DBM_PIZZA_ERROR_USAGE				= "Usage: /dbm [broadcast] timer <time> <text>"
+
+DBM_CORE_ERROR_DBMV3_LOADED			= "Deadly Boss Mods 가 실행중에 있습니다.\n당신께서 DBMv3 와 DBMv4를 동시에 설치하여 이런 상황이 발생한 것 같습니다.\n\"수락\"을 클릭하시면 DBMv3를 차단하고, UI가 다시시작 될 것입니다.\n또한, 애드온 폴더내에 존재하고 있는 DBMv3 폴더를 삭제하시길 권장합니다."
+
+DBM_CORE_MINIMAP_TOOLTIP_HEADER		= "죽이는 보스 모드"
+DBM_CORE_MINIMAP_TOOLTIP_FOOTER		= "Shift+click 또는 right-click 으로 움직이세요.\nAlt+shift+click 으로 움직이면 자유롭게 배치할 수 있습니다."
+
+DBM_CORE_RANGECHECK_HEADER			= "거리 체크 (%d m)"
+DBM_CORE_RANGECHECK_SETRANGE		= "거리 지정"
+DBM_CORE_RANGECHECK_SOUNDS			= "소리"
+DBM_CORE_RANGECHECK_SOUND_OPTION_1	= "거리 이내 1명의 플레이어가 있을 경우의 소리"
+DBM_CORE_RANGECHECK_SOUND_OPTION_2	= "1명 이상의 플레이어가 있을 경우의 소리"
+DBM_CORE_RANGECHECK_SOUND_0			= "소리 없음"
+DBM_CORE_RANGECHECK_SOUND_1			= "기본 소리"
+DBM_CORE_RANGECHECK_SOUND_2			= "짜증나는 소리(-_-;)"
+DBM_CORE_RANGECHECK_HIDE			= "숨기기"
+DBM_CORE_RANGECHECK_SETRANGE_TO		= "%d m"
+DBM_CORE_RANGECHECK_LOCK			= "프레임 잠금"
+
+DBM_CORE_SLASHCMD_HELP				= {
+	"사용 가능한 명령어:",
+	"/dbm version: 공격대 전체 버전 체크 (줄임말: ver)",
+	"/dbm unlock: 상태 바 타이머를 움직일 수 있도록 보여줍니다.(줄임말: move)",
+	"/dbm timer <숫자> <글자>: <숫자> 초만큼 진행되는 <글자>바가 생성 됨.",
+	"/dbm broadcast timer <숫자> <글자>: <숫자> 초만큼 진행되는 <글자>바를 공격대에 알립니다.(공격대 권한이 있을 경우만 작동)",
+	"/dbm help: 현재 보고 있는 도움말.",
+}
+
+DBM_ERROR_NO_PERMISSION				= "해당 명령어를 실행하기 위한 권한을 가지고 있지 않습니다."
+
+DBM_CORE_BOSSHEALTH_HIDE_FRAME		= "보스 체력 프레임 닫기"
+
+DBM_CORE_ALLIANCE					= "얼라이언스"
+DBM_CORE_HORDE						= "호드"
+
+DBM_CORE_UNKNOWN					= "알 수 없음"
+
+DBM_CORE_TIMER_PULL					= "풀링 중 입니다."
+DBM_CORE_ANNOUNCE_PULL				= "%d 초 뒤 풀링 합니다."
+DBM_CORE_ANNOUNCE_PULL_NOW			= "풀링했습니다. 긴장하세요.!"
+
+DBM_CORE_ACHIEVEMENT_TIMER_SPEED_KILL = "Speed Kill(업적 타이머)"
+
+-- Auto-generated Timer Localizations
+DBM_CORE_AUTO_TIMER_TEXTS = {
+	target = "%s: %%s",
+	cast = "%s",
+	active = "%s",	
+	cd = "%s 쿨다운",
+	next = "다음 %s",
+	achievement = "%s",	
+}
+
+DBM_CORE_AUTO_TIMER_OPTIONS = {
+	target = "|cff71d5ff|Hspell:%d|h%s|h|r 디버프 타이머 보기",
+	cast = "|cff71d5ff|Hspell:%d|h%s|h|r 시전 타이머 보기",
+	active = "|cff71d5ff|Hspell:%d|h%s|h|r 유지 타이머 보기",
+	cd = "|cff71d5ff|Hspell:%d|h%s|h|r 쿨다운 타이머 보기",
+	next = "다음 |cff71d5ff|Hspell:%d|h%s|h|r 타이머 보기",
+	achievement = "%s 위한 타이머 보기", -- translate	
+}
+
+-- Auto-generated Warning Localizations
+DBM_CORE_AUTO_ANNOUNCE_TEXTS = {
+	target = "%s : >%%s<",
+	spell = "%s",
+	cast = "주문시전 %s : %.1f 초",
+	soon = "곧 %s",
+	prewarn = "%s : %s",
+	phase = "페이즈 %d",	
+}
+
+local prewarnOption = "|cff71d5ff|Hspell:%d|h%s|h|r의 사전 경고 보기"
+DBM_CORE_AUTO_ANNOUNCE_OPTIONS = {
+	target	= "대상의 |cff71d5ff|Hspell:%d|h%s|h|r을 알리기",
+	spell 	= "|cff71d5ff|Hspell:%d|h%s|h|r을 시전할 경우 경고 보기",
+	cast = "|cff71d5ff|Hspell:%d|h%s|h|r의 시전을 시작할 경우 경고 보기",
+	soon = prewarnOption,
+	prewarn = prewarnOption,
+	phase = "페이즈 %d 의 경고 보기"	
+}
+
+-- New special warnings
+DBM_CORE_MOVE_SPECIAL_WARNING_BAR	= "특수 경고 움직이기"
+DBM_CORE_MOVE_SPECIAL_WARNING_TEXT	= "특수 경고"
+
+end
---
+++ tags/4.29r1989-stable/localization.tw.lua
@@ -0,0 +1,134 @@
+﻿if GetLocale() ~= "zhTW" then return end
+DBM_CORE_LOAD_MOD_ERROR				= "讀取%s模組時發生錯誤：%s"
+DBM_CORE_LOAD_MOD_SUCCESS			= "成功讀取%s模組!"
+DBM_CORE_LOAD_GUI_ERROR				= "無法讀取圖形介面：%s"
+
+DBM_CORE_COMBAT_STARTED				= "%s開戰. 祝好運與盡興! :)";
+DBM_CORE_BOSS_DOWN				= "%s 倒地, 經過 %s!"
+DBM_CORE_BOSS_DOWN_LONG				= "%s倒地!本次經過%s，上次經過%s，最快紀錄%s。"
+DBM_CORE_BOSS_DOWN_NEW_RECORD			= "%s倒地! 經過	%s，這是一個新記錄!（舊紀錄為%s）"
+DBM_CORE_COMBAT_ENDED				= "%s 的戰鬥經過 %s 結束."
+
+DBM_CORE_TIMER_FORMAT_SECS			= "%d秒"
+DBM_CORE_TIMER_FORMAT_MINS			= "%d分鐘"
+DBM_CORE_TIMER_FORMAT				= "%d分%d秒"
+
+DBM_CORE_MIN					= "分"
+DBM_CORE_MIN_FMT				= "%d 分"
+DBM_CORE_SEC					= "秒"
+DBM_CORE_SEC_FMT				= "%d 秒"
+DBM_CORE_DEAD					= "死亡"
+DBM_CORE_OK					= "確定"
+
+DBM_CORE_GENERIC_WARNING_ENRAGE			= "%s%s後狂怒"
+DBM_CORE_GENERIC_TIMER_ENRAGE			= "狂怒"
+DBM_CORE_OPTION_TIMER_ENRAGE			= "為狂怒顯示計時器"
+DBM_CORE_OPTION_HEALTH_FRAME			= "顯示首領血量框架"
+
+DBM_CORE_OPTION_CATEGORY_TIMERS			= "計時條"
+DBM_CORE_OPTION_CATEGORY_WARNINGS		= "提示"
+DBM_CORE_OPTION_CATEGORY_MISC			= "其它"
+
+DBM_CORE_AUTO_RESPONDED				= "已自動回復密語。"
+DBM_CORE_STATUS_WHISPER				= "%s：%s，%d/%d存活"
+DBM_CORE_AUTO_RESPOND_WHISPER			= "%s正在與%s交戰，（當前%s，%d/%d存活）"
+
+DBM_CORE_VERSIONCHECK_HEADER			= "Deadly Boss Mods - 版本檢測"
+DBM_CORE_VERSIONCHECK_ENTRY			= "%s：%s(r%d)"
+DBM_CORE_VERSIONCHECK_ENTRY_NO_DBM		= "%s：尚未安裝DBM"
+DBM_CORE_VERSIONCHECK_FOOTER			= "團隊中有%d名成員正在使用Deadly Boss Mods"
+
+DBM_CORE_UPDATEREMINDER_HEADER			= "你的 Deadly Boss Mod 已經過期! \n你可以在以下位址下載到新版本%s(r%d)："
+DBM_CORE_UPDATEREMINDER_FOOTER			= "Ctrl-C：複製下載地址到剪貼簿。"
+DBM_CORE_UPDATEREMINDER_NOTAGAIN		= "當有新版本時顯示彈出提示"
+
+DBM_CORE_MOVABLE_BAR				= "拖動我!"
+
+DBM_PIZZA_SYNC_INFO				= "|Hplayer:%1$s|h[%1$s]|h向你發送了一個倒計時：'%2$s'\n|HDBM:cancel:%2$s:nil|h|cff3588ff[取消該計時]|r|h  |HDBM:ignore:%2$s:%1$s|h|cff3588ff[忽略來自%1$s的計時]|r|h"
+DBM_PIZZA_CONFIRM_IGNORE			= "是否要在該次遊戲連接中忽略來自%s的計時？"
+DBM_PIZZA_ERROR_USAGE				= "命令：/dbm [broadcast] timer <時間（秒）> <文字>"
+
+DBM_CORE_ERROR_DBMV3_LOADED			= "目前有2個版本的Deadly Boss Mods正在運行：DBMv3和DBMv4。\n按一下“確定”按鈕可將DBMv3關閉並重載使用者介面。\n我們建議將外掛程式目錄下的DBMv3刪除。"
+
+DBM_CORE_MINIMAP_TOOLTIP_HEADER			= "Deadly Boss Mods"
+DBM_CORE_MINIMAP_TOOLTIP_FOOTER			= "Shift+按一下或右鍵點擊即可移動"
+
+DBM_CORE_RANGECHECK_HEADER			= "距離監視（%d碼）"
+DBM_CORE_RANGECHECK_SETRANGE			= "設置距離"
+DBM_CORE_RANGECHECK_SOUNDS			= "音效"
+DBM_CORE_RANGECHECK_SOUND_OPTION_1		= "當一位玩家在範圍內時播放音效"
+DBM_CORE_RANGECHECK_SOUND_OPTION_2		= "當多於一位玩家時播放音效"
+DBM_CORE_RANGECHECK_SOUND_0			= "沒有音效"
+DBM_CORE_RANGECHECK_SOUND_1			= "預設音效"
+DBM_CORE_RANGECHECK_SOUND_2			= "蜂鳴聲"
+DBM_CORE_RANGECHECK_HIDE			= "隱藏"
+DBM_CORE_RANGECHECK_SETRANGE_TO			= "%d碼"
+DBM_CORE_RANGECHECK_LOCK			= "鎖定框架"
+
+DBM_CORE_SLASHCMD_HELP				= {
+	"可用命令：",
+	"/dbm version：進行團隊範圍內的版本檢測（也可使用：ver）",
+	"/dbm unlock：顯示一個可移動的計時條，可通過對它來移動所有DBM計時條的位置（也可使用：move）",
+	"/dbm timer <x> <文本>：開始一個以<文本>為名稱的時間為<x>秒的倒計時",
+	"/dbm broadcast timer <x> <文本>：向團隊廣播一個以<文本>為名稱的時間為<x>秒的倒計時（需要團隊隊長或助理權限）",
+	"/dbm help：顯示該説明資訊",
+}
+
+DBM_ERROR_NO_PERMISSION				= "無權進行該操作。"
+
+DBM_CORE_BOSSHEALTH_HIDE_FRAME			= "關閉血量框架"
+
+DBM_CORE_ALLIANCE				= "聯盟"
+DBM_CORE_HORDE					= "部落"
+
+DBM_CORE_UNKNOWN				= "未知"
+
+DBM_CORE_TIMER_PULL				= "戰鬥準備"
+DBM_CORE_ANNOUNCE_PULL				= "%d秒後拉怪"
+DBM_CORE_ANNOUNCE_PULL_NOW			= "拉怪囉!"
+
+DBM_CORE_ACHIEVEMENT_TIMER_SPEED_KILL = "快速擊殺"
+
+-- Auto-generated Timer Localizations
+DBM_CORE_AUTO_TIMER_TEXTS = {
+	target = "%s: %%s",
+	cast = "%s",
+	active = "%s",
+	cd = "%s 冷卻",
+	next = "下一次 %s",
+	achievement = "%s",
+}
+
+DBM_CORE_AUTO_TIMER_OPTIONS = {
+	target = "為|cff71d5ff|Hspell:%d|h%s|h|r顯示減益計時器",
+	cast = "為|cff71d5ff|Hspell:%d|h%s|h|r顯示施法計時器",
+	active = "為|cff71d5ff|Hspell:%d|h%s|h|r顯示持續時間計時器",
+	cd = "為|cff71d5ff|Hspell:%d|h%s|h|r顯示冷卻計時器",
+	next = "為下一次 |cff71d5ff|Hspell:%d|h%s|h|r顯示計時器",
+	achievement = "為成就:%s顯示計時器",
+}
+
+-- Auto-generated Warning Localizations
+DBM_CORE_AUTO_ANNOUNCE_TEXTS = {
+	target = "%s: >%%s<",
+	spell = "%s",
+	cast = "施放 %s: %.1f 秒",
+	soon = "%s 即將到來",
+	prewarn = "%s 在 %s",
+	phase = "第%d階段",
+}
+
+local prewarnOption = "為|cff71d5ff|Hspell:%d|h%s|h|r顯示預先警告"
+DBM_CORE_AUTO_ANNOUNCE_OPTIONS = {
+	target = "當|cff71d5ff|Hspell:%d|h%s|h|r施放在玩家身上時顯示警告",
+	spell = "為|cff71d5ff|Hspell:%d|h%s|h|r顯示警告",
+	cast = "當|cff71d5ff|Hspell:%d|h%s|h|r施放時顯示警告",
+	soon = prewarnOption,
+	prewarn = prewarnOption,
+	phase = "為第%d階段顯示警告"
+}
+
+
+-- New special warnings
+DBM_CORE_MOVE_SPECIAL_WARNING_BAR	= "可拖動的特別警告"
+DBM_CORE_MOVE_SPECIAL_WARNING_TEXT	= "特別警告"
