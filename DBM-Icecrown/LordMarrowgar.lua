41e3e76e7ad5dd4ba951d1ab96f4db9dbd7b1fac
Adam <MysticalOS@users.noreply.github.com>
2009-10-29 04:22:27 +0000

    Some more lord marrowgar fixes thanks to newer combat logs, slight timer tuning.

--- DBM-Icecrown/LordMarrowgar.lua
+++ DBM-Icecrown/LordMarrowgar.lua
@@ -8,36 +8,41 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_CAST_START",
+	"SPELL_CAST_SUCCESS",
 	"SPELL_PERIODIC_DAMAGE",
 	"SPELL_SUMMON",
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_REMOVED"
 )
 
-local timerNextWhirlwind			= mod:NewNextTimer(60, 69076)
-local warnBoneSpike					= mod:NewSpellAnnounce(69057)
-local timerBoneSpike				= mod:NewCDTimer(18, 69057) --Roughly 18-23 second delay between casts, using an 18sec cooldown timer.
-local timerWhirlwind				= mod:NewBuffActiveTimer(30, 69076)
-local warnImpale					= mod:NewAnnounce("warnImpale")
-local specWarnWhirlwind				= mod:NewSpecialWarning("specWarnWhirlwind")
-local specWarnColdflame				= mod:NewSpecialWarning("specWarnColdflame")
+local timerWhirlwindCD			= mod:NewCDTimer(60, 69076)--Changed to a CD timer, it's always at least a minute but new logs indicate it's not dead on, i'm seing 61-65sec variation
+local warnBoneSpike				= mod:NewSpellAnnounce(69057)
+local timerBoneSpike			= mod:NewCDTimer(18, 69057) --Roughly 18-23 second delay between casts, using an 18 sec cooldown timer.
+local timerWhirlwind			= mod:NewBuffActiveTimer(28, 69076)--Seems to be 28 second duration, down from 30 in last test. Will watch for more PTR adjustments.
+local warnImpale				= mod:NewAnnounce("warnImpale")
+local specWarnWhirlwind			= mod:NewSpecialWarning("specWarnWhirlwind")
+local specWarnColdflame			= mod:NewSpecialWarning("specWarnColdflame")
 
 mod:AddBoolOption("PlaySoundOnWhirlwind", true, "announce")
 
 function mod:OnCombatStart(delay)
-	timerNextWhirlwind:Start(45-delay)
+	timerWhirlwindCD:Start(45-delay)
 	timerBoneSpike:Start(-delay)
 end
 
 function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(69057, 70826) then					-- Bone Spike Graveyard
+		warnBoneSpike:Show(args.spellName)
+		timerBoneSpike:Start()
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(69076) then						-- Whirlwind
 		specWarnWhirlwind:Show()
 		if self.Options.PlaySoundOnWhirlwind then
 			PlaySoundFile("Sound\\Creature\\HoodWolf\\HoodWolfTransformPlayer01.wav")
-		end	
-	elseif args:IsSpellID(69057) then					-- Bone Spike Graveyard
-		warnBoneSpike:Show(args.spellName)
-		timerBoneSpike:Start()
+		end
 	end
 end
 
@@ -53,24 +58,23 @@ end
 
 local impaleTargets = {}
 
-function mod:SPELL_SUMMON(args)
-	if args:IsSpellID(69062) then							-- Impale (This function is iffy and I'm not sure it'll work)
-		self:UnscheduleMethod("warnImpale")
-		impaleTargets[#impaleTargets + 1] = args.sourceName
-		mod:ScheduleMethod(0.2, "warnImpale")   end
-	timerBoneSpike:Start()
-	end
-end
-
 function mod:warnImpale() 
 	warnImpale:Show(table.concat(impaleTargets, "<, >")) 
 	table.wipe(impaleTargets) 
 end
 
+function mod:SPELL_SUMMON(args)
+	if args:IsSpellID(69062) then							-- Impale (This function is iffy and I'm not sure it'll work, but it is the earliest combatlog entry to detect off of.)
+		self:UnscheduleMethod("warnImpale")
+		impaleTargets[#impaleTargets + 1] = args.sourceName
+		mod:ScheduleMethod(0.2, "warnImpale")   end
+	end
+end
+
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(69076) then						-- Whirlwind Begins
 		timerWhirlwind:Show(args.destName)
---[[	elseif args:IsSpellID(69065) then					-- Impale (this function is a last resort if summon doesn't work. This happens at least 1-2 seconds after the summon event and after person has already taken damage from it.
+--[[	elseif args:IsSpellID(69065) then					-- Impale (this function is a backup if summon doesn't work. This happens at least 1-2 seconds after the summon event and after person has already taken damage from it.
 		self:UnscheduleMethod("warnImpale")
 		impaleTargets[#impaleTargets + 1] = args.destName
 		mod:ScheduleMethod(0.2, "warnImpale")   end]]--
@@ -79,6 +83,6 @@ end
 
 function mod:SPELL_AURA_REMOVED(args)
 	if args:IsSpellID(69076) then						-- Whirlwind Ends
-		timerNextWhirlwind:Start()
+		timerWhirlwindCD:Start()
 	end
 end
