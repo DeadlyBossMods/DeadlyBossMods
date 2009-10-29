0d5dba7e9c79088606b7173fddcf4714ea26a60d
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-10-29 05:31:41 +0000

    Lady Deathwhisper mod created. Currently no warnings though.

---
+++ trunk/DBM-Icecrown/Deathwhisper.lua
@@ -0,0 +1,11 @@
+local mod = DBM:NewMod("Deathwhisper", "DBM-Icecrown")
+local L = mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision: 1799 $"):sub(12, -3))
+mod:SetCreatureID(36855)
+--mod:SetUsedIcons(3, 4, 5, 6, 7, 8)
+mod:RegisterCombat("combat")
+
+mod:RegisterEvents(
+	"SPELL_AURA_APPLIED"
+)
