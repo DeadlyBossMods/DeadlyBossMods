348447ba362e7e73feef97bd41140cbf61b8bf2a
Adam <MysticalOS@users.noreply.github.com>
2009-10-31 02:28:26 +0000

    Filled in some creatureids out of mmochamps database.

--- DBM-Icecrown/BPCouncil.lua
+++ DBM-Icecrown/BPCouncil.lua
@@ -2,7 +2,7 @@ local mod = DBM:NewMod("BPCouncil", "DBM-Icecrown")
 local L = mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 1799 $"):sub(12, -3))
-mod:SetCreatureID()
+mod:SetCreatureID(37970, 37972, 37973)
 --mod:SetUsedIcons(3, 4, 5, 6, 7, 8)
 mod:RegisterCombat("combat")
 
--- DBM-Icecrown/Lanathel.lua
+++ DBM-Icecrown/Lanathel.lua
@@ -2,7 +2,7 @@ local mod = DBM:NewMod("Lanathel", "DBM-Icecrown")
 local L = mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 1799 $"):sub(12, -3))
-mod:SetCreatureID()
+mod:SetCreatureID(37955)
 --mod:SetUsedIcons(3, 4, 5, 6, 7, 8)
 mod:RegisterCombat("combat")
 
