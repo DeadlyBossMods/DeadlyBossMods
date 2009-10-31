a6aa092e71ec37ebb09405254a75bd5a53b814e5
Azael <paul.poon.kw@gmail.com>
2009-10-31 17:56:12 +0000

    zhtw update

--- DBM-Icecrown/localization.tw.lua
+++ DBM-Icecrown/localization.tw.lua
@@ -25,6 +25,48 @@ L:SetOptionLocalization({
 })
 
 
+-------------------------
+--  Lady Deathwhisper  --
+-------------------------
+L = DBM:GetModLocalization("Deathwhisper")
+
+L:SetGeneralLocalization({
+	name = "Lady Deathwhisper"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
+
+
+------------------------
+--  The Deathbringer  --
+------------------------
+L = DBM:GetModLocalization("Deathbringer")
+
+L:SetGeneralLocalization({
+	name = "The Deathbringer"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
+
+
+----------------------
+--  Gunship Battle  --
+----------------------
+L = DBM:GetModLocalization("GunshipBattle")
+
+L:SetGeneralLocalization({
+	name = "Gunship Battle"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
+
+
 -----------------
 --  Festergut  --
 -----------------
@@ -48,24 +90,33 @@ L:SetGeneralLocalization({
 	name = "Rotface"
 })
 L:SetWarningLocalization({
-	SpecWarnStickyOoze = "Ooze - 快跑出去!"
+	SpecWarnStickyOoze = "Ooze - 快跑出去!",
+	SpecWarnRadiationOoze		= "Radiation Ooze",
+	SpecWarnMutatedInfection 	= "Mutated Infection on you"
+})
+L:SetTimerLocalization({
+	NextPoisonSlimePipes		= "Next Poison Slime Pipes"
 })
 L:SetOptionLocalization({
 	SpecWarnStickyOoze = "為Sticky Ooze顯示特別警告",
-	SpecWarnRadiationOoze = "為Radiation Ooze顯示特別警告"
+	SpecWarnRadiationOoze = "為Radiation Ooze顯示特別警告",
+	NextPoisonSlimePipes		= "Show timer for next Poison Slime Pipes",
+	SpecWarnMutatedInfection 	= "Show special warning for Mutated Infection",
+	InfectionIcon				= "Set icon on Mutated Infection target",
+	WarnOozeSpawn				= "Show warning for Little Ooze spawning"
 })
 L:SetMiscLocalization({
 	YellSlimePipes		= "Good news, everyone! I've fixed the poison slime pipes!"	-- Professor Putricide
 })
 
 
-----------------------
---  Gunship Battle  --
-----------------------
-L = DBM:GetModLocalization("GunshipBattle")
+---------------------------
+--  Professor Putricide  --
+---------------------------
+L = DBM:GetModLocalization("Putricide")
 
 L:SetGeneralLocalization({
-	name = "Gunship Battle"
+	name = "Professor Putricide"
 })
 L:SetWarningLocalization({
 })
@@ -73,6 +124,93 @@ L:SetOptionLocalization({
 })
 
 
+----------------------------
+--  Blood Prince Council  --
+----------------------------
+L = DBM:GetModLocalization("BPCouncil")
+
+L:SetGeneralLocalization({
+	name = "Blood Prince Council"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
 
 
+-----------------------
+--  Queen Lana'thel  --
+-----------------------
+L = DBM:GetModLocalization("Lanathel")
 
+L:SetGeneralLocalization({
+	name = "Queen Lana'thel"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
+
+
+-----------------------------
+--  Valithria Dreamwalker  --
+-----------------------------
+L = DBM:GetModLocalization("Valithria")
+
+L:SetGeneralLocalization({
+	name = "Valithria Dreamwalker"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
+
+
+------------------
+--  Sindragosa  --
+------------------
+L = DBM:GetModLocalization("Sindragosa")
+
+L:SetGeneralLocalization({
+	name = "Sindragosa"
+})
+L:SetTimerLocalization({
+	NextAirphase 		= "Next airphase",
+	NextGroundphase		= "Next groundphase"
+})
+L:SetWarningLocalization({
+	WarnAirphase		= "Airphase",
+	WarnBlisteringCold	= "Blistering Cold",
+	SpecWarnBlisteringCold	= "Blisstering Cold - run out",
+	SpecWarnFrostBeacon	= "Frost beacon on you",
+	PrewarnGroundphase	= "Sindragosa landing soon",
+	SpecWarnUnchainedMagic	= "Unchained Magic on you"
+})
+L:SetOptionLocalization({
+	WarnAirphase		= "Show warning for airphase",
+	WarnBlisteringCold	= "Show warning for Blistering Cold",
+	SpecWarnBlisteringCold	= "Show special warning for Blistering Cold",
+	SpecWarnFrostBeacon	= "Show special warning for Frost Beacon",
+	PrewarnGroundphase	= "Show prewarning for groundphase",
+	NextAirphase		= "Show timer for next airphase",
+	NextGroundphase		= "Show timer for next groundphase",
+	SpecWarnUnchainedMagic	= "Show special warning for Unchained Magix"
+})
+L:SetMiscLocalization({
+	YellAirphase		= "Your incursion ends here! None shall survive!",
+	YellPull		= "You are fools to have come to this place. The icy winds of Northrend will consume your souls!"
+})
+
+
+---------------------
+--  The Lich King  --
+---------------------
+L = DBM:GetModLocalization("LichKing")
+
+L:SetGeneralLocalization({
+	name = "The Lich King"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
