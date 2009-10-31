4ef834b08816b8563df18d3f0fbe0f7543b7dd31
Arta <Arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-10-31 16:03:03 +0000

    Sindragosa , added pull yell

--- trunk/DBM-Icecrown/Sindragosa.lua
+++ trunk/DBM-Icecrown/Sindragosa.lua
@@ -4,7 +4,7 @@ local L = mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision: 1799 $"):sub(12, -3))
 mod:SetCreatureID(36853)
 --mod:SetUsedIcons(3, 4, 5, 6, 7, 8)
-mod:RegisterCombat("combat")
+mod:RegisterCombat("YELL", L.YellPull)
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
