b487a7ba9e6bd390165d3e3934ae81c6dc18eef1
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-10-31 03:25:57 +0000

    More creatureid touch on Valithria

--- trunk/DBM-Icecrown/Valithria.lua
+++ trunk/DBM-Icecrown/Valithria.lua
@@ -2,7 +2,7 @@ local mod = DBM:NewMod("Valithria", "DBM-Icecrown")
 local L = mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 1799 $"):sub(12, -3))
-mod:SetCreatureID(37868, 38167, 36791, 38169, 37863, 38171, 37886, 38166)--All the add creatureids for now drycoded from mmo database.
+mod:SetCreatureID(36789, 37868, 36791, 37863, 37886)--All the add creatureids for now drycoded from mmo database. (37950 phased bossid?)
 --mod:SetUsedIcons(3, 4, 5, 6, 7, 8)
 mod:RegisterCombat("combat")
 
