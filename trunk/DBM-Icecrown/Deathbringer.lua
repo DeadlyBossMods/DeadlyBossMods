0983da09365cc8b2d7e9746ba00c351ca0bcfa0e
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-10-31 02:28:26 +0000

    Filled in some creatureids out of mmochamps database.

--- trunk/DBM-Icecrown/Deathbringer.lua
+++ trunk/DBM-Icecrown/Deathbringer.lua
@@ -2,7 +2,7 @@ local mod = DBM:NewMod("Deathbringer", "DBM-Icecrown")
 local L = mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 1799 $"):sub(12, -3))
-mod:SetCreatureID()
+mod:SetCreatureID()--No creature IDs til blizz unviels the surprise boss on us.
 --mod:SetUsedIcons(3, 4, 5, 6, 7, 8)
 mod:RegisterCombat("combat")
 
--- trunk/DBM-Icecrown/Putricide.lua
+++ trunk/DBM-Icecrown/Putricide.lua
@@ -2,7 +2,7 @@ local mod = DBM:NewMod("Putricide", "DBM-Icecrown")
 local L = mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 1799 $"):sub(12, -3))
-mod:SetCreatureID()
+mod:SetCreatureID(36678, 38216)
 --mod:SetUsedIcons(3, 4, 5, 6, 7, 8)
 mod:RegisterCombat("combat")
 
