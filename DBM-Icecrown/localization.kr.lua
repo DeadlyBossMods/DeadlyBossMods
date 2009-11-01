e3b677c0e5551dec25d670b243111a726c8e236b
bluenyx <everfinale@gmail.com>
2009-11-01 18:34:50 +0000

    koKR update

--- DBM-Icecrown/localization.kr.lua
+++ DBM-Icecrown/localization.kr.lua
@@ -13,16 +13,59 @@ L:SetGeneralLocalization({
 
 L:SetWarningLocalization({
 	warnImpale				= ">%s< is impaled",
-	specWarnWhirlwind			= "Whirlwind! Run away!",
-	specWarnColdflame			= "Coldflame, MOVE!"
+	specWarnWhirlwind		= "Whirlwind - Run away",
+	specWarnColdflame		= "Coldflame - Move away"
 })
 
 L:SetOptionLocalization({
-	warnImpale				= "Show warning for Impale target",
-	specWarnWhirlwind			= "Show special warning for Whirlwind",
-	specWarnColdflame			= "Show special warning when you take damage from Coldflame",
-	PlaySoundOnWhirlwind		= "Play sound for Whirlwind"
+	warnImpale				= "Show warning for Impale targets",
+	specWarnWhirlwind		= "Show special warning for Whirlwind",
+	specWarnColdflame		= "Show special warning when you take damage from Coldflame",
+	PlaySoundOnWhirlwind	= "Play sound on Whirlwind"
 })
+
+
+-------------------------
+--  Lady Deathwhisper  --
+-------------------------
+L = DBM:GetModLocalization("Deathwhisper")
+
+L:SetGeneralLocalization({
+	name = "Lady Deathwhisper"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
+
+
+------------------------
+--  The Deathbringer  --
+------------------------
+L = DBM:GetModLocalization("Deathbringer")
+
+L:SetGeneralLocalization({
+	name = "The Deathbringer"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
+
+
+----------------------
+--  Gunship Battle  --
+----------------------
+L = DBM:GetModLocalization("GunshipBattle")
+
+L:SetGeneralLocalization({
+	name = "Gunship Battle"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
+
 
 -----------------
 --  Festergut  --
@@ -47,28 +90,130 @@ L:SetGeneralLocalization({
 	name = "Rotface"
 })
 L:SetWarningLocalization({
-	SpecWarnStickyOoze	= "Ooze - Move out"
+	SpecWarnStickyOoze			= "Sticky Ooze - Move out",
+	SpecWarnRadiationOoze		= "Radiation Ooze",
+	SpecWarnMutatedInfection 	= "Mutated Infection on you"
+})
+L:SetTimerLocalization({
+	NextPoisonSlimePipes		= "Next Poison Slime Pipes"
 })
 L:SetOptionLocalization({
-	SpecWarnStickyOoze	= "Show special warning for Sticky Ooze"
+	SpecWarnStickyOoze			= "Show special warning for Sticky Ooze",
+	SpecWarnRadiationOoze		= "Show special warning for Radiation Ooze",
+	NextPoisonSlimePipes		= "Show timer for next Poison Slime Pipes",
+	SpecWarnMutatedInfection 	= "Show special warning for Mutated Infection",
+	InfectionIcon				= "Set icon on Mutated Infection target",
+	WarnOozeSpawn				= "Show warning for Little Ooze spawning"
 })
 L:SetMiscLocalization({
-	YellSlimePipes		= "Good news, everyone! I've fixed the poison slime pipes!"	-- Professor Putricide
-})	
+	YellSlimePipes				= "Good news, everyone! I've fixed the poison slime pipes!"	-- Professor Putricide
+})
 
-----------------------
---  Gunship Battle  --
-----------------------
-L = DBM:GetModLocalization("GunshipBattle")
+
+---------------------------
+--  Professor Putricide  --
+---------------------------
+L = DBM:GetModLocalization("Putricide")
 
 L:SetGeneralLocalization({
-	name = "Gunship Battle"
+	name = "Professor Putricide"
 })
 L:SetWarningLocalization({
 })
 L:SetOptionLocalization({
 })
 
+
+----------------------------
+--  Blood Prince Council  --
+----------------------------
+L = DBM:GetModLocalization("BPCouncil")
+
+L:SetGeneralLocalization({
+	name = "Blood Prince Council"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
+
+
+-----------------------
+--  Queen Lana'thel  --
+-----------------------
+L = DBM:GetModLocalization("Lanathel")
+
+L:SetGeneralLocalization({
+	name = "Queen Lana'thel"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
+
+
+-----------------------------
+--  Valithria Dreamwalker  --
+-----------------------------
+L = DBM:GetModLocalization("Valithria")
+
+L:SetGeneralLocalization({
+	name = "Valithria Dreamwalker"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
+
+
+------------------
+--  Sindragosa  --
+------------------
+L = DBM:GetModLocalization("Sindragosa")
+
+L:SetGeneralLocalization({
+	name = "Sindragosa"
+})
+L:SetTimerLocalization({
+	NextAirphase			= "Next air phase",
+	NextGroundphase			= "Next ground phase"
+})
+L:SetWarningLocalization({
+	WarnAirphase			= "Air phase",
+	WarnBlisteringCold		= "Blistering Cold",
+	SpecWarnBlisteringCold	= "Blistering Cold - Run away",
+	SpecWarnFrostBeacon		= "Frost Beacon on you",
+	PrewarnGroundphase		= "Sindragosa landing soon",
+	SpecWarnUnchainedMagic	= "Unchained Magic on you"
+})
+L:SetOptionLocalization({
+	WarnAirphase			= "Show warning for air phase",
+	WarnBlisteringCold		= "Show warning for Blistering Cold",
+	SpecWarnBlisteringCold	= "Show special warning for Blistering Cold",
+	SpecWarnFrostBeacon		= "Show special warning for Frost Beacon",
+	PrewarnGroundphase		= "Show pre-warning for ground phase",
+	NextAirphase			= "Show timer for next air phase",
+	NextGroundphase			= "Show timer for next ground phase",
+	SpecWarnUnchainedMagic	= "Show special warning for Unchained Magic"
+})
+L:SetMiscLocalization({
+	YellAirphase			= "Your incursion ends here! None shall survive!",
+	YellPull				= "You are fools to have come to this place. The icy winds of Northrend will consume your souls!"
+})
+
+
+---------------------
+--  The Lich King  --
+---------------------
+L = DBM:GetModLocalization("LichKing")
+
+L:SetGeneralLocalization({
+	name = "The Lich King"
+})
+L:SetWarningLocalization({
+})
+L:SetOptionLocalization({
+})
 
 
 
