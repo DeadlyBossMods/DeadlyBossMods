5eeb72515a14c24b6de937f9d884806d30c72e59
bluenyx <bluenyx@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-11-02 00:54:48 +0000

    koKR update

--- DBM-Coliseum/localization.kr.lua
+++ DBM-Coliseum/localization.kr.lua
@@ -22,13 +22,6 @@ L:SetMiscLocalization{
 }
 
 L:SetOptionLocalization{
-	WarningImpale				= "꿰뚫기 경보 보기",
-	WarningFireBomb				= "화염 폭탄 경보 보기",
-	WarningBreath				= "북극의 숨결 경보 보기",
-	WarningRage					= "분노 경보 보기",
-	WarningCharge				= "사나운 돌진 대상 경보 알리기",
-	WarningToxin				= "마비 독 대상 경보 알리기",
-	WarningBile					= "타오르는 담즙 대상 경보 알리기",	
 	SpecialWarningImpale3		= "꿰뚫기 특수 경보 보기(3 중첩이거나 그 이상일 경우)",
 	SpecialWarningFireBomb		= "자신의 화염 폭탄 특수 경보 보기",
 	SpecialWarningSlimePool		= "독 구름 특수 경보 보기",
@@ -52,13 +45,6 @@ L:SetTimerLocalization{
 }
 
 L:SetWarningLocalization{
-	WarningImpale				= " %s : >%s<",
-	WarningFireBomb				= "화염 폭탄",
-	WarningBreath				= "북극의 숨결",
-	WarningRage					= "분노",
-	WarningCharge				= "사나운 돌진 : >%s<",	
-	WarningToxin				= "마비 독 : >%s<",
-	WarningBile					= "타오르는 담즙 : >%s<",	
 	SpecialWarningImpale3		= "꿰뚫기 경고! 위험합니다!",
 	SpecialWarningFireBomb		= "당신에게 화염 폭탄!",
 	SpecialWarningSlimePool		= "독 구름, 벗어나세요!",
@@ -81,11 +67,7 @@ L:SetGeneralLocalization{
 }
 
 L:SetWarningLocalization{
-	WarnFlame				= "군단 불꽃 : >%s<!",
-	WarnTouch				= "자락서스의 손길 : >%s<",
 	WarnNetherPower			= "자락서스가 황천의 힘을 얻었습니다! 해제하세요!",
-	WarnPortalSoon			= "곧 황천의 차원문!",
-	WarnVolcanoSoon			= "곧 지옥불 정령 분출!",
 	SpecWarnFlesh			= "당신에게 살점 소각!",
 	SpecWarnTouch			= "당신에게 자락서스의 손길!",
 	SpecWarnKiss			= "여군주의 키스",
@@ -101,11 +83,7 @@ L:SetMiscLocalization{
 }
 
 L:SetOptionLocalization{
-	WarnFlame				= "군단 불꽃 경고 알리기",
-	WarnTouch				= "자락서스의 손길 경고 알리기",
 	WarnNetherPower			= "자락서스가 황천의 힘을 얻었을 경우 경고 하기 (해제를 위한 경고)",
-	WarnPortalSoon			= "황천의 차원문 생성 사전 경고 알리기",
-	WarnVolcanoSoon			= "지옥불 정령 분출 사전 경고 알리기",
 	SpecWarnFlame			= "군단 불꽃에 걸렸을 경우 특수 경보 알리기",
 	SpecWarnFlesh			= "살점 소각에 걸렸을 경우 특수 경보 알리기",
 	SpecWarnTouch			= "당신이 자락서스의 손길에 걸렸을 경우 특수 경고 알리기",
