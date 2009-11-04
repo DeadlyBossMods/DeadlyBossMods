bafbb2a6b3b824b57f7813de0248b25a216fb534
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-11-04 07:38:03 +0000

    Attempt real spellsteal fix with nitrams new rules

--- trunk/DBM-Party-WotLK/TrialoftheChampion/Confessor.lua
+++ trunk/DBM-Party-WotLK/TrialoftheChampion/Confessor.lua
@@ -18,12 +18,12 @@ local isDispeller = select(2, UnitClass("player")) == "MAGE"
              or select(2, UnitClass("player")) == "SHAMAN"
 
 local warnReflectiveShield	= mod:NewTargetAnnounce(66515)
-local warnRenew				= mod:NewTargetAnnounce(66537)--Still bugged for mages spellsteal, Fixing it is beyond me.
+local warnRenew				= mod:NewTargetAnnounce(66537)
 local warnOldWounds			= mod:NewTargetAnnounce(67679)
 local timerOldWounds		= mod:NewTargetTimer(12, 67679)
 local warnHolyFire			= mod:NewTargetAnnounce(67676)
 local timerHolyFire			= mod:NewTargetTimer(8, 67676)
-local specwarnRenew			= mod:NewSpecialWarning("specwarnRenew", isDispeller)--Still bugged for mages spellsteal, Fixing it is beyond me.
+local specwarnRenew			= mod:NewSpecialWarning("specwarnRenew", isDispeller)
 
 local shielded = false
 
@@ -35,7 +35,7 @@ function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(66515) then                     -- Shield Gained
 		warnReflectiveShield:Show(args.destName)
 		shielded = true
-	elseif args:IsSpellID(66537, 67675) then               -- Renew
+	elseif args:IsSpellID(66537, 67675) and not args:IsDestTypePlayer() then               -- Renew
 		warnRenew:Show(args.destName)
 		if args.destName == L.name and shielded then
 			-- nothing, she casted it on herself and you cant dispel
