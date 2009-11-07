1d0b6b5d0fae511d17b398caa6e6ca1736a40e72
Arta <Arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-11-07 19:18:27 +0000

    Professor Putricide - basic addon .. timers are unreliable as we had only 1 try on PTR (then he bugged out)

--- DBM-Icecrown/ThePlagueworks/Putricide.lua
+++ DBM-Icecrown/ThePlagueworks/Putricide.lua
@@ -4,9 +4,42 @@ local L = mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision: 1799 $"):sub(12, -3))
 mod:SetCreatureID(36678, 38216)
 --mod:SetUsedIcons(3, 4, 5, 6, 7, 8)
-mod:RegisterCombat("combat")
+mod:RegisterCombat("yell", "Good news, everyone! I think I've perfected a plague that will destroy all life on Azeroth!")
 
 mod:RegisterEvents(
-	"SPELL_AURA_APPLIED",
-	"SPELL_CAST_START"
+	"SPELL_CAST_START",
+	"SPELL_CAST_SUCCESS",
+	"SPELL_AURA_APPLIED"
 )
+
+local warnSlimePuddle		= mod:NewCastAnnounce(70341, 3)
+local warnUnstableExperiment	= mod:NewCastAnnounce(70351, 3)
+local warnVolatileOozeAdhesive	= mod:NewTargetAnnounce(70447, 4)
+
+local timerSlimePuddleCD	= mod:NewNextTimer(35, 70341)	-- guessed timer atm
+local timerUnstableExperimentCD	= mod:NewNextTimer(35, 70351)
+
+
+function mod:OnCombatStart(delay)
+	timerSlimePuddleCD:Start(10-delay)
+	timerUnstableExperimentCD:Start(30-delay)
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(70351, 71966) then
+		warnUnstableExperiment:Show()
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(70341) then
+		warnSlimePuddle:Show()
+		timerSlimePuddleCD:Start()
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(70447) then
+		warnVolatileOozeAdhesive:Show(args.destName)
+	end
+end
