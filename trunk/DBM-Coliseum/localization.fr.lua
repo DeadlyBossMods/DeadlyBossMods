151f323c06fc82dce98abc9f383e58abfe608f31
psyco <psyco@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-11-18 03:41:05 +0000

    Fr update ( Next commit to fix ulduar and Coliseum problem )

--- trunk/DBM-Coliseum/localization.fr.lua
+++ trunk/DBM-Coliseum/localization.fr.lua
@@ -24,12 +24,14 @@ L:SetMiscLocalization{
 L:SetOptionLocalization{
 	WarningSnobold				= "Montre une alerte quand les Vassal arrivent",
 	SpecialWarningImpale3		= "Montre une alerte spéciale pour l'empaler (>=3 Stacks)",
+	SpecialWarningAnger3		= "Montre une alerte pour la Colère croissante (>=3 stacks)",
 	SpecialWarningFireBomb		= "Montre une alerte spéciale quand la Bombe incendiaire est sur vous",
 	SpecialWarningSlimePool		= "Montre une alerte spéciale pour les Flaque de bave",
 	SpecialWarningSilence		= "Montre une alerte spéciale pour le Piétinement ahurissant",
 	SpecialWarningToxin			= "Montre une alerte spéciale si vous êtes victime de la Toxine paralysante",
 	SpecialWarningBile			= "Montre une alerte spéciale si vous avez la Bile brûlante sur Vous",
 	SpecialWarningCharge		= "Montre une alerte spéciale quand Glace-hurlante est sur le point de vous charger",
+	SpecialWarningTranq			= "Montre une alerte spéciale quand GLace-Hurlante gagne la Rage écumeuse (Tir tranq)",
 	PingCharge					= "Ping sur la minimap quand Glace-hurlante va vous charger",
 	SpecialWarningChargeNear	= "Montre une alerte spéciale quand Glace-hurlante charge à côté de vous",
 	SetIconOnChargeTarget		= "Met une icône sur la cible de la charge ( Tête de mort )",
@@ -47,13 +49,15 @@ L:SetTimerLocalization{
 L:SetWarningLocalization{
 	WarningSnobold				= "Un Vassal frigbold viens d'arriver",
 	SpecialWarningImpale3		= "Empaler >%d< sur VOUS",
+	SpecialWarningAnger3		= "Colère croissante >%d<",
 	SpecialWarningFireBomb		= "Bombe incendiaire sur VOUS",
 	SpecialWarningSlimePool		= "Flaque de bave, BOUGEZ!",
 	SpecialWarningSilence		= "Piétinement ahurissant dans ~1.5 Seconde!",
 	SpecialWarningToxin			= "Toxine paralysante sur vous, BOUGEZ!",
 	SpecialWarningCharge		= "Charge sur vous! COUREZ!",
 	SpecialWarningChargeNear	= "Charge à côté de vous! COUREZ!",
-	SpecialWarningBile			= "Bile brûlante sur VOUS !"
+	SpecialWarningBile			= "Bile brûlante sur VOUS !",
+	SpecialWarningTranq			= "Rage écumeuse - Tir tranq Maintenant"
 }
 
 -------------------
@@ -66,41 +70,41 @@ L:SetGeneralLocalization{
 }
 
 L:SetWarningLocalization{
-	WarnNetherPower			= "Puissance du Néant sur Jaraxxus! Dispel Maintenant!",
-	SpecWarnFlesh			= "Incinérer la chair sur VOUS!",
-	SpecWarnTouch			= "Toucher de Jaraxxus sur VOUS!",
-	SpecWarnKiss			= "Baiser de la Maîtresse",
-	SpecWarnTouchNear		= "Toucher de Jaraxxus sur >%s< à côté de vous",
-	SpecWarnFlame			= "Flamme de la Légion! COUREZ!",
-	SpecWarnNetherPower		= "Dispell Maintenant!",
-	SpecWarnFelInferno		= "Inferno gangrené! Courez plus loin!",
-	SpecWarnFelFireball		= "Boule de feu - CASSEZ",
+	WarnNetherPower				= "Puissance du Néant sur Jaraxxus! Dispel Maintenant!",
+	SpecWarnFlesh				= "Incinérer la chair sur VOUS!",
+	SpecWarnTouch				= "Toucher de Jaraxxus sur VOUS!",
+	SpecWarnKiss				= "Baiser de la Maîtresse",
+	SpecWarnTouchNear			= "Toucher de Jaraxxus sur >%s< à côté de vous",
+	SpecWarnFlame				= "Flamme de la Légion! COUREZ!",
+	SpecWarnNetherPower			= "Dispell Maintenant!",
+	SpecWarnFelInferno			= "Inferno gangrené! Courez plus loin!",
+	SpecWarnFelFireball			= "Boule de feu - CASSEZ",
 	SpecWarnFelFireballDispel	= "Boule de feu sur %s - Dispel"
 }
 
 L:SetMiscLocalization{
-	WhisperFlame			= "Flamme de la Légion sur VOUS!",
-	IncinerateTarget		= "Incinérer la chair: %s"
+	WhisperFlame				= "Flamme de la Légion sur VOUS!",
+	IncinerateTarget			= "Incinérer la chair: %s"
 }
 
 L:SetOptionLocalization{
-	WarnNetherPower			= "Alerte quand Jaraxxus gagne Puissance du Néant (Pour le dispell)",
-	SpecWarnFlame			= "Montre une alerte spéciale quand vous avez Flamme de la Légion",
-	SpecWarnFlesh			= "Montre une alerte spéciale quand vous avez Incinérer la chair",
-	SpecWarnTouch			= "Montre une alerte spéciale quand le Toucher de Jaraxxus est sur vous",
-	SpecWarnTouchNear		= "Montre une alerte spéciale quand le Toucher de Jaraxxus est à côté de vous",
-	SpecWarnKiss			= "Montre une alerte spéciale pour le Baiser de la Maîtresse",
-	SpecWarnNetherPower		= "Montre une alerte spéciale pour la Puissance du Néant (Pour dispell Jaraxxus)",
-	SpecWarnFelInferno		= "Montre une alerte spéciale quand vous êtes près d'un Inferno gangrené",
-	SpecWarnFelFireball		= "Montre une alerte spéciale pour la boule de feu gangrené (Pour casser)",
+	WarnNetherPower				= "Alerte quand Jaraxxus gagne Puissance du Néant (Pour le dispell)",
+	SpecWarnFlame				= "Montre une alerte spéciale quand vous avez Flamme de la Légion",
+	SpecWarnFlesh				= "Montre une alerte spéciale quand vous avez Incinérer la chair",
+	SpecWarnTouch				= "Montre une alerte spéciale quand le Toucher de Jaraxxus est sur vous",
+	SpecWarnTouchNear			= "Montre une alerte spéciale quand le Toucher de Jaraxxus est à côté de vous",
+	SpecWarnKiss				= "Montre une alerte spéciale pour le Baiser de la Maîtresse",
+	SpecWarnNetherPower			= "Montre une alerte spéciale pour la Puissance du Néant (Pour dispell Jaraxxus)",
+	SpecWarnFelInferno			= "Montre une alerte spéciale quand vous êtes près d'un Inferno gangrené",
+	SpecWarnFelFireball			= "Montre une alerte spéciale pour la boule de feu gangrené (Pour casser)",
 	SpecWarnFelFireballDispel	= "Montre une alerte spécale pour la boule de feu gangrené (Pour dispel)",
-	TouchJaraxxusIcon		= "Met une icône sur la cible du Toucher de Jaraxxus (Croix)",
-	IncinerateFleshIcon		= "Met une icône sur la cible d'Incinérer la chair (Tête de mort)",
-	LegionFlameIcon			= "Met une icône sur la cible des Flammes de la Légion (Carré)",
-	LegionFlameWhisper		= "Chuchote à la cible des Flammes de la Légion pour la prévenir",
+	TouchJaraxxusIcon			= "Met une icône sur la cible du Toucher de Jaraxxus (Croix)",
+	IncinerateFleshIcon			= "Met une icône sur la cible d'Incinérer la chair (Tête de mort)",
+	LegionFlameIcon				= "Met une icône sur la cible des Flammes de la Légion (Carré)",
+	LegionFlameWhisper			= "Chuchote à la cible des Flammes de la Légion pour la prévenir",
+	LegionFlameRunSound			= "Joue une sons pour les Flammes de la légion",
 	IncinerateShieldFrame		= "Montre la vie du Boss avec une barre de vie pour Incinérer la chair"
 }
-
 
 -----------------------
 -- Faction Champions --
