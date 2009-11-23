25b0f5d84334129fc051dc74aa49ceb36915e3a6
bluenyx <bluenyx@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-11-23 19:14:25 +0000

    koKR update(not yet, translate)

--- trunk/DBM-Icecrown/localization.kr.lua
+++ trunk/DBM-Icecrown/localization.kr.lua
@@ -8,7 +8,7 @@ local L
 L = DBM:GetModLocalization("LordMarrowgar")
 
 L:SetGeneralLocalization({
-	name = "Lord Marrowgar"
+	name = "군주 매로우가르"
 })
 
 L:SetWarningLocalization({
@@ -21,7 +21,8 @@ L:SetOptionLocalization({
 	WarnImpale				= "꿰뚫기 대상 알리기",
 	SpecWarnWhirlwind		= "소용돌이 특수 경고 보기",
 	SpecWarnColdflame		= "냉기화염에 영향을 받을 경우 특수 경고 보기",
-	PlaySoundOnWhirlwind	= "소용돌이 특수 경고 소리 듣기"
+	PlaySoundOnWhirlwind	= "소용돌이 특수 경고 소리 듣기",
+	achievementBoned		= "Show timer for Boned Achievement"
 })
 
 -------------------------
@@ -30,14 +31,34 @@ L:SetOptionLocalization({
 L = DBM:GetModLocalization("Deathwhisper")
 
 L:SetGeneralLocalization({
-	name = "Lady Deathwhisper"
+	name = "여교주 데스위스퍼"
 })
 
-L:SetWarningLocalization({
-})
+L:SetTimerLocalization{
+}
 
-L:SetOptionLocalization({
-})
+L:SetWarningLocalization{
+	WarnReanimating					= "Add reviving",			-- Reanimating an adherent or fanatic
+	WarnTouchInsignificance			= "%s on >%s< (%s)",		-- Touch of Insignificance on >args.destName< (args.amount)
+	SpecWarnDeathDecay				= "Death and Decay - Move away",
+	SpecWarnCurseTorpor				= "Curse of Torpor on you",
+	SpecWarnTouchInsignificance		= "Touch of Insignificance (3)",
+	WarnAddsSoon					= "New adds soon"
+}
+
+L:SetOptionLocalization{
+	WarnAddsSoon					= "Show pre-warning for adds spawning",
+	WarnTouchInsignificance			= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.spell:format(71204, GetSpellInfo(71204) or "unknown"),		-- Warning isnt default (it has a count number), option is default tho (no need for translation this way)
+	WarnReanimating					= "Show warning when an add is getting revived",								-- Reanimated Adherent/Fanatic spawning
+	SpecWarnTouchInsignificance		= "Show special warning when you have 3 stacks of Touch of Insignificance",
+	SpecWarnDeathDecay				= "Show special warning when you are affected by Death and Decay",
+	SpecWarnCurseTorpor				= "Show special warning when you are affected by Curse of Torpor"
+}
+
+L:SetMiscLocalization{
+	YellPull						= "What is this disturbance? You dare trespass upon this hallowed ground? This shall be your final resting place!",
+	YellReanimatedFanatic			= "Arise, and exult in your pure form!",
+}
 
 ------------------------
 --  The Deathbringer  --
@@ -122,14 +143,15 @@ L:SetMiscLocalization({
 L = DBM:GetModLocalization("Putricide")
 
 L:SetGeneralLocalization({
-	name = "Professor Putricide"
+	name = "교수 퓨트리사이드"
 })
 
 L:SetWarningLocalization({
 })
 
-L:SetOptionLocalization({
-})
+L:SetMiscLocalization{
+	YellPull	= "Good news, everyone! I think I've perfected a plague that will destroy all life on Azeroth!"
+}
 
 ----------------------------
 --  Blood Prince Council  --
@@ -140,11 +162,28 @@ L:SetGeneralLocalization({
 	name = "Blood Prince Council"
 })
 
-L:SetWarningLocalization({
-})
+L:SetWarningLocalization{
+	WarnTargetSwitch		= "Switch target to: %s",		-- Ugh, no nice spellname/id to use for general localization :(
+	WarnTargetSwitchSoon	= "Target switch soon",			-- Spellname = Invocation of Blood   or   Spellname = Invocation of Blood (X) Move  (server side script where X indicates the first letter of the Prince name
+	SpecWarnResonance		= "Shadow Resonance - Move"
+}
 
-L:SetOptionLocalization({
-})
+L:SetTimerLocalization{
+	TimerTargetSwitch		= "Possible target switch"
+}
+
+L:SetOptionLocalization{
+	WarnTargetSwitch		= "Show warning to switch targets",								-- Warn when another Prince needs to be damaged
+	WarnTargetSwitchSoon	= "Show pre-warning to switch targets",							-- Every ~31 secs, you have to dps a different Prince
+	TimerTargetSwitch		= "Show timer for target switch cooldown",
+	SpecWarnResonance		= "Show special warning when a Dark Nucleus is following you"	-- If it follows you, you have to move to the tank
+}
+
+L:SetMiscLocalization{
+	Keleseth				= "Prince Keleseth",
+	Taldaram				= "Prince Taldaram",
+	Valanar					= "Prince Valanar"
+}
 
 -----------------------
 --  Queen Lana'thel  --
@@ -152,7 +191,7 @@ L:SetOptionLocalization({
 L = DBM:GetModLocalization("Lanathel")
 
 L:SetGeneralLocalization({
-	name = "Queen Lana'thel"
+	name = "피의 여왕 라나텔"
 })
 
 L:SetWarningLocalization({
@@ -167,14 +206,17 @@ L:SetOptionLocalization({
 L = DBM:GetModLocalization("Valithria")
 
 L:SetGeneralLocalization({
-	name = "Valithria Dreamwalker"
+	name = "발리스리아 드림워커"
 })
 
-L:SetWarningLocalization({
-})
+L:SetWarningLocalization{
+	SpecWarnPactDarkfallen	= "Pact of the Darkfallen on you"
+}
 
-L:SetOptionLocalization({
-})
+L:SetOptionLocalization{
+	SpecWarnPactDarkfallen	= "Show special warning when you are affected by Pact of the Darkfallen"
+}
+
 
 ------------------
 --  Sindragosa  --
@@ -182,7 +224,7 @@ L:SetOptionLocalization({
 L = DBM:GetModLocalization("Sindragosa")
 
 L:SetGeneralLocalization({
-	name = "Sindragosa"
+	name = "신드라고사"
 })
 
 L:SetTimerLocalization({
@@ -192,7 +234,6 @@ L:SetTimerLocalization({
 
 L:SetWarningLocalization({
 	WarnAirphase			= "공중 페이즈",
-	WarnBlisteringCold		= "얼음 손아귀!",
 	SpecWarnBlisteringCold	= "얼음 손아귀! 뛰세요!",
 	SpecWarnFrostBeacon		= "당신에게 냉기 봉화",
 	WarnGroundphaseSoon		= "곧 신드라고사 착륙",
@@ -201,7 +242,6 @@ L:SetWarningLocalization({
 
 L:SetOptionLocalization({
 	WarnAirphase			= "공중 페이즈 알리기",
-	WarnBlisteringCold		= "얼음 손아귀 경고 보기",
 	SpecWarnBlisteringCold	= "얼음 손아귀 특수 경고 보기",
 	SpecWarnFrostBeacon		= "냉기 봉화에 영향을 받을 경우 특수 경고 보기",
 	WarnGroundphaseSoon		= "지상 페이즈 사전 경고 보기",
@@ -221,7 +261,7 @@ L:SetMiscLocalization({
 L = DBM:GetModLocalization("LichKing")
 
 L:SetGeneralLocalization({
-	name = "The Lich King"
+	name = "리치 왕"
 })
 
 L:SetWarningLocalization({
