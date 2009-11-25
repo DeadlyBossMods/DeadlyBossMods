36d9572b5a48993b07d5a363039e22962cf2d545
Arta <Arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-11-25 09:25:15 +0000

    fixing the previous fix :P

--- trunk/DBM-Coliseum/NorthrendBeasts.lua
+++ trunk/DBM-Coliseum/NorthrendBeasts.lua
@@ -279,7 +279,7 @@ function mod:OnSync(msg, arg)
 		end
 		timerNextCrash:Start(45)
 		timerNextBoss:Cancel()
-		self:ScheduleMethod("WormsSubmerge")				-- To cancel most P2 timers
+		self:ScheduleMethod(0.1, "WormsSubmerge")	-- To cancel most P2 timers
 		self:UnscheduleMethod("WormsEmerge")	-- There wont be another Emerge phase ;)
 		timerEmerge:Cancel()			-- ^
 	end
