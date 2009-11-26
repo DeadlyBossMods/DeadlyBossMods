34e3982d11851b19c19f63b06cc8fa26ea49eafe
Arta <Arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-11-26 13:29:05 +0000

    Marwyn code,  Well of Corruption + Corrupted Flesh

--- DBM-Party-WotLK/HallsofReflection/Marwyn.lua
+++ DBM-Party-WotLK/HallsofReflection/Marwyn.lua
@@ -2,16 +2,41 @@ local mod = DBM:NewMod("Marwyn", "DBM-Party-WotLK", 16)
 local L = mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 2153 $"):sub(12, -3))
-mod:SetCreatureID(36658) 		-- correct this
---mod:SetUsedIcons(8)
+mod:SetCreatureID(38113)
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
-	"SPELL_CAST_START",
 	"SPELL_AURA_APPLIED",
-	"CHAT_MSG_MONSTER_YELL",
-	"CHAT_MSG_RAID_BOSS_EMOTE",
-	"SPELL_PERIODIC_DAMAGE"
+	"SPELL_CAST_SUCCESS"
 )
 
+local warnWellCorruption		= mod:NewCastAnnounce(72362,3)
+local warnCorruptedFlesh		= mod:NewTargetAnnounce(72436, 3)
+
+local timerWellCorruptionCD		= mod:NewCDTimer(13, 72362)
+local timerCorruptedFlesh		= mod:NewBuffActiveTimer(8, 72436)
+local timerCorruptedFleshCD		= mod:NewCDTimer(20, 72436)
+
+local specWarnWellCorruption		= mod:NewSpecialWarning("SpecWarnWellCorruption")
+
+local spam = 0
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(72362) and args:IsPlayer() then
+		specWarnWellCorruption:Show()
+	elseif args:IsSpellID(72436) then
+		if GetTime()-spam > 5 then
+			warnCorruptedFlesh:Show()
+			timerCorruptedFlesh:Start()
+			timerCorruptedFleshCD:Start()
+		end
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(72462) then
+		warnWellCorruption:Show()
+		timerWellCorruptionCD:Start()
+	end
+end
