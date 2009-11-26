f16fc8125eb2216f7351cc8ba040b8ff56c0a0c9
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-11-26 21:24:07 +0000

    Fixed more careless bugs in party mods for new ptr instances.

--- trunk/DBM-Party-WotLK/ForgeofSouls/Bronjahm.lua
+++ trunk/DBM-Party-WotLK/ForgeofSouls/Bronjahm.lua
@@ -7,30 +7,30 @@ mod:SetCreatureID(36497)
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+    "SPELL_CAST_START",
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_REMOVED"
 )
 
 local warnCorruptSoul		= mod:NewTargetAnnounce(68839)
 local specwarnSoulstorm		= mod:NewSpecialWarning("specwarnSoulstorm")
-local timerSoulstorm		= mod:NewBuffActiveTimer(12, 68872)
+--local timerSoulstorm		= mod:NewBuffActiveTimer(12, 68872)
+local timerSoulstormCast	= mod:NewCastTimer(4, 68872)
 local timerNextSoulstorm	= mod:NewNextTimer(30, 68872) --Experimental, Timer may not be right
 
 function mod:OnCombatStart(delay)
-timerNextSoulstorm:Start(-delay)
+    timerNextSoulstorm:Start(-delay)
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(68872) then							-- Soulstorm
+		specwarnSoulstorm:Show()
+		timerSoulstormCast:Start()
+	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(68839) then							-- Corrupt Soul
-			warnCorruptSoul:Show(args.destName)
-	elseif args:IsSpellID(68872) then							-- Soulstorm
-			specwarnSoulstorm:Show()
-			timerSoulstorm:Show(args.destName)
-	end
-end
-
-function mod:SPELL_AURA_REMOVED(args)
-	if args:IsSpellID(68872) then							-- Soulstorm
-		timerNextSoulstorm:Start()
+		warnCorruptSoul:Show(args.destName)
 	end
 end
