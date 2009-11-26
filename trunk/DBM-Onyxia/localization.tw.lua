1892ccbabf042ad6d062272ac9b49e8235d51ac7
Azael <Azael@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-11-26 13:36:25 +0000

    zhtw update

--- trunk/DBM-Onyxia/localization.tw.lua
+++ trunk/DBM-Onyxia/localization.tw.lua
@@ -8,7 +8,7 @@ local L
 L = DBM:GetModLocalization("Onyxia")
 
 L:SetGeneralLocalization{
-	name = "奧妮克希亞"
+	name 			= "奧妮克希亞"
 }
 
 L:SetWarningLocalization{
@@ -19,21 +19,21 @@ L:SetWarningLocalization{
 }
 
 L:SetTimerLocalization{
-	TimerWhelps = "奧妮克希亞幼龍"
+	TimerWhelps 		= "奧妮克希亞幼龍"
 }
 
 L:SetOptionLocalization{
-	SpecWarnBreath			= "為深呼吸顯示特別警告",
-	SpecWarnBlastNova		= "為衝擊新星顯示特別警告",
-	TimerWhelps				= "為奧妮克希亞幼龍顯示計時器",
-	WarnWhelpsSoon			= "為奧妮克希亞幼龍顯示預先警告",
-	SoundBreath				= "為深呼吸播放音效",
+	SpecWarnBreath		= "為深呼吸顯示特別警告",
+	SpecWarnBlastNova	= "為衝擊新星顯示特別警告",
+	TimerWhelps		= "為奧妮克希亞幼龍顯示計時器",
+	WarnWhelpsSoon		= "為奧妮克希亞幼龍顯示預先警告",
+	SoundBreath		= "為深呼吸播放音效",
 	PlaySoundOnBlastNova	= "為衝擊新星播放音效",
-	SoundWTF				= "為經典傳奇式奧妮克希亞副本播放一些有趣的音效",
-	WarnPhase3Soon			= "為第3階段顯示預先警告 (大約在41%)"
+	SoundWTF		= "為經典傳奇式奧妮克希亞副本播放一些有趣的音效",
+	WarnPhase3Soon		= "為第3階段顯示預先警告 (大約在41%)"
 }
 
 L:SetMiscLocalization{
-	YellP2 = "這毫無意義的行動讓我很厭煩。我會從上空把你們都燒成灰!",
-	YellP3 = "看起來需要再給你一次教訓，凡人!"
+	YellP2 			= "這毫無意義的行動讓我很厭煩。我會從上空把你們都燒成灰!",
+	YellP3 			= "看起來需要再給你一次教訓，凡人!"
 }
