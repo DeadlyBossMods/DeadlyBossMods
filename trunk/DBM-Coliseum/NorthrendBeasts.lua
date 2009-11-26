851856aa87633d175a391523694d81e522e9d734
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-11-26 05:12:42 +0000

    Make combat start still 15sec, but method still on 17 second delay, combat does start after 15 seconds but timers get delayed by 2 extra seconds for some reason or another.

--- trunk/DBM-Coliseum/NorthrendBeasts.lua
+++ trunk/DBM-Coliseum/NorthrendBeasts.lua
@@ -270,7 +270,7 @@ end
 function mod:OnSync(msg, arg)
 	if msg == "Phase2" then
 		self:ScheduleMethod(17, "WormsEmerge")
-		timerCombatStart:Show(17)
+		timerCombatStart:Show(15)
 		updateHealthFrame(2)
 	elseif msg == "Phase3" then
 		updateHealthFrame(3)
