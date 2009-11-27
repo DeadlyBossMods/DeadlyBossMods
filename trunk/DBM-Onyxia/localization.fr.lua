e469ff65825496ea833c5a7b528d06169808b7ae
psyco <psyco@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-11-27 11:02:17 +0000

    Fr update

--- trunk/DBM-Onyxia/localization.fr.lua
+++ trunk/DBM-Onyxia/localization.fr.lua
@@ -15,6 +15,7 @@ L:SetWarningLocalization{
 	SpecWarnBreath		= "Respiration profonde",
 	SpecWarnBlastNova	= "Nova explosive. BOUGEZ !",
 	WarnWhelpsSoon		= "Les Jeunes dragonnets onyxien arrivent bientôt",
+	WarnPhase2Soon		= "Phase 2 bientôt",
 	WarnPhase3Soon		= "Phase 3 bientôt"
 }
 
@@ -30,6 +31,7 @@ L:SetOptionLocalization{
 	SoundBreath				= "Joue un son pour la Respiration profonde",
 	PlaySoundOnBlastNova	= "Joue un sons pour la Nova explosive",
 	SoundWTF				= "Joue des sons amusants du légendaire raid classic d'Onyxia",
+	WarnPhase2Soon			= "Montre une alerte pour la phase 2 (~67%)",
 	WarnPhase3Soon			= "Montre une alerte pour la phase 3 (~41%)"
 }
 
