0fb7962ba823a00200855c3bcd5e9f98842b518b
Adam <MysticalOS@users.noreply.github.com>
2009-12-01 13:46:37 +0000

    Lets clean up that grandmagus split again, as well as add pre warning for second split if it's heroic difficulty.

--- DBM-Party-WotLK/TheNexus/GrandMagusTelestra.lua
+++ DBM-Party-WotLK/TheNexus/GrandMagusTelestra.lua
@@ -16,22 +16,22 @@ local warningSplitSoon	= mod:NewAnnounce("WarningSplitSoon", 2)
 local warningSplitNow	= mod:NewAnnounce("WarningSplitNow", 3)
 local warningMerge		= mod:NewAnnounce("WarningMerge", 2)
 
-local warnedSplit		= false
+local warnedSplit1		= false
+local warnedSplit2		= false
 
 function mod:OnCombatStart()
-	warnedSplit = false
+	warnedSplit1 = false
+	warnedSplit2 = false
 end
 
 function mod:UNIT_HEALTH(uId)
-    if self:GetUnitCreatureId(uId) == 26731 then
-        local h = UnitHealth(uId) / UnitHealthMax(uId)
-        if h > 0.85 then
-           warnedSplit = false
-        elseif h > 0.50 and h < 0.54 and not warnedSplit then
-           warningSplitSoon: Show()
-           warnedSplit = true
-        end
-    end
+	if not warnedSplit1 and self:GetUnitCreatureId(uId) == 26731 and UnitHealth(uId) / UnitHealthMax(uId) <= 0.54 then
+		warnedSplit1 = true
+		warningSplitSoon:Show()
+	elseif not warnedSplit2 and mod:IsDifficulty("heroic5") and self:GetUnitCreatureId(uId) == 26731 and UnitHealth(uId) / UnitHealthMax(uId) <= 0.19 then
+		warnedSplit2 = true
+		warningSplitSoon:Show()
+	end
 end
 
 function mod:CHAT_MSG_MONSTER_YELL(msg)
