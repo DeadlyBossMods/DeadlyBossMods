785c1b5352e31fc49dca5cd50250a5c48f39cbde
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-12-04 08:52:08 +0000

    Slight tweaks to some party mod bosses. Add timer for insanity cast, improve commander creatureid code some, Cancel chrono lord epoch curse timer if dispelled.

--- trunk/DBM-Party-WotLK/TheNexus/Commander.lua
+++ trunk/DBM-Party-WotLK/TheNexus/Commander.lua
@@ -1,14 +1,13 @@
-local mod			= DBM:NewMod("Commander", "DBM-Party-WotLK", 8)
-local L				= mod:GetLocalizedStrings()
-local faction		= UnitFactionGroup("player")
-local CreatureID	= 26796
-if faction == "Alliance" then
-	CreatureID = 26798
-end
+local mod = DBM:NewMod("Commander", "DBM-Party-WotLK", 8)
+local L = mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(CreatureID)
-mod:SetZone()
+
+if UnitFactionGroup("player") == "Alliance" then
+	mod:SetCreatureID(26798)
+else
+	mod:SetCreatureID(26796)
+end
 
 mod:RegisterCombat("combat")
 
@@ -24,16 +23,14 @@ local timerWhirlwindCD	= mod:NewCDTimer(15, 38619)
 
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(19134) 
-	and (args.sourceGUID == 26796 or args.sourceGUID == 26798) then
+	if args:IsSpellID(19134) and self:IsInCombat() then
 		warningFear:Show()
 		timerFearCD:Start()
 	end
 end
 
 function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(38619, 38618)
-	and (args.sourceGUID == 26796 or args.sourceGUID == 26798) then
+	if args:IsSpellID(38619, 38618) and self:IsInCombat() then
 		warningWhirlwind:Show()
 		timerWhirlwindCD:Start()
 	end
