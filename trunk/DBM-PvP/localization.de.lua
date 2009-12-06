cd080e33cd1bf96526e8f31a2c0100ebc1cf5d91
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-12-06 14:26:14 +0000

    Fix a bug in core causing alteracvalley lua errors. Mod was renamed from Alterac to AlteracValley but core still had a function calling old mod name. also redid folder structure of battlegrounds to now be dbm-pvp mods as this package is more than just battlegrounds now and has evolved into something bigger. Also added subcatagories. Another commit coming after this one.

---
+++ trunk/DBM-PvP/localization.de.lua
@@ -0,0 +1,231 @@
+﻿if GetLocale() ~= "deDE" then return end
+
+local L
+
+----------------------------
+--  General BG functions  --
+----------------------------
+L = DBM:GetModLocalization("Battlegrounds")
+
+L:SetGeneralLocalization({
+	name = "Allgemeine SF Funktionen"
+})
+
+L:SetTimerLocalization({
+	TimerInvite = "%s"
+})
+
+L:SetOptionLocalization({
+	ColorByClass	= "Einfärbung der Spielernamen in der Schlachtfeld-Punktetafel",
+	ShowInviteTimer	= "Zeige Zeit für Schlachtfeld Beitrittsmöglichkeit",
+	AutoSpirit	= "Automatisch Geist freilassen"
+})
+
+
+--------------
+--  Arenas  --
+--------------
+L = DBM:GetModLocalization("Arenas")
+
+L:SetGeneralLocalization({
+	name = "Arena"
+})
+
+L:SetTimerLocalization({
+	TimerStart	= "Spiel startet in",
+})
+
+L:SetOptionLocalization({
+	TimerStart = "Zeite Startzeit an"
+})
+
+L:SetMiscLocalization({
+	Start60 = "Noch eine Minute bis der Arenakampf beginnt!",
+	Start30 = "Noch dreißig Sekunden bis der Arenakampf beginnt!",
+	Start15 = "Noch fünfzehn Sekunden bis der Arenakampf beginnt!"
+})
+
+---------------
+--  Alterac  --
+---------------
+L = DBM:GetModLocalization("AlteracValley")
+
+L:SetGeneralLocalization({
+	name = "Alteractal"
+})
+
+L:SetTimerLocalization({
+	TimerStart = "Spiel startet in", 
+	TimerTower = "%s",
+	TimerGY = "%s",
+})
+
+L:SetMiscLocalization({
+	BgStart60 = "Der Kampf um das Alteractal beginnt in 1 Minute.",
+	BgStart30 = "Der Kampf um das Alteractal beginnt in 30 Sekunden.",
+	ZoneName = "Alteractal",
+})
+
+L:SetOptionLocalization({
+	TimerStart  = "Zeige Startzeit an",
+	TimerTower = "Zeige Turmzerstörungs Zeiten",
+	TimerGY = "Zeige Friedhof Eroberungszeiten",
+	AutoTurnIn = "Automatisches abgeben der Quests im Alterac Tal"
+})
+
+---------------
+--  Arathi  --
+---------------
+L = DBM:GetModLocalization("ArathiBasin")
+
+L:SetGeneralLocalization({
+	name = "Arathibecken"
+})
+
+L:SetMiscLocalization({
+	BgStart60 = "Die Schlacht um das Arathibecken wird in 1 Minute beginnen.",
+	BgStart30 = "Die Schlacht um das Arathibecken wird in 30 Sekunden beginnen.",
+	ZoneName = "Arathibecken",
+	ScoreExpr = "(%d+)/2000",
+	Alliance = "Allianz",
+	Horde = "Horde",
+	WinBarText = "%s gewinnt",
+	BasesToWin = "Punkte nötig um zu gewinnen: %d",
+	Flag = "Flagge"
+})
+
+L:SetTimerLocalization({
+	TimerStart = "Spiel startet in", 
+	TimerCap = "%s",
+})
+
+L:SetOptionLocalization({
+	TimerStart  = "Zeige Startzeit an",
+	TimerWin = "Zeige Siegzeiten an",
+	TimerCap = "Zeige Eroberungszeit an",
+	ShowAbEstimatedPoints = "Zeige geschätzte Kampf-Endpunkte",
+	ShowAbBasesToWin = "Zeige benötigte Anzahl an Punkten zum Sieg"
+})
+
+-----------------------
+--  Eye of the Storm --
+-----------------------
+L = DBM:GetModLocalization("EyeOfTheStorm")
+
+L:SetGeneralLocalization({
+	name = "Auge des Sturms"
+})
+
+L:SetMiscLocalization({
+	BgStart60 = "Die Schlacht beginnt in 1 Minute!",
+	BgStart30 = "Die Schlacht beginnt in 30 Sekunden!",
+	ZoneName = "Auge des Sturms",
+	ScoreExpr = "(%d+)/2000",
+	Alliance = "Alliance",
+	Horde = "Horde",
+	WinBarText = "%s wins",
+	FlagReset = "Die Flagge wurde zurückgesetzt.",
+	FlagTaken = "(.+) hat die Flagge erobert%.",
+	FlagCaptured = "Die %w+ hat die F%w+ erobert!",
+	FlagDropped = "Die F%w+ wurde fallengelassen.",
+
+})
+
+L:SetTimerLocalization({
+	TimerStart = "Spiel startet in", 
+	TimerFlag = "Flaggen respawn",
+})
+
+L:SetOptionLocalization({
+	TimerStart  = "Zeige Startzeit an",
+	TimerWin = "Zeige Siegzeiten an",
+	TimerFlag = "Zeige Flaggen Respawnzeit an",
+	ShowPointFrame = "Zeige Flaggenträger und Entpunktestand",
+})
+
+--------------------
+--  Warsong Gulch --
+--------------------
+L = DBM:GetModLocalization("WarsongGulch")
+
+L:SetGeneralLocalization({
+	name = "Kriegshymnenschlucht"
+})
+
+L:SetMiscLocalization({
+	BgStart60 = "Der Kampf um die Kriegshymnenschlucht beginnt in 1 Minute.",
+	BgStart30 = "Der Kampf um die Kriegshymnenschlucht beginnt in 30 Sekunden. Haltet Euch bereit!",
+	ZoneName = "Kriegshymnenschlucht",
+	Alliance = "Allianz",
+	Horde = "Horde",	
+	InfoErrorText = "Die Zielauswahl funktion für den Flaggenträger wird bei Kampfende wiederhergestellt",
+	ExprFlagPickUp = "(.*) hat die Flagge der (%w+) aufgenommen!",
+	ExprFlagCaptured = "(.+) hat die Flagge der (%w+) errungen!",
+	ExprFlagReturn = "Die Flagge der (%w+) wurde von (.+) zu ihrem Stützpunkt zurückgebracht!",
+	FlagAlliance = "Allianz Flagge: ",
+	FlagHorde = "Horde Flagge: ",
+	FlagBase = "Basis",
+})
+
+L:SetTimerLocalization({
+	TimerStart = "Spiel startet in", 
+	TimerFlag = "Flaggen respawn",
+})
+
+L:SetOptionLocalization({
+	TimerStart  = "Zeige Startzeit an",
+	TimerWin = "Zeige Siegzeiten an",
+	TimerFlag = "Zeige Flaggen Respawnzeit an",
+	ShowFlagCarrier = "Zeige Flaggenträger",
+	ShowFlagCarrierErrorNote = "Zeige Fehlermeldung wenn Flaggenträger Zielfunktion nicht gesetzt werden kann",
+})
+
+
+
+------------------------
+--  Isle of Conquest  --
+------------------------
+
+L = DBM:GetModLocalization("IsleOfConquest")
+
+L:SetGeneralLocalization({
+	name = "Isle of Conquest"
+})
+
+L:SetWarningLocalization({
+	WarnSiegeEngine		= "Belagerungsmaschine",
+	WarnSiegeEngineSoon	= "Belagerungsmaschine in ~10 Sekunden",
+})
+
+L:SetTimerLocalization({
+	TimerStart		= "Game starts", 
+	TimerPOI		= "%s",
+	TimerSiegeEngine	= "Belagerungsmaschine"
+})
+
+L:SetOptionLocalization({
+	TimerStart		= "Zeige Startzeit an", 
+	TimerPOI		= "Zeige Zeit für Eroberungen",
+	TimerSiegeEngine	= "Zeige Zeit für Belagerungsmaschine",
+	WarnSiegeEngine		= "Warn when Siege Engine is ready",
+	WarnSiegeEngineSoon	= "Warn when Siege Engine is almost ready"
+})
+
+L:SetMiscLocalization({
+	ZoneName		= "Insel der Eroberung",
+	BgStart60		= "Die Schlacht beginnt in 60 Sekunden.",
+	BgStart30		= "Die Schlacht beginnt in 30 Sekunden.",
+	BgStart15		= "Die Schlacht beginnt in 15 Sekunden.",
+	GoblinStartAlliance	= "Seht Ihr diese Zephyriumbomben? Benutzt sie an den Toren, während ich die Belagerungsmaschine repariere!",
+	GoblinStartHorde	= "Ich arbeite an der Belagerungsmaschine. Haltet mir einfach nur den Rücken frei. Benutzt diese Zephyriumbomben an den Toren, solltet Ihr sie brauchen!",
+	GoblinHalfwayAlliance	= "Ich hab's gleich! Haltet die Horde von hier fern. Kämpfen stand in der Ingenieursschule nicht auf dem Lehrplan!",
+	GoblinHalfwayHorde	= "Ich hab's gleich! Haltet mir die Allianz vom Leib. Kämpfen steht nicht in meinem Vertrag!",
+	GoblinFinishedAlliance	= "Meine beste Arbeit bisher! Diese Belagerungsmaschine ist bereit, ein bisschen Aktion zu sehen!",
+	GoblinFinishedHorde	= "Die Belagerungsmaschine ist bereit, loszurollen!",
+	GoblinBrokenAlliance	= "unknown !",	-- needs a translation
+	GoblinBrokenHorde	= "Schon wieder kaputt?! Ich werde es richten... Ihr solltet allerdings nicht davon ausgehen, dass das noch unter die Garantie fällt."
+})
+
+
+
+
---
+++ trunk/DBM-PvP/localization.ru.lua
@@ -0,0 +1,295 @@
+﻿if GetLocale() ~= "ruRU" then return end
+
+local L
+
+--------------------------
+--  General BG Options  --
+--------------------------
+L = DBM:GetModLocalization("Battlegrounds")
+
+L:SetGeneralLocalization({
+	name 			= "Общие параметры"
+})
+L:SetTimerLocalization({
+	TimerInvite 		= "%s"
+})
+
+L:SetOptionLocalization({
+	ColorByClass 		= "Показывать имена цветом класса в таблице очков",
+	ShowInviteTimer 	= "Отсчет времени до входа на ПБ",
+	AutoSpirit 		= "Автоматически покидать тело"
+})
+
+L:SetMiscLocalization({
+	ArenaInvite		= "Приглашение на Арену"
+})
+--------------
+--  Arenas  --
+--------------
+L = DBM:GetModLocalization("Arenas")
+
+L:SetGeneralLocalization({
+	name 			= "Арены"
+})
+
+L:SetTimerLocalization({
+	TimerStart 		= "Битва начнется через",
+    TimerShadow			= "Сумеречное зрение"
+})
+
+L:SetOptionLocalization({
+	TimerStart 		= "Отсчет времени до начала битвы",
+	TimerShadow 		= "Отсчет времени для Сумрачного зрения"
+})
+
+L:SetMiscLocalization({
+	Start60 		= "Битва на Арене начнется через минуту!",
+	Start30 		= "Битва на Арене начнется через 30 секунд!",
+	Start15 		= "Пятнадцать секунд до начала схватки на арене!"
+})
+----------------------
+--  Alterac Valley  --
+----------------------
+L = DBM:GetModLocalization("AlteracValley")
+
+L:SetGeneralLocalization({
+	name 			= "Альтеракская долина"
+})
+
+L:SetTimerLocalization({
+	TimerStart 		= "Битва начнется через", 
+	TimerTower 		= "%s",
+	TimerGY 		= "%s"
+})
+
+L:SetMiscLocalization({
+	BgStart60 		= "До начала сражения за Альтеракскую долину остается 1 минута.",
+	BgStart30 		= "30 секунд до начала сражения в Альтеракской долине.",
+	ZoneName 		= "Альтеракская долина"
+})
+
+L:SetOptionLocalization({
+	TimerStart  		= "Отсчет времени до начала битвы",
+	TimerTower 		= "Отсчет времени до захвата",
+	TimerGY 		= "Отсчет времени до захвата кладбищь",
+	AutoTurnIn 		= "Автоматическая сдача заданий"
+})
+
+--------------------
+--  Arathi Basin  --
+--------------------
+L = DBM:GetModLocalization("ArathiBasin")
+
+L:SetGeneralLocalization({
+	name 			= "Низина Арати"
+})
+
+L:SetMiscLocalization({
+	BgStart60 		= "Битва за Низину Арати начнется через минуту.",
+	BgStart30 		= "Битва за Низину Арати начнется через 30 секунд.",
+	ZoneName 		= "Низина Арати",
+	ScoreExpr 		= "(%d+)/1600",
+	Alliance 		= "Альянса",
+	Horde 			= "Орды",
+	WinBarText 		= "Победа %s",
+	BasesToWin 		= "Захвачено баз: %d",
+	Flag 			= "Флаг"
+})
+
+L:SetTimerLocalization({
+	TimerStart 		= "Битва начнется через", 
+	TimerCap 		= "%s"
+})
+
+L:SetOptionLocalization({
+	TimerStart  		= "Отсчет времени до начала битвы",
+	TimerWin 		= "Отсчет времени до победы",
+	TimerCap 		= "Отсчет времени до захвата",
+	ShowAbEstimatedPoints 	= "Отображать предполагаемые очки оставшиеся до победы/поражения",
+	ShowAbBasesToWin 	= "Отображать базы, которые необходимо захватить"
+})
+
+------------------------
+--  Eye of the Storm  --
+------------------------
+L = DBM:GetModLocalization("EyeOfTheStorm")
+
+L:SetGeneralLocalization({
+	name 			= "Око Бури"
+})
+
+L:SetMiscLocalization({
+	BgStart60 		= "Битва начнется через минуту!",
+	BgStart30 		= "Битва начнется через 30 секунд!",
+	ZoneName 		= "Око Бури",
+	ScoreExpr 		= "(%d+)/1600",
+	Alliance 		= "Альянса",
+	Horde 			= "Орды",
+	WinBarText 		= "Победа %s",
+	FlagReset 		= "Флаг возвращен на базу.",
+	FlagTaken 		= "(.+) захватывает флаг!",
+	FlagCaptured 		= "(.+) захватывает флаг (%w+)!",
+	FlagDropped 		= "Флаг уронили!"
+
+})
+
+L:SetTimerLocalization({
+	TimerStart 		= "Битва начнется через", 
+	TimerFlag 		= "Флаг восстановлен"
+})
+
+L:SetOptionLocalization({
+	TimerStart  		= "Отсчет времени до начала битвы",
+	TimerWin 		= "Отсчет времени до победы",
+	TimerFlag 		= "Отсчет времени до восстановления флага",
+	ShowPointFrame 		= "Отображать флагоносца и предполагаемые очки"
+})
+
+---------------------
+--  Warsong Gulch  --
+---------------------
+L = DBM:GetModLocalization("WarsongGulch")
+
+L:SetGeneralLocalization({
+	name 			= "Ущелье Песни Войны"
+})
+
+L:SetMiscLocalization({
+	BgStart60 		= "Битва за Ущелье Песни Войны начнется через 1 минуту.",
+	BgStart30 		= "Битва за Ущелье Песни Войны начнется через 30 секунд. Приготовтесь!",
+	ZoneName 		= "Ущелье Песни Войны",
+	Alliance 		= "Альянса",
+	Horde 			= "Орды",
+	InfoErrorText 		= "Функция выбора флагоносца, будет восстановлена после выхода из режима боя.",
+	ExprFlagPickUp 		= "(.+) подымает флаг (%w+)",
+	ExprFlagCaptured 	= "(.+) захватывает флаг (%w+)!",
+	ExprFlagReturn 		= "(.+) возвращает флаг (%w+) на базу!",
+	FlagAlliance 		= "Флаг Альянса: ",
+	FlagHorde 		= "Флаг Орды: ",
+	FlagBase 		= "База"
+})
+
+L:SetTimerLocalization({
+	TimerStart 		= "Битва начнется через", 
+	TimerFlag 		= "Восстановление флага"
+})
+
+L:SetOptionLocalization({
+	TimerStart  		= "Отсчет времени до начала битвы",
+	TimerFlag 		= "Отсчет времени до восстановления флага",
+	ShowFlagCarrier 	= "Показать флагоносца",
+	ShowFlagCarrierErrorNote = "Сообщения об ошибках в режиме боя"
+})
+
+----------------------------------
+--  Archavon the Stone Watcher  --
+----------------------------------
+
+L = DBM:GetModLocalization("Archavon")
+
+L:SetGeneralLocalization({
+	name 			= "Склеп Аркавона"
+})
+
+L:SetWarningLocalization({
+	WarningShards 		= "Град из острых камней на >%s<",
+	WarningGrab 		= "Страж бросается к >%s<"
+})
+
+L:SetTimerLocalization({
+	TimerShards 		= "Каменные осколки: %s"
+})
+
+L:SetMiscLocalization({
+	TankSwitch 		= "%%s нападает на (%S+)!"
+})
+
+L:SetOptionLocalization({
+	TimerShards 		= "Отсчет времени до града из Каменных осколков",
+	WarningShards 		= "Объявлять цели под Каменными осколками",
+	WarningGrab 		= "Объявлять о захвате цели"
+})
+--------------------------------
+--  Emalon the Storm Watcher  --
+--------------------------------
+
+L = DBM:GetModLocalization("Emalon")
+
+L:SetGeneralLocalization{
+	name 			= "Эмалон Созерцатель Бури"
+}
+
+L:SetWarningLocalization{
+	specWarnNova		= "Вспышка молнии - бегите",
+	warnNova 		= "Вспышка молнии",
+	warnOverCharge		= "Перезарядка"
+}
+
+L:SetTimerLocalization{
+	timerMobOvercharge	= "Взрыв избыточно заряда"
+}
+
+L:SetOptionLocalization{
+	specWarnNova 		= ("Спец-предупреждение для |cff71d5ff|Hspell:%d|h%s|h|r"):format(64216, "Вспышка молнии"),
+	warnNova 		= ("Предупреждение для |cff71d5ff|Hspell:%d|h%s|h|r"):format(64216, "Вспышка молнии"),
+	warnOverCharge 		= ("Предупреждение для |cff71d5ff|Hspell:%d|h%s|h|r"):format(64218, "Перезарядка"),
+	NovaSound		= "Звуковой сигнал при Вспышке молнии",
+	timerMobOvercharge	= "Отсчет времени для Избыточно заряда (суммирующийся отрицательный эффект)"
+}
+
+---------------------------------
+--  Koralon the Flame Watcher  --
+---------------------------------
+
+L = DBM:GetModLocalization("Koralon")
+
+L:SetGeneralLocalization{
+	name 			= "Коралон Страж Огня"
+}
+
+L:SetWarningLocalization{
+	SpecWarnCinder		= "Пылающая головня - бегите"
+}
+
+L:SetTimerLocalization{
+}
+
+L:SetOptionLocalization{
+	SpecWarnCinder		= "Спец-предупреждение, когда вы в Пылающей головне",
+	PlaySoundOnCinder	= "Звуковое уведомление, когда вы в Пылающей головне"
+}
+
+L:SetMiscLocalization{
+	Meteor			= "%s бросает Кулаки-метеоры!"
+}
+
+
+------------------------
+--  Isle of Conquest  --
+------------------------
+
+L = DBM:GetModLocalization("IsleOfConquest")
+
+L:SetGeneralLocalization({
+	name 			= "Остров Завоеваний"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+	TimerStart		= "Битва начнется через", 
+	TimerPOI		= "%s"
+})
+
+L:SetOptionLocalization({
+	TimerStart		= "Отсчет времени до начала битвы", 
+	TimerPOI		= "Отсчет времени до захвата"
+})
+
+L:SetMiscLocalization({
+	ZoneName		= "Остров Завоеваний",
+	BgStart60		= "Битва начнется через 60 секунд.",
+	BgStart30		= "Битва начнется через 30 секунд.",
+	BgStart15		= "Битва начнется через 15 секунд."
+})
