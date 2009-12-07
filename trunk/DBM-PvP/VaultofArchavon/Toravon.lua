cb3b695fac9adb9fa3dc236632fefa4a3d759412
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-12-07 16:57:52 +0000

    Pretty sure that's correct creatureid of the two.

--- trunk/DBM-PvP/VaultofArchavon/Toravon.lua
+++ trunk/DBM-PvP/VaultofArchavon/Toravon.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod("Toravon", "DBM-PvP", 3)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 2521 $"):sub(12, -3))
-mod:SetCreatureID(38433, 38462)
+mod:SetCreatureID(38433)
 
 mod:RegisterCombat("combat")
 
