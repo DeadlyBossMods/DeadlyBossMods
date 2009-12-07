19519228b02e5ad859708efe979c67d4633e6be3
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-12-07 10:42:19 +0000

    Flag the two mods that use icon with appropriate mod:SetUsedIcons(1, 2, 3, 4, 5, 6, 7, 8)

--- trunk/DBM-Naxx/FrostwyrmLair/Kel'Thuzad.lua
+++ trunk/DBM-Naxx/FrostwyrmLair/Kel'Thuzad.lua
@@ -4,6 +4,7 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(15990)
 mod:SetMinCombatTime(60)
+mod:SetUsedIcons(1, 2, 3, 4, 5, 6, 7, 8)
 
 mod:RegisterCombat("yell", L.Yell)
 
