b61005a5fbe22686e32bb8c2d8f1e05e3e65440d
Arta <Arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-12-07 21:27:32 +0000

    Fixed rare bug with Brain Link targets

--- DBM-Ulduar/YoggSaron.lua
+++ DBM-Ulduar/YoggSaron.lua
@@ -10,12 +10,10 @@ mod:RegisterEvents(
 	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
 	"SPELL_SUMMON",
-	"CHAT_MSG_MONSTER_YELL",
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_REMOVED",
 	"SPELL_AURA_REMOVED_DOSE",
-	"UNIT_HEALTH",
-	"UNIT_DIED"
+	"UNIT_HEALTH"
 )
 
 mod:SetUsedIcons(6, 7, 8)
@@ -30,7 +28,7 @@ local warnCrusherTentacleSpawned	= mod:NewAnnounce("WarningCrusherTentacleSpawne
 local warnP2 						= mod:NewPhaseAnnounce(2, 2)
 local warnP3 						= mod:NewPhaseAnnounce(3, 2)
 local warnSanity 					= mod:NewAnnounce("WarningSanity", 3)
-local warnBrainLink 				= mod:NewAnnounce("WarningBrainLink", 2)
+local warnBrainLink 				= mod:NewTargetAnnounce(63802, 2)
 local warnBrainPortalSoon			= mod:NewAnnounce("WarnBrainPortalSoon", 1)
 local warnEmpowerSoon				= mod:NewAnnounce("WarnEmpowerSoon", 4)
 
@@ -66,11 +64,11 @@ mod:AddBoolOption("SetIconOnBrainLinkTarget")
 
 local phase							= 1
 local targetWarningsShown			= {}
-local brainLink1
+local brainLinkTargets = {}
+local brainLinkIcon = 7
 
 function mod:OnCombatStart(delay)
 	phase = 1
-	brainLink1 = nil
 	enrageTimer:Start()
 	timerAchieve:Start()
 	if self.Options.ShowSaraHealth and not self.Options.HealthFrame then
@@ -80,6 +78,21 @@ function mod:OnCombatStart(delay)
 		DBM.BossHealth:AddBoss(33134, L.Sara)
 	end
 	table.wipe(targetWarningsShown)
+	table.wipe(brainLinkTargets)
+end
+
+function mod:FervorTarget()
+	local targetname = self:GetBossTarget(33134)
+	if not targetname then return end
+	if targetname == UnitName("player") then
+		specWarnFervorCast:Show(targetname)
+	end
+end
+
+function mod:warnBrainLink()
+	warnBrainLink:Show(table.concat(brainLinkTargets, "<, >"))
+	table.wipe(brainLinkTargets)
+	brainLinkIcon = 7
 end
 
 function mod:SPELL_CAST_START(args)
@@ -101,22 +114,6 @@ function mod:SPELL_CAST_START(args)
 	end
 end
 
-function mod:FervorTarget()
-	local targetname = self:GetBossTarget(33134)
-	if not targetname then return end
-	if targetname == UnitName("player") then
-		specWarnFervorCast:Show(targetname)
-	end
-end
-
-function mod:CHAT_MSG_MONSTER_YELL(msg, sender)
-	if msg == L.YellRage then
-		if self.Options.RaidRageSpam then
---			SendChatMessage(L.RaidRage, "RAID")
-		end
-	end
-end
-
 function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(64144) and self:GetUnitCreatureId(args.sourceGUID) == 33966 then 
 		warnCrusherTentacleSpawned:Show()
@@ -130,25 +127,16 @@ function mod:SPELL_SUMMON(args)
 	end
 end
 
--- 4/24 21:56:29.656  SPELL_AURA_APPLIED,0xF150008208022C87,"Yogg-Saron",0xa48,0xF150008208022C87,"Yogg-Saron",0xa48,63894,"Shadowy Barrier",0x20,BUFFfunction mod:SPELL_AURA_APPLIED(args)
-
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(63802) then		-- Brain Link
-		if not brainLink1 then
-			if self.Options.SetIconOnBrainLinkTarget then
-				self:SetIcon(args.destName, 6, 30)
-			end
-			brainLink1 = args.destName
-		else
-			if self.Options.SetIconOnBrainLinkTarget then
-				self:SetIcon(args.destName, 7, 30)
-			end
-			warnBrainLink:Show(brainLink1, args.destName)
-			self:AnnounceBrainLink(brainLink1, args.destName)
-			self:AnnounceBrainLink(args.destName, brainLink1)
-			brainLink1 = nil
+		self:UnscheduleMethod("warnBrainLink")
+		brainLinkTargets[#brainLinkTargets + 1] = args.destName
+		self:SetIcon(args.destName, brainLinkIcon, 30)
+		brainLinkIcon = brainLinkIcon - 1
+		if args:IsPlayer() then
+			specBrainLink:Show()
 		end
-
+		mod:ScheduleMethod(0.2, "warnBrainLink")
 	elseif args:IsSpellID(63830, 63881) then   -- Malady of the Mind (Fear) 
 		if self.Options.SetIconOnFearTarget then
 			self:SetIcon(args.destName, 8, 30) 
@@ -160,16 +148,13 @@ function mod:SPELL_AURA_APPLIED(args)
 				specWarnMaladyNear:Show(args.destName) 
 			end 
 		end 
-		
 	elseif args:IsSpellID(63042) and self.Options.SetIconOnMCTarget then	-- MC
 		self:SetIcon(args.destName, 8, 30)
-
 	elseif args:IsSpellID(64126, 64125) then	-- Squeeze		
 		warnSqueeze:Show(args.destName)		
 		if args:IsPlayer() and self.Options.WarningSqueeze then			
 			SendChatMessage(L.WarningYellSqueeze, "YELL")			
 		end	
-
 	elseif args:IsSpellID(63138) then	-- Sara's Fervor
 		warnFervor:Show(args.destName)
 		if self.Options.SetIconOnFervorTarget then
@@ -178,7 +163,6 @@ function mod:SPELL_AURA_APPLIED(args)
 		if args:IsPlayer() then 
 			specWarnFervor:Show()
 		end
-
 	elseif args:IsSpellID(63894) then	-- Shadowy Barrier of Yogg-Saron (this is happens when p2 starts)
 		phase = 2
 		brainportal:Start(60)
@@ -194,7 +178,7 @@ function mod:SPELL_AURA_APPLIED(args)
 	elseif args:IsSpellID(64167, 64163) then	-- Lunatic Gaze (reduces sanity)
 		timerLunaricGaze:Start()
 	elseif args:IsSpellID(64465) then
-        timerEmpower:Start()
+		timerEmpower:Start()
 		timerEmpowerDuration:Start()
 		warnEmpowerSoon:Schedule(40)
 	end
@@ -214,16 +198,6 @@ function mod:SPELL_AURA_REMOVED(args)
 		timerNextLunaricGaze:Start()
 	end
 end
-
-function mod:AnnounceBrainLink(player, other)
-	if player == UnitName("player") then
-		specWarnBrainLink:Show(other)
-	end
-	if DBM:GetRaidRank() >= 1 and self.Options.WhisperBrainLink then
-		self:SendWhisper(L.WhisperBrainLink:format(other), player)
-	end
-end
-
 
 function mod:SPELL_AURA_REMOVED_DOSE(args)
 	if args:IsSpellID(63050) and args.destGUID == UnitGUID("player") then
