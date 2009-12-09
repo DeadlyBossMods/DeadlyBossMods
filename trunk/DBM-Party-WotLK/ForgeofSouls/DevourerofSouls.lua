d536837a9c237c8e5ef0e3ca4295a84fb6f59c70
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-12-09 13:05:28 +0000

    Fix doubling of timers/warnings by makings ure destination is only player (since boss AND player both gain buff/debuff from spellid at same time)

--- trunk/DBM-Party-WotLK/ForgeofSouls/DevourerofSouls.lua
+++ trunk/DBM-Party-WotLK/ForgeofSouls/DevourerofSouls.lua
@@ -20,21 +20,21 @@ local timerUnleashedSouls		= mod:NewBuffActiveTimer(5, 68939)
 local specwarnMirroredSoul		= mod:NewSpecialWarning("specwarnMirroredSoul")
 
 function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(68820) then							-- Well of Souls
+	if args:IsSpellID(68820) then								-- Well of Souls
 		warnWellofSouls:Show()
-	elseif args:IsSpellID(68939) then						-- Unleashed Souls
+	elseif args:IsSpellID(68939) then							-- Unleashed Souls
 		warnUnleashedSouls:Show()
-	elseif args:IsSpellID(68899, 70324) then				-- Wailing Souls
+	elseif args:IsSpellID(68899, 70324) then					-- Wailing Souls
 		warnWailingSouls:Show()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpellID(69051) then							-- Mirrored Soul
+	if args:IsSpellID(69051) and args:IsDestTypePlayer() then	-- Mirrored Soul
 		warnMirroredSoul:Show(args.destName)
 		timerMirroredSoul:Show(args.destName)
 		specwarnMirroredSoul:Show()
-	elseif args:IsSpellID(68939) then						-- Unleashed Souls
+	elseif args:IsSpellID(68939) then							-- Unleashed Souls
 		timerUnleashedSouls:Start()
 	end
 end
