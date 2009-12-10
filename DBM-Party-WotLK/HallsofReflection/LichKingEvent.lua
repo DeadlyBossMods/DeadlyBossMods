50255e5860c79d221fa3998b2e7a84684c62f682
Adam <MysticalOS@users.noreply.github.com>
2009-12-10 10:02:54 +0000

    Fix scourgelord combat start delay with min combat 20 now, shoudln't pre maturely end combat during rollplay now. added many new warnings to Ick as well as fixes. Fix lichkingevent so it doesn't load during scourgelordtyrannus.

--- DBM-Party-WotLK/HallsofReflection/LichKingEvent.lua
+++ DBM-Party-WotLK/HallsofReflection/LichKingEvent.lua
@@ -2,7 +2,7 @@ local mod = DBM:NewMod("LichKingEvent", "DBM-Party-WotLK", 16)
 local L = mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 2153 $"):sub(12, -3))
-mod:SetCreatureID(36658) 		-- correct this
+mod:SetCreatureID(90000) 		-- correct this
 --mod:SetUsedIcons(8)
 
 mod:RegisterCombat("combat")
--- DBM-Party-WotLK/PitofSaron/Ick.lua
+++ DBM-Party-WotLK/PitofSaron/Ick.lua
@@ -8,25 +8,49 @@ mod:SetCreatureID(36476, 36477)
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
 	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_REMOVED",
 	"CHAT_MSG_RAID_BOSS_WHISPER",
 	"SPELL_PERIODIC_DAMAGE"
 )
 
+local isMelee = select(2, UnitClass("player")) == "ROGUE"
+			or select(2, UnitClass("player")) == "WARRIOR"
+			or select(2, UnitClass("player")) == "DEATHKNIGHT"
+
 local warnPursuitCast			= mod:NewCastAnnounce(68987)
+local warnPoisonNova			= mod:NewCastAnnounce(68989)
+local warnExplosiveBarrage		= mod:NewSpellAnnounce(69015)
 local timerPursuitConfusion		= mod:NewBuffActiveTimer(12, 69029)
 local warnPustulantFlesh		= mod:NewTargetAnnounce(69581)
 local timerPustulantFlesh		= mod:NewTargetTimer(10, 69581)
+local timerPoisonNova			= mod:NewCastTimer(5, 68989)
 local specWarnToxic				= mod:NewSpecialWarning("specWarnToxic")
 local specWarnPursuit			= mod:NewSpecialWarning("specWarnPursuit")
+local specWarnPoisonNova		= mod:NewSpecialWarning("specWarnPoisonNova", isMelee)
 
+mod:AddBoolOption("PlaySoundOnPoisonNova", isMelee)
 mod:AddBoolOption("PlaySoundOnPursuit", true)
 mod:AddBoolOption("SetIconOnPursuitTarget", true)
 
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(68987) then							-- Pursuit
 		warnPursuitCast:Show()
+	elseif args:IsSpellID(68989, 70434) then				-- Poison Nova
+		warnPoisonNova:Show()
+		timerPoisonNova:Start()
+		specWarnPoisonNova:Show()
+		if self.Options.PlaySoundOnPoisonNova then
+			PlaySoundFile("Sound\\Creature\\HoodWolf\\HoodWolfTransformPlayer01.wav")
+		end
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(69015) then							-- Explosive Barrage
+		warnExplosiveBarrage:Show()
 	end
 end
 
@@ -34,9 +58,15 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(69029, 70850) then							-- Pursuit Confusion
 		timerPursuitConfusion:Show(args.destName)
-	elseif args:IsSpellID(69581, 70273) then							-- Pustulant Flesh
+	elseif args:IsSpellID(69581, 70273) and self:IsInCombat() then	-- Pustulant Flesh
 		warnPustulantFlesh:Show(args.destName)
 		timerPustulantFlesh:Show(args.destName)
+	end
+end
+
+function mod:SPELL_AURA_REMOVED(args)
+	if args:IsSpellID(69581, 70273) and self:IsInCombat() then		-- Pustulant Flesh
+		timerPustulantFlesh:Cancel(args.destName)
 	end
 end
 
