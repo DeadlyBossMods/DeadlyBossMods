ec0a4343dfe1c3883886db3f2d3c4dd10f4faa0b
Adam <MysticalOS@users.noreply.github.com>
2009-12-11 16:33:50 +0000

    This doesn't actually fix the lua error that exists in both deathbringer and deathwhisper, it just prevents the error from  happening in 25man normal til it's fixed (so code matches deathbringer)

--- DBM-Icecrown/TheLowerSpire/Deathwhisper.lua
+++ DBM-Icecrown/TheLowerSpire/Deathwhisper.lua
@@ -47,7 +47,7 @@ local function showDominateMindWarning()
 end
 
 local lastDD	= 0
-local MCTargets	={}
+local MCTargets	= {}
 local MCIcon = 7
 
 function mod:addsTimer()
@@ -76,7 +76,7 @@ function mod:SPELL_AURA_APPLIED(args)
 			MCIcon = MCIcon - 1
 		end
 		self:Unschedule(showDominateMindWarning)
-		if not mod:IsDifficulty("heroic25") or #MCTargets >= 3 then
+		if not mod:IsDifficulty("normal25") or #MCTargets >= 3 then
 			showDominateMindWarning()
 		else
 			self:Schedule(0.3, showDominateMindWarning)
