facad6b608266646e7ad555eeb755e142a87c4ed
Arta <Arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-12-12 16:28:59 +0000

    ruRU localization (thanks to Swix)

--- trunk/DBM-Icecrown/localization.ru.lua
+++ trunk/DBM-Icecrown/localization.ru.lua
@@ -2,275 +2,309 @@
 
 local L
 
+---------------------------
+--  Trash - Lower Spire  --
+---------------------------
+L = DBM:GetModLocalization("LowerSpireTrash")
+
+L:SetGeneralLocalization{
+	name = "Треш-мобы"
+}
+
+L:SetWarningLocalization{
+	SpecWarnDisruptingShout			= "Разрушительный крик - остановите чтение заклинаний",
+}
+
+L:SetOptionLocalization{
+	SpecWarnDisruptingShout			= "Спец-предупреждение для заклинания $spell:71022",	
+}
+
+
 ----------------------
 --  Lord Marrowgar  --
 ----------------------
 L = DBM:GetModLocalization("LordMarrowgar")
 
-L:SetGeneralLocalization({
+L:SetGeneralLocalization{
 	name = "Лорд Ребрад"
-})
+}
 
-L:SetWarningLocalization({
-	warnImpale			= "Прокалывание: >%S<",
-	specWarnWhirlwind		= "Вихрь - бегите",
-	specWarnColdflame		= "Холодное пламя - отбегите"
-})
+L:SetTimerLocalization{
+	achievementBoned	= "Время до освобождения"
+}
 
-L:SetOptionLocalization({
-	warnImpale			= "Предупредить о проткнутой цели",
-	specWarnWhirlwind		= "Спец-предупреждение для Вихря",
-	specWarnColdflame		= "Спец-предупреждение, когда вы получаете урон от Холодного пламени",
-	PlaySoundOnWhirlwind		= "Звуковой сигнал при Вихре"
-})
+L:SetWarningLocalization{
+	WarnImpale			= "Прокалывание: >%s<",
+	SpecWarnWhirlwind	= "Вихрь - бегите",
+	SpecWarnColdflame	= "Холодное пламя - отбегите"
+}
+
+L:SetOptionLocalization{
+	WarnImpale				= "Анонсировать проткнутые цели",
+	SpecWarnWhirlwind		= "Спец-предупреждение для $spell:69076",
+	SpecWarnColdflame		= "Спец-предупреждение, при получении урона от $spell:70825",
+	PlaySoundOnWhirlwind	= "Звуковой сигнал при $spell:69076",
+	achievementBoned		= "Показывать для достижения Косточка попалась",
+	SetIconOnImpale			= "Маркировать игроков, насаженных на шип"
+}
 
 -------------------------
 --  Lady Deathwhisper  --
 -------------------------
 L = DBM:GetModLocalization("Deathwhisper")
 
-L:SetGeneralLocalization({
-	name = "Леди Смертный Шёпот"
-})
+L:SetGeneralLocalization{
+	name = "Леди Смертный Шепот"
+}
 
-L:SetTimerLocalization({
-	TimerAdds			= "Призыв помощников"
-})
+L:SetTimerLocalization{
+	TimerAdds				= "Призыв помощников"
+}
 
-L:SetWarningLocalization({
-	WarnAddsSoon			= "Скоро призыв помощников",
-	WarnAdherent			= "Cult Adherent transforming",
-	SpecWarnDeathDecay		= "Смерть и разложение - бегите",
-	SpecWarnCurseTorpor		= "Curse of Torpor on you"
-})
+L:SetWarningLocalization{
+	WarnReanimating					= "Помощник трансформируется",			-- Reanimating an adherent or fanatic
+	WarnTouchInsignificance			= "%s на >%s< (%s)",		-- Touch of Insignificance on >args.destName< (args.amount)
+	SpecWarnDeathDecay				= "Смерть и разложение - бегите!",
+	SpecWarnCurseTorpor				= "Проклятие оцепенения на вас!",
+	SpecWarnTouchInsignificance		= "Прикосновение незначительности (3 стака)",
+	WarnAddsSoon					= "Скоро призыв помощников",
+	SpecWarnDarkReckoning			= "Мрачный итог - бегите!"
+}
 
-L:SetOptionLocalization({
-	WarnAddsSoon			= "Предупреждать заранее о призыве помощников",
-	WarnAdherent			= "Show warning for Cult Adherent transformation",	-- Cult Adherent -> Reanimated Adherent transformation iirc
-	SpecWarnDeathDecay		= "Спец-предупреждение, когда вы под воздействем эффекта Смерти и разложения",
-	SpecWarnCurseTorpor		= "Show special warning when you are affected by Curse of Torpor",
-	TimerAdds			= "Отсчет времени до призыва помощников",
+L:SetOptionLocalization{
+	WarnAddsSoon					= "Предупреждать заранее о призыве помощников",
 	WarnTouchInsignificance			= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.spell:format(71204, GetSpellInfo(71204) or "unknown"),		-- Warning isnt default (it has a count number), option is default tho (no need for translation this way)
-})
+	WarnReanimating					= "Предупреждать, при трансформации помощника",											-- Reanimated Adherent/Fanatic spawning
+	SpecWarnTouchInsignificance		= "Спец-предупреждение, когда на вас 3 стака $spell:71204",
+	SpecWarnDeathDecay				= "Спец-предупреждение, когда вы стоите в луже $spell:72108",
+	SpecWarnCurseTorpor				= "Спец-предупреждение, когда на вас $spell:71237",
+	TimerAdds						= "Отсчет времени до призыва помощников",
+	SetIconOnDominateMind			= "Устанавливать метки на игроков, взятых под $spell:71289",
+	SpecWarnDarkReckoning			= "Спец-предупреждение, когда на вас $spell:69483",
+	PlaySoundOnDarkReckoning		= "Звуковой сигнал, при $spell:69483"
+}
 
-L:SetMiscLocalization({
-	YellAdds				= "Arise, and exult in your pure form!",
-	YellPull				= "What is this disturbance? You dare trespass upon this hallowed ground? This shall be your final resting place!",
-	YellAdherent			= "Loyal adherent, I release you from the curse of flesh!",
-	YellEmbrace				= "Embrace the darkness... darkness eternal.",
-	YellBlessing			= "Take this blessing and show these intruders a taste of the Master's power!"
-})
-
-------------------------
---  The Deathbringer  --
-------------------------
-L = DBM:GetModLocalization("Deathbringer")
-
-L:SetGeneralLocalization({
-	name = "Смертеносец"
-})
-
-L:SetWarningLocalization({
-})
-
-L:SetOptionLocalization({
-})
-
+L:SetMiscLocalization{
+	YellPull				= "Как вы смеете ступать в эти священные покои? Это место станет вашей могилой!",
+	YellReanimatedFanatic	= "Восстань и обрети истинную форму!",
+}
 ----------------------
 --  Gunship Battle  --
 ----------------------
 L = DBM:GetModLocalization("GunshipBattle")
 
-L:SetGeneralLocalization({
-	name = "Воздушное Сражение"
-})
+L:SetGeneralLocalization{
+	name = "Воздушный бой"
+}
 
-L:SetWarningLocalization({
-})
+L:SetWarningLocalization{
+	WarnBattleFury	= "%s (%d)"
+}
 
-L:SetOptionLocalization({
-})
+L:SetOptionLocalization{
+	WarnBattleFury	= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.spell:format(69638, GetSpellInfo(69638) or "Battle Fury")
+}
+
+-----------------------------
+--  Deathbringer Saurfang  --
+-----------------------------
+L = DBM:GetModLocalization("Deathbringer")
+
+L:SetGeneralLocalization{
+	name = "Саурфанг Смертоносный"
+}
+
+L:SetWarningLocalization{
+	warnFrenzySoon	= "Скоро Бешенство",
+	specwarnRuneofBlood	= "Руна крови на вас"
+
+}
+
+L:SetOptionLocalization{
+	warnFrenzySoon	= "Предупреждать о скором Бешенстве (на ~33%)",
+	specwarnRuneofBlood	= "Спец-предупреждение, когда на вас накладывается $spell:72410",
+	RangeFrame		= "Показывать окно допустимой дистанции"
+}
 
 -----------------
 --  Festergut  --
 -----------------
 L = DBM:GetModLocalization("Festergut")
 
-L:SetGeneralLocalization({
+L:SetGeneralLocalization{
 	name = "Тухлопуз"
-})
+}
 
-L:SetWarningLocalization({
-})
+L:SetWarningLocalization{
+}
 
-L:SetOptionLocalization({
-})
+L:SetOptionLocalization{
+}
 
 ---------------
 --  Rotface  --
 ---------------
 L = DBM:GetModLocalization("Rotface")
 
-L:SetGeneralLocalization({
+L:SetGeneralLocalization{
 	name = "Гниломорд"
-})
+}
 
-L:SetWarningLocalization({
-	SpecWarnStickyOoze		= "Липкая жижа - бегите",
+L:SetWarningLocalization{
+	SpecWarnStickyOoze			= "Липкая жижа - бегите",
 	SpecWarnRadiatingOoze		= "Radiating Ooze",
-	SpecWarnMutatedInfection 	= "Mutated Infection on you"
-})
+	SpecWarnMutatedInfection	= "Mutated Infection on you"
+}
 
-L:SetTimerLocalization({
-	NextPoisonSlimePipes		= "Next Poison Slime Pipes"
-})
+L:SetTimerLocalization{
+	NextPoisonSlimePipes	= "Next Poison Slime Pipes"
+}
 
-L:SetOptionLocalization({
-	SpecWarnStickyOoze		= "Спец-предупреждение для Липкой жижи",
+L:SetOptionLocalization{
+	SpecWarnStickyOoze			= "Спец-предупреждение для Липкой жижи",
 	SpecWarnRadiatingOoze		= "Show special warning for Radiating Ooze",
 	NextPoisonSlimePipes		= "Show timer for next Poison Slime Pipes",
 	SpecWarnMutatedInfection 	= "Show special warning when you are affected by Mutated Infection",
 	InfectionIcon				= "Set icons on Mutated Infection targets",
 	WarnOozeSpawn				= "Show warning for Little Ooze spawning"
-})
+}
 
-L:SetMiscLocalization({
-	YellSlimePipes		= "Хорошие новости, для всех!"	-- Good news, everyone! I've fixed the poison slime pipes!
-})
+L:SetMiscLocalization{
+	YellSlimePipes	= "Good news, everyone! I've fixed the poison slime pipes!"	-- Professor Putricide
+}
 
 ---------------------------
 --  Professor Putricide  --
 ---------------------------
 L = DBM:GetModLocalization("Putricide")
 
-L:SetGeneralLocalization({
+L:SetGeneralLocalization{
 	name = "Профессор Мерзоцид"
-})
+}
 
-L:SetWarningLocalization({
-})
+L:SetWarningLocalization{
+}
 
-L:SetOptionLocalization({
-})
+L:SetOptionLocalization{
+}
 
-L:SetMiscLocalization({
+L:SetMiscLocalization{
 	YellPull	= "Good news, everyone! I think I've perfected a plague that will destroy all life on Azeroth!"
-})
+}
 
 ----------------------------
 --  Blood Prince Council  --
 ----------------------------
 L = DBM:GetModLocalization("BPCouncil")
 
-L:SetGeneralLocalization({
-	name = "Совет Принцев Крови"
-})
+L:SetGeneralLocalization{
+	name = "Совет Кровавых принцев"
+}
 
-L:SetWarningLocalization({
+L:SetWarningLocalization{
 	WarnTargetSwitch		= "Switch target to: %s",		-- Ugh, no nice spellname/id to use for general localization :(
 	WarnTargetSwitchSoon	= "Target switch soon",			-- Spellname = Invocation of Blood   or   Spellname = Invocation of Blood (X) Move  (server side script where X indicates the first letter of the Prince name
 	SpecWarnResonance		= "Теневой резонанс - бегите"
-})
+}
 
-L:SetTimerLocalization({
-	TimerTargetSwitch		= "Possible target switch"
-})
+L:SetTimerLocalization{
+	TimerTargetSwitch	= "Possible target switch"
+}
 
-L:SetOptionLocalization({
+L:SetOptionLocalization{
 	WarnTargetSwitch		= "Show warning to switch targets",								-- Warn when another Prince needs to be damaged
 	WarnTargetSwitchSoon	= "Show pre-warning to switch targets",							-- Every ~31 secs, you have to dps a different Prince
 	TimerTargetSwitch		= "Show timer for target switch cooldown",
 	SpecWarnResonance		= "Show special warning when a Dark Nucleus is following you"	-- If it follows you, you have to move to the tank
-})
+}
 
-L:SetMiscLocalization({
-	Keleseth				= "Принц Келесет",
-	Taldaram				= "Принц Талдарам",
-	Valanar					= "Принц Валанар"
-})
+L:SetMiscLocalization{
+	Keleseth	= "Принц Келесет",
+	Taldaram	= "Принц Талдарам",
+	Valanar		= "Принц Валанар"
+}
 
------------------------
---  Queen Lana'thel  --
------------------------
+-----------------------------
+--  Blood-Queen Lana'thel  --
+-----------------------------
 L = DBM:GetModLocalization("Lanathel")
 
-L:SetGeneralLocalization({
-	name = "Королева Лана'Тель"
-})
+L:SetGeneralLocalization{
+	name = "Кровавая королева Лана'тель"
+}
 
-L:SetWarningLocalization({
+L:SetWarningLocalization{
 	SpecWarnPactDarkfallen	= "Pact of the Darkfallen on you"
-})
+}
 
-L:SetOptionLocalization({
+L:SetOptionLocalization{
 	SpecWarnPactDarkfallen	= "Show special warning when you are affected by Pact of the Darkfallen"
-})
+}
 
 -----------------------------
 --  Valithria Dreamwalker  --
 -----------------------------
 L = DBM:GetModLocalization("Valithria")
 
-L:SetGeneralLocalization({
-	name = "Валитрия Сновидица"
-})
+L:SetGeneralLocalization{
+	name = "Валитрия Сноходица"
+}
 
-L:SetWarningLocalization({
-})
+L:SetWarningLocalization{
+}
 
-L:SetOptionLocalization({
-})
+L:SetOptionLocalization{
+}
 
 ------------------
 --  Sindragosa  --
 ------------------
 L = DBM:GetModLocalization("Sindragosa")
 
-L:SetGeneralLocalization({
+L:SetGeneralLocalization{
 	name = "Синдрагоса"
-})
+}
 
-L:SetTimerLocalization({
-	TimerNextAirphase 		= "Воздушная фаза",
-	TimerNextGroundphase		= "Наземная фаза"
-})
+L:SetTimerLocalization{
+	TimerNextAirphase		= "Воздушная фаза",
+	TimerNextGroundphase	= "Наземная фаза"
+}
 
-L:SetWarningLocalization({
+L:SetWarningLocalization{
 	WarnAirphase			= "Воздушная фаза",
-	SpecWarnBlisteringCold	= "Blisstering Cold - run out",
-	SpecWarnFrostBeacon		= "Frost beacon on you",
+	SpecWarnBlisteringCold	= "Blistering Cold - Run away",
+	SpecWarnFrostBeacon		= "Frost Beacon on you",
 	WarnGroundphaseSoon		= "Синдрагоса приземляется",
 	SpecWarnUnchainedMagic	= "Unchained Magic on you"
-})
+}
 
-L:SetOptionLocalization({
-	WarnAirphase			= "Предупредить о воздушной фазе",
-	WarnBlisteringCold		= "Show warning for Blistering Cold",
+L:SetOptionLocalization{
+	WarnAirphase			= "Предупреждать о воздушной фазе",
 	SpecWarnBlisteringCold	= "Show special warning for Blistering Cold",
-	SpecWarnFrostBeacon		= "Show special warning for Frost Beacon",
-	WarnGroundphaseSoon		= "Предупредить зарание о наземной фазе",
+	SpecWarnFrostBeacon		= "Show special warning when you are affected by Frost Beacon",
+	WarnGroundphaseSoon		= "Заранее предупреждать о наземной фазе",
 	TimerNextAirphase		= "Отсчет времени до воздушной фазы",
-	TimerNextGroundphase		= "Отсчет времени до наземной фазы",
+	TimerNextGroundphase	= "Отсчет времени до наземной фазы",
 	SpecWarnUnchainedMagic	= "Show special warning when you are affected by Unchained Magic"
-})
+}
 
-L:SetMiscLocalization({
-	YellAirphase			= "Your incursion ends here! None shall survive!",
-	YellPull				= "You are fools to have come to this place. The icy winds of Northrend will consume your souls!"
-})
+L:SetMiscLocalization{
+	YellAirphase	= "Your incursion ends here! None shall survive!",
+	YellPull		= "You are fools to have come to this place. The icy winds of Northrend will consume your souls!"
+}
 
 ---------------------
 --  The Lich King  --
 ---------------------
 L = DBM:GetModLocalization("LichKing")
 
-L:SetGeneralLocalization({
-	name = "Король-Лич"
-})
+L:SetGeneralLocalization{
+	name = "Король-лич"
+}
 
-L:SetWarningLocalization({
-})
+L:SetWarningLocalization{
+}
 
-L:SetOptionLocalization({
-})
+L:SetOptionLocalization{
+}
 
