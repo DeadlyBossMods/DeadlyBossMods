d3a26b013d0c9c8112c9e99c51a923be0da6562b
Adam <MysticalOS@users.noreply.github.com>
2009-12-14 17:54:08 +0000

    changed two warnings on emalon to generics, updated locals to use $Spell

--- DBM-PvP/VaultofArchavon/Emalon.lua
+++ DBM-PvP/VaultofArchavon/Emalon.lua
@@ -16,8 +16,8 @@ mod:RegisterEvents(
 )
 
 local specWarnNova			= mod:NewSpecialWarning("specWarnNova")
-local warnNova				= mod:NewAnnounce("warnNova", 3)
-local warnOverCharge		= mod:NewAnnounce("warnOverCharge", 2)
+local warnNova				= mod:NewSpellAnnounce(65279, 3)
+local warnOverCharge		= mod:NewSpellAnnounce(64218, 2)
 
 local timerNova				= mod:NewCastTimer(65279)
 local timerNovaCD			= mod:NewCDTimer(45, 65279)--Varies, 45-60seconds in between nova's
