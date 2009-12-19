33681dcbfd13f4f7af952a6719098322442c7d73
Adam <MysticalOS@users.noreply.github.com>
2009-12-19 20:48:27 +0000

    Improve Sorrowgrave's sacrifice, announce who it's on, and show timer they have to be freed (or how much time you have to move your abom into place)

--- DBM-Party-WotLK/UtgardePinnacle/SvalaSorrowgrave.lua
+++ DBM-Party-WotLK/UtgardePinnacle/SvalaSorrowgrave.lua
@@ -8,13 +8,24 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
-	"SPELL_CAST_START"
+	"SPELL_CAST_START",
+	"SPELL_AURA_APPLIED"
 )
 
-local warningSword	= mod:NewSpellAnnounce(48276, 2)
+local warningSword		= mod:NewSpellAnnounce(48276, 2)
+local warningSacrifice	= mod:NewTargetAnnounce(48267, 2)
+local timerSacrifice	= mod:NewBuffActiveTimer(25, 48276)
 
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(48276) then
 		warningSword:Show()
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(48267) then
+		warningSacrifice:Show(args.destName)
+	elseif args:IsSpellID(48276) then
+		timerSacrifice:Start()
 	end
 end
