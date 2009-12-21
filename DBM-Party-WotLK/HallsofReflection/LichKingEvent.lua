cd3ea29b388888a5f8d09aa517cba48111350a35
Adam <MysticalOS@users.noreply.github.com>
2009-12-21 08:07:33 +0000

    Detect horde pull for lich king event

--- DBM-Party-WotLK/HallsofReflection/LichKingEvent.lua
+++ DBM-Party-WotLK/HallsofReflection/LichKingEvent.lua
@@ -4,7 +4,12 @@ local L = mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision: 2153 $"):sub(12, -3))
 mod:SetCreatureID(37226, 36954)--Both creature Ids used in Halls of Reflection
 
-mod:RegisterCombat("yell", L.CombatStart)
+if UnitFactionGroup("player") == "Alliance" then
+	mod:RegisterCombat("yell", L.ACombatStart)
+else
+	mod:RegisterCombat("yell", L.HCombatStart)
+end
+
 mod:RegisterKill("yell", L.YellCombatEnd)
 mod:SetMinCombatTime(480)-- if you wipe, pull yell happens as soon as you zone back in despite the event not actually being started until you tell jaina you're ready to try again. This is only way to hack around this so mod doesn't wipe you for not being in combat after yell.
 --The above also makes an achievement timer not possible. It would be right first pull but any pull after a wipe there is no way at all to detect when real event is started
