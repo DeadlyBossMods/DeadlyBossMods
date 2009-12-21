1dbd784ffc34db6064cec4f3858b7deea034df8a
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-12-21 22:28:52 +0000

    added soon warnings for Ionar and Prophet in light of the recent hotfixes that make them only use this abilities once (at 50%)

--- trunk/DBM-Party-WotLK/DrakTharon/ProphetTharonja.lua
+++ trunk/DBM-Party-WotLK/DrakTharon/ProphetTharonja.lua
@@ -8,13 +8,28 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
-	"SPELL_CAST_SUCCESS"
+	"SPELL_CAST_SUCCESS",
+	"UNIT_HEALTH"
 )
 
-local warningCloud = mod:NewSpellAnnounce(49548, 3)
+local warningDecayFleshSoon	= mod:NewSoonAnnounce(49356, 2)
+local warningCloud 			= mod:NewSpellAnnounce(49548, 3)
+
+local warnedDecay		= false
+
+function mod:OnCombatStart()
+	warnedDecay = false
+end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(49548, 59969) then
 		warningCloud:Show()
+	end
+end
+
+function mod:UNIT_HEALTH(uId)
+	if not warnedDecay and self:GetUnitCreatureId(uId) == 26632 and UnitHealth(uId) / UnitHealthMax(uId) <= 0.54 then
+		warnedDecay = true
+		warningDecayFleshSoon:Show()
 	end
 end
