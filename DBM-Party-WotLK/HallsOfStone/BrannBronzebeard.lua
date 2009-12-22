de71eede0fda03c08fe7b91715383f42138e9162
Paul Emmerich <emmericp@net.in.tum.de>
2009-12-22 21:25:16 +0000

    Party-WotLK/HallsOfStone * BrannBronzebeard: now uses SetWipeTime instead of min combat time

--- DBM-Party-WotLK/HallsOfStone/BrannBronzebeard.lua
+++ DBM-Party-WotLK/HallsOfStone/BrannBronzebeard.lua
@@ -7,25 +7,7 @@ mod:SetZone()
 
 mod:RegisterCombat("yell", L.Pull)
 mod:RegisterKill("yell", L.Kill)
-mod:SetMinCombatTime(313)--This boss you leave combat every set of new mobs so this is only work around.
---[[		["REGEN_ENABLED"] = {
-			"<58.7>  -- < Regen Enabled : Leaving combat! -- < ", -- [1]
-			"<80.7>  -- < Regen Enabled : Leaving combat! -- < ", -- [2]
-			"<103.7>  -- < Regen Enabled : Leaving combat! -- < ", -- [3]
-			"<110.5>  -- < Regen Enabled : Leaving combat! -- < ", -- [4]
-			"<133.4>  -- < Regen Enabled : Leaving combat! -- < ", -- [5]
-			"<159.1>  -- < Regen Enabled : Leaving combat! -- < ", -- [6]
-			"<178.0>  -- < Regen Enabled : Leaving combat! -- < ", -- [7]
-			"<188.0>  -- < Regen Enabled : Leaving combat! -- < ", -- [8]
-			"<200.9>  -- < Regen Enabled : Leaving combat! -- < ", -- [9]
-			"<208.9>  -- < Regen Enabled : Leaving combat! -- < ", -- [10]
-			"<239.1>  -- < Regen Enabled : Leaving combat! -- < ", -- [11]
-			"<250.7>  -- < Regen Enabled : Leaving combat! -- < ", -- [12]
-			"<261.2>  -- < Regen Enabled : Leaving combat! -- < ", -- [13]
-			"<273.2>  -- < Regen Enabled : Leaving combat! -- < ", -- [14]
-			"<281.2>  -- < Regen Enabled : Leaving combat! -- < ", -- [15]
-			"<298.5>  -- < Regen Enabled : Leaving combat! -- < ", -- [16]
-			"<313.8>  -- < Regen Enabled : Leaving combat! -- < ", -- [17]--]]
+mod:SetWipeTime(25)
 
 mod:RegisterEvents(
 	"CHAT_MSG_MONSTER_YELL"
