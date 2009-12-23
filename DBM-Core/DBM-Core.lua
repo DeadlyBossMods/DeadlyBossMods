cbf20a5168b3dde55580b23ffe5504edb5023ade
Azael <paul.poon.kw@gmail.com>
2009-12-23 12:30:19 +0000

    zhtw update

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -2570,7 +2570,11 @@ do
 		end
 		table.insert(self.announces, obj)
 		if announceType == "stack" then
-			self.localization.options[text] = DBM_CORE_AUTO_SPEC_WARN_OPTIONS[announceType]:format(stacks or 3, spellId)
+			if GetLocale() == "zhTW" then
+				self.localization.options[text] = DBM_CORE_AUTO_SPEC_WARN_OPTIONS[announceType]:format(spellId, stacks or 3)
+			then
+				self.localization.options[text] = DBM_CORE_AUTO_SPEC_WARN_OPTIONS[announceType]:format(stacks or 3, spellId)
+			end
 		else
 			self.localization.options[text] = DBM_CORE_AUTO_SPEC_WARN_OPTIONS[announceType]:format(spellId)
 		end
