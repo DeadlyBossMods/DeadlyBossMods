e47464d750590afb2b0121bae83a12e75f124c3a
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-12-23 23:35:47 +0000

    set icons on boiling blood

--- trunk/DBM-Icecrown/localization.en.lua
+++ trunk/DBM-Icecrown/localization.en.lua
@@ -137,8 +137,9 @@ L:SetWarningLocalization{
 }
 
 L:SetOptionLocalization{
-	warnFrenzySoon	= "Show pre-warning for Frenzy (at ~33%)",
-	RangeFrame		= "Show range frame (11 yards)"
+	warnFrenzySoon			= "Show pre-warning for Frenzy (at ~33%)",
+	SetIconOnBoilingBlood	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(72441),
+	RangeFrame				= "Show range frame (11 yards)"
 }
 
 -----------------
