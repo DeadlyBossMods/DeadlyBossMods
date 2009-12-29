b921aa5bf3343f66855b98bdb168d57a7123cc98
bluenyx <bluenyx@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-12-29 10:47:58 +0000

    koKR update

--- DBM-Coliseum/localization.kr.lua
+++ DBM-Coliseum/localization.kr.lua
@@ -27,11 +27,7 @@ L:SetOptionLocalization{
 	WarningSnobold				= "스노볼트 부하 생성 경고 보기",
 	SpecialWarningImpale3		= "꿰뚫기 특수 경보 보기(3 중첩이거나 그 이상일 경우)",
 	SpecialWarningAnger3		= "솟구치는 분노 특수 경고 보기(3 중첩이거나 그 이상일 경우)",	
-	SpecialWarningFireBomb		= "자신의 화염 폭탄 특수 경보 보기",
-	SpecialWarningSlimePool		= "독 구름 특수 경보 보기",
 	SpecialWarningSilence		= "진동의 발구르기 특수 경보 보기",
-	SpecialWarningToxin			= "당신이 마비독에 걸릴 경우 특수 경보 보기",
-	SpecialWarningBile			= "당신이 타오르는 담즙에 걸릴 경우 특수 경보 보기",
 	SpecialWarningCharge		= "얼음울음이 당신에게 사나운 돌진을 할 경우 특수 경보 보기",
 	SpecialWarningChargeNear	= "얼음울음이 당신 주변에 사나운 돌진을 할 경우 특수 경보 보기",
 	SpecialWarningTranq			= "얼음울음이 거품 이는 분노를 얻었을 경우 특수 경고 보기(평정 사용 클래스)",	
@@ -57,11 +53,7 @@ L:SetWarningLocalization{
 	WarningSnobold				= "스노볼트 부하 생성",
 	SpecialWarningImpale3		= "꿰뚫기 >%d<!",
 	SpecialWarningAnger3		= "솟구치는 분노 >%d<",	
-	SpecialWarningFireBomb		= "당신에게 화염 폭탄!",
-	SpecialWarningSlimePool		= "독 구름, 벗어나세요!",
-	SpecialWarningBile			= "당신에게 타오르는 담즙!",
 	SpecialWarningSilence		= "1.5 초이내 진동의 발구르기!!!",
-	SpecialWarningToxin			= "당신에게 마비독! 이동하세요!",
 	SpecialWarningCharge		= "당신에게 사나운 돌진! 뛰세요!",
 	SpecialWarningChargeNear	= "당신 주변에 사나운 돌진! 뛰세요!",
 	SpecialWarningTranq			= "거품 이는 분노! - 지금 평정!"
@@ -79,15 +71,10 @@ L:SetGeneralLocalization{
 
 L:SetWarningLocalization{
 	WarnNetherPower				= "자락서스가 황천의 힘을 얻었습니다! 해제하세요!",
-	SpecWarnFlesh				= "당신에게 살점 소각!",
 	SpecWarnTouch				= "당신에게 자락서스의 손길!",
-	SpecWarnKiss				= "당신에게 여군주의 키스",
 	SpecWarnTouchNear			= "당신 주변에 자락서스의 손길 : >%s<",
-	SpecWarnFlame				= "당신에게 군단 불꽃! 뛰세요!",
 	SpecWarnNetherPower			= "지금 해제!",
-	SpecWarnFelInferno			= "군단 불꽃! 움직이세요!",
 	SpecWarnFelFireball			= "지옥 화염구 - 차단!",
-	SpecWarnFelFireballDispel	= "지옥 화염구 해제 : %s"	
 }
 
 L:SetMiscLocalization{
@@ -97,15 +84,10 @@ L:SetMiscLocalization{
 
 L:SetOptionLocalization{
 	WarnNetherPower				= "자락서스가 황천의 힘을 얻었을 경우 경고 하기 (해제를 위한 경고)",
-	SpecWarnFlame				= "군단 불꽃에 걸렸을 경우 특수 경보 알리기",
-	SpecWarnFlesh				= "살점 소각에 걸렸을 경우 특수 경보 알리기",
+	SpecWarnFelFireball			= "지옥 화염구의 특수 경고 보기(차단 관련)",	
 	SpecWarnTouch				= "당신이 자락서스의 손길에 걸렸을 경우 특수 경고 알리기",
 	SpecWarnTouchNear			= "당신 주변의 플레이어가 자락서스의 손길에 걸렸을 경우 특수 경고 알리기",
-	SpecWarnKiss				= "당신이 여군주의 키스에 걸렸을 경우 특수 경고 알리기",
 	SpecWarnNetherPower			= "황천의 힘 특수 경고 알리기 (자락서스 해제클래스-법사/사제/주술사)",
-	SpecWarnFelInferno			= "당신 주변에 군단 불꽃이 있을 경우 특수 경고 알리기(바닥)",
-	SpecWarnFelFireball			= "지옥 화염구의 특수 경고 보기(차단 관련)",
-	SpecWarnFelFireballDispel	= "지옥 화염구의 특수 경고 보기(해제 관련)",	
 	TouchJaraxxusIcon			= "자락서스의 손길 대상 공격대 아이콘 설정",
 	IncinerateFleshIcon			= "살점 소각의 공격대 대상 아이콘 설정",
 	LegionFlameIcon				= "군단의 불꽃의 공격대 대상 아이콘 설정",
@@ -128,10 +110,6 @@ L:SetTimerLocalization{
 }
 
 L:SetWarningLocalization{
-	SpecWarnHellfire		= "지옥의 불길! 이동 하세요!",
-	SpecWarnHandofProt		= "보호의 손길 : >%s<",
-	SpecWarnDivineShield	= "천상의 보호막 :  >%s<",
-	specWarnIceBlock       	= "얼음 방어막 : %s"
 }
 
 L:SetMiscLocalization{
@@ -169,10 +147,6 @@ L:SetMiscLocalization{
 }
 
 L:SetOptionLocalization{
-	SpecWarnHellfire		= "지옥의 불길로 인해 피해를 입을 경우 특수 경고 보기",
-	SpecWarnHandofProt		= "성기사가 보호의 손길을 시전 했을 경우 특수 경고 보기(해제)",
-	SpecWarnDivineShield	= "성기사가 천상의 보호막을 시전 했을 경우 특수 경고 보기(해제)",
-	specWarnIceBlock       	= "마법사가 얼음 방어막을 시전 했을 경우 특수 경고 보기(해제)",
 	PlaySoundOnBladestorm	= "전사가 칼폭을 시전 했을 경우 특수 소리 재생"	
 }
 
@@ -193,8 +167,6 @@ L:SetTimerLocalization{
 L:SetWarningLocalization{
 	WarnSpecialSpellSoon		= "곧 속성의 소용돌이!",
 	SpecWarnSpecial				= "속성(빛/어둠) 변경!",
-	SpecWarnEmpoweredDarkness	= "당신에게 강화된 어둠",
-	SpecWarnEmpoweredLight		= "당신에게 강화된 빛",
 	SpecWarnSwitchTarget		= "대상 전환!",
 	SpecWarnKickNow				= "지금 차단!",
 	WarningTouchDebuff			= "디버프 : >%s<",
@@ -212,8 +184,6 @@ L:SetOptionLocalization{
 	TimerSpecialSpell			= "다음 속성의 소용돌이의 타이머 보기",
 	WarnSpecialSpellSoon		= "다음 속성의 소용돌이 사전 경고 보기",
 	SpecWarnSpecial				= "속성(색) 변경을 해야할 때 특수 경고 보기",
-	SpecWarnEmpoweredDarkness	= "강화된 어둠 특수 경고 보기",
-	SpecWarnEmpoweredLight		= "강화된 빛 특수 경고 보기",
 	SpecWarnSwitchTarget		= "다른 보스에게 시전해야 할 경우 특수 경고 보기",
 	SpecWarnKickNow				= "당신이 차단을 해야할 경우 특수 경고 보기",
 	SpecialWarnOnDebuff			= "디버프일 경우 특수 경고 보기 (디버프를 바꿀 경우)",
@@ -244,9 +214,9 @@ L:SetWarningLocalization{
 	WarnEmergeSoon			= "아눕아락 10초 이내 출현!",
 	WarnSubmerge			= "아눕아락 잠수!",
 	WarnSubmergeSoon		= "아눕아락 10초 이내 잠수!",
+	specWarnSubmergeSoon	= "아눕아락 잠수 10초 전!",	
 	SpecWarnPursue			= "당신을 추격 합니다!",
 	warnAdds				= "네루비안 땅무지 추가!",	
-	SpecWarnPCold	 		= "당신은 냉기 관통! 조심하세요!",
 	SpecWarnShadowStrike	= "어둠의 일격! 지금 차단!"
 }
 
@@ -264,6 +234,7 @@ L:SetOptionLocalization{
 	WarnEmergeSoon			= "출현의 사전 경고 보기",
 	WarnSubmerge			= "숨기 경고 보기",
 	WarnSubmergeSoon		= "숨기의 사전 경고 보기",
+	specWarnSubmergeSoon	= "숨기 10초전 특수 경고 보기",	
 	SpecWarnPursue			= "당신을 추격하기 시작할 때 특수 경고 알리기",
 	warnAdds				= "새로운 네루비안 땅무지가 추가 될 때 경고 보기",
 	timerAdds				= "새로운 네루비안 땅무지 추가 타이머 보기",	
@@ -271,7 +242,6 @@ L:SetOptionLocalization{
 	TimerSubmerge			= "숨기 타이머 보기",
 	PlaySoundOnPursue		= "당신을 추격하기 시작할 때 특수 소리 재생",
 	PursueIcon				= "추격 대상자 공격대 아이콘 설정하기",
-	SpecWarnPCold			= "$spell:68510 에 영향을 받을 경우 특수 경고 보기",
 	SetIconsOnPCold			= "$spell:68510 대상자 공격대 아이콘 설정하기",
 	SpecWarnShadowStrike	= "$spell:66134 특수 경고 보기(차단 관련)",
 	RemoveHealthBuffsInP3	= "3 페이즈를 시작할 때 HP 버프 지우기",
