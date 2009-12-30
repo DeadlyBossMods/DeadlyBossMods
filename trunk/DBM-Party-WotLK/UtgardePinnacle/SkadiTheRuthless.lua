ed158735135ac832305bf84242ed53912a9ebf31
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-12-30 23:38:21 +0000

    *Skadi mod is now an event mod that starts when event is started. *Added achievement timer to Skadi mod *Fixed whirlwind warning in Skadi mod so it should now work correctly *Added special warning and run away sound to whirlwind for melee.

--- trunk/DBM-Party-WotLK/UtgardePinnacle/SkadiTheRuthless.lua
+++ trunk/DBM-Party-WotLK/UtgardePinnacle/SkadiTheRuthless.lua
@@ -3,25 +3,50 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(26693)
-mod:SetZone()
 
-mod:RegisterCombat("combat")
+--mod:RegisterCombat("combat")
+mod:RegisterCombat("yell", L.CombatStart)
+mod:RegisterKill("yell", L.YellCombatEnd)
+mod:SetMinCombatTime(30)
+mod:SetWipeTime(25)
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START"
+	"CHAT_MSG_MONSTER_YELL"
 )
 
+local isMelee = select(2, UnitClass("player")) == "ROGUE"
+			or select(2, UnitClass("player")) == "WARRIOR"
+			or select(2, UnitClass("player")) == "DEATHKNIGHT"
+
+local warnPhase2		= mod:NewPhaseAnnounce(2)
 local warningPoison		= mod:NewTargetAnnounce(59331, 2)
 local warningWhirlwind	= mod:NewSpellAnnounce(59322, 3)
 local timerPoison		= mod:NewTargetTimer(12, 59331)
-local timerWhirlwindCD	= mod:NewCDTimer(30, 59322)
+local timerWhirlwindCD	= mod:NewCDTimer(23, 59322)
+
+local specWarnWhirlwind	= mod:NewSpecialWarningRun(59322, isMelee)
+
+local timerAchieve		= mod:NewAchievementTimer(180, 1873, "TimerSpeedKill")
+
+local soundWhirlwind	= mod:NewSound(59322, nil, isMelee)
+
+function mod:OnCombatStart(delay)
+	if mod:IsDifficulty("heroic5") then
+		timerAchieve:Start(-delay)
+	end
+end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(59331, 50255) then
 		warningPoison:Show(args.destName)
 		timerPoison:Start(args.destName)
+	elseif args:IsSpellID(59322, 50228) then
+		warningWhirlwind:Show()
+		timerWhirlwindCD:Start()
+		specWarnWhirlwind:Show()
+		soundWhirlwind:Play()
 	end
 end
 
@@ -31,9 +56,14 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end
 
-function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(59322, 50228) then
-		warningWhirlwind:Show()
-		timerWhirlwindCD:Start()
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg == L.Phase2 or msg:find(L.Phase2) then
+		self:SendSync("Phase2")
+	end
+end
+
+function mod:OnSync(msg, arg)
+	if msg == "Phase2" then
+		warnPhase2:Show()
 	end
 end
