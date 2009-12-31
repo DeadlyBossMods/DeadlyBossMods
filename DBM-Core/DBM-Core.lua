eb7d194cc2e28d345fa1ace080474807557fc729
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2009-12-31 05:52:56 +0000

    ups, fix again ;)

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -711,7 +711,7 @@ SlashCmdList["DEADLYBOSSMODS"] = function(msg)
 		local timer = timer * 60
 		DBM:CreatePizzaTimer(timer, "Break time!", true)
 		DBM:Unschedule(SendChatMessage)
-		SendChatMessage(DBM_CORE_BREAK_START:format(timer/60), channel)
+		SendChatMessage(DBM_CORE_BREAK_START:format(timer/60), "RAID_WARNING")
 		if timer/60 > 5 then DBM:Schedule(timer - 5*60, SendChatMessage, DBM_CORE_BREAK_MIN:format(5), "RAID") end
 		if timer/60 > 2 then DBM:Schedule(timer - 2*60, SendChatMessage, DBM_CORE_BREAK_MIN:format(2), "RAID") end
 		if timer/60 > 1 then DBM:Schedule(timer - 1*60, SendChatMessage, DBM_CORE_BREAK_MIN:format(1), "RAID_WARNING") end
