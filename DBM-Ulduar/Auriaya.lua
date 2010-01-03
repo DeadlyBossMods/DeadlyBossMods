da941db9762baa4b90a6e29e37c6af72166a236e
Adam <MysticalOS@users.noreply.github.com>
2010-01-03 19:19:25 +0000

    change auriaya back to "combat" pull. rathor have timers be off by 1second when tank doesn't use dbm versus a mod not working in some locals.

--- DBM-Ulduar/Auriaya.lua
+++ DBM-Ulduar/Auriaya.lua
@@ -4,7 +4,7 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 
 mod:SetCreatureID(33515)
-mod:RegisterCombat("yell", L.YellPull)
+mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_CAST_START",
