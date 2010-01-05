f4a8a962890c1fb6d3532776fe6887d9dd0d3bcf
Nitram <Nitram@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-01-05 23:21:32 +0000

    Spanish translation update

--- trunk/DBM-Onyxia/localization.es.lua
+++ trunk/DBM-Onyxia/localization.es.lua
@@ -14,6 +14,8 @@ L:SetGeneralLocalization{
 L:SetWarningLocalization{
 	SpecWarnBreath	= "Aliento profundo",
 	WarnWhelpsSoon	= "Pronto saldran crias",
+	WarnPhase2Soon		= "Fase 2 pronto",
+	WarnPhase3Soon		= "Fase 3 pronto"
 }
 
 L:SetTimerLocalization{
@@ -24,10 +26,14 @@ L:SetOptionLocalization{
 	SpecWarnBreath	= "Mostrar aviso especial para Aliento Profundo ( Echa fuego )",
 	TimerWhelps		= "Mostrar tiempo para las crias",
 	WarnWhelpsSoon	= "Mostrar pre-aviso para las crias",
-	SoundBreath		= "Sonido si estas en el area del fuego"
+	SoundBreath		= "Sonido si estas en el area del fuego",
+	PlaySoundOnBlastNova	= "Reproducir sonido en Nova Explosiva",
+	SoundWTF				= "Reproducir sonidos divertidos de una raid legendaria de Onyxia classico",
+	WarnPhase2Soon			= "Mostrar pre-aviso para Fase 2 (al ~67%)",
+	WarnPhase3Soon			= "Mostrar pre-aviso para Fase 3 (al ~41%)"
 }
 
-L:SetMiscLocalization{ -- these yells are copy and pasted from our old onyxia mod...I don't know if they still work ;)
+L:SetMiscLocalization{
 	YellP2 = "Este ejercicio sin sentido me aburre. ¡Os inceneraré a todos desde arriba!",
-	YellP3 = "¡Aprende a saber estar, mortal!"
+	YellP3 = "¡Parece ser que vais a necesitar otra lección, mortales!"
 }
