712755deacb11988af0a66d643272e214d7a42a5
Adam <MysticalOS@users.noreply.github.com>
2010-01-06 05:52:39 +0000

    Some serious fixes to festergut. hopefully is a good mod now

--- DBM-Icecrown/ThePlagueworks/PlagueworksTrash.lua
+++ DBM-Icecrown/ThePlagueworks/PlagueworksTrash.lua
@@ -2,25 +2,25 @@ local mod	= DBM:NewMod("PlagueworksTrash", "DBM-Icecrown", 2)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(37025, 37217)
-mod:RegisterCombat("combat")
+--mod:SetCreatureID(37025, 37217)
+--mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_APPLIED_DOSE",
 	"SPELL_SUMMON",
-	"SPELL_CAST_SUCCESS"
+	"SPELL_CAST_START"
 )
 
-local warnZombies	= mod:NewSpellAnnounce(71159)
+local warnZombies		= mod:NewSpellAnnounce(71159)
 local warnMortalWound	= mod:NewTargetAnnounce(71127)
 local warnDecimateSoon	= mod:NewSoonAnnounce(71123)
 
-local specWarnMortalWound	= mod:NewSpecialWarningStack(71127, nil, 3)
+local specWarnMortalWound	= mod:NewSpecialWarningStack(71127, nil, 5)
 
-local timerZombies	= mod:NewNextTimer(20, 71159)
+local timerZombies		= mod:NewNextTimer(20, 71159)
 local timerMortalWound	= mod:NewTargetTimer(15, 71127)
-local timerDecimate	= mod:NewNextTimer(33, 71123)
+local timerDecimate		= mod:NewNextTimer(33, 71123)
 
 mod:RemoveOption("HealthFrame")
 
@@ -36,8 +36,8 @@ function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(71127) then
 		warnMortalWound:Show(args.destName)
 		timerMortalWound:Start(args.destName)
-		if args:IsPlayer() and (args.amount or 1) >= 3 then
-			specWarnMortalWound:Show()
+		if args:IsPlayer() and (args.amount or 1) >= 5 then
+			specWarnMortalWound:Show(args.destName)
 		end
 	end
 end
@@ -52,7 +52,7 @@ function mod:SPELL_SUMMON(args)
 	end
 end
 
-function mod:SPELL_CAST_SUCCESS(args)
+function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(71123) then
 		warnDecimateSoon:Cancel()	-- in case the first 1 is inaccurate, you wont have an invalid soon warning
 		warnDecimateSoon:Schedule(28)
