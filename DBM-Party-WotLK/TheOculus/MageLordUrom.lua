6d946ff0f415c4351e3f4445473596f22954a7d4
Adam <MysticalOS@users.noreply.github.com>
2010-01-13 08:59:18 +0000

    another mod:SetMinSyncRevision for another mod that had a pull type fixed not long ago.

--- DBM-Party-WotLK/TheOculus/MageLordUrom.lua
+++ DBM-Party-WotLK/TheOculus/MageLordUrom.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(27655)
-mod:SetZone()
+mod:SetMinSyncRevision(2824)
 
 mod:RegisterCombat("yell", L.CombatStart)
 
