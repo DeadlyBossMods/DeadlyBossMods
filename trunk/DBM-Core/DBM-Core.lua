94b8176668c129add555cf38c4b1d749c41fb9de
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-01-18 23:59:42 +0000

    added slash command for 12-yd range frame

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -736,7 +736,7 @@ SlashCmdList["DBMRANGE"] = function(msg)
 		DBM.RangeCheck:Hide()
 	else
 		local r = tonumber(msg)
-		if r and (r == 10 or r == 11 or r == 15 or r == 28) then
+		if r and (r == 10 or r == 11 or r == 15 or r == 28 or r == 12) then
 			DBM.RangeCheck:Show(r)
 		else
 			DBM.RangeCheck:Show(10)
