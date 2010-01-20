4e80cdaf197aea249d6bb9fda33b546d390fb174
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-01-20 22:52:20 +0000

    Timer for Svala Sorrowgrave rollplay

--- trunk/DBM-Party-WotLK/UtgardePinnacle/SvalaSorrowgrave.lua
+++ trunk/DBM-Party-WotLK/UtgardePinnacle/SvalaSorrowgrave.lua
@@ -8,16 +8,30 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
-	"SPELL_AURA_APPLIED"
+	"SPELL_AURA_APPLIED",
+	"CHAT_MSG_MONSTER_YELL"
 )
 
 local warningSacrifice	= mod:NewTargetAnnounce(48267, 2)
 local timerSacrifice	= mod:NewBuffActiveTimer(25, 48276)
+local timerRollplay		= mod:NewTimer(67, "timerRollplay")--rollplay for boss is active
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(48267) then
 		warningSacrifice:Show(args.destName)
 	elseif args:IsSpellID(48276) then
 		timerSacrifice:Start()
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg == L.SvalaRollplayStart or msg:find(L.SvalaRollplayStart) then
+		self:SendSync("SvalaRollplay")
+	end
+end
+
+function mod:OnSync(msg, arg)
+	if msg == "SvalaRollplay" then
+		timerRollplay:Start()
 	end
 end
