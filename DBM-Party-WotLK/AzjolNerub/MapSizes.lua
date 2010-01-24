02f80b4760499a6d9478db16dd0b833b610c16a7
Adam <MysticalOS@users.noreply.github.com>
2010-01-24 23:02:39 +0000

    #AzjolNerub/Mapsizes - Added map data

--- DBM-Party-WotLK/AzjolNerub/MapSizes.lua
+++ DBM-Party-WotLK/AzjolNerub/MapSizes.lua
@@ -1,2 +1,5 @@
--- todo: find map size
---DBM:RegisterMapSize("ZoneName")
+DBM:RegisterMapSize("AzjolNerub",
+	3, 367.50033034529, 245.00084955382,	-- The Gilded Gate
+	2, 292.97446500284, 195.31506198937,	-- Hadronox's Lair
+	1, 753.00604240622, 501.98354738306		-- The Brood Pit
+)
