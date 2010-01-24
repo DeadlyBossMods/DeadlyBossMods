07259951f053e05ca042cc369aa882e088748b53
Paul Emmerich <emmericp@net.in.tum.de>
2010-01-24 16:35:00 +0000

    addons can now register map sizes for the range check (and arrow stuff)

---
+++ DBM-EyeOfEternity/MapSizes.lua
@@ -0,0 +1,3 @@
+-- the map size data is copied from Deus Vox Encounters (http://wow.curse.com/downloads/wow-addons/details/deus-vox-encounters.aspx)
+-- todo: confirm that this is actually correct and accurate
+DBM:RegisterMapSize("TheEyeofEternity", 1, 400.728405332355, 267.09113174487)
