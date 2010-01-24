2a3ab5fbe892a71ed2cdff54516c46fbe39ffebb
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-01-24 20:56:32 +0000

    adjusted map sizes of Ulduar

--- trunk/DBM-Ulduar/MapSizes.lua
+++ trunk/DBM-Ulduar/MapSizes.lua
@@ -1,10 +1,10 @@
--- the map size data is copied from Deus Vox Encounters (http://wow.curse.com/downloads/wow-addons/details/deus-vox-encounters.aspx)
+-- 
 -- todo: confirm that this is actually correct and accurate
 DBM:RegisterMapSize("Ulduar",
-	1, 3064.9614761023, 2039.5413309668,	-- Expedition Base Camp			-- CONFIRM
-	2, 624.19069622949, 415.89374357805,	-- Antechamber of Ulduar		-- CONFIRM
-	3, 1238.37427179,	823.90183235628,	-- Conservatory of Life			-- CONFIRM
-	4, 848.38069183829, 564.66888353370,	-- Prison of Yogg-Saron			-- CONFIRM
-	5, 1460.4694647684, 974.65312886234,	-- Spark of Imagination			-- CONFIRM
-	6, 576.71549337896, 384.46653291368		-- The Mind's Eye (Under Yogg)	-- CONFIRM
+	1, 3287.5408347411, 2191.6709278048,	-- The Grand Approach
+	2, 669.45221810866, 446.29840569337,	-- The Antechamber of Ulduar
+	3, 1328.4581100659, 885.64270386813,	-- The Inner Sanctum of Ulduar
+	4, 910.49817408689, 607.00180510881,	-- The Prison of Yogg-Saron
+	5, 1569.4723670012, 1046.3012526423,	-- The Spark of Imagination
+	6, 576.71549337896, 384.46653291368		-- The Mind's Eye -- CONFIRM -- copied from Deus Vox Encounters (http://wow.curse.com/downloads/wow-addons/details/deus-vox-encounters.aspx)
 )
