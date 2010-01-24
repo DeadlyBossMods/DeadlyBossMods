d14181a200feb6232232397b016b286f125e380d
Adam <MysticalOS@users.noreply.github.com>
2010-01-24 22:24:20 +0000

    #Trial of Crusader/Mapsizes - Confirmed/adjusted area 2.

--- DBM-Coliseum/MapSizes.lua
+++ DBM-Coliseum/MapSizes.lua
@@ -1,4 +1,4 @@
 DBM:RegisterMapSize("TheArgentColiseum",
 	1, 369.98599024229, 246.65769016535,	-- The Argent Coliseum
-	2, 688.60679691348, 458.95801567569		-- The Icy Depths --CONFIRM --data is copied from Deus Vox Encounters (http://wow.curse.com/downloads/wow-addons/details/deus-vox-encounters.aspx)
+	2, 739.99522937124, 493.33062806372		-- The Icy Depths
 )
