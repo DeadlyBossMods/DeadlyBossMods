172f88cb38af8c7cbec106bfc6d5da6cba398b09
Adam <MysticalOS@users.noreply.github.com>
2010-01-24 21:42:51 +0000

    #ICC/Mapsizes - Updated mapsizes for 4 of the 6 areas. #Forge of Souls/Mapsizes - Added mapsize

--- DBM-Icecrown/MapSizes.lua
+++ DBM-Icecrown/MapSizes.lua
@@ -1,10 +1,10 @@
 -- the map size data is copied from Deus Vox Encounters (http://wow.curse.com/downloads/wow-addons/details/deus-vox-encounters.aspx)
 -- todo: confirm that this is actually correct and accurate
 DBM:RegisterMapSize("IcecrownCitadel",
-	1, 1262.8025621533, 841.91669450207,	-- The Lower Citadel		-- CONFIRM
-	2, 993.25701607873, 662.58829476644,	-- The Rampart of Skulls	-- CONFIRM
-	3, 181.83564716405, 121.29684810833,	-- Deathbringer's Rise		-- CONFIRM
+	1, 1355.9805038983, 903.64714475245,	-- The Lower Citadel
+	2, 1066.9057914008, 711.33331498279,	-- The Rampart of Skulls
+	3, 195.42805299015, 130.31517450585,	-- Deathbringer's Rise
 	4, 720.60965618252, 481.16215066130,	-- The Frost Queen's Lair	-- CONFIRM
-	5, 1069.6156745738, 713.83371679543,	-- The Upper Reaches		-- CONFIRM
+	5, 1149.0762925934, 301.86934021889,	-- The Upper Reaches
 	6, 348.05218433541, 232.05964286208		-- Royal Quarters			-- CONFIRM
 )
