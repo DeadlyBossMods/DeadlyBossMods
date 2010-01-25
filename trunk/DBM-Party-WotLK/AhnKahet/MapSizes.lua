598a548c3c557cd593d99968d0afa6e53e188c4b
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-01-25 06:55:14 +0000

    Some more heroic data.

--- trunk/DBM-Party-WotLK/AhnKahet/MapSizes.lua
+++ trunk/DBM-Party-WotLK/AhnKahet/MapSizes.lua
@@ -1,2 +1,3 @@
--- todo: find map size
---DBM:RegisterMapSize("ZoneName")
+DBM:RegisterMapSize("Ahnkahet", 
+	1, 972.41528036991, 648.27816847614
+)
--- trunk/DBM-Party-WotLK/UtgardePinnacle/MapSizes.lua
+++ trunk/DBM-Party-WotLK/UtgardePinnacle/MapSizes.lua
@@ -1,2 +1,3 @@
--- todo: find map size
---DBM:RegisterMapSize("ZoneName")
+DBM:RegisterMapSize("UtgardePinnacle",
+	2, 756.19668984222, 504.12106240865	-- Upper Pinnacle
+)
