7a95699b5969f953044d78501701907b44a76f01
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-01-25 06:22:59 +0000

    #Nax - Update to correct mapsizes

--- trunk/DBM-Naxx/MapSizes.lua
+++ trunk/DBM-Naxx/MapSizes.lua
@@ -1,10 +1,8 @@
--- the map size data is copied from Deus Vox Encounters (http://wow.curse.com/downloads/wow-addons/details/deus-vox-encounters.aspx)
--- todo: confirm that this is actually correct and accurate
 DBM:RegisterMapSize("Naxxramas",
-	1, 1018.3655494957, 679.40523953718,	-- Construct	-- CONFIRM
-	2, 1019.1310739251, 679.18864376555,	-- Arachnid		-- CONFIRM
-	3, 1118.1083638787, 744.57895516418,	-- Military		-- CONFIRM
-	4, 1117.0809918236, 745.97398439776,	-- Plague		-- CONFIRM
-	5, 1927.3190541014, 1284.6530841959,	-- Entrance		-- CONFIRM
-	6, 610.62737087301, 407.3875157986		-- KT/Sapphiron	-- CONFIRM
+	1, 1093.8230088067, 729.21799501687,	-- The Construct Quarter
+	2, 1093.8286438728, 729.22512034741,	-- The Arachnid Quarter
+	3, 1200.0013386685, 799.99890624803,	-- The Military Quarter
+	4, 1200.3295449078, 800.21908252442,	-- The Plague Quarter
+	5, 2069.8942952744, 1379.8711461146,	-- The Lower Necropolis
+	6, 655.96762454632, 437.28113454485		-- The Upper Necropolis
 )
