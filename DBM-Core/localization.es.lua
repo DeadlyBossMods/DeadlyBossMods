52e231184f4a3c63b4028d23b60a127bb48480f8
Paul Emmerich <emmericp@net.in.tum.de>
2010-01-26 16:53:08 +0000

    esES locales are now also loaded for esMX

--- DBM-Core/localization.es.lua
+++ DBM-Core/localization.es.lua
@@ -1,4 +1,4 @@
-﻿if GetLocale() ~= "esES" then return end
+﻿if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
 
 DBM_CORE_LOAD_MOD_ERROR				= "Error al cargar modulo %s: %s"
 DBM_CORE_LOAD_MOD_SUCCESS			= "Cargado modulo de '%s' !"
