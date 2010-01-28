67c7c1248a5bd814e7b5541542643c73b5a81209
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-01-28 07:42:02 +0000

    #Party/Gundrak: Experimental less-rabi helper using unit health, requires testing.

--- trunk/DBM-Party-WotLK/Gundrak/Moorabi.lua
+++ trunk/DBM-Party-WotLK/Gundrak/Moorabi.lua
@@ -8,13 +8,20 @@ mod:SetCreatureID(29305)
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
-	"SPELL_CAST_START"
+	"SPELL_CAST_START",
+	"UNIT_HEALTH"
 )
 
 local warningTransform	= mod:NewSpellAnnounce(55098, 3)
+local timerTransform	= mod:NewCDTimer(10, 55098)--experimental
 
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(55098) then
 		warningTransform:Show()
+		if self:GetUnitCreatureId(uId) == 29305 and UnitHealth(uId) / UnitHealthMax(uId) >= 0.50 then
+			timerTransform:Start()--cast every 10 seconds above 50% health
+		elseif self:GetUnitCreatureId(uId) == 29305 and UnitHealth(uId) / UnitHealthMax(uId) <= 0.49 then
+			timerTransform:Start(5)--cast every 5 seconds below 50% health
+		end
 	end
 end
