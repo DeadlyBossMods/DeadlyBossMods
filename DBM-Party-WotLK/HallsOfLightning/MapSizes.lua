045785a62596852b1178a309acc4aab468473249
Adam <MysticalOS@users.noreply.github.com>
2010-01-28 06:10:40 +0000

    #Party/Halls of Lightning: Added missing map size

--- DBM-Party-WotLK/HallsOfLightning/MapSizes.lua
+++ DBM-Party-WotLK/HallsOfLightning/MapSizes.lua
@@ -1,3 +1,4 @@
 DBM:RegisterMapSize("HallsofLightning",
-	1, 566.23833612138, 377.49063598374 -- Unyielding Garrison
+	1, 566.23833612138, 377.49063598374, -- Unyielding Garrison
+	2, 708.23832903423, 472.14350959317	 -- Walk of the Makers
 )
