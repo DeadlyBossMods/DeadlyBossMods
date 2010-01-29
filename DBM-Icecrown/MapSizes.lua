f7b73ada8b7eb7b5510758c0a7db5210675ae248
Adam <MysticalOS@users.noreply.github.com>
2010-01-29 21:45:48 +0000

    #ICC/Mapsizes: All map sizes complete and 100% accurate from game files themself

--- DBM-Icecrown/MapSizes.lua
+++ DBM-Icecrown/MapSizes.lua
@@ -1,10 +1,10 @@
--- the map size data is copied from Deus Vox Encounters (http://wow.curse.com/downloads/wow-addons/details/deus-vox-encounters.aspx)
--- todo: confirm that this is actually correct and accurate
 DBM:RegisterMapSize("IcecrownCitadel",
-	1, 1355.9805038983, 903.64714475245,	-- The Lower Citadel
-	2, 1066.9057914008, 711.33331498279,	-- The Rampart of Skulls
-	3, 195.42805299015, 130.31517450585,	-- Deathbringer's Rise
---	4, 720.60965618252, 481.16215066130,	-- The Frost Queen's Lair	-- CONFIRM
-	5, 1149.0762925934, 301.86934021889		-- The Upper Reaches
---	6, 348.05218433541, 232.05964286208		-- Royal Quarters			-- CONFIRM
+	1, 1355,47009278, 903,64703369,		-- The Lower Citadel
+	2, 1067,00000000, 711,33369064,		-- The Rampart of Skulls
+	3, 195,46997071, 130,31500244,		-- Deathbringer's Rise
+	4, 773,71008301, 515,81030273,		-- The Frost Queen's Lair
+	5, 1148,73999024, 765,82006836,		-- The Upper Reaches
+	6, 373,70996094, 249,12988281,		-- Royal Quarters
+	7, 293,26000977, 195,50701904,		-- The Frozen Throne
+	8, 247,92993165, 165,28799439		-- Frostmourne
 )
