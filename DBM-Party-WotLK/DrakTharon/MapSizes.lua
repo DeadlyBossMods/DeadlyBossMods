a0d274b0b51a5d87579d3bcebee287ed3a1deb1b
Adam <MysticalOS@users.noreply.github.com>
2010-01-29 22:06:16 +0000

    Ulduar/Mapsizes: Commented out the bad area 6 data since it's incorrect anyways. Ulduar/Mapsizes: White space commented drak tharon a placeholder spot for missing area

--- DBM-Party-WotLK/DrakTharon/MapSizes.lua
+++ DBM-Party-WotLK/DrakTharon/MapSizes.lua
@@ -1,4 +1,5 @@
 --needs area 2
 DBM:RegisterMapSize("DrakTharonKeep",
 	1, 619.93917093835, 413.29113734848 -- The Vestibules of Drak'Tharon
+--
 )
--- DBM-Ulduar/MapSizes.lua
+++ DBM-Ulduar/MapSizes.lua
@@ -5,6 +5,6 @@ DBM:RegisterMapSize("Ulduar",
 	2, 669.45221810866, 446.29840569337,	-- The Antechamber of Ulduar
 	3, 1328.4581100659, 885.64270386813,	-- The Inner Sanctum of Ulduar
 	4, 910.49817408689, 607.00180510881,	-- The Prison of Yogg-Saron
-	5, 1569.4723670012, 1046.3012526423,	-- The Spark of Imagination
-	6, 576.71549337896, 384.46653291368		-- The Mind's Eye -- CONFIRM -- copied from Deus Vox Encounters (http://wow.curse.com/downloads/wow-addons/details/deus-vox-encounters.aspx)
+	5, 1569.4723670012, 1046.3012526423		-- The Spark of Imagination
+--	6, 576.71549337896, 384.46653291368		-- The Mind's Eye -- CONFIRM -- copied from Deus Vox Encounters (http://wow.curse.com/downloads/wow-addons/details/deus-vox-encounters.aspx)
 )
