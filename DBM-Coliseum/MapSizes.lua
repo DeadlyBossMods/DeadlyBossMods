79b7e9d36cfe11672e4aedbf9d9a39e2d2262c14
Adam <MysticalOS@users.noreply.github.com>
2010-01-30 06:57:24 +0000

    #Raid/All: All mapsizes updated based on datamined and more accurate data (with exception of Ulduar/ChamberofAspects, those are commented as still using mage calculated values)

--- DBM-Coliseum/MapSizes.lua
+++ DBM-Coliseum/MapSizes.lua
@@ -1,4 +1,4 @@
 DBM:RegisterMapSize("TheArgentColiseum",
-	1, 369.98599024229, 246.65769016535,	-- The Argent Coliseum
-	2, 739.99522937124, 493.33062806372		-- The Icy Depths
+	1, 369.9861869814, 246.657989502,		-- The Argent Coliseum
+	2, 739.996017456, 493.33001709			-- The Icy Depths
 )
