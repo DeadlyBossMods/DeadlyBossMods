c493d63951b87f363d026d85cdd281ea4c2fb006
Adam <MysticalOS@users.noreply.github.com>
2010-01-30 13:18:01 +0000

    #Ulduar/Mapsizes: All 6 now using data mined values and are perfect. #ChamberofAspects/Mapsizes: Now using data mined values #Party/PitofSaron: Now using data mined values #Party/Oldstratholme: Now using data mined values

--- DBM-Party-WotLK/OldStratholme/MapSizes.lua
+++ DBM-Party-WotLK/OldStratholme/MapSizes.lua
@@ -1,5 +1,4 @@
---area 1 mage calculated based still.
 DBM:RegisterMapSize("CoTStratholme",
-	1, 1825.0029471539, 1216.6705146937,	-- Norndir Preperation
+	1, 1824.997, 1216.67,					-- Norndir Preperation
 	2, 1125.299987791, 750.19995117			-- Stratholme City
 )
