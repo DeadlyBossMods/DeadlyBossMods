2710b65afd81d7e315a82a68f421c72f2d23f629
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-01-31 08:54:46 +0000

    #Party/NovosTheSummoner: Warning for crystal handlers and timers for next one.

--- trunk/DBM-Party-WotLK/DrakTharon/NovosTheSummoner.lua
+++ trunk/DBM-Party-WotLK/DrakTharon/NovosTheSummoner.lua
@@ -8,4 +8,34 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"CHAT_MSG_RAID_BOSS_EMOTE"
 )
+
+local WarnCrystalHandler 	= mod:NewAnnounce("WarnCrystalHandler")
+
+local timerCrystalHandler 	= mod:NewTimer(20, "timerCrystalHandler")
+
+local CrystalHandlers = 4
+
+function mod:OnCombatStart(delay)
+	timerCrystalHandler:Start(25.5-delay)
+	CrystalHandlers = 4
+end
+
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
+	if msg:find(L.HandlerEmote) then
+		self:SendSync("HandlerSpawned")
+	end
+end
+
+function mod:OnSync(msg)
+	if msg == "HandlerSpawned" then
+		CrystalHandlers = CrystalHandlers - 1
+		if CrystalHandlers > 0 then
+			WarnCrystalHandler:Show(CrystalHandlers)
+			timerCrystalHandler:Start()
+		else
+			WarnCrystalHandler:Show(CrystalHandlers)
+		end
+	end
+end
