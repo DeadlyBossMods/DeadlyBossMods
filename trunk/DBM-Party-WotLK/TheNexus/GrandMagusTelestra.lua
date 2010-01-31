25b34e6376ef8e4b06088fe2e892d719f8c889d0
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-01-31 08:27:14 +0000

    Tweaked when Grandmagus and prophettharonja do their soon warnings, since they tended to come too late.

--- trunk/DBM-Party-WotLK/TheNexus/GrandMagusTelestra.lua
+++ trunk/DBM-Party-WotLK/TheNexus/GrandMagusTelestra.lua
@@ -25,7 +25,7 @@ function mod:OnCombatStart()
 end
 
 function mod:UNIT_HEALTH(uId)
-	if not warnedSplit1 and self:GetUnitCreatureId(uId) == 26731 and UnitHealth(uId) / UnitHealthMax(uId) <= 0.54 then
+	if not warnedSplit1 and self:GetUnitCreatureId(uId) == 26731 and UnitHealth(uId) / UnitHealthMax(uId) <= 0.58 then
 		warnedSplit1 = true
 		warningSplitSoon:Show()
 	elseif not warnedSplit2 and mod:IsDifficulty("heroic5") and self:GetUnitCreatureId(uId) == 26731 and UnitHealth(uId) / UnitHealthMax(uId) <= 0.19 then
