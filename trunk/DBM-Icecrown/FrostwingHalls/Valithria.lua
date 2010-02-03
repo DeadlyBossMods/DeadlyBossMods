4e4977192eebd6c6142dce20864ee89d86b07d1e
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-02-03 06:24:12 +0000

    #ICC/Valithria: Added working portal timers/warnings pre warnings #ICC/Valithria: Removed Broken Columb of Frost warning (it doesn't show in combat log) #ICC/Valithria: Added special warnings for Lay waste and buff active timer #ICC/Valithria: Suppression warning is now working with anti spam code #ICC/Valithria: Added working portal timers/warnings pre warnings #ICC/Valithria: Added optional special warning to dispel gut spray ICC/Valithria: Commented out berserk timer for now, i didn't see it.

--- trunk/DBM-Icecrown/FrostwingHalls/Valithria.lua
+++ trunk/DBM-Icecrown/FrostwingHalls/Valithria.lua
@@ -3,55 +3,47 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(36789)
-mod:RegisterCombat("combat")
+mod:RegisterCombat("YELL", L.YellPull)
+mod:RegisterKill("YELL", L.YellKill)
 
 mod:RegisterEvents(
-	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
 	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED"
+	"SPELL_AURA_REMOVED",
+	"CHAT_MSG_MONSTER_YELL"
 )
 
 local warnCorrosion		= mod:NewAnnounce("warnCorrosion", 2, nil, false)
 local warnGutSpray		= mod:NewTargetAnnounce(71283)
-local warnLayWaste		= mod:NewSpellAnnounce(69325)
-local warnManaVoid		= mod:NewSpellAnnounce(71179)
-local warnColumnofFrost	= mod:NewSpellAnnounce(70702)
+local warnManaVoid		= mod:NewSpellAnnounce(71741)
 local warnSupression	= mod:NewSpellAnnounce(70588)
+local warnPortalSoon	= mod:NewSoonAnnounce(71987)
 local warnPortal		= mod:NewSpellAnnounce(71987)
 
---local timerLayWaste		= mod:NewBuffActiveTimer(12, 69325)--don't know spell mechanic yet (interuptable?)
---local timerNextPortal		= mod:NewNextTimer(30, 71987)--unknown value at this time
+local specWarnLayWaste	= mod:NewSpecialWarningSpell(71730)
+local specWarnGutSpray	= mod:NewSpecialWarningDispel(71283, false)
+
+local timerLayWaste		= mod:NewBuffActiveTimer(12, 69325)
+local timerNextPortal	= mod:NewCDTimer(46, 71987)
 local timerGutSpray		= mod:NewTargetTimer(12, 71283)
 local timerCorrosion	= mod:NewTargetTimer(6, 70751)
 
-local berserkTimer					= mod:NewBerserkTimer(420)
+--local berserkTimer	= mod:NewBerserkTimer(420)--Seems to exist just kinda funny, the adds just blow you up, not her
 
-local spamPortals = 0
+local spamSupression = 0
 
 function mod:OnCombatStart(delay)
---	timerNextPortal:Start(-delay)
-	berserkTimer:Start(-delay)
-end
-
-function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(69325, 71730) then--Lay Waste (spellids drycoded, will confirm later)
-		warnLayWaste:Show()
---		timerLayWaste:Start()
-	end
+	warnPortalSoon:Schedule(41-delay)
+	timerNextPortal:Start(-delay)
+--	berserkTimer:Start(-delay)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(71085) then--Mana Void (spellids drycoded, will confirm later)
+	if args:IsSpellID(71741) then--Mana Void (spellids drycoded, will confirm later)
 		warnManaVoid:Show()
-	elseif args:IsSpellID(70715) then--Column of Frost (spellids drycoded, will confirm later)
-		warnColumnofFrost:Show()
-	elseif args:IsSpellID(70588) then--Supression (spellids drycoded, will confirm later)
-		warnSupression:Show()
-	elseif args:IsSpellID(71987) and GetTime() - spamPortals > 5 then--Summon Nightmare Portal (spellids drycoded, will confirm later)
-		warnPortal:Show()
---		timerNextPortal:Start()
-		spamPortals = GetTime()
+	elseif args:IsSpellID(70588) and GetTime() - spamSupression > 5 then--Supression
+		warnSupression:Show(args.destName)
+		spamSupression = GetTime()
 	end
 end
 
@@ -59,16 +51,36 @@ function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(70633, 71283, 72025, 72026) then--Gut Spray (spellids drycoded, will confirm later)
 		warnGutSpray:Show(args.destName)
 		timerGutSpray:Start(args.destName)
+		specWarnGutSpray:Show(args.destName)
 	elseif args:IsSpellID(70751) then--Corrosion (spellids drycoded, will confirm later)
 		warnCorrosion:Show(args.spellName, args.destName, args.amount or 1)
 		timerCorrosion:Start(args.destName)
+	elseif args:IsSpellID(69325, 71730) then--Lay Waste (spellids drycoded, will confirm later)
+		specWarnLayWaste:Show()
+		timerLayWaste:Start()
 	end
 end
 
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
-	if args:IsSpellID(70633, 71283, 72025, 72026) then
+	if args:IsSpellID(70633, 71283, 72025, 72026) then--Gut Spray (spellids drycoded, will confirm later)
 		timerGutSpray:Cancel(args.destName)
+	elseif args:IsSpellID(69325, 71730) then--Lay Waste (spellids drycoded, will confirm later)
+		timerLayWaste:Cancel()
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg == L.YellPortals or msg:find(L.YellPortals) then
+		self:SendSync("NightmarePortal")
+	end
+end
+
+function mod:OnSync(msg, arg)
+	if msg == "NightmarePortal" then
+		warnPortal:Show()
+		warnPortalSoon:Schedule(41)
+		timerNextPortal:Start()
 	end
 end
