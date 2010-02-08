7c2d92a67af2bfd30602114b08de7ad46cdd6ad1
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-02-08 10:10:34 +0000

    #ICC/Valithria: Try to set icons on Blazing Skeleton whenever possible. #ICC/Trash Multiple: Fix up timers again to be target specific.

--- trunk/DBM-Icecrown/FrostwingHalls/Valithria.lua
+++ trunk/DBM-Icecrown/FrostwingHalls/Valithria.lua
@@ -3,14 +3,17 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(36789)
+mod:SetUsedIcons(8)
 mod:RegisterCombat("yell", L.YellPull)
 mod:RegisterKill("yell", L.YellKill)
 
 mod:RegisterEvents(
+	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_REMOVED",
-	"CHAT_MSG_MONSTER_YELL"
+	"CHAT_MSG_MONSTER_YELL",
+	"UNIT_TARGET"
 )
 
 local warnCorrosion		= mod:NewAnnounce("warnCorrosion", 2, nil, false)
@@ -21,21 +24,53 @@ local warnPortalSoon	= mod:NewSoonAnnounce(71987)
 local warnPortal		= mod:NewSpellAnnounce(71987)
 
 local specWarnLayWaste	= mod:NewSpecialWarningSpell(71730)
-local specWarnGutSpray	= mod:NewSpecialWarningDispel(71283, false)
 
 local timerLayWaste		= mod:NewBuffActiveTimer(12, 69325)
 local timerNextPortal	= mod:NewCDTimer(46, 71987)
 local timerGutSpray		= mod:NewTargetTimer(12, 71283)
 local timerCorrosion	= mod:NewTargetTimer(6, 70751)
 
---local berserkTimer	= mod:NewBerserkTimer(420)--Seems to exist just kinda funny, the adds just blow you up, not her
+local berserkTimer	= mod:NewBerserkTimer(420)--Seems to exist just kinda funny, the adds just blow you up, not her
 
+mod:AddBoolOption("SetIconOnBlazingSkeleton", true)
+
+local GutSprayTargets = {}
 local spamSupression = 0
+local BlazingSkeleton
+
+local function warnGutSprayTargets()
+	warnGutSpray:Show(table.concat(GutSprayTargets, "<, >"))
+	table.wipe(GutSprayTargets)
+end
 
 function mod:OnCombatStart(delay)
 	warnPortalSoon:Schedule(41-delay)
 	timerNextPortal:Start(-delay)
---	berserkTimer:Start(-delay)
+	berserkTimer:Start(-delay)
+	BlazingSkeleton = nil
+end
+
+function mod:TrySetTarget()
+	if DBM:GetRaidRank() >= 1 then
+		for i = 1, GetNumRaidMembers() do
+			if UnitGUID("raid"..i.."target") == BlazingSkeleton then
+				BlazingSkeleton = nil
+				SetRaidTarget("raid"..i.."target", 8)
+			end
+			if not BlazingSkeleton then
+				break
+			end
+		end
+	end
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(70754, 71748) then--Fireball (its the first spell Blazing SKeleton's usually cast upon spawning)
+		if self.Options.SetIconOnBlazingSkeleton then
+			BlazingSkeleton = args.sourceGUID
+			self:TrySetTarget()
+		end
+	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
@@ -49,15 +84,20 @@ end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(70633, 71283, 72025, 72026) then--Gut Spray (spellids drycoded, will confirm later)
-		warnGutSpray:Show(args.destName)
+		GutSprayTargets[#GutSprayTargets + 1] = args.destName
 		timerGutSpray:Start(args.destName)
-		specWarnGutSpray:Show(args.destName)
+		self:Unschedule(warnGutSprayTargets)
+		self:Schedule(0.3, warnGutSprayTargets)
 	elseif args:IsSpellID(70751) then--Corrosion (spellids drycoded, will confirm later)
 		warnCorrosion:Show(args.spellName, args.destName, args.amount or 1)
 		timerCorrosion:Start(args.destName)
 	elseif args:IsSpellID(69325, 71730) then--Lay Waste (spellids drycoded, will confirm later)
 		specWarnLayWaste:Show()
 		timerLayWaste:Start()
+		if self.Options.SetIconOnBlazingSkeleton then
+			BlazingSkeleton = args.sourceGUID
+			self:TrySetTarget()
+		end
 	end
 end
 
@@ -82,5 +122,11 @@ function mod:OnSync(msg, arg)
 		warnPortal:Show()
 		warnPortalSoon:Schedule(41)
 		timerNextPortal:Start()
+	end
+end
+
+function mod:UNIT_TARGET()
+	if BlazingSkeleton then
+		self:TrySetTarget()
 	end
 end
