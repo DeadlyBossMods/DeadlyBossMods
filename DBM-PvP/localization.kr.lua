525eaab26d7ae6999c8e39aec51dcaa6807c96c6
bluenyx <bluenyx@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-02-11 09:11:37 +0000

    koKR update

--- DBM-PvP/localization.kr.lua
+++ DBM-PvP/localization.kr.lua
@@ -236,7 +236,7 @@ L:SetOptionLocalization{
 	NovaSound			= "$spell:65279의 특수 소리 재생",	
 	timerMobOvercharge	= "과충전 된 몹의 시간 보기(디버프 중첩)",
 	EmalonEnrage		= "$spell:26662 타이머 보기",
-	RangeFrame			= "거리 프레임 보기"
+	RangeFrame			= "거리 프레임 보기(10 m)"
 }
 
 ---------------
