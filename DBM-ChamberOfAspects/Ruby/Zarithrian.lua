7ad3f0762436fa48c60fdcfd7057495774f8d2a6
Adam <MysticalOS@users.noreply.github.com>
2010-02-19 20:40:50 +0000

    pretty up chamber of aspects and add ruby sanctum to it for upcoming content patch. Mods are clearly placeholders right now since bosses aren't active, but now the chamber of aspects mod can look more like it was intended to to begin with :)

---
+++ DBM-ChamberOfAspects/Ruby/Zarithrian.lua
@@ -0,0 +1,16 @@
+local mod	= DBM:NewMod("Zarithrian", "DBM-ChamberOfAspects", 2)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision: 2317 $"):sub(12, -3))
+mod:SetCreatureID()
+
+mod:RegisterCombat("combat")
+
+mod:RegisterEvents(
+	"SPELL_CAST_SUCCESS",
+	"SPELL_AURA_APPLIED",
+	"SPELL_DAMAGE",
+	"CHAT_MSG_RAID_BOSS_EMOTE",
+	"CHAT_MSG_MONSTER_EMOTE"
+)
+
