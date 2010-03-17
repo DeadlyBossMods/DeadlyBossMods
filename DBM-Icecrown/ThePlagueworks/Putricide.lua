266fe9bf2fae4316e625a1694d61a2be32b2cfb3
Adam <MysticalOS@users.noreply.github.com>
2010-03-17 18:17:44 +0000

    update minsync version again for putricide

--- DBM-Icecrown/ThePlagueworks/Putricide.lua
+++ DBM-Icecrown/ThePlagueworks/Putricide.lua
@@ -4,7 +4,7 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(36678)
 mod:RegisterCombat("yell", L.YellPull)
-mod:SetMinSyncRevision(3808)
+mod:SetMinSyncRevision(3837)
 mod:SetUsedIcons(1, 2, 6, 7, 8)
 
 mod:RegisterEvents(
