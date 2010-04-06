2e0fa97a5cba4a402341d12ec677d2ad4de283cc
Adam <MysticalOS@users.noreply.github.com>
2010-04-06 12:33:32 +0000

    fix sindragosa 10 man icons

--- DBM-Icecrown/FrostwingHalls/Sindragosa.lua
+++ DBM-Icecrown/FrostwingHalls/Sindragosa.lua
@@ -142,15 +142,14 @@ function mod:SPELL_AURA_APPLIED(args)
 		end
 		if self.Options.SetIconOnFrostBeacon then
 			table.insert(beaconIconTargets, DBM:GetRaidUnitId(args.destName))
-			if phase == 2 or (mod:IsDifficulty("normal25") and #beaconTargets >= 5) or (mod:IsDifficulty("heroic25") and #beaconTargets >= 6) or (mod:IsDifficulty("normal10") or mod:IsDifficulty("heroic10") and #beaconTargets >= 2) then
+			if phase == 2 or (mod:IsDifficulty("normal25") and #beaconTargets >= 5) or (mod:IsDifficulty("heroic25") and #beaconTargets >= 6) or ((mod:IsDifficulty("normal10") or mod:IsDifficulty("heroic10")) and #beaconTargets >= 2) then
 				self:SetBeaconIcons()
 			else
 				self:ScheduleMethod(0.8, "SetBeaconIcons")
 			end
 		end
 		self:Unschedule(warnBeaconTargets)
-		if phase == 2 or (mod:IsDifficulty("normal25") and #beaconTargets >= 5) or (mod:IsDifficulty("heroic25") and #beaconTargets >= 6) or (mod:IsDifficulty("normal10") or mod:IsDifficulty("heroic10") and #beaconTargets >= 2) then
-			warnBeaconTargets()
+		if phase == 2 or (mod:IsDifficulty("normal25") and #beaconTargets >= 5) or (mod:IsDifficulty("heroic25") and #beaconTargets >= 6) or ((mod:IsDifficulty("normal10") or mod:IsDifficulty("heroic10")) and #beaconTargets >= 2) then
 		else
 			self:Schedule(0.8, warnBeaconTargets)
 		end
