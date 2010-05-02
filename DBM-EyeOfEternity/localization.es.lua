70382da9e0cfa0856da30ca17ec389eb35c0d56f
Snamor <Snamor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-05-02 16:03:33 +0000

    #EoE/All: esES update (Typo fix)

--- DBM-EyeOfEternity/localization.es.lua
+++ DBM-EyeOfEternity/localization.es.lua
@@ -31,7 +31,7 @@ L:SetOptionLocalization({
 })
 
 L:SetMiscLocalization({
-	YellPull		= "Habeis agotado mi paciencia. ¡Me desharé de vosotros!",
+	YellPull		= "Habéis agotado mi paciencia. ¡Me desharé de vosotros!",
 	EmoteSpark		= "¡Un Chispazo toma forma a partir de una falla cercana!",
 	YellPhase2		= "Esperaba acabar con vuestras vidas rápidamente, pero habéis resultado más... resistentes de lo que pensaba. Aun así, vuestros esfuerzos son en vano. Esta guerra es culpa vuestra, imprudentes mortales descuidados. Yo hago lo que debo... y si eso implica vuestra extinción... ¡QUE ASÍ SEA!",
 	EmoteBreath		= "%s respira hondo.",
