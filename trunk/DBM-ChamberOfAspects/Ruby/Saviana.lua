e4a633d8167729f9d77322e5f06ffad9280aacac
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-05-14 00:02:45 +0000

    added setrevision flag on the 2 new files

--- trunk/DBM-ChamberOfAspects/Ruby/Saviana.lua
+++ trunk/DBM-ChamberOfAspects/Ruby/Saviana.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Saviana", "DBM-ChamberOfAspects", 2)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 3695 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(39747)
 
 mod:RegisterCombat("combat")
