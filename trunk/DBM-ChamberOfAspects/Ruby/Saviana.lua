b2c2d4baaebc3ed70664dcb3fe01dbafea8d478f
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-05-14 00:02:02 +0000

    Some ruby sanctum placeholders, and guessed creature IDs. unfortunately i will be able to do nothing on ptr since servers are broken. hopefully people will submit full logs, chat/combat as well as creatureids that are valid.

--- trunk/DBM-ChamberOfAspects/Ruby/Saviana.lua
+++ trunk/DBM-ChamberOfAspects/Ruby/Saviana.lua
@@ -1,8 +1,8 @@
-local mod	= DBM:NewMod("Zarithrian", "DBM-ChamberOfAspects", 2)
+local mod	= DBM:NewMod("Saviana", "DBM-ChamberOfAspects", 2)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(39746)
+mod:SetRevision(("$Revision: 3695 $"):sub(12, -3))
+mod:SetCreatureID(39747)
 
 mod:RegisterCombat("combat")
 
