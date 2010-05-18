8bee68a3841763d8ada30a2aa2e5998e6bb0833f
Adam <MysticalOS@users.noreply.github.com>
2010-05-18 20:18:22 +0000

    #Ruby Sanctum/Baltharus: Added some stuff to start, split and whirlwind. More to be added in coming hours.

--- DBM-ChamberOfAspects/Ruby/Baltharus.lua
+++ DBM-ChamberOfAspects/Ruby/Baltharus.lua
@@ -11,6 +11,48 @@ mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
 	"SPELL_DAMAGE",
 	"CHAT_MSG_RAID_BOSS_EMOTE",
-	"CHAT_MSG_MONSTER_EMOTE"
+	"CHAT_MSG_MONSTER_EMOTE",
+	"UNIT_HEALTH"
 )
 
+local warningSplitSoon	= mod:NewAnnounce("WarningSplitSoon", 2)
+local warningSplitNow	= mod:NewAnnounce("WarningSplitNow", 3)
+--local preWarnWhirlwind   	= mod:NewSoonAnnounce(75127, 3)--Unknown
+
+local specWarnWhirlwind		= mod:NewSpecialWarningRun(75127)
+
+--local timerWhirlwindCD		= mod:NewCDTimer(90, 75127)--Unknown
+--local timerWhirlwind			= mod:NewBuffActiveTimer(20, 75127)--Unknown
+
+local soundWhirlwind = mod:NewSound(75127)
+
+local warnedSplit		= false
+
+function mod:OnCombatStart(delay)
+--	preWarnWhirlwind:Schedule(-delay)
+--	timerWhirlwindCD:Start(-delay)
+	warnedSplit = false
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(75127, 75125) then
+		specWarnWhirlwind:Show()
+--		timerWhirlwindCD:Start()
+--		preWarnWhirlwind:Schedule(85)
+--		timerWhirlwind:Show()
+		soundWhirlwind:Play()
+	end
+end
+
+function mod:UNIT_HEALTH(uId)
+	if not warnedSplit and self:GetUnitCreatureId(uId) == 39751 and UnitHealth(uId) / UnitHealthMax(uId) <= 0.54 then
+		warnedSplit = true
+		warningSplitSoon:Show()
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg == L.SplitTrigger then
+		warningSplitNow:Show()
+	end
+end
