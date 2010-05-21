6caabe253aa8561b9a0950b75d4447bc278675a7
Azael <Azael@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-05-21 17:49:21 +0000

    update without any translation~

--- DBM-ChamberOfAspects/localization.tw.lua
+++ DBM-ChamberOfAspects/localization.tw.lua
@@ -94,6 +94,22 @@ L:SetGeneralLocalization({
 	name = "Baltharus the Warborn"
 })
 
+L:SetWarningLocalization({
+	WarningSplitSoon	= "Split soon",
+	WarningSplitNow		= "Split"
+})
+
+L:SetOptionLocalization({
+	WarningSplitSoon	= "Show pre-warning for Split (54%)",
+	WarningSplitNow		= "Show warning for Split",
+	RangeFrame			= "Show range frame (12 yards)",
+	SetIconOnBrand		= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(74505)
+})
+
+L:SetMiscLocalization({
+	SplitTrigger		= ""
+})
+
 -------------------------
 --  Saviana Ragefire  --
 -------------------------
@@ -102,6 +118,23 @@ L = DBM:GetModLocalization("Saviana")
 L:SetGeneralLocalization({
 	name = "Saviana Ragefire"
 })
+
+L:SetWarningLocalization({
+	specWarnConflagNear		= "Conflagration near you - Watch out",
+	SpecialWarningTranq		= "Enrage - Tranq now"
+})
+
+L:SetOptionLocalization({
+	specWarnConflagNear		= "Show special warning for $spell:74452 near you",
+	SpecialWarningTranq		= "Show special warning for Enrage (to tranq)",--$spell:78722 is not in 3.3.3 game files, it cannot be added in to local until 3.3.5
+	YellOnConflag			= "Yell on $spell:74452",
+	RangeFrame				= "Show range frame (10 yards)",
+	ConflagIcon				= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(74452)
+})
+
+L:SetMiscLocalization{
+	YellConflag	= "Conflagration on me!"
+}
 
 --------------------------
 --  General Zarithrian  --
@@ -112,6 +145,12 @@ L:SetGeneralLocalization({
 	name = "General Zarithrian"
 })
 
+L:SetWarningLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
 -------------------------------------
 --  Halion the Twilight Destroyer  --
 -------------------------------------
@@ -119,4 +158,15 @@ L = DBM:GetModLocalization("Halion")
 
 L:SetGeneralLocalization({
 	name = "Halion the Twilight Destroyer"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetOptionLocalization({
+	SoundOnConsumption			= "Play sound on Combustion",--We use localized text for these functions
+	SetIconOnConsumption		= "Set icons on Combustion targets"--So we can use single functions for both versions of spell.
+})
+
+L:SetMiscLocalization({
 })
