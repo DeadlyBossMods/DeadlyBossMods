6e3ed2dd940bf47984e63a5dde2e3ccb0fe044a6
Adam <MysticalOS@users.noreply.github.com>
2010-05-26 15:19:33 +0000

    #Ruby/Halion: Thanks to combat logs, all timers adjusted and combat events corrected. Should now be basically functional minus heroic or fine tuning. #Ruby/Zarithrian: Mod implimented

--- DBM-ChamberOfAspects/Ruby/Zarithrian.lua
+++ DBM-ChamberOfAspects/Ruby/Zarithrian.lua
@@ -7,10 +7,37 @@ mod:SetCreatureID(39746)
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
-	"SPELL_CAST_SUCCESS",
-	"SPELL_AURA_APPLIED",
-	"SPELL_DAMAGE",
-	"CHAT_MSG_RAID_BOSS_EMOTE",
-	"CHAT_MSG_MONSTER_EMOTE"
+	"SPELL_CAST_START",
+	"CHAT_MSG_MONSTER_YELL"
 )
 
+local warningAdds					= mod:NewAnnounce("WarnAdds", 3)
+local warningFear					= mod:NewSpellAnnounce(74384, 3)
+
+local timerFearCD					= mod:NewCDTimer(35, 74384)
+local timerAddsCD					= mod:NewNextTimer(45.5, "TimerAdds")
+
+function mod:OnCombatStart(delay)
+	timerFearCD:Start(15-delay)
+	timerAddsCD:Start(15-delay)
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(74384) then
+		warningFear:Show()
+		timerFearCD:Start()
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg:match(L.SummonMinions) then
+		self:SendSync("SummonMinions")
+	end
+end
+
+function mod:OnSync(msg)
+	if msg == "SummonMinions" then
+		warningFear:Show()
+		timerAddsCD:Start()
+	end
+end
