4c9ba87b7b031112245ce573ec084a8d32a86ceb
Samira <Samira@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-06-21 20:14:48 +0000

    Ahune, german Update

--- DBM-WorldEvents/localization.de.lua
+++ DBM-WorldEvents/localization.de.lua
@@ -17,7 +17,7 @@ L:SetGeneralLocalization({
 })
 
 L:SetWarningLocalization({
-	specWarnBrew		= "Get rid of the brew before she tosses you another one!",	--to be translated
+	specWarnBrew		= "Get rid of the brew befüre she tosses you another one!",	--to be translated
 	specWarnBrewStun	= "HINT: You were bonked, remember to drink the brew next time!"	--to be translated
 })
 
@@ -53,7 +53,7 @@ L:SetOptionLocalization({
 L:SetMiscLocalization({
 	HorsemanHead		= "Get over here, you idiot!",	--to be translated
 	HorsemanSoldiers	= "Soldiers arise, stand and fight! Bring victory at last to this fallen knight!",	--to be translated
-	SayCombatEnd		= "This end have I reached before.  What new adventure lies in store?"	--to be translated
+	SayCombatEnd		= "This end have I reached befüre.  What new adventure lies in store?"	--to be translated
 })
 
 -----------------------
@@ -80,4 +80,38 @@ L:SetOptionLocalization({
 
 L:SetMiscLocalization({
 	SayCombatStart		= "Haben sie sich die Mühe gemacht und Euch gesagt, wer ich bin und warum ich das hier tue?"
+})
+
+-----------------------
+--  Lord Ahune  --
+-----------------------
+L = DBM:GetModLocalization("Ahune")
+
+L:SetGeneralLocalization({
+	name = "Fürst Ahune"
+})
+
+L:SetWarningLocalization({
+	Submerged		= "Ahune untergetaucht",
+	Emerged			= "Ahune aufgetaucht",
+	specWarnAttack	= "Ahune ist verwundbar - Angriff!"
+})
+
+L:SetTimerLocalization{
+	SubmergTimer	= "Untertauchen",
+	EmergeTimer		= "Auftauchen",
+	TimerCombat		= "Kampfbeginn"
+}
+
+L:SetOptionLocalization({
+	Submerged		= "Zeige Warnung wenn Ahune untertaucht",
+	Emerged			= "Zeige Warnung wenn Ahune auftaucht",
+	specWarnAttack	= "Zeige Spezialwarnun wenn Ahune verwundbar wird",
+	SubmergTimer	= "Zeige Timer für Untertauchen",
+	EmergeTimer		= "Zeige Timer für Auftauchen",
+	TimerCombat		= "Zeige Timer für Kampfbeginn",
+})
+
+L:SetMiscLocalization({
+	Pull			= "Der Eisbrocken ist geschmolzen!"
 })
