6799fa8bfde6a4966125883aee75ea0b8f2c84e0
Adam <MysticalOS@users.noreply.github.com>
2010-06-21 01:37:33 +0000

    ##ALL: Pulled world events out of WotLK party mods in prep for cataclysm. Before long wraith party mods will be old content and an optional alternate download much like BC mods are now, but world event boss mods will move on to the new package. Also, with world events being very limited time encounters, it was a poor use of memory/cpu to load them in party mods. pulling them out of party frees this memory/load time back up. #World Events/Ahune: Ahune now has a basic mod that still needs more testing #World Events/Coren Direwbrew: Coren direbrew no longer plays run away sound for disarm, disarm is not threatening just an inconvinience it doesn't need to be treated so severely #Ulduar/FL: Added run away sound to pursuit on you.
    Note to localizers. To make sure the mods at least loaded from my chance i left all sub load zones in both party and in world event. Please update both by removing unnessesary load zones from each to prevent both mods from loading in all dungeons in your language.

---
+++ DBM-WorldEvents/localization.ru.lua
@@ -0,0 +1,78 @@
+﻿if GetLocale() ~= "ruRU" then return end
+
+local L
+
+----------------------
+--  Coren Direbrew  --
+----------------------
+L = DBM:GetModLocalization("CorenDirebrew")
+
+L:SetGeneralLocalization({
+	name = "Корен Худовар"
+})
+
+L:SetWarningLocalization({
+	specWarnBrew		= "Избавьтесь от варева прежде, чем она бросит вам другое!",
+	specWarnBrewStun	= "СОВЕТ: Вы получили удар, не забудьте выпить варево в следующий раз!"
+})
+
+L:SetOptionLocalization({
+	specWarnBrew		= "Спец-предупреждение для Пива темной официантки",
+	specWarnBrewStun	= "Спец-предупреждение для Оглушения темным пивом официантки",
+	YellOnBarrel		= "Крикнуть, когда на вас Бочка"
+})
+
+L:SetMiscLocalization({
+	YellBarrel	= "Бочка на мне!"
+})
+
+-------------------------
+--  Headless Horseman  --
+-------------------------
+L = DBM:GetModLocalization("HeadlessHorseman")
+
+L:SetGeneralLocalization({
+	name = "Всадник без головы"
+})
+
+L:SetWarningLocalization({
+	warnHorsemanSoldiers	= "Призыв Пульсирующих тыкв",
+	specWarnHorsemanHead	= "Вихрь - переключитесь на голову"
+})
+
+L:SetOptionLocalization({
+	warnHorsemanSoldiers	= "Предупреждать о призыве Пульсирующих тыкв",
+	specWarnHorsemanHead	= "Спец-предупреждение для Вихря (призыв 2ой и следующей головы)"
+})
+
+L:SetMiscLocalization({
+	HorsemanHead		= "Не надоело еще убегать?",
+	HorsemanSoldiers	= "Восстаньте слуги, устремитесь в бой! Пусть павший рыцарь обретет покой!",
+	SayCombatEnd		= "Со смертью мы давно уже друзья...Что ждет теперь на пустоши меня?"
+})
+
+-----------------------
+--  Apothecary Trio  --
+-----------------------
+L = DBM:GetModLocalization("ApothecaryTrio")
+
+L:SetGeneralLocalization({
+	name = "Трое аптекарей"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization{
+	HummelActive	= "Хаммел вступает в бой",
+	BaxterActive	= "Бакстер вступает в бой",
+	FryeActive		= "Фрай вступает в бой"
+}
+
+L:SetOptionLocalization({
+	TrioActiveTimer		= "Отсчет времени до вступления Троих аптекарей в бой"
+})
+
+L:SetMiscLocalization({
+	SayCombatStart		= "Тебе хоть сказали, кто я и чем занимаюсь?"
+})
