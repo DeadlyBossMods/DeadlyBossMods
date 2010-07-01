0f160af8663b5fa7909fc69651e4c824bb18469c
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-07-01 05:11:05 +0000

    #Ruby Sanctum/Zarithrian: added warning and timers for Cleave Armor

--- trunk/DBM-ChamberOfAspects/Ruby/Zarithrian.lua
+++ trunk/DBM-ChamberOfAspects/Ruby/Zarithrian.lua
@@ -8,14 +8,20 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_CAST_START",
+	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_APPLIED_DOSE",
 	"CHAT_MSG_MONSTER_YELL"
 )
 
-local warningAdds					= mod:NewAnnounce("WarnAdds", 3)
-local warningFear					= mod:NewSpellAnnounce(74384, 3)
+local warningAdds				= mod:NewAnnounce("WarnAdds", 3)
+local warnCleaveArmor			= mod:NewAnnounce("warnCleaveArmor", 2, 74367)
+local warningFear				= mod:NewSpellAnnounce(74384, 3)
 
-local timerAddsCD					= mod:NewTimer(45.5, "TimerAdds")
-local timerFearCD					= mod:NewCDTimer(33, 74384)--typically every 40 seconds but soemtimes earlier.
+local specWarnCleaveArmor		= mod:NewSpecialWarningStack(74367, nil, 2)
+
+local timerAddsCD				= mod:NewTimer(45.5, "TimerAdds")
+local timerCleaveArmor			= mod:NewTimer(30, 74367)
+local timerFearCD				= mod:NewCDTimer(33, 74384)--typically every 40 seconds but soemtimes earlier.
 
 function mod:OnCombatStart(delay)
 	timerFearCD:Start(15-delay)
@@ -29,8 +35,20 @@ function mod:SPELL_CAST_START(args)
 	end
 end
 
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(74367) then
+		warnCleaveArmor:Show(args.spellName, args.destName, args.amount or 1)
+		timerCleaveArmor:Start(args.destName)
+		if args:IsPlayer() and (args.amount or 1) >= 2 then
+			specWarnCleaveArmor:Show(args.amount)
+		end
+	end
+end
+
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+
 function mod:CHAT_MSG_MONSTER_YELL(msg)
-	if msg:match(L.SummonMinions) then
+	if msg:match(L.SummonMinions) and mod:LatencyCheck() then
 		self:SendSync("SummonMinions")
 	end
 end
