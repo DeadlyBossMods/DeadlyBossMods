7e5c3df06f2e4b25ccbcfbaa71488f3927be668f
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-07-02 08:33:09 +0000

    ##DBM/Core: Split Vault of Archavon out of pvp mods. in cataclysm voa will no longer be part of pvp mods since it'll be old content, pvp mods will move on so this would have had to be done eventually, better now than later :) Besides voa graduated from 1 boss to 4, it's a raid now :)

---
+++ trunk/DBM-VoA/localization.cn.lua
@@ -0,0 +1,34 @@
+﻿-- yleaf(yaroot@gmail.com)
+
+if GetLocale() ~= "zhCN" then return end
+
+local L
+
+----------------
+--  Archavon  --
+----------------
+
+L = DBM:GetModLocalization("Archavon")
+
+L:SetGeneralLocalization({
+	name = "远古海滩"
+})
+
+L:SetWarningLocalization({
+	WarningShards	= "Rock Shards on >%s<",
+	WarningGrab		= "Archavon grabbed >%s<"
+})
+
+L:SetTimerLocalization({
+	TimerShards = "Rock Shards: %s"
+})
+
+L:SetMiscLocalization({
+	TankSwitch = "%%s lunges for (%S+)!"
+})
+
+L:SetOptionLocalization({
+	TimerShards = "Show Rock Shards timer",
+	WarningShards = "Show Rock Shards warning",
+	WarningGrab = "Show Tank Grab warning"
+})
