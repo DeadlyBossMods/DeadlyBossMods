9815898057c80434e28fcc270941a0febcf246ba
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-07-02 08:34:36 +0000

    DBM/VoA: Set revision flag

--- trunk/DBM-VoA/Emalon.lua
+++ trunk/DBM-VoA/Emalon.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Emalon", "DBM-VoA")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 3726 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(33993)
 mod:SetUsedIcons(8)
 
