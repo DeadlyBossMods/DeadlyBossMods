d16856a31622af2435fd5723c40459469c4e1019
Adam <MysticalOS@users.noreply.github.com>
2010-07-02 08:34:36 +0000

    DBM/VoA: Set revision flag

--- DBM-VoA/Koralon.lua
+++ DBM-VoA/Koralon.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Koralon", "DBM-VoA")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 3737 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(35013)
 
 mod:RegisterCombat("combat")
