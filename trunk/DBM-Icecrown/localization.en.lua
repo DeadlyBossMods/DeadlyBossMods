a794a29df8eeb637ef95631889beceee81bf16c1
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-07-05 04:51:08 +0000

    removed unused locals

--- trunk/DBM-Icecrown/localization.en.lua
+++ trunk/DBM-Icecrown/localization.en.lua
@@ -278,7 +278,6 @@ L:SetOptionLocalization{
 	RangeFrame					= "Show range frame (8 yards)",
 	WarnUnstableOoze			= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.spell:format(69558, GetSpellInfo(69558) or "unknown"),
 	InfectionIcon				= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(71224),
-	ExplosionIcon				= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(69839),
 	TankArrow					= "Show DBM arrow for Big Ooze kiter (Experimental)"
 }
 
