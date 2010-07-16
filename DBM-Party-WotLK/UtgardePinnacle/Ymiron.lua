e4ae2cc6695be39785703430de49abcd511c7452
Adam <MysticalOS@users.noreply.github.com>
2010-07-16 16:20:05 +0000

    #ICC/Sindragosa: Changed icon setting method for beacons to not use a timer at all, instead use a count. once you have enough targets set them immediately. No scheduling timers used (to prevent it being schedule 2 or 5 or 6 times then bugging out in super rare cases if the unschedule method doesn't actually stop the extra runs. #Party/Halls of Reflection: Fixed a bug where it would say "boss incoming" for wave 6 after a wipe. (it remembers if falric is dead now so it doesn't announce wave 5 again since world states in this zone are funny. #Party/Falric: Added timer/announce for Falric's fear #Party/Falric: Added timer/announce for Falric's fear #Party/Ymiron: Added missing spellid for heroic bane, added bane timer, added warning/timer for his stun as well.

--- DBM-Party-WotLK/UtgardePinnacle/Ymiron.lua
+++ DBM-Party-WotLK/UtgardePinnacle/Ymiron.lua
@@ -11,10 +11,18 @@ mod:RegisterEvents(
 	"SPELL_AURA_APPLIED"
 )
 
-local warningBane	= mod:NewSpellAnnounce(48294, 3)
+local warningBane		= mod:NewSpellAnnounce(48294, 3)
+local warningScreams	= mod:NewSpellAnnounce(51750, 2)
+
+local timerBane			= mod:NewBuffActiveTimer(5, 48294)
+local timerScreams		= mod:NewBuffActiveTimer(8, 51750)
 
 function mod:APELL_AURA_APPLIED(args)
-	if args:IsSpellID(48294) then
+	if args:IsSpellID(48294, 59301) then
 		warningBane:Show()
+		timerBane:Start()
+	elseif args:IsSpellID(51750) then
+		warningScreams:Show()
+		timerScreams:Start()
 	end
 end
