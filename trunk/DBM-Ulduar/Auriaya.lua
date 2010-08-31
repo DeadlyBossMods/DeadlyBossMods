ccb7d14f1682432dd6a53cb4b38c5198643b7c6a
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-08-31 03:03:19 +0000

    revert pull changes here too.

--- trunk/DBM-Ulduar/Auriaya.lua
+++ trunk/DBM-Ulduar/Auriaya.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 
-mod:SetCreatureID(33515, 34014)
+mod:SetCreatureID(33515)--, 34014
 mod:RegisterCombat("combat", 33515)
 
 mod:RegisterEvents(
