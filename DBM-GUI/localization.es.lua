9bda1300e1ac9e51736bba2a25ffdc4083059f24
Snamor <romanscat@hotmail.com>
2010-09-26 14:28:19 +0000

    #GUI: esES update

--- DBM-GUI/localization.es.lua
+++ DBM-GUI/localization.es.lua
@@ -43,6 +43,9 @@ L.Statistic_Kills	= "Muertes:"
 L.Statistic_Wipes	= "Wipes:"
 L.Statistic_BestKill	= "Mejor muerte:"
 L.Statistic_Heroic	= "heroico"
+L.Statistic_10Man		= "Banda de 10"
+L.Statistic_25Man		= "Banda de 25"
+
 
 -- Tab: General Options
 L.General 		= "Opciones de DBM"
