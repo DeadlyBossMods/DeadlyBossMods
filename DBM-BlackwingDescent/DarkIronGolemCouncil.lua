ef701e4ba76ae4b6349ace95fc86d7c51f4e0de0
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-10-18 12:28:15 +0000

    # Blackwing Descent:  Added creature IDs for the Dark Iron Golem Coucil encounter

--- DBM-BlackwingDescent/DarkIronGolemCouncil.lua
+++ DBM-BlackwingDescent/DarkIronGolemCouncil.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod("DarkIronGolemCouncil", "DBM-BlackwingDescent", 2)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(42180)	-- add members!	(42180 = Toxitron)
+mod:SetCreatureID(42180, 42178, 42179, 42166)	-- add members!	(42180 = Toxitron // 42178 = Magmatron // 42179 = Electron // 42166 = Arcanotron)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
