e9dc941987cffc56db63aefbf504c6547c9f9fde
Snamor <Snamor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-10-18 11:25:17 +0000

    #Cataclysm: Added esES dummy mods

---
+++ trunk/DBM-BastionTwilight/localization.es.lua
@@ -0,0 +1,87 @@
+if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
+
+local L
+
+---------------------------
+--  Valiona & Theralion  --
+---------------------------
+L = DBM:GetModLocalization("ValionaTheralion")
+
+L:SetGeneralLocalization({
+	name =	"Valiona & Theralion"--translate
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+--------------------------
+--  Halfus Wyrmbreaker  --
+--------------------------
+L = DBM:GetModLocalization("HalfusWyrmbreaker")
+
+L:SetGeneralLocalization({
+	name =	"Halfus Wyrmbreaker"--translate
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+----------------------------------
+--  Twilight Ascendant Council  --
+----------------------------------
+L = DBM:GetModLocalization("AscendantCouncil")
+
+L:SetGeneralLocalization({
+	name =	"Twilight Ascendant Council"--translate
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+----------------
+--  Cho'gall  --
+----------------
+L = DBM:GetModLocalization("Chogall")
+
+L:SetGeneralLocalization({
+	name =	"Cho'gall"--translate
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
---
+++ trunk/DBM-BlackwingDescent/localization.es.lua
@@ -0,0 +1,129 @@
+if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
+
+local L
+
+--------------
+--  Magmaw  --
+--------------
+L = DBM:GetModLocalization("Magmaw")
+
+L:SetGeneralLocalization({
+	name = "Magmaw"--translate
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+-------------------------------
+--  Dark Iron Golem Council  --
+-------------------------------
+L = DBM:GetModLocalization("DarkIronGolemCouncil")
+
+L:SetGeneralLocalization({
+	name = "Dark Iron Golem Council"--translate
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+----------------
+--  Maloriak  --
+----------------
+L = DBM:GetModLocalization("Maloriak")
+
+L:SetGeneralLocalization({
+	name = "Maloriak"--translate
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+-----------------
+--  Chimaeron  --
+-----------------
+L = DBM:GetModLocalization("Chimaeron")
+
+L:SetGeneralLocalization({
+	name = "Chimaeron"--translate
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+-----------------
+--  Atramedes  --
+-----------------
+L = DBM:GetModLocalization("Atramedes")
+
+L:SetGeneralLocalization({
+	name = "Atramedes"--translate
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+----------------
+--  Nefarian  --
+----------------
+L = DBM:GetModLocalization("Nefarian-BD")	-- No conflict with BWL version :)
+
+L:SetGeneralLocalization({
+	name = "Nefarian"--translate
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
