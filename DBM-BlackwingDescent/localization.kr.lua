f0f0e0b1c739434ac3a3ebd77d716ab3e8e6d35d
bluenyx <everfinale@gmail.com>
2010-10-29 16:01:23 +0000

    add koKR files

---
+++ DBM-BlackwingDescent/localization.kr.lua
@@ -0,0 +1,131 @@
+local L
+
+--------------
+--  Magmaw  --
+--------------
+L = DBM:GetModLocalization("Magmaw")
+
+L:SetGeneralLocalization({
+	name = "Magmaw"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+-------------------------------
+--  Dark Iron Golem Council  --
+-------------------------------
+L = DBM:GetModLocalization("DarkIronGolemCouncil")
+
+L:SetGeneralLocalization({
+	name = "Dark Iron Golem Council"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+----------------
+--  Maloriak  --
+----------------
+L = DBM:GetModLocalization("Maloriak")
+
+L:SetGeneralLocalization({
+	name = "Maloriak"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+-----------------
+--  Chimaeron  --
+-----------------
+L = DBM:GetModLocalization("Chimaeron")
+
+L:SetGeneralLocalization({
+	name = "Chimaeron"
+})
+
+L:SetWarningLocalization({
+	WarnPhase2Soon	= "Phase 2 soon",
+	WarnBreak	= "%s on >%s< (%d)"
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+	WarnPhase2Soon	= "Show a prewarning for Phase 2",
+	WarnBreak	= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.spell:format(82881, GetSpellInfo(82881) or "unknown")
+})
+
+-----------------
+--  Atramedes  --
+-----------------
+L = DBM:GetModLocalization("Atramedes")
+
+L:SetGeneralLocalization({
+	name = "Atramedes"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+----------------
+--  Nefarian  --
+----------------
+L = DBM:GetModLocalization("Nefarian-BD")	-- No conflict with BWL version :)
+
+L:SetGeneralLocalization({
+	name = "Nefarian"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
