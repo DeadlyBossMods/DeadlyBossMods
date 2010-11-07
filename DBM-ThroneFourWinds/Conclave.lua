f2bb0722125169e2bda4d5b01a4403d4f3758909
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-11-07 11:18:57 +0000

    # Throne of Four Winds\Conclave of Wind: Added/Updated quite some spells with correct spellIDs (Added: Wind Chill next timer / Wind Blast warning + duration timer / Hurricane warning)

--- DBM-ThroneFourWinds/Conclave.lua
+++ DBM-ThroneFourWinds/Conclave.lua
@@ -13,18 +13,22 @@ mod:RegisterEvents(
 	"SPELL_CAST_SUCCESS"
 )
 
---local warnPermafrost		= mod:NewSpellAnnounce()    spellID unknown
-local warnSleetStorm		= mod:NewSpellAnnounce(86367, 3)
+local warnPermafrost		= mod:NewSpellAnnounce(86082, 3)
+local warnSleetStorm		= mod:NewSpellAnnounce(84644, 3)
 local warnNurture		= mod:NewSpellAnnounce(85422, 3)
-local warnSoothingBreeze	= mod:NewSpellAnnounce(86207, 3)	-- spell ID?
-local warnZephyr		= mod:NewSpellAnnounce(84651, 3)
+local warnSoothingBreeze	= mod:NewSpellAnnounce(86207, 3)	-- using a spellID here with a better description of the spell
+local warnZephyr		= mod:NewSpellAnnounce(84638, 3)
 local warnSummonTornados	= mod:NewSpellAnnounce(86192, 3)
 local warnSlicingGale		= mod:NewSpellAnnounce(93058, 3)
+local warnWindBlast		= mod:NewSpellAnnounce(86193, 3)
+local warnHurricane		= mod:NewSpellAnnounce(84643, 3)
 
---local timerPermafrost		= mod:NewCastTimer(2.5, id)
+local timerPermafrost		= mod:NewCastTimer(2.5, 86082)
 local timerSleetStorm		= mod:NewBuffActiveTimer(15, 86367)
+local timerWindChill		= mod:NewNextTimer(11, 84645)
 local timerZephyr		= mod:NewBuffActiveTimer(15, 84651)
 local timerSlicingGale		= mod:NewTargetTimer(30, 93058)
+local timerWindBlast		= mod:NewBuffActiveTimer(10, 86193)		-- Cooldown: 1st->2nd = 22sec || 2nd->3rd = 60sec || 3rd->4th = 60sec ?
 
 function mod:OnCombatStart(delay)
 end
@@ -38,21 +42,28 @@ end
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(85422) then
 		warnNurture:Show()
-	elseif args:IsSpellID(86205) then	-- spellID?
+	elseif args:IsSpellID(86205) then
 		warnSoothingBreeze:Show()
 	elseif args:IsSpellID(96192) then
 		warnSummonTornados:Show()
-	elseif args:IsSpellID(86182, 93056, 93057, 93058) then
+	elseif args:IsSpellID(86182, 93056, 93057, 93058) then		-- 86182 confirmed (normal10)
 		warnSlicingGale:Show()
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(86367, 93135, 93136, 93137) then
+	if args:IsSpellID(84644, 93135, 93136, 93137) then		-- 84644 confirmed (normal10)
 		warnSleetStorm:Show()
 		timerSleetStorm:Start()
-	elseif args:IsSpellID(84651, 84638, 93119, 93118) or args:IsSpellID(93117) then
+	elseif args:IsSpellID(84638, 93119, 93118, 93117) then		-- 84638 confirmed (normal10)
 		warnZephyr:Show()
 		timerZephyr:Start()
+	elseif args:IsSpellID(84645) then
+		timerWindChill:Start()
+	elseif args:IsSpellID(86193) then
+		warnWindBlast:Show()
+		timerWindBlast:Start()
+	elseif args:IsSpellID(84643) then
+		warnHurricane:Show()
 	end
 end
