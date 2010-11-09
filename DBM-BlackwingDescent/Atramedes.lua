2e4734f64118cf053c7dab835ba384d98278c145
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-11-09 12:12:24 +0000

    #Blackwing Descent\Atramades: Added warnings and timers for Atramedes encounter. Sonic Breath warning + next timer. Searing Flames next timer. Airphase+Groundphase warning + next timers.

--- DBM-BlackwingDescent/Atramedes.lua
+++ DBM-BlackwingDescent/Atramedes.lua
@@ -8,4 +8,56 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_AURA_APPLIED",
+	"SPELL_CAST_SUCCESS",
+	"CHAT_MSG_MONSTER_YELL",
+	"UNIT_DIED"
 )
+
+local warnSonicBreath		= mod:NewSpellAnnounce(78075, 3)
+local warnAirphase		= mod:NewAnnounce("WarnAirphase", 3)
+local warnGroundphase		= mod:NewAnnounce("WarnGroundphase", 3)
+local warnShieldsLeft		= mod:NewAnnounce("WarnShieldsLeft", 3)
+
+local timerSonicBreath		= mod:NewNextTimer(43, 78075)
+local timerSearingFlame		= mod:NewNextTimer(50, 77840)
+local timerAirphase		= mod:NewTimer(90, "TimerAirphase")
+local timerGroundphase		= mod:NewTimer(35, "TimerGroundphase")
+
+local shieldsLeft = 10
+
+local groundphase = function()
+	timerAirphase:Start()
+	timerSonicBreath:Start(28)
+	timerSearingFlame:Start()
+end
+
+function mod:OnCombatStart(delay)
+	timerSonicBreath:Start(25-delay)
+	timerSearingFlame:Start(45-delay)
+	shieldsLeft = 10
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(78075) then
+		timerSonicBreath:Start()
+		warnSonicBreath:Show()
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg == L.Airphase then
+		warnAirphase:Show()
+		timerSonicBreath:Cancel()
+		timerSearingFlame:Cancel()
+		timerGroundphase:Start()
+		self:Schedule(35, groundphase())
+	end
+end
+
+function mod:UNIT_DIED(args)
+	if args.sourceName == L.AncientDwarvenShield then
+		shieldsLeft = shieldsLeft - 1
+		warnShieldsLeft:Show(shieldsLeft)
+	end
+end
