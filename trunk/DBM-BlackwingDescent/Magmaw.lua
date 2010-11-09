c416d08d92471a88e0ad1ca3007e3528fbdc413a
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-11-09 14:42:21 +0000

    #Blackwing Descent\Magmaw: Added some warnings for Magmaw encounter

--- trunk/DBM-BlackwingDescent/Magmaw.lua
+++ trunk/DBM-BlackwingDescent/Magmaw.lua
@@ -8,4 +8,23 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_AURA_APPLIED"
 )
+
+local warnLavaSpew	= mod:NewSpellAnnounce(77689, 3)
+local warnPillarFlame	= mod:NewSpellAnnounce(78006, 3)
+local warnMoltenTantrum	= mod:NewSpellAnnounce(78403, 4)
+
+local timerLavaSpew	= mod:NewCDTimer(25, 77689)
+
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(77689) then
+		warnLavaSpew:Show()
+		timerLavaSpew:Start()
+	elseif args:IsSpellID(78006) then
+		warnPillarFlame:Show()
+	elseif args:IsSpellID(78403) then
+		warnMoltenTantrum:Show()
+	end
+end
