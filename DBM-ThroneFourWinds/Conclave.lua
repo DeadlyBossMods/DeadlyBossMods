ed9a889799fb71a05d50dbbe53b6cf1ff9172a2f
Arta <Arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-11-17 18:08:39 +0000

    #Throne of Four Winds\Conclave of Wind:  Added and updated timers

--- DBM-ThroneFourWinds/Conclave.lua
+++ DBM-ThroneFourWinds/Conclave.lua
@@ -25,12 +25,20 @@ local warnHurricane		= mod:NewSpellAnnounce(84643, 3)
 
 local timerPermafrost		= mod:NewCastTimer(2.5, 86082)
 local timerSleetStorm		= mod:NewBuffActiveTimer(15, 86367)
-local timerWindChill		= mod:NewNextTimer(11, 84645)
+local timerSleetStormCD		= mod:NewCDTimer(112, 84638)
+local timerNurture		= mod:NewCDTimer(112, 85422)
+local timerWindChill		= mod:NewNextTimer(10.5, 84645)
 local timerZephyr		= mod:NewBuffActiveTimer(15, 84651)
 local timerSlicingGale		= mod:NewTargetTimer(30, 93058)
 local timerWindBlast		= mod:NewBuffActiveTimer(10, 86193)		-- Cooldown: 1st->2nd = 22sec || 2nd->3rd = 60sec || 3rd->4th = 60sec ?
+local timerWindBlastCD		= mod:NewCDTimer(60, 86193)
+local timerHurricane		= mod:NewCDTimer(112, 84643)
 
+-- Nurture, Sleet Storm and Hurricane are on the same CD and are used simultanously --> Might make 1 timer with the text "BIG SPECIAL ABBILITY!" ? :P
+
+local windBlastCounter
 function mod:OnCombatStart(delay)
+	windBlastCounter = 0
 end
 
 function mod:SPELL_AURA_APPLIED(args)
@@ -40,9 +48,7 @@ function mod:SPELL_AURA_APPLIED(args)
 end
 
 function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(85422) then
-		warnNurture:Show()
-	elseif args:IsSpellID(86205) then
+	if args:IsSpellID(86205) then
 		warnSoothingBreeze:Show()
 	elseif args:IsSpellID(96192) then
 		warnSummonTornados:Show()
@@ -52,18 +58,29 @@ function mod:SPELL_CAST_START(args)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(84644, 93135, 93136, 93137) then		-- 84644 confirmed (normal10)
+	if args:IsSpellID(85422) then
+		warnNurture:Show()
+		timerNurture:Start()
+	elseif args:IsSpellID(84644, 93135, 93136, 93137) then		-- 84644 confirmed (normal10)
 		warnSleetStorm:Show()
 		timerSleetStorm:Start()
+		timerSleetStormCD:Start()
 	elseif args:IsSpellID(84638, 93119, 93118, 93117) then		-- 84638 confirmed (normal10)
 		warnZephyr:Show()
 		timerZephyr:Start()
 	elseif args:IsSpellID(84645) then
 		timerWindChill:Start()
 	elseif args:IsSpellID(86193) then
+		windBlastCounter = windBlastCounter + 1
 		warnWindBlast:Show()
 		timerWindBlast:Start()
+		if windBlastCounter == 1 then
+			timerWindBlastCD:Start(82)
+		else
+			timerWindBlastCD:Start()
+		end
 	elseif args:IsSpellID(84643) then
 		warnHurricane:Show()
+		timerHurricane:Start()
 	end
 end
