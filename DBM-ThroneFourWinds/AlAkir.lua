e8e1eda909bfe911adee33ec545ccbb8c0a0e76c
Arta <Arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-11-17 18:16:36 +0000

    #Throne of Four Winds\Al'Akir:  Added Wind Burst timers+warning

--- DBM-ThroneFourWinds/AlAkir.lua
+++ DBM-ThroneFourWinds/AlAkir.lua
@@ -8,4 +8,21 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_CAST_START"
 )
+
+local warnWindBurst		= mod:NewSpellAnnounce(87770, 3)
+
+local timerWindBurst		= mod:NewCastTimer(5, 87770)
+local timerWindBurstCD		= mod:NewCDTimer(25, 87770)		-- need more logs for confirmation
+
+function mod:OnCombatStart(delay)
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(87770) then
+		warnWindBurst:Show()
+		timerWindBurst:Start()
+		timerWindBurstCD:Start()
+	end
+end
