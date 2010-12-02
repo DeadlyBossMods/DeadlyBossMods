cdbc82287a2c774ccb75701fe11709e2b04b194a
Azael <Azael@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-12-02 16:17:26 +0000

    Prelim Cataclysm zhtw update

---
+++ DBM-BastionTwilight/localization.tw.lua
@@ -0,0 +1,102 @@
+﻿if GetLocale() ~= "zhTW" then return end
+
+local L
+
+---------------------------
+--  Valiona & Theralion  --
+---------------------------
+L = DBM:GetModLocalization("ValionaTheralion")
+
+L:SetGeneralLocalization({
+	name =	"Valiona & Theralion"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+--------------------------
+--  Halfus Wyrmbreaker  --
+--------------------------
+L = DBM:GetModLocalization("HalfusWyrmbreaker")
+
+L:SetGeneralLocalization({
+	name =	"Halfus Wyrmbreaker"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+----------------------------------
+--  Twilight Ascendant Council  --
+----------------------------------
+L = DBM:GetModLocalization("AscendantCouncil")
+
+L:SetGeneralLocalization({
+	name =	"Twilight Ascendant Council"
+})
+
+L:SetWarningLocalization({
+	SpecWarnGrounded	= "Get Grounded buff",
+	SpecWarnSearingWinds	= "Get Searing Winds buff"
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+	Quake			= "The ground beneath you rumbles ominously....",
+	Thundershock		= "The surrounding air crackles with energy....",
+	Switch			= "We will handle them!",
+	Phase3			= "BEHOLD YOUR DOOM!",
+	Ignacious		= "Ignacious",
+	Feludius		= "Feludius",
+	Arion			= "Arion",
+	Terrastra		= "Terrastra",
+	Monstrosity		= "Elementium Monstrosity"
+})
+
+L:SetOptionLocalization({
+	SpecWarnGrounded	= "Show special warning when you are missing $spell:83581 buff\n(~10sec before cast)",
+	SpecWarnSearingWinds	= "Show special warning when you are missing $spell:83500 buff\n(~10sec before cast)"
+})
+
+----------------
+--  Cho'gall  --
+----------------
+L = DBM:GetModLocalization("Chogall")
+
+L:SetGeneralLocalization({
+	name =	"Cho'gall"
+})
+
+L:SetWarningLocalization({
+	WarnPhase2Soon	= "Phase 2 soon"
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+L:SetOptionLocalization({
+	WarnPhase2Soon	= "Show pre-warning for Phase 2"
+})
