008bb4f238a15112ca04710da023685f640a4aec
Arta <Arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-12-10 09:43:57 +0000

    # Party-Cataclysm\Foe Reaper 9000: Added Harvest/Overdrive/"Enrage" warnings/timers

--- DBM-Party-Cataclysm/Deadmines/FoeReaper.lua
+++ DBM-Party-Cataclysm/Deadmines/FoeReaper.lua
@@ -8,10 +8,27 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_AURA_APPLIED",
 	"SPELL_CAST_SUCCESS"
 )
 
+local warnOverdrive		= mod:NewSpellAnnounce(88481, 3)
+local warnHarvest		= mod:NewSpellAnnounce(88495, 3)
+local warnEnrage		= mod:NewSpellAnnounce(91720, 4)
 local warnSpiritStrike		= mod:NewSpellAnnounce(59304, 3)
+
+local timerOverdrive		= mod:NewBuffActiveTimer(10, 88481)
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(88481) then
+		warnOverdrive:Show()
+		timerOVerdrive:Start()
+	elseif args:IsSpellID(88495) then
+		warnHarvest:Show()
+	elseif args:IsSpellID(91720) then
+		warnEnrage:Show()
+	end
+end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(59304) and mod:IsInCombat() then
