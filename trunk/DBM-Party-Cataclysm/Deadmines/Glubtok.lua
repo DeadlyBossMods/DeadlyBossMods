44680ae6be54ecb3b6a76960922a76975bbc7642
Arta <Arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-12-10 09:34:42 +0000

    # Party-Cataclysm\Glubtok: Added Fists of Flame/Frost and Arcane Power warning/timers

--- trunk/DBM-Party-Cataclysm/Deadmines/Glubtok.lua
+++ trunk/DBM-Party-Cataclysm/Deadmines/Glubtok.lua
@@ -8,10 +8,29 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_AURA_APPLIED",
 	"SPELL_CAST_SUCCESS"
 )
 
+local warnFistsFlame		= mod:NewSpellAnnounce(87859, 3)
+local warnFistsFrost		= mod:NewSpellAnnounce(87861, 3)
+local warnArcanePower		= mod:NewSpellAnnounce(88009, 3)
 local warnSpiritStrike		= mod:NewSpellAnnounce(59304, 3)
+
+local timerFistsFlame		= mod:NewBuffActiveTimer(10, 87859)
+local timerFistsFrost		= mod:NewBuffActiveTimer(10, 87861)
+
+function mod:SPELL_AURA_APPLIED
+	if args:IsSpellID(87859) and args.destName == args.srcName then
+		warnFistsFlame:Show()
+		timerFistsFlame:Start()
+	elseif args:IsSpellID(87861) and args.destName == args.srcName then
+		warnFistsFrost:Show()
+		timerFistsFrost:Start()
+	elseif args:IsSpellID(88009) then
+		warnArcanePower:Show()
+	end
+end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(59304) and mod:IsInCombat() then
