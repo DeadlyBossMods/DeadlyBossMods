597d103592e659b8bad8b0bdcbb3c84d79baa309
Snamor <Snamor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-12-15 11:51:55 +0000

    esES update

--- trunk/DBM-Party-Cataclysm/localization.es.lua
+++ trunk/DBM-Party-Cataclysm/localization.es.lua
@@ -414,6 +414,10 @@ L:SetGeneralLocalization({
 	name = "Altarius"
 })
 
+L:SetOptionLocalization({
+	BreathIcon	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(88308)
+})
+
 -----------
 -- Asaad --
 -----------
