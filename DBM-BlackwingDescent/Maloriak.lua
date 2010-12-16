5db33e3eaaf50b194c0d340fa3f4c48943f7dbfa
Adam <MysticalOS@users.noreply.github.com>
2010-12-16 07:02:21 +0000

    Some fixes and improvements to magmaw and maloriak. more needed. will need to convert yells to raid boss emotes next log i do to avoid issues with curse of tongues

--- DBM-BlackwingDescent/Maloriak.lua
+++ DBM-BlackwingDescent/Maloriak.lua
@@ -10,6 +10,7 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_REMOVED",
 	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
 	"CHAT_MSG_MONSTER_YELL"
@@ -17,7 +18,7 @@ mod:RegisterEvents(
 
 local warnPhase			= mod:NewAnnounce("WarnPhase", 4)
 local warnReleaseAdds		= mod:NewSpellAnnounce(77569, 3)
-local warnRemainingAdds		= mod:NewAnnounce("WarnRemainingAdds", 2, nil, false)
+--local warnRemainingAdds		= mod:NewAnnounce("WarnRemainingAdds", 2, nil, false)--this function would need interupt detection before it's useful
 local warnFlashFreeze		= mod:NewTargetAnnounce(77699, 3)
 local warnBitingChill		= mod:NewTargetAnnounce(77760, 3)
 local warnRemedy		= mod:NewSpellAnnounce(77912, 4)
@@ -26,7 +27,7 @@ local warnConsumingFlames	= mod:NewTargetAnnounce(77786, 3)
 local warnScorchingBlast	= mod:NewSpellAnnounce(77679, 3)
 local warnPhase2		= mod:NewPhaseAnnounce(2)
  
-local timerPhase		= mod:NewTimer(45, "TimerPhase")
+local timerPhase			= mod:NewTimer(45, "TimerPhase")
 local timerBitingChill		= mod:NewTargetTimer(10, 77760)
 local timerFlashFreeze		= mod:NewCDTimer(25, 77699)
 local timerArcaneStorm		= mod:NewBuffActiveTimer(6, 77896)
@@ -35,6 +36,7 @@ local timerScorchingBlast	= mod:NewCDTimer(17, 77679)
 
 local specWarnBitingChill	= mod:NewSpecialWarningYou(77760)
 local specWarnConsumingFlames	= mod:NewSpecialWarningYou(77786)
+local specWarnArcaneStorm	= mod:NewSpecialWarningInterupt(77896, false)
 
 local berserkTimer		= mod:NewBerserkTimer(360)
 
@@ -42,11 +44,11 @@ mod:AddBoolOption("FlashFreezeIcon")
 mod:AddBoolOption("BitingChillIcon")
 mod:AddBoolOption("ConsumingFlamesIcon")
 
-local adds = 18
+--local adds = 18
 
 function mod:OnCombatStart(delay)
 	berserkTimer:Start(-delay)
-	adds = 18
+--	adds = 18
 end
 
 function mod:SPELL_AURA_APPLIED(args)
@@ -64,10 +66,11 @@ function mod:SPELL_AURA_APPLIED(args)
 		if args:IsPlayer() then
 			specWarnBitingChill:Show()
 		end
-	elseif args:IsSpellID(77912, 92965, 92966, 92967) and args.destName == L.name then--Drycodes
+	elseif args:IsSpellID(77912, 92965, 92966, 92967) then--Drycodes
 		warnRemedy:Show()
 	elseif args:IsSpellID(77896) then
 		warnArcaneStorm:Show()
+		specWarnArcaneStorm:Show()
 		timerArcaneStorm:Start()
 	elseif args:IsSpellID(77786, 92971, 92972, 92973) then--Drycodes
 		warnConsumingFlames:Show(args.destName)
@@ -81,6 +84,22 @@ function mod:SPELL_AURA_APPLIED(args)
 	end
 end
 
+function mod:SPELL_AURA_REMOVED(args)
+	if args:IsSpellID(77699, 92978, 92979, 92980) then--Some spellids drycoded and not verified yet.
+		if self.Options.FlashFreezeIcon then
+			self:SetIcon(args.destName, 0)
+		end
+	elseif args:IsSpellID(77760, 92975, 92976, 92977) then--Drycodes
+		if self.Options.BitingChillIcon then
+			self:SetIcon(args.destName, 0)
+		end
+	elseif args:IsSpellID(77786, 92971, 92972, 92973) then--Drycodes
+		if self.Options.ConsumingFlamesIcon then
+			self:SetIcon(args.destName, 0)
+		end
+	end
+end
+
 function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(77679, 92968, 92969, 92970) then
 		warnScorchingBlast:Show()
@@ -90,26 +109,26 @@ end
 
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(77569) then
-		adds = adds - 3
+--		adds = adds - 3
 		warnReleaseAdds:Show()
-		warnRemainingAdds:Show(adds)
+--		warnRemainingAdds:Show(adds)--Maybe a way to monitor cast finished/succeeded unit event?
 	elseif args:IsSpellID(77991) then
 		warnPhase2:Show()
 	end
 end
 
-function mod:CHAT_MSG_MONSTER_YELL(msg)
-	if msg == L.YellRed then
+function mod:CHAT_MSG_MONSTER_YELL(msg)--Method is terrible, breaks from curse of tongues. Need to use raidboss emote instead. but need a new log that contains them
+	if msg == L.YellRed or msg:find(L.YellRed) then
 		warnPhase:Show(L.Red)
 		timerPhase:Start()
-	elseif msg == L.YellBlue then
+	elseif msg == L.YellBlue or msg:find(L.YellBlue) then
 		warnPhase:Show(L.Blue)
 		timerPhase:Start()
 		timerFlashFreeze:Start()
-	elseif msg == L.YellGreen then
+	elseif msg == L.YellGreen or msg:find(L.YellGreen) then
 		warnPhase:Show(L.Green)
 		timerPhase:Start()
-	elseif msg == L.YellDark then
+	elseif msg == L.YellDark or msg:find(L.YellDark) then
 		warnPhase:Show(L.Dark)
 		timerPhase:Start(100)		-- copied from BigWigs as I didnt have a timer yet
 	end
