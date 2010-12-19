0accb34385b893af6b4ddbcac2939733ff1aa5b2
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-12-19 22:14:47 +0000

    Redo walden with hopefully more functional and informative red/green warnings

--- trunk/DBM-Party-Cataclysm/ShadowFangKeep/Walden.lua
+++ trunk/DBM-Party-Cataclysm/ShadowFangKeep/Walden.lua
@@ -16,26 +16,29 @@ mod:RegisterEvents(
 local warnFrostMix	= mod:NewSpellAnnounce(93702, 3)
 local warnIceShards	= mod:NewSpellAnnounce(93527, 3)
 local warnPoisonMix	= mod:NewSpellAnnounce(93704, 4)
---local warnRedMix	= mod:NewSpellAnnounce(93704, 4)--don't know spellid yet
+local warnRedMix	= mod:NewSpellAnnounce(93562, 4)
 
-local specWarnCoagulant	= mod:NewSpecialWarningMove(93617)
---local specWarnRedMix	= mod:NewSpecialWarning("DoNOTMove")--Need a combat log i keep forgetting and wowhead doesn't have it catagorized.
+local specWarnCoagulant	= mod:NewSpecialWarning("specWarnCoagulant", nil, false)
+local specWarnRedMix	= mod:NewSpecialWarning("specWarnRedMix", nil, false)
+mod:AddBoolOption("RedLightGreenLight", true, "announce")
 
 local timerIceShards	= mod:NewBuffActiveTimer(5, 93527)
-
-local lastCoagulant = 0
+local timerGreenMix		= mod:NewBuffActiveTimer(16.5, 93704)--It's 15 second buff plus 1.5sec cast. using 16.5 to be safe.
+local timerRedMix		= mod:NewBuffActiveTimer(12, 93562)
 
 function mod:OnCombatStart(delay)
-	lastCoagulant = 0
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(93527) then
 		warnIceShards:Show()
 		timerIceShards:Start()
-	elseif args:IsSpellID(93617) and args:IsPlayer() and GetTime() - lastCoagulant > 4 then
-		specWarnCoagulant:Show()
-		lastCoagulant = GetTime()
+	elseif args:IsSpellID(93562) then--Red Light
+		warnRedMix:Show()
+		timerRedMix:Start()
+		if self.Options.RedLightGreenLight then
+			specWarnRedMix:Show()
+		end
 	end
 end
 
@@ -44,7 +47,11 @@ mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(93702) then
 		warnFrostMix:Show()
-	elseif args:IsSpellID(93704) then
+	elseif args:IsSpellID(93704) then--Green Light
 		warnPoisonMix:Show()
+		timerGreenMix:Start()
+		if self.Options.RedLightGreenLight then
+			specWarnCoagulant:Show()
+		end
 	end
 end
