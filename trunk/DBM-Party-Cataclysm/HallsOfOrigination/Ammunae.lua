d6e6ff77999445ecebd1f5049ec15ff190d4d3bf
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-12-21 09:48:56 +0000

    setrevision wasn't flagged on 35 cata files.

--- trunk/DBM-Party-Cataclysm/HallsOfOrigination/Ammunae.lua
+++ trunk/DBM-Party-Cataclysm/HallsOfOrigination/Ammunae.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Ammunae", "DBM-Party-Cataclysm", 4)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision:$"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(39731)
 mod:SetZone()
 
