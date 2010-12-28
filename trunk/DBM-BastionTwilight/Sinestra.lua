66dc63140eb8565e530f705b1ebf3a9eef10725f
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2010-12-28 22:11:18 +0000

    Sinestra creature ID

--- trunk/DBM-BastionTwilight/Sinestra.lua
+++ trunk/DBM-BastionTwilight/Sinestra.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod("Sinestra", "DBM-BastionTwilight", 1)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 4829 $"):sub(12, -3))
-mod:SetCreatureID()
+mod:SetCreatureID(45213)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
