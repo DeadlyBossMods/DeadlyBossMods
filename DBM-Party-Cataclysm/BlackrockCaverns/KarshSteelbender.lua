10f2213569109d371dbe200797ff3c7fa81c7e78
Adam <MysticalOS@users.noreply.github.com>
2010-12-31 10:39:18 +0000

    tuned anti spam tobe a little less strict on karsh

--- DBM-Party-Cataclysm/BlackrockCaverns/KarshSteelbender.lua
+++ DBM-Party-Cataclysm/BlackrockCaverns/KarshSteelbender.lua
@@ -29,8 +29,8 @@ function mod:SPELL_AURA_APPLIED(args)
 		warnObsidianArmor:Show()
 	elseif args:IsSpellID(75846, 93567) then
 		timerSuperheated:Cancel()--try to fix a problem with multiple timers spawning with diff args.
-		timerSuperheated:Start(args.amount or 1)
-		if GetTime() - lastSuperheated > 5 then
+		timerSuperheated:Start(17, args.amount or 1)
+		if GetTime() - lastSuperheated > 3 then
 			warnSuperheated:Show()
 			if args.amount and args.amount >= 5 then
 				specWarnSuperheated:Show(args.amount)
