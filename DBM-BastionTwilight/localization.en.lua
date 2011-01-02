ca4972198500b866c67b701b6925c24c196a1047
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-01-02 21:43:26 +0000

    double    L:SetMiscLocalization({ ... })  -> merged them to 1 :)

--- DBM-BastionTwilight/localization.en.lua
+++ DBM-BastionTwilight/localization.en.lua
@@ -15,11 +15,6 @@ L:SetWarningLocalization({
 L:SetTimerLocalization({
 })
 
-L:SetMiscLocalization({
-	BlackoutIcon	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(92878),
-	EngulfingIcon	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(86622)
-})
-
 L:SetOptionLocalization({
 	YellOnEngulfing			= "Yell on $spell:86622",
 	RangeFrame		= "Show range frame (10)"
@@ -27,7 +22,9 @@ L:SetOptionLocalization({
 
 L:SetMiscLocalization{
 	Trigger1				= "Theralion, I will engulf the hallway. Cover their escape!",--Terrible phase trigger, even transcriptor couldn't grab anything more usefull than this :(
-	YellEngulfing				= "Engulfing Magic on me!"
+	YellEngulfing				= "Engulfing Magic on me!",
+	BlackoutIcon	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(92878),
+	EngulfingIcon	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(86622)
 }
 
 --------------------------
