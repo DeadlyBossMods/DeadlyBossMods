a79cb5239df76e69c9f1252203d3cf4b91f04476
Snamor <romanscat@hotmail.com>
2011-01-11 23:27:07 +0000

    esES update

--- DBM-Party-Cataclysm/localization.es.lua
+++ DBM-Party-Cataclysm/localization.es.lua
@@ -473,7 +473,7 @@ L:SetOptionLocalization({
 })
 
 L:SetWarningLocalization({
-	SpecWarnStaticCling	= "¡SALTA!"	-- does $spell: work here ?
+	SpecWarnStaticCling	= "¡SALTA!"
 })
 
 ---------------------------
