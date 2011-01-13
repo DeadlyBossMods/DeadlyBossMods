7897748c1a0f016fe08b65b8b6ee098e88754de5
Snamor <Snamor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-01-13 13:17:28 +0000

    esES update

--- trunk/DBM-Core/DBT.lua
+++ trunk/DBM-Core/DBT.lua
@@ -13,7 +13,7 @@
 --    * zhCN: Diablohu				http://wow.gamespot.com.cn
 --    * ruRU: BootWin				bootwin@gmail.com
 --    * zhTW: Azael/kc10577			kc10577@hotmail.com
---    * esES: Interplay/1nn7erpLaY      http://www.1nn7erpLaY.com
+--    * esES: Snamor/1nn7erpLaY     romanscat@hotmail.com
 --    * (add your names here!)
 --
 -- Special thanks to:
