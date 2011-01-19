167240b6f84cd0a804c961976fbe1d8bd9168fcb
Adam <MysticalOS@users.noreply.github.com>
2011-01-19 06:49:22 +0000

    Adjust  vanessa achievement timer.

--- DBM-Party-Cataclysm/Deadmines/Vanessa.lua
+++ DBM-Party-Cataclysm/Deadmines/Vanessa.lua
@@ -9,7 +9,6 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
 	"SPELL_CAST_SUCCESS"
 )
 
@@ -44,7 +43,7 @@ end
 --"<42.6> [MONSTER_SAY] CHAT_MSG_MONSTER_SAY:Especially venoms that affect the mind.:Vanessa VanCleef:::Blacklist::0:0::0:494::0:false:false:", -- [20]
 --"<47.4> [RAID_BOSS_EMOTE] CHAT_MSG_RAID_BOSS_EMOTE:Vanessa injects you with the Nightmare Elixir!:Vanessa VanCleef:::Blacklist::0:0::0:495::0:false:false:", -- [25]
 --"<50.3> [CLEU] SPELL_AURA_REMOVED:0xF130C1150003F0F9:Vanessa VanCleef:2632:0x04000000035FAB24:Omegal:1297:92100:Noxious Concoction:8:DEBUFF:", -- [27]
-function mod:SPELL_AURA_REMOVED(args)
+function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(92100) then
 		timerGauntlet:Start()
 	end
