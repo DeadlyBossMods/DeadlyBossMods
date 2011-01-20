7fde1d413f38a70476c8c9032820663f91a25efc
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-01-20 16:24:44 +0000

    #Maloriak: Dark Sludge update to only show if you are affected by it

--- DBM-BlackwingDescent/Maloriak.lua
+++ DBM-BlackwingDescent/Maloriak.lua
@@ -137,7 +137,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		spamSlime = GetTime()
 		warnDebilitatingSlime:Show()
 		timerDebilitatingSlime:Start()
-	elseif args:IsSpellID(92987, 92988) and GetTime() - spamSludge >= 4 then
+	elseif args:IsSpellID(92987, 92988) and GetTime() - spamSludge >= 4 and args:IsPlayer() then
 		spamSludge = GetTime()
 		specWarnSludge:Show()
 	end
