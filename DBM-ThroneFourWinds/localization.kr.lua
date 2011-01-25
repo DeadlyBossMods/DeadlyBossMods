a053fb54e4c76bbdfa31c6ee940b099e937aee4f
bluenyx <everfinale@gmail.com>
2011-01-25 18:06:46 +0000

    koKR update

--- DBM-ThroneFourWinds/localization.kr.lua
+++ DBM-ThroneFourWinds/localization.kr.lua
@@ -43,16 +43,20 @@ L:SetGeneralLocalization({
 
 L:SetWarningLocalization({
 	WarnFeedback		= "%s : >%s< (%d)",		-- Feedback on >args.destName< (args.amount)
+	WarnAdd				= "Stormling add spawned"	
 })
 
 L:SetTimerLocalization({
-	TimerFeedback 		= "역순환 (%d)"
+	TimerFeedback 		= "역순환 (%d)",
+	TimerAddCD			= "Next add"	
 })
 
 L:SetOptionLocalization({
 	WarnFeedback		= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.spell:format(87904, GetSpellInfo(87904) or "알 수 없음"),
 	LightningRodIcon	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(89668),
-	TimerFeedback		= "$spell:87904 유지 타이머 보기"
+	TimerFeedback		= "$spell:87904 유지 타이머 보기",
+	WarnAdd				= "Show warning when Stormling add spawns",
+	TimerAddCD			= "Show timer for new add"
 })
 
 L:SetMiscLocalization({
