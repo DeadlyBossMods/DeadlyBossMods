45f3dcae2214c87bc08719e80163f412c23c647a
bluenyx <bluenyx@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-02-01 17:40:26 +0000

    koKR localization fix

--- trunk/DBM-PvP/localization.kr.lua
+++ trunk/DBM-PvP/localization.kr.lua
@@ -120,14 +120,14 @@ L:SetGeneralLocalization({
 })
 
 L:SetMiscLocalization({
-	BgStart60		= "1분 후 폭풍의 눈 전투가 시작됩니다.",
-	BgStart30 		= "30초 후 폭풍의 눈 전투가 시작됩니다.",
+	BgStart60		= "1분 후 전투가 시작됩니다!",
+	BgStart30 		= "30초 후 전투가 시작됩니다!",
 	ZoneName		= "폭풍의 눈",
 	ScoreExpr 		= "(%d+)/1600",
 	Alliance 		= "얼라이언스",
 	Horde 			= "호드",
 	WinBarText		= "%s 점령",
-	FlagReset 		= "깃발이 제자리로 돌아갔습니다.!",
+	FlagReset 		= "깃발이 다시 제자리로 돌아갔습니다.",
 	FlagTaken 		= "^(.+)|1이;가; 깃발을 차지했습니다!",
 	FlagCaptured 	= "(.+)|1이;가; 깃발을 차지했습니다!",
 	FlagDropped 	= "깃발을 떨어뜨렸습니다!"
