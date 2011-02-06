9ecb6c2cd8725a6be9d1789ec43c03909bcfefeb
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-02-06 01:21:58 +0000

    Fix a couple bugs reported by nbluewiz

--- trunk/DBM-Party-Cataclysm/Deadmines/Cookie.lua
+++ trunk/DBM-Party-Cataclysm/Deadmines/Cookie.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod("Cookie", "DBM-Party-Cataclysm", 2)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(4739)
+mod:SetCreatureID(47739)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
