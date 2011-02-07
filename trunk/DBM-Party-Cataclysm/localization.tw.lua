3b2b1387fd21dd18be7c26e1f1d5951914b05060
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-02-07 01:41:58 +0000

    Improved harvest with target scanning and special warnings

--- trunk/DBM-Party-Cataclysm/localization.tw.lua
+++ trunk/DBM-Party-Cataclysm/localization.tw.lua
@@ -98,6 +98,10 @@ L:SetGeneralLocalization({
 	name = "敵人收割者5000"
 })
 
+L:SetOptionLocalization{
+	HarvestIcon		= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(88495)
+}
+
 ----------------------
 -- Admiral Ripsnarl --
 ----------------------
