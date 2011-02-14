2e8b1fd26b3189536fff8660f167dc4c10d2c220
bluenyx <bluenyx@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-02-14 20:44:50 +0000

    koKR update

--- trunk/DBM-PvP/localization.kr.lua
+++ trunk/DBM-PvP/localization.kr.lua
@@ -18,7 +18,8 @@ L:SetTimerLocalization({
 L:SetOptionLocalization({
 	ColorByClass		= "스코어 화면의 유저 이름을 클래스 이름으로 보이기",
 	ShowInviteTimer		= "전장 입장 타이머 보이기",
-	AutoSpirit			= "자동 부활"
+	AutoSpirit			= "자동 부활",
+	HideBossEmoteFrame	= "레이드 보스 감정표현 프레임 숨기기"
 })
 
 L:SetMiscLocalization({
