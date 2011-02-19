193fb6b2b9f5a77cca50525398534817e986cab1
Adam <MysticalOS@users.noreply.github.com>
2011-02-19 08:54:41 +0000

    Experimental phase 2 pre warning for siamat. Going over a few logs i've determined he does not push when 3 adds dies, he pushes when third add dies (or rathor, exactly 5 seconds after 3rd add to have spawned starts casting lighting charge, regardless whether or not the other 2 adds even cast theirs yet). This hack scans GUID on spawn, when 3rd one spawns, saves GUID, then compares that GUID to GUID of servents who cast lighting charge, if that GUID matches, then we know the 3rd add that spawned is dying. Yay for solving the puzzles of encounter mechanics :)

--- DBM-Party-Cataclysm/LostCityOfTheTolvir/Siamat.lua
+++ DBM-Party-Cataclysm/LostCityOfTheTolvir/Siamat.lua
@@ -20,11 +20,23 @@ local warnDeflectingWinds	= mod:NewSpellAnnounce(84589, 3)
 local warnWailingWinds		= mod:NewSpellAnnounce(83066, 3)
 local warnAbsorbStorms		= mod:NewSpellAnnounce(83151, 2, false)
 local warnGatheredStorms	= mod:NewSpellAnnounce(84982, 3)
+local warnLightningCharge	= mod:NewCastAnnounce(91872, 3)
+
+local specWarnPhase2Soon	= mod:NewSpecialWarning("specWarnPhase2Soon", true)
 
 local timerThunderCrash		= mod:NewCastTimer(3, 84522)
 local timerWailingWinds		= mod:NewBuffActiveTimer(6, 83066)
 local timerAbsorbStorms		= mod:NewCDTimer(33, 83151)
 local timerGatheredStorms	= mod:NewBuffActiveTimer(25, 84982)
+local timerPhase2Start		= mod:NewTimer(5, "timerPhase2", 91872)
+
+local servantSpawn = 0
+local thirdServant = 0
+
+function mod:OnCombatStart(delay)
+	servantSpawn = 0
+	thirdServant = 0
+end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(84982) then
@@ -37,6 +49,12 @@ function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(84522, 90016) then
 		warnThunderCrash:Show()
 		timerThunderCrash:Start()
+	elseif args:IsSpellID(91872) then
+		warnLightningCharge:Show()
+		if args.sourceGUID = thirdServant then--Third add to have spawned is dying and casting Lightning Charge
+			specWarnPhase2Soon:Show()
+			timerPhase2Start:Start()--Phase 2 starts 5 seconds after 3rd add casts static charge regardless of whether or not other adds are dead.
+		end
 	end
 end
 
@@ -53,7 +71,11 @@ function mod:SPELL_CAST_SUCCESS(args)
 end
 
 function mod:SPELL_SUMMON(args)
-	if args:IsSpellID(84547, 90014) then
+	if args:IsSpellID(84547, 84553, 84554) or args:IsSpellID(90013, 90014) then
 		warnStaticShock:Show()
+		servantSpawn = servantSpawn + 1
+		if servantSpawn == 3 then--Third add spawned
+			thirdServant = args.sourceGUID
+		end
 	end
 end
