036fb4869ad80ffa53f92b405ae73e91347b9df3
Adam <MysticalOS@users.noreply.github.com>
2011-03-03 22:31:02 +0000

    add 10 man spellid for poisoncloud

--- DBM-BlackwingDescent/DarkIronGolemCouncil.lua
+++ DBM-BlackwingDescent/DarkIronGolemCouncil.lua
@@ -223,7 +223,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		end
 	elseif args:IsSpellID(80011, 91504, 91505, 91506) then
 		timerSoaked:Start(args.destName)
-	elseif args:IsSpellID(91473) and args:IsPlayer() and GetTime() - cloudSpam > 4 then
+	elseif args:IsSpellID(91472, 91473) and args:IsPlayer() and GetTime() - cloudSpam > 4 then
 		specWarnChemicalCloud:Show()
 		cloudSpam = GetTime()
 	elseif args:IsSpellID(79629, 91555, 91556, 91557) and args:GetDestCreatureID() == 42166 then--Check if Generator buff is gained by Arcanotron
