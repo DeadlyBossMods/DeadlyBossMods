935c948323c20b848fee9a5e93213a3cd9908ea8
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-03-08 23:50:10 +0000

    Sinestra: Added some stuff (experimental, but useful)

--- DBM-BastionTwilight/Sinestra.lua
+++ DBM-BastionTwilight/Sinestra.lua
@@ -11,12 +11,13 @@ mod:RegisterEvents(
 	"SPELL_CAST_START",
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_REMOVED",
+	"SPELL_DISPEL",
 	"CHAT_MSG_MONSTER_YELL",
 	"UNIT_DIED"
 )
 
 local warnBreath		= mod:NewSpellAnnounce(92944, 3)
-local warnSlicer		= mod:NewSpellAnnounce(92954, 4)
+local warnSlicerSoon	= mod:NewAnnounce("WarnSlicerSoon", 2, 92954) -- yeah, this stuff can be very spammy, but in Sinestra, Twilight Slicer is very very very important, so on it by default.
 local warnWrack			= mod:NewTargetAnnounce(92955, 3)
 local warnExtinction	= mod:NewSpellAnnounce(86227, 4)
 local warnIndomitable	= mod:NewSpellAnnounce(92946, 3)
@@ -24,6 +25,10 @@ local warnEggWeaken		= mod:NewAnnounce("WarnEggWeaken", 4, 61357)
 local warnDragon		= mod:NewAnnounce("WarnDragon", 3, 69002)
 local warnPhase2		= mod:NewPhaseAnnounce(2)
 local warnPhase3		= mod:NewPhaseAnnounce(3)
+
+local specWarnSlicer	= mod:NewSpecialWarning("SpecWarnSlicer")
+local specWarnDispel	= mod:NewSpecialWarning("SpecWarnDispel", mod:IsHealer()) -- this can be personal stuff, but Warck dispel also important In sinestra. adjust appropriately.
+local specWarnBreath	= mod:NewSpecialWarningSpell(92944, false)
 
 local timerBreathCD		= mod:NewCDTimer(24, 92944)
 local timerSlicer		= mod:NewNextTimer(28, 92954)
@@ -34,20 +39,40 @@ local timerDragon		= mod:NewTimer(50, "TimerDragon", 69002)
 
 local eggDown = 0
 local eggSpam = 0
+local wrackTargets = {}
+local wrackCount = 0
+local lastDispeled = 0
 
 function mod:SlicerRepeat()
-	warnSlicer:Show()
+	specWarnSlicer:Show()
 	timerSlicer:Start()
+	if self.Options.WarnSlicerSoon then
+		warnSlicerSoon:Schedule(23, 5)
+		warnSlicerSoon:Schedule(24, 4)
+		warnSlicerSoon:Schedule(25, 3)
+		warnSlicerSoon:Schedule(26, 2)
+		warnSlicerSoon:Schedule(27, 1)
+	end
 	self:ScheduleMethod(28, "SlicerRepeat")
 end
 
 function mod:OnCombatStart(delay)
 	eggDown = 0
 	eggSpam = 0
+	table.wipe(wrackTargets)
+	wrackCount = 0
+	lastDispeled = 0
 	timerDragon:Start(16-delay)
-	timerBreathCD:Start(21-delay)
-	timerSlicer:Start(30-delay)
-	self:ScheduleMethod(30-delay, "SlicerRepeat")
+	timerBreathCD:Start(23-delay)
+	timerSlicer:Start(29-delay)
+	if self.Options.WarnSlicerSoon then
+		warnSlicerSoon:Schedule(24, 5)
+		warnSlicerSoon:Schedule(25, 4)
+		warnSlicerSoon:Schedule(26, 3)
+		warnSlicerSoon:Schedule(27, 2)
+		warnSlicerSoon:Schedule(28, 1)
+	end
+	self:ScheduleMethod(29-delay, "SlicerRepeat")
 end
 
 function mod:SPELL_CAST_START(args)
@@ -66,12 +91,32 @@ function mod:SPELL_AURA_APPLIED(args)
 	elseif args:IsSpellID(92955) then
 		warnWrack:Show(args.destName)
 		timerWrack:Start()
+		table.wipe(wrackTargets)
+		wrackCount = 1
+		wrackTargets[args.destName] = true
+		specWarnDispel:Schedule(18, 18)
+		self:Schedule(60, function()
+			specWarnDispel:Cancel()
+		end)
+	elseif args:IsSpellID(92956) then -- jumped warcks
+		if not wrackTargets[args.destName] then
+			wrackTargets[args.destName] = true
+			wrackCount = WrackCount + 1
+		end
+		if wrackCount > 3 and GetTime() - lastDispeled < 5 then
+			specWarnDispel:Schedule(12, 12)
+		elseif wrackCount > 1 and GetTime() - lastDispeled < 5 then
+			specWarnDispel:Schedule(14, 14)
+		end
 	elseif args:IsSpellID(87299) then
 		eggDown = 0
 		warnPhase2:Show()
 		timerDragon:Cancel()
 		timerBreathCD:Cancel()
 		timerSlicer:Cancel()
+		if self.Options.WarnSlicerSoon then
+			warnSlicerSoon:Cancel()
+		end
 		self:UnscheduleMethod("SlicerRepeat")
 	end
 end
@@ -80,6 +125,17 @@ function mod:SPELL_AURA_REMOVED(args)
 	if args:IsSpellID(87654) and GetTime() - eggSpam >= 3 then
 		eggSpam = GetTime()
 		warnEggWeaken:Show()
+	elseif args:IsSpellID(92955, 92956) then
+		if wrackTargets[args.destName] then
+			wrackTargets[args.destName] = nil
+			wrackCount = WrackCount - 1
+		end
+	end
+end
+
+function mod:SPELL_DISPEL(args)
+	if args:IsSpellID(92955, 92956) and GetTime() - lastDispeled > 5 then
+		lastDispeled = GetTime()
 	end
 end
 
@@ -99,9 +155,16 @@ function mod:UNIT_DIED(args)
 		if eggDown >= 2 then
 			warnPhase3:Show()
 			timerBreathCD:Start()
-			timerSlicer:Start()
+			timerSlicer:Start(29)
 			timerDragon:Start()
-			self:ScheduleMethod(28, "SlicerRepeat")
+			if self.Options.WarnSlicerSoon then
+				warnSlicerSoon:Schedule(24, 5)
+				warnSlicerSoon:Schedule(25, 4)
+				warnSlicerSoon:Schedule(26, 3)
+				warnSlicerSoon:Schedule(27, 2)
+				warnSlicerSoon:Schedule(28, 1)
+			end
+			self:ScheduleMethod(29, "SlicerRepeat")
 		end
 	end
 end
