570f614598c3e7332449f345497a87ccf130c304
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-03-09 17:13:42 +0000

    Sinestra: Added revision keywords

--- DBM-BastionTwilight/Sinestra.lua
+++ DBM-BastionTwilight/Sinestra.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Sinestra", "DBM-BastionTwilight")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 4829 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(45213)
 mod:SetZone()
 
