4935937e0827954e11a095636d8af4ebfbe9e1d6
Adam <MysticalOS@users.noreply.github.com>
2011-03-11 05:11:35 +0000

    because of changes in icon code on chimereon and omnotron, i am gonna bump the version in core to make the alpha disable last release icons to prevent conflicts, this alpha is gonna need thorough testing and that cannot be done on chimereon very well if old versions are setting icons. I want to know what the new icon functions are working properly for chimereon.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -43,7 +43,7 @@
 -------------------------------
 DBM = {
 	Revision = ("$Revision$"):sub(12, -3),
-	Version = "4.74",
+	Version = "4.75",
 	DisplayVersion = "4.75 alpha", -- the string that is shown as version
 	ReleaseRevision = 5279 -- the revision of the latest stable version that is available (for /dbm ver2)
 }
