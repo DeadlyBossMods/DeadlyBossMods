0a0e3fb566b506437fd199f2857466b7b4b23834
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-04-03 20:55:13 +0000

    While working on generics, lets improve the grammer and wording a little on them so they more clearly express what option means, that they are for when YOU are affected by debuffs instead of just saying "yell/play sound on debuffname" without mentioning specificly that it means ON YOU.

--- trunk/DBM-Core/localization.en.lua
+++ trunk/DBM-Core/localization.en.lua
@@ -185,8 +185,8 @@ DBM_CORE_AUTO_SPEC_WARN_TEXTS = {
 
 
 DBM_CORE_AUTO_ICONS_OPTION_TEXT		= "Set icons on $spell:%d targets"
-DBM_CORE_AUTO_SOUND_OPTION_TEXT		= "Play sound on $spell:%d"
-DBM_CORE_AUTO_YELL_OPTION_TEXT		= "Yell on $spell:%d"
+DBM_CORE_AUTO_SOUND_OPTION_TEXT		= "Play sound when you are affected by $spell:%d"
+DBM_CORE_AUTO_YELL_OPTION_TEXT		= "Yell when you are affected by $spell:%d"
 DBM_CORE_AUTO_YELL_ANNOUNCE_TEXT	= "%s on me!"
 
 
