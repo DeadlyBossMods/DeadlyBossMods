99f5e326462033039f5e58baa50704dd0d290424
Adam <MysticalOS@users.noreply.github.com>
2011-04-09 19:49:27 +0000

    Couple more tweaks to last. more or less, i want the pvp sound to be something where YOU need to move out/in for something. so something like valiona's breath, is an aoe thing, but it's actually something you need to move for, vs something like electrocute, or searing flames, which you need to be aware of but you don't have to up and start running somewhere immediately. Some abilities though i'm conflicted on like armageddon, aegis shield, where you don't move, but you still have to react (ie switch targets). especially since those abilities can overlap with some other events like say waterlogged, or pillar of flame. So for now they use custom sound even though they are kind of right in middle of what i think defines the difference between two types of special warnings.

--- DBM-BlackwingDescent/Chimaeron.lua
+++ DBM-BlackwingDescent/Chimaeron.lua
@@ -27,8 +27,8 @@ local warnFeud				= mod:NewSpellAnnounce(88872, 3)
 local warnPhase2Soon		= mod:NewPrePhaseAnnounce(2, 3)
 local warnPhase2			= mod:NewPhaseAnnounce(2)
 
-local specWarnFailure		= mod:NewSpecialWarningSpell(88853, nil, nil, nil, "Sound\\Creature\\AlgalonTheObserver\\UR_Algalon_BHole01.wav")
-local specWarnMassacre		= mod:NewSpecialWarningSpell(82848, mod:IsHealer())
+local specWarnFailure		= mod:NewSpecialWarningSpell(88853)
+local specWarnMassacre		= mod:NewSpecialWarningSpell(82848, mod:IsHealer(), nil, nil, "Sound\\Creature\\AlgalonTheObserver\\UR_Algalon_BHole01.wav")
 local specWarnDoubleAttack	= mod:NewSpecialWarningSpell(88826, mod:IsTank())
 
 local timerBreak			= mod:NewTargetTimer(60, 82881)
