1137d21a94d62dd893849f589ab8e7716ce5d892
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-04-09 10:59:16 +0000

    Forgot a line :(

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -3095,6 +3095,7 @@ do
 						  self.runSound == 2 and "Sound\\Creature\\AlgalonTheObserver\\UR_Algalon_BHole01.wav" or
 						  self.runSound == 3 and "Sound\\Doodad\\BellTollNightElf.wav" or
 						  self.runSound
+print(playSound)
 				if DBM.Options.UseMasterVolume then
 					PlaySoundFile(playSound, "Master")
 				else
@@ -3142,6 +3143,7 @@ do
 				option = optionName or text,
 				mod = self,
 				sound = not noSound,
+				runSound = runSound or DBM.Options.SpecialWarningSound,
 			},
 			mt
 		)
