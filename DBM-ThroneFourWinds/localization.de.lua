53edaeea465903f092c88ff10de1e4f0a2f8f9a6
Samira <Samira@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-04-10 08:34:50 +0000

    finished deDE translation for Throne of the 4 Winds (Al'akir)

--- DBM-ThroneFourWinds/localization.de.lua
+++ DBM-ThroneFourWinds/localization.de.lua
@@ -12,7 +12,8 @@ L:SetGeneralLocalization({
 
 L:SetWarningLocalization({
 	warnSpecial			= "Hurrikan/Zephyr/Graupelsturm aktiv",
-	specWarnSpecial		= "Spezialfähigkeiten aktiv!"
+	specWarnSpecial		= "Spezialfähigkeiten aktiv!",
+	warnSpecialSoon		= "Spezialfähigkeiten in 10 Sekunden!"
 })
 
 L:SetTimerLocalization({
@@ -20,15 +21,21 @@ L:SetTimerLocalization({
 	timerSpecialActive		= "Spezialfähigkeiten aktiv"
 })
 
-L:SetMiscLocalization({
-	gatherstrength			= "%s beinnt von den verbliebenen Windlords Stärke zu beziehen!" --yes the typo is from the logfiles (4.06a) "<356.9> CHAT_MSG_RAID_BOSS_EMOTE#%s beinnt von den verbliebenen Windlords Stärke zu beziehen!#Rohash#####0#0##0#1616##0#false#false", -- [6]
-})
-
 L:SetOptionLocalization({
 	warnSpecial			= "Zeige Warnung, wenn Hurrikan/Zephyr/Graupelsturm gewirkt werden",
 	specWarnSpecial		= "Zeige Spezialwarnung, wenn Spezialfähigkeiten gewirkt werden",
 	timerSpecial		= "Zeige Timer für Spezialfähigkeiten CD",
-	timerSpecialActive	= "Zeige Timer für die Dauer der Spezialfähigkeiten"
+	timerSpecialActive	= "Zeige Timer für die Dauer der Spezialfähigkeiten",
+	warnSpecialSoon		= "Zeige Vorwarnung 10 Sekunden vor den Spezialfähigkeiten",
+	OnlyWarnforMyTarget	= "Zeige Warnungen und Timer nur für das aktuelle Zeil und Fokusziel\n(Versteckt den Rest. Dies beinhaltet den PULL!)"
+})
+
+L:SetMiscLocalization({
+	gatherstrength			= "%s beinnt von den verbliebenen Windlords Stärke zu beziehen!", --yes the typo is from the logfiles (4.06a) "<356.9> CHAT_MSG_RAID_BOSS_EMOTE#%s beinnt von den verbliebenen Windlords Stärke zu beziehen!#Rohash#####0#0##0#1616##0#false#false", -- [6]
+	Anshal			= "Anshal",
+	Nezir			= "Nezir",
+	Rohash			= "Rohash"
+
 })
 
 ---------------
@@ -41,12 +48,11 @@ L:SetGeneralLocalization({
 })
 
 L:SetWarningLocalization({
-	WarnFeedback	= "%s auf >%s< (%d)",
-	WarnAdd			= "Sturmling gespawned" --check name
+	WarnAdd			= "Sturmling gespawned"
 })
 
 L:SetTimerLocalization({
-	TimerFeedback 	= "Rückkopplung (%d)", --translate
+	TimerFeedback 	= "Rückkopplung (%d)",
 	TimerAddCD		= "Nächstes Add"
 })
 
@@ -54,14 +60,13 @@ L:SetOptionLocalization({
 	WarnFeedback	= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.spell:format(87904, GetSpellInfo(87904) or "unknown"),
 	LightningRodIcon= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(89668),
 	TimerFeedback	= "Zeige Timer für die Dauer von $spell:87904",
-	WarnAdd			= "Zeige Warnung, wenn ein Sturmling spawned", -- check name
+	WarnAdd			= "Zeige Warnung, wenn ein Sturmling spawned", 
 	TimerAddCD		= "Zeige Timer für neues Add"
 
 })
 
 L:SetMiscLocalization({
-	summonSquall	=	"Storms! I summon you to my side!", --translate
---	phase2		=	"Your futile persistance angers me!",--Not used, Acid rain is, but just in case there is reliability issues with that, localize this anyways. -- translate
-	phase3		=	"Enough! I will no longer be contained!" --translate
+	summonSquall	=	"Stürme! Ich rufe euch an meine Seite!",
+	phase3		=	"Genug! Ich werde mich nicht länger zurückhalten!"
 })
 
