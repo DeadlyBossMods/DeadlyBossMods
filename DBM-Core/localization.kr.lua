ab622bab9d5e7eed7ceb2563d0a4c746e034fe20
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-04-17 14:22:04 +0000

    koKR update

--- DBM-Core/localization.kr.lua
+++ DBM-Core/localization.kr.lua
@@ -1,6 +1,6 @@
 if (GetLocale() == "koKR") then
 
-DBM_CORE_NEED_SUPPORT				= "안녕하세요. DBM Team입니다. 만약 프로그래밍 또는 언어에 대해 잘 아신다면, DBM Team에 어떤 도움이든 주실 수 있습니다. 건의/수정 요청 사항이 있다면 wow.somegate.com 또는 흑묘서희@에이그윈(Twitter: @Nyx_Khang) 또는 엘룬@영원이란(everfinale@gmail.com | 인벤:영원하늘)에게 연락바랍니다. DBM Team에 참가하고 싶다면 www.deadlybossmods.com 또는 tandanu@deadlybossmods.com 또는 nitram@deadlybossmods.com에 연락바랍니다. 물론, 영어를 매우 잘하셔야겠죠?"
+DBM_CORE_NEED_SUPPORT				= "안녕하세요. DBM Team입니다. 만약 프로그래밍 또는 언어에 대해 잘 아신다면, DBM Team에 어떤 도움이든 주실 수 있습니다. 건의/수정 요청 사항이 있다면 wow.somegate.com 또는 흑묘서희@에이그윈(Twitter: @Nyx_Khang) 또는 다시날아@아즈샤라(everfinale@gmail.com | 인벤:영원하늘)에게 연락바랍니다. DBM Team에 참가하고 싶다면 www.deadlybossmods.com 또는 tandanu@deadlybossmods.com 또는 nitram@deadlybossmods.com에 연락바랍니다. 물론, 영어를 매우 잘하셔야겠죠?"
 DBM_HOW_TO_USE_MOD					= "DBM 사용을 환영합니다. 대화창에서 /dbm help를 입력하면 사용 가능한 명령어 리스트를 볼 수 있습니다. 설정을 위해서는 /dbm을 입력하고 나타나는 창에서 보스 모드를 수동으로 불러온 후에 세부 보스에 대한 설정을 할 수 있습니다. DBM은 처음 실행시 당신의 특성을 검색한후 특성에 맞는 적절한 경고를 선택하지만, 추가 경고를 원하신다면 옵션에서 설정하시면 됩니다."
 
 DBM_CORE_LOAD_MOD_ERROR				= "%s 모드 로딩중 에러 발생: %s"
