39a526414da3f7d4050715e0bdcca8bb11f54f5b
Adam <MysticalOS@users.noreply.github.com>
2011-04-22 06:25:50 +0000

    Lighting discharge timer wasn't worth a crap, just wasted cpu needlessly for a timer that was wrong half the time. scrapped whole thing. However, since i got accustomed to the first discharge lining up with nef landing perfectly, i decided to convert it to just that, a nef landing timer (30 seconds from pull( Also while at it, scrapped add spawning warnings, registering SPELL_SUMMON for an event that happens on pull and not at all rest of fight is pointless waste too. everyone knows when these spawn, we don't need a spammy warning for it. Lastly, blast nova warning now only warns for YOUR platform, just like special warning. Reduce more needless spam.

--- DBM-BlackwingDescent/Nefarian.lua
+++ DBM-BlackwingDescent/Nefarian.lua
@@ -19,9 +19,7 @@ mod:RegisterEvents(
 	"SPELL_AURA_REMOVED",
 	"SPELL_CAST_SUCCESS",
 	"SPELL_DAMAGE",
-	"SPELL_MISSED",
 	"SWING_DAMAGE",
-	"SPELL_SUMMON",
 	"CHAT_MSG_MONSTER_YELL",
 	"CHAT_MSG_RAID_BOSS_EMOTE",
 	"UNIT_DIED"
@@ -33,7 +31,6 @@ local warnOnyShadowflameBreath	= mod:NewAnnounce("OnyBreath", 3, 94124, mod:IsTa
 local warnNefShadowflameBreath	= mod:NewAnnounce("NefBreath", 3, 94124, mod:IsTank())
 local warnBlastNova				= mod:NewSpellAnnounce(80734, 3, nil, false)--Can be spammy so now off by default.
 local warnShadowBlaze			= mod:NewSpellAnnounce(94085, 4)--May be quirky
-local warnHailBones				= mod:NewSpellAnnounce(94104, 3, nil, false)--spams a lot (every ~2sec a new one spawns, 5 adds 10 man 12 adds 25 man)
 local warnCinder				= mod:NewTargetAnnounce(79339, 4)
 local warnPhase2				= mod:NewPhaseAnnounce(2)
 local warnPhase3				= mod:NewPhaseAnnounce(3)
@@ -50,7 +47,7 @@ local yellCinder				= mod:NewYell(79339)
 
 local timerBlastNova			= mod:NewCastTimer(1.5, 80734)
 local timerElectrocute			= mod:NewCastTimer(5, 81198)
-local timerLightningDischarge	= mod:NewCDTimer(20, 77942)--92456, every 20-25 seconds. Need to emphesise this is a CD timer not a next timer. It will also only trigger if it hits something (including pets).
+local timerNefLanding			= mod:NewTimer(30, "timerNefLanding", 78620)
 local timerShadowflameBarrage	= mod:NewBuffActiveTimer(150, 78621)
 local timerShadowBlazeCD		= mod:NewCDTimer(10, 94085)
 local timerOnySwipeCD			= mod:NewTimer(10, "OnySwipeTimer", 77827)--10-20 second cd (18 being the most consistent)
@@ -65,10 +62,10 @@ local berserkTimer				= mod:NewBerserkTimer(630)
 
 local soundCinder				= mod:NewSound(79339)
 
-mod:AddBoolOption("RangeFrame")
+mod:AddBoolOption("RangeFrame", true)
 mod:AddBoolOption("SetIconOnCinder", true)
 mod:AddBoolOption("HealthFrame", true)
-mod:AddBoolOption("InfoFrame")
+mod:AddBoolOption("InfoFrame", true)
 mod:AddBoolOption("SetWater", false)
 mod:AddBoolOption("TankArrow", false)--May be prone to some issues if you have 2 kiters, or unpicked up adds, but it's off by default so hopefully feature is used by smart people.
 
@@ -118,7 +115,7 @@ function mod:OnCombatStart(delay)
 	playerDebuffs = 0
 	table.wipe(cinderTargets)
 	table.wipe(dominionTargets)
-	timerLightningDischarge:Start(30-delay)--First one seems pretty precise (it happens at same time nef lands.)
+	timerNefLanding:Start(-delay)
 	if mod:IsDifficulty("heroic10", "heroic25") then
 		berserkTimer:Start(-delay)
 		timerDominionCD:Start(50-delay)
@@ -158,8 +155,8 @@ function mod:SPELL_CAST_START(args)
 		if not DBM.BossHealth:HasBoss(args.sourceGUID) then
 			DBM.BossHealth:AddBoss(args.sourceGUID, args.sourceName)
 		end
-		warnBlastNova:Show()
 		if args.sourceGUID == UnitGUID("target") then--Only show warning/timer for your own target.
+			warnBlastNova:Show()
 			specWarnBlastsNova:Show()
 			timerBlastNova:Start()
 		end
@@ -238,22 +235,12 @@ function mod:SPELL_DAMAGE(args)
 	if args:IsPlayer() and args:IsSpellID(81007, 94085, 94086, 94087) and GetTime() - spamShadowblaze > 5 then--Drycodes
 		specWarnShadowblaze:Show()
 		spamShadowblaze = GetTime()
-	elseif (args:IsSpellID(77939, 77942, 77943, 77944) or args:IsSpellID(94107, 94108, 94109, 94110) or args:IsSpellID(94111, 94112, 94113, 94114) or args:IsSpellID(94115, 94116, 94117, 94118)) and GetTime() - spamLightningDischarge > 15 then--Nost most ideal solution but difficult with an ability that has no cast trigger to speak of.
-		timerLightningDischarge:Start()
-		spamLightningDischarge = GetTime()
 	elseif args:GetDestCreatureID() == 41918 and args:IsSrcTypePlayer() and not args:IsSpellID(50288) and self:IsInCombat() then--Any spell damage except for starfall
 		if args.sourceName ~= UnitName("player") then
 			if self.Options.TankArrow then
 				DBM.Arrow:ShowRunTo(args.sourceName, 0, 0)
 			end
 		end
-	end
-end
-
-function mod:SPELL_MISSED(args)
-	if args:IsSpellID(77833, 77838, 77919) and GetTime() - spamLightningDischarge > 15 then--Same as above only these are unique spellids for misses/immunities.
-		timerLightningDischarge:Start()
-		spamLightningDischarge = GetTime()
 	end
 end
 
@@ -267,16 +254,9 @@ function mod:SWING_DAMAGE(args)
 	end
 end
 
-function mod:SPELL_SUMMON(args)
-	if args:IsSpellID(78684, 94104, 94105, 94106) then
-		warnHailBones:Show()
-	end
-end
-
 function mod:CHAT_MSG_MONSTER_YELL(msg)
 	if msg == L.YellPhase2 or msg:find(L.YellPhase2) then
 		warnPhase2:Show()
-		timerLightningDischarge:Cancel()
 		timerOnySwipeCD:Cancel()
 		timerNefSwipeCD:Cancel()
 		timerOnyBreathCD:Cancel()
