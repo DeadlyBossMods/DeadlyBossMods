4c587cd38ac1481166cc17ca86a0176819533d5e
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-04-29 18:28:38 +0000

    small updates to ZA mods

--- trunk/DBM-Party-Cataclysm/ZulAman/Janalai5.lua
+++ trunk/DBM-Party-Cataclysm/ZulAman/Janalai5.lua
@@ -10,15 +10,18 @@ mod:SetUsedIcons(8)
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_AURA_APPLIED",
 	"SPELL_CAST_START",
 	"CHAT_MSG_MONSTER_YELL"
 )
 
+local warnFlameCast		= mod:NewSpellAnnounce(97855, 2)
 local warnFlame			= mod:NewTargetAnnounce(97855, 3)
 local warnAdds			= mod:NewSpellAnnounce(43962, 4)
 local warnAddsSoon		= mod:NewSoonAnnounce(43962, 3)
 local warnFireBomb		= mod:NewSpellAnnounce(42630, 3)
 
+local specWarnFlameBreath	= mod:NewSpecialWarningMove(97855)
 local specWarnAdds		= mod:NewSpecialWarningSpell(43962)
 local specWarnBomb		= mod:NewSpecialWarningSpell(42630)
 
@@ -27,13 +30,15 @@ local timerAdds			= mod:NewNextTimer(92, 43962)
 
 local berserkTimer		= mod:NewBerserkTimer(600)
 
+local spamFlameBreath = 0
+
 mod:AddBoolOption("FlameIcon", true)
 
 function mod:FlameTarget()
 	local targetname = self:GetBossTarget(23578)
 	if not targetname then return end
 	warnFlame:Show(targetname)
-	if self.Options.FlameIcon and mod:LatencyCheck() then--Latency check when using icons with target scanning to reduce false icons on tanks
+	if self.Options.FlameIcon and mod:LatencyCheck() then
 		self:SetIcon(targetname, 8, 8)
 	end	
 end
@@ -41,10 +46,19 @@ end
 function mod:OnCombatStart(delay)
 	timerAdds:Start(10)
 	berserkTimer:Start(-delay)
+	spamFlameBreath = 0
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(97855) and args:IsPlayer() and GetTime() - spamFlameBreath >= 3 then
+		specWarnFlameBreath:Show()
+		spamFlameBreath = GetTime()
+	end
 end
 
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(97855) then
+		warnFlameCast:Show()	-- Seems he doesn't target the person :(
 		self:ScheduleMethod(0.2, "FlameTarget")
 	end
 end
