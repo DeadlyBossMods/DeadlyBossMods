d74c3d330d32dfebc973e77ff0808bf1211b9fb6
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-04-29 11:56:12 +0000

    #Party-Cataclysm\Zul'Aman\Akil'zon: Added arrow for Electrical Storm (and fixed spell ID) #Party-Cataclysm\Zul'Aman\Akil'zon: Added option to set an icon on the Amani Kidnapper

--- DBM-Party-Cataclysm/ZulAman/Akilzon5.lua
+++ DBM-Party-Cataclysm/ZulAman/Akilzon5.lua
@@ -5,7 +5,7 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(23574)
 mod:SetZone()
-mod:SetUsedIcons(8)
+mod:SetUsedIcons(1, 8)
 
 mod:RegisterCombat("combat")
 
@@ -13,24 +13,42 @@ mod:RegisterEvents(
 	"SPELL_AURA_APPLIED"
 )
 
-local warnStorm			= mod:NewTargetAnnounce(43648, 4)
-local warnStormSoon		= mod:NewSoonAnnounce(43648, 5, 3)
+local warnStorm			= mod:NewTargetAnnounce(97300, 4)
+local warnStormSoon		= mod:NewSoonAnnounce(97300, 5, 3)
 local warnPlucked		= mod:NewTargetAnnounce(97318, 3)
 
-local specWarnStorm		= mod:NewSpecialWarningSpell(43648)
+local specWarnStorm		= mod:NewSpecialWarningSpell(97300)
 
-local timerStorm		= mod:NewCastTimer(8, 43648)
-local timerStormCD		= mod:NewCDTimer(55, 43648)
+local timerStorm		= mod:NewCastTimer(8, 97300)
+local timerStormCD		= mod:NewCDTimer(55, 97300)
 
 local berserkTimer		= mod:NewBerserkTimer(600)
 
 mod:AddBoolOption("RangeFrame", true)
 mod:AddBoolOption("StormIcon", true)
+mod:AddBoolOption("StormArrow", true)
+mod:AddBoolOption("SetIconOnEagle", true)
+
+local eagleGUID = nil
+
+mod:RegisterOnUpdateHandler(function(self)
+	if self.Options.SetIconOnEagle and eagleGUID then
+		for i = 1, GetNumPartyMembers() do
+			local uId = "party"..i.."target"
+			local guid = UnitGUID(uId)
+			if guid == eagleGUID then
+				SetRaidTarget(uId, 8)
+				eagleGUID = nil
+			end
+		end
+	end
+end, 1)
 
 function mod:OnCombatStart(delay)
 	warnStormSoon:Schedule(43)
 	timerStormCD:Start(48)
 	berserkTimer:Start(-delay)
+	eagleGUID = nil
 	if self.Options.RangeFrame then
 		DBM.RangeCheck:Show()
 	end
@@ -43,7 +61,7 @@ function mod:OnCombatEnd()
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpellID(43648) then
+	if args:IsSpellID(97300) then
 		warnStorm:Show(args.destName)
 		specWarnStorm:Show()
 		timerStorm:Start()
@@ -56,9 +74,16 @@ function mod:SPELL_AURA_APPLIED(args)
 			end)
 		end
 		if self.Options.StormIcon then
-			self:SetIcon(args.destName, 8, 8)
+			self:SetIcon(args.destName, 1, 8)
 		end
-	elseif args:IsSpellID(97318) and args:IsDestTypePlayer() then
-		warnPlucked:Show(args.destName)	
+		if self.Options.StormArrow then
+			DBM.Arrow:ShowRunTo(args.destName, 0, 0, 8)
+		end
+	elseif args:IsSpellID(97318)then
+		if args:IsDestTypePlayer() then
+			warnPlucked:Show(args.destName)	
+		else
+			eagleGUID = args.destGUID
+		end
 	end
 end
