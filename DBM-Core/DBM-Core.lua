e790419d0b76c4ffb6b19862db7385927ab69b66
Adam <MysticalOS@users.noreply.github.com>
2011-04-30 17:18:32 +0000

    Remove 4.1 CLEU hack. wasted overhead now that we don't need it.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -364,7 +364,7 @@ do
 	end
 	
 
-	function DBM:COMBAT_LOG_EVENT_UNFILTERED(timestamp, event, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags, ...)
+	function DBM:COMBAT_LOG_EVENT_UNFILTERED(timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags, ...)
 		if not registeredEvents[event] then return end
 		twipe(args)
 		args.timestamp = timestamp
@@ -480,17 +480,6 @@ do
 		end
 		return handleEvent(nil, event, args)
 	end
-	
-	-- fix for 4.1 which introduces the hideCaster argument
-	-- this is a temporary work-around which just drops the new argument for a quick and easy fix. It doesn't look like that we are going to need this argument ever, so this is okay for now.
-	-- TODO: apply this change to the actual function above when 4.1 goes live, hideCaster can then be added to the args table
-	if tonumber((select(2, GetBuildInfo()))) >= 13682 then
-		local oldHandler = DBM.COMBAT_LOG_EVENT_UNFILTERED
-		function DBM:COMBAT_LOG_EVENT_UNFILTERED(timestamp, event, hideCaster, ...)
-			return oldHandler(self, timestamp, event, ...)
-		end
-	end
-	
 	mainFrame:SetScript("OnEvent", handleEvent)
 end
 
