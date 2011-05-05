301308b8d8a826bdd38eaf8b8cb87b005407ea21
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-05-05 19:40:09 +0000

    unfuck 4.2 compatiblity code since the 4.1a build number is newer than build number on 4.2 currently

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -483,7 +483,7 @@ do
 	-- this is a temporary work-around which just drops the new argument for a quick and easy fix that is compatible with 4.2
 	-- TODO: figure out the purpose of this argument...looks like a bitfield, let's have a look at the default UI source later...
 	-- TODO: apply this change to the actual function above when 4.2 goes live, the new argument can then be added to the args table if necessary
-	if tonumber((select(2, GetBuildInfo()))) >= 14002 then
+	if tonumber((select(4, GetBuildInfo()))) >= 40200 then
 		local oldHandler = DBM.COMBAT_LOG_EVENT_UNFILTERED
 		function DBM:COMBAT_LOG_EVENT_UNFILTERED(timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, mysteryFlags, ...)
 			return oldHandler(self, timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, ...)
