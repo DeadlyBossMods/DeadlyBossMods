db4102218718d84b659f11be0fab65bf83dcd2d8
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-05-06 07:08:53 +0000

    koKR update

--- DBM-BlackwingDescent/localization.kr.lua
+++ DBM-BlackwingDescent/localization.kr.lua
@@ -225,7 +225,7 @@ L:SetMiscLocalization({
 	NefAoe				= "전기가 튀며 파지직하는 소리가 납니다!",
 	YellPhase2			= "저주받을 필멸자들! 내 소중한 작품을 이렇게 망치다니! 쓴맛을 봐야 정신을 차리겠군!",
 	YellPhase3			= "품위있는 집주인답게 행동하려 했건만, 네놈들이 도무지 죽질 않는군! 겉치레는 이제 집어치우자고. 그냥 모두 없애 버리겠어!", -- "I have tried to be an accommodating host, but you simply will not die! Time to throw all pretense aside and just... KILL YOU ALL!",
-	YellShadowBlaze		= "Flesh turns to ash!",
+	YellShadowBlaze		= "살을 재로 만들어 주마!",
 	Nefarian			= "네파리안",
 	Onyxia				= "오닉시아",
 	Charge				= "전하 충전"
