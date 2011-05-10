2a8c1539f6c93fb5755c071cc7ee67e1c409d6b1
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-05-10 19:20:38 +0000

    Zul'Aman and Zul'Gurub model IDs

--- DBM-Party-Cataclysm/ZulAman/Halazzi5.lua
+++ DBM-Party-Cataclysm/ZulAman/Halazzi5.lua
@@ -3,6 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(23577)
+mod:SetModelID(21632)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
--- DBM-Party-Cataclysm/ZulAman/Nalorakk5.lua
+++ DBM-Party-Cataclysm/ZulAman/Nalorakk5.lua
@@ -3,6 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(23576)
+mod:SetModelID(21631)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
