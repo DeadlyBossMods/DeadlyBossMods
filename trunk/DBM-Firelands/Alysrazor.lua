002b72f9e17556e320e643acc36f9bd007fe832f
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-05-11 06:45:22 +0000

    Temporary model IDs for Firelands bosses

--- trunk/DBM-Firelands/Alysrazor.lua
+++ trunk/DBM-Firelands/Alysrazor.lua
@@ -3,6 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(52530)
+mod:SetModelID(38265) -- Temporary till real modelID is known
 mod:SetZone()
 mod:SetUsedIcons()
 
--- trunk/DBM-Firelands/FandralStaghelm.lua
+++ trunk/DBM-Firelands/FandralStaghelm.lua
@@ -3,6 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(52571)
+mod:SetModelID(37953) -- Temporary till real modelID is known
 mod:SetZone()
 mod:SetUsedIcons()
 
