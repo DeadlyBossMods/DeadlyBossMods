fc967753a620e41e935f7e7fee5525b797afe72e
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-05-12 10:32:07 +0000

    #DBM-BaradinHold\Occu'thar: creature ID based on db.mmo-champion.com and guessing it looks like a big core-hound as model Id :)

--- trunk/DBM-BaradinHold/Occuthar.lua
+++ trunk/DBM-BaradinHold/Occuthar.lua
@@ -2,8 +2,8 @@ local mod	= DBM:NewMod("Occuthar", "DBM-BaradinHold")
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(47120)
---mod:SetModelID(35426)
+mod:SetCreatureID(52363)	-- guessed
+mod:SetModelID(35904)		-- Guessing it looks like a kind of core hound :p
 mod:SetZone()
 --mod:SetUsedIcons(1, 2, 3, 4, 5, 6, 7, 8)
 
