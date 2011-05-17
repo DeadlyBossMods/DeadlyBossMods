e049f53341189951ddbef409334ba9478ad9feba
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-05-17 07:18:02 +0000

    #Firelands\Alysrazor: Info Frame for Blazing Power, warning/timer for Molting

--- DBM-Firelands/Alysrazor.lua
+++ DBM-Firelands/Alysrazor.lua
@@ -10,7 +10,29 @@ mod:SetUsedIcons()
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_AURA_APPLIED",
+	"SPELL_CAST_SUCCESS"
 )
 
+
+local warnMolting		= mod:NewSpellAnnounce(99464, 3)
+
+local timerMoltingCD		= mod:NewNextTimer(60, 99464)
+
+mod:AddBoolOption("InfoFrame")
+
 function mod:OnCombatStart(delay)
+	timerMoltingCD:Start(10-delay)
+
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:SetHeader(L.PowerLevel)
+		DBM.InfoFrame:Show(5, "playerpower", 10, ALTERNATE_POWER_INDEX)
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(99464) then
+		warnMolting:Show()
+		timerMoltingCD:Start()
+	end
 end
