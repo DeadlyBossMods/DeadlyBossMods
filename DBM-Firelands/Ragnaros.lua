18f49e2da85d6a5ded577f5d1e2aee78b1878965
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-05-28 08:53:19 +0000

    #Firelands\Ragnaros: Some drycoded phase 2 stuff. Molten Seed, Engulfing Flames timers/warnings. Added option to show range frame, only show the frame when needed (Phase 1 - Wrath of Ragnaros; Phase 2 - 10sec before Molten Seed; Phase 3 - Living Meteor)

--- DBM-Firelands/Ragnaros.lua
+++ DBM-Firelands/Ragnaros.lua
@@ -21,25 +21,53 @@ mod:RegisterEvents(
 local warnHandRagnaros		= mod:NewSpellAnnounce(99237, 3)
 local warnWrathRagnaros		= mod:NewSpellAnnounce(98263, 3)
 local warnBurningWound		= mod:NewStackAnnounce(99399, 3)
+local warnMoltenSeed		= mod:NewSpellAnnounce(98520, 3)	-- spell ID ??
 local warnPhase2Soon		= mod:NewPrePhaseAnnounce(2, 3)
 local warnPhase3Soon		= mod:NewPrePhaseAnnounce(3, 3)
 
-local timerMagmaTrap		= mod:NewCDTimer(25, 98164)	-- might even be a "next" timer
-local timerSulfurasSmash	= mod:NewCDTimer(40, 98710)	-- might even be a "next" timer
-local timerHandRagnaros		= mod:NewCDTimer(35, 99237)	-- might even be a "next" timer
-local timerWrathRagnaros	= mod:NewCDTimer(25, 98263)	-- 25-35sec variation?
+local timerMagmaTrap		= mod:NewCDTimer(25, 98164)		-- might even be a "next" timer
+local timerSulfurasSmash	= mod:NewCDTimer(40, 98710)		-- might even be a "next" timer
+local timerHandRagnaros		= mod:NewCDTimer(35, 99237)		-- might even be a "next" timer
+local timerWrathRagnaros	= mod:NewCDTimer(25, 98263)		-- 25-35sec variation?
 local timerBurningWound		= mod:NewTargetTimer(20, 99399)
-local timerPhaseSons		= mod:NewPhaseSons(45, "TimerPhaseSons")	-- lasts 45secs or till all sons are dead
+local timerFlames		= mod:NewCDTimer(60, 99171)		-- Engulfing Flames spell ID?
+local timerMoltenSeed 		= mod:NewBuffActiveTimer(10, 98520)	-- spell ID ??
+local timerMoltenSeedCD		= mod:NewCDTimer(60, 98520)		-- spell ID ??  Might even be a "next" timer
+local timerPhaseSons		= mod:NewPhaseSons(45, "TimerPhaseSons")-- lasts 45secs or till all sons are dead
 
 local specWarnBurningWound	= mod:NewSpecialWarningStack(99399, 5, nil, mod:IsTank())
 
+mod:AddBoolOption("RangeFrame")
+
 local sonsDied = 0
+local phase = 1
 local prewarnedPhase2 = false
 local prewarnedPhase3 = false
+
+local function showRangeFrame()
+	if mod.Options.RangeFrame then
+		if phase < 3 then
+			DBM.RangeCheck:Show(6)
+		else
+			DBM.RangeCheck:Show(5)
+		end
+	end
+end
+
+local function hideRangeFrame()
+	if mod.Options.RangeFram then
+		DBM.RangeCheck:Hide()
+	end
+end
+
 function mod:OnCombatStart(delay)
+	if self.Options.RangeFrame then
+		DBM.RangeCheck:Show()
+	end
 	timerMagmaTrap:Start(17-delay)
 	timerSulfurasSmash:Start(32-delay)
 	sonsDied = 0
+	phase = 1
 	prewarnedPhase2 = false
 	prewarnedPhase3 = false
 end
@@ -61,8 +89,14 @@ function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(98710) then
 		timerSulfurasSmash:Start()
 	elseif args:IsSpellID(98952) then
+		timerMagmaTrap:Cancel()
+		timerSulfurasSmash:Cancel()
+		timerHandRagnaros:Cancel()
+		timerWrathRagnaros:Cancel()
 		timerPhaseSons:Start(53)	-- 45 + 8sec cast
+		self:hideRangeFrame()
 		sonsDied = 0
+		phase = phase + 1
 	end
 end
 
@@ -75,6 +109,14 @@ function mod:SPELL_CAST_SUCCESS(args)
 	elseif args:IsSpellID(98263) then
 		warnWrathRagnaros:Show()
 		timerWrathRagnaros:Start()
+	elseif args:IsSpellID(99171) then	-- correct SpellID ???
+		timerFlames:Start()
+	elseif args:IsSpellID(98520) then	-- correct spell ID ???
+		warnMoltenSeed:Show()
+		timerMoltenSeed:Start()
+		timerMoltenSeedCD:Start()
+		self:HideRangeFrame()			-- only have to spread just before the Molten Seeds
+		self:Schedule(50, showRangeFrame)	-- show the range frame 10secs before next Molten Seeds
 	end
 end
 
@@ -82,8 +124,16 @@ function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 53140 then
 		sonsDied = sonsDied + 1
-		if sonsDied > 10 then		-- how many are there at which difficulties?
+		if sonsDied > 6 then		-- 6 on normal 10?  How many are there at which difficulties?
 			timerPhaseSons:Cancel()
+			if phase == 2 then
+				timerSulfurasSmash:Start(25)
+				timerFlames:Start(44)
+				timerMoltenSeedCD:Start(16)
+				self:Schedule(6, showRangeFrame)
+			elseif phase == 3 then
+				self:showRangeFrame()	-- Meteors needs spreading
+			end
 		end
 	end
 end
