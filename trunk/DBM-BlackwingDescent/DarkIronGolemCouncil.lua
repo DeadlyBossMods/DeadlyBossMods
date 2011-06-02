1cc73dc7c10e8add39b8039edc11425cf87ec9fb
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-06-02 04:22:31 +0000

    clean up and improve expression while at it to be one line instead of multiple rules

--- trunk/DBM-BlackwingDescent/DarkIronGolemCouncil.lua
+++ trunk/DBM-BlackwingDescent/DarkIronGolemCouncil.lua
@@ -346,10 +346,8 @@ function mod:SPELL_CAST_START(args)
 		timerNefAbilityCD:Start()
 		cloudSpam = GetTime()
 	elseif args:IsSpellID(79710, 91540, 91541, 91542) then
-		if self:IsMelee() and (self:GetUnitCreatureId("target") == 42166 or self:GetUnitCreatureId("focus") == 42166) then--Only warn for melee targeting him or exclicidly put him on focus.
-			specWarnAnnihilator:Show()
-		elseif not self:IsMelee() then--Warn regardless if he's your target/focus or not if you aren't a melee since warning is off by default for ranged, you probably enabled it for a reason and have more luxury to switch targets then melee does.
-			specWarnAnnihilator:Show()
+		if self:IsMelee() and (self:GetUnitCreatureId("target") == 42166 or self:GetUnitCreatureId("focus") == 42166) or not self:IsMelee() then
+			specWarnAnnihilator:Show()--Only warn for melee targeting him or exclicidly put him on focus, else warn regardless if he's your target/focus or not if you aren't a melee
 		end
 	end
 end
