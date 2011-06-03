e76666a121df6398ee9b501b1f54dc09de43e628
Adam <MysticalOS@users.noreply.github.com>
2011-06-03 23:23:16 +0000

    and, GUI done for voice options

--- DBM-GUI/localization.en.lua
+++ DBM-GUI/localization.en.lua
@@ -90,6 +90,7 @@ L.WarningIconRight 			= "Show icon on right side"
 L.RaidWarnMessage 			= "Thanks for using Deadly Boss Mods"
 L.BarWhileMove 				= "Raid warning movable"
 L.RaidWarnSound				= "Play sound on raid warning"
+L.CountdownVoice			= "Set voice for countdown sounds"
 L.SpecialWarnSound			= "Set sound for special warnings affecting you or your specific role"
 L.SpecialWarnSound2			= "Set sound for special warnings affecting everyone"
 
