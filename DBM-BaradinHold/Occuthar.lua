c2dee7536beaaa6882013d20b377a2427dadfcb4
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-06-08 17:15:50 +0000

    updated model IDs to match the encounter journal

--- DBM-BaradinHold/Occuthar.lua
+++ DBM-BaradinHold/Occuthar.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(52363)
-mod:SetModelID(35904)	-- Guessing it looks like the trapped mob inside Baradin Hold
+mod:SetModelID(37876)
 mod:SetZone()
 --mod:SetUsedIcons(1, 2, 3, 4, 5, 6, 7, 8)
 
