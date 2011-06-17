278a337db6fe6e408bea3d822cf4f15cacf92a0e
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-06-17 04:22:14 +0000

    try this small timing tweak to cutters, they are ever so slightly slow, but fall behind considerbly at a point in phase3, although i have a feeling there is more to it then that as there seem to be variations by how much

--- trunk/DBM-BastionTwilight/Sinestra.lua
+++ trunk/DBM-BastionTwilight/Sinestra.lua
@@ -151,9 +151,9 @@ function mod:OrbsRepeat()
 		warnOrbSoon:Schedule(26, 2)
 		warnOrbSoon:Schedule(27, 1)
 	end
-	OrbsCountdown:Start(28)
+	OrbsCountdown:Start(27.5)
 	specWarnOrbs:Show()--generic aoe warning on spawn, before we have actual targets yet.
-	self:ScheduleMethod(28, "OrbsRepeat")
+	self:ScheduleMethod(27.5, "OrbsRepeat")
 	self:Schedule(0.5, showOrbWarning, "spawn")--Start spawn checks
 end
 
