e6f8524b3bbd6b23dc149cf87f107776d2339bcc
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-06-28 07:13:46 +0000

    koKR update

--- DBM-ThroneFourWinds/localization.kr.lua
+++ DBM-ThroneFourWinds/localization.kr.lua
@@ -62,7 +62,8 @@ L:SetOptionLocalization({
 	LightningRodIcon	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(89668),
 	TimerFeedback		= "$spell:87904 유지 타이머 보기",
 	WarnAdd				= "폭풍 정령이 생성 될 때 경고 보기",
-	TimerAddCD			= "다음 폭풍 정령 생성 타이머 보기"
+	TimerAddCD			= "다음 폭풍 정령 생성 타이머 보기",
+	RangeFrame			= "$spell:89668의 영향을 받을 경우 거리 프레임 보기(20 m)"
 })
 
 L:SetMiscLocalization({
