d7e22cdc3f4ada756e3ffcde3bf677d9d298393c
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-06-28 09:22:08 +0000

    Oops, deleted a bit too much. Model ID fetching from Encounter Journal needs improvement (it breaks the GUI atm) so lets restore the old mod:SetModelID() in Firelands mods

--- trunk/DBM-Firelands/FandralStaghelm.lua
+++ trunk/DBM-Firelands/FandralStaghelm.lua
@@ -3,6 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(52571)
+mod:SetModelID(37953)
 mod:SetZone()
 mod:SetUsedIcons()
 
--- trunk/DBM-Firelands/Rhyolith.lua
+++ trunk/DBM-Firelands/Rhyolith.lua
@@ -3,6 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(52558)
+mod:SetModelID(38414)
 mod:SetZone()
 mod:SetUsedIcons()
 
