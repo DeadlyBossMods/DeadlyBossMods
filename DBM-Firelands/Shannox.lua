2c6da2d7d226b76667192c31364cdfdc1843632d
Adam <MysticalOS@users.noreply.github.com>
2011-06-29 05:02:38 +0000

    Fix lua error on rhyolith, make shannox mod actually useful

--- DBM-Firelands/Shannox.lua
+++ DBM-Firelands/Shannox.lua
@@ -11,36 +11,38 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_APPLIED_DOSE",
 	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS"
+	"SPELL_CAST_SUCCESS",
+	"SPELL_SUMMON"
 )
 
-local warnFaceRage	= mod:NewTargetAnnounce(99945, 4)
-local warnRage		= mod:NewTargetAnnounce(100415, 3)
-local warnWary		= mod:NewTargetAnnounce(100167, 2, nil, false)	-- which hound has this buff? They get it when being trapped
-local warnTears		= mod:NewStackAnnounce(99937, 3)
-local warnSpear		= mod:NewSpellAnnounce(100002, 3)
+local warnFaceRage				= mod:NewTargetAnnounce(99945, 4)
+local warnRage					= mod:NewTargetAnnounce(100415, 3)
+local warnWary					= mod:NewTargetAnnounce(100167, 2, nil, false)	-- which hound has this buff? They get it when being trapped
+local warnTears					= mod:NewStackAnnounce(99937, 3)
+local warnMagmaFlare			= mod:NewSpellAnnounce(100495, 3)
+local warnCrystalPrison			= mod:NewSpellAnnounce(99836, 2)
+local warnCrystalPrisonTarget	= mod:NewTargetAnnounce(99837, 4)
 
-local timerRage		= mod:NewTargetTimer(15, 100415)
-local timerWary		= mod:NewTargetTimer(25, 100167, nil, false)
-local timerTears	= mod:NewTargetTimer(30, 99937)
+local timerRage				= mod:NewTargetTimer(15, 100415)
+local timerWary				= mod:NewTargetTimer(25, 100167, nil, false)
+local timerTears			= mod:NewTargetTimer(30, 99937)
+local timerCrystalPrisonCD	= mod:NewCDTimer(25.5, 99836)--Seems consistent, other trap is not.
+local timerMagmaFlareCD		= mod:NewCDTimer(42, 100495)--Use this, since he only does hurl spear until rip dies. every 42-47 seconds
 
-local specWarnRage	= mod:NewSpecialWarningYou(100415)
-local specWarnTears	= mod:NewSpecialWarningStack(99937, 5, nil, mod:IsTank())
+local specWarnRage			= mod:NewSpecialWarningYou(100415)
+local specWarnImmTrap		= mod:NewSpecialWarningMove(99838)
+local specWarnTears			= mod:NewSpecialWarningStack(99937, 8, nil, mod:IsTank())
 
 mod:AddBoolOption("SetIconOnFaceRage")
 mod:AddBoolOption("SetIconOnRage")
 
---[[
-SPELL_SUMMON for traps needed?
-99836 = Throw Crystal Prison Trap		--> SPELL_AURA_APPLIED = 99837
-99839 = Throw Immolation Trap			--> SPELL_AURA_APPLIED = 101209 (might be more depending on difficulty)
---]]
-
 local spamFaceRage = 0
 function mod:OnCombatStart(delay)
 	spamFaceRage = 0
+--	timerCrystalPrisonCD:Start(-delay)--Don't know yet, Need to run transcriptor with combat logging turned OFF to get the timestamps right.
+	timerMagmaFlareCD:Start(20-delay)--Guesswork
 end
 
 function mod:SPELL_AURA_APPLIED(args)
@@ -53,31 +55,34 @@ function mod:SPELL_AURA_APPLIED(args)
 		if self.Options.SetIconOnRage then
 			self:SetIcon(args.destName, 6, 15)
 		end
-	elseif args:IsSpellID(100167, 101215, 101216, 101217) then	--100167, 101216 confirmed
+	elseif args:IsSpellID(100167, 101215, 101216, 101217) then
 		warnWary:Show(args.destName)
 		timerWary:Start(args.destName)
-	elseif args:IsSpellID(99937, 101218, 101219, 101220) then	--99937, 101219 confirmed
-		if args.amount or 0 % 2 == 0 then	-- announce: 1 - 2 - 4 - 6 - 8 stacks
+	elseif args:IsSpellID(99838, 101208, 101209, 101210) then
+		if args:IsPlayer() then
+			specWarnImmTrap:Show()
+		end
+	elseif args:IsSpellID(99837) then
+		warnCrystalPrisonTarget:Show(args.destName)
+	elseif args:IsSpellID(99937, 101218, 101219, 101220) then
+		if args.amount or 0 % 3 == 0 then	--Warn every 3 stacks
 			warnTears:Show(args.destName, args.amount or 1)
 		end
-		if args.amount or 0 >= 5 then		-- tank switch @ 5?
+		if args.amount or 0 >= 8 then		-- tank switch @ 8?
 			specWarnTears:Show()
 		end
 		timerTears:Start(args.destName)
 	end
 end
 
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+
 function mod:SPELL_AURA_REMOVED(args)
 	if args:IsSpellID(99945) then
+		timerRage:Cancel(args.destName)
 		if self.Options.SetIconOnFaceRage then
 			self:SetIcon(args.destName, 0)
 		end
-	end
-end
-
-function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(100002, 100031) then	--100002 confirmed .. all/right ids?
-		warnSpear:Show()
 	end
 end
 
@@ -88,6 +93,15 @@ function mod:SPELL_CAST_SUCCESS(args)
 		if self.Options.SetIconOnFaceRage then
 			self:SetIcon(args.destName, 8)
 		end
+	elseif args:IsSpellID(100495) then	--This is cast 2 seconds after spear, although when rip dies he stops casting spear and only casts this.
+		warnMagmaFlare:Show()
+		timerMagmaFlareCD:Start()
 	end
 end
 
+function mod:SPELL_SUMMON(args)
+	if args:IsSpellID(99836) then
+		warnCrystalPrison:Show()
+		timerCrystalPrisonCD:Start()
+	end
+end
