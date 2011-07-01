06f67c860a8dd2f99451b8aa673d1b32628b7f4a
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-07-01 21:34:43 +0000

    Was supposed to fix, not further breaking it :)

--- DBM-Firelands/Ragnaros.lua
+++ DBM-Firelands/Ragnaros.lua
@@ -35,7 +35,7 @@ local timerMoltenSeed 		= mod:NewBuffActiveTimer(10, 98520)	-- spell ID ??
 local timerMoltenSeedCD		= mod:NewCDTimer(60, 98520)		-- spell ID ??  Might even be a "next" timer
 local timerPhaseSons		= mod:NewTimer(45, "TimerPhaseSons")	-- lasts 45secs or till all sons are dead
 
-local specWarnBurningWound	= mod:NewSpecialWarningStack(99399, nil, 5, mod:IsTank())
+local specWarnBurningWound	= mod:NewSpecialWarningStack(99399, mod:IsTank(), 5)
 
 mod:AddBoolOption("RangeFrame")
 
