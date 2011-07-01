5e7e6d2e1d630d8dbb88f83560ab66694805eb2b
Adam <MysticalOS@users.noreply.github.com>
2011-07-01 04:32:43 +0000

    ok these mapsizes will actually work, damn the buggy api and the wrong data files, these mapsizes were calculated manually but very accurately with a mage.

--- DBM-Firelands/MapSizes.lua
+++ DBM-Firelands/MapSizes.lua
@@ -1,4 +1,4 @@
 DBM:RegisterMapSize("Firelands", 
-	1, 1440.0, 960.0, 	-- floor 1
-	2, 668.0, 768.0,	-- floor 2
-	3, 668.0, 768.0)	-- floor 3
+	1, 1270.0, 846.0, 	-- The Firelands
+	2, 1152.0, 768.0)	-- Sulfuron Keep
+--	3, 668.0, 768.0)	-- The Anvil of Conflagration
