d13aa69e9e55395761fc3fa8c671809b733f0c0b
Adam <MysticalOS@users.noreply.github.com>
2011-07-09 00:21:59 +0000

    fuck that, still not working, tantrum and gushing would support removed

--- DBM-Firelands/Alysrazor.lua
+++ DBM-Firelands/Alysrazor.lua
@@ -60,7 +60,7 @@ function mod:OnCombatEnd()
 	end
 end 
 
-function mod:SPELL_AURA_APPLIED(args)
+--[[function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(99362) and self:IsInCombat() and (args.sourceGUID == UnitGUID("target") and mod:IsTank()) or not mod:IsTank() then--Only give tantrum warning if it's mob you're targeting and you're a tank, else, always give tantrum warning regardless of target
 		specWarnTantrum:Show()
 	elseif args:IsSpellID(99308) then--Gushing Wound
@@ -69,7 +69,7 @@ function mod:SPELL_AURA_APPLIED(args)
 			specWarnGushingWoundSelf:Show()
 		end
 	end
-end
+end--]]
 
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(101223, 101294, 101295, 101296) then
