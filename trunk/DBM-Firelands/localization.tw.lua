6a86297b082a748a6d93b10b56dba8ad22870455
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-07-09 20:17:29 +0000

    add zhTW localization by a9012456

--- trunk/DBM-Firelands/localization.tw.lua
+++ trunk/DBM-Firelands/localization.tw.lua
@@ -7,43 +7,51 @@ local L
 L= DBM:GetModLocalization(194)
 
 L:SetWarningLocalization({
-	WarnPhase		= "Phase %d"
+	WarnPhase			= "階段 %d",
+	WarnNewInitiate		= "熾炎爪擊啟動 (%d)"
 })
 
 L:SetTimerLocalization({
-	TimerPhaseChange	= "Phase %d",
-	TimerHatchEggs		= "Hatch Eggs"
+	TimerPhaseChange	= "階段 %d",
+	TimerHatchEggs		= "熔岩蛋孵化",
+	timerNextInitiate	= "下次熾炎爪擊啟動"
 })
 
 L:SetOptionLocalization({
-	WarnPhase		= "Show a warning for each phase change",
-	TimerPhaseChange	= "Show a timer till next phase",
-	TimerHatchEggs		= "Show a timer till next eggs are hatched",
-	InfoFrame		= "Show info frame for Blazing Power"
+	WarnPhase			= "為每次轉換階段顯示警告",
+	WarnNewInitiate		= "為新的熾炎爪擊啟動顯示警告",
+	timerNextInitiate	= "為下一次熾炎爪擊啟動顯示計時器",
+	TimerPhaseChange	= "為下次階段轉換顯示計時器",
+	TimerHatchEggs		= "為下次熔岩蛋孵化顯示計時器",
+	InfoFrame			= "在資訊框架顯示熔岩之力"
 })
 
 L:SetMiscLocalization({
-	YellPull		= "I serve a new master now, mortals!",
-	YellPhase2		= "These skies are MINE!",
-	PowerLevel		= "Blazing Power"
+	YellPull		= "我現在有新的主人了，凡人!",
+	YellInitiate1	= "我們呼喚你，炎魔!",
+	YellInitiate2	= "看看他的力量!",
+	YellInitiate3	= "把不信者全都燒死!",
+	YellInitiate4	= "見證壯麗的火焰。",
+	YellPhase2		= "這片天空屬於我。",
+	PowerLevel		= "熔岩之力"
 })
 
 -------------------
 -- Lord Rhyolith --
 -------------------
-L= DBM:GetModLocalization(192)
+L= DBM:GetModLocalization(193)
 
 L:SetWarningLocalization({
-	WarnElementals		= "Elementals spawning"
+	WarnElementals		= "元素怪出現"
 })
 
 L:SetTimerLocalization({
-	TimerElementals		= "Next elementals"
+	TimerElementals		= "下一次召喚元素怪"
 })
 
 L:SetOptionLocalization({
-	WarnElementals		= "Show a warning when elementals are spawning",
-	TimerElementals		= "Show a timer for next elementals spawning"
+	WarnElementals		= "當元素怪出現時顯示警告",
+	TimerElementals		= "為下一次元素怪出現顯示警告"
 })
 
 L:SetMiscLocalization({
@@ -58,19 +66,20 @@ L:SetWarningLocalization({
 })
 
 L:SetTimerLocalization({
-	TimerSpinners 		= "Next Spinners",
-	TimerSpiderlings	= "Next Spiderlings",
-	TimerDrone		= "Next Drone"
+	TimerSpinners 		= "下一次 燼網紡織者",
+	TimerSpiderlings	= "下一次 燼網小蜘蛛",
+	TimerDrone		= "下一次 燼網雄蛛"
 })
 
 L:SetOptionLocalization({
-	TimerSpinners		= "Show timer for next $journal:2770",
-	TimerSpiderlings	= "Show timer for next $journal:2778",
-	TimerDrone		= "Show timer for next $journal:2773"
+	TimerSpinners		= "為下一次$journal:2770出現顯示計時器",
+	TimerSpiderlings	= "為下一次$journal:2778出現顯示計時器",
+	TimerDrone		= "為下一次$journal:2773出現顯示計時器",
+	RangeFrame				= "顯示距離框(10碼)",
 })
 
 L:SetMiscLocalization({
-	EmoteSpiderlings 	= "Spiderlings have been roused from their nest!"
+	EmoteSpiderlings 	= "小蜘蛛從牠們的巢穴中被驚動了!"
 })
 
 -------------
@@ -90,6 +99,8 @@ L:SetOptionLocalization({
 })
 
 L:SetMiscLocalization({
+	Riplimb		= "裂軀",
+	Rageface	= "怒面"
 })
 
 -------------
@@ -102,17 +113,19 @@ L:SetWarningLocalization({
 
 L:SetTimerLocalization({
 	TimerBladeActive	= "%s",
-	TimerBladeNext		= "Next blade"
+	TimerBladeNext		= "下一次 強化刀刃"
 })
 
 L:SetOptionLocalization({
-	TimerBladeActive	= "Show a duration timer for the active blade",
-	TimerBladeNext		= "Show a next timer for blade",
+	TimerBladeActive	= "為當前強化的刀刃顯示持續時間計時器",
+	TimerBladeNext		= "為下一次強化刀刃顯示計時器",
 	SetIconOnCountdown	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(99516),
-	ArrowOnCountdown	= "Show DBM Arrow when you are affected by $spell:99516 "
+	ArrowOnCountdown	= "當你中了$spell:99516時顯示DBM箭頭",
+	InfoFrame		= "顯示活力火花堆疊訊息框"
 })
 
 L:SetMiscLocalization({
+	VitalSpark		= GetSpellInfo(99262).." 堆疊"
 })
 
 --------------------------------
@@ -127,6 +140,8 @@ L:SetTimerLocalization({
 })
 
 L:SetOptionLocalization({
+	RangeFrameSeeds				= "為$spell:98450顯示距離框(12碼)",
+	RangeFrameCat				= "為$spell:98374顯示距離框(10碼)"
 })
 
 L:SetMiscLocalization({
@@ -141,12 +156,35 @@ L:SetWarningLocalization({
 })
 
 L:SetTimerLocalization({
-	TimerPhaseSons		= "Sons phase ends"
+	TimerPhaseSons		= "烈焰之子階段結束"
 })
 
 L:SetOptionLocalization({
-	TimerPhaseSons		= "Show a duration timer for the \"Sons of Flame phase\"",
-	RangeFrame		= "Show range frame"
+	TimerPhaseSons		= "為\"烈焰之子階段\"顯示持續時間計時器",
+	RangeFrame		= "顯示距離監視框"
+})
+
+L:SetMiscLocalization({
+})
+
+-----------------------
+--  Firelands Trash  --
+-----------------------
+L = DBM:GetModLocalization("FirelandsTrash")
+
+L:SetGeneralLocalization({
+	name = "火源之界小怪"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetOptionLocalization({
+	RangeFrame	= "為$spell:100012顯示距離框(10碼)",
+	BlazingHeatIcons	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(100983)
 })
 
 L:SetMiscLocalization({
