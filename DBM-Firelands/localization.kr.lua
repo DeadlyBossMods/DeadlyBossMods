4ed9c279cceb4ecee5f199bafc15221d35a23870
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-07-15 12:05:13 +0000

    koKR update

--- DBM-Firelands/localization.kr.lua
+++ DBM-Firelands/localization.kr.lua
@@ -33,6 +33,7 @@ L:SetMiscLocalization({
 	YellInitiate3	= "믿지 않는 자, 불로 멸하리라!",
 	YellInitiate4	= "화염의 장엄함을 지켜봐라!",
 	YellPhase2		= "이 하늘은 나의 것이다!",
+	LavaWorms		= "불타는 용암 벌레가 땅에서 튀어나옵니다!",--Might use this one day if i feel it needs a warning for something. Or maybe pre warning for something else (like transition soon)
 	PowerLevel		= "타오르는 힘"
 })
 
@@ -120,6 +121,7 @@ L:SetOptionLocalization({
 	TimerBladeActive	= "칼날 유지 타이머 보기",
 	TimerBladeNext		= "다음 칼날 타이머 보기",
 	SetIconOnCountdown	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(99516),
+	SetIconOnTorment	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(100232),
 	ArrowOnCountdown	= "$spell:99516의 영향을 받은 경우 DBM 화살표 보기",
 	InfoFrame			= "생기의 불꽃 중첩 정보 프레임 보기"
 })
@@ -159,13 +161,13 @@ L:SetWarningLocalization({
 })
 
 L:SetTimerLocalization({
-	TimerPhaseSons		= "다음 단계"
+	TimerPhaseSons		= "전환 단계"
 })
 
 L:SetOptionLocalization({
 	warnSplittingBlow	= "$spell:100877 경고 보기",
 	warnEngulfingFlame	= "$spell:99171 경고 보기",
-	TimerPhaseSons		= "'용암의 후예' 단계 지속 타이머 보기",
+	TimerPhaseSons		= "전환 단계 지속 타이머 보기",
 	RangeFrame			= "거리 프레임이 필요하게 될 때 거리 프레임 보기",
 	BlazingHeatIcons	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(100983)
 })
@@ -175,7 +177,8 @@ L:SetMiscLocalization({
 	West				= "서쪽",
 	Middle				= "중앙",
 	North				= "북쪽",
-	South				= "남쪽"
+	South				= "남쪽",
+	transitionended		= "여기까지! 이제 끝내주마."--The adds detection doesn't always work right for some reason. May have to switch to this so translate it in case of switch.
 })
 
 -----------------------
@@ -198,4 +201,30 @@ L:SetOptionLocalization({
 })
 
 L:SetMiscLocalization({
+})
+
+----------------
+--  Volcanus  --
+----------------
+L = DBM:GetModLocalization("Volcanus")
+
+L:SetGeneralLocalization({
+	name = "Volcanus"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+	timerStaffTransition	= "전환 단계"
+})
+
+L:SetOptionLocalization({
+	timerStaffTransition	= "전환 단계 지속 타이머 보기"
+})
+
+L:SetMiscLocalization({
+	StaffEvent			= "The Branch of Nordrassil reacts violently",--Partial, not sure if pull detection will work with partials yet :\
+	StaffTrees			= "Burning Treants erupt from the ground to aid the Protector!",--Might add a spec warning for this later.
+	StaffTransition		= "The fires consuming the Tormented Protector wink out!"
 })
