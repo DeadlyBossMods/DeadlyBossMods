857c05bba553a2e07aa8605e703d480e4cc10a3e
Adam <MysticalOS@users.noreply.github.com>
2011-07-27 05:44:56 +0000

    Redid Rag phase detection to use locals. the sons method was prone to failure as sometimes 1 randomly would despawn or something, point is all 8 wouldn't die. Further more, the yells seem to provide more accurate timers too as it smooths out the variations a bit. Old method is still in code commented out for now til I test new method for sure (although i have no reason to believe it wouldn't work) Updated trash mod some more. Small update to alysrazor

--- DBM-Firelands/FirelandsTrash.lua
+++ DBM-Firelands/FirelandsTrash.lua
@@ -16,6 +16,8 @@ mod:RegisterEvents(
 
 local warnMoltenArmor		= mod:NewStackAnnounce(99532, 3, nil, mod:IsTank() or mod:IsHealer())
 local warnDruidLeap			= mod:NewTargetAnnounce(99629, 3, nil, false)--Probably spammy, more for debugging timing then anything.
+local warnRaiselava			= mod:NewSpellAnnounce(99503, 3)
+local warnMoltenBolt		= mod:NewSpellAnnounce(99579, 3)
 
 local specWarnFieroblast	= mod:NewSpecialWarningInterrupt(100094, false)
 local specWarnMoltenArmor	= mod:NewSpecialWarningStack(99532, mod:IsTank(), 4)
@@ -24,6 +26,7 @@ local specWarnDruidLeapNear	= mod:NewSpecialWarningClose(99629)
 
 local timerMoltenArmor		= mod:NewTargetTimer(15, 99532, nil, mod:IsTank() or mod:IsHealer())
 local timerRaiseLavaCD		= mod:NewNextTimer(17, 99503)--Every 15 sec + 2 sec cast.
+local timerMoltenBoltCD		= mod:NewNextTimer(15, 99579)--The worm gyser things that always kill people for not moving.
 
 mod:AddBoolOption("TrashRangeFrame", false)--off by default, this was NOT well recieved.
 
@@ -63,7 +66,7 @@ function mod:LeapTarget(sGUID)
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpellID(99532, 100767) then
+	if args:IsSpellID(99532, 100767) and args:IsDestTypePlayer() then
 		warnMoltenArmor:Show(args.destName, args.amount or 1)
 		if args:IsPlayer() and (args.amount or 1) >= 4 then
 			specWarnMoltenArmor:Show(args.amount)
@@ -87,6 +90,7 @@ function mod:SPELL_CAST_START(args)
 	elseif args:IsSpellID(99629) then--Druid of the Flame Leaping
 		self:ScheduleMethod(0.2, "LeapTarget", args.sourceGUID)
 	elseif args:IsSpellID(99503) then
+		warnRaiselava:Show()
 		timerRaiseLavaCD:Start()
 	end
 end
@@ -98,6 +102,9 @@ function mod:SPELL_CAST_SUCCESS(args)
 		if self.Options.TrashRangeFrame then
 			DBM.RangeCheck:Show(10)
 		end
+	elseif args:IsSpellID(99579) then
+		warnMoltenBolt:Show()
+		timerMoltenBoltCD:Start()
 	end
 end
 			
@@ -113,6 +120,8 @@ function mod:UNIT_DIED(args)
 		end
 	elseif self:GetCIDFromGUID(args.destGUID) == 53575 then
 		timerRaiseLavaCD:Cancel()
+	elseif self:GetCIDFromGUID(args.destGUID) == 53617 then
+		timerMoltenBoltCD:Cancel()
 	end	
 end
 
