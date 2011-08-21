65b4c86b4bb30e79d24eef898caeb5b1fb3493b8
Adam <MysticalOS@users.noreply.github.com>
2011-08-21 19:54:59 +0000

    Switch shannox and valiona to map based checks for the "near" warnings so they are more precise and don't warn you if you aren't actually too close. Removed map checks from some mods that weren't even using them for anything. Populated Gri'lek mod

--- DBM-Party-Cataclysm/Deadmines/FoeReaper.lua
+++ DBM-Party-Cataclysm/Deadmines/FoeReaper.lua
@@ -44,11 +44,6 @@ function mod:HarvestTarget()
 		local uId = DBM:GetRaidUnitId(targetname)
 		if uId then
 			local inRange = CheckInteractDistance(uId, 2)
-			local x, y = GetPlayerMapPosition(uId)
-			if x == 0 and y == 0 then
-				SetMapToCurrentZone()
-				x, y = GetPlayerMapPosition(uId)
-			end
 			if inRange then
 				specWarnHarvestNear:Show(targetname)
 			end
--- DBM-Party-Cataclysm/TheVortexPinnacle/Altairus.lua
+++ DBM-Party-Cataclysm/TheVortexPinnacle/Altairus.lua
@@ -39,11 +39,6 @@ function mod:BreathTarget()
 		local uId = DBM:GetRaidUnitId(targetname)
 		if uId then
 			local inRange = CheckInteractDistance(uId, 2)
-			local x, y = GetPlayerMapPosition(uId)
-			if x == 0 and y == 0 then
-				SetMapToCurrentZone()
-				x, y = GetPlayerMapPosition(uId)
-			end
 			if inRange then
 				specWarnBreathNear:Show(targetname)
 			end
