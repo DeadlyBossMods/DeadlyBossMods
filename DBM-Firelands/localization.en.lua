f7f92cc6f9a7dae8a65bbcc3e8257ac22fa9787f
Adam <MysticalOS@users.noreply.github.com>
2011-08-30 05:55:10 +0000

    add a near warning and arrow for leap on staghelm

--- DBM-Firelands/localization.en.lua
+++ DBM-Firelands/localization.en.lua
@@ -154,6 +154,7 @@ L:SetOptionLocalization({
 	timerNextSpecial			= "Show timer for next special ability",
 	RangeFrameSeeds				= "Show range frame (12) for $spell:98450",
 	RangeFrameCat				= "Show range frame (10) for $spell:98374",
+	LeapArrow					= "Show DBM arrow when $spell:98476 is near you",
 	IconOnLeapingFlames			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(100208)
 })
 
