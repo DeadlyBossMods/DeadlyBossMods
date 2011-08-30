4d18f0e8e648e781ecee6841bfa335f288df6e5e
Adam <MysticalOS@users.noreply.github.com>
2011-08-30 05:16:36 +0000

    Added run away sound to fixate on ragnaros (how did i not notice this was missing?). Populated the world bosses, mostly drycodes as although i fought them many times i've only logged julak. Fixed a couple bugs on julak

--- DBM-Party-Cataclysm/WorldBosses/Garr.lua
+++ DBM-Party-Cataclysm/WorldBosses/Garr.lua
@@ -9,6 +9,26 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
-	"SPELL_CAST_SUCCESS"
+	"SPELL_CAST_START"
 )
 
+local warnMassiveEruption		= mod:NewCastAnnounce(93508, 4)--An attack that one shots anyone not in a twilight zone.
+
+local specWarnMassiveEruption	= mod:NewSpecialWarningSpell(93508, mod:IsMelee())
+
+--local timerMassiveEruptionCD	= mod:NewNextTimer(28.5, 93508)
+
+local soundMassiveEruption		= mod:NewSound(93508, nil, mod:IsMelee())
+
+function mod:OnCombatStart(delay)
+	--timerMassiveEruptionCD:Start(-delay)
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(93492) then--Possibly use 93507 (Magma Shackles) instead if it's always cast before eruption, for an earlier warning?
+		warnMassiveEruption:Show()
+		specWarnMassiveEruption:Show()
+		soundMassiveEruption:Play()
+--		timerMassiveEruptionCD:Start()
+	end
+end
