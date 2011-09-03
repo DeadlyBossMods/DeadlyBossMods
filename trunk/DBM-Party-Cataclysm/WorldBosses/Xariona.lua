05d5b82333dd289ad72d0264b4a491dfb5d98a1b
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-09-03 17:00:57 +0000

    Updates for mobus and xariona

--- trunk/DBM-Party-Cataclysm/WorldBosses/Xariona.lua
+++ trunk/DBM-Party-Cataclysm/WorldBosses/Xariona.lua
@@ -13,16 +13,28 @@ mod:RegisterEvents(
 )
 
 local warnTwilightZone			= mod:NewSpellAnnounce(93553, 2)--Used for protection against UnleashedMagic
-local warnTwilightFissure		= mod:NewSpellAnnounce(93546, 3)--Typical void zone.
+local warnTwilightFissure		= mod:NewTargetAnnounce(93546, 3)--Typical void zone.
 local warnUnleashedMagic		= mod:NewCastAnnounce(93556, 4)--An attack that one shots anyone not in a twilight zone.
 
 local specWarnUnleashedMagic	= mod:NewSpecialWarningSpell(93556, nil, nil, nil, true)
+local specWarnTwilightFissure	= mod:NewSpecialWarningYou(93546)
 
---local timerTwilightZoneCD		= mod:NewNextTimer(28.5, 93553)
---local timerUnleashedMagicCD	= mod:NewNextTimer(28.5, 93494)
+local timerTwilightFissureCD	= mod:NewCDTimer(23, 93546)
+local timerTwilightZoneCD		= mod:NewNextTimer(30, 93553)
+--local timerUnleashedMagicCD	= mod:NewNextTimer(60, 93494)--She doesn't seem to use this ability if you don't have at least 3 ranged in group. Melee comps or tiny groups avoid this completely.
+
+function mod:FissureTarget()
+	local targetname = self:GetBossTarget(50061)
+	if not targetname then return end
+	warnTwilightFissure:Show(targetname)
+	if targetname == UnitName("player") then
+		specWarnTwilightFissure:Show()
+	end
+end
 
 function mod:OnCombatStart(delay)
-	--timerTwilightZoneCD:Start(-delay)
+	timerTwilightZoneCD:Start(-delay)--Not a large sample size but seems like it'd be right.
+	timerTwilightFissureCD:Start(-delay)--May not be right, not a large sample size
 	--timerUnleashedMagicCD:Start(-delay)
 end
 
@@ -31,13 +43,15 @@ function mod:SPELL_CAST_START(args)
 		warnUnleashedMagic:Show()
 		specWarnUnleashedMagic:Show()
 --		timerUnleashedMagicCD:Start()
+	elseif args:IsSpellID(93546) then--Assumed spell event, could be spell summon or something else.
+		self:ScheduleMethod(0.2, "FissureTarget")
+		timerTwilightFissureCD:Start()
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(93553) then--Assumed spell event, could be spell summon or something else.
 		warnTwilightZone:Show()
-	elseif args:IsSpellID(93546) then--Assumed spell event, could be spell summon or something else.
-		warnTwilightFissure:Show()
+		timerTwilightZoneCD:Start()
 	end
 end
