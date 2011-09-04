e184613a6b0cb10d56264a32670b70bf3b3c5d40
Adam <MysticalOS@users.noreply.github.com>
2011-09-04 09:25:05 +0000

    working combat detection for world bosses

--- DBM-Party-Cataclysm/WorldBosses/Akmahat.lua
+++ DBM-Party-Cataclysm/WorldBosses/Akmahat.lua
@@ -4,6 +4,7 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(50063)
 mod:SetModelID(34573)
+mod:SetZone(748, 720)--Uldum (both versions of it)
 
 mod:RegisterCombat("combat")
 
