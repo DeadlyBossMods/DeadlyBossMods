5a7e777ae0408f996d0dc0916a72b8239723d1c6
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-09-10 13:02:01 +0000

    add antimagic pulse in as well

--- trunk/DBM-Party-Cataclysm/WorldBosses/Garr.lua
+++ trunk/DBM-Party-Cataclysm/WorldBosses/Garr.lua
@@ -9,14 +9,17 @@ mod:SetZone(606, 683)--Hyjal (both versions of it)
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
-	"SPELL_CAST_START"
+	"SPELL_CAST_START",
+	"SPELL_CAST_SUCCESS"
 )
 
+local warnAntiMagicPulse		= mod:NewCastAnnounce(93506, 2)--An attack that one shots anyone not in a twilight zone.
 local warnMassiveEruption		= mod:NewCastAnnounce(93508, 4)--An attack that one shots anyone not in a twilight zone.
 
 local specWarnMassiveEruption	= mod:NewSpecialWarningSpell(93508, mod:IsMelee())
 
 local timerMassiveEruptionCD	= mod:NewNextTimer(30, 93508)
+local timerAntiMagicPulseCD		= mod:NewCDTimer(17, 93506)--Every 17-25 seconds. So only a CD bar usuable here.
 
 local soundMassiveEruption		= mod:NewSound(93508, nil, mod:IsMelee())
 
@@ -30,5 +33,12 @@ function mod:SPELL_CAST_START(args)
 		specWarnMassiveEruption:Show()
 		soundMassiveEruption:Play()
 		timerMassiveEruptionCD:Start()
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(93506) then--Possibly use 93507 (Magma Shackles) instead if it's always cast before eruption, for an earlier warning?
+		warnAntiMagicPulse:Show()
+		timerAntiMagicPulseCD:Start()
 	end
 end
