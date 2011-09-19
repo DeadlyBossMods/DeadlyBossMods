b08ec60df7a438f669bd5e0d25e6d765d8e529cd
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-09-19 20:45:20 +0000

    Added dungeon boss dummies according to: "Patch 4.3 Dungeons Preview Part One & Two"

---
+++ trunk/DBM-Party-Cataclysm/EndTime/Sylvanas.lua
@@ -0,0 +1,12 @@
+local mod	= DBM:NewMod("EchoSylvanas", "DBM-Party-Cataclysm", 12)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision:$"):sub(12, -3))
+mod:SetCreatureID(12345)
+mod:SetModelID(12345)
+mod:SetZone()
+
+mod:RegisterCombat("combat")
+
+mod:RegisterEvents(
+)
---
+++ trunk/DBM-Party-Cataclysm/WellOfEternity/Azshara.lua
@@ -0,0 +1,12 @@
+local mod	= DBM:NewMod("Azshara", "DBM-Party-Cataclysm", 13)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision:$"):sub(12, -3))
+mod:SetCreatureID(12345)
+mod:SetModelID(12345)
+mod:SetZone()
+
+mod:RegisterCombat("combat")
+
+mod:RegisterEvents(
+)
