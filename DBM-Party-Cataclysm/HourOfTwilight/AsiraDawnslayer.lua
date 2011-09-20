0f4fe020b4617487a09ed9c45b6eb1413e3eee71
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-09-20 07:31:07 +0000

    Revision property

--- DBM-Party-Cataclysm/HourOfTwilight/AsiraDawnslayer.lua
+++ DBM-Party-Cataclysm/HourOfTwilight/AsiraDawnslayer.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("AsiraDawnslayer", "DBM-Party-Cataclysm", 14)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision:$"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(12345)
 mod:SetModelID(12345)
 mod:SetZone()
