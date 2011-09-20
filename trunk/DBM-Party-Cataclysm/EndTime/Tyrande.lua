359e6fe7c79780f383e664ddc83b9b32918756d5
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-09-20 07:31:07 +0000

    Revision property

--- trunk/DBM-Party-Cataclysm/EndTime/Tyrande.lua
+++ trunk/DBM-Party-Cataclysm/EndTime/Tyrande.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("EchoTyrande", "DBM-Party-Cataclysm", 12)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision:$"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(12345)
 mod:SetModelID(12345)
 mod:SetZone()
