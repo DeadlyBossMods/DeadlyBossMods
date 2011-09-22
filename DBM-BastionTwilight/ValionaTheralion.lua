f414d9485815d728b320820290467c48eefef994
Adam <MysticalOS@users.noreply.github.com>
2011-09-22 04:03:24 +0000

    remove some debugging code

--- DBM-BastionTwilight/ValionaTheralion.lua
+++ DBM-BastionTwilight/ValionaTheralion.lua
@@ -24,8 +24,7 @@ mod:RegisterEventsInCombat(
 	"SPELL_HEAL",
 	"SPELL_PERIODIC_HEAL",
 	"RAID_BOSS_EMOTE",
-	"UNIT_AURA",
-	"UNIT_SPELLCAST_SUCCEEDED"
+	"UNIT_AURA"
 )
 
 --Valiona Ground Phase
@@ -360,10 +359,3 @@ function mod:UNIT_AURA(uId)
 	end
 end
 
---Guesswork on event, another fight i'll need to re-transcribe with new working version.
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, spellName)
-	if spellName == GetSpellInfo(86497) then
-		print("Fabulous Flames cast detected")
-		DBM.Bars:CreateBar(15, "Fab Flames Test Bar")
-	end
-end
