bd11cfa21bf122b04833e52080fd6727a50c34e0
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-09-27 20:48:43 +0000

    #CurseBeta tag

--- trunk/DBM-Firelands/Ragnaros.lua
+++ trunk/DBM-Firelands/Ragnaros.lua
@@ -555,7 +555,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, spellName)
 		if self.Options.AggroFrame then--Show aggro frame regardless if health frame is still up, it should be more important than health frame at this point. Shouldn't be blowing up traps while elementals are up.
 			DBM.InfoFrame:SetHeader(L.NoAggro)
 			if self:IsDifficulty("normal25", "heroic25") then
-				DBM.InfoFrame:Show(10, "playeraggro", 0)--20 man has at least 5 targets without aggro, often more do to immunities. because of it's size i may just remove this feature from 25 mans. it's now off by default.
+				DBM.InfoFrame:Show(10, "playeraggro", 0)--20 man has at least 5 targets without aggro, often more do to immunities. because of it's size, it's now off by default.
 			else
 				DBM.InfoFrame:Show(5, "playeraggro", 0)
 			end
