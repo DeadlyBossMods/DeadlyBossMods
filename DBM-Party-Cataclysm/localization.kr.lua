cb90fba5f4df51e0443a3cdf9a3e2b89f0887aa1
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-09-28 08:43:04 +0000

    Murozond dummy file + localizations

--- DBM-Party-Cataclysm/localization.kr.lua
+++ DBM-Party-Cataclysm/localization.kr.lua
@@ -1157,6 +1157,27 @@ L:SetOptionLocalization{
 L:SetMiscLocalization{
 }
 
+--------------
+-- Murozond --
+--------------
+L = DBM:GetModLocalization("Murozond")
+
+L:SetGeneralLocalization{
+	name = "Murozond"
+}
+
+L:SetWarningLocalization{
+}
+
+L:SetTimerLocalization{
+}
+
+L:SetOptionLocalization{
+}
+
+L:SetMiscLocalization{
+}
+
 ------------------------
 --  Well of Eternity  --
 ------------------------
