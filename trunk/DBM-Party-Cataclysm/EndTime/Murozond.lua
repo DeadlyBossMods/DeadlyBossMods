77f9eb21ac4fc71763576fd079a1ee1c42170463
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-09-28 08:40:30 +0000

    4.3 PTR Model IDs

--- trunk/DBM-Party-Cataclysm/EndTime/Murozond.lua
+++ trunk/DBM-Party-Cataclysm/EndTime/Murozond.lua
@@ -1,9 +1,9 @@
-local mod	= DBM:NewMod("EchoJaina", "DBM-Party-Cataclysm", 12)
+local mod	= DBM:NewMod("Murozond", "DBM-Party-Cataclysm", 12)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(12345)
-mod:SetModelID(38802)
+mod:SetModelID(38931)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
