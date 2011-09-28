c8d99dc60463bf568df3d21468fd127ea6a22908
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-09-28 08:40:30 +0000

    4.3 PTR Model IDs

--- DBM-Party-Cataclysm/EndTime/Baine.lua
+++ DBM-Party-Cataclysm/EndTime/Baine.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(12345)
-mod:SetModelID(12345)
+mod:SetModelID(38791)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
