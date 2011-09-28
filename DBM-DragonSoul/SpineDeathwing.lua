0525b2323f93a28802ea4820725cfba790f0dc7e
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-09-28 08:56:23 +0000

    4.3 PTR Model IDs

--- DBM-DragonSoul/SpineDeathwing.lua
+++ DBM-DragonSoul/SpineDeathwing.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(12345)
-mod:SetModelID(12345)
+mod:SetModelID(35268)
 mod:SetZone()
 mod:SetUsedIcons()
 
