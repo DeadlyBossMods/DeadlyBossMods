8958c37418e64c36752f5a96f4a4cd0f1926e06c
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-10-05 22:37:43 +0000

    2 model IDs changed this PTR patch

--- DBM-Party-Cataclysm/EndTime/Tyrande.lua
+++ DBM-Party-Cataclysm/EndTime/Tyrande.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(54544)
-mod:SetModelID(38918)
+mod:SetModelID(39617)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
