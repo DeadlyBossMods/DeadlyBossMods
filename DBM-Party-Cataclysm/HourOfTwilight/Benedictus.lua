4a175117b5405f26f03cfd67a61e3bf873f3923a
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-10-06 12:27:37 +0000

    small updates to Hour of Twilight & End Time (PTR)

--- DBM-Party-Cataclysm/HourOfTwilight/Benedictus.lua
+++ DBM-Party-Cataclysm/HourOfTwilight/Benedictus.lua
@@ -16,11 +16,11 @@ mod:RegisterEventsInCombat(
 
 local warnRighteousShear	= mod:NewSpellAnnounce(103151, 2)
 local warnPurifyingLight	= mod:NewSpellAnnounce(103565, 3)
-local warnWaveVirtue		= mod:NewSpellAnnounce(103678, 4)		-- not seen in the log, but its in the dungeon journal
+local warnWaveVirtue		= mod:NewSpellAnnounce(103681, 4, nil, false)	-- blizzard warning
 local prewarnP2			= mod:NewPrePhaseAnnounce(2, 2)
 local warnTwilightShear		= mod:NewSpellAnnounce(103363, 2)
 local warnCorruptingTwilight	= mod:NewSpellAnnounce(103767, 3)
-local warnWaveTwilight		= mod:NewSpellAnnounce(103783, 4)
+local warnWaveTwilight		= mod:NewSpellAnnounce(103784, 4, nil, false)	-- blizzard warning
 -- warn actual 'phase' change?  (Twilight Epiphany (ID = 103754))
 
 
@@ -40,22 +40,22 @@ function mod:SPELL_CAST_SUCCESS(args)
 		warnRighteousShear:Show()
 	elseif args:IsSpellID(103565) then
 		warnPurifyingLight:Show()
-	elseif args:IsSpellID(103678) then
+	elseif args:IsSpellID(103681) then
 		warnWaveVirtue:Show()
 	elseif args:IsSpellID(103363) then
 		warnTwilightShear:Show()
 	elseif args:IsSpellID(103767) then
 		warnCorruptingTwilight:Show()
-	elseif args:IsSpellID(103783) then
+	elseif args:IsSpellID(103784) then
 		warnWaveTwilight:Show()
 	end
 end
 
 function mod:SPELL_DAMAGE(args)
-	if args:IsSpellID(103653) and GetTime() - spamDamage > 5 then
+	if args:IsSpellID(103653) and args:IsPlayer() and GetTime() - spamDamage > 5 then
 		specwarnPurified:Show()
 		spamDamage = GetTime()
-	elseif args:IsSpellID(103775) and GetTime() - spamDamage > 5 then
+	elseif args:IsSpellID(103775) and args:IsPlayer() and GetTime() - spamDamage > 5 then
 		specwarnTwilight:Show()
 		spamDamage = GetTime()
 	end
@@ -73,14 +73,14 @@ function mod:UNIT_HEALTH(uId)
 	end
 end
 
+--: 14:05:48.764
 
-
--- Timers from 1 log so they most likely are incorrect :)
+-- Timers from 2 logs so they most likely are incorrect :)  (?? = unknown, didnt happen)
 -- "Holy" timers: X secs after combat start
-	-- 1st Purifying Light after 4secs?
-	-- 1st Wave Of Virtue after 25secs? (not in the log, phase change happened after 21secs)
+	-- 1st Purifying Light after [4 or 5] secs
+	-- 1st Wave Of Virtue after [?? or 24] secs 
 
 -- "Shadow" timers:  X secs after SPELL_AURA_APPLIED event for Twilight Epiphany
-	-- 1st Twilight Shear after 15 secs
-	-- 1st Corrupting Twilight after 17 secs
-	-- 1st Wave of Twilight after 37 secs
+	-- 1st Twilight Shear after [15 or 12] secs
+	-- 1st Corrupting Twilight after [17 or 17] secs
+	-- 1st Wave of Twilight after [37 or 36] secs
