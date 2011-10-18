2a38b61fb3fde8023ea216c096a555bce381c4fd
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-10-18 18:02:56 +0000

    updated creature IDs & model IDs

--- DBM-DragonSoul/Morchok.lua
+++ DBM-DragonSoul/Morchok.lua
@@ -4,7 +4,7 @@ local mod	= DBM:NewMod(311, "DBM-DragonSoul", nil, 187)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(12345)
+mod:SetCreatureID(55265)
 mod:SetModelID(39094)
 mod:SetZone()
 mod:SetUsedIcons()
