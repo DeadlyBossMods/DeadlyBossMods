3e6d442870ca17c9fa1de9a76d54bf76faaeedcc
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-10-27 12:49:17 +0000

    Morchok stuff based on LFRaid log

--- trunk/DBM-DragonSoul/Morchok.lua
+++ trunk/DBM-DragonSoul/Morchok.lua
@@ -11,5 +11,79 @@ mod:SetUsedIcons()
 
 mod:RegisterCombat("combat")
 
-mod:RegisterEvents(
+mod:RegisterEventsInCombat(
+	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_APPLIED_DOSE",
+	"SPELL_CAST_START",
+	"SPELL_CAST_SUCCESS",
+	"SPELL_DAMAGE"
 )
+
+local warnCrushArmor	= mod:NewStackAnnounce(103687, 3, nil, mod:IsTank() or mod:IsHealer())
+local warnCrystal	= mod:NewSpellAnnounce(103640, 3)
+local warnStomp		= mod:NewSpellAnnounce(108571, 3)
+local warnVortex	= mod:NewSpellAnnounce(110047, 4)
+local warnFurious	= mod:NewSpellAnnounce(103846, 3)
+
+local timerCrushArmor	= mod:NewStackTimer(20, 103687, nil, mod:IsTank())
+local timerCrystal	= mod:NewCDTimer(12, 103640)	-- 12-14sec variation (is also time till 'detonate')
+local timerStomp 	= mod:NewCDTimer(12, 108571)	-- 12-14sec variation
+local timerVortex	= mod:NewDurationTimer(5, 110047)
+local timerVortexNext	= mod:NewNextTimer(97, 110047)
+local timerBlood	= mod:NewDurationTimer(19, 103851)
+
+local specwarnCrushArmor	= mod:NewSpecialWarningStack(103687, 3, mod:IsTank())
+local specwarnVortexAfter	= mod:NewSpecialWarning("SpecwarnVortexAfter")	-- show a specwarning when Vortex ends
+local specwarnBlood		= mod:NewSpecialWarningMove(108570)
+
+local spamCrystal = 0		-- might be just on PTR, but 2 events are shown for 1 crystal
+local spamBlood = 0
+function mod:OnCombatStart(delay)
+	spamCrystal = 0
+	spamBlood = 0
+	timerCrystal:Start(-delay)
+	timerStomp:Start(6-delay)	-- first after 6-8secs
+	timerVortex:Start(50-delay)
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(103687) then
+		warnCrushArmor:Show(args.destName, args.amount or 1)
+		timerCrushArmor:Show()
+		if (args.amount or 1) > 3 then
+			specwarnCrushArmor:Show(args.amount or 1)
+		end
+	elseif args:IsSpellID(103851) then
+		timerBlood:Start()
+	elseif args:IsSpellID(103846) then
+		warnFurious:Show()
+	end
+end
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(108571) then
+		warnStomp:Show()
+		timerStomp:Start()
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(103640) and GetTime() - spamCrystal > 3 then
+		warnCrystal:Show()
+		timerCrystal:Start()
+		spamCrystal = GetTime()
+	elseif args:IsSpellID(110047) then
+		warnVortex:Show()
+		timerVortex:Start()
+		timerVortexNext:Start()
+		specwarnAfterVortex:Schedule(5)
+	end
+end
+
+function mod:SPELL_DAMAGE(args)
+	if args:IsSpellID(108570) and GetTime - spamBlood > 3 then
+		specwarnBlood:Show()
+		spamBlood = GetTime()
+	end
+end
