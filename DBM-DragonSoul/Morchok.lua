2bf2c68750ba8351e3f8089b63cfab8bd20d342e
Adam <MysticalOS@users.noreply.github.com>
2011-10-27 14:12:16 +0000

    Fix morchok, no such thing as a stacktimer or duration timer :P drycoded some additional difficulty spellids from wowhead data

--- DBM-DragonSoul/Morchok.lua
+++ DBM-DragonSoul/Morchok.lua
@@ -20,24 +20,25 @@ mod:RegisterEventsInCombat(
 )
 
 local warnCrushArmor	= mod:NewStackAnnounce(103687, 3, nil, mod:IsTank() or mod:IsHealer())
-local warnCrystal	= mod:NewSpellAnnounce(103640, 3)
-local warnStomp		= mod:NewSpellAnnounce(108571, 3)
-local warnVortex	= mod:NewSpellAnnounce(110047, 4)
-local warnFurious	= mod:NewSpellAnnounce(103846, 3)
-
-local timerCrushArmor	= mod:NewStackTimer(20, 103687, nil, mod:IsTank())
-local timerCrystal	= mod:NewCDTimer(12, 103640)	-- 12-14sec variation (is also time till 'detonate')
-local timerStomp 	= mod:NewCDTimer(12, 108571)	-- 12-14sec variation
-local timerVortex	= mod:NewDurationTimer(5, 110047)
-local timerVortexNext	= mod:NewNextTimer(97, 110047)
-local timerBlood	= mod:NewDurationTimer(19, 103851)
+local warnCrystal		= mod:NewSpellAnnounce(103640, 3)
+local warnStomp			= mod:NewSpellAnnounce(108571, 3)
+local warnVortex		= mod:NewSpellAnnounce(110047, 4)
+local warnFurious		= mod:NewSpellAnnounce(103846, 3)
 
 local specwarnCrushArmor	= mod:NewSpecialWarningStack(103687, 3, mod:IsTank())
 local specwarnVortexAfter	= mod:NewSpecialWarning("SpecwarnVortexAfter")	-- show a specwarning when Vortex ends
-local specwarnBlood		= mod:NewSpecialWarningMove(108570)
+local specwarnBlood			= mod:NewSpecialWarningMove(108570)
+
+local timerCrushArmor	= mod:NewTargetTimer(20, 103687, nil, mod:IsTank())
+local timerCrystal		= mod:NewCDTimer(12, 103640)	-- 12-14sec variation (is also time till 'detonate')
+local timerStomp 		= mod:NewCDTimer(12, 108571)	-- 12-14sec variation
+local timerVortex		= mod:NewBuffActiveTimer(5, 110047)
+local timerVortexNext	= mod:NewNextTimer(97, 110047)
+local timerBlood		= mod:NewBuffActiveTimer(19, 103851)
 
 local spamCrystal = 0		-- might be just on PTR, but 2 events are shown for 1 crystal
 local spamBlood = 0
+
 function mod:OnCombatStart(delay)
 	spamCrystal = 0
 	spamBlood = 0
@@ -49,7 +50,7 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(103687) then
 		warnCrushArmor:Show(args.destName, args.amount or 1)
-		timerCrushArmor:Show()
+		timerCrushArmor:Start(args.destName)
 		if (args.amount or 1) > 3 then
 			specwarnCrushArmor:Show(args.amount or 1)
 		end
@@ -62,7 +63,7 @@ end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(108571) then
+	if args:IsSpellID(103414, 108571, 109033, 109034) then
 		warnStomp:Show()
 		timerStomp:Start()
 	end
@@ -73,7 +74,7 @@ function mod:SPELL_CAST_SUCCESS(args)
 		warnCrystal:Show()
 		timerCrystal:Start()
 		spamCrystal = GetTime()
-	elseif args:IsSpellID(110047) then
+	elseif args:IsSpellID(103821, 110045, 110046, 110047) then
 		warnVortex:Show()
 		timerVortex:Start()
 		timerVortexNext:Start()
@@ -82,7 +83,7 @@ function mod:SPELL_CAST_SUCCESS(args)
 end
 
 function mod:SPELL_DAMAGE(args)
-	if args:IsSpellID(108570) and GetTime - spamBlood > 3 then
+	if args:IsSpellID(108570, 103179, 103785) and GetTime - spamBlood > 3 then
 		specwarnBlood:Show()
 		spamBlood = GetTime()
 	end
