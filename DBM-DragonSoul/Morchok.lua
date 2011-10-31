52fb85cc5706ef68d6961e50b860507ce7b7082f
Adam <MysticalOS@users.noreply.github.com>
2011-10-31 21:02:24 +0000

    Morchok improvements. Mainly to timer efficiency, don't start timers when there won't be another cast (ie vortex is next), and don't start timers so early when there are better later places to start them.

--- DBM-DragonSoul/Morchok.lua
+++ DBM-DragonSoul/Morchok.lua
@@ -14,15 +14,18 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_APPLIED_DOSE",
+	"SPELL_AURA_REMOVED",
 	"SPELL_CAST_START",
+	"SPELL_SUMMON",
 	"SPELL_CAST_SUCCESS",
 	"SPELL_DAMAGE"
 )
 
 local warnCrushArmor	= mod:NewStackAnnounce(103687, 3, nil, mod:IsTank() or mod:IsHealer())
-local warnCrystal		= mod:NewSpellAnnounce(103640, 3)
+local warnCrystal		= mod:NewSpellAnnounce(103639, 3)
 local warnStomp			= mod:NewSpellAnnounce(108571, 3)
-local warnVortex		= mod:NewSpellAnnounce(110047, 4)
+local warnVortex		= mod:NewSpellAnnounce(110047, 3)
+local warnBlood			= mod:NewSpellAnnounce(103851, 4)
 local warnFurious		= mod:NewSpellAnnounce(103846, 3)
 
 local specwarnCrushArmor	= mod:NewSpecialWarningStack(103687, mod:IsTank(), 3)
@@ -33,18 +36,18 @@ local timerCrushArmor	= mod:NewTargetTimer(20, 103687, nil, mod:IsTank())
 local timerCrystal		= mod:NewCDTimer(12, 103640)	-- 12-14sec variation (is also time till 'detonate')
 local timerStomp 		= mod:NewCDTimer(12, 108571)	-- 12-14sec variation
 local timerVortex		= mod:NewBuffActiveTimer(5, 110047)
-local timerVortexNext	= mod:NewNextTimer(97, 110047)
-local timerBlood		= mod:NewBuffActiveTimer(19, 103851)
+local timerVortexNext	= mod:NewNextTimer(71, 110047)--97 sec after last vortex, but only 71 after last blood ended. More efficent this way.
+local timerBlood		= mod:NewBuffActiveTimer(21, 103851)
 
-local spamCrystal = 0		-- might be just on PTR, but 2 events are shown for 1 crystal
 local spamBlood = 0
+local crystalCount = 0--3 crystals between each vortex
 
 function mod:OnCombatStart(delay)
-	spamCrystal = 0
 	spamBlood = 0
-	timerCrystal:Start(-delay)
-	timerStomp:Start(6-delay)	-- first after 6-8secs
-	timerVortex:Start(50-delay)
+	crystalCount = 1--2 before first however so we superficially set it to 1 on pull.
+	timerStomp:Start(-delay)
+	timerCrystal:Start(19-delay)
+	timerVortex:Start(54-delay)
 end
 
 function mod:SPELL_AURA_APPLIED(args)
@@ -54,36 +57,53 @@ function mod:SPELL_AURA_APPLIED(args)
 		if (args.amount or 1) > 3 then
 			specwarnCrushArmor:Show(args.amount or 1)
 		end
-	elseif args:IsSpellID(103851) then
-		timerBlood:Start()
 	elseif args:IsSpellID(103846) then
 		warnFurious:Show()
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
+function mod:SPELL_AURA_REMOVED(args)
+	if args:IsSpellID(103851) then
+		timerStomp:Start(15)
+		timerCrystal:Start(21)
+		timerVortexNext:Start()
+	end
+end
+
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(103414, 108571, 109033, 109034) then
 		warnStomp:Show()
-		timerStomp:Start()
+		if crystalCount < 3 then
+			timerStomp:Start()
+		end
+	elseif args:IsSpellID(103851) then
+		warnBlood:Show()
+		timerBlood:Start()
+		specwarnAfterVortex:Show()
+	end
+end
+
+function mod:SPELL_SUMMON(args)
+	if args:IsSpellID(103639) then
+		crystalCount = crystalCount + 1
+		warnCrystal:Show()
+		if crystalCount < 3 then
+			timerCrystal:Start()
+		end
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(103640) and GetTime() - spamCrystal > 3 then
-		warnCrystal:Show()
-		timerCrystal:Start()
-		spamCrystal = GetTime()
-	elseif args:IsSpellID(103821, 110045, 110046, 110047) then
+	if args:IsSpellID(103821, 110045, 110046, 110047) then
+		crystalCount = 0
 		warnVortex:Show()
 		timerVortex:Start()
-		timerVortexNext:Start()
-		specwarnAfterVortex:Schedule(5)
 	end
 end
 
 function mod:SPELL_DAMAGE(args)
-	if args:IsSpellID(108570, 103179, 103785) and GetTime - spamBlood > 3 then
+	if args:IsSpellID(103785, 108570, 110287, 110288) and GetTime - spamBlood > 3 then--103785 10 man confirmed.
 		specwarnBlood:Show()
 		spamBlood = GetTime()
 	end
