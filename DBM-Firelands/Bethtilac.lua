a298324682242d66b2367d5b7ce111a79f4b484e
Adam <MysticalOS@users.noreply.github.com>
2011-11-04 01:37:36 +0000

    herp, SPELL_CAST_SUCCESS wasn't registered, sigh, so no wonder ember timers never worked

--- DBM-Firelands/Bethtilac.lua
+++ DBM-Firelands/Bethtilac.lua
@@ -12,6 +12,7 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED",
 	"SPELL_CAST_START",
+	"SPELL_CAST_SUCCESS",
 	"SPELL_DAMAGE",
 	"SPELL_MISSED",
 	"RAID_BOSS_EMOTE"
