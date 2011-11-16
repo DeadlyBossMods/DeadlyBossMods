033951c6d2bdc94d66faa987dc70e4bc729d1103
Adam <MysticalOS@users.noreply.github.com>
2011-11-16 22:15:17 +0000

    more fixes to ultraxion, on normal the Cd on fading light is 15, not 10.

--- DBM-DragonSoul/Ultraxion.lua
+++ DBM-DragonSoul/Ultraxion.lua
@@ -32,7 +32,7 @@ local timerCombatStart				= mod:NewTimer(35, "TimerCombatStart", 2457)
 local timerHourofTwilightCD			= mod:NewNextTimer(45, 109416)
 local timerTwilightEruptionCD		= mod:NewNextTimer(360, 106388)--Berserk timer more or less.
 local timerTwilightEruption			= mod:NewCastTimer(5, 106388)
-local timerFadingLightCD			= mod:NewNextTimer(10, 110080)
+local timerFadingLightCD			= mod:NewNextTimer(10, 110080)--10 second on heroic, 15 on normal
 
 local FadingLightCountdown			= mod:NewCountdown(10, 110080)--5-10 second variation that's random according to EJ
 
@@ -60,6 +60,11 @@ function mod:SPELL_CAST_START(args)
 		warnHourofTwilight:Show()
 		specWarnHourofTwilight:Show()
 		timerHourofTwilightCD:Start()
+		if self:IsDifficulty("heroic10", "heroic25") then
+			timerFadingLightCD:Start(15)
+		else
+			timerFadingLightCD:Start(20)--Same in raid finder too? too many difficulties now
+		end
 	elseif args:IsSpellID(106388) then
 		specWarnTwilightEruption:Show()
 		timerTwilightEruption:Start()
@@ -68,25 +73,27 @@ end
 
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpellID(106371, 109415, 109416, 109417) then
-		timerFadingLightCD:Start()--Start first fading light Cd here when buff is actually gained, that way we don't have to account for normal/heroic having variable cast lengths in SPELL_CAST_START event
-	elseif args:IsSpellID(110068, 110069, 110078, 110079) then--Damage taken IDs, tank specific debuffs.
+	if args:IsSpellID(110068, 110069, 110078, 110079) then--Damage taken IDs, tank specific debuffs.
 		fadingLightCount = fadingLightCount + 1
 		fadingLightTargets[#fadingLightTargets + 1] = args.destName
-		if fadingLightCount < 3 then--It's cast 3 times during hour of twilight buff duration on ultraxion. 20 secomds remaining, 10 seconds remaining, and at 0 seconds remainings.
+		if self:IsDifficulty("heroic10", "heroic25") and fadingLightCount < 3 then--It's cast 3 times during hour of twilight buff duration on ultraxion heroic. 20 secomds remaining, 10 seconds remaining, and at 0 seconds remainings.
 			timerFadingLightCD:Start()
+		elseif self:IsDifficulty("normal10", "normal25") and fadingLightCount < 2 then--It's cast 2 times during hour of twilight buff duration on ultraxion normal. 15 secomds remaining and at 0 seconds remainings.
+			timerFadingLightCD:Start(15)
+		--elseif self:IsDifficulty("lfr25") then
+			--timerFadingLightCD:Start(15)--Same in raid finder as normal?
 		end
 		if args:IsPlayer() then
-			local _, _, _, _, _, duration, expires, _, _ = UnitDebuff("player", GetSpellInfo(105925))--Find out what our specific seed timer is
+			local _, _, _, _, _, duration, expires, _, _ = UnitDebuff("player", args.spellName)--Find out what our specific fading light is
 			specWarnFadingLight:Show()
-			FadingLightCountdown:Start(duration)
+			FadingLightCountdown:Start(expires - GetTime() - 5)
 		end
 		self:Unschedule(warnFadingLightTargets)
 		self:Schedule(0.3, warnFadingLightTargets)
 	elseif args:IsSpellID(105925, 109075, 110070, 110080) then--Damage done IDs, dps/healer debuffs
 		fadingLightTargets[#fadingLightTargets + 1] = args.destName
 		if args:IsPlayer() then
-			local _, _, _, _, _, duration, expires, _, _ = UnitDebuff("player", GetSpellInfo(105925))--Find out what our specific fading light is
+			local _, _, _, _, _, duration, expires, _, _ = UnitDebuff("player", args.spellName)--Find out what our specific fading light is
 			specWarnFadingLight:Show()
 			FadingLightCountdown:Start(expires - GetTime() - 5)
 		end
