d2dbd93470e144cc9910a4ccbc50d1e742411649
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-11-29 15:29:10 +0000

    updated 5 man mapsizes

--- trunk/DBM-Party-Cataclysm/HourOfTwilight/MapSizes.lua
+++ trunk/DBM-Party-Cataclysm/HourOfTwilight/MapSizes.lua
@@ -1,3 +1,4 @@
---DBM:RegisterMapSize("EndTime",
---	0, 1200, 400
---)
+DBM:RegisterMapSize("HourofTwilight",
+	1, 3043.7498779297, 2029.1665039062,
+	2, 0, 0
+)
--- trunk/DBM-Party-Cataclysm/WellOfEternity/MapSizes.lua
+++ trunk/DBM-Party-Cataclysm/WellOfEternity/MapSizes.lua
@@ -1,3 +1,3 @@
---DBM:RegisterMapSize("EndTime",
---	0, 1200, 400
---)
+DBM:RegisterMapSize("WellofEternity",
+	0, 1252.0830078125, 833.3332519532--Not sure if floor is 0 or 1, will need to verify
+)
