42d72c4e20cad1c04077368899a5e78e859b471c
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-11-30 16:58:46 +0000

    Lets only announce if YOU are standing in bad stuff instead of anyone :)

--- DBM-DragonSoul/Morchok.lua
+++ DBM-DragonSoul/Morchok.lua
@@ -103,7 +103,7 @@ function mod:SPELL_CAST_SUCCESS(args)
 end
 
 function mod:SPELL_DAMAGE(args)
-	if args:IsSpellID(103785, 108570, 110287, 110288) and GetTime() - spamBlood > 3 then--103785 10 man confirmed.
+	if args:IsSpellID(103785, 108570, 110287, 110288) and args:IsPlayer() and GetTime() - spamBlood > 3 then--103785 10 man confirmed.
 		specwarnBlood:Show()
 		spamBlood = GetTime()
 	end
