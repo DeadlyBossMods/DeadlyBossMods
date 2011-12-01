96baf7b90957274ef0eb09a23c4d87c62ebc8c51
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-12-01 03:12:09 +0000

    fix counter for hour of twilight, and maybe fix counter being wrong for fading light too

--- trunk/DBM-DragonSoul/Zonozz.lua
+++ trunk/DBM-DragonSoul/Zonozz.lua
@@ -56,7 +56,9 @@ function mod:OnCombatStart(delay)
 	timerFocusedAngerCD:Start(10.5-delay)
 	timerPsychicDrainCD:Start(-delay)
 	timerShadowsCD:Start(-delay)
-	berserkTimer:Start(-delay)
+	if not self:IsDifficulty("lfr25") then--Can confirm what others saw, LFR definitely doesn't have a 6 min berserk. It's either much longer or not there.
+		berserkTimer:Start(-delay)
+	end
 end
 
 function mod:OnCombatEnd()
