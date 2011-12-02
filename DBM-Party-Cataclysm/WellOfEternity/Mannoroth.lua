dfda6ceef0986055cf84d29ad73d0e380940f13c
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-12-02 02:03:33 +0000

    # Add# Added Mannoroth mod.

--- DBM-Party-Cataclysm/WellOfEternity/Mannoroth.lua
+++ DBM-Party-Cataclysm/WellOfEternity/Mannoroth.lua
@@ -9,5 +9,31 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 mod:RegisterKill("yell", L.Kill)
 
-mod:RegisterEvents(
+mod:RegisterEventsInCombat(
+	"SPELL_AURA_APPLIED",
+	"SPELL_CAST_START"
 )
+
+local warnFelBlade			= mod:NewSpellAnnounce(103966, 3)
+
+local specWarnFelStorm		= mod:NewSpecialWarningRun(103888)
+
+local timerFelBlade			= mod:NewBuffActiveTimer(10, 103966)
+local timerFelStorm			= mod:NewBuffActiveTimer(15, 103888)
+
+function mod:OnCombatStart(delay)
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(103966) then
+		warnFelBlade:Show()
+		timerFelBlade:Start()
+	end
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(103888) then
+		specWarnFelStorm:Show()
+		timerFelStorm:Show()
+	end
+end
