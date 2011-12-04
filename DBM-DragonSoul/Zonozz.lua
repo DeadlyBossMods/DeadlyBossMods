05791127673e45ae9a85a129aa2550febfcbbbbd
Adam <MysticalOS@users.noreply.github.com>
2011-12-04 01:38:16 +0000

    do not make changes if you don't know what the hell you are doing, there is a REASON that was using spellid and not spell name

--- DBM-DragonSoul/Zonozz.lua
+++ DBM-DragonSoul/Zonozz.lua
@@ -111,9 +111,9 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end	
 
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, spellName)
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, spellName, _, _, spellID)
 	if uId ~= "boss1" then return end--Anti spam to ignore all other args (like target/focus/mouseover)
-	if spellName == GetSpellInfo(103571) then--Void of the unmaking cast, do not use spellname because we want to ignore vents using spellid 103627 which fires when the sphere dispurses on the boss.
+	if spellID == 103571 then--Void of the unmaking cast, do not use spellname because we want to ignore vents using spellid 103627 which fires when the sphere dispurses on the boss.
 		warnVoidofUnmaking:Show()
 		specWarnVoidofUnmaking:Show()
 		timerVoidofUnmakingCD:Start()
