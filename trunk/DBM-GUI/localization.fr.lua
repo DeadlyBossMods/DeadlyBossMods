93b3f2bbe4c05814ac6a54b80633aff7990d8ca4
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-12-07 09:17:56 +0000

    French localization (Hope I did add them all correctly (had to convert from ANSI to UTF8))

--- trunk/DBM-GUI/localization.fr.lua
+++ trunk/DBM-GUI/localization.fr.lua
@@ -1,155 +1,156 @@
 ﻿if GetLocale() ~= "frFR" then return end
-
 if not DBM_GUI_Translations then DBM_GUI_Translations = {} end
 local L = DBM_GUI_Translations
--- Initial translation : Psyco
--- Last update : 12/27/2010
 
-L.MainFrame 		= "Deadly Boss Mods"
+L.MainFrame = "Deadly Boss Mods"
 
-L.TranslationBy 	= "Sasmira Alias Zakinda & Obscurantys@EU-Archimonde"
+L.TranslationBy 			= "Psyco/Sasmira/Pettigrow"
 
-L.OTabBosses	= "Bosses"
+L.OTabBosses	= "Boss"
 L.OTabOptions	= "Options"
 
-L.TabCategory_Options 	= "Options Générales"
-L.TabCategory_CATA	 	= "Cataclysm"
-L.TabCategory_WOTLK 		= "Wrath of the Lich King"
-L.TabCategory_BC 		= "The Burning Crusade"
-L.TabCategory_CLASSIC 	= "WoW Classique"
-L.TabCategory_OTHER     	= "Autres Boss Mods"
+L.TabCategory_Options		= "Options générales"
+L.TabCategory_CATA			= "Cataclysm"
+L.TabCategory_WOTLK			= "Wrath of the Lich King"
+L.TabCategory_BC			= "The Burning Crusade"
+L.TabCategory_CLASSIC		= "WoW classique"
+L.TabCategory_OTHER			= "Autres modules"
 
-L.BossModLoaded 	= "%s statistiques"
-L.BossModLoad_now 	= [[Ce boss mod n'est pas chargé.
-Il sera chargé quand vous entrerez dans l'instance. 
-Vous pouvez aussi cliquer sur le bouton pour charger le mod manuellement.]]
+L.BossModLoaded 			= "Statistiques |2 %s"
+L.BossModLoad_now 			= [[Ce boss mod n'est pas chargé. 
+Il le sera une fois que vous serez dans l'instance. 
+Cliquez sur le bouton pour charger manuellement.]]
 
-L.PosX = "Position X"
-L.PosY = "Position Y"
+L.PosX						= 'Position en X'
+L.PosY						= 'Position en Y'
 
-L.MoveMe 				= "Déplacez-moi"
-L.Button_OK 			= "OK"
-L.Button_Cancel 		= "Annuler"
-L.Button_LoadMod 		= "Charger l'AddOn"
-L.Mod_Enabled			= "Activer boss mod"
-L.Mod_EnableAnnounce	= "Annoncer au raid"
-L.Reset 		= "Reset"
+L.MoveMe 					= 'Déplacez-moi'
+L.Button_OK 				= 'OK'
+L.Button_Cancel 			= 'Annuler'
+L.Button_LoadMod 			= 'Charger l\'AddOn'
+L.Mod_Enabled				= "Activer ce module"
+L.Mod_EnableAnnounce		= "Annoncer au raid (nécessite d'être le chef)"
+L.Reset 					= "Réinit."
 
-L.Enable  		= "Activer"
-L.Disable		= "Désactiver"
+L.Enable  					= "Activer"
+L.Disable					= "Désactiver"
 
-L.NoSound		= "Pas de Son"
+L.NoSound					= "Pas de son"
 
-L.IconsInUse	= "Icônes utilisées par cet addon"
+L.IconsInUse				= "Icônes utilisées par ce module"
 
 -- Tab: Boss Statistics
-L.BossStatistics		= "Statistiques des boss"
-L.Statistic_Kills		= "Tués:"
-L.Statistic_Wipes		= "Wipes:"
-L.Statistic_BestKill	= "Meilleur down:"
-L.Statistic_Heroic		= "Héroïque"
-L.Statistic_10Man			= "Raid10"
-L.Statistic_25Man			= "Raid25"
+L.BossStatistics			= "Boss Statistics"
+L.Statistic_Kills			= "Victoires :"
+L.Statistic_Wipes			= "Échecs :"
+L.Statistic_BestKill		= "Meilleur :"
+L.Statistic_Heroic			= "Héroïque"
+L.Statistic_10Man			= "Raid à 10"
+L.Statistic_25Man			= "Raid à 25"
 
 -- Tab: General Options
-L.General 			= "Options Générales de DBM"
-L.EnableDBM 		= "Activer DBM"
-L.EnableStatus 		= "Répondre au chuchotement par son 'status'"
-L.AutoRespond 		= "Activer la réponse automatique pendant les combats"
-L.EnableMiniMapIcon	= "Afficher le bouton sur minicarte "
-L.FixCLEUOnCombatStart		= "Effacer le log de combat au pull"
-L.SetCurrentMapOnPull		= "Régler la map sur la zone actuelle du pull ( Assure /range & /arrow accuracy)"
-L.SKT_Enabled				= "Toujours afficher le temps de la mort du Boss (Remplace les options spécifiques au boss)"
-L.Latency_Text				= "Régler la sync max du seuil de latence: %d"
+L.General 					= "Options générales de DBM"
+L.EnableDBM 				= "Activer DBM"
+L.EnableStatus 				= "Répondre aux chuchotements 'status'"
+L.AutoRespond 				= "Activer le répondeur pendant les rencontres"
+L.EnableMiniMapIcon			= "Afficher l'icône de la minicarte"
+L.UseMasterVolume			= "Utiliser le canal audio principal pour la lecture des fichiers sonores"
+L.SKT_Enabled				= "Tj afficher le délai pour battre le record (surplante toute autre option)"
+L.Latency_Text				= "Seuil de latence max. pour synchro : %d"
 
-L.Button_RangeFrame	= "Afficher/Cacher la fenêtre de portée"
-L.Button_TestBars	= "Lancer les barres de test"
+L.ModelOptions				= "Options du visionneur de modèle 3D"
+L.EnableModels				= "Activer les modèles 3D dans les options des boss"
+L.ModelSoundOptions			= "Option sonore pour le visionneur"
+L.ModelSoundShort			= "Court"
+L.ModelSoundLong			= "Long"
 
-L.PizzaTimer_Headline 	= 'Crée un "Pizza Timer"'
-L.PizzaTimer_Title		= 'Nom (ex. "Pizza!")'
-L.PizzaTimer_Hours 		= "Heures"
-L.PizzaTimer_Mins 		= "Min"
-L.PizzaTimer_Secs 		= "Sec"
-L.PizzaTimer_ButtonStart = "Démarrer le Timer"
-L.PizzaTimer_BroadCast	= "Annoncer au Raid"
+L.Button_RangeFrame			= "Cadre des portées"
+L.Button_TestBars			= "Barres de test"
+
+L.PizzaTimer_Headline 		= 'Création d\'un "délai pizza"'
+L.PizzaTimer_Title			= 'Nom (par ex. : "Pizza !")'
+L.PizzaTimer_Hours 			= "Hrs"
+L.PizzaTimer_Mins 			= "Min"
+L.PizzaTimer_Secs 			= "Sec"
+L.PizzaTimer_ButtonStart 	= "Lancer le délai"
+L.PizzaTimer_BroadCast		= "Diffuser au raid"
 
 -- Tab: Raidwarning
-L.Tab_RaidWarning 	= "Alertes raid"
-L.RaidWarning_Header		= "Options des alertes de raid"
-L.RaidWarnColors 	= "Couleurs des alertes raid"
-L.RaidWarnColor_1 	= "Couleur 1"
-L.RaidWarnColor_2 	= "Couleur 2"
-L.RaidWarnColor_3 	= "Couleur 3"
-L.RaidWarnColor_4 	= "Couleur 4"
-L.InfoRaidWarning	= [[Vous pouvez spécifier la position et la couleur de l'affichage des Alertes Raid.
-Cet affichage est utilisé pour des messages comme "Joueur X est affecté par Y"]]
-L.ColorResetted 	= "La couleur de ce champs a été réinitialisée."
-L.ShowWarningsInChat 	= "Afficher les alertes dans la fenêtre de dialogue"
-L.ShowFakedRaidWarnings = "Afficher les alertes comme de faux avertissements de raid"
-L.WarningIconLeft 	= "Afficher l'icône à gauche"
-L.WarningIconRight 	= "Afficher l'icône à droite"
-L.RaidWarnMessage 	= "Merci d'utiliser Deadly Boss Mods"
-L.BarWhileMove 		= "Alerte-raid déplaçable"
-L.RaidWarnSound		= "Jouer un son pour les alertes raid"
-L.SpecialWarnSound	= "Jouer un son pour les alertes spéciales"
+L.Tab_RaidWarning 			= "Alertes raid"
+L.RaidWarning_Header		= "Options des alertes raid"
+L.RaidWarnColors 			= "Couleurs des alertes raid"
+L.RaidWarnColor_1 			= "Couleur 1"
+L.RaidWarnColor_2 			= "Couleur 2"
+L.RaidWarnColor_3		 	= "Couleur 3"
+L.RaidWarnColor_4 			= "Couleur 4"
+L.InfoRaidWarning			= [[Vous pouvez spécifier la position et les couleurs du cadre des alertes. 
+Ce cadre est utilisé pour les messages du genre "Joueur X subit Y".]]
+L.ColorResetted 			= "Les paramètres de couleur pour ce champ ont été réinitialisés."
+L.ShowWarningsInChat 		= "Afficher les alertes dans la fenêtre de discussion"
+L.ShowFakedRaidWarnings 	= "Afficher les alertes comme de faux Avertissements raid"
+L.WarningIconLeft 			= "Afficher l'icône sur le côté gauche"
+L.WarningIconRight 			= "Afficher l'icône sur le côté droit"
+L.RaidWarnMessage 			= "Merci d'utiliser Deadly Boss Mods"
+L.BarWhileMove 				= "Alerte raid mobile"
+L.RaidWarnSound				= "Son des alertes"
+L.CountdownVoice			= "Voix des comptes à rebours"
+L.SpecialWarnSound			= "Son des alertes spéciales affectant vous-même ou votre rôle"
+L.SpecialWarnSound2			= "Son des alertes spéciales affectant tout le monde"
 
 -- Tab: Barsetup
 L.BarSetup   				= "Style des barres"
 L.BarTexture 				= "Texture des barres"
-L.BarStartColor 			= "Couleur de départ"
-L.BarEndColor 				= "Couleur de fin"
-L.ExpandUpwards				= "Nouvelles barres au-dessus"
-L.Bar_Font					= "Police utiliser pour les barres"
-L.Bar_FontSize				= "Taille des polices"
-L.Slider_BarOffSetX 		= "Position X: %d"
-L.Slider_BarOffSetY 		= "Position Y: %d"
-L.Slider_BarWidth 			= "Largeur: %d"
-L.Slider_BarScale 			= "Echelle: %0.2f"
-L.AreaTitle_BarSetup 		= "Options générales des barres"
+L.BarStartColor				= "Début"
+L.BarEndColor 				= "Fin"
+L.ExpandUpwards				= "Ajouter vers le haut"
+L.Bar_Font					= "Police des barres"
+L.Bar_FontSize				= "Taille de la police"
+L.Slider_BarOffSetX 		= "Décalage X : %d"
+L.Slider_BarOffSetY 		= "Décalage Y : %d"
+L.Slider_BarWidth 			= "Largeur : %d"
+L.Slider_BarScale 			= "Échelle : %0.2f"
+L.AreaTitle_BarSetup		= "Options générales des barres"
 L.AreaTitle_BarSetupSmall 	= "Options des petites barres"
-L.AreaTitle_BarSetupHuge 	= "Options des grandes barres"
-L.BarIconLeft 				= "Icône gauche"
-L.BarIconRight 				= "Icône droit"
-L.EnableHugeBar 			= "Activer les grandes barres (Barre 2)"
+L.AreaTitle_BarSetupHuge	= "Options des grosses barres"
+L.BarIconLeft 				= "Icône à G"
+L.BarIconRight 				= "Icône à D"
+L.EnableHugeBar 			= "Activer les grosses barre (alias Barre 2)"
 L.FillUpBars				= "Remplir les barres"
-L.ClickThrough				= "Enlève le contrôle par la souris (Vous autorise à cliquer à travers les barres)"
+L.ClickThrough				= "Désactiver toute interaction avec la souris (clic à travers)"
 
 -- Tab: Spec Warn Frame
 L.Panel_SpecWarnFrame		= "Alertes spéciales"
-L.Area_SpecWarn				= "Configuration des alertes spéciales"
-L.SpecWarn_Enabled			= "Montre les alertes spéciales pour les capacités des boss"
-L.SpecWarn_Font				= "Police utilisée pour les alertes spéciales"
-L.SpecWarn_DemoButton		= "Montre un exemple"
+L.Area_SpecWarn				= "Options des alertes spéciales"
+L.SpecWarn_Enabled			= "Afficher des alertes spéciales pour les techniques des boss"
+L.SpecWarn_LHFrame			= "Faire flasher l'écran lors des alertes spéciales"
+L.SpecWarn_Font				= "Police des alertes spéciales"
+L.SpecWarn_DemoButton		= "Aff. un exemple"
 L.SpecWarn_MoveMe			= "Définir la position"
-L.SpecWarn_FontSize			= "Taille de police"
-L.SpecWarn_FontColor			= "Couleur de police"
-L.SpecWarn_FontType			= "Choisir la police"
-L.SpecWarn_ResetMe			= "Réinitialiser"
+L.SpecWarn_FontSize			= "Taille de la police"
+L.SpecWarn_FontColor		= "Couleur"
+L.SpecWarn_FontType			= "Choix de la police"
+L.SpecWarn_ResetMe			= "Valeurs par défaut"
 
 -- Tab: HealthFrame
-L.Panel_HPFrame				= "Barre de vie"
-L.Area_HPFrame				= "Configurer la Barre de vie"
-L.HP_Enabled				= "Toujours montrer la Barre de vie, même si elle est désactivée dans le Module"
-L.HP_GrowUpwards			= "Prolonge la barre de vie vers le haut"
-L.HP_ShowDemo				= "Montre la fenêtre des points de vie"
-L.BarWidth					= "Longueur de la barre: %d"
+L.Panel_HPFrame				= "Cadre des vies"
+L.Area_HPFrame				= "Options du cadre des vies"
+L.HP_Enabled				= "Toujours afficher le cadre (surplante toute autre option)"
+L.HP_GrowUpwards			= "Étendre le cadre des vies vers le haut"
+L.HP_ShowDemo				= "Aff. cadre des vies"
+L.BarWidth					= "Largeur des barres : %d"
 
 -- Tab: Spam Filter
-L.Panel_SpamFilter				= "Filtre anti-spam"
-L.Area_SpamFilter				= "Options générales du filtre anti-spam"
-L.HideBossEmoteFrame			= "Cacher la fenêtre des emotes de boss"
-L.SpamBlockRaidWarning			= "Filtrer les annonces venant d'autres boss mods"
-L.SpamBlockBossWhispers			= "Filtre: Alerter les chuchotements en commencant par <DBM> lors des combats"
-L.BlockVersionUpdateNotice		= "Enlève le message pop-up quand vous êtes sur un boss"
-L.ShowBigBrotherOnCombatStart	= "Autoriser Big Brother à regarder les buffs quand le combat débute"
-L.BigBrotherAnnounceToRaid		= "Autoriser Big Brother à annoncer les résultats au raid"
+L.Panel_SpamFilter				= "Filtres globaux et de spam"
+L.Area_SpamFilter				= "Options des filtres de spam"
+L.HideBossEmoteFrame			= "Masquer le cadre des émotes des boss de raid"
+L.SpamBlockRaidWarning			= "Filtrer les annonces des autres boss mods" 
+L.SpamBlockBossWhispers			= "Filtrer les chuchotements d'alerte <DBM> pendant les rencontres"
+L.BlockVersionUpdateNotice		= "Désactiver le popup de notification de mise à jour"
+L.ShowBigBrotherOnCombatStart	= "Effectuer une vérif. des buffs Big Brother au début du combat"
+L.BigBrotherAnnounceToRaid		= "Annoncer les résultats de Big Brother au raid"
 
-L.Area_SpamFilter_Outgoing		= "Options Global des Filtres"
-L.SpamBlockNoShowAnnounce		= "Ne pas montrer les annonces ou jouer les sons"
-L.SpamBlockNoSendAnnounce		= "Ne pas écrire les annonces dans le chat de raid"
-L.SpamBlockNoSendWhisper		= "Ne pas chuchoter les autres joueurs"
-L.SpamBlockNoSetIcon			= "Ne pas mettre d'icônes sur la cible"
-
-
--- Misc
-L.FontHeight	= 16
+L.Area_SpamFilter_Outgoing		= "Options des filtres globaux"
+L.SpamBlockNoShowAnnounce		= "Ne pas afficher d'annonces ou jouer de sons d'alerte"
+L.SpamBlockNoSendAnnounce		= "Ne pas envoyer d'annonces au canal Raid"
+L.SpamBlockNoSendWhisper		= "Ne pas envoyer de chuchotements aux autres joueurs"
+L.SpamBlockNoSetIcon			= "Ne pas placer d'icônes sur les cibles"
