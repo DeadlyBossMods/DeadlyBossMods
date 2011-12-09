8079c2cadd3f8bd9933eb8f4550004ea5b298c5f
Adam <MysticalOS@users.noreply.github.com>
2011-12-09 06:09:06 +0000

    shorten first timerPsychicDrainCD cd on engage by a little, same 5-6 second variation on it though, but it does indeed come between 16-23 seconds

--- DBM-DragonSoul/Zonozz.lua
+++ DBM-DragonSoul/Zonozz.lua
@@ -64,7 +64,7 @@ function mod:OnCombatStart(delay)
 	table.wipe(shadowsTargets)
 	timerVoidofUnmakingCD:Start(6-delay)
 	timerFocusedAngerCD:Start(10.5-delay)
-	timerPsychicDrainCD:Start(-delay)
+	timerPsychicDrainCD:Start(16-delay)
 	timerShadowsCD:Start(-delay)
 	if not self:IsDifficulty("lfr25") then--Can confirm what others saw, LFR definitely doesn't have a 6 min berserk. It's either much longer or not there.
 		berserkTimer:Start(-delay)
