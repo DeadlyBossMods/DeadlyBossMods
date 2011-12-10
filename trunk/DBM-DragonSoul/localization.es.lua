d9d5f667838e1c2bb9ed52f317ee8a991a60d8ef
Snamor <Snamor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-12-10 16:24:19 +0000

    esES update

--- trunk/DBM-DragonSoul/localization.es.lua
+++ trunk/DBM-DragonSoul/localization.es.lua
@@ -35,7 +35,8 @@ L:SetTimerLocalization({
 })
 
 L:SetOptionLocalization({
-	RangeFrame	= "Mostrar distancia (10) para $spell:104601\n(sólo heroico)"
+	RangeFrame			= "Mostrar distancia dinámica basada en el estado del estado del debuff \n$spell:104601 en los jugadores en dificultad Heroica",
+	NoFilterRangeFrame	= "Desactivar el filtro de distancia y mostrar siempre a todo el mundo"
 })
 
 L:SetMiscLocalization({
@@ -134,12 +135,14 @@ L:SetWarningLocalization({
 
 L:SetTimerLocalization({
 	TimerCombatStart	= "Empieza el combate",
-	TimerSapper			= "Siguiente Zapador Crepuscular"
+	TimerSapper			= "Siguiente Zapador Crepuscular",
+	TimerAdd			= "Siguientes Élites"
 })
 
 L:SetOptionLocalization({
 	TimerCombatStart	= "Mostrar tiempo para el inicio del combate",
-	TimerSapper			= "Mostrar tiempo para la aparición del siguiente Zapador Crepuscular"
+	TimerSapper			= "Mostrar tiempo para la aparición del siguiente Zapador Crepuscular",--npc=56923
+	TimerAdd			= "Mostrar tiempo para que salgan los siguientes Élites Crepusculares"
 })
 
 L:SetMiscLocalization({
