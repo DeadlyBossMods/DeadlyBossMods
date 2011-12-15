64bba2d63e9a4a64e5669937698332f7e7c52c73
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-12-15 12:18:43 +0000

    # Hagara : remove double Watery.

--- trunk/DBM-DragonSoul/Hagara.lua
+++ trunk/DBM-DragonSoul/Hagara.lua
@@ -34,7 +34,6 @@ local specWarnAssault		= mod:NewSpecialWarningSpell(107851, mod:IsTank())
 --local specWarnFrostTomb		= mod:NewSpecialWarningYou(104451)
 local specWarnWatery		= mod:NewSpecialWarningMove(110317)
 local specWarnStormPillars	= mod:NewSpecialWarningClose(109557)	-- if target != nil
-local specWarnWatery		= mod:NewSpecialWarningMove(110317)
 local specWarnFrostflake	= mod:NewSpecialWarningYou(109325)
 local yellFrostflake		= mod:NewYell(109325)
 
