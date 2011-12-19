b42772d8b4008bec71f52792f01a7d6698046ea4
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-12-19 02:13:35 +0000

    local update for last

--- trunk/DBM-DragonSoul/localization.en.lua
+++ trunk/DBM-DragonSoul/localization.en.lua
@@ -84,7 +84,7 @@ L:SetTimerLocalization({
 
 L:SetOptionLocalization({
 	TimerSpecial			= "Show timer for first special ability cast",
-	RangeFrame				= "Show range frame (3) for $spell:105269",
+	RangeFrame				= "Show range frame: (3) for $spell:105269, (10) for $journal:4327",
 	AnnounceFrostTombIcons	= "Announce icons for $spell:104451 targets to raid chat\n(requires raid leader)",
 	warnFrostTombCast		= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.cast:format(104448, GetSpellInfo(104448)),
 	SetIconOnFrostTomb		= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(104451),
