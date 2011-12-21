af2ebb0c2e1a3868a8acd56b6ae061cf6e98c9e8
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-12-21 03:31:52 +0000

    koKR update

--- trunk/DBM-DragonSoul/localization.kr.lua
+++ trunk/DBM-DragonSoul/localization.kr.lua
@@ -86,7 +86,7 @@ L:SetTimerLocalization({
 })
 
 L:SetOptionLocalization({
-	TimerSpecial			= "$spell:105456 또는 $spell:109552 까지 남은 시간 바 표시",
+	TimerSpecial			= "다음 $spell:105256 또는 $spell:105465 까지 남은 시간 바 표시",
 	RangeFrame				= "$spell:105269 (3m), $journal:4327 (10m) 주문의 영향을 받은 경우 거리 프레임 보기",
 	AnnounceFrostTombIcons	= "$spell:104451 대상을 공격대 대화로 알리기\n(승급 권한 필요)",
 	warnFrostTombCast		= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.cast:format(104448, GetSpellInfo(104448)),
