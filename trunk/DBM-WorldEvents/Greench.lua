7add1860b743cf70af7da74898ef68d3dc6a6059
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-12-22 19:18:35 +0000

    shortan shrink heart CD slightly

--- trunk/DBM-WorldEvents/Greench.lua
+++ trunk/DBM-WorldEvents/Greench.lua
@@ -23,7 +23,7 @@ local warnTree					= mod:NewSpellAnnounce(101938, 2)--Needs a custom icon, i'll 
 
 local specWarnShrinkHeart		= mod:NewSpecialWarningMove(101860)
 
-local timerShrinkHeartCD		= mod:NewCDTimer(35, 101873)
+local timerShrinkHeartCD		= mod:NewCDTimer(32.5, 101873)
 local timerSnowmanCD			= mod:NewCDTimer(10, 101910)--He alternates these
 local timerTreeCD				= mod:NewCDTimer(10, 101938)
 local timerCrushCD				= mod:NewCDTimer(5, 101885)--Used 5 seconds after tree casts (on the tree itself). Right before stomp he stops targeting tank. He has no target during stomp, usable for cast trigger? Only trigger in log is the stomp landing.
