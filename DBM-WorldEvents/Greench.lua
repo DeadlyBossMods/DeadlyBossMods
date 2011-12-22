1afb34fa8f4e0fcefd51cda759eb9c7d8d057589
Adam <MysticalOS@users.noreply.github.com>
2011-12-22 00:20:59 +0000

    Small greench update

--- DBM-WorldEvents/Greench.lua
+++ DBM-WorldEvents/Greench.lua
@@ -9,21 +9,37 @@ mod:SetZone(24)--Hillsbread Foothills
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
+	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_APPLIED_DOSE",
 	"UNIT_SPELLCAST_SUCCEEDED"
 )
 
 local warnShrinkHeart			= mod:NewSpellAnnounce(101873, 2)
-local warnSnowman				= mod:NewSpellAnnounce(101910, 3)--target scanning doesn't work, he doesn't look at anyone but tank.
-local warnTree					= mod:NewSpellAnnounce(101938, 3)--Needs a custom icon, i'll find one soon.
+local warnSnowman				= mod:NewSpellAnnounce(101910, 2)
+local warnSnowCrash				= mod:NewCastAnnounce(101907, 3)
+local warnTree					= mod:NewSpellAnnounce(101938, 2)--Needs a custom icon, i'll find one soon.
+
+local specWarnShrinkHeart		= mod:NewSpecialWarningMove(101860)
 
 local timerShrinkHeartCD		= mod:NewCDTimer(35, 101873)
 local timerSnowmanCD			= mod:NewCDTimer(10, 101910)--He alternates these
 local timerTreeCD				= mod:NewCDTimer(10, 101938)
 local timerCrushCD				= mod:NewCDTimer(5, 101885)--Used 5 seconds after tree casts (on the tree itself). Right before stomp he stops targeting tank. He has no target during stomp, usable for cast trigger? Only trigger in log is the stomp landing.
+local timerSnowCrash			= mod:NewCastTimer(5, 101907)
+
+local antiSpam = 0
 
 function mod:OnCombatStart(delay)
 	timerSnowmanCD:Start(-delay)
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(101907) then
+		warnSnowCrash:Show()
+		timerSnowCrash:Start()
+	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
@@ -32,6 +48,14 @@ function mod:SPELL_CAST_SUCCESS(args)
 		timerShrinkHeartCD:Start()
 	end
 end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(101860) and args:IsPlayer() and GetTime() - antiSpam > 2 then
+		specWarnShrinkHeart:Show()
+		antiSpam = GetTime()
+	end
+end
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, spellName)
 --	The Abominable Greench:Possible Target<Omegathree>:target:Throw Strange Snowman Trigger::0:101942", -- [230]
