413a25f0d6a07ead872239608d3c99b5ad96bded
thiagovfar <thiagovfar@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2011-12-25 16:38:49 +0000

    Finishing ptBR messages localization
    All DBM Messages, options and so on are now localized to ptBR.
    
    However, triggers are still not localized.

---
+++ DBM-WorldEvents/localization.pt.lua
@@ -0,0 +1,139 @@
+﻿if GetLocale() ~= "ptBR" then return end
+
+local L
+
+-----------------------
+--  Apothecary Trio  --
+-----------------------
+L = DBM:GetModLocalization("ApothecaryTrio")
+
+L:SetGeneralLocalization({
+	name = "Apothecary Trio"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization{
+	HummelActive		= "Hummel se ativa",
+	BaxterActive		= "Baxter se ativa",
+	FryeActive			= "Frye se ativa"
+}
+
+L:SetOptionLocalization({
+	TrioActiveTimer		= "Exibir cronógrafos para quando o Apothecary Trio tornar-se ativo"
+})
+
+L:SetMiscLocalization({
+	SayCombatStart		= "Did they bother to tell you who I am and why I am doing this?"
+})
+
+-------------
+--  Ahune  --
+-------------
+L = DBM:GetModLocalization("Ahune")
+
+L:SetGeneralLocalization({
+	name = "Ahune"
+})
+
+L:SetWarningLocalization({
+	Submerged		= "Ahune submergiu",
+	Emerged			= "Ahune emergiu",
+	specWarnAttack	= "Ahune está vulnerável - Ataque agora!"
+})
+
+L:SetTimerLocalization{
+	SubmergTimer	= "Submergir",
+	EmergeTimer		= "Emergir",
+	TimerCombat		= "Combate inicia"
+}
+
+L:SetOptionLocalization({
+	Submerged		= "Exibir aviso quando Ahune submergir",
+	Emerged			= "Exibir aviso quando Ahune emergir",
+	specWarnAttack	= "Exibir aviso especial quando Ahune tornar-se vulnerável",
+	SubmergTimer	= "Exibir cronógrafo para submergir",
+	EmergeTimer		= "Exibir cronógrafo para emerge",
+	TimerCombat		= "Exibir cronógrafo para início do combate",
+})
+
+L:SetMiscLocalization({
+	Pull			= "The Ice Stone has melted!"
+})
+
+----------------------
+--  Coren Direbrew  --
+----------------------
+L = DBM:GetModLocalization("CorenDirebrew")
+
+L:SetGeneralLocalization({
+	name = "Coren Direbrew"
+})
+
+L:SetWarningLocalization({
+	specWarnBrew		= "Livre-se da bebida antes que ela te atire outra!",
+	specWarnBrewStun	= "DICA: Você foi atordoado, lembre-se de beber da próxima vez!"
+})
+
+L:SetOptionLocalization({
+	specWarnBrew		= "Exibir aviso especial para $spell:47376",
+	specWarnBrewStun	= "Exibir aviso especial para $spell:47340",
+	YellOnBarrel		= "Gritar no $spell:51413"
+})
+
+L:SetMiscLocalization({
+	YellBarrel			= "Estou Embarrilado!"
+})
+
+-------------------------
+--  Headless Horseman  --
+-------------------------
+L = DBM:GetModLocalization("HeadlessHorseman")
+
+L:SetGeneralLocalization({
+	name = "Headless Horseman"
+})
+
+L:SetWarningLocalization({
+	WarnPhase				= "Fase %d",
+	warnHorsemanSoldiers	= "Abóboras Pulsantes surgindo",
+	warnHorsemanHead		= "Cabeça do Horseman ativa"
+})
+
+L:SetTimerLocalization{
+	TimerCombatStart		= "Combate inicia"
+}
+
+L:SetOptionLocalization({
+	WarnPhase				= "Exibir aviso para cada mudança de fase",
+	TimerCombatStart		= "Exibir cronógrafo para início do combate",
+	warnHorsemanSoldiers	= "Exibir aviso para surgimento de Abóboras Pulsantes",
+	warnHorsemanHead		= "Exibir aviso para surgimento da Cabeça do Horseman"
+})
+
+L:SetMiscLocalization({
+	HorsemanSummon			= "Horseman rise...",
+	HorsemanSoldiers		= "Soldiers arise, stand and fight! Bring victory at last to this fallen knight!"
+})
+
+------------------------------
+--  The Abominable Greench  --
+------------------------------
+L = DBM:GetModLocalization("Greench")
+
+L:SetGeneralLocalization({
+	name = "O Aboi Greench"
+})
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization{
+}
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
