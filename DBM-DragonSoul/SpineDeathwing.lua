4105f09af14bef232f71897b00ca33ebb08be657
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-01-02 20:10:22 +0000

    # Spine of Deathwing : final fix.

--- DBM-DragonSoul/SpineDeathwing.lua
+++ DBM-DragonSoul/SpineDeathwing.lua
@@ -160,9 +160,9 @@ function mod:SPELL_CAST_START(args)
 		end
 		corruptionActive[args.sourceGUID] = corruptionActive[args.sourceGUID] + 1
 		if corruptionActive[args.sourceGUID] == 2 and self:IsDifficulty("normal25", "heroic25") then
-			timerGripCD:Update(8, 16)
+			timerGripCD:Update(8, 16, args.sourceGUID)
 		elseif corruptionActive[args.sourceGUID] == 4 and self:IsDifficulty("normal10", "heroic10") then
-			timerGripCD:Update(24, 32)
+			timerGripCD:Update(24, 32, args.sourceGUID)
 		end
 	end
 end
