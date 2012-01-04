22f853341067d23faa370c2516c9d89ec4290933
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-01-04 14:40:00 +0000

    # Morchok : oops, forget to reset stomp and crystal count.

--- DBM-DragonSoul/Morchok.lua
+++ DBM-DragonSoul/Morchok.lua
@@ -86,6 +86,8 @@ mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	if args:IsSpellID(103851) and args:GetSrcCreatureID() == 55265 then--Filter twin here, they vortex together but we don't want to trigger everything twice needlessly.
+		stompCount = 0
+		crystalCount = 0
 		timerStomp:Start(19)
 		timerCrystal:Start(26)
 		timerVortexNext:Start()
