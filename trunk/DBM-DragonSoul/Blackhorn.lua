78b69665b3f40a9314a402e15d6f9d932faf8721
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-01-16 03:08:13 +0000

    add special warning for standing in deckfire

--- trunk/DBM-DragonSoul/Blackhorn.lua
+++ trunk/DBM-DragonSoul/Blackhorn.lua
@@ -34,7 +34,8 @@ local warnConsumingShroud			= mod:NewTargetAnnounce(110598)
 
 local specWarnHarpoon				= mod:NewSpecialWarningTarget(108038, false)
 local specWarnTwilightOnslaught		= mod:NewSpecialWarningSpell(107588, nil, nil, nil, true)
-local specWarnDeckFire				= mod:NewSpecialWarningSpell(110095, false, nil, nil, true)
+local specWarnDeckFireCast			= mod:NewSpecialWarningSpell(110095, false, nil, nil, true)
+local specWarnDeckFire				= mod:NewSpecialWarningMove(110095)
 local specWarnElites				= mod:NewSpecialWarning("SpecWarnElites", mod:IsTank())
 local specWarnShockwave				= mod:NewSpecialWarningMove(108046)
 local specWarnShockwaveOther		= mod:NewSpecialWarningTarget(108046, false)
@@ -189,6 +190,11 @@ function mod:SPELL_DAMAGE(args)
 			specWarnTwilightFlames:Show()
 			lastFlames = GetTime()
 		end
+	elseif args:IsSpellID(110095) then
+		if args:IsPlayer() and GetTime() - lastFlames > 3  then
+			specWarnDeckFire:Show()
+			lastFlames = GetTime()
+		end
 	end
 end
 mod.SPELL_MISSED = mod.SPELL_DAMAGE
@@ -199,7 +205,7 @@ function mod:RAID_BOSS_EMOTE(msg)
 	elseif msg == L.Broadside or msg:find(L.Broadside) then
 		timerBroadsideCD:Start()
 	elseif msg == L.DeckFire or msg:find(L.DeckFire) then
-		specWarnDeckFire:Show()
+		specWarnDeckFireCast:Show()
 	elseif msg == L.GorionaRetreat or msg:find(L.GorionaRetreat) then
 		timerTwilightBreath:Cancel()
 		timerConsumingShroud:Cancel()
