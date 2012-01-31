c2d4a0ef947f36e5ef82f1da1596dc690ded58b0
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-01-31 03:38:07 +0000

    Omen boss, just cause.

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -1516,7 +1516,7 @@ function DBM:UPDATE_MOUSEOVER_UNIT()
 					break
 				end
 			end
-		elseif (cId == 55003 or cId == 54499) and not DBM:GetModByName("Greench") then--The Abominable Greench (Winter Veil world boss)
+		elseif (cId == 55003 or cId == 54499 or cId == 15467 or cId == 15466) and not DBM:GetModByName("Greench") then--The Abominable Greench & his helpers (Winter Veil world boss), Omen & his minions (Lunar Festival world boss)
 			for i, v in ipairs(DBM.AddOns) do
 				if v.modId == "DBM-WorldEvents" then
 					DBM:LoadMod(v)
@@ -1546,7 +1546,7 @@ function DBM:PLAYER_TARGET_CHANGED()
 					break
 				end
 			end
-		elseif (cId == 55003 or cId == 54499) and not DBM:GetModByName("Greench") then--The Abominable Greench & his helpers (Winter Veil world boss)
+		elseif (cId == 55003 or cId == 54499 or cId == 15467 or cId == 15466) and not DBM:GetModByName("Greench") then--The Abominable Greench & his helpers (Winter Veil world boss), Omen & his minions (Lunar Festival world boss)
 			for i, v in ipairs(DBM.AddOns) do
 				if v.modId == "DBM-WorldEvents" then
 					DBM:LoadMod(v)
---
+++ trunk/DBM-WorldEvents/Holidays/Omen.lua
@@ -0,0 +1,49 @@
+local mod	= DBM:NewMod("Omen", "DBM-WorldEvents", 1)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision: 7125 $"):sub(12, -3))
+mod:SetCreatureID(15467)
+mod:SetModelID(15879)
+mod:SetZone(241)--Moonglade
+
+mod:RegisterCombat("combat")
+
+mod:RegisterEvents(
+	"SPELL_CAST_SUCCESS",
+	"SPELL_PERIODIC_DAMAGE",
+	"SPELL_PERIODIC_MISS"
+)
+
+local warnCleave				= mod:NewSpellAnnounce(104903, 2)
+local warnStarfall				= mod:NewSpellAnnounce(26540, 3)
+
+local specWarnStarfall			= mod:NewSpecialWarningMove(26540)
+
+local timerCleaveCD				= mod:NewCDTimer(8.5, 104903)
+local timerStarfallCD			= mod:NewCDTimer(15, 26540)
+
+function mod:OnCombatStart(delay)
+	timerCleaveCD:Start(10.5-delay)--Consistent?
+	timerStarfallCD:Start(11-delay)--^?
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(104903) then
+		warnCleave:Show()
+		timerCleaveCD:Start()
+	elseif args:IsSpellID(26540) then
+		warnStarfall:Show()
+		timerStarfallCD:Start()
+	end
+end
+
+do 
+	local antiSpam = 0
+	function mod:SPELL_PERIODIC_DAMAGE(args)
+		if args:IsSpellID(26540) and args:IsPlayer() and GetTime() - antiSpam > 3 then
+			specWarnStarfall:Show()
+			antiSpam = GetTime()
+		end
+	end
+	mod.SPELL_PERIODIC_MISS = mod.SPELL_PERIODIC_DAMAGE
+end
