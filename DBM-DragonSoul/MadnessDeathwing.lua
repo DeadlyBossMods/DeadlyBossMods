f828854acc4235cbf897dd491dae1d547a6f2a1f
Adam <MysticalOS@users.noreply.github.com>
2012-02-05 00:24:40 +0000

    it's possible to be out of combat log range of thrall so add a sync in to make sure we get an end combat from anyone that does pick it up.

--- DBM-DragonSoul/MadnessDeathwing.lua
+++ DBM-DragonSoul/MadnessDeathwing.lua
@@ -151,7 +151,7 @@ function mod:SPELL_CAST_SUCCESS(args)
 			specWarnElementiumBolt:Schedule(7.5)
 		end
 	elseif args:IsSpellID(110063) and phase2 and self:IsInCombat() then--Astral Recall. Thrall teleports off back platform back to front on defeat.
-		DBM:EndCombat(self)
+		self:SendSync("MadnessDown")
 	end
 end
 
@@ -256,5 +256,7 @@ end
 function mod:OnSync(msg)
 	if msg == "BoltDied" then
 		timerElementiumBlast:Cancel()--Lot of work just to cancel a timer, why the heck did blizz break this mob firing UNIT_DIED when it dies? Sigh.
+	elseif msg == "MadnessDown" then
+		DBM:EndCombat(self)
 	end
 end
