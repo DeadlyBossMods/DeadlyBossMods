cba543677dfa5a942019c1aecc5be59cae8fad07
Adam <MysticalOS@users.noreply.github.com>
2012-02-05 20:33:28 +0000

    Darkmoon updates

--- DBM-WorldEvents/DMF/Gnoll.lua
+++ DBM-WorldEvents/DMF/Gnoll.lua
@@ -17,26 +17,36 @@ local specWarnHogger			= mod:NewSpecialWarning("specWarnHogger")
 
 local timerGame					= mod:NewBuffActiveTimer(60, 101612)
 
+local countdownGame				= mod:NewCountdown(60, 101612)
+
 mod:RemoveOption("HealthFrame")
 mod:RemoveOption("SpeedKillTimer")
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(101612) and args:IsPlayer() then
 		timerGame:Start()
+		countdownGame:Start(60)
 	end
 end
 
 function mod:SPELL_AURA_REMOVED(args)
 	if args:IsSpellID(101612) and args:IsPlayer() then
 		timerGame:Cancel()
+		countdownGame:Cancel()
 	end
 end
 
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, spellName, _, _, spellID)
-	if spellID == 102044 then--Hogger
-		warnHogger:Show()
-		specWarnHogger:Show()
-	elseif spellID == 102036 then--Gnoll
-		warnGnoll:Show()
+do 
+	local antiSpam = 0
+	function mod:UNIT_SPELLCAST_SUCCEEDED(uId, spellName, _, _, spellID)
+		if spellID == 102044 then--Hogger
+			warnHogger:Show()
+			if GetTime() - antiSpam > 2 then
+				specWarnHogger:Show()
+			end
+			antiSpam = GetTime()
+		elseif spellID == 102036 then--Gnoll
+			warnGnoll:Show()
+		end
 	end
 end
