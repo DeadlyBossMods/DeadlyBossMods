2dcb2743470faa000b5c650169e79510901c4471
Snamor <Snamor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-02-07 17:46:01 +0000

    esES update

--- DBM-DragonSoul/localization.es.lua
+++ DBM-DragonSoul/localization.es.lua
@@ -98,7 +98,9 @@ L:SetOptionLocalization({
 	AnnounceFrostTombIcons	= "Anunciar iconos de los objetivos de $spell:104451 a la banda\n(requiere líder)",
 	warnFrostTombCast		= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.cast:format(104448, GetSpellInfo(104448)),
 	SetIconOnFrostTomb		= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(104451),
-	SetIconOnFrostflake		= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(109325)
+	SetIconOnFrostflake		= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(109325),
+	SpecialCount			= "Reproducir sonido de cuenta atrás para $spell:105256 o $spell:105465",
+	SetBubbles				= "Desactivar los bocadillos de chat automaticamente cuando haya $spell:104451\n(se restauran una vez el combate termina)"
 })
 
 
