436234e247d970e82d7a4ba76d2b9f46e6caa231
Adam <MysticalOS@users.noreply.github.com>
2012-02-09 03:32:19 +0000

    Pointless change really but to keep uniformity. Void bolt was nerfed, but the timer was already correct do to the way i pull it dynamically in game. but changed it in options too just to match up

--- DBM-DragonSoul/Yorsahj.lua
+++ DBM-DragonSoul/Yorsahj.lua
@@ -35,7 +35,7 @@ local timerOozesActive		= mod:NewTimer(7, "timerOozesActive", 16372) -- varies (
 local timerAcidCD			= mod:NewNextTimer(8.3, 108352)--Green ooze aoe
 local timerSearingCD		= mod:NewNextTimer(6, 108358)--Red ooze aoe
 local timerVoidBoltCD		= mod:NewNextTimer(6, 108383, nil, mod:IsTank())
-local timerVoidBolt			= mod:NewTargetTimer(21, 108383, nil, mod:IsTank() or mod:IsHealer())--Tooltip says 30 but combat logs clearly show it fading at 20-22 (varies)
+local timerVoidBolt			= mod:NewTargetTimer(12, 108383, nil, mod:IsTank() or mod:IsHealer())--Nerfed yet again, its now 12. Good thing dbm timers were already right since i dbm pulls duration from aura heh.
 local timerDeepCorruption	= mod:NewBuffFadesTimer(25, 105171, nil, mod:IsTank() or mod:IsHealer())
 
 local berserkTimer		= mod:NewBerserkTimer(600)
