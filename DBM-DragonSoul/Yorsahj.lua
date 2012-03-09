778ee1f159108e5f121bc16df64d716ca8ff0da4
Adam <MysticalOS@users.noreply.github.com>
2012-03-09 00:48:34 +0000

    A new DBM core prototype, an anti spam function, that can be used instead of writing anti spam functions in so many mods individually. a LOT of mods can be updated to use this but first i want to make sure it actually works as right now it's just a drycode. Also make sure T12 and 13 mods using UNIT_SPELLCAST_SUCCEEDED all work in the event bossN is missing. This issue is still hit or miss, In test pulling a re-pulling bosses it was hit or miss on whether or not the Boss unit IDs were present. this is a strange and new bug since 4.3.2. But it means we can't rely on those UnitIDs alone for detecting some of the important casts that only work with UNIT_SPELLCAST_SUCCEEDED, so instead we filter no unitIDs at all, and instead use anti spam. it becomes a lot more complicated for bosses that have MANY diff spells, some of which can be cast at SAME TIME like alakir, a global anti spam won't work and each spell would need it's own, But i don't feel like doing that, i'm hoping only T12 and 13 need these work arounds cause i'm hoping this bug is a temporary blizzard bug.

--- DBM-DragonSoul/Yorsahj.lua
+++ DBM-DragonSoul/Yorsahj.lua
@@ -191,12 +191,6 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end		
 
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if oozeColors[spellId] then
-		self:SendSync("Oozes", spellId)
-	end
-end
-
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 55862 or cid == 55866 or cid == 55865 or cid == 55867 or cid == 55864 or cid == 55863 then--Oozes
@@ -204,8 +198,8 @@ function mod:UNIT_DIED(args)
 	end
 end
 
-function mod:OnSync(msg, spellId)
-	if msg == "Oozes" then
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
+	if oozeColors[spellId] and self:AntiSpam() then
 		table.wipe(oozesHitTable)
 		specWarnOozes:Show()
 		timerVoidBoltCD:Start(42)
