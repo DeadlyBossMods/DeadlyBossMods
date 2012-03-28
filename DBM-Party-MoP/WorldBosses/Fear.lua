64c78171a84ed5f786236c7e39415f361aef890b
Adam <MysticalOS@users.noreply.github.com>
2012-03-28 22:11:03 +0000

    Our first MoP party mod, yay, will have a few more by end of night. Just wanted to commit current work so far.

--- DBM-Party-MoP/WorldBosses/Fear.lua
+++ DBM-Party-MoP/WorldBosses/Fear.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Fear", "DBM-Party-MoP", 10)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7378 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 --mod:SetCreatureID(50063)
 --mod:SetModelID(34573)
 --mod:SetZone(748, 720)--Dread Wastes (zoneid not yet known)
