3ec5478be28e549f9d5671a2a1c3515955bc38bb
Adam <MysticalOS@users.noreply.github.com>
2012-03-28 22:11:03 +0000

    Our first MoP party mod, yay, will have a few more by end of night. Just wanted to commit current work so far.

--- DBM-Party-MoP/ScarletHalls/Braun.lua
+++ DBM-Party-MoP/ScarletHalls/Braun.lua
@@ -1,10 +1,9 @@
-local mod	= DBM:NewMod("Anger", "DBM-Party-MoP", 10)
+local mod	= DBM:NewMod("Braun", "DBM-Party-MoP", 8)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 --mod:SetCreatureID(50063)
 --mod:SetModelID(34573)
---mod:SetZone(748, 720)--Kun-Lai Summit (zoneid not yet known)
 
 mod:RegisterCombat("combat")
 
