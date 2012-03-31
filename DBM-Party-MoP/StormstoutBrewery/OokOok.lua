e387b7c6463beb44f017a5618abb4f28d28fdf54
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-03-31 17:58:45 +0000

    Datamined model IDs  (need confirmation tho ;) )

--- DBM-Party-MoP/StormstoutBrewery/OokOok.lua
+++ DBM-Party-MoP/StormstoutBrewery/OokOok.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 --mod:SetCreatureID(54432)
---mod:SetModelID(38931)
+--mod:SetModelID(39498)
 mod:SetZone()
 
 --mod:RegisterCombat("combat")
