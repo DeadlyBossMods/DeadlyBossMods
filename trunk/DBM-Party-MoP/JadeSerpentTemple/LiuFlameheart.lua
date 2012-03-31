6938d5ea3bba65cdd63545a948d379413ff3f098
arta <arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-03-31 18:59:12 +0000

    Some more datamined model IDs (needs ingame confirmation)

--- trunk/DBM-Party-MoP/JadeSerpentTemple/LiuFlameheart.lua
+++ trunk/DBM-Party-MoP/JadeSerpentTemple/LiuFlameheart.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 --mod:SetCreatureID(54432)
---mod:SetModelID(38487)
+--mod:SetModelID(39487)
 mod:SetZone()
 
 --mod:RegisterCombat("combat")
