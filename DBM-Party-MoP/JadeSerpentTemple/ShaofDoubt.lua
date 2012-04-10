44b6db44a8206dffbc09b94597fb64b455aa338d
Adam <MysticalOS@users.noreply.github.com>
2012-04-10 23:51:04 +0000

    Sha Of Doubt and LiuFlameheart done.

--- DBM-Party-MoP/JadeSerpentTemple/ShaofDoubt.lua
+++ DBM-Party-MoP/JadeSerpentTemple/ShaofDoubt.lua
@@ -9,39 +9,57 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
+	"SPELL_CAST_SUCCESS",
 	"SPELL_AURA_APPLIED",
-	"SPELL_CAST_START"
+	"SPELL_AURA_REMOVED"
 )
 
---[[
-local warnBlast			= mod:NewSpellAnnounce(102381, 3)
-local warnBreath		= mod:NewSpellAnnounce(102569, 4)
-local warnRewind		= mod:NewSpellAnnounce(101591, 3)
 
-local timerBlastCD		= mod:NewNextTimer(12, 102381)
-local timerBreathCD		= mod:NewNextTimer(22, 102569)
+local warnWitherWill			= mod:NewSpellAnnounce(120236, 3)
+local warnTouchofNothingness	= mod:NewTargetAnnounce(106113, 4)
+local warnBoundsOfReality		= mod:NewSpellAnnounce(117665, 3)
+
+local specwarnTouchOfNothingness= mod:NewSpecialWarningDispel(106113, mod:IsHealer())
+
+local timerWitherWillCD			= mod:NewCDTimer(6, 120236)--6-10 second variations.
+local timerTouchofNothingnessCD	= mod:NewNextTimer(19.5, 106113)
+local timerTouchofNothingness	= mod:NewTargetTimer(30, 106113)
+local timerBoundsOfRealityCD	= mod:NewNextTimer(60, 117665)
+local timerBoundsOfReality		= mod:NewBuffFadesTimer(30, 117665)
 
 function mod:OnCombatStart(delay)
-	timerBlastCD:Start(-delay)
-	timerBreathCD:Start(-delay)
+	timerWitherWillCD:Start(-delay)
+	timerTouchofNothingnessCD:Start(13-delay)
+	timerBoundsOfReality:Start(22-delay)
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(106736, 120236) then
+		warnWitherWill:Show()
+		timerWitherWillCD:Start()
+	elseif args:IsSpellID(106113) then--Start Cd here in case it's resisted
+		timerTouchofNothingnessCD:Start()
+	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpellID(101591) and self:AntiSpam() then
-		warnRewind:Show()
-		timerBlastCD:Cancel()
-		timerBreathCD:Cancel()
-		timerBlastCD:Start()
-		timerBreathCD:Start()
+	if args:IsSpellID(117665) then
+		timerBoundsOfReality:Show()
+		timerWitherWillCD:Cancel()
+		timerTouchofNothingnessCD:Cancel()
+		timerBoundsOfReality:Start()
+		timerBoundsOfRealityCD:Start()
+	elseif args:IsSpellID(106113) then
+		warnTouchofNothingness:Show(args.destName)
+		specwarnTouchOfNothingness:Show(args.destName)
+		timerTouchofNothingness:Start(args.destName)
 	end
 end
 
-function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(102381) then
-		warnBlast:Show()
-		timerBlastCD:Start()
-	elseif args:IsSpellID(102569) then
-		warnBreath:Show()
-		timerBreathCD:Start()
+function mod:SPELL_AURA_REMOVED(args)
+	if args:IsSpellID(117665) then
+		timerBoundsOfReality:Cancel()
+	elseif args:IsSpellID(106113) then
+		timerTouchofNothingness:Cancel(args.destName)
 	end
-end--]]
+end
