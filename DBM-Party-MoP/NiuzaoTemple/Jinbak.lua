58cd3218ae5bd253b686c4cf29fcd2cb00f9e116
Adam <MysticalOS@users.noreply.github.com>
2012-04-12 03:48:46 +0000

    More boss updates based on new EJ info from latest build

--- DBM-Party-MoP/NiuzaoTemple/Jinbak.lua
+++ DBM-Party-MoP/NiuzaoTemple/Jinbak.lua
@@ -1,7 +1,7 @@
-local mod	= DBM:NewMod("ResinharvesterPH", "DBM-Party-MoP", 6)
+local mod	= DBM:NewMod("Jinbak", "DBM-Party-MoP", 6)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision$"):sub(12, -3))
+mod:SetRevision(("$Revision: 7464 $"):sub(12, -3))
 --mod:SetCreatureID(54432)
 --mod:SetModelID(27394)
 mod:SetZone()
