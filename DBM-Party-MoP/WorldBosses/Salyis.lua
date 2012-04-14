36e015278aec32ef0fb30ac6609c31508755e7b1
Adam <MysticalOS@users.noreply.github.com>
2012-04-14 23:27:47 +0000

    Update zoneID for Salyis/galleon

--- DBM-Party-MoP/WorldBosses/Salyis.lua
+++ DBM-Party-MoP/WorldBosses/Salyis.lua
@@ -2,9 +2,9 @@ local mod	= DBM:NewMod("Salyis", "DBM-Party-MoP", 10)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(50063)
+--mod:SetCreatureID(50063)--Needs two IDs, one ofr Salyis and one for Galleon
 --mod:SetModelID(41772)
---mod:SetZone(858)--Dread Wastes (zoneid not yet known)
+--mod:SetZone(807)--Valley of the Four winds now?
 --Not listed as a world boss anymore in latest patch EJ, was this boss moved?
 
 mod:RegisterCombat("combat")
