e842d12e13996bf7a41520f9c89461ff2d8779dd
Adam <MysticalOS@users.noreply.github.com>
2012-04-17 18:44:46 +0000

    Some mod prep

--- DBM-Party-MoP/WorldBosses/Salyis.lua
+++ DBM-Party-MoP/WorldBosses/Salyis.lua
@@ -2,10 +2,9 @@ local mod	= DBM:NewMod("Salyis", "DBM-Party-MoP", 10)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(50063)--Needs two IDs, one ofr Salyis and one for Galleon
---mod:SetModelID(41772)
---mod:SetZone(807)--Valley of the Four winds now?
---Not listed as a world boss anymore in latest patch EJ, was this boss moved?
+mod:SetCreatureID(62352, 62346)--Salyis 62352, Galleon 62346
+mod:SetModelID(41772)
+mod:SetZone(807)--Valley of the Four winds now?
 
 mod:RegisterCombat("combat")
 
