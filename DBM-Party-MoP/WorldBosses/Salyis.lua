0c91ab6a002040ea8d7cbf6131ba4e21d1a8bc88
Adam <MysticalOS@users.noreply.github.com>
2012-04-17 18:45:37 +0000

    forgot model idea, now that mod is correct

--- DBM-Party-MoP/WorldBosses/Salyis.lua
+++ DBM-Party-MoP/WorldBosses/Salyis.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(62352, 62346)--Salyis 62352, Galleon 62346
-mod:SetModelID(41772)
+mod:SetModelID(42468)
 mod:SetZone(807)--Valley of the Four winds now?
 
 mod:RegisterCombat("combat")
