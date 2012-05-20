2e19be5824f51e2a884a25f9303a6b60449b630a
Arta <Arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-05-20 10:01:29 +0000

    updated mapSizes (Terrace of Endless Spring not in DBC files yet)

--- branches/MoP/DBM-Party-MoP/Scholomance/MapSizes.lua
+++ branches/MoP/DBM-Party-MoP/Scholomance/MapSizes.lua
@@ -1,7 +1,6 @@
---[[DBM:RegisterMapSize("NewScholomance",
-	1, 1799.99993, 1200,		-- Might the from the old scholomance
-	2, 340.000361, 226.6669,
-	3, 445.000366, 296.6669,
-	4, 262.5, 175,
-	5, 262.5, 175
-)--]]
+DBM:RegisterMapSize("NewScholomance",
+	1, 3306.251, 2204.166,
+	2, 4327.298, 2894.588,
+	3, 2552.618, 1701.744,
+	4, 2552.618, 1701.744
+)
