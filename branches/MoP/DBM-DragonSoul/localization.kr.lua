b636645632b3b9e5936bb58c51c0838332e87f96
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-06-01 18:40:32 +0000

    Port changes from live DBM into MoP Branch

--- branches/MoP/DBM-DragonSoul/localization.kr.lua
+++ branches/MoP/DBM-DragonSoul/localization.kr.lua
@@ -52,11 +52,13 @@ L:SetWarningLocalization({
 
 L:SetTimerLocalization({
 	timerOozesActive	= "핏방울 공격 가능",
+	timerOozesReach		= "핏방울 도착"
 })
 
 L:SetOptionLocalization({
 	warnOozesHit		= "보스에게 흡수된 핏방울에 대한 알림 보기",
 	timerOozesActive	= "핏방울이 소환된 후 공격 가능하기까지 남은시간 바 표시",
+	timerOozesReach		= "소환된 핏방울이 보스에게 도착하기까지 남은시간 바 표시",
 	RangeFrame			= "$spell:104898 주문이 활성화 된 경우 거리 프레임 보기 (4m)\n(일반 난이도 이상)"
 })
 
