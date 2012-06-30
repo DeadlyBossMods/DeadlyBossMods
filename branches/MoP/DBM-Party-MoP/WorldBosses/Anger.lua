dc17a9bd901fe65874da5202ac152a0d3a4827a8
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-06-30 07:52:02 +0000

    # fix more bug. # Update Sha of Anger mod.

--- branches/MoP/DBM-Party-MoP/WorldBosses/Anger.lua
+++ branches/MoP/DBM-Party-MoP/WorldBosses/Anger.lua
@@ -19,15 +19,15 @@ local warnGrowingAnger			= mod:NewTargetAnnounce(119622, 4)--Mind control trigge
 local warnAggressiveBehavior	= mod:NewTargetAnnounce(119626, 4)--Actual mind control targets
 
 local specWarnUnleashedWrath	= mod:NewSpecialWarningSpell(119488, mod:IsTank() or mod:IsHealer())--Defaults to tank and healers cause tank probalby want to Cd through this and healers have to heal it, dps just do what they always do and kill stuff.
-local specWarnGrowingAnger		= mod:NewSpecialWarningSpell(119622, nil, nil, nil, true)
+local specWarnGrowingAnger		= mod:NewSpecialWarningYou(119622)
 
 local timerGrowingAngerCD		= mod:NewCDTimer(33, 119622)--Not enough data, as majority of log was boss fighting immune NPCS. it may be 45sec but 33sec while berserked (don't ask, pretty bugged log i have heh)
-local timerWrathCD				= mod:NewCDTimer(50, 119488)--Based on rage, but timing is consistent enough to use a CD bar, might require some perfecting later, similar to xariona's special, if rage doesn't reset after wipes, etc.
-local timerWrath				= mod:NewBuffActiveTimer(28, 119488, nil, mod:IsTank() or mod:IsHealer())
+local timerUnleashedWrathCD		= mod:NewCDTimer(50, 119488)--Based on rage, but timing is consistent enough to use a CD bar, might require some perfecting later, similar to xariona's special, if rage doesn't reset after wipes, etc.
+local timerUnleashedWrath		= mod:NewBuffActiveTimer(28, 119488, nil, mod:IsTank() or mod:IsHealer())
 
 local berserkTimer				= mod:NewBerserkTimer(900)--at least 13 min, speculate 15 but can also be 13 or 14min
 
-mod:AddBoolOption("RangeFrame", true)--For Mind control spreading.
+mod:AddBoolOption("RangeFrame", false)--For Mind control spreading.
 mod:AddBoolOption("SetIconOnMC", true)
 
 local warnpreMCTargets = {}
@@ -41,12 +41,13 @@ do
 	end
 end
 
+-- in field, we can't use 5 yard range frame. Disabled temporarily.
 function mod:updateRangeFrame()
 	if not self.Options.RangeFrame then return end
 	if UnitDebuff("player", GetSpellInfo(119622)) then
-		DBM.RangeCheck:Show(5, nil)--Show everyone.
+		DBM.RangeCheck:Show(10, nil)--Show everyone.
 	else
-		DBM.RangeCheck:Show(5, debuffFilter)--Show only people who have debuff.
+		DBM.RangeCheck:Show(10, debuffFilter)--Show only people who have debuff.
 	end
 end
 
@@ -69,7 +70,7 @@ function mod:OnCombatStart(delay)
 	table.wipe(warnpreMCTargets)
 	table.wipe(warnMCTargets)
 	mcIcon = 8
---	timerWrathCD:Start(-delay)
+--	timerUnleashedWrathCD:Start(-delay)
 --	timerGrowingAngerCD:Start(-delay)
 	berserkTimer:Start(-delay)
 end
@@ -84,10 +85,8 @@ function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(119488) then
 		warnUnleashedWrath:Show()
 		specWarnUnleashedWrath:Show()
-		timerWrath:Start()
+		timerUnleashedWrath:Start()
 	elseif args:IsSpellID(119622) then
-		warnGrowingAnger:Show()
-		specWarnGrowingAnger:Show()
 		timerGrowingAngerCD:Start()
 	end
 end
@@ -99,11 +98,14 @@ function mod:SPELL_AURA_APPLIED(args)
 			self:SetIcon(args.destName, mcIcon)
 			mcIcon = mcIcon - 1
 		end
+		if args:IsPlayer() then
+			specWarnGrowingAnger:Show()			
+		end
 		self:Unschedule(showpreMC)
 		if #warnpreMCTargets >= 3 then
 			showpreMC()
 		else
-			self:Schedule(2.5, showpreMC)
+			self:Schedule(1.0, showpreMC)
 		end
 	elseif args:IsSpellID(119626) then
 		--Maybe add in function to update icons here in case of a spread that results in more then the original 3 getting the final MC debuff.
@@ -117,6 +119,6 @@ function mod:SPELL_AURA_REMOVED(args)
 	if args:IsSpellID(119626) and self.Options.SetIconOnMC then--Remove them after the MCs break.
 		self:SetIcon(args.destName, 0)
 	elseif args:IsSpellID(119488) then
-		timerWrathCD:Start()
+		timerUnleashedWrathCD:Start()
 	end
 end
