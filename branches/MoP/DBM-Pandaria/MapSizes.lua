5d3f799fa54aa17f1a535ba7d72c00c1f66a2b16
Arta <Arta@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-07-02 16:22:30 +0000

    comment map sizes were correct ;-) Calculated the X and Y differences :)

--- branches/MoP/DBM-Pandaria/MapSizes.lua
+++ branches/MoP/DBM-Pandaria/MapSizes.lua
@@ -1,9 +1,7 @@
 DBM:RegisterMapSize("KunLaiSummit",--Kun-Lai Summit
-	0, 850, 566.666016
+	0, 6258.333, 4172.917
 )
---807 870 5805 "ValleyoftheFourWinds" 2679.167 -1245.834 1095.833 -1520.834 -1 0
 
 DBM:RegisterMapSize("ValleyoftheFourWinds",----Valley of the Four winds
-	0, 850, 566.666016
+	0, 3925.001, 2616.667
 )
---809 870 5841 "KunLaiSummit" 4839.583 -1418.75 5618.75 1445.833 -1 0 0 0
