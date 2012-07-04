38a04f9082746b651d9b66573e39643cd4fd2a2e
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-07-04 04:37:29 +0000

    # Remove MoP code in live version. # Update Salyis creature id. (I lost my combat log. Boss waring will be added later.)

--- branches/MoP/DBM-Pandaria/Salyis.lua
+++ branches/MoP/DBM-Pandaria/Salyis.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(725, "DBM-Pandaria", nil, 322)	-- 322 = Pandaria/Outdoor 
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(62352, 62346)--Salyis 62352, Galleon 62346
+mod:SetCreatureID(62346)--Salyis not dies. Only Galleon attackable and dies.
 mod:SetModelID(42468)	--Galleon=42439
 mod:SetZone(807)--Valley of the Four winds
 
