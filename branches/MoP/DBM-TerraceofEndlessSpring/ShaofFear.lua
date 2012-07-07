0bd728410d74588c54107dc62b075999c2122c6c
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-07-07 01:27:21 +0000

    Some CIDs for sha of fear mod, will do the actual mod tomorrow. WTB Nerf to Cheng Kang, Jinlun Kun, and Yang Guoshi

--- branches/MoP/DBM-TerraceofEndlessSpring/ShaofFear.lua
+++ branches/MoP/DBM-TerraceofEndlessSpring/ShaofFear.lua
@@ -2,10 +2,13 @@ local mod	= DBM:NewMod(709, "DBM-TerraceofEndlessSpring", nil, 320)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(12345)	-- Needs to be changed
+mod:SetCreatureID(60999)--61042 Cheng Kang, 61046 Jinlun Kun, 61038 Yang Guoshi, 61034 Terror Spawn
 mod:SetModelID(41772)
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 )
+
+--have all the innerworking and understanding of this mod and logs ready to do it, will actually do it tomorrow. Work at 5am.
+
