813c5d4135b4af55abc1ec3746e15ba4264ac03c
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-07-07 00:12:27 +0000

    # kokR update.

--- branches/MoP/DBM-TerraceofEndlessSpring/localization.kr.lua
+++ branches/MoP/DBM-TerraceofEndlessSpring/localization.kr.lua
@@ -16,11 +16,28 @@ L:SetOptionLocalization({
 ------------
 L= DBM:GetModLocalization(742)
 
+L:SetMiscLocalization{
+	Sunbeam	= "spell:122789",
+	Victory	= "I thank you, strangers. I have been freed."
+}
+
 
 -------------------------------
 -- Lei Shi --
 -------------------------------
 L= DBM:GetModLocalization(729)
+
+L:SetWarningLocalization({
+	warnHideOver			= "%s 종료"
+})
+
+L:SetOptionLocalization({
+	warnHideOver			= "$spell:123244 종료 알림 보기"
+})
+
+L:SetMiscLocalization{
+	Victory	= "I... ah... oh! Did I...? Was I...? It was... so... cloudy."--wtb alternate and less crappy victory event.
+}
 
 
 ----------------------
