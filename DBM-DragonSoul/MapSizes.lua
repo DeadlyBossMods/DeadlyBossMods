0dca8c6c6d595d339a70aebb2f8fff1eeab64147
Adam <MysticalOS@users.noreply.github.com>
2012-07-09 14:10:37 +0000

    Update mapsizes for HoT and Dragonsoul

--- DBM-DragonSoul/MapSizes.lua
+++ DBM-DragonSoul/MapSizes.lua
@@ -1,7 +1,7 @@
 DBM:RegisterMapSize("DragonSoul", 
-	1, 3106.7084960938, 2063.0651855469,	-- Dragonblight
-	2, 397.49887572464, 264.99992263558,	-- Maw of Go'rath (Caution: map data from game files is wrong here)
-	3, 427.50311666243, 285.00046747363,	-- Maw of Shu'ma (Caution: map data from game files is wrong here)
+	1, 3106.70849609375,2063.065185546875,	-- Dragonblight
+	2, 397.5,265.0,							-- Maw of Go'rath
+	3, 427.5,285.0,							-- Maw of Shu'ma
 	4, 185.19921875, 123.466796875,			-- Eye of Eternity
 --	5, 1.5, 1,								-- Gunship (It doesn't actually have usuable coords, useless map data)
 --	6, 1.5, 1,								-- Spine (Same probelm as above)
