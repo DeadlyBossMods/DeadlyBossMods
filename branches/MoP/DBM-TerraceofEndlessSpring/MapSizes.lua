feafe54690f2f88fd3e51007c3b924599028045a
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-07-17 19:22:29 +0000

    Update mapsizes for the 3 raids, fix range check not working if monk is in raid (right now monks will show up as druids but i'll fix that later, this is just to get a quick test fix out

--- branches/MoP/DBM-TerraceofEndlessSpring/MapSizes.lua
+++ branches/MoP/DBM-TerraceofEndlessSpring/MapSizes.lua
@@ -1,5 +1,3 @@
---[[DBM:RegisterMapSize("TerraceOfEndlessSpring", 
-	1, 2599.99902, 1733.333,
-	2, 1635.6512, 1090.43338,
-	3, 2835.32379, 1890.88122
-)--]]
+DBM:RegisterMapSize("TerraceOfEndlessSpring", 
+	1, 702.083984375, 468.75,2497.916015625
+)
