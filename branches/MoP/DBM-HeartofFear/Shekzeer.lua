21676d1ba0e02108d8010357aaf55a2976757d72
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-07-20 18:21:30 +0000

    rush some quick pull detection and tank warnings into shekzeer

--- branches/MoP/DBM-HeartofFear/Shekzeer.lua
+++ branches/MoP/DBM-HeartofFear/Shekzeer.lua
@@ -2,11 +2,29 @@ local mod	= DBM:NewMod(743, "DBM-HeartofFear", nil, 330)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(12345)	-- Needs to be changed
+mod:SetCreatureID(62837)
 mod:SetModelID(42730)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
+	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_APPLIED_DOSE"
 )
+
+local warnEyes					= mod:NewStackAnnounce(123707, 3, nil, mod:IsTank())
+
+local specWarnEyes				= mod:NewSpecialWarningStack(123707, mod:IsTank(), 3)
+local specWarnEyesOther			= mod:NewSpecialWarningTarget(123707, mod:IsTank())
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(123707) then
+		warnEyes:Show(args.destName, args.amount or 1)
+--		timerEyes:Start(args.destName)
+		if args:IsPlayer() and (args.amount or 1) >= 4 then
+			specWarnEyes:Show(args.amount)
+		end
+	end
+end
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
