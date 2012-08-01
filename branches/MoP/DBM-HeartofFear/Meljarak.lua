b368b79bf66ea792e18cade52af23adc8d8746ca
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-08-01 20:13:22 +0000

    target scanning doesn't work, for either warning, so remove it

--- branches/MoP/DBM-HeartofFear/Meljarak.lua
+++ branches/MoP/DBM-HeartofFear/Meljarak.lua
@@ -27,10 +27,10 @@ local warnAmberPrison					= mod:NewTargetAnnounce(121881, 3)
 local warnCorrosiveResin				= mod:NewTargetAnnounce(122064, 3)
 local warnMending						= mod:NewCastAnnounce(122193, 4)
 local warnQuickening					= mod:NewCastAnnounce(122149, 4)
-local warnKorthikStrike					= mod:NewTargetAnnounce(123963, 3)--Target scanning not tested, probably broken.
+local warnKorthikStrike					= mod:NewSpellAnnounce(123963, 3)--Target scanning does NOT work
 
 local specWarnWhirlingBlade				= mod:NewSpecialWarningSpell(121896, nil, nil, nil, true)
-local specWarnWhirlingBladeYou			= mod:NewSpecialWarningYou(121896)--Depends on if target scanning works, otherwise, tanks going to get a lot of wrong spam, :)
+--local specWarnWhirlingBladeYou			= mod:NewSpecialWarningYou(121896)--Depends on if target scanning works, otherwise, tanks going to get a lot of wrong spam, :)
 --local specWarnWhirlingBladeNear		= mod:NewSpecialWarningClose(121896)--Relevant? does it have a radius on it's impact target?
 local yellWhirlingBlade					= mod:NewYell(121896)
 local specWarnRainOfBlades				= mod:NewSpecialWarningSpell(122406, nil, nil, nil, true)
@@ -44,7 +44,7 @@ local yellCorrosiveResin				= mod:NewYell(122064)
 local specWarnCorrosiveResinPool		= mod:NewSpecialWarningMove(122125)
 local specWarnMending					= mod:NewSpecialWarningInterrupt(122193, false)--Whoever is doing this or feels responsible should turn it on.
 local specWarnQuickening				= mod:NewSpecialWarningSpell(122149, false)--^^
-local specWarnKorthikStrike				= mod:NewSpecialWarningYou(123963)--Depends on if target scanning works, otherwise, tanks going to get a lot of wrong spam, :)
+local specWarnKorthikStrike				= mod:NewSpecialWarningSpell(123963, nil, nil, nil, true)--Depends on if target scanning works, otherwise, tanks going to get a lot of wrong spam, :)
 
 local timerWhirlingBladeCD				= mod:NewNextTimer(45.5, 121896)
 local timerRainOfBladesCD				= mod:NewNextTimer(61.5, 122406)--60 CD, but Cd starts when last cast ends, IE 60+cast time. Starting cd off cast start is 61.5, but on pull it's 60.0
@@ -59,7 +59,6 @@ local timerKorthikStrikeCD				= mod:NewCDTimer(40.5, 123963)--^^
 
 mod:AddBoolOption("AmberPrisonIcons", true)
 
-local targetScansDone = 0
 local addsCount = 0
 local amberPrisonIcon = 2
 
@@ -73,64 +72,6 @@ local function isTank(unit)
 		return true
 	end
 	return false
-end
-
-function mod:WhirlingTarget(targetname)
-	warnWhirlingBlade:Show(targetname)
-	if targetname == UnitName("player") then
-		specWarnWhirlingBlade:Show()
-		yellWhirlingBlade:Yell()
---[[	else
-		local uId = DBM:GetRaidUnitId(targetname)
-		if uId then
-			local x, y = GetPlayerMapPosition(uId)
-			if x == 0 and y == 0 then
-				SetMapToCurrentZone()
-				x, y = GetPlayerMapPosition(uId)
-			end
-			local inRange = DBM.RangeCheck:GetDistance("player", x, y)
-			if inRange and inRange < 9 then
-				specWarnWhirlingBladeNear:Show(targetname)
-			end
-		end--]]
-	end
-end
-
---The mobs cast this at same time, so this will probably fuck up and just spam that both casts are on same target, we'll see.
-function mod:KorthikStrikeTarget(sGUID)
-	local targetname, realm = nil
-	for i=1, GetNumGroupMembers() do
-		if UnitGUID("raid"..i.."target") == sGUID then
-			targetname = UnitName("raid"..i.."targettarget")
-			break
-		end
-	end
-	if not targetname then return end
-	warnKorthikStrike:Show(targetname)
-	if targetname == UnitName("player") then
-		specWarnKorthikStrike:Show()
-	end
-end
-
-function mod:TargetScanner(ScansDone)
-	targetScansDone = targetScansDone + 1
-	local targetname, uId = self:GetBossTarget(62397)
-	if UnitExists(targetname) then--Better way to check if target exists and prevent nil errors at same time, without stopping scans from starting still. so even if target is nil, we stil do more checks instead of just blowing off a warning.
-		if isTank(uId) and not ScansDone then--He's targeting his highest threat target.
-			if targetScansDone < 16 then--Make sure no infinite loop.
-				self:ScheduleMethod(0.05, "TargetScanner")--Check multiple times to be sure it's not on something other then tank.
-			else
-				self:TargetScanner(true)--It's still on tank, force true isTank and activate else rule and warn target is on tank.
-			end
-		else--He's not targeting highest threat target (or isTank was set to true after 16 scans) so this has to be right target.
-			self:UnscheduleMethod("TargetScanner")--Unschedule all checks just to be sure none are running, we are done.
-			self:WhirlingTarget(targetname)
-		end
-	else--target was nil, lets schedule a rescan here too.
-		if targetScansDone < 16 then--Make sure not to infinite loop here as well.
-			self:ScheduleMethod(0.05, "TargetScanner")
-		end
-	end
 end
 
 function mod:OnCombatStart(delay)
@@ -203,8 +144,6 @@ function mod:SPELL_CAST_START(args)
 		warnQuickening:Show()
 		specWarnQuickening:Show(args.sourceName)
 		timerQuickeningCD:Start(36, args.sourceGUID)
-	elseif args:IsSpellID(122409) then
-		self:ScheduleMethod(0.2, "KorthikStrikeTarget", args.sourceGUID)
 	end
 end
 
@@ -230,14 +169,14 @@ end
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 	if spellId == 124850 and self:AntiSpam(2, 1) then--Whirling Blade (Throw Cast spellid)
-		targetScansDone = 0
-		self:TargetScanner(124850)
 		specWarnWhirlingBlade:Show()
 		timerWhirlingBladeCD:Start()
 --	"<173.1> [UNIT_SPELLCAST_SUCCEEDED] The Kor'thik [[boss4:Kor'thik Strike::0:123963]]", -- [10366]
 --	"<175.6> [CLEU] SPELL_CAST_START#false#0xF130F3C200000FC8#Kor'thik Elite Blademaster#2632#0#0x0000000000000000#nil#-2147483648#-2147483648#122409#Kor'thik Strike#1", -- [10535]
 --	"<175.6> [CLEU] SPELL_CAST_START#false#0xF130F3C200000FC7#Kor'thik Elite Blademaster#2632#8#0x0000000000000000#nil#-2147483648#-2147483648#122409#Kor'thik Strike#1", -- [10536]
 	elseif spellId == 123963 and self:AntiSpam(2, 2) then--Kor'thik Strike Trigger, only triggered once, then all non CCed Kor'thik cast the strike about 2 sec later
+		warnKorthikStrike:Show()
+		specWarnKorthikStrike:Show()
 		timerKorthikStrikeCD:Start()
 	end
 end
