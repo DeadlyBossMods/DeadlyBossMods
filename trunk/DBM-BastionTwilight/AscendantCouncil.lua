6ff5076f9aab5d655d555d8e0b0c7815c5b95698
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-08-07 02:56:03 +0000

    # Fix Shield Health bar not working.

--- trunk/DBM-BastionTwilight/AscendantCouncil.lua
+++ trunk/DBM-BastionTwilight/AscendantCouncil.lua
@@ -229,9 +229,9 @@ do
 		if shieldedMob == destGUID then
 			local absorbed
 			if subEvent == "SWING_MISSED" then 
-				absorbed = select( 2, ... ) 
+				absorbed = select( 3, ... ) 
 			elseif subEvent == "RANGE_MISSED" or subEvent == "SPELL_MISSED" or subEvent == "SPELL_PERIODIC_MISSED" then 
-				absorbed = select( 5, ... )
+				absorbed = select( 6, ... )
 			end
 			if absorbed then
 				absorbRemaining = absorbRemaining - absorbed
