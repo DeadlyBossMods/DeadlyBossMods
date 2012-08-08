fbcd0c3d6552ebb2cd1be3b69a448e980a8ecf8b
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-08-08 03:04:04 +0000

    # Core : added GetGroupMembers() function to use older mods in MoP without any changes.

--- DBM-DragonSoul/DSTrash.lua
+++ DBM-DragonSoul/DSTrash.lua
@@ -47,7 +47,7 @@ end
 
 function mod:BoulderTarget(sGUID)
 	local targetname = nil
-	for i=1, GetNumRaidMembers() do
+	for i=1, DBM:GetGroupMembers() do
 		if UnitGUID("raid"..i.."target") == sGUID then
 			targetname = DBM:GetUnitFullName("raid"..i.."targettarget")
 			break
