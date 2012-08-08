581c46f61e73ff97339bbbc9acac4fd051f40162
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-08-08 00:52:36 +0000

    # Garajal : add server name check sort_by_group

--- branches/MoP/DBM-MogushanVaults/GarajalTheSpiritBinder.lua
+++ branches/MoP/DBM-MogushanVaults/GarajalTheSpiritBinder.lua
@@ -75,10 +75,13 @@ end--]]
 
 do
 	local function sort_by_group(v1, v2)
-		-- This function seems to broken in cross-realm raid.
-		-- first parameter of UnitName returns only UnitName on other server players. but GetRaidSubgroup return value includes servernames.
-		-- I think it's better that check servernames, because player can be same name on other servers.
-		return DBM:GetRaidSubgroup(UnitName(v1)) < DBM:GetRaidSubgroup(UnitName(v2))
+		-- add server name check
+		local name_v1, r_v1, name_v2, r_v2
+		name_v1, r_v1 = UnitName(v1)
+		name_v2, r_v2 = UnitName(v2)
+		if r_v1 then name_v1 = name_v1.."-"..r_v1 end
+		if r_v2 then name_v2 = name_v2.."-"..r_v2 end
+		return DBM:GetRaidSubgroup(name_v1) < DBM:GetRaidSubgroup(name_v2)
 	end
 	function mod:SetVoodooIcons()
 		if DBM:GetRaidRank() > 0 then
