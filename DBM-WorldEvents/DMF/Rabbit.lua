3f14c7a9c6ed116ed4225250148e7ada660d4774
Adam <MysticalOS@users.noreply.github.com>
2012-09-05 21:46:30 +0000

    added darkmoon fair rabbit world boss. Because I can't pass up an opportunity to make a mod that does "HUGE, SHARP TEETH! on me!" yells when you're neck is being eaten through

---
+++ DBM-WorldEvents/DMF/Rabbit.lua
@@ -0,0 +1,26 @@
+local mod	= DBM:NewMod("Bunny", "DBM-WorldEvents", 2)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision: 7445 $"):sub(12, -3))
+mod:SetCreatureID(58336)
+mod:SetZone()
+
+mod:RegisterCombat("combat")
+mod:SetWipeTime(60)--Not sure how to really handle the coprse cannoning this boss is. You pretty much die and run back repeatedly. Perhaps set this to infinite and end combat on zone change or death? or unit health reset on rabbit?
+
+mod:RegisterEvents(
+	"SPELL_AURA_APPLIED"
+)
+
+local warnTeeth				= mod:NewTargetAnnounce(114078, 4)
+
+local yellTeeth				= mod:NewYell(114078)
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(114078) then
+		warnTeeth:Show(args.destName)
+		if args:IsPlayer() then
+			yellTeeth:Yell()
+		end
+	end
+end
