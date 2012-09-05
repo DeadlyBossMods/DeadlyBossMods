960b27af61edede8c86f04f3badd6c201302602c
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-09-05 21:48:19 +0000

    add a proper set revision flag

--- trunk/DBM-WorldEvents/DMF/Rabbit.lua
+++ trunk/DBM-WorldEvents/DMF/Rabbit.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Bunny", "DBM-WorldEvents", 2)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 7445 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(58336)
 mod:SetZone()
 
