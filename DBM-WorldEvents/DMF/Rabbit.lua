3526ea9d89ea936863888751b743985f5306d3d3
Adam <MysticalOS@users.noreply.github.com>
2012-09-06 00:38:41 +0000

    Fix some fail

--- DBM-WorldEvents/DMF/Rabbit.lua
+++ DBM-WorldEvents/DMF/Rabbit.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod("Bunny", "DBM-WorldEvents", 2)
+local mod	= DBM:NewMod("Rabbit", "DBM-WorldEvents", 2)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
