0695ad6bd418f69e996737d8761a241e8fb24dcb
Adam <MysticalOS@users.noreply.github.com>
2012-09-09 01:07:26 +0000

    Fix invalid thrash spellid so the warnings for it work. Add missing ominous cackle IDs since it actually one of the very few spells that have multiple (ie, it's locational, which means in future when learned, can even have dbm say which platform the cackle is going to). Account for fact in LFR it happens far less often

--- DBM-TerraceofEndlessSpring/ShaofFear.lua
+++ DBM-TerraceofEndlessSpring/ShaofFear.lua
@@ -28,12 +28,12 @@ local specWarnTerrorSpawn				= mod:NewSpecialWarningSwitch("ej6088",  mod:IsDps(
 local specWarnDreadSpray				= mod:NewSpecialWarningSpell(120047, nil, nil, nil, true)--Platform ability, particularly nasty damage, and fear.
 local specWarnDeathBlossom				= mod:NewSpecialWarningSpell(119888, nil, nil, nil, true)--Cast, warns the entire raid.
 
-local timerThrashCD						= mod:NewCDTimer(10, 131996, nil, mod:IsTank() or mod:IsHealer())--Every 10-15 seconds.
+local timerThrashCD						= mod:NewCDTimer(7, 131996, nil, mod:IsTank() or mod:IsHealer())--Every 7-12 seconds.
 local timerBreathOfFearCD				= mod:NewNextTimer(33.5, 119414)--Based off bosses energy, he casts at 100 energy, and gains about 3 energy per second, so every 33-34 seconds is a breath.
 local timerOminousCackleCD				= mod:NewNextTimer(55, 119693)
 local timerDreadSpray					= mod:NewBuffActiveTimer(8, 120047)
 local timerDreadSprayCD					= mod:NewNextTimer(20.5, 120047)
-local timerTerrorSpawnCD				= mod:NewNextTimer(60, 119108)--every 60 or so seconds, maybe a little more maybe a little less, not sure. this is just based on instinct after seeing where 30 fit.
+--local timerTerrorSpawnCD				= mod:NewNextTimer(60, 119108)--every 60 or so seconds, maybe a little more maybe a little less, not sure. this is just based on instinct after seeing where 30 fit.
 
 local ominousCackleTargets = {}
 local platformGUIDs = {}
@@ -47,7 +47,11 @@ end
 
 function mod:OnCombatStart(delay)
 	warnBreathOfFearSoon:Schedule(23.4-delay)
-	timerOminousCackleCD:Start(25.5-delay)
+	if self:IsDifficulty("lfr25") then
+		timerOminousCackleCD:Start(40-delay)
+	else
+		timerOminousCackleCD:Start(25.5-delay)
+	end
 --	timerTerrorSpawnCD:Start(25.5-delay)--still not perfect, it's hard to do yells when you're always the tank sent out of range of them. I need someone else to do /yell when they spawn and give me timing
 --	self:ScheduleMethod(25.5-delay, "TerrorSpawns")
 	timerBreathOfFearCD:Start(-delay)
@@ -58,13 +62,13 @@ function mod:OnCombatStart(delay)
 end
 
 --This may now be depricated, i think blizz synced these up to omninous cackle.
-function mod:TerrorSpawns()
+--[[function mod:TerrorSpawns()
 	if self:IsInCombat() then
 		timerTerrorSpawnCD:Start()
 		self:UnscheduleMethod("TerrorSpawns")
 		self:ScheduleMethod(60, "TerrorSpawns")
 	end
-end
+end--]]
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(119414) and self:AntiSpam(5) then--using this with antispam is still better then registering SPELL_CAST_SUCCESS for a single event when we don't have to. Less cpu cause mod won't have to check every SPELL_CAST_SUCCESS event.
@@ -90,7 +94,7 @@ function mod:SPELL_AURA_APPLIED(args)
 	elseif args:IsSpellID(118977) and args:IsPlayer() then--Fearless, you're leaving platform
 		onPlatform = false
 		platformMob = nil
-	elseif args:IsSpellID(118977) then--Fearless, you're leaving platform
+	elseif args:IsSpellID(131996) then
 		warnThrash:Show()
 		specWarnThrash:Show()
 		timerThrashCD:Start()
@@ -104,9 +108,13 @@ function mod:SPELL_AURA_REMOVED(args)
 end
 
 function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(119693) then
+	if args:IsSpellID(119593, 119692, 119693) then--This seems to have multiple spellids, depending on which platform he's going to send you to. TODO, figure out which is which platform and add additional warnings
 		specWarnOminousCackle:Show()
-		timerOminousCackleCD:Start()
+		if self:IsDifficulty("lfr25") then
+			timerOminousCackleCD:Start(90)--Far less often on LFR
+		else
+			timerOminousCackleCD:Start()
+		end
 		if not onPlatform then--Can't switch to them if you aren't in middle.
 			specWarnTerrorSpawn:Show()
 		end
