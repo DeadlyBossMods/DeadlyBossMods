00dbceee2b813b8183b2fefe4e7b6ed86e1424d2
Adam <MysticalOS@users.noreply.github.com>
2012-09-15 18:57:19 +0000

    apparently that only accepts spellids not EJ entries, so just use call water instead of add name (which is less accurate, but better then localizing it)

--- DBM-Party-MoP/localization.en.lua
+++ DBM-Party-MoP/localization.en.lua
@@ -9,7 +9,7 @@ local L
 L= DBM:GetModLocalization(672)
 
 L:SetOptionLocalization({
-	SetIconOnAdds	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format("ej5616")
+	SetIconOnAdds	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(106526)
 })
 
 -----------------------
