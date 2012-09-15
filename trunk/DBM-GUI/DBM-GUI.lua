81289c7192f7babbafa98896d3893009b03efab0
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-09-15 17:02:19 +0000

    Improve cinematic options, for one, make original option more specific that it only does it inside instances Second, add a new option that actually does do it outside. MoP speed leveling will appreciate such an option to skip the RP and get right to the instances at 90. :)

--- trunk/DBM-GUI/DBM-GUI.lua
+++ trunk/DBM-GUI/DBM-GUI.lua
@@ -1253,7 +1253,7 @@ local function CreateOptionsMenu()
 		----------------------------------------------
 		--             General Options              --
 		----------------------------------------------
-		local generaloptions = DBM_GUI_Frame:CreateArea(L.General, nil, 230, true)
+		local generaloptions = DBM_GUI_Frame:CreateArea(L.General, nil, 250, true)
 	
 		local enabledbm = generaloptions:CreateCheckButton(L.EnableDBM, true)
 		enabledbm:SetScript("OnShow",  function() enabledbm:SetChecked(DBM:IsEnabled()) end)
@@ -1269,10 +1269,11 @@ local function CreateOptionsMenu()
 		end)
 		local UseMasterVolume			= generaloptions:CreateCheckButton(L.UseMasterVolume, true, nil, "UseMasterVolume")
 		local DisableCinematics			= generaloptions:CreateCheckButton(L.DisableCinematics, true, nil, "DisableCinematics")
+		local DisableCinematicsOutside	= generaloptions:CreateCheckButton(L.DisableCinematicsOutside, true, nil, "DisableCinematicsOutside")
 		generaloptions:CreateCheckButton(L.SKT_Enabled, true, nil, "AlwaysShowSpeedKillTimer")
 
 		local bmrange  = generaloptions:CreateButton(L.Button_RangeFrame)
-		bmrange:SetPoint('TOPLEFT', MiniMapIcon, "BOTTOMLEFT", 0, -70)
+		bmrange:SetPoint('TOPLEFT', MiniMapIcon, "BOTTOMLEFT", 0, -100)
 		bmrange:SetScript("OnClick", function(self) 
 			if DBM.RangeCheck:IsShown() then
 				DBM.RangeCheck:Hide()
