f6220dc4ae39a9264c581286e4c2dea618312dd7
Adam <MysticalOS@users.noreply.github.com>
2012-09-18 02:18:49 +0000

    Fix some theramore problems I think, it SHOULD work completely for alliance and halfway for horde

--- DBM-Scenario-MoP/localization.en.lua
+++ DBM-Scenario-MoP/localization.en.lua
@@ -11,5 +11,6 @@ L:SetGeneralLocalization{
 }
 
 L:SetMiscLocalization{
-	Victory = "All of you have my deepest thanks. With the Focusing Iris removed, this lifeless bomb is merely a sickening testament to Garrosh's brutality. The winds of change blow fiercely; Azeroth is on the brink of war. My apologies, but you must excuse me... I have much to consider. Farewell."
+	AllianceVictory = "All of you have my deepest thanks. With the Focusing Iris removed, this lifeless bomb is merely a sickening testament to Garrosh's brutality. The winds of change blow fiercely; Azeroth is on the brink of war. My apologies, but you must excuse me... I have much to consider. Farewell.",
+	HordeVictory	= "Horde victory message still unknown"
 }
