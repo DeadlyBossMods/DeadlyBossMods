8ec5994e9139a4fad70bf39b795fd71953927da0
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-09-20 10:59:06 +0000

    # change Scenario combat start and ends.

--- DBM-Scenario-MoP/localization.kr.lua
+++ DBM-Scenario-MoP/localization.kr.lua
@@ -10,8 +10,3 @@ L= DBM:GetModLocalization("TheramoreFall")
 L:SetGeneralLocalization{
 	name = "테라모어의 몰락"
 }
-
-L:SetMiscLocalization{
-	AllianceVictory = "진심으로 감사드려요. 집중의 눈동자를 제거했으니, 이 폭탄은 이제 가로쉬의 잔인성을 보여주는 증거, 그 이상은 아니에요. 변화의 바람이 불고 있어요. 아제로스는 전쟁의 문턱에 있고요. 죄송하지만 가야겠군요... 생각할 게 많거든요. 그럼, 안녕히.",
-	HordeVictory	= "감사합니다! 이제 이 우울한 섬에서 나가 볼까요?"
-}
