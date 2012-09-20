3e8def58652b577fd6248d9e84aa041c86b3e386
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-09-20 08:25:55 +0000

    # Theramores Fall : remove health frame option. # koKR update.

--- DBM-Scenario-MoP/localization.kr.lua
+++ DBM-Scenario-MoP/localization.kr.lua
@@ -12,6 +12,6 @@ L:SetGeneralLocalization{
 }
 
 L:SetMiscLocalization{
-	AllianceVictory = "All of you have my deepest thanks. With the Focusing Iris removed, this lifeless bomb is merely a sickening testament to Garrosh's brutality. The winds of change blow fiercely; Azeroth is on the brink of war. My apologies, but you must excuse me... I have much to consider. Farewell.",
-	HordeVictory	= "My thanks! Shall we make our way off this miserable little island?"
+	AllianceVictory = "진심으로 감사드려요. 집중의 눈동자를 제거했으니, 이 폭탄은 이제 가로쉬의 잔인성을 보여주는 증거, 그 이상은 아니에요. 변화의 바람이 불고 있어요. 아제로스는 전쟁의 문턱에 있고요. 죄송하지만 가야겠군요... 생각할 게 많거든요. 그럼, 안녕히.",
+	HordeVictory	= "감사합니다! 이제 이 우울한 섬에서 나가 볼까요?"
 }
