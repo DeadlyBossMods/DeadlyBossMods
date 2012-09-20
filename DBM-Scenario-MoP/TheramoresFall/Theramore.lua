3a7763aebf57f7d02db4cb611b9a2426ca7b4a6b
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-09-20 10:59:06 +0000

    # change Scenario combat start and ends.

--- DBM-Scenario-MoP/TheramoresFall/Theramore.lua
+++ DBM-Scenario-MoP/TheramoresFall/Theramore.lua
@@ -2,13 +2,9 @@ local mod	= DBM:NewMod("TheramoreFall", "DBM-Scenario-MoP")
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(65442, 58840)--Warlord Rok'nah (Alliance), Hedric Evencane (Horde)
---mod:SetModelID(43283)
 mod:SetZone()
 
-mod:RegisterCombat("combat")
-mod:RegisterKill("say", L.AllianceVictory, L.HordeVictory)--Required, leader dying doesn't trigger ending, leader AND all his adds do.
-mod:SetWipeTime(1800)--Lame hack, I need to write an exclusions function so i can set, on a mod level, what determines end combat, so at very least we can exclude combat_regen
+mod:RegisterCombat("scenario", 906, 851)
 
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED"
