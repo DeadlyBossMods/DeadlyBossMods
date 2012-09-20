11bc0c6fc2216e90efa2caa93c2a3f375893b85f
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-09-20 08:25:55 +0000

    # Theramores Fall : remove health frame option. # koKR update.

--- DBM-Scenario-MoP/TheramoresFall/Theramore.lua
+++ DBM-Scenario-MoP/TheramoresFall/Theramore.lua
@@ -19,6 +19,8 @@ local warnWarEnginesSights		= mod:NewTargetAnnounce(114570, 4)
 local specWarnWarEnginesSights	= mod:NewSpecialWarningMove(114570)--Actually used by his trash, but in a speed run, you tend to pull it all together
 local yellWarEnginesSights		= mod:NewYell(114570)
 
+mod:RemoveOption("HealthFrame")
+
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(114570) then
 		warnWarEnginesSights:Show(args.destName)
