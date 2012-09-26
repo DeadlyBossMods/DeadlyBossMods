ee4c3eeb1465cdc1dcce657b49bdec405bf2da99
Adam <MysticalOS@users.noreply.github.com>
2012-09-26 23:53:56 +0000

    looks like trial of king does have an english pull local now, so switch it back to that instead of the current one which is iffy, also, fix the group yells while at it since they were changed slightly too Fix kill detection on durand, it does fire a UNIT_DIED event when he dies first time now.

--- DBM-Party-MoP/ScarletCathedral/Durand.lua
+++ DBM-Party-MoP/ScarletCathedral/Durand.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(674, "DBM-Party-MoP", 9, 316)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(60040)--3977 is High Inquisitor Whitemane and 60040 is Commander Durand, we don't really need to add her ID, because we don't ever engage her, and he true death is at same time as her.
+mod:SetCreatureID(60040, 99999)--3977 is High Inquisitor Whitemane and 60040 is Commander Durand, we don't really need to add her ID, because we don't ever engage her, and he true death is at same time as her.
 mod:SetModelID(2043) -- 41220=Durand | 2043=Whitemane
 
 mod:RegisterCombat("combat")
@@ -10,7 +10,8 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
-	"UNIT_SPELLCAST_SUCCEEDED"
+	"UNIT_SPELLCAST_SUCCEEDED",
+	"UNIT_DIED"
 )
 
 --local warnRes					= mod:NewCastAnnounce(111670, 4)--This spell seems to be only found in combatlog. Also, I didn't see any casting bar. (both trashes and bosses). Needs more review for this spell.
@@ -29,7 +30,10 @@ local timerDashingStrike		= mod:NewCDTimer(26, 115676)--not confirmed.
 local timerMassResCD			= mod:NewCDTimer(14, 113134)--every 14-17 seconds? (new heroic log doesn't show it cast this often, but i'll leave it for now)
 local timerDeepSleep			= mod:NewBuffFadesTimer(10, 9256)
 
+local phase = 1
+
 function mod:OnCombatStart(delay)
+	phase = 1
 	timerFlashofSteel:Start(9-delay)
 	timerDashingStrike:Start(24-delay)
 	--Only need these in phase 1 so no sense in wasting cpu in phase 2 and 3.
@@ -57,6 +61,7 @@ end
 --3/28 16:22:43.810  SPELL_CAST_SUCCESS,0xF1300F8900000065,"High Inquisitor Whitemane",0xa48,0x0,0x0000000000000000,nil,0x80000000,0x80000000,9256,"Deep Sleep",0x20
 function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(9256) then--Phase 3
+		phase = 3
 		warnDeepSleep:Show()
 		timerDeepSleep:Start()
 	end
@@ -67,6 +72,7 @@ function mod:SPELL_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, _, _, _, overk
 	if (overkill or 0) > 0 then
 		local cid = self:GetCIDFromGUID(destGUID)
 		if cid == 60040 then--Phase 1 ends when he dies first time, but he doesn't UNIT_DIED first time, there is no other usuable event.
+			phase = 2
 			self:UnregisterShortTermEvents()
 			timerMassResCD:Start()
 			timerFlashofSteel:Cancel()
@@ -85,5 +91,11 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 	elseif spellId == 115676 and self:AntiSpam(2, 2) then
 		warnDashingStrike:Show()
 		timerDashingStrike:Start()
+	end
+end
+
+function mod:UNIT_DIED(args)
+	if phase == 3 and self:GetCIDFromGUID(args.destGUID) == 60040 then
+		DBM:EndCombat(self)
 	end
 end
