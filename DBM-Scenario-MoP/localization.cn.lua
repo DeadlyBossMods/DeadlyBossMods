7ae3ab04120b318c943499faab0023700a7cb1b1
DiabloHu <DiabloHu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-09-28 06:11:24 +0000

    zhCN update

--- DBM-Scenario-MoP/localization.cn.lua
+++ DBM-Scenario-MoP/localization.cn.lua
@@ -1,5 +1,5 @@
 ﻿-- Simplified Chinese by Diablohu(diablohudream@gmail.com)
--- Last update: 9/27/2012
+-- Last update: 9/28/2012
 
 if GetLocale() ~= "zhCN" then return end
 local L
@@ -17,4 +17,14 @@ L:SetGeneralLocalization{
 L:SetMiscLocalization{
 --	AllianceVictory = "All of you have my deepest thanks. With the Focusing Iris removed, this lifeless bomb is merely a sickening testament to Garrosh's brutality. The winds of change blow fiercely; Azeroth is on the brink of war. My apologies, but you must excuse me... I have much to consider. Farewell.",
 --	HordeVictory	= "My thanks! Shall we make our way off this miserable little island?"
+}
+
+----------------------
+-- Arena Of Annihilation --
+----------------------
+
+L= DBM:GetModLocalization("ArenaAnnihilation")
+
+L:SetGeneralLocalization{
+	name = "破军比武场"
 }
