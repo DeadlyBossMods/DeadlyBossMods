c1868335d843ad76397778aff64c9e7d53978fde
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-09-28 14:02:22 +0000

    # Korlof : Flykick seems removed. and update spellid. # koKR update

--- trunk/DBM-Scenario-MoP/localization.kr.lua
+++ trunk/DBM-Scenario-MoP/localization.kr.lua
@@ -10,3 +10,13 @@ L= DBM:GetModLocalization("TheramoreFall")
 L:SetGeneralLocalization{
 	name = "테라모어의 몰락"
 }
+
+----------------------
+-- Arena Of Annihilation --
+----------------------
+
+L= DBM:GetModLocalization("ArenaAnnihilation")
+
+L:SetGeneralLocalization{
+	name = "파멸의 투기장"
+}
