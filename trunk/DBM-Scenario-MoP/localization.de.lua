d28b7326d8e5c314c89c90967a57b3c334510e56
ebmor <ebmor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-09-30 10:41:03 +0000

    deDE update

--- trunk/DBM-Scenario-MoP/localization.de.lua
+++ trunk/DBM-Scenario-MoP/localization.de.lua
@@ -12,6 +12,16 @@ L:SetGeneralLocalization{
 }
 
 L:SetMiscLocalization{
-	AllianceVictory = "All of you have my deepest thanks. With the Focusing Iris removed, this lifeless bomb is merely a sickening testament to Garrosh's brutality. The winds of change blow fiercely; Azeroth is on the brink of war. My apologies, but you must excuse me... I have much to consider. Farewell.", --translate (trigger)
-	HordeVictory	= "Danke! Sollen wir von dieser elenden kleinen Insel verschwinden?"
+--	AllianceVictory = "All of you have my deepest thanks. With the Focusing Iris removed, this lifeless bomb is merely a sickening testament to Garrosh's brutality. The winds of change blow fiercely; Azeroth is on the brink of war. My apologies, but you must excuse me... I have much to consider. Farewell.", --translate (trigger)
+--	HordeVictory	= "Danke! Sollen wir von dieser elenden kleinen Insel verschwinden?"
+}
+
+---------------------------
+-- Arena Of Annihilation --
+---------------------------
+
+L= DBM:GetModLocalization("ArenaAnnihilation")
+
+L:SetGeneralLocalization{
+	name = "Arena der Auslöschung"
 }
