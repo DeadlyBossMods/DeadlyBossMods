4150a2def8be7d42de95bfff27d3c0921812808d
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-10-12 09:37:04 +0000

    # Update "playerpower" info frame for 5-man instance.

--- DBM-Party-MoP/ShadoPanMonastery/TaranZhu.lua
+++ DBM-Party-MoP/ShadoPanMonastery/TaranZhu.lua
@@ -31,7 +31,7 @@ local Hate = EJ_GetSectionInfo(5827)
 function mod:OnCombatStart(delay)
 	if self.Options.InfoFrame then
 		DBM.InfoFrame:SetHeader(Hate)
-		DBM.InfoFrame:Show(5, "playerpower", 5, ALTERNATE_POWER_INDEX) -- this not works. need to find another way.
+		DBM.InfoFrame:Show(5, "playerpower", 5, ALTERNATE_POWER_INDEX)
 	end
 end
 
