15d3086fc3067e040e32c7245bc9f837bb87be03
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-10-31 23:45:31 +0000

    # koKR update.

--- DBM-MogushanVaults/localization.kr.lua
+++ DBM-MogushanVaults/localization.kr.lua
@@ -85,7 +85,8 @@ L:SetTimerLocalization({
 
 L:SetOptionLocalization({
 	specWarnDespawnFloor		= "가운데 바닥이 무너지기 전에 특수 경고 보기",
-	timerDespawnFloor			= "가운데 바닥이 무너지기 전까지 남은 시간 바 표시"
+	timerDespawnFloor			= "가운데 바닥이 무너지기 전까지 남은 시간 바 표시",
+	SetIconOnDestabilized	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(132226)
 })
 
 
@@ -95,7 +96,8 @@ L:SetOptionLocalization({
 L= DBM:GetModLocalization(677)
 
 L:SetOptionLocalization({
-	InfoFrame		= "$spell:116525 주문의 영향을 받은 플레이어를 정보 프레임에 표시"
+	InfoFrame		= "$spell:116525 주문의 영향을 받은 플레이어를 정보 프레임에 표시",
+	ArrowOnCombo	= "$journal:5673 도중 정보 프레임 표시\n알림: 방어전담이 보스 앞에 있고 나머지 공격대원이 뒤에 있을때를 기준으로 합니다."
 })
 
 L:SetMiscLocalization({
