93e1f0c17b2b95496d2ab3fe456bf958837345fb
Adam <MysticalOS@users.noreply.github.com>
2012-11-02 08:30:52 +0000

    More Unsok stuff, still a lot more to do tomorrow, but bed time.

--- DBM-HeartofFear/Unsok.lua
+++ DBM-HeartofFear/Unsok.lua
@@ -15,26 +15,28 @@ mod:RegisterEventsInCombat(
 	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
 	"SPELL_DAMAGE",
-	"SPELL_MISSED"
+	"SPELL_MISSED",
+	"UNIT_POWER"
 )
 
 --[[WoL Reg Expression
-(spellid = 45477 or spellid = 122540 or spellid = 122532 or spellid = 122348) and fulltype = SPELL_CAST_SUCCESS or (spellid =122784 or spellid =121949 or spellid = 122395 or spellid = 121994) and fulltype = SPELL_AURA_APPLIED or (spellid =122370 or spellid = 122540 or spellid = 122395 or spellid = 121994) and fulltype = SPELL_AURA_REMOVED or (spellid = 122408 or spellid = 122413 or spellid = 122398) and fulltype = SPELL_CAST_START or fulltype = UNIT_DIED and (targetname = "Omegal" or targetname = "Shiramune")
+(spellid = 45477 or spellid = 122540 or spellid = 122532 or spellid = 122348) and fulltype = SPELL_CAST_SUCCESS or (spellid =122784 or spellid =121949 or spellid = 122395 or spellid = 121994) and fulltype = SPELL_AURA_APPLIED or (spellid =122370 or spellid = 122540 or spellid = 122395 or spellid = 121994) and fulltype = SPELL_AURA_REMOVED or (spellid = 122408 or spellid = 122413 or spellid = 122398 or spellid = 122540) and fulltype = SPELL_CAST_START or fulltype = UNIT_DIED and (targetname = "Omegal" or targetname = "Shiramune")
 --]]
 --Boss
 local warnReshapeLife			= mod:NewTargetAnnounce(122784, 4)
 local warnAmberScalpel			= mod:NewSpellAnnounce(121994, 3)
 local warnParasiticGrowth		= mod:NewTargetAnnounce(121949, 4, nil, mod:IsHealer())
 --Construct
-local warnAmberExplosion		= mod:NewCastAnnounce(122398, 3, nil, false)--In case you want to get warned for all of them, but could be spammy later fight.
+local warnAmberExplosion		= mod:NewCastAnnounce(122398, 3, nil, nil, false)--In case you want to get warned for all of them, but could be spammy later fight.
 local warnStruggleForControl	= mod:NewTargetAnnounce(122395, 2)--Disabled in phase 3 as at that point it's just a burn.
 local warnDestabalize			= mod:NewStackAnnounce(123059, 1, nil, false)--This can be super spammy so off by default.
 --Living Amber
 local warnLivingAmber			= mod:NewSpellAnnounce("ej6261", 2, nil, false)--122348 is what you check spawns with. ALso spamming and off by default
-local warnBurningAmber			= mod:NewCountAnnounce("ej6567", 2, nil, mod:IsTank())--Keep track of Burning Amber Puddles. Spammy, but nessesary for constructs (most definitely tanks)
+local warnBurningAmber			= mod:NewCountAnnounce("ej6567", 2, nil, false)--Keep track of Burning Amber Puddles. Spammy, but nessesary for constructs (most definitely tanks)
 --Amber Monstrosity
 local warnAmberCarapace			= mod:NewTargetAnnounce(122540, 4)--Monstrosity Shielding Boss (phase 2 start)
 local warnMassiveStomp			= mod:NewCastAnnounce(122408, 3)
+local warnAmberExplosionSoon	= mod:NewPreWarnAnnounce(122402, 10, 3)
 local warnFling					= mod:NewSpellAnnounce(122413, 3)--think this always does his aggro target but not sure. If it does random targets it will need target scanning.
 
 --Boss
@@ -43,8 +45,9 @@ local specwarnReshape			= mod:NewSpecialWarningYou(122784)
 local specwarnParasiticGrowth	= mod:NewSpecialWarningTarget(121949, mod:IsHealer())
 --Construct
 local specwarnAmberExplosionYou	= mod:NewSpecialWarningInterrupt(122398)--Only interruptable by the player controling construct casting, so only person that gets warning.
-local specwarnAmberExplosion	= mod:NewSpecialWarningSpell(122398, nil, nil, nil, true)
---local specwarnWillPower		= mod:NewSpecialWarning("specwarnWillPower")--Some special warning about getting low on Will power needs to be here that tracks alternate UNIT_POWER on self.
+local specwarnAmberExplosionAM	= mod:NewSpecialWarning("specwarnAmberExplosionAM")--Generic not used because dbm can't tell optoins apart since they both have same spell name. This is Amber Monstrosity version of Amber Explosion (122402)
+local specwarnAmberExplosion	= mod:NewSpecialWarningSpell(122398, nil, nil, nil, true)--One you can't interrupt (ie someone screwed up)
+local specwarnWillPower			= mod:NewSpecialWarning("specwarnWillPower")--Some special warning about getting low on Will power needs to be here that tracks alternate UNIT_POWER on self.
 --local specwarnBossDebuff		= mod:NewSpecialWarning("specwarnBossDebuff")--Some special warning that says "get your ass to boss and refresh debuff NOW" (Debuff stacks up to 255 with 10% damage taken increase every stack, keeping buff up and stacking is paramount to dps check)
 --Living Amber
 local specwarnBurningAmber		= mod:NewSpecialWarningMove(122504)--Standing in a puddle
@@ -53,10 +56,10 @@ local specwarnAmberMonstrosity	= mod:NewSpecialWarningSwitch("ej6254", not mod:I
 local specwarnMassiveStomp		= mod:NewSpecialWarningSpell(122408, nil, nil, nil, true)
 
 --Boss
-local timerReshapeLifeCD		= mod:NewNextTimer(50, 122784)
+local timerReshapeLifeCD		= mod:NewNextTimer(50, 122784)--50 second cd in phase 1-2, 15 second in phase 3. if no construct is up, cd is ignored and boss casts it anyways to make sure 1 is always up.
 local timerAmberScalpelCD		= mod:NewCDTimer(40, 121994)--40 seconds after last one ENDED
 local timerAmberScalpel			= mod:NewBuffActiveTimer(10, 121994)
-local timerParasiticGrowthCD	= mod:NewCDTimer(50, 121949, nil, mod:IsHealer())
+local timerParasiticGrowthCD	= mod:NewCDTimer(35, 121949, nil, mod:IsHealer())--35-50 variation (most of the time 50, rare pulls he decides to use 35 sec cd instead)
 local timerParasiticGrowth		= mod:NewTargetTimer(30, 121949, nil, mod:IsHealer())
 --Construct
 local timerAmberExplosionCD		= mod:NewNextSourceTimer(13, 122398)--13 second cd on player controled units, 18 seconds on non player controlled constructs
@@ -64,7 +67,10 @@ local timerDestabalize			= mod:NewTargetTimer(10, 123059)
 local timerStruggleForControl	= mod:NewTargetTimer(5, 122395)
 --Amber Monstrosity
 local timerMassiveStompCD		= mod:NewCDTimer(18, 122540)--18-25 seconds variation
-local timerFlingCD				= mod:NewCDTimer(25, 122413)--25-30sec variation.
+local timerFlingCD				= mod:NewCDTimer(25, 122413)--25-40sec variation.
+local timerAmberExplosionAMCD	= mod:NewTimer(48, "timerAmberExplosionAMCD", 122402)--13 second cd on player controled units, 18 seconds on non player controlled constructs
+
+mod:AddBoolOption("InfoFrame", false)--Needs more work.
 
 local Puddles = 0
 local Phase = 1
@@ -75,7 +81,17 @@ function mod:OnCombatStart(delay)
 	timerAmberScalpelCD:Start(9-delay)
 	timerReshapeLifeCD:Start(20-delay)
 	timerParasiticGrowthCD:Start(23.5-delay)
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:SetHeader("Will Power")--This is a work in progress
+		DBM.InfoFrame:Show(10, "playerpower", 1, ALTERNATE_POWER_INDEX)--At a point i need to add an arg that lets info frame show the 5 LOWEST not the 5 highest, instead of just showing 10
+	end
 end
+
+function mod:OnCombatEnd()
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:Hide()
+	end
+end 
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(123059) and not args:GetDestCreatureID() == 62691 then--Only track debuffs on boss, constructs, or monstrosity, ignore oozes.
@@ -88,7 +104,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		timerParasiticGrowthCD:Start()
 	elseif args:IsSpellID(122540) then
 		Phase = 2
-		warnAmberCarapace:Show()
+		warnAmberCarapace:Show(args.destName)
 		specwarnAmberMonstrosity:Show()
 		timerMassiveStompCD:Start(20)
 		timerFlingCD:Start(33)
@@ -100,10 +116,10 @@ function mod:SPELL_AURA_APPLIED(args)
 		if args:IsPlayer() then
 			specwarnReshape:Show()
 		end
-		if Phase < 3 then--More often in phase 3
+		if Phase < 3 then
 			timerReshapeLifeCD:Start()
 		else
---			timerReshapeLifeCD:Start()--But don't know how often yet, don't have a phase 3 log.
+			timerReshapeLifeCD:Start(15)--More often in phase 3
 		end
 	end
 end
@@ -120,6 +136,9 @@ function mod:SPELL_AURA_REMOVED(args)
 		Phase = 3
 		timerMassiveStompCD:Cancel()
 		timerFlingCD:Cancel()
+		timerAmberExplosionAMCD:Cancel()
+		warnAmberExplosionSoon:Cancel()
+		--He does NOT reset reshape live cd here, he finishes out last one first, THEN starts using new one.
 	end
 end
 
@@ -130,9 +149,14 @@ function mod:SPELL_CAST_START(args)
 			specwarnAmberExplosion:Show()
 			timerAmberExplosionCD:Start(18, args.sourceName, args.sourceGUID)--Longer CD if it's a non player controlled construct. Everyone needs to see this bar because there is no way to interrupt these.
 		elseif args.sourceGUID == UnitGUID("player") then--Cast by YOU
-			specwarnAmberExplosionYou:Show()
+			specwarnAmberExplosionYou:Show(args.sourceName)
 			timerAmberExplosionCD:Start(13, args.sourceName)--Only player needs to see this, they are only person who can do anything about it.
 		end
+	elseif args:IsSpellID(122402) then--Amber Monstrosity
+		specwarnAmberExplosionAM:Show(args.spellName, args.sourceName)
+		warnAmberExplosionSoon:Cancel()
+		warnAmberExplosionSoon:Schedule(39)
+		timerAmberExplosionAMCD:Start(49, args.sourceName, args.sourceGUID)
 	elseif args:IsSpellID(122408) then
 		warnMassiveStomp:Show()
 		specwarnMassiveStomp:Show()
@@ -164,3 +188,13 @@ function mod:SPELL_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 	end
 end
 mod.SPELL_MISSED = mod.SPELL_DAMAGE
+
+function mod:UNIT_POWER(uId)
+	if uId ~= "player" then return end
+	if UnitPower(uId, ALTERNATE_POWER_INDEX) < 28 and not warnedWill then
+		warnedWill = true
+		specwarnWillPower:Show()
+	elseif UnitPower(uId, ALTERNATE_POWER_INDEX) >= 32 and warnedWill then
+		warnedWill = false
+	end
+end
