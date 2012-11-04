cc342986a199bfd669c54523eebf5ab0b765fa26
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-11-04 21:07:26 +0000

    put that icon local in right place

--- trunk/DBM-HeartofFear/localization.en.lua
+++ trunk/DBM-HeartofFear/localization.en.lua
@@ -89,10 +89,10 @@ L= DBM:GetModLocalization(743)
 
 L:SetOptionLocalization({
 	InfoFrame			= "Show info frame for players affected by $spell:125390",
-	RangeFrame			= "Show range frame (5) for $spell:123735"
+	RangeFrame			= "Show range frame (5) for $spell:123735",
+	StickyResinIcons	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(124097)
 })
 
 L:SetMiscLocalization({
-	PlayerDebuffs		= "Fixated",
-	StickyResinIcons	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(124097)
+	PlayerDebuffs		= "Fixated"
 })
