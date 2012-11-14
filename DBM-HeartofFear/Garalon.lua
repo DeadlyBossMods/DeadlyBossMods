fdb0c1ad7a6cf1bdb15aa7d741f79c0b15699738
Adam <MysticalOS@users.noreply.github.com>
2012-11-14 04:23:42 +0000

    print debug didn't work, change it to dbm:addmsg instead. Interrupt warnings still useless and completely non functional

--- DBM-HeartofFear/Garalon.lua
+++ DBM-HeartofFear/Garalon.lua
@@ -64,7 +64,11 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(122754) and args:GetDestCreatureID() == 63191 then--It applies to both creatureids, so we antispam it
 		warnFury:Show(args.destName, args.amount or 1)
-		timerFury:Start()
+		if self:IsDifficulty("lfr25") then
+			timerFury:Start(15)
+		else
+			timerFury:Start()
+		end
 	elseif args:IsSpellID(122786) and args:GetDestCreatureID() == 63191 then--This one also hits both the leg and the boss, so filter second one here as well.
 		-- this warn seems not works? needs review.
 		warnBrokenLeg:Show(args.destName, args.amount or 1)
