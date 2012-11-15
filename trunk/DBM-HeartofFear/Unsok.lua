4578f3b2e0a3eb807d99b88bacada999dfec166b
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-11-15 07:29:38 +0000

    fix logic failure in nameplates code

--- trunk/DBM-HeartofFear/Unsok.lua
+++ trunk/DBM-HeartofFear/Unsok.lua
@@ -163,9 +163,11 @@ function mod:OnCombatStart(delay)
 		--Now change all settings to make the nameplates while in constructs not terrible.
 		if Totems then
 			SetCVar("nameplateShowEnemyTotems", 0)
-		elseif Guardians then
+		end
+		if Guardians then
 			SetCVar("nameplateShowEnemyGuardians", 0)
-		elseif Pets then
+		end
+		if Pets then
 			SetCVar("nameplateShowEnemyPets", 0)
 		end
 		--Check for Tidy plates threat plates (it has additional options to even further hide worthless nameplates on unsok.
@@ -188,9 +190,11 @@ function mod:OnCombatEnd()
 		--if any of settings were on before pull, we put them back to way they were.
 		if Totems then
 			SetCVar("nameplateShowEnemyTotems", 1)
-		elseif Guardians then
+		end
+		if Guardians then
 			SetCVar("nameplateShowEnemyGuardians", 1)
-		elseif Pets then
+		end
+		if Pets then
 			SetCVar("nameplateShowEnemyPets", 1)
 		end
 		if IsAddOnLoaded("TidyPlates_ThreatPlates") then
