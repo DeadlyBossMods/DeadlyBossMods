c27b0d39162331eeece0c8a0b0f6582fcef9e585
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-11-23 01:52:52 +0000

    comment out some of the debug on unsok, those areas are ruled out. still need more info from other spots. i need debug from someone who isn't bad at constructs and actually doesn't have amber strike on CD when monstrosity casts amber explosion. :\

--- trunk/DBM-HeartofFear/Zorlok.lua
+++ trunk/DBM-HeartofFear/Zorlok.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(745, "DBM-HeartofFear", nil, 330)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(62980)
+mod:SetCreatureID(62980)--63554 (Special invisible Vizier that casts the direction based spellid versions of attenuation)
 mod:SetModelID(42807)
 mod:SetZone()
 
