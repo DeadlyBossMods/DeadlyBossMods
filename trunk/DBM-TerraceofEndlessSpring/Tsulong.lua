706a842deb053fa38a74034557f4f5afad92d90a
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-11-26 07:52:39 +0000

    # Tsulong : Fix in lfr combat re-starts after killed.

--- trunk/DBM-TerraceofEndlessSpring/Tsulong.lua
+++ trunk/DBM-TerraceofEndlessSpring/Tsulong.lua
@@ -4,6 +4,7 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(62442)--62919 Unstable Sha, 62969 Embodied Terror
 mod:SetModelID(42532)
+mod:SetReCombatTime(60)--fix lfr combat re-starts after killed.
 
 mod:RegisterCombat("combat")
 mod:RegisterKill("yell", L.Victory)
