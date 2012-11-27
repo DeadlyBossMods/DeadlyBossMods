4dd2ccbb27668567edb1713d56c6750b77a5b8e4
Adam <MysticalOS@users.noreply.github.com>
2012-11-27 20:40:15 +0000

    based on diffs, BATTLEGROUND chat type is just gone. so change all references to the replacement 5.1 has which is INSTANCE_CHAT

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -238,12 +238,12 @@ local function removeEntry(t, val)
 	return existed
 end
 
--- automatically sends an addon message to the appropriate channel (BATTLEGROUND, RAID or PARTY)
+-- automatically sends an addon message to the appropriate channel (INSTANCE_CHAT, RAID or PARTY)
 local function sendSync(prefix, msg)
 	local zoneType = select(2, IsInInstance())
 	msg = msg or ""
 	if zoneType == "pvp" or zoneType == "arena" then
-		SendAddonMessage("D4", prefix .. "\t" .. msg, "BATTLEGROUND")
+		SendAddonMessage("D4", prefix .. "\t" .. msg, "INSTANCE_CHAT")
 	elseif IsInRaid() then
 		SendAddonMessage("D4", prefix .. "\t" .. msg, "RAID")
 	elseif IsInGroup() then
@@ -2173,7 +2173,7 @@ do
 	end
 
 	function DBM:CHAT_MSG_ADDON(prefix, msg, channel, sender)
-		if prefix == "D4" and msg and (channel == "PARTY" or channel == "RAID" or channel == "BATTLEGROUND" or channel == "WHISPER" and self:GetRaidUnitId(sender) ~= "none") then
+		if prefix == "D4" and msg and (channel == "PARTY" or channel == "RAID" or channel == "INSTANCE_CHAT" or channel == "WHISPER" and self:GetRaidUnitId(sender) ~= "none") then
 			handleSync(channel, sender, strsplit("\t", msg))
 		end
 	end
