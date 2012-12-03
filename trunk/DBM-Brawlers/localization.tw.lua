f91bc303f45514fec27fcaf97cf53e3ed706d9af
ultrashining <ultrashining@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-12-03 01:59:15 +0000

    zhTW update(brawler)

---
+++ trunk/DBM-Brawlers/localization.tw.lua
@@ -0,0 +1,42 @@
+﻿if GetLocale() ~= "zhTW" then return end
+local L
+
+-----------------------
+-- Brawlers --
+-----------------------
+L= DBM:GetModLocalization("Brawlers")
+
+L:SetGeneralLocalization({
+	name = "鬥陣俱樂部"
+})
+
+L:SetWarningLocalization({
+	specWarnYourTurn	= "輪到你上場了!"
+})
+
+L:SetOptionLocalization({
+	specWarnYourTurn	= "輪到你上時顯示特別警告",
+	SpectatorMode		= "當旁觀戰鬥時顯示警告/計時器"
+})
+
+L:SetMiscLocalization({
+	Bizmo			= "Bizmo",
+	--I wish there was a better way to do this....so much localizing. :(
+	EnteringArena1	= "Now entering the arena",
+	EnteringArena2	= "Here's our challenger",
+	EnteringArena3	= "Look out... here comes",
+	Victory1		= "is our victor",
+	Victory2		= "Congratulations",
+	Victory3		= "Brilliant victory",
+	Victory4		= "wins",
+	Victory5		= "Keep 'em comin'",
+	Victory6		= "Great job not dying",
+	Lost1			= "were you even trying",
+	Lost2			= "Now would you kindly remove your corpse",
+	Lost3			= "So much blood! Nice",
+	Lost4			= "Get back in line and try again",
+	Lost5			= "you're gonna have to break a few eggs",
+	Lost6			= "try not to die so much",
+	Lost7			= "what a mess",
+	Lost8			= "His name was",--LoL at fight club reference here
+})
