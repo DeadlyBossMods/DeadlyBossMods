02b5106e9179ab5ef5e0ad5ad621a8911e76aa37
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-12-07 06:46:39 +0000

    Disable pungency stuff in LFR though. it's irrelevant there. 20 stacks is cap and does pathetic damage. lets not confuse them with information they aren't used to needing.

--- trunk/DBM-HeartofFear/Garalon.lua
+++ trunk/DBM-HeartofFear/Garalon.lua
@@ -102,7 +102,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		if self.Options.PheromonesIcon then
 			self:SetIcon(args.destName, 2)
 		end
-	elseif args:IsSpellID(123081) then
+	elseif args:IsSpellID(123081) and not self:IsDifficulty("lfr25") then
 		if (args.amount or 1) >= 9 and (args.amount or 1) % 3 == 0 then
 			warnPungency:Show(args.destName, args.amount)
 		end
