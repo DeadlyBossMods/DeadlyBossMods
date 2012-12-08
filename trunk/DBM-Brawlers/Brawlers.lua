6df7bc59261e56e15857d731b2630ffbfe66d3e9
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-12-08 20:36:05 +0000

    Restructure brawlers mod to be a lot cleaner. Bosses will be combined by their rank now. Core options will be split from boss options.

--- trunk/DBM-Brawlers/Brawlers.lua
+++ trunk/DBM-Brawlers/Brawlers.lua
@@ -7,125 +7,27 @@ mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetZone()
 
 mod:RegisterEvents(
-	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS",
-	"SPELL_AURA_APPLIED",
 	"PLAYER_REGEN_ENABLED",
-	"CHAT_MSG_MONSTER_YELL",
-	"UNIT_SPELLCAST_CHANNEL_START",
-	"UNIT_DIED"
+	"CHAT_MSG_MONSTER_YELL"
 )
 
-local warnChomp					= mod:NewSpellAnnounce(135342, 4)
-local warnLumberingCharge		= mod:NewSpellAnnounce(134527, 4)
-local warnHeatedPokers			= mod:NewSpellAnnounce(133286, 4)
-local warnThrowNet				= mod:NewSpellAnnounce(133308, 3)--Pretty dangerous but probably no need for special warning.
-local warnGoblinDevice			= mod:NewSpellAnnounce(133227, 4)
-local warnPyroblast				= mod:NewCastAnnounce(132666, 3)--Hits fairly hard, interruptable, not make or break though. So no special warning. If it hits you you won't wipe.
-local warnFireWall				= mod:NewSpellAnnounce(132666, 4)
-local warnVolatileFlames		= mod:NewSpellAnnounce(134740, 3)
-local warnFireLine				= mod:NewCastAnnounce(133607, 4)
-local warnDevastatingThrust		= mod:NewSpellAnnounce(134777, 4)--1.5 second cast, does 2 million damage. pretty brutal
-local warnSummonTwister			= mod:NewSpellAnnounce(132670, 3)
-local warnStormCloud			= mod:NewSpellAnnounce(135234, 3)--Can be interrupted
-
 local specWarnYourTurn			= mod:NewSpecialWarning("specWarnYourTurn")
-local specWarnChomp				= mod:NewSpecialWarningMove(135342)
-local specWarnLumberingCharge	= mod:NewSpecialWarningMove(134527)
-local specWarnHeatedPokers		= mod:NewSpecialWarningSpell(133286)--Can be interrupted, if you don't have one. can stun through buff or run away. How you handle varies, but you MUST handle it.
-local specWarnGoblinDevice		= mod:NewSpecialWarningSpell(133227)--This is debuff cast, it makes YOU drop mines 3-4 seconds later. you can drop these where you want.
-local specWarnFireWall			= mod:NewSpecialWarningSpell(132666)
-local specWarnFireLine			= mod:NewSpecialWarningMove(133607)
-local specWarnDevastatingThrust	= mod:NewSpecialWarningMove(134777)
-local specWarnStormCloud		= mod:NewSpecialWarningInterrupt(135234)
-
-local timerChompCD				= mod:NewCDTimer(8, 135342)
-local timerLumberingChargeCD	= mod:NewCDTimer(7, 134527)--7-10 sec variation
-local timerHeatedPokers			= mod:NewBuffActiveTimer(8, 133286)
-local timerHeatedPokersCD		= mod:NewCDTimer(29, 133286)
-local timerThrowNetCD			= mod:NewCDTimer(20, 133308)
-local timerGoblinDeviceCD		= mod:NewCDTimer(22, 133227)
-local timerFirewallCD			= mod:NewCDTimer(18, 132666)--18-22 sec variation
-local timerVolatileFlamesCD		= mod:NewCDTimer(11, 134740)--11-20 sec variation
-local timerFireLineCD			= mod:NewCDTimer(15, 133607)--15-22 sec variation
-local timerDevastatingThrustCD	= mod:NewCDTimer(12, 134777)--Need more data to verify CD
-local timerSummonTwisterCD		= mod:NewCDTimer(15, 132670)--15-17 sec variation
 
 mod:AddBoolOption("SpectatorMode", true)
 
 local matchActive = false
 local playerisFighting = false
+local currentRank = 0--Used to stop bars for the right sub mod based on dynamic rank detection from pulls
 
-function mod:SPELL_CAST_START(args)
-	if not self.Options.SpectatorMode and not playerisFighting then return end--Spectator mode is disabled, do nothing.
-	if args:IsSpellID(135342) then
-		warnChomp:Show()--Give reg warnings for spectators
-		timerChompCD:Start()--And timers (first one is after 6 seconds)
-		if playerisFighting then--Only give special warnings if you're in arena though.
-			specWarnChomp:Show()
-		end
-	elseif args:IsSpellID(133286) then
-		warnHeatedPokers:Show()
-		timerHeatedPokersCD:Start()
-		if playerisFighting then
-			specWarnHeatedPokers:Show()
-		end
-	elseif args:IsSpellID(133308) then
-		warnThrowNet:Show()
-		timerThrowNetCD:Start()
-	elseif args:IsSpellID(33975) then--Spellid is used by 5 diff mobs in game, but SetZone sould filter the other 4 mobs.
-		warnPyroblast:Show()
-	elseif args:IsSpellID(132666) then
-		warnFireWall:Show()
-		timerFirewallCD:Start()--First one is 5 seconds after combat start
-		if playerisFighting then
-			specWarnFireWall:Show()
-		end
-	elseif args:IsSpellID(133607) then
-		warnFireLine:Show()
-		timerFireLineCD:Start()--First one is 9-10 seconds after combat start
-		if playerisFighting then
-			specWarnFireLine:Show()
-		end
-	elseif args:IsSpellID(134777) then
-		warnDevastatingThrust:Show()
-		timerDevastatingThrustCD:Start()--First one is 7-8 seconds after combat start
-		if playerisFighting then
-			specWarnDevastatingThrust:Show()
-		end
-	elseif args:IsSpellID(135234) then
-		warnStormCloud:Show()
-		--CD seems to be 32 seconds usually but sometimes only 16? no timer for now
-		if playerisFighting then
-			specWarnStormCloud:Show()
-		end
-	end
-end
-
-function mod:SPELL_CAST_SUCCESS(args)
-	if not self.Options.SpectatorMode and not playerisFighting then return end
-	if args:IsSpellID(133227) then
-		warnGoblinDevice:Show()
-		timerGoblinDeviceCD:Start()--6 seconds after combat start, if i do that kind of detection later
-		if playerisFighting then--Only give special warnings if you're in arena though.
-			specWarnGoblinDevice:Show()
-		end
-	elseif args:IsSpellID(132670) then
-		warnSummonTwister:Show()
-		timerSummonTwisterCD:Start()--22 seconds after combat start?
-	end
-end
-
-function mod:SPELL_AURA_APPLIED(args)
-	if not self.Options.SpectatorMode and not playerisFighting then return end
-	if args:IsSpellID(133286) then
-		timerHeatedPokers:Start()
-	end
+function mod:PlayerFighting()--So external mods can check value of a local variable
+	if playerisFighting then return true end
+	if not playerisFighting then return false end
 end
 
 function mod:CHAT_MSG_MONSTER_YELL(msg, npc, _, _, target)
 --	"<17.2 15:06:00> [CHAT_MSG_MONSTER_YELL] CHAT_MSG_MONSTER_YELL#Now entering the arena: a Rank 1 human warrior, Omegal! Omegal is pretty new around here, so go easy!#Bizmo###Omegal##0#0##0#988##0#false#false"
-	if msg:find(L.EnteringArena1) or msg:find(L.EnteringArena2) or msg:find(L.EnteringArena3) or msg:find(L.EnteringArena4) then
+	if msg:find(L.Rank) then
+		currentRank = L.Rank--This is wrong, need some fancy code to pull number from message (ie "Rank 1") so we can set it as current rank
 		if target == UnitName("player") then
 			specWarnYourTurn:Show()
 			playerisFighting = true
@@ -144,33 +46,12 @@ function mod:PLAYER_REGEN_ENABLED()
 	end
 end
 
-function mod:UNIT_DIED(args)
-	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 67524 then--These 2 have a 1 min 50 second berserk
-		timerThrowNetCD:Cancel()
-	elseif cid == 67525 then--These 2 have a 1 min 50 second berserk
-		timerGoblinDeviceCD:Cancel()
-	end
-end
-
---This event won't really work well for spectators if they target the player instead of boss. This event only fires if boss is on target/focus
---It is however the ONLy event you can detect this spell using.
-function mod:UNIT_SPELLCAST_CHANNEL_START(uId, _, _, _, spellId)
-	if spellId == 134527 and self:AntiSpam() then
-		warnLumberingCharge:Show()
-		timerLumberingChargeCD:Start()
-		if playerisFighting then
-			specWarnLumberingCharge:Show()
-		end
-	end
-end
-
 --Most group up for this so they can buff eachother for matches. Syncing should greatly improve reliability, especially for match end since the person fighting definitely should detect that (probalby missing yells still)
 function mod:OnSync(msg)
 	if msg == "MatchBegin" then
 		matchActive = true
 	elseif msg == "MatchEnd" then
 		matchActive = false
-		DBM:GetModByName("Brawlers"):Stop()--Stop all timers and warnings
+		DBM:GetModByName(currentRank):Stop()--Stop all timers and warnings
 	end
 end
---
+++ trunk/DBM-Brawlers/Rank3.lua
@@ -0,0 +1,42 @@
+local mod	= DBM:NewMod("BrawlRank3", "DBM-Brawlers")
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+--mod:SetCreatureID(60491)
+mod:SetModelID(28649)
+mod:SetZone()
+
+mod:RegisterEvents(
+	"SPELL_CAST_START"
+)
+
+local warnPyroblast				= mod:NewCastAnnounce(132666, 3)--Hits fairly hard, interruptable, not make or break though. So no special warning. If it hits you you won't wipe.
+local warnFireWall				= mod:NewSpellAnnounce(132666, 4)
+local warnDevastatingThrust		= mod:NewSpellAnnounce(134777, 4)--1.5 second cast, does 2 million damage. pretty brutal
+
+local specWarnFireWall			= mod:NewSpecialWarningSpell(132666)
+local specWarnDevastatingThrust	= mod:NewSpecialWarningMove(134777)
+
+local timerFirewallCD			= mod:NewCDTimer(18, 132666)--18-22 sec variation
+local timerDevastatingThrustCD	= mod:NewCDTimer(12, 134777)--Need more data to verify CD
+
+local brawlersMod = DBM:GetModByName("Brawlers")
+
+function mod:SPELL_CAST_START(args)
+	if not brawlersMod.Options.SpectatorMode and not brawlersMod:PlayerFighting() then return end--Spectator mode is disabled, do nothing.
+	if args:IsSpellID(33975) then--Spellid is used by 5 diff mobs in game, but SetZone sould filter the other 4 mobs.
+		warnPyroblast:Show()
+	elseif args:IsSpellID(132666) then
+		warnFireWall:Show()
+		timerFirewallCD:Start()--First one is 5 seconds after combat start
+		if brawlersMod:PlayerFighting() then
+			specWarnFireWall:Show()
+		end
+	elseif args:IsSpellID(134777) then
+		warnDevastatingThrust:Show()
+		timerDevastatingThrustCD:Start()--First one is 7-8 seconds after combat start
+		if brawlersMod:PlayerFighting() then
+			specWarnDevastatingThrust:Show()
+		end
+	end
+end
