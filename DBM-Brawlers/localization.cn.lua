75f6cfbfb5ce2b1bb549f1804e258ebbde755fc0
DiabloHu <DiabloHu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-12-10 18:12:44 +0000

    zhCN update

--- DBM-Brawlers/localization.cn.lua
+++ DBM-Brawlers/localization.cn.lua
@@ -1,16 +1,16 @@
 ﻿-- Simplified Chinese by Diablohu(diablohudream@gmail.com)
--- Last update: 12/5/2012
+-- Last update: 12/11/2012
 
 if GetLocale() ~= "zhCN" then return end
 local L
 
------------------------
+--------------
 -- Brawlers --
------------------------
+--------------
 L= DBM:GetModLocalization("Brawlers")
 
 L:SetGeneralLocalization({
-	name = "搏击俱乐部"
+	name = "搏击俱乐部：设置"
 })
 
 L:SetWarningLocalization({
@@ -18,28 +18,102 @@ L:SetWarningLocalization({
 })
 
 L:SetOptionLocalization({
-	specWarnYourTurn	= "特殊警报：轮到玩家登场",
+	specWarnYourTurn	= "特殊警报：轮到玩家登场h",
 	SpectatorMode		= "在观看比赛时显示警报与计时条"
 })
 
 L:SetMiscLocalization({
-	Bizmo			= "比兹莫",
+--	Bizmo			= "比兹莫",
 	--I wish there was a better way to do this....so much localizing. :(
-	EnteringArena1	= "请大家鼓掌欢迎——",
-	EnteringArena2	= "现在进场的是一位",
-	EnteringArena3	= "Look out... here comes",
-	Victory1		= "is our victor",
-	Victory2		= "Congratulations",
+	Rank			= "(%d+)级",--Experimental "Entering arena" detection by scanning for Rank plus number
+--	EnteringArena1	= "Now entering the arena",
+--	EnteringArena2	= "Here's our challenger",
+--	EnteringArena3	= "Look out... here comes",
+--	EnteringArena4	= "Put your hands together",
+	Victory1		= "胜利者是：",
+	Victory2		= "官方消息！",
 	Victory3		= "辉煌的胜利",
-	Victory4		= "获胜！",
-	Victory5		= "Keep 'em comin'",
-	Victory6		= "Great job not dying",
+	Victory4		= "啊哈哈哈！真血腥！很好。",
+	Victory5		= "让他们放马过来吧",
+	Victory6		= "祝贺你，",
 	Lost1			= "were you even trying",
 	Lost2			= "Now would you kindly remove your corpse",
 	Lost3			= "So much blood! Nice",
 	Lost4			= "Get back in line and try again",
-	Lost5			= "you're gonna have to break a few eggs",
-	Lost6			= "try not to die so much",
+	Lost5			= "想要收获，你就得先付出。",
+	Lost6			= "拜托你下回别再死那么多次了。",
 	Lost7			= "呃……真是一团糟",
-	Lost8			= "His name was",--LoL at fight club reference here
+	Lost8			= "他的名字是",--LoL at fight club reference here
+})
+
+------------
+-- Rank 1 --
+------------
+L= DBM:GetModLocalization("BrawlRank1")
+
+L:SetGeneralLocalization({
+	name = "搏击俱乐部：1级"
+})
+
+------------
+-- Rank 2 --
+------------
+L= DBM:GetModLocalization("BrawlRank2")
+
+L:SetGeneralLocalization({
+	name = "搏击俱乐部：2级"
+})
+
+------------
+-- Rank 3 --
+------------
+L= DBM:GetModLocalization("BrawlRank3")
+
+L:SetGeneralLocalization({
+	name = "搏击俱乐部：3级"
+})
+
+------------
+-- Rank 4 --
+------------
+L= DBM:GetModLocalization("BrawlRank4")
+
+L:SetGeneralLocalization({
+	name = "搏击俱乐部：4级"
+})
+
+------------
+-- Rank 5 --
+------------
+L= DBM:GetModLocalization("BrawlRank5")
+
+L:SetGeneralLocalization({
+	name = "搏击俱乐部：5级"
+})
+
+------------
+-- Rank 6 --
+------------
+L= DBM:GetModLocalization("BrawlRank6")
+
+L:SetGeneralLocalization({
+	name = "搏击俱乐部：6级"
+})
+
+------------
+-- Rank 7 --
+------------
+L= DBM:GetModLocalization("BrawlRank7")
+
+L:SetGeneralLocalization({
+	name = "搏击俱乐部：7级"
+})
+
+------------
+-- Rank 8 --
+------------
+L= DBM:GetModLocalization("BrawlRank8")
+
+L:SetGeneralLocalization({
+	name = "搏击俱乐部：8级"
 })
