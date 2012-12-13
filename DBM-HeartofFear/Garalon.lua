ccc6bca1df864ebed5f90f5495a38d62b765a378
Adam <MysticalOS@users.noreply.github.com>
2012-12-13 20:26:00 +0000

    throttle crush warnings so dbm doesn't freak out when entire raid runs under boss to intentionally wipe it

--- DBM-HeartofFear/Garalon.lua
+++ DBM-HeartofFear/Garalon.lua
@@ -151,7 +151,7 @@ function mod:SPELL_CAST_SUCCESS(args)
 end
 
 function mod:SPELL_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 123120 and destGUID == UnitGUID("player") and self:AntiSpam(3) then
+	if spellId == 123120 and destGUID == UnitGUID("player") and self:AntiSpam(3, 1) then
 		specwarnPheromoneTrail:Show()
 	end
 end
@@ -159,8 +159,10 @@ mod.SPELL_MISSED = mod.SPELL_DAMAGE
 
 function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, _, _, _, target)
 	if msg:find("spell:122774") then
-		warnCrush:Show()
-		specwarnCrush:Show()
+		if self:AntiSpam(3, 2) then
+			warnCrush:Show()
+			specwarnCrush:Show()
+		end
 		timerCrush:Start()
 		if self:IsDifficulty("heroic10", "heroic25") and not msg:find(L.UnderHim) then--unconfirmed
 			timerCrushCD:Start()--unconfirmed, assumed by video
