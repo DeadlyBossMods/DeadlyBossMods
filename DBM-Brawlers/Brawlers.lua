51a3d36b4c95214d3766216aec8063e043aec03b
Adam <MysticalOS@users.noreply.github.com>
2012-12-18 07:31:51 +0000

    add in a small work around for when brawler npcs don't yell on match end. Mainly for berserk timer/warnings

--- DBM-Brawlers/Brawlers.lua
+++ DBM-Brawlers/Brawlers.lua
@@ -71,6 +71,7 @@ end
 --Most group up for this so they can buff eachother for matches. Syncing should greatly improve reliability, especially for match end since the person fighting definitely should detect that (probably missing yells still)
 function mod:OnSync(msg)
 	if msg == "MatchBegin" then
+		self:Stop()--Sometimes bizmo doesn't yell when a match ends too early, if a new match begins we stop on begin before starting new stuff
 		matchActive = true
 		berserkTimer:Start()
 	elseif msg == "MatchEnd" then
