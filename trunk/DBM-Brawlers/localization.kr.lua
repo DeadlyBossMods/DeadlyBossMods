800155a3b1f17b50a5129ad3668a4e6e436bdb93
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-12-18 12:52:31 +0000

    # koKR update.

--- trunk/DBM-Brawlers/localization.kr.lua
+++ trunk/DBM-Brawlers/localization.kr.lua
@@ -7,7 +7,7 @@ local L
 L= DBM:GetModLocalization("Brawlers")
 
 L:SetGeneralLocalization({
-	name = "기본 설정"
+	name = "싸움꾼: 공통 기능"
 })
 
 L:SetWarningLocalization({
@@ -20,36 +20,44 @@ L:SetOptionLocalization({
 })
 
 L:SetMiscLocalization({
---	Bizmo			= "Bizmo",
+--	Bizmo			= "대표 바젤플랜지",
 	--I wish there was a better way to do this....so much localizing. :(
---	Rank			= "(%d+) 계급",--Experimental "Entering arena" detection by scanning for Rank plus number
---	EnteringArena1	= "Now entering the arena",
---	EnteringArena2	= "Here's our challenger",
---	EnteringArena3	= "Look out... here comes",
---	EnteringArena4	= "Put your hands together",
-	Rank1			= "1 계급",
-	Rank2			= "2 계급",
-	Rank3			= "3 계급",
-	Rank4			= "4 계급",
-	Rank5			= "5 계급",
-	Rank6			= "6 계급",
-	Rank7			= "7 계급",
-	Rank8			= "8 계급",
-	Victory1		= "is our victor",
-	Victory2		= "Congratulations",
-	Victory3		= "Brilliant victory",
-	Victory4		= "wins",
-	Victory5		= "Keep 'em comin'",
-	Victory6		= "Great job not dying",
-	Lost1			= "were you even trying",
-	Lost2			= "Now would you kindly remove your corpse",
-	Lost3			= "So much blood! Nice",
-	Lost4			= "Get back in line and try again",
-	Lost5			= "you're gonna have to break a few eggs",
-	Lost6			= "try not to die so much",
-	Lost7			= "what a mess",
-	Lost8			= "His name was",--LoL at fight club reference here
+--	Rank			= "(%d+)계급",--Experimental "Entering arena" detection by scanning for Rank plus number
+--	EnteringArena1	= "들어서는 건",-1,7
+--	EnteringArena2	= "도전자가 나타났습니다.",-4
+--	EnteringArena3	= "조심하십시오...",-2
+--	EnteringArena4	= "박수를 부탁드립니다",-3,8
+	Rank1			= "1계급",
+	Rank2			= "2계급",
+	Rank3			= "3계급",
+	Rank4			= "4계급",
+	Rank5			= "5계급",
+	Rank6			= "6계급",
+	Rank7			= "7계급",
+	Rank8			= "8계급",
+	Victory1		= "승자는",-- 승자는 %s입니다!
+	Victory2		= "축하합니다.",--축하합니다, %s! 거기 투기장 밖에 피투성이 발자국은 남기지 마세요.
+	Victory3		= "멋진 승리였습니다.",--멋진 승리였습니다, 아버님댁보일러! 이제 다시 줄을 서세요.
+	Victory4		= "이겼",--이런 이런 이런... %s가 이겼네요! 저도 돈을 좀 잃은 것 같아요. / %s가 이겼습니다! %s가 이겼어요!
+	Victory5		= "멈추지 마시라고요",--멈추지 마시라고요, %s.
+	Victory6		= "가까스로 살아남았습니다",--끝났습니다! %s가 가까스로 살아남았습니다. 정말 아슬아슬했네요, 전사도적마법사제!
+	Lost1			= "솔직히 말할게요.",--솔직히 말할게요, %s... 대체 지금 뭘 하시는 건가요?
+	Lost2			= "안됐네요,",--안됐네요, %s. 이제 그 시체 좀 치워 주실래요? 다음 싸움꾼들이 기다리고 있어서요.
+	Lost3			= "다음 번에는 이렇게 쉽게 죽지 마세요.",--%s %s 우리 %s... 다음 번에는 이렇게 쉽게 죽지 마세요.
+	Lost4			= "줄은 다시 서야 해요.",
+	Lost5			= "정말 운이 없군요",--하하! 정말 운이 없군요! %s입니다! 아하하하! 당신이 불길 속에서 죽는다에 25골드를 걸었어요!
+	Lost6			= "아이구... 그렇게 아팠으니",--아이구... 그렇게 아팠으니 많이 성숙해지겠어요.
+	Lost7			= "그거 참",--그거 참... 안 좋게 끝났네요.
+	Lost8			= "지금까지",--지금까지 %s 였습니다.
 	Lost9			= "did not end well"
+--투기장에 들어서는 건... 1계급 %s 입니다! %s은 여기 새로 온 친구니까, 살살 해봐요!
+--요새 한창 잘나가는 2계급 사냥꾼, %s가 싸움장에 들어섭니다
+--%s에게 박수를 부탁드립니다! %s는 3계급 주술사입니다.
+--도전자가 나타났습니다. 4계급 언데드 흑마법사, %s를 환영해 주십시오!
+--지금 싸움장에 주먹이 매콤한 주술사가 등장했습니다. 5계급 싸움꾼, %s게 인사하세요!
+--조심하십시오... 무시무시한 6계급 언데드, %가 등장합니다!
+--지금 싸움장에 들어서는 건... 돌아온 최고의 싸움꾼, 7계급 %s 입니다!
+--드디어! 다들 아시죠? 다들 사랑하시죠? 8계급 싸움꾼, %s게 박수를 부탁드립니다!
 })
 
 ------------
