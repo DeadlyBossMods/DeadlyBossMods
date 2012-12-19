4b5731526670bbd9482a90b6a69d54d9d5f8a7e6
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-12-19 07:32:51 +0000

    add some battletron stuff

--- trunk/DBM-Brawlers/Rank7.lua
+++ trunk/DBM-Brawlers/Rank7.lua
@@ -7,10 +7,15 @@ mod:SetModelID(46798)
 mod:SetZone()
 
 mod:RegisterEvents(
-	"SPELL_CAST_START"
+	"SPELL_CAST_START",
+	"SPELL_AURA_APPLID"
 )
 
 local warnRockets				= mod:NewCastAnnounce(133212, 4)
+local warnMines					= mod:NewCountAnnounce(133018, 3)
+local warnMinesSpawning			= mod:NewSpellAnnounce(133018, 4)
+
+local specWarnMinesSpawning		= mod:NewSpecialWarningSpell(133015)
 
 local timerRockets				= mod:NewBuffActiveTimer(9, 133212)
 
@@ -18,11 +23,27 @@ mod:RemoveOption("HealthFrame")
 mod:RemoveOption("SpeedKillTimer")
 
 local brawlersMod = DBM:GetModByName("Brawlers")
+local remainingMines = 7
 
 function mod:SPELL_CAST_START(args)
 	if not brawlersMod.Options.SpectatorMode and not brawlersMod:PlayerFighting() then return end--Spectator mode is disabled, do nothing.
 	if args:IsSpellID(133212) then
 		warnRockets:Show()
 		timerRockets:Schedule(4)
+	elseif args:IsSpellID(133017) then
+		remainingMines = remainingMines - 1
+		warnMines:Show(remainingMines)
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if not brawlersMod.Options.SpectatorMode and not brawlersMod:PlayerFighting() then return end
+	if args:IsSpellID(133015) then
+		warnMinesSpawning:Start()
+		if brawlersMod:PlayerFighting() then
+			specWarnMinesSpawning:Show()
+		end
+	elseif args:IsSpellID(133018) then
+		remainingMines = 7
 	end
 end
