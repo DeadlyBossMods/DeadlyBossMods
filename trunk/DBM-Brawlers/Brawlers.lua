94f788759200d8a80aad8a3dfb5eadf3d2e1566a
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-12-19 21:37:37 +0000

    Fix unit died detection for CRZ'd brawlers, improve debug for same

--- trunk/DBM-Brawlers/Brawlers.lua
+++ trunk/DBM-Brawlers/Brawlers.lua
@@ -83,7 +83,7 @@ end
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 	--"<43.1 01:41:37> [UNIT_SPELLCAST_SUCCEEDED] All›nnar [[focus:General Trigger 1::0:136195]]", -- [251]
 	if spellId == 136195 and self:AntiSpam() then
-		print("Teleport Detected", uId, UnitName(uId))
+		print("Teleport Detected", uId, DBM:GetUnitFullName(uId))
 --		if playerIsFighting then--We check playerIsFighting to filter bar brawls, this should only be true if we were ported into ring.
 --			playerIsFighting = false
 --		end
@@ -92,7 +92,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 end
 
 function mod:UNIT_DIED(args)
-	if currentFighter and currentFighter == args.destName then--They wiped.
+	if currentFighter and currentFighter == DBM:GetUnitFullName(args.destName) then--They wiped.
 		self:SendSync("MatchEnd")
 	end
 end
