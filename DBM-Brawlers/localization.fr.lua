5ea347b649ff49c22ca742bcd0a902b875bfa253
Edoz <Edoz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-12-21 16:25:38 +0000

    # frFR Update

--- DBM-Brawlers/localization.fr.lua
+++ DBM-Brawlers/localization.fr.lua
@@ -24,7 +24,7 @@ L:SetOptionLocalization({
 
 L:SetMiscLocalization({
 	Bizmo			= "Bizmo",--Alliance
-	Bazzelflange	= "Boss Bazzelflange",--Horde
+	Bazzelflange	= "Chef Bazzelcol",--Horde
 	--I wish there was a better way to do this....so much localizing. :(
 	Rank1			= "rang 1",
 	Rank2			= "rang 2",
@@ -34,21 +34,21 @@ L:SetMiscLocalization({
 	Rank6			= "rang 6",
 	Rank7			= "rang 7",
 	Rank8			= "rang 8",
-	Victory1		= "est notre",-- Good
+--[[	Victory1		= "est notre",-- Good
 	Victory2		= "Félicitations,",-- Good
 	Victory3		= "Quelle magnifique victoire,",-- Good
 	Victory4		= "remporte la victoire",-- Good
 	Victory5		= "Continuez comme ça",-- Good
 	Victory6		= "Bravo,",-- Good
+	Victory7		= "Tellement de sang ! Joli",-- Good
 	Lost1			= "Vous avez vraiment essayé",-- Good
 	Lost2			= "Auriez-vous l’obligeance de bien vouloir enlever votre cadavre",-- Good
-	Lost3			= "Tellement de sang ! Joli",-- Good
-	Lost4			= "Retournez dans la file et retentez votre chance",-- Good
-	Lost5			= "On ne fait pas d’omelette sans casser des œufs",-- Good
-	Lost6			= "essayez de moins mourir",-- Good
-	Lost7			= "quel carnage",-- Good
-	Lost8			= "s’appelait",--LoL at fight club reference here
-	Lost9			= "ne s’est pas très bien terminé",-- Good
+	Lost3			= "Retournez dans la file et retentez votre chance",-- Good
+	Lost4			= "On ne fait pas d’omelette sans casser des œufs",-- Good
+	Lost5			= "essayez de moins mourir",-- Good
+	Lost6			= "quel carnage",-- Good
+	Lost7			= "s’appelait",--LoL at fight club reference here
+	Lost8			= "ne s’est pas très bien terminé",-- Good --]]
 	Proboskus		= "Oh dear... I'm sorry, but it looks like you're going to have to fight Proboskus."--This boss is only boss out of 32 that has a custom berserk, so we need a chat yell to detect when he specificly is pulled to adjust berserk timer
 })
 
