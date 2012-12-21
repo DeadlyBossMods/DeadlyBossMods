cd2f5c1c5cdad22d9afcf9287ba567759dad9d17
Adam <MysticalOS@users.noreply.github.com>
2012-12-21 04:16:12 +0000

    oops, fix a bug with the canceling order

--- DBM-HeartofFear/Zorlok.lua
+++ DBM-HeartofFear/Zorlok.lua
@@ -151,6 +151,9 @@ end
 function mod:RAID_BOSS_EMOTE(msg)
 	if msg == L.Platform or msg:find(L.Platform) then
 		platform = platform + 1
+		specwarnPlatform:Show()
+		timerForceCD:Cancel()
+		timerAttenuationCD:Cancel()
 		if platform == 1 and self:IsDifficulty("heroic10", "heroic25") then--Always flies right on heroic
 			timerForceCD:Start(24)
 		elseif platform == 2 and self:IsDifficulty("heroic10", "heroic25") then--Which means left is always 2nd platform
@@ -158,9 +161,6 @@ function mod:RAID_BOSS_EMOTE(msg)
 		elseif platform == 3 then--This platform is same in all modes
 			timerConvertCD:Start(22.5)
 		end
-		specwarnPlatform:Show()
-		timerForceCD:Cancel()
-		timerAttenuationCD:Cancel()
 	end
 end
 
