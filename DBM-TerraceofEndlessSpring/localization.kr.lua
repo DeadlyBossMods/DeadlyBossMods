a0bcc8194ab211072efd1ba51b028ce0d631c25d
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-12-21 23:56:48 +0000

    # Brawlers/General : This will really "fix" berserk timer starting in other zones. # Lei Shi : Rearrange misc option order. # koKR update.

--- DBM-TerraceofEndlessSpring/localization.kr.lua
+++ DBM-TerraceofEndlessSpring/localization.kr.lua
@@ -7,7 +7,7 @@ local L
 L= DBM:GetModLocalization(683)
 
 L:SetOptionLocalization({
-	RangeFrame			= "$spell:111850 주문에 대한 거리 프레임 표시(8m)\n(효과가 있을 경우 모든 플레이어를 표시, 효과가 없으면 효과가 있는 플레이어만 표시)",
+	RangeFrame			= "$spell:111850 주문에 대한 거리 프레임 표시(8m)\n(영향을 받은 경우 모든 공격대원을 표시, 그 외에는 대상자만 표시)",
 	SetIconOnPrison		= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(117436)
 })
 
@@ -37,7 +37,7 @@ L:SetOptionLocalization({
 	warnHideOver			= "$spell:123244 종료 알림 보기",
 	timerSpecialCD			= "$spell:123244 또는 $spell:123461 대기시간 바 표시",
 	SetIconOnGuard			= "$journal:6224에 전술 목표 아이콘 설정",
-	RangeFrame				= "$spell:123121 주문에 대한 거리 프레임 표시(3m/방어전담만)",
+	RangeFrame				= "$spell:123121 주문에 대한 거리 프레임 표시(3m)\n(숨기 중일때는 모든 공격대원 보임, 그 외에는 방어전담만 보임)",
 	GWHealthFrame			= "$spell:123461 주문 사라짐까지 남은 체력을 체력 프레임에 표시\n(보스 체력 프레임이 켜져 있어야 작동함)"
 })
 
