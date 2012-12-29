c9df43766fd788e97898bc0ff9c2bb57a07f7ed1
Edoz <Edoz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2012-12-29 00:17:28 +0000

    # frFR Update

--- DBM-TerraceofEndlessSpring/localization.fr.lua
+++ DBM-TerraceofEndlessSpring/localization.fr.lua
@@ -36,13 +36,13 @@ L:SetWarningLocalization({
 })
 
 L:SetTimerLocalization({
-	timerSpecialCD			= "Prochaine capacité spécial"
+	timerSpecialCD			= "Délai capacité spécial"
 })
 
 L:SetOptionLocalization({
 	warnHideOver			= "Alerte quand $spell:123244 est fini",
-	warnHideProgress		= "Alerte de progression de $spell:123244",
-	timerSpecialCD			= "Afficher le temps avant que la prochaine capacité spécial ne sois lancé",
+	warnHideProgress		= "Alerte des statistiques quand $spell:123244 est fini",
+	timerSpecialCD			= "Délai avant la prochaine capacité spécial", -- revoir
 	SetIconOnGuard			= "Mettre un icone sur les $journal:6224",
 	RangeFrame				= "Afficher le cadre de distance (3m) pour $spell:123121\n(Affiche tout le monde pendant $spell:123244, sinon, ne montre que les Tank)",
 	GWHealthFrame			= "Afficher le cadre de la vie restante avant que $spell:123461 soit fini\n(Nécessite que le cadre de vie des Boss sois activé)"
