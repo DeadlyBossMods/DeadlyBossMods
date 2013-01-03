c7a99ffb0ad1eddd2c4c1f870f8b952c3e7ba4ea
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-01-03 17:32:05 +0000

    Update pandaria mapsizes for 2 new zones in 5.2

--- trunk/DBM-Pandaria/MapSizes.lua
+++ trunk/DBM-Pandaria/MapSizes.lua
@@ -1,7 +1,15 @@
-DBM:RegisterMapSize("KunLaiSummit",--Kun-Lai Summit
+DBM:RegisterMapSize("KunLaiSummit",
 	0, 6258.333, 4172.917
 )
 
-DBM:RegisterMapSize("ValleyoftheFourWinds",----Valley of the Four winds
+DBM:RegisterMapSize("ValleyoftheFourWinds",
 	0, 3925.001, 2616.667
+)
+
+DBM:RegisterMapSize("IsleOfGiants",
+	0, 1787.5009765625,1191.666015625
+)
+
+DBM:RegisterMapSize("IsleoftheThunderKing",
+	0, 4135.416015625,2756.25
 )
