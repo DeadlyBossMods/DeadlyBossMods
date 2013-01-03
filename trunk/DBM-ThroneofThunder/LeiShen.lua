f9182e767d6fce2717e5a3d55313054fc36dd86f
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-01-03 20:15:23 +0000

    bump sort order for mods. Add blank mods and map data and encounter IDs for 5.2 raid.

---
+++ trunk/DBM-ThroneofThunder/LeiShen.lua
@@ -0,0 +1,13 @@
+if select(4, GetBuildInfo()) < 50200 then return end--Don't load on live
+local mod	= DBM:NewMod(832, "DBM-ThroneofThunder", nil, 362)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+--mod:SetCreatureID(62983)
+--mod:SetModelID(42811)
+
+mod:RegisterCombat("combat")
+
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_START"
+)
