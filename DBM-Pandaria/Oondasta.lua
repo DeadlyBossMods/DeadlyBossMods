a074d32e895357cc31f953815f5011d12342bd58
Adam <MysticalOS@users.noreply.github.com>
2013-01-03 01:28:52 +0000

    Fix bug in last Add loading condition for it to core

--- DBM-Pandaria/Oondasta.lua
+++ DBM-Pandaria/Oondasta.lua
@@ -1,11 +1,11 @@
 if not select(4, GetBuildInfo()) >= 50200 then return end--Don't load on live
-local mod	= DBM:NewMod(814, "DBM-Pandaria", nil, 322)
+local mod	= DBM:NewMod(826, "DBM-Pandaria", nil, 322)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(69161)--Not yet available in PTR, CID not known
+mod:SetCreatureID(69161)--Not yet available in PTR, CID not known
 --mod:SetModelID(41448)
---mod:SetZone(929)--No map ID for area yet. Lightning Isle
+mod:SetZone(929)--No map ID for area yet. Lightning Isle
 
 mod:RegisterCombat("combat")
 mod:SetWipeTime(120)
