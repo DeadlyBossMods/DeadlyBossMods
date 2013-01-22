8c4c1ebc755fe0b904e3c272599e58eccc9d7969
ultrashining <ultrashining@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-01-22 12:31:47 +0000

    zhTW update

--- DBM-ThroneofThunder/localization.tw.lua
+++ DBM-ThroneofThunder/localization.tw.lua
@@ -11,14 +11,27 @@ L= DBM:GetModLocalization(827)
 --------------
 L= DBM:GetModLocalization(819)
 
+L:SetTimerLocalization({
+	timerAddsCD		= "Next Tribal Door",
+})
+
+L:SetOptionLocalization({
+	timerAddsCD		= "Show timer for next Tribal Door phase",
+})
+
+L:SetMiscLocalization({
+	newForces		= "forces pour from the",--Farraki forces pour from the Farraki Tribal Door!
+	chargeTarget	= "stamps his tail!"--Horridon sets his eyes on Eraeshio and stamps his tail!
+})
+
 ---------------------------
 -- The Council of Elders --
 ---------------------------
 L= DBM:GetModLocalization(816)
 
 L:SetOptionLocalization({
-	warnSandBolt			= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.target:format(136189),
-	RangeFrame				= "顯示距離框架"
+	warnSandBolt	= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.target:format(136189),
+	RangeFrame		= "顯示距離框架"
 })
 
 ------------
@@ -55,6 +68,10 @@ L= DBM:GetModLocalization(824)
 -- Iron Qon --
 --------------
 L= DBM:GetModLocalization(817)
+
+L:SetOptionLocalization({
+	RangeFrame		= "顯示動態距離框架(當太多人太接近時會動態顯示)"
+})
 
 -------------------
 -- Twin Consorts --
