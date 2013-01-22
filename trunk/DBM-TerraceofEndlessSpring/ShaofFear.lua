30916187bb112bcc0feb9bbff9d69cb0cd928700
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-01-22 04:46:26 +0000

    Fix platform check, among other issues (SPELL_AURA_REMOVED not registered)

--- trunk/DBM-TerraceofEndlessSpring/ShaofFear.lua
+++ trunk/DBM-TerraceofEndlessSpring/ShaofFear.lua
@@ -11,6 +11,7 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_APPLIED_DOSE",
+	"SPELL_AURA_REMOVED",
 	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
 	"UNIT_DIED",
