c5b1c96579054d00347d6476682923a529fce821
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-01-28 14:26:48 +0000

    # Lions Landing is only for alliance. So change mod and folder name to neutral and added horde's name and map ids. # koKR update.

--- DBM-Scenario-MoP/Landfall/Landfall.lua
+++ DBM-Scenario-MoP/Landfall/Landfall.lua
@@ -1,10 +1,10 @@
-local mod	= DBM:NewMod("LionsLanding", "DBM-Scenario-MoP")
+local mod	= DBM:NewMod("Landfall", "DBM-Scenario-MoP")
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 7871 $"):sub(12, -3))
 mod:SetZone()
 
-mod:RegisterCombat("scenario", 911)
+mod:RegisterCombat("scenario", 911, 920)
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START"
@@ -13,7 +13,7 @@ mod:RegisterEventsInCombat(
 local warnDivineStorm		= mod:NewSpellAnnounce(135404, 4, nil, mod:IsMelee())
 local warnDivineLight		= mod:NewSpellAnnounce(135403, 4)
 
-local specWarnDivineLight	= mod:NewSpecWarnInterrupt(135403)
+local specWarnDivineLight	= mod:NewSpecialWarningInterrupt(135403)
 
 mod:RemoveOption("HealthFrame")
 
