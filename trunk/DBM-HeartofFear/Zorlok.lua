181d313e6e121d99afe073e2024cd481d1dc183a
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-01-31 09:20:07 +0000

    try to fix a rare bug where the bosses highest threat target is a valid unit id that is NOT a player (such as a pet). non player units do not appear on map and give bad coords causing no warning to fire. Now, if coords return 0 0 we warn anyways as a failsafe. Hopefully this solves problem. It's rare but it happens.

--- trunk/DBM-HeartofFear/Zorlok.lua
+++ trunk/DBM-HeartofFear/Zorlok.lua
@@ -97,7 +97,7 @@ function mod:SPELL_AURA_APPLIED(args)
 			x, y = GetPlayerMapPosition(uId)
 		end
 		local inRange = DBM.RangeCheck:GetDistance("player", x, y)
-		if inRange and inRange < 40 then--Only show exhale warning if the target is near you (ie on same platform as you). Otherwise, we ignore it since we are likely with the echo somewhere else and this doesn't concern us
+		if (inRange and inRange < 40) or (x == 0 and y == 0) then--Only show exhale warning if the target is near you (ie on same platform as you). Otherwise, we ignore it since we are likely with the echo somewhere else and this doesn't concern us
 			warnExhale:Show(args.destName)
 			specwarnExhale:Show(args.destName)
 			timerExhale:Start(args.destName)
@@ -144,7 +144,7 @@ function mod:SPELL_CAST_START(args)
 				x, y = GetPlayerMapPosition(uId)
 			end
 			local inRange = DBM.RangeCheck:GetDistance("player", x, y)--We check how far we are from the tank who has that boss
-			if inRange and inRange < 60 then--Only show warning if we are near the boss casting it (or rathor, the player tanking that boss). I realize orbs go very far, but the special warning is for the dance, not stray discs, that's what normal warning is for
+			if (inRange and inRange < 60) or (x == 0 and y == 0) then--Only show warning if we are near the boss casting it (or rathor, the player tanking that boss). I realize orbs go very far, but the special warning is for the dance, not stray discs, that's what normal warning is for
 				if self.Options.ArrowOnAttenuation then
 					DBM.Arrow:ShowStatic(lastDirection == L.Left and 90 or 270, 12)
 				end
