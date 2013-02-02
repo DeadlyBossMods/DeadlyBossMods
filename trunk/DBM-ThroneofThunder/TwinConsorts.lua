a35e1aadcbe28722ffaff27cc589ff92a544ecc4
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-02-02 05:34:54 +0000

    that should be good enough for now for tortos mod Add CIDs for consorts Much more to do tomorrow

--- trunk/DBM-ThroneofThunder/TwinConsorts.lua
+++ trunk/DBM-ThroneofThunder/TwinConsorts.lua
@@ -3,7 +3,7 @@ local mod	= DBM:NewMod(829, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(62983)
+mod:SetCreatureID(68905, 68904)--Lu'lin 68905, Suen 68904
 mod:SetModelID(46975)--Lu'lin, 46974 Suen
 
 mod:RegisterCombat("combat")
