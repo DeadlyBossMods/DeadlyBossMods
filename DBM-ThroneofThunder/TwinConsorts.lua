2f9c4c6ac059e67bc037d4a0d1b5c044c1855cad
Adam <MysticalOS@users.noreply.github.com>
2013-02-02 09:21:22 +0000

    Ok going to bed. but figured i'd throw that in twins mod for now. Seemed to be most important starting place

--- DBM-ThroneofThunder/TwinConsorts.lua
+++ DBM-ThroneofThunder/TwinConsorts.lua
@@ -9,5 +9,26 @@ mod:SetModelID(46975)--Lu'lin, 46974 Suen
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START"
+	"SPELL_CAST_START",
+	"UNIT_SPELLCAST_SUCCEEDED"
 )
+
+local warnNight							= mod:NewSpellAnnounce("ej7641", 2, 108558)
+local warnDay							= mod:NewSpellAnnounce("ej7645", 2, 122789)
+
+local timerNightCD						= mod:NewNextTimer(184, "ej7641", nil, nil, nil, 130013)
+local timerDayCD						= mod:NewNextTimer(184, "ej7645", nil, nil, nil, 122789)--Probably just need localizing, no short text version.
+
+function mod:OnCombatStart(delay)
+
+end
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
+	if spellId == 137105 and self:AntiSpam(2, 1) then--Suen POrts away (Night Phase)
+		warnNight:Show()
+		timerDayCD:Start()
+	elseif spellId == 137187 and self:AntiSpam(2, 2) then--Lu'lin Ports away (Day Phase)
+		warnDay:Show()
+		timerNightCD:Start()
+	end
+end
