68171119a06af7023fc34df3b5f0fd5e656ed1ff
Adam <MysticalOS@users.noreply.github.com>
2013-02-06 19:52:46 +0000

    commit quills timer for now. will get rest later but this should give some ptr some ease

--- DBM-ThroneofThunder/JiKun.lua
+++ DBM-ThroneofThunder/JiKun.lua
@@ -3,7 +3,7 @@ local mod	= DBM:NewMod(828, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(62983)
+mod:SetCreatureID(69712)
 mod:SetModelID(46675)
 
 mod:RegisterCombat("combat")
@@ -12,6 +12,20 @@ mod:RegisterEventsInCombat(
 	"SPELL_CAST_START"
 )
 
-function mod:OnCombatStart(delay)
+local warnQuills			= mod:NewSpellAnnounce(134380, 3)
 
+local specWarnQuills		= mod:NewSpecialWarningSpell(134380, nil, nil, nil, true)
+
+local timerQuillsCD			= mod:NewCDTimer(60, 134380)
+
+function mod:OnCombatStart(delay)
+	timerQuillsCD:Start(50-delay)
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(134380) then
+		warnQuills:Show()
+		specWarnQuills:Show()
+		timerQuillsCD:Start()
+	end
 end
