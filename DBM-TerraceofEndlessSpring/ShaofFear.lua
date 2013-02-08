46e4ee5d4bdc8295ad78f157453d3a066dcf6919
Adam <MysticalOS@users.noreply.github.com>
2013-02-08 23:36:03 +0000

    give the movement stuff in sha of fear more clarity in options so you can really understand how it works

--- DBM-TerraceofEndlessSpring/ShaofFear.lua
+++ DBM-TerraceofEndlessSpring/ShaofFear.lua
@@ -44,7 +44,7 @@ local specWarnThrash					= mod:NewSpecialWarningSpell(131996, mod:IsTank())
 local specWarnOminousCackleYou			= mod:NewSpecialWarningYou(129147)--You have debuff, just warns you.
 local specWarnDreadSpray				= mod:NewSpecialWarningSpell(120047, nil, nil, nil, 2)--Platform ability, particularly nasty damage, and fear.
 local specWarnDeathBlossom				= mod:NewSpecialWarningSpell(119888, nil, nil, nil, 2)--Cast, warns the entire raid.
-mod:AddBoolOption("specWarnMovement", false, "announce")
+mod:AddBoolOption("specWarnMovement", false, "announce")--http://mysticalos.com/terraceofendlesssprings.jpg
 local MoveWarningForward				= mod:NewSpecialWarning("MoveForward", nil, false)--Warning to switch sites on platform
 local MoveWarningRight					= mod:NewSpecialWarning("MoveRight", nil, false)--Warning to move one eighth to the right
 local MoveWarningBack					= mod:NewSpecialWarning("MoveBack", nil, false)--Move back to starting position
