705bde5d8a3c7ff7b1c3efe2d748a30c5b11cfe0
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-02-19 23:52:12 +0000

    remove yells on "visions of demise"

--- trunk/DBM-HeartofFear/Shekzeer.lua
+++ trunk/DBM-HeartofFear/Shekzeer.lua
@@ -48,7 +48,6 @@ local specwarnFixate			= mod:NewSpecialWarningYou(125390)
 local specWarnDispatch			= mod:NewSpecialWarningInterrupt(124077, mod:IsMelee())
 local specWarnAdvance			= mod:NewSpecialWarningSpell(125304)
 local specwarnVisions			= mod:NewSpecialWarningYou(124862)
-local yellVisions				= mod:NewYell(124862, nil, false)
 local specWarnConsumingTerror	= mod:NewSpecialWarningSpell(124849, not mod:IsTank())
 local specWarnHeartOfFear		= mod:NewSpecialWarningYou(125638)
 local yellHeartOfFear			= mod:NewYell(125638)
@@ -156,7 +155,6 @@ function mod:SPELL_AURA_APPLIED(args)
 		visonsTargets[#visonsTargets + 1] = args.destName
 		if args:IsPlayer() then
 			specwarnVisions:Show()
-			yellVisions:Yell()
 		end
 		self:Unschedule(warnVisionsTargets)
 		self:Schedule(0.3, warnVisionsTargets)
