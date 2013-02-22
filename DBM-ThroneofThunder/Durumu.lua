a3373a7981e1697ad0b5891066b27551f1c61288
Adam <MysticalOS@users.noreply.github.com>
2013-02-22 08:33:53 +0000

    Add visions yell back in but disable it in LFR where it's useless LingeringGaze now shows in combat log so we can now warn the targets yay. Force of will no longer shows in combat log so detection method had to be changed Finished phase rotation timing so mod is far more complete now.

--- DBM-ThroneofThunder/Durumu.lua
+++ DBM-ThroneofThunder/Durumu.lua
@@ -23,11 +23,12 @@ mod:RegisterEventsInCombat(
 
 local warnHardStare					= mod:NewSpellAnnounce(133765, 3, nil, mod:IsTank() or mod:IsHealer())--Announce CAST not debuff, cause it misses a lot, plus we have 1 sec to hit an active mitigation
 local warnForceOfWill				= mod:NewSpellAnnounce(136932, 4)
-local warnLingeringGaze				= mod:NewSpellAnnounce(138467, 3)--Seems highly variable Cd so no timer for this yet
+local warnLingeringGaze				= mod:NewTargetAnnounce(138467, 3)--Seems highly variable Cd so no timer for this yet
 local warnBlueBeam					= mod:NewTargetAnnounce(134122, 2)
 local warnRedBeam					= mod:NewTargetAnnounce(134123, 2)
 local warnYellowBeam				= mod:NewTargetAnnounce(134124, 2)
 local warnCrimsonLeft				= mod:NewAddsLeftAnnounce("ej6892", 2, 134123)
+local warnDisintegrationBeam		= mod:NewSpellAnnounce(133775, 4, 134169)
 local warnLifeDrain					= mod:NewTargetAnnounce(133795, 3, nil, mod:IsTank() or mod:IsHealer())
 local warnDarkParasite				= mod:NewTargetAnnounce(133597, 3, nil, mod:IsHealer())--Heroic
 local warnIceWall					= mod:NewSpellAnnounce(134587, 3)
@@ -37,33 +38,54 @@ local specWarnSeriousWoundOther		= mod:NewSpecialWarningTarget(133767, mod:IsTan
 local specWarnForceOfWill			= mod:NewSpecialWarningYou(136932, nil, nil, nil, 3)--VERY important, if you get hit by this you are out of fight for rest of pull.
 local specWarnForceOfWillNear		= mod:NewSpecialWarningClose(136932)
 local yellForceOfWill				= mod:NewYell(136932)
-local specWarnLingeringGaze			= mod:NewSpecialWarningMove(134044)
+local specWarnLingeringGaze			= mod:NewSpecialWarningYou(134044)
+local yellLingeringGaze				= mod:NewYell(134044, nil, false)
+local specWarnLingeringGazeMove		= mod:NewSpecialWarningMove(134044)
 local specWarnBlueBeam				= mod:NewSpecialWarningYou(134122)
 local specWarnRedBeam				= mod:NewSpecialWarningYou(134123)
 local specWarnYellowBeam			= mod:NewSpecialWarningYou(134124)
-local specWarnDisintegrationBeam	= mod:NewSpecialWarning("specWarnDisintegrationBeam", nil, nil, nil, true)
+local specWarnDisintegrationBeam	= mod:NewSpecialWarning("specWarnDisintegrationBeam", nil, nil, nil, 2)
+local specWarnEyeSore				= mod:NewSpecialWarningMove(140502)
 local specWarmLifeDrain				= mod:NewSpecialWarningTarget(133795, mod:IsTank())--Pretty much exhale all over again from zorlok. Tank intercepts beam to take damage instead
 
 local timerHardStareCD				= mod:NewCDTimer(12, 133765, mod:IsTank() or mod:IsHealer())--10 second cd but delayed by everything else. Example variation, 12, 15, 9, 25, 31
 local timerSeriousWound				= mod:NewTargetTimer(60, 133767, mod:IsTank() or mod:IsHealer())
-local timerLingeringGazeCD			= mod:NewCDTimer(25, 138467)
+local timerLingeringGazeCD			= mod:NewCDTimer(45, 138467)
 local timerForceOfWillCD			= mod:NewCDTimer(60, 136932)
 local timerLightSpectrumCD			= mod:NewCDTimer(60, "ej6891")--Don't know when 2nd one is cast.
 local timerDarkParasite				= mod:NewTargetTimer(30, 136932, mod:IsHealer())--Only healer/dispeler needs to know this.
-local timerDarkPlague				= mod:NewTargetTimer(30, 133598)--EVERYONE needs to know this, if dispeler fucked up and dispelled parasite too early you're going to get a new add every 3 seconds for remaining duration of this bar.
-local timerDisintegrationBeamCD		= mod:NewNextTimer(135, 133775)
+local timerDarkPlague				= mod:NewTargetTimer(30, 133598)--EVERYONE needs to know this, if dispeler messed up and dispelled parasite too early you're going to get a new add every 3 seconds for remaining duration of this bar.
+local timerDisintegrationBeam		= mod:NewBuffActiveTimer(60, 133775)--use 134169 for icon later
+local timerDisintegrationBeamCD		= mod:NewNextTimer(131, 133775)--use 134169 for icon later
 
 local crimsonFogs = 3
+local lingeringGazeTargets = {}
 
 mod:AddBoolOption("ArrowOnBeam", true)
 
+local function warnLingeringGazeTargets()
+	warnLingeringGaze:Show(table.concat(lingeringGazeTargets, "<, >"))
+	table.wipe(lingeringGazeTargets)
+end
+
+local function BeamEnded()
+	if mod.Options.ArrowOnBeam then
+		DBM.Arrow:Hide()
+	end
+	timerForceOfWillCD:Start(19)
+	timerLingeringGazeCD:Start(25)
+	timerLightSpectrumCD:Start(37)
+	timerDisintegrationBeamCD:Start()
+end
+
 function mod:OnCombatStart(delay)
 	crimsonFogs = 3
+	table.wipe(lingeringGazeTargets)
 	timerHardStareCD:Start(5-delay)
-	timerLingeringGazeCD:Start(15-delay)
+	timerLingeringGazeCD:Start(15.5-delay)
 	timerForceOfWillCD:Start(30.5-delay)
-	timerLightSpectrumCD:Start(42-delay)
-	timerDisintegrationBeamCD:Start(-delay)
+	timerLightSpectrumCD:Start(41-delay)
+	timerDisintegrationBeamCD:Start(135-delay)
 end
 
 function mod:OnCombatEnd()
@@ -85,7 +107,11 @@ function mod:SPELL_CAST_START(args)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(136932) then--Force of Will Precast
+	if args:IsSpellID(133795) then
+		warnLifeDrain:Show(args.destName)
+		specWarmLifeDrain:Show(args.destName)
+--[[Blizz disabled this from combat log in latest build, wtf? so now we HAVE to use emote for it
+	elseif args:IsSpellID(136932) then--Force of Will Precast
 		warnForceOfWill:Show(args.destName)
 		if args:IsPlayer() then
 			specWarnForceOfWill:Show()
@@ -103,10 +129,7 @@ function mod:SPELL_CAST_SUCCESS(args)
 					specWarnForceOfWillNear:Show(args.destName)
 				end
 			end
-		end
-	elseif args:IsSpellID(133795) then
-		warnLifeDrain:Show(args.destName)
-		specWarmLifeDrain:Show(args.destName)
+		end--]]
 	end
 end
 
@@ -130,6 +153,18 @@ function mod:SPELL_AURA_APPLIED(args)
 		local _, _, _, _, _, duration, expires = UnitDebuff(args.destName, args.spellName)
 		--maybe add a warning/special warning for everyone if duration is too high and many adds expected
 		timerDarkPlague:Start(duration)
+	elseif args:IsSpellID(134626) then
+		lingeringGazeTargets[#lingeringGazeTargets + 1] = args.destName
+		if args:IsPlayer() then
+			specWarnLingeringGaze:Show()
+			yellLingeringGaze:Yell()
+		end
+		self:Unschedule(warnLingeringGazeTargets)
+		if #lingeringGazeTargets >= 5 and self:IsDifficulty("normal25", "heroic25") or #lingeringGazeTargets >= 2 and self:IsDifficulty("normal10", "heroic10") then--TODO, add LFR number of targets
+			warnLingeringGazeTargets()
+		else
+			self:Schedule(0.5, warnLingeringGazeTargets)
+		end
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
@@ -142,27 +177,54 @@ end
 
 function mod:SPELL_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 	if spellId == 134044 and destGUID == UnitGUID("player") and self:AntiSpam(3, 1) then
-		specWarnLingeringGaze:Show()
+		specWarnLingeringGazeMove:Show()
 	end
 end
 mod.SPELL_MISSED = mod.SPELL_DAMAGE
+
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
+	if spellId == 140502 and destGUID == UnitGUID("player") and self:AntiSpam(3, 1) then
+		specWarnEyeSore:Show()
+	end
+end
+mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
 --Blizz doesn't like combat log anymore
 --Currently very bugged too so warnings aren't working right (since fight isn't working right)
 --Beams wildly jump targets and don't give new target a warning at all nor does it even show in damn combat log.
 function mod:CHAT_MSG_MONSTER_EMOTE(msg, _, _, _, target)
-	if msg:find("spell:134122") then--Blue Rays
+	if msg:find("spell:136932") then--Force of Will
+		warnForceOfWill:Show(target)
+		if target == UnitName("player") then
+			specWarnForceOfWill:Show()
+			yellForceOfWill:Yell()
+		else
+			local uId = DBM:GetRaidUnitId(target)
+			if uId then
+				local x, y = GetPlayerMapPosition(uId)
+				if x == 0 and y == 0 then
+					SetMapToCurrentZone()
+					x, y = GetPlayerMapPosition(uId)
+				end
+				local inRange = DBM.RangeCheck:GetDistance("player", x, y)
+				if inRange and inRange < 11 then--Guessed range.
+					specWarnForceOfWillNear:Show(target)
+				end
+			end
+		end
+	elseif msg:find("spell:134122") then--Blue Rays
 		timerForceOfWillCD:Cancel()
 		warnBlueBeam:Show(target)
 		if target == UnitName("player") then
 			warnBlueBeam:Show()
 		end
 	elseif msg:find("spell:134123") then--Infrared Light (red)
+		crimsonFogs = 3
 		warnRedBeam:Show(target)
 		if target == UnitName("player") then
 			warnRedBeam:Show()
 		end
-	elseif msg:find("spell:134124") and self:IsDifficulty("heroic10", "heroic25") then--useful only on heroic since there are only amber adds on heroic (therefor where we initially position the beam before it untethers is VERY important)
+	elseif msg:find("spell:134124") and self:IsDifficulty("heroic10", "heroic25", "lfr25") then--useful only on heroic and LFR since there are only amber adds in them. Normal 10 and normal 25 do not have amber adds (why LFR does is beyond me)
 		warnYellowBeam:Show(target)
 		if target == UnitName("player") then
 			specWarnYellowBeam:Show()
@@ -174,7 +236,7 @@ function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 69050 then--Crimson Fog
 		crimsonFogs = crimsonFogs - 1
-		if crimsonFogs >= 1 then
+		if crimsonFogs >= 1 and not self:IsDifficulty("lfr25") then--LFR does something completely different than kill 3 crimson adds to end phase. in LFR, they kill 1 of each color (which is completely against what you do in 10N, 25N, 10H, 25H)
 			warnCrimsonLeft:Show(crimsonFogs)
 		end
 	end
@@ -182,16 +244,22 @@ end
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, spellName, _, _, spellId)
 	if spellId == 136316 and self:AntiSpam(2, 2) then--Disintegration Beam (clockwise)
+		timerLingeringGazeCD:Cancel()
+		warnDisintegrationBeam:Show()
 		specWarnDisintegrationBeam:Show(spellName, DBM_CORE_LEFT)
+		timerDisintegrationBeam:Start()
 		if self.Options.ArrowOnBeam then
 			DBM.Arrow:ShowStatic(90)
 		end
-		print("Mod beyond this point is incomplete and most timers will be unavailable")
+		self:Schedule(60, BeamEnded)
 	elseif spellId == 133775 and self:AntiSpam(2, 2) then--Disintegration Beam (counter-clockwise)
+		timerLingeringGazeCD:Cancel()
+		warnDisintegrationBeam:Show()
 		specWarnDisintegrationBeam:Show(spellName, DBM_CORE_RIGHT)
+		timerDisintegrationBeam:Start()
 		if self.Options.ArrowOnBeam then
 			DBM.Arrow:ShowStatic(270)
 		end
-		print("Mod beyond this point is incomplete and most timers will be unavailable")
+		self:Schedule(60, BeamEnded)
 	end
 end
