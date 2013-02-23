e4aa9fac91f3fa794997f52b8657d6ec40698631
Adam <MysticalOS@users.noreply.github.com>
2013-02-23 21:26:16 +0000

    Don't load GUI in combat, it just script too long errors most of time. Instead print a friendly message to load it out of combat (at least the first time)

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -1102,6 +1102,10 @@ do
 	local callOnLoad = {}
 	function DBM:LoadGUI()
 		if not IsAddOnLoaded("DBM-GUI") then
+			if InCombatLockdown() then
+				self:AddMsg(DBM_CORE_LOAD_GUI_COMBAT)
+				return
+			end
 			local _, _, _, enabled = GetAddOnInfo("DBM-GUI")
 			if not enabled then
 				EnableAddOn("DBM-GUI")
