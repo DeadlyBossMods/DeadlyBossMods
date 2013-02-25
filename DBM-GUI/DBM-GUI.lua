0ec0e82c9d0b13e625d56cfb11e547f5e6fbc460
ebmor <ebmor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-02-25 20:22:14 +0000

    default sound for SpecialWarning3 replaced with a new shorter one ("Destruction")

--- DBM-GUI/DBM-GUI.lua
+++ DBM-GUI/DBM-GUI.lua
@@ -1870,6 +1870,7 @@ local function CreateOptionsMenu()
 			{	text	= L.NoSound,		value	= "" },
 			{	text	= "Default",		value 	= "Sound\\Spells\\PVPFlagTaken.wav", 		sound=true },
 			{	text	= "Beware!",		value 	= "Sound\\Creature\\AlgalonTheObserver\\UR_Algalon_BHole01.wav", 		sound=true },--Great sound, short and to the point. Best pick for a secondary default!
+			{	text	= "Destruction",	value 	= "Sound\\Creature\\KilJaeden\\KILJAEDEN02.wav", 		sound=true },
 			{	text	= "NotPrepared",	value 	= "Sound\\Creature\\Illidan\\BLACK_Illidan_04.wav", 		sound=true },--Maybe a bit long? wouldn't recommend it as a default, but good for customizing.
 			{	text	= "NightElfBell",	value 	= "Sound\\Doodad\\BellTollNightElf.wav", 	sound=true }
 		}
