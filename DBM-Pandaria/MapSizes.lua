a377093742274df31cc60385ab4bc8b0ea4076cf
Adam <MysticalOS@users.noreply.github.com>
2013-03-02 02:23:39 +0000

    comment out bad data

--- DBM-Pandaria/MapSizes.lua
+++ DBM-Pandaria/MapSizes.lua
@@ -10,7 +10,9 @@ DBM:RegisterMapSize("IsleOfGiants",
 	0, 1787.5009765625, 1191.666015625
 )
 
+--[[Flat out wrong. It only has 1 dungeon level and level is 0.
+Need real info
 DBM:RegisterMapSize("IsleoftheThunderKing",
 	1, 480.0,320.0,
 	2, 362.0,241.3330078125
-)
+)--]]
--- DBM-ThroneofThunder/TwinConsorts.lua
+++ DBM-ThroneofThunder/TwinConsorts.lua
@@ -68,7 +68,7 @@ local timerTidalForceCD					= mod:NewCDTimer(74, 137531)
 
 local berserkTimer						= mod:NewBerserkTimer(600)
 
-mod:AddBoolOption("RangeFrame")--For various abilities that target even melee. SO yes, even melee need to spread out or not come.
+mod:AddBoolOption("RangeFrame")--For various abilities that target even melee. UPDATE, cosmic barrage (worst of the 3 abilities) no longer target melee. However, light of day and tears of teh sun still do. melee want to split into 2-3 groups (depending on how many) but no longer have to stupidly spread about all crazy and out of range of boss during cosmic barrage to avoid dying. On that note, MAYBE change this to ranged default instead of all.
 
 function mod:OnCombatStart(delay)
 	berserkTimer:Start(-delay)
