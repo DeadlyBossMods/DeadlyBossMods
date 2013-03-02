79cfa45274fb32a58ec064e17100005afd1537a1
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-03-02 02:39:50 +0000

    calculated mapsizes for that island for now using mage blink

--- trunk/DBM-Pandaria/MapSizes.lua
+++ trunk/DBM-Pandaria/MapSizes.lua
@@ -10,9 +10,6 @@ DBM:RegisterMapSize("IsleOfGiants",
 	0, 1787.5009765625, 1191.666015625
 )
 
---[[Flat out wrong. It only has 1 dungeon level and level is 0.
-Need real info
 DBM:RegisterMapSize("IsleoftheThunderKing",
-	1, 480.0,320.0,
-	2, 362.0,241.3330078125
-)--]]
+	0, 4135.2816551249, 2756.2332535822
+)
