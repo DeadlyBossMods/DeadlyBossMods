77675617cc3f6d7603a53ef761a58f4f82325ef0
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-03-10 01:54:52 +0000

    oondasta update. As it stands now. timers are near worthless do to the terrible design that is this fight. shared loot on a field boss so unforgiving = a corpse cannon that no one in world can even TRY to do correctly. Timers vary wildly based on how long he runs around not tanked

--- trunk/DBM-Pandaria/Oondasta.lua
+++ trunk/DBM-Pandaria/Oondasta.lua
@@ -3,7 +3,7 @@ local mod	= DBM:NewMod(826, "DBM-Pandaria", nil, 322)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(69161)--Not yet available in PTR, CID not known
+mod:SetCreatureID(69161)
 --mod:SetModelID(41448)
 mod:SetZone(929)--Isle of Giants
 
@@ -18,60 +18,32 @@ mod:RegisterEventsInCombat(
 	"SPELL_AURA_REMOVED"
 )
 
+mod:RegisterEvents(
+	"CHAT_MSG_MONSTER_YELL"
+)
+
 local warnCrush					= mod:NewStackAnnounce(137504, 2, nil, mod:IsTank() or mod:IsHealer())--Cast every 30 seconds roughly, lasts 1 minute. you need 3 tanks to be able to tank the boss without debuff. 2 tanks CAN do but they will always have 1 stack and take 25% more damage
 local warnPiercingRoar			= mod:NewSpellAnnounce(137457, 2, nil)
 local warnSpiritfireBeam		= mod:NewTargetAnnounce(137511, 3)
-local warnFrillBlast			= mod:NewSpellAnnounce(137505, 4, mod:IsTank() or mod:IsHealer())
+local warnFrillBlast			= mod:NewSpellAnnounce(137505, 4)--While this SHOULD be a tank only warning, thanks to terrible blizzard design, this fight is anything but a tanked fight, so it's now an everyone warning since god knows what fucking way the boss will be facing when he casts this
 
-local specWarnCrush				= mod:NewSpecialWarningTarget(137504, mod:IsTank())--This should not go over 1 stack so don't need stack warning just a "taunt the boss" warning
-local specWarnFrillBlast		= mod:NewSpecialWarningSpell(137505, mod:IsTank())
+local specWarnCrush				= mod:NewSpecialWarningStack(137504, mod:IsTank(), 2)
+local specWarnCrushOther		= mod:NewSpecialWarningTarget(137504, mod:IsTank())--This should not go over 1 stack so don't need stack warning just a "taunt the boss" warning
+local specWarnFrillBlast		= mod:NewSpecialWarningSpell(137505, nil, nil, nil, 2)
 
 local timerCrush				= mod:NewTargetTimer(60, 137504, nil, mod:IsTank() or mod:IsHealer())
 local timerCrushCD				= mod:NewCDTimer(32, 137504)
-local timerPiercingRoarCD		= mod:NewCDTimer(29, 137457)--29-34sec variation
-local timerSpiritfireBeamCD		= mod:NewCDTimer(25, 137511)--25-30sec variation
-local timerFrillBlastCD			= mod:NewCDTimer(25, 137505, nil, mod:IsTank() or mod:IsHealer())--25-30sec variation
+local timerPiercingRoarCD		= mod:NewCDTimer(25, 137457)--25-60sec variation (i'm going to guess like all the rest of the variations, the timers are all types of fucked up when the boss is running around untanked, which delays casts of crush and frill blast, but makes him cast spitfire twice as often)
+--local timerSpiritfireBeamCD		= mod:NewCDTimer(25, 137511)--25-30sec variation (disabled because he also seems to spam it far more often if there is no tank, making it difficult to find an ACTUAL cd when fight is done incorrectly
+local timerFrillBlastCD			= mod:NewCDTimer(25, 137505)--25-30sec variation
 
 mod:AddBoolOption("RangeFrame", true)
 
-local numberTanks = 0
-
-local function isTank(unit)
-	-- 1. check blizzard tanks first
-	-- 2. check blizzard roles second
-	-- 3. check boss's current target
-	if GetPartyAssignment("MAINTANK", unit, 1) then
-		return true
-	end
-	if UnitGroupRolesAssigned(unit) == "TANK" then
-		return true
-	end
-	--Since this is checked on pull, it will fail if someone other than tank facepulls it (or rathor think that dps is a tank
-	local targetname, uId = mod:GetBossTarget(69161)
-	if targetname and UnitIsUnit(unit, uId) then
-		return true
-	end
-	return false
-end
-
-local function findTanks()
-	for i=1, MAX_RAID_MEMBERS do
-		if isTank(i) then
-			numberTanks = numberTanks + 1
-		end
-	end
-	--If we could not find at least 2, set it to 2
-	if numberTanks < 2 then
-		numberTanks = 2
-	end
-end
-
 function mod:OnCombatStart(delay)
-	findTanks()
---	timerCrushCD:Start(-delay)
---	timerPiercingRoarCD:Start(-delay)
---	timerSpiritfireBeamCD:Start(-delay)
---	timerFrillBlastCD:Start(-delay)
+--	timerCrushCD:Start(-delay)--There was no tank, so he pretty much never cast this, just ran like a wild animal around area while corpse cannoned
+--	timerSpiritfireBeamCD:Start(15-delay)
+	timerPiercingRoarCD:Start(20-delay)
+	timerFrillBlastCD:Start(40-delay)
 	if self.Options.RangeFrame then
 		DBM.RangeCheck:Show(10)--range is guessed. spell tooltip and EJ do not save what range is right now.
 	end
@@ -87,7 +59,7 @@ function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(137457) then
 		warnPiercingRoar:Show()
 		timerPiercingRoarCD:Start()
-	elseif args:IsSpellID(137457) then
+	elseif args:IsSpellID(137505) then
 		warnFrillBlast:Show()
 		specWarnFrillBlast:Show()
 		timerFrillBlastCD:Start()
@@ -95,24 +67,23 @@ function mod:SPELL_CAST_START(args)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(137511) then
+	if args:IsSpellID(137508, 137511) then
 		warnSpiritfireBeam:Show(args.destName)
-		timerSpiritfireBeamCD:Start()
+--		timerSpiritfireBeamCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(137504) then
-		if numberTanks == 0 then--We DCed and need to check again
-			findTanks()
-		end
 		warnCrush:Show(args.destName, args.amount or 1)
 		timerCrush:Start(args.destName)
 		timerCrushCD:Start()
-		if not UnitDebuff("player", GetSpellInfo(137504)) and not UnitIsDeadOrGhost("player") then--We don't have debuff so we should taunt from other tank now. Likely this is a 3 or more tank scenario
-			specWarnCrush:Show(args.destName)
-		elseif numberTanks == 2 then--Only 2 tanks, you will always taunt at 1 stack, but drop that stack before it refreshes. You alternate and both just maintain 1 stack entire fight
-			specWarnCrush:Show(args.destName)
+		if args:IsPlayer() and (args.amount or 1) >= 2 then
+			specWarnCrush:Show(args.amount)
+		else
+			if (args.amount or 1) >= 2 and not UnitIsDeadOrGhost("player") or not UnitDebuff("player", GetSpellInfo(137504)) then
+				specWarnCrushOther:Show(args.destName)
+			end
 		end
 	end
 end
@@ -121,6 +92,12 @@ mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED--<--if this happens you're 
 function mod:SPELL_AURA_REMOVED(args)
 	if args:IsSpellID(137504) then
 		timerCrush:Cancel(args.destName)
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg == L.Pull and not self:IsInCombat() then
+		DBM:StartCombat(self, 0)
 	end
 end
 
