b3bf46e1e1f5d9b75ca23b382ebd219876de524b
Paul Emmerich <emmericp@net.in.tum.de>
2013-03-12 23:31:55 +0000

    tag as 5.2.1-release

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -45,8 +45,8 @@
 -------------------------------
 DBM = {
 	Revision = tonumber(("$Revision$"):sub(12, -3)),
-	DisplayVersion = "5.2.1 alpha", -- the string that is shown as version
-	ReleaseRevision = 8828 -- the revision of the latest stable version that is available
+	DisplayVersion = "5.2.1", -- the string that is shown as version
+	ReleaseRevision = 8892 -- the revision of the latest stable version that is available
 }
 
 -- Legacy crap; that stupid "Version" field was never a good idea.
