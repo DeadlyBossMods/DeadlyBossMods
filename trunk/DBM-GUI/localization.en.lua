d43c8cf7f7a2aa3babfdfaac35b7bb3eb7c02e4e
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-03-20 22:04:54 +0000

    Better wording/spelling

--- trunk/DBM-GUI/localization.en.lua
+++ trunk/DBM-GUI/localization.en.lua
@@ -30,7 +30,7 @@ L.Button_OK 				= 'OK'
 L.Button_Cancel 			= 'Cancel'
 L.Button_LoadMod 			= 'Load AddOn'
 L.Mod_Enabled				= "Enable boss mod"
-L.Mod_Reset					= "Load deafult option for this boss (WIP)"
+L.Mod_Reset					= "Load default options for this mod (WIP)"
 L.Reset 					= "Reset"
 
 L.Enable  					= "Enable"
