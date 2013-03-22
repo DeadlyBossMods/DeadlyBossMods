24d4f2107fcea7d185007da219c68fa71335cc42
Paul Emmerich <emmericp@net.in.tum.de>
2013-03-22 20:54:28 +0000

    just wait a minute after logging in before accessing the player GUID

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -1367,16 +1367,18 @@ do
 
 	--	save playerinfo into raid table on load. (for solo raid)
 	DBM:RegisterOnLoadCallback(function()
-		raid[playerName] = {}
-		raid[playerName].name = playerName
-		raid[playerName].shortname = playerName
-		raid[playerName].guid = UnitGUID("player")
-		raid[playerName].rank = 0
-		raid[playerName].class = class
-		raid[playerName].id = "player"
-		raidUIds["player"] = playerName
-		raidGuids[UnitGUID("player")] = playerName
-		raidShortNames[playerName] = playerName
+		DBM:Schedule(60, function()
+			raid[playerName] = {}
+			raid[playerName].name = playerName
+			raid[playerName].shortname = playerName
+			raid[playerName].guid = UnitGUID("player")
+			raid[playerName].rank = 0
+			raid[playerName].class = class
+			raid[playerName].id = "player"
+			raidUIds["player"] = playerName
+			raidGuids[UnitGUID("player")] = playerName
+			raidShortNames[playerName] = playerName
+		end)
 	end)
 
 	local function updateAllRoster()
