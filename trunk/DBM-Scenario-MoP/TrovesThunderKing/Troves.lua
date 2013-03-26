28cd30490945546bbec0cc8a48ca15944448c859
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-03-26 23:56:43 +0000

    Half of a warlock scenario mod. other boss not supported because logger didn't know how to properly log and i can't figure out where the hell they pulled the first boss. time stamp mismatches from combat log and transcriptor and everything. FYI, i prefer transcriptor logs that INCLUDE the CLEU and logs aren't split with two different time stamp formats.

--- trunk/DBM-Scenario-MoP/TrovesThunderKing/Troves.lua
+++ trunk/DBM-Scenario-MoP/TrovesThunderKing/Troves.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Troves", "DBM-Scenario-MoP")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 8980 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetZone()
 
 mod:RegisterCombat("scenario", 934)
