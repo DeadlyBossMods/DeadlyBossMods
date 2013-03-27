263e6b3dbf2bf03351fe4076e7bf2f4e8145cc85
Adam <MysticalOS@users.noreply.github.com>
2013-03-27 20:16:11 +0000

    add some debug to figure out how more shit broke in core

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -1436,7 +1436,9 @@ do
 					else
 						raidShortNames[shortname] = DBM_CORE_GENERIC_WARNING_DUPLICATE:format(name:gsub("%-.*$", ""))
 					end
-					if not playerWithHigherVersionPromoted and rank >= 1 and raid[name].version and raid[name].version ~= "" and raid[name].version > tonumber(DBM.Version) then
+					print("DBM: Debug:", raid[name].version, rank, tonumber(DBM.Version))
+					--46x DBM-Core\DBM-Core-5.2.1-170-gea8e833.lua:1439: attempt to compare number with string
+					if not playerWithHigherVersionPromoted and rank >= 1 and raid[name].version and raid[name].version > tonumber(DBM.Version) then--Why the fuck is this line erroring every time i join a raid? it hasn't changed?
 						playerWithHigherVersionPromoted = true
 					end
 				end
