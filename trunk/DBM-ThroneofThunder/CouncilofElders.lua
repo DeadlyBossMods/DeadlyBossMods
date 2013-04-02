1803dc4886bb30aecb1dc02e185a0937ab55d960
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-04-02 22:15:43 +0000

    Disable some stuff in LFR that isn't relevant there (yes spread mechanics are still there, no, no one uses the range frame what so ever anyways because the damage is too trivial to care)

--- trunk/DBM-ThroneofThunder/CouncilofElders.lua
+++ trunk/DBM-ThroneofThunder/CouncilofElders.lua
@@ -154,7 +154,7 @@ function mod:OnCombatStart(delay)
 	timerRecklessChargeCD:Start(10-delay)--the trigger is 6 seconds from pull, charge will happen at 10. I like timer ending at cast finish for this one though vs tryng to have TWO timers for something that literally only has 6 second cd
 	timerBitingColdCD:Start(15-delay)--15 seconds until debuff, 13 til cast.
 	timerBlessedLoaSpiritCD:Start(25-delay)
-	if self.Options.RangeFrame then
+	if self.Options.RangeFrame and not self:IsDifficulty("lfr25") then
 		DBM.RangeCheck:Show(5)
 	end
 end
