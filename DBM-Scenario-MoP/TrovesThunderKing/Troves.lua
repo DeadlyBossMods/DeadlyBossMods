99c9ecc55d0d904c46c46e6258b29a1bbb2c7e30
Adam <MysticalOS@users.noreply.github.com>
2013-04-03 00:04:39 +0000

    add warning and special warning for treasure saurok now that i finally got a spawn while logging

--- DBM-Scenario-MoP/TrovesThunderKing/Troves.lua
+++ DBM-Scenario-MoP/TrovesThunderKing/Troves.lua
@@ -8,14 +8,17 @@ mod:RegisterCombat("scenario", 934)
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START",
+	"SPELL_AURA_APPLIED",
 	"UNIT_AURA"
 )
 
 --Todo, get luck enough to have a treasure goblin spawn and capture event for it so we can special warn for it.
 local warnStoneSmash		= mod:NewCastAnnounce(139777, 3, nil, nil, false)
 local warnMightyCrash		= mod:NewCastAnnounce(136844, 3)
+local warnSaurok			= mod:NewSpellAnnounce(140009, 4)
 
 local specWarnMightycrash	= mod:NewSpecialWarningMove(136844)
+local specWarnSaurok		= mod:NewSpecialWarningSpell(140009)
 
 local timerEvent			= mod:NewBuffFadesTimer(299, 140000)
 local timerStoneSmash		= mod:NewCastTimer(3, 139777, nil, false)
@@ -34,6 +37,15 @@ function mod:SPELL_CAST_START(args)
 	elseif args.spellId == 136844 then
 		warnMightyCrash:Show()
 		specWarnMightycrash:Show()
+	end
+end
+
+--"<165.2 19:33:11> [UNIT_SPELLCAST_SUCCEEDED] Unknown [[target:Suddenly, a Treasure Saurok!::0:140009]]", -- [191]
+--"<165.2 19:33:11> [CLEU] SPELL_AURA_APPLIED#false#0xF131130E0003BB9B#Unknown#68168#0#0xF131130E0003BB9B#Unknown#68168#0#139812#Take the Loot and Run!#1#BUFF", -- [192]
+function mod:SPELL_AURA_APPLIED(args)
+	if args.spellId == 139812 then
+		warnSaurok:Show()
+		specWarnSaurok:Show()
 	end
 end
 
