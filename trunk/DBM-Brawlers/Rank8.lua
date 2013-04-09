59c822df9770ae6be623cf92b1dafcf9040302ad
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-04-09 07:29:05 +0000

    add first new boss warning, sadly, this warning isn't for the fighter, it's for the audience, lol. Real cute blizzard. boss tormenting audience by casting debuffs on 5 random audience players. Def need special warning so you don't die while tabbed out.

--- trunk/DBM-Brawlers/Rank8.lua
+++ trunk/DBM-Brawlers/Rank8.lua
@@ -7,11 +7,35 @@ mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetZone()
 
 mod:RegisterEvents(
-	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS"
+	"SPELL_AURA_APPLIED"
 )
+
+--Boss Key
+--http://mysticalos.com/images/MoP/new_brawlers/rank8.jpeg
+local warnSmolderingHeat			= mod:NewTargetAnnounce(142400, 4)--A real cute troll, Anthracite (rank 8) debuffs random players in audience, this will KILL YOU if you do not get heals
+
+local specWarnSmolderingHeat		= mod:NewSpecialWarningYou(142400)
 
 mod:RemoveOption("HealthFrame")
 mod:RemoveOption("SpeedKillTimer")
 
 local brawlersMod = DBM:GetModByName("Brawlers")
+
+local smolderingHeaTargets = {}
+
+local function warnSmolderingHeatTargets()
+	warnSmolderingHeat:Show(table.concat(smolderingHeaTargets, "<, >"))
+	table.wipe(smolderingHeaTargets)
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+--	if not brawlersMod.Options.SpectatorMode and not brawlersMod:PlayerFighting() then return end--Commented out for now to see if blizzard keeps the rank 8 boss that attacks spectators
+	if args.spellId == 142400 then
+		smolderingHeaTargets[#smolderingHeaTargets + 1] = args.destName
+		if args:IsPlayer() then
+			specWarnSmolderingHeat:Show()
+		end
+		self:Unschedule(warnSmolderingHeatTargets)
+		self:Schedule(0.5, warnSmolderingHeatTargets)
+	end
+end
