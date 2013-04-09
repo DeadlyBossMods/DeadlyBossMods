b2f73a7658679b73308db6378d6e43b45fa00386
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-04-09 06:05:23 +0000

    # Rank 8 and 9 : Add revision tag. # koKR update.

--- DBM-Brawlers/Rank8.lua
+++ DBM-Brawlers/Rank8.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("BrawlRank8", "DBM-Brawlers")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: -1 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 --mod:SetCreatureID(60491)
 --mod:SetModelID(46265)
 mod:SetZone()
