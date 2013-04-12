b80abffaca445513d871f080ea424b028cc82297
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-04-12 04:52:00 +0000

    # Raden : Add creature id and basic spell information. NO WARNINGS YET.

--- DBM-ThroneofThunder/Raden.lua
+++ DBM-ThroneofThunder/Raden.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(831, "DBM-ThroneofThunder", nil, 362)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(62983)
+mod:SetCreatureID(69473)
 mod:SetModelID(47739) -- most likely a placeholder :)
 
 mod:RegisterCombat("combat")
@@ -10,3 +10,16 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START"
 )
+
+-- NO WARNINGS, THIS IS ONLY SPELL INFORMATION
+-- To complete this mod, combatlog required..
+local warnAnima					= mod:NewSpellAnnounce(138331)
+local warnMurderousStrike		= mod:NewSpellAnnounce(138333)--Tank Spells?
+local warnUnstableAnima			= mod:NewSpellAnnounce(138295)--May range frame needed
+local warnSanguineHorror		= mod:NewSpellAnnounce(138338)--Adds?
+local warnVita					= mod:NewSpellAnnounce(138332)
+local warnFatalStrike			= mod:NewSpellAnnounce(138334)--Tank Spells?
+local warnUnstableVita			= mod:NewSpellAnnounce(138308)
+local warnCracklingStalker		= mod:NewSpellAnnounce(138339)--Adds?
+local warnCreation				= mod:NewSpellAnnounce(138321)
+local warnRuinBolt				= mod:NewSpellAnnounce(139087)
