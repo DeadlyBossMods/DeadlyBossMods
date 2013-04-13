6d940c774c4f0fb991c031514dcb538f585972d8
Adam <MysticalOS@users.noreply.github.com>
2013-04-13 05:22:09 +0000

    Fix an obvious logic fail my my part also my bad on healer thing. I meant to COPY and paste it to cooldowns prototype, not CUT and paste. :)

--- DBM-MogushanVaults/GarajalTheSpiritBinder.lua
+++ DBM-MogushanVaults/GarajalTheSpiritBinder.lua
@@ -218,7 +218,7 @@ function mod:OnSync(msg, guid)
 			--Added to fix a bug with duplicate entries of same person in icon table more than once
 			local foundDuplicate = false
 			for i = #voodooDollTargetIcons, 1, -1 do
-				if not voodooDollTargetIcons[i].targetUnitID then--make sure they aren't in table before inserting into table again. (not sure why this happens in LFR but it does, probably someone really high ping that cranked latency check way up)
+				if voodooDollTargetIcons[i].targetUnitID then--make sure they aren't in table before inserting into table again. (not sure why this happens in LFR but it does, probably someone really high ping that cranked latency check way up)
 					foundDuplicate = true
 				end
 			end
