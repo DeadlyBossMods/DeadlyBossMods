18d1ea57ad05a5504e4ae248b19e114b7c6d31c3
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-04-14 01:08:37 +0000

    #Also add "A Brewing Storm" scenario mod

---
+++ trunk/DBM-Scenario-MoP/ABrewingStorm/BrewingStorm.lua
@@ -0,0 +1,113 @@
+local mod	= DBM:NewMod("BrewingStorm", "DBM-Scenario-MoP")
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+mod:SetZone()
+
+mod:RegisterCombat("scenario", 878)
+
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_START",
+	"SPELL_AURA_APPLIED",
+	"CHAT_MSG_MONSTER_SAY",
+	"UNIT_DIED"
+)
+
+--[[First event
+"<24.7 19:33:15> [CHAT_MSG_MONSTER_SAY] CHAT_MSG_MONSTER_SAY#The storm is starting! Get ready.#Brewmaster Blanche###Omegal##0#0##0#995#nil#0#false#false", -- [40]
+"<29.8 19:33:20> [UPDATE_WORLD_STATES] |0#1#false#Brew Progress: 10
+"<39.5 19:33:30> [UPDATE_WORLD_STATES] |0#1#false#Brew Progress: 20
+"<47.0 19:33:37> [UPDATE_WORLD_STATES] |0#1#false#Brew Progress: 30
+"<53.9 19:33:44> [UPDATE_WORLD_STATES] |0#1#false#Brew Progress: 40
+"<68.4 19:33:59> [UPDATE_WORLD_STATES] |0#1#false#Brew Progress: 50
+"<78.5 19:34:09> [UPDATE_WORLD_STATES] |0#1#false#Brew Progress: 60
+"<83.6 19:34:14> [UPDATE_WORLD_STATES] |0#1#false#Brew Progress: 70
+"<92.8 19:34:23> [UPDATE_WORLD_STATES] |0#1#false#Brew Progress: 80
+"<98.2 19:34:29> [UPDATE_WORLD_STATES] |0#1#false#Brew Progress: 90
+"<107.4 19:34:38> [UPDATE_WORLD_STATES] |0#1#false#Brew Progress: 100
+"<107.5 19:34:38> [CHAT_MSG_MONSTER_SAY] CHAT_MSG_MONSTER_SAY#You did it! Let's get this brew to the Monastery...#Brewmaster Blanche###Brewkeg##0#0##0#1005#nil#0#false#false", -- [698]
+--]]
+
+--[[Possible Timer update stuff we can use to auto adjust remaining time to these values every 10 energy
+Start to finish, 82.8
+10 to finish,  77.7
+20 to finish,  68
+30 to finish,  60.5
+40 to finish,  53.6
+50 to finish,  39.1
+60 to finish,  29
+70 to finish,  23.9
+80 to finish,  14.7
+90 to finish,  9.3
+--]]
+
+--Event Warnings
+---Maybe add warning for saurok waves? or if a barrel catches fire? Not get transcriptor of fire since we didn't let any saurok reach barrels
+--Borokhula the Destroyer
+local warnSwampSmash			= mod:NewSpellAnnounce(115013, 3)--TODO, see if target scanning works and change to target warning and target special warning instead
+local warnEarthShattering		= mod:NewSpellAnnounce(122142, 3)
+
+--Event
+---NOTHING HERE!
+--Borokhula the Destroyer
+local specWarnSwampSmash		= mod:NewSpecialWarningSpell(115013, nil, nil, nil, 2)
+
+--Event
+local timerEvent				= mod:NewTimer(82.8, "timerEvent", 106648)
+--Borokhula the Destroyer
+local timerSwampSmashCD			= mod:NewCDTimer(18, 115013)--Limited sample size, may be shorter
+local timerEarthShatteringCD	= mod:NewNextTimer(25, 122142)--Limited sample size, may be shorter
+
+mod:RemoveOption("HealthFrame")
+
+function mod:SPELL_CAST_START(args)
+	if args.spellId == 115013 then
+		warnSwampSmash:Show()
+		specWarnSwampSmash:Show()
+		timerSwampSmashCD:Start()
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args.spellId == 122142 then
+		warnEarthShattering:Show()
+		timerEarthShatteringCD:Start()
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_SAY(msg)
+	if msg == L.BrewStart or msg:find(L.BrewStart) then
+		self:SendSync("BrewStarted")
+	elseif msg == L.BrewFinish or msg:find(L.BrewFinish) then
+		self:SendSync("BrewEnded")
+	elseif msg == L.BorokhulaPull or msg:find(L.BorokhulaPull) then
+		self:SendSync("BorokhulaPulled")
+	elseif msg == L.BorokhulaAdds or msg:find(L.BorokhulaAdds) then
+		self:SendSync("BorokhulaAdds")
+	end
+end
+
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 58739 then--Borokhula the Destroyer
+		timerSwampSmashCD:Cancel()
+		timerEarthShatteringCD:Cancel()
+	end
+end
+
+function mod:OnSync(msg)
+	if msg == "BrewStarted" then
+		timerEvent:Start()
+	elseif msg == "BrewEnded" then
+		timerEvent:Cancel()
+	elseif msg == "BorokhulaPulled" then
+		timerSwampSmashCD:Start()
+		timerEarthShatteringCD:Start()
+	elseif msg == "BorokhulaAdds" then
+--[[--Insufficent data to determine consistent timing, or if it's even time based at all (maybe he just calls more when old ones all die?)
+"<378.3 19:39:09> [CHAT_MSG_MONSTER_SAY] CHAT_MSG_MONSTER_SAY#Last call, you fork-tongued dip-slithers!
+"<396.4 19:39:27> RAID_BOSS_EMOTE#%s calls out for reinforcements!#Borokhula the Destroyer#0#true", -- [3]
+"<409.6 19:39:40> RAID_BOSS_EMOTE#%s calls out for reinforcements!#Borokhula the Destroyer#0#true", -- [4]
+--]]
+	end
+end
--- trunk/DBM-Scenario-MoP/localization.en.lua
+++ trunk/DBM-Scenario-MoP/localization.en.lua
@@ -1,6 +1,31 @@
 local L
 
 --------------------------------
+-- A Brewing Storm --
+--------------------------------
+
+L= DBM:GetModLocalization("BrewingStorm")
+
+L:SetGeneralLocalization{
+	name = "A Brewing Storm"
+}
+
+L:SetTimerLocalization{
+	timerEvent			= "Brew Done (Aprox)"
+}
+
+L:SetOptionLocalization{
+	timerEvent			= "Show timer for approximate brew completion"
+}
+
+L:SetMiscLocalization{
+	BrewStart			= "The storm is starting! Get ready.",
+	BrewFinish			= "You did it! Let's get this brew to the Monastery...",--Maybe switch to UPDATE_WORLD_STATES 100 progress instead in a more polished version of mod.
+	BorokhulaPull		= "Last call, you fork-tongued dip-slithers!",
+	BorokhulaAdds		= "calls out for reinforcements!"--In case useful/important on heroic. On normal just zerg boss and ignore these unless you want achievement.
+}
+
+--------------------------------
 -- Crypt of Forgotten Kings --
 --------------------------------
 
@@ -43,17 +68,17 @@ else
 	landfall = "Domination Point"
 end
 
-L:SetGeneralLocalization({
+L:SetGeneralLocalization{
 	name = landfall
-})
+}
 
-L:SetWarningLocalization({
+L:SetWarningLocalization{
 	WarnAchFiveAlive	= "Achievement \"Number Five Is Alive\" failed"
-})
+}
 
-L:SetOptionLocalization({
+L:SetOptionLocalization{
 	WarnAchFiveAlive	= "Show warning if achievement \"Number Five Is Alive\" failed."
-})
+}
 
 --------------------------------
 -- Troves of the Thunder King --
@@ -75,23 +100,23 @@ L:SetGeneralLocalization{
 	name = "Pursuing the Black Harvest"
 }
 
-L:SetWarningLocalization({
+L:SetWarningLocalization{
 	specWarnLostSouls		= "Lost Souls!",
 	specWarnEnslavePitLord	= "Pit Lord - Enslave Now!"
-})
+}
 
-L:SetTimerLocalization({
+L:SetTimerLocalization{
 	timerCombatStarts		= "Combat starts",
 	timerLostSoulsCD		= "Lost Souls CD"
-})
+}
 
-L:SetOptionLocalization({
+L:SetOptionLocalization{
 	specWarnLostSouls		= "Show special warning when Lost Souls spawn",
 	specWarnEnslavePitLord	= "Show special warning to enslave demon when Pit Lord activates/breaks free",
 	timerCombatStarts		= "Show time for start of combat",
 	timerLostSoulsCD		= "Show cooldown timer for next Lost Souls spawn"
-})
+}
 
-L:SetMiscLocalization({
+L:SetMiscLocalization{
 	LostSouls				= "Face the souls of those your kind doomed to perish, Warlock!"
-})
+}
