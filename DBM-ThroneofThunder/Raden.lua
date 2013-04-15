90b7d70964923e43d7a9da9f7262be3317360b0e
Adam <MysticalOS@users.noreply.github.com>
2013-04-15 21:55:22 +0000

    comment out raden abilities, damage is probably done though, everyone is going to have crap option defaults on this fight (ALL ON) unless we write some hack to force reset defaults when better defaults than "all on" are implemented.

--- DBM-ThroneofThunder/Raden.lua
+++ DBM-ThroneofThunder/Raden.lua
@@ -13,6 +13,8 @@ mod:RegisterEventsInCombat(
 
 -- NO WARNINGS, THIS IS ONLY SPELL INFORMATION
 -- To complete this mod, combatlog required..
+--[[
+Adding like this bad idea, now these will option default to ON for all warnings when they do get added and we won't be able to reset the crap defaults for people who zoned into ToT at least once with this version of mod
 local warnAnima					= mod:NewSpellAnnounce(138331)
 local warnMurderousStrike		= mod:NewSpellAnnounce(138333)--Tank Spells?
 local warnUnstableAnima			= mod:NewSpellAnnounce(138295)--May range frame needed
@@ -23,3 +25,4 @@ local warnUnstableVita			= mod:NewSpellAnnounce(138308)
 local warnCracklingStalker		= mod:NewSpellAnnounce(138339)--Adds?
 local warnCreation				= mod:NewSpellAnnounce(138321)
 local warnRuinBolt				= mod:NewSpellAnnounce(139087)
+--]]
