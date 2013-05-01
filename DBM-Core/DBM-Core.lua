099cbc6a58517708ed4a7be4d186745f20b3f03e
Adam <MysticalOS@users.noreply.github.com>
2013-05-01 22:48:55 +0000

    better way to start combat with scenarios

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -1897,12 +1897,6 @@ do
 				end
 			end
 		end
-		if instanceType == "scenario" then
-			--Depending on speed of computer, scenario check needs to run multiple times to ensure it fires properly (it will fail if it tries to start in a loading screen)
-			self:Schedule(4, DBM.ScenarioCheck)
-			self:Schedule(8, DBM.ScenarioCheck)
-			self:Schedule(12, DBM.ScenarioCheck)
-		end
 	end
 end
 
@@ -1964,7 +1958,10 @@ function DBM:LoadMod(mod)
 		if DBM_GUI then
 			DBM_GUI:UpdateModList()
 		end
-		local _, instanceType, difficulty, _, maxPlayers = GetInstanceInfo()
+		local _, instanceType = GetInstanceInfo()
+		if instanceType == "scenario" then
+			self:Schedule(1, DBM.ScenarioCheck)
+		end
 		if not InCombatLockdown() then--We loaded in combat because a raid boss was in process, but lets at least delay the garbage collect so at least load mod is half as bad, to do our best to avoid "script ran too long"
 			collectgarbage("collect")
 		end
