0f74a6a2ba7939e6f7ab658fac061a3a08cd031e
Adam <MysticalOS@users.noreply.github.com>
2013-05-06 23:15:59 +0000

    add rest of challenge mode stubs Fix all map sizes for 5 man dungeons since every single one of them was wrong.

---
+++ DBM-Party-MoP/Scholomance/ShMGeneral.lua
@@ -0,0 +1,12 @@
+local mod	= DBM:NewMod("ShMGeneral", "DBM-Party-MoP", 7)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+mod:SetZone()
+
+mod:RegisterCombat("challenge", 898)
+
+--this is basically a stub mod to enable instance completion timers, such as a "fastast clear" timer for a challenge mode. :)
+--TODO: maybe expand this with options to enable it's use beyond just challenge modes (such as supporting timer in normal heroic or challenge)
+--This also gives more freedom to add 5 man trash warnings in future if each zone has a zone based mod
+mod:RemoveOption("HealthFrame")
