ac5129c97556409d212c1a397992b5476ae11d2b
Paul Emmerich <emmericp@net.in.tum.de>
2013-05-09 00:36:01 +0000

    From ba4513b5015e785f8f28e154182549e65d0b2b71 Mon Sep 17 00:00:00 2001 From: Alberto Cortes-Martin <alcortes@it.uc3m.es> Date: Thu, 9 May 2013 00:37:34 +0200 Subject: [PATCH] Spanish localization for Jin-rokh, Horridon, Council of Elders and Tortos

--- DBM-ThroneofThunder/localization.es.lua
+++ DBM-ThroneofThunder/localization.es.lua
@@ -6,9 +6,43 @@ local L
 --------------------------
 L= DBM:GetModLocalization(827)
 
+L:SetWarningLocalization({
+	specWarnWaterMove	= "%s pronto - ¡abandona el Agua conductiva!"
+})
+
+L:SetOptionLocalization({
+	specWarnWaterMove	= "Mostrar aviso especial si permaneces en $spell:138470\n(Aviso al comenzar $spell:137313 o si $spell:138732 expirará pronto)",
+	RangeFrame			= "Mostrar radar de rango (8/4)"
+})
+
+
+--------------
+-- Horridon --
+--------------
+L:SetWarningLocalization({
+	warnAdds				= "%s",
+	warnOrbofControl		= "Orbe de control disponible",
+	specWarnOrbofControl	= "¡Orbe de crontrol disponible!"
+})
+
+L:SetTimerLocalization({
+	timerDoor				= "Siguiente puerta tribal",
+	timerAdds				= "Siguientes %s"
+})
+
+L:SetOptionLocalization({
+	warnAdds				= "Anunciar cuando aparecen nuevos adds",
+	warnOrbofControl		= "Anunciar cuando un $journal:7092 está disponible",
+	specWarnOrbofControl	= "Mostrar aviso especial cuando un $journal:7092 está disponible",
+	timerDoor				= "Mostrar temporizador para la siguiente fase de puerta tribal",
+	timerAdds				= "Mostrar temporizador cuando aparecen nuevos adds",
+	RangeFrame				= "Mostrar distancia (5) para $spell:136480",
+	SetIconOnCharge			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(136769)
+})
+
 L:SetMiscLocalization({
-	newForces		= "salen en tropel desde la puerta",--Farraki forces pour from the Farraki Tribal Door! (needs verified, may be wrong)
-	chargeTarget	= "golpea con su cola"--Horridon sets his eyes on Eraeshio and stamps his tail! (needs verified, may be wrong)
+	newForces		= "salen en tropel desde",--Farraki forces pour from the Farraki Tribal Door! (needs verified, may be wrong)
+	chargeTarget	= "fija la vista"--Horridon sets his eyes on Eraeshio and stamps his tail! (needs verified, may be wrong)
 })
 
 ---------------------------
@@ -16,11 +50,44 @@ L:SetMiscLocalization({
 ---------------------------
 L= DBM:GetModLocalization(816)
 
+L:SetWarningLocalization({
+	specWarnPossessed		= "%s en %s - cambio de objetivo"
+})
+
+L:SetOptionLocalization({
+	warnPossessed		= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.target:format(136442),
+	specWarnPossessed	= DBM_CORE_AUTO_SPEC_WARN_OPTIONS.switch:format(136442),
+	PHealthFrame		= "Mostrar salud restante para que $spell:136442 desaparezca\n(Requiere barra de vida del boss)",
+	RangeFrame			= "Mostrar radar de rango",
+	AnnounceCooldowns	= "Mostrar cuenta de $spell:137166 (hasta 3) para uso de cooldowns de banda",
+	SetIconOnBitingCold	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(136992),
+	SetIconOnFrostBite	= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(136922)
+})
+
+
 
 ------------
 -- Tortos --
 ------------
 L= DBM:GetModLocalization(825)
+
+L:SetWarningLocalization({
+	warnKickShell			= "%s usó >%s< (%d restantes)",
+	specWarnCrystalShell	= "Obtén %s"
+})
+
+L:SetOptionLocalization({
+	warnKickShell			= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.spell:format(134031),
+	specWarnCrystalShell	= "Mostrar aviso especial cuando te falta $spell:137633",
+	InfoFrame				= "Mostrar cuadro de información para los jugadores sin $spell:137633",
+	SetIconOnTurtles		= "Poner iconos en $journal:7129",
+	ClearIconOnTurtles		= "Quitar iconos en $journal:7129 cuando les afecte $spell:133971",
+	AnnounceCooldowns		= "Mostrar cuenta de $spell:134920 para el uso de cooldowns de banda"
+})
+
+L:SetMiscLocalization({
+	WrongDebuff		= "No %s"
+})
 
 -------------
 -- Megaera --
@@ -66,4 +133,3 @@ L= DBM:GetModLocalization(832)
 -- Ra-den --
 ------------
 L= DBM:GetModLocalization(831)
-
