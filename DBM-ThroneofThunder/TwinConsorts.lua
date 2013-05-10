4a92532e11018cecd5b7cca76dfe6258891f18f7
Adam <MysticalOS@users.noreply.github.com>
2013-05-10 19:25:59 +0000

    damnit, i'm always missing option defaults. beast of nightmares is not supposed to be on by default for dps. No wonder hamlet is always angry. some of these are just bugs though in defaults code :\

--- DBM-ThroneofThunder/TwinConsorts.lua
+++ DBM-ThroneofThunder/TwinConsorts.lua
@@ -64,7 +64,7 @@ local timerCrashingStar					= mod:NewNextTimer(5.5, 137129)
 local timerCosmicBarrageCD				= mod:NewCDTimer(22, 136752)--VERY IMPORTANT on heroic, do not remove. many heroic strat ignore adds and group up BEFORE day phase starts so adds come to middle at phase start. Variation is unimportant, timer isn't to see when next cast is, it's to show safety window for when no cast will happen
 local timerTearsOfTheSunCD				= mod:NewCDTimer(41, 137404)
 local timerTearsOfTheSun				= mod:NewBuffActiveTimer(10, 137404)
-local timerBeastOfNightmaresCD			= mod:NewCDTimer(51, 137375)
+local timerBeastOfNightmaresCD			= mod:NewCDTimer(51, 137375, nil, mod:IsTank() or mod:IsHealer())
 --Light
 local timerDuskCD						= mod:NewTimer(360, "timerDuskCD", "Interface\\Icons\\achievement_zone_easternplaguelands")--it seems always 360s after combat entered. (day timer is variables, so not reliable to day phase)
 local timerLightOfDayCD					= mod:NewCDTimer(6, 137403, nil, false)--Trackable in day phase using UNIT event since boss1 can be used in this phase. Might be useful for heroic to not run behind in shadows too early preparing for a special
