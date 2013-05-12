fad3264c9696e36f5840320087f73880df848447
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-05-12 21:57:09 +0000

    fix some GUI alignment on new option

--- trunk/DBM-GUI/localization.en.lua
+++ trunk/DBM-GUI/localization.en.lua
@@ -114,7 +114,7 @@ L.RaidWarnSound				= "Play sound on raid warning"
 L.CountdownVoice			= "Set voice for countdown and countout sounds"
 L.SpecialWarnSound			= "Set sound for special warnings affecting you or your specific role"
 L.SpecialWarnSound2			= "Set sound for special warnings affecting everyone"
-L.SpecialWarnSound3			= "Set sound for VERY important special warnings (devastating to you or raid)"
+L.SpecialWarnSound3			= "Set sound for VERY important special warnings"
 
 -- Tab: Generalwarnings
 L.Tab_GeneralMessages 		= "General Messages"
