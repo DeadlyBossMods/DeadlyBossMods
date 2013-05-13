01c2d8dd2497eec6b4dc1915ff245ca2820ea873
ebmor <ebmor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-05-13 06:01:23 +0000

    IronQon: SetUsedIcons added

--- DBM-ThroneofThunder/IronQon.lua
+++ DBM-ThroneofThunder/IronQon.lua
@@ -6,6 +6,7 @@ mod:SetCreatureID(68078, 68079, 68080, 68081)--Ro'shak 68079, Quet'zal 68080, Da
 mod:SetMainBossID(68078)
 mod:SetQuestID(32754)
 mod:SetZone()
+mod:SetUsedIcons(8)
 mod:SetBossHPInfoToHighest()
 
 mod:RegisterCombat("combat")
