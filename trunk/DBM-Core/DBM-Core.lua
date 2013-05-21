5e099c55b517fc3d18fcae487d4a9d93271bd8d4
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-05-21 20:07:25 +0000

    increment alpha version number

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -45,8 +45,8 @@
 -------------------------------
 DBM = {
 	Revision = tonumber(("$Revision$"):sub(12, -3)),
-	DisplayVersion = "5.2.6 alpha", -- the string that is shown as version
-	ReleaseRevision = 9413 -- the revision of the latest stable version that is available
+	DisplayVersion = "5.3.1 alpha", -- the string that is shown as version
+	ReleaseRevision = 9592 -- the revision of the latest stable version that is available
 }
 
 -- Legacy crap; that stupid "Version" field was never a good idea.
