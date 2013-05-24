cb4b27a2d793dcffaeb81ee631b23252eff4f92d
Adam <MysticalOS@users.noreply.github.com>
2013-05-24 10:42:55 +0000

    unregister UNIT_AURA during rampages or when there are no ice heads in back. This greatly reduces amount of cpu waste working around CLEU bug. This way, if UNIT_AURA processing during rampage does have something to do with combat lag, we can't get blamed for it.

--- DBM-ThroneofThunder/Tortos.lua
+++ DBM-ThroneofThunder/Tortos.lua
@@ -256,6 +256,8 @@ function mod:SPELL_CAST_SUCCESS(args)
 end
 
 --Does not show in combat log, so UNIT_AURA must be used instead
+--This needs to be switched to RegisterUnitEvent once tandanu is done wit that code.
+--that way dbm isn't checking if it's boss1 325635325 times a fight.
 function mod:UNIT_AURA(uId)
 	if uId ~= "boss1" then return end
 	local _, _, _, _, _, duration, expires = UnitDebuff(uId, shellConcussion)
