db2f817703ab07f6281716b818c888be7c187bdf
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-05-24 23:36:55 +0000

    fix 3 of those

--- trunk/DBM-WorldEvents/DMF/Cannon.lua
+++ trunk/DBM-WorldEvents/DMF/Cannon.lua
@@ -6,7 +6,7 @@ mod:SetZone()
 
 mod:RegisterEvents(
 	"SPELL_CAST_SUCCESS",
-	"UNIT_AURA_UNFILTERED"
+	"UNIT_AURA player"
 )
 
 local timerMagicWings				= mod:NewBuffFadesTimer(8.5, 102116)
@@ -30,7 +30,6 @@ function mod:SPELL_CAST_SUCCESS(args)
 end
 
 function mod:UNIT_AURA_UNFILTERED(uId)
-	if uId ~= "player" then return end
 	if UnitBuff("player", GetSpellInfo(102116)) and not markWings then
 		MagicWingsCountdown:Start(7.5)--Might need to reduce it by 1 or use UnitDebuff duration arg.
 		timerMagicWings:Start()
