b910fbc4ece1de97cc737ceace702af87c98fa4a
Adam <MysticalOS@users.noreply.github.com>
2013-05-26 08:35:27 +0000

    rev flag

--- DBM-HeartofFear/HoFTrash.lua
+++ DBM-HeartofFear/HoFTrash.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("HoFTrash", "DBM-HeartofFear")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 9656 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 --mod:SetModelID(47785)
 mod:SetZone()
 
