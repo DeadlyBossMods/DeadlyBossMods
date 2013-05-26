9a3a2ff01f9ca301e482f0214209538dc6887034
Adam <MysticalOS@users.noreply.github.com>
2013-05-26 23:05:00 +0000

    remove antispams again, we tracked down the bug to blizzards end and implemented workaround. Also, because of bug, avoid using mouseover in UNIT_SPELLCAST events, it just duplicates events

--- DBM-HeartofFear/Zorlok.lua
+++ DBM-HeartofFear/Zorlok.lua
@@ -195,7 +195,7 @@ end
 
 --"<55.0 21:38:55> [CLEU] UNIT_DIED#true#0x0000000000000000#nil#-2147483648#-2147483648#0xF130FE9600003072#Echo of Force and Verve#68168#0", -- [10971]
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if spellId == 122933 and self:AntiSpam() then--Clear Throat (4 seconds before force and verve)
+	if spellId == 122933 then--Clear Throat (4 seconds before force and verve)
 		warnForceandVerve:Show()
 		specwarnForce:Show()
 		timerForceCast:Start()
