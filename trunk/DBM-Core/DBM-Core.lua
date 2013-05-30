8919720d75eceade07c47d0416dd88be8525c7ac
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-05-30 21:36:39 +0000

    remove barrens sound, i'm going to make an external with that plus other stuff

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -165,7 +165,6 @@ DBM.DefaultOptions = {
 	LastRevision = 0,
 	FilterSayAndYell = false,
 	ChatFrame = "DEFAULT_CHAT_FRAME",
-	barrensSounds = true,
 }
 
 DBM.Bars = DBT:New()
@@ -2801,13 +2800,6 @@ do
 	end
 
 	function DBM:CHAT_MSG_MONSTER_EMOTE(msg)
-		if DBM.Options.barrensSounds and LastZoneMapID == 11 then--Northern barrens caravan messages
-			if DBM.Options.UseMasterVolume then
-				PlaySoundFile("Sound\\interface\\UI_RaidBossWhisperWarning.ogg", "Master")
-			else
-				PlaySoundFile("Sound\\interface\\UI_RaidBossWhisperWarning.ogg")
-			end
-		end
 		return onMonsterMessage("emote", msg)
 	end
 
