6314d1c00ec570fe07f2d9018d811d59bcdd4f43
Adam <MysticalOS@users.noreply.github.com>
2013-06-12 04:59:44 +0000

    Add Orgrimmar raid since EJ info is data mined now and all bosses are known. Fix flex difficulty detection.

---
+++ DBM-FallOfOrgrimmar/Malkorok.lua
@@ -0,0 +1,86 @@
+if select(4, GetBuildInfo()) >= 50400 then return end
+local mod	= DBM:NewMod(846, "DBM-FallOfOrgrimmar", nil, 369)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+--mod:SetCreatureID(69465)
+--mod:SetQuestID(32744)
+mod:SetZone()
+
+mod:RegisterCombat("combat")
+
+--[[
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_START",
+	"SPELL_CAST_SUCCESS",
+	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_REMOVED",
+	"SPELL_PERIODIC_DAMAGE",
+	"SPELL_PERIODIC_MISSED",
+	"CHAT_MSG_RAID_BOSS_EMOTE"
+)
+
+
+function mod:OnCombatStart(delay)
+
+end
+
+function mod:OnCombatEnd()
+
+end
+
+function mod:SPELL_CAST_START(args)
+	if args.spellId == 137399 then
+		self:BossTargetScanner(69465, "FocusedLightningTarget", 0.025, 12)
+		timerFocusedLightningCD:Start()
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 137162 then
+		timerStaticBurstCD:Start()
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args.spellId == 137162 then
+		warnStaticBurst:Show(args.destName)
+		if args:IsPlayer() then
+			specWarnStaticBurst:Show()
+		else
+			specWarnStaticBurstOther:Show(args.destName)
+		end
+	end
+end
+
+function mod:SPELL_AURA_REMOVED(args)
+	if args.spellId == 138732 and args:IsPlayer() then
+		timerIonization:Cancel()
+		self:Unschedule(checkWaterIonization)
+		if self.Options.RangeFrame and not UnitDebuff("player", GetSpellInfo(137422)) then--if you have 137422 we don't want to hide it either.
+			DBM.RangeCheck:Hide()
+		end
+	end
+end
+
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, destName, _, _, spellId)
+	if spellId == 138006 and destGUID == UnitGUID("player") and self:AntiSpam() then
+		specWarnElectrifiedWaters:Show()
+	end
+end
+mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
+
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, _, _, _, target)
+	if msg:find("spell:137175") then
+		local target = DBM:GetFullNameByShortName(target)
+		warnThrow:Show(target)
+		timerStormCD:Start()
+		self:Schedule(55.5, checkWaterStorm)--check before 5 sec.
+		if target == UnitName("player") then
+			specWarnThrow:Show()
+		else
+			specWarnThrowOther:Show(target)
+		end
+	end
+end
+--]]
