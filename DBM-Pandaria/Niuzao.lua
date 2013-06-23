d353cdc402aca5d2cabf81a425f605c93eac02ac
Adam <MysticalOS@users.noreply.github.com>
2013-06-23 17:41:39 +0000

    creatureids for all 5 world bosses on isle of timeless

--- DBM-Pandaria/Niuzao.lua
+++ DBM-Pandaria/Niuzao.lua
@@ -3,7 +3,7 @@ local mod	= DBM:NewMod(859, "DBM-Pandaria", nil, 322)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(72464)
+mod:SetCreatureID(71954)
 --mod:SetQuestID(32519)--Note, this is currently bugged and returns nalak's answer (ie, yes if nalak has been killed no if nalak hasn't, it doesn't reference oondasta at all until blizzard fixes it)
 mod:SetZone()
 
