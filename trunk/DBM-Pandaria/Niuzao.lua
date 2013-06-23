13208960d860ea10aaacda67bbca8fe33cd8c4e0
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-06-23 21:20:18 +0000

    Niuzao, The Black Ox

--- trunk/DBM-Pandaria/Niuzao.lua
+++ trunk/DBM-Pandaria/Niuzao.lua
@@ -4,106 +4,78 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(71954)
---mod:SetQuestID(32519)--Note, this is currently bugged and returns nalak's answer (ie, yes if nalak has been killed no if nalak hasn't, it doesn't reference oondasta at all until blizzard fixes it)
-mod:SetZone()
+--mod:SetQuestID(32519)
 
 mod:RegisterCombat("combat")
---[[
+
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
 	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_APPLIED_DOSE",
-	"SPELL_AURA_REMOVED"
+	"SPELL_AURA_APPLIED_DOSE"
 )
 
+--[[
 mod:RegisterEvents(
 	"CHAT_MSG_MONSTER_YELL"
 )
+--]]
 
-local warnCrush					= mod:NewStackAnnounce(137504, 2, nil, mod:IsTank() or mod:IsHealer())--Cast every 30 seconds roughly, lasts 1 minute. you need 3 tanks to be able to tank the boss without debuff. 2 tanks CAN do but they will always have 1 stack and take 25% more damage
-local warnPiercingRoar			= mod:NewSpellAnnounce(137457, 2, nil)
-local warnSpiritfireBeam		= mod:NewTargetAnnounce(137511, 3)
-local warnFrillBlast			= mod:NewSpellAnnounce(137505, 4)--While this SHOULD be a tank only warning, thanks to terrible blizzard design, this fight is anything but a tanked fight, so it's now an everyone warning since god knows what fucking way the boss will be facing when he casts this
+local warnHeadbutt				= mod:NewSpellAnnounce(144610, 3, nil, mod:IsTank())
+local warnOxenFortitude			= mod:NewStackAnnounce(144606, 2)--144607 player version, but better to just track boss and announce stacks
+local warnMassiveQuake			= mod:NewSpellAnnounce(144611, 3)
+local warnCharge				= mod:NewSpellAnnounce(144609, 4)
 
-local specWarnCrush				= mod:NewSpecialWarningStack(137504, mod:IsTank(), 2)
-local specWarnCrushOther		= mod:NewSpecialWarningTarget(137504, mod:IsTank())--This should not go over 1 stack so don't need stack warning just a "taunt the boss" warning
-local specWarnPiercingRoar		= mod:NewSpecialWarningCast(137457, mod:IsRanged() or mod:IsHealer())
-local specWarnFrillBlast		= mod:NewSpecialWarningSpell(137505, nil, nil, nil, 2)
+local specWarnHeadbutt			= mod:NewSpecialWarningSpell(144610, mod:IsTank())
+local specWarnMassiveQuake		= mod:NewSpecialWarningCast(144611, mod:IsHealer())
+local specWarnCharge			= mod:NewSpecialWarningSpell(144609, nil, nil, nil, 2)
 
-local timerCrush				= mod:NewTargetTimer(60, 137504, nil, mod:IsTank() or mod:IsHealer())
-local timerCrushCD				= mod:NewCDTimer(26, 137504)
-local timerPiercingRoarCD		= mod:NewCDTimer(25, 137457)--25-60sec variation (i'm going to guess like all the rest of the variations, the timers are all types of fucked up when the boss is running around untanked, which delays casts of crush and frill blast, but makes him cast spitfire twice as often)
---local timerSpiritfireBeamCD		= mod:NewCDTimer(25, 137511)--25-30sec variation (disabled because he also seems to spam it far more often if there is no tank, making it difficult to find an ACTUAL cd when fight is done incorrectly
-local timerFrillBlastCD			= mod:NewCDTimer(25, 137505)--25-30sec variation
+--local timerHeadbuttCD			= mod:NewCDTimer(60, 144610, nil, mod:IsTank())
+--local timerMassiveQuakeCD		= mod:NewCDTimer(60, 137511)
 
-mod:AddBoolOption("RangeFrame", true)
-
-local yellTriggered = false
+--local yellTriggered = false
 
 function mod:OnCombatStart(delay)
-	if yellTriggered then--We know for sure this is an actual pull and not diving into in progress
---		timerCrushCD:Start(-delay)--There was no tank, so he pretty much never cast this, just ran like a wild animal around area while corpse cannoned
---		timerSpiritfireBeamCD:Start(15-delay)
-		timerPiercingRoarCD:Start(20-delay)
-		timerFrillBlastCD:Start(40-delay)
-	end
-	if self.Options.RangeFrame then
-		DBM.RangeCheck:Show(10)--range is guessed. spell tooltip and EJ do not save what range is right now.
-	end
+--[[	if yellTriggered then
+		timerHeadbuttCD:Start(15-delay)
+		timerMassiveQuakeCD:Start(20-delay)
+	end--]]
 end
 
 function mod:OnCombatEnd()
-	if self.Options.RangeFrame then
-		DBM.RangeCheck:Hide()
-	end
-	yellTriggered = false
+--	yellTriggered = false
 end
 
 function mod:SPELL_CAST_START(args)
-	if args.spellId == 137457 then
-		warnPiercingRoar:Show()
-		specWarnPiercingRoar:Show()
-		timerPiercingRoarCD:Start()
-	elseif args.spellId == 137505 then
-		warnFrillBlast:Show()
-		specWarnFrillBlast:Show()
-		timerFrillBlastCD:Start()
+	if args.spellId == 144610 then
+		warnHeadbutt:Show()
+		specWarnHeadbutt:Show()
+--		timerHeadbuttCD:Start()
+	elseif args.spellId == 144611 then
+		warnMassiveQuake:Show()
+		specWarnMassiveQuake:Show()
+--		timerMassiveQuakeCD:Start()
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(137508, 137511) then
-		warnSpiritfireBeam:Show(args.destName)
---		timerSpiritfireBeamCD:Start()
+	if args.spellId == 144609 then
+		warnCharge:Show()
+		specWarnCharge:Show()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 137504 then
-		warnCrush:Show(args.destName, args.amount or 1)
-		timerCrush:Start(args.destName)
-		timerCrushCD:Start()
-		if args:IsPlayer() and (args.amount or 1) >= 2 then
-			specWarnCrush:Show(args.amount)
-		else
-			if (args.amount or 1) >= 2 and not UnitIsDeadOrGhost("player") or not UnitDebuff("player", GetSpellInfo(137504)) then
-				specWarnCrushOther:Show(args.destName)
-			end
-		end
+	if args.spellId == 144606 then
+		warnOxenFortitude:Show(args.destName, args.amount or 1)
 	end
 end
-mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED--<--if this happens you're doing fight wrong. But we announce it anyways to identify the problem
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
-function mod:SPELL_AURA_REMOVED(args)
-	if args.spellId == 137504 then
-		timerCrush:Cancel(args.destName)
-	end
-end
-
+--[[
 function mod:CHAT_MSG_MONSTER_YELL(msg)
 	if msg == L.Pull and not self:IsInCombat() then
-		if self:GetCIDFromGUID(UnitGUID("target")) == 69161 or self:GetCIDFromGUID(UnitGUID("targettarget")) == 69161 then--Whole zone gets yell, so lets not engage combat off yell unless he is our target (or the target of our target for healers)
+		if self:GetCIDFromGUID(UnitGUID("target")) == 71954 or self:GetCIDFromGUID(UnitGUID("targettarget")) == 71954 then
 			yellTriggered = true
 			DBM:StartCombat(self, 0)
 		end
