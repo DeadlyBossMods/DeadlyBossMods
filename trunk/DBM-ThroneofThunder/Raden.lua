4a7795598e35287a0558449531a59c90b4b1fad3
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-06-28 03:20:09 +0000

    that needs an antispam but looks good

--- trunk/DBM-ThroneofThunder/Raden.lua
+++ trunk/DBM-ThroneofThunder/Raden.lua
@@ -47,7 +47,7 @@ local specWarnCracklingStalker	= mod:NewSpecialWarningSwitch(138339, mod:IsRange
 local specWarnVitaSensitive		= mod:NewSpecialWarningYou(138372)
 local specWarnVitaSoaker		= mod:NewSpecialWarning("specWarnVitaSoaker")
 local specWarnUnstablVita		= mod:NewSpecialWarningYou(138297, nil, nil, nil, 3)
-local specWarnUnstablVitaJump	= mod:NewSpecialWarningYou(138297, nil, nil, nil, 1)
+local specWarnUnstablVitaJump	= mod:NewSpecialWarningYou(138308, nil, nil, nil, 1)
 local yellUnstableVita			= mod:NewYell(138297, nil, false)
 --General
 local specWarnCreation			= mod:NewSpecialWarningSpell(138321, mod:IsDps())
@@ -252,7 +252,7 @@ end
 
 function mod:UNIT_POWER_FREQUENT(uId)
 	local power = UnitPower(uId)
-	if power == 90 and UnitBuff(uId, GetSpellInfo(138332)) then
+	if power == 90 and UnitBuff(uId, GetSpellInfo(138332)) and self:AntiSpam() then
 		specWarnFatalStrike:Show()
 	end
 end
