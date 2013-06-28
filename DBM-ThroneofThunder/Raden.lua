659475e35626a377fa9386b40e16b9bccc94fab9
Adam <MysticalOS@users.noreply.github.com>
2013-06-28 02:57:57 +0000

    fix another bug

--- DBM-ThroneofThunder/Raden.lua
+++ DBM-ThroneofThunder/Raden.lua
@@ -14,6 +14,7 @@ mod:RegisterEventsInCombat(
 	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
 	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_REMOVED",
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
@@ -199,7 +200,6 @@ function mod:SPELL_AURA_APPLIED(args)
 		if self.Options.SetIconsOnVita then
 			playerWithVita = DBM:GetRaidUnitId(args.destName)
 			self:SetIcon(args.destName, 1)
-			self:UnscheduleMethod("checkVitaDistance")
 			self:ScheduleMethod(6, "checkVitaDistance")--Wait about 6 seconds for initial. don't want to spam icons before next target actually runs out. May raise initial even higher, debuff is 12 seconds. so maybe 7-8 if 6 too soon 
 		end
 		warnUnstableVita:Show(args.destName)
