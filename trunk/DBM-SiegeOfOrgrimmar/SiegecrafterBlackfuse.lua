be64235fc3fa6cf59baab505bb6278262bb2feca
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-07-03 02:37:05 +0000

    Commit what i have for blackfuse fix a paragons bug

--- trunk/DBM-SiegeOfOrgrimmar/SiegecrafterBlackfuse.lua
+++ trunk/DBM-SiegeOfOrgrimmar/SiegecrafterBlackfuse.lua
@@ -11,9 +11,14 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START",
 	"SPELL_CAST_SUCCESS",
+	"SPELL_SUMMON",
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_APPLIED_DOSE",
-	"SPELL_AURA_REMOVED"
+	"SPELL_AURA_REMOVED",
+	"SPELL_PERIODIC_DAMAGE",
+	"SPELL_PERIODIC_MISSED",
+	"UNIT_DIED",
+	"RAID_BOSS_WHISPER"
 )
 
 --Siegecrafter Blackfuse
@@ -21,47 +26,58 @@ local warnLaunchSawblade				= mod:NewTargetAnnounce(143265, 3)
 local warnProtectiveFrenzy				= mod:NewTargetAnnounce(145365, 3, nil, mod:IsTank() or mod:IsHealer())
 local warnElectroStaticCharge			= mod:NewStackAnnounce(143385, 2, nil, mod:IsTank())
 --Automated Shredders
-local warnDeathFromAbove				= mod:NewTargetAnnounce(144208, 4)--Player target, not vulnerable shredder target.
+local warnDeathFromAbove				= mod:NewTargetAnnounce(144208, 4)--Player target, not vulnerable shredder target. (should always be cast on highest threat target, but i like it still being a "target" warning)
 --The Assembly Line
-local warnShockwaveMissileActivated		= mod:NewSpellAnnounce("ej8204", 3)--Unsure if this will even show in CLEU, may need UNIT event or emote
-local warnShockwaveMissile				= mod:NewCountAnnounce(143639, 3)--Trigger ID for actual missiles is 143642
---local warnLaserTurretActivated			= mod:NewSpellAnnounce("ej8208", 3)--Many scripted triggers. gonna need an emote or UNIT_SPELL event for this i'm sure
-local warnSuperheated					= mod:NewSpellAnnounce(144040, 3)--Unsure if right ID
+local warnShockwaveMissileActivated		= mod:NewSpellAnnounce("ej8204", 3, 143639)--Unsure if this will even show in CLEU, may need UNIT event or emote
+local warnShockwaveMissile				= mod:NewCountAnnounce(143641, 3)
+local warnLaserTurretActivated			= mod:NewSpellAnnounce("ej8208", 3, 143867, false)--Many scripted triggers. gonna need an emote or UNIT_SPELL event for this i'm sure
+local warnLaserFixate					= mod:NewTargetAnnounce(143833, 3, 143867)--Not in combat log, needs more debugging to find a way around blizz fail
 local warnMagneticCrush					= mod:NewSpellAnnounce(144466, 3)--Unsure if correct ID, could be 143487 instead
-local warnBreakinPeriod					= mod:NewTargetAnnounce(145269, 3)
-local warnReadyToGo						= mod:NewTargetAnnounce(145580, 4)
+local warnBreakinPeriod					= mod:NewTargetAnnounce(145269, 3)--Crawler Mine Spawning
+local warnReadyToGo						= mod:NewTargetAnnounce(145580, 4)--Crawler mine not dead fast enough
 
 --Siegecrafter Blackfuse
 local specWarnLaunchSawblade			= mod:NewSpecialWarningYou(143265)
 local yellLaunchSawblade				= mod:NewYell(143265)
 local specWarnProtectiveFrenzy			= mod:NewSpecialWarningTarget(145365, mod:IsTank())
 --Automated Shredders
---local specWarnAutomatedShredder		= mod:NewSpecialWarningSpell("ej8199", mod:IsTank())--No sense in dps switching when spawn, has damage reduction. This for tank pickup
+local specWarnAutomatedShredder			= mod:NewSpecialWarningSpell("ej8199", mod:IsTank())--No sense in dps switching when spawn, has damage reduction. This for tank pickup
 local specWarnDeathFromAbove			= mod:NewSpecialWarningYou(144208)
 local specWarnAutomatedShredderSwitch	= mod:NewSpecialWarningSwitch("ej8199")--i.e. It's vunerable after Death From Above. This is when everyone else switch.
 --The Assembly Line
+local specWarnAssemblyLine				= mod:NewSpecialWarningSpell("ej8199", mod:IsDps())
+local specWarnShockwaveMissileActive	= mod:NewSpecialWarningSpell("ej8204", nil, nil, nil, 2)
 local specWarnReadyToGo					= mod:NewSpecialWarningTarget(145580)
+local specWarnLaserFixate				= mod:NewSpecialWarningRun(143833)
+local yellLaserFixate					= mod:NewYell(143867)
+local specWarnSuperheated				= mod:NewSpecialWarningMove(143856)--From lasers. Hard to see, this warning will help a ton
+local specWarnMagneticCrush				= mod:NewSpecialWarningSpell(144466, nil, nil, nil, 2)
+local specWarnCrawlerMineFixate			= mod:NewSpecialWarningRun(144010)
+local yellCrawlerMineFixate				= mod:NewYell(144010, nil, false)
 
 --Siegecrafter Blackfuse
 local timerProtectiveFrenzy				= mod:NewBuffActiveTimer(10, 145365, mod:IsTank() or mod:IsHealer())
 local timerElectroStaticCharge			= mod:NewTargetTimer(60, 143385, mod:IsTank())
+local timerElectroStaticChargeCD		= mod:NewCDTimer(17, 143385, mod:IsTank())--17-22 second variation
+local timerLaunchSawbladeCD				= mod:NewCDTimer(10, 143265)--10-15sec cd
 --Automated Shredders
---local timerAutomatedShredderCD		= mod:NewNextTimer(60, "ej8199")
+local timerAutomatedShredderCD			= mod:NewNextTimer(60, "ej8199", nil, nil, nil, 85914)--Don't know true cd yet, just timing or first
 local timerDeathFromAboveDebuff			= mod:NewTargetTimer(5, 144210, not mod:IsHealer())
+local timerDeathFromAboveCD				= mod:NewNextTimer(40, 144208, not mod:IsHealer())
 --The Assembly Line
+local timerAssemblyLineCD				= mod:NewNextTimer(40, "ej8199", nil, nil, nil, 59193)
 local timerPatternRecognition			= mod:NewBuffActiveTimer(60, 144236)
-local timerShockwaveMissileActive		= mod:NewBuffActiveTimer(120, 143639)--Assumed from tooltips
-local timerShockwaveMissileCD			= mod:NewNextTimer(12, 143639)--1 missile every 12 seconds for 120 seconds, ie 10 missles)
+local timerDisintegrationLaserCD		= mod:NewNextCountTimer(10, 143867)
+local timerShockwaveMissileActive		= mod:NewBuffActiveTimer(30, 143639)
+local timerShockwaveMissileCD			= mod:NewNextCountTimer(12, 143641)--1 missile every 12 seconds for 120 seconds, ie 10 missles)
 local timerBreakinPeriod				= mod:NewTargetTimer(60, 145269)--How many mines up at once? will this be spammy?
 
 local missileCount = 0
+local laserCount = 0--Fires 3 times i think
+local activeWeaponsGUIDS = {}
 
 function mod:LaunchSawBladeTarget(targetname, uId)
 	warnLaunchSawblade:Show(targetname)
-	if targetname == UnitName("player") then
-		specWarnLaunchSawblade:Show()
-		yellLaunchSawblade:Yell()
-	end
 end
 
 --May be two up at once so can't use generic boss scanner.
@@ -79,18 +95,46 @@ function mod:DeathFromAboveTarget(sGUID)
 	end
 end
 
-function mod:OnCombatStart(delay)
-	missileCount = 0
+--like many important debuffs last tier and now this tier, APPLIED & REMOVED SPELL_CAST events are disabled, so we have to waste cpu to find them.
+local spellName = GetSpellInfo(143867)
+local spellName2 = GetSpellInfo(143833)
+local function findLaser()
+	for uId in DBM:GetGroupMembers() do
+		local name = DBM:GetUnitFullName(uId)
+		if UnitDebuff(uId, spellName) then
+			print("DBM DEBUG: Possible match to laser targeting using "..spellName)
+			warnLaserFixate:Show(name)
+			if name == UnitName("player") then
+				specWarnLaserFixate:Show()
+				yellLaserFixate:Yell()
+			end
+			return
+		elseif UnitDebuff(uId, spellName2) then
+			print("DBM DEBUG: Possible match to laser targeting using "..spellName2)
+			warnLaserFixate:Show(name)
+			if name == UnitName("player") then
+				specWarnLaserFixate:Show()
+				yellLaserFixate:Yell()
+			end
+			return
+		end
+	end
+	mod:Schedule(0.1, findLaser)
 end
 
-function mod:OnCombatEnd()
-
+function mod:OnCombatStart(delay)
+	table.wipe(activeWeaponsGUIDS)
+	missileCount = 0
+	laserCount = 0
+	timerAutomatedShredderCD:Start(35-delay)
 end
 
 function mod:SPELL_CAST_START(args)
 	if args.spellId == 143265 then
+		timerLaunchSawbladeCD:Start()
 		self:BossTargetScanner(71504, "LaunchSawBladeTarget", 0.1, 16)
 	elseif args.spellId == 144208 then
+		timerDeathFromAboveCD:Start(args.sourceGUID)
 		self:ScheduleMethod(0.2, "DeathFromAboveTarget", args.sourceGUID)--Always targets tank, so 1 scan all needed
 		specWarnAutomatedShredderSwitch:Schedule(3)--Better here then when debuff goes up, give dps 2 seconds rampup time so spells in route when debuff goes up.
 	end
@@ -99,16 +143,20 @@ end
 function mod:SPELL_CAST_SUCCESS(args)
 	if args.spellId == 143639 then--Missile Activation
 		warnShockwaveMissileActivated:Show()
+		specWarnShockwaveMissileActive:Show()
 		timerShockwaveMissileActive:Start()
 		missileCount = 0
-	elseif args.spellId == 143642 then--Missile Launching
+		timerShockwaveMissileCD:Start(3, 1)
+	end
+end
+
+function mod:SPELL_SUMMON(args)
+	if args.spellId == 143641 then--Missile Launching
 		missileCount = missileCount + 1
 		warnShockwaveMissile:Show(missileCount)
-		if missileCount <= 9 then--TODO< verify this
-			timerShockwaveMissileCD:Start()
+		if missileCount < 3 then
+			timerShockwaveMissileCD:Start(nil, missileCount+1)
 		end
-	elseif args.spellId == 144040 then
-		warnSuperheated:Show()
 	end
 end
 
@@ -121,6 +169,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		local amount = args.amount or 1
 		warnElectroStaticCharge:Show(args.destName, amount)
 		timerElectroStaticCharge:Start(args.destName)
+		timerElectroStaticChargeCD:Start()
 	elseif args.spellId == 144210 and not args:IsDestTypePlayer() then
 		timerDeathFromAboveDebuff:Start(args.destName)
 	elseif args.spellId == 144236 and args:IsPlayer() then
@@ -131,6 +180,21 @@ function mod:SPELL_AURA_APPLIED(args)
 	elseif args.spellId == 145580 then
 		warnReadyToGo:Show(args.destName)
 		specWarnReadyToGo:Show(args.destName)
+	elseif args.spellId == 143867 then
+		if not activeWeaponsGUIDS[args.sourceGUID] then
+			activeWeaponsGUIDS[args.sourceGUID] = true
+			laserCount = 0
+			warnLaserTurretActivated:Show()
+		end
+		laserCount = laserCount + 1
+		if laserCount < 3 then--Seems each laser construction casts 3 times, then disapears.
+			timerDisintegrationLaserCD:Start(nil, laserCount+1)
+		end
+		self:Unschedule(findLaser)
+		findLaser()
+	elseif args.spellId == 144466 and self:AntiSpam(15, 1) then--Only way i see to detect magnet activation, antispam is so it doesn't break if a player dies during it.
+		warnMagneticCrush:Show()
+		specWarnMagneticCrush:Show()
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
@@ -145,25 +209,37 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end
 
---[[
-function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, destName, _, _, spellId)
-	if spellId == 138006 and destGUID == UnitGUID("player") and self:AntiSpam() then
-		specWarnElectrifiedWaters:Show()
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
+	if spellId == 143856 and destGUID == UnitGUID("player") and self:AntiSpam(2, 2) then
+		specWarnSuperheated:Show()
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
-function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, _, _, _, target)
-	if msg:find("spell:137175") then
-		local target = DBM:GetFullNameByShortName(target)
-		warnThrow:Show(target)
-		timerStormCD:Start()
-		self:Schedule(55.5, checkWaterStorm)--check before 5 sec.
-		if target == UnitName("player") then
-			specWarnThrow:Show()
-		else
-			specWarnThrowOther:Show(target)
-		end
+function mod:RAID_BOSS_WHISPER(msg)
+	if msg:find("spell:143266") then--Target scanning works on this one, but is about 1 second slower than emote. emote is .2 seconds after SPELL_CAST_START, but target scanning can't grab right target until like 1.0 or 1.2 sec into cast
+		specWarnLaunchSawblade:Show()
+		yellLaunchSawblade:Yell()
+	elseif msg == L.mineTarget or msg:find(L.mineTarget) then--Doesn't show in combat log at all (what else is new)
+		specWarnCrawlerMineFixate:Show()
+		yellCrawlerMineFixate:Yell()
 	end
 end
---]]
+
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, _, _, _, target)
+	if msg == L.newWeapons or msg:find(L.newWeapons) then
+		specWarnAssemblyLine:Show()
+		timerAssemblyLineCD:Start()
+	elseif msg == L.newShredder or msg:find(L.newShredder) then
+		specWarnAutomatedShredder:Show()
+		timerDeathFromAboveCD:Start(17)
+--		timerAutomatedShredderCD:Start()
+	end
+end
+
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 71591 then
+		timerDeathFromAboveCD:Cancel(args.destGUID)
+	end
+end
