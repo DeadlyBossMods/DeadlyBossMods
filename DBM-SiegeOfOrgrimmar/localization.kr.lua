aba0e06b7528632d0a87fb9a96e5dc7cac40bca1
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-07-05 14:28:44 +0000

    # koKR update. # Siege OF Orgirmmar TOC : Remove X-DBM-Mod-Name. Not Needed.

---
+++ DBM-SiegeOfOrgrimmar/localization.kr.lua
@@ -0,0 +1,232 @@
+﻿local L
+
+---------------
+-- Immerseus --
+---------------
+L= DBM:GetModLocalization(852)
+
+L:SetWarningLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+---------------------------
+-- The Fallen Protectors --
+---------------------------
+L= DBM:GetModLocalization(849)
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+---------------------------
+-- Norushen --
+---------------------------
+L= DBM:GetModLocalization(866)
+
+L:SetWarningLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+------------------
+-- Sha of Pride --
+------------------
+L= DBM:GetModLocalization(867)
+
+L:SetWarningLocalization({
+})
+
+L:SetOptionLocalization({
+	InfoFrame			= "$journal:8255에 대한 정보 창 보기"
+})
+
+L:SetMiscLocalization({
+})
+
+--------------
+-- Galakras --
+--------------
+L= DBM:GetModLocalization(868)
+
+L:SetTimerLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+--------------------
+--Iron Juggernaut --
+--------------------
+L= DBM:GetModLocalization(864)
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+--------------------------
+-- Kor'kron Dark Shaman --
+--------------------------
+L= DBM:GetModLocalization(856)
+
+L:SetWarningLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+---------------------
+-- General Nazgrim --
+---------------------
+L= DBM:GetModLocalization(850)
+
+L:SetWarningLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({--정식버전 출시시 로컬라이징 필요
+	newForces1				= "Warriors, on the double!",
+	newForces2				= "Defend the gate!",
+	newForces3				= "Rally the forces!",
+	newForces4				= "Kor'kron, at my side!",
+	newForces5				= "Next squad, to the front!"
+})
+
+-----------------
+-- Malkorok -----
+-----------------
+L= DBM:GetModLocalization(846)
+
+L:SetWarningLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+	bloodRageEnds	= "subsides!"
+})
+
+------------------------
+-- Spoils of Pandaria --
+------------------------
+L= DBM:GetModLocalization(870)
+
+L:SetWarningLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+---------------------------
+-- Thok the Bloodthirsty --
+---------------------------
+L= DBM:GetModLocalization(851)
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+----------------------------
+-- Siegecrafter Blackfuse --
+----------------------------
+L= DBM:GetModLocalization(865)
+
+L:SetWarningLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+	mineTarget	= "A Crawler Mine has targeted you!",
+	newWeapons	= "Unfinished weapons begin to roll out on the assembly line.",
+	newShredder	= "An Automated Shredder draws near!"
+})
+
+----------------------------
+-- Paragons of the Klaxxi --
+----------------------------
+L= DBM:GetModLocalization(853)
+
+L:SetWarningLocalization({
+	specWarnActivatedVulnerable		= "%s에 취약함 - 피하세요!!",
+	specWarnCriteriaLinked			= "%s에 연결됨!"
+})
+
+L:SetOptionLocalization({
+	specWarnActivatedVulnerable		= "활성화된 용장에게 당신이 취약할 경우 특수 경고 보기",
+	specWarnCriteriaLinked			= "$spell:144095에 연결된 경우 특수 경고 보기"
+})
+
+L:SetMiscLocalization({
+	calculatedTarget	= "calculating eye!",
+	hisekFlavor			= "Look who's quiet now",--http://ptr.wowhead.com/quest=31510
+	KilrukFlavor		= "Just another day, culling the swarm",--http://ptr.wowhead.com/quest=31109
+	XarilFlavor			= "I see only dark skies in your future",--http://ptr.wowhead.com/quest=31216
+	KaztikFlavor		= "Reduced to mere kunchong treats",--http://ptr.wowhead.com/quest=31024
+	KaztikFlavor2		= "1 Mantid down, only 199 to go",--http://ptr.wowhead.com/quest=31808
+	KorvenFlavor		= "The end of an ancient empire",--http://ptr.wowhead.com/quest=31232
+	KorvenFlavor2		= "Take your Gurthani Tablets and choke on them",--http://ptr.wowhead.com/quest=31232
+	IyyokukFlavor		= "See opportunities. Exploit them!",--Does not have quests, http://ptr.wowhead.com/npc=65305
+	KarozFlavor			= "You won't be leaping anymore!",---Does not have questst, http://ptr.wowhead.com/npc=65303
+	SkeerFlavor			= "A bloody delight!",--http://ptr.wowhead.com/quest=31178
+	RikkalFlavor		= "Specimen request fulfilled"--http://ptr.wowhead.com/quest=31508
+})
+
+------------------------
+-- Garrosh Hellscream --
+------------------------
+L= DBM:GetModLocalization(869)
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("FoOTrash")
+
+L:SetGeneralLocalization({
+	name =	"오그리마 공성전: 일반구간"
+})
+
+L:SetOptionLocalization({
+})
