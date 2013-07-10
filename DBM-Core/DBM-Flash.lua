92649a6f954ca29ec015e3be1158a16e5eeb3d82
Paul Emmerich <emmericp@net.in.tum.de>
2013-07-10 22:20:24 +0000

    * change special warning fade in/out animation to a quadratic curve which gives a much smoother result * update default values for the display duration * change font size slider to not offer way too big font sizes as these won't work anyways...

--- DBM-Core/DBM-Flash.lua
+++ DBM-Core/DBM-Flash.lua
@@ -3,7 +3,8 @@ DBM.Flash = {}
 -- locals
 local flashFrame = DBM.Flash
 local r, g, b, t, a
-local t2
+local duration
+local elapsed = 0
 
 --------------------
 --  Create Frame  --
@@ -18,26 +19,23 @@ frame:SetFrameStrata("BACKGROUND")
 --  OnUpdate Handler  --
 ------------------------
 do
-	frame:SetScript("OnUpdate", function(self, elapsed)
-		self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed
-		if self.TimeSinceLastUpdate >= t then
+	frame:SetScript("OnUpdate", function(self, e)
+		elapsed = elapsed + e
+		if elapsed >= t then
 			self:Hide()
 			self:SetAlpha(0)
 			return
 		end
-		if self.TimeSinceLastUpdate >= t2 then
-			self:SetAlpha(t - self.TimeSinceLastUpdate)
-		else
-			self:SetAlpha(self.TimeSinceLastUpdate)
-		end
+		-- quadratic fade in/out
+		self:SetAlpha(-(elapsed / (duration / 2) - 1)^2 + 1)
 	end)
 	frame:Hide()
 end
 
 function flashFrame:Show(red, green, blue, dur, alpha)
 	r, g, b, t, a = red or 1, green or 0, blue or 0, dur or 1, alpha or 0.5
-	t2 = t / 2
-	frame.TimeSinceLastUpdate = 0
+	duration = dur
+	elapsed = 0
 	frame:SetAlpha(0)
 	frame:SetBackdropColor(r, g, b, a)
 	frame:Show()
