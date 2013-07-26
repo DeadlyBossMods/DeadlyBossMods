2cd17e8603f57732baab3e26ef5c7149161b4df7
Adam <MysticalOS@users.noreply.github.com>
2013-07-26 00:54:17 +0000

    Some Malkorok updates

--- DBM-SiegeOfOrgrimmar/Malkorok.lua
+++ DBM-SiegeOfOrgrimmar/Malkorok.lua
@@ -33,8 +33,9 @@ local specWarnDisplacedEnergy			= mod:NewSpecialWarningRun(142913)
 local yellDisplacedEnergy				= mod:NewYell(142913)
 --Might of the Kor'kron
 local specWarnArcingSmash				= mod:NewSpecialWarningCount(142815, nil, nil, nil, 2)
-local specWarnBreathofYShaarj			= mod:NewSpecialWarningCount(142842, nil, nil, nil, 2)
-local specWarnFatalStrike				= mod:NewSpecialWarningStack(142990, mod:IsTank(), 10)--stack guessed, based on CD
+local specWarnImplodingEnergySoon		= mod:NewSpecialWarningPreWarn(142986, nil, 5)
+local specWarnBreathofYShaarj			= mod:NewSpecialWarningCount(142842, nil, nil, nil, 3)
+local specWarnFatalStrike				= mod:NewSpecialWarningStack(142990, mod:IsTank(), 12)--stack guessed, based on CD
 local specWarnFatalStrikeOther			= mod:NewSpecialWarningTarget(142990, mod:IsTank())
 
 local timerBloodRage					= mod:NewBuffActiveTimer(22.5, 142879)--2.5sec cast plus 20 second duration
@@ -42,11 +43,14 @@ local timerDisplacedEnergyCD			= mod:NewNextTimer(11, 142913)
 local timerBloodRageCD					= mod:NewNextTimer(124.7, 142879)
 --Might of the Kor'kron
 local timerArcingSmashCD				= mod:NewNextCountTimer(17.5, 142815)--17-18 variation (the 23 second ones are delayed by Breath of Yshaarj)
+local timerImplodingEnergy				= mod:NewCastTimer(10, 142986)--Always 10 seconds after arcing
 local timerSeismicSlamCD				= mod:NewNextCountTimer(17.5, 142851)--Works exactly same as arcingsmash 18 sec unless delayed by breath. two sets of 3
 local timerBreathofYShaarjCD			= mod:NewNextCountTimer(59, 142842)
 local timerFatalStrike					= mod:NewTargetTimer(30, 142990, nil, mod:IsTank())
 
 local berserkTimer						= mod:NewBerserkTimer(360)
+
+local countdownImplodingEnergy			= mod:NewCountdown(10, 142986)
 
 local soundDisplacedEnergy				= mod:NewSound(142913)
 
@@ -61,6 +65,8 @@ local breathCast = 0
 local arcingSmashCount = 0
 local seismicSlamCount = 0
 local displacedCast = false
+local UnitDebuff = UnitDebuff
+local UnitIsDeadOrGhost = UnitIsDeadOrGhost
 
 local debuffFilter
 do
@@ -111,7 +117,7 @@ function mod:OnCombatStart(delay)
 	breathCast = 0
 	arcingSmashCount = 0
 	seismicSlamCount = 0
-	timerArcingSmashCD:Start(5-delay, 1)
+	timerSeismicSlamCD:Start(5-delay, 1)
 	timerArcingSmashCD:Start(11-delay, 1)
 	timerBreathofYShaarjCD:Start(-delay, 1)
 	timerBloodRageCD:Start(122-delay)
@@ -168,7 +174,6 @@ function mod:SPELL_CAST_SUCCESS(args)
 			timerSeismicSlamCD:Start(nil, seismicSlamCount+1)
 		end
 	elseif args.spellId == 143913 then--May not be right spell event
-		print("Essence of Y'Shaarj (5 rage gain) detected via SPELL_CAST_SUCCESS")
 		--5 rage gained from Essence of Y'Shaarj would progress timer about 2.5 seconds
 		--May choose a more accurate UNIT_POWER monitoring method if this doesn't feel accurate enough
 		if self:AntiSpam() then
@@ -198,11 +203,11 @@ function mod:SPELL_AURA_APPLIED(args)
 		self:Schedule(0.3, warnDisplacedEnergyTargets)
 	elseif args.spellId == 142990 then
 		local amount = args.amount or 1
-		if amount % 3 == 0 or amount >= 10 then
+		if amount % 3 == 0 or amount >= 12 then
 			warnFatalStrike:Show(args.destName, amount)
 		end
 		timerFatalStrike:Start(args.destName)
-		if amount >= 10 then
+		if amount >= 12 then
 			if args:IsPlayer() then--At this point the other tank SHOULD be clear.
 				specWarnFatalStrike:Show(amount)
 			else--Taunt as soon as stacks are clear, regardless of stack count.
@@ -235,16 +240,11 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 		arcingSmashCount = arcingSmashCount + 1
 		warnArcingSmash:Show(arcingSmashCount)
 		specWarnArcingSmash:Show(arcingSmashCount)
+		timerImplodingEnergy:Start()
+		countdownImplodingEnergy:Start()
+		specWarnImplodingEnergySoon:Schedule(5)
 		if arcingSmashCount < 3 then
 			timerArcingSmashCD:Start(nil, arcingSmashCount+1)
-		end
-	elseif spellId == 143913 then--May not be right spell event
-		print("Essence of Y'Shaarj (5 rage gain) detected via UNIT_SPELLCAST_SUCCEEDED")
-		--5 rage gained from Essence of Y'Shaarj would progress timer about 2.5 seconds
-		--May choose a more accurate UNIT_POWER monitoring method if this doesn't feel accurate enough
-		if self:AntiSpam() then
-			local elapsed, total = timerBloodRageCD:GetTime()
-			timerBloodRageCD:Update(elapsed+2.5, total)
 		end
 	end
 end
