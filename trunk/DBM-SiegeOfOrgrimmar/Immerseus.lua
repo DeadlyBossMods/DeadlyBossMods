00fe02f4558757e8e9f9e3e601035d40da6c13b9
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-07-29 07:08:13 +0000

    also remove useless cid

--- trunk/DBM-SiegeOfOrgrimmar/Immerseus.lua
+++ trunk/DBM-SiegeOfOrgrimmar/Immerseus.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(852, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(71543, 72436)--Doesn't die, will need kill detection
+mod:SetCreatureID(71543)--Doesn't die, will need kill detection
 mod:SetReCombatTime(45)--Lets just assume he has same bug as tsulong in advance and avoid problems
 --mod:SetQuestID(32744)
 mod:SetZone()
