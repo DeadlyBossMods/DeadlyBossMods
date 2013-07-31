cc538599ae2426881382ccb7a535cf82b1667ddf
Adam <MysticalOS@users.noreply.github.com>
2013-07-31 22:28:48 +0000

    combat detection ofr Galakras added (varian) adding some of the missing abilities that weren't in EJ Don't have very good logs so most timers not available and none of tower commanders ever got pulled do to buggy trash.

--- DBM-SiegeOfOrgrimmar/Galakras.lua
+++ DBM-SiegeOfOrgrimmar/Galakras.lua
@@ -2,11 +2,11 @@ local mod	= DBM:NewMod(868, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(72249)
+mod:SetCreatureID(72311, 72249)--Boss needs to engage off king varian, not the boss. I include the boss too so we don't detect a win off losing varian. :)
+mod:SetMainBossID(72249)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
-
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START",
@@ -14,12 +14,17 @@ mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED_DOSE",
 	"SPELL_AURA_REMOVED",
 	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED"
+	"SPELL_PERIODIC_MISSED",
+	"UNIT_DIED",
+	"CHAT_MSG_RAID_BOSS_EMOTE"
 )
 
---Stage 1: Clear the Landing
-----Nothing in EJ so can't drycode this
---Stage 2: Bring Her Down!
+--Stage 1: Clear the Landing/Bring Her Down!
+----TODO, don't want this mod to register events in entire zone so it can warn for prelude trash.
+----I'll put duplicate eevnts in trash mod instead since trash mod will be disabled during encounters
+local warnWarBanner					= mod:NewSpellAnnounce(147328, 3)
+local warnFracture					= mod:NewTargetAnnounce(146899, 3)--TODO: see if target scanning works with one of earlier events
+local warnChainHeal					= mod:NewCastAnnounce(146757, 4)
 ----Master Cannoneer Dragryn
 local warnMuzzleSpray				= mod:NewSpellAnnounce(147824, 3)--Wowhead data: 147825 activates a periodicly trigger (147823) which triggers 147824 as secondary effect
 ----Lieutenant General Krugruk
@@ -29,14 +34,15 @@ local warnCrushersCall				= mod:NewCastAnnounce(146769, 3)
 local warnSkullCracker				= mod:NewCastAnnounce(146848, 4)
 ----Korgra the Snake
 local warnPoisonTippedBlades		= mod:NewStackAnnounce(146902, 2, nil, mod:IsTank())
-----Foot Soldiers
-------Nothing in EJ so can't drycode this
 --Phase 3: Galakras,The Last of His Progeny
 local warnFlamesofGalakrondTarget	= mod:NewTargetAnnounce(147068, 4)
 
---Stage 1: Clear the Landing
-----Nothing in EJ so can't drycode this
---Stage 2: Bring Her Down!
+--Stage 1: Clear the Landing/Bring Her Down!
+local specWarnWarBanner				= mod:NewSpecialWarningSwitch(147328, not mod:IsHealer())
+local specWarnFractureYou			= mod:NewSpecialWarningYou(146899)
+local yellFracture					= mod:NewYell(146899)
+local specWarnFracture				= mod:NewSpecialWarningTarget(146899, mod:IsHealer())
+local specWarnChainheal				= mod:NewSpecialWarningInterrupt(146757)
 ----Master Cannoneer Dragryn
 local specWarnMuzzleSpray			= mod:NewSpecialWarningSpell(147824, nil, nil, nil, 2)
 ----Lieutenant General Krugruk
@@ -46,16 +52,14 @@ local specWarnCrushersCall			= mod:NewSpecialWarningSpell(146769, false, nil, ni
 local specWarnSkullCracker			= mod:NewSpecialWarningRun(146848, nil, nil, nil, 3)--TODO, only warn the ranged who were gripped in by crushers call, instead of all of them
 ----Korgra the Snake
 local specWarnPoisonCloud			= mod:NewSpecialWarningMove(147705)
-----Foot Soldiers
-------Nothing in EJ so can't drycode this
 --Phase 3: Galakras,The Last of His Progeny
 local specWarnFlamesofGalakrond		= mod:NewSpecialWarningSpell(147029, nil, nil, nil, 2)
 local specWarnFlamesofGalakrondYou	= mod:NewSpecialWarningYou(147068)
 local yellFlamesofGalakrond			= mod:NewYell(147068)
 
---Stage 1: Clear the Landing
+--Stage 1: Clear the Landing/Bring Her Down!
 ----Nothing in EJ so can't drycode this
---Stage 2: Bring Her Down!
+local timerDemolisherCD				= mod:NewTimer(20, "timerDemolisherCD", 116040)--EJ is just not complete yet, shouldn't need localizing
 ----Master Cannoneer Dragryn
 
 ----Lieutenant General Krugruk
@@ -64,8 +68,6 @@ local yellFlamesofGalakrond			= mod:NewYell(147068)
 
 ----Korgra the Snake
 local timerPoisontippedBlades		= mod:NewTargetTimer(15, 146902, nil, mod:IsTank() or mod:IsHealer())
-----Foot Soldiers
-------Nothing in EJ so can't drycode this
 --Phase 3: Galakras,The Last of His Progeny
 local timerFlamesofGalakrond		= mod:NewTargetTimer(5, 147068)
 --local timerFlamesofGalakrondCD		= mod:NewCDTimer(50, 147029)
@@ -118,6 +120,12 @@ function mod:SPELL_CAST_START(args)
 	elseif args.spellId == 146848 then
 		warnSkullCracker:Show()
 		specWarnSkullCracker:Show()
+	elseif args.spellId == 146757 then
+		local source = args.sourceName
+		warnChainHeal:Show()
+		if source == UnitName("target") or source == UnitName("focus") then 
+			specWarnChainheal:Show(source)
+		end
 	end
 end
 
@@ -148,6 +156,17 @@ function mod:SPELL_AURA_APPLIED(args)
 		if self.Options.FixateIcon then
 			self:SetIcon(args.destName, 8)
 		end
+	elseif args.spellId == 147328 then
+		warnWarBanner:Show()
+		specWarnWarBanner:Show()
+	elseif args.spellId == 146899 then
+		warnFracture:Show(args.destName)
+		if args:IsPlayer() then
+			specWarnFractureYou:Show()
+			yellFracture:Yell()
+		else
+			specWarnFracture:Show(args.destName)
+		end
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
@@ -170,18 +189,17 @@ function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, destName, _, _, spellId
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
---[[
-function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, _, _, _, target)
-	if msg:find("spell:137175") then
-		local target = DBM:GetFullNameByShortName(target)
-		warnThrow:Show(target)
-		timerStormCD:Start()
-		self:Schedule(55.5, checkWaterStorm)--check before 5 sec.
-		if target == UnitName("player") then
-			specWarnThrow:Show()
-		else
-			specWarnThrowOther:Show(target)
-		end
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 72249 then--Main Boss ID
+		DBM:EndCombat(self)
 	end
 end
---]]
+
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, _, _, _, target)
+	if msg:find("spell:116040") then--Not a mistake, it emotes epiccenter (maybe a bug?)
+		
+	elseif msg == L.tower or msg:find(L.tower) then
+		timerDemolisherCD:Start()
+	end
+end
