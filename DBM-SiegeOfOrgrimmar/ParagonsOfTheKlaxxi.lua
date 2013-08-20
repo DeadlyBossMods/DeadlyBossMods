972ef98f2679039efeb13bf8a022abb6108fa900
Adam <MysticalOS@users.noreply.github.com>
2013-08-20 22:48:36 +0000

    add mod:SetBossHPInfoToHighest() to paragons

--- DBM-SiegeOfOrgrimmar/ParagonsOfTheKlaxxi.lua
+++ DBM-SiegeOfOrgrimmar/ParagonsOfTheKlaxxi.lua
@@ -5,6 +5,7 @@ mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(71152, 71153, 71154, 71155, 71156, 71157, 71158, 71160, 71161)
 mod:SetZone()
 mod:SetUsedIcons(1)
+mod:SetBossHPInfoToHighest()
 
 mod:RegisterCombat("combat")
 
