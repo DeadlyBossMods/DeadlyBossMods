09072feb0965e566c54935c3589b0d02e6b06753
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-08-20 01:25:09 +0000

    more world boss tweaks

--- trunk/DBM-Pandaria/ChiJi.lua
+++ trunk/DBM-Pandaria/ChiJi.lua
@@ -20,23 +20,23 @@ mod:RegisterEvents(
 
 local warnInspiringSong			= mod:NewSpellAnnounce(144468, 3)
 local warnBeaconOfHope			= mod:NewTargetAnnounce(144473, 1)
+local warnFirestorm				= mod:NewSpellAnnounce(144461, 3)
 local warnBlazingSong			= mod:NewSpellAnnounce(144471, 4)
 local warnCraneRush				= mod:NewSpellAnnounce(144470, 3)--Health based, 66% and 33% (maybe register UNIT_HEALTH and give soon warning?)
 
 local specWarnInspiringSong		= mod:NewSpecialWarningInterrupt(144468)
 local specWarnBeaconOfHope		= mod:NewSpecialWarningSpell(144473)
+local specWarnFirestorm			= mod:NewSpecialWarningSpell(144461, nil, nil, nil, 2)
 local specWarnBlazingSong		= mod:NewSpecialWarningSpell(144471, nil, nil, nil, 3)
 local specWarnCraneRush			= mod:NewSpecialWarningSpell(144470, nil, nil, nil, 2)
 
 local timerInspiringSongCD		= mod:NewCDTimer(30, 144468)--30-50sec variation?
-local timerCraneRushCD			= mod:NewCDTimer(49, 144470)--Needs verification
 
 local yellTriggered = false
 
 function mod:OnCombatStart(delay)
 	if yellTriggered then--We know for sure this is an actual pull and not diving into in progress
 		timerInspiringSongCD:Start(20-delay)
-		timerCraneRushCD:Start(37-delay)
 	end
 end
 
@@ -55,10 +55,12 @@ function mod:SPELL_CAST_START(args)
 	elseif args.spellId == 144470 then
 		warnCraneRush:Show()
 		specWarnCraneRush:Show()
-		timerCraneRushCD:Start()
 	elseif args.spellId == 144473 then
-		warnBeaconOfHope:Show(args.destName)
+		warnBeaconOfHope:Show()
 		specWarnBeaconOfHope:Show()
+	elseif args.spellId == 144461 then
+		warnFirestorm:Show()
+		specWarnFirestorm:Show()
 	end
 end
 
