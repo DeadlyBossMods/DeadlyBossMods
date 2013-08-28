b05fdac607d2c9dc254bb6a13eb34232583589db
Adam <MysticalOS@users.noreply.github.com>
2013-08-28 19:52:35 +0000

    Several SoO updates from last few tests and flex testing over the weekend.

--- DBM-SiegeOfOrgrimmar/GarroshHellscream.lua
+++ DBM-SiegeOfOrgrimmar/GarroshHellscream.lua
@@ -20,7 +20,7 @@ mod:RegisterEventsInCombat(
 )
 
 --Stage 1: The True Horde
-local warnDesecrate					= mod:NewSpellAnnounce(144748, 3)
+local warnDesecrate					= mod:NewTargetAnnounce(144748, 3)
 local warnHellscreamsWarsong		= mod:NewSpellAnnounce(144821, 3)
 local warnFireUnstableIronStar		= mod:NewSpellAnnounce(147047, 3)
 --local warnKorkronWarbringer		= mod:NewSpellAnnounce("ej8292", 3)--unlike shaman which very conviniently cast 144585 the instant they join the fight, warbringers are going to need additional work
@@ -42,6 +42,7 @@ local warnEmpGrippingDespair		= mod:NewStackAnnounce(145195, 3, nil, mod:IsTank(
 
 --Stage 1: The True Horde
 local specWarnDesecrate				= mod:NewSpecialWarningSpell(144748, nil, nil, nil, 2)
+local yellDesecrate					= mod:NewYell(144748)
 local specWarnHellscreamsWarsong	= mod:NewSpecialWarningSpell(144821, mod:IsTank() or mod:IsHealer())
 local specWarnFireUnstableIronStar	= mod:NewSpecialWarningSpell(147047, nil, nil, nil, 3)
 --local specWarnKorkronWarbringer	= mod:NewSpecialWarningSwitch("ej8292", not mod:IsHealer())
@@ -66,11 +67,11 @@ local timerFarseerWolfRiderCD		= mod:NewNextTimer(50, "ej8294", nil, nil, nil, 1
 local timerSiegeEngineerCD			= mod:NewNextTimer(40, "ej8298", nil, nil, nil, 144616)
 local timerPowerIronStar			= mod:NewCastTimer(15, 144616)
 --Intermission: Realm of Y'Shaarj
-local timerYShaarjsProtection		= mod:NewCastTimer(60, 144985)
+local timerYShaarjsProtection		= mod:NewCastTimer(60, 144945)
 --Stage Two: Power of Y'Shaarj
 local timerWhirlingCorruptionCD		= mod:NewCDTimer(40, 144985)--One bar for both, "empowered" makes timer too long. CD not yet known except for first
 local timerWhirlingCorruption		= mod:NewBuffActiveTimer(9, 144985)
-local timerTouchOfYShaarjCD			= mod:NewCDTimer(40, 145071)--^^ CD not yet known except for first
+local timerTouchOfYShaarjCD			= mod:NewCDTimer(45, 145071)
 local timerGrippingDespairCD		= mod:NewCDTimer(8.5, 145183)
 local timerGrippingDespair			= mod:NewTargetTimer(15, 145183, nil, mod:IsTank())
 
@@ -82,9 +83,10 @@ local touchOfYShaarjTargets = {}
 local firstIronStar = false
 local engineerDied = 0
 local phase = 1
+local UnitExists = UnitExists
 
-local function warnTouchOfYShaarjTargets(empowered)
-	if empowered then
+local function warnTouchOfYShaarjTargets(spellId)
+	if spellId == 145171 then
 		warnEmpTouchOfYShaarj:Show(table.concat(touchOfYShaarjTargets, "<, >"))
 	else
 		warnTouchOfYShaarj:Show(table.concat(touchOfYShaarjTargets, "<, >"))
@@ -92,12 +94,17 @@ local function warnTouchOfYShaarjTargets(empowered)
 	table.wipe(touchOfYShaarjTargets)
 end
 
-function mod:DesecrateTarget(targetname, uId)
-	if not targetname then
-		print("DBM DEBUG: DesecrateTarget Scan failed")
-		return
+--Another pre target scan (ie targets player BEFORE cast like iron qon)
+local function DesecrateScan()
+	if UnitExists("boss1target") and not self:IsTanking(uId, "boss1") then--Boss 1 is looking at someone that isn't his highest threat or a tank (have to filter tanks cause he looks at them to cast impale, have to filter his highest threat in case it's not a tank, ie a healer)
+		mod:Unschedule(DesecrateScan)
+		local targetname = DBM:GetUnitFullName("boss1target")
+		warnDesecrate:Show(targetname)
+		if UnitIsUnit("boss1target", "player") then--you are target
+			yellDesecrate:Yell()
+		end
 	else
-		print("DBM DEBUG: DesecrateTarget Scan returned: "..targetname)
+		mod:Schedule(0.25, DesecrateScan)
 	end
 end
 
@@ -106,6 +113,7 @@ function mod:OnCombatStart(delay)
 	engineerDied = 0
 	phase = 1
 	table.wipe(touchOfYShaarjTargets)
+	self:Schedule(3, DesecrateScan)
 	timerDesecrateCD:Start(10.5-delay)
 	timerSiegeEngineerCD:Start(20-delay)
 	timerHellscreamsWarsongCD:Start(22-delay)
@@ -146,11 +154,6 @@ function mod:SPELL_CAST_START(args)
 		--timerWhirlingCorruptionCD:Start()
 		--countdownWhirlingCorruption:Start()
 		soundWhirlingCorrpution:Play()
-	elseif args.spellId == 145065 then--Start timer here because it can be blocked if timed really well
-		--"<259.0 20:29:21> [CLEU] SPELL_CAST_START#false#0xF15118B900011259#Garrosh Hellscream#68168#0##nil#-2147483648#-2147483648#145065#Touch of Y'Shaarj#32", -- [41491]
-		--"<259.9 20:29:21> [CLEU] SPELL_CAST_SUCCESS#false#0x01000000001FC86D#Torima#1300#0##nil#-2147483648#-2147483648#31224#Cloak of Shadows#1", -- [41583]
-		--"<260.7 20:29:22> [CLEU] SPELL_MISSED#false#0xF15118B900011259#Garrosh Hellscream#68168#0#0x01000000001FC86D#Torima#1300#0#145065#Touch of Y'Shaarj#32#IMMUNE#nil", -- [41679]
-		--timerTouchOfYShaarjCD:Start()
 	end
 end
 
@@ -165,35 +168,42 @@ function mod:SPELL_CAST_SUCCESS(args)
 			timerDesecrateCD:Start()
 		end
 	elseif args.spellId == 144749 then
-		self:BossTargetScanner(71865, "DesecrateTarget", 0.025, 12)--Forgot to see if this is working
+		self:Unschedule(DesecrateScan)
 		warnEmpDesecrate:Show()
 		specWarnEmpDesecrate:Show()
 		if phase == 2 then
 			timerDesecrateCD:Start(35)
+			self:Schedule(30, DesecrateScan)
 		else
 			timerDesecrateCD:Start()
+			self:Schedule(35, DesecrateScan)
 		end
 	elseif args:IsSpellID(145183, 145195) then--Can miss, so we start timer here
 		timerGrippingDespairCD:Start()
+--[[	elseif args:IsSpellID(145065, 145171) then--Seems no longer in combat log.
+		--"<259.0 20:29:21> [CLEU] SPELL_CAST_START#false#0xF15118B900011259#Garrosh Hellscream#68168#0##nil#-2147483648#-2147483648#145065#Touch of Y'Shaarj#32", -- [41491]
+		--"<259.9 20:29:21> [CLEU] SPELL_CAST_SUCCESS#false#0x01000000001FC86D#Torima#1300#0##nil#-2147483648#-2147483648#31224#Cloak of Shadows#1", -- [41583]
+		--"<260.7 20:29:22> [CLEU] SPELL_MISSED#false#0xF15118B900011259#Garrosh Hellscream#68168#0#0x01000000001FC86D#Torima#1300#0#145065#Touch of Y'Shaarj#32#IMMUNE#nil", -- [41679]
+		timerTouchOfYShaarjCD:Start()--]]
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 144945 then--Intermission (there are earlier events but until the shield goes up phase 1 abilities can still be cast. the phase isn't truely over until this is cast, this is why we don't use the 2-3 second earlier events
 		timerDesecrateCD:Cancel()
+		self:Unschedule(DesecrateScan)
 		timerSiegeEngineerCD:Cancel()
 		timerFarseerWolfRiderCD:Cancel()
 		timerHellscreamsWarsongCD:Cancel()
 		warnYShaarjsProtection:Show(args.destName)
 		timerYShaarjsProtection:Start()
-	elseif args.spellId == 145071 then
+	elseif args:IsSpellID(145065, 145171) then
 		touchOfYShaarjTargets[#touchOfYShaarjTargets + 1] = args.destName
 		self:Unschedule(warnTouchOfYShaarjTargets)
-		self:Schedule(0.5, warnTouchOfYShaarjTargets)
-	elseif args.spellId == 145175 then
-		touchOfYShaarjTargets[#touchOfYShaarjTargets + 1] = args.destName
-		self:Unschedule(warnTouchOfYShaarjTargets)
-		self:Schedule(0.5, warnTouchOfYShaarjTargets, true)
+		self:Schedule(0.5, warnTouchOfYShaarjTargets, args.spellId)
+		timerTouchOfYShaarjCD:Start()
+	elseif args:IsSpellID(145071, 145175) then--Touch of Yshaarj Spread IDs?
+
 	elseif args:IsSpellID(145183, 145195) then
 		local amount = args.amount or 1
 		if args.spellId == 145183 then
@@ -244,6 +254,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 	elseif spellId == 144956 then--Jump To Ground (intermission ending) Used instead of 144945 because 144945 can be removed early during intermission for free damage.
 		phase = 2
 		timerGrippingDespairCD:Start(5.5)
+		self:Schedule(5, DesecrateScan)
 		timerDesecrateCD:Start(10)
 		timerTouchOfYShaarjCD:Start(15)
 		timerWhirlingCorruptionCD:Start(30)
