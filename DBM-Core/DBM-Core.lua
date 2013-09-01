65400c03afba8f86114ddc1a6aeba274420b6279
Adam <MysticalOS@users.noreply.github.com>
2013-09-01 03:14:52 +0000

    more improvements to that

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -3938,10 +3938,13 @@ function DBM:FindInstanceIDs()
 	end
 end
 
-function DBM:FindEncounterIDs(instanceID)
+function DBM:FindEncounterIDs(instanceID, diff)
 	if not instanceID then
 		self:AddMsg("Error: Function requires instanceID be provided")
 	end
+	local _, instanceType, difficultyID = GetInstanceInfo()
+	if difficultyID == 0 then difficultyID = 6 end--EJ in 5.4 considers world bosses as 25man normal
+	EJ_SetDifficulty(diff or difficultyID)--Make sure it's set to right difficulty or it'll ignore mobs (ie ra-den if it's not set to heroic). Use user specified one as primary, with curernt zone difficulty as fallback
 	for i=1, 25 do
 		local name, _, encounterID = EJ_GetEncounterInfoByIndex(i, instanceID)
 		if name then
