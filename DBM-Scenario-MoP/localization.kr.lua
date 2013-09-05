3fd5bf2052300aa2a671ebe0fb0c2c5668d2db30
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-09-05 06:51:36 +0000

    # koKR update

--- DBM-Scenario-MoP/localization.kr.lua
+++ DBM-Scenario-MoP/localization.kr.lua
@@ -7,17 +7,17 @@ local L
 L= DBM:GetModLocalization("d517")
 
 L:SetTimerLocalization{
-	timerEvent			= "양조 완료(추정시간)"
+	timerEvent			= "양조 완료 (추정)"
 }
 
 L:SetOptionLocalization{
-	timerEvent			= "양조 완료 시간 바 보기(추정)"
+	timerEvent			= "양조 완료 바 보기(추정)"
 }
 
 L:SetMiscLocalization{
-	BrewStart			= "The storm is starting! Get ready.",
-	BorokhulaPull		= "Last call, you fork-tongued dip-slithers!",
-	BorokhulaAdds		= "calls out for reinforcements!"--In case useful/important on heroic. On normal just zerg boss and ignore these unless you want achievement.
+	BrewStart			= "폭풍이 몰려오고 있어요! 준비하세요.",
+	BorokhulaPull		= "마지막 기회다, 이 갈래 혀 꿈틀이들아!",
+	BorokhulaAdds		= "원군을 부릅니다!"--In case useful/important on heroic. On normal just zerg boss and ignore these unless you want achievement.
 }
 
 -----------------------
@@ -26,7 +26,7 @@ L:SetMiscLocalization{
 L= DBM:GetModLocalization("d589")
 
 L:SetMiscLocalization{
-	ScargashPull		= "Your Alliance is WEAK!"--Not yet in use but could be with more logs and combat start timers
+	ScargashPull		= "너희 얼라이언스는 약해 빠졌어!"--Not yet in use but could be with more logs and combat start timers
 }
 
 ---------------------------
@@ -40,7 +40,7 @@ L= DBM:GetModLocalization("d511")
 L= DBM:GetModLocalization("d593")
 
 L:SetMiscLocalization{
-	TelvrakPull			= "Zan'vess will never fall!"
+	TelvrakPull			= "잔베스는 함락되지 않는다!"
 }
 
 ------------------------------
@@ -59,18 +59,18 @@ L= DBM:GetModLocalization("d646")
 L= DBM:GetModLocalization("d539")
 
 L:SetTimerLocalization{
-	timerBossCD		= "다음 %s"
+	timerBossCD		= "%s"
 }
 
 L:SetOptionLocalization{
-	timerBossCD		= "다음 우두머리 소환 바 보기"
+	timerBossCD		= "우두머리 등장 바 보기"
 }
 
 L:SetMiscLocalization{
-	RatEngage	= "It's the Den Mother! Look out",
-	BeginAttack	= "We must defend the villagers!",
-	Yeti		= "Bataari War Yeti",
-	Qobi		= "Warbringer Qobi"
+	RatEngage	= "무리 어미예요! 조심하세요!",
+	BeginAttack	= "주민들을 보호해야 하네!",
+	Yeti		= "바타리 전투 설인",
+	Qobi		= "전쟁인도자 코비"
 }
 
 ------------------------------
@@ -88,11 +88,11 @@ L:SetTimerLocalization{
 }
 
 L:SetOptionLocalization{
-	timerAddsCD		= "도마뱀군주의 추가 병력 가능 바 보기"
+	timerAddsCD		= "도마뱀군주 추가 병력 대기시간 바 보기"
 }
 
 L:SetMiscLocalization{
-	LizardLord		= "Dem Saurok be guardin de cave.  Let's take care of 'em."
+	LizardLord		= "사우록이 동굴을 지키고 있네. 처치하세."
 }
 
 ----------------------------
@@ -127,8 +127,8 @@ L:SetOptionLocalization{
 L= DBM:GetModLocalization("d649")
 
 L:SetMiscLocalization{
-	XorenthPull		= "All lesser races are enemies of the true Horde!",
-	ElagloPull		= "Fools! The true horde cannot be stopped by the likes of you."
+	XorenthPull		= "하등한 종족들은 모두 진정한 호드의 적이다!",
+	ElagloPull		= "어리석구나! 너희 따위가 진정한 호드를 막아낼 순 없다!"
 }
 
 ----------------------
