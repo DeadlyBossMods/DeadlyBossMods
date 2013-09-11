b83b788b563c511dc9fd0b108eb2b4ad3a9381a7
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-09-11 18:52:36 +0000

    adjust projection to be higher emphesis

--- trunk/DBM-SiegeOfOrgrimmar/ShaofPride.lua
+++ trunk/DBM-SiegeOfOrgrimmar/ShaofPride.lua
@@ -50,7 +50,7 @@ local yellCorruptedPrison		= mod:NewYell(144574, nil, false)
 --Pride
 local specWarnBurstingPride		= mod:NewSpecialWarningMove(144911)--25-49 Energy
 local yellBurstingPride			= mod:NewYell(144911)
-local specWarnProjection		= mod:NewSpecialWarningYou(146822)--50-74 Energy
+local specWarnProjection		= mod:NewSpecialWarningYou(146822, nil, nil, nil, 3)--50-74 Energy
 local specWarnAuraOfPride		= mod:NewSpecialWarningYou(146817)--75-99 Energy
 local yellAuraOfPride			= mod:NewYell(146817)
 local specWarnOvercome			= mod:NewSpecialWarningYou(144843, nil, nil, nil, 3)--100 EnergyHonestly, i have a feeling your best option if this happens is to find a way to kill yourself!
