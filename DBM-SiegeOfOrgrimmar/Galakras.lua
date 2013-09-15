48ee99de04cdddc7af0295680f57191c4108e38a
Adam <MysticalOS@users.noreply.github.com>
2013-09-15 18:09:48 +0000

    Add other 2 alliance npcs to Galakras, IEEU doesn't always fire for varian first, sometimes there it fires for jaina or ver and there is a delay on varian that causes delayed pull detection. Probably same problem you saw with horde bluewiz. Probably just need other horde npcs too.

--- DBM-SiegeOfOrgrimmar/Galakras.lua
+++ DBM-SiegeOfOrgrimmar/Galakras.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(868, "DBM-SiegeOfOrgrimmar", nil, 369)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(72311, 72560, 72249)--Boss needs to engage off Varian/Lor'themar, not the boss. I include the boss too so we don't detect a win off losing varian. :)
+mod:SetCreatureID(72311, 72560, 72249, 73910, 72302)--Boss needs to engage off friendly NCPS, not the boss. I include the boss too so we don't detect a win off losing varian. :)
 mod:SetReCombatTime(120)--fix combat re-starts after killed. Same issue as tsulong. Fires TONS of IEEU for like 1-2 minutes after fight ends.
 mod:SetMainBossID(72249)
 mod:SetZone()
