3e730679deaba17b0847af61c69dc45bd3fcc04f
ebmor <ebmor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-09-19 16:38:23 +0000

    Nazgrim/Garrosh: SetUsedIcons; deDE update

--- DBM-Pandaria/localization.de.lua
+++ DBM-Pandaria/localization.de.lua
@@ -105,3 +105,7 @@ L:SetMiscLocalization({
 -- Ordos, Fire-God of the Yaungol --
 ------------------------------------
 L= DBM:GetModLocalization(861)
+
+L:SetMiscLocalization({
+	Pull					= "Ihr werdet meinen Platz in den ewigen Flammen einnehmen."
+})
