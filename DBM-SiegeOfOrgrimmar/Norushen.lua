da09d992dc85fad0830f853df616c358f2159f04
Adam <MysticalOS@users.noreply.github.com>
2013-09-19 05:56:17 +0000

    I'm not smart enough to make the sender filtering work, so just use time based filtering instead.

--- DBM-SiegeOfOrgrimmar/Norushen.lua
+++ DBM-SiegeOfOrgrimmar/Norushen.lua
@@ -208,6 +208,8 @@ function mod:OnSync(msg)
 		unleashedAngerCast = 0
 	elseif msg == "prepull" then
 		timerCombatStarts:Start()
+	elseif msg == "ManifestationDied" and not playerInside and self:AntiSpam(1) then
+		specWarnManifestationSoon:Show()
 	end
 end
 
@@ -215,13 +217,7 @@ function mod:CHAT_MSG_ADDON(prefix, message, channel, sender)
 	--Because core already registers BigWigs prefix with server, shouldn't need it here
 	if prefix == "BigWigs" and message then
 		local bwPrefix, bwMsg = message:match("^(%u-):(.+)")
-		if bwMsg == "InsideBigAddDeath" and not playerInside and self:AntiSpam(5, sender) then
-			specWarnManifestationSoon:Show()
-		end
-	--Filter the DBm message here vs OnSync because OnSync pre handles sender and it's not as easy to filter as this
-	elseif prefix == "D4" and message then
-		local dbmPrefix, dbmMsg = strsplit("\t", message)
-		if dbmMsg == "ManifestationDied" and not playerInside and self:AntiSpam(5, sender) then
+		if bwMsg == "InsideBigAddDeath" and not playerInside and self:AntiSpam(1) then
 			specWarnManifestationSoon:Show()
 		end
 	end
