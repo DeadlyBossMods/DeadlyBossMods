673190af8622245df70518ca3a29cfd666ef9c17
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-09-20 01:19:13 +0000

    # Well, I reviewed all syncing stuff including old mods. I conclude that not needed 2.5 sec interval because we do not use sync in frequently used spell (to prevent sync spam and wired warning). Even we need syncing in frequently used spell, we can use syncing with args. Syncing with different args not affects on spam protection. And currently, there is no mod that syncing interval lower than 10 sec. So 10s change only prevents very laggy player sync only. Not affects on LaserTarget syncing in Iron Juggernaut. (this sync including args, spamId protections do not affects with different args)

--- DBM-Pandaria/localization.kr.lua
+++ DBM-Pandaria/localization.kr.lua
@@ -105,3 +105,7 @@ L:SetMiscLocalization({
 -- Ordos, Fire-God of the Yaungol --
 ------------------------------------
 L= DBM:GetModLocalization(861)
+
+L:SetMiscLocalization({
+	Pull					= "You will take my place on the eternal brazier."
+})
