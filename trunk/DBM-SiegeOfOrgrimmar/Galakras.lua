0c0757d233e5f64d449f8dd1ce31172079df485c
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-09-26 04:51:50 +0000

    raise galakras repull to 3 minutes, since apparently the dumb pug persists longer than 2

--- trunk/DBM-SiegeOfOrgrimmar/Galakras.lua
+++ trunk/DBM-SiegeOfOrgrimmar/Galakras.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(72311, 72560, 72249, 73910, 72302, 72561, 73909)--Boss needs to engage off friendly NCPS, not the boss. I include the boss too so we don't detect a win off losing varian. :)
-mod:SetReCombatTime(120, 15)--fix combat re-starts after killed. Same issue as tsulong. Fires TONS of IEEU for like 1-2 minutes after fight ends.
+mod:SetReCombatTime(180, 15)--fix combat re-starts after killed. Same issue as tsulong. Fires TONS of IEEU for like 1-2 minutes after fight ends.
 mod:SetMainBossID(72249)
 mod:SetZone()
 mod:SetUsedIcons(8)
