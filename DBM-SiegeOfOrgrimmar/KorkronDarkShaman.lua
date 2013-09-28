3a63665aeebe242847a8b4ad374c075f93523cee
Adam <MysticalOS@users.noreply.github.com>
2013-09-28 15:01:36 +0000

    Oops, fix a stupid

--- DBM-SiegeOfOrgrimmar/KorkronDarkShaman.lua
+++ DBM-SiegeOfOrgrimmar/KorkronDarkShaman.lua
@@ -71,7 +71,7 @@ local timerIronPrisonCD				= mod:NewCDTimer(31.5, 144330)--Pretty much a next ti
 local timerIronPrisonSelf			= mod:NewBuffFadesTimer(60, 144330)
 
 local countdownFoulGeyser			= mod:NewCountdown(32.5, 143990, mod:IsTank() or mod:IsRangedDps(), nil, nil, nil, nil, 2)
-local countdownFallingAsh			= mod:NewCountdown(15, 143973)
+local countdownFallingAsh			= mod:NewCountdown(15, 143973, nil, nil, nil, nil, true)
 
 local berserkTimer					= mod:NewBerserkTimer(540)
 
