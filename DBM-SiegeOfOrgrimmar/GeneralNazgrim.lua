b53db890b638b3b445a22092e26cb71de4575f63
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-09-29 06:04:07 +0000

    # Nazgrim : Fix attack warning bug.

--- DBM-SiegeOfOrgrimmar/GeneralNazgrim.lua
+++ DBM-SiegeOfOrgrimmar/GeneralNazgrim.lua
@@ -393,7 +393,7 @@ function mod:SPELL_DAMAGE(sourceGUID, _, _, _, destGUID, _, _, _, spellId)
 	if spellId == 143873 and destGUID == UnitGUID("player") and self:AntiSpam(3, 2) then
 		specWarnRavagerMove:Show()
 	elseif (sourceGUID == UnitGUID("player") or sourceGUID == UnitGUID("pet")) and destGUID == UnitGUID("boss1") and self:AntiSpam(3, 1) then--If you've been in LFR at all, you'll see that even 3 is generous. 8 is WAY too leaniant.
-		if not UnitDebuff("player", sunder) then
+		if not UnitDebuff("player", sunder) and defensiveActive then
 			specWarnDefensiveStanceAttack:Show()
 		end
 	end
@@ -403,7 +403,7 @@ mod.SWING_DAMAGE = mod.SPELL_DAMAGE
 
 function mod:SPELL_PERIODIC_DAMAGE(sourceGUID, _, _, _, destGUID, _, _, _, spellId)--Prevent spam on DoT
 	if (sourceGUID == UnitGUID("player") or sourceGUID == UnitGUID("pet")) and destGUID == UnitGUID("boss1") and self:AntiSpam(3, 1) then
-		if not UnitDebuff("player", sunder) and not dotWarned[spellId] then
+		if not UnitDebuff("player", sunder) and defensiveActive and not dotWarned[spellId] then
 			dotWarned[spellId] = true
 			specWarnDefensiveStanceAttack:Show()
 		end
