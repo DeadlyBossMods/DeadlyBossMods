454afea97a3b3a52d8240050d3f12875a36d3043
Adam <MysticalOS@users.noreply.github.com>
2013-09-29 06:09:51 +0000

    Also, prevent lua error in pandaria mods loading because L.Victory doesn't exist (1x DBM-Core\DBM-Core.lua:6371: table index is nil)

--- DBM-Pandaria/localization.en.lua
+++ DBM-Pandaria/localization.en.lua
@@ -88,7 +88,7 @@ L= DBM:GetModLocalization(859)
 
 L:SetMiscLocalization({
 	Pull					= "We shall see.",
---	Victory					= "",
+	Victory					= "SOME RANDOM BLAH BLAH BLAH",
 	VictoryDem				= "Rakkas shi alar re pathrebosh il zila rethule kiel shi shi belaros rikk kanrethad adare revos shi xi thorje Rukadare zila te lok zekul melar "--Cover all bases and all
 })
 
