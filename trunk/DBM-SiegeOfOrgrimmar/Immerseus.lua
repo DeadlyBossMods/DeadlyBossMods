fd0c0a254cb510e0f990295b832a8dbe1b305ec1
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-10-05 01:55:33 +0000

    # Immerseus : Prevent false wipe. / Fix timer not freshing when residue stacking.

--- trunk/DBM-SiegeOfOrgrimmar/Immerseus.lua
+++ trunk/DBM-SiegeOfOrgrimmar/Immerseus.lua
@@ -4,6 +4,7 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(71543)--Doesn't die, will need kill detection
 mod:SetReCombatTime(45)--Lets just assume he has same bug as tsulong in advance and avoid problems
+mod:SetWipeTime(20)--Kill yell fires litte lately, prevent wipe.
 mod:SetZone()
 
 mod:RegisterCombat("combat")
@@ -11,6 +12,7 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START",
 	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_APPLIED_DOSE",
 	"SPELL_AURA_REMOVED",
 	"SPELL_PERIODIC_DAMAGE",
 	"SPELL_PERIODIC_MISSED",
@@ -85,6 +87,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		specWarnSwellingCorruptionTarget:Show(args.destName)
 	end
 end
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	if args.spellId == 143459 and args:IsPlayer() then
