894583ed6d9811c133e4ea96fd4614ab5957da70
ebmor <ebmor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-10-11 03:40:58 +0000

    deDE update

--- DBM-WorldEvents/localization.de.lua
+++ DBM-WorldEvents/localization.de.lua
@@ -170,6 +170,20 @@ L:SetGeneralLocalization({
 	name = "Dunkelmond-Kaninchen"
 })
 
+-------------------------
+--  Darkmoon Moonfang  --
+-------------------------
+L = DBM:GetModLocalization("Moonfang")
+
+L:SetGeneralLocalization({
+	name = "Mondfang"
+})
+
+L:SetWarningLocalization({
+	specWarnCallPack		= "Rudelruf - Lauf mehr als 40 Meter von Mondfang weg!",
+	specWarnMoonfangCurse	= "Mondfangs Fluch - Lauf mehr als 10 Meter von Mondfang weg!"
+})
+
 --------------------------
 --  Plants Vs. Zombies  --
 --------------------------
