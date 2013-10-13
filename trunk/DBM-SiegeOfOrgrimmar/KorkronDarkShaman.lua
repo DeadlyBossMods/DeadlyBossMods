4c4b54a5fa775c152f534351d0bcd7095cf9c275
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-10-13 01:54:31 +0000

    The back and forth reports of berserk being 9 min or 10 min are both wrong, it's in between that.

--- trunk/DBM-SiegeOfOrgrimmar/KorkronDarkShaman.lua
+++ trunk/DBM-SiegeOfOrgrimmar/KorkronDarkShaman.lua
@@ -73,7 +73,7 @@ local timerIronPrisonSelf			= mod:NewBuffFadesTimer(60, 144330)
 local countdownFoulGeyser			= mod:NewCountdown(32.5, 143990, mod:IsTank() or mod:IsRangedDps(), nil, nil, nil, nil, 2)
 local countdownFallingAsh			= mod:NewCountdown(15, 143973, nil, nil, nil, nil, true)
 
-local berserkTimer					= mod:NewBerserkTimer(540)
+local berserkTimer					= mod:NewBerserkTimer(555)
 
 mod:AddRangeFrameOption(4, 143990)--This is more or less for foul geyser and foul stream splash damage
 mod:AddSetIconOption("SetIconOnToxicMists", 144089, false)
