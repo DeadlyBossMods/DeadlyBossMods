ee049a308b322ce264d278b6ef9e390e417e298e
Adam <MysticalOS@users.noreply.github.com>
2013-10-14 07:12:56 +0000

    fix some fail

--- DBM-SiegeOfOrgrimmar/ThokTheBloodthirsty.lua
+++ DBM-SiegeOfOrgrimmar/ThokTheBloodthirsty.lua
@@ -141,13 +141,14 @@ end
 function mod:SPELL_CAST_SUCCESS(args)
 	if args.spellId == 143343 then--Assumed, 2 second channel but "Instant" cast flagged, this generally means SPELL_AURA_APPLIED
 		timerDeafeningScreechCD:Cancel()
-		if screechCount < 7 then--Don't spam special warning once cd is lower than 4.8 seconds.
-			timerDeafeningScreechCD:Start(screechTimers[screechCount], screechCount+1)
-			specWarnDeafeningScreech:Schedule(screechTimers[screechCount]-1.5)
-		end
 		if self:IsDifficulty("lfr25") then
 			timerDeafeningScreechCD:Start(18, screechCount+1)
 			specWarnDeafeningScreech:Show()
+		else
+			if screechCount < 7 then--Don't spam special warning once cd is lower than 4.8 seconds.
+				timerDeafeningScreechCD:Start(screechTimers[screechCount], screechCount+1)
+				specWarnDeafeningScreech:Schedule(screechTimers[screechCount]-1.5)
+			end
 		end
 	elseif args.spellId == 143428 then
 		warnTailLash:Show()
