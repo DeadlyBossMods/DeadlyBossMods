2068b98f03452307df8d7513c3d930ffa01be05f
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-10-18 06:11:24 +0000

    improvements to immuneCaster

--- trunk/DBM-SiegeOfOrgrimmar/ThokTheBloodthirsty.lua
+++ trunk/DBM-SiegeOfOrgrimmar/ThokTheBloodthirsty.lua
@@ -21,16 +21,18 @@ mod:RegisterEventsInCombat(
 
 --People who aren't affected by the bosses screech (so they don't need to track devotion aura up time.
 --Ret Paladins excluded because they do need to track devotion aura to use theirs properly
-local function customMelee()
+local function immuneCaster(includeMeleePal)
 	return class == "ROGUE"
 	or class == "WARRIOR"
 	or class == "DEATHKNIGHT"
+	or class == "HUNTER"
 	or (class == "MONK" and (GetSpecialization() == 1 or GetSpecialization() == 3))
     or (class == "SHAMAN" and (GetSpecialization() == 2))
 	or (class == "DRUID" and (GetSpecialization() == 2 or GetSpecialization() == 3))
+	or (class == "PALADIN" and includeMeleePal and (GetSpecialization() ~= 1))
 end
 
-local warnDevotionAura				= mod:NewTargetAnnounce(31821, 1, nil, not customMelee())
+local warnDevotionAura				= mod:NewTargetAnnounce(31821, 1, nil, not immuneCaster())
 --Stage 1: A Cry in the Darkness
 local warnFearsomeRoar				= mod:NewStackAnnounce(143766, 2, nil, mod:IsTank())--143426
 local warnAcceleration				= mod:NewStackAnnounce(143411, 3)--Staghelm 2.0
@@ -53,11 +55,11 @@ local warnFirePustules				= mod:NewSpellAnnounce(143970, 2)
 local warnScorchingBreath			= mod:NewStackAnnounce(143767, 2, nil, mod:IsTank())
 local warnBurningBlood				= mod:NewTargetAnnounce(143783, 3, nil, false, nil, nil, nil, nil, 2)
 
-local specWarnDevotionAura			= mod:NewSpecialWarningFaded(31821, not customMelee())
+local specWarnDevotionAura			= mod:NewSpecialWarningFaded(31821, not immuneCaster())
 --Stage 1: A Cry in the Darkness
 local specWarnFearsomeRoar			= mod:NewSpecialWarningStack(143766, mod:IsTank(), 2)
 local specWarnFearsomeRoarOther		= mod:NewSpecialWarningTarget(143766, mod:IsTank())
-local specWarnDeafeningScreech		= mod:NewSpecialWarningCast(143343, not customMelee(), nil, nil, 2, 2)
+local specWarnDeafeningScreech		= mod:NewSpecialWarningCast(143343, not immuneCaster(true), nil, nil, 2, 2)
 --Stage 2: Frenzy for Blood!
 local specWarnBloodFrenzy			= mod:NewSpecialWarningSpell(143440, nil, nil, nil, 2)
 local specWarnFixate				= mod:NewSpecialWarningRun(143445, nil, nil, nil, 3)
@@ -79,7 +81,7 @@ local specWarnBurningBloodMove		= mod:NewSpecialWarningMove(143784)
 local yellBurningBlood				= mod:NewYell(143783, nil, false)
 
 
-local timerDevotionAura				= mod:NewBuffActiveTimer(6, 31821, nil, not customMelee())
+local timerDevotionAura				= mod:NewBuffActiveTimer(6, 31821, nil, not immuneCaster())
 --Stage 1: A Cry in the Darkness
 local timerFearsomeRoar				= mod:NewTargetTimer(30, 143766, nil, mod:IsTank() or mod:IsHealer())
 local timerFearsomeRoarCD			= mod:NewCDTimer(11, 143766, nil, mod:IsTank())
