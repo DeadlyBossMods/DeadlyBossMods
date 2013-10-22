ce291e8d48859a605290af531875ed18ee52ff63
alcortes <alcortes@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-10-22 17:08:27 +0000

    Spanish update

--- DBM-Scenario-MoP/localization.es.lua
+++ DBM-Scenario-MoP/localization.es.lua
@@ -1,12 +1,96 @@
 ﻿if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
 local L
 
-----------------------
--- Theramore's Fall --
-----------------------
-L= DBM:GetModLocalization("d566")
+---------------------
+-- A Brewing Storm --
+---------------------
+L= DBM:GetModLocalization("d517")
+
+-----------------------
+-- A Little Patience --
+-----------------------
+L= DBM:GetModLocalization("d589")
 
 ---------------------------
 -- Arena Of Annihilation --
 ---------------------------
 L= DBM:GetModLocalization("d511")
+
+-------------------------
+-- Assault of Zan'vess --
+-------------------------
+L= DBM:GetModLocalization("d593")
+
+------------------------------
+-- Battle on the High Seas ---
+------------------------------
+L= DBM:GetModLocalization("d652")
+
+-----------------------
+-- Blood in the Snow --
+-----------------------
+L= DBM:GetModLocalization("d646")
+
+-----------------------
+-- Brewmoon Festival --
+-----------------------
+L= DBM:GetModLocalization("d539")
+
+------------------------------
+-- Crypt of Forgotten Kings --
+------------------------------
+L= DBM:GetModLocalization("d504")
+
+-----------------------
+-- Dagger in the Dark --
+-----------------------
+L= DBM:GetModLocalization("d616")
+
+----------------------------
+-- Dark Heart of Pandaria --
+----------------------------
+L= DBM:GetModLocalization("d647")
+
+L:SetMiscLocalization{
+	summonElemental		= "Minions, destroy these insects!" --TODO, needs translation
+}
+
+------------------------
+-- Greenstone Village --
+------------------------
+L= DBM:GetModLocalization("d492")
+
+--------------
+-- Landfall --
+--------------
+L = DBM:GetModLocalization("Landfall")
+
+----------------------------
+-- The Secret of Ragefire --
+----------------------------
+L= DBM:GetModLocalization("d649")
+
+----------------------
+-- Theramore's Fall --
+----------------------
+L= DBM:GetModLocalization("d566")
+
+--------------------------------
+-- Troves of the Thunder King --
+--------------------------------
+L= DBM:GetModLocalization("d620")
+
+----------------
+-- Unga Ingoo --
+----------------
+L= DBM:GetModLocalization("d499")
+
+------------------------
+-- Warlock Green Fire --
+------------------------
+L= DBM:GetModLocalization("d594")
+
+-------------------------------
+-- Finding Secret Ingredient --
+-------------------------------
+L= DBM:GetModLocalization("d745")
