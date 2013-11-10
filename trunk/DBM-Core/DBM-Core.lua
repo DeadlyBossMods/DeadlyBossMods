5ba9f83342a5eb5f72de8ead1d99642f21cb403f
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-11-10 23:30:58 +0000

    Holy crap, most obvious frigging bug in world. no wonder that doesn't work. I didn't even register the events.

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -270,6 +270,7 @@ local IsInInstance = IsInInstance
 local UnitAffectingCombat = UnitAffectingCombat
 local UnitExists = UnitExists
 local UnitIsDead = UnitIsDead
+local UnitIsFriend = UnitIsFriend
 local GetSpellInfo = GetSpellInfo
 local EJ_GetSectionInfo = EJ_GetSectionInfo
 local GetInstanceInfo = GetInstanceInfo
@@ -4561,7 +4562,6 @@ local bossTargetuIds = {
 	"target", "focus", "boss1", "boss2", "boss3", "boss4", "boss5"
 }
 
--- leave this function for older mods compatiblity.
 function bossModPrototype:GetBossTarget(cid)
 	cid = cid or self.creatureId
 	local name, uid, bossuid
