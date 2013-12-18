4413fd22c10774308c4f96dcd23eb778a12cead0
Adam <MysticalOS@users.noreply.github.com>
2013-12-18 00:54:31 +0000

    Fix plants

--- DBM-WorldEvents/Quests/Plants.lua
+++ DBM-WorldEvents/Quests/Plants.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod("PlantsVsZombies", "DBM-WorldEvents", 3)
+local mod	= DBM:NewMod("PlantsVsZombies", "DBM-WorldEvents", 2)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
