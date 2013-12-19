28a425c7619b9227cd1d82b42cec52d39f657bec
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-12-19 09:01:17 +0000

    # Fix move bar not starts as enlarged. # koKR update.

--- DBM-Core/localization.kr.lua
+++ DBM-Core/localization.kr.lua
@@ -1,26 +1,26 @@
 ﻿if (GetLocale() == "koKR") then
 
-DBM_CORE_NEED_SUPPORT				= "오류/건의사항은 법사세린-아즈샤라(everfinale@gmail.com) 또는 인벤 닉네임 다시날아 로 보내주시기 바랍니다. DBM Team은 프로그래밍 또는 외국어에 능통하신 분의 도움을 기다리고 있습니다. 자세한 사항은 www.deadlybossmods.com 을 방문하시거나 tandanu@deadlybossmods.com 또는 nitram@deadlybossmods.com 으로 메일을 보내주시기 바랍니다."
-DBM_HOW_TO_USE_MOD					= "DBM 사용을 환영합니다. 대화창에서 /dbm help를 입력하면 사용 가능한 명령어 목록을 볼 수 있습니다. 설정을 위해서는 /dbm을 입력후 나타나는 창에서 각 우두머리 세부 설정을 할 수 있습니다. DBM은 처음 설치시 캐릭터 특성에 맞는 적절한 경고를 선택하며, 옵션에서 추가 설정 또한 가능합니다."
+DBM_CORE_NEED_SUPPORT				= "문의/건의사항은 설정 화면에서 사용자 포럼 버튼을 누른 후, DBM 사용자 포럼에 남겨 주시기 바랍니다. DBM Team은 프로그래밍 또는 외국어에 능통하신 분의 도움을 기다리고 있습니다. 자세한 사항은 everfinale@gmail.com 으로 메일을 보내주시기 바랍니다."
+DBM_HOW_TO_USE_MOD					= "DBM 사용을 환영합니다. 대화창에서 /dbm help를 입력하면 사용 가능한 명령어 목록을 볼 수 있습니다. 설정을 위해서는 /dbm을 입력하면 설정창이 나타납니다. 각 경고는 최초 불러오기시 캐릭터 특성에 맞는 적절한 경고를 선택합니다. 선택된 경고는 설정창에서 언제든지 변경이 가능합니다."
 
-DBM_FORUMS_MESSAGE					= "오류나 잘못된 바를 발견하셨나요? 일부 전투에서 추가 알림, 특수 경고 또는 바가 있으면 좋을텐데라고 생각하셨나요?\n새로 개설된 Deadly Boss Mods 토론, 오류 제보 및 요청 게시판을 방문해 보세요. 주소는 |HDBM:forums|h|cff3588ffhttp://www.deadlybossmods.com|r 입니다. (링크를 클릭하신 후 주소를 복사하실 수 있습니다.)"
-DBM_FORUMS_COPY_URL_DIALOG			= "DBM 사용자 포럼에 방문하셔서 여러분의 의견을 남겨주세요.\r\n(서비스 제공 : 와우 인벤)"
+DBM_FORUMS_MESSAGE					= "오류를 발견하셨나요? 일부 전투에서 추가 경고가 있으면 좋을텐데라고 생각하셨나요?\n그렇다면 DBM 사용자 포럼을 방문해 보세요. 주소는 |HDBM:forums|h|cff3588ffhttp://www.inven.co.kr/board/powerbbs.php?come_idx=3716|r 입니다. (클릭후 주소 복사 가능)"
+DBM_FORUMS_COPY_URL_DIALOG			= "문의/건의사항은 DBM 사용자 포럼에 남겨주세요.\r\n(서비스 제공 : 와우 인벤)"
 
 DBM_CORE_LOAD_MOD_ERROR				= "%s 경보를 불러오는 중 오류 발생: %s"
 DBM_CORE_LOAD_MOD_SUCCESS			= "'%s' 경보를 불러왔습니다 !"
-DBM_CORE_LOAD_MOD_COMBAT			= "전투가 종료 될때까지 '%s' 경보를 불러오지 않습니다."
+DBM_CORE_LOAD_MOD_COMBAT			= "오류 방지를 위해 전투가 종료 될때까지 '%s' 경보를 불러오지 않습니다."
 DBM_CORE_LOAD_GUI_ERROR				= "설정창을 불러올 수 없습니다: %s"
 DBM_CORE_LOAD_GUI_COMBAT			= "전투 중에는 설정창을 불러올 수 없습니다. 전투가 종료되면 설정창이 열릴 것입니다. 이후에는 전투 중이라도 설정창을 불러 올 수 있습니다."
 DBM_CORE_BAD_LOAD					= "DBM이 현재 인스턴스의 경고를 완전히 불러오지 못한 것을 발견했습니다. 전투가 종료된 후에, /console reloadui 명령어를 입력하여 경고를 다시 불러오시기 바랍니다."
 
 DBM_CORE_LOOT_SPEC_REMINDER			= "현재 전문화는 %s 입니다. 현재 선택된 전리품 전문화는 %s 입니다."
 
-DBM_CORE_BIGWIGS_ICON_CONFLICT		= "DBM이 BigWigs와 DBM의 전술 목표 아이콘 설정이 모두 활성화 되어 있는 것이 확인되었습니다. 아이콘 충돌을 방지하기 위해 둘 중 하나의 아이콘 설정을 비활성화 해주시기 바랍니다."
+DBM_CORE_BIGWIGS_ICON_CONFLICT		= "DBM이 BigWigs와 DBM의 전술 목표 아이콘 설정이 모두 활성화 되어 있는 것이 확인했습니다. 아이콘 충돌을 방지하기 위해 두 애드온중 하나의 아이콘 설정을 비활성화 해주시기 바랍니다."
 
 DBM_CORE_COMBAT_STARTED				= "%s 작동됨. 행운을 빕니다! :)";
 DBM_CORE_COMBAT_STARTED_IN_PROGRESS	= "%s 전투 진행 도중 참가하였습니다. 행운을 빕니다! :)"
 DBM_CORE_SCENARIO_STARTED			= "%s 시작됨. 행운을 빕니다! :)";
-DBM_CORE_SCENARIO_STARTED_IN_PROGRESS	=	"%s 진행 도중 참가하였습니다. 행운을 빕니다! :)"
+DBM_CORE_SCENARIO_STARTED_IN_PROGRESS	= "%s 진행 도중 참가하였습니다. 행운을 빕니다! :)"
 DBM_CORE_BOSS_DOWN					= "%s 전투에서 승리하였습니다! (전투 시간: %s)"
 DBM_CORE_BOSS_DOWN_I				= "%s 전투에서 승리하였습니다! (이 난이도에서 승리: %d회)"
 DBM_CORE_BOSS_DOWN_L				= "%s 전투에서 승리하였습니다! (전투 시간: %s, 최종 전투 기록: %s, 가장 빨랐던 기록: %s, 이 난이도에서 승리: %d회)"
@@ -78,24 +78,23 @@ DBM_CORE_VERSIONCHECK_ENTRY			= "%s: %s (r%d)"
 DBM_CORE_VERSIONCHECK_ENTRY_TWO		= "%s: %s (r%d) & %s (r%d)"
 DBM_CORE_VERSIONCHECK_ENTRY_NO_DBM	= "%s: 공격대 경보 애드온이 설치되어 있지 않습니다."
 DBM_CORE_VERSIONCHECK_FOOTER		= "DBM 설치 : %d명, BigWigs 설치 : %d명이 확인되었습니다."
-DBM_CORE_YOUR_VERSION_OUTDATED      = "사용중인 DBM 버전이 오래되었습니다! http://wow.inven.co.kr(korean) 또는 http://www.deadlybossmods.com에 접속하셔서 최신 버전을 다운로드 받으세요."
-DBM_CORE_OUTDATED_PVP_MODS			= "설치되어 있는 DBM-PvP 경고가 구버전 입니다. http://www.deadlybossmods.com에 접속하셔서 별도로 배포되는 최신 버전으로 업데이트 하시거나, 사용하지 않을 경우 제거하시기 바랍니다. PvP 경고는 DBM-Core에 더 이상 포함되지 않습니다."
+DBM_CORE_YOUR_VERSION_OUTDATED      = "사용중인 DBM 버전이 오래되었습니다! DBM 사용자 포럼에 오셔서 최신 버전을 다운로드 받으세요."
+DBM_CORE_OUTDATED_PVP_MODS			= "설치되어 있는 DBM-PvP 경고가 구버전 입니다. DBM 사용자 포럼에 오셔서 최신 버전으로 업데이트 하시거나, 애드온 폴더에서 DBM-PvP 폴더를 제거하시기 바랍니다. 이제 PvP 경고는 DBM-Core에 더 이상 포함되지 않습니다."
 --DBM_BIG_WIGS (Same as English locales)
 --DBM_BIG_WIGS_ALPHA (Same as English locales)
 
-DBM_CORE_UPDATEREMINDER_HEADER			= "사용중인 DBM 버전이 오래되었습니다!\n Version %s (r%d) 가 새로운 버전이며, 최신 버전 위치:"
-DBM_CORE_UPDATEREMINDER_HEADER_ALPHA	= "사용중인 DBM alpha 버전이 오래되었습니다.\n 최소 %d 버전 이상 업데이트 가능합니다. 가능한 최신 alpha 버전이나 안정화 버전으로 업데이트 하시기 바랍니다. 구 버전은 잘못되거나 미완성된 기능들이 있을 수 있습니다."
-DBM_CORE_UPDATEREMINDER_FOOTER			= (IsMacClient() and "Cmd-C" or "Ctrl-C").. " 를 누르면 다운로드 주소를 복사하실 수 있습니다."
+DBM_CORE_UPDATEREMINDER_HEADER			= "사용중인 DBM 버전이 오래되었습니다!\n Version %s (r%d) 가 새로운 버전입니다. 포럼 주소:"
+DBM_CORE_UPDATEREMINDER_HEADER_ALPHA	= "사용중인 DBM alpha 버전이 오래되었습니다.\n %d 버전 이상 업데이트 가능합니다. 가능한 빠른 시일내로 최신버전으로 업데이트 하시기 바랍니다. 계속 사용하실 경우 잘못된 경고를 표시할 수도 있습니다."
+DBM_CORE_UPDATEREMINDER_FOOTER			= (IsMacClient() and "Cmd-C" or "Ctrl-C").. " 를 누르면 주소를 복사하실 수 있습니다."
 DBM_CORE_UPDATEREMINDER_FOOTER_GENERIC	= (IsMacClient() and "Cmd-C" or "Ctrl-C").. " 를 누르면 주소를 복사하실 수 있습니다."
 DBM_CORE_UPDATEREMINDER_URL				= "http://www.inven.co.kr/board/powerbbs.php?come_idx=3716"
-DBM_CORE_UPDATEREMINDER_NOTAGAIN		= "새로운 버전이 발견되면 알림창을 보입니다."
-DBM_CORE_UPDATEREMINDER_DISABLE			= "경고: 현재 사용중인 DBM 버전이 지나치게 오래되어 (%d 버전 가량) 업데이트 전까지 작동을 중지합니다. 이것은 오래되거나 호환되지 않는 기능이 작동하여 당신 또는 공격대원에게 잘못된 알림이 되지 않기 위한 조치입니다. 지금 즉시 최신 버전으로 업데이트 하여 주시기 바랍니다."
-DBM_CORE_UPDATEREMINDER_HOTFIX			= "지금 사용하시는 DBM 버전은 이 우두머리 전투에서 잘못된 알림과 바를 표시할 것입니다. 이 오류는 다음 정식 버전에서 수정될 것이며, 최신 Alpha 버전으로 업데이트 하면 즉시 수정할 수 있습니다."
+DBM_CORE_UPDATEREMINDER_DISABLE			= "경고: 현재 사용중인 DBM 버전은 지나치게 오래되어 (%d 버전 가량) 사용하실 수 없습니다. 이것은 당신 및 공격대원에게 잘못된 알림으로 혼동을 주지 않기 위함입니다. 계속 사용하시려면 지금 즉시 최신 버전으로 업데이트하여 주시기 바랍니다."
+DBM_CORE_UPDATEREMINDER_HOTFIX			= "지금 사용하시는 DBM 버전은 이 우두머리 전투에서 잘못된 경고를 표시합니다. 이 오류는 다음 정식 버전에서 수정되오나, 최신 alpha 버전으로 업데이트 하면 즉시 수정할 수 있습니다."
 
 DBM_CORE_MOVABLE_BAR				= "나를 움직이세요~_~"
 
 DBM_PIZZA_SYNC_INFO					= "|Hplayer:%1$s|h[%1$s]|h님이 당신에게 DBM 바 보냄: '%2$s'\n|HDBM:cancel:%2$s:nil|h|cff3588ff[바 취소]|r|h  |HDBM:ignore:%2$s:%1$s|h|cff3588ff[%1$s 바 차단]|r|h"
-DBM_PIZZA_CONFIRM_IGNORE			= "현재 공격대에서 %s님이 보낸 DBM 바를 무시하도록 하겠습니까?"
+DBM_PIZZA_CONFIRM_IGNORE			= "%s님의 DBM 바를 차단하시겠습니까? 이 공격대에 있는 동안에만 적용됩니다."
 DBM_PIZZA_ERROR_USAGE				= "사용법: /dbm [broadcast] timer <시간> <글자>"
 
 --DBM_CORE_MINIMAP_TOOLTIP_HEADER (Same as English locales)
@@ -105,7 +104,7 @@ DBM_CORE_RANGECHECK_HEADER			= "거리 확인 (%dm)"
 DBM_CORE_RANGECHECK_SETRANGE		= "거리 지정"
 DBM_CORE_RANGECHECK_SOUNDS			= "소리"
 DBM_CORE_RANGECHECK_SOUND_OPTION_1	= "지정 거리 이내 사람이 1명 있을 경우 소리"
-DBM_CORE_RANGECHECK_SOUND_OPTION_2	= "지정 거리 이내 사람이 1명 이상 있을 경우 소리"
+DBM_CORE_RANGECHECK_SOUND_OPTION_2	= "지정 거리 이내 사람이 2명 이상 있을 경우 소리"
 DBM_CORE_RANGECHECK_SOUND_0			= "소리 없음"
 DBM_CORE_RANGECHECK_SOUND_1			= "기본 소리"
 DBM_CORE_RANGECHECK_SOUND_2			= "짜증나는 소리(-_-;)"
@@ -122,17 +121,18 @@ DBM_CORE_INFOFRAME_SHOW_SELF		= "내 정보 항상 보기"
 DBM_LFG_INVITE						= "던전 입장"
 
 DBM_CORE_SLASHCMD_HELP				= {
-	"사용 가능한 명령어:",
-	"/dbm version: 공격대원의 DBM 버전 체크(줄임말: ver)",
---	"/dbm version2: 공격대 전체 DBM 버전 체크 + 구버전 귓속말 알림(줄임말: ver2).",
-	"/dbm unlock(move): 바의 위치조정 바를 보임",
-	"/dbm timer <숫자> <글자>: <숫자>초 지속되는 <글자>바를 생성",
-	"/dbm broadcast timer <숫자> <글자>: <숫자>초 지속되는 <글자>바를 공격대원에게 보냄(승급 권한 필요)",
-	"/dbm break <min>: <min>분 지속되는 휴식 바를 보냄. DBM을 설치한 모든 공격대원이 볼 수 있음(승급 권한 필요)",
-	"/dbm pull <sec>: <sec>초 후 전투 예정 바를 보냄. DBM을 설치한 모든 공격대원이 볼 수 있음(승급 권한 필요)",
-	"/dbm arrow: DBM 화살표 보임. 추가 도움말은 /dbm arrow help 를 입력",
-	"/dbm lockout: 현재 공격대원에게 공격대 귀속 정보를 요(출력형식: 던전이름, 던전 ID)(승급 권한 필요)",
-	"/dbm lag: 공격대원의 지연시간을 확인",
+	"명령어 일람:",
+	"/dbm version(ver): 공격대원의 DBM 버전 확인",
+--	"/dbm version2: 공격대 전체 DBM 버전 확인 + 구버전 귓속말 알림(줄임말: ver2).",
+	"/dbm unlock(move): 20초 동안 바 위치 조정 가능 상태가 됨",
+	"* 아래 명령어의 숫자나 글자를 입력할때 < > 는 입력하지 않음 *",
+	"/dbm timer <숫자> <글자>: <글자>에 입력한 대로 <숫자>초 동안 지속되는 바를 생성",
+	"/dbm broadcast timer <숫자> <글자>: <글자>에 입력한 대로 <숫자>초 동안 지속되는 바를 생성 후 모든 공격대원에게 보냄(승급 권한 필요)",
+	"/dbm break <숫자>: <숫자>분 지속되는 휴식 바를 보냄. DBM을 설치한 모든 공격대원이 볼 수 있음(승급 권한 필요)",
+	"/dbm pull <숫자>: <숫자>초 후 전투 예정 바를 보냄. DBM을 설치한 모든 공격대원이 볼 수 있음(승급 권한 필요)",
+	"/dbm arrow: DBM 화살표 도움말을 봄",
+	"/dbm lockout: 현재 공격대원에게 공격대 귀속 정보를 요청 (출력형식: 던전이름, 던전 ID)(승급 권한 필요)",
+	"/dbm lag: 공격대원의 지연시간을 확인함",
 	"/dbm help: 이 도움말을 봄"
 }
 
@@ -146,7 +146,7 @@ DBM_CORE_RIGHT						= "오른쪽"
 DBM_CORE_BACK						= "뒤쪽"--BACK
 DBM_CORE_FRONT						= "앞쪽"
 
-DBM_CORE_BREAK_START				= "쉬는 시간 시작 -- %s 분 남았습니다!"
+DBM_CORE_BREAK_START				= "쉬는 시간 시작 -- %s분 남았습니다!"
 DBM_CORE_BREAK_MIN					= "%s분 후 쉬는 시간이 끝납니다!"
 DBM_CORE_BREAK_SEC					= "%s초 후 쉬는 시간이 끝납니다!"
 DBM_CORE_TIMER_BREAK				= "쉬는 시간"
@@ -273,13 +273,13 @@ DBM_ARROW_MOVABLE						= "화살표 움직이기"
 DBM_ARROW_NO_RAIDGROUP					= "이 기능은 공격대에 속해 있고, 공격대 던전 내부에 있을때만 사용 가능합니다."
 
 DBM_ARROW_ERROR_USAGE	= {
-	"DBM-Arrow 사용법:",
-	"/dbm arrow <x> <y>  특정 좌표(X/Y)에 화살표를 만듭니다.(0 < x/y < 100)",
-	"/dbm arrow <player>  현재 파티 또는 공격대의 특정 대상을 가리키는 화살표를 만듭니다.",
-	"/dbm arrow hide  화살표를 숨깁니다.",
-	"/dbm arrow move  화살표를 움직이도록 합니다.",
-	"/dbm arrow target 대상을 가리키는 화살표를 만듭니다.",
-	"/dbm arrow focus  주시대상을 가리키는 화살표를 만듭니다."
+	"DBM 화살표 사용법:",
+	"/dbm arrow <x> <y>  지정된 좌표(X/Y) 방향으로 화살표를 만듭니다.(0 < x/y < 100)",
+	"/dbm arrow <대상이름>  현재 파티 또는 공격대의 <대상이름> 방향으로 화살표를 만듭니다.",
+	"/dbm arrow target  현재 대상 방향으로 화살표를 만듭니다.",
+	"/dbm arrow focus  현재 주시대상 방향으로 화살표를 만듭니다.",
+	"/dbm arrow move  화살표가 25초 동안 이동가능 상태가 됩니다.",
+	"/dbm arrow hide  화살표를 화면에서 숨깁니다."
 }
 
 DBM_SPEED_KILL_TIMER_TEXT	= "최고 기록"
@@ -287,7 +287,7 @@ DBM_SPEED_KILL_TIMER_OPTION	= "최고 기록을 바로 보기"
 DBM_SPEED_CLEAR_TIMER_TEXT	= "최고 기록"
 
 
-DBM_REQ_INSTANCE_ID_PERMISSION		= "%s 님이 당신의 공격대 귀속 정보를 요청합니다.\n%s 님의 요청을 수락하시겠습니까? 정보 확인이 완료될때 까지 이 메세지가 여러번 보일 수도 있습니다."
+DBM_REQ_INSTANCE_ID_PERMISSION		= "%s 님이 당신의 공격대 귀속 정보를 요청합니다.\n%s 님의 요청을 수락하시겠습니까? 확인이 완료될 때까지 이 메세지가 여러번 보일 수도 있습니다."
 DBM_ERROR_NO_RAID					= "이 기능을 사용하기 위해서는 공격대에 속해 있어야 합니다."
 DBM_INSTANCE_INFO_REQUESTED			= "공격대원의 귀속 정보를 요청합니다.\n모든 공격대원이 요청 메세지에 응답하기 전까지 약간의 시간이 소요됩니다."
 DBM_INSTANCE_INFO_STATUS_UPDATE		= "%d명이 응답했습니다. (총 %d명): %d명이 요청을 수락했으며, %d명은 요청을 거부했습니다. (응답 대기 시간 %d초 남음)"
