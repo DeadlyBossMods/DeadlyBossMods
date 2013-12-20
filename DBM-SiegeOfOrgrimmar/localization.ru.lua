ea2cd1792901a8f728188bfa9e274f9d8eaa7598
Adam <MysticalOS@users.noreply.github.com>
2013-12-20 22:37:05 +0000

    Ru update

--- DBM-SiegeOfOrgrimmar/localization.ru.lua
+++ DBM-SiegeOfOrgrimmar/localization.ru.lua
@@ -50,13 +50,15 @@ L:SetOptionLocalization({
 })
 
 L:SetMiscLocalization({
-	Pull		= "Воины Драконьей Пасти! Отбейте пристань и сбросьте врага в море! Во имя Истинной Орды!",
-	newForces1	= "Вот и они!",--Jaina's line, horde may not be same
-	newForces1H	= "Сбейте ее скорее, не терпится взять ее за глотку.",--Sylva's line, horde
-	newForces2	= "Драконья Пасть, вперед!",
-	newForces3	= "За Гарроша!",
-	newForces4	= "Следующий отряд!",
-	tower		= "Дверь "--Дверь южной/северной башни разбита!
+	wasteOfTime		= "Well done! Landing parties, form up! Footmen to the front!",--Alliance Version
+	wasteOfTime2	= "Отлично, первый отряд высадился.",--Horde Version
+	Pull			= "Воины Драконьей Пасти! Отбейте пристань и сбросьте врага в море! Во имя Истинной Орды!",
+	newForces1		= "Вот и они!",--Jaina's line, horde may not be same
+	newForces1H		= "Сбейте ее скорее, не терпится взять ее за глотку.",--Sylva's line, horde
+	newForces2		= "Драконья Пасть, вперед!",
+	newForces3		= "За Гарроша!",
+	newForces4		= "Следующий отряд!",
+	tower			= "Дверь "--Дверь южной/северной башни разбита!
 })
 
 --------------------
@@ -162,7 +164,7 @@ L:SetMiscLocalization({
 	KorvenFlavor		= "Конец древней империи",--http://ptr.wowhead.com/quest=31232
 	KorvenFlavor2		= "Забери свои гуртанские таблички и подавись ими",--http://ptr.wowhead.com/quest=31232
 	IyyokukFlavor		= "Видишь возможности. Используй их!",--Does not have quests, http://ptr.wowhead.com/npc=65305
-	KarozFlavor			= "Ты больше не будешь прыгать!",---Does not have questst, http://ptr.wowhead.com/npc=65303
+	KarozFlavor			= "Ты больше не будешь прыгать!",---Does not have quests, http://ptr.wowhead.com/npc=65303
 	SkeerFlavor			= "Кровавое удовольствие!",--http://ptr.wowhead.com/quest=31178
 	RikkalFlavor		= "Запрос образцов выполнен"--http://ptr.wowhead.com/quest=31508
 })
@@ -171,6 +173,10 @@ L:SetMiscLocalization({
 -- Garrosh Hellscream --
 ------------------------
 L= DBM:GetModLocalization(869)
+
+L:SetOptionLocalization({
+	RangeFrame	= "Показывать динамическое окно проверки дистанции (8)<br/>(Это умное окно проверки дистанции, которое появляется когда вы достигаете порога $spell:147126)"
+})
 
 -------------
 --  Trash  --
