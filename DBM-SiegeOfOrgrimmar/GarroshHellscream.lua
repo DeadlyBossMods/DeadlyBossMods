f0e0e0a66ade23bbe09c3f19a377c3a930fe74b7
Adam <MysticalOS@users.noreply.github.com>
2013-12-30 02:27:12 +0000

    wrong revision, fix that

--- DBM-SiegeOfOrgrimmar/GarroshHellscream.lua
+++ DBM-SiegeOfOrgrimmar/GarroshHellscream.lua
@@ -4,7 +4,7 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(71865)
 mod:SetEncounterID(1623)
-mod:SetHotfixNoticeRev(10827)
+mod:SetHotfixNoticeRev(10828)
 mod:SetZone()
 mod:SetUsedIcons(8, 7, 6, 5, 4, 3, 2, 1)
 
