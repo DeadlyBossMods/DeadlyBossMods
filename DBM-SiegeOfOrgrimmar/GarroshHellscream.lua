345ee453635997b7b16ff5ba8ee855b12e1430c8
Adam <MysticalOS@users.noreply.github.com>
2013-12-30 02:20:23 +0000

    Also, since i fixed many issues in mod, set hotfix notice so people don't complain mod is wrong for me cause they didn't update.

--- DBM-SiegeOfOrgrimmar/GarroshHellscream.lua
+++ DBM-SiegeOfOrgrimmar/GarroshHellscream.lua
@@ -4,6 +4,7 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(71865)
 mod:SetEncounterID(1623)
+mod:SetHotfixNoticeRev(10827)
 mod:SetZone()
 mod:SetUsedIcons(8, 7, 6, 5, 4, 3, 2, 1)
 
