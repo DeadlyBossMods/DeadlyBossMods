8f50af9fa4c663cc19bd8b449ccc9a107b9506ae
ultrashining <ultrashining@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2013-12-30 07:09:20 +0000

    zhTW update

--- DBM-SiegeOfOrgrimmar/localization.tw.lua
+++ DBM-SiegeOfOrgrimmar/localization.tw.lua
@@ -172,7 +172,12 @@ L:SetMiscLocalization({
 L= DBM:GetModLocalization(869)
 
 L:SetOptionLocalization({
-	RangeFrame	= "顯示動態距離框架(10碼)<br/>(這是智慧距離框架，當到達$spell:147126門檻時自動切換)"
+	RangeFrame	= "顯示動態距離框架(10碼)<br/>(這是智慧距離框架，當到達$spell:147126門檻時自動切換)",
+	yellMaliceFading	= "當$spell:147209將要退去時大喊"
+})
+
+L:SetMiscLocalization({
+	MaliceFadeYell		= "%s的惡意消退中(%d)"
 })
 
 -------------
