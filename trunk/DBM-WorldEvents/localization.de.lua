6141550e16f38347b196177f218ef49935650adb
ebmor <ebmor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-01-05 15:08:47 +0000

    deDE update

--- trunk/DBM-WorldEvents/localization.de.lua
+++ trunk/DBM-WorldEvents/localization.de.lua
@@ -55,34 +55,6 @@ L:SetMiscLocalization({
 	Pull			= "Der Eisbrocken ist geschmolzen!"
 })
 
---------------------------
---  Plants Vs. Zombies  --
---------------------------
-L = DBM:GetModLocalization("PlantsVsZombies")
-
-L:SetGeneralLocalization({
-	name = "Pflanzen gegen Zombies"
-})
-
-L:SetWarningLocalization({
-	warnTotalAdds	= "Anzahl erschienener Zombies seit letzter Riesiger Welle: %d",
-	specWarnWave	= "Riesige Welle!"
-})
-
-L:SetTimerLocalization{
-	timerWave		= "Nächste Riesige Welle"
-}
-
-L:SetOptionLocalization({
-	warnTotalAdds	= "Verkünde die Anzahl der erschienenen Zombies zwischen jeder Riesigen Welle",
-	specWarnWave	= "Spezialwarnung, wenn eine Riesige Welle beginnt",
-	timerWave		= "Zeige Zeit bis nächste Riesige Welle"
-})
-
-L:SetMiscLocalization({
-	MassiveWave		= "Eine riesige Zombiewelle nähert sich!" --needs to be verified (video-captured translation)
-})
-
 ----------------------
 --  Coren Direbrew  --
 ----------------------
@@ -132,4 +104,32 @@ L = DBM:GetModLocalization("Greench")
 
 L:SetGeneralLocalization({
 	name = "Der monströse Griesgram"
+})
+
+--------------------------
+--  Plants Vs. Zombies  --
+--------------------------
+L = DBM:GetModLocalization("PlantsVsZombies")
+
+L:SetGeneralLocalization({
+	name = "Pflanzen gegen Zombies"
+})
+
+L:SetWarningLocalization({
+	warnTotalAdds	= "Anzahl erschienener Zombies seit letzter Riesiger Welle: %d",
+	specWarnWave	= "Riesige Welle!"
+})
+
+L:SetTimerLocalization{
+	timerWave		= "Nächste Riesige Welle"
+}
+
+L:SetOptionLocalization({
+	warnTotalAdds	= "Verkünde die Anzahl der erschienenen Zombies zwischen jeder Riesigen Welle",
+	specWarnWave	= "Spezialwarnung, wenn eine Riesige Welle beginnt",
+	timerWave		= "Zeige Zeit bis nächste Riesige Welle"
+})
+
+L:SetMiscLocalization({
+	MassiveWave		= "Eine riesige Zombiewelle nähert sich!" --needs to be verified (video-captured translation)
 })
