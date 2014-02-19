4676fe40cb11caab2cc0ec7d7f36772aa40ee6fa
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-02-19 10:44:44 +0000

    increment version number for alpha builds

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -51,9 +51,9 @@
 -------------------------------
 DBM = {
 	Revision = tonumber(("$Revision$"):sub(12, -3)),
-	DisplayVersion = "5.4.9 alpha", -- the string that is shown as version
-	DisplayReleaseVersion = "5.4.8", -- Needed to work around old versions of BW sending improper version information
-	ReleaseRevision = 11048 -- the revision of the latest stable version that is available
+	DisplayVersion = "5.4.10 alpha", -- the string that is shown as version
+	DisplayReleaseVersion = "5.4.9", -- Needed to work around old versions of BW sending improper version information
+	ReleaseRevision = 11056 -- the revision of the latest stable version that is available
 }
 
 -- Legacy crap; that stupid "Version" field was never a good idea.
