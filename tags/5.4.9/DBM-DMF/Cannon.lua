d882cda08351093b94b9e7d0fd74e0dc15c59ae3
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-02-19 10:42:12 +0000

    tag as 5.4.9

---
+++ tags/5.4.9/DBM-DMF/Cannon.lua
@@ -0,0 +1,39 @@
+local mod	= DBM:NewMod("Cannon", "DBM-DMF")
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+mod:SetZone()
+
+mod:RegisterEvents(
+	"SPELL_CAST_SUCCESS 102120",
+	"UNIT_AURA player"
+)
+
+local timerMagicWings				= mod:NewBuffFadesTimer(8.5, 102116)
+
+local MagicWingsCountdown			= mod:NewCountdownFades(7.5, 102116)
+
+local markWings = false
+
+mod:RemoveOption("HealthFrame")
+mod:RemoveOption("SpeedKillTimer")
+
+local function wingsRemoved()
+	markWings = false
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 102120 and args:IsPlayer() then
+		MagicWingsCountdown:Cancel()
+		timerMagicWings:Cancel()
+	end
+end
+
+function mod:UNIT_AURA(uId)
+	if UnitBuff("player", GetSpellInfo(102116)) and not markWings then
+		MagicWingsCountdown:Start(7.5)--Might need to reduce it by 1 or use UnitDebuff duration arg.
+		timerMagicWings:Start()
+		markWings = true
+		self:Schedule(8.5, wingsRemoved)
+	end
+end
