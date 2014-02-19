9f8e53e768bc35c17fad26d4cc8268a204bd21ed
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-02-19 17:36:41 +0000

    Make sure latest alpha is newer than release

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -45,7 +45,6 @@
 --    * Share Alike. If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one.
 --
 
-
 -------------------------------
 --  Globals/Default Options  --
 -------------------------------
