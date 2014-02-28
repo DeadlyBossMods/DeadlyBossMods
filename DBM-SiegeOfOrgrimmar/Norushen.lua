e965a7a6213d949193e40efc4d6386f4629628f2
Adam <MysticalOS@users.noreply.github.com>
2014-02-28 16:15:20 +0000

    ##Fix DBM auto responding to whispers from people fighting same boss (missed a spot that needed ambiguate) #Fixed an issue with norushen pre pull timer from starting on some wipes. #Fixed an issue where 2 of the 4 prisons were missing warnings on sha of pride.

--- DBM-SiegeOfOrgrimmar/Norushen.lua
+++ DBM-SiegeOfOrgrimmar/Norushen.lua
@@ -263,6 +263,7 @@ end
 
 function mod:ENCOUNTER_START(id)
 	if id == 1624 then
+		if self.lastWipeTime and GetTime() - self.lastWipeTime < 20 then return end--False ENCOUNTER_START firing on a wipe (blizz bug), ignore it so we don't start pre pull timer
 		self:SendSync("prepull")
 	end
 end
@@ -275,6 +276,7 @@ end
 
 function mod:OnSync(msg, guid)
 	if msg == "prepull" then
+		if self.lastWipeTime and GetTime() - self.lastWipeTime < 20 then return end
 		timerCombatStarts:Start()
 	end
 end
