7083bacfd3f15f80db1772fe1df4dd069f1f0c57
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-04-12 02:46:08 +0000

    Make IsMythic stand apart from Isheroic

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -5124,12 +5124,19 @@ end
 
 function bossModPrototype:IsHeroic()
 	local diff = DBM:GetCurrentInstanceDifficulty()
-	if diff == "mythic" or diff == "heroic5" or diff == "heroic10" or diff == "heroic25" then
+	if diff == "heroic" or diff == "heroic5" or diff == "heroic10" or diff == "heroic25" then
 		return true
 	end
 	return false
 end
-bossModPrototype.IsMythic = bossModPrototype.IsHeroic
+
+function bossModPrototype:IsMythic()
+	local diff = DBM:GetCurrentInstanceDifficulty()
+	if diff == "mythic" then
+		return true
+	end
+	return false
+end
 
 function bossModPrototype:IsTrivial(level)
 	if UnitLevel("player") >= level then
