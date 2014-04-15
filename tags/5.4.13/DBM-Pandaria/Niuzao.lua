4be1b75bd6518111e85f031874066eb8efef8e58
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-04-15 19:00:09 +0000

    tag as 5.4.13

---
+++ tags/5.4.13/DBM-Pandaria/Niuzao.lua
@@ -0,0 +1,77 @@
+local mod	= DBM:NewMod(859, "DBM-Pandaria", nil, 322, 1)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+mod:SetCreatureID(71954)
+mod:SetReCombatTime(20)
+mod:SetZone()
+mod:SetMinSyncRevision(10466)
+
+mod:RegisterCombat("combat_yell", L.Pull)
+mod:RegisterKill("yell", L.Victory, L.VictoryDem)
+
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_START 144610 144611 144608",
+	"SPELL_AURA_APPLIED 144606",
+	"SPELL_AURA_APPLIED_DOSE 144606",
+	"UNIT_SPELLCAST_SUCCEEDED target focus"
+)
+
+local warnHeadbutt				= mod:NewSpellAnnounce(144610, 3, nil, mod:IsTank())
+local warnOxenFortitude			= mod:NewStackAnnounce(144606, 2)--144607 player version, but better to just track boss and announce stacks
+local warnMassiveQuake			= mod:NewSpellAnnounce(144611, 3)
+local warnCharge				= mod:NewSpellAnnounce(144609, 4)
+
+local specWarnHeadbutt			= mod:NewSpecialWarningSpell(144610, mod:IsTank())
+local specWarnMassiveQuake		= mod:NewSpecialWarningSpell(144611, mod:IsHealer())
+local specWarnCharge			= mod:NewSpecialWarningSpell(144609, nil, nil, nil, 2)--66 and 33%. Maybe add pre warns
+
+local timerHeadbuttCD			= mod:NewCDTimer(47, 144610, nil, mod:IsTank())
+local timerMassiveQuake			= mod:NewBuffActiveTimer(13, 144611)
+local timerMassiveQuakeCD		= mod:NewCDTimer(48, 144611)
+
+mod:AddReadyCheckOption(33117, false)
+
+function mod:OnCombatStart(delay, yellTriggered)
+	if yellTriggered then
+		timerHeadbuttCD:Start(16-delay)
+		timerMassiveQuakeCD:Start(45-delay)
+	end
+end
+
+function mod:SPELL_CAST_START(args)
+	local spellId = args.spellId
+	if spellId == 144610 then
+		warnHeadbutt:Show()
+		specWarnHeadbutt:Show()
+		timerHeadbuttCD:Start()
+	elseif spellId == 144611 then
+		warnMassiveQuake:Show()
+		specWarnMassiveQuake:Show()
+		timerMassiveQuake:Start()
+		timerMassiveQuakeCD:Start()
+	elseif spellId == 144608 then
+		warnCharge:Show()
+		specWarnCharge:Show()
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	local spellId = args.spellId
+	if spellId == 144606 then
+		warnOxenFortitude:Show(args.destName, args.amount or 1)
+	end
+end
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
+	if spellId == 148318 or spellId == 148317 or spellId == 149304 and self:AntiSpam(3, 2) then--use all 3 because i'm not sure which ones fire on repeat kills
+		self:SendSync("Victory")
+	end
+end
+
+function mod:OnSync(msg)
+	if msg == "Victory" and self:IsInCombat() then
+		DBM:EndCombat(self)
+	end
+end
---
+++ tags/5.4.13/DBM-Scenario-MoP/localization.es.lua
@@ -0,0 +1,180 @@
+﻿if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
+local L
+
+---------------------
+-- A Brewing Storm --
+---------------------
+L= DBM:GetModLocalization("d517")
+
+L:SetTimerLocalization{
+	timerEvent			= "Fermentación (Aprox)"
+}
+
+L:SetOptionLocalization{
+   timerEvent			= "Mostrar temporizador para la fermentación (aproximada)"
+}
+
+L:SetMiscLocalization{
+	BrewStart			= "¡Ya empieza la tormenta! Preparaos.",
+	BorokhulaPull		= "¡Último aviso, reptiles de lengua bífida!",
+	BorokhulaAdds		= "solicita refuerzos!"--In case useful/important on heroic. On normal just zerg boss and ignore these unless you want achievement.
+}
+
+-----------------------
+-- A Little Patience --
+-----------------------
+L= DBM:GetModLocalization("d589")
+
+L:SetMiscLocalization{
+	ScargashPull		= "Your Alliance is WEAK!"--Not yet in use but could be with more logs and combat start timers, TODO needs translation
+}
+---------------------------
+-- Arena Of Annihilation --
+---------------------------
+L= DBM:GetModLocalization("d511")
+
+-------------------------
+-- Assault of Zan'vess --
+-------------------------
+L= DBM:GetModLocalization("d593")
+
+L:SetMiscLocalization{
+	TelvrakPull			= "Zan'vess will never fall!" -- TODO needs translation
+}
+
+------------------------------
+-- Battle on the High Seas ---
+------------------------------
+L= DBM:GetModLocalization("d652")
+
+-----------------------
+-- Blood in the Snow --
+-----------------------
+L= DBM:GetModLocalization("d646")
+
+-----------------------
+-- Brewmoon Festival --
+-----------------------
+L= DBM:GetModLocalization("d539")
+
+L:SetTimerLocalization{
+	timerBossCD		= "%s llegando"
+}
+
+L:SetOptionLocalization{
+	timerBossCD		= "Mostrar temporizador para el siguiente jefe"
+}
+
+L:SetMiscLocalization{
+	RatEngage	= "It's the Den Mother! Look out", -- TODO needs translation
+	BeginAttack	= "We must defend the villagers!", -- TODO needs translation
+	Yeti		= "Bataari War Yeti", -- TODO needs translation
+	Qobi		= "Warbringer Qobi" -- TODO needs translation
+}
+
+------------------------------
+-- Crypt of Forgotten Kings --
+------------------------------
+L= DBM:GetModLocalization("d504")
+
+-----------------------
+-- Dagger in the Dark --
+-----------------------
+L= DBM:GetModLocalization("d616")
+
+L:SetTimerLocalization{
+	timerAddsCD		= "Invocar adds"
+}
+
+L:SetOptionLocalization{
+	timerAddsCD		= "Mostrar temporizador para la invocación de adds"
+}
+
+L:SetMiscLocalization{
+	LizardLord		= "Dem Saurok be guardin de cave.  Let's take care of 'em." -- TODO needs translation
+}
+
+----------------------------
+-- Dark Heart of Pandaria --
+----------------------------
+L= DBM:GetModLocalization("d647")
+
+L:SetMiscLocalization{
+	summonElemental		= "Minions, destroy these insects!" --TODO, needs translation
+}
+
+------------------------
+-- Greenstone Village --
+------------------------
+L= DBM:GetModLocalization("d492")
+
+--------------
+-- Landfall --
+--------------
+L = DBM:GetModLocalization("Landfall")
+
+L:SetWarningLocalization{
+	WarnAchFiveAlive	= "Logro fallado: \"Los cinco en el Desembarco del León\""
+}
+
+L:SetOptionLocalization{
+	WarnAchFiveAlive	= "Mostrar aviso si se ha fallado el logro \"Los cinco en el Desembarco del León\""
+}
+
+----------------------------
+-- The Secret of Ragefire --
+----------------------------
+L= DBM:GetModLocalization("d649")
+
+L:SetMiscLocalization{
+	XorenthPull		= "All lesser races are enemies of the true Horde!", -- TODO needs translation
+	ElagloPull		= "Fools! The true horde cannot be stopped by the likes of you." -- TODO needs translation
+}
+
+----------------------
+-- Theramore's Fall --
+----------------------
+L= DBM:GetModLocalization("d566")
+
+--------------------------------
+-- Troves of the Thunder King --
+--------------------------------
+L= DBM:GetModLocalization("d620")
+
+----------------
+-- Unga Ingoo --
+----------------
+L= DBM:GetModLocalization("d499")
+
+------------------------
+-- Warlock Green Fire --
+------------------------
+L= DBM:GetModLocalization("d594")
+
+L:SetWarningLocalization{
+	specWarnLostSouls		= "Almas perdidas!",
+	specWarnEnslavePitLord	= "Señor del foso - ¡Esclavízalo ahora!"
+}
+
+L:SetTimerLocalization{
+	timerLostSoulsCD		= "Siguientes Almas perdidas"
+}
+
+L:SetOptionLocalization{
+	specWarnLostSouls		= "Mostrar aviso especial cuando aparezcan Almas Perdidas",
+	specWarnEnslavePitLord	= "Mostrar aviso especial para escalvizar a los Señores del foso cuando se activen o se liberen",
+	timerLostSoulsCD		= "Mostrar temporizador para las siguientes Almas perdidas"
+}
+
+L:SetMiscLocalization{
+	LostSouls				= "Face the souls of those your kind doomed to perish, Warlock!" -- TODO needs translation
+}
+
+-------------------------------
+-- Finding Secret Ingredient --
+-------------------------------
+L= DBM:GetModLocalization("d745")
+
+L:SetMiscLocalization{
+	Clear		= "Well done!" -- TODO needs translation
+}
