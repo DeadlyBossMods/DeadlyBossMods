765da53e86d46981dbfa868d9a8b56a725601683
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-06-17 03:20:46 +0000

    change chiji target scanning speed, needs to be a lot slower for that

--- trunk/DBM-SiegeOfOrgrimmar/Immerseus.lua
+++ trunk/DBM-SiegeOfOrgrimmar/Immerseus.lua
@@ -24,15 +24,15 @@ mod:RegisterEventsInCombat(
 local warnBreath						= mod:NewSpellAnnounce(143436, 3, nil, mod:IsTank() or mod:IsHealer())
 local warnShaBolt						= mod:NewSpellAnnounce(143295, 3, nil, false)
 local warnSwirl							= mod:NewSpellAnnounce(143309, 4)
-local warnSplit							= mod:NewSpellAnnounce(143020, 2)
-local warnReform						= mod:NewSpellAnnounce(143469, 2)
+local warnSplit							= mod:NewSpellAnnounce("OptionVersion2", 143020, 2, nil, false)--Blizzard ones are loud enough
+local warnReform						= mod:NewSpellAnnounce("OptionVersion2", 143469, 2, nil, false)--These are redundant, but some DO like them for the DBM sound vs blizz one so not completely removed
 local warnSwellingCorruptionCast		= mod:NewSpellAnnounce(143578, 2, 143574)--Heroic (this is the boss spellcast trigger spell NOT personal debuff warning)
 
 local specWarnBreath					= mod:NewSpecialWarningSpell(143436, mod:IsTank())
 local specWarnShaSplash					= mod:NewSpecialWarningMove(143297)
 local specWarnSwirl						= mod:NewSpecialWarningSpell(143309, nil, nil, nil, 2)
-local specWarnSwellingCorruptionTarget	= mod:NewSpecialWarningTarget(143578)
-local specWarnSwellingCorruptionFades	= mod:NewSpecialWarningFades(143578)
+local specWarnSwellingCorruptionTarget	= mod:NewSpecialWarningTarget("OptionVersion2", 143578, false)
+local specWarnSwellingCorruptionFades	= mod:NewSpecialWarningFades("OptionVersion2", 143578, false)
 
 local timerBreathCD						= mod:NewCDTimer(35, 143436, nil, mod:IsTank() or mod:IsHealer())--35-65 second variation wtf?
 local timerSwirl						= mod:NewBuffActiveTimer(13, 143309)
