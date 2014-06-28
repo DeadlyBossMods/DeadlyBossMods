e0eaa4729fab323c7ac5813264ca0dacf72cd502
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-06-28 23:14:41 +0000

    add branch for WoD

---
+++ branches/WoD/DBM-Scenario-MoP/localization.fr.lua
@@ -0,0 +1,12 @@
+﻿if GetLocale() ~= "frFR" then return end
+local L
+
+----------------------
+-- Theramore's Fall --
+----------------------
+L= DBM:GetModLocalization("d566")
+
+---------------------------
+-- Arena Of Annihilation --
+---------------------------
+L= DBM:GetModLocalization("d511")
