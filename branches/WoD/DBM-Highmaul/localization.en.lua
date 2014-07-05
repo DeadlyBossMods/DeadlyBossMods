a5bb4b367f9d408e2c8aa7c69610731a25425d56
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-07-05 00:33:13 +0000

    Highmaul

---
+++ branches/WoD/DBM-Highmaul/localization.en.lua
@@ -0,0 +1,54 @@
+local L
+
+---------------
+-- Kargath Bladefist --
+---------------
+L= DBM:GetModLocalization(1128)
+
+L:SetWarningLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+---------------------------
+-- The Butcher --
+---------------------------
+L= DBM:GetModLocalization(971)
+
+---------------------------
+-- Tectus, the Living Mountain --
+---------------------------
+L= DBM:GetModLocalization(1195)
+
+------------------
+-- Brackenspore, Walker of the Deep --
+------------------
+L= DBM:GetModLocalization(1196)
+
+--------------
+-- Twin Ogron --
+--------------
+L= DBM:GetModLocalization(1148)
+
+--------------------
+--Fel Breaker --
+--------------------
+L= DBM:GetModLocalization(1153)
+
+--------------------------
+-- Imperator Mar'gok --
+--------------------------
+L= DBM:GetModLocalization(1197)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("HighmaulTrash")
+
+L:SetGeneralLocalization({
+	name =	"Highmaul Trash"
+})
