06fbc71c458e207766c007cd9ecb0baac6008dc2
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-07-09 05:46:40 +0000

    Roltall

--- branches/WoD/DBM-Party-WoD/BloodmaulSlagMines/Roltall.lua
+++ branches/WoD/DBM-Party-WoD/BloodmaulSlagMines/Roltall.lua
@@ -2,67 +2,104 @@ local mod	= DBM:NewMod(887, "DBM-Party-WoD", 2, 385)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(56439)
+mod:SetCreatureID(75786)
 mod:SetEncounterID(1652)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
---[[
+
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_SUCCESS",
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED"
+	"SPELL_CAST_START 153247 152940 152939",
+	"SPELL_AURA_APPLIED 153227",
+	"SPELL_PERIODIC_DAMAGE",
+	"SPELL_PERIODIC_MISSED"
 )
 
 
-local warnWitherWill			= mod:NewSpellAnnounce(106736, 3)
-local warnTouchofNothingness	= mod:NewTargetAnnounce(106113, 4)
-local warnBoundsOfReality		= mod:NewSpellAnnounce(117665, 3)
+local warnFieryBoulder			= mod:NewCountAnnounce(153247, 3)
+local warnHeatWave				= mod:NewSpellAnnounce(152940, 3)
+local warnBurningSlag			= mod:NewSpellAnnounce(152939, 3)
 
-local specWarnTouchOfNothingness= mod:NewSpecialWarningDispel(106113, mod:IsHealer())
-local specWarnShadowsOfDoubt	= mod:NewSpecialWarningMove(110099)--Actually used by his trash, but in a speed run, you tend to pull it all together
+local specWarnFieryBoulder		= mod:NewSpecialWarningSpell(153247, false)
+local specWarnHeatWave			= mod:NewSpecialWarningSpell(152940, false, nil, nil, 2)
+local specWarnBurningSlag		= mod:NewSpecialWarningSpell(152939, false, nil, nil, 2)
+local specWarnBurningSlagFire	= mod:NewSpecialWarningMove(152939)
 
-local timerWitherWillCD			= mod:NewCDTimer(6, 106736)--6-10 second variations.
-local timerTouchofNothingnessCD	= mod:NewCDTimer(15.5, 106113)--15.5~20 second variations.
-local timerTouchofNothingness	= mod:NewTargetTimer(30, 106113)
-local timerBoundsOfRealityCD	= mod:NewCDTimer(60, 117665)
-local timerBoundsOfReality		= mod:NewBuffFadesTimer(30, 117665)
+local timerFieryBoulderCD		= mod:NewNextTimer(13.3, 153247)--13.3-13.4 Observed
+local timerHeatWaveCD			= mod:NewCDTimer(9.5, 152940)--9.5-9.8 Observed
+local timerBurningSlagCD		= mod:NewCDTimer(10.7, 152939)--10.7-11 Observed
+
+mod.vb.boulderCount = 0
+mod.vb.burningSlagCast = false--More robust than using a really huge anti spam, because this will work with recovery, antispam won't
 
 function mod:OnCombatStart(delay)
-	timerWitherWillCD:Start(-delay)
-	timerTouchofNothingnessCD:Start(13-delay)
-	timerBoundsOfRealityCD:Start(22-delay)
+	self.vb.boulderCount = 0
+	self.vb.burningSlagCast = false
+	timerFieryBoulderCD:Start(8-delay)
 end
 
-function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 106736 then
-		warnWitherWill:Show()
-		timerWitherWillCD:Start()
-	elseif args.spellId == 106113 then--Start Cd here in case it's resisted
-		timerTouchofNothingnessCD:Start()
+--[[
+"<2.7 01:12:08> [INSTANCE_ENCOUNTER_ENGAGE_UNIT] Fake Args:#true#true#Roltall#
+"<10.9 01:12:16> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#153247#Fiery Boulder#4", -- [137]
+"<14.3 01:12:20> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#153247#Fiery Boulder#4", -- [200]
+"<18.1 01:12:23> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#153247#Fiery Boulder#4", -- [248]+33.8 (+9.5)
+
+"<27.6 01:12:33> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#152940#Heat Wave#4", -- [449]  +41.1 (+11)
+
+"<38.6 01:12:44> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#152939#Burning Slag#4", -- [665] +40.8 (13.3)
+
+"<51.9 01:12:57> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#153247#Fiery Boulder#4", -- [931]
+"<55.3 01:13:01> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#153247#Fiery Boulder#4", -- [1027]
+"<59.0 01:13:04> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#153247#Fiery Boulder#4", -- [1094] +33.7 (+9.7)
+
+"<68.7 01:13:14> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#152940#Heat Wave#4", -- [1244] +41 (+10.7)
+
+"<79.4 01:13:25> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#152939#Burning Slag#4", -- [1455] +41 (+13.3)
+
+"<92.7 01:13:38> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#153247#Fiery Boulder#4", -- [1627]
+"<96.3 01:13:42> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#153247#Fiery Boulder#4", -- [1681]
+"<99.9 01:13:45> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#153247#Fiery Boulder#4", -- [1723] +33.9 (+9.8)
+
+"<109.7 01:13:55> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#152940#Heat Wave#4", -- [1832] (+10.7)
+
+"<120.4 01:14:06> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#152939#Burning Slag#4", -- [2022] (+13.4)
+
+"<133.8 01:14:19> [CLEU] SPELL_CAST_START#false#Creature:0:3314:1175:11531:75786:00003A26C0#Roltall#68168#0##nil#-2147483648#-2147483648#153247#Fiery Boulder#4", -- [2229]
+--]]
+
+function mod:SPELL_CAST_START(args)
+	local spellId = args.spellId
+	if spellId == 153247 then--Boulder
+		if self.vb.burningSlagCast then self.vb.burningSlagCast = false end
+		self.vb.boulderCount = self.vb.boulderCount + 1
+		warnFieryBoulder:Show(self.vb.boulderCount)
+		specWarnFieryBoulder:Show()
+		if self.vb.boulderCount == 3 then
+			timerHeatWaveCD:Start()
+		else
+			timerFieryBoulderCD:Start(3.5)--Not to be confused with cast timer, that's 3 seconds. The previous meteor WILL hit ground before next cast.
+		end
+	elseif spellId == 152940 then--Heat Wave
+		warnHeatWave:Show()
+		specWarnHeatWave:Show()
+		timerBurningSlagCD:Start()
+	elseif spellId == 152939 and not self.vb.burningSlagCast then--Burning Slag
+		self.vb.burningSlagCast = true
+		warnBurningSlag:Show()
+		specWarnBurningSlag:Show()
+		timerFieryBoulderCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 117665 then
-		warnBoundsOfReality:Show()
-		timerWitherWillCD:Cancel()
-		timerTouchofNothingnessCD:Cancel()
-		timerBoundsOfReality:Start()
-		timerBoundsOfRealityCD:Start()
-	elseif args.spellId == 106113 then
-		warnTouchofNothingness:Show(args.destName)
-		specWarnTouchOfNothingness:Show(args.destName)
-		timerTouchofNothingness:Start(args.destName)
-	elseif args.spellId == 110099 and args:IsPlayer() then
-		specWarnShadowsOfDoubt:Show()
+	if args.spellId == 153227 and self:AntiSpam(2, 1) then
+		specWarnBurningSlagFire:Show()
 	end
 end
 
-function mod:SPELL_AURA_REMOVED(args)
-	if args.spellId == 117665 then
-		timerBoundsOfReality:Cancel()
-	elseif args.spellId == 106113 then
-		timerTouchofNothingness:Cancel(args.destName)
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
+	if spellId == 153227 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
+		specWarnBurningSlagFire:Show()
 	end
-end--]]
+end
+mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
