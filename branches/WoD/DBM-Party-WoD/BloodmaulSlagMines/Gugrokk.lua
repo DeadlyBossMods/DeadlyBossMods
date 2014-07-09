a3fbad1d1fc80ffc57a465271188307e7013d7d0
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-07-09 08:09:04 +0000

    Gugrokk

--- branches/WoD/DBM-Party-WoD/BloodmaulSlagMines/Gugrokk.lua
+++ branches/WoD/DBM-Party-WoD/BloodmaulSlagMines/Gugrokk.lua
@@ -2,77 +2,55 @@ local mod	= DBM:NewMod(889, "DBM-Party-WoD", 2, 385)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(56448)
+mod:SetCreatureID(74790)
 mod:SetEncounterID(1654)
 mod:SetZone()
---mod:SetUsedIcons(8)
 
 mod:RegisterCombat("combat")
---[[
+
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED",
---	"SPELL_CAST_SUCCESS",
-	"SPELL_CAST_START",
-	"SPELL_DAMAGE",
-	"SPELL_MISSED",
-	"UNIT_DIED",
-	"UNIT_TARGET"
+	"SPELL_CAST_START 150677 150784 150755",
+	"SPELL_PERIODIC_DAMAGE",
+	"SPELL_PERIODIC_MISSED"
 )
 
-local warnBubbleBurst			= mod:NewCastAnnounce(106612, 3, nil)
-local warnAddsLeft				= mod:NewAddsLeftAnnounce("ej5616", 2, 106526)
+--TODO, Add heroic ability "Flame Buffet" Since ability name to common can't just wowhead the spellid
+local warnMoltenBlast			= mod:NewCastAnnounce(150677, 4)
+local warnUnstableSlag			= mod:NewSpellAnnounce(150677, 3)
+local warnMagmaEruption			= mod:NewSpellAnnounce(150784, 3)
 
-local specWarnLivingWater		= mod:NewSpecialWarningSwitch("ej5616", not mod:IsHealer())
-local specWarnCorruptingWaters	= mod:NewSpecialWarningMove(115167)
-local specWarnShaResidue		= mod:NewSpecialWarningMove(106653)
+local specWarnMoltenBlast		= mod:NewSpecialWarningInterrupt(150677)
+local specWarnUnstableSlag		= mod:NewSpecialWarningSwitch(150755, not mod:IsHealer())
+local specWarnMagmaEruptionCast	= mod:NewSpecialWarningSpell(150784, nil, nil, nil, 2)
+local specWarnMagmaEruption		= mod:NewSpecialWarningMove(150784)
 
-local timerLivingWater			= mod:NewCastTimer(5.5, 106526)
-local timerLivingWaterCD		= mod:NewCDTimer(13, 106526)
-local timerWashAway				= mod:NewNextTimer(8, 106334)
-
-mod:AddBoolOption("SetIconOnAdds", false)
-
-local addsRemaining = 4--Also 4 on heroic?
-local addsName = EJ_GetSectionInfo(5616)
-
-function mod:UNIT_TARGET()
-	if self.Options.SetIconOnAdds and not DBM.Options.DontSetIcons and UnitName("target") == addsName then
-		SetRaidTarget("target", 8)
-	end
-end
+local timerMagmaEruptionCD		= mod:NewCDTimer(20, 150784)
+local timerUnstableSlagCD		= mod:NewCDTimer(20, 150755)
 
 function mod:OnCombatStart(delay)
-	addsRemaining = 4
-	timerLivingWaterCD:Start(13-delay)
-end
-
-function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 106653 and args:IsPlayer() and self:AntiSpam(4, 1) then
-		specWarnShaResidue:Show()
-	end
+--	timerMagmaEruptionCD:Start(8-delay)--Poor sample size
+	timerUnstableSlagCD:Start(-delay)--Also poor sample size but more likely to be correct.
 end
 
 function mod:SPELL_CAST_START(args)
-	if args.spellId == 106526 then--Call Water
-		timerLivingWater:Start()
-		specWarnLivingWater:Schedule(5.5)
-	elseif args.spellId == 106612 then--Bubble Burst (phase 2)
-		warnBubbleBurst:Show()
-		timerWashAway:Start()
+	local spellId = args.spellId
+	if spellId == 150677 then
+		warnMoltenBlast:Show()
+		specWarnMoltenBlast:Show(args.sourceName)
+	elseif spellId == 150784 then
+		warnMagmaEruption:Show()
+		specWarnMagmaEruptionCast:Show()
+		timerMagmaEruptionCD:Start()
+	elseif spellId == 150755 then
+		warnUnstableSlag:Show()
+		specWarnUnstableSlag:Show()
+		timerUnstableSlagCD:Start()
 	end
 end
 
-function mod:SPELL_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 115167 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
-		specWarnCorruptingWaters:Show()
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
+	if spellId == 150784 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
+		specWarnMagmaEruption:Show()
 	end
 end
-mod.SPELL_MISSED = mod.SPELL_DAMAGE
-
-function mod:UNIT_DIED(args)
-	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 56511 then--Corrupt Living Water
-		addsRemaining = addsRemaining - 1
-		warnAddsLeft:Show(addsRemaining)
-	end
-end--]]
+mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
