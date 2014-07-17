fd9cdeb9990ab9c392df20ec600d252c30717ec3
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-07-17 07:00:59 +0000

    change warning type for gouge so it filters

--- branches/MoP-6.0/DBM-SiegeOfOrgrimmarV2/FallenProtectors.lua
+++ branches/MoP-6.0/DBM-SiegeOfOrgrimmarV2/FallenProtectors.lua
@@ -73,7 +73,7 @@ local specWarnInfernoStrike			= mod:NewSpecialWarningYou(143962)
 local yellInfernoStrike				= mod:NewYell(143962)
 --He Softfoot
 local specWarnGouge					= mod:NewSpecialWarningMove(143330, nil, nil, nil, 3)--Maybe localize it as a "turn away" warning.
-local specWarnGougeStunOther		= mod:NewSpecialWarningTarget(143301)--Tank is stunned, other tank must taunt or he'll start killing people
+local specWarnGougeStunOther		= mod:NewSpecialWarningTaunt(143301)--Tank is stunned, other tank must taunt or he'll start killing people
 local specWarnNoxiousPoison			= mod:NewSpecialWarningMove(144367)
 ----He Softfoot's Desperate measures
 local specWarnMarkOfAnquish			= mod:NewSpecialWarningSpell(143812)
