40f76eb049bc3067ddd95fd18499f9038b3211a1
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-07-24 05:25:47 +0000

    here

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -2547,7 +2547,7 @@ end
 --------------------------------
 do
 	local targetEventsRegistered = false
-	local function FixForShittyComputers(firstRun)
+	local function FixForShittyComputers()
 		timerRequestInProgress = false
 		local _, instanceType, difficulty, _, _, _, _, mapID, instanceGroupSize = GetInstanceInfo()
 		if LastInstanceMapID == mapID then return end--ID hasn't changed, don't waste cpu doing anything else (example situation, porting into garrosh phase 4 is a loading screen)
@@ -2574,13 +2574,11 @@ do
 		end
 		-- LoadMod
 		DBM:LoadModsOnDemand("mapId", mapID)
-		if firstRun then--Two pass check for slow computers or people with high latency who didn't get accurate return from GetInstanceInfo yet
-			DBM:Schedule(3, FixForShittyComputers, false, DBM)
-		end
 	end
 	--Faster and more accurate loading for instances, but useless outside of them
 	function DBM:LOADING_SCREEN_DISABLED()
-		FixForShittyComputers(true)
+		FixForShittyComputers()
+		DBM:Schedule(3, FixForShittyComputers, DBM)
 	end
 
 	function DBM:LoadModsOnDemand(checkTable, checkValue)
