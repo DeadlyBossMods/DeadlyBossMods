06e10348f4614accd967af29a6a55388595f96fd
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-07-28 21:12:04 +0000

    Finished Oregorger mod Special thanks to midwinter for outstanding log data :)

--- branches/WoD/DBM-BlackrockFoundry/Oregorger.lua
+++ branches/WoD/DBM-BlackrockFoundry/Oregorger.lua
@@ -10,106 +10,119 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 156877 156240 156179",
-	"SPELL_AURA_APPLIED 156370 155898 155819",
+	"SPELL_AURA_APPLIED 155819",
 	"SPELL_AURA_APPLIED_DOSE 155819",
-	"SPELL_AURA_REMOVED 156370 155898"
+	"SPELL_AURA_REMOVED 155819",
+	"SPELL_CAST_SUCCESS 156390",
+	"SPELL_PERIODIC_DAMAGE 156203",
+	"SPELL_PERIODIC_MISSED 156203",
+	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
-local warnBlackrockBarrage			= mod:NewSpellAnnounce(156877, 2)
+--[[Berserk?
+"<4.4 14:25:47> [INSTANCE_ENCOUNTER_ENGAGE_UNIT] Fake Args:#true#true#Oregorger#Creature:0:3314:1205:13906:77182
+"<328.2 14:31:10> CHAT_MSG_RAID_BOSS_EMOTE#Oregorger has gone insane from hunger!#Oregorger#####0#0##0#164#0000000000000000#0#false#false", -- [5]--]]
+local warnBlackrockBarrage			= mod:NewCountAnnounce(156877, 2)
 local warnAcidTorrent				= mod:NewSpellAnnounce(156240, 3)
-local warnRetchedBlackrock			= mod:NewTargetAnnounce(156179, 3)--Target scanning assumed. (can it be avoided? if so add special warning to move)
-local warnExplosiveShard			= mod:NewTargetAnnounce(156370, 4)
-local warnRollingFury				= mod:NewTargetAnnounce(155898, 4)
-local warnHungerDrive				= mod:NewStackAnnounce(155819, 3, nil, false)--Similar to thok, count may be useful. We'll see
+local warnRetchedBlackrock			= mod:NewTargetAnnounce(156179, 3)--Target scanning verified.
+local warnExplosiveShard			= mod:NewSpellAnnounce(156390, 4)--No target scanning available.
+local warnHungerDrive				= mod:NewSpellAnnounce(165127, 4)
+local warnHungerDriveStack			= mod:NewStackAnnounce(155819, 3, nil, false)--Similar to thok, count may be useful measure of how long to drag phase out for.
 
-local specWarnBlackrockBarrage		= mod:NewSpecialWarningInterrupt(156877, false)--How much is it spammed? should this be on by default?
+local specWarnBlackrockBarrage		= mod:NewSpecialWarningInterrupt(156877, false)--Strategy dependant
 local specWarnAcidTorrent			= mod:NewSpecialWarningSpell(156240, mod:IsTank(), nil, nil, 3)
-local specWarnExplosiveShard		= mod:NewSpecialWarningYou(156370)
-local yellExplosiveShard			= mod:NewYell(156370)
-local specWarnRollingFury			= mod:NewSpecialWarningSpell(155898, nil, nil, nil, 2)
-local specWarnRollingFuryEnded		= mod:NewSpecialWarningFades(155898)
+local yellRetchedBlackrock			= mod:NewYell(156179)
+local specWarnRetchedBlackrock		= mod:NewSpecialWarningMove(165127)
+local specWarnExplosiveShard		= mod:NewSpecialWarningSpell(156390, mod:IsMelee())
+local specWarnHungerDrive			= mod:NewSpecialWarningSpell(165127, nil, nil, nil, 2)
+local specWarnHungerDriveEnded		= mod:NewSpecialWarningFades(165127)
 
---local timerBlackrockBarrageCD		= mod:NewNextTimer(30, 156879)
---local timerAcidTorrentCD			= mod:NewNextTimer(30, 156240)
+--local timerBlackrockBarrageCD		= mod:NewCDTimer(30, 156879)--Far to variable. probably based on energy as well as if previous ones interrupted or not.
+local timerAcidTorrentCD			= mod:NewCDTimer(12, 156240)--Every 12-15 seconds
+local timerExplosiveShardCD			= mod:NewCDTimer(12, 156390)--Every 12-20 seconds
+local timerRetchedBlackrockCD		= mod:NewCDTimer(17, 156179)--Every 17-18 seconds
 
-mod:AddRangeFrameOption(7, 156370)--Tooltip of 156388 (explosion spellid that goes off at end of debuff) says 7, Unverified
+--local berserkTimer					= mod:NewBerserkTimer(324)--May not be exact science. may be phase based instead, like tsulong. Needs more than one log to verify. Only saw one berserk.
+
+mod.vb.barrageCount = 0
 
 function mod:RetchedBlackrockTarget(targetname, uId)
 	if not targetname then return end
 	warnRetchedBlackrock:Show(targetname)
+	if targetname == UnitName("player") then
+		yellRetchedBlackrock:Yell()
+	end
 end
 
 function mod:OnCombatStart(delay)
-
+	self.vb.barrageCount = 0
+	timerAcidTorrentCD:Start(11.5-delay)
+--	berserkTimer:Start(-delay)
 end
 
 function mod:OnCombatEnd()
-	if self.Options.RangeFrame then
-		DBM.RangeCheck:Hide()
-	end
+
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 156877 then
-		warnBlackrockBarrage:Show()
+		self.vb.barrageCount = self.vb.barrageCount + 1
+		warnBlackrockBarrage:Show(self.vb.barrageCount)
 		specWarnBlackrockBarrage:Show(args.sourceName)
 --		timerBlackrockBarrageCD:Start()
+		if self.vb.barrageCount == 3 then--Always in sets of 3
+			self.vb.barrageCount = 0
+		end
 	elseif spellId == 156240 then
 		warnAcidTorrent:Show()
 		specWarnAcidTorrent:Show()
---		timerAcidTorrentCD:Start()
+		timerAcidTorrentCD:Start()
 	elseif spellId == 156179 then
 		self:BossTargetScanner(77182, "RetchedBlackrockTarget", 0.02, 16)
+		timerRetchedBlackrockCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 156370 then
-		warnExplosiveShard:Show(args.destName)
-		if args:IsPlayer() then
-			specWarnExplosiveShard:Show()
-			yellExplosiveShard:Yell()
-			if self.Options.RangeFrame then
-				DBM.RangeCheck:Show(7)
-			end
-		end
-	elseif spellId == 155898 then
-		warnRollingFury:Show(args.destName)
-	elseif spellId == 155819 then
+	if spellId == 155819 then
 		local amount = args.amount or 1
-		warnHungerDrive:Show(args.destName, amount)
+		warnHungerDriveStack:Show(args.destName, amount)
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 156370 and args:IsPlayer() and self.Options.RangeFrame then
-		DBM.RangeCheck:Hide()
-	elseif spellId == 155898 then
-		specWarnRollingFuryEnded:Show()
+	if spellId == 155819 then
+		specWarnHungerDriveEnded:Show()
+		self.vb.barrageCount = 0
 	end
 end
 
---[[
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+	if spellId == 156390 then
+		warnExplosiveShard:Show()
+		specWarnExplosiveShard:Show()
+		timerExplosiveShardCD:Start()
+	end
+end
+
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
+	if spellId == 156203 and destGUID == UnitGUID("player") and self:AntiSpam(3, 2) then
+		specWarnRetchedBlackrock:Show()
+	end
+end
+mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
+
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if spellId == 50630 and self:AntiSpam(2, 3) then--Eject All Passengers:
-	
+	if spellId == 165127 then--Hunger Dive Phase
+		timerRetchedBlackrockCD:Cancel()
+		timerAcidTorrentCD:Cancel()
+		timerExplosiveShardCD:Cancel()
+		warnHungerDrive:Show()
+		specWarnHungerDrive:Show()
 	end
 end
-
-function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
-	if msg:find("cFFFF0404") then
-
-	elseif msg:find(L.tower) then
-
-	end
-end
-
-function mod:OnSync(msg)
-	if msg == "Adds" and self:AntiSpam(20, 4) and self:IsInCombat() then
-
-	end
-end--]]
