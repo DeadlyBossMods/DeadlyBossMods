f2ddb75fcdb180cbedfdc7d329089e2f36dd8770
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-08-17 10:34:08 +0000

    tag as 5.4.18

---
+++ tags/5.4.18/DBM-MogushanVaults/localization.es.lua
@@ -0,0 +1,110 @@
+﻿if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
+local L
+
+------------
+-- The Stone Guard --
+------------
+L= DBM:GetModLocalization(679)
+
+L:SetWarningLocalization({
+	SpecWarnOverloadSoon		= "¡%s casteable en 7s!",
+	specWarnBreakJasperChains	= "¡Rompe las cadenas de jade!"
+})
+
+L:SetOptionLocalization({
+	SpecWarnOverloadSoon		= "Mostrar aviso especial antes de sobrecarga",
+	specWarnBreakJasperChains	= "Mostrar un aviso especial cuando sea seguro romper break $spell:130395",
+	ArrowOnJasperChains			= "Mostrar flecha cuando te afecten $spell:130395",
+	InfoFrame					= "Mostrar información para poder del boss, petrificación de jugador y que boss está canalizando petrificación"
+})
+
+L:SetMiscLocalization({
+	Overload	= "¡%s se empieza a sobrecargar!"
+})
+
+
+------------
+-- Feng the Accursed --
+------------
+L= DBM:GetModLocalization(689)
+
+L:SetWarningLocalization({
+	WarnPhase	= "Fase %d"
+})
+
+L:SetOptionLocalization({
+	WarnPhase	= "Anunciar transición de fase",
+	RangeFrame	= "Mostrar distancia (6) durante la fase arcana"
+})
+
+L:SetMiscLocalization({
+	Fire		= "¡Oh, exaltado! ¡Soy tu herramienta para desgarrar la carne de los huesos!",
+	Arcane		= "¡Oh, sabio eterno! ¡Transmíteme tu sapiencia Arcana!",
+	Nature		= "¡Oh, gran espíritu! ¡Otórgame el poder de la tierra!",
+	Shadow		= "Great soul of champions past! Bear to me your shield!"--translate
+})
+
+
+-------------------------------
+-- Gara'jal the Spiritbinder --
+-------------------------------
+L= DBM:GetModLocalization(682)
+
+L:SetMiscLocalization({
+	Pull		= "¡Ya es hora de morir!"
+})
+
+
+----------------------
+-- The Spirit Kings --
+----------------------
+L = DBM:GetModLocalization(687)
+
+L:SetWarningLocalization({
+	DarknessSoon		= "Escudo de la oscuridad en %ds"
+})
+
+L:SetOptionLocalization({
+	DarknessSoon		= "Mostrar pre-aviso con contador para for $spell:117697 (5s antes)",
+	RangeFrame			= "Mostrar distancia (8)"
+})
+
+
+------------
+-- Elegon --
+------------
+L = DBM:GetModLocalization(726)
+
+L:SetWarningLocalization({
+	specWarnDespawnFloor		= "¡Evacúen la Pista!"
+})
+
+L:SetTimerLocalization({
+	timerDespawnFloor			= "¡Evacúen la Pista!"
+})
+
+L:SetOptionLocalization({
+	specWarnDespawnFloor		= "Mostrar aviso especial antes de que el suelo se desaparezca",
+	timerDespawnFloor			= "Mostrar tiempo para que el suelo desaparezca"
+})
+
+
+------------
+-- Will of the Emperor --
+------------
+L= DBM:GetModLocalization(677)
+
+L:SetOptionLocalization({
+	InfoFrame		= "Mostrar información de jugadores a los que les afecta $spell:116525",
+	CountOutCombo	= "Cuenta lanzamientos de $journal:5673<br/>NOTA: Solo en la versión de voz femenina.",
+	ArrowOnCombo	= "Mostrar flecha durante $journal:5673<br/>NOTA: Esto asume que el tanque está delante del boss y todos los demás detrás."
+})
+
+L:SetMiscLocalization({
+	Pull		= "¡La máquina vuelve a la vida! ¡Baja el nivel inferior!",
+	Rage		= "La ira del Emperador resuena por las colinas.",
+	Strength	= "¡La fuerza del Emperador aparece en la habitación!",
+	Courage		= "¡El coraje del Emperador aparece en la habitación!",
+	Boss		= "¡Aparecen dos construcciones titánicas en las enormes habitaciones!"
+})
+
---
+++ tags/5.4.18/DBM-ThroneofThunder/ToTTrash.lua
@@ -0,0 +1,160 @@
+local mod	= DBM:NewMod("ToTTrash", "DBM-ThroneofThunder")
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+mod:SetModelID(47785)
+mod:SetZone()
+
+mod.isTrashMod = true
+
+mod:RegisterEvents(
+	"SPELL_CAST_START 139895 136751 139899",
+	"SPELL_AURA_APPLIED 139322 139900 140296",
+	"UNIT_DIED",
+	"UNIT_SPELLCAST_SUCCEEDED target focus"
+)
+
+local warnStormEnergy			= mod:NewTargetAnnounce(139322, 4)
+local warnSpiritFire			= mod:NewTargetAnnounce(139895, 3)--This is morchok entryway trash that throws rocks at random poeple.
+local warnShadowNova			= mod:NewCastAnnounce(139899, 4)
+local warnStormCloud			= mod:NewTargetAnnounce(139900, 4)
+local warnFixated				= mod:NewSpellAnnounce(140306, 3)
+local warnConductiveShield		= mod:NewTargetAnnounce(140296, 4)
+
+local specWarnStormEnergy		= mod:NewSpecialWarningYou(139322)
+local specWarnShadowNova		= mod:NewSpecialWarningRun(139899, mod:IsMelee(), nil, nil, 3)--This hurls you pretty damn far. If you aren't careful you're as good as gone.
+local specWarnStormCloud		= mod:NewSpecialWarningYou(139900)
+local specWarnSonicScreech		= mod:NewSpecialWarningInterrupt(136751)
+local specWarnConductiveShield	= mod:NewSpecialWarningTarget(140296)
+
+local timerSpiritfireCD			= mod:NewCDTimer(12, 139895)
+local timerShadowNovaCD			= mod:NewCDTimer(12, 139899)
+local timerFixatedCD			= mod:NewNextTimer(15.7, 140306)
+local timerConductiveShield		= mod:NewTargetTimer(10, 140296)
+local timerConductiveShieldCD	= mod:NewCDSourceTimer(20, 140296)--On 25 man, it always 20, But 10 man, it variables.
+
+mod:RemoveOption("HealthFrame")
+mod:RemoveOption("SpeedKillTimer")
+mod:AddBoolOption("RangeFrame")
+
+local stormEnergyTargets = {}
+local stormCloudTargets = {}
+
+local function warnStormEnergyTargets()
+	warnStormEnergy:Show(table.concat(stormEnergyTargets, "<, >"))
+	table.wipe(stormEnergyTargets)
+end
+
+local function warnStormCloudTargets()
+	warnStormCloud:Show(table.concat(stormCloudTargets, "<, >"))
+	table.wipe(stormCloudTargets)
+end
+
+local function hideRangeFrame()
+	DBM.RangeCheck:Hide()
+end
+
+local function SpiritFireTarget(sGUID)
+	local targetname = nil
+	for uId in DBM:GetGroupMembers() do
+		if UnitGUID(uId.."target") == sGUID then
+			targetname = DBM:GetUnitFullName(uId.."targettarget")
+			break
+		end
+	end
+	if targetname and mod:AntiSpam(2, targetname) then--Anti spam using targetname as an identifier, will prevent same target being announced double/tripple but NOT prevent multiple targets being announced at once :)
+		warnSpiritFire:Show(targetname)
+	end
+end
+
+function mod:SPELL_CAST_START(args)
+	if not self.Options.Enabled then return end
+	local spellId = args.spellId
+	if spellId == 139895 then
+		self:Schedule(0.2, SpiritFireTarget, args.sourceGUID)
+		timerSpiritfireCD:Start()
+		if self.Options.RangeFrame and not DBM.RangeCheck:IsShown() then
+			DBM.RangeCheck:Show(3)
+		end
+	elseif spellId == 136751 and (args.sourceGUID == UnitGUID("target") or args.sourceGUID == UnitGUID("focus")) then
+		specWarnSonicScreech:Show(args.sourceName)
+	elseif spellId == 139899 then
+		warnShadowNova:Show()
+		specWarnShadowNova:Show()
+		timerShadowNovaCD:Start()
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if not self.Options.Enabled then return end
+	local spellId = args.spellId
+	if spellId == 139322 then--Or 139559, not sure which
+		stormEnergyTargets[#stormEnergyTargets + 1] = args.destName
+		if args:IsPlayer() then
+			specWarnStormEnergy:Show()
+		end
+		if self.Options.RangeFrame and not DBM.RangeCheck:IsShown() then
+			DBM.RangeCheck:Show(10)
+		end
+		self:Unschedule(warnStormEnergyTargets)
+		self:Schedule(1.5, warnStormEnergyTargets)--For some reason debuffs can go out as slow as 1.2, set to 1.5 in case it can get even worse then that
+	elseif spellId == 139900 then
+		stormCloudTargets[#stormCloudTargets + 1] = args.destName
+		if args:IsPlayer() then
+			specWarnStormCloud:Show()
+		end
+		if self.Options.RangeFrame and not DBM.RangeCheck:IsShown() then
+			DBM.RangeCheck:Show(10)
+		end
+		self:Unschedule(warnStormCloudTargets)
+		self:Schedule(0.5, warnStormCloudTargets)
+	elseif spellId == 140296 then
+		warnConductiveShield:Show(args.destName)
+		timerConductiveShield:Start(nil, args.destName)
+		timerConductiveShieldCD:Start(20, args.destName, args.sourceGUID)
+		if self:AntiSpam(3, 2) then
+			specWarnConductiveShield:Show(args.destName)
+		end
+	end
+end
+
+function mod:UNIT_DIED(args)
+	if not self.Options.Enabled then return end
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 70308 then--Soul-Fed Construct
+		timerSpiritfireCD:Cancel()
+		if self.Options.RangeFrame then
+			self:Schedule(3, hideRangeFrame)
+		end
+	elseif cid == 70440 then--Monara
+		timerShadowNovaCD:Cancel()
+	elseif cid == 70236 then--Zandalari Storm-Caller
+		if self.Options.RangeFrame then
+			self:Schedule(3, hideRangeFrame)
+		end
+	elseif cid == 70445 then--Stormbringer Draz'kil
+		if self.Options.RangeFrame then
+			self:Schedule(3, hideRangeFrame)
+		end
+	elseif cid == 69834 or cid == 69821 then
+		timerConductiveShield:Cancel(args.destName)
+		timerConductiveShieldCD:Cancel(args.destName, args.destGUID)
+	elseif cid == 68220 then--Gastropod
+		timerFixatedCD:Cancel(args.destGUID)
+	end
+end
+
+--"<1.0 17:57:05> [UNIT_SPELLCAST_SUCCEEDED] Gastropod [[target:Fixated::0:140306]]", -- [23]
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
+	if not self.Options.Enabled then return end
+	if spellId == 140306 and self:AntiSpam(3, 2) then
+		self:SendSync("OMGSnail", UnitGUID(uId))
+	end
+end
+
+function mod:OnSync(msg, guid)
+	if msg == "OMGSnail" and guid  then
+		warnFixated:Show()
+		timerFixatedCD:Start(nil, guid)
+	end
+end
