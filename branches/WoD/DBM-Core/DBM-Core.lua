66f7805896c05a5352a71a9a7cda70c5dd156b2f
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-08-18 01:36:51 +0000

    # Revert changes. (C_Scenario.IsInScenario() is fucked up. shit blizzzzzzzzzzzzzzzzzzzzzzzzzzzzz!!!!!!!!!!!!!!!!)

--- branches/WoD/DBM-Core/DBM-Core.lua
+++ branches/WoD/DBM-Core/DBM-Core.lua
@@ -2508,7 +2508,7 @@ function DBM:CINEMATIC_START()
 end
 
 function DBM:LFG_COMPLETION_REWARD()
-	if #inCombat > 0 and IsInScenarioGroup() then
+	if #inCombat > 0 and C_Scenario.IsInScenario() then
 		for i = #inCombat, 1, -1 do
 			local v = inCombat[i]
 			if v.inScenario then
@@ -3899,7 +3899,7 @@ function DBM:StartCombat(mod, delay, event, synced, syncedStartHp)
 		savedDifficulty, difficultyText, difficultyIndex, LastGroupSize = DBM:GetCurrentInstanceDifficulty()
 		local name = mod.combatInfo.name
 		local modId = mod.id
-		if IsInScenarioGroup() then
+		if C_Scenario.IsInScenario() and (mod.type == "SCENARIO") then
 			mod.inScenario = true
 		end
 		mod.inCombat = true
