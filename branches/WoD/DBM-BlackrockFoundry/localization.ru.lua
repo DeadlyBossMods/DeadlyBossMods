bdbe76bc6b2486bbb54793902cce05ae2210ec67
TOM_RUS <TOM_RUS@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-09-01 22:12:34 +0000

    ruRU update

---
+++ branches/WoD/DBM-BlackrockFoundry/localization.ru.lua
@@ -0,0 +1,78 @@
+﻿if GetLocale() ~= "ruRU" then return end
+local L
+
+---------------
+-- Gruul --
+---------------
+L= DBM:GetModLocalization(1161)
+
+L:SetWarningLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+---------------------------
+-- Oregorger, The Devourer --
+---------------------------
+L= DBM:GetModLocalization(1202)
+
+---------------------------
+-- The Blast Furnace --
+---------------------------
+L= DBM:GetModLocalization(1154)
+
+------------------
+-- Hans'gar And Franzok --
+------------------
+L= DBM:GetModLocalization(1155)
+
+--------------
+-- Flamebender Ka'graz --
+--------------
+L= DBM:GetModLocalization(1123)
+
+--------------------
+--Kromog, Legend of the Mountain --
+--------------------
+L= DBM:GetModLocalization(1162)
+
+--------------------------
+-- Beastlord Darmac --
+--------------------------
+L= DBM:GetModLocalization(1122)
+
+--------------------------
+-- Operator Thogar --
+--------------------------
+L= DBM:GetModLocalization(1147)
+
+L:SetMiscLocalization({
+	helperMessage	= "Рекомендуется использовать 'Thogar Assist' в комбинации с DBM на этом боссе. Доступен на: http://wow.curseforge.com/addons/thogar-assist/"
+})
+
+--------------------------
+-- The Iron Maidens --
+--------------------------
+L= DBM:GetModLocalization(1203)
+
+L:SetMiscLocalization({
+	shipMessage		= "prepares to man the Dreadnaught's Main Cannon!"
+})
+
+--------------------------
+-- Blackhand --
+--------------------------
+L= DBM:GetModLocalization(959)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("BlackrockFoundryTrash")
+
+L:SetGeneralLocalization({
+	name =	"Трэш мобы Литейной клана Черной горы"
+})
