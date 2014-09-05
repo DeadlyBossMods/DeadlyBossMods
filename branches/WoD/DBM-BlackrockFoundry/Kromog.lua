9dfb9f56e57760291586b8429a44c154a35a8c2b
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-09-05 19:46:26 +0000

    Kromog updates, need better logs (longer pulls) to finish it though.

--- branches/WoD/DBM-BlackrockFoundry/Kromog.lua
+++ branches/WoD/DBM-BlackrockFoundry/Kromog.lua
@@ -9,21 +9,23 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 157060 157054 156704 157592 158215",
+	"SPELL_CAST_START 157060 157054 156704 157592 158217",
 	"SPELL_CAST_SUCCESS 158130 170469",
-	"SPELL_AURA_APPLIED 156766 161923",
+	"SPELL_AURA_APPLIED 156766 161923 173917",
 	"SPELL_AURA_APPLIED_DOSE 156766"
 )
 
---Figure out how Reverberations (http://beta.wowhead.com/spell=157247) work. (Pretty sure it's one of the "dummy nuke" spellids. Need to find out which one
+--TODO, see if normal/heroic is still have 111sec timer on grasping Earth since mythic was about 123
+--TODO, see how second trembling earth CD works and if current code even works for other timers. Mythic pulls were very short :\
 local warnGraspingEarth				= mod:NewSpellAnnounce(157060, 3)
 local warnThunderingBlows			= mod:NewSpellAnnounce(157054, 4)
 local warnRipplingSmash				= mod:NewSpellAnnounce(157592, 3)
 local warnCrushingEarth				= mod:NewTargetAnnounce(161923, 3, nil, false)--Players who failed to move. Off by default since announcing failures is not something DBM generally does by default. Can't announce pre cast unfortunately. No detection
 local warnStoneGeyser				= mod:NewSpellAnnounce(158130, 2)
-local warnCalloftheMountain			= mod:NewSpellAnnounce(158217, 3)
 local warnSlam						= mod:NewCastAnnounce(156704, 4, nil, nil, mod:IsMelee())
 local warnWarpedArmor				= mod:NewStackAnnounce(156766, 2, nil, mod:IsTank())
+local warnTremblingEarth			= mod:NewSpellAnnounce(173917, 3)--Mythic
+local warnCalloftheMountain			= mod:NewCastAnnounce(158217, 4, 5)--Mythic
 
 local specWarnGraspingEarth			= mod:NewSpecialWarningSpell(157060)
 local specWarnThunderingBlows		= mod:NewSpecialWarningSpell(157054, nil, nil, nil, 3)
@@ -31,6 +33,8 @@ local specWarnRipplingSmash			= mod:NewSpecialWarningSpell(157592, nil, nil, nil
 local specWarnSlam					= mod:NewSpecialWarningSpell(156704, mod:IsTank())
 local specWarnWarpedArmor			= mod:NewSpecialWarningStack(156766, nil, 3)--stack bugged right now, requires tanks going to 5 stacks before they can clear. Blizz will likely fix this because 5 too much
 local specWarnWarpedArmorOther		= mod:NewSpecialWarningTaunt(156766)
+local specWarnTremblingEarth		= mod:NewSpecialWarningSpell(173917, nil, nil, nil, 2)
+local specWarnCalloftheMountain		= mod:NewSpecialWarningCount(158217, nil, nil, nil, 3)
 
 local timerGraspingEarthCD			= mod:NewCDTimer(111, 157060)
 local timerThunderingBlowsCD		= mod:NewNextTimer(12, 157054)
@@ -38,10 +42,17 @@ local timerRipplingSmashCD			= mod:NewCDTimer(22, 157592)--If it comes off CD ea
 --local timerStoneGeyserCD			= mod:NewNextTimer(30, 158130)
 local timerSlamCD					= mod:NewCDTimer(28, 156704, nil, mod:IsTank())
 local timerWarpedArmorCD			= mod:NewCDTimer(14, 156766, nil, mod:IsTank())
+local timerTremblingEarthCD			= mod:NewNextTimer(30, 173917)
+local timerTremblingEarth			= mod:NewBuffActiveTimer(25, 173917)
+local timerCalloftheMountain		= mod:NewCastTimer(5, 158217)
 
 local berserkTimer					= mod:NewBerserkTimer(600)
 
+mod.vb.mountainCast = 0
+
 function mod:OnCombatStart(delay)
+	self.vb.mountainCast = 0
+	timerWarpedArmorCD:Start(15-delay)
 	timerRipplingSmashCD:Start(20-delay)
 	timerSlamCD:Start(25-delay)--More data needed
 	timerGraspingEarthCD:Start(53-delay)
@@ -54,10 +65,15 @@ function mod:SPELL_CAST_START(args)
 		warnGraspingEarth:Show()
 		specWarnGraspingEarth:Show()
 		timerThunderingBlowsCD:Start()
-		timerGraspingEarthCD:Start()
 		timerSlamCD:Cancel()--Can't cast slam during this
 		timerRipplingSmashCD:Cancel()--Or rippling
 		timerWarpedArmorCD:Cancel()
+		if self:IsMythic() then
+			timerTremblingEarthCD:Start()
+			timerGraspingEarthCD:Start(123)--TODO, see if normal is still 111 after last
+		else
+			timerGraspingEarthCD:Start()
+		end
 	elseif spellId == 157054 then
 		warnThunderingBlows:Show()
 		specWarnThunderingBlows:Show()
@@ -71,8 +87,11 @@ function mod:SPELL_CAST_START(args)
 		warnSlam:Show()
 		specWarnSlam:Show()
 		timerSlamCD:Start()
-	elseif spellId == 158215 then--Probably not in combat log, it's scripted. Probably needs a UNIT_SPELLCAST event
+	elseif spellId == 158217 then--Probably not in combat log, it's scripted. Probably needs a UNIT_SPELLCAST event
+		self.vb.mountainCast = self.vb.mountainCast + 1
 		warnCalloftheMountain:Show()
+		specWarnCalloftheMountain:Show(self.vb.mountainCast)
+		timerCalloftheMountain:Start()
 	end
 end
 
@@ -101,6 +120,13 @@ function mod:SPELL_AURA_APPLIED(args)
 		end
 	elseif spellId == 161923 then
 		warnCrushingEarth:CombinedShow(0.5, args.destName)
+	elseif spellId == 173917 then
+		warnTremblingEarth:Show()
+		specWarnTremblingEarth:Show()
+		timerTremblingEarth:Start()
+		timerSlamCD:Cancel()--Can't cast slam during this
+		timerRipplingSmashCD:Cancel()--Or rippling
+		timerWarpedArmorCD:Cancel()
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
