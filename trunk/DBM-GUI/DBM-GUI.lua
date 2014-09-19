93b8b471afe8240f25ebcf5f6280f23b6fe04270
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-09-19 00:55:57 +0000

    Allow manual loading of worldbosses

--- trunk/DBM-GUI/DBM-GUI.lua
+++ trunk/DBM-GUI/DBM-GUI.lua
@@ -2979,7 +2979,7 @@ do
 	end
 
 	local function LoadAddOn_Button(self)
-		if DBM:LoadMod(self.modid) then
+		if DBM:LoadMod(self.modid, true) then
 			self:Hide()
 			self.headline:Hide()
 			CreateBossModTab(self.modid, self.modid.panel)
