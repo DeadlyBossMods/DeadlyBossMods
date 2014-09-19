492e46d0cc31b5c5d1320509dab4b708103ff982
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-09-19 03:22:27 +0000

    # koKR update.

---
+++ branches/WoD/DBM-Draenor/localization.kr.lua
@@ -0,0 +1,35 @@
+﻿if GetLocale() ~= "koKR" then return end
+local L
+
+-----------------------
+-- Drov the Ruiner --
+-----------------------
+L= DBM:GetModLocalization(1291)
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+--	Pull				= "Yes, YES! Bring your rage to bear! Try to strike me down!"
+})
+
+-----------------------
+-- Tarlna the Ageless --
+-----------------------
+L= DBM:GetModLocalization(1211)
+
+L:SetMiscLocalization({
+--	Pull				= "Bring me their corpses!"
+})
+
+--------------
+-- Rukhmar --
+--------------
+L= DBM:GetModLocalization(1262)
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+--	Pull				= "How dare you interrupt our preparations! The Zandalari will not be stopped, not this time!"
+})
