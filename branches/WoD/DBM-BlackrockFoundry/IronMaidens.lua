efae1a5f8a23a804fdd3630f2b788b0a4148ec15
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-09-20 01:50:35 +0000

    Remove the 3 abilities deleted from iron maidens. Add icon option for two debuffs that now appear in combat log (yay).

--- branches/WoD/DBM-BlackrockFoundry/IronMaidens.lua
+++ branches/WoD/DBM-BlackrockFoundry/IronMaidens.lua
@@ -6,22 +6,24 @@ mod:SetCreatureID(77557, 77231, 77477)
 mod:SetEncounterID(1695)
 mod:SetZone()
 mod:SetBossHPInfoToHighest()
+mod:SetUsedIcons(2, 1)
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 158849 158695 158708 158707 158710 158692 156683 158599 155794 156366 158078 156626",
+	"SPELL_CAST_START 158849 158695 158708 158707 158710 158692 158599 155794 158078 156626",
 	"SPELL_CAST_SUCCESS 158701 157854 157886",
-	"SPELL_AURA_APPLIED 158702 164271 156214 156007 158315 157950",
+	"SPELL_AURA_APPLIED 158702 164271 156214 158315 157950 159724 156626",
+	"SPELL_AURA_REMOVED 159724",
 	"SPELL_PERIODIC_DAMAGE 158683",
 	"SPELL_PERIODIC_MISSED 158683",
 	"RAID_BOSS_WHISPER",
 	"CHAT_MSG_RAID_BOSS_EMOTE"
 )
 
---TODO, find a way, either by map or unit power to filter ship deck from ground warnings
 --TODO, find out how many bombardments there are so timer doesn't start after last one.
 --TODO, get longer log to verify all missing drycoded abilities.
+--TODO, timer recovery when leaving boat back to main platform (how do we detect leaving boat? never gotten a log where people return.)
 --Ship
 local warnShip							= mod:NewSpellAnnounce("ej10019", 3, 76204)
 local warnBombardmentAlpha				= mod:NewSpellAnnounce(157854, 3)--From ship, but affects NON ship.
@@ -39,17 +41,14 @@ local warnFixate						= mod:NewTargetAnnounce(158702, 3)
 --Ground
 ----Admiral Gar'an
 local warnRapidFire						= mod:NewTargetAnnounce(156631, 3)
-local warnIncendiaryDevice				= mod:NewSpellAnnounce(156683, 4)--Mythic. How do you determine target, must find that out!
 local warnPenetratingShot				= mod:NewTargetAnnounce(164271, 3)--Could not verify
 local warnDeployTurret					= mod:NewSpellAnnounce(158599, 3)--Could not verify
 ----Enforcer Sorka
 local warnBladeDash						= mod:NewTargetAnnounce(155794, 3)
 local warnConvulsiveShadows				= mod:NewTargetAnnounce(156214, 4)
-local warnImpale						= mod:NewTargetAnnounce(156007, 3)--Could not verify
 local warnDarkHunt						= mod:NewTargetAnnounce(158315, 4)--Could not verify
 ----Marak the Blooded
 local warnBloodRitual					= mod:NewTargetAnnounce(158078, 3)
-local warnWhirlOfBlood					= mod:NewSpellAnnounce(156366, 3)--Mythic
 local warnBloodsoakedHeartseeker		= mod:NewTargetAnnounce(157950, 3)--Probably hidden from combatlog, will need alternate detection i'm sure. Blizzard likes pointless extra work.
 
 --Ship
@@ -67,7 +66,6 @@ local specWarnCorruptedBlood			= mod:NewSpecialWarningMove(158683)
 ----Admiral Gar'an
 local specWarnRapidFire					= mod:NewSpecialWarningYou(156631)
 local yellRapidFire						= mod:NewYell(156631)
-local specWarnIncendiaryDevice			= mod:NewSpecialWarningSpell(156683, nil, nil, nil, 3)
 local specWarnPenetratingShot			= mod:NewSpecialWarningYou(164271)
 local yellPenetratingShot				= mod:NewYell(164271)
 local specWarnDeployTurret				= mod:NewSpecialWarningSpell(158599, nil, nil, nil, 2)
@@ -81,7 +79,6 @@ local specWarnDarkHunt					= mod:NewSpecialWarningTarget(158315, false)--Healer 
 local specWarnBloodRitual				= mod:NewSpecialWarningYou(158078)
 local specWarnBloodRitualOther			= mod:NewSpecialWarningTarget(158078, mod:IsTank())
 local yellBloodRitual					= mod:NewYell(158078)
-local specWarnWhirlOfBlood				= mod:NewSpecialWarningSpell(156366, nil, nil, nil, 2)--Mythic
 local specWarnBloodsoakedHeartseeker	= mod:NewSpecialWarningYou(157950)
 local yellBloodsoakedHeartseeker		= mod:NewYell(157950)
 
@@ -99,6 +96,9 @@ local timerRapidFireCD					= mod:NewNextTimer(30, 156626)
 local timerBloodRitualCD				= mod:NewNextTimer(12, 158078)
 ----Marak the Blooded
 local timerBladeDashCD					= mod:NewNextTimer(20, 155794)
+
+mod:AddSetIconOption("SetIconOnRapidFire", 156626, true)
+mod:AddSetIconOption("SetIconOnBloodRitual", 158078, true)
 
 local Marak = EJ_GetSectionInfo(10033)
 local Sorka = EJ_GetSectionInfo(10030)
@@ -132,10 +132,6 @@ function mod:OnCombatStart(delay)
 	timerBladeDashCD:Start(11-delay)
 	timerRapidFireCD:Start(16-delay)
 	timerShipCD:Start(-delay)
-	if not self.Options.ShowedLimitationMessage then
-		DBM:AddMsg(L.warningNotice)
-		self.Options.ShowedLimitationMessage = true
-	end
 end
 
 function mod:OnCombatEnd()
@@ -145,10 +141,7 @@ end
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	local noFilter = not DBM.Options.DontShowFarWarnings
-	if spellId == 156683 and (noFilter or not isPlayerOnBoat()) then
-		warnIncendiaryDevice:Show()
-		specWarnIncendiaryDevice:Show()
-	elseif spellId == 158078 and (noFilter or not isPlayerOnBoat()) then--Blood Ritual. Still safest way to start timer, in case no sync
+	if spellId == 158078 and (noFilter or not isPlayerOnBoat()) then--Blood Ritual. Still safest way to start timer, in case no sync
 		timerBloodRitualCD:Start()
 	elseif spellId == 156626 and (noFilter or not isPlayerOnBoat()) then--Rapid Fire. Still safest way to start timer, in case no sync
 		timerRapidFireCD:Start()
@@ -158,9 +151,6 @@ function mod:SPELL_CAST_START(args)
 	elseif spellId == 155794 and (noFilter or not isPlayerOnBoat()) then
 		self:BossTargetScanner(77231, "BladeDashTarget", 0.1, 16)
 		timerBladeDashCD:Start()
-	elseif spellId == 156366 and (noFilter or not isPlayerOnBoat()) then
-		warnWhirlOfBlood:Show()
-		specWarnWhirlOfBlood:Show()
 	--Begin Deck Abilities
 	elseif spellId == 158849 then
 		warnWarmingUp:Show()
@@ -213,8 +203,6 @@ function mod:SPELL_AURA_APPLIED(args)
 			specWarnConvulsiveShadows:Show()
 			yellConvulsiveShadows:Yell()
 		end
-	elseif spellId == 156007 and (noFilter or not isPlayerOnBoat()) then
-		warnImpale:Show(args.destName)
 	elseif spellId == 158315 and (noFilter or not isPlayerOnBoat()) then
 		warnDarkHunt:Show(args.destName)
 		specWarnDarkHunt:Show()
@@ -224,15 +212,39 @@ function mod:SPELL_AURA_APPLIED(args)
 			specWarnBloodsoakedHeartseeker:Show()
 			yellBloodsoakedHeartseeker:Yell()
 		end
+	elseif spellId == 159724 and (noFilter or not isPlayerOnBoat()) then
+		warnBloodRitual:Show(args.destName)
+		specWarnBloodRitualOther:Show(args.destName)
+		if args:IsPlayer() then
+			specWarnBloodRitual:Show()
+			yellBloodRitual:Yell()
+		end
+		if self.Options.SetIconOnBloodRitual then
+			self:SetIcon(args.destName, 2)
+		end
+	elseif spellId == 156626 and (noFilter or not isPlayerOnBoat()) then
+		if self:AntiSpam(5, args.destName) then--check antispam so we don't warn if we got a user sync 3 seconds ago.
+			warnRapidFire:Show(args.destName)
+			if self.Options.SetIconOnRapidFire then
+				self:SetIcon(args.destName, 1, 7)
+			end
+		end
 	--Begin Deck Abilities
 	elseif spellId == 158702 and (noFilter or isPlayerOnBoat()) then
 		warnFixate:Show(args.destName)
 		if args:IsPlayer() and self:AntiSpam(3, 1) then--it spams sometimes
 			specWarnFixate:Show()
 		end
+		
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+
+function mod:SPELL_AURA_REMOVED(args)
+	if spellId == 159724 and self.Options.SetIconOnBloodRitual then
+		self:SetIcon(args.destName, 0)
+	end
+end
 
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 	if spellId == 158683 and destGUID == UnitGUID("player") and self:AntiSpam(2, 2) then
@@ -241,13 +253,9 @@ function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
---Neither one of these show in combat log at all. What else is new
+--Rapid fire is still 3 seconds faster to use emote instead of debuff.
 function mod:RAID_BOSS_WHISPER(msg)
-	if msg:find("spell:158079") then
-		specWarnBloodRitual:Show()
-		yellBloodRitual:Yell()
-		self:SendSync("BloodRitualTarget", UnitGUID("player"))
-	elseif msg:find("spell:156626") then
+	if msg:find("spell:156626") then
 		specWarnRapidFire:Show()
 		yellRapidFire:Yell()
 		self:SendSync("RapidFireTarget", UnitGUID("player"))
@@ -263,13 +271,14 @@ end
 function mod:OnSync(msg, guid)
 	if not self:IsInCombat() then return end
 	if (not DBM.Options.DontShowFarWarnings or isPlayerOnBoat()) then return end--Anything below this line doesn't concern people on boat
-	if msg == "BloodRitualTarget" and guid then
+	if msg == "RapidFireTarget" and guid then
 		local targetName = DBM:GetFullPlayerNameByGUID(guid)
-		warnBloodRitual:Show(targetName)
-		specWarnBloodRitualOther:Show(targetName)
-	elseif msg == "RapidFireTarget" and guid then
-		local targetName = DBM:GetFullPlayerNameByGUID(guid)
-		warnRapidFire:Show(targetName)
+		if self:AntiSpam(5, targetName) then--Set antispam if we got a sync, to block 3 second late SPELL_AURA_APPLIED if we got the early warning
+			warnRapidFire:Show(targetName)
+			if self.Options.SetIconOnRapidFire then
+				self:SetIcon(targetName, 1, 10)
+			end
+		end
 	elseif msg == "Ship" and guid then--technically not guid but it's fine.
 		warnShip:Show()
 		timerBombardmentAlphaCD:Start(15)
