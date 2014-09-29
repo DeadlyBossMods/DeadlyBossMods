22e27b258ed92e793f07f5716d0cb73a6dc03e55
Adam <MysticalOS@users.noreply.github.com>
2014-09-29 23:42:32 +0000

    don't apply tooltip disable to scenarios

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -3921,7 +3921,7 @@ function DBM:StartCombat(mod, delay, event, synced, syncedStartHp)
 				watchFrameRestore = true
 			end
 		end
-		if DBM.Options.HideTooltips then
+		if DBM.Options.HideTooltips and not mod.inScenario then
 			--Better or cleaner way?
 			GameTooltip.Temphide = function() GameTooltip:Hide() end; GameTooltip:SetScript("OnShow", GameTooltip.Temphide)
 		end
@@ -4286,7 +4286,7 @@ function DBM:EndCombat(mod, wipe)
 				WatchFrame:Show()
 				watchFrameRestore = false
 			end
-			if DBM.Options.HideTooltips then
+			if DBM.Options.HideTooltips and not mod.inScenario then
 				--Better or cleaner way?
 				GameTooltip:SetScript("OnShow", GameTooltip.Show)
 			end
