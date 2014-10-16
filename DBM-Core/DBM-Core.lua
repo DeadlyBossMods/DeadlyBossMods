ed94b6a9acc583cc94a6d357380b76fb3aba1b0b
Paul Emmerich <emmericp@net.in.tum.de>
2014-10-16 06:06:03 +0000

    increment version number

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -52,8 +52,8 @@
 -------------------------------
 DBM = {
 	Revision = tonumber(("$Revision$"):sub(12, -3)),
-	DisplayVersion = "6.0.2 alpha", -- the string that is shown as version
-	ReleaseRevision = 11772 -- the revision of the latest stable version that is available
+	DisplayVersion = "6.0.3 alpha", -- the string that is shown as version
+	ReleaseRevision = 11780 -- the revision of the latest stable version that is available
 }
 
 -- Legacy crap; that stupid "Version" field was never a good idea.
