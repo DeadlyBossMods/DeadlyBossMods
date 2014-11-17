0741d55dfb59b09b125befbf13a920cfed4b2b2a
ultrashining <ultrashining@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-11-17 01:04:14 +0000

    zhTW update

--- DBM-GUI/localization.tw.lua
+++ DBM-GUI/localization.tw.lua
@@ -6,7 +6,7 @@ L.MainFrame		 = "Deadly Boss Mods"
 
 L.TranslationByPrefix		= "翻譯:"
 L.TranslationBy 			= "Imbav@聖光之願"
-L.Website					= "拜訪我們新的討論與支援論壇:|cFF73C2FBwww.deadlybossmods.com|r (代管於Elitist Jerks!)"
+L.Website					= "拜訪我們的討論與支援論壇:|cFF73C2FBwww.deadlybossmods.com|r (代管於Elitist Jerks!)"
 L.WebsiteButton				= "論壇"
 
 L.OTabBosses	= "首領模組"
