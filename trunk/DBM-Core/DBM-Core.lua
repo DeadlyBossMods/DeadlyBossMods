fafa42fe946978a8b8c7db755aa9a9ae471bcad1
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-11-23 23:35:12 +0000

    # Fix rare version check lua error.

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -1964,6 +1964,7 @@ do
 					raidUIds[v.id] = nil
 					raidGuids[v.guid] = nil
 					raid[i] = nil
+					removeEntry(newerVersionPerson, i)
 					fireEvent("raidLeave", i)
 				else
 					v.updated = nil
@@ -2026,6 +2027,7 @@ do
 					raidUIds[v.id] = nil
 					raidGuids[v.guid] = nil
 					raid[i] = nil
+					removeEntry(newerVersionPerson, i)
 					fireEvent("partyLeave", i)
 				else
 					v.updated = nil
@@ -2047,6 +2049,7 @@ do
 			enableIcons = true
 			fireEvent("raidLeave", playerName)
 			twipe(raid)
+			twipe(newerVersionPerson)
 			-- restore playerinfo into raid table on raidleave. (for solo raid)
 			raid[playerName] = {}
 			raid[playerName].name = playerName
