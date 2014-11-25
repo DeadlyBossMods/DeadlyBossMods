94374595fdbeb10607f5c9e0e2d0c2ed2f5d798e
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-11-25 06:41:46 +0000

    Try again, i have no idea why that last commit pushed everything BUT dbm to curse.

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -47,6 +47,7 @@
 --    * Share Alike. If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one.
 --
 
+
 -------------------------------
 --  Globals/Default Options  --
 -------------------------------
