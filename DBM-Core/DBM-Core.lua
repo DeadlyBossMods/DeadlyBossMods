118b9d7c9807d85e354458e8fab79eb487c77534
Adam <MysticalOS@users.noreply.github.com>
2014-11-26 01:31:19 +0000

    Move that line

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -2640,8 +2640,8 @@ do
 		for i, v in ipairs(DBM.AddOns) do
 			local modTable = v[checkTable]
 			local enabled = GetAddOnEnableState(playerName, v.modId)
-			DBM:Debug("LoadModsOnDemand fired")
 			if not IsAddOnLoaded(v.modId) and modTable and checkEntry(modTable, checkValue) then
+				DBM:Debug("LoadModsOnDemand fired")
 				if enabled ~= 0 then
 					self:LoadMod(v)
 				else
