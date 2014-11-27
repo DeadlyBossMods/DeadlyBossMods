78b1c809415a1a642f6bb381ec3da695517ed6e5
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-11-27 08:05:12 +0000

    Fix slash warning (wrong spellid) Add Noxious warning/timer Fix missing event on Xeritac Remove a bad interrupt warning that is never right thing to do.

--- trunk/DBM-Party-WoD/EverBloom/AncientProtectors.lua
+++ trunk/DBM-Party-WoD/EverBloom/AncientProtectors.lua
@@ -9,25 +9,29 @@ mod:SetBossHPInfoToHighest()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 168082 168041 168105 168383",
-	"SPELL_AURA_APPLIED 168105 168041 168520"
+	"SPELL_CAST_START 168082 168041 168105 168383 175997",
+	"SPELL_AURA_APPLIED 168105 168041 168520",
+	"SPELL_AURA_REMOVED 168520",
+	"UNIT_DIED"
 )
 
---maybe add CD timers if i care enough to check for briarskin and cancel them if it applies to a boss
-local warnRevitalizingWaters		= mod:NewSpellAnnounce(168082, 4)--This is always followed instantly by Firestorm kick, so no reason to warn both.
-local warnBriarskin					= mod:NewTargetAnnounce(168041, 4)
-local warnRapidTides				= mod:NewTargetAnnounce(168105, 3)
-local warnSlash						= mod:NewSpellAnnounce(168383, 3)
+--Timers are too difficult to do, rapidTides messes up any chance of ever having decent timers.
+local warnRevitalizingWaters		= mod:NewSpellAnnounce(168082, 4)
+local warnBriarskin					= mod:NewTargetAnnounce("OptionVersion2", 168041, 3, nil, mod:IsMagicDispeller())
+local warnRapidTides				= mod:NewTargetAnnounce(168105, 4)
+local warnSlash						= mod:NewSpellAnnounce("OptionVersion2", 168383, 3, nil, mod:IsMelee())
+local warnNoxious					= mod:NewSpellAnnounce(175997, 3, nil, mod:IsMelee())
 local warnShapersFortitude			= mod:NewTargetAnnounce(168520, 3)
 
 local specWarnRevitalizingWaters	= mod:NewSpecialWarningInterrupt(168082, not mod:IsHealer())
 local specWarnBriarskin				= mod:NewSpecialWarningInterrupt(168041, false)--if you have more than one interruptor, great. but off by default because we can't assume you can interrupt every bosses abilities. and heal takes priority
 local specWarnBriarskinDispel		= mod:NewSpecialWarningDispel(168041, false)--Not as important as rapid Tides and to assume you have at least two dispellers is big assumption
-local specWarnRapidTides			= mod:NewSpecialWarningInterrupt(168105, false)--if you have more than one interruptor, great. but off by default because we can't assume you can interrupt every bosses abilities. and heal takes priority
-local specWarnRapidTidesDispel		= mod:NewSpecialWarningDispel(168105, mod:IsMagicDispeller())
+local specWarnRapidTidesDispel		= mod:NewSpecialWarningDispel(168105, mod:IsMagicDispeller(), nil, nil, 3)
 local specWarnSlash					= mod:NewSpecialWarningSpell(168383, mod:IsMelee(), nil, nil, 2)--Because it's 8 yard cone in random direction.
+local specWarnNoxious				= mod:NewSpecialWarningRun(175997, mod:IsMelee())
 
-local timerShapersFortitude			= mod:NewTargetTimer(15, 168520)
+local timerShapersFortitude			= mod:NewTargetTimer("OptionVersion2", 15, 168520, nil, false)
+local timerNoxiousCD				= mod:NewCDTimer(16, 175997, nil, mod:IsMelee())
 
 function mod:OnCombatStart(delay)
 
@@ -44,11 +48,13 @@ function mod:SPELL_CAST_START(args)
 		specWarnRevitalizingWaters:Show(args.sourceName)
 	elseif spellId == 168041 then
 		specWarnBriarskin:Show(args.sourceName)
-	elseif spellId == 168105 then
-		specWarnRapidTides:Show(args.sourceName)
-	elseif spellId == 168105 then
+	elseif spellId == 168383 then
 		warnSlash:Show()
 		specWarnSlash:Show()
+	elseif spellId == 175997 then
+		warnNoxious:Show()
+		specWarnNoxious:Show()
+		timerNoxiousCD:Start()
 	end
 end
 
@@ -63,5 +69,19 @@ function mod:SPELL_AURA_APPLIED(args)
 	elseif spellId == 168520 then
 		warnShapersFortitude:Show(args.destName)
 		timerShapersFortitude:Start(args.destName)
+	end
+end
+
+function mod:SPELL_AURA_REMOVED(args)
+	local spellId = args.spellId
+	if spellId == 168520 then
+		timerShapersFortitude:Cancel(args.destName)
+	end
+end
+
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 83894 then
+		timerNoxiousCD:Cancel()
 	end
 end
