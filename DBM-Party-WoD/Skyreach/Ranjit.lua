5fc8b6538db839077880fae86af894c9d96fb253
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-11-30 00:09:41 +0000

    # fix little bug.

--- DBM-Party-WoD/Skyreach/Ranjit.lua
+++ DBM-Party-WoD/Skyreach/Ranjit.lua
@@ -25,7 +25,7 @@ local timerFourWinds		= mod:NewBuffActiveTimer(18, 156793)
 local timerFourWindsCD		= mod:NewCDTimer(30, 156793)
 
 function mod:OnCombatStart(delay)
-	timerFourWinds:Start(-delay)
+	timerFourWindsCD:Start(-delay)
 end
 
 function mod:SPELL_CAST_START(args)
