95ce4185f6553f7d2fcb806f1ac870d7e0ccf559
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-11-30 00:13:56 +0000

    # disable Dark Communion countdown temporarily.

--- trunk/DBM-Party-WoD/SMBurialGrounds/SadanaBloodfury.lua
+++ trunk/DBM-Party-WoD/SMBurialGrounds/SadanaBloodfury.lua
@@ -18,14 +18,14 @@ local warnDarkEclipse			= mod:NewSpellAnnounce(164974, 4)
 local specWarnDarkCommunion		= mod:NewSpecialWarningSwitch(153153, not mod:IsHealer())--On Test, even tank and healer needed to dps to kill it. I'm going to assume it's an overtuning and at least excempt healer.
 local specWarnDarkEclipse		= mod:NewSpecialWarningSpell(164974, nil, nil, nil, 3)
 
-local timerDarkCommunionCD		= mod:NewNextTimer(45.5, 153153)
-local timerDarkEclipseCD		= mod:NewNextTimer(45.5, 164974)
+local timerDarkCommunionCD		= mod:NewCDTimer(45.5, 153153)
+local timerDarkEclipseCD		= mod:NewNextTimer(45.5, 164974)--timer seems changed?
 
-local countdownDarkCommunion	= mod:NewCountdown(45.5, 153153)
+--local countdownDarkCommunion	= mod:NewCountdown(45.5, 153153)
 
 function mod:OnCombatStart(delay)
 	timerDarkCommunionCD:Start(15-delay)
-	countdownDarkCommunion:Start(15-delay)
+	--countdownDarkCommunion:Start(15-delay)
 	timerDarkEclipseCD:Start(-delay)
 end
 
@@ -37,7 +37,7 @@ function mod:SPELL_CAST_SUCCESS(args)
 		warnDarkCommunion:Show()
 		specWarnDarkCommunion:Show()
 		timerDarkCommunionCD:Start()
-		countdownDarkCommunion:Start()
+		--countdownDarkCommunion:Start()
 	elseif spellId == 164974 then
 		specWarnDarkEclipse:Show()
 		timerDarkEclipseCD:Start()
