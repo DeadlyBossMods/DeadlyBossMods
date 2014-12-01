9af8759b96e7461fcdb9b205eb234b4010e55e40
Adam <MysticalOS@users.noreply.github.com>
2014-12-01 05:04:19 +0000

    Grim Rail update

--- DBM-Party-WoD/GrimRailDepot/BlackrockAssaultCommander.lua
+++ DBM-Party-WoD/GrimRailDepot/BlackrockAssaultCommander.lua
@@ -10,7 +10,7 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED 160681",
-	"SPELL_CAST_START 163550 160680",
+	"SPELL_CAST_START 163550 160680 160943",
 	"SPELL_PERIODIC_DAMAGE 166570",
 	"SPELL_PERIODIC_MISSED 166570",
 	"UNIT_TARGETABLE_CHANGED"
@@ -21,10 +21,12 @@ local warnPhase2				= mod:NewPhaseAnnounce(2)
 local warnSupressiveFire		= mod:NewTargetAnnounce(160681, 2)--In a repeating loop
 --local warnGrenadeDown			= mod:NewAnnounce("warnGrenadeDown", 1, "ej9711", nil, DBM_CORE_AUTO_ANNOUNCE_OPTIONS.spell:format("ej9711"))--Boss is killed by looting using these positive items on him.
 --local warnMortarDown			= mod:NewAnnounce("warnMortarDown", 4, "ej9712", nil, DBM_CORE_AUTO_ANNOUNCE_OPTIONS.spell:format("ej9712"))--So warn when adds that drop them die
+local warnShrapnelBlast			= mod:NewCastAnnounce(160943, 4)
 local warnPhase3				= mod:NewPhaseAnnounce(3)
 
 local specWarnSupressiveFire	= mod:NewSpecialWarningYou(160681)
 local yellSupressiveFire		= mod:NewYell(160681)
+local specWarnShrapnelblast		= mod:NewSpecialWarningMove(160943, mod:IsTank(), nil, nil, 3)--160943 boss version, 166675 trash version.
 local specWarnSlagBlast			= mod:NewSpecialWarningMove(166570)
 
 local timerSupressiveFire		= mod:NewTargetTimer(10, 160681)
@@ -57,6 +59,9 @@ function mod:SPELL_CAST_START(args)
 		warnMortar:Show()
 	elseif args.spellId == 160680 then
 		self:BossTargetScanner(79548, "SupressiveFireTarget", 0.2, 15)
+	elseif spellId == 160943 and self:AntiSpam(2, 1) then
+		warnShrapnelBlast:Show()
+		specWarnShrapnelblast:Show()
 	end
 end
 
