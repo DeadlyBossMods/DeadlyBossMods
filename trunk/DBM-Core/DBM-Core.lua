f6b7be22443122c63a397be0355d9356ff4a4d84
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-12-09 23:17:43 +0000

    Inc the alpha

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -53,8 +53,8 @@
 -------------------------------
 DBM = {
 	Revision = tonumber(("$Revision$"):sub(12, -3)),
-	DisplayVersion = "6.0.7 alpha", -- the string that is shown as version
-	ReleaseRevision = 11976 -- the revision of the latest stable version that is available
+	DisplayVersion = "6.0.9 alpha", -- the string that is shown as version
+	ReleaseRevision = 11978 -- the revision of the latest stable version that is available
 }
 
 -- Legacy crap; that stupid "Version" field was never a good idea.
