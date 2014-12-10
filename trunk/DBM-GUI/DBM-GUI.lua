e37b3da45faf8c66c574f665d5f8629e02fa0d8d
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-12-10 07:50:30 +0000

    sizes

--- trunk/DBM-GUI/DBM-GUI.lua
+++ trunk/DBM-GUI/DBM-GUI.lua
@@ -2409,7 +2409,7 @@ local function CreateOptionsMenu()
 
 	do
 		local hideBlizzPanel = DBM_GUI_Frame:CreateNewPanel(L.Panel_HideBlizzard, "option")
-		local hideBlizzArea = hideBlizzPanel:CreateArea(L.Area_HideBlizzard, nil, 160, true)
+		local hideBlizzArea = hideBlizzPanel:CreateArea(L.Area_HideBlizzard, nil, 200, true)
 		hideBlizzArea:CreateCheckButton(L.HideBossEmoteFrame, true, nil, "HideBossEmoteFrame")
 		hideBlizzArea:CreateCheckButton(L.HideWatchFrame, true, nil, "HideObjectivesFrame")
 		hideBlizzArea:CreateCheckButton(L.HideGarrisonUpdates, true, nil, "HideGarrisonUpdates")
