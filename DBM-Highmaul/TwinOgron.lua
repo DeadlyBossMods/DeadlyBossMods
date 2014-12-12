1a9dc21f7fe13997d24368f8b8eebf4b84dd839a
Adam <MysticalOS@users.noreply.github.com>
2014-12-12 09:50:19 +0000

    Margok. Even smarter range finder. Only force the 35 yard range on non tanks if the tank failed to get far enough away, else, show branded even if mark is active.
    Translation
    If a tank has mark of chaos and is LESS than 35 yards away from the raid, priority is
    mark of chaos > branded
    If tank has mark of chaos and is greater than 35 yards away (ie the tank isn't terrible)
    branded > mark of chaos
    If no mark of chaos is out at all, then obviously
    branded
    
    Also, range frame improvements ton twin ogron too for pulverize
    Disable useless tank warning
    Add arcane buff the bosses pass back and forth to twin ogron too.
    Disable arcane volatility CD timer. Too variable, too useless
    
    Lastly, added balls timer (when possible) to karogh

--- DBM-Highmaul/TwinOgron.lua
+++ DBM-Highmaul/TwinOgron.lua
@@ -12,20 +12,19 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 158057 157943 158134 158093 158200 157952 158415 158419 163336",
-	"SPELL_AURA_APPLIED 163372 167200 158241",
+	"SPELL_AURA_APPLIED 163372 167200 158241 163297",
 	"SPELL_AURA_APPLIED_DOSE 167200 158241",
 	"SPELL_AURA_REMOVED 163372",
 	"SPELL_CAST_SUCCESS 158385"
 )
 
---TODO, figure out stack tanks swap at for Arcane Wound(or if they can avoid swapping somehow). (bugged, there were no swapps on mythic, in fact tank debuff was irrelevant
---Note, watch to see if whirlwind returns to 60 60 86, repeating, or if the hotfix to make it old way, is perm
 --Phemos
 local warnEnfeeblingroar			= mod:NewCountAnnounce(158057, 3)
 local warnWhirlwind					= mod:NewCountAnnounce(157943, 3)
 local warnQuake						= mod:NewCountAnnounce(158200, 3)
+local warnArcaneTwisted				= mod:NewTargetAnnounce(163297, 2)--Mythic, the boss that's going to use empowered abilities
 local warnArcaneVolatility			= mod:NewTargetAnnounce(163372, 4)--Mythic
-local warnArcaneWound				= mod:NewStackAnnounce(167200, 2, nil, mod:IsTank())
+local warnArcaneWound				= mod:NewStackAnnounce("OptionVersion2", 167200, 2, nil, false)--Arcane debuff irrelevant. off by default, even for tanks unless blizz changes it.
 --Pol
 local warnShieldCharge				= mod:NewSpellAnnounce(158134, 4)--Target scanning assumed
 local warnInterruptingShout			= mod:NewCastAnnounce(158093, 3)
@@ -54,7 +53,8 @@ local timerShieldChargeCD			= mod:NewNextTimer(28, 158134)
 local timerInterruptingShoutCD		= mod:NewNextTimer(28, 158093)
 local timerPulverizeCD				= mod:NewNextTimer(29, 158385)
 --^^Even though 6 cd timers, coded smart to only need 2 up at a time, by using the predictability of "next ability" timing.
-local timerArcaneVolatilityCD		= mod:NewNextTimer(60, 163372)--NOT BOSS POWER BASED, this debuff is cast by outside influence every 60 seconds
+local timerArcaneTwistedCD			= mod:NewNextTimer(55, 163297)
+local timerArcaneVolatilityCD		= mod:NewNextTimer(60, 163372)--Only first one acurate now. Now it's a mess, was fine on beta. 60 second cd. but now it's boss power based, off BOTH bosses and is a real mess
 
 local berserkTimer					= mod:NewBerserkTimer(420)--As reported in feedback threads
 
@@ -69,7 +69,9 @@ mod.vb.EnfeebleCount = 0
 mod.vb.QuakeCount = 0
 mod.vb.WWCount = 0
 mod.vb.PulverizeCount = 0
+mod.vb.PulverizeRadar = false
 mod.vb.LastQuake = 0
+mod.vb.arcaneDebuff = 0
 local GetTime = GetTime
 local PhemosEnergyRate = 33
 local polEnergyRate = 28
@@ -90,11 +92,14 @@ function mod:OnCombatStart(delay)
 	self.vb.WWCount = 0
 	self.vb.PulverizeCount = 0
 	self.vb.LastQuake = 0
+	self.vb.arcaneDebuff = 0
+	self.vb.PulverizeRadar = false
 	timerQuakeCD:Start(11.5-delay, 1)
 	countdownPhemos:Start(11.5-delay)
 	timerShieldChargeCD:Start(37.5-delay)--Variable on pull
 	countdownPol:Start(37.5-delay)
 	if self:IsMythic() then
+		timerArcaneTwistedCD:Start(33-delay)
 		timerArcaneVolatilityCD:Start(65-delay)
 		countdownArcaneVolatility:Start(65-delay)
 		berserkTimer:Start(-delay)
@@ -109,9 +114,6 @@ function mod:OnCombatStart(delay)
 		PhemosEnergyRate = 33
 		polEnergyRate = 28
 	end
---[[	if not self:IsLFR() then
-		berserkTimer:Start(-delay)
-	end--]]
 end
 
 function mod:OnCombatEnd()
@@ -151,8 +153,27 @@ function mod:SPELL_CAST_START(args)
 		warnQuake:Show(self.vb.QuakeCount)
 		specWarnQuake:Show(self.vb.QuakeCount)
 		timerWhirlwindCD:Start(PhemosEnergyRate, self.vb.WWCount+1)
-		countdownPhemos:Start(PhemosEnergyRate)	
-	elseif args:IsSpellID(157952, 158415, 158419) then--Pulverize channel IDs
+		countdownPhemos:Start(PhemosEnergyRate)
+	elseif spellId == 157952 then--Pulverize first cast that needs range finder
+		self.vb.PulverizeCount = self.vb.PulverizeCount + 1
+		warnPulverize:Show(self.vb.PulverizeCount)
+	elseif spellId == 158415 then--Pulverize channel ID2
+		self.vb.PulverizeRadar = false
+		self.vb.PulverizeCount = self.vb.PulverizeCount + 1
+		warnPulverize:Show(self.vb.PulverizeCount)
+		--Hide range frame if arcane debuff not active, else switch 
+		if self.Options.RangeFrame then
+			if self.vb.arcaneDebuff > 0 then
+				if UnitDebuff("player", arcaneDebuff) then
+					DBM.RangeCheck:Show(8, nil)
+				else
+					DBM.RangeCheck:Show(8, debuffFilter)
+				end
+			else
+				DBM.RangeCheck:Hide()
+			end
+		end
+	elseif spellId == 158419 then--Pulverize channel ID3
 		self.vb.PulverizeCount = self.vb.PulverizeCount + 1
 		warnPulverize:Show(self.vb.PulverizeCount)
 	elseif spellId == 163336 and self:AntiSpam(2, 1) then
@@ -164,42 +185,59 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
 	if spellId == 163372 then
+		self.vb.arcaneDebuff = self.vb.arcaneDebuff + 1
 		warnArcaneVolatility:CombinedShow(1, args.destName)--Applies slowly to all targets
 		if self:AntiSpam(15, 2) then
-			timerArcaneVolatilityCD:Start()
-			countdownArcaneVolatility:Start()
+--			timerArcaneVolatilityCD:Start()
+--			countdownArcaneVolatility:Start()
 		end
 		if args:IsPlayer() then
 			specWarnArcaneVolatility:Show()
 			yellArcaneVolatility:Yell()
 		end
-		if UnitDebuff("player", arcaneDebuff) then
-			DBM.RangeCheck:Show(8, nil)
-		else
-			DBM.RangeCheck:Show(8, debuffFilter)
+		if self.Options.RangeFrame then
+			if UnitDebuff("player", arcaneDebuff) then
+				DBM.RangeCheck:Show(8, nil)
+			else
+				DBM.RangeCheck:Show(8, debuffFilter)
+			end
 		end
 	elseif spellId == 167200 then
 		local amount = args.amount or 1
 		warnArcaneWound:Show(args.destName, amount)
 	elseif spellId == 158241 and args:IsPlayer() and self:AntiSpam(2, 3) then
 		specWarnBlaze:Show()
+	elseif spellId == 163297 then
+		warnArcaneTwisted:Show(args.destName)
+		timerArcaneTwistedCD:Start()
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 163372 and args:IsPlayer() and self.Options.RangeFrame then
-		DBM.RangeCheck:Show(8, debuffFilter)
+	if spellId == 163372 then
+		self.vb.arcaneDebuff = self.vb.arcaneDebuff - 1
+		if args:IsPlayer() and self.Options.RangeFrame then
+			if self.vb.PulverizeRadar then
+				DBM.RangeCheck:Show(3, nil)
+			else
+				DBM.RangeCheck:Show(8, debuffFilter)
+			end
+		end
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
 	if spellId == 158385 then--Activation
+		self.vb.PulverizeRadar = true
 		self.vb.PulverizeCount = 0
 		specWarnPulverize:Show()
 		timerShieldChargeCD:Start(polEnergyRate)--Next Special
 		countdownPol:Start(polEnergyRate)
+		if self.Options.RangeFrame and not UnitDebuff("player", arcaneDebuff) then--Show range 3 for everyone, unless have arcane debuff, then you already have range 8 showing everyone that's more important
+			DBM.RangeCheck:Show(3, nil)
+		end
 	end
 end
