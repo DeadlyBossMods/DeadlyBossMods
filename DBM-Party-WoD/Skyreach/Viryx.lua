18f272aa7278089a520fb43caa81912378f45862
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-12-21 12:56:26 +0000

    # more 5 man updates. (incl. viryx)

--- DBM-Party-WoD/Skyreach/Viryx.lua
+++ DBM-Party-WoD/Skyreach/Viryx.lua
@@ -21,12 +21,14 @@ mod:RegisterEventsInCombat(
 --TODO, had bugged transcriptor so no IEEU events. See if IEEU is better for adds joining fight.
 local warnCastDown			= mod:NewTargetAnnounce(153954, 4)
 local warnLensFlare			= mod:NewSpellAnnounce(154032, 3)
+local warnAdd				= mod:NewAnnounce("warnAdd", 1, "Interface\\Icons\\Spell_Holy_BorrowedTime")
 local warnShielding			= mod:NewTargetAnnounce(154055, 2)
 
 local specWarnCastDownSoon	= mod:NewSpecialWarningSoon(153954)--Everyone, becaus it can grab healer too, which affects healer/tank
 local specWarnCastDown		= mod:NewSpecialWarningSwitch(153954, mod:IsDps(), nil, nil, 3)--Only dps, because it's their job to stop it.
 local specWarnLensFlareCast	= mod:NewSpecialWarningSpell(154032, nil, nil, nil, 2)--If there is any way to find actual target, like maybe target scanning, this will be changed.
 local specWarnLensFlare		= mod:NewSpecialWarningMove(154043)
+local specWarnAdd			= mod:NewSpecialWarning("specWarnAdd", mod:IsDps())
 local specWarnShielding		= mod:NewSpecialWarningInterrupt(154055, mod:IsDps())
 
 local timerLenseFlareCD		= mod:NewCDTimer(38, 154032)
@@ -39,12 +41,21 @@ local voiceShielding		= mod:NewVoice(154055, mod:IsDps())
 mod:AddSetIconOption("SetIconOnCastDown", 153954)
 
 mod.vb.lastGrab = nil
-local skyTrashMod = DBM:GetModByName("SkyreachTrash")
+
+function mod:CastDownTarget(targetname, uId)
+	if not targetname then return end
+	self.vb.lastGrab = targetname
+	warnCastDown:Show(self.vb.lastGrab)
+	if self.Options.SetIconOnCastDown then
+		self:SetIcon(1, self.vb.lastGrab)
+	end
+end
 
 function mod:OnCombatStart(delay)
 	self.vb.lastGrab = nil
 	timerLenseFlareCD:Start(-delay)
 	timerCastDownCD:Start(15-delay)
+	local skyTrashMod = DBM:GetModByName("SkyreachTrash")--can be script lan too long?
 	if skyTrashMod.Options.RangeFrame and skyTrashMod.vb.debuffCount ~= 0 then--In case of bug where range frame gets stuck open from trash pulls before this boss.
 		skyTrashMod.vb.debuffCount = 0--Fix variable
 		DBM.RangeCheck:Hide()--Close range frame.
@@ -85,6 +96,7 @@ end
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 	if spellId == 153954 then--Cast Down (4-5 sec before pre warning)
 		specWarnCastDownSoon:Show()
+		self:BossTargetScanner(76266, "CastDownTarget", 0.05, 15)
 		voiceCastDown:Play("mobsoon")
 	elseif spellId == 165834 then--Force Demon Creator to Ride Me
 		--TODO, see if victom detectable here instead
@@ -92,18 +104,9 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 		timerCastDownCD:Start()
 		voiceCastDown:Play("helpme")
 		voiceCastDown:Schedule(2, "helpme2")
-	elseif spellId == 136522 then--Force Demon Creator to Ride Me
-		--TODO, see if vehicle check works here.
-		for uId in DBM:GetGroupMembers() do
-			if UnitInVehicle(uId) then
-				self.vb.lastGrab = UnitName(uId)
-				warnCastDown:Show(self.vb.lastGrab)
-				if self.Options.SetIconOnCastDown then
-					self:SetIcon(1, self.vb.lastGrab)
-				end
-				return
-			end
-		end
+	elseif spellId == 154049 then-- Call Adds
+		warnAdd:Show()
+		specWarnAdd:Show()
 	elseif spellId == 154032 then--Actual Lens Flare cast. 154043 is not cast, despite SUCCESS event. It only fires if beam makes contact with a player. Then SPELL_CAST_SUCCESS and SPELL_AURA_APPLIED fire
 		warnLensFlare:Show()
 		specWarnLensFlareCast:Show()
--- DBM-Party-WoD/localization.kr.lua
+++ DBM-Party-WoD/localization.kr.lua
@@ -198,6 +198,16 @@ L= DBM:GetModLocalization(967)
 -----------------------
 L= DBM:GetModLocalization(968)
 
+L:SetWarningLocalization({
+	warnAdd			= DBM_CORE_AUTO_ANNOUNCE_TEXTS.spell:format("하늘탑 보호 피조물"),
+	specWarnAdd		= DBM_CORE_AUTO_SPEC_WARN_TEXTS.switch:format("하늘탑 보호 피조물")
+})
+
+L:SetOptionLocalization({
+	warnAdd			= "하늘탑 보호 피조물 알림 보기",
+	specWarnAdd		= "하늘탑 보호 피조물 대상 전환 특수 경고 보기"
+})
+
 -------------
 --  Skyreach Trash  --
 -------------
