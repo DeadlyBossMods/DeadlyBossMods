fdd46d94d7c0340d3bbc35786041a377c5fff143
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-12-22 06:09:45 +0000

    # Grasping Vine warn, fix bad spell name.

--- DBM-Party-WoD/EverBloom/AncientProtectors.lua
+++ DBM-Party-WoD/EverBloom/AncientProtectors.lua
@@ -23,7 +23,8 @@ mod:SetBossHealthInfo(83894, 83892, 83893)
 local warnRevitalizingWaters		= mod:NewSpellAnnounce(168082, 4)
 local warnBriarskin					= mod:NewTargetAnnounce("OptionVersion2", 168041, 3, nil, mod:IsMagicDispeller())
 local warnRapidTides				= mod:NewTargetAnnounce(168105, 4)
-local warnSlash						= mod:NewSpellAnnounce("OptionVersion2", 168383, 3, nil, mod:IsMelee())
+local warnGraspingVine				= mod:NewTargetAnnounce(168375, 2)
+local warnSlash						= mod:NewSpellAnnounce("OptionVersion3", 168383, 3)
 local warnNoxious					= mod:NewSpellAnnounce("OptionVersion2", 175997, 3)
 local warnShapersFortitude			= mod:NewTargetAnnounce(168520, 3)
 
@@ -31,24 +32,22 @@ local specWarnRevitalizingWaters	= mod:NewSpecialWarningInterrupt(168082, not mo
 local specWarnBriarskin				= mod:NewSpecialWarningInterrupt(168041, false)--if you have more than one interruptor, great. but off by default because we can't assume you can interrupt every bosses abilities. and heal takes priority
 local specWarnBriarskinDispel		= mod:NewSpecialWarningDispel(168041, false)--Not as important as rapid Tides and to assume you have at least two dispellers is big assumption
 local specWarnRapidTidesDispel		= mod:NewSpecialWarningDispel(168105, mod:IsMagicDispeller(), nil, nil, 3)
-local specWarnSlash					= mod:NewSpecialWarningSpell(168383, mod:IsMelee(), nil, nil, 2)--Because it's 8 yard cone in random direction.
+local specWarnSlash					= mod:NewSpecialWarningSpell(168383)
 local specWarnNoxious				= mod:NewSpecialWarningRun("OptionVersion2", 175997)
 local specWarnBramble				= mod:NewSpecialWarningMove(167977)
 
 local timerShapersFortitude			= mod:NewTargetTimer("OptionVersion2", 15, 168520, nil, false)
 local timerNoxiousCD				= mod:NewCDTimer(16, 175997, nil, mod:IsMelee())
+local timerGraspingVineCD			= mod:NewNextTimer(31.5, 168375)
 
 local voiceRevitalizingWaters		= mod:NewVoice(168082, not mod:IsHealer())
 local voiceNoxious					= mod:NewVoice(175997, mod:IsMelee())
 local voiceRapidTides				= mod:NewVoice(168105, mod:IsMagicDispeller())
 local voiceBramble					= mod:NewVoice(167977)
 
-function mod:OnCombatStart(delay)
-
-end
-
-function mod:OnCombatEnd()
-
+function mod:GraspingVineTarget(targetname, uId)
+	if not targetname then return end
+	warnGraspingVine:Show(targetname)
 end
 
 function mod:SPELL_CAST_START(args)
@@ -71,6 +70,13 @@ function mod:SPELL_CAST_START(args)
 		specWarnNoxious:Show()
 		timerNoxiousCD:Start()
 		voiceNoxious:Play("watchstep")
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 168375 then
+		self:BossTargetScanner(83894, "GraspingVineTarget", 0.05, 10)
+		timerGraspingVineCD:Start()
 	end
 end
 
