f93d3fd522caa6ca7d44561a01fbec49dc2dd88c
ultrashining <ultrashining@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-12-26 00:40:31 +0000

    zhTW update

--- DBM-Party-WoD/localization.tw.lua
+++ DBM-Party-WoD/localization.tw.lua
@@ -199,13 +199,13 @@ L= DBM:GetModLocalization(967)
 L= DBM:GetModLocalization(968)
 
 L:SetWarningLocalization({
-	warnAdd			= DBM_CORE_AUTO_ANNOUNCE_TEXTS.spell:format("Skyreach Shield Construct"),
-	specWarnAdd		= DBM_CORE_AUTO_SPEC_WARN_TEXTS.switch:format("Skyreach Shield Construct")
+	warnAdd			= DBM_CORE_AUTO_ANNOUNCE_TEXTS.spell:format("擎天護盾傀儡"),
+	specWarnAdd		= DBM_CORE_AUTO_SPEC_WARN_TEXTS.switch:format("擎天護盾傀儡")
 })
 
 L:SetOptionLocalization({
-	warnAdd			= "Show warning for Skyreach Shield Construct",
-	specWarnAdd		= "Show special warning to switch targets for Skyreach Shield Construct"
+	warnAdd			= "為擎天護盾傀儡顯示警告",
+	specWarnAdd		= "為轉換攻擊目標至擎天護盾傀儡顯示特別警告"
 })
 
 -------------
