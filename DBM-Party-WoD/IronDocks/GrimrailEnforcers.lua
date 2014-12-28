95b61b03c7b6a9d584cccbe68a761c1d23bfe94b
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2014-12-28 02:19:36 +0000

    # big boom warn.

--- DBM-Party-WoD/IronDocks/GrimrailEnforcers.lua
+++ DBM-Party-WoD/IronDocks/GrimrailEnforcers.lua
@@ -10,7 +10,7 @@ mod:SetBossHPInfoToHighest()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 163665 163390",
+	"SPELL_CAST_START 163665 163390 163379",
 	"SPELL_AURA_APPLIED 163689",
 	"SPELL_AURA_REMOVED 163689",
 	"UNIT_DIED",
@@ -23,12 +23,14 @@ local warnSanguineSphere		= mod:NewTargetAnnounce(163689, 3)
 local warnFlamingSlash			= mod:NewCastAnnounce(163665, 4)
 local warnLavaSwipe				= mod:NewSpellAnnounce(165152, 2)
 local warnOgreTraps				= mod:NewCastAnnounce(163390, 3)
+local warnBigBoom				= mod:NewSpellAnnounce(163379, 1)
 
 local specWarnSanguineSphere	= mod:NewSpecialWarningReflect(163689)
 local specWarnSanguineSphereEnd	= mod:NewSpecialWarningEnd(163689)
 local specWarnFlamingSlash		= mod:NewSpecialWarningSpell(163665, nil, nil, nil, 3)--Devastating in challenge modes. move or die.
 local specWarnLavaSwipe			= mod:NewSpecialWarningSpell(165152, nil, nil, nil, 2)
 local specWarnOgreTraps			= mod:NewSpecialWarningSpell(163390, mod:IsRanged())--Pre warning for bomb immediately after. Maybe change to a Soon warning with bomb spellid instead so that's clear?
+local specWarnBigBoom			= mod:NewSpecialWarningSpell(163379)--maybe use switch.
 
 local timerSanguineSphere		= mod:NewTargetTimer(15, 163689)
 local timerFlamingSlashCD		= mod:NewNextTimer(29, 163665)
@@ -56,6 +58,9 @@ function mod:SPELL_CAST_START(args)
 		warnOgreTraps:Show()
 		specWarnOgreTraps:Show()
 		timerOgreTrapsCD:Start()
+	elseif spellId == 163379 then
+		warnBigBoom:Show()
+		specWarnBigBoom:Show()
 	end
 end
 
