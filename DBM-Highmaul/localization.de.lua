2832cc809a3e3422037b05bbd54d677fc7a12de7
ebmor <ebmor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-01-01 16:41:00 +0000

    deDE update

--- DBM-Highmaul/localization.de.lua
+++ DBM-Highmaul/localization.de.lua
@@ -25,6 +25,13 @@ L:SetMiscLocalization({
 ------------------
 L= DBM:GetModLocalization(1196)
 
+L:SetOptionLocalization({
+	InterruptCounter	= "Setze \"Verrottung\"-Zähler zurück nach",
+	Two					= "zwei Wirkungen",
+	Three				= "drei Wirkungen",
+	Four				= "vier Wirkungen"
+})
+
 --------------
 -- Twin Ogron --
 --------------
@@ -42,6 +49,15 @@ L:SetOptionLocalization({
 --------------------
 L= DBM:GetModLocalization(1153)
 
+
+L:SetWarningLocalization({
+	specWarnExpelMagicFelFades	= "Teufelsenergie endet in 5s - geh zum Start"
+})
+
+L:SetOptionLocalization({
+	specWarnExpelMagicFelFades	= "Spezialwarnung zum Hingehen zur Startposition, wenn $spell:172895 endet"
+})
+
 L:SetMiscLocalization({
 	supressionTarget1	= "Ich werde Euch zermalmen!",
 	supressionTarget2	= "Schweigt!",
@@ -55,7 +71,9 @@ L:SetMiscLocalization({
 L= DBM:GetModLocalization(1197)
 
 L:SetMiscLocalization({
-	BrandedYell			= "Gebrandmarkt (%s) auf %s"
+	BrandedYell			= "Gebrandmarkt (%s) auf %s",
+	GazeYell			= "Starren endet in %d",
+	PlayerDebuffs		= "Closest to Glimpse"--translate
 })
 
 -------------
