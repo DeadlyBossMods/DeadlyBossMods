c12c6030adeb87d4a1f8c034e5e19ae6f4aadc20
Adam <MysticalOS@users.noreply.github.com>
2015-01-04 22:45:13 +0000

    uncomment spammy trash warnings and make them off by default instead. Add chat yells to non LFR difficulties (stuff doesn't matter in LFR)

--- DBM-Highmaul/HighmaulTrash.lua
+++ DBM-Highmaul/HighmaulTrash.lua
@@ -11,13 +11,17 @@ mod:RegisterEvents(
 	"SPELL_AURA_APPLIED 175636 173827 172066 166200"
 )
 
---local warnRuneofDestruction			= mod:NewTargetAnnounce(175636, 3)
---local warnRadiatingPoison			= mod:NewTargetAnnounce(172066, 3)
---local warnArcaneVol					= mod:NewTargetAnnounce(166200, 3)
+--Maybe add auto range finder for moveaway warnings, and REMOVED event to close it after
+local warnRuneofDestruction			= mod:NewTargetAnnounce("OptionVersion2", 175636, 3, nil, false)
+local warnRadiatingPoison			= mod:NewTargetAnnounce("OptionVersion2", 172066, 3, nil, false)
+local warnArcaneVol					= mod:NewTargetAnnounce("OptionVersion2", 166200, 3, nil, false)
 
 local specWarnRuneofDestruction		= mod:NewSpecialWarningMoveAway(175636)
+local yellRuneofDestruction			= mod:NewYell(175636)
 local specWarnRadiatingPoison		= mod:NewSpecialWarningMoveAway(172066)
+local yellRadiatingPoison			= mod:NewYell(172066)
 local specWarnArcaneVol				= mod:NewSpecialWarningMoveAway(166200)
+local yellArcaneVol					= mod:NewYell(166200)
 local specWarnWildFlames			= mod:NewSpecialWarningMove(173827)
 
 mod:RemoveOption("HealthFrame")
@@ -27,19 +31,28 @@ function mod:SPELL_AURA_APPLIED(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
 	if spellId == 175636 then
-		--warnRuneofDestruction:CombinedShow(1, args.destName)
+		warnRuneofDestruction:CombinedShow(1, args.destName)
 		if args:IsPlayer() then
 			specWarnRuneofDestruction:Show()
+			if not self:IsLFR() then
+				yellRuneofDestruction:Yell()
+			end
 		end
 	elseif spellId == 172066 then
-		--warnRadiatingPoison:CombinedShow(1, args.destName)
+		warnRadiatingPoison:CombinedShow(1, args.destName)
 		if args:IsPlayer() and not self:IsTank() then--tank can't run out even if they have debuff
 			specWarnRadiatingPoison:Show()
+			if not self:IsLFR() then
+				yellRadiatingPoison:Yell()
+			end
 		end
 	elseif spellId == 166200 then
-		--warnArcaneVol:CombinedShow(1, args.destName)
+		warnArcaneVol:CombinedShow(1, args.destName)
 		if args:IsPlayer() then
 			specWarnArcaneVol:Show()
+			if not self:IsLFR() then
+				yellArcaneVol:Yell()
+			end
 		end
 	elseif spellId == 173827 and args:IsPlayer() then
 		specWarnWildFlames:Show()
