e3c8107e5ab04e6c59f9579238a9c5d08d6f27e5
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-01-05 00:41:41 +0000

    # koKR update.

--- trunk/DBM-Core/localization.kr.lua
+++ trunk/DBM-Core/localization.kr.lua
@@ -101,7 +101,8 @@ DBM_CORE_VERSIONCHECK_OUTDATED		= "구버전 애드온 사용중 (%d명): %s"
 DBM_CORE_YOUR_VERSION_OUTDATED      = "사용중인 DBM 버전이 오래되었습니다! DBM 사용자 포럼에 오셔서 최신 버전을 다운로드 받으세요."
 DBM_CORE_OUTDATED_PVP_MODS			= "설치되어 있는 DBM-PvP 경고가 구버전 입니다. DBM 사용자 포럼에 오셔서 최신 버전으로 업데이트 하시거나, 애드온 폴더에서 DBM-PvP 폴더를 제거하시기 바랍니다. 이제 PvP 경고는 DBM-Core에 더 이상 포함되지 않습니다."
 DBM_CORE_VOICE_PACK_OUTDATED		= "현재 설치되어 있는 음성안내는 이 버전의 DBM에서 지원하는 음성 중 일부가 빠져 있습니다. 특수 경고 소리 끄기 기능이 비활성화 됩니다. 음성안내의 새 버전을 설치하시거나 음성안내 제작자에게 문의하여 업데이트가 가능한지 확인바랍니다."
-DBM_CORE_VOICE_MISSING				= "설정되여 있는 음성안내를 찾을 수 없습니다. 음성안내 설정이 'None' 으로 초기화 됩니다. 이 메세지가 오류라고 생각되면, 음성안내가 제대로 설치되어 있고 애드온 목록에서 활성화 되어 있는지 확인해 보시기 바랍니다."
+DBM_CORE_VOICE_MISSING				= "설정되어 있는 음성안내를 찾을 수 없습니다. 음성안내 설정이 'None' 으로 초기화 됩니다. 이 메세지가 오류라고 생각되면, 음성안내가 제대로 설치되어 있고 애드온 목록에서 활성화 되어 있는지 확인해 보시기 바랍니다."
+DBM_CORE_VOICE_COUNT_MISSING		= "%d번째 초읽기 소리 설정이 설정되어 있는 음성안내와 맞지 않습니다. 기본 설정으로 초기화 됩니다."
 --DBM_BIG_WIGS (Same as English locales)
 --DBM_BIG_WIGS_ALPHA (Same as English locales)
 
