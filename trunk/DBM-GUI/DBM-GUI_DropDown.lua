f528f51dbe3cf2137231fff32fc544d42a00dfcf
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-01-06 11:47:05 +0000

    Make Yike appear better in US client drop down menus

--- trunk/DBM-GUI/DBM-GUI_DropDown.lua
+++ trunk/DBM-GUI/DBM-GUI_DropDown.lua
@@ -135,7 +135,7 @@ do
 				if values[i+self.offset].font then
 					_G[self.buttons[i]:GetName().."NormalText"]:SetFont(values[i+self.offset].font, values[i+self.offset].fontsize or 14)
 				else
-					_G[self.buttons[i]:GetName().."NormalText"]:SetFont(STANDARD_TEXT_FONT, 10)
+					_G[self.buttons[i]:GetName().."NormalText"]:SetFontObject(GameFontHighlightSmall)
 				end
 				self.buttons[i]:Show()
 			else
