ddd0b494a7e7a8a92b4ebf55582f4db24ecbe25b
Adam <MysticalOS@users.noreply.github.com>
2015-01-08 07:32:49 +0000

    fix maybe

--- DBM-Highmaul/Koragh.lua
+++ DBM-Highmaul/Koragh.lua
@@ -106,7 +106,7 @@ local function ballsWarning(self)
 		specWarnBallsSoon:Show()
 		voiceBalls:Play("161612")
 	else
-		if self:IsMythic() and self.vb.ballsCount+1 % 2 then
+		if self:IsMythic() and (self.vb.ballsCount+1) % 2 then
 			specWarnMCSoon:Show()
 		end
 	end
