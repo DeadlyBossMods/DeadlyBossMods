0c916c8f938301357c3af4c4d4901cbe885eee3b
Adam <MysticalOS@users.noreply.github.com>
2015-01-09 22:21:54 +0000

    bw version

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -297,7 +297,7 @@ local timerRequestInProgress = false
 local updateNotificationDisplayed = 0
 local tooltipsHidden = false
 local SWFilterDisabed = false
-local fakeBWRevision = 12508
+local fakeBWRevision = 12525
 
 local enableIcons = true -- set to false when a raid leader or a promoted player has a newer version of DBM
 local guiRequested = false
