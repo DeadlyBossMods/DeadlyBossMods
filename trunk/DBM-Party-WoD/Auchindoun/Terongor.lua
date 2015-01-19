eb8e4e84fa6833482228437db79bb62bac5268ae
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-01-19 08:06:53 +0000

    # kill bad flag. # commented flag check function.

--- trunk/DBM-Party-WoD/Auchindoun/Terongor.lua
+++ trunk/DBM-Party-WoD/Auchindoun/Terongor.lua
@@ -35,7 +35,7 @@ local warnChaosWave				= mod:NewTargetAnnounce(157001, 3)
 local warnDoom					= mod:NewTargetAnnounce(156965, 3, nil, "Healer")
 
 --Basic Abilities
-local specWarnDrainLife			= mod:NewSpecialWarningInterrupt(156854, "-IsHealer")
+local specWarnDrainLife			= mod:NewSpecialWarningInterrupt(156854, "-Healer")
 local specWarnCorruption		= mod:NewSpecialWarningDispel(156842, "Healer")
 local specWarnRainOfFire		= mod:NewSpecialWarningSpell(156857, nil, nil, nil, 2)--156856 fires SUCCESS but do not use, it fires for any player walking in or out of it
 local specWarnRainOfFireMove	= mod:NewSpecialWarningMove(156857)
