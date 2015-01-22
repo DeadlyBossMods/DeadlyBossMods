f869f15d99b48f8f6be0bfc56b223a98a2bfc1e8
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-01-22 21:34:45 +0000

    CN Update

--- trunk/DBM-WorldEvents/localization.cn.lua
+++ trunk/DBM-WorldEvents/localization.cn.lua
@@ -1,5 +1,8 @@
-﻿-- Simplified Chinese by Diablohu(diablohudream@gmail.com) & yleaf(yaroot@gmail.com)
--- Last update: 10/20/2012
+﻿-- Diablohu(diablohudream@gmail.com)
+-- yleaf(yaroot@gmail.com)
+-- Mini_Dragon(projecteurs@gmail.com)
+-- Yike Xia
+-- Last update: Jan 22, 2015@12534
 
 if GetLocale() ~= "zhCN" then return end
 
@@ -14,9 +17,9 @@ L:SetGeneralLocalization({
 	name = "年兽"
 })
 
------------------------
---  Apothecary Trio  --
------------------------
+------------------------------
+--  The Crown Chemical Co.  --
+------------------------------
 L = DBM:GetModLocalization("d288")
 
 L:SetTimerLocalization({
@@ -33,9 +36,9 @@ L:SetMiscLocalization({
 	SayCombatStart		= "他们顾得上告诉你我是谁或者我在做些什么吗？"
 })
 
--------------
---  Ahune  --
--------------
+----------------------------
+--  The Frost Lord Ahune  --
+----------------------------
 L = DBM:GetModLocalization("d286")
 
 L:SetWarningLocalization({
@@ -145,4 +148,43 @@ L:SetOptionLocalization({
 
 L:SetMiscLocalization({
 	MassiveWave		= "一大波僵尸正在靠近！"
+})
+
+--------------------------
+--  Garrison Invasions  --
+--------------------------
+-- Thanks Blizzard Entertainment
+L = DBM:GetModLocalization("GarrisonInvasions")
+
+L:SetGeneralLocalization({
+	name = "要塞入侵"
+})
+
+L:SetWarningLocalization({
+	specWarnRylak	= "暗翼食腐者即将到来",
+	specWarnWorker	= "一个惊恐的工人暴露在开阔地带",
+	specWarnSpy		= "一个间谍混了进来",
+	specWarnBuilding= "一座建筑正受到攻击"
+})
+
+L:SetOptionLocalization({
+	specWarnRylak	= "当暗翼食腐者到来时发出特殊警报",
+	specWarnWorker	= "当工人暴露在开阔地带时发出特殊警报",
+	specWarnSpy		= "当间谍混进来时发出特殊警报",
+	specWarnBuilding= "当建筑受到攻击时发出特殊警报"
+})
+
+L:SetMiscLocalization({
+	--General
+	preCombat			= "各就各位！准备战斗！",--Common in all yells, rest varies based on invasion
+	preCombat2			= "空气开始变得污浊了……",--Shadow Council doesn't follow format of others :\
+	rylakSpawn			= "战斗引发的骚乱引来了一头双头飞龙！",--Source npc Darkwing Scavenger, target playername
+	terrifiedWorker		= "一个惊恐的工人在开阔地带被抓了！",
+	sneakySpy			= "一个间谍趁乱混了进来！",--Shortened to cut out "horde/alliance"
+	buildingAttack		= "你的%s正受到攻击！",--Your Salvage Yard is under attack!
+    --Ogre
+	GorianwarCaller		= "一名高里亚战争召唤者加入战斗，提升了士气！",--Maybe combined "add" special warning most adds?
+	WildfireElemental	= "一个野火元素被召唤到了前门！",--Maybe combined "add" special warning most adds?
+    --Iron Horde
+	Assassin			= "一名刺客正在猎杀你的守卫！"--Maybe combined "add" special warning most adds?
 })
