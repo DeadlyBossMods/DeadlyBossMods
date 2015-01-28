943e8bbe2049fe28ec53cbc04e1888b415b9f535
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-01-28 01:01:58 +0000

    Forgot something in last,now good.

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -3336,7 +3336,9 @@ do
 		if LastInstanceType == "none" and (not UnitAffectingCombat("player") or #inCombat > 0) then--world boss
 			local senderuId = DBM:GetRaidUnitId(sender)
 			if not senderuId then return end--Should never happen, but just in case. If happens, MANY "C" syncs are sent. losing 1 no big deal.
-			local range = DBM.RangeCheck:GetDistance("player", senderuId)
+			local playerZone, senderZone = select(4, UnitPosition("player")), select(4, UnitPosition(senderuId))
+			if playerZone ~= senderZone then return end--not same zone
+			local range = DBM.RangeCheck:GetDistance("player", senderuId)--Same zone, so check range
 			if not range or range > 60 then return end
 		end
 		if not cSyncSender[sender] then
