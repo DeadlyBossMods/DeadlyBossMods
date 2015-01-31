4e271cddf967bc5381cc87f760de4c539b450762
Adam <MysticalOS@users.noreply.github.com>
2015-01-31 02:27:39 +0000

    hans and frans refactoring (AGAIN). UNIT_TARGETABLE_CHANGED no longer fires. new method had to be used. Added skullcrusher timer. Added smart stamper timers.

--- DBM-BlackrockFoundry/HansgarAndFranzok.lua
+++ DBM-BlackrockFoundry/HansgarAndFranzok.lua
@@ -11,15 +11,15 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 160838 160845 160847 160848 153470 156938",
-	"SPELL_AURA_APPLIED 157139",
+	"SPELL_AURA_APPLIED 157139 162124",
 	"SPELL_AURA_APPLIED_DOSE 157139",
 	"UNIT_SPELLCAST_SUCCEEDED boss1 boss2",
-	"UNIT_TARGETABLE_CHANGED"
+	"CHAT_MSG_MONSTER_YELL"
 )
 
 --TODO, find target scanning for skullcracker. Also, find out how it behaves when it's more than 1 target (just recast?)
 --TODO, maybe use http://beta.wowhead.com/spell=154785 for aftershock/Shattered Vertebrae instead?'
---TODO, collect more data to figure out how roar stops/resumes on jump down. One pull/kill is not a sufficient sampling.
+--TODO, collect more data to figure out how roar starts/resumes on jump down. One pull/kill is not a sufficient sampling.
 local warnSkullcracker					= mod:NewSpellAnnounce(153470, 3, nil, false)--This seems pretty worthless.
 local warnShatteredVertebrae			= mod:NewStackAnnounce(157139, 2, nil, "Tank")--Possibly useless or changed. Needs further logs.
 
@@ -31,17 +31,29 @@ local specWarnSearingPlates				= mod:NewSpecialWarningSpell(161570, nil, nil, ni
 local specWarnStampers					= mod:NewSpecialWarningSpell(174825, nil, nil, nil, 2)
 local specWarnEnvironmentalThreatsEnd	= mod:NewSpecialWarningEnd("ej10089", nil)
 
-local timerDisruptingRoar				= mod:NewCastTimer(2.5, 160838)
-local timerDisruptingRoarCD				= mod:NewCDTimer(46, 160838)
+local timerDisruptingRoar				= mod:NewCastTimer(2.5, 160838, nil, "SpellCaster")
+local timerDisruptingRoarCD				= mod:NewCDTimer(46, 160838, nil, "SpellCaster")
+local timerSkullcrackerCD				= mod:NewCDTimer(22, 153470, nil, "Healer")
+mod:AddTimerLine(ENCOUNTER_JOURNAL_SECTION_FLAG12)
+local timerSmartStamperCD				= mod:NewNextTimer(12, 162124)--Activation
+local timerStamperDodge					= mod:NewNextCountTimer(10, 160582)--Time until stamper falls (spell name fits well, time you have to stamper dodge)
 
 local voiceEnvironmentalThreats			= mod:NewVoice("ej10089")
 local voiceShatteredVertebrae			= mod:NewVoice(157139)
 
 mod.vb.phase = 1
+mod.vb.stamperDodgeCount = 0
+mod.vb.bossUp = "NoBody"
 
 function mod:OnCombatStart(delay)
 	self.vb.phase = 1
+	self.vb.stamperDodgeCount = 0
+	self.vb.bossUp = "NoBody"
+	timerSkullcrackerCD:Start(20-delay)
 	timerDisruptingRoarCD:Start(-delay)
+	if self:IsMythic() then
+		timerSmartStamperCD:Start(13-delay)
+	end
 end
 
 function mod:OnCombatEnd()
@@ -61,6 +73,7 @@ function mod:SPELL_CAST_START(args)
 		end
 	elseif spellId == 153470 then
 		warnSkullcracker:Show()
+		timerSkullcrackerCD:Start()
 	elseif spellId == 156938 then
 		specWarnCripplingSuplex:Show()
 	end
@@ -84,16 +97,29 @@ function mod:SPELL_AURA_APPLIED(args)
 				voiceShatteredVertebrae:Play("changemt")
 			end
 		end
+	elseif spellId == 162124 and self:AntiSpam(2, args.sourceGUID) then
+		self.vb.stamperDodgeCount = self.vb.stamperDodgeCount + 1
+		timerStamperDodge:Start(nil, self.vb.stamperDodgeCount)--Multiple timers at once is intended. But capped at 3
+		--Run antispam code. If raid moves too fast MANY stampers spawn at once. This code will auto cancel timers as new timers are added once we reach 3
+		--So only max of 3 bars at once. Cancel current -2 all new timer start
+		if self.vb.stamperDodgeCount >= 3 then
+			timerStamperDodge:Cancel(self.vb.stamperDodgeCount-2)
+		end
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if spellId == 156220 or spellId == 156883 then--Tactical Retreat
+	if spellId == 156220 then--Tactical Retreat (156883 is not valid. It's return ID, which also fires for jumps so not very useful)
+		DBM:Debug("Tactical Retreat "..UnitName(uId))
 		self.vb.phase = self.vb.phase + 1
+		self.vb.stamperDodgeCount = 0
+		timerStamperDodge:Cancel()--Cancel all of them
+		self.vb.bossUp = UnitName(uId)
 		local cid = self:GetCIDFromGUID(UnitGUID(uId))
 		if cid == 76974 then--Fran
 			timerDisruptingRoarCD:Cancel()
+			timerSkullcrackerCD:Cancel()
 		end
 		--The triggers are these percentages for sure but there is a delay before they do it so it always appears later, but the trigger has been triggered
 		if self.vb.phase == 2 then--First belt 85% (15 Energy) (fire plates)
@@ -109,14 +135,39 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 	end
 end
 
-function mod:UNIT_TARGETABLE_CHANGED(uId)
-	if UnitExists(uId) then--Return, not retreat
+function mod:CHAT_MSG_MONSTER_YELL(msg, npc, _, _, target)
+	if not target and self.vb.bossUp == npc then--Bosses don't yell with a target for phase change yells. other yells do have targets. For good measure we also make sure sender is boss that's up
+		self.vb.bossUp = "NoBody"
 		if self.vb.phase == 4 then--Stampers activate on their own after 3rd jump away, when they return.
 			specWarnStampers:Show()
-			voiceEnvironmentalThreats:Play("watchstep")	
+			voiceEnvironmentalThreats:Play("watchstep")
 		else
-			specWarnEnvironmentalThreatsEnd:Show()
-			voiceEnvironmentalThreats:Play("safenow")
+			if self:IsMythic() then
+				timerSmartStamperCD:Start()
+				voiceEnvironmentalThreats:Play("gather")--Must restack for smart stampers
+			else
+				specWarnEnvironmentalThreatsEnd:Show()
+				voiceEnvironmentalThreats:Play("safenow")
+			end
 		end
 	end
 end
+
+--[[
+--Currently not functional. UNIT_TARGETABLE_CHANGED no longer fires. CHAT_MSG_MONSTER_YELL is slower backup method
+function mod:UNIT_TARGETABLE_CHANGED(uId)
+	DBM:Debug("UNIT_TARGETABLE_CHANGED event fired")
+	if UnitExists(uId) then--Return, not retreat
+		if self.vb.phase == 4 then--Stampers activate on their own after 3rd jump away, when they return.
+			specWarnStampers:Show()
+			voiceEnvironmentalThreats:Play("watchstep")
+		else
+			if self:IsMythic() then
+				--Smart Stampers reactivate. NOT SAFE
+			else
+				specWarnEnvironmentalThreatsEnd:Show()
+				voiceEnvironmentalThreats:Play("safenow")
+			end
+		end
+	end
+end--]]
