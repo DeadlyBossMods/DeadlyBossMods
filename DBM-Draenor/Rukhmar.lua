1cc11a4f4e50f6661c869299371a2e4d1754c830
Adam <MysticalOS@users.noreply.github.com>
2015-02-03 19:54:08 +0000

    Small rukhmar update Also, fire a callback for DBMS RAID_BOSS_WHISPER snooping so it's logged by transcriptor. Now, dbm users running transcriptor will log RAID_BOSS_WHISPER for ALL other raid members also running dbm.

--- DBM-Draenor/Rukhmar.lua
+++ DBM-Draenor/Rukhmar.lua
@@ -12,27 +12,24 @@ mod:RegisterCombat("combat_yell", L.Pull)
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED 167647 167615",
 	"SPELL_AURA_APPLIED_DOSE 167615",
-	"SPELL_CAST_START 167614 167687",
-	"SPELL_CAST_SUCCESS 167710"
+	"SPELL_CAST_START 167614 167679",
+	"RAID_BOSS_WHISPER"
 )
 
---TODO, does breath face tank?
 --TODO, taunt stacks and what not
 --TODO, timers.
 --TODO, health percents feathers/glory happen at. Add warnings if cast detectable too.
 --TODO, add warnings for fixates birds do if they fixate.
 local warnPiercedArmor			= mod:NewStackAnnounce(167615, 3, nil, "Tank")
-local warnSolarBreath			= mod:NewSpellAnnounce(167687, 3, nil, "Tank")
-local warnSolarRadiation		= mod:NewSpellAnnounce(167710, 3, nil, "Healer")
 
 local specWarnLooseQuills		= mod:NewSpecialWarningSpell(167647, nil, nil, nil, 2)
-local specWarnSolarBreath		= mod:NewSpecialWarningSpell(167687, false)
+local specWarnSolarBreath		= mod:NewSpecialWarningSpell(167679, "Tank")
+local specWarnExplode			= mod:NewSpecialWarningYou(167630)
 
 local timerLooseQuills			= mod:NewBuffActiveTimer(30, 167647)
 --local timerLooseQuillsCD		= mod:NewCDTimer(30, 167647)
-local timerSolarRadiationCD		= mod:NewNextTimer(15, 167710)--15 seconds according to journal. Doesn't mean it's right though.
---local timerSharpBeakCD		= mod:NewCDTimer(15, 167614)
---local timerSolarBreathCD		= mod:NewCDTimer(15, 167687)
+local timerSharpBeakCD			= mod:NewCDTimer(18, 167614, nil, "Tank")--Could be wrong, server was highly unstable
+--local timerSolarBreathCD		= mod:NewCDTimer(15, 167687, nil, "Tank")
 
 --mod:AddReadyCheckOption(37474, false)
 
@@ -57,18 +54,15 @@ mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 167614 then
-		--timerSharpBeakCD:Start()
-	elseif spellId == 167687 then
-		warnSolarBreath:Show()
+		timerSharpBeakCD:Start()
+	elseif spellId == 167679 then
 		specWarnSolarBreath:Show()
 		--timerSolarBreathCD:Start()
 	end
 end
 
-function mod:SPELL_CAST_SUCCESS(args)
-	local spellId = args.spellId
-	if spellId == 167710 then
-		warnSolarRadiation:Show()
-		timerSolarRadiationCD:Start()
+function mod:RAID_BOSS_WHISPER(msg)
+	if msg:find("spell:167630") then
+		specWarnExplode:Show()
 	end
 end
