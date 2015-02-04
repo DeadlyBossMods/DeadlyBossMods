1f84b7746b7066c480c2be76beca0652a6b074ae
ebmor <ebmor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-02-04 21:23:17 +0000

    deDE update

--- trunk/DBM-WorldEvents/localization.de.lua
+++ trunk/DBM-WorldEvents/localization.de.lua
@@ -140,34 +140,34 @@ L:SetMiscLocalization({
 L = DBM:GetModLocalization("GarrisonInvasions")
 
 L:SetGeneralLocalization({
-	name = "Garnisionsinvasionen"
+	name = "Garnisonsinvasionen"
 })
----- TODO: translate ----
+
 L:SetWarningLocalization({
-	specWarnRylak	= "Darkwing Scavenger Incoming",
-	specWarnWorker	= "Terrified worker in open",
-	specWarnSpy		= "A spy has snuck in",
-	specWarnBuilding= "A building is being attacked"
+	specWarnRylak	= "Darkwing Scavenger Incoming",--translate
+	specWarnWorker	= "Ein panischer Arbeiter ist schutzlos.",
+	specWarnSpy		= "Ein Spion hat sich eingeschlichen.",
+	specWarnBuilding= "Ein Gebäude wird angegriffen."
 })
 
 L:SetOptionLocalization({
-	specWarnRylak	= "Show special warning when a rylak is incoming",
-	specWarnWorker	= "Show special warning when a terrified worker is caught in open",
-	specWarnSpy		= "Show special warning when a spy has snuck in",
-	specWarnBuilding= "Show special warning when a building is under attack"
+	specWarnRylak	= "Show special warning when a rylak is incoming",--translate
+	specWarnWorker	= "Spezialwarnung, wenn ein panischer Arbeiter schutzlos ist",
+	specWarnSpy		= "Spezialwarnung, wenn sich ein Spion eingeschlichen hat",
+	specWarnBuilding= "Spezialwarnung, wenn ein Gebäude angegriffen wird"
 })
 
 L:SetMiscLocalization({
 	--General
-	preCombat			= "To arms! To your posts!",--Common in all yells, rest varies based on invasion
-	preCombat2			= "The air has taken a turn for the foul...",--Shadow Council doesn't follow format of others :\
-	rylakSpawn			= "The commotion of the battle attracts a rylak!",--Source npc Darkwing Scavenger, target playername
-	terrifiedWorker		= "A terrified worker is caught in the open!",
-	sneakySpy			= "spy has snuck in amidst the chaos!",--Shortened to cut out "horde/alliance"
-	buildingAttack		= "Your %s is under attack!",--Your Salvage Yard is under attack!
+	preCombat			= "Zu den Waffen! Auf eure Posten!",
+	preCombat2			= "So ein gewisser Gestank liegt in der Luft. Dämonen...",
+	rylakSpawn			= "The commotion of the battle attracts a rylak!",--translate, Source npc Darkwing Scavenger, target playername
+	terrifiedWorker		= "Ein panischer Arbeiter ist schutzlos!",
+	sneakySpy			= "hat das Chaos genutzt, um sich einzuschleichen!",
+	buildingAttack		= "Dein %s wird angegriffen!",--needs to be verified (guessed)
 	--Ogre
-	GorianwarCaller		= "A Gorian Warcaller joins the battle to raise morale!",--Maybe combined "add" special warning most adds?
-	WildfireElemental	= "A Wildfire Elemental is being summoned at the front gates!",--Maybe combined "add" special warning most adds?
+	GorianwarCaller		= "Ein gorianischer Zauberbinder trifft ein, um Eure Truppen zu verlangsamen!",
+	WildfireElemental	= "Ein Wildfeuerelementar wird am Haupttor beschworen!",
 	--Iron Horde
-	Assassin			= "An Assassin is hunting your guards!"--Maybe combined "add" special warning most adds?
+	Assassin			= "An Assassin is hunting your guards!"--translate
 })
