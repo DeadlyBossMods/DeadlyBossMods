9eb7ceb7925a365d074d8a0ec92a9438556d0aa1
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-02-04 04:36:26 +0000

    # allow duplicate timer for Thogar's train cooldown. # remove useless brf localization. # remove bad voice for Roltall.

--- DBM-BlackrockFoundry/localization.kr.lua
+++ DBM-BlackrockFoundry/localization.kr.lua
@@ -6,15 +6,6 @@ local L
 ---------------
 L= DBM:GetModLocalization(1161)
 
-L:SetWarningLocalization({
-})
-
-L:SetOptionLocalization({
-})
-
-L:SetMiscLocalization({
-})
-
 ---------------------------
 -- Oregorger, The Devourer --
 ---------------------------
@@ -35,6 +26,10 @@ L= DBM:GetModLocalization(1155)
 --------------
 L= DBM:GetModLocalization(1123)
 
+L:SetMiscLocalization({
+	TorrentYell	= "녹아내린 격류 %d초 후 사라짐!"
+})
+
 --------------------
 --Kromog, Legend of the Mountain --
 --------------------
@@ -51,34 +46,8 @@ L= DBM:GetModLocalization(1122)
 L= DBM:GetModLocalization(1147)
 
 L:SetMiscLocalization({
-	--Might not even need to use yells if my npc target works in all languages.
-	--depends on if "Train" is boss target in all languages and if that spellid hack also matches it in all languages.
-	--At the very least this helps read transcriptor logs :)
-	cannonTrain		= "Cannon",
-	threeTrains		= " 3 Random Lanes",
-	helperMessage	= "DBM과 함께 'Thogar Assist' 애드온을 사용하시는 것을 권장드립니다. http://wow.curseforge.com/addons/thogar-assist/ 에서 받으실 수 있습니다.",
-	commandTrain1	= "The command car is here.",
-	commandTrain2	= "Here comes the brass.",
-	moreThanOne1	= "Trains inbound!",
-	moreThanOne2	= "Double-time.",
-	moreThanOne3	= "Clear the tracks!",
-	cannon1			= "Here's my artillery.",
-	cannon2			= "Here come the boomers!",
-	driveBy1		= "Faster! Bat the stack off her!",
-	driveBy2		= "Express, coming through.",
-	driveBy3		= "Redball incoming!",
-	driveBy4		= "Send 'er on down the line!",
-	driveBy5		= "Coming in hot.",
-	smallAdds1		= "Here they come - hit the grit, boys!",
-	smallAdds2		= "Troop train - inbound!",
-	smallAdds3		= "Ah - reinforcements.",
-	--Some of these flamethrowers are iffy so verify flamethrower again in videos to be very sure.
-	--These may also be something else entirely so going to only debug these right now
-	flameThrower1	= "I have a schedule to keep!",
-	flameThrower2	= "You are running out of time...",
-	flameThrower3	= "I'm not impressed. More trains are inbound!",
-	flameThrower4	= "You're just in time for the rush.!",
-	flameThrower5	= "Let's step up the pace."
+	threeTrains		= " 무작위 3대",
+	helperMessage	= "DBM과 함께 'Thogar Assist' 애드온을 사용하시는 것을 권장드립니다. http://wow.curseforge.com/addons/thogar-assist/ 에서 받으실 수 있습니다."
 })
 
 --------------------------
@@ -87,7 +56,7 @@ L:SetMiscLocalization({
 L= DBM:GetModLocalization(1203)
 
 L:SetMiscLocalization({
-	shipMessage		= "prepares to man the Dreadnaught's Main Cannon!"
+	shipMessage		= "주 대포를 쏠 준비를 합니다!"
 })
 
 --------------------------
