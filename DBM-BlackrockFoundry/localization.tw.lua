f85caa76512410111af45230c3b73da069ec87f8
ultrashining <ultrashining@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-02-07 12:07:09 +0000

    zhTW update

--- DBM-BlackrockFoundry/localization.tw.lua
+++ DBM-BlackrockFoundry/localization.tw.lua
@@ -6,15 +6,6 @@ local L
 ---------------
 L= DBM:GetModLocalization(1161)
 
-L:SetWarningLocalization({
-})
-
-L:SetOptionLocalization({
-})
-
-L:SetMiscLocalization({
-})
-
 ---------------------------
 -- Oregorger, The Devourer --
 ---------------------------
@@ -35,6 +26,10 @@ L= DBM:GetModLocalization(1155)
 --------------
 L= DBM:GetModLocalization(1123)
 
+L:SetMiscLocalization({
+	TorrentYell	= "%d秒後熔岩灼流"
+})
+
 --------------------
 --Kromog, Legend of the Mountain --
 --------------------
@@ -51,34 +46,13 @@ L= DBM:GetModLocalization(1122)
 L= DBM:GetModLocalization(1147)
 
 L:SetMiscLocalization({
-	--Might not even need to use yells if my npc target works in all languages.
-	--depends on if "Train" is boss target in all languages and if that spellid hack also matches it in all languages.
-	--At the very least this helps read transcriptor logs :)
-	cannonTrain		= "Cannon",
-	threeTrains		= " 3 Random Lanes",
-	helperMessage	= "It is recommended you use 'Thogar Assist' in combination with DBM on this boss. Available from: http://wow.curseforge.com/addons/thogar-assist/",
-	commandTrain1	= "The command car is here.",
-	commandTrain2	= "Here comes the brass.",
-	moreThanOne1	= "Trains inbound!",
-	moreThanOne2	= "Double-time.",
-	moreThanOne3	= "Clear the tracks!",
-	cannon1			= "Here's my artillery.",
-	cannon2			= "Here come the boomers!",
-	driveBy1		= "Faster! Bat the stack off her!",
-	driveBy2		= "Express, coming through.",
-	driveBy3		= "Redball incoming!",
-	driveBy4		= "Send 'er on down the line!",
-	driveBy5		= "Coming in hot.",
-	smallAdds1		= "Here they come - hit the grit, boys!",
-	smallAdds2		= "Troop train - inbound!",
-	smallAdds3		= "Ah - reinforcements.",
-	--Some of these flamethrowers are iffy so verify flamethrower again in videos to be very sure.
-	--These may also be something else entirely so going to only debug these right now
-	flameThrower1	= "I have a schedule to keep!",
-	flameThrower2	= "You are running out of time...",
-	flameThrower3	= "I'm not impressed. More trains are inbound!",
-	flameThrower4	= "You're just in time for the rush.!",
-	flameThrower5	= "Let's step up the pace."
+	Train			= GetSpellInfo(174806),
+	lane			= "車道",
+	oneTrain		= "一個隨機列車道",
+	oneRandom		= "出現一個隨機列車道",
+	threeTrains		= "三個隨機列車道",
+	threeRandom		= "出現三個隨機列車道",
+	helperMessage	= "在這戰鬥推薦搭配協力插件'Thogar Assist'或是新版本的DBM語音包。可從Curse下載 "
 })
 
 --------------------------
@@ -87,7 +61,7 @@ L:SetMiscLocalization({
 L= DBM:GetModLocalization(1203)
 
 L:SetMiscLocalization({
-	shipMessage		= "prepares to man the Dreadnaught's Main Cannon!"
+	shipMessage		= "準備裝填無畏號的主炮了！"
 })
 
 --------------------------
