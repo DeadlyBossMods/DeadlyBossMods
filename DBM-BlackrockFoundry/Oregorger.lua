48838ef7574a56ba4dcfc01a21c4315db3a837b3
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-02-08 12:22:37 +0000

    # fix acid torrent cd.

--- DBM-BlackrockFoundry/Oregorger.lua
+++ DBM-BlackrockFoundry/Oregorger.lua
@@ -34,7 +34,7 @@ local specWarnHungerDrive			= mod:NewSpecialWarningSpell(165127, nil, nil, nil, 
 local specWarnHungerDriveEnded		= mod:NewSpecialWarningFades(165127)
 
 local timerBlackrockSpinesCD		= mod:NewCDTimer(20, 156834)--20-23 (cd for barrages themselves too inconsistent and useless. but CD for when he recharges his spines, quite consistent)
-local timerAcidTorrentCD			= mod:NewCDTimer("OptionVersion2", 23, 156240, nil, "Tank|Healer")--Every 23 seconds
+local timerAcidTorrentCD			= mod:NewCDTimer("OptionVersion2", 13, 156240, nil, "Tank|Healer")
 local timerExplosiveShardCD			= mod:NewCDTimer("OptionVersion3", 12, 156390, nil, "MeleeDps")--Every 12-20 seconds
 local timerExplosiveShard			= mod:NewCastTimer(3.5, 156390, nil, "MeleeDps")
 local timerRetchedBlackrockCD		= mod:NewCDTimer("OptionVersion2", 17, 156179, nil, "Ranged")--Every 17-23 seconds
