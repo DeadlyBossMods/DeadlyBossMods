1357b4303e211c5c269cd786aa3ad090dc670389
Adam <MysticalOS@users.noreply.github.com>
2015-02-12 07:52:09 +0000

    re-enable it i guess. but leave it as "soon"

--- DBM-BlackrockFoundry/HansgarAndFranzok.lua
+++ DBM-BlackrockFoundry/HansgarAndFranzok.lua
@@ -31,7 +31,7 @@ local specWarnDisruptingRoar			= mod:NewSpecialWarningCast("OptionVersion2", 160
 local specWarnShatteredVertebrae		= mod:NewSpecialWarningStack(157139, nil, 2, nil, nil, nil, nil, 2)--stack guessed
 local specWarnShatteredVertebraeOther	= mod:NewSpecialWarningTaunt(157139)
 --Move specWarnCripplingSupplex to a health check, warn when near 85, 55, or 25%
---local specWarnCripplingSupplex			= mod:NewSpecialWarningSoon("OptionVersion2", 156938, "Tank|Healer", 3.5, nil, nil, 3)--pop a cooldown.
+local specWarnCripplingSupplex			= mod:NewSpecialWarningSoon("OptionVersion2", 156938, "Tank|Healer", 3.5, nil, nil, 3)--pop a cooldown.
 local specWarnSearingPlates				= mod:NewSpecialWarningSpell(161570, nil, nil, nil, 2)
 local specWarnStampers					= mod:NewSpecialWarningSpell(174825, nil, nil, nil, 2)
 local specWarnSearingPlatesEnd			= mod:NewSpecialWarningEnd(161570)
@@ -165,7 +165,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 	--"<954.06 23:33:08> [CLEU] SPELL_CAST_SUCCESS#Vehicle-0-3783-1205-31925-76974-0000518B7D#Franzok#Player-76-0580DD5F#playerName#156938#Crippling Suplex#nil#nil", -- [6639]--SMASH (5.5 seconds after stun, VERY tight to cover attack with 6 second cd)
 	--http://blue.mmo-champion.com/topic/360651-blackrock-foundry-clarification-known-issues/
 	elseif spellId == 156546 or spellId == 156542 then
---		specWarnCripplingSupplex:Show()
+		specWarnCripplingSupplex:Show()
 		timerCripplingSupplex:Start()
 		countCripplingSupplex:Start()
 	elseif spellId == 157926 then--Jump Activation
