02a28aea8b8078cef8fdf3b96116d0ec6e1fb68a
Adam <MysticalOS@users.noreply.github.com>
2015-02-13 00:46:11 +0000

    don't show warnRetchedBlackrock if you already saw a special warning for same cast. Added count to acid torrent.

--- DBM-BlackrockFoundry/Oregorger.lua
+++ DBM-BlackrockFoundry/Oregorger.lua
@@ -21,12 +21,12 @@ mod:RegisterEventsInCombat(
 --[[Berserk?
 "<4.4 14:25:47> [INSTANCE_ENCOUNTER_ENGAGE_UNIT] Fake Args:#true#true#Oregorger#Creature:0:3314:1205:13906:77182
 "<328.2 14:31:10> CHAT_MSG_RAID_BOSS_EMOTE#Oregorger has gone insane from hunger!#Oregorger#####0#0##0#164#0000000000000000#0#false#false", -- [5]--]]
-local warnAcidTorrent				= mod:NewSpellAnnounce(156240, 3)
+local warnAcidTorrent				= mod:NewCountAnnounce(156240, 3)
 local warnRetchedBlackrock			= mod:NewTargetAnnounce("OptionVersion2", 156179, 3, nil, "Ranged")
 local warnCollectOre				= mod:NewCountAnnounce(165184, 2)
 
 local specWarnBlackrockBarrage		= mod:NewSpecialWarningInterruptCount(156877, false, nil, nil, nil, nil, 3)--Off by default since only interruptors want this on for their duty
-local specWarnAcidTorrent			= mod:NewSpecialWarningSpell(156240, "Tank", nil, nil, 3)--No voice filter, because voice is for tank swap that comes AFTER breath, this warning is to alert tank they need to move into position to soak breath, NOT taunt
+local specWarnAcidTorrent			= mod:NewSpecialWarningCount(156240, "Tank", nil, nil, 3)--No voice filter, because voice is for tank swap that comes AFTER breath, this warning is to alert tank they need to move into position to soak breath, NOT taunt
 local yellRetchedBlackrock			= mod:NewYell(156179)
 local specWarnRetchedBlackrockNear	= mod:NewSpecialWarningClose(156179)
 local specWarnRetchedBlackrock		= mod:NewSpecialWarningMove(156203, nil, nil, nil, nil, nil, 2)
@@ -35,7 +35,7 @@ local specWarnHungerDrive			= mod:NewSpecialWarningSpell("ej9964", nil, nil, nil
 local specWarnHungerDriveEnded		= mod:NewSpecialWarningFades("ej9964")
 
 local timerBlackrockSpinesCD		= mod:NewCDTimer(20, 156834)--20-23 (cd for barrages themselves too inconsistent and useless. but CD for when he recharges his spines, quite consistent)
-local timerAcidTorrentCD			= mod:NewCDTimer("OptionVersion2", 13, 156240, nil, "Tank|Healer")
+local timerAcidTorrentCD			= mod:NewCDCountTimer("OptionVersion2", 13, 156240, nil, "Tank|Healer")
 local timerExplosiveShardCD			= mod:NewCDTimer("OptionVersion3", 12, 156390, nil, "MeleeDps")--Every 12-20 seconds
 local timerExplosiveShard			= mod:NewCastTimer(3.5, 156390, nil, "MeleeDps")
 local timerRetchedBlackrockCD		= mod:NewCDTimer("OptionVersion2", 17, 156179, nil, "Ranged")--Every 17-23 seconds
@@ -49,11 +49,12 @@ local voiceAcidTorrent				= mod:NewVoice(156240)--changemt after 3 seconds (afte
 
 --local berserkTimer				= mod:NewBerserkTimer(324)--Auto berserk when reaching 3rd hunger drive phase. Time bariable because phase slightly variable.
 
+mod.vb.torrentCount = 0
+
 local lastOre = 0 -- not need sync
 
 function mod:RetchedBlackrockTarget(targetname, uId)
 	if not targetname then return end
-	warnRetchedBlackrock:Show(targetname)
 	if targetname == UnitName("player") then
 		if self:AntiSpam(2.5, 2) then
 			specWarnRetchedBlackrock:Show()
@@ -61,13 +62,16 @@ function mod:RetchedBlackrockTarget(targetname, uId)
 		yellRetchedBlackrock:Yell()
 	elseif self:CheckNearby(6, targetname) then
 		specWarnRetchedBlackrockNear:Show(targetname)
+	else
+		warnRetchedBlackrock:Show(targetname)
 	end
 end
 
 function mod:OnCombatStart(delay)
+	self.vb.torrentCount = 0
 	timerRetchedBlackrockCD:Start(5-delay)--5-7
 	timerExplosiveShardCD:Start(9.5-delay)
-	timerAcidTorrentCD:Start(12-delay)--12-13
+	timerAcidTorrentCD:Start(12-delay, 1)--12-13
 	countdownAcidTorrent:Start(12-delay)
 	timerBlackrockSpinesCD:Start(13-delay)--13-16
 --	berserkTimer:Start(-delay)
@@ -80,12 +84,13 @@ end
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 156240 then
+		self.vb.torrentCount = self.vb.torrentCount + 1
 		if self.Options.SpecWarn156240spell then
-			specWarnAcidTorrent:Show()
+			specWarnAcidTorrent:Show(self.vb.torrentCount)
 		else
-			warnAcidTorrent:Show()
+			warnAcidTorrent:Show(self.vb.torrentCount)
 		end
-		timerAcidTorrentCD:Start()
+		timerAcidTorrentCD:Start(nil, self.vb.torrentCount+1)
 		countdownAcidTorrent:Start()
 		voiceAcidTorrent:Schedule(3, "changemt")
 	elseif spellId == 156179 then
@@ -97,12 +102,13 @@ end
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
 	if spellId == 155819 then
+		self.vb.torrentCount = 0
 		self:UnregisterShortTermEvents()
 		specWarnHungerDriveEnded:Show()
 		voicePhaseChange:Play("phasechange")
 		timerRetchedBlackrockCD:Start(5)
 		timerExplosiveShardCD:Start(7)--7-9
-		timerAcidTorrentCD:Start(11)--11-12
+		timerAcidTorrentCD:Start(11, 1)--11-12
 		countdownAcidTorrent:Start(11)
 		timerBlackrockSpinesCD:Start(15)
 	elseif spellId == 156834 then
