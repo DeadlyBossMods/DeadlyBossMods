2c821de903c4abd36413ef12da2ddd999fe6730f
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-02-15 06:17:34 +0000

    # more dps's dispel.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -6236,10 +6236,10 @@ end
 
 function bossModPrototype:CheckDispelFilter()
 	if not DBM.Options.FilterDispel then return true end
-	--Druid: Nature's Cure (88423), Monk: Detox (115450), Priest: Purify (527), Plaadin: Cleanse (4987), Shaman: Purify Spirit (77130)
+	--Druid: Nature's Cure (88423), Remove Corruption (2782), Monk: Detox (115450), Priest: Purify (527), Plaadin: Cleanse (4987), Shaman: Cleanse Spirit (51886), Purify Spirit (77130), Mage: Remove Curse (475)
 	--start, duration, enable = GetSpellCooldown
 	--start & duration == 0 if spell not on cd
-	if (GetSpellCooldown(88423)) == 0 or (GetSpellCooldown(115450)) == 0 or (GetSpellCooldown(527)) == 0 or (GetSpellCooldown(4987)) == 0 or (GetSpellCooldown(77130)) == 0 then
+	if (GetSpellCooldown(88423)) == 0 or (GetSpellCooldown(2782)) == 0 or (GetSpellCooldown(115450)) == 0 or (GetSpellCooldown(527)) == 0 or (GetSpellCooldown(4987)) == 0 or (GetSpellCooldown(51886)) == 0 or (GetSpellCooldown(77130)) == 0 or (GetSpellCooldown(475)) == 0 then
 		return true
 	end
 	return false
