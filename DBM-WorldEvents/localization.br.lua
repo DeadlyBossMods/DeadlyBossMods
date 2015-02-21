94aa203e8e0eeecc26555bc413b69884392b4729
Adam <MysticalOS@users.noreply.github.com>
2015-02-21 20:56:20 +0000

    user submitted ptbr update (white space is a mess) Add missing turret timer on iron maidens. (it shouldn't need boat recovery, if you are getting turrets and boats you did something seriously wrong)

--- DBM-WorldEvents/localization.br.lua
+++ DBM-WorldEvents/localization.br.lua
@@ -1,7 +1,5 @@
 ﻿if GetLocale() ~= "ptBR" then return end
-
 local L
-
 ------------
 --  Omen  --
 ------------
@@ -17,17 +15,17 @@ L:SetGeneralLocalization({
 L = DBM:GetModLocalization("d288")
 
 L:SetTimerLocalization{
-	HummelActive		= "Humberto se ativa",
-	BaxterActive		= "Balduíno se ativa",
-	FryeActive			= "Frias se ativa"
+	HummelActive  = "Humberto se ativa",
+	BaxterActive  = "Balduíno se ativa",
+	FryeActive   = "Frias se ativa"
 }
 
 L:SetOptionLocalization({
-	TrioActiveTimer		= "Exibir cronógrafos para quando o Apothecary Trio tornar-se ativo"
+	TrioActiveTimer  = "Exibir cronógrafos para quando o Apothecary Trio tornar-se ativo"
 })
 
 L:SetMiscLocalization({
-	SayCombatStart		= "Eles se deram ao trabalho de lhe dizer quem eu sou e por que estou fazendo isso?"
+	SayCombatStart  = "Eles se deram ao trabalho de lhe dizer quem eu sou e por que estou fazendo isso?"
 })
 
 -------------
@@ -36,28 +34,24 @@ L:SetMiscLocalization({
 L = DBM:GetModLocalization("d286")
 
 L:SetWarningLocalization({
-	Submerged		= "Ahune submergiu",
-	Emerged			= "Ahune emergiu",
-	specWarnAttack	= "Ahune está vulnerável - Ataque agora!"
+	Emerged   = "Ahune Emergiu",
+	specWarnAttack = "Ahune está vulnerável - Ataque agora!"
 })
 
 L:SetTimerLocalization{
-	SubmergTimer	= "Submergir",
-	EmergeTimer		= "Emergir",
-	TimerCombat		= "Combate inicia"
+	SubmergTimer         = "Submergir",
+	EmergeTimer  = "Emergir"
 }
 
 L:SetOptionLocalization({
-	Submerged		= "Exibir aviso quando Ahune submergir",
-	Emerged			= "Exibir aviso quando Ahune emergir",
-	specWarnAttack	= "Exibir aviso especial quando Ahune tornar-se vulnerável",
-	SubmergTimer	= "Exibir cronógrafo para submergir",
-	EmergeTimer		= "Exibir cronógrafo para emerge",
-	TimerCombat		= "Exibir cronógrafo para início do combate"
+	Emerged   = "Exibir aviso quando Ahune emergir",
+	specWarnAttack = "Exibir aviso especial quando Ahune tornar-se vulnerável",
+	SubmergTimer = "Exibir cronógrafo para submergir",
+	EmergeTimer  = "Exibir cronógrafo para emergir"
 })
 
 L:SetMiscLocalization({
-	Pull			= "The Ice Stone has melted!"
+	Pull   = "A pedra de gelo derreteu!"
 })
 
 ----------------------
@@ -66,18 +60,18 @@ L:SetMiscLocalization({
 L = DBM:GetModLocalization("d287")
 
 L:SetWarningLocalization({
-	specWarnBrew		= "Livre-se da bebida antes que ela te atire outra!",
-	specWarnBrewStun	= "DICA: Você foi atordoado, lembre-se de beber da próxima vez!"
+	specWarnBrew  = "Livre-se da bebida antes que ela te jogue outra!",
+	specWarnBrewStun = "DICA: Você foi atordoado, lembre-se de beber da próxima vez!"
 })
 
 L:SetOptionLocalization({
-	specWarnBrew		= "Exibir aviso especial para $spell:47376",
-	specWarnBrewStun	= "Exibir aviso especial para $spell:47340",
-	YellOnBarrel		= "Gritar no $spell:51413"
+	specWarnBrew  = "Exibir aviso especial para $spell:47376",
+	specWarnBrewStun = "Exibir aviso especial para $spell:47340",
+	YellOnBarrel  = "Gritar em $spell:51413"
 })
 
 L:SetMiscLocalization({
-	YellBarrel			= "Estou Embarrilado!"
+	YellBarrel   = "Barril em mim!"
 })
 
 -------------------------
@@ -86,25 +80,20 @@ L:SetMiscLocalization({
 L = DBM:GetModLocalization("d285")
 
 L:SetWarningLocalization({
-	WarnPhase				= "Fase %d",
-	warnHorsemanSoldiers	= "Abóboras Pulsantes surgindo",
-	warnHorsemanHead		= "Cabeça do Horseman ativa"
+	WarnPhase    = "Fase %d",
+	warnHorsemanSoldiers = "Abóboras Pulsantes surgindo",
+	warnHorsemanHead  = "Cabeça do Horseman ativa"
 })
 
-L:SetTimerLocalization{
-	TimerCombatStart		= "Combate inicia"
-}
-
 L:SetOptionLocalization({
-	WarnPhase				= "Exibir aviso para cada mudança de fase",
-	TimerCombatStart		= "Exibir cronógrafo para início do combate",
-	warnHorsemanSoldiers	= "Exibir aviso para surgimento de Abóboras Pulsantes",
-	warnHorsemanHead		= "Exibir aviso para surgimento da Cabeça do Horseman"
+	WarnPhase    = "Exibir aviso para cada mudança de fase",
+	warnHorsemanSoldiers = "Exibir aviso para surgimento de Abóboras Pulsantes",
+	warnHorsemanHead  = "Exibir aviso para surgimento da Cabeça do Cavaleiro"
 })
 
 L:SetMiscLocalization({
-	HorsemanSummon			= "Horseman rise...",
-	HorsemanSoldiers		= "Soldiers arise, stand and fight! Bring victory at last to this fallen knight!"
+	HorsemanSummon   = "Cavaleiro ascende...",
+	HorsemanSoldiers  = "Soldados levantem-se, fiquem e lutem! Tragam finalmente à vitória para este cavaleiro caido!"
 })
 
 ------------------------------
@@ -113,5 +102,71 @@ L:SetMiscLocalization({
 L = DBM:GetModLocalization("Greench")
 
 L:SetGeneralLocalization({
-	name = "The Abominable Greench"
+	name = "O Greench abominável"
+})
+
+--------------------------
+--  Plants Vs. Zombies  --
+--------------------------
+L = DBM:GetModLocalization("PlantsVsZombies")
+
+L:SetGeneralLocalization({
+	name = "Plantas Vs. Zumbis"
+})
+
+L:SetWarningLocalization({
+	warnTotalAdds = "Total zumbis gerados desde a última Onda Massiva: %d",
+	specWarnWave = "Onda Massiva!"
+})
+
+L:SetTimerLocalization{
+	timerWave  = "Próxima Onda Massiva"
+}
+
+L:SetOptionLocalization({
+	warnTotalAdds = "Anuncia a contagem total de adds gerados entre cada onda massiva",
+	specWarnWave = "Mostra aviso especial quando uma onda massiva começar",
+	timerWave  = "Mostra contador para próxima onda massiva"
+})
+
+L:SetMiscLocalization({
+	MassiveWave  = "Uma onda Massiva de Zumbis está se aproximando!"
+})
+
+--------------------------
+--  Garrison Invasions  --
+--------------------------
+L = DBM:GetModLocalization("GarrisonInvasions")
+
+L:SetGeneralLocalization({
+	name = "Invasão das guarnições"
+})
+
+L:SetWarningLocalization({
+	specWarnRylak = "Catador Alanegra esta vindo",
+	specWarnWorker = "Trabalhador aterrorizado em perigo",
+	specWarnSpy  = "Um espião infiltrou-se em",
+	specWarnBuilding= "Uma construção esta sendo atacada"
+})
+
+L:SetOptionLocalization({
+	specWarnRylak = "Mostra aviso especial quando um Catador Alanegra estiver vindo",
+	specWarnWorker = "Mostra aviso especial quando um trabalhador aterrorizado estiver em perigo",
+	specWarnSpy  = "Mostra aviso especial quando um espião infiltrar-se",
+	specWarnBuilding= "Mostra aviso especial quando uma construção estiver sendo atacada"
+})
+
+L:SetMiscLocalization({
+	--General
+	preCombat   = "Às armas! para os seus postos!",--Comum em todos os gritos, restante varia de acordo com a invasão
+	preCombat2   = "O céu se escureceu abruptamente...",--Conselho das sombras não segue o formato das outras :\
+	rylakSpawn   = "A comoção da batalha atraiu um Catador Alanegra!",--origem pnj Catador Alanegra, alvo nomejogador
+	terrifiedWorker  = "Trabalhador aterrorizado em perigo!",
+	sneakySpy   = "espião se infiltrou em meio ao caos!",--encurtado para ajustar "horde/alliance"
+	buildingAttack  = "A %s esta sob ataque!",--Seu ferro-velho esta sob ataque!
+	--Ogre
+	GorianwarCaller  = "Um Belarauto Goriano juntou-se a batalha e levantou o moral!",--Talvez combinado "add" aviso especial maioria dos adds?
+	WildfireElemental = "Um elemental do fogo indômito esta sendo convocado no portão frontal!",--Talvez combinado "add" aviso especial maioria dos adds?
+	--Iron Horde
+	Assassin   = "Um assassino esta caçando seus guardas!"--Talvez combinado "add" aviso especial maioria dos adds??
 })
