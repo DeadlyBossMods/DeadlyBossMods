935480dbbc76c9626fee1ea65f74936cdb85c83f
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-02-23 14:53:23 +0000

    # fix mythic shield timer.

--- trunk/DBM-BlackrockFoundry/BlastFurnace.lua
+++ trunk/DBM-BlackrockFoundry/BlastFurnace.lua
@@ -59,6 +59,7 @@ local specWarnCauterizeWounds	= mod:NewSpecialWarningInterrupt(155186, "-Healer"
 local specWarnPyroclasm			= mod:NewSpecialWarningInterrupt(156937, false)
 local specVolatileFire			= mod:NewSpecialWarningMoveAway(176121)
 local yellVolatileFire			= mod:NewYell(176121)
+--local specWarnSlagElemental		= mod:NewSpecialWarningSwitch("ej9657", "Dps")-- is really needed in mythic? needs review (Slay Elemental, 176143)
 local specWarnShieldsDown		= mod:NewSpecialWarningSwitch("ej9655", "Dps")
 local specWarnEarthShield		= mod:NewSpecialWarningDispel(155173, "MagicDispeller")
 local specWarnSlagPool			= mod:NewSpecialWarningMove(155743)
@@ -370,12 +371,12 @@ function mod:SPELL_AURA_APPLIED(args)
 		end
 	elseif spellId == 158345 and self:AntiSpam(10, 3) then--Might be SPELL_CAST_SUCCESS instead.
 		specWarnShieldsDown:Show()
-		if self:IsDifficulty("normal", "lfr") then--40 seconds on normal. at least that much on LFR too, probably even longer in LFR.
+		if self:IsDifficulty("normal") then--40 seconds on normal
 			timerShieldsDown:Start(40)
 		elseif self:IsHeroic() then
 			timerShieldsDown:Start()--30 in heroic
 		else
-			timerShieldsDown:Start(25)--20 on mythic? or 25?
+			timerShieldsDown:Start(20)
 		end
 	elseif spellId == 155242 then
 		local amount = args.amount or 1
