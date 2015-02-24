5803cf76e239ab0da264d2c0988da0ae73744421
Paul Emmerich <emmericp@net.in.tum.de>
2015-02-24 17:00:01 +0000

    increment alpha version

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -54,8 +54,8 @@
 -------------------------------
 DBM = {
 	Revision = tonumber(("$Revision$"):sub(12, -3)),
-	DisplayVersion = "6.0.17 alpha", -- the string that is shown as version
-	ReleaseRevision = 12955 -- the revision of the latest stable version that is available
+	DisplayVersion = "6.1.1 alpha", -- the string that is shown as version
+	ReleaseRevision = 13101 -- the revision of the latest stable version that is available
 }
 
 -- support for git svn which doesn't support svn keyword expansion
