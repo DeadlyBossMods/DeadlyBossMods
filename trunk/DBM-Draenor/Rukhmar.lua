36be32f9cd81b13233d1997a1e3beaaaee1caf09
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-02-27 20:33:45 +0000

    Add encounter IDs to ruk and tarlna. drov doesn’t have one?

--- trunk/DBM-Draenor/Rukhmar.lua
+++ trunk/DBM-Draenor/Rukhmar.lua
@@ -3,6 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(83746)
+mod:SetEncounterID(1755)
 mod:SetReCombatTime(20)
 mod:SetZone()
 mod:SetMinSyncRevision(11969)
--- trunk/DBM-Draenor/Tarlna.lua
+++ trunk/DBM-Draenor/Tarlna.lua
@@ -3,6 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(81535)
+mod:SetEncounterID(1770)
 mod:SetReCombatTime(20)
 mod:SetZone()
 mod:SetMinSyncRevision(11969)
