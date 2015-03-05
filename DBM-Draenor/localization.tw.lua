af6b938172d25e87d1bd0851b3f0bdc53881362c
ultrashining <ultrashining@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-03-05 16:12:34 +0000

    zhTW update

--- DBM-Draenor/localization.tw.lua
+++ DBM-Draenor/localization.tw.lua
@@ -6,24 +6,13 @@ local L
 -----------------------
 L= DBM:GetModLocalization(1291)
 
-L:SetMiscLocalization({
-	Pull				= "TODO: Get Pull Yell!"
-})
-
 -----------------------
 -- Tarlna the Ageless --
 -----------------------
 L= DBM:GetModLocalization(1211)
-
-L:SetMiscLocalization({
-	Pull				= "TODO: Get Pull Yell!"
-})
 
 --------------
 -- Rukhmar --
 --------------
 L= DBM:GetModLocalization(1262)
 
-L:SetMiscLocalization({
-	Pull				= "TODO: Get Pull Yell!"
-})
