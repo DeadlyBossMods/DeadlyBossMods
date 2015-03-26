dfd005c96c9303efd8118d6e254ff8205cc5bab2
Adam <MysticalOS@users.noreply.github.com>
2015-03-26 06:03:58 +0000

    Bump HFRevision

--- DBM-BlackrockFoundry/Blackhand.lua
+++ DBM-BlackrockFoundry/Blackhand.lua
@@ -6,7 +6,7 @@ mod:SetCreatureID(77325)--68168
 mod:SetEncounterID(1704)
 mod:SetZone()
 mod:SetUsedIcons(3, 2, 1)
-mod:SetHotfixNoticeRev(12813)
+mod:SetHotfixNoticeRev(13427)
 
 mod:RegisterCombat("combat")
 
