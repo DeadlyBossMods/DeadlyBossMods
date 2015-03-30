0b48783fd88c00fa3a99e7afbcdde2b6b92bf7ea
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-03-30 10:07:29 +0000

    # revision keyword for brf trash.

--- trunk/DBM-BlackrockFoundry/BlackrockFoundryTrash.lua
+++ trunk/DBM-BlackrockFoundry/BlackrockFoundryTrash.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("BlackrockFoundryTrash", "DBM-BlackrockFoundry")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 11326 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 --mod:SetModelID(47785)
 mod:SetZone()
 mod.isTrashMod = true
