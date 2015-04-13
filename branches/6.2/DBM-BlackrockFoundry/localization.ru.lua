4d485b545fec4da08e8f95e41b2689cfa35778c2
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-04-13 23:33:53 +0000

    create 6.2 branch

---
+++ branches/6.2/DBM-BlackrockFoundry/localization.ru.lua
@@ -0,0 +1,117 @@
+﻿if GetLocale() ~= "ruRU" then return end
+local L
+
+---------------
+-- Gruul --
+---------------
+L= DBM:GetModLocalization(1161)
+
+L:SetOptionLocalization({
+	MythicSoakBehavior	= "Настройка получения стаков от Инфернального удара для спец предупреждений (эпох. сложность)",
+	ThreeGroup			= "3 группы по 1 стаку каждая",
+	TwoGroup			= "2 группы по 2 стака каждая"
+})
+
+---------------------------
+-- Oregorger, The Devourer --
+---------------------------
+L= DBM:GetModLocalization(1202)
+
+---------------------------
+-- The Blast Furnace --
+---------------------------
+L= DBM:GetModLocalization(1154)
+
+L:SetWarningLocalization({
+	warnRegulators		= "Осталось Регуляторов температуры: %d",
+	warnBlastFrequency	= "Частота взрывов увеличилась: каждые ~%d сек"
+})
+
+L:SetOptionLocalization({
+	warnRegulators		= "Объявлять сколько Регуляторов температуры осталось",
+	warnBlastFrequency	= "Объявлять когда увеличивается частота $spell:155209",
+	VFYellType2			= "Тип крика для Неустойчивого огня (только Эпохальный режим)",
+	Countdown			= "Отсчет до спадения",
+	Apply				= "Только получение дебаффа"
+})
+
+L:SetMiscLocalization({
+	heatRegulator		= "Регулятор температуры"
+})
+
+------------------
+-- Hans'gar And Franzok --
+------------------
+L= DBM:GetModLocalization(1155)
+
+--------------
+-- Flamebender Ka'graz --
+--------------
+L= DBM:GetModLocalization(1123)
+
+--------------------
+--Kromog, Legend of the Mountain --
+--------------------
+L= DBM:GetModLocalization(1162)
+
+--------------------------
+-- Beastlord Darmac --
+--------------------------
+L= DBM:GetModLocalization(1122)
+
+--------------------------
+-- Operator Thogar --
+--------------------------
+L= DBM:GetModLocalization(1147)
+
+L:SetWarningLocalization({
+	specWarnSplitSoon	= "Разделение рейда через 10 сек"
+})
+
+L:SetOptionLocalization({
+	specWarnSplitSoon	= "Спец-предупредение за 10 секунд до разделения рейда",
+	InfoFrameSpeed		= "Настройка отображения след. поезда в информационном окне",
+	Immediately			= "Как только открываются двери для текущего поезда",
+	Delayed				= "После того как текущий поезд выехал"
+})
+
+L:SetMiscLocalization({
+	Train			= GetSpellInfo(174806),
+	lane			= "Путь",
+	oneTrain		= "1 случайный путь: Поезд",
+	oneRandom		= "Появляется на 1 случайном пути",
+	threeTrains		= "3 случайных пути: Поезд",
+	threeRandom		= "Появляются на 3 случайных путях",
+	helperMessage	= "Этот бой может быть упрощен с помощью аддона 'Thogar Assist' или одного из множества доступных голосовых пакетов DBM, которые можно найти на http://wow.curse.com/."
+})
+
+--------------------------
+-- The Iron Maidens --
+--------------------------
+L= DBM:GetModLocalization(1203)
+
+L:SetWarningLocalization({
+	specWarnReturnBase	= "Возвращайтесь на причал!"
+})
+
+L:SetOptionLocalization({
+	specWarnReturnBase	= "Спец-предупреждение когда игрок на борту может безопасно вернуться на причал"
+})
+
+L:SetMiscLocalization({
+	shipMessage		= "готовится занять позицию у главного орудия дредноута!"
+})
+
+--------------------------
+-- Blackhand --
+--------------------------
+L= DBM:GetModLocalization(959)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("BlackrockFoundryTrash")
+
+L:SetGeneralLocalization({
+	name =	"Трэш мобы Литейной клана Черной горы"
+})
---
+++ branches/6.2/DBM-WorldEvents/Invasions/Teluur.lua
@@ -0,0 +1,75 @@
+local mod	= DBM:NewMod("Teluur", "DBM-WorldEvents", 3)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+mod:SetCreatureID(90946)
+mod:SetZone(1159, 1331, 1158, 1153, 1152, 1330)--4 of these not needed, but don't know what's what ATM
+
+mod:RegisterCombat("combat")
+mod:SetMinCombatTime(15)
+
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_START 180849",
+	"SPELL_CAST_SUCCESS 180836 180849",
+	"SPELL_AURA_APPLIED 180836",
+	"UNIT_SPELLCAST_SUCCEEDED"
+)
+
+local warnPodlingSwarm			= mod:NewSpellAnnounce(180836, 2)
+local warnEntanglement			= mod:NewTargetAnnounce(180836, 3)--Players who didn't move and got caught
+local warnSpore					= mod:NewSpellAnnounce(180825, 3)--Hidden from combat log, until it's too late. Unit event gives enough time to run out but don't know who it's targeting then. target scanning seems to kinda work but not reliable enough. There is somewhat of a delay and often no target at all
+
+local specWarnEntanglement		= mod:NewSpecialWarningDodge(180836)--Dodgable. puts green swirly under random player. traps everyone there after 4 seconds. Target scanning not possible, warn everyone to check feet
+--local specWarnSpore				= mod:NewSpecialWarningRun(180825, nil, nil, nil, 4)
+--local yellSpore					= mod:NewYell(180825)
+
+local timerEntanglementCD		= mod:NewNextTimer(10, 180836)--CD is 10 unless delayed by podlings
+local timerPodlingSwarmCD		= mod:NewCDTimer(30, 180836)--30-32 variable, clearly a 30 second cd from cast finish or engage
+local timerSporeCD				= mod:NewCDTimer(15, 180825)--15-20
+
+function mod:OnCombatStart(delay, summonTriggered)
+	if summonTriggered then
+		timerEntanglementCD:Start(10)
+		timerSporeCD:Start(11)
+		timerPodlingSwarmCD:Start(30)
+	end
+end
+
+function mod:SPELL_CAST_START(args)
+	local spellId = args.spellId
+	if spellId == 180849 then
+		warnPodlingSwarm:Show()
+		timerEntanglementCD:Cancel()
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+	if spellId == 180836 then
+		specWarnEntanglement:Show()
+		timerEntanglementCD:Start()
+	elseif spellId == 180849 then
+		timerEntanglementCD:Start()--Will be cast 10 seconds after cast FINISH of podlings
+		timerPodlingSwarmCD:Start()
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	local spellId = args.spellId
+	if spellId == 180836 then
+		warnEntanglement:CombinedShow(0.5, args.destName)
+	end
+end
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
+	if spellId == 180825 and self:AntiSpam() then
+		self:SendSync("Spore")
+	end
+end
+
+function mod:OnSync(msg)
+	if msg == "Spore" then
+		warnSpore:Show()
+		timerSporeCD:Start()
+	end
+end
