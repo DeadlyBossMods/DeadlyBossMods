ef4c7c4aa37a13ab17504386004c25e40599f564
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-04-14 20:18:22 +0000

    fix error

--- branches/6.2/DBM-HellfireCitadel/Socrethar.lua
+++ branches/6.2/DBM-HellfireCitadel/Socrethar.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod(1427, "DBM-BlackrockFoundry", nil, 669)
+local mod	= DBM:NewMod(1427, "DBM-HellfireCitadel", nil, 669)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
