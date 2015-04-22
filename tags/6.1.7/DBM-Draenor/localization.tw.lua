cc7e7335c14b1173891aab956d21bc61efcc688a
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-04-22 12:22:11 +0000

    tag as 6.1.7

---
+++ tags/6.1.7/DBM-Draenor/localization.tw.lua
@@ -0,0 +1,18 @@
+﻿if GetLocale() ~= "zhTW" then return end
+local L
+
+-----------------------
+-- Drov the Ruiner --
+-----------------------
+L= DBM:GetModLocalization(1291)
+
+-----------------------
+-- Tarlna the Ageless --
+-----------------------
+L= DBM:GetModLocalization(1211)
+
+--------------
+-- Rukhmar --
+--------------
+L= DBM:GetModLocalization(1262)
+
