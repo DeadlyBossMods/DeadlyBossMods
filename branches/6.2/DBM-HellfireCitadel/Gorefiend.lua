d34cf033ec38525340d2b2d6525e60f689b13536
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-04-22 01:19:27 +0000

    Gorefiend 3 to go…

--- branches/6.2/DBM-HellfireCitadel/Gorefiend.lua
+++ branches/6.2/DBM-HellfireCitadel/Gorefiend.lua
@@ -5,67 +5,98 @@ mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(91809)
 mod:SetEncounterID(1783)
 mod:SetZone()
---mod:SetUsedIcons(8, 7, 6, 4, 2, 1)
+mod:SetUsedIcons(2, 1)
 --mod:SetRespawnTime(20)
 
 mod:RegisterCombat("combat")
 
---[[
+
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS,
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_APPLIED_DOSE",
-	"SPELL_AURA_REMOVED",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_ABSORBED",
+	"SPELL_CAST_START 182049 181973 182788 181582",
+--	"SPELL_CAST_SUCCESS",
+	"SPELL_AURA_APPLIED 179864 179977 179909 179908 180148",
+--	"SPELL_AURA_APPLIED_DOSE",
+	"SPELL_AURA_REMOVED 179909 179908",
+--	"SPELL_PERIODIC_DAMAGE",
+--	"SPELL_ABSORBED",
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
-local warnCrumblingRoar				= mod:NewSpellAnnounce(155730, 3, nil, false)
+--TODO, figure out surging shadows. i see no player applied debuff ID
+--TODO, figure out crushing darkness. probably a rapid cast sequence of 3-5 missles with a non special warning count warning
+--TODO, use syncing for non stomach timers, hidden, but still saved, to use timer recovery when leaving stomach.
+--TODO, UNIT_DIED and add timers for bellowingshout?
+--TODO, more voices? "run to player"? "Shadow of Death"?
+local warnShadowofDeath					= mod:NewTargetAnnounce(179864, 3)
+local warnTouchofDoom					= mod:NewTargetAnnounce(179978, 4)
+local warnSharedFate					= mod:NewTargetAnnounce(179909, 4)--Announce all 2/3
+local warnHungerforLife					= mod:NewTargetAnnounce(180148, 3)
 
-local specWarnRampage				= mod:NewSpecialWarningSpell(155539, nil, nil, nil, 2)
+local specWarnShadowofDeath				= mod:NewSpecialWarningYou(179864)
+local specWarnTouchofDoom				= mod:NewSpecialWarningRun(179977, nil, nil, nil, 4, nil, 2)
+local specWarnSharedFate				= mod:NewSpecialWarningMoveTo(179908, nil, nil, nil, 3)--Only non rooted player get moveto. rooted player can't do anything.
+local yellSharedFate					= mod:NewYell(179909)--Only rooted player should yell
+local specWarnFeastofSouls				= mod:NewSpecialWarningSpell(181973, nil, nil, nil, 2)--Energy based, probably no cd, maybe add a soon announce off UNIT POWER
+local specWarnHungerforLife				= mod:NewSpecialWarningRun(180148, nil, nil, nil, 4, nil, 2)
+local specWarnBellowingShout			= mod:NewSpecialWarningInterrupt(181582, "-Healer", nil, nil, 1, nil, 2)
 
-local timerRampageCD				= mod:NewCDTimer(107, 155539)
+--local timerShadowofDeathCD			= mod:NewCDTimer(30, 179864)
+--local timerTouchofDoomCD				= mod:NewCDTimer(45, 179977)
+--local timerSharedFateCD				= mod:NewCDTimer(45, 179909)
 
 --local berserkTimer					= mod:NewBerserkTimer(360)
 
---local countdownInfernoSlice			= mod:NewCountdown(12, 155080, "Tank")
+local countdownShadowofDeath			= mod:NewCountdownFades("Alt5", 179864)
+local countdownDigest					= mod:NewCountdown("Alt45", 155080)--40+5
 
---local voiceInfernoSlice				= mod:NewVoice(155080)
+local voiceTouchofDoom					= mod:NewVoice(179977)--runout
+local voiceHungerforLife				= mod:NewVoice(180148)--justrun
+local voiceBellowingShout				= mod:NewVoice(181582, "-Healer")--kickcast
 
---mod:AddRangeFrameOption(8, 155530)
---mod:AddHudMapOption("HudMapOnShatter", 155530, false)
+mod:AddSetIconOption("SetIconOnFate", 179909)
+mod:AddHudMapOption("HudMapOnSharedFate", 179909)--Smart hud, distinquishes rooted from non rooted by color coding.
+mod:AddArrowOption("SharedFateArrow", 179909, true, 2)
+mod:AddRangeFrameOption(5, 182049)--Needs to be optimized when surging shadows is figured out.
 
-
-local debuffFilter
-do
-	local debuffName = GetSpellInfo(155323)
-	local UnitDebuff = UnitDebuff
-	debuffFilter = function(uId)
-		if UnitDebuff(uId, debuffName) then
-			return true
-		end
-	end
-end
+mod.vb.rootedFate = nil
+mod.vb.rootedFate2 = nil--Just in case, but if this happens you're doing things badly
 
 function mod:OnCombatStart(delay)
-
+	self.vb.rootedFate = nil
+	self.vb.rootedFate2 = nil
+	if self.Options.RangeFrame then
+		DBM.RangeCheck:Show(5)
+	end
 end
 
 function mod:OnCombatEnd()
 	if self.Options.RangeFrame then
 		DBM.RangeCheck:Hide()
 	end
-	if self.Options.HudMapOnShatter then
+	if self.Options.HudMapOnSharedFate then
 		DBMHudMap:Disable()
 	end
 end 
 
+local function sharedFateDelay(self)
+	if self.vb.rootedFate2 then--Check this first, assume you are linked to most recent
+		specWarnSharedFate:Show(self.vb.rootedFate2)
+	elseif self.vb.rootedFate then
+		specWarnSharedFate:Show(self.vb.rootedFate)
+	end
+end
+
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 155080 then
-
+	if spellId == 182049 then
+		DBM:Debug("Surging Shadows Cast", 2)
+	elseif spellId == 181973 then
+		specWarnFeastofSouls:Show()
+	elseif spellId == 182788 then
+		DBM:Debug("Crushing Darkness Cast", 2)
+	elseif spellId == 181582 and self:CheckInterruptFilter(args.sourceGUID) then
+		specWarnBellowingShout:Show(args.sourceName)
+		voiceBellowingShout:Play("kickcast")
 	end
 end
 
@@ -78,32 +109,79 @@ end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 155323 then
+	if spellId == 179864 then
+		warnShadowofDeath:CombinedShow(0.5, args.destName)--More than 1 target?
 		if args:IsPlayer() then
-
-			if self.Options.RangeFrame then
-				DBM.RangeCheck:Show(8)
+			specWarnShadowofDeath:Show()
+			countdownShadowofDeath:Start()
+			countdownDigest:Start()
+		end
+	elseif spellId == 179977 then
+		warnTouchofDoom:CombinedShow(0.5, args.destName)--More than 1 target?
+		if args:IsPlayer() then
+			specWarnTouchofDoom:Show()
+			voiceTouchofDoom:Play("runout")
+		end
+	elseif spellId == 179909 then--Root version
+		warnSharedFate:CombinedShow(0.5, args.destName)
+		if self.vb.rootedFate then--One already exists
+			self.vb.rootedFate2 = args.destName
+		else
+			self.vb.rootedFate = args.destName
+		end
+		if self.Options.SetIconOnFate then
+			if self.vb.rootedFate2 then
+				self:SetIcon(args.destName, 2)
+			else
+				self:SetIcon(args.destName, 1)
 			end
 		end
-		if self.Options.HudMapOnShatter then
-			DBMHudMap:RegisterRangeMarkerOnPartyMember(spellId, "timer", args.destName, 8, 10, 0, 1, 0, 0.6):Appear():RegisterForAlerts():Rotate(360, 9.5)
+		if self.Options.HudMapOnSharedFate then
+			DBMHudMap:RegisterRangeMarkerOnPartyMember(179909, "highlight", args.destName, 3, 900, 1, 1, 0, 0.5, nil, true):Pulse(0.5, 0.5)--Yellow
+		end
+		if args:IsPlayer() then
+			yellSharedFate:Yell()
+		end
+	elseif spellId == 179908 then--Non root version (must run to rooted player)
+		warnSharedFate:CombinedShow(0.5, args.destName)
+		if args:IsPlayer() then
+			self:Schedule(0.5, sharedFateDelay, self)--Just in case rooted ID fires after non rooted ones
+		end
+		if self.Options.HudMapOnSharedFate then
+			DBMHudMap:RegisterRangeMarkerOnPartyMember(179908, "highlight", args.destName, 3, 900, 0, 1, 0, 0.5, nil, true):Pulse(0.5, 0.5)--Green
+		end
+	elseif spellId == 180148 then
+		warnHungerforLife:CombinedShow(0.5, args.destName)--More than 1 target?
+		if args:IsPlayer() then
+			specWarnHungerforLife:Show()
+			voiceHungerforLife:Play("justrun")
 		end
 	end
 end
-mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+--mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 155323 then
-		if args:IsPlayer() and self.Options.RangeFrame then
-			DBM.RangeCheck:Hide()
+	if spellId == 179909 then--Root version
+		if self.vb.rootedFate == args.destName then
+			self.vb.rootedFate = nil
+		elseif self.vb.rootedFate2 == args.destName then
+			self.vb.rootedFate2 = nil
 		end
-		if self.Options.HudMapOnShatter then
-			DBMHudMap:FreeEncounterMarkerByTarget(spellId, args.destName)
+		if self.Options.HudMapOnSharedFate then
+			DBMHudMap:FreeEncounterMarkerByTarget(179909, args.destName)
+		end
+		if self.Options.SetIconOnFate then
+			self:SetIcon(args.destName, 0)
+		end
+	elseif spellId == 179908 then--Non root version (must run to rooted player)
+		if self.Options.HudMapOnSharedFate then
+			DBMHudMap:FreeEncounterMarkerByTarget(179908, args.destName)
 		end
 	end
 end
 
+--[[
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 	if spellId == 173195 then
 		
