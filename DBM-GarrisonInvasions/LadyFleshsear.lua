3d375144827f3260def4bdf867ba619cfb7a0eb7
Adam <MysticalOS@users.noreply.github.com>
2015-05-10 01:26:17 +0000

    Remove all timers from garrison invasion bosses, because the bosses all use scalable cd tech based on number of players, cds are shorter more players in raid, which makes it impossible for any semblance of timer accuracy. Lets hope blizzard NEVER does this on a real boss

--- DBM-GarrisonInvasions/LadyFleshsear.lua
+++ DBM-GarrisonInvasions/LadyFleshsear.lua
@@ -10,7 +10,7 @@ mod:SetMinCombatTime(15)
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 180779",
-	"SPELL_CAST_SUCCESS 180776 180774",
+	"SPELL_CAST_SUCCESS 180774",
 	"SPELL_AURA_APPLIED 180776",
 	"SPELL_PERIODIC_DAMAGE 180775"
 )
@@ -23,31 +23,16 @@ local yellOverwhelmingFlames	= mod:NewYell(180776)
 local specWarnRainofFireGTFO	= mod:NewSpecialWarningMove(180775)
 local specWarnCallofFlame		= mod:NewSpecialWarningSpell(180779, nil, nil, nil, 2)
 
-local timerOverwhelmingFlamesCD	= mod:NewCDTimer(21, 180776)--21-32 Delays caused by Call of Flame/Rain of Fire
-local timerRainofFireCD			= mod:NewCDTimer(23, 180774)--23-29 Delays caused by Call of Flame/Overwhelming Flames
-local timerCallofFlameCD		= mod:NewCDTimer(37, 180774)--37-38 Priority spell
-
-function mod:OnCombatStart(delay, summonTriggered)
-	if summonTriggered then
-		timerRainofFireCD:Start(11.5)
-		timerOverwhelmingFlamesCD:Start(18)
-		timerCallofFlameCD:Start(27)
-	end
-end
-
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 180779 then
 		specWarnCallofFlame:Show()
-		timerCallofFlameCD:Start()
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 180776 then
-		timerOverwhelmingFlamesCD:Start()
-	elseif spellId == 180774 then
+	if spellId == 180774 then
 		warnRainofFire:Show()
 	end
 end
