8ec5b80ca227c725138689025644db9eb848bbf7
Adam <MysticalOS@users.noreply.github.com>
2015-06-20 01:26:50 +0000

    make massive blast warnings clearer so one tank doesn’t taunt too early on mannoroth. Add a few trash warnings.

--- DBM-HellfireCitadel/HellfireCitadelTrash.lua
+++ DBM-HellfireCitadel/HellfireCitadelTrash.lua
@@ -7,44 +7,73 @@ mod:SetZone()
 mod.isTrashMod = true
 
 mod:RegisterEvents(
-	"SPELL_CAST_START",
-	"SPELL_AURA_APPLIED"
+	"SPELL_CAST_START 189595 189612",
+	"SPELL_AURA_APPLIED 189533 188476 182644 186961",
+	"SPELL_AURA_REMOVED 186961"
 )
---[[
-local warnLivingBlaze				= mod:NewTargetAnnounce(175583, 3, nil, false)
 
-local specWarnOverheadSmash			= mod:NewSpecialWarningTaunt(175765)
-local yellLivingBlaze				= mod:NewYell(175583)
+--TODO, http://ptr.wowhead.com/spell=188510/graggra-smash
+--http://ptr.wowhead.com/spell=188448/blazing-fel-touch
+--Add Solar Chakram once right spellid is seen
+--Add Phantasmal Corruption when trash version is observed.
+--Add Phantasmal Fel Bomb when trash version is observed
+--Add Harbinger's Mending when trash version is observed
+local warnDarkFate					= mod:NewTargetAnnounce(182644, 3)
+
+local specWarnCrowdControl			= mod:NewSpecialWarningSpell(189595, nil, nil, nil, 1, 2)--Maybe use a custom "Look Away" warning
+local specWarnSeverSoul				= mod:NewSpecialWarningYou(189533, nil, nil, nil, 1, 2)
+local specWarnSeverSoulOther		= mod:NewSpecialWarningTaunt(189533, nil, nil, nil, 1, 2)
+local specWarnBadBreathOther		= mod:NewSpecialWarningTaunt(188476, nil, nil, nil, 1, 2)
+local specWarnRendingHowl			= mod:NewSpecialWarningInterrupt(189612, "-Healer", nil, nil, 1, 2)
+local yellDarkFate					= mod:NewFadesYell(182644)
+
+local voiceSeverSoul				= mod:NewVoice(189533, "Tank")--changemt
+local voiceCrowdControl				= mod:NewVoice(189595)--turnaway
+local voiceRendingHowl				= mod:NewVoice(189612, "-Healer")--kickcast
 
 mod:RemoveOption("HealthFrame")
-
 
 function mod:SPELL_CAST_START(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
-	if spellId == 156446 then
-		
+	if spellId == 189595 then
+		specWarnCrowdControl:Show()
+		voiceCrowdControl:Play("turnaway")
+	elseif spellId == 189612 and self:CheckInterruptFilter(args.sourceGUID) and self:AntiSpam(3, 1) then
+		specWarnRendingHowl:Show(args.sourceName)
+		voiceRendingHowl:Play("kickcast")
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
-	if spellId == 175583 then
-
+	if spellId == 189533 then
+		voiceSeverSoul:Play("changemt")
 		if args:IsPlayer() then
-
-			if not self:IsLFR() then
-
-			end
+			specWarnSeverSoul:Show()
+		else
+			specWarnSeverSoulOther:Show(args.destName)
+		end
+	elseif spellId == 188476 and not args:IsPlayer() and not UnitDebuff("player", GetSpellInfo(188476)) then
+		specWarnBadBreathOther:Show(args.destName)
+	elseif spellId == 186961 then
+		warnDarkFate:Show(args.destName)
+		if args:IsPlayer() then
+			specWarnNetherBanish:Show()
+			yellDarkFate:Schedule(14, 1)
+			yellDarkFate:Schedule(13, 2)
+			yellDarkFate:Schedule(12, 3)
+			yellDarkFate:Schedule(11, 4)
+			yellDarkFate:Schedule(10, 5)
 		end
 	end
 end
-mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+--mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 159750 then
-		
+	if spellId == 186961 and args:IsPlayer() then
+		yellDarkFate:Cancel()
 	end
-end--]]
+end
