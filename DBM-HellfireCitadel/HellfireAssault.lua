0608da724d1f1812117f1b0d0a53b53acb5433a6
Adam <MysticalOS@users.noreply.github.com>
2015-06-24 23:56:51 +0000

    Enable bugfix notices

--- DBM-HellfireCitadel/HellfireAssault.lua
+++ DBM-HellfireCitadel/HellfireAssault.lua
@@ -6,6 +6,7 @@ mod:SetCreatureID(90019)--Main ID is door, door death= win. 94515 Siegemaster Ma
 mod:SetEncounterID(1778)
 mod:SetZone()
 --mod:SetUsedIcons(8, 7, 6, 4, 2, 1)
+mod:SetHotfixNoticeRev(13909)
 --mod:SetRespawnTime(20)
 
 mod:RegisterCombat("combat")
