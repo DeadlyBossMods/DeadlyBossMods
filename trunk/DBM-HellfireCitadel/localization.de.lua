fb6b9d91de7ec76ce6c814cd42a07ca9e2adb344
ebmor <ebmor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-06-26 18:55:31 +0000

    deDE update (Hellfire Citadel, without triggers)

--- trunk/DBM-HellfireCitadel/localization.de.lua
+++ trunk/DBM-HellfireCitadel/localization.de.lua
@@ -6,6 +6,20 @@ local L
 ---------------
 L= DBM:GetModLocalization(1426)
 
+L:SetTimerLocalization({
+	timerSiegeVehicleCD		= "Nächste Maschine %s",
+})
+
+L:SetOptionLocalization({
+	timerSiegeVehicleCD =	"Zeige Zeit bis nächste Belagerungsmaschinen erscheinen"
+})
+
+L:SetMiscLocalization({
+	AddsSpawn1		=	"Comin' in hot!",--translate (trigger) (unused)
+	AddsSpawn2		=	"Fire in the hole!",--translate (trigger) (unused)
+	BossLeaving		=	"I'll be back..."--translate (trigger)
+})
+
 ---------------------------
 -- Iron Reaver --
 ---------------------------
@@ -27,7 +41,7 @@ L= DBM:GetModLocalization(1392)
 L= DBM:GetModLocalization(1396)
 
 L:SetMiscLocalization({
-	BloodthirstersSoon		=	"Come brothers! Seize your destiny!"--translate
+	BloodthirstersSoon		=	"Come brothers! Seize your destiny!"--translate (trigger)
 })
 
 --------------------
@@ -35,17 +49,29 @@ L:SetMiscLocalization({
 --------------------
 L= DBM:GetModLocalization(1372)
 
+L:SetTimerLocalization({
+	SoDDPS		= "Nächste Schatten (%s)",
+	SoDTank		= "Nächste Schatten (%s)",
+	SoDHealer	= "Nächste Schatten (%s)"
+})
+
+L:SetOptionLocalization({
+	SoDDPS		= "Zeige Zeit bis nächste $spell:179864 die DDs betreffen",
+	SoDTank		= "Zeige Zeit bis nächste $spell:179864 die Tanks betreffen",
+	SoDHealer	= "Zeige Zeit bis nächste $spell:179864 die Heiler betreffen"
+})
+
 --------------------------
 -- Shadow-Lord Iskar --
 --------------------------
 L= DBM:GetModLocalization(1433)
 
 L:SetWarningLocalization({
-	specWarnThrowAnzu =	"Throw Eye of Anzu to %s!"--translate
+	specWarnThrowAnzu =	"Wirf Auge von Anzu zu %s!"
 })
 
 L:SetOptionLocalization({
-	specWarnThrowAnzu =	"Show special warning when you need to throw $spell:179202"--translate
+	specWarnThrowAnzu =	"Spezialwarnung, wenn du das $spell:179202 werfen musst"
 })
 
 --------------------------
@@ -53,10 +79,39 @@ L:SetOptionLocalization({
 --------------------------
 L= DBM:GetModLocalization(1391)
 
+L:SetWarningLocalization({
+	specWarnSeedPosition =	"Saatposition: %s"
+})
+
+L:SetOptionLocalization({
+	SeedsBehavior		= "Auswahl der Positionierungsschreie für Saat der Zerstörung (nur als Schlachtzugsleiter)",
+	Iconed				= "Totenschädel, Kreuz, Quadrat, Mond, Dreieck (für Strategien mit Weltmarkierungen)",
+	Numbered			= "1, 2, 3, 4, 5 (für Strategien mit nummerierten Positionen)",
+	DirectionLine		= "Links, Mitte Links, Mitte, Mitte Rechts, Rechts (typisch für geradlinige Formationen)", 
+	FreeForAll			= "Nur Standardschrei verwenden (ohne Positionszuweisung)",
+	--Currently these 3 below are unused unless I see anyone want/need them
+	CrossPerception		= "Vorne, Hinten, Links, Rechts, Mitte. (typisch für Strategien mit Kreuzformationen)",
+	CrossCardinal		= "Norden, Süden, Osten, Westen, Mitte. (typisch für Strategien mit Kreuzformationen)",
+	ExCardinal			= "Nordosten, Südosten, Nordwesten, Südwesten, Mitte (typisch für \"Ex\" Strategien)"
+})
+
+L:SetMiscLocalization({
+	DBMConfigMsg		= "Die Positionierungsschreie für Saat der Zerstörung wurden wie beim Schlachtzugsleiter auf \"%s\" gesetzt.",
+	BWConfigMsg			= "Der Schlachtzugsleiter nutzt \"BigWigs\". Die Positionierungsschreie für Saat der Zerstörung wurden zur Angleichung an \"BigWigs\" auf <...> gesetzt.",
+	customSeedsSay		= "Saat %s auf %s"
+})
+
 --------------------------
 -- Xhul'horac --
 --------------------------
 L= DBM:GetModLocalization(1447)
+
+L:SetOptionLocalization({
+	ChainsBehavior		= "Warnverhalten bei Dämonenketten",
+	Cast				= "nur ursprüngliches Ziel bei Zauberbeginn (Timersynchronisierung auf Zauberbeginn)",
+	Applied				= "nur betroffene Ziele bei Zauberende (Timersynchronisierung auf Zauberende)",
+	Both				= "urspüngliches Ziel bei Zauberbeginn und betroffene Ziele bei Zauberende"
+})
 
 --------------------------
 -- Socrethar the Eternal --
@@ -73,10 +128,36 @@ L= DBM:GetModLocalization(1394)
 --------------------------
 L= DBM:GetModLocalization(1395)
 
+L:SetMiscLocalization({
+	felSpire		=	"begins to empower the Fel Spire!"--translate (trigger)
+})
+
 --------------------------
 -- Archimonde --
 --------------------------
 L= DBM:GetModLocalization(1438)
+
+L:SetWarningLocalization({
+	specWarnBreakShackle	= "Gefesselte Pein: Breche als %s!"
+})
+
+L:SetOptionLocalization({
+	specWarnBreakShackle	= "Spezialwarnung, wenn du von $spell:184964 betroffen bist (diese Warnung weist zur Minimierung des gleichzeitigen Schadens die Brechungsreihenfolge automatisch zu)",
+	FilterOtherPhase		= "Zeige keine Warnungen für Ereignisse, die sich nicht in deiner Phase befinden"
+})
+
+L:SetMiscLocalization({
+	phase2				= "I grow tired of this pointless game. You face the immortal Legion, scourge of a thousand worlds.",--translate (trigger)
+	phase2point5		= "Look upon the endless forces of the Burning Legion and know the folly of your resistance.",--translate (trigger)
+	phase3				= "Enough! Your meaningless struggle ends now!",--translate (trigger)
+	phase3point5		= "I will shatter this pathetic world! Its broken husk will be torn apart in the twisting nether for all time!",--translate (trigger) (unused)
+	First				= "Erster",
+	Second				= "Zweiter",
+	Third				= "Dritter",
+	Fourth				= "Vierter",
+	Fifth				= "Fünfter",
+	customShackledSay	= "%s Gefesselt auf %s"
+})
 
 -------------
 --  Trash  --
