1fc898f36e264f6fc4c20662a92f7bfa6d12b48e
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-06-26 19:01:51 +0000

    Increase alpha before alpha users get bad notifications

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -53,8 +53,8 @@
 -------------------------------
 DBM = {
 	Revision = tonumber(("$Revision$"):sub(12, -3)),
-	DisplayVersion = "6.2.1 alpha", -- the string that is shown as version
-	ReleaseRevision = 13904 -- the revision of the latest stable version that is available
+	DisplayVersion = "6.2.2 alpha", -- the string that is shown as version
+	ReleaseRevision = 13933 -- the revision of the latest stable version that is available
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
