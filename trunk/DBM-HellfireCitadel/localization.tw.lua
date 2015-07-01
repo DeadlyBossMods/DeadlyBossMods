3a9a20bad196c9bb998690e8ae42ace47d39b2c2
ultrashining <ultrashining@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-07-01 15:12:21 +0000

    zhTW update

--- trunk/DBM-HellfireCitadel/localization.tw.lua
+++ trunk/DBM-HellfireCitadel/localization.tw.lua
@@ -7,17 +7,17 @@ local L
 L= DBM:GetModLocalization(1426)
 
 L:SetTimerLocalization({
-	timerSiegeVehicleCD		= "Next Vehicle %s",
+	timerSiegeVehicleCD		= "下一個載具%s",
 })
 
 L:SetOptionLocalization({
-	timerSiegeVehicleCD =	"Show timer for when new siege vehicles spawn"
+	timerSiegeVehicleCD =	"為下一個攻城載具重生顯示計時器"
 })
 
 L:SetMiscLocalization({
-	AddsSpawn1		=	"Comin' in hot!",--Blizzard seems to have disabled these
-	AddsSpawn2		=	"Fire in the hole!",--Blizzard seems to have disabled these
-	BossLeaving		=	"I'll be back..."
+	AddsSpawn1		=	"火力全開！",
+	AddsSpawn2		=	"開火！",
+	BossLeaving		=	"我會回來的…"
 })
 
 ---------------------------
@@ -41,7 +41,7 @@ L= DBM:GetModLocalization(1392)
 L= DBM:GetModLocalization(1396)
 
 L:SetMiscLocalization({
-	BloodthirstersSoon		=	"Come brothers! Seize your destiny!"
+	BloodthirstersSoon	= "都給我上！盡你們的責任！"
 })
 
 --------------------
@@ -49,17 +49,29 @@ L:SetMiscLocalization({
 --------------------
 L= DBM:GetModLocalization(1372)
 
+L:SetTimerLocalization({
+	SoDDPS		= "下一次死亡之影(%s)",
+	SoDTank		= "下一次死亡之影(%s)",
+	SoDHealer	= "下一次死亡之影(%s)"
+})
+
+L:SetOptionLocalization({
+	SoDDPS		= "為下一個傷害職中了$spell:179864顯示計時器",
+	SoDTank		= "為下一個坦克職中了$spell:179864顯示計時器",
+	SoDHealer	= "為下一個治療職中了$spell:179864顯示計時器"
+})
+
 --------------------------
 -- Shadow-Lord Iskar --
 --------------------------
 L= DBM:GetModLocalization(1433)
 
 L:SetWarningLocalization({
-	specWarnThrowAnzu =	"Throw Eye of Anzu to %s!"
+	specWarnThrowAnzu =	"把安祖之眼丟給%s！"
 })
 
 L:SetOptionLocalization({
-	specWarnThrowAnzu =	"Show special warning when you need to throw $spell:179202"
+	specWarnThrowAnzu =	"當你需要把$spell:179202丟給其他人顯示特別警告"
 })
 
 --------------------------
@@ -68,23 +80,24 @@ L:SetOptionLocalization({
 L= DBM:GetModLocalization(1391)
 
 L:SetWarningLocalization({
-	specWarnSeedPosition =	"Seed Position: %s"
+	specWarnSeedPosition =	"種子位置: %s"
 })
 
 L:SetOptionLocalization({
-	SeedsBehavior		= "Set seeds yell behavior for raid (Requires raid leader)",
-	Iconed				= "Star, Circle, Diamond, Triangle, Moon. Usuable for any strat using flare positions",--Default
-	Numbered			= "1, 2, 3, 4, 5. Usable for any strat using numbered positions.",
-	DirectionLine		= "Left, Middle Left, Middle, Middle Right, Right. Typical for straight line strat",
+	SeedsBehavior		= "設定團隊的種子大喊行為(需要團隊隊長)",
+	Iconed				= "星星,圈圈,鑽石,三角,月亮。適用於用於分散站位",
+	Numbered			= "1, 2, 3, 4, 5。適用於分區站位",
+	DirectionLine		= "左, 中偏左, 中間, 中偏右, 右。適用於直線站位",
+	FreeForAll			= "自由模式。不指定佔位，只使用普通的大喊",
 	--Currently these 3 below are unused unless I see anyone want/need them
-	CrossPerception		= "Front, Back, Left, Right, Middle. Typical for Cross strat",--Unsure if viable with 5 targets/will remain
-	CrossCardinal		= "North, South, East, West, Middle. Typical for Cross strat",--Unsure if viable 5 targets/will remain
-	ExCardinal			= "NorthEast, Southeast, Northwest, Southwest, Middle. Typical for Ex strat"--Unsure if viable 5 targets/will remain
+	CrossPerception		= "前, 後, 左, 右, 中。適用於十字站位",
+	CrossCardinal		= "北, 南, 東, 西, 中。適用於十字站位",
+	ExCardinal			= "東北, 東南, 西北, 西南, 中。適用於十字站位"
 })
 
 L:SetMiscLocalization({
-	DBMConfigMsg		= "Seed configuration set to %s to match raid leaders configuration.",
-	BWConfigMsg			= "Raid leader is using Bigwigs, configuring DBM to <Insert whatever the hell bigwigs ends up doing for this here> to match BW for seeds."
+	DBMConfigMsg		= "種子設定已設定為%s以符合團隊隊長設定。",
+	BWConfigMsg			= "團隊隊長使用Bigwigs中，設定DBM為編號以符合Bigwigs的種子大喊。"
 	--TODO, talk to some guilds, maybe trim list above, add finalized directions here
 })
 
@@ -94,10 +107,10 @@ L:SetMiscLocalization({
 L= DBM:GetModLocalization(1447)
 
 L:SetOptionLocalization({
-	ChainsBehavior		= "Set Fel Chains warning behavior",
-	Cast				= "Only give orininal target on begin cast. Timer syncs to cast start.",
-	Applied				= "Only give all targets affected on cast finish. Timer syncs to cast finish.",
-	Both				= "Give original target on cast start and all affected targets on cast finish. Timer syncs to cast start."
+	ChainsBehavior		= "設定魔化鎖鍊警告行為",
+	Cast				= "只給原始目標施放開始時警告。計時器同步在施放開始時。",
+	Applied				= "只會中招目標施放結束時警告。計時器同步在施放結束時。",
+	Both				= "原始目標施放開始時和中招目標施放結束時警告。"
 })
 
 --------------------------
@@ -116,7 +129,7 @@ L= DBM:GetModLocalization(1394)
 L= DBM:GetModLocalization(1395)
 
 L:SetMiscLocalization({
-	felSpire		=	"begins to empower the Fel Spire!"
+	felSpire		=	"開始強化惡魔尖塔！"
 })
 
 --------------------------
@@ -125,24 +138,24 @@ L:SetMiscLocalization({
 L= DBM:GetModLocalization(1438)
 
 L:SetWarningLocalization({
-	specWarnBreakShackle	= "Shackled Torment: Break %s!"
+	specWarnBreakShackle	= "束縛折磨：拉斷%s!"
 })
 
 L:SetOptionLocalization({
-	specWarnBreakShackle	= "Show special warning when affected by $spell:184964. This warning auto assigns break order to minimize similtanious damage.",
-	FilterOtherPhase		= "Filter out warnings for events not in same phase as you"
+	specWarnBreakShackle	= "當中了$spell:184964時顯示特別警告。此警告會自動分配拉斷順序去最小化承受傷害。",
+	FilterOtherPhase		= "過濾掉與你不同階段的警告"
 })
 
 L:SetMiscLocalization({
-	phase2				= "I grow tired of this pointless game. You face the immortal Legion, scourge of a thousand worlds.",--1 second faster than CLEU. Probably not needed, but just in case
-	phase2point5		= "Look upon the endless forces of the Burning Legion and know the folly of your resistance.",--3 seconds faster than CLEU, used as primary, slower CLEU secondary
-	phase3				= "Lok'tar ogar! They are pushed back! To the portal! Gul'dan is mine!",--11 seconds faster than CLEU, used as primary, slower CLEU secondary
+	phase2				= "我玩膩這種無聊的遊戲了。面對永垂不朽的軍團，萬千世界的災禍吧！",
+	phase2point5		= "看看燃燒軍團的軍容有多壯盛，就知道你們無謂的抵抗有多愚蠢。",
+	phase3				= "不勝即亡！惡魔被逼退了！攻向傳送門！把古爾丹留給我！",
 	phase3point5		= "I will shatter this pathetic world! Its broken husk will be torn apart in the twisting nether for all time!",--Not currently used. seems fire at same time as UNIT event that's present. Here only in case things change from now and live
-	First				= "First",
-	Second				= "Second",
-	Third				= "Third",
-	Fourth				= "Fourth",--Just in case, not sure how many targets in 30 man raid
-	Fifth				= "Fifth"--Just in case, not sure how many targets in 30 man raid
+	First				= "第一個",
+	Second				= "第二個",
+	Third				= "第三個",
+	Fourth				= "第四個",
+	Fifth				= "第五個",
 })
 
 -------------
