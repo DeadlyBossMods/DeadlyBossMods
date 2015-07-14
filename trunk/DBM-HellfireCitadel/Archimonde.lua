3517904448baf22f166126392eedeab6b941a97f
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-07-14 07:20:32 +0000

    Always show Burst warning, even if you are no where near it or not affected by it, still need to know it happened. Removed a bunch of non working timers from iron reaver mythic. Added volatilebomb cd bar for ground phase mythic Fixed unstable orb timers, they were nerfed significantly since i last looked at them. Removed redundant variable resets on pull that are already reset by ground phase trigger.

--- trunk/DBM-HellfireCitadel/Archimonde.lua
+++ trunk/DBM-HellfireCitadel/Archimonde.lua
@@ -34,6 +34,7 @@ mod:RegisterEventsInCombat(
 local warnDoomfireFixate			= mod:NewTargetAnnounce(182879, 3)
 local warnAllureofFlamesSoon		= mod:NewSoonAnnounce(183254, 2)
 local warnFelBurstSoon				= mod:NewSoonAnnounce(183817, 3)
+local warnFelBurst					= mod:NewTargetAnnounce(183817, 3)
 local warnDemonicHavoc				= mod:NewTargetAnnounce(183865, 3)--Mythic
 --Phase 2: Hand of the Legion
 local warnShackledTorment			= mod:NewTargetAnnounce(184964, 3)
@@ -382,6 +383,7 @@ function mod:SPELL_AURA_APPLIED(args)
 			yellDoomfireFixate:Yell()
 		end
 	elseif spellId == 183634 then
+		warnFelBurst:CombinedShow(0.3, args.destName)
 		if args:IsPlayer() then
 			specWarnFelBurst:Show()
 			yellFelBurst:Yell()
