3e23afaf87209cbaf55c744388c0ae7a689173ea
Adam <MysticalOS@users.noreply.github.com>
2015-07-17 10:46:15 +0000

    BW revision

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -388,7 +388,7 @@ local statusWhisperDisabled = false
 local wowTOC = select(4, GetBuildInfo())
 local dbmToc = 0
 
-local fakeBWRevision = 13433
+local fakeBWRevision = 13459
 
 local enableIcons = true -- set to false when a raid leader or a promoted player has a newer version of DBM
 local guiRequested = false
