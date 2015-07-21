20a6329885bbbb4b42eac1b0598b454107efd21e
Adam <MysticalOS@users.noreply.github.com>
2015-07-21 03:16:39 +0000

    fix bug

--- DBM-HellfireCitadel/IronReaver.lua
+++ DBM-HellfireCitadel/IronReaver.lua
@@ -114,6 +114,7 @@ local function sortInfoFrame(a, b)
 	if a > b then return true else return false end
 end
 
+local reactiveName, burningName, quickfuseName, reinforcedName, volatileName = GetSpellInfo(186676), GetSpellInfo(186667), GetSpellInfo(186660), GetSpellInfo(188294), GetSpellInfo(182523)
 local function updateInfoFrame()
 	table.wipe(lines)
 	local total = mod.vb.burningCount + mod.vb.reactiveCount + mod.vb.quickfuseCount + mod.vb.volatileCount + mod.vb.reinforcedCount
