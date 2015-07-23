4b7b001756a3a54f471b338900edb93c6026e775
Adam <MysticalOS@users.noreply.github.com>
2015-07-23 21:43:16 +0000

    *Fix another stupid infernal/imp bug on mannoroth non mythic *substantial work on mythic mannoroth now that a public log exists on WCL. Still incomplete since it’s impossible to make mod perfect without transcriptor to see phase changes.

--- DBM-HellfireCitadel/Mannoroth.lua
+++ DBM-HellfireCitadel/Mannoroth.lua
@@ -11,11 +11,11 @@ mod.respawnTime = 30
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 181126 181132 181557 183376 181793 181792 181738 181799 182084 185830 181948 182040 182076 182077 186348 181099 181597 182006",
+	"SPELL_CAST_START 181126 181132 181557 183376 181793 181792 181738 181799 182084 185830 181948 182040 182076 182077 181099 181597 182006",
 	"SPELL_CAST_SUCCESS 181190 181597 182006",
-	"SPELL_AURA_APPLIED 181099 181275 181191 181597 182006",
+	"SPELL_AURA_APPLIED 181099 181275 181191 181597 182006 186362",
 	"SPELL_AURA_APPLIED_DOSE 181119",
-	"SPELL_AURA_REMOVED 181099 181275 185147 182212 185175 181597 182006 181275",
+	"SPELL_AURA_REMOVED 181099 181275 185147 182212 185175 181597 182006 181275 186362",
 --	"SPELL_PERIODIC_DAMAGE",
 --	"SPELL_ABSORBED",
 	"SPELL_SUMMON 181255 181180",
@@ -25,14 +25,13 @@ mod:RegisterEventsInCombat(
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---(ability.id = 181557 or ability.id = 181948 or ability.id = 181799 or ability.id = 182084 or ability.id = 186348) and type = "begincast" or (ability.id = 181597 or ability.id = 182006) and type = "cast" or (ability.id = 185147 or ability.id = 182212 or ability.id = 185175) and type = "removebuff"
---TODO, get timer for 2nd doom lord spawning, if some group decides to do portals in a bad order and not kill that portal summoner first
---TODO, get longer phase 4 log because log i have isn't long enough to see why felstorm has a longer cd in phase 4
+--(ability.id = 181557 or ability.id = 181948 or ability.id = 181799 or ability.id = 182084 or ability.id = 186348 or ability.id = 181793 or ability.id = 183377 or ability.id = 185831 or ability.id = 182077) and type = "begincast" or (ability.id = 181597 or ability.id = 182006) and type = "cast" or (ability.id = 185147 or ability.id = 182212 or ability.id = 185175) and type = "removebuff" or (ability.id = 186362 or ability.id = 181275) and type = "applydebuff"
+--(ability.id = 181255 or ability.id = 181180) and type = "summon" or (ability.id = 186362 or ability.id = 181275) and type = "applydebuff"
+--TODO, get timer for 2nd doom lord spawning on non mythic, if some group decides to do portals in a bad order and not kill that portal summoner first
 --TODO, custom voice for shadowforce? It works almost identical to helm of command from lei shen. Did that have a voice usuable here?
---TODO, Mythic timers and whether or not they reset on phase changes
 --Adds
 ----Doom Lords
-local warnCurseoftheLegion			= mod:NewTargetAnnounce(181275, 3)--Spawn
+local warnCurseoftheLegion			= mod:NewTargetCountAnnounce(181275, 3)--Spawn
 local warnMarkofDoom				= mod:NewTargetAnnounce(181099, 4)
 local warnDoomSpike					= mod:NewStackAnnounce(181119, 3, nil, "Tank")
 ----Fel Imp
@@ -40,6 +39,8 @@ local warnFelImplosion				= mod:NewCountAnnounce(181255, 3)--Spawn
 ----Dread Infernals
 local warnInferno					= mod:NewCountAnnounce(181180, 3)--Spawn
 local warnFelStreak					= mod:NewSpellAnnounce(181190, 3, nil, "Melee")--Change to target scan/personal/near warning if possible
+--Gul'dan
+local warnWrathofGuldan				= mod:NewTargetAnnounce(186362, 4)
 --Mannoroth
 local warnPhase2					= mod:NewPhaseAnnounce(2)
 local warnPhase3					= mod:NewPhaseAnnounce(3)
@@ -60,7 +61,8 @@ local specWarnFelBlast				= mod:NewSpecialWarningInterrupt(181132, false, nil, 2
 ----Dread Infernals
 local specWarnFelHellfire			= mod:NewSpecialWarningDodge(181191, "Melee", nil, nil, 4, 2)
 ----Gul'dan
-local specWarnWrathofGuldan			= mod:NewSpecialWarningSpell(186348, nil, nil, nil, 2)
+local specWarnWrathofGuldan			= mod:NewSpecialWarningYou(186362, nil, nil, nil, 1)
+local yellWrathofGuldan				= mod:NewYell(186362)
 --Mannoroth
 local specWarnGlaiveCombo			= mod:NewSpecialWarningSpell(181354, "Tank", nil, nil, 3, 2)--Active mitigation or die mechanic
 local specWarnMassiveBlast			= mod:NewSpecialWarningSpell(181359, nil, nil, nil, 1, 2)
@@ -74,7 +76,7 @@ local specWarnShadowForce			= mod:NewSpecialWarningSpell(181799, nil, nil, nil, 
 --Adds
 mod:AddTimerLine(OTHER)
 ----Doom Lords
-local timerCurseofLegionCD			= mod:NewAITimer(107, 181275, nil, nil, nil, 1)--Maybe see one day, in LFR or something when group is terrible or doesn't kill doom lord portal first
+local timerCurseofLegionCD			= mod:NewNextCountTimer(65, 181275, nil, nil, nil, 1)--Maybe see one day, in LFR or something when group is terrible or doesn't kill doom lord portal first
 local timerMarkofDoomCD				= mod:NewCDTimer(31.5, 181099, nil, "-Tank", nil, 3)
 local timerShadowBoltVolleyCD		= mod:NewCDTimer(12, 181126, nil, "-Healer", nil, 4)
 ----Fel Imps
@@ -82,7 +84,7 @@ local timerFelImplosionCD			= mod:NewNextCountTimer(46, 181255, nil, nil, nil, 1
 ----Infernals
 local timerInfernoCD				= mod:NewNextCountTimer(107, 181180, nil, nil, nil, 1)
 ----Gul'dan
-local timerWrathofGuldanCD			= mod:NewAITimer(107, 186348, nil, nil, nil, 3)
+local timerWrathofGuldanCD			= mod:NewCDTimer(107, 186348, nil, nil, nil, 3)
 --Mannoroth
 mod:AddTimerLine(L.name)
 local timerGlaiveComboCD			= mod:NewCDTimer(30, 181354, nil, "Tank", nil, 5)--30 seconds unless delayed by something else
@@ -116,6 +118,7 @@ mod.vb.portalsLeft = 3
 mod.vb.phase = 1
 mod.vb.impCount = 0
 mod.vb.infernalCount = 0
+mod.vb.doomlordCount = 0
 mod.vb.ignoreAdds = false
 local phase1ImpTimers = {15, 32.2, 24, 15, 10}--Spawn 33% faster each wave, but cannot confirm it goes lower than 10, if it does, next would be 6.6
 local phase1ImpTimersN = {15, 32.2, 24, 24}--Normal doesn't go below 24? need larger sample size. Normal differently two 24s in a row and didn't drop to 15
@@ -130,6 +133,7 @@ local phase3InfernalTimersN = {28, 34.8, 35, 34.8, 34.8}--^^
 
 local gazeTargets = {}
 local doomTargets = {}
+local guldanTargets = {}
 local AddsSeen = {}
 local playerName = UnitName("player")
 local debuffFilter
@@ -165,10 +169,11 @@ local function sortInfoFrame(a, b)
 	if a < b then return true else return false end
 end
 
-local gaze1, gaze2 = GetSpellInfo(181597), GetSpellInfo(182006)
+local gaze1, gaze2, guldanSpellName = GetSpellInfo(181597), GetSpellInfo(182006), GetSpellInfo(186362)
 local function updateInfoFrame()
 	table.wipe(lines)
 	local total = 0
+	local total2 = 0
 	for i = 1, #gazeTargets do
 		local name = gazeTargets[i]
 		local uId = DBM:GetRaidUnitId(name)
@@ -177,7 +182,17 @@ local function updateInfoFrame()
 			lines[name] = i
 		end
 	end
-	if total == 0 then--None found, hide infoframe because all broke
+	--Mythic, show guldan targets and number of charges left
+	for i = 1, #guldanTargets do
+		local name = guldanTargets[i]
+		local uId = DBM:GetRaidUnitId(name)
+		local _, _, _, currentStack = UnitDebuff(uId, guldanSpellName)
+		if currentStack then
+			total2 = total2 + 1
+			lines[name] = currentStack
+		end
+	end
+	if total == 0 and total2 == 0 then--None found, hide infoframe because all broke
 		DBM.InfoFrame:Hide()
 	end
 	return lines
@@ -193,9 +208,9 @@ local function warnGazeTargts(self)
 			yellGaze:Yell(i)
 		end
 	end
-	if self.Options.InfoFrame then
-		DBM.InfoFrame:SetHeader(gaze1)
-		DBM.InfoFrame:Show(5, "function", updateInfoFrame, sortInfoFrame)
+	if self.Options.InfoFrame and not DBM.InfoFrame:IsShown() then
+		--DBM.InfoFrame:SetHeader(gaze1)
+		DBM.InfoFrame:Show(8, "function", updateInfoFrame, sortInfoFrame)
 	end
 end
 
@@ -215,20 +230,28 @@ function mod:OnCombatStart(delay)
 	table.wipe(doomTargets)
 	table.wipe(gazeTargets)
 	table.wipe(AddsSeen)
+	table.wipe(guldanTargets)
 	self.vb.ignoreAdds = false
 	self.vb.impCount = 0
 	self.vb.infernalCount = 0
+	self.vb.doomlordCount = 0
 	self.vb.phase = 1
 	self.vb.portalsLeft = 3
 	self.vb.DoomTargetCount = 0
-	timerFelImplosionCD:Start(15-delay, 1)
-	timerInfernoCD:Start(18.4-delay, 1)--Verify, seems 20 now
-	timerCurseofLegionCD:Start(1)
 	if self:IsMythic() then
-		--Start fight on phase 2?
-		--Do phase 2 or phase 1 infernal/imp timers get used on combat start?
-		timerWrathofGuldanCD:Start(1)
-		DBM:AddMsg("This mod will not work correctly on mythic. Not until someones makes their logs available for this. Or more idealy, captures logs with transcriptor so the phase change detection can be fixed.")
+		timerCurseofLegionCD:Start(23, 1)
+		timerFelHellfireCD:Start(28)
+		timerGlaiveComboCD:Start(42.5)
+		countdownGlaiveCombo:Start(42.5)
+		timerFelImplosionCD:Start(45-delay, 1)
+		timerFelSeekerCD:Start(57.8)
+		timerGazeCD:Start(68)
+		timerInfernoCD:Start(70-delay, 1)--Verify, seems 20 now
+		DBM:AddMsg("This mod may contain errors on mythic. Log captured with Transcriptor may be needed to perfect this mod.")
+	else
+		timerCurseofLegionCD:Start(6, 1)
+		timerFelImplosionCD:Start(15-delay, 1)
+		timerInfernoCD:Start(18.4-delay, 1)--Verify, seems 20 now
 	end
 end
 
@@ -277,9 +300,6 @@ function mod:SPELL_CAST_START(args)
 		countdownShadowForce:Start(52.5)
 		if self:IsTank() and self.vb.phase == 3 then return end--Doesn't target tanks in phase 3, ever.
 		specWarnShadowForce:Show()
-	elseif spellId == 186348 then
-		specWarnWrathofGuldan:Show()
-		timerWrathofGuldanCD:Start()
 	elseif spellId == 181099 then
 		table.wipe(doomTargets)
 	elseif spellId == 181597 or spellId == 182006 then
@@ -294,7 +314,9 @@ function mod:SPELL_SUMMON(args)
 		warnFelImplosion:Show(self.vb.impCount)
 		if self.vb.ignoreAdds then return end--Ignore late sets of adds that spawn after phase transition but before summon adds script runs that updates timers for new phase
 		local nextCount = self.vb.impCount + 1
-		if self.vb.phase == 1 then
+		if self:IsMythic() then
+			timerFelImplosionCD:Start(49, nextCount)
+		elseif self.vb.phase == 1 then
 			local timers1 = self:IsNormal() and phase1ImpTimersN[nextCount] or phase1ImpTimers[nextCount]
 			if timers1 then
 				timerFelImplosionCD:Start(timers1, nextCount)
@@ -316,7 +338,7 @@ function mod:SPELL_SUMMON(args)
 				timerInfernoCD:Start(timers1, nextCount)
 			end
 		elseif self.vb.phase == 2 then
-			local timers2 = self:IsNormal() and phase2InfernalTimersN[nextCount] or phase2InfernalTimers[nextCount]
+			local timers2 = self:IsMythic() and 55 or self:IsNormal() and phase2InfernalTimersN[nextCount] or phase2InfernalTimers[nextCount]
 			if timers2 then
 				timerInfernoCD:Start(timers2, nextCount)
 			end
@@ -341,7 +363,8 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
 	if spellId == 181275 then
-		timerCurseofLegionCD:Start()
+		self.vb.doomlordCount = self.vb.doomlordCount + 1
+		timerCurseofLegionCD:Start(nil, self.vb.doomlordCount+1)
 		if args:IsPlayer() then
 			specWarnCurseofLegion:Show()
 			local _, _, _, _, _, _, expires = UnitDebuff("Player", args.spellName)
@@ -352,7 +375,7 @@ function mod:SPELL_AURA_APPLIED(args)
 			yellCurseofLegion:Schedule(debuffTime - 2, 4)
 			yellCurseofLegion:Schedule(debuffTime - 5, 5)
 		else
-			warnCurseoftheLegion:Show(args.destName)
+			warnCurseoftheLegion:Show(self.vb.doomlordCount, args.destName)
 		end
 	elseif spellId == 181099 then
 		timerMarkofDoomCD:Start(args.sourceGUID)
@@ -392,6 +415,16 @@ function mod:SPELL_AURA_APPLIED(args)
 				specWarnDoomSpikeOther:Show(args.destName)
 			end
 		end
+	elseif spellId == 186362 then--Only cast once per phase transition (twice whole fight)
+		table.insert(guldanTargets, args.destName)
+		warnWrathofGuldan:CombinedShow(0.3, args.destName)
+		if args:IsPlayer() then
+			specWarnWrathofGuldan:Show()
+			yellWrathofGuldan:Yell()
+		end
+		if self.Options.InfoFrame and not DBM.InfoFrame:IsShown() then
+			DBM.InfoFrame:Show(8, "function", updateInfoFrame, sortInfoFrame)
+		end
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
@@ -406,29 +439,29 @@ function mod:SPELL_AURA_REMOVED(args)
 		updateRangeFrame(self)
 	elseif spellId == 185147 or spellId == 182212 or spellId == 185175 then--Portals
 		self.vb.portalsLeft = self.vb.portalsLeft - 1
-		if spellId == 185147 then--Doom Lords Portal
+		if spellId == 185147 and not self:IsMythic() then--Doom Lords Portal
 			timerCurseofLegionCD:Cancel()
 			--I'd add a cancel for the Doom Lords here, but since everyone killed this portal first
 			--no one ever actually learned what the cooldown was, so no timer to cancel yet!
-		elseif spellId == 182212 then--Infernals Portal
+		elseif spellId == 182212 and not self:IsMythic() then--Infernals Portal
 			timerInfernoCD:Cancel()
-		elseif spellId == 185175 then--Imps Portal
+		elseif spellId == 185175 and not self:IsMythic() then--Imps Portal
 			timerFelImplosionCD:Cancel()
 		end
 		if self.vb.portalsLeft == 0 and self:AntiSpam(10, 4) and self:IsInCombat() then
 			self.vb.phase = 2
-			self.vb.ignoreAdds = true
-			--These should be active already from pull on mythic
-			--Whether or not they update is unknown, better to start no timers until more info
+			warnPhase2:Show()
+			voicePhaseChange:Play("ptwo")
 			if not self:IsMythic() then
+				self.vb.ignoreAdds = true
+				--These should be active already from pull on mythic
+				--Whether or not they update is unknown, better to start no timers until more info
 				timerFelHellfireCD:Start(28)
 				timerGazeCD:Start(40)
 				timerGlaiveComboCD:Start(42.5)
 				countdownGlaiveCombo:Start(42.5)
 				timerFelSeekerCD:Start(58)
 			end
-			warnPhase2:Show()
-			voicePhaseChange:Play("ptwo")
 		end
 	elseif spellId == 181597 or spellId == 182006 then
 		if self.Options.HudMapOnGaze then
@@ -438,6 +471,8 @@ function mod:SPELL_AURA_REMOVED(args)
 		if args:IsPlayer() then
 			yellCurseofLegion:Cancel()
 		end
+	elseif spellId == 186362 then--Only cast once per phase transition (twice whole fight)
+		table.remove(guldanTargets, args.destName)
 	end
 end
 
@@ -486,10 +521,21 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, npc)
 			timerGazeCD:Start(44.5)
 			timerGlaiveComboCD:Start(44.9)
 			countdownGlaiveCombo:Start(44.9)
-			self.vb.ignoreAdds = true
 			timerFelSeekerCD:Start(68)
 			warnPhase3:Show()
 			voicePhaseChange:Play("pthree")
+			if self:IsMythic() then--Not sure this emote even happens on mythic
+				self.vb.impCount = 0
+				self.vb.doomlordCount = 0
+				timerCurseofLegionCD:Cancel()
+				timerFelImplosionCD:Cancel()
+				timerInfernoCD:Cancel()
+				timerWrathofGuldanCD:Start(9.5)--Still requires transcriptor log to be precise
+				timerFelImplosionCD:Start(40, 1)--Probably not right place to start it but without transcirtor, this is only way
+				timerCurseofLegionCD:Start(62.4, 1)--This may not be correct place to do it but it's only place I can do it using WCL public logs. Need transcriptor to verify
+			else
+				self.vb.ignoreAdds = true
+			end
 		elseif self.vb.phase == 4 then
 			timerFelHellfireCD:Cancel()
 			timerShadowForceCD:Cancel()
@@ -508,12 +554,19 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, npc)
 			timerFelSeekerCD:Start(65.6)
 			warnPhase4:Show()
 			voicePhaseChange:Play("pfour")
+			if self:IsMythic() then--Not sure this emote even happens on mythic
+				self.vb.impCount = 0
+				timerCurseofLegionCD:Cancel()
+				timerWrathofGuldanCD:Start(15)--Still requires transcriptor log to be precise
+				timerFelImplosionCD:Cancel()--is this correct place to start it?
+				timerFelImplosionCD:Start(25, 1)
+			end
 		end
 	end
 end
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if spellId == 181735 then
+	if spellId == 181735 then--0.1 seconds faster than combat log event for 10 yard cast.
 		specWarnFelSeeker:Show()
 		timerFelSeekerCD:Start()
 		voiceFelSeeker:Play("watchstep")
@@ -522,23 +575,28 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 		self.vb.ignoreAdds = false
 		self.vb.impCount = 0
 		self.vb.infernalCount = 0
-		timerFelImplosionCD:Cancel()
-		timerInfernoCD:Cancel()
-		timerFelImplosionCD:Start(24.5, 1)
-		timerInfernoCD:Start(47.5, 1)
+		--TODO, fix on mythic these adds timers
+		if not self:IsMythic() then
+			timerFelImplosionCD:Cancel()
+			timerInfernoCD:Cancel()
+			timerFelImplosionCD:Start(24.5, 1)
+			timerInfernoCD:Start(47.5, 1)
+		end
 	elseif spellId == 182262 then--Summon Adds (phase 3 start)
 		DBM:Debug("Summon adds 182262 fired", 2)
 		self.vb.ignoreAdds = false
 		self.vb.infernalCount = 0
-		timerFelImplosionCD:Cancel()
-		timerInfernoCD:Cancel()
-		timerInfernoCD:Start(28, 1)
+		--TODO, fix on mythic these adds timers
+		if not self:IsMythic() then
+			timerFelImplosionCD:Cancel()
+			timerInfernoCD:Cancel()
+			timerInfernoCD:Start(28, 1)
+		end
 	elseif spellId == 181156 then--Exists on wowhead and added in 6.2, likely related to this fight but unknown purpose, probably mythic?
 		DBM:Debug("Summon adds 181156 fired", 2)
 	--Backup phase detection. a bit slower than CHAT_MSG_RAID_BOSS_EMOTE (5.5 seconds slower)
 	elseif spellId == 182263 and self.vb.phase == 2 then--Phase 3
 		self.vb.phase = 3
-		self.vb.ignoreAdds = true
 		timerFelHellfireCD:Cancel()
 		timerShadowForceCD:Cancel()
 		countdownShadowForce:Cancel()
@@ -553,13 +611,19 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 		timerGlaiveComboCD:Start(39.4)
 		countdownGlaiveCombo:Start(39.4)
 		timerFelSeekerCD:Start(62.5)
-		if self:IsMythic() then
-			--Assumed it may not reset like other abilities
-			timerWrathofGuldanCD:Cancel()
-			timerWrathofGuldanCD:Start(3)
-		end
 		warnPhase3:Show()
 		voicePhaseChange:Play("pthree")
+		if self:IsMythic() then
+			self.vb.impCount = 0
+			timerCurseofLegionCD:Cancel()
+			timerFelImplosionCD:Cancel()
+			timerInfernoCD:Cancel()
+			timerWrathofGuldanCD:Start(4.5)--Still requires transcriptor log to be precise
+			timerFelImplosionCD:Start(35, 1)--Probably not right place to start it but without transcirtor, this is only way
+			timerCurseofLegionCD:Start(57.4)--This may not be correct place to do it but it's only place I can do it using WCL public logs. Need transcriptor to verify
+		else
+			self.vb.ignoreAdds = true
+		end
 	elseif spellId == 185690 and self.vb.phase == 3 then--Phase 4
 		self.vb.phase = 4
 		timerFelHellfireCD:Cancel()
@@ -577,14 +641,16 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 		timerShadowForceCD:Start(41.8)
 		countdownShadowForce:Start(45.8)
 		timerFelSeekerCD:Start(60.1)
-		if self:IsMythic() then
-			--Assumed it may not reset like other abilities
-			timerWrathofGuldanCD:Cancel()
-			timerWrathofGuldanCD:Start(4)
-		end
 		warnPhase4:Show()
 		voicePhaseChange:Play("pfour")
-	elseif spellId == 181354 then--183377 or 185831 also usable with SPELL_CAST_START but i like this way more, cleaner.
+		if self:IsMythic() then
+			self.vb.impCount = 0
+			timerCurseofLegionCD:Cancel()
+			timerWrathofGuldanCD:Start(10)--Still requires transcriptor log to be precise
+			timerFelImplosionCD:Cancel()--is this correct place to start it?
+			timerFelImplosionCD:Start(20, 1)
+		end
+	elseif spellId == 181354 then--183377 or 185831 also usable with SPELL_CAST_START but i like this way more, cleaner than Antispamming the other spellids
 		specWarnGlaiveCombo:Show()
 		timerGlaiveComboCD:Start()
 		countdownGlaiveCombo:Start()
