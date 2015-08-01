f7fe7932de15b8324bcbffb4848d57c5de0663bc
Adam <MysticalOS@users.noreply.github.com>
2015-08-01 04:11:52 +0000

    Tweaks to last

--- DBM-HellfireCitadel/Gorefiend.lua
+++ DBM-HellfireCitadel/Gorefiend.lua
@@ -114,6 +114,18 @@ Mythic
 84s: 2 DPS
 --]]
 
+local function sharedFateDelay(self)
+	if self.vb.rootedFate2 then--Check this first, assume you are linked to most recent
+		specWarnSharedFate:Show(self.vb.rootedFate2)
+		voiceSharedFate:Play("linegather")
+		DBMHudMap:AddEdge(0, 1, 0, 0.5, 600, UnitName("player"), self.vb.rootedFate2)
+	elseif self.vb.rootedFate then
+		specWarnSharedFate:Show(self.vb.rootedFate)
+		voiceSharedFate:Play("linegather")
+		DBMHudMap:AddEdge(0, 1, 0, 0.5, 600, UnitName("player"), self.vb.rootedFate)
+	end
+end
+
 function mod:OnCombatStart(delay)
 	self.vb.rootedFate = nil
 	self.vb.rootedFate2 = nil
@@ -162,18 +174,6 @@ function mod:OnCombatEnd()
 		DBM.InfoFrame:Hide()
 	end
 end 
-
-local function sharedFateDelay(self)
-	if self.vb.rootedFate2 then--Check this first, assume you are linked to most recent
-		specWarnSharedFate:Show(self.vb.rootedFate2)
-		voiceSharedFate:Play("linegather")
-		DBMHudMap:AddEdge(0, 1, 0, 0.5, 600, UnitName("player"), self.vb.rootedFate2)
-	elseif self.vb.rootedFate then
-		specWarnSharedFate:Show(self.vb.rootedFate)
-		voiceSharedFate:Play("linegather")
-		DBMHudMap:AddEdge(0, 1, 0, 0.5, 600, UnitName("player"), self.vb.rootedFate)
-	end
-end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
