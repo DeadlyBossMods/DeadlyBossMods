a59dbffa23b07a2c8f8c02da0d20141fe2c36bc0
Adam <MysticalOS@users.noreply.github.com>
2015-08-01 03:17:26 +0000

    Change how font debuff does countdown

--- DBM-HellfireCitadel/Velhari.lua
+++ DBM-HellfireCitadel/Velhari.lua
@@ -272,9 +272,13 @@ function mod:SPELL_AURA_APPLIED(args)
 	elseif spellId == 180526 then
 		warnFontofCorruption:CombinedShow(0.3, args.destName)
 		if args:IsPlayer() then
+			local _, _, _, _, _, duration, expires, _, _ = UnitDebuff("player", args.spellName)--Find out what our specific seed timer is
+			if expires then
+				local remaining = expires-GetTime()
+				countdownFontofCorruption:Start(remaining)
+			end
 			specWarnFontofCorruption:Show()
 			yellFontofCorruption:Yell()
-			countdownFontofCorruption:Start()
 		end
 	elseif spellId == 180025 then
 		specWarnHarbingersMendingDispel:Show(args.destName)
