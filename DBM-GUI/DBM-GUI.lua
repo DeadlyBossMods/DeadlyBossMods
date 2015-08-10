1bd5cecd2ff353a761872526214095c72ef3ce96
Adam <MysticalOS@users.noreply.github.com>
2015-08-10 23:37:36 +0000

    Add disable for this, because people are going to ask for it

--- DBM-GUI/DBM-GUI.lua
+++ DBM-GUI/DBM-GUI.lua
@@ -3510,6 +3510,7 @@ local function CreateOptionsMenu()
 		local WorldBossNearAlert	= soundAlertsArea:CreateCheckButton(L.WorldBossNearAlert, true, nil, "WorldBossNearAlert")
 		local RLReadyCheckSound		= soundAlertsArea:CreateCheckButton(L.RLReadyCheckSound, true, nil, "RLReadyCheckSound")
 		local AFKHealthWarning		= soundAlertsArea:CreateCheckButton(L.AFKHealthWarning, true, nil, "AFKHealthWarning")
+		local AutoReplySound		= soundAlertsArea:CreateCheckButton(L.AutoReplySound, true, nil, "AutoReplySound")
 
 		local generaltimeroptions	= extraFeaturesPanel:CreateArea(L.TimerGeneral, nil, 125, true)
 
