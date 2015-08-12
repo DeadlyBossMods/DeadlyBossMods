245b18be9590c4e846655a19d9954ccd31ac8dfc
Adam <MysticalOS@users.noreply.github.com>
2015-08-12 03:41:37 +0000

    Some kazzak fixes

--- DBM-Draenor/localization.en.lua
+++ DBM-Draenor/localization.en.lua
@@ -19,3 +19,7 @@ L= DBM:GetModLocalization(1262)
 -- Supreme Lord Kazzak --
 -------------------------
 L= DBM:GetModLocalization(1452)
+
+L:SetMiscLocalization({
+	Pull				= "You face the might of the Burning Legion!"
+})
