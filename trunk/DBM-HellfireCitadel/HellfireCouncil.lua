fe51d5e5b7c69b9ff603fc60e4fdb851d6458651
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-08-12 23:20:01 +0000

    Improve fel rage timers too after log scouring. Best log was https://www.warcraftlogs.com/reports/1Aznpyht3MJP7Yv8#fight=7&pins=2%24Off%24%23244F4B%24expression%24ability.id+%3D+184360+and+type+%3D+%22applydebuff%22&view=events

--- trunk/DBM-HellfireCitadel/HellfireCouncil.lua
+++ trunk/DBM-HellfireCitadel/HellfireCouncil.lua
@@ -73,7 +73,7 @@ local timerNightmareVisageCD		= mod:NewCDTimer(30, 184657, nil, "Tank", nil, 5)
 local timerDarknessCD				= mod:NewCDTimer(75, 184681, nil, nil, nil, 2)
 mod:AddTimerLine(Gurtogg)
 --Gurtogg Bloodboil
-local timerRelRageCD				= mod:NewCDCountTimer(62, 184360, nil, nil, nil, 3)--62-84 (maybe this is HP based, cause this variation is stupid)
+local timerRelRageCD				= mod:NewCDCountTimer(60, 184360, nil, nil, nil, 3)--60-84 (maybe this is HP based, cause this variation is stupid)
 local timerDemoLeapCD				= mod:NewCDTimer(75, 184366, nil, nil, nil, 2)--Most will never see this ability since he's 3rd in the special rotation and he dies first in most strats
 local timerTaintedBloodCD			= mod:NewNextCountTimer(15.8, 184357)
 local timerBloodBoilCD				= mod:NewCDTimer(7.3, 184355, nil, false, nil, 3)
@@ -98,6 +98,7 @@ mod.vb.bloodboilDead = false
 mod.vb.reapActive = false
 --mod.vb.firstReap = false
 mod.vb.visageCount = 0
+local felRageTimers = {28, 80, 60, 85}--Post august 11th hotfix timers.
 local UnitExists, UnitGUID, UnitDetailedThreatSituation = UnitExists, UnitGUID, UnitDetailedThreatSituation
 local markofNecroDebuff = GetSpellInfo(184449)--Spell name should work, without knowing what right spellid is, For this anyways.
 
@@ -107,6 +108,19 @@ local markofNecroDebuff = GetSpellInfo(184449)--Spell name should work, without 
 		timerReapCD:Start(10)--Start 10 second timer for 65 second reap
 	end
 end--]]
+
+--[[
+--Fel rage variation table
+30, 80, 63
+31, 81, 60, 85
+31, 81, 61
+31, 81
+31, 81
+31, 80
+30, 82
+28, 80
+31, 81, 61
+--]]
 
 function mod:OnCombatStart(delay)
 	self.vb.DiaPushed = false
@@ -121,7 +135,7 @@ function mod:OnCombatStart(delay)
 	timerMarkofNecroCD:Start(7-delay)--7-13
 	timerNightmareVisageCD:Start(15-delay)
 --	timerFelstormCD:Start(20.5-delay)--Review
-	timerRelRageCD:Start(30-delay, 1)
+	timerRelRageCD:Start(28-delay, 1)--Almost always 30-31. but in rare cases i've seen as early as 28
 	timerReapCD:Start(50-delay)--50-73 variation on pull. It's USUALLY between 65-67, because it's delayed by visage. But not always: reason https://www.warcraftlogs.com/reports/HN42ftpvVk3BYQjJ#fight=5&type=summary&view=events&pins=2%24Off%24%23244F4B%24expression%24(target.id+%3D+92142+or+target.id+%3D+92144+or+target.id+%3D+92146)+and+type+%3D+%22death%22+or+(ability.id+%3D+184657+or+ability.id+%3D+184476+or+ability.id+%3D+184355)+and+type+%3D+%22begincast%22+or+(ability.id+%3D+184449+or+ability.id+%3D+183480+or+ability.id+%3D+184357)+and+type+%3D+%22cast%22+or+(ability.id+%3D+183701+or+ability.id+%3D+184360+or+ability.id+%3D+184365)+and+type+%3D+%22applydebuff%22+or+ability.id+%3D+184674
 	timerDarknessCD:Start(75-delay)
 	berserkTimer:Start(-delay)
@@ -218,7 +232,12 @@ function mod:SPELL_AURA_APPLIED(args)
 		end
 	elseif spellId == 184360 then
 		self.vb.felRageCount = self.vb.felRageCount + 1
-		timerRelRageCD:Start(nil, self.vb.felRageCount+1)
+		local cooldown = felRageTimers[self.vb.felRageCount+1]
+		if cooldown then
+			timerRelRageCD:Start(cooldown, self.vb.felRageCount+1)
+		else--Just start 60 second timer if we don't know timer
+			timerRelRageCD:Start(nil, self.vb.felRageCount+1)
+		end
 		if args:IsPlayer() then
 			specWarnFelRage:Show()
 		else
