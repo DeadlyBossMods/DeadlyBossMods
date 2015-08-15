a5873b3d400f4dd6ce39b71eaef1ec3cf7451f91
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-08-15 23:39:10 +0000

    Try to fix an impossible error reported by someone.

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -7360,7 +7360,7 @@ do
 	}]]
 
 	local specRoleTable = {
-		[62] = {	--Aracne Mage
+		[62] = {	--Arcane Mage
 			["Dps"] = true,
 			["Ranged"] = true,
 			["RangedDps"] = true,
@@ -7585,6 +7585,9 @@ do
 	--disable flag check normally because double flag check comsumes more cpu on mod load.
 	function bossModPrototype:GetRoleFlagValue(flag)
 		if not flag then return false end
+		if not currentSpecID then
+			DBM:SetCurrentSpecInfo()
+		end
 		local flags = {strsplit("|", flag)}
 		for i = 1, #flags do
 			local flagText = flags[i]
