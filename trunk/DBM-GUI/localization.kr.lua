70680217120bd3e592ba744217c219667e7275ff
nbluewiz <nbluewiz@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-08-17 00:54:31 +0000

    # koKR update. # GUI text fix.

--- trunk/DBM-GUI/localization.kr.lua
+++ trunk/DBM-GUI/localization.kr.lua
@@ -303,6 +303,7 @@ L.LFDEnhance				= "전장/공격대 찾기 입장, 역할 확인 소리를 주 �
 L.WorldBossNearAlert		= "근처에서 야외 우두머리 전투가 시작된 경우 전투 준비 소리 듣기(우두머리별 설정 무시)"
 L.RLReadyCheckSound			= "전투 준비 소리를 주 음량 또는 대화 음량에 맞추어 듣기"
 L.AFKHealthWarning			= "자리 비움 상태 도중 체력이 줄면 경고음 듣기"
+L.AutoReplySound			= "자동 응답 귓속말을 보낼 때 경고음 듣기"
 --
 L.TimerGeneral 				= "DBM 바 설정"
 L.SKT_Enabled				= "최고 기록 바 보기"
