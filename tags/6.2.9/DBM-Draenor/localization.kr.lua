84d9b6dbc633547c3d7aacf5cdc47290973bb3cf
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-08-25 07:06:12 +0000

    tag as 6.2.9

---
+++ tags/6.2.9/DBM-Draenor/localization.kr.lua
@@ -0,0 +1,26 @@
+﻿if GetLocale() ~= "koKR" then return end
+local L
+
+-----------------------
+-- Drov the Ruiner --
+-----------------------
+L= DBM:GetModLocalization(1291)
+
+-----------------------
+-- Tarlna the Ageless --
+-----------------------
+L= DBM:GetModLocalization(1211)
+
+--------------
+-- Rukhmar --
+--------------
+L= DBM:GetModLocalization(1262)
+
+-------------------------
+-- Supreme Lord Kazzak --
+-------------------------
+L= DBM:GetModLocalization(1452)
+
+L:SetMiscLocalization({
+	Pull				= "불타는 군단의 압도적인 힘을 느껴라!"
+})
