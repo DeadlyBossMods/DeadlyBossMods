1bd8760b0b06a9d2958135c7d436dad2bb30d4f2
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-08-27 20:01:03 +0000

    tag as 6.2.10

---
+++ tags/6.2.10/DBM-Brawlers/Rank4.lua
@@ -0,0 +1,50 @@
+local mod	= DBM:NewMod("BrawlRank4", "DBM-Brawlers")
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+mod:SetModelID(28115)
+mod:SetZone()
+mod:SetUsedIcons(8)
+
+mod:RegisterEvents(
+	"SPELL_CAST_SUCCESS 141013",
+	"SPELL_AURA_APPLIED 133129",
+	"PLAYER_TARGET_CHANGED"
+)
+
+local warnSpitAcid				= mod:NewSpellAnnounce(141013, 4)--Nibbleh
+
+local specWarnSpitAcid			= mod:NewSpecialWarningSpell(141013)--Nibbleh
+
+local timerSpitAcidCD			= mod:NewNextTimer(20, 141013)--Nibbleh
+
+mod:RemoveOption("HealthFrame")
+mod:AddBoolOption("SetIconOnDominika", true)--Dominika the Illusionist 
+
+local brawlersMod = DBM:GetModByName("Brawlers")
+local DominikaGUID = 0
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if not brawlersMod.Options.SpectatorMode and not brawlersMod:PlayerFighting() then return end
+	if args.spellId == 141013 then
+		timerSpitAcidCD:Start()
+		if brawlersMod:PlayerFighting() then
+			specWarnSpitAcid:Show()
+		else
+			warnSpitAcid:Show()
+		end
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if not brawlersMod.Options.SpectatorMode and not brawlersMod:PlayerFighting() then return end--Spectator mode is disabled, do nothing.
+	if args.spellId == 133129 then
+		DominikaGUID = args.destGUID
+	end
+end
+
+function mod:PLAYER_TARGET_CHANGED()
+	if self.Options.SetIconOnDominika and not DBM.Options.DontSetIcons and UnitGUID("target") == DominikaGUID and GetRaidTargetIndex("target") ~= 8 then
+		SetRaidTarget("target", 8)
+	end
+end
