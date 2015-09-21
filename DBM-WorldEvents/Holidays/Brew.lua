638b07917dc27cc4ae1356d36ef0bca02641def1
Adam <MysticalOS@users.noreply.github.com>
2015-09-21 20:08:14 +0000

    Head from ass

--- DBM-WorldEvents/Holidays/Brew.lua
+++ DBM-WorldEvents/Holidays/Brew.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod("Omen", "DBM-WorldEvents", 1)
+local mod	= DBM:NewMod("Brew", "DBM-WorldEvents", 1)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
