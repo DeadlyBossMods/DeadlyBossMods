658fd12e07ff00ed023e4680e042ec777c765a80
Adam <MysticalOS@users.noreply.github.com>
2015-10-02 07:10:50 +0000

    Adjust iskar berserk I guess. Logs I had showed 8:30 for mythic, but some claim 8

--- DBM-HellfireCitadel/Iskar.lua
+++ DBM-HellfireCitadel/Iskar.lua
@@ -202,10 +202,8 @@ function mod:OnCombatStart(delay)
 			timerShadowRiposteCD:Start(9.5-delay)
 		end
 	end
-	if self:IsNormal() then--Harder berserk on normal vs all other difficulties. Kromog all over again.
+	if self:IsNormal() or self:IsMythic() then
 		berserkTimer:Start(480-delay)
-	elseif self:IsMythic() then
-		berserkTimer:Start(510-delay)
 	else
 		berserkTimer:Start(-delay)
 	end
