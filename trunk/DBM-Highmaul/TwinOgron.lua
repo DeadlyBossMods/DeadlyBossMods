bedadd041e41348a49bc10849cbd6114f35cdbca
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-10-05 00:38:22 +0000

    Fixed one voice pack filter on Koragh Added a Note to Phemos. I think the whirlwind sound should be renamed in 7.0 to “whirlwind”. It’ll have more uses. This little note will just be a reminder to edit file name in this mod when that happens.

--- trunk/DBM-Highmaul/TwinOgron.lua
+++ trunk/DBM-Highmaul/TwinOgron.lua
@@ -264,7 +264,7 @@ function mod:SPELL_CAST_START(args)
 		specWarnQuake:Show(self.vb.QuakeCount)
 		timerWhirlwindCD:Start(PhemosEnergyRate, self.vb.WWCount+1)
 		countdownPhemos:Start(PhemosEnergyRate)
-		voicePhemos:Schedule(PhemosEnergyRate - 6.5, "157943") --ww
+		voicePhemos:Schedule(PhemosEnergyRate - 6.5, "157943")--Probably get this sound file renamed to "whirlwind" in 7.0
 	elseif spellId == 157952 then--Pulverize first cast that needs range finder
 		self.vb.PulverizeCount = self.vb.PulverizeCount + 1
 		warnPulverize:Show(self.vb.PulverizeCount)
