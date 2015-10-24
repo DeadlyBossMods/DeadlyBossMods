ce3e972bfffe950025157be952f5dc9300b66b47
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2015-10-24 20:41:56 +0000

    tweaks to headless horseman

--- trunk/DBM-WorldEvents/Holidays/HeadlessHorseman.lua
+++ trunk/DBM-WorldEvents/Holidays/HeadlessHorseman.lua
@@ -21,14 +21,14 @@ mod:RegisterEventsInCombat(
 )
 
 local warnConflag				= mod:NewTargetAnnounce(42380, 3)
-local warnSquashSoul			= mod:NewTargetAnnounce(42514, 2)
+local warnSquashSoul			= mod:NewTargetAnnounce(42514, 2, nil, false, 2)
 local warnPhase					= mod:NewAnnounce("WarnPhase", 2, "Interface\\Icons\\Spell_Nature_WispSplode")
 local warnHorsemanSoldiers		= mod:NewAnnounce("warnHorsemanSoldiers", 2, 97133)
 local warnHorsemanHead			= mod:NewAnnounce("warnHorsemanHead", 3)
 
 --local timerCombatStart			= mod:NewCombatTimer(17)--rollplay for first pull
 local timerConflag				= mod:NewTargetTimer(4, 42380, nil, "Healer")
-local timerSquashSoul			= mod:NewTargetTimer(15, 42514)
+local timerSquashSoul			= mod:NewTargetTimer(15, 42514, nil, false, 2)
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
@@ -72,7 +72,7 @@ function mod:OnSync(event, arg)
 end
 
 function mod:CHAT_MSG_MONSTER_SAY(msg)
-	if msg == L.HorsemanSoldiers then	-- Warning for adds spawning. No CLEU or UNIT event for it.
+	if msg == L.HorsemanSoldiers and self:AntiSpam(5, 1) then	-- Warning for adds spawning. No CLEU or UNIT event for it.
 		warnHorsemanSoldiers:Show()
 	end
 end
