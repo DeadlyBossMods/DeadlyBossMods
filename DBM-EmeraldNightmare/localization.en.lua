5a472c2ae62d9f4a74f707f8f89050a564c380aa
Adam <MysticalOS@users.noreply.github.com>
2016-01-15 22:26:02 +0000

    Some stubs for emerald nightmare

---
+++ DBM-EmeraldNightmare/localization.en.lua
@@ -0,0 +1,42 @@
+local L
+
+---------------
+-- Nythendra --
+---------------
+L= DBM:GetModLocalization(1703)
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+---------------------------
+-- Ursoc --
+---------------------------
+L= DBM:GetModLocalization(1667)
+
+---------------------------
+-- Dragons of Nightmare --
+---------------------------
+L= DBM:GetModLocalization(1704)
+
+------------------
+-- Xavius --
+------------------
+L= DBM:GetModLocalization(1726)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("HighmaulTrash")
+
+L:SetGeneralLocalization({
+	name =	"Emerald Nightmare Trash"
+})
