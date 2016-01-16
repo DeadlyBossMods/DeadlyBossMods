72a354177ed3d6bf66e4910830bee8c806569d6c
Adam <MysticalOS@users.noreply.github.com>
2016-01-16 06:11:45 +0000

    that’s enough for now, I want to at least push the mostly done mod.

--- DBM-EmeraldNightmare/Ursok.lua
+++ DBM-EmeraldNightmare/Ursok.lua
@@ -11,83 +11,95 @@ mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
+
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS",
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
---	"SPELL_DAMAGE",
---	"SPELL_MISSED",
-	"CHAT_MSG_MONSTER_YELL"
+	"SPELL_CAST_START 197942 197969",
+	"SPELL_CAST_SUCCESS 197943",
+	"SPELL_AURA_APPLIED 198006 197943",
+	"SPELL_AURA_REMOVED 198006",
+	"SPELL_PERIODIC_DAMAGE 205611",
+	"SPELL_PERIODIC_MISSED 205611"
 )
 
-local warnExpelMagicFrost			= mod:NewTargetAnnounce(161411, 3)
+--TODO, info frame for unbalanced maybe, depends on how fast raid gets it (unbalanced can't soak charges)
+--TODO, countdown yell maybe for focused gaze. Arrow or goto warnings system too, maybe. Hud as well
+--TODO, find a good voice for roaring. Maybe watch step? move away?
+--TODO, figure out a good number to repeat for charge count
+local warnFocusedGaze				= mod:NewTargetAnnounce(198006, 3)
 
-local yellTrample					= mod:NewYell(163101)
-local specWarnTrampleNear			= mod:NewSpecialWarningClose(163101)
-local specWarnExpelMagicShadow		= mod:NewSpecialWarningSpell(162184, "Healer", nil, nil, nil, 2)
+local specWarnFocusedGaze			= mod:NewSpecialWarningYou(198006, nil, nil, nil, 1, 2)
+local yellFocusedGaze				= mod:NewYell(198006)
+local specWarnRoaringCacophony		= mod:NewSpecialWarningCount(197969, nil, nil, nil, 2)--Don't know what voice to give it yet, i'll figure it out.
+local specWarnMiasma				= mod:NewSpecialWarningMove(205611, nil, nil, nil, 1, 2)
+local specWarnOverwhelm				= mod:NewSpecialWarningTaunt(197943, "Tank", nil, nil, 1, 2)
 
-local timerTrampleCD				= mod:NewCDTimer(16, 163101, nil, nil, nil, 3)
+local timerFocusedGazeCD			= mod:NewNextTimer(40, 198006, nil, nil, nil, 3)
+local timerRendFleshCD				= mod:NewNextTimer(20, 197942, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerOverwhelmCD				= mod:NewNextTimer(10, 197943, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerRoaringCacophonyCD		= mod:NewNextCountTimer(30, 197969, nil, nil, nil, 2)
 
-local countdownMagicFire			= mod:NewCountdownFades(11.5, 162185)
+local berserkTimer					= mod:NewBerserkTimer(300)
 
-local voiceExpelMagicFire			= mod:NewVoice(162185)
+--local countdownMagicFire			= mod:NewCountdownFades(11.5, 162185)
 
-mod:AddRangeFrameOption("5")
-mod:AddSetIconOption("SetIconOnMC", 163472, false)
-mod:AddHudMapOption("HudMapOnMC", 163472)
+--local voiceFocusedGaze			= mod:NewVoice(198006)--chargemove? For this charge you actually do NOT want to move. you run and help soak it so unsure of voice selection yet
+local voiceOverwhelm				= mod:NewVoice(197943)--Tauntboss/changemt
+local voiceMiasma					= mod:NewVoice(205611)--runaway
+--local voiceRoaringCacophony			= mod:NewVoice(197969)--watchstep
 
-function mod:ArcaneTarget()
-	local targetName, uId = self:GetBossTarget(79015)
-	local tanking, status = UnitDetailedThreatSituation("player", "boss1")
-	if tanking or (status == 3) then--Player is current target
-		specWarnExpelMagicArcaneYou:Show()--So show tank warning
-		voiceExpelMagicArcane:Play("runout")
-	else
-		if self:AntiSpam(3.5, targetName) then--Set anti spam with target name
-			specWarnExpelMagicArcane:Show(targetName)--Sometimes targetname is nil, and then it warns for unknown, but with the new status == 3 check, it'll still warn correct tank, so useful anyways
-			voiceExpelMagicArcane:Play("changemt")
-		end
-	end
-end
+mod.vb.roarCount = 0
 
 function mod:OnCombatStart(delay)
-
+	self.vb.roarCount = 0
+	timerOverwhelmCD:Start(-delay)
+	timerRendFleshCD:Start(13-delay)
+	timerFocusedGazeCD:Start(19-delay)
+	berserkTimer:Start(-delay)
 end
 
 function mod:OnCombatEnd()
-	if self.Options.RangeFrame then
-		DBM.RangeCheck:Hide()
-	end
-	if self.Options.FelArrow then
-		DBM.Arrow:Hide()
-	end
-	if self.Options.HudMapOnMC then
-		DBMHudMap:Disable()
-	end
+
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 162185 then
-
+	if spellId == 197942 then
+		timerRendFleshCD:Start()
+	elseif spellId == 197969 then
+		self.vb.roarCount = self.vb.roarCount + 1
+		specWarnRoaringCacophony:Show(self.vb.roarCount)
+		--voiceRoaringCacophony:Play("watchstep")
+		if self.vb.roarCount % 2 == 0 then
+			timerRoaringCacophonyCD:Start(30, self.vb.roarCount + 1)
+		else
+			timerRoaringCacophonyCD:Start(10, self.vb.roarCount + 1)
+		end
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 161612 and self:AntiSpam(5, 4) then--This won't show balls that hit, only ones caught. Balls that hit require high cpu spell_damage event
-		self:SendSync("Ball")
+	if spellId == 197943 then
+		timerOverwhelmCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 156803 then
-		
-	elseif spellId == 172917 and args:IsPlayer() then
-
+	if spellId == 198006 then
+		warnFocusedGaze:Show(args.destName)
+		timerFocusedGazeCD:Start()
+		if args:IsPlayer() then
+			specWarnFocusedGaze:Show()
+			yellFocusedGaze:Yell()
+		end
+	elseif spellId == 197943 then
+		if not args:IsPlayer() then
+			specWarnOverwhelm:Show(args.destName)
+			voiceOverwhelm:Play("tauntboss")
+		else
+			voiceOverwhelm:Play("changemt")
+		end
 	end
 end
 
@@ -95,28 +107,13 @@ function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
 	if spellId == 162186 then
 
-	elseif spellId == 163472 then
-
 	end
 end
 
-function mod:SPELL_DAMAGE()
-	if self:AntiSpam(5, 4) then
-		self:SendSync("Ball")
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
+	if spellId == 205611 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
+		specWarnMiasma:Show()
+		voiceMiasma:Play("runaway")
 	end
 end
-mod.SPELL_MISSED = mod.SPELL_DAMAGE
-
-function mod:CHAT_MSG_MONSTER_YELL(msg, _, _, _, target)
-	if msg:find(L.supressionTarget1) then
-		self:SendSync("ChargeTo", target)
-	end
-end
-
-function mod:OnSync(msg, targetname)
-	if not self:IsInCombat() then return end
-	if msg == "ChargeTo" then
-		
-	end
-end
---]]
+mod.SPELL_ABSORBED = mod.SPELL_PERIODIC_DAMAGE
