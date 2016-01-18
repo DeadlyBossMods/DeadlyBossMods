b71c75a8d7e5ef667d574ec5cd8bef5f2642aa01
Adam <MysticalOS@users.noreply.github.com>
2016-01-18 18:48:11 +0000

    Improve finddungeonIDs function since GetDungeonInfo can return nil when GetRealZoneText still returns valid dungeon names Add in stubs for the encounters in journal so far for Suramar Raid. There are a lot of encounters not in journal yet so can’t make stubs yet.

---
+++ DBM-Suramar/localization.en.lua
@@ -0,0 +1,47 @@
+local L
+
+---------------
+-- Crystal Scorpion --
+---------------
+L= DBM:GetModLocalization(1706)
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+---------------------------
+-- Chronomatic Anomaly --
+---------------------------
+L= DBM:GetModLocalization(1725)
+
+---------------------------
+-- Trilliax --
+---------------------------
+L= DBM:GetModLocalization(1731)
+
+------------------
+-- Krosus --
+------------------
+L= DBM:GetModLocalization(1713)
+
+------------------
+-- Astromancer --
+------------------
+L= DBM:GetModLocalization(1732)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("SuramarTrash")
+
+L:SetGeneralLocalization({
+	name =	"Suramar Trash"
+})
