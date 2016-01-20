6845e39c8fe97c32b33ad2b356d6618a1d74d835
Adam <MysticalOS@users.noreply.github.com>
2016-01-20 20:52:35 +0000

    rest of the dungeon stubs

--- DBM-Party-Legion/localization.en.lua
+++ DBM-Party-Legion/localization.en.lua
@@ -102,3 +102,160 @@ L= DBM:GetModLocalization(1488)
 -----------------------
 L= DBM:GetModLocalization(1489)
 
+-----------------------
+-- <<<Neltharion's Lair>>> --
+-----------------------
+-----------------------
+-- Rokmora --
+-----------------------
+L= DBM:GetModLocalization(1662)
+
+-----------------------
+-- Ularogg Cragshaper --
+-----------------------
+L= DBM:GetModLocalization(1665)
+
+-----------------------
+-- Naraxas --
+-----------------------
+L= DBM:GetModLocalization(1673)
+
+-----------------------
+-- Dargrul the Underking --
+-----------------------
+L= DBM:GetModLocalization(1687)
+
+-----------------------
+-- <<<Suramar Catacombs>>> --
+-----------------------
+-----------------------
+-- Ivanyr --
+-----------------------
+L= DBM:GetModLocalization(1497)
+
+-----------------------
+-- Nightwell Sentry --
+-----------------------
+L= DBM:GetModLocalization(1498)
+
+-----------------------
+-- General Xakal --
+-----------------------
+L= DBM:GetModLocalization(1499)
+
+-----------------------
+-- Nal'tira --
+-----------------------
+L= DBM:GetModLocalization(1500)
+
+-----------------------
+-- Advisor Vandros --
+-----------------------
+L= DBM:GetModLocalization(1501)
+
+-----------------------
+-- <<<Suramar Noble District>>> --
+-----------------------
+-----------------------
+-- Patrol Captain Volendis --
+-----------------------
+L= DBM:GetModLocalization(1718)
+
+-----------------------
+-- Talixae --
+-----------------------
+L= DBM:GetModLocalization(1719)
+
+-----------------------
+-- High Councillor Gerdo --
+-----------------------
+L= DBM:GetModLocalization(1720)
+
+-----------------------
+-- <<<The Maw of Souls>>> --
+-----------------------
+-----------------------
+-- Ymiron, the Fallen King --
+-----------------------
+L= DBM:GetModLocalization(1502)
+
+-----------------------
+-- Harbaron --
+-----------------------
+L= DBM:GetModLocalization(1512)
+
+-----------------------
+-- Helya --
+-----------------------
+L= DBM:GetModLocalization(1663)
+
+-----------------------
+-- <<<The Violet Hold>>> --
+-----------------------
+-----------------------
+-- Mindflayer Kaahrj --
+-----------------------
+L= DBM:GetModLocalization(1686)
+
+-----------------------
+-- Millificent Manastorm --
+-----------------------
+L= DBM:GetModLocalization(1688)
+
+-----------------------
+-- Festerface --
+-----------------------
+L= DBM:GetModLocalization(1693)
+
+-----------------------
+-- Shivermaw --
+-----------------------
+L= DBM:GetModLocalization(1694)
+
+-----------------------
+-- Blood-Princess Thal'ena --
+-----------------------
+L= DBM:GetModLocalization(1702)
+
+-----------------------
+-- Anub'esset --
+-----------------------
+L= DBM:GetModLocalization(1696)
+
+-----------------------
+-- Sael'orn --
+-----------------------
+L= DBM:GetModLocalization(1697)
+
+-----------------------
+-- Fel Lord Betrug --
+-----------------------
+L= DBM:GetModLocalization(1711)
+
+-----------------------
+-- <<<Vault of the Wardens>>> --
+-----------------------
+-----------------------
+-- Tirathon Saltheril --
+-----------------------
+L= DBM:GetModLocalization(1467)
+
+-----------------------
+-- Inquisitor Tormentorum --
+-----------------------
+L= DBM:GetModLocalization(1695)
+
+-----------------------
+-- Ash'golm --
+-----------------------
+L= DBM:GetModLocalization(1468)
+
+-----------------------
+-- Glazer --
+-----------------------
+L= DBM:GetModLocalization(1469)
+
+-----------------------
+-- Cordana --
+-----------------------
+L= DBM:GetModLocalization(1470)
