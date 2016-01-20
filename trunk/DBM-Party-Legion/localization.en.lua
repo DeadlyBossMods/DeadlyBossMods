b7c864c38b966642ba4936ec4e9344f5449943b4
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-01-20 19:26:47 +0000

    Commit progress so far with dungeon stubs. this fully supports combat detection and auto loading for 4 of the 10 5 man dungeons (all 3 of current open ones are covered in first 4). Actual mods don’t have warnings yet. I will add that after Finishing other 6 stubs.

---
+++ trunk/DBM-Party-Legion/localization.en.lua
@@ -0,0 +1,104 @@
+local L
+
+-----------------------
+-- <<<Black Rook Hold>>> --
+-----------------------
+-----------------------
+-- The Amalgam of Souls --
+-----------------------
+L= DBM:GetModLocalization(1518)
+
+-----------------------
+-- Illysanna Ravencrest --
+-----------------------
+L= DBM:GetModLocalization(1653)
+
+-----------------------
+-- Smashspite the Hateful --
+-----------------------
+L= DBM:GetModLocalization(1664)
+
+-----------------------
+-- Lord Kur'talos Ravencrest --
+-----------------------
+L= DBM:GetModLocalization(1672)
+
+-----------------------
+-- <<<Darkheart Thicket>>> --
+-----------------------
+-----------------------
+-- Arch-Druid Glaidalis --
+-----------------------
+L= DBM:GetModLocalization(1654)
+
+-----------------------
+-- Oakheart --
+-----------------------
+L= DBM:GetModLocalization(1655)
+
+-----------------------
+-- Dresaron --
+-----------------------
+L= DBM:GetModLocalization(1656)
+
+-----------------------
+-- Shade of Xavius --
+-----------------------
+L= DBM:GetModLocalization(1657)
+
+-----------------------
+-- <<<Eye of Azshara>>> --
+-----------------------
+-----------------------
+-- Warlord Parjesh --
+-----------------------
+L= DBM:GetModLocalization(1480)
+
+-----------------------
+-- Lady Hatecoil --
+-----------------------
+L= DBM:GetModLocalization(1490)
+
+-----------------------
+-- King Deepbeard --
+-----------------------
+L= DBM:GetModLocalization(1491)
+
+-----------------------
+-- Serpentrix --
+-----------------------
+L= DBM:GetModLocalization(1479)
+
+-----------------------
+-- Wrath of Azshara --
+-----------------------
+L= DBM:GetModLocalization(1492)
+
+-----------------------
+-- <<<Halls of Valor>>> --
+-----------------------
+-----------------------
+-- Hymdall --
+-----------------------
+L= DBM:GetModLocalization(1485)
+
+-----------------------
+-- Hyrja --
+-----------------------
+L= DBM:GetModLocalization(1486)
+
+-----------------------
+-- Fenryr --
+-----------------------
+L= DBM:GetModLocalization(1487)
+
+-----------------------
+-- God-King Skovald --
+-----------------------
+L= DBM:GetModLocalization(1488)
+
+-----------------------
+-- Odyn --
+-----------------------
+L= DBM:GetModLocalization(1489)
+
