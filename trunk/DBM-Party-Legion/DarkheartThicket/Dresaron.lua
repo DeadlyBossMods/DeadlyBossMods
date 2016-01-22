d1a23f3c143d06cc83eee65c7744cc34f6f15d44
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-01-22 22:25:18 +0000

    Push Darkheart Thicket mods

--- trunk/DBM-Party-Legion/DarkheartThicket/Dresaron.lua
+++ trunk/DBM-Party-Legion/DarkheartThicket/Dresaron.lua
@@ -8,74 +8,70 @@ mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED",
-	"SPELL_SUMMON",
+	"SPELL_CAST_START 199389 199345",
+	"SPELL_PERIODIC_DAMAGE 199460",
+	"SPELL_PERIODIC_MISSED 199460",
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---local warnCurtainOfFlame			= mod:NewTargetAnnounce(153396, 4)
+--TODO: Breath scanning tweaks. it CAN target tank so we need a more complex system to grab any target he looks at that's not tank during cast, but fall back to tank as target if no other target found.
+local warnBreath					= mod:NewTargetAnnounce(199332, 3)
+local warnRoar						= mod:NewSpellAnnounce(199389, 2)
 
---local specWarnCurtainOfFlame		= mod:NewSpecialWarningMoveAway(153396)
+local specWarnDownDraft				= mod:NewSpecialWarningSpell(199345, nil, nil, nil, 2, 2)
+local yellBreath					= mod:NewYell(199332)
+local specWarnFallingRocks			= mod:NewSpecialWarningMove(199460, nil, nil, nil, 2, 2)
 
---local timerCurtainOfFlameCD			= mod:NewNextTimer(20, 153396, nil, nil, nil, 3)
+local timerBreathCD					= mod:NewCDTimer(30, 199332, nil, nil, nil, 3)
+local timerEarthShakerCD			= mod:NewCDTimer(29, 199389, nil, nil, nil, 3)
+local timerDownDraftCD				= mod:NewCDTimer(38, 199345, nil, nil, nil, 2)--38-42 (health based or varaible?)
 
---local voiceCurtainOfFlame			= mod:NewVoice(153392)
+local voiceDownDraft				= mod:NewVoice(199345)--keepmove
+local voiceBreath					= mod:NewVoice(199332)--breathsoon
+local voiceFallingRocks				= mod:NewVoice(199460)--runaway
 
 --mod:AddRangeFrameOption(5, 153396)
 
+function mod:BreathTarget(targetname, uId)
+	if not targetname then
+		DBM:Debug("Probably on the tank")
+		return
+	end
+	warnBreath:Show(targetname)
+	if targetname == UnitName("player") then
+		yellBreath:Yell()
+	end
+end
+
 function mod:OnCombatStart(delay)
-
-end
-
-function mod:OnCombatEnd()
---	if self.Options.RangeFrame then
---		DBM.RangeCheck:Hide()
---	end
-end
-
-function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 153396 then
-
-	end
-end
-
-function mod:SPELL_AURA_APPLIED(args)
-	local spellId = args.spellId
-	if spellId == 153392 then
-
-	end
-end
-
-function mod:SPELL_AURA_REMOVED(args)
-	local spellId = args.spellId
-	if spellId == 153392 then
-
-	end
+	timerBreathCD:Start(8-delay)
+	timerEarthShakerCD:Start(15-delay)
+	timerDownDraftCD:Start(19-delay)
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 153764 then
-
+	if spellId == 199389 then
+		warnRoar:Show()
+	elseif spellId == 199345 then
+		specWarnDownDraft:Show()
+		voiceDownDraft:Play("keepmove")
 	end
 end
 
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 153616 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
-
+	if spellId == 199460 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
+		specWarnFallingRocks:Show()
+		voiceFallingRocks:Play("runaway")
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if spellId == 153500 then
-
+	if spellId == 199332 then--Even with this scanner, it's abougt 50/50 hit or miss you can grab a target at all
+		voiceBreath:Play("breathsoon")
+		self:BossUnitTargetScanner(uId, "BreathTarget", 2)
+		timerBreathCD:Start()
 	end
 end
---]]
