412f56b24d9067eb7d514000046af53fa9cf69df
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-02-04 08:51:21 +0000

    increase alpha version

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -41,8 +41,8 @@
 -------------------------------
 DBM = {
 	Revision = tonumber(("$Revision$"):sub(12, -3)),
-	DisplayVersion = "6.2.18 alpha", -- the string that is shown as version
-	ReleaseRevision = 14720 -- the revision of the latest stable version that is available
+	DisplayVersion = "6.2.19 alpha", -- the string that is shown as version
+	ReleaseRevision = 14770 -- the revision of the latest stable version that is available
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
