11fa8155b536238ed90168b371fa07b6a39bb900
Adam <MysticalOS@users.noreply.github.com>
2016-02-12 18:26:56 +0000

    Push hymdall mod, will do more later.

--- DBM-Party-Legion/HallsOfValor/Hymdall.lua
+++ DBM-Party-Legion/HallsOfValor/Hymdall.lua
@@ -2,80 +2,73 @@ local mod	= DBM:NewMod(1485, "DBM-Party-Legion", 4, 721)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(99200)
+mod:SetCreatureID(94960)
 mod:SetEncounterID(1805)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED",
-	"SPELL_SUMMON",
+	"SPELL_CAST_START 191284 193235",
+	"SPELL_PERIODIC_DAMAGE 193234",
+	"SPELL_PERIODIC_MISSED 193234",
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---local warnCurtainOfFlame			= mod:NewTargetAnnounce(153396, 4)
+local warnDancingBlade				= mod:NewTargetAnnounce(193235, 3)
+local warnSweep						= mod:NewSpellAnnounce(193092, 2, nil, "Tank")
 
---local specWarnCurtainOfFlame		= mod:NewSpecialWarningMoveAway(153396)
+local specWarnHornOfValor			= mod:NewSpecialWarningSoon(188404, nil, nil, nil, 2)
+local specWarnDancingBlade			= mod:NewSpecialWarningMove(193235)
+local yellDancingBlade				= mod:NewYell(193235)
 
---local timerCurtainOfFlameCD			= mod:NewNextTimer(20, 153396, nil, nil, nil, 3)
+local timerDancingBladeCD			= mod:NewCDTimer(10, 193235, nil, nil, nil, 3)--10-15
+local timerHornCD					= mod:NewCDTimer(31, 191284, nil, nil, nil, 3)--31-36, possibly health based and invalid
 
---local voiceCurtainOfFlame			= mod:NewVoice(153392)
+local voiceDancingBlade				= mod:NewVoice(193235)
 
 --mod:AddRangeFrameOption(5, 153396)
 
+function mod:BladeTarget(targetname, uId)
+	if not targetname then
+		warnDancingBlade:Show(DBM_CORE_UNKNOWN)
+		return
+	end
+	if targetname == UnitName("player") then
+		specWarnDancingBlade:Show()
+		voiceDancingBlade:Play("runaway")
+		yellDancingBlade:Yell()
+	else
+		warnDancingBlade:Show(targetname)
+	end
+end
+
 function mod:OnCombatStart(delay)
-
-end
-
-function mod:OnCombatEnd()
---	if self.Options.RangeFrame then
---		DBM.RangeCheck:Hide()
---	end
-end
-
-function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 153396 then
-
-	end
-end
-
-function mod:SPELL_AURA_APPLIED(args)
-	local spellId = args.spellId
-	if spellId == 153392 then
-
-	end
-end
-
-function mod:SPELL_AURA_REMOVED(args)
-	local spellId = args.spellId
-	if spellId == 153392 then
-
-	end
+	timerHornCD:Start(5.5-delay)
+	timerDancingBladeCD:Start(19-delay)
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 153764 then
-
+	if spellId == 191284 then
+		specWarnHornOfValor:Show()
+		timerHornCD:Start()
+	elseif spellId == 193235 then
+		self:BossTargetScanner(94960, "BladeTarget", 0.1, 20, true, nil, nil, nil, true)
+		timerDancingBladeCD:Start()
 	end
 end
 
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 153616 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
-
+	if spellId == 193234 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
+		specWarnDancingBlade:Show()
+		voiceDancingBlade:Play("runaway")
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if spellId == 153500 then
-
+	if spellId == 193092 then
+		warnSweep:Show()
 	end
 end
---]]
