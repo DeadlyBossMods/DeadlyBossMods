61715a2f787da8d55c8f10e8ed8ead8e6c6f5012
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-02-12 03:50:42 +0000

    Add in more stubs

---
+++ trunk/DBM-Suramar/Elisande.lua
@@ -0,0 +1,104 @@
+if DBM:GetTOC() < 70000 then return end
+local mod	= DBM:NewMod(1743, "DBM-Suramar", nil, 786)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+mod:SetCreatureID(103769)----TODO, verify
+mod:SetEncounterID(1872)
+mod:SetZone()
+--mod:SetUsedIcons(8, 7, 6, 3, 2, 1)
+--mod:SetHotfixNoticeRev(12324)
+
+mod:RegisterCombat("combat")
+
+--[[
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_START",
+	"SPELL_CAST_SUCCESS",
+	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_REMOVED",
+--	"SPELL_DAMAGE",
+--	"SPELL_MISSED",
+	"CHAT_MSG_MONSTER_YELL"
+)
+
+--local warnExpelMagicFrost			= mod:NewTargetAnnounce(161411, 3)
+
+--local specWarnTrampleNear			= mod:NewSpecialWarningClose(163101)
+--local yellTrample					= mod:NewYell(163101)
+
+--local timerTrampleCD				= mod:NewCDTimer(16, 163101, nil, nil, nil, 3)
+
+--local countdownMagicFire			= mod:NewCountdownFades(11.5, 162185)
+
+--local voiceExpelMagicFire			= mod:NewVoice(162185)
+
+--mod:AddRangeFrameOption("5")
+--mod:AddSetIconOption("SetIconOnMC", 163472, false)
+--mod:AddHudMapOption("HudMapOnMC", 163472)
+
+function mod:OnCombatStart(delay)
+
+end
+
+function mod:OnCombatEnd()
+--	if self.Options.RangeFrame then
+--		DBM.RangeCheck:Hide()
+--	end
+--	if self.Options.FelArrow then
+--		DBM.Arrow:Hide()
+--	end
+--	if self.Options.HudMapOnMC then
+--		DBMHudMap:Disable()
+--	end
+end
+
+function mod:SPELL_CAST_START(args)
+	local spellId = args.spellId
+	if spellId == 162185 then
+
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+	if spellId == 161612 then
+
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	local spellId = args.spellId
+	if spellId == 156803 then
+
+	end
+end
+
+function mod:SPELL_AURA_REMOVED(args)
+	local spellId = args.spellId
+	if spellId == 162186 then
+
+	end
+end
+
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
+	if spellId == 205611 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
+--		specWarnMiasma:Show()
+--		voiceMiasma:Play("runaway")
+	end
+end
+mod.SPELL_ABSORBED = mod.SPELL_PERIODIC_DAMAGE
+
+function mod:CHAT_MSG_MONSTER_YELL(msg, _, _, _, target)
+	if msg:find(L.supressionTarget1) then
+--		self:SendSync("ChargeTo", target)
+	end
+end
+
+function mod:OnSync(msg, targetname)
+	if not self:IsInCombat() then return end
+	if msg == "ChargeTo" then
+		
+	end
+end
+--]]
