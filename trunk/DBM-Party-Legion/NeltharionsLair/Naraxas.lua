b6ab556a174c594c0012399a21a75bc16fda5862
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-02-14 06:00:27 +0000

    Push Ularogg and Naraxas

--- trunk/DBM-Party-Legion/NeltharionsLair/Naraxas.lua
+++ trunk/DBM-Party-Legion/NeltharionsLair/Naraxas.lua
@@ -2,35 +2,51 @@ local mod	= DBM:NewMod(1673, "DBM-Party-Legion", 5, 767)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(99200)
+mod:SetCreatureID(91005)
 mod:SetEncounterID(1792)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED",
-	"SPELL_SUMMON",
+	"SPELL_AURA_APPLIED 209906",
+--	"SPELL_AURA_REMOVED 209906",
+	"SPELL_CAST_START 199176 210150 205549",
+--	"SPELL_PERIODIC_DAMAGE 188494",
+--	"SPELL_PERIODIC_MISSED 188494",
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---local warnCurtainOfFlame			= mod:NewTargetAnnounce(153396, 4)
+local warnFixate					= mod:NewTargetAnnounce(209906, 2, nil, false)--Could be spammy, optional
+local warnSpikedTongue				= mod:NewTargetAnnounce(199176, 4)
 
---local specWarnCurtainOfFlame		= mod:NewSpecialWarningMoveAway(153396)
+local specWarnAdds					= mod:NewSpecialWarningSwitch(199817, "Dps", nil, nil, 1, 2)
+local specWarnFixate				= mod:NewSpecialWarningYou(209906)
+--local specWarnSpikedTongue			= mod:NewSpecialWarningYou(199176)--Needed?
+--local specWarnRancidMaw				= mod:NewSpecialWarningMove(188494)--Needs confirmation this is pool damage and not constant fight aoe damage
 
---local timerCurtainOfFlameCD			= mod:NewNextTimer(20, 153396, nil, nil, nil, 3)
+local timerSpikedTongueCD			= mod:NewNextTimer(50, 199176, nil, nil, nil, 3, nil, DBM_CORE_DEADLY_ICON)
+local timerAddsCD					= mod:NewNextTimer(66, 199817, nil, nil, nil, 1)
+local timerRancidMawCD				= mod:NewCDTimer(18, 205549, nil, false, nil, 3)--Needed?
+local timerToxicWretchCD			= mod:NewCDTimer(14.5, 210150, nil, false, nil, 3)--Needed?
 
---local voiceCurtainOfFlame			= mod:NewVoice(153392)
+local voiceAdds						= mod:NewVoice(199817, "Dps")--mobsoon
 
 --mod:AddRangeFrameOption(5, 153396)
 
-function mod:OnCombatStart(delay)
+function mod:SpikeTarget(targetname, uId)
+	if not targetname then return end
+	warnSpikedTongue:Show(targetname)
+--	if targetname == UnitName("player") then
+--		specWarnSpikedTongue:Show()
+--	end
+end
 
+function mod:OnCombatStart(delay)
+	timerAddsCD:Start(5.5-delay)
+	timerRancidMawCD:Start(8-delay)
+	timerToxicWretchCD:Start(12.9-delay)
+	timerSpikedTongueCD:Start(-delay)
 end
 
 function mod:OnCombatEnd()
@@ -39,43 +55,40 @@ function mod:OnCombatEnd()
 --	end
 end
 
-function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 153396 then
-
-	end
-end
-
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 153392 then
-
-	end
-end
-
-function mod:SPELL_AURA_REMOVED(args)
-	local spellId = args.spellId
-	if spellId == 153392 then
-
+	if spellId == 209906 then
+		warnFixate:Show(args.destName)
+		if args:IsPlayer() and self:AntiSpam(4, 1) then
+			specWarnFixate:Show()
+		end
 	end
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 153764 then
-
+	if spellId == 199176 then
+		timerSpikedTongueCD:Start()
+		self:BossTargetScanner(91005, "SpikeTarget", 0.2, 12, true, nil, nil, nil, true)
+	elseif spellId == 205549 then
+		timerRancidMawCD:Start()
+	elseif spellId == 210150 then
+		timerToxicWretchCD:Start()
 	end
 end
-
+--[[
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 153616 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
-
+	if spellId == 188494 and destGUID == UnitGUID("player") and self:AntiSpam(3, 2) then
+		specWarnRancidMaw:Show()
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
+--]]
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if spellId == 153500 then
-
+	if spellId == 199817 then--Call Minions
+		specWarnAdds:Show()
+		voiceAdds:Play("mobsoon")
+		timerAddsCD:Start()
 	end
 end
---]]
--- trunk/DBM-Party-Legion/NeltharionsLair/Ularogg.lua
+++ trunk/DBM-Party-Legion/NeltharionsLair/Ularogg.lua
@@ -2,80 +2,70 @@ local mod	= DBM:NewMod(1665, "DBM-Party-Legion", 5, 767)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(99200)
+mod:SetCreatureID(91004)
 mod:SetEncounterID(1791)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED",
-	"SPELL_SUMMON",
+	"SPELL_CAST_START 198496 198443 193375",
+--	"SPELL_DAMAGE 193273",
+--	"SPELL_MISSED 193273",
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---local warnCurtainOfFlame			= mod:NewTargetAnnounce(153396, 4)
+local warnStrikeofMountain			= mod:NewTargetAnnounce(198443, 2)
+local warnBellowofDeeps				= mod:NewSpellAnnounce(193375, 2)--Change to special warning if they become important enough to switch to
+local warnStanceofMountain			= mod:NewSpellAnnounce(198509, 2)
 
---local specWarnCurtainOfFlame		= mod:NewSpecialWarningMoveAway(153396)
+local specWarnSunder				= mod:NewSpecialWarningSpell(198496, "Tank", nil, nil, 1, 2)
+local yellStrikeofMountain			= mod:NewYell(198443)
 
---local timerCurtainOfFlameCD			= mod:NewNextTimer(20, 153396, nil, nil, nil, 3)
+local timerSunderCD					= mod:NewCDTimer(8.5, 198496, nil, "Tank", nil, 5)
+local timerStrikeCD					= mod:NewCDTimer(15.5, 198443, nil, "Tank", nil, 5)
 
---local voiceCurtainOfFlame			= mod:NewVoice(153392)
+local voiceSunder					= mod:NewVoice(198496, "Tank")--defensive
 
---mod:AddRangeFrameOption(5, 153396)
+function mod:StrikeTarget(targetname, uId)
+	if not targetname then return end
+	warnStrikeofMountain:Show(targetname)
+	if targetname == UnitName("player") then
+		yellStrikeofMountain:Yell()
+	end
+end
 
 function mod:OnCombatStart(delay)
-
-end
-
-function mod:OnCombatEnd()
---	if self.Options.RangeFrame then
---		DBM.RangeCheck:Hide()
---	end
-end
-
-function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 153396 then
-
-	end
-end
-
-function mod:SPELL_AURA_APPLIED(args)
-	local spellId = args.spellId
-	if spellId == 153392 then
-
-	end
-end
-
-function mod:SPELL_AURA_REMOVED(args)
-	local spellId = args.spellId
-	if spellId == 153392 then
-
-	end
+	timerSunderCD:Start(7-delay)
+	timerStrikeCD:Start(17-delay)
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 153764 then
-
+	if spellId == 198496 then
+		specWarnSunder:Show()
+		voiceSunder:Play("defensive")
+		timerSunderCD:Start()
+	elseif spellId == 198443 then
+		timerStrikeCD:Start()
+		self:BossTargetScanner(91004, "StrikeTarget", 0.2, 12, true, nil, nil, nil, true)
+	elseif spellId == 193375 then
+		warnBellowofDeeps:Show()
 	end
 end
 
+--[[
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 153616 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
+	if spellId == 193273 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
 
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
-
+--]]
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if spellId == 153500 then
-
+	if spellId == 198509 then--Stance of the Mountain
+		warnStanceofMountain:Show()
+		timerSunderCD:Cancel()
+		timerStrikeCD:Cancel()
 	end
 end
---]]
