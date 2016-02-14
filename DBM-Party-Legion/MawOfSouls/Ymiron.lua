b926e8c4f585257c5e0abc28b0e9967c61b6f2bf
Adam <MysticalOS@users.noreply.github.com>
2016-02-14 03:32:23 +0000

    Maw of Souls mods

--- DBM-Party-Legion/MawOfSouls/Ymiron.lua
+++ DBM-Party-Legion/MawOfSouls/Ymiron.lua
@@ -2,80 +2,54 @@ local mod	= DBM:NewMod(1502, "DBM-Party-Legion", 8, 727)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(99200)
+mod:SetCreatureID(96756)
 mod:SetEncounterID(1822)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED",
-	"SPELL_SUMMON",
-	"UNIT_SPELLCAST_SUCCEEDED boss1"
+	"SPELL_CAST_START 193211 193364 193977 193460"
 )
 
---local warnCurtainOfFlame			= mod:NewTargetAnnounce(153396, 4)
+local warnBane						= mod:NewSpellAnnounce(193460, 3)--Can't think of any reason this needs to be special
 
---local specWarnCurtainOfFlame		= mod:NewSpecialWarningMoveAway(153396)
+local specWarnDarkSlash				= mod:NewSpecialWarningSpell(193211, "Tank")
+local specWarnScreams				= mod:NewSpecialWarningRun(193364, "Melee", nil, nil, 4, 2)
+local specWarnWinds					= mod:NewSpecialWarningSpell(193977, nil, nil, nil, 2, 2)
 
---local timerCurtainOfFlameCD			= mod:NewNextTimer(20, 153396, nil, nil, nil, 3)
+local timerDarkSlashCD				= mod:NewCDTimer(14.6, 193211, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerScreamsCD				= mod:NewCDTimer(23, 193364, nil, "Melee", nil, 2)
+local timerWindsCD					= mod:NewCDTimer(24, 193977, nil, nil, nil, 2)
+local timerBaneCD					= mod:NewCDTimer(49.5, 193460, nil, false, nil, 2)--Again can't hink of any reason why this matters
 
---local voiceCurtainOfFlame			= mod:NewVoice(153392)
-
---mod:AddRangeFrameOption(5, 153396)
+local voiceDarkSlash				= mod:NewVoice(193211, "Tank")--defensive
+local voiceScreams					= mod:NewVoice(193364, "Melee")--runout
+local voiceWinds					= mod:NewVoice(193977)--carefly
 
 function mod:OnCombatStart(delay)
-
-end
-
-function mod:OnCombatEnd()
---	if self.Options.RangeFrame then
---		DBM.RangeCheck:Hide()
---	end
-end
-
-function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 153396 then
-
-	end
-end
-
-function mod:SPELL_AURA_APPLIED(args)
-	local spellId = args.spellId
-	if spellId == 153392 then
-
-	end
-end
-
-function mod:SPELL_AURA_REMOVED(args)
-	local spellId = args.spellId
-	if spellId == 153392 then
-
-	end
+	timerDarkSlashCD:Start(3.5-delay)
+	timerScreamsCD:Start(5.9-delay)
+	timerWindsCD:Start(15.5-delay)
+	timerBaneCD:Start(21.5-delay)
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 153764 then
-
+	if spellId == 193211 then
+		specWarnDarkSlash:Show()
+		voiceDarkSlash:Play("defensive")
+		timerDarkSlashCD:Start()
+	elseif spellId == 193364 then
+		specWarnScreams:Show()
+		voiceScreams:Play("runout")
+		timerScreamsCD:Start()
+	elseif spellId == 193977 then
+		specWarnWinds:Show()
+		voiceWinds:Play("carefly")
+		timerWindsCD:Start()
+	elseif spellId == 193460 then
+		warnBane:Show()
+		timerBaneCD:Start()
 	end
 end
-
-function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 153616 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
-
-	end
-end
-mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
-
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if spellId == 153500 then
-
-	end
-end
---]]
