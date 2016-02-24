d2bee8752322a5933b8f055b5c53f63de81767e6
Adam <MysticalOS@users.noreply.github.com>
2016-02-24 08:24:29 +0000

    Ash golm Tirathon Tormentorum

--- DBM-Party-Legion/GroveOfWarden/TirathonSaltheril.lua
+++ DBM-Party-Legion/GroveOfWarden/TirathonSaltheril.lua
@@ -2,72 +2,69 @@ local mod	= DBM:NewMod(1467, "DBM-Party-Legion", 10, 707)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(99200)
+mod:SetCreatureID(95885)
 mod:SetEncounterID(1815)
+mod:DisableESCombatDetection()--Remove if blizz fixes trash firing ENCOUNTER_START
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED",
-	"SPELL_SUMMON",
+	"SPELL_AURA_APPLIED 191941",
+	"SPELL_AURA_REMOVED 191941",
+	"SPELL_CAST_START 204151 191823 191941 202913",
+--	"SPELL_PERIODIC_DAMAGE",
+--	"SPELL_PERIODIC_MISSED",
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---local warnCurtainOfFlame			= mod:NewTargetAnnounce(153396, 4)
+--TODO, timers possibly. Right now it's up in hair and possibly all health based. No timer matched between multiple pulls
+--TODO, GTFO for standing in fire maybe?
+local specWarnDarkStrikes			= mod:NewSpecialWarningSpell(204151, "Tank", nil, nil, 3, 2)
+local specWarnFuriousBlast			= mod:NewSpecialWarningInterrupt(191823, "-Healer", nil, nil, 1, 2)
+local specWarnFelMortar				= mod:NewSpecialWarningDodge(202913, nil, nil, nil, 2, 2)
 
---local specWarnCurtainOfFlame		= mod:NewSpecialWarningMoveAway(153396)
+local timerDarkStrikes				= mod:NewBuffActiveTimer(11, 191941, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)--tooltip says 15 but every log was 10-11
 
---local timerCurtainOfFlameCD			= mod:NewNextTimer(20, 153396, nil, nil, nil, 3)
-
---local voiceCurtainOfFlame			= mod:NewVoice(153392)
-
---mod:AddRangeFrameOption(5, 153396)
+local voiceDarkStrikes				= mod:NewVoice(204151, "Tank")--defensive
+local voiceFuriousBlast				= mod:NewVoice(191823, "-Healer")--kickcast
+local voiceFelMortar				= mod:NewVoice(202913)--watchstep
 
 function mod:OnCombatStart(delay)
 
 end
 
-function mod:OnCombatEnd()
---	if self.Options.RangeFrame then
---		DBM.RangeCheck:Hide()
---	end
-end
-
-function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 153396 then
-
-	end
-end
-
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 153392 then
-
+	if spellId == 191941 then
+		timerDarkStrikes:Start()
 	end
 end
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 153392 then
-
+	if spellId == 191941 then
+		timerDarkStrikes:Cancel()
 	end
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 153764 then
-
+	if (spellId == 204151 or spellId == 191941) and self:AntiSpam(3, 1) then--Why two Ids?
+		specWarnDarkStrikes:Show()
+		voiceDarkStrikes:Play("defensive")
+	elseif spellId == 191823 then
+		specWarnFuriousBlast:Show(args.sourceName)
+		voiceFuriousBlast:Play("kickcast")
+	elseif spellId == 202913 then
+		specWarnFelMortar:Show()
+		voiceFelMortar:Play("watchstep")
 	end
 end
 
+--[[
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 153616 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
+	if spellId == 153616 and destGUID == UnitGUID("player") and self:AntiSpam(2, 2) then
 
 	end
 end
--- DBM-Party-Legion/GroveOfWarden/Tormentorum.lua
+++ DBM-Party-Legion/GroveOfWarden/Tormentorum.lua
@@ -2,80 +2,44 @@ local mod	= DBM:NewMod(1695, "DBM-Party-Legion", 10, 707)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(99200)
+mod:SetCreatureID(96015)
 mod:SetEncounterID(1850)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED",
-	"SPELL_SUMMON",
+	"SPELL_CAST_START 201488 200898",
+	"SPELL_CAST_SUCCESS 200905",
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---local warnCurtainOfFlame			= mod:NewTargetAnnounce(153396, 4)
+local warnTeleport				= mod:NewSpellAnnounce(200898, 2)
 
---local specWarnCurtainOfFlame		= mod:NewSpecialWarningMoveAway(153396)
+local specWarnSapSoul			= mod:NewSpecialWarningInterrupt(200905, nil, nil, nil, 1, 2)
+local specWarnFear				= mod:NewSpecialWarningSpell(201488, nil, nil, nil, 2)
 
---local timerCurtainOfFlameCD			= mod:NewNextTimer(20, 153396, nil, nil, nil, 3)
+local timerSapSoulCD			= mod:NewNextTimer(21.5, 200905, nil, nil, nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
 
---local voiceCurtainOfFlame			= mod:NewVoice(153392)
-
---mod:AddRangeFrameOption(5, 153396)
+local voiceSapSoul				= mod:NewVoice(200905)--Kickcast
 
 function mod:OnCombatStart(delay)
-
-end
-
-function mod:OnCombatEnd()
---	if self.Options.RangeFrame then
---		DBM.RangeCheck:Hide()
---	end
-end
-
-function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 153396 then
-
-	end
-end
-
-function mod:SPELL_AURA_APPLIED(args)
-	local spellId = args.spellId
-	if spellId == 153392 then
-
-	end
-end
-
-function mod:SPELL_AURA_REMOVED(args)
-	local spellId = args.spellId
-	if spellId == 153392 then
-
-	end
+	timerSapSoulCD:Start(10-delay)
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 153764 then
-
+	if spellId == 201488 then
+		specWarnFear:Show()
+	elseif spellId == 200898 then
+		warnTeleport:Show()
 	end
 end
 
-function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 153616 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
-
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 200905 then
+		specWarnSapSoul:Show(args.sourceName)
+		voiceSapSoul:Play("kickcast")
+		timerSapSoulCD:Start()
 	end
 end
-mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
-
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if spellId == 153500 then
-
-	end
-end
---]]
