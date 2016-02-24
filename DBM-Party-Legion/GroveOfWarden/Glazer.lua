88cb202b7ac05c172d78b4b93a74ed070cb2e240
Adam <MysticalOS@users.noreply.github.com>
2016-02-24 22:42:16 +0000

    Finish Grove of Wardens

--- DBM-Party-Legion/GroveOfWarden/Glazer.lua
+++ DBM-Party-Legion/GroveOfWarden/Glazer.lua
@@ -2,80 +2,46 @@ local mod	= DBM:NewMod(1469, "DBM-Party-Legion", 10, 707)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(99200)
+mod:SetCreatureID(95887)
 mod:SetEncounterID(1817)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED",
-	"SPELL_SUMMON",
-	"UNIT_SPELLCAST_SUCCEEDED boss1"
+	"SPELL_CAST_START 194289 194942",
+	"SPELL_PERIODIC_DAMAGE 194945",
+	"SPELL_PERIODIC_MISSED 194945"
 )
 
---local warnCurtainOfFlame			= mod:NewTargetAnnounce(153396, 4)
+--TODO, maybe voice for mirrors to bounce beam back into boss? But how do you say this short
+local warnGaze						= mod:NewSpellAnnounce(194942, 2)
 
---local specWarnCurtainOfFlame		= mod:NewSpecialWarningMoveAway(153396)
+local specWarnFocused				= mod:NewSpecialWarningSpell(194289, nil, nil, nil, 2)
+local specWarnGazeGTFO				= mod:NewSpecialWarningMove(194289, nil, nil, nil, 1, 2)
 
---local timerCurtainOfFlameCD			= mod:NewNextTimer(20, 153396, nil, nil, nil, 3)
+local timerGazeCD					= mod:NewCDTimer(19.4, 194289, nil, nil, nil, 3)
 
---local voiceCurtainOfFlame			= mod:NewVoice(153392)
-
---mod:AddRangeFrameOption(5, 153396)
+local voiceGazeGTFO					= mod:NewVoice(194289)--runaway
 
 function mod:OnCombatStart(delay)
-
-end
-
-function mod:OnCombatEnd()
---	if self.Options.RangeFrame then
---		DBM.RangeCheck:Hide()
---	end
-end
-
-function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 153396 then
-
-	end
-end
-
-function mod:SPELL_AURA_APPLIED(args)
-	local spellId = args.spellId
-	if spellId == 153392 then
-
-	end
-end
-
-function mod:SPELL_AURA_REMOVED(args)
-	local spellId = args.spellId
-	if spellId == 153392 then
-
-	end
+	timerGazeCD:Start(11.8-delay)
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 153764 then
-
+	if spellId == 194289 then
+		specWarnFocused:Show()
+	elseif spellId == 194942 then
+		warnGaze:Show()
+		timerGazeCD:Start()
 	end
 end
 
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 153616 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
-
+	if spellId == 194945 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
+		specWarnGazeGTFO:Show()
+		voiceGazeGTFO:Play("runaway")
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
-
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if spellId == 153500 then
-
-	end
-end
---]]
