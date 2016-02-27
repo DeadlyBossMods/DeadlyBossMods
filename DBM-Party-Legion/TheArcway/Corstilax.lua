344c02d1cdcae9cd8246c06e9eeb7a8a6e65ac8e
Adam <MysticalOS@users.noreply.github.com>
2016-02-27 00:22:42 +0000

    Push Ivanyr mod, rename sentry to Corstilax

--- DBM-Party-Legion/TheArcway/Corstilax.lua
+++ DBM-Party-Legion/TheArcway/Corstilax.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(1498, "DBM-Party-Legion", 6, 726)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(99200)
+mod:SetCreatureID(98205)
 mod:SetEncounterID(1825)
 mod:SetZone()
 
