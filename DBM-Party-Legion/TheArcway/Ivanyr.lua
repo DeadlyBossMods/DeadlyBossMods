5cad264e6fbd7360f25bebf1cae4f5fca94bda53
Adam <MysticalOS@users.noreply.github.com>
2016-02-27 00:22:42 +0000

    Push Ivanyr mod, rename sentry to Corstilax

--- DBM-Party-Legion/TheArcway/Ivanyr.lua
+++ DBM-Party-Legion/TheArcway/Ivanyr.lua
@@ -2,80 +2,117 @@ local mod	= DBM:NewMod(1497, "DBM-Party-Legion", 6, 726)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(99200)
+mod:SetCreatureID(98203)
 mod:SetEncounterID(1827)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED",
-	"SPELL_SUMMON",
-	"UNIT_SPELLCAST_SUCCEEDED boss1"
+	"SPELL_AURA_APPLIED 196562 196805",
+	"SPELL_AURA_REMOVED 196562",
+	"SPELL_CAST_START 196549 196392",
+	"SPELL_CAST_SUCCESS 196562 196804 196877",
+	"SPELL_PERIODIC_DAMAGE 196824",
+	"SPELL_PERIODIC_MISSED 196824"
 )
 
---local warnCurtainOfFlame			= mod:NewTargetAnnounce(153396, 4)
+--TODO, verify some of this is actually timer based and not just mana depletion related.
+local warnVolatileMagic				= mod:NewTargetAnnounce(196562, 3)
+local warnNetherLink				= mod:NewTargetAnnounce(196805, 4)
+local warnPhase2					= mod:NewPhaseAnnounce(2, 2)
+local warnConsumeEssence			= mod:NewSpellAnnounce(196877, 3)
 
---local specWarnCurtainOfFlame		= mod:NewSpecialWarningMoveAway(153396)
+local specWarnVolatileMagic			= mod:NewSpecialWarningMoveAway(196562, nil, nil, nil, 1, 2)
+local yellVolatileMagic				= mod:NewYell(196562)
+local specWarnNetherLink			= mod:NewSpecialWarningYou(196805, nil, nil, nil, 1, 2)
+local specWarnNetherLinkGTFO		= mod:NewSpecialWarningMove(196805, nil, nil, nil, 1, 2)
+local specWarnOverchargeMana		= mod:NewSpecialWarningInterrupt(196392, nil, nil, nil, 1, 2)
 
---local timerCurtainOfFlameCD			= mod:NewNextTimer(20, 153396, nil, nil, nil, 3)
+local timerVolatileMagicCD			= mod:NewCDTimer(22, 196562, nil, nil, nil, 3)
+local timerNetherLinkCD				= mod:NewCDTimer(21, 196804, nil, nil, nil, 3)
+local timerOverchargeManaCD			= mod:NewCDTimer(22, 196392, nil, nil, nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
+local timerConsumeEssenceCD			= mod:NewNextTimer(18.2, 196877, nil, nil, nil, 2)
 
---local voiceCurtainOfFlame			= mod:NewVoice(153392)
+local voiceVolatileMagic			= mod:NewVoice(196562)--runout
+local voiceNetherLink				= mod:NewVoice(196805)--linegather/runaway
+local voiceOverchargeMana			= mod:NewVoice(196392)--kickcast
+local voicePhaseChange				= mod:NewVoice(nil, nil, DBM_CORE_AUTO_VOICE2_OPTION_TEXT)
 
---mod:AddRangeFrameOption(5, 153396)
+mod:AddRangeFrameOption(15, 196562)
 
 function mod:OnCombatStart(delay)
-
+	timerVolatileMagicCD:Start(11.7-delay)
+	timerNetherLinkCD:Start(23.5-delay)
+	timerOverchargeManaCD:Start(26-delay)
 end
 
 function mod:OnCombatEnd()
---	if self.Options.RangeFrame then
---		DBM.RangeCheck:Hide()
---	end
-end
-
-function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 153396 then
-
+	if self.Options.RangeFrame then
+		DBM.RangeCheck:Hide()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 153392 then
-
+	if spellId == 196562 then
+		warnVolatileMagic:CombinedShow(0.5, args.destName)
+		if args:IsPlayer() then
+			specWarnVolatileMagic:Show()
+			voiceVolatileMagic:Play("runout")
+			yellVolatileMagic:Yell()
+			if self.Options.RangeFrame then
+				DBM.RangeCheck:Show(15)
+			end
+		end
+	elseif spellId == 196805 then
+		warnNetherLink:CombinedShow(0.5, args.destName)
+		if args:IsPlayer() then
+			specWarnNetherLink:Show()
+			voiceNetherLink:Play("linegather")
+		end
 	end
 end
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 153392 then
-
+	if spellId == 196562 and args:IsPlayer() and self.Options.RangeFrame then
+		DBM.RangeCheck:Hide()
 	end
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 153764 then
+	if spellId == 196549 then--Withering Consumption
+		warnPhase2:Show()
+		voicePhaseChange:Play("ptwo")
+		timerVolatileMagicCD:Cancel()
+		timerNetherLinkCD:Cancel()
+		timerOverchargeManaCD:Cancel()
+		timerConsumeEssenceCD:Start(10.5)
+	elseif spellId == 196392 then
+		specWarnOverchargeMana:Show(args.sourceName)
+		voiceOverchargeMana:Play("kickcast")
+		timerOverchargeManaCD:Start()
+	end
+end
 
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+	if spellId == 196562 then
+		timerVolatileMagicCD:Start()
+	elseif spellId == 196804 then
+		timerNetherLinkCD:Start()
+	elseif spellId == 196877 then
+		warnConsumeEssence:Show()
+		timerConsumeEssenceCD:Start()
 	end
 end
 
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 153616 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
-
+	if spellId == 196824 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
+		specWarnNetherLinkGTFO:Show()
+		voiceNetherLink:Play("runaway")
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
-
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if spellId == 153500 then
-
-	end
-end
---]]
