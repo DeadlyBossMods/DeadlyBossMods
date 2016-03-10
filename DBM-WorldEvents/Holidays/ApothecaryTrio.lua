3391420399c8fb6187163658a19e98a0fd881546
Adam <MysticalOS@users.noreply.github.com>
2016-03-10 18:57:25 +0000

    Defaults tweak

--- DBM-WorldEvents/Holidays/ApothecaryTrio.lua
+++ DBM-WorldEvents/Holidays/ApothecaryTrio.lua
@@ -21,7 +21,7 @@ mod:RegisterEventsInCombat(
 
 mod:SetBossHealthInfo(36296, 36565, 36272)
 
-local warnChainReaction			= mod:NewCastAnnounce(68821, 3)
+local warnChainReaction			= mod:NewCastAnnounce(68821, 3, nil, "Melee")
 
 local specWarnPerfumeSpill		= mod:NewSpecialWarningMove(68927)
 local specWarnCologneSpill		= mod:NewSpecialWarningMove(68934)
@@ -30,7 +30,7 @@ local timerHummel				= mod:NewTimer(10.5, "HummelActive", 2457, nil, false)
 local timerBaxter				= mod:NewTimer(18.5, "BaxterActive", 2457, nil, false)
 local timerFrye					= mod:NewTimer(26.5, "FryeActive", 2457, nil, false)
 mod:AddBoolOption("TrioActiveTimer", true, "timer")
-local timerChainReaction		= mod:NewCastTimer(3, 68821)
+local timerChainReaction		= mod:NewCastTimer(3, 68821, nil, "Melee")
 
 function mod:SPELL_CAST_START(args)
 	if args.spellId == 68821 then
