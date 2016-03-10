8dcb1b14ff62f405feb875d71be1729696733797
Adam <MysticalOS@users.noreply.github.com>
2016-03-10 04:54:21 +0000

    Update encounter Ids

--- DBM-Nighthold/Krosus.lua
+++ DBM-Nighthold/Krosus.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(103769)----TODO, verify
---mod:SetEncounterID(1864)--unknown, too many generic names for encounter Ids in this raid
+mod:SetEncounterID(1842)
 mod:SetZone()
 --mod:SetUsedIcons(8, 7, 6, 3, 2, 1)
 --mod:SetHotfixNoticeRev(12324)
