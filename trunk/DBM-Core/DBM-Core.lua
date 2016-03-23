3f0322f1b6178bca18ef9ebd4ad91c3535156d37
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-03-23 18:05:01 +0000

    Fix another missed bnet function

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -3419,6 +3419,7 @@ end
 do
 	--TODO: REMOVE COMPAT CODE IN LEGION/6.2.4
 	local BNGetFriendGameAccountInfo = BNGetFriendToonInfo or BNGetFriendGameAccountInfo
+	local BNGetNumFriendGameAccounts = BNGetNumFriendToons or BNGetNumFriendGameAccounts
 	local function AcceptPartyInvite()
 		AcceptGroup()
 		for i=1, STATICPOPUP_NUMDIALOGS do
@@ -3445,7 +3446,7 @@ do
 			for i = 1, numBNetOnline do
 				local presenceID, _, _, _, _, _, _, isOnline = BNGetFriendInfo(i)
 				local friendIndex = BNGetFriendIndex(presenceID)--Check if they are on more than one client at once (very likely with new launcher)
-				for i=1, BNGetNumFriendToons(friendIndex) do
+				for i=1, BNGetNumFriendGameAccounts(friendIndex) do
 					local _, toonName, client = BNGetFriendGameAccountInfo(friendIndex, i)
 					if toonName and client == BNET_CLIENT_WOW then--Check if toon name exists and if client is wow. If yes to both, we found right client
 						self:Debug("Found a wow tooname: "..toonName, 3)
