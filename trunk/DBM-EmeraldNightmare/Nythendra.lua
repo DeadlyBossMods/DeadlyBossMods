5265c00b89843bbb985313bc404c8cfefb8e791d
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-04-08 03:06:52 +0000

    Mythic updates for Nythendra

--- trunk/DBM-EmeraldNightmare/Nythendra.lua
+++ trunk/DBM-EmeraldNightmare/Nythendra.lua
@@ -11,9 +11,9 @@ mod:SetUsedIcons(4, 3, 2, 1)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 203552 202977",
+	"SPELL_CAST_START 203552 202977 205070",
 	"SPELL_CAST_SUCCESS 204463",
-	"SPELL_AURA_APPLIED 204463 203096",
+	"SPELL_AURA_APPLIED 204463 203096 205043",
 	"SPELL_AURA_REMOVED 204463 203096 203552",
 	"SPELL_PERIODIC_DAMAGE 203045",
 	"SPELL_PERIODIC_MISSED 203045",
@@ -25,6 +25,7 @@ mod:RegisterEventsInCombat(
 local warnVolatileRot				= mod:NewTargetAnnounce(204463, 4)
 local warnRot						= mod:NewTargetAnnounce(203096, 3)
 local warnHeartofSwarm				= mod:NewSpellAnnounce(203552, 2)
+local warnInfestedMind				= mod:NewTargetAnnounce(205043, 4)
 
 local specWarnBreath				= mod:NewSpecialWarningDodge(202977, nil, nil, nil, 2, 2)
 local specWarnVolatileRot			= mod:NewSpecialWarningRun(204463, nil, nil, nil, 3, 2)
@@ -33,6 +34,8 @@ local yellVolatileRot				= mod:NewFadesYell(204463)
 local specWarnRot					= mod:NewSpecialWarningRun(203096, nil, nil, nil, 1, 2)
 local yellRot						= mod:NewFadesYell(203096)
 local specWarnInfestedGround		= mod:NewSpecialWarningMove(203045, nil, nil, nil, 1, 2)
+local specWarnInfestedMind			= mod:NewSpecialWarningSwitch(205043, "Dps", nil, nil, 1, 2)
+local specWarnSpreadInfestation		= mod:NewSpecialWarningInterrupt(205070, "HasInterrupt", nil, nil, 1, 2)
 
 local timerBreathCD					= mod:NewCDCountTimer(40, 202977, nil, nil, nil, 3)
 local timerVolatileRotCD			= mod:NewCDCountTimer(22.8, 204463, nil, "Tank", nil, 5)
@@ -46,9 +49,11 @@ local voiceBreath					= mod:NewVoice(202977)--breathsoon
 local voiceRot						= mod:NewVoice(203096)--runout
 local voiceVolatileRot				= mod:NewVoice(204463)--runout/TauntBoss
 local voiceInfestedGround			= mod:NewVoice(203045)--runaway
+local voiceInfestedMind				= mod:NewVoice(205043, "Dps")--findmc
+local voiceSpreadInfestation		= mod:NewVoice(205070, "HasInterrupt")--kickcast
 
 mod:AddSetIconOption("SetIconOnRot", 203096)--Of course I'll probably be forced to change method when BW does their own thing, for compat.
---mod:AddHudMapOption("HudMapOnMC", 163472)
+mod:AddInfoFrameOption(204506)
 
 mod.vb.breathCount = 0
 mod.vb.rotCast = 0
@@ -65,12 +70,16 @@ function mod:OnCombatStart(delay)
 	timerBreathCD:Start(35-delay, 1)--35-40 variable
 	countdownBreath:Start(35-delay)
 	timerSwarmCD:Start(86-delay, 1)--86-92
+	if self.Options.InfoFrame and self:IsMythic() then
+		DBM.InfoFrame:SetHeader(GetSpellInfo(204506))
+		DBM.InfoFrame:Show(5, "playerdebuffstacks", 204506)
+	end
 end
 
 function mod:OnCombatEnd()
---	if self.Options.HudMapOnMC then
---		DBMHudMap:Disable()
---	end
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:Hide()
+	end
 end
 
 function mod:SPELL_CAST_START(args)
@@ -86,6 +95,9 @@ function mod:SPELL_CAST_START(args)
 			timerBreathCD:Start(nil, self.vb.breathCount+1)
 			countdownBreath:Start()
 		end
+	elseif spellId == 205070 and self:CheckInterruptFilter(args.sourceGUID) then
+		specWarnSpreadInfestation:Show(args.sourceName)
+		voiceSpreadInfestation:Play("kickcast")
 	end
 end
 
@@ -133,6 +145,12 @@ function mod:SPELL_AURA_APPLIED(args)
 		end
 		if self.Options.SetIconOnRot then
 			self:SetAlphaIcon(0.5, args.destName, 1)--Number of icons variable by raid size and duration of fight
+		end
+	elseif spellId == 205043 then
+		warnInfestedMind:CombinedShow(0.5, args.destName)
+		if self:AntiSpam(5, 2) then
+			specWarnInfestedMind:Show()
+			voiceInfestedMind:Play("findmc")
 		end
 	end
 end
