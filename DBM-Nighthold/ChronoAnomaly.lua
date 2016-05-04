4b242aab3a0046811e4b3460b930d0be10bb1281
Adam <MysticalOS@users.noreply.github.com>
2016-05-04 05:23:35 +0000

    Post testing Anomaly updates Removed any references to EJ from DBM

--- DBM-Nighthold/ChronoAnomaly.lua
+++ DBM-Nighthold/ChronoAnomaly.lua
@@ -12,7 +12,7 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 211927 207228",
---	"SPELL_CAST_SUCCESS 206618 206610 206614",
+	"SPELL_CAST_SUCCESS 219815",
 	"SPELL_AURA_APPLIED 206617 206609 207052 207051 206607 211927",
 	"SPELL_AURA_APPLIED_DOSE 206607",
 	"SPELL_AURA_REMOVED 206617 206609 207052 207051 211927",
@@ -28,7 +28,7 @@ local warnTimeBomb					= mod:NewTargetAnnounce(206617, 3)
 local warnTimeRelease				= mod:NewTargetAnnounce(206610, 3, nil, false)--Too many targets
 local warnChronometricPart			= mod:NewStackAnnounce(206607, 3, nil, "Tank")
 
-local specWarnPowerOverwhelming		= mod:NewSpecialWarningDodge(211927, nil, nil, nil, 2, 2)
+local specWarnTemporalOrbs			= mod:NewSpecialWarningDodge(219815, nil, nil, nil, 2, 2)
 local specWarnPowerOverwhelmingKick	= mod:NewSpecialWarningInterrupt(211927, "Tank", nil, nil, 3, 2)
 local specWarnTimeBomb				= mod:NewSpecialWarningMoveAway(206617, nil, nil, nil, 1, 2)--When close to expiring, not right away
 local yellTimeBomb					= mod:NewFadesYell(206617)
@@ -36,9 +36,10 @@ local specWarnWarp					= mod:NewSpecialWarningInterrupt(207228, "HasInterrupt", 
 local specWarnBigAdd				= mod:NewSpecialWarningSwitch(206700, "-Healer", nil, nil, 1, 2)--Switch to waning time particle when section info known
 local specWarnSmallAdd				= mod:NewSpecialWarningSwitch(206699, "Tank", nil, nil, 1, 2)
 
+local timerTemporalOrbsCD			= mod:NewNextTimer(30, 219815, nil, nil, nil, 2)
 local timerPowerOverwhelmingCD		= mod:NewNextTimer(60, 211927, nil, nil, nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
 local timerTimeBomb					= mod:NewBuffFadesTimer(30, 206617, nil, nil, nil, 5)
-local timerTimeBombCD				= mod:NewNextTimer(30, 206617, nil, nil, nil, 3)
+local timerTimeBombCD				= mod:NewNextCountTimer(30, 206617, nil, nil, nil, 3)
 local timerBurstofTimeCD			= mod:NewNextCountTimer(30, 206614, nil, nil, nil, 3)
 local timerTimeReleaseCD			= mod:NewNextCountTimer(30, 206610, nil, "Healer", nil, 5, nil, DBM_CORE_HEALER_ICON)
 local timerChronoPartCD				= mod:NewCDTimer(10.4, 206607, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
@@ -48,7 +49,8 @@ local timerSmallAddCD				= mod:NewNextTimer(30, 206699, nil, nil, nil, 1)--Switc
 local countdownBigAdd				= mod:NewCountdown(30, 206700)--Switch to waning time particle when section info known
 local countdownTimeBomb				= mod:NewCountdownFades("AltTwo30", 206617)
 
-local voicePowerOverwhelming		= mod:NewVoice(211927)--kickcast/watchstep
+local voiceTemporalOrbs				= mod:NewVoice(219815)--watchstep
+local voicePowerOverwhelming		= mod:NewVoice(211927)--kickcast
 local voiceTimeBomb					= mod:NewVoice(206617)--scatter
 local voiceWarp						= mod:NewVoice(207228, "HasInterrupt")--kickcast
 local voiceBigAdd					= mod:NewVoice(206700, "-Healer")
@@ -58,9 +60,15 @@ mod:AddRangeFrameOption(10, 206617)
 --mod:AddSetIconOption("SetIconOnTimeRelease", 206610, true)
 
 mod.vb.OPActive = false
+mod.vb.normCount = 0
+mod.vb.fastCount = 0
+mod.vb.slowCount = 0
 
 function mod:OnCombatStart(delay)
 	self.vb.OPActive = false
+	self.vb.normCount = 0
+	self.vb.fastCount = 0
+	self.vb.slowCount = 0
 	--No timers here, started by speed events
 end
 
@@ -75,23 +83,19 @@ function mod:SPELL_CAST_START(args)
 	if spellId == 211927 then
 		timerChronoPartCD:Stop()--Will be used immediately when this ends.
 		timerPowerOverwhelmingCD:Start()
-		specWarnPowerOverwhelming:Show()
-		voicePowerOverwhelming:Play("watchstep")
 	elseif spellId == 207228 and self:CheckInterruptFilter(args.sourceGUID) then
 		specWarnWarp:Show(args.sourceName)
 		voiceWarp:Play("kickcast")
 	end
 end
 
---[[
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 206618 then
-		timerTimeBombCD:Start()
-	elseif spellId == 206610 then
-		timerTimeReleaseCD:Start()
+	if spellId == 219815 then
+		specWarnTemporalOrbs:Show()
+		voiceTemporalOrbs:Play("watchstep")
 	end
-end--]]
+end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
@@ -156,38 +160,103 @@ end
 local function delayedTimeRelease(self, time, count)
 	timerTimeReleaseCD:Start(time, count)
 end
+local function delayedTimeBomb(self, time, count)
+	timerTimeBombCD:Start(time, count)
+end
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, spellGUID)
 	local spellId = tonumber(select(5, strsplit("-", spellGUID)), 10)
+	--Boss has 3 phase speeds with multiple scripts. Order is predictable however
+	--The timers for each variation are hard coded because it's most accurate means of doing them
+	--Doing sequence tables is less accurate because of how uninterrupted overwhelming power affect such design.
 	if spellId == 207012 then--Speed: Normal
-		timerTimeReleaseCD:Start(7, 1)
-		timerBurstofTimeCD:Start(10, 1)
-		timerSmallAddCD:Start(10)
-		self:Schedule(10, delayedTimeRelease, self, 15, 2)--22
-		timerBigAddCD:Start(25)
-		countdownBigAdd:Start(25)
-		timerTimeBombCD:Start(35)
-		self:Schedule(10, delayedBurst, self, 25, 2)--35
-		self:Schedule(35, delayedBurst, self, 5, 3)--40
-		self:Schedule(22, delayedTimeRelease, self, 25, 3)--47
-		self:Schedule(47, delayedTimeRelease, self, 10, 3)--57
-		timerPowerOverwhelmingCD:Start(60)
+		self.vb.normCount = self.vb.normCount + 1
+		if self.vb.normCount == 1 then
+			timerTimeReleaseCD:Start(5, 1)
+			timerBurstofTimeCD:Start(10, 1)
+			timerSmallAddCD:Start(10)
+			self:Schedule(5, delayedTimeRelease, self, 15, 2)--20
+			timerBigAddCD:Start(25)
+			countdownBigAdd:Start(25)
+			timerTimeBombCD:Start(35)
+			self:Schedule(10, delayedBurst, self, 25, 2)--35
+			timerTemporalOrbsCD:Start(40)
+			self:Schedule(20, delayedTimeRelease, self, 25, 3)--45
+			self:Schedule(35, delayedBurst, self, 20, 3)--55
+			timerPowerOverwhelmingCD:Start(60)
+		elseif self.vb.normCount == 2 then
+			timerTimeBombCD:Start(5)
+			timerTimeReleaseCD:Start(10, 1)
+			timerBurstofTimeCD:Start(20, 1)
+			self:Schedule(10, delayedTimeRelease, self, 15, 2)--25
+			self:Schedule(5, delayedTimeBomb, self, 25, 2)--30
+			self:Schedule(25, delayedTimeRelease, self, 10, 3)--35
+			timerBigAddCD:Start(45)
+			countdownBigAdd:Start(45)
+			timerTemporalOrbsCD:Start(55)
+			timerPowerOverwhelmingCD:Start(60)
+			--There may be some missing timers here based on duration of power overwhelming clipping some other timers
+			self:Schedule(35, delayedTimeRelease, self, 50, 4)--85
+		else
+			DBM:AddMsg("There is no timer data going this far into the fight. Please submit transcriptor log to improve this mod")
+		end
 	elseif spellId == 207011 then--Speed: Slow
-		timerTimeReleaseCD:Start(7.5, 1)
-		timerBigAddCD:Start(15)
-		countdownBigAdd:Start(15)
-		timerBurstofTimeCD:Start(20, 1)
-		timerTimeBombCD:Start(20)
+		self.vb.slowCount = self.vb.slowCount + 1
+		if self.vb.slowCount == 1 then
+			timerTimeReleaseCD:Start(5, 1)
+			timerTimeBombCD:Start(20, 1)
+			timerBurstofTimeCD:Start(20, 1)
+			timerSmallAddCD:Start(25)
+			timerTemporalOrbsCD:Start(30)
+			self:Schedule(20, delayedBurst, self, 10, 2)--30
+			self:Schedule(5, delayedTimeRelease, self, 30, 2)--35
+			self:Schedule(20, delayedTimeBomb, self, 20, 2)--40
+			timerBigAddCD:Start(45)
+			countdownBigAdd:Start(45)
+			self:Schedule(30, delayedBurst, self, 20, 3)--50
+			timerPowerOverwhelmingCD:Start(60)
+		elseif self.vb.slowCount == 2 then
+			timerTimeReleaseCD:Start(5, 1)
+			timerTemporalOrbsCD:Start(15)
+			timerBurstofTimeCD:Start(20, 1)
+			timerBigAddCD:Start(25)
+			countdownBigAdd:Start(25)
+			self:Schedule(20, delayedBurst, self, 10, 2)--30
+			self:Schedule(30, delayedBurst, self, 5, 3)--35
+			timerPowerOverwhelmingCD:Start(40)
+			self:Schedule(35, delayedBurst, self, 10, 4)--45
+			self:Schedule(45, delayedBurst, self, 5, 5)--50
+			--This may be incomplete.
+		else
+			DBM:AddMsg("There is no timer data going this far into the fight. Please submit transcriptor log to improve this mod")
+		end
 	elseif spellId == 207013 then--Speed: Fast
-		timerBurstofTimeCD:Start(5, 1)
-		timerTimeReleaseCD:Start(6.5, 1)
-		timerTimeBombCD:Start(10)
-		self:Schedule(6.5, delayedTimeRelease, self, 10, 2)--16.5
-		self:Schedule(5, delayedBurst, self, 15, 2)--20
-		timerSmallAddCD:Start(20)
-		timerBigAddCD:Start(25)
-		countdownBigAdd:Start(25)
-		timerPowerOverwhelmingCD:Start(35)
+		self.vb.fastCount = self.vb.fastCount + 1
+		if self.vb.fastCount == 1 then
+			timerTimeReleaseCD:Start(5, 1)
+			timerSmallAddCD:Start(10)
+			self:Schedule(5, delayedTimeRelease, self, 5, 2)--10
+			timerBurstofTimeCD:Start(15, 1)
+			timerTimeBombCD:Start(25, 1)
+			self:Schedule(10, delayedTimeRelease, self, 20, 3)--30
+			timerBigAddCD:Start(35)
+			countdownBigAdd:Start(35)
+			self:Schedule(30, delayedTimeRelease, self, 10, 4)--40
+			timerTemporalOrbsCD:Start(50)
+			timerPowerOverwhelmingCD:Start(60)
+		elseif self.vb.fastCount == 2 then
+			timerBurstofTimeCD:Start(5, 1)
+			self:Schedule(5, delayedBurst, self, 5, 2)--10
+			self:Schedule(10, delayedBurst, self, 5, 3)--15
+			timerTemporalOrbsCD:Start(20)
+			self:Schedule(15, delayedBurst, self, 10, 4)--25
+			timerTimeBombCD:Start(30, 1)
+			timerTimeReleaseCD:Start(40, 1)
+			timerSmallAddCD:Start(45)
+			self:Schedule(25, delayedBurst, self, 25, 5)--50
+		else
+			DBM:AddMsg("There is no timer data going this far into the fight. Please submit transcriptor log to improve this mod")
+		end
 	elseif spellId == 206699 then--Summon Haste Add (Small Adds)
 		specWarnSmallAdd:Show()
 		voiceSmallAdd:Play("mobsoon")
