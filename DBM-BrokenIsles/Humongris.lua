40da6862e22627676fdeda73316c2b386f5b8010
Adam <MysticalOS@users.noreply.github.com>
2016-05-14 02:56:43 +0000

    Push drugon and Humongris

--- DBM-BrokenIsles/Humongris.lua
+++ DBM-BrokenIsles/Humongris.lua
@@ -10,42 +10,87 @@ mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 175791",
-	"SPELL_AURA_APPLIED 175827"
+	"SPELL_CAST_START 216428 216430 216432",
+	"SPELL_CAST_SUCCESS 216817",
+	"SPELL_AURA_APPLIED 216467 216817 216822"
 )
 
-local specWarnColossalSlam		= mod:NewSpecialWarningDodge(175791, nil, nil, nil, 2, 2)
-local specWarnCallofEarth		= mod:NewSpecialWarningSpell(175827)
+--TODO, evaluate severity of some warnings and promote/demote between warn/specwarn as needed
+local warnIceFist				= mod:NewSpellAnnounce(216432, 3)
+local warnSnow					= mod:NewSpellAnnounce(216467, 3)
 
-local timerColossalSlamCD		= mod:NewCDTimer(16, 175791, nil, nil, nil, 3)
-local timerCallofEarthCD		= mod:NewCDTimer(90, 175827, nil, nil, nil, 1)
+local specWarnFireBoom			= mod:NewSpecialWarningSpell(216428, nil, nil, nil, 2, 2)
+local specWarnStomp				= mod:NewSpecialWarningSpell(216430, nil, nil, nil, 2, 2)
+local specWarnGoBangYou			= mod:NewSpecialWarningDefensive(216822, nil, nil, nil, 1, 2)
+local specWarnGoBangSwap		= mod:NewSpecialWarningTaunt(216822, nil, nil, nil, 1, 2)
 
-local voiceColossalSlam			= mod:NewVoice(175791)
+local timerFireBoomCD			= mod:NewAITimer(16, 216428, nil, nil, nil, 2)
+local timerStompCD				= mod:NewAITimer(16, 216430, nil, nil, nil, 2)
+local timerIceFistCD			= mod:NewAITimer(16, 216432, nil, nil, nil, 2)
+local timerSnowCD				= mod:NewAITimer(16, 216467, nil, nil, nil, 2)
+local timerGoBangCD				= mod:NewAITimer(16, 216822, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerGoBangStarts			= mod:NewTargetTimer(12, 216822, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+
+local voiceFireBoom				= mod:NewVoice(216428)--aesoon
+local voiceStomp				= mod:NewVoice(216430)--carefly
+local voiceGoBank				= mod:NewVoice(216822)--tauntboss
 
 --mod:AddReadyCheckOption(37460, false)
+mod:AddRangeFrameOption(10, 216428)
 
 function mod:OnCombatStart(delay, yellTriggered)
 	if yellTriggered then
 
 	end
+	if self.Options.RangeFrame then
+		DBM.RangeCheck:Show(10)
+	end
 end
 
 function mod:OnCombatEnd()
+	if self.Options.RangeFrame then
+		DBM.RangeCheck:Hide()
+	end
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 175791 then
+	if spellId == 216428 then
+		specWarnFireBoom:Show()
+		voiceFireBoom:Play("aesoon")
+		timerFireBoomCD:Start()
+	elseif spellId == 216430 then
+		specWarnStomp:Show()
+		voiceStomp:Play("carefly")
+		timerStompCD:Start()
+	elseif spellId == 216432 then
+		warnIceFist:Show()
+		timerIceFistCD:Start()
+	end
+end
 
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+	if spellId == 216817 then
+		timerGoBangCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 175827 then
-
+	if spellId == 216467 then
+		warnSnow:Show()
+		timerSnowCD:Start()
+	elseif spellId == 216817 then
+		timerGoBangStarts:Start(args.destName)
+	elseif spellId == 216822 then
+		if args:IsPlayer() then
+			specWarnGoBangYou:Show()
+			voiceGoBank:Play("defensive")
+		else
+			specWarnGoBangSwap:Show(args.destName)
+			voiceGoBank:Play("tauntboss")
+		end
 	end
 end
---]]
