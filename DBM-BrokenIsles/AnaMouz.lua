29358936120adf909be263095fec392bc4820a44
Adam <MysticalOS@users.noreply.github.com>
2016-05-14 02:20:50 +0000

    Push Calamir and Anamouz

--- DBM-BrokenIsles/AnaMouz.lua
+++ DBM-BrokenIsles/AnaMouz.lua
@@ -10,19 +10,30 @@ mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 175791",
-	"SPELL_AURA_APPLIED 175827"
+	"SPELL_CAST_START 218823 218888 219045 219254",
+	"SPELL_AURA_APPLIED 219045 219068"
 )
 
-local specWarnColossalSlam		= mod:NewSpecialWarningDodge(175791, nil, nil, nil, 2, 2)
-local specWarnCallofEarth		= mod:NewSpecialWarningSpell(175827)
+--TODO: Tank swap warning if breath is often enough and threatening.
+--TODO: figure out Gaseous Breath stuff.
+--TODO, icon option for mind controls maybe. Coordinate healer dispels better
+local warnMothersEmbrace		= mod:NewTargetAnnounce(219045, 3)
+local warnMothersEmbraceFail	= mod:NewTargetAnnounce(219068, 4)
+local warnGaseousBreath			= mod:NewSpellAnnounce(219254, 2)
 
-local timerColossalSlamCD		= mod:NewCDTimer(16, 175791, nil, nil, nil, 3)
-local timerCallofEarthCD		= mod:NewCDTimer(90, 175827, nil, nil, nil, 1)
+local specWarnFelGeyser			= mod:NewSpecialWarningDodge(218823, nil, nil, nil, 2, 2)
+local specWarnImpishFlames		= mod:NewSpecialWarningDefensive(218888, "Tank")
+local specWarnMothersEmbrace	= mod:NewSpecialWarningDispel(219045, "Healer")
 
-local voiceColossalSlam			= mod:NewVoice(175791)
+local timerFelGeyserCD			= mod:NewAITimer(16, 218823, nil, nil, nil, 2)
+local timerImpishFlamesCD		= mod:NewAITimer(90, 218888, nil, "Tank", nil, 5)
+local timerMothersEmbraceCD		= mod:NewAITimer(90, 219045, nil, nil, nil, 3)
+local timerGaseousBreathCD		= mod:NewAITimer(90, 219254, nil, nil, nil, 1)
+
+local voiceFelGeyser			= mod:NewVoice(218823)--watchstep
+local voiceImpishFlames			= mod:NewVoice(218888, "Tank")--breathsoon
+local voiceMothersEmbrace		= mod:NewVoice(219045, "Healer")--helpdispel
 
 --mod:AddReadyCheckOption(37460, false)
 
@@ -33,19 +44,39 @@ function mod:OnCombatStart(delay, yellTriggered)
 end
 
 function mod:OnCombatEnd()
+
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 175791 then
-
+	if spellId == 218823 then
+		specWarnFelGeyser:Show()
+		voiceFelGeyser:Play("watchstep")
+		timerFelGeyserCD:Start()
+	elseif spellId == 218888 then
+		specWarnImpishFlames:Show()
+		voiceImpishFlames:Play("breathsoon")
+		timerImpishFlamesCD:Start()
+	elseif spellId == 219045 then
+		timerMothersEmbraceCD:Start()
+	elseif spellId == 219254 then
+		warnGaseousBreath:Show()
+		timerGaseousBreathCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 175827 then
-
+	if spellId == 219045 then
+		if self.Options.SpecWarn219045dispel then
+			specWarnMothersEmbrace:CombinedShow(0.3, args.destName)
+		else
+			warnMothersEmbrace:CombinedShow(0.3, args.destName)
+		end
+		if self:AntiSpam(3, 1) then
+			voiceMothersEmbrace:Play("helpdispel")
+		end
+	elseif spellId == 219068 then--Dispel failure.
+		warnMothersEmbraceFail:CombinedShow(0.3, args.destName)
 	end
 end
---]]
