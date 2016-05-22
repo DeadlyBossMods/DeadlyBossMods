13e96974fc4ae4f728d967e9379636be268eb2cb
Adam <MysticalOS@users.noreply.github.com>
2016-05-22 01:28:30 +0000

    Bump packager

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -2974,7 +2974,7 @@ function DBM:LoadModOptions(modId, inCombat, first)
 						if savedOptions[id][profileNum][option] and (type(savedOptions[id][profileNum][option]) == "boolean") then
 							savedOptions[id][profileNum][option] = mod.DefaultOptions[option].value
 						end
-					--Fix default options for colored bar by type that were set to 0 because at time options were created, no defaults existed, but do now.
+					--Fix default options for colored bar by type that were set to 0 because no defaults existed at time they were created, but do now.
 					elseif option:find("TColor") then
 						if savedOptions[id][profileNum][option] and savedOptions[id][profileNum][option] == 0 and mod.DefaultOptions[option] and mod.DefaultOptions[option] ~= 0 then
 							savedOptions[id][profileNum][option] = mod.DefaultOptions[option]
