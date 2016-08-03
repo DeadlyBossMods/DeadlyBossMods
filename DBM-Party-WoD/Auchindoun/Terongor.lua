37c6248203f7ac884092f160f0a27a9581f34514
Adam <MysticalOS@users.noreply.github.com>
2016-08-03 05:39:29 +0000

    Update remove curse warnings to now be off by default for mages

--- DBM-Party-WoD/Auchindoun/Terongor.lua
+++ DBM-Party-WoD/Auchindoun/Terongor.lua
@@ -41,7 +41,7 @@ local specWarnFixate			= mod:NewSpecialWarningRun(157168, nil, nil, 2, 4)
 --Affliction Abilities
 --TODO : Maybe need shit warning.
 local specWarnSeedOfMelevolence	= mod:NewSpecialWarningMoveAway(156921, nil, nil, nil, 1, 2)
-local specWarnExhaustion		= mod:NewSpecialWarningDispel(164841, "RemoveCurse", nil, nil, 1, 2)
+local specWarnExhaustion		= mod:NewSpecialWarningDispel(164841, "RemoveCurse", nil, 2, 1, 2)
 --Destruction Abilities
 local specWarnChaosBolt			= mod:NewSpecialWarningInterrupt(156975, "-Healer", nil, nil, 3, 2)
 local specWarnImmolate			= mod:NewSpecialWarningDispel(156964, "Healer", nil, nil, 1, 2)
