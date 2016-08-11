ff1a744fa924bc7ca3291cffa629fedbc266659a
Adam <MysticalOS@users.noreply.github.com>
2016-08-11 03:51:38 +0000

    Further warnings for demonic invasion, this time bolster up fire ability set warnings

--- DBM-WorldEvents/Quests/DemonInvasions.lua
+++ DBM-WorldEvents/Quests/DemonInvasions.lua
@@ -6,7 +6,9 @@ mod:SetZone(DBM_DISABLE_ZONE_DETECTION)
 
 mod:RegisterEvents(
 	"SPELL_CAST_START 223420 219469 219441 219112 219110",
+	"SPELL_CAST_SUCCESS 218639",
 	"SPELL_AURA_APPLIED 218625 218657",
+	"SPELL_AURA_REMOVED 218657",
 	"SPELL_PERIODIC_DAMAGE 219367 207576",
 	"SPELL_PERIODIC_MISSED 219367 207576",
 	"SCENARIO_UPDATE"
@@ -22,15 +24,21 @@ local warnFlameBreath				= mod:NewSpellAnnounce(219441, 3)
 
 local specWarnRainofFire			= mod:NewSpecialWarningMove(219367, nil, nil, nil, 1, 2)
 local specWarnFelFire				= mod:NewSpecialWarningMove(207576, nil, nil, nil, 1, 2)
-local specWarnCharredFlesh			= mod:NewSpecialWarningYou(218657)--Don't really have an appropriate voice file, if it were organized raids This would be a healer warning not a personal. Instead it's basically a "you're gonna die"
+local specWarnDestructiveFlames		= mod:NewSpecialWarningDodge(218639, nil, nil, nil, 2, 2)--Is this a reflect?
+local specWarnCharredFlesh			= mod:NewSpecialWarningMoveAway(218657)--Don't really have an appropriate voice file, if it were organized raids This would be a healer warning not a personal. Instead it's basically a "you're gonna die"
+local yellCharredFlesh				= mod:NewFadesYell(218657)
 local specWarnEyeOfDarkness			= mod:NewSpecialWarningMoveTo(219112, nil, DBM_CORE_AUTO_SPEC_WARN_OPTIONS.spell:format(219112), nil, 3, 2)
 local specWarnShadowNova			= mod:NewSpecialWarningRun(219110, nil, nil, nil, 4, 2)
 
+local timerDestructiveFlamesCD		= mod:NewCDTimer(45, 218639, nil, nil, nil, 2, nil, DBM_CORE_DEADLY_ICON)
 local timerEyeofDarknessCD			= mod:NewCDTimer(34, 219112, nil, nil, nil, 2, nil, DBM_CORE_DEADLY_ICON)
 local timerShadowNovaCD				= mod:NewCDTimer(35.5, 219110, nil, nil, nil, 2)
 
+local countdownCharredFlesh			= mod:NewCountdownFades(15, 218657)
+
 local voiceRainofFire				= mod:NewVoice(219367)--runaway
 local voiceFelFire					= mod:NewVoice(207576)--runaway
+local voiceDestructiveFlames		= mod:NewVoice(218639)--watchstep
 local voiceEyeOfDarkness			= mod:NewVoice(219112)--runin
 local voiceShadowNova				= mod:NewVoice(219110)--runout
 
@@ -56,13 +64,41 @@ function mod:SPELL_CAST_START(args)
 	end
 end
 
+function mod:SPELL_CAST_SUCCESS(args)
+	if not self.Options.Enabled then return end
+	local spellId = args.spellId
+	if spellId == 218639 then
+		specWarnDestructiveFlames:Show()
+		voiceDestructiveFlames:Play("watchstep")
+		timerDestructiveFlamesCD:Start()
+	end
+end
+
 function mod:SPELL_AURA_APPLIED(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
 	if spellId == 218625 and self:AntiSpam(3, 3) then
 		warnBlazingHellfire:Show()
 	elseif spellId == 218657 and args:IsPlayer() then
-		specWarnCharredFlesh:Show()
+		specWarnCharredFlesh:Schedule(10)
+		countdownCharredFlesh:Start()
+		local _, _, _, _, _, duration, expires, _, _ = UnitDebuff("player", args.spellName)
+		if expires then
+			local remaining = expires-GetTime()
+			yellCharredFlesh:Schedule(remaining-1, 1)
+			yellCharredFlesh:Schedule(remaining-2, 2)
+			yellCharredFlesh:Schedule(remaining-3, 3)
+		end
+	end
+end
+
+function mod:SPELL_AURA_REMOVED(args)
+	if not self.Options.Enabled then return end
+	local spellId = args.spellId
+	if spellId == 218657 and args:IsPlayer() then
+		specWarnCharredFlesh:Cancel()
+		countdownCharredFlesh:Cancel()
+		yellCharredFlesh:Cancel()
 	end
 end
 
@@ -86,5 +122,6 @@ function mod:SCENARIO_UPDATE()
 	if completed then
 		timerShadowNovaCD:Stop()
 		timerEyeofDarknessCD:Stop()
+		timerDestructiveFlamesCD:Stop()
 	end
 end
