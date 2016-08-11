0b6535971e1a80f8b35dea2c15e15c53158eddb3
Adam <MysticalOS@users.noreply.github.com>
2016-08-11 06:50:47 +0000

    Add waves of dread Add carrion swarm Add legion infernals

--- DBM-WorldEvents/Quests/DemonInvasions.lua
+++ DBM-WorldEvents/Quests/DemonInvasions.lua
@@ -5,8 +5,9 @@ mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetZone(DBM_DISABLE_ZONE_DETECTION)
 
 mod:RegisterEvents(
-	"SPELL_CAST_START 223420 219469 219441 219112 219110 224067",
+	"SPELL_CAST_START 223420 219469 219441 219112 219110 224067 213890 216916",
 	"SPELL_CAST_SUCCESS 218639 218659",
+	"SPELL_SUMMON 213861",
 	"SPELL_AURA_APPLIED 218625 218657 224044",
 	"SPELL_AURA_REMOVED 218657 224044",
 	"SPELL_PERIODIC_DAMAGE 219367 207576",
@@ -23,7 +24,7 @@ local warnExplosiveOrbs				= mod:NewSpellAnnounce(219469, 2)
 local warnFlameBreath				= mod:NewSpellAnnounce(219441, 3)
 local warnWakeofBlood				= mod:NewSpellAnnounce(224067, 4)--See if target scanning works? Might always be on person tanking that mob which means even if it doesn't can assume it's "tank"
 
-local specWarnMarkofBlood			= mod:NewSpecialWarningMoveAway(224044)
+local specWarnMarkofBlood			= mod:NewSpecialWarningMoveAway(224044, nil, nil, nil, 1, 2)
 local specWarnRainofFire			= mod:NewSpecialWarningMove(219367, nil, nil, nil, 1, 2)
 local specWarnFelFire				= mod:NewSpecialWarningMove(207576, nil, nil, nil, 1, 2)
 local specWarnDestructiveFlames		= mod:NewSpecialWarningDodge(218639, nil, nil, nil, 2, 2)--Is this a reflect?
@@ -31,10 +32,15 @@ local specWarnCharredFlesh			= mod:NewSpecialWarningMoveAway(218657)--Don't real
 local yellCharredFlesh				= mod:NewFadesYell(218657)
 local specWarnEyeOfDarkness			= mod:NewSpecialWarningMoveTo(219112, nil, DBM_CORE_AUTO_SPEC_WARN_OPTIONS.spell:format(219112), nil, 3, 2)
 local specWarnShadowNova			= mod:NewSpecialWarningRun(219110, nil, nil, nil, 4, 2)
+local specWarnCarrionSwarm			= mod:NewSpecialWarningRun(213890, nil, nil, nil, 4, 2)
+local specWarnLegionInfernals		= mod:NewSpecialWarningSwitch(213861, "RangedDps", nil, nil, 1, 2)
+local specWarnWavesofDread			= mod:NewSpecialWarningDodge(216916, nil, nil, nil, 2, 2)
 
 local timerDestructiveFlamesCD		= mod:NewCDTimer(45, 218639, nil, nil, nil, 2, nil, DBM_CORE_DEADLY_ICON)
 local timerEyeofDarknessCD			= mod:NewCDTimer(34, 219112, nil, nil, nil, 2, nil, DBM_CORE_DEADLY_ICON)
 local timerShadowNovaCD				= mod:NewCDTimer(34, 219110, nil, nil, nil, 2)
+local timerCarrionSwarmCD			= mod:NewCDTimer(32.5, 213890, nil, nil, nil, 2)
+local timerLegionInfernalsCD		= mod:NewCDTimer(30, 213861, nil, nil, nil, 1)
 local timerCharredFlesh				= mod:NewBuffFadesTimer(17, 218657, nil, nil, nil, 3)
 
 local countdownCharredFlesh			= mod:NewCountdownFades(17, 218657)
@@ -45,6 +51,9 @@ local voiceFelFire					= mod:NewVoice(207576)--runaway
 local voiceDestructiveFlames		= mod:NewVoice(218639)--watchstep
 local voiceEyeOfDarkness			= mod:NewVoice(219112)--runin
 local voiceShadowNova				= mod:NewVoice(219110)--runout
+local voiceCarrionSwarm				= mod:NewVoice(213890)--runout
+local voiceLegionInfernals			= mod:NewVoice(213861, "RangedDps")--killmob
+local voiceWavesofDread				= mod:NewVoice(216916)--watchwave
 --local voiceWakeofBlood			= mod:NewVoice(224067)--keepmove
 
 mod:RemoveOption("HealthFrame")
@@ -69,6 +78,13 @@ function mod:SPELL_CAST_START(args)
 		timerShadowNovaCD:Start()
 	elseif spellId == 224067 then
 		warnWakeofBlood:Show()
+	elseif spellId == 213890 then
+		specWarnCarrionSwarm:Show()
+		voiceCarrionSwarm:Play("runout")
+		timerCarrionSwarmCD:Start()
+	elseif spellId == 216916 and self:AntiSpam(8, 6) then
+		specWarnWavesofDread:Show()
+		voiceWavesofDread:Play("watchwave")
 	end
 end
 
@@ -82,6 +98,16 @@ function mod:SPELL_CAST_SUCCESS(args)
 	elseif spellId == 218659 then
 		timerCharredFlesh:Start()
 		countdownCharredFlesh:Start()
+	end
+end
+
+function mod:SPELL_SUMMON(args)
+	if not self.Options.Enabled then return end
+	local spellId = args.spellId
+	if spellId == 213861 and self:AntiSpam(3, 5) then
+		specWarnLegionInfernals:Show()
+		voiceLegionInfernals:Play("killmob")
+		timerLegionInfernalsCD:Start()
 	end
 end
 
@@ -140,5 +166,7 @@ function mod:SCENARIO_UPDATE()
 		timerShadowNovaCD:Stop()
 		timerEyeofDarknessCD:Stop()
 		timerDestructiveFlamesCD:Stop()
+		timerCarrionSwarmCD:Stop()
+		timerLegionInfernalsCD:Stop()
 	end
 end
