e2e8261af66d65b35ee90bc79f8ad54b46025c6d
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-08-11 06:12:24 +0000

    Fix lua error

--- trunk/DBM-WorldEvents/Quests/DemonInvasions.lua
+++ trunk/DBM-WorldEvents/Quests/DemonInvasions.lua
@@ -35,7 +35,7 @@ local specWarnShadowNova			= mod:NewSpecialWarningRun(219110, nil, nil, nil, 4, 
 local timerDestructiveFlamesCD		= mod:NewCDTimer(45, 218639, nil, nil, nil, 2, nil, DBM_CORE_DEADLY_ICON)
 local timerEyeofDarknessCD			= mod:NewCDTimer(34, 219112, nil, nil, nil, 2, nil, DBM_CORE_DEADLY_ICON)
 local timerShadowNovaCD				= mod:NewCDTimer(34, 219110, nil, nil, nil, 2)
-local timerCharredFlesh				= mod:NewFadesTimer(17, 218657, nil, nil, nil, 3)
+local timerCharredFlesh				= mod:NewBuffFadesTimer(17, 218657, nil, nil, nil, 3)
 
 local countdownCharredFlesh			= mod:NewCountdownFades(17, 218657)
 
