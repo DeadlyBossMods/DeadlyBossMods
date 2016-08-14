6d04f43e6ebb488ee6977f069dcd05140fbacb95
Adam <MysticalOS@users.noreply.github.com>
2016-08-14 06:27:13 +0000

    Fix option default for skovald

--- DBM-Party-Legion/HallsOfValor/KingSkovald.lua
+++ DBM-Party-Legion/HallsOfValor/KingSkovald.lua
@@ -28,7 +28,7 @@ local specWarnRagnarok				= mod:NewSpecialWarningMoveTo(193826, nil, DBM_CORE_AU
 local specWarnFlames				= mod:NewSpecialWarningMove(193702, nil, nil, nil, 1, 2)
 
 local timerRushCD					= mod:NewCDTimer(11, 193659, nil, nil, nil, 3)--11-13 unless delayed by claim aegis or ragnarok
-local timerSavageBladeCD			= mod:NewCDTimer(19.5, 193668, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)--23 unless delayed by claim aegis or ragnarok
+local timerSavageBladeCD			= mod:NewCDTimer(19.5, 193668, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)--23 unless delayed by claim aegis or ragnarok
 local timerRagnarokCD				= mod:NewCDTimer(51, 193826, nil, nil, nil, 2, nil, DBM_CORE_DEADLY_ICON)--60 now? or maybe health based?
 
 local voiceSavageBlade				= mod:NewVoice(193668, "Tank")--defensive
