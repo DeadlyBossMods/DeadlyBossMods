ef05f6f84cdd20ca49849e8cb98b9ae282d586f5
Adam <MysticalOS@users.noreply.github.com>
2016-09-02 00:43:18 +0000

    Small voice tweak on hymdall Add Helya’s breath timers/warnings/countdown

--- DBM-Party-Legion/MawOfSouls/Helya.lua
+++ DBM-Party-Legion/MawOfSouls/Helya.lua
@@ -9,6 +9,7 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
+	"SPELL_CAST_START 227233",
 	"SPELL_AURA_APPLIED 196947 197262",
 	"SPELL_AURA_REMOVED 196947",
 	"CHAT_MSG_RAID_BOSS_EMOTE",
@@ -20,20 +21,33 @@ local warnSubmerged						= mod:NewTargetAnnounce(196947, 2)
 
 local specWarnDestructorTentacle		= mod:NewSpecialWarningSwitch("ej12364", "Tank")
 local specWarnSubmergedOver				= mod:NewSpecialWarningEnd(196947)
+local specWarnBreath					= mod:NewSpecialWarningDodge(227233, nil, nil, nil, 1, 2)
 
 local timerTaintofSeaCD					= mod:NewCDTimer(12, 197262, nil, false, nil, 3)
 local timerPiercingTentacleCD			= mod:NewNextTimer(9, 197596, nil, nil, nil, 3)
 --local timerDestructorTentacleCD		= mod:NewCDTimer(26, "ej12364", nil, nil, nil, 1)--More data
 local timerSubmerged					= mod:NewBuffFadesTimer(15, 196947, nil, nil, nil, 6)
+local timerBreathCD						= mod:NewNextTimer(22, 227233, nil, nil, nil, 3)
 
---local voiceCurtainOfFlame				= mod:NewVoice(153392)
+local countdownBreath					= mod:NewCountdown(22, 227233)
+
+local voiceBreath						= mod:NewVoice(227233)--breathsoon
 
 mod.vb.phase = 1
 
 function mod:OnCombatStart(delay)
 	self.vb.phase = 1
 	timerPiercingTentacleCD:Start(8.5)
---	timerDestructorTentacleCD:Start()--Too variable
+end
+
+function mod:SPELL_CAST_START(args)
+	local spellId = args.spellId
+	if spellId == 227233 then
+		specWarnBreath:Show()
+		voiceBreath:Play("breathsoon")
+		timerBreathCD:Start()
+		countdownBreath:Start()
+	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
@@ -41,6 +55,8 @@ function mod:SPELL_AURA_APPLIED(args)
 	if spellId == 196947 then
 		timerPiercingTentacleCD:Stop()
 		timerTaintofSeaCD:Stop()
+		timerBreathCD:Stop()
+		countdownBreath:Cancel()
 		warnSubmerged:Show(args.destName)
 		timerSubmerged:Start()
 		if self.vb.phase == 1 then
@@ -55,6 +71,8 @@ function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
 	if spellId == 196947 then
 		specWarnSubmergedOver:Show()
+		timerBreathCD:Start(15)
+		countdownBreath:Start(15)
 	end
 end
 
