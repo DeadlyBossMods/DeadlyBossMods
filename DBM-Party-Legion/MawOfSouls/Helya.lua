ccdd2ace9a154616e19e8e79abe311ca540452af
Adam <MysticalOS@users.noreply.github.com>
2016-09-04 19:22:30 +0000

    Add torrent to Helya

--- DBM-Party-Legion/MawOfSouls/Helya.lua
+++ DBM-Party-Legion/MawOfSouls/Helya.lua
@@ -9,7 +9,7 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 227233",
+	"SPELL_CAST_START 227233 198495",
 	"SPELL_AURA_APPLIED 196947 197262",
 	"SPELL_AURA_REMOVED 196947",
 	"CHAT_MSG_RAID_BOSS_EMOTE",
@@ -22,16 +22,19 @@ local warnSubmerged						= mod:NewTargetAnnounce(196947, 2)
 local specWarnDestructorTentacle		= mod:NewSpecialWarningSwitch("ej12364", "Tank")
 local specWarnSubmergedOver				= mod:NewSpecialWarningEnd(196947)
 local specWarnBreath					= mod:NewSpecialWarningDodge(227233, nil, nil, nil, 1, 2)
+local specWarnTorrent					= mod:NewSpecialWarningInterrupt(198495, "HasInterrupt", nil, nil, 1, 2)
 
 local timerTaintofSeaCD					= mod:NewCDTimer(12, 197262, nil, false, nil, 3)
 local timerPiercingTentacleCD			= mod:NewNextTimer(9, 197596, nil, nil, nil, 3)
 --local timerDestructorTentacleCD		= mod:NewCDTimer(26, "ej12364", nil, nil, nil, 1)--More data
 local timerSubmerged					= mod:NewBuffFadesTimer(15, 196947, nil, nil, nil, 6)
 local timerBreathCD						= mod:NewNextTimer(22, 227233, nil, nil, nil, 3)
+local timerTorrentCD					= mod:NewCDTimer(12, 198495, nil, nil, nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
 
 local countdownBreath					= mod:NewCountdown(22, 227233)
 
 local voiceBreath						= mod:NewVoice(227233)--breathsoon
+local voiceTorrent						= mod:NewVoice(198495, "HasInterrupt")--kickcast
 
 mod.vb.phase = 1
 
@@ -47,6 +50,12 @@ function mod:SPELL_CAST_START(args)
 		voiceBreath:Play("breathsoon")
 		timerBreathCD:Start()
 		countdownBreath:Start()
+	elseif spellId == 198495 then
+		timerTorrentCD:Start()
+		if self:CheckInterruptFilter(args.sourceGUID) then
+			specWarnTorrent:Show(args.sourceName)
+			voiceTorrent:Play("kickcast")
+		end
 	end
 end
 
@@ -56,6 +65,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		timerPiercingTentacleCD:Stop()
 		timerTaintofSeaCD:Stop()
 		timerBreathCD:Stop()
+		timerTorrentCD:Stop()
 		countdownBreath:Cancel()
 		warnSubmerged:Show(args.destName)
 		timerSubmerged:Start()
@@ -70,6 +80,7 @@ end
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
 	if spellId == 196947 then
+		timerTorrentCD:Start(5)
 		specWarnSubmergedOver:Show()
 		timerBreathCD:Start(15)
 		countdownBreath:Start(15)
