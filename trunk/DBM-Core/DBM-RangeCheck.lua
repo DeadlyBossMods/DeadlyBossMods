1b1b59cc5c37522aaf4568dbb8b419c84515067f
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-09-16 20:00:18 +0000

    tweaks

--- trunk/DBM-Core/DBM-RangeCheck.lua
+++ trunk/DBM-Core/DBM-RangeCheck.lua
@@ -189,6 +189,20 @@ do
 				UIDropDownMenu_AddButton(info, 2)
 
 				info = UIDropDownMenu_CreateInfo()
+				info.text = DBM_CORE_RANGECHECK_SETRANGE_TO:format(6)
+				info.func = setRange
+				info.arg1 = 6
+				info.checked = (mainFrame.range == 6)
+				UIDropDownMenu_AddButton(info, 2)
+				
+				info = UIDropDownMenu_CreateInfo()
+				info.text = DBM_CORE_RANGECHECK_SETRANGE_TO:format(8)
+				info.func = setRange
+				info.arg1 = 8
+				info.checked = (mainFrame.range == 8)
+				UIDropDownMenu_AddButton(info, 2)
+
+				info = UIDropDownMenu_CreateInfo()
 				info.text = DBM_CORE_RANGECHECK_SETRANGE_TO:format(10)
 				info.func = setRange
 				info.arg1 = 10
@@ -579,10 +593,12 @@ do
 				if restricted then--API restrictions are in play, so pretend we're back in BC
 					--Start at bottom and work way up.
 					--Definitely not most efficient way of doing it. Refactor later when 7.1 hits PTR
-					if IsItemInRange(37727, uId) then range = 5
+					if IsItemInRange(37727, uId) then range = 5--Ruby Acorn
+					elseif IsItemInRange(63427, uId) then range = 6--Worgsaw
+					elseif IsItemInRange(34368, uId) then range = 8--Attuned Crystal Cores
 					elseif CheckInteractDistance(uId, 3) then range = 10
 					elseif CheckInteractDistance(uId, 2) then range = 11
-					elseif IsItemInRange(6450, uId) then range = 18
+					elseif IsItemInRange(6450, uId) then range = 18--Bandages. (despite popular sites saying it's 15 yards, it's actually 18 yards erified even by UnitDistanceSquared
 					elseif CheckInteractDistance(uId, 1) then range = 30
 					elseif UnitInRange(uId) then range = 43
 					else range = 1000 end--Just so it has a numeric value, even if it's unknown to protect from nil errors
@@ -686,6 +702,8 @@ local getDistanceBetween
 do
 	local function itsBCAgain(uId)
 		if IsItemInRange(37727, uId) then return 5
+		elseif IsItemInRange(63427, uId) then return 6
+		elseif IsItemInRange(34368, uId) then return 8
 		elseif CheckInteractDistance(uId, 3) then return 10
 		elseif CheckInteractDistance(uId, 2) then return 11
 		elseif IsItemInRange(6450, uId) then return 18
@@ -753,6 +771,10 @@ function rangeCheck:Show(range, filter, forceshow, redCircleNumPlayers, reverse,
 		if restrictionsActive then
 			if range <= 5 then
 				range = 5
+			elseif range <= 6 then
+				range = 6
+			elseif range <= 8 then
+				range = 8
 			elseif range <= 10 then
 				range = 10
 			elseif range <= 11 then
