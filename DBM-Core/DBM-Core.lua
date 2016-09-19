a846a0a9fd07b4390e1c1e7d4bd52caac06c2e42
Adam <MysticalOS@users.noreply.github.com>
2016-09-19 03:09:55 +0000

    if TomTom is not installed, support /way with dbm arrow. Note this is still not intended as a full replacement for TomTom. just a simpler way to invoke a single use arrow. For waypoint queuing of multiple waypoints and the like use TomTom. this just ensures the dbm arrow gets SOME use post 7.1. It shouldn’t rot. :\

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -1750,6 +1750,34 @@ do
 	
 	SLASH_DEADLYBOSSMODS1 = "/dbm"
 	SLASH_DEADLYBOSSMODSRPULL1 = "/rpull"
+	if GetAddOnEnableState(playerName, "TomTom") == 0 then
+		SLASH_DEADLYBOSSMODSWAY1 = "/way"--/way not used because DBM would load before TomTom and can't check 
+		SlashCmdList["DEADLYBOSSMODSWAY"] = function(msg)
+			if DBM:HasMapRestrictions() then
+				DBM:AddMsg(DBM_CORE_NO_ARROW)
+				return
+			end
+			local x, y = string.split(" ", msg:sub(1):trim())
+			local xNum, yNum = tonumber(x or ""), tonumber(y or "")
+			local success
+			if xNum and yNum then
+				DBM.Arrow:ShowRunTo(xNum, yNum, 0)
+				success = true
+			else--Check if they used , instead of space.
+				x, y = string.split(",", msg:sub(1):trim())
+				xNum, yNum = tonumber(x or ""), tonumber(y or "")
+				if xNum and yNum then
+					DBM.Arrow:ShowRunTo(xNum, yNum, 0)
+					success = true
+				end
+			end
+			if not success then
+				DBM:AddMsg(DBM_ARROW_WAY_USAGE)
+			else
+				DBM:AddMsg(DBM_ARROW_WAY_SUCCESS)
+			end
+		end
+	end
 	if not BigWigs then
 		--Register pull and break slash commands for BW converts, if BW isn't loaded
 		--This shouldn't raise an issue since BW SHOULD load before DBM in any case they are both present.
