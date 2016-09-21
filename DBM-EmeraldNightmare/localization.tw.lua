acda54a480685ffc824f9110690aeaa572121b94
Adam <MysticalOS@users.noreply.github.com>
2016-09-21 22:43:22 +0000

    Fix that error for real

--- DBM-EmeraldNightmare/localization.tw.lua
+++ DBM-EmeraldNightmare/localization.tw.lua
@@ -34,7 +34,7 @@ L= DBM:GetModLocalization(1744)
 L= DBM:GetModLocalization(1667)
 
 L:SetMiscLocalization({
-	SoakersText		=	"分傷分配: %"
+	SoakersText		=	"分傷分配: %s"
 })
 
 ---------------------------
