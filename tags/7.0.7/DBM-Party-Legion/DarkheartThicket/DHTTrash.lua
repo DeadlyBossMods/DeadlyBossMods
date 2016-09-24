ff8bbd913e5c4731ee57285a6ad085743e4c7af3
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-09-24 09:58:40 +0000

    tag as 7.0.7

---
+++ tags/7.0.7/DBM-Party-Legion/DarkheartThicket/DHTTrash.lua
@@ -0,0 +1,30 @@
+local mod	= DBM:NewMod("DHTTrash", "DBM-Party-Legion", 2, 762)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+--mod:SetModelID(47785)
+mod:SetZone()
+
+mod.isTrashMod = true
+
+mod:RegisterEvents(
+	"SPELL_CAST_START 198379"
+)
+
+local specWarnPrimalRampage			= mod:NewSpecialWarningDodge(198379, "Melee", nil, nil, 1, 2)
+
+local voicePrimalRampage			= mod:NewVoice(198379, "Melee")--chargemove
+
+mod:RemoveOption("HealthFrame")
+
+function mod:SPELL_CAST_START(args)
+	if not self.Options.Enabled then return end
+	local spellId = args.spellId
+	if spellId == 198379 then
+		specWarnPrimalRampage:Show()
+		voicePrimalRampage:Play("chargemove")
+--[[	elseif spellId == 195046 and self:CheckInterruptFilter(args.sourceGUID) then
+		specWarnStorm:Show(args.sourceName)
+		specWarnRejuvWaters:Play("kickcast")--]]
+	end
+end
