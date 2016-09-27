db697b1656ef7e2bd2958bef5084fec89a0eb391
Adam <MysticalOS@users.noreply.github.com>
2016-09-27 09:35:45 +0000

    Add new voice to cenarius Add more Ilgynoth timers

--- DBM-EmeraldNightmare/Ilgynoth.lua
+++ DBM-EmeraldNightmare/Ilgynoth.lua
@@ -115,11 +115,11 @@ mod.vb.CorruptorSpawn = 0
 local UnitExists, UnitGUID, UnitDetailedThreatSituation = UnitExists, UnitGUID, UnitDetailedThreatSituation
 local eyeName = EJ_GetSectionInfo(13185)
 local addsTable = {}
-local phase1Deathglares = {26, 69}--Probably 25, 70 but numbers actually detectable by combat log are used.
-local phase1Corruptors = {90}
+local phase1Deathglares = {26, 69, 85, 55}
+local phase1Corruptors = {90, 95, 35}
 
 local phase2Deathglares = {21.5, 95, 130}
-local phase2Corruptors = {45, 95, 35}
+local phase2Corruptors = {45, 95, 35, 85, 40}
 
 local updateInfoFrame, sortInfoFrame
 do
