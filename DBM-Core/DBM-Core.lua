1572296736d854d0ba4e8902f3d5ce7c35bb81bc
Adam <MysticalOS@users.noreply.github.com>
2016-09-28 08:15:48 +0000

    Bump packager

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -420,7 +420,7 @@ local dbmToc = 0
 local isTalkingHeadLoaded = false
 local talkingHeadUnregistered = false
 
-local fakeBWVersion, fakeBWHash = 12, "97ac161"
+local fakeBWVersion, fakeBWHash = 12, "3f0df6d"
 local versionQueryString, versionResponseString = "Q:%d-%s", "V:%d-%s"
 
 local enableIcons = true -- set to false when a raid leader or a promoted player has a newer version of DBM
