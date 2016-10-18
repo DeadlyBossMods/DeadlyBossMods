6ae5284982ce6994d3890c9684e3c8620dc54d67
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-10-18 03:28:53 +0000

    Add a couple trash warnings

--- trunk/DBM-Party-Legion/NeltharionsLair/NLTrash.lua
+++ trunk/DBM-Party-Legion/NeltharionsLair/NLTrash.lua
@@ -8,6 +8,7 @@ mod:SetZone()
 mod.isTrashMod = true
 
 mod:RegisterEvents(
+	"SPELL_CAST_START 183088",
 	"SPELL_AURA_APPLIED 200154 183407"
 )
 
@@ -15,27 +16,22 @@ local warnBurningHatred			= mod:NewTargetAnnounce(182879, 3)
 
 local specWarnBurningHatred		= mod:NewSpecialWarningYou(200154, nil, nil, nil, 1, 2)
 local specWarnAcidSplatter		= mod:NewSpecialWarningMove(183407, nil, nil, nil, 1, 2)
+local specWarnAvalanche			= mod:NewSpecialWarningDodge(183088, "Tank", nil, nil, 1, 2)
 
 local voiceBurningHatred		= mod:NewVoice(200154)--targetyou
 local voiceAcidSplatter			= mod:NewVoice(183407)--runaway
+local voiceAvalanche			= mod:NewVoice(183088, "Tank")--shockwave
 
 mod:RemoveOption("HealthFrame")
 
---[[
 function mod:SPELL_CAST_START(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
-	if spellId == 209027 then
-		specWarnQuellingStrike:Show()
-		voiceQuellingStrike:Play("shockwave")
-	elseif spellId == 212031 then
-		specWarnChargedBlast:Show()
-		voiceChargedBlast:Play("shockwave")
-	elseif spellId == 209485 and self:CheckInterruptFilter(args.sourceGUID) then
-		specWarnDrainMagic:Show(args.sourceName)
-		voiceDrainMagic:Play("kickcast")
+	if spellId == 183088 then
+		specWarnAvalanche:Show()
+		voiceAvalanche:Play("shockwave")
 	end
-end--]]
+end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if not self.Options.Enabled then return end
