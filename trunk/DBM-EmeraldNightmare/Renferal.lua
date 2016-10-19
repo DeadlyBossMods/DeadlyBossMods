cc685f2240c1fb9a553536e3f8b407c71f66edfb
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-10-19 05:00:19 +0000

    Sync important timers/dragon rotations on nightmare dragons so people dealing with lumbering have them when they return Disable breath timers for shades Added interrupt warning for collapsing nightmare Added respawn time. Disable shadow burst yells by default
    Removed volatile winds taunt warning from renferal entirely.

--- trunk/DBM-EmeraldNightmare/Renferal.lua
+++ trunk/DBM-EmeraldNightmare/Renferal.lua
@@ -56,7 +56,6 @@ local specWarnRakingTalonOther		= mod:NewSpecialWarningTaunt(215582, nil, nil, n
 ----Mythic
 local specViolentWinds				= mod:NewSpecialWarningYou(218124, nil, nil, nil, 3, 2)
 local yellViolentWinds				= mod:NewYell(218124)
-local specWarnViolentWindsOther		= mod:NewSpecialWarningTaunt(218124, nil, nil, nil, 1, 2)
 
 --Spider Form
 mod:AddTimerLine(GetSpellInfo(210326))
@@ -259,9 +258,6 @@ function mod:SPELL_AURA_APPLIED(args)
 				voiceViolentWinds:Play("justrun")
 				voiceViolentWinds:Schedule(1, "keepmove")
 				yellViolentWinds:Yell()
-			elseif self.Options.SpecWarn218124taunt then
-				specWarnViolentWindsOther:Show(args.destName)
-				voiceViolentWinds:Play("tauntboss")
 			else
 				warnViolentWinds:Show(args.destName)
 			end
