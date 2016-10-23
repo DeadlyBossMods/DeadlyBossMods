e266c1e556e1f8f826d3a2c95a735f243369e027
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-10-23 02:08:09 +0000

    Remove print Change cenarius to work in normal too. while some of us consider it trivial, others do not.

--- trunk/DBM-EmeraldNightmare/NightmareDragons.lua
+++ trunk/DBM-EmeraldNightmare/NightmareDragons.lua
@@ -283,9 +283,7 @@ function mod:OnCombatStart(delay)
 	end
 	if self.Options.InfoFrame and not self:IsLFR() then
 		DBM.InfoFrame:SetHeader(EJ_GetSectionInfo(12809))
-		--DBM.InfoFrame:Show(5, "function", updateInfoFrame, sortInfoFrame, true)
 		DBM.InfoFrame:Show(5, "function", updateInfoFrame)
-		DBM:AddMsg("If you experience a freeze early in fight, disable InfoFrame option in botton of Nightmare Dragons options. This MAY be fixed but fix is unverified")
 	end
 end
 
