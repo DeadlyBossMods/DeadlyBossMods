14a372c0f61f2079ecbf19e354d7470cba51fed4
ultrashining <ultrashining@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-10-25 01:36:36 +0000

    zhTW update

--- DBM-TrialofValor/localization.tw.lua
+++ DBM-TrialofValor/localization.tw.lua
@@ -27,3 +27,16 @@ L= DBM:GetModLocalization(1830)
 -- Helya --
 ---------------------------
 L= DBM:GetModLocalization(1829)
+
+L:SetMiscLocalization({
+	phaseThree =	"Your efforts are for naught, mortals! Odyn will NEVER be free!"
+})
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("TrialofValorTrash")
+
+L:SetGeneralLocalization({
+	name =	"勇氣試煉小怪"
+})
