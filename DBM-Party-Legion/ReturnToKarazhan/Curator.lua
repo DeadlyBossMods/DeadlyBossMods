19bf9dcbaf651835575437f23fb9dc80aacf0ba8
Adam <MysticalOS@users.noreply.github.com>
2016-10-25 04:11:01 +0000

    Push rest of kara

--- DBM-Party-Legion/ReturnToKarazhan/Curator.lua
+++ DBM-Party-Legion/ReturnToKarazhan/Curator.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(1836, "DBM-Party-Legion", 11, 860)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(100497)
+mod:SetCreatureID(114462)
 mod:SetEncounterID(1964)
 mod:SetZone()
 --mod:SetUsedIcons(1)
@@ -11,78 +11,76 @@ mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS",
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED"
+	"SPELL_CAST_SUCCESS 227267",
+	"SPELL_AURA_APPLIED 227254",
+	"SPELL_AURA_REMOVED 227254",
+	"SPELL_PERIODIC_DAMAGE 227465",
+	"SPELL_PERIODIC_MISSED 227465"
 )
 
-local warnBloodFrenzy				= mod:NewSpellAnnounce(198388, 4)
+--TODO, figure out which one is cast ID for discharge and whether it needs a timer/announce or not
+--TODO, assess other needs
+local warnAdds						= mod:NewSpellAnnounce(227267, 2)--if not cast too often make special warning?
+local warnEvo						= mod:NewSpellAnnounce(227254, 1)
+local warnEvoOver					= mod:NewEndAnnounce(227254, 2)
 
-local specWarnFocusedGaze			= mod:NewSpecialWarningYou(198006, nil, nil, nil, 1, 2)
-local yellFocusedGaze				= mod:NewPosYell(198006)
+local specWarnPowerDischarge		= mod:NewSpecialWarningMove(227465, nil, nil, nil, 1, 2)
 
-local timerFocusedGazeCD			= mod:NewNextCountTimer(40, 198006, nil, nil, nil, 3)
+local timerSummonAddCD				= mod:NewAITimer(40, 227267, nil, nil, nil, 1)
+local timerEvoCD					= mod:NewAITimer(40, 227254, nil, nil, nil, 6)
+local timerEvo						= mod:NewBuffActiveTimer(20, 227254, nil, nil, nil, 6)
 
-local berserkTimer					= mod:NewBerserkTimer(300)
+--local berserkTimer					= mod:NewBerserkTimer(300)
 
-local countdownFocusedGazeCD		= mod:NewCountdown(40, 198006)
+--local countdownFocusedGazeCD		= mod:NewCountdown(40, 198006)
 
-local voiceFocusedGaze				= mod:NewVoice(198006, "-Tank")--targetyou/share
-
---mod:AddSetIconOption("SetIconOnCharge", 198006, true)
---mod:AddInfoFrameOption(198108, false)
+local voicePowerDischarge			= mod:NewVoice(227465)--runaway
 
 function mod:OnCombatStart(delay)
-
+	timerSummonAddCD:Start(1-delay)
+	timerEvoCD:Start(1-delay)
 end
 
 function mod:OnCombatEnd()
---	if self.Options.InfoFrame then
---		DBM.InfoFrame:Hide()
---	end
-end
 
-function mod:SPELL_CAST_START(args)
-	local spellId = args.spellId
-	if spellId == 197942 then
-
-	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 197943 then
-
+	if spellId == 227267 then
+		warnAdds:Show()
+		timerSummonAddCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 198006 then
-
+	if spellId == 227254 then
+		timerSummonAddCD:Stop()
+		warnEvo:Show()
+		timerEvo:Start()
 	end
 end
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 198006 then
-
+	if spellId == 227254 then
+		warnEvoOver:Show()
+		timerSummonAddCD:Start(2)
+		timerEvoCD:Start()
 	end
 end
 
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 205611 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
---		specWarnMiasma:Show()
---		voiceMiasma:Play("runaway")
+	if spellId == 227465 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
+		specWarnPowerDischarge:Show()
+		voicePowerDischarge:Play("runaway")
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
+--[[
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 103695 then
--- DBM-Party-Legion/ReturnToKarazhan/ManaDevourer.lua
+++ DBM-Party-Legion/ReturnToKarazhan/ManaDevourer.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(1818, "DBM-Party-Legion", 11, 860)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(100497)
+--mod:SetCreatureID(100497)--Has 3 CIDs, figure out which one before enable
 mod:SetEncounterID(1959)
 mod:SetZone()
 --mod:SetUsedIcons(1)
@@ -11,67 +11,78 @@ mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS",
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED"
+	"SPELL_CAST_START 227507",
+	"SPELL_CAST_SUCCESS 227618",
+	"SPELL_AURA_APPLIED 227297"
+--	"SPELL_AURA_REMOVED",
+--	"SPELL_PERIODIC_DAMAGE",
+--	"SPELL_PERIODIC_MISSED"
 )
 
-local warnBloodFrenzy				= mod:NewSpellAnnounce(198388, 4)
+--TODO: Energy Discharge warning/timer?
+--TODO, arcane bomb and void energy timers/warnings?
+local warnArcaneBomb				= mod:NewSpellAnnounce(227618, 4)
 
-local specWarnFocusedGaze			= mod:NewSpecialWarningYou(198006, nil, nil, nil, 1, 2)
-local yellFocusedGaze				= mod:NewPosYell(198006)
+local specWarnDecimatingEssence		= mod:NewSpecialWarningSpell(227507, nil, nil, nil, 3, 2)
+local specWarnCoalescePower			= mod:NewSpecialWarningMoveTo(227297, nil, nil, nil, 1, 2)
+--local yellFocusedGaze				= mod:NewPosYell(198006)
 
-local timerFocusedGazeCD			= mod:NewNextCountTimer(40, 198006, nil, nil, nil, 3)
+local timerCoalescePowerCD			= mod:NewAITimer(40, 227297, nil, nil, nil, 1)
 
-local berserkTimer					= mod:NewBerserkTimer(300)
+--local berserkTimer				= mod:NewBerserkTimer(300)
 
-local countdownFocusedGazeCD		= mod:NewCountdown(40, 198006)
+--local countdownFocusedGazeCD		= mod:NewCountdown(40, 198006)
 
-local voiceFocusedGaze				= mod:NewVoice(198006, "-Tank")--targetyou/share
+local voiceDecimatingEssence		= mod:NewVoice(227507)--aesoon (is there a voice file for "you fucked up and are going to die"?)
+local voiceColaescePower			= mod:NewVoice(227297)--helpsoak
 
 --mod:AddSetIconOption("SetIconOnCharge", 198006, true)
---mod:AddInfoFrameOption(198108, false)
+mod:AddInfoFrameOption(227502, true)
 
 function mod:OnCombatStart(delay)
-
+	timerCoalescePowerCD:Start(1-delay)
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:SetHeader(GetSpellInfo(227502))
+		DBM.InfoFrame:Show(8, "playerdebuffstacks", 227502)
+	end
 end
 
 function mod:OnCombatEnd()
---	if self.Options.InfoFrame then
---		DBM.InfoFrame:Hide()
---	end
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:Hide()
+	end
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 197942 then
-
+	if spellId == 227507 then
+		specWarnDecimatingEssence:Show()
+		voiceDecimatingEssence:Play("aesoon")
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 197943 then
-
+	if spellId == 227618 then
+		warnArcaneBomb:Show()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 198006 then
-
+	if spellId == 227297 then
+		specWarnCoalescePower:Show(GetSpellInfo(227296))
+		voiceColaescePower:Play("helpsoak")
+		timerCoalescePowerCD:Start()
 	end
 end
 
+--[[
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 198006 then
-
+	if spellId == 227297 then
+		
 	end
 end
 
