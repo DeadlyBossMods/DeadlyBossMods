ea27cfc47d77bf6393d8b17bb2222e4363e22421
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-11-10 00:44:03 +0000

    Oh guess I should commit this. got distracted by twitters Odyn update CN update

---
+++ trunk/DBM-TrialofValor/localization.cn.lua
@@ -0,0 +1,44 @@
+-- Mini Dragon(projecteurs@gmail.com)
+-- Last update: Nov 1 2016, 6:03 UTC@15435
+
+if GetLocale() ~= "zhCN" then return end
+local L
+---------------
+-- Odyn --
+---------------
+L= DBM:GetModLocalization(1819)
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+---------------------------
+-- Guarm --
+---------------------------
+L= DBM:GetModLocalization(1830)
+
+---------------------------
+-- Helya --
+---------------------------
+L= DBM:GetModLocalization(1829)
+
+L:SetMiscLocalization({
+	phaseThree =	"Your efforts are for naught, mortals! Odyn will NEVER be free!" --pending
+})
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("TrialofValorTrash")
+
+L:SetGeneralLocalization({
+	name =	"勇气的试炼小怪"
+})
