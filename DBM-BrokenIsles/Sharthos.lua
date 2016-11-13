775e57d98b459e66fea745286d8342661c17e1fb
Adam <MysticalOS@users.noreply.github.com>
2016-11-13 01:26:26 +0000

    Sharthos update

--- DBM-BrokenIsles/Sharthos.lua
+++ DBM-BrokenIsles/Sharthos.lua
@@ -11,29 +11,30 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 215821 216044",
-	"SPELL_AURA_APPLIED",
+	"SPELL_CAST_START 216044",
+	"SPELL_CAST_SUCCESS 215821",
 	"SPELL_PERIODIC_DAMAGE 215876",
 	"SPELL_PERIODIC_MISSED 215876"
 )
 
 local specWarnBreath			= mod:NewSpecialWarningSpell(215821, "Tank", nil, nil, 1, 2)
-local specWarnBurningEarth		= mod:NewSpecialWarningMove(215876)
-local specWarnFear				= mod:NewSpecialWarningSpell(216044, nil, nil, nil, 2)
+local specWarnBurningEarth		= mod:NewSpecialWarningMove(215876, nil, nil, nil, 1, 2)
+local specWarnFear				= mod:NewSpecialWarningSpell(216044, nil, nil, nil, 2, 2)
 
-local timerBreathCD				= mod:NewAITimer(16, 215821, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)
-local timerFearCD				= mod:NewAITimer(90, 216044, nil, nil, nil, 1)
+local timerBreathCD				= mod:NewCDTimer(19.8, 215821, nil, "Tank", 2, 5, nil, DBM_CORE_TANK_ICON)--19.8-23
+local timerFearCD				= mod:NewCDTimer(63, 216044, nil, nil, nil, 1)--63-65
 
 local voiceBreath				= mod:NewVoice(215821, "Tank")--breathsoon
 local voiceBurningEarth			= mod:NewVoice(215876)--runaway
---local voiceFear				= mod:NewVoice(216044)--How is there no voice for fear? Could not find one, maybe add something simple. "fearincoming"
+local voiceFear					= mod:NewVoice(216044)
 
 --mod:AddReadyCheckOption(37460, false)
 mod:AddRangeFrameOption(5, 216043)--Range unknown, journal nor spell tooltip say.
 
 function mod:OnCombatStart(delay, yellTriggered)
 	if yellTriggered then
-
+		timerBreathCD:Start(16-delay)
+		timerFearCD:Start(60-delay)
 	end
 	if self.Options.RangeFrame then
 		DBM.RangeCheck:Show(5)
@@ -54,7 +55,17 @@ function mod:SPELL_CAST_START(args)
 		timerBreathCD:Start()
 	elseif spellId == 216044 then
 		specWarnFear:Show()
+		voiceFear:Play("fearsoon")
 		timerFearCD:Start()
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+	if spellId == 215821 then
+		specWarnBreath:Show()
+		voiceBreath:Play("breathsoon")
+		timerBreathCD:Start()
 	end
 end
 
