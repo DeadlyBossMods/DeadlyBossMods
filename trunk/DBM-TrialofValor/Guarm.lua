a20ec41ce2bb7578cd0244bb4794669469f6127f
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2016-11-16 20:42:36 +0000

    #Guarm Update *Fix one of foam warnings not working on mythic *Fix mythic berserk *Added info frame for foams *Added icon marking for foams *Added audio count down for breaths and fangs

--- trunk/DBM-TrialofValor/Guarm.lua
+++ trunk/DBM-TrialofValor/Guarm.lua
@@ -5,7 +5,7 @@ mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(114323)
 mod:SetEncounterID(1962)
 mod:SetZone()
---mod:SetUsedIcons(1)
+mod:SetUsedIcons(1, 2, 3)
 mod:SetHotfixNoticeRev(15448)
 --mod.respawnTime = 30
 
@@ -14,7 +14,8 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 227514",
 	"SPELL_CAST_SUCCESS 227883 227816",
-	"SPELL_AURA_APPLIED 228818 228810 228818 232173 228228 228253 228248",
+	"SPELL_AURA_APPLIED 228744 228810 228818 232173 228228 228253 228248",
+	"SPELL_AURA_REMOVED 228744 228810 228818",
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
@@ -49,20 +50,45 @@ local timerChargeCD					= mod:NewCDTimer(10.9, 227816, nil, nil, nil, 3)
 
 local berserkTimer					= mod:NewBerserkTimer(300)
 
---local countdownFocusedGazeCD		= mod:NewCountdown(40, 198006)
+local countdownBreath				= mod:NewCountdown(20.5, 228187)
+local countdownFangs				= mod:NewCountdown("Alt20", 227514, "Tank")
 
 local voiceBreath					= mod:NewVoice(228187)--breathsoon
 local voiceCharge					= mod:NewVoice(227816)--chargemove
 local voiceFlameLick				= mod:NewVoice(228228)--runout
 local voiceFrostLick				= mod:NewVoice(228248)--helpdispel
 
---mod:AddSetIconOption("SetIconOnCharge", 198006, true)
---mod:AddInfoFrameOption(198108, true)
+mod:AddSetIconOption("SetIconOnFoam", "ej14535", true)
+mod:AddInfoFrameOption("ej14535", true)
 mod:AddRangeFrameOption(5, "ej14463")
 
 mod.vb.fangCast = 0
 mod.vb.breathCast = 0
 mod.vb.leapCast = 0
+--Ugly way to do it, vs a local table, but this ensures that if icon setter disconnects, it doesn't get messed up
+mod.vb.one = false
+mod.vb.two = false
+mod.vb.three = false
+
+local updateInfoFrame
+do
+	local lines = {}
+	local fireDebuff, frostDebuff, shadowDebuff = GetSpellInfo(228744), GetSpellInfo(228818), GetSpellInfo(228818)
+	local UnitDebuff = UnitDebuff
+	updateInfoFrame = function()
+		table.wipe(lines)
+		for uId in DBM:GetGroupMembers() do
+			if UnitDebuff(uId, fireDebuff) then
+				lines[UnitName(uId)] = fireDebuff
+			elseif UnitDebuff(uId, frostDebuff) then
+				lines[UnitName(uId)] = frostDebuff
+			elseif UnitDebuff(uId, shadowDebuff) then
+				lines[UnitName(uId)] = shadowDebuff
+			end
+		end
+		return lines
+	end
+end
 
 function mod:OnCombatStart(delay)
 	self.vb.fangCast = 0
@@ -70,7 +96,18 @@ function mod:OnCombatStart(delay)
 	self.vb.leapCast = 0
 	--All other combat start timers started by Helyatosis
 	if not self:IsLFR() then
-		berserkTimer:Start(-delay)
+		if self:IsMythic() then
+			self.vb.one = false
+			self.vb.two = false
+			self.vb.three = false
+			berserkTimer:Start(240-delay)
+			if self.Options.InfoFrame then
+				DBM.InfoFrame:SetHeader(EJ_GetSectionInfo(14535))
+				DBM.InfoFrame:Show(5, "function", updateInfoFrame, false, true)
+			end
+		else
+			berserkTimer:Start(-delay)
+		end
 		if self.Options.RangeFrame then
 			DBM.RangeCheck:Show(5)
 		end
@@ -81,6 +118,9 @@ function mod:OnCombatEnd()
 	if self.Options.RangeFrame then
 		DBM.RangeCheck:Hide()
 	end
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:Hide()
+	end
 end
 
 function mod:SPELL_CAST_START(args)
@@ -90,6 +130,7 @@ function mod:SPELL_CAST_START(args)
 		warnFangs:Show(self.vb.fangCast)
 		if self.vb.fangCast == 1 then
 			timerFangsCD:Start(nil, 2)
+			countdownFangs:Start(20.5)
 		end
 	end
 end
@@ -110,17 +151,31 @@ end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 228744 then
-		if args:IsPlayer() then
-			specWarnFlamingFoam:Show()
+	if spellId == 228744 or spellId == 228810 or spellId == 228818 then
+		if spellId == 228744 then
+			if args:IsPlayer() then
+				specWarnFlamingFoam:Show()
+			end
+		elseif spellId == 228810 then
+			if args:IsPlayer() then
+				specWarnBrineyFoam:Show()
+			end
+		elseif spellId == 228818 then
+			if args:IsPlayer() then
+				specWarnShadowyFoam:Show()
+			end
 		end
-	elseif spellId == 228810 then
-		if args:IsPlayer() then
-			specWarnBrineyFoam:Show()
-		end
-	elseif spellId == 228818 then
-		if args:IsPlayer() then
-			specWarnShadowyFoam:Show()
+		if self.Options.SetIconOnFoam then
+			if not self.vb.one then
+				self.vb.one = true
+				self:SetIcon(args.destName, 1)
+			elseif not self.vb.two then
+				self.vb.two = true
+				self:SetIcon(args.destName, 2)
+			elseif not self.vb.three then
+				self.vb.three = true
+				self:SetIcon(args.destName, 3)
+			end
 		end
 	elseif spellId == 232173 then--Berserk
 		timerLeapCD:Stop()
@@ -152,6 +207,24 @@ function mod:SPELL_AURA_APPLIED(args)
 	end
 end
 
+function mod:SPELL_AURA_REMOVED(args)
+	local spellId = args.spellId
+	if spellId == 228744 or spellId == 228810 or spellId == 228818 then
+		local uId = DBM:GetRaidUnitId(args.destName)
+		local currentIcon = GetRaidTargetIndex(uId)
+		if self.Options.SetIconOnFoam then
+			if currentIcon == 1 then
+				self.vb.one = false
+			elseif currentIcon == 2 then
+				self.vb.two = false
+			elseif currentIcon == 3 then
+				self.vb.three = false
+			end
+			self:SetIcon(args.destName, 0)
+		end
+	end
+end
+
 --[[
 Might be inversed depending on perspective.
 227658: Fiery left, Salty middle, Dark right
@@ -171,6 +244,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, spellGUID)
 		voiceBreath:Play("breathsoon")
 		if self.vb.breathCast == 1 then
 			timerBreathCD:Start(nil, 2)
+			countdownBreath:Start()
 		end
 	elseif spellId == 228201 then--Off the Leash
 		self.vb.leapCast = 0
@@ -182,11 +256,14 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, spellGUID)
 		self.vb.fangCast = 0
 		self.vb.breathCast = 0
 		timerFangsCD:Start(4, 1)
+		countdownFangs:Start(4)
 		timerLeashCD:Start()--45
 		if self:IsEasy() then
 			timerBreathCD:Start(11, 1)
+			countdownBreath:Start(11)
 		else
 			timerBreathCD:Start(14, 1)
+			countdownBreath:Start(14)
 		end
 	end
 end
