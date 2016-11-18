f2675ef4f715254122c7e993d949faf5b4cd174b
Adam <MysticalOS@users.noreply.github.com>
2016-11-18 08:30:38 +0000

    More tentacle refactoring, as well as some terrible google translations that are probably wrong. localizers please verify and update helya translations

--- DBM-TrialofValor/localization.ru.lua
+++ DBM-TrialofValor/localization.ru.lua
@@ -1,23 +1,11 @@
-﻿if GetLocale() ~= "zhTW" then return end
+if GetLocale() ~= "ruRU" then return end
+
 local L
 
 ---------------
 -- Odyn --
 ---------------
 L= DBM:GetModLocalization(1819)
-
-L:SetWarningLocalization({
-})
-
-L:SetTimerLocalization({
-})
-
-L:SetOptionLocalization({
-})
-
-L:SetMiscLocalization({
-})
-
 ---------------------------
 -- Guarm --
 ---------------------------
@@ -30,9 +18,9 @@ L= DBM:GetModLocalization(1829)
 
 L:SetMiscLocalization({
 	phaseThree =	"Your efforts are for naught, mortals! Odyn will NEVER be free!",
-	near =			"近",
-	far =			"遠",
-	multiple =		"多"
+	near =			"возле",
+	far =			"далеко",
+	multiple =		"множественный"
 })
 
 -------------
@@ -41,5 +29,5 @@ L:SetMiscLocalization({
 L = DBM:GetModLocalization("TrialofValorTrash")
 
 L:SetGeneralLocalization({
-	name =	"勇氣試煉小怪"
+	name =	"Trial of Valor Trash"
 })
