9e074bc0d2d9248595b38f44f5b055c3c9227892
Adam <MysticalOS@users.noreply.github.com>
2016-11-18 06:47:51 +0000

    Add range frame rules for phase 3 mythic odyn Add yells for branded to match BW Added more trash warnings to Trials of Valor Minor timer tweaks to guarm and helya

--- DBM-TrialofValor/TrialofValorTrash.lua
+++ DBM-TrialofValor/TrialofValorTrash.lua
@@ -7,12 +7,16 @@ mod:SetZone()
 mod.isTrashMod = true
 
 mod:RegisterEvents(
-	"SPELL_AURA_APPLIED 228845"
+	"SPELL_AURA_APPLIED 228845 228371 228395"
 )
 
 local specWarnShatterboneShield		= mod:NewSpecialWarningReflect(228845, nil, nil, nil, 1, 2)
+local specWarnBreathOfDread			= mod:NewSpecialWarningMove(228371, nil, nil, nil, 1, 2)
+local specWarnBindSpirit			= mod:NewSpecialWarningDispel(228395, "MagicDispeller", nil, nil, 1, 2)
 
 local voiceShatterboneShield		= mod:NewVoice(228845)--stopattack
+local voiceBreathOfDread			= mod:NewVoice(228371)--runaway
+local voiceBindSpirit				= mod:NewVoice(228395, "MagicDispeller")--dispelnow
 
 mod:RemoveOption("HealthFrame")
 
@@ -22,5 +26,11 @@ function mod:SPELL_AURA_APPLIED(args)
 	if spellId == 228845 and self:AntiSpam(3, 1) then
 		specWarnShatterboneShield:Show(args.destName)
 		voiceShatterboneShield:Play("stopattack")
+	elseif spellId == 228371 and args:IsPlayer() and self:AntiSpam(2.5, 1) then
+		specWarnBreathOfDread:Show(args.destName)
+		voiceBreathOfDread:Play("runaway")
+	elseif spellId == 228395 and self:AntiSpam(2.5, 2) then
+		specWarnBindSpirit:Show(args.destName)
+		voiceBindSpirit:Play("dispelnow")
 	end
 end
