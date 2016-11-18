85b5623beec39c60c6c727e3183dea1aef2ef8e8
Adam <MysticalOS@users.noreply.github.com>
2016-11-18 08:03:18 +0000

    Only special warn once, but generic announce all locations with regular warning and show cast bars with locations

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -10228,6 +10228,17 @@ do
 		end
 		return newTimer(self, "cast", timer, ...)
 	end
+	
+	function bossModPrototype:NewCastSourceTimer(timer, ...)
+		if tonumber(timer) and timer > 1000 then -- hehe :) best hack in DBM. This makes the first argument optional, so we can omit it to use the cast time from the spell id ;)
+			local spellId = timer
+			timer = select(4, GetSpellInfo(spellId)) or 1000 -- GetSpellInfo takes YOUR spell haste into account...WTF?
+			local spellHaste = select(4, GetSpellInfo(53142)) / 10000 -- 53142 = Dalaran Portal, should have 10000 ms cast time
+			timer = timer / spellHaste -- calculate the real cast time of the spell...
+			return self:NewCastSourceTimer(timer / 1000, spellId, ...)
+		end
+		return newTimer(self, "castsource", timer, ...)
+	end
 
 	function bossModPrototype:NewCDTimer(...)
 		return newTimer(self, "cd", ...)
