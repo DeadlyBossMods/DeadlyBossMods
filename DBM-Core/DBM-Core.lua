f495a084bc840b079c76ac6e41e6e7ccc8ac7bf2
Adam <MysticalOS@users.noreply.github.com>
2016-12-27 19:55:11 +0000

    Update version check

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -420,7 +420,7 @@ local dbmToc = 0
 local UpdateChestTimer
 local breakTimerStart
 
-local fakeBWVersion, fakeBWHash = 30, "3fd1ebb"
+local fakeBWVersion, fakeBWHash = 31, "df1c3cd"
 local versionQueryString, versionResponseString = "Q^%d^%s", "V^%d^%s"
 
 local enableIcons = true -- set to false when a raid leader or a promoted player has a newer version of DBM
