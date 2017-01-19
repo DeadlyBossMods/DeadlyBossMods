6da536082914ef1b650039e634d80320ff925a32
Adam <MysticalOS@users.noreply.github.com>
2017-01-19 01:23:59 +0000

    Enable HF notice

--- DBM-Nighthold/Elisande.lua
+++ DBM-Nighthold/Elisande.lua
@@ -6,7 +6,7 @@ mod:SetCreatureID(106643)
 mod:SetEncounterID(1872)
 mod:SetZone()
 mod:SetUsedIcons(8, 7, 6, 5, 4, 3, 2, 1)--TODO< figure out debuff count cap
---mod:SetHotfixNoticeRev(12324)
+mod:SetHotfixNoticeRev(15673)
 mod.respawnTime = 30
 
 mod:RegisterCombat("combat")
