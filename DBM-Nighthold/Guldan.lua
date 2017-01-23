7463951c47a022062fe768f5a244ee8326ab0db3
Adam <MysticalOS@users.noreply.github.com>
2017-01-23 05:36:26 +0000

    Fix gul’dan’s creatureID so boss health is returned correctly.

--- DBM-Nighthold/Guldan.lua
+++ DBM-Nighthold/Guldan.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(1737, "DBM-Nighthold", nil, 786)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(105503)--104537 (Fel Lord Kuraz'mal)
+mod:SetCreatureID(104154)--104537 (Fel Lord Kuraz'mal)
 mod:SetEncounterID(1866)
 mod:SetZone()
 mod:SetUsedIcons(1, 2, 3)
