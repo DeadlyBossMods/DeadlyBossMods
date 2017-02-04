a2d2f2ef4a9c5f5afb59f8ba94380f1b11e433dc
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2017-02-04 01:34:28 +0000

    Some totally untested work in progress stuff, BEWARE!

---
+++ trunk/DBM-Core/DBM-Nameplate.lua
@@ -0,0 +1,94 @@
+--VERY WIP and probably not functional yet.
+--The idea here though is a nameplate function that can show large visuals above players with certain buffs/debuffs
+--With support for unique textures/spell tracking per unit so it's not limited to one debuff/texture at a time.
+
+-- globals
+DBM.Nameplate = {}
+-- locals
+local nameplateFrame = DBM.Nameplate
+local units = {}
+local unitspells = {}
+
+--------------------
+--  Create Frame  --
+--------------------
+local DBMNameplateFrame = CreateFrame("Frame", "DBMNameplate", UIParent)
+DBMNameplateFrame:Hide()
+--DBMNameplateFrame:SetBackdrop({bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",})
+--DBMNameplateFrame:SetAllPoints(UIParent)
+--DBMNameplateFrame:SetFrameStrata("BACKGROUND")
+
+function nameplateFrame:Show(unit, spellId, texture)
+	if not DBMNameplateFrame:IsShown() then
+		DBMNameplateFrame:Show()
+		DBMNameplateFrame:RegisterEvent("NAME_PLATE_CREATED")
+		DBMNameplateFrame:RegisterEvent("NAME_PLATE_UNIT_ADDED")
+		DBMNameplateFrame:RegisterEvent("NAME_PLATE_UNIT_REMOVED")
+	end
+	--Support custom texture, or just pull it from spellid
+	units[unit] = texture or GetSpellTexture(spellId)
+	unitspells[unit] = GetSpellInfo(spellId)
+end
+
+function nameplateFrame:Hide(unit, force)
+	units[unit] = nil
+	unitspells[unit] = nil
+	if force or #units == 0 then
+		table.wipe(units)
+		table.wipe(unitspells)
+		DBMNameplateFrame:UnregisterAllEvents()
+		DBMNameplateFrame:Hide()
+	end
+end
+
+function nameplateFrame:IsShown()
+	return DBMNameplateFrame and DBMNameplateFrame:IsShown()
+end
+
+function nameplateFrame:CreateTexture(frame, unit)
+	local dbmtexture = frame:CreateTexture()
+	dbmtexture:SetTexture(units[unit])
+	dbmtexture:SetPoint("BOTTOM", frame, "TOP")
+	dbmtexture:SetHeight(64)
+	dbmtexture:SetWidth(64)
+	dbmtexture:SetVertexColor(1, 0, 0)
+	dbmtexture:SetParent(UIParent)
+	dbmtexture:Hide()
+	frame.DBM.texture = dbmtexture
+end
+
+function nameplateFrame:UpdateAll()
+	for _, frame in pairs(C_NamePlate.GetNamePlates()) do
+		local unit = frame.namePlateUnitToken
+		nameplateFrame:UpdateUnit(frame, unit)
+	end
+end
+
+function nameplateFrame:UpdateUnit(frame, unit)
+	if units[unit] then
+		if not frame.DBM.texture then
+			nameplateFrame:CreateTexture(frame, unit)
+		end
+		if UnitDebuff(unit, unitspells[unit]) or UnitBuff(unit, unitspells[unit]) then--Debuff/Buff still present
+			frame.DBM.texture:Show()
+		else
+			frame.DBM.texture:Hide()
+			nameplateFrame:Hide(unit)
+		end
+	end	
+end
+
+function NAME_PLATE_CREATED(frame, ...)
+	local unit = frame.namePlateUnitToken
+	nameplateFrame:UpdateUnit(frame, unit)
+end
+
+function NAME_PLATE_UNIT_ADDED(unit)
+	local frame = C_NamePlate.GetNamePlateForUnit(unit)
+	nameplateFrame:UpdateUnit(frame, unit)
+end
+
+function NAME_PLATE_UNIT_REMOVED(unit)
+	local frame = C_NamePlate.GetNamePlateForUnit(unit)
+	nameplateFrame:UpdateUnit(frame, unit)
+end
