4196f8a0802c7de1e47ce898c678a61f20e5e52c
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2017-02-07 21:48:54 +0000

    Fix trio timer icons

--- trunk/DBM-WorldEvents/Holidays/ApothecaryTrio.lua
+++ trunk/DBM-WorldEvents/Holidays/ApothecaryTrio.lua
@@ -26,9 +26,9 @@ local warnChainReaction			= mod:NewCastAnnounce(68821, 3, nil, "Melee")
 local specWarnPerfumeSpill		= mod:NewSpecialWarningMove(68927)
 local specWarnCologneSpill		= mod:NewSpecialWarningMove(68934)
 
-local timerHummel				= mod:NewTimer(10.5, "HummelActive", 2457, nil, false)
-local timerBaxter				= mod:NewTimer(18.5, "BaxterActive", 2457, nil, false)
-local timerFrye					= mod:NewTimer(26.5, "FryeActive", 2457, nil, false)
+local timerHummel				= mod:NewTimer(10.5, "HummelActive", "Interface\\Icons\\ability_warrior_offensivestance", nil, false)
+local timerBaxter				= mod:NewTimer(18.5, "BaxterActive", "Interface\\Icons\\ability_warrior_offensivestance", nil, false)
+local timerFrye					= mod:NewTimer(26.5, "FryeActive", "Interface\\Icons\\ability_warrior_offensivestance", nil, false)
 mod:AddBoolOption("TrioActiveTimer", true, "timer")
 local timerChainReaction		= mod:NewCastTimer(3, 68821, nil, "Melee")
 
