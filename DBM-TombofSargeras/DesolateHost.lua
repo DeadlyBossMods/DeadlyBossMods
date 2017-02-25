e04156498c2d4545630a147a5369e54c7e4bbead
Adam <MysticalOS@users.noreply.github.com>
2017-02-25 05:43:57 +0000

    Desolate Host update from public logs

--- DBM-TombofSargeras/DesolateHost.lua
+++ DBM-TombofSargeras/DesolateHost.lua
@@ -13,7 +13,7 @@ mod.respawnTime = 40
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 235989 235927 238627 235968 236340 241566 236542 236544",
+	"SPELL_CAST_START 238570 235927 236542 236544",
 	"SPELL_CAST_SUCCES 236449 235933 235907 236138 236131 235969",
 	"SPELL_AURA_APPLIED 236459 235924 238018 236513 236138 236131 235969 236361 239923 236548",
 	"SPELL_AURA_APPLIED_DOSE 236548",
@@ -25,23 +25,28 @@ mod:RegisterEventsInCombat(
 	"UNIT_SPELLCAST_SUCCEEDED boss1 boss2"
 )
 
---TODO, see if syncing is needed. for time being it's assumed the two phases don't share information
---TODO, bone shards needed? Seems lke a basic spammed ability that targets tank
---TODO, collapsing Fissure event/spellid probably wrong. only not script spell id was damage, so probably UNIT_SPELLCAST event/script needed for timer/warning
 --TODO, improve tormenting cries voice
---TODO, evalulate adds warnings/timers, because need to keep unimportant stuff out of clutter
 --TODO, interrupt count/rotation setup? Of course that might be a mess with realm swaps.
 --TODO, new voice "switchphase" (Switch Phase). I suspect it'l be used on a lot of warnings here like wither, tank swaps, etc.
 --TODO, figure out when withered souls casts. right now based on interaction and both having 1 minute duration I highly suspect it always follows withered
 --TODO, what to do with spirit chains?
 --TODO, infoframe add tracker? or infoframe debuff tracker (adds with bone armor count and players with screach count, etc)
+--TODO, 235907 (Collapsing Fissure) missing from combat log
+--TODO, adds timers (not incombat log, robably need videos/scheduling
+--TODO, why is there a huge break in soulbind? is Soul queen taking a fap break?
+--[[
+(ability.id = 238570 or ability.id = 235927 or ability.id = 236542 or ability.id = 236544) and type = "begincast" or
+(ability.id = 235907 or ability.id = 236072 or ability.id = 236507 or ability.id = 235969 or ability.id = 236449 or ability.id =  236138 or ability.id = 236131) and type = "cast" or
+(ability.id = 235924) and type = "applydebuff" or
+ability.id = 236072 and (type = "applybuff" or type = "removebuff")
+(ability.id = 236459 or ability.id = 235969 or ability.id = 236513) and (type = "applydebuff" or type = "removedebuff" or type = "applybuff" or type = "removebuff")
+--]]
 --Corporeal Realm
 local warnSpearofAnguish			= mod:NewTargetAnnounce(235924, 3)
 local warnCollapsingFissure			= mod:NewSpellAnnounce(235907, 3)--Upgrade to special, if needed
 local warnTormentingCries			= mod:NewTargetAnnounce(238018, 3)
 ----Adds
 local warnRupturingSlam				= mod:NewSpellAnnounce(235927, 3)
---local warnGraspingDarkness			= mod:NewSpellAnnounce(235968, 3)
 local warnBonecageArmor				= mod:NewTargetAnnounce(236513, 3)
 --Spirit Realm
 local warnSoulbind					= mod:NewTargetAnnounce(228003, 4)
@@ -56,11 +61,11 @@ local specWarnSpearofAnguish		= mod:NewSpecialWarningYou(235924, nil, nil, nil, 
 local yellSpearofAnguish			= mod:NewFadesYell(235924)
 local specWarnTormentingCries		= mod:NewSpecialWarningYou(238018, nil, nil, nil, 1, 2)
 --Spirit Realm
---local specWarnCrushMind				= mod:NewSpecialWarningInterrupt(236340, "HasInterrupt", nil, nil, 1, 2)
 local specWarnSoulbind				= mod:NewSpecialWarningYou(236459, nil, nil, nil, 3, 2)
 local yellSoulbind					= mod:NewYell(236459)
 local specWarnWither				= mod:NewSpecialWarningYou(236138, nil, nil, nil, 1, 7)
 local specWarnShatteringScream		= mod:NewSpecialWarningMoveTo(235969, nil, DBM_CORE_AUTO_SPEC_WARN_OPTIONS.you:format(235969), nil, 3, 2)
+local specWarnWailingSouls			= mod:NewSpecialWarningCount(236072, nil, nil, nil, 2, 2)
 --The Desolate Host
 local specWarnSunderingDoomGather	= mod:NewSpecialWarningMoveTo(236542, nil, nil, nil, 1, 2)
 local specWarnSunderingDoomRun		= mod:NewSpecialWarningRun(236542, nil, nil, nil, 4, 2)
@@ -68,13 +73,15 @@ local specWarnDoomedSunderingGather	= mod:NewSpecialWarningMoveTo(236544, nil, n
 local specWarnDoomedSunderingRun	= mod:NewSpecialWarningRun(236544, nil, nil, nil, 4, 2)
 
 --Corporeal Realm
-local timerSpearofAnquishCD			= mod:NewAITimer(31, 235924, nil, nil, nil, 3)
-local timerCollapsingFissureCD		= mod:NewAITimer(31, 235907, nil, nil, nil, 3)
-local timerTormentedCriesCD			= mod:NewAITimer(31, 235989, nil, nil, nil, 3)
+local timerSpearofAnquishCD			= mod:NewCDTimer(20, 235924, nil, nil, nil, 3)
+--local timerCollapsingFissureCD		= mod:NewAITimer(31, 235907, nil, nil, nil, 3)
+local timerTormentedCriesCD			= mod:NewCDTimer(31, 238570, nil, nil, nil, 3)
+local timerRupturingSlamCD			= mod:NewCDTimer(23, 235927, nil, nil, nil, 3)--23 seconds, per add
 --Spirit Realm
-local timerSoulbindCD				= mod:NewAITimer(31, 236459, nil, nil, nil, 3)--Should be shown to both phases, cause it picks targets from both
-local timerWitherCD					= mod:NewAITimer(31, 236138, nil, nil, nil, 3)
-local timerShatteringScreamCD		= mod:NewAITimer(31, 235969, nil, nil, nil, 3)
+local timerSoulbindCD				= mod:NewCDCountTimer(24, 236459, nil, nil, nil, 3)
+local timerWitherCD					= mod:NewCDTimer(9.4, 236138, nil, nil, nil, 3)
+local timerShatteringScreamCD		= mod:NewCDTimer(12, 235969, nil, nil, nil, 3)--12 seconds, per add
+local timerWailingSoulsCD			= mod:NewCDCountTimer(60, 236072, nil, nil, nil, 2)
 
 --local berserkTimer				= mod:NewBerserkTimer(300)
 
@@ -84,10 +91,10 @@ local timerShatteringScreamCD		= mod:NewAITimer(31, 235969, nil, nil, nil, 3)
 local voiceSpearofAnguish			= mod:NewVoice(235924)--runout
 local voiceTormentingCries			= mod:NewVoice(238018)--targetyou (another generic until I know what to do with these). Might be a "keepmove" event
 --Spirit Realm
---local voiceCrushMind				= mod:NewVoice(236340, "HasInterrupt")--kickcast
 local voiceSoulbind					= mod:NewVoice(236459)--targetyou (should give at least one of the players "switchphase" but not both of them)
 local voiceWither					= mod:NewVoice(236138)--switchphase
 local voiceShatteringScream			= mod:NewVoice(235969)--getboned (kinda close enough? maybe custom new one later)
+local voiceWailingSouls				= mod:NewVoice(236072)--aesoon
 
 local voiceSunderingDoom			= mod:NewVoice(236542)--gathershare/justrun
 local voiceDoomedSunderin			= mod:NewVoice(236544)--gathershare/justrun
@@ -100,15 +107,20 @@ mod:AddNamePlateOption("NPAuraOnSoulbind", 236459)
 mod:AddNamePlateOption("NPAuraOnBonecageArmor", 236513)
 mod:AddNamePlateOption("NPAuraOnShatteringScream", 235969)
 
+mod.vb.soulboundCast = 0
+mod.vb.wailingSoulsCast = 0
 local spiritRealm = GetSpellInfo(235621)
 local boneArmor = GetSpellInfo(236513)
 
 function mod:OnCombatStart(delay)
-	timerSoulbindCD:Start(1-delay)
-	timerSpearofAnquishCD:Start(1-delay)
-	timerTormentedCriesCD:Start(1-delay)
-	timerCollapsingFissureCD:Start(1-delay)
-	timerWitherCD:Start(1-delay)
+	self.vb.soulboundCast = 0
+	self.vb.wailingSoulsCast = 0
+	timerSoulbindCD:Start(14.2-delay, 1)
+	timerSpearofAnquishCD:Start(22-delay)
+	--timerCollapsingFissureCD:Start(1-delay)
+	timerWitherCD:Start(23-delay)
+	timerWailingSoulsCD:Start(59.4-delay, 1)
+	timerTormentedCriesCD:Start(119-delay)
 	if self.Options.NPAuraOnSoulbind or self.Options.NPAuraOnSpearofAnguish or self.Options.NPAuraOnShatteringScream then
 		DBM:FireEvent("BossMod_EnableFriendlyNameplates")
 	end
@@ -131,17 +143,11 @@ end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	--Looks like it'd be excessive comms but the spellid filter will make sure it isn't.
-	self:SendSync("SpellCastStart", spellId, args.sourceGUID)
-	if spellId == 235927 or spellId == 238627 then
+	if spellId == 238570 then--Tormented Cries
+		timerTormentedCriesCD:Start()
+	elseif spellId == 235927 then
 		warnRupturingSlam:Show()
-	elseif spellId == 235968 then
---		warnGraspingDarkness:Show()
---[[	elseif (spellId == 236340 or spellId == 241566) then
-		if self:CheckInterruptFilter(args.sourceGUID) then
-			specWarnCrushMind:Show(args.sourceName)
-			voiceCrushMind:Play("kickcast")
-		end--]]
+		timerRupturingSlamCD:Start(nil, args.sourceGUID)
 	elseif spellId == 236542 then
 		if UnitBuff("player", spiritRealm) or UnitDebuff("player", spiritRealm) then--Figure out which it is
 			specWarnSunderingDoomRun:Show()
@@ -158,31 +164,56 @@ function mod:SPELL_CAST_START(args)
 			specWarnDoomedSunderingRun:Show()
 			voiceDoomedSunderin:Play("justrun")
 		end
+	elseif spellId == 236072 then
+		self.vb.wailingSoulsCast = self.vb.wailingSoulsCast + 1
+		timerSoulbindCD:Stop()
+		timerWitherCD:Stop()
+		specWarnWailingSouls:Show(self.vb.wailingSoulsCast)
+		voiceWailingSouls:Play("aesoon")
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	--Looks like it'd be excessive comms but the spellid filter will make sure it isn't.
-	self:SendSync("SpellCastSuccess", spellId, args.sourceGUID)
 	if spellId == 235907 then
 		warnCollapsingFissure:Show()
+	elseif spellId == 236449 then--Soulbind Cast
+		self.vb.soulboundCast = self.vb.soulboundCast + 1
+		--Uusally casts 2 between cries, but sometimes only 1 even when it's off cd
+		--Never seen cast a 3rd, but for time being starting 3rd timer and canceling it if wails is cast
+		--if self.vb.soulboundCast < 2 then
+			timerSoulbindCD:Start(nil, self.vb.soulboundCast+1)
+		--end
+	elseif spellId == 235933 then--Spear of Anquish
+		timerSpearofAnquishCD:Start()
+	elseif spellId == 235907 then
+		--timerCollapsingFissureCD:Start()
+	elseif spellId ==  236138 or spellId == 236131 then
+		timerWitherCD:Start()
+	elseif spellId == 235969 then--Shattering Scream
+		timerShatteringScreamCD:Start(nil, args.sourceGUID)
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	--Looks like it'd be excessive comms but the spellid filter will make sure it isn't.
-	self:SendSync("SpellAuraApplied", spellId, args.destGUID)
-	--Continue to handle personal warnings via direct combat log, not syncs.
-	if spellId == 236459 and args:IsPlayer() then
-		specWarnSoulbind:Show()
-		voiceSoulbind:Play("targetyou")
-		yellSoulbind:Yell()
-		if self.Options.RangeFrame then
-			DBM.RangeCheck:Show(5)
+	if spellId == 236459 then
+		warnSoulbind:CombinedShow(0.5, args.destName)
+		if self.Options.NPAuraOnSoulbind then
+			DBM.Nameplate:Show(true, args.destGUID, spellId, nil, 60)
+		end
+		if args:IsPlayer() then
+			specWarnSoulbind:Show()
+			voiceSoulbind:Play("targetyou")
+			yellSoulbind:Yell()
+			if self.Options.RangeFrame then
+				DBM.RangeCheck:Show(5)
+			end
 		end
 	elseif spellId == 235924 then
+		if self.Options.NPAuraOnSpearofAnguish then
+			DBM.Nameplate:Show(true, args.destGUID, spellId, nil, 6)
+		end
 		if args:IsPlayer() then
 			specWarnSpearofAnguish:Show()
 			voiceSpearofAnguish:Play("runout")
@@ -204,15 +235,24 @@ function mod:SPELL_AURA_APPLIED(args)
 		end
 	elseif spellId == 236513 then
 		warnBonecageArmor:Show(args.destName)
+		if self.Options.NPAuraOnBonecageArmor then
+			DBM.Nameplate:Show(true, args.destGUID, spellId, nil, 60)
+		end
 	elseif (spellId ==  236138 or spellId == 236131) then
 		warnWither:CombinedShow(0.3, args.destName)
 		if args:IsPlayer() then
 			specWarnWither:Show()
 			voiceWither:Play("switchphase")
 		end
-	elseif spellId == 235969 and args:IsPlayer() then
-		specWarnShatteringScream:Show(boneArmor)
-		voiceShatteringScream:Play("getboned")
+	elseif spellId == 235969 then
+		if args:IsPlayer() then
+			specWarnShatteringScream:Show(boneArmor)
+			voiceShatteringScream:Play("getboned")
+		end
+		warnShatteringScream:CombinedShow(0.5, args.destName)
+		if self.Options.NPAuraOnShatteringScream then
+			DBM.Nameplate:Show(true, args.destGUID, spellId, nil, 7.5)
+		end
 	elseif spellId == 236361 or spellId == 239923 then
 		warnSpiritChains:CombinedShow(0.3, args.destName)
 	elseif spellId == 236548 then
@@ -225,94 +265,47 @@ mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	--Looks like it'd be excessive comms but the spellid filter will make sure it isn't.
-	self:SendSync("SpellAuraRemoved", spellId, args.destGUID)
-	if spellId == 236459 and args:IsPlayer() then
-		if self.Options.RangeFrame then
+	if spellId == 236459 then
+		if self.Options.RangeFrame and args:IsPlayer() then
 			DBM.RangeCheck:Hide()
 		end
-	elseif spellId == 235924 and args:IsPlayer() then
-		yellSpearofAnguish:Cancel()
-		if self.Options.RangeFrame then
-			DBM.RangeCheck:Hide()
+		if self.Options.NPAuraOnSoulbind then
+			DBM.Nameplate:Hide(true, args.destGUID, spellId)
 		end
+	elseif spellId == 235924 then
+		if args:IsPlayer() then
+			yellSpearofAnguish:Cancel()
+			if self.Options.RangeFrame then
+				DBM.RangeCheck:Hide()
+			end
+		end
+		if self.Options.NPAuraOnSpearofAnguish then
+			DBM.Nameplate:Hide(true, args.destGUID, spellId)
+		end
+	elseif spellId == 236513 then--Bonecage Armor
+		if self.Options.NPAuraOnBonecageArmor then
+			DBM.Nameplate:Hide(true, args.destGUID, spellId)
+		end
+	elseif spellId == 235969 then--Shattering Scream
+		if self.Options.NPAuraOnShatteringScream then
+			DBM.Nameplate:Hide(true, args.destGUID, spellId)
+		end
+	elseif spellId == 236072 then
+		self.vb.soulboundCast = 0
+		timerSoulbindCD:Start(12, 1)
+		timerWitherCD:Start(19.7)
+		timerWailingSoulsCD:Start(58, self.vb.wailingSoulsCast+1)
 	end
 end
 
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 119938 then--Reanimated templar
-	
+		timerRupturingSlamCD:Stop(args.destName)
 	elseif cid == 119939 then--Ghastly Bonewarden
 	
 	elseif cid == 119940 then--Fallen Priestess
-		self:SendSync("UnitDied", cid, args.destGUID)
-	end
-end
-
-local playerName = UnitName("player")
-function mod:OnSync(msg, stringSpellId, targetGUID)
-	if not self:IsInCombat() or not stringSpellId then return end
-	local spellId = tonumber(stringSpellId)
-	local target = DBM:GetFullPlayerNameByGUID(targetGUID)
-	if msg == "SpellAuraApplied" and targetGUID then
-		if spellId == 236459 then--Soulbind
-			warnSoulbind:CombinedShow(0.5, target)
-			if self.Options.NPAuraOnSoulbind then
-				DBM.Nameplate:Show(true, targetGUID, spellId, nil, 60)
-			end
-		elseif spellId == 235924 then--Spear of Anguish
-			if self.Options.NPAuraOnSpearofAnguish then
-				DBM.Nameplate:Show(true, targetGUID, spellId, nil, 6)
-			end
-		elseif spellId == 236513 then--Bonecage Armor
-			if self.Options.NPAuraOnBonecageArmor then
-				DBM.Nameplate:Show(true, targetGUID, spellId, nil, 60)
-			end
-		elseif spellId == 235969 then--Shattering Scream
-			warnShatteringScream:CombinedShow(0.5, target)
-			if self.Options.NPAuraOnShatteringScream then
-				DBM.Nameplate:Show(true, targetGUID, spellId, nil, 7.5)
-			end
-		end
-	elseif msg == "SpellAuraRemoved" and targetGUID then
-		if spellId == 236459 then
-			if self.Options.NPAuraOnSoulbind then
-				DBM.Nameplate:Hide(true, targetGUID, spellId)
-			end
-		elseif spellId == 235924 then--Spear of Anguish
-			if self.Options.NPAuraOnSpearofAnguish then
-				DBM.Nameplate:Hide(true, targetGUID, spellId)
-			end
-		elseif spellId == 236513 then--Bonecage Armor
-			if self.Options.NPAuraOnBonecageArmor then
-				DBM.Nameplate:Hide(true, targetGUID, spellId)
-			end
-		elseif spellId == 235969 then--Shattering Scream
-			if self.Options.NPAuraOnShatteringScream then
-				DBM.Nameplate:Hide(true, targetGUID, spellId)
-			end
-		end
-	elseif msg == "SpellCastStart" then
-		if spellId == 235989 then--Tormented Cries
-			timerTormentedCriesCD:Start()
-		end
-	elseif msg == "SpellCastSuccess" then
-		if spellId == 236449 then--Soulbind Cast
-			timerSoulbindCD:Start()
-		elseif spellId == 235933 then--Spear of Anquish
-			timerSpearofAnquishCD:Start()
-		elseif spellId == 235907 then
-			timerCollapsingFissureCD:Start()
-		elseif spellId ==  236138 or spellId == 236131 then
-			timerWitherCD:Start()
-		elseif spellId == 235969 then--Shattering Scream
-			timerShatteringScreamCD:Start(nil, targetGUID)
-		end
-	elseif msg == "UnitDied" then
-		if spellId == 119940 then--Not technically spellid but passed in same arg and already tonumbered so perfect arg
-			timerShatteringScreamCD:Stop(targetGUID)
-		end
+		timerShatteringScreamCD:Stop(args.destGUID)
 	end
 end
 
