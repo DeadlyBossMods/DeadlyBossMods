cac39453bbc3d8cd091a21dd11ded56e376c36a0
Adam <MysticalOS@users.noreply.github.com>
2017-03-26 00:45:50 +0000

    Thrashbite prep

--- DBM-Party-Legion/CathedralofEternalNight/Thrashbite.lua
+++ DBM-Party-Legion/CathedralofEternalNight/Thrashbite.lua
@@ -2,32 +2,41 @@ local mod	= DBM:NewMod(1906, "DBM-Party-Legion", 12, 900)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(91005)
+mod:SetCreatureID(117194)
 mod:SetEncounterID(2057)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_APPLIED 238484 237726",
 --	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START",
-	"UNIT_SPELLCAST_SUCCEEDED boss1"
+	"SPELL_CAST_START 237276"
 )
 
-local warnFixate					= mod:NewTargetAnnounce(209906, 2, nil, false)
+--TODO, see if book local is ok in non english
+--TODO, add http://ptr.wowhead.com/spell=238678/stifling-satire?
+local warnScornfulGaze				= mod:NewTargetAnnounce(237726, 4, nil, false)
 
-local specWarnAdds					= mod:NewSpecialWarningSwitch(199817, "Dps", nil, nil, 1, 2)
+local specWarnPulvCrudgel			= mod:NewSpecialWarningRun(237276, "Melee", nil, nil, 4, 2)
+local specWarnMindControl			= mod:NewSpecialWarningSwitchCount(238484, nil, DBM_CORE_AUTO_SPEC_WARN_OPTIONS.switch:format(238484), nil, 1, 2)
+local specWarnScornfulGaze			= mod:NewSpecialWarningMoveTo(237726, nil, DBM_CORE_AUTO_SPEC_WARN_OPTIONS.you:format(237726), nil, 3, 2)
+local yellScornfulGaze				= mod:NewYell(237726)
 
-local timerSpikedTongueCD			= mod:NewNextTimer(55, 199176, nil, "Tank|Healer", nil, 5, nil, DBM_CORE_DEADLY_ICON..DBM_CORE_TANK_ICON)
+local timerPulvCrudgelCD			= mod:NewAITimer(55, 237276, nil, nil, nil, 2, nil, DBM_CORE_TANK_ICON)
+local timerScornfulGazeCD			= mod:NewAITimer(55, 237726, nil, nil, nil, 3, nil, DBM_CORE_DEADLY_ICON)
 
-local voiceAdds						= mod:NewVoice(199817, "Dps")--mobsoon
+local voiceMindControl				= mod:NewVoice(238484)--findmc
+local voicePulvCrudgel				= mod:NewVoice(237276)--runout
+local voiceScornfulGaze				= mod:NewVoice(237726)--findshelter
 
 --mod:AddRangeFrameOption(5, 153396)
 
-function mod:OnCombatStart(delay)
+local book = GetSpellInfo(230940)
 
+function mod:OnCombatStart(delay)
+	timerPulvCrudgelCD:Start(1-delay)
+	timerScornfulGazeCD:Start(1-delay)
 end
 
 function mod:OnCombatEnd()
@@ -39,17 +48,35 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
 	if spellId == 209906 then
-
+		specWarnMindControl:Show(args.destName)
+		if args:IsPlayer() then
+			--Add a yell?
+			voiceMindControl:Play("targetyou")
+		else
+			voiceMindControl:Play("findmc")
+		end
+	elseif spellId == 237726 then
+		timerScornfulGazeCD:Start()
+		if args:IsPlayer() then
+			specWarnScornfulGaze:Show(book)
+			yellScornfulGaze:Yell()
+			voiceScornfulGaze:Play("findshelter")
+		else
+			warnScornfulGaze:Show(args.destName)
+		end
 	end
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 199176 then
-
+	if spellId == 237276 then
+		specWarnPulvCrudgel:Show()
+		voicePulvCrudgel:Play("runout")
+		timerPulvCrudgelCD:Start()
 	end
 end
 
+--[[
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 	if spellId == 188494 and destGUID == UnitGUID("player") and self:AntiSpam(3, 2) then
 		specWarnRancidMaw:Show()
@@ -63,5 +90,4 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, spellGUID)
 
 	end
 end
-
 --]]
