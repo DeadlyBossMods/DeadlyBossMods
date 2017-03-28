e08fdab306c6ef53a98474ed7dec42300067217c
Adam <MysticalOS@users.noreply.github.com>
2017-03-28 01:40:03 +0000

    Finish Domatrax and Mephistroth

--- DBM-Party-Legion/CathedralofEternalNight/Mephistroth.lua
+++ DBM-Party-Legion/CathedralofEternalNight/Mephistroth.lua
@@ -2,38 +2,67 @@ local mod	= DBM:NewMod(1878, "DBM-Party-Legion", 12, 900)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(91003)
+mod:SetCreatureID(120793)
 mod:SetEncounterID(2039)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED"
+	"SPELL_CAST_START 233155 233206",
+	"SPELL_AURA_APPLIED 233963"
 )
 
-local warnShatter					= mod:NewSpellAnnounce(188114, 2)
+--TODO: Can tank dodge swarm once cast starts?
+--TODO, shadowfade ending and initial timers post shadow phase
+--TODO, verify if more debuff spellids for Demonic Upheavel than one. determine if best place to do timer
+--TODO< shadow of mephistro spawn warnings, probably 234034
+local warnShadowFade				= mod:NewSpellAnnounce(233206, 2)
+local warnDemonicUpheaval			= mod:NewTargetAnnounce(233963, 3)
 
-local specWarnRazorShards			= mod:NewSpecialWarningSpell(188169, "Tank", nil, nil, 1, 2)
+local specWarnCarrionSwarm			= mod:NewSpecialWarningSpell(233155, "Tank", nil, nil, 1, 2)
+local specWarnDemonicUpheaval		= mod:NewSpecialWarningMoveAway(233963, nil, nil, nil, 1, 2)
+local yellDemonicUpheaval			= mod:NewYell(233963)
 
-local timerShatterCD				= mod:NewCDTimer(24.2, 188114, nil, nil, nil, 2)
+local timerCarrionSwarmCD			= mod:NewAITimer(24.2, 233155, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerDemonicUpheavalCD		= mod:NewAITimer(24.2, 233963, nil, nil, nil, 3)
 
-local voiceRazorShards				= mod:NewVoice(188169, "Tank")--shockwave
+local voiceCarrionSwarm				= mod:NewVoice(233155, "Tank")--shockwave
+local voiceDemonicUpheaval			= mod:NewVoice(233963)--runout
 
 function mod:OnCombatStart(delay)
-
+	timerCarrionSwarmCD:Start(1-delay)
+	timerDemonicUpheavalCD:Start(1-delay)
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 188169 then
-
+	if spellId == 233155 then
+		specWarnCarrionSwarm:Show()
+		voiceCarrionSwarm:Play("shockwave")
+		timerCarrionSwarmCD:Start()
+	elseif spellId == 233206 then--Shadow Fade
+		warnShadowFade:Show()
+		timerCarrionSwarmCD:Stop()
+		timerDemonicUpheavalCD:Stop()
 	end
 end
 
+function mod:SPELL_AURA_APPLIED(args)
+	local spellId = args.spellId
+	if spellId == 233963 then
+		timerDemonicUpheavalCD:Start()
+		if args:IsPlayer() then
+			specWarnDemonicUpheaval:Show()
+			voiceDemonicUpheaval:Play("runout")
+			yellDemonicUpheaval:Yell()
+		else
+			warnDemonicUpheaval:Show(args.destName)
+		end
+	end
+end
+
+--[[
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 	if spellId == 192800 and destGUID == UnitGUID("player") and self:AntiSpam(2.5, 1) then
 		specWarnGas:Show()
@@ -41,5 +70,4 @@ function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
-
 --]]
