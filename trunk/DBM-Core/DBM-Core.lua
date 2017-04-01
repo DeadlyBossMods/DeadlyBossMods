26a0d4e67f23b3cf89b75fb64d3b7d05ddbdca90
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2017-04-01 02:20:03 +0000

    Inc

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -42,7 +42,7 @@
 -------------------------------
 DBM = {
 	Revision = tonumber(("$Revision$"):sub(12, -3)),
-	DisplayVersion = "7.2.1", -- the string that is shown as version
+	DisplayVersion = "7.2.2 alpha", -- the string that is shown as version
 	ReleaseRevision = 16113 -- the revision of the latest stable version that is available
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
