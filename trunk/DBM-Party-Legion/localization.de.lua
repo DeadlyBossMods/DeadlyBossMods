92b6947ab0360a1fcb3fd53a71fb24eec3cec8e6
ebmor <ebmor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2017-04-06 18:35:03 +0000

    deDE update, EN: fix typo

--- trunk/DBM-Party-Legion/localization.de.lua
+++ trunk/DBM-Party-Legion/localization.de.lua
@@ -544,6 +544,10 @@ L= DBM:GetModLocalization(1905)
 -----------------------
 L= DBM:GetModLocalization(1906)
 
+L:SetMiscLocalization({
+	bookCase	=	"hinter Bücherregal"
+})
+
 -----------------------
 -- Domatrax --
 -----------------------
