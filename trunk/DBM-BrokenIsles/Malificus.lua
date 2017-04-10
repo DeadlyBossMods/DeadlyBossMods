6147721c6a609accb31c2edb529e71de3c1558d4
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2017-04-10 04:38:43 +0000

    Update Malificus

--- trunk/DBM-BrokenIsles/Malificus.lua
+++ trunk/DBM-BrokenIsles/Malificus.lua
@@ -14,6 +14,7 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 233614 234452",
+	"SPELL_CAST_SUCCESS 233570",
 	"SPELL_AURA_APPLIED 233570 233568",
 	"SPELL_PERIODIC_DAMAGE 233850",
 	"SPELL_PERIODIC_MISSED 233850"
@@ -29,9 +30,9 @@ local yellIncitePanic				= mod:NewYell(233568)
 local specWarnIncitePanicNear		= mod:NewSpecialWarningClose(233568, nil, nil, nil, 1, 2)
 local specWarnVirulentInfection		= mod:NewSpecialWarningMove(233850, nil, nil, nil, 1, 2)
 
---local timerIncitePanicCD			= mod:NewAITimer(16, 233568, nil, nil, nil, 1)
-local timerPestilenceCD				= mod:NewAITimer(16, 233614, nil, nil, nil, 3)
-local timerShadowBarrageCD			= mod:NewAITimer(16, 234452, nil, nil, nil, 2)
+local timerIncitePanicCD			= mod:NewCDTimer(14.6, 233568, nil, nil, nil, 1)
+local timerPestilenceCD				= mod:NewCDTimer(14.6, 233614, nil, nil, nil, 3)
+local timerShadowBarrageCD			= mod:NewCDTimer(17.1, 234452, nil, nil, nil, 2)
 
 local voiceIncitePanic				= mod:NewVoice(233568)--scatter?
 local voiceVirulentInfection		= mod:NewVoice(233850)--runaway
@@ -74,6 +75,14 @@ function mod:SPELL_CAST_START(args)
 		timerShadowBarrageCD:Start()
 	end
 end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+	if spellId == 233570 and self:AntiSpam(4, 3) then
+		timerIncitePanicCD:Start()
+	end
+end
+
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
