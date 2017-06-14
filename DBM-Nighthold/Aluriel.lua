e6bb3289366843351cb42338a0be3fe185c4cedf
Adam <MysticalOS@users.noreply.github.com>
2017-06-14 06:01:55 +0000

    Add voice for echo so it no longer uses generic sound if voice pack is installed tweaked one timer by 1 second reduced chosen fate spam

--- DBM-Nighthold/Aluriel.lua
+++ DBM-Nighthold/Aluriel.lua
@@ -157,7 +157,7 @@ local rangeShowAll = false
 local chargeTable = {}
 local annihilateTimers = {8.0, 45.0, 40.0, 44.0, 38.0, 37.0, 33.0, 47.0, 41.0, 44.0, 38.0, 37.0, 33.0}--Need longer pulls/more data. However this pattern did prove to always be same
 local mythicAnnihilateTimers = {8, 46, 30, 37, 35, 43, 27, 37, 41, 37, 35, 43, 27}
-local felLashTimers = {21, 10.9, 6, 12, 6}
+local felLashTimers = {21, 10.9, 6, 11, 6}
 local searingDetonateIcons = {}
 
 local debuffFilter
