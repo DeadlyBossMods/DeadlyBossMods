cb8aece77702a6ccc15b06d3513a0c1876fb73d8
Adam <MysticalOS@users.noreply.github.com>
2017-06-27 05:20:42 +0000

    Bump HF

--- DBM-TombofSargeras/Kiljaeden.lua
+++ DBM-TombofSargeras/Kiljaeden.lua
@@ -7,7 +7,7 @@ mod:SetEncounterID(2051)
 mod:SetZone()
 --mod:SetBossHPInfoToHighest()
 mod:SetUsedIcons(1, 2, 3, 4, 5, 6)
-mod:SetHotfixNoticeRev(16288)
+mod:SetHotfixNoticeRev(16350)
 mod.respawnTime = 29
 
 mod:RegisterCombat("combat")
