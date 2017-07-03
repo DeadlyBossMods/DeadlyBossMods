4e8b73781db04ce3821622e04b0b7f6834daab78
Adam <MysticalOS@users.noreply.github.com>
2017-07-03 15:33:00 +0000

    CN Update

---
+++ DBM-Argus/localization.cn.lua
@@ -0,0 +1,48 @@
+-- Mini Dragon(projecteurs@gmail.com)
+-- 夏一可
+-- Blizzard Entertainment
+-- Last update: 2017/07/03
+
+if GetLocale() ~= "zhCN" then return end
+local L
+
+-----------------------
+-- Inquisitor Percontatum --
+-----------------------
+L= DBM:GetModLocalization(2012)
+
+-----------------------
+-- Mistress Alluradel --
+-----------------------
+L= DBM:GetModLocalization(2011)
+
+-----------------------
+-- Ovetigo --
+-----------------------
+L= DBM:GetModLocalization(2013)
+
+-----------------------
+-- Sotanathor --
+-----------------------
+L= DBM:GetModLocalization(2014)
+
+-----------------------
+-- Matron Folnuna --
+-----------------------
+L= DBM:GetModLocalization(2010)
+
+-----------------------
+-- Keeper Aedis --
+-----------------------
+L= DBM:GetModLocalization(2002)
+
+-----------------------
+-- Void-Blade Zedaat --
+-----------------------
+L= DBM:GetModLocalization(2003)
+
+-----------------------
+-- Pit Lord Vilemus --
+-----------------------
+L= DBM:GetModLocalization(2015)
+
