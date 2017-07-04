7a35d299f3d79f34920cb0b58a1d492e8f25e5a4
Paul Emmerich <emmericp@net.in.tum.de>
2017-07-04 18:49:32 +0000

    fix parameter validation

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -4219,7 +4219,7 @@ do
 						local temp1, temp2 = string.split(" ", DBM.NewerVersion)--Strip down to just version, no alpha
 						if temp1 then
 							local temp3, temp4, temp5 = string.split(".", temp1)--Strip version down to 3 numbers
-							if temp3 and temp4 and temp5 then
+							if temp3 and temp4 and temp5 and tonumber(temp5) then
 								temp5 = tonumber(temp5)
 								temp5 = temp5 - 1
 								temp5 = tostring(temp5)
