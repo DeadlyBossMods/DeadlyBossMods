5ecb077535770fccd6c1eb53e797fa7ade91fd63
Adam <MysticalOS@users.noreply.github.com>
2017-07-05 04:51:41 +0000

    Full drycode of all bosses in Seat of Triumvirate

--- DBM-Party-Legion/SeatofTriumvirate/ViceroyNezhar.lua
+++ DBM-Party-Legion/SeatofTriumvirate/ViceroyNezhar.lua
@@ -8,44 +8,52 @@ mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START",
+	"SPELL_CAST_START 246324 248736",
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_REMOVED",
-	"CHAT_MSG_RAID_BOSS_EMOTE",
+--	"CHAT_MSG_RAID_BOSS_EMOTE",
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
-local warnSubmerged						= mod:NewTargetAnnounce(196947, 2)
+--TODO, power gain rate consistent, if so give berserk timer
+--TODO, special warning to switch to tentacles once know for sure how to tell empowered apart from non empowered
+local warnScrewed						= mod:NewCastAnnounce(248736, 4)
 
-local specWarnSubmergedOver				= mod:NewSpecialWarningEnd(196947)
-local specWarnTorrent					= mod:NewSpecialWarningInterrupt(198495, "HasInterrupt", nil, nil, 1, 2)
+local specWarnHowlingDark				= mod:NewSpecialWarningInterrupt(244751, "HasInterrupt", nil, nil, 1, 2)
+local specWarnEntropicForce				= mod:NewSpecialWarningSpell(246324, nil, nil, nil, 1, 2)
 
-local timerTaintofSeaCD					= mod:NewAITimer(12, 197262, nil, nil, nil, 3)
---local timerTorrentCD					= mod:NewCDTimer(9.7, 198495, nil, nil, nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
+local timerUmbralTentaclesCD			= mod:NewAITimer(12, 244769, nil, nil, nil, 1)
+local timerHowlingDarkCD				= mod:NewAITimer(9.7, 244751, nil, nil, nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
+local timerEntropicForceCD				= mod:NewAITimer(12, 244751, nil, nil, nil, 2)
+local timerScrewed						= mod:NewCastTimer(10, 248736, nil, nil, nil, 2, nil, DBM_CORE_DEADLY_ICON)
 
-local countdownBreath					= mod:NewCountdown(22, 227233)
+local countdownScrewed					= mod:NewCountdown("AltTwo10", 248736)
 
-local voiceBrackwaterBarrage			= mod:NewVoice(202088)--breathsoon
-local voiceTorrent						= mod:NewVoice(198495, "HasInterrupt")--kickcast
-
-mod.vb.phase = 1
+local voiceHowlingDark					= mod:NewVoice(244751, "HasInterrupt")--kickcast
+local voiceEntropicForce				= mod:NewVoice(246324)--keepmove
 
 function mod:OnCombatStart(delay)
-
+	timerUmbralTentaclesCD:Start(1-delay)
+	timerEntropicForceCD:Start(1-delay)
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 227233 then
-
-	elseif spellId == 198495 then
-		timerTorrentCD:Start()
+	if spellId == 246324 then
+		specWarnEntropicForce:Show()
+		voiceEntropicForce:Play("keepmove")
+		timerEntropicForceCD:Start()
+	elseif spellId == 244751 then
+		timerHowlingDarkCD:Start()
 		if self:CheckInterruptFilter(args.sourceGUID) then
-			specWarnTorrent:Show(args.sourceName)
-			voiceTorrent:Play("kickcast")
+			specWarnHowlingDark:Show(args.sourceName)
+			voiceHowlingDark:Play("kickcast")
 		end
+	elseif spellId == 248736 and self:AntiSpam(3, 1) then
+		warnScrewed:Show()
+		timerScrewed:Start()
+		countdownScrewed:Start()
 	end
 end
 
@@ -62,16 +70,17 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end
 
+--[[
 function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 	if msg:find("inv_misc_monsterhorn_03") then
 
 	end
 end
+--]]
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, spellGUID)
 	local spellId = tonumber(select(5, strsplit("-", spellGUID)), 10)
-	if spellId == 197596 then
-
+	if spellId == 244769 then
+		timerUmbralTentaclesCD:Start()
 	end
 end
---]]
