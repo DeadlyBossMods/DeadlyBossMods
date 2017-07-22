4413fd83f4109585de5cb1fcf45e00462cdf7a5a
Adam <MysticalOS@users.noreply.github.com>
2017-07-22 05:34:10 +0000

    Preliminary Eonar stuff. more work needed, need journal update

--- DBM-AntorusBurningThrone/Eonar.lua
+++ DBM-AntorusBurningThrone/Eonar.lua
@@ -12,41 +12,74 @@ mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS",
-	"SPELL_AURA_APPLIED",
+	"SPELL_CAST_START 248332 249121 250701",
+--	"SPELL_CAST_SUCCESS",
+	"SPELL_AURA_APPLIED 248333 250074",
 	"SPELL_AURA_APPLIED_DOSE",
-	"SPELL_AURA_REMOVED",
+	"SPELL_AURA_REMOVED 248333 250074",
 --	"SPELL_PERIODIC_DAMAGE",
 --	"SPELL_PERIODIC_MISSED",
 	"UNIT_DIED",
-	"CHAT_MSG_RAID_BOSS_EMOTE",
-	"UNIT_SPELLCAST_SUCCEEDED boss1"
+--	"CHAT_MSG_RAID_BOSS_EMOTE",
+	"RAID_BOSS_WHISPER",
+	"UNIT_SPELLCAST_SUCCEEDED boss1 boss2 boss3 boss4 boss5",
+	"UNIT_SPELLCAST_CHANNEL_STOP boss1 boss2 boss3 boss4 boss5",
+	"UNIT_SPELLCAST_STOP boss1 boss2 boss3 boss4 boss5"
 )
 
---local warnEruptingRelections		= mod:NewTargetAnnounce(236710, 2)
+--TODO, figure out how to warn Artillery Strike.
+--TODO, verify Meteor Storm
+--TODO, verify interrupt for Final Doom
+--TODO, scan for cloak, artiliary mode to find other adds?
+--TODO, add rest of mythic stuff
+--TODO, adds waves timers
+--The Paraxis
+local warnMeteorStorm					= mod:NewEndAnnounce(248333, 1)
+local warnSpearofDoom					= mod:NewTargetAnnounce(248789, 3)
 
---local specWarnFelclaws				= mod:NewSpecialWarningDefensive(239932, nil, nil, nil, 1, 2)
---local yellBurstingDreadflame		= mod:NewPosYell(238430, DBM_CORE_AUTO_YELL_CUSTOM_POSITION)
+--The Paraxis
+local specWarnMeteorStorm				= mod:NewSpecialWarningDodge(248333, nil, nil, nil, 2, 2)
+local specWarnSpearofDoom				= mod:NewSpecialWarningMoveAway(248789, nil, nil, nil, 1, 2)
+local yellSpearofDoom					= mod:NewYell(248789)
+local specWarnRainofFel					= mod:NewSpecialWarningCount(248332, nil, nil, nil, 1, 2)
+--Adds
+local specWarnSwing						= mod:NewSpecialWarningDefensive(250701, "Tank", nil, nil, 1, 2)
+--local yellBurstingDreadflame			= mod:NewPosYell(238430, DBM_CORE_AUTO_YELL_CUSTOM_POSITION)
 --local specWarnMalignantAnguish		= mod:NewSpecialWarningInterrupt(236597, "HasInterrupt")
 --local specWarnGTFO					= mod:NewSpecialWarningGTFO(238028, nil, nil, nil, 1, 2)
 
---local timerFelclawsCD				= mod:NewAITimer(25, 239932, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
---local timerRupturingSingularityCD	= mod:NewAITimer(61, 235059, nil, nil, nil, 3)
+--The Paraxis
+local timerMeteorStormCD				= mod:NewAITimer(61, 248333, nil, nil, nil, 3)
+local timerSpearofDoomCD				= mod:NewAITimer(61, 248789, nil, nil, nil, 3)
+local timerRainofFelCD					= mod:NewAITimer(61, 248332, nil, nil, nil, 3)
+local timerFinalDoom					= mod:NewCastTimer(70, 249121, nil, nil, nil, 2)
+--Mythic
+local timerFinalDoomCD					= mod:NewAITimer(61, 249121, nil, nil, nil, 4, nil, DBM_CORE_HEROIC_ICON)
+--local timerFelclawsCD					= mod:NewAITimer(25, 239932, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
 
 --local berserkTimer					= mod:NewBerserkTimer(600)
 
+--The Paraxis
 --local countdownSingularity			= mod:NewCountdown(50, 235059)
 
+--The Paraxis
+local voiceMeteorStorm					= mod:NewVoice(248333)--watchstep
+local voiceSpearofDoom					= mod:NewVoice(248789)--laserrun
+local voiceRainofFel					= mod:NewVoice(248332)--helpsoak?
+--Adds
+local voiceSwing						= mod:NewVoice(250701)--shockwave/defensive?
 --local voiceMalignantAnguish			= mod:NewVoice(236597, "HasInterrupt")--kickcast
 --local voiceGTFO						= mod:NewVoice(238028, nil, DBM_CORE_AUTO_VOICE4_OPTION_TEXT)--runaway
 
---mod:AddSetIconOption("SetIconOnFocusedDread", 238502, true)
---mod:AddInfoFrameOption(239154, true)
+mod:AddSetIconOption("SetIconOnSpearofDoom", 248789, true)
+mod:AddInfoFrameOption(250030, true)
+mod:AddNamePlateOption("NPAuraOnPurification", 250074)
 --mod:AddRangeFrameOption("5/10")
 
+mod.vb.rainOfFelCount = 0
+
+--[[
 local debuffFilter
 local UnitDebuff = UnitDebuff
 local playerDebuff = nil
@@ -74,23 +107,52 @@ local function updateRangeFrame(self)
 	end
 end
 
-function mod:OnCombatStart(delay)
+--]]
 
+function mod:OnCombatStart(delay)
+	self.vb.rainOfFelCount = 0
+	timerMeteorStormCD:Start(1-delay)
+	timerSpearofDoomCD:Start(1-delay)
+	if not self:IsLFR() then
+		timerRainofFelCD:Start(1-delay)
+		if self:IsMythic() then
+			timerFinalDoomCD:Start(1-delay)
+		end
+	end
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:SetHeader(GetSpellInfo(250030))
+		DBM.InfoFrame:Show(2, "enemypower", 2)--Using regular power for now, if it's wrong, change to ALTERNATE
+	end
+	if self.Options.NPAuraOnPurification then
+		DBM:FireEvent("BossMod_EnableHostileNameplates")
+	end
 end
 
 function mod:OnCombatEnd()
 --	if self.Options.RangeFrame then
 --		DBM.RangeCheck:Hide()
 --	end
---	if self.Options.InfoFrame then
---		DBM.InfoFrame:Hide()
---	end
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:Hide()
+	end
+	if self.Options.NPAuraOnPurification then
+		DBM.Nameplate:Hide(true, nil, nil, nil, true, true)
+	end
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 238999 then
-
+	if spellId == 248332 then
+		self.vb.rainOfFelCount = self.vb.rainOfFelCount + 1
+		specWarnRainofFel:Show(self.vb.rainOfFelCount)
+		voiceRainofFel:Play("helpsoak")
+		timerRainofFelCD:Start()
+	elseif spellId == 249121 then
+		timerFinalDoom:Start()
+		timerFinalDoomCD:Start()--Move to stop event or power event if cleaner
+	elseif spellId == 250701 then
+		specWarnSwing:Show()
+		voiceSwing:Play("shockwave")
 	end
 end
 
@@ -103,20 +165,51 @@ end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 245509 then
-
+	if spellId == 248333 then
+		specWarnMeteorStorm:Show()
+		voiceMeteorStorm:Play("watchstep")
+		timerMeteorStormCD:Start()
+	elseif spellId == 250074 then--Purification
+		if self.Options.NPAuraOnPurification then
+			DBM.Nameplate:Show(true, args.destGUID, spellId)
+		end
 	end
 end
-mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+--mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 236378 then
-
+	if spellId == 248333 then
+		warnMeteorStorm:Show()
+	elseif spellId == 250074 then--Purification
+		if self.Options.NPAuraOnPurification then
+			DBM.Nameplate:Hide(true, args.destGUID, spellId)
+		end
 	end
 end
 
+function mod:RAID_BOSS_WHISPER(msg)
+	if msg:find("spell:248861") or msg:find("spell:248789") then
+		specWarnSpearofDoom:Show()
+		voiceSpearofDoom:Play("runout")
+		yellSpearofDoom:Yell()
+	end
+end
 
+function mod:OnTranscriptorSync(msg, targetName)
+	if msg:find("spell:248861") or msg:find("spell:248789") then
+		targetName = Ambiguate(targetName, "none")
+		if self:AntiSpam(4, targetName) then
+			local icon = self.vb.bladesIcon
+			warnSpearofDoom:CombinedShow(0.5, targetName)
+			if self.Options.SetIconOnSpearofDoom then
+				--self:SetIcon(targetName, icon, 5)
+			end
+		end
+	end
+end
+
+--[[
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 	if spellId == 228007 and destGUID == UnitGUID("player") and self:AntiSpam(2, 4) then
 		specWarnGTFO:Show()
@@ -131,17 +224,27 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, npc, _, _, target)
 	end
 end
 
+--http://ptr.wowhead.com/npc=123726/fel-powered-purifier
+--http://ptr.wowhead.com/npc=123760/fel-infused-destructor
+--http://ptr.wowhead.com/npc=124207/fel-charged-obfuscator
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 121193 then
 
 	end
 end
+--]]
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, spellGUID)
 	local spellId = tonumber(select(5, strsplit("-", spellGUID)), 10)
-	if spellId == 242377 then
-
+	if spellId == 248861 then
+		timerSpearofDoomCD:Start()
 	end
 end
---]]
+
+function mod:UNIT_SPELLCAST_CHANNEL_STOP(uId, _, _, _, spellId)
+	if spellId == 249121 then
+		timerFinalDoom:Stop()
+	end
+end
+mod.UNIT_SPELLCAST_STOP = mod.UNIT_SPELLCAST_CHANNEL_STOP
