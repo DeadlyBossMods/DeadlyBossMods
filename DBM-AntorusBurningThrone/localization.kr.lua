f20c1731d03cefab61574ba8e6b2313e2857786e
Adam <MysticalOS@users.noreply.github.com>
2017-08-05 21:56:56 +0000

    KR update

--- DBM-AntorusBurningThrone/localization.kr.lua
+++ DBM-AntorusBurningThrone/localization.kr.lua
@@ -35,6 +35,16 @@ L= DBM:GetModLocalization(2025)
 ---------------------------
 L= DBM:GetModLocalization(1985)
 
+L:SetOptionLocalization({
+	ShowAllPlatforms =	"자신의 위치와 관계 없이 모든 공대원의 단상 위치 알림 보기"
+})
+
+L:SetMiscLocalization({
+	Xoroth = "소로스를 봐라, 지옥불의 열기와 불타버린 뼈의 세계다!",
+	Rancora = "란코라를 올려다 봐라, 부패한 웅덩이와 스멀거리는 죽음의 대지다!",
+	Nathreza = "Nathreza... once a world of magic and knowledge, now a twisted landscape from which none escape."
+})
+
 ---------------------------
 -- Imonar the Soulhunter --
 ---------------------------
@@ -44,6 +54,10 @@ L= DBM:GetModLocalization(2009)
 -- Kin'garoth --
 ---------------------------
 L= DBM:GetModLocalization(2004)
+
+L:SetOptionLocalization({
+	InfoFrame =	"전투의 전반적인 상황을 정보 창에 표시"
+})
 
 ---------------------------
 -- Varimathras --
