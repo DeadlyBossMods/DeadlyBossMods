e294179f656771b3066b18f9c24a8d3ff79a92a1
ultrashining <ultrashining@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2017-08-21 08:50:08 +0000

    zhTW update

--- DBM-AntorusBurningThrone/localization.tw.lua
+++ DBM-AntorusBurningThrone/localization.tw.lua
@@ -60,6 +60,10 @@ L= DBM:GetModLocalization(1983)
 ---------------------------
 L= DBM:GetModLocalization(1986)
 
+L:SetOptionLocalization({
+	timerBossIncoming		= "Show timer for next boss swap"
+})
+
 ---------------------------
 -- Aggramar --
 ---------------------------
