db164aa78963faa9558b0690e62cabb7ad5027d3
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2017-08-23 04:43:03 +0000

    Fix warning being too spammy for healers

--- trunk/DBM-TombofSargeras/SistersoftheMoon.lua
+++ trunk/DBM-TombofSargeras/SistersoftheMoon.lua
@@ -346,8 +346,8 @@ function mod:SPELL_AURA_APPLIED(args)
 	elseif spellId == 233263 then--Healer Embrace of the Eclipse
 		self.vb.eclipseCount = self.vb.eclipseCount + 1
 		if self:IsHealer() then
-			specWarnEmbraceofEclipse:CombinedShow(0.5, args.destName)
 			if self:AntiSpam(3, 1) then
+				specWarnEmbraceofEclipse:Show(ALL)
 				voiceEmbraceofEclipse:Play("healall")
 			end
 		end
