0f0cfb708561ae5ea02b439a12cbec620eba4711
Adam <MysticalOS@users.noreply.github.com>
2017-08-27 05:26:07 +0000

    small antispam to talarn 3/6 argus invasion drycodes

--- DBM-Argus/Meto.lua
+++ DBM-Argus/Meto.lua
@@ -10,27 +10,34 @@ mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS",
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED",
-	"UNIT_SPELLCAST_SUCCEEDED"
+	"SPELL_CAST_START 247492 247585 247632",
+	"SPELL_CAST_SUCCESS 247495",
+	"SPELL_AURA_APPLIED 247495",
+	"SPELL_AURA_APPLIED_DOSE 247495"
+--	"SPELL_AURA_REMOVED",
+--	"UNIT_SPELLCAST_SUCCEEDED"
 )
 
---local warnAncientRageFire		= mod:NewSpellAnnounce(217563, 2)
+--TODO, see if run over flowr is appropriate or i it lacks clarity, since here we want to STAND on them not run over them.
+--TODO, target scan death field?
+local warnSow					= mod:NewStackAnnounce(247495, 2, nil, "Tank")
 
---local specBurningBomb			= mod:NewSpecialWarningYou(217877, nil, nil, nil, 1, 2)
---local yellBurningBomb			= mod:NewFadesYell(217877)
---local specWarnGTFO					= mod:NewSpecialWarningGTFO(238028, nil, nil, nil, 1, 2)
+local specReap					= mod:NewSpecialWarningSpell(247492, "Tank", nil, nil, 1, 2)
+local specWarnSow				= mod:NewSpecialWarningStack(247495, nil, 2, nil, nil, 1, 2)
+local specWarnSowOther			= mod:NewSpecialWarningTaunt(247495, nil, nil, nil, 1, 2)
+local specSeedsofChaos			= mod:NewSpecialWarningSpell(247585, "-Tank", nil, nil, 1, 2)
+local specWarnDeathField		= mod:NewSpecialWarningDodge(247632, nil, nil, nil, 2, 2)
 
---local timerBurningBombCD		= mod:NewAITimer(13.4, 217877, nil, nil, nil, 3)
+local timerReapCD				= mod:NewAITimer(13.4, 247492, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerSowCD				= mod:NewAITimer(13.4, 247495, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerSeedsofChaosCD		= mod:NewAITimer(13.4, 247585, nil, nil, nil, 5, nil, DBM_CORE_DEADLY_ICON)
+local timerDeathFieldCD			= mod:NewAITimer(13.4, 247632, nil, nil, nil, 3)
 
---local voiceBurningBomb			= mod:NewVoice(217877)--targetyou
---local voiceGTFO						= mod:NewVoice(238028, nil, DBM_CORE_AUTO_VOICE4_OPTION_TEXT)--runaway
+local voiceReap					= mod:NewVoice(247492)--shockwave
+local voiceSow					= mod:NewVoice(247495)--tauntboss/stackhigh
+local voiceSeedsofChaos			= mod:NewVoice(247585, "-Tank")--169613 (run over the flower)?
+local voiceDeathField			= mod:NewVoice(247632)--shockwave (will this confuse tanks?)
 
 --mod:AddReadyCheckOption(43193, false)
 --mod:AddRangeFrameOption(10, 217877)
@@ -49,25 +56,55 @@ end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 217966 then
-
+	if spellId == 247492 then
+		specReap:Show()
+		voiceReap:Play("shockwave")
+		timerReapCD:Start()
+	elseif spellId == 247585 then
+		specSeedsofChaos:Show()
+		voiceSeedsofChaos:Play("169613")
+		timerSeedsofChaosCD:Start()
+	elseif spellId == 247632 then
+		specWarnDeathField:Show()
+		voiceDeathField:Play("shockwave")
+		timerDeathFieldCD:Start()
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 217877 then
-
+	if spellId == 247495 then
+		timerSowCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 217877 then
-
+	if spellId == 247495 then
+		local uId = DBM:GetRaidUnitId(args.destName)
+		if self:IsTanking(uId) then
+			local amount = args.amount or 1
+			if amount >= 2 then--Lasts 30 seconds, cast every 5 seconds, swapping will be at 6
+				if args:IsPlayer() then--At this point the other tank SHOULD be clear.
+					specWarnSow:Show(amount)
+					voiceSow:Play("stackhigh")
+				else--Taunt as soon as stacks are clear, regardless of stack count.
+					if not UnitIsDeadOrGhost("player") and not UnitDebuff("player", args.spellName) then
+						specWarnSowOther:Show(args.destName)
+						voiceSow:Play("tauntboss")
+					else
+						warnSow:Show(args.destName, amount)
+					end
+				end
+			else
+				warnSow:Show(args.destName, amount)
+			end
+		end
 	end
 end
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
+--[[
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
 	if spellId == 217877 then
--- DBM-Argus/Occularus.lua
+++ DBM-Argus/Occularus.lua
@@ -10,27 +10,34 @@ mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS",
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_PERIODIC_MISSED",
-	"UNIT_SPELLCAST_SUCCEEDED"
+	"SPELL_CAST_START 247320 247393",
+	"SPELL_CAST_SUCCESS 247318 247325",
+	"SPELL_AURA_APPLIED 247318 247330",
+	"SPELL_AURA_APPLIED_DOSE 247318"
+--	"UNIT_SPELLCAST_SUCCEEDED"
 )
 
---local warnAncientRageFire		= mod:NewSpellAnnounce(217563, 2)
+--TODO, see if tanks tuff needs to be more complex, like Ursoc. Also adjust stacks (if it even stacks)
+local warnGushingWound					= mod:NewStackAnnounce(247318, 2, nil, "Tank")
+local warnLash							= mod:NewSpellAnnounce(247325, 2, nil, "Tank")
 
---local specBurningBomb			= mod:NewSpecialWarningYou(217877, nil, nil, nil, 1, 2)
---local yellBurningBomb			= mod:NewFadesYell(217877)
---local specWarnGTFO					= mod:NewSpecialWarningGTFO(238028, nil, nil, nil, 1, 2)
+local specWarnGushingWound				= mod:NewSpecialWarningStack(247318, nil, 2, nil, nil, 1, 2)
+local specWarnGushingWoundOther			= mod:NewSpecialWarningTaunt(247318, nil, nil, nil, 1, 2)
+local specWarnSearingGaze				= mod:NewSpecialWarningInterrupt(247320, "HasInterrupt", nil, nil, 1, 2)
+local specWarnPhantasm					= mod:NewSpecialWarningDodge(247393, nil, nil, nil, 2, 2)
+local specWarnEyeSore					= mod:NewSpecialWarningTarget(247330, "Healer", nil, nil, 1, 2)
 
---local timerBurningBombCD		= mod:NewAITimer(13.4, 217877, nil, nil, nil, 3)
+local timerGushingWoundCD				= mod:NewAITimer(13.4, 247318, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerLashCD						= mod:NewAITimer(13.4, 247325, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerSearingGazeCD				= mod:NewAITimer(61, 247320, nil, nil, nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
+local timerPhantasmCD					= mod:NewAITimer(61, 247393, nil, nil, nil, 3, nil, DBM_CORE_DEADLY_ICON)
+local timerEyeSoreCD					= mod:NewAITimer(61, 247330, nil, "Healer", nil, 3, nil, DBM_CORE_HEALER_ICON)
 
---local voiceBurningBomb			= mod:NewVoice(217877)--targetyou
---local voiceGTFO						= mod:NewVoice(238028, nil, DBM_CORE_AUTO_VOICE4_OPTION_TEXT)--runaway
+local voiceGushingWound					= mod:NewVoice(247318)--tauntboss/stackhigh
+local voiceSearingGaze					= mod:NewVoice(247320, "HasInterrupt")--kickcast
+local voicePhantasm						= mod:NewVoice(247393)--watchorb
+local voiceEyeSore						= mod:NewVoice(247330, "Healer")--healall
 
 --mod:AddReadyCheckOption(43193, false)
 --mod:AddRangeFrameOption(10, 217877)
@@ -41,50 +48,73 @@ function mod:OnCombatStart(delay, yellTriggered)
 	end
 end
 
-function mod:OnCombatEnd()
+--function mod:OnCombatEnd()
 --	if self.Options.RangeFrame then
 --		DBM.RangeCheck:Hide()
 --	end
-end
+--end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 217966 then
-
+	if spellId == 247320 then
+		timerSearingGazeCD:Start()
+		if self:CheckInterruptFilter(args.sourceGUID) then
+			specWarnSearingGaze:Show(args.sourceName)
+			voiceSearingGaze:Play("kickcast")
+		end
+	elseif spellId == 247393 then
+		specWarnPhantasm:Show()
+		voicePhantasm:Play("watchorb")
+		timerPhantasmCD:Start()
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 217877 then
-
+	if spellId == 247318 then
+		timerGushingWoundCD:Start()
+	elseif spellId == 247325 then
+		warnLash:Show()
+		timerLashCD:Start()
+	elseif spellId == 247330 then
+		timerEyeSoreCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 217877 then
-
+	if spellId == 247318 then
+		local uId = DBM:GetRaidUnitId(args.destName)
+		if self:IsTanking(uId) then
+			local amount = args.amount or 1
+			if amount >= 2 then--Lasts 30 seconds, cast every 5 seconds, swapping will be at 6
+				if args:IsPlayer() then--At this point the other tank SHOULD be clear.
+					specWarnGushingWound:Show(amount)
+					voiceGushingWound:Play("stackhigh")
+				else--Taunt as soon as stacks are clear, regardless of stack count.
+					if not UnitIsDeadOrGhost("player") and not UnitDebuff("player", args.spellName) then
+						specWarnGushingWoundOther:Show(args.destName)
+						voiceGushingWound:Play("tauntboss")
+					else
+						warnGushingWound:Show(args.destName, amount)
+					end
+				end
+			else
+				warnGushingWound:Show(args.destName, amount)
+			end
+		end
+	elseif spellId == 247330 then
+		specWarnEyeSore:CombinedShow(0.3, args.destName)
+		if self:AntiSpam(4, 1) then
+			voiceEyeSore:Play("healall")
+		end
 	end
 end
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
-function mod:SPELL_AURA_REMOVED(args)
-	local spellId = args.spellId
-	if spellId == 217877 then
-
-	end
-end
-
-function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 217907 and destGUID == UnitGUID("player") and self:AntiSpam(2, 5) then
-		specWarnGTFO:Show()
-		voiceGTFO:Play("runaway")
-	end
-end
-mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
-
+--[[
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
-	if spellId == 217563 and self:AntiSpam(4, 1) then
+	if spellId == 217563 and self:AntiSpam(4, 2) then
 
 	end
 end
