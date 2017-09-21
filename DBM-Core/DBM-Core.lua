75f2100b42eebbfd4899cc74c1be8968e34ca81a
Paul Emmerich <emmericp@net.in.tum.de>
2017-09-21 17:18:35 +0000

    increment alpha version

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -42,8 +42,8 @@
 -------------------------------
 DBM = {
 	Revision = tonumber(("$Revision$"):sub(12, -3)),
-	DisplayVersion = "7.3.3 alpha", -- the string that is shown as version
-	ReleaseRevision = 16694 -- the revision of the latest stable version that is available
+	DisplayVersion = "7.3.4 alpha", -- the string that is shown as version
+	ReleaseRevision = 16725 -- the revision of the latest stable version that is available
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
