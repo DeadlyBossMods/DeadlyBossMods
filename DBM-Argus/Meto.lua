0657ba06c694ef3730f298d8368befdd609c6e06
Adam <MysticalOS@users.noreply.github.com>
2017-09-27 22:10:22 +0000

    Update Meto timers Fix seeds of chaos warning/timer Enable yell pull detection and readycheck options Antispam tank warnings some

--- DBM-Argus/Meto.lua
+++ DBM-Argus/Meto.lua
@@ -8,40 +8,44 @@ mod:SetCreatureID(124592)
 mod:SetZone()
 --mod:SetMinSyncRevision(11969)
 
-mod:RegisterCombat("combat")
+mod:RegisterCombat("combat_yell", L.Pull)
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 247492 247585 247632",
+	"SPELL_CAST_START 247492 247632",
 	"SPELL_CAST_SUCCESS 247495",
 	"SPELL_AURA_APPLIED 247495",
-	"SPELL_AURA_APPLIED_DOSE 247495"
+	"SPELL_AURA_APPLIED_DOSE 247495",
+	"UNIT_SPELLCAST_SUCCEEDED"
 )
 
 --TODO, see if run over flowr is appropriate or i it lacks clarity, since here we want to STAND on them not run over them.
---TODO, target scan death field?
 local warnSow					= mod:NewStackAnnounce(247495, 2, nil, "Tank")
+local warnDeathField			= mod:NewSpellAnnounce(247632, 2)
 
 local specReap					= mod:NewSpecialWarningSpell(247492, "Tank", nil, nil, 1, 2)
 local specWarnSow				= mod:NewSpecialWarningStack(247495, nil, 2, nil, nil, 1, 2)
 local specWarnSowOther			= mod:NewSpecialWarningTaunt(247495, nil, nil, nil, 1, 2)
 local specSeedsofChaos			= mod:NewSpecialWarningSpell(247585, "-Tank", nil, nil, 1, 2)
-local specWarnDeathField		= mod:NewSpecialWarningDodge(247632, nil, nil, nil, 2, 2)
+--local specWarnDeathField		= mod:NewSpecialWarningDodge(247632, nil, nil, nil, 2, 2)
 
-local timerReapCD				= mod:NewAITimer(13.4, 247492, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
-local timerSowCD				= mod:NewAITimer(13.4, 247495, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
-local timerSeedsofChaosCD		= mod:NewAITimer(13.4, 247585, nil, nil, nil, 5, nil, DBM_CORE_DEADLY_ICON)
-local timerDeathFieldCD			= mod:NewAITimer(13.4, 247632, nil, nil, nil, 3)
+local timerReapCD				= mod:NewCDTimer(18.7, 247492, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)--18-25
+local timerSowCD				= mod:NewCDTimer(13.4, 247495, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)--13-19
+local timerSeedsofChaosCD		= mod:NewCDTimer(29.2, 247585, nil, nil, nil, 5, nil, DBM_CORE_DEADLY_ICON)
+local timerDeathFieldCD			= mod:NewCDTimer(13.3, 247632, nil, nil, nil, 3)
 
 local voiceReap					= mod:NewVoice(247492)--shockwave
 local voiceSow					= mod:NewVoice(247495)--tauntboss/stackhigh
 local voiceSeedsofChaos			= mod:NewVoice(247585, "-Tank")--169613 (run over the flower)?
-local voiceDeathField			= mod:NewVoice(247632)--shockwave (will this confuse tanks?)
+--local voiceDeathField			= mod:NewVoice(247632)--shockwave (will this confuse tanks?)
 
---mod:AddReadyCheckOption(49198, false)
+mod:AddReadyCheckOption(49198, false)
 
 function mod:OnCombatStart(delay, yellTriggered)
 	if yellTriggered then
-
+		timerSowCD:Start(5.6-delay)
+		timerReapCD:Start(11.7-delay)
+		timerDeathFieldCD:Start(14.7-delay)
+		--timerSeedsofChaosCD:Start(31-delay)
 	end
 end
 
@@ -51,13 +55,9 @@ function mod:SPELL_CAST_START(args)
 		specReap:Show()
 		voiceReap:Play("shockwave")
 		timerReapCD:Start()
-	elseif spellId == 247585 then
-		specSeedsofChaos:Show()
-		voiceSeedsofChaos:Play("169613")
-		timerSeedsofChaosCD:Start()
 	elseif spellId == 247632 then
-		specWarnDeathField:Show()
-		voiceDeathField:Play("shockwave")
+		warnDeathField:Show()
+		--voiceDeathField:Play("shockwave")
 		timerDeathFieldCD:Start()
 	end
 end
@@ -79,7 +79,7 @@ function mod:SPELL_AURA_APPLIED(args)
 				if args:IsPlayer() then--At this point the other tank SHOULD be clear.
 					specWarnSow:Show(amount)
 					voiceSow:Play("stackhigh")
-				else--Taunt as soon as stacks are clear, regardless of stack count.
+				elseif self:AntiSpam(3, 1) then--Taunt as soon as stacks are clear, regardless of stack count.
 					if not UnitIsDeadOrGhost("player") and not UnitDebuff("player", args.spellName) then
 						specWarnSowOther:Show(args.destName)
 						voiceSow:Play("tauntboss")
@@ -87,10 +87,18 @@ function mod:SPELL_AURA_APPLIED(args)
 						warnSow:Show(args.destName, amount)
 					end
 				end
-			else
+			elseif self:AntiSpam(3, 1) then
 				warnSow:Show(args.destName, amount)
 			end
 		end
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
+	if spellId == 247585 and self:AntiSpam(3, 2) then--Seeds of Chaos
+		specSeedsofChaos:Show()
+		voiceSeedsofChaos:Play("169613")
+		timerSeedsofChaosCD:Start()
+	end
+end
