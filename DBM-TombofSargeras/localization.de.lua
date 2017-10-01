4f9ff73f045051577128017e69d988ad9f546502
ebmor <ebmor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2017-10-01 18:21:18 +0000

    deDE update

--- DBM-TombofSargeras/localization.de.lua
+++ DBM-TombofSargeras/localization.de.lua
@@ -21,6 +21,10 @@ L= DBM:GetModLocalization(1856)
 ---------------------------
 L= DBM:GetModLocalization(1861)
 
+L:SetOptionLocalization({
+	TauntOnPainSuccess	= "Sync timers and taunt warning to Burden of Pain cast SUCCESS instead of START (for certain mythic strats where you let burden tick once on purpose, otherwise it's NOT recommended to use this options)"--translate
+})
+
 ---------------------------
 -- Sisters of the Moon --
 ---------------------------
@@ -30,6 +34,10 @@ L= DBM:GetModLocalization(1903)
 -- The Desolate Host --
 ---------------------------
 L= DBM:GetModLocalization(1896)
+
+L:SetOptionLocalization({
+	IgnoreTemplarOn3Tank	= "Ignoriere Reanimierte Templer für Knochenkäfigrüstung Infofenster/Ansagen/Namensplaketten bei Verwendung von 3 oder mehr Tanks (nicht mitten im Kampf ändern, das ruiniert die Zählung)"
+})
 
 ---------------------------
 -- Maiden of Vigilance --
@@ -42,13 +50,21 @@ L= DBM:GetModLocalization(1897)
 L= DBM:GetModLocalization(1873)
 
 L:SetOptionLocalization({
-	InfoFrame =	"Zeige Infofenster für Bossenergie"
+	InfoFrame =	"Zeige Infofenster für Kampfübersicht"
+})
+
+L:SetMiscLocalization({
+	FallenAvatarDialog	= "The husk before you was once a vessel for the might of Sargeras. But this temple itself is our prize. The means by which we will reduce your world to cinders!"--translate (trigger)
 })
 
 ---------------------------
 -- Kil'jaeden --
 ---------------------------
 L= DBM:GetModLocalization(1898)
+
+L:SetMiscLocalization({
+	Obelisklasers	= "Obeliskenlaser"
+})
 
 -------------
 --  Trash  --
