a101d9b4559f32ed3d13fea6fa631fa89e760eeb
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2017-10-02 06:12:52 +0000

    just commiting what’s done so far, stage 1 argus. will do up stage 2-3-4 in coming couple days Fix lua error in Imonar

--- trunk/DBM-AntorusBurningThrone/ArgustheUnmaker.lua
+++ trunk/DBM-AntorusBurningThrone/ArgustheUnmaker.lua
@@ -12,41 +12,66 @@ mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS",
-	"SPELL_AURA_APPLIED",
+	"SPELL_CAST_START 248317 257296 255594",
+	"SPELL_CAST_SUCCESS 248165 248499",
+	"SPELL_AURA_APPLIED 248499",
 	"SPELL_AURA_APPLIED_DOSE",
 	"SPELL_AURA_REMOVED",
 --	"SPELL_PERIODIC_DAMAGE",
 --	"SPELL_PERIODIC_MISSED",
 	"UNIT_DIED",
 	"CHAT_MSG_RAID_BOSS_EMOTE",
-	"UNIT_SPELLCAST_SUCCEEDED boss1"
+	"UNIT_SPELLCAST_SUCCEEDED boss1 boss2 boss3 boss4 boss5"
 )
 
---local warnEruptingRelections		= mod:NewTargetAnnounce(236710, 2)
+--TODO, is cone of death tank ony? update role defaults
+--TODO, get correct spellID for p2 and don't use spell name to broad check all of them
+--Stage One: Storm and Sky
+local warnConeofDeath				= mod:NewSpellAnnounce(248165, 2)
+local warnBlightOrb					= mod:NewSpellAnnounce(248317, 2)
+local warnSkyandSea					= mod:NewSpellAnnounce(255594, 1)
+--Stage Two: The Protector Redeemed
+local warnPhase2					= mod:NewPhaseAnnounce(2, 2)
 
---local specWarnFelclaws				= mod:NewSpecialWarningDefensive(239932, nil, nil, nil, 1, 2)
+--Stage One: Storm and Sky
+local specWarnTorturedRage			= mod:NewSpecialWarningCount(257296, nil, nil, nil, 2, 2)
+local specWarnSweepingScythe		= mod:NewSpecialWarningTaunt(248499, nil, nil, nil, 1, 2)
 --local yellBurstingDreadflame		= mod:NewPosYell(238430, DBM_CORE_AUTO_YELL_CUSTOM_POSITION)
 --local specWarnMalignantAnguish		= mod:NewSpecialWarningInterrupt(236597, "HasInterrupt")
 --local specWarnGTFO					= mod:NewSpecialWarningGTFO(238028, nil, nil, nil, 1, 2)
+--Stage Two: The Protector Redeemed
 
---local timerFelclawsCD				= mod:NewAITimer(25, 239932, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
---local timerRupturingSingularityCD	= mod:NewAITimer(61, 235059, nil, nil, nil, 3)
+--Stage One: Storm and Sky
+local timerSweepingScytheCD			= mod:NewAITimer(25, 248499, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerConeofDeathCD			= mod:NewAITimer(20, 248165, nil, nil, nil, 3)--20-30 in february, power based
+local timerBlightOrbCD				= mod:NewAITimer(20, 248317, nil, nil, nil, 3)
+local timerTorturedRageCD			= mod:NewAITimer(20, 257296, nil, nil, nil, 2, nil, DBM_CORE_HEALER_ICON)
+local timerSkyandSeaCD				= mod:NewAITimer(20, 255594, nil, nil, nil, 5)
+--Stage Two: The Protector Redeemed
 
 --local berserkTimer					= mod:NewBerserkTimer(600)
 
+--Stage One: Storm and Sky
 --local countdownSingularity			= mod:NewCountdown(50, 235059)
+--Stage Two: The Protector Redeemed
 
+--Stage One: Storm and Sky
+local voiceTorturedRage					= mod:NewVoice(257296)--aesoon
+local voiceSweepingScythe				= mod:NewVoice(248499)--tauntboss
 --local voiceMalignantAnguish			= mod:NewVoice(236597, "HasInterrupt")--kickcast
 --local voiceGTFO						= mod:NewVoice(238028, nil, DBM_CORE_AUTO_VOICE4_OPTION_TEXT)--runaway
+--Stage Two: The Protector Redeemed
+
 
 --mod:AddSetIconOption("SetIconOnFocusedDread", 238502, true)
---mod:AddInfoFrameOption(239154, true)
+mod:AddInfoFrameOption(258040, true)--Change to EJ entry since spell not localized
 --mod:AddRangeFrameOption("5/10")
 
+mod.vb.phase = 1
+mod.vb.TorturedRage = 0
+
+--[[
 local debuffFilter
 local UnitDebuff = UnitDebuff
 local playerDebuff = nil
@@ -73,8 +98,21 @@ local function updateRangeFrame(self)
 		DBM.RangeCheck:Hide()
 	end
 end
+--]]
 
 function mod:OnCombatStart(delay)
+	self.vb.phase = 1
+	self.vb.TorturedRage = 0
+	timerConeofDeathCD:Start(1-delay)--was 10 in february
+	timerBlightOrbCD:Start(1-delay)
+	timerTorturedRageCD:Start(1-delay)
+	timerSweepingScytheCD:Start(1-delay)
+	timerSkyandSeaCD:Start(1-delay)
+	if self.Options.InfoFrame then
+		--DBM.InfoFrame:SetHeader(7.3_ARGUS_RAID_DEATH_TITAN_ENERGY)--Validator won't accept this global so disabled for now
+		DBM.InfoFrame:Show(2, "enemypower", 2)
+		--DBM.InfoFrame:Show(7, "function", updateInfoFrame, false, false)
+	end
 	local wowTOC, testBuild = DBM:GetTOC()
 	if not testBuild then
 		DBM:AddMsg(DBM_CORE_NEED_LOGS)
@@ -85,9 +123,9 @@ function mod:OnCombatEnd()
 --	if self.Options.RangeFrame then
 --		DBM.RangeCheck:Hide()
 --	end
---	if self.Options.InfoFrame then
---		DBM.InfoFrame:Hide()
---	end
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:Hide()
+	end
 	local wowTOC, testBuild = DBM:GetTOC()
 	if not testBuild then
 		DBM:AddMsg(DBM_CORE_NEED_LOGS)
@@ -96,25 +134,40 @@ end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 238999 then
-
+	if spellId == 248317 then
+		warnBlightOrb:Show()
+		timerBlightOrbCD:Start()
+	elseif spellId == 257296 then
+		self.vb.TorturedRage = self.vb.TorturedRage + 1
+		specWarnTorturedRage:Show(self.vb.TorturedRage)
+		voiceTorturedRage:Play("aesoon")
+		timerTorturedRageCD:Start()
+	elseif spellId == 255594 then
+		warnSkyandSea:Show()
+		timerSkyandSeaCD:Start()
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 236378 then
-
+	if spellId == 248165 then
+		warnConeofDeath:Show()
+		timerConeofDeathCD:Start()
+	elseif spellId == 248499 then
+		timerSweepingScytheCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 245509 then
-
+	if spellId == 248499 then
+		if not args:IsPlayer() then
+			specWarnSweepingScythe:Show(args.destName)
+			voiceSweepingScythe:Play("tauntboss")
+		end
 	end
 end
-mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+--mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
@@ -123,7 +176,7 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end
 
-
+--[[
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 	if spellId == 228007 and destGUID == UnitGUID("player") and self:AntiSpam(2, 4) then
 		specWarnGTFO:Show()
@@ -144,11 +197,18 @@ function mod:UNIT_DIED(args)
 
 	end
 end
+--]]
 
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, spellGUID)
-	local spellId = tonumber(select(5, strsplit("-", spellGUID)), 10)
-	if spellId == 242377 then
-
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, spellName, _, _, spellId)
+	if spellName == GetSpellInfo(255683) and self.vb.phase < 2 then--Golganneth's Wrath
+		self.vb.phase = 2
+		warnPhase2:Show()
+		timerConeofDeathCD:Stop()
+		timerBlightOrbCD:Stop()
+		timerTorturedRageCD:Stop()
+		timerSweepingScytheCD:Stop()
+		timerSkyandSeaCD:Stop()
+		timerSweepingScytheCD:Start(2)
+		timerTorturedRageCD:Start(2)
 	end
 end
---]]
