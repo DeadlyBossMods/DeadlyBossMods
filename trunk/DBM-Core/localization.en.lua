a42e05b5d2f8ffd9f7a2864a18ebec87cb2e3661
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2017-10-29 05:18:48 +0000

    Text tweaks

--- trunk/DBM-Core/localization.en.lua
+++ trunk/DBM-Core/localization.en.lua
@@ -1,10 +1,10 @@
 
 DBM_CORE_NEED_LOCALS				= "Are you good with programming or languages? If yes, the DBM needs your help in localizing DBM in more languages. If you can help, visit DBM forums or DBM Discord and contact MysticalOS"
 DBM_CORE_NEED_LOGS					= "DBM needs Transcriptor (http://www.wowace.com/addons/transcriptor/) log of this fight to make best mods possible. If you want to help, log this fight with transcriptor and post them to DBM forums or discord"
-DBM_HOW_TO_USE_MOD					= "Welcome to DBM. Type /dbm help for a list of supported commands. To access options type /dbm in your chat to begin configuration. Load specific zones manually to configure any boss specific settings to your liking as well. DBM tries to do this for you by scanning your spec on first run, but some might want additional options turned on anyways."
+DBM_HOW_TO_USE_MOD					= "Welcome to DBM. Type /dbm help for a list of supported commands. To access options type /dbm in your chat to begin configuration. Load specific zones manually to configure any boss specific settings to your liking as well. DBM will setup defaults for your spec, but you may want to fine tune these."
 DBM_SILENT_REMINDER					= "Reminder: DBM is still in silent mode."
 
-DBM_FORUMS_MESSAGE					= "Found a bug or wrong timer? Do you think some mod would need an additional warning, timer or special feature?\nVisit the DBM forums or DBM Discord server and give your feedback."
+DBM_FORUMS_MESSAGE					= "Found a bug or wrong timer? Do you think some mod could use an additional warning, timer or special feature?\nVisit the DBM forums or DBM Discord and give your feedback."
 DBM_FORUMS_COPY_URL_DIALOG			= "Come visit our discussion and support forums"
 DBM_FORUMS_COPY_URL_DIALOG_NEWS		= "To learn more about this new feature and how it works, visit our forums"
 
