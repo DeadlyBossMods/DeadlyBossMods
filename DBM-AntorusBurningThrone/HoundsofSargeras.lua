6e10604cd19044abf6f960c56259496ef8c24179
Adam <MysticalOS@users.noreply.github.com>
2017-12-07 03:19:31 +0000

    kill icon marking, it’s not useful and doesn’t work

--- DBM-AntorusBurningThrone/HoundsofSargeras.lua
+++ DBM-AntorusBurningThrone/HoundsofSargeras.lua
@@ -6,7 +6,7 @@ mod:SetCreatureID(122477, 122135)--122477 F'harg, 122135 Shatug
 mod:SetEncounterID(2074)
 mod:SetZone()
 mod:SetBossHPInfoToHighest()
---mod:SetUsedIcons(1, 2, 3, 4, 5, 6)
+mod:SetUsedIcons(1, 2, 3, 4, 5, 6)
 --mod:SetHotfixNoticeRev(16350)
 mod.respawnTime = 29--Guessed, it's not 4 anymore
 
@@ -56,7 +56,7 @@ local yellEnflamed						= mod:NewShortFadesYell(248815)
 --Shatug
 local specWarnComsumingSphere			= mod:NewSpecialWarningDodge(244131, nil, nil, nil, 2, 2)
 local specWarnWeightOfDarkness			= mod:NewSpecialWarningMoveTo(254429, nil, nil, nil, 1, 2)
-local yellWeightOfDarkness				= mod:NewPosYell(254429, DBM_CORE_AUTO_YELL_CUSTOM_POSITION)
+local yellWeightOfDarkness				= mod:NewYell(254429)
 local yellWeightOfDarknessFades			= mod:NewShortFadesYell(254429)
 local specWarnSiphoned					= mod:NewSpecialWarningMoveTo(248819, nil, nil, nil, 1, 2)
 local yellSiphoned						= mod:NewShortFadesYell(248819)
@@ -96,12 +96,12 @@ local voiceSiphoned						= mod:NewVoice(248819)--gathershare?/killmob on mythic?
 local voiceFlameTouched					= mod:NewVoice(244054)--flameonyou
 local voiceShadowtouched				= mod:NewVoice(244055)--shadowonyou
 
-mod:AddSetIconOption("SetIconOnWeightofDarkness", 254429, true)
+--mod:AddSetIconOption("SetIconOnWeightofDarkness", 254429, true)
 --mod:AddInfoFrameOption(239154, true)
 mod:AddRangeFrameOption("5/8")
 mod:AddBoolOption("SequenceTimers", false)
 
-mod.vb.WeightDarkIcon = 1
+--mod.vb.WeightDarkIcon = 1
 mod.vb.longTimer = 95.9
 mod.vb.mediumTimer = 77
 
@@ -150,7 +150,7 @@ function mod:OnCombatStart(delay)
 	if self:AntiSpam(10, 1) then
 		--Do nothing, it just disables UpdateAllTimers/Focused Power from firing on pull
 	end
-	self.vb.WeightDarkIcon = 1
+	--self.vb.WeightDarkIcon = 1
 	--Fire doggo
 	timerBurningMawCD:Start(8.2-delay)--was same on heroic/mythic, or now
 	timerCorruptingMawCD:Start(8.9-delay)--was same on heroic/normal, for now
@@ -257,7 +257,7 @@ function mod:SPELL_CAST_SUCCESS(args)
 		warnCorruptingMaw:Show(args.destName)
 		timerCorruptingMawCD:Start()
 	elseif spellId == 254429 then
-		self.vb.WeightDarkIcon = 1
+		--self.vb.WeightDarkIcon = 1
 		if not self.Options.SequenceTimers or self:IsEasy() then
 			timerWeightOfDarknessCD:Start()
 		else
@@ -308,13 +308,13 @@ function mod:SPELL_AURA_APPLIED(args)
 		if args:IsPlayer() then
 			specWarnWeightOfDarkness:Show(DBM_ALLY)
 			voiceWeightOfDarkness:Play("gathershare")
-			yellWeightOfDarkness:Yell(self.vb.WeightDarkIcon, args.spellName, self.vb.WeightDarkIcon)
+			yellWeightOfDarkness:Yell()
 			yellWeightOfDarknessFades:Countdown(5)
 		end
-		if self.Options.SetIconOnWeightofDarkness then
-			self:SetIcon(args.destName, self.vb.WeightDarkIcon)
-		end
-		self.vb.WeightDarkIcon = self.vb.WeightDarkIcon + 1
+		--if self.Options.SetIconOnWeightofDarkness then
+			--self:SetIcon(args.destName, self.vb.WeightDarkIcon)
+		--end
+		--self.vb.WeightDarkIcon = self.vb.WeightDarkIcon + 1
 	elseif spellId == 244054 then--Flametouched
 		if args:IsPlayer() then
 			specWarnFlameTouched:Show(self:IconNumToTexture(7))--Red X for flame (more voted on red x than orange circle)
@@ -348,9 +348,9 @@ function mod:SPELL_AURA_REMOVED(args)
 			end
 		end
 	elseif spellId == 254429 then
-		if self.Options.SetIconOnWeightofDarkness then
-			self:SetIcon(args.destName, 0)
-		end
+		--if self.Options.SetIconOnWeightofDarkness then
+			--self:SetIcon(args.destName, 0)
+		--end
 		if args:IsPlayer() then
 			yellWeightOfDarknessFades:Cancel()
 		end
