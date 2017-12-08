03e9e6565e9aa3fb2f2bc229072cd49e079ae23e
Paul Emmerich <emmericp@net.in.tum.de>
2017-12-08 19:51:26 +0000

    tag as 7.3.11

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -42,8 +42,8 @@
 -------------------------------
 DBM = {
 	Revision = tonumber(("$Revision$"):sub(12, -3)),
-	DisplayVersion = "7.3.11 alpha", -- the string that is shown as version
-	ReleaseRevision = 16934 -- the revision of the latest stable version that is available
+	DisplayVersion = "7.3.11", -- the string that is shown as version
+	ReleaseRevision = 16951 -- the revision of the latest stable version that is available
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
