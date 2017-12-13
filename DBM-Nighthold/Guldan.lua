a3455f04279d9a5ef0dfbf2cba57fec6464779be
Adam <MysticalOS@users.noreply.github.com>
2017-12-13 00:44:22 +0000

    Compatability layer for GetSectionInfo change in 7.3.5. it’s ugly but temporary to handle DBM working on ptr and live at same time

--- DBM-Nighthold/Guldan.lua
+++ DBM-Nighthold/Guldan.lua
@@ -35,9 +35,9 @@ or (ability.id = 227427 or ability.id = 206516) and type = "applybuff"
 or (ability.id = 227427 or ability.id = 206516) and type = "removebuff"
 --]]
 
-local Kurazmal = EJ_GetSectionInfo(13121)
-local Vethriz = EJ_GetSectionInfo(13124)
-local Dzorykx = EJ_GetSectionInfo(13129)
+local Kurazmal = DBM:EJ_GetSectionInfo(13121)
+local Vethriz = DBM:EJ_GetSectionInfo(13124)
+local Dzorykx = DBM:EJ_GetSectionInfo(13129)
 
 --Stage One: The Council of Elders
 ----Gul'dan
