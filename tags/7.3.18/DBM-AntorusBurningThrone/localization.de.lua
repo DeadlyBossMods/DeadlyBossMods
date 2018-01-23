cfc5e1b29b9920fcd2768e09980a1a3db09bfba0
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-01-23 21:33:33 +0000

    tag as 7.3.18

---
+++ tags/7.3.18/DBM-AntorusBurningThrone/localization.de.lua
@@ -0,0 +1,95 @@
+﻿if GetLocale() ~= "deDE" then return end
+local L
+
+---------------------------
+-- Garothi Worldbreaker --
+---------------------------
+L= DBM:GetModLocalization(1992)
+
+L:SetTimerLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+---------------------------
+-- Hounds of Sargeras --
+---------------------------
+L= DBM:GetModLocalization(1987)
+
+L:SetOptionLocalization({
+	SequenceTimers =	"Ändere die Reihung der Timer für die Abklingzeiten der Bossfähigkeiten zugunsten der Übersichtlichkeit auf Kosten der Genauigkeit (1-2s zeitiger)"
+})
+
+---------------------------
+-- War Council --
+---------------------------
+L= DBM:GetModLocalization(1997)
+
+---------------------------
+-- Eonar, the Lifebinder --
+---------------------------
+L= DBM:GetModLocalization(2025)
+
+---------------------------
+-- Portal Keeper Hasabel --
+---------------------------
+L= DBM:GetModLocalization(1985)
+
+L:SetOptionLocalization({
+	ShowAllPlatforms =	"Zeige alle Ansagen unabhängig von der Spielerplattform"
+})
+
+---------------------------
+-- Imonar the Soulhunter --
+---------------------------
+L= DBM:GetModLocalization(2009)
+
+---------------------------
+-- Kin'garoth --
+---------------------------
+L= DBM:GetModLocalization(2004)
+
+L:SetOptionLocalization({
+	InfoFrame =	"Zeige Infofenster für Kampfübersicht"
+})
+
+---------------------------
+-- Varimathras --
+---------------------------
+L= DBM:GetModLocalization(1983)
+
+---------------------------
+-- The Coven of Shivarra --
+---------------------------
+L= DBM:GetModLocalization(1986)
+
+L:SetTimerLocalization({
+	timerBossIncoming		= DBM_INCOMING
+})
+
+L:SetOptionLocalization({
+	timerBossIncoming		= "Zeige Zeit bis nächsten Bosswechsel"
+})
+
+---------------------------
+-- Aggramar --
+---------------------------
+L= DBM:GetModLocalization(1984)
+
+---------------------------
+-- Argus the Unmaker --
+---------------------------
+L= DBM:GetModLocalization(2031)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("AntorusTrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash des Antorus"
+})
