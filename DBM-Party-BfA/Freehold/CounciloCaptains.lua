a44a910407569665137bc233e38e7fe1adc8cb94
Adam <MysticalOS@users.noreply.github.com>
2018-02-08 08:31:05 +0000

    Fixed a bug with dungeon assignment

--- DBM-Party-BfA/Freehold/CounciloCaptains.lua
+++ DBM-Party-BfA/Freehold/CounciloCaptains.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod(2093, "DBM-Party-BfA", 1, 1001)
+local mod	= DBM:NewMod(2093, "DBM-Party-BfA", 2, 1001)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
