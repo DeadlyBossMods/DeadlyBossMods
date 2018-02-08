a293fa7df9c85d324f7186aaebe44f5db68cf68b
Adam <MysticalOS@users.noreply.github.com>
2018-02-08 08:15:41 +0000

    stub in the two dungeons from alpha. I have data for rest of dungeons, but so many place holder names, best to only push stubs for the ones that are currently being tested since they’ll be populated soon.

---
+++ DBM-Party-BfA/localization.en.lua
@@ -0,0 +1,65 @@
+local L
+
+-----------------------
+-- <<<Atal'Dazar >>> --
+-----------------------
+-----------------------
+-- Priestess Alun'za --
+-----------------------
+L= DBM:GetModLocalization(2082)
+
+-----------------------
+-- Vol'kaal --
+-----------------------
+L= DBM:GetModLocalization(2036)
+
+-----------------------
+-- Rezan --
+-----------------------
+L= DBM:GetModLocalization(2083)
+
+-----------------------
+-- Yazma --
+-----------------------
+L= DBM:GetModLocalization(2030)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("AtalDazarTrash")
+
+L:SetGeneralLocalization({
+	name =	"Atal'Dazar Trash"
+})
+
+-----------------------
+-- <<<Freehold >>> --
+-----------------------
+-----------------------
+-- Skycap'n Kragg --
+-----------------------
+L= DBM:GetModLocalization(2102)
+
+-----------------------
+-- Council o' Captains --
+-----------------------
+L= DBM:GetModLocalization(2093)
+
+-----------------------
+-- Ring of Booty --
+-----------------------
+L= DBM:GetModLocalization(2094)
+
+-----------------------
+-- Harlan Sweete --
+-----------------------
+L= DBM:GetModLocalization(2095)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("FreeholdTrash")
+
+L:SetGeneralLocalization({
+	name =	"Freehold Trash"
+})
