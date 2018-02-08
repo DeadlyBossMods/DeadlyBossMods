98af135acf28382c140adead1fd7e64b495a0a0f
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-02-08 08:31:05 +0000

    Fixed a bug with dungeon assignment

--- trunk/DBM-Party-BfA/Freehold/FreeholdTrash.lua
+++ trunk/DBM-Party-BfA/Freehold/FreeholdTrash.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod("FreeholdTrash", "DBM-Party-BfA", 1)
+local mod	= DBM:NewMod("FreeholdTrash", "DBM-Party-BfA", 2)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
