9cdb964b85a791d01b2830ca35e78e492f47e0fc
Adam <MysticalOS@users.noreply.github.com>
2018-02-09 01:09:09 +0000

    Change couple names with some new encounter data, and add available encounterIds which will enable pull/kill/wipe detection for most of these

--- DBM-Party-BfA/Boralus/Ashvane.lua
+++ DBM-Party-BfA/Boralus/Ashvane.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 --mod:SetCreatureID(98542)
---mod:SetEncounterID(1832)
+mod:SetEncounterID(2100)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
