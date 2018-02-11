4eef180644b30b7780d69e53461a9d84f598527e
Adam <MysticalOS@users.noreply.github.com>
2018-02-11 07:33:26 +0000

    3/4 for freehold, will do sweete tommorrow.

--- DBM-Party-BfA/Freehold/HarlanSweete.lua
+++ DBM-Party-BfA/Freehold/HarlanSweete.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(2095, "DBM-Party-BfA", 2, 1001)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(98542)
+mod:SetCreatureID(126983)
 mod:SetEncounterID(2096)
 mod:SetZone()
 
