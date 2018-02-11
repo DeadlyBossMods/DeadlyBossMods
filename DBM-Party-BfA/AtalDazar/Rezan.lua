afd070d434fe92f7d9e087df6d9a52a0a1e613db
Adam <MysticalOS@users.noreply.github.com>
2018-02-11 02:51:10 +0000

    Push Atal’Dazar, Freehold will come later

--- DBM-Party-BfA/AtalDazar/Rezan.lua
+++ DBM-Party-BfA/AtalDazar/Rezan.lua
@@ -2,53 +2,71 @@ local mod	= DBM:NewMod(2083, "DBM-Party-BfA", 1, 968)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
---mod:SetCreatureID(98542)
+mod:SetCreatureID(122963)
 mod:SetEncounterID(2086)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
---[[
 mod:RegisterEventsInCombat(
---	"SPELL_AURA_APPLIED",
---	"SPELL_CAST_START"
+	"SPELL_AURA_APPLIED 257407",
+	"SPELL_CAST_START 255371 257407",
+	"SPELL_CAST_SUCCESS 255434"
 )
 
---local warnSwirlingScythe			= mod:NewTargetAnnounce(195254, 2)
+--TODO, relog fight, when logging fight doesn't cause client to crash, then use real timers not AI timers
+--TODO, try to warn pursuit faster is possible
+local warnPursuit				= mod:NewTargetAnnounce(257407, 2)
 
---local specWarnReapSoul			= mod:NewSpecialWarningDodge(194956, "Tank", nil, nil, 3)
---local yellSwirlingScythe			= mod:NewYell(195254)
---local specWarnGTFO				= mod:NewSpecialWarningGTFO(238028, nil, nil, nil, 1, 2)
+local specWarnTeeth				= mod:NewSpecialWarningDefensive(255434, "Tank", nil, nil, 1, 2)
+local specWarnFear				= mod:NewSpecialWarningDodge(255371, nil, nil, nil, 3, 2)--Dodge warning on purpose, you dodge it by LOS behind pillar
+local yellPursuit				= mod:NewYell(257407)
+local specWarnPursuit			= mod:NewSpecialWarningRun(257407, nil, nil, nil, 4, 2)
 
---local timerReapSoulCD				= mod:NewNextTimer(13, 194956, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON..DBM_CORE_DEADLY_ICON)
-
---mod:AddRangeFrameOption(5, 194966)
+local timerTeethCD				 mod:NewAITimer(13, 255434, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerFearCD				 mod:NewAITimer(13, 255371, nil, nil, nil, 2)
+local timerPursuitCD			 mod:NewAITimer(13, 257407, nil, nil, nil, 3)
 
 function mod:OnCombatStart(delay)
-
-end
-
-function mod:OnCombatEnd()
---	if self.Options.RangeFrame then
---		DBM.RangeCheck:Hide()
---	end
+	timerTeethCD:Start(1)
+	timerFearCD:Start(1)
+	timerPursuitCD:Start(1)
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 194966 then
-	
+	if spellId == 257407 then
+		if args:IsPlayer() then
+			specWarnPursuit:Show()
+			specWarnPursuit:Play("justrun")
+			yellPursuit:Yell()
+		else
+			warnPursuit:Show(args.destName)
+		end
 	end
 end
---mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 195254 then
-
+	if spellId == 255371 then
+		specWarnFear:Show()
+		specWarnFear:Play("findshelter")
+		timerFearCD:Start()
+	elseif spellId == 257407 then
+		timerPursuitCD:Start()
 	end
 end
 
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+	if spellId == 255434 then
+		specWarnTeeth:Show()
+		specWarnTeeth:Play("defensive")
+		timerTeethCD:Start()
+	end
+end
+
+--[[
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 	if spellId == 228007 and destGUID == UnitGUID("player") and self:AntiSpam(2, 4) then
 		specWarnGTFO:Show()
