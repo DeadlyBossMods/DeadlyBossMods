57a1cb9d1c6b3c67b7f317a88ec8260ee7dc996d
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-02-12 00:27:16 +0000

    Finished Sweete mod Popualted Atal and freehold trash mods with notable interrupts

--- trunk/DBM-Party-BfA/AtalDazar/AtalDazarTrash.lua
+++ trunk/DBM-Party-BfA/AtalDazar/AtalDazarTrash.lua
@@ -7,27 +7,32 @@ mod:SetZone()
 
 mod.isTrashMod = true
 
---[[
 mod:RegisterEvents(
-	"SPELL_CAST_START",
-	"SPELL_AURA_APPLIED",
-	"SPELL_CAST_SUCCESS"
+	"SPELL_CAST_START 253562 255041"
+--	"SPELL_AURA_APPLIED",
+--	"SPELL_CAST_SUCCESS"
 )
 
 --local warnSoulEchoes				= mod:NewTargetAnnounce(194966, 2)
 
---local yellArrowBarrage				= mod:NewYell(200343)
---local specWarnWhirlOfFlame			= mod:NewSpecialWarningDodge(221634, nil, nil, nil, 2, 2)
---local specWarnDarkMending			= mod:NewSpecialWarningInterrupt(225573, "HasInterrupt", nil, nil, 1, 2)
+--local yellArrowBarrage			= mod:NewYell(200343)
+--local specWarnWhirlOfFlame		= mod:NewSpecialWarningDodge(221634, nil, nil, nil, 2, 2)
+local specWarnWildFire				= mod:NewSpecialWarningInterrupt(253562, "HasInterrupt", nil, nil, 1, 2)
+local specWarnTerrifyingScreech		= mod:NewSpecialWarningInterrupt(255041, "HasInterrupt", nil, nil, 1, 2)
 
 function mod:SPELL_CAST_START(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
-	if spellId == 200261 then
-	
+	if spellId == 253562 and self:CheckInterruptFilter(args.sourceGUID) then
+		specWarnWildFire:Show()
+		specWarnWildFire:Play("kickcast")
+	elseif spellId == 255041 and self:CheckInterruptFilter(args.sourceGUID) then
+		specWarnTerrifyingScreech:Show()
+		specWarnTerrifyingScreech:Play("kickcast")
 	end
 end
 
+--[[
 function mod:SPELL_AURA_APPLIED(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
