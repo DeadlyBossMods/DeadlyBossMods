b4cee6464aa4cbf8379db671cf47f1fbf8e7ed86
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-02-12 02:20:55 +0000

    Zandalar template

---
+++ trunk/DBM-Zandalar/Tzane.lua
@@ -0,0 +1,83 @@
+local mod	= DBM:NewMod(2139, "DBM-Zandalar", nil, 1029)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+mod:SetCreatureID(132701)
+--mod:SetEncounterID(1880)
+mod:SetReCombatTime(20)
+mod:SetZone()
+--mod:SetMinSyncRevision(11969)
+
+mod:RegisterCombat("combat")
+
+--[[
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_START",
+	"SPELL_CAST_SUCCESS",
+	"SPELL_AURA_APPLIED"
+)
+
+--local warnMothersEmbrace			= mod:NewTargetAnnounce(219045, 3)
+
+--local specWarnFelGeyser			= mod:NewSpecialWarningDodge(218823, nil, nil, nil, 2, 2)
+--local yellSwirlingScythe			= mod:NewYell(195254)
+--local specWarnGTFO				= mod:NewSpecialWarningGTFO(238028, nil, nil, nil, 1, 2)
+
+--local timerFelGeyserCD			= mod:NewAITimer(16, 218823, nil, nil, nil, 2)
+
+--mod:AddRangeFrameOption(5, 194966)
+--mod:AddReadyCheckOption(37460, false)
+
+function mod:OnCombatStart(delay, yellTriggered)
+	if yellTriggered then
+
+	end
+end
+
+function mod:OnCombatEnd()
+--	if self.Options.RangeFrame then
+--		DBM.RangeCheck:Hide()
+--	end
+end
+
+function mod:SPELL_CAST_START(args)
+	local spellId = args.spellId
+	if spellId == 218823 then
+
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+	if spellId == 218823 then
+
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	local spellId = args.spellId
+	if spellId == 219045 then
+
+	end
+end
+
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
+	if spellId == 228007 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
+		specWarnGTFO:Show()
+		specWarnGTFO:Play("runaway")
+	end
+end
+mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
+
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 124396 then
+		
+	end
+end
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
+	if spellId == 257939 then
+	end
+end
+--]]
