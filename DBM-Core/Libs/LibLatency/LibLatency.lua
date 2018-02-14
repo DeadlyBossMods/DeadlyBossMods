1caafff9e1fe9fa61c430a3daa8f32b4edacc55c
Adam <MysticalOS@users.noreply.github.com>
2018-02-14 06:22:40 +0000

    Fix duplication

--- DBM-Core/Libs/LibLatency/LibLatency.lua
+++ DBM-Core/Libs/LibLatency/LibLatency.lua
@@ -24,10 +24,10 @@ local callbackMap = LL.callbackMap
 local frame = LL.frame
 
 local next, type, error, tonumber, format, match = next, type, error, tonumber, string.format, string.match
-local Ambiguate, GetTime, GetNetStats, IsInGroup, IsInRaid, SendAddonMessage = Ambiguate, GetTime, GetNetStats, IsInGroup, IsInRaid, SendAddonMessage
+local Ambiguate, GetTime, GetNetStats, IsInGroup, IsInRaid, SendAddonMessage = Ambiguate, GetTime, GetNetStats, IsInGroup, IsInRaid, SendAddonMessage or C_ChatInfo.SendAddonMessage
 local pName = UnitName("player")
 
-local RegisterAddonMessagePrefix, SendAddonMessage = RegisterAddonMessagePrefix or C_ChatInfo.RegisterAddonMessagePrefix, SendAddonMessage or C_ChatInfo.SendAddonMessage
+local RegisterAddonMessagePrefix = RegisterAddonMessagePrefix or C_ChatInfo.RegisterAddonMessagePrefix
 
 RegisterAddonMessagePrefix("Lag")
 frame:SetScript("OnEvent", function(_, _, prefix, msg, channel, sender)
