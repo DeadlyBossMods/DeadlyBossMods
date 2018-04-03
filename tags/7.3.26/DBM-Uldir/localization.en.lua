460127ac34137a5b767624a35fdac9fa3c86bac2
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-04-03 20:41:46 +0000

    tag as 7.3.26

---
+++ tags/7.3.26/DBM-Uldir/localization.en.lua
@@ -0,0 +1,57 @@
+local L
+
+---------------
+-- Titan Keeper Hezrel --
+---------------
+L= DBM:GetModLocalization(2168)
+
+L:SetWarningLocalization({
+})
+
+L:SetTimerLocalization({
+})
+
+L:SetOptionLocalization({
+})
+
+L:SetMiscLocalization({
+})
+
+---------------------------
+-- Taloc the Corrupted --
+---------------------------
+L= DBM:GetModLocalization(2168)
+
+---------------------------
+-- MOTHER --
+---------------------------
+L= DBM:GetModLocalization(2167)
+
+---------------------------
+-- Fetid Devourer --
+---------------------------
+L= DBM:GetModLocalization(2146)
+
+---------------------------
+-- Zek'vhozj --
+---------------------------
+L= DBM:GetModLocalization(2169)
+
+---------------------------
+-- Blood of G'huun --
+---------------------------
+L= DBM:GetModLocalization(2166)
+
+------------------
+-- G'huun --
+------------------
+L= DBM:GetModLocalization(2147)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("UldirTrash")
+
+L:SetGeneralLocalization({
+	name =	"Uldir Trash"
+})
