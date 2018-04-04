b1f94f5bedf85c4533af4d63e3c1d9b2c3680847
Adam <MysticalOS@users.noreply.github.com>
2018-04-04 03:23:45 +0000

    Some 8.x drycode fixes. unable to really test more right now

--- DBM-Zandalar/Tzane.lua
+++ DBM-Zandalar/Tzane.lua
@@ -117,7 +117,7 @@ function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
 	if spellId == 257939 then
 	end
 end
