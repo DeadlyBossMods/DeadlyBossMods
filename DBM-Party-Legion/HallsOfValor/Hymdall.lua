579837b557b303b44606d4275469a9ba056d9b82
Adam <MysticalOS@users.noreply.github.com>
2018-04-18 02:36:11 +0000

    Just disable the smart prediction for now, since fight is bugged and blizz unlikley to ever fix it (sometimes aggramar can re-use a combo before he’s used all 4. happens maybe 1 in 10 pulls, but it’s enough to ruin having a better mod, since having prediction be wrong 10% of time is unacceptable. so now there is no prediction)

--- DBM-Party-Legion/HallsOfValor/Hymdall.lua
+++ DBM-Party-Legion/HallsOfValor/Hymdall.lua
@@ -56,8 +56,7 @@ function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, spellGUID)
-	local spellId = tonumber(select(5, strsplit("-", spellGUID)), 10)
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 	if spellId == 193092 then
 		warnSweep:Show()
 	end
