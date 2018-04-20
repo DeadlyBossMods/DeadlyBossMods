28aa7cde4980c688f28d61e6c858553f5e31d336
Adam <MysticalOS@users.noreply.github.com>
2018-04-20 18:00:27 +0000

    Fix rangecheck option

--- DBM-AntorusBurningThrone/Aggramar.lua
+++ DBM-AntorusBurningThrone/Aggramar.lua
@@ -72,7 +72,7 @@ local countdownWakeofFlame				= mod:NewCountdown("AltTwo24", 244693, "-Tank")
 mod:AddSetIconOption("SetIconOnBlaze2", 254452, false)--Both off by default, both conflit with one another
 mod:AddSetIconOption("SetIconOnAdds", 244903, false, true)--Both off by default, both conflit with one another
 mod:AddInfoFrameOption(244688, true)
-mod:AddRangeFrameOption("6", "Ranged")
+mod:AddRangeFrameOption("6", nil, "Ranged")
 mod:AddNamePlateOption("NPAuraOnPresence", 244903)
 mod:AddBoolOption("ignoreThreeTank", true)
 
