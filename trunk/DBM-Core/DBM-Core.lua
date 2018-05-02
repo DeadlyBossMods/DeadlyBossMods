066d444547132e773580c14b088bde6a8494d485
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-05-02 18:41:58 +0000

    Update version check

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -401,7 +401,7 @@ local breakTimerStart
 local AddMsg
 local delayedFunction
 
-local fakeBWVersion, fakeBWHash = 93, "aaab2d6"
+local fakeBWVersion, fakeBWHash = 97, "1bd751b"
 local versionQueryString, versionResponseString = "Q^%d^%s", "V^%d^%s"
 
 local enableIcons = true -- set to false when a raid leader or a promoted player has a newer version of DBM
