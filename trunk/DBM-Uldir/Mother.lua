ce3d81bc507b0d8fc13221c0e42568e779858a6d
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-05-03 00:30:04 +0000

    Finish mother mod from todays testing

--- trunk/DBM-Uldir/Mother.lua
+++ trunk/DBM-Uldir/Mother.lua
@@ -2,12 +2,9 @@ local mod	= DBM:NewMod(2167, "DBM-Uldir", nil, 1031)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(135452)
+mod:SetCreatureID(135452)--136429 Chamber 01, 137022 Chamber 02, 137023 Chamber 03
 mod:SetEncounterID(2141)
---mod:DisableESCombatDetection()
 mod:SetZone()
---mod:SetBossHPInfoToHighest()
---mod:SetUsedIcons(1, 2, 3, 4, 5, 6)
 --mod:SetHotfixNoticeRev(16950)
 --mod:SetMinSyncRevision(16950)
 --mod.respawnTime = 35
@@ -16,74 +13,105 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 267787 268198",
---	"SPELL_CAST_SUCCESS",
+	"SPELL_CAST_SUCCESS 267795 267945 267885 267878 269827 269051 268089",
 	"SPELL_AURA_APPLIED 267787 274205",
-	"SPELL_AURA_APPLIED_DOSE 267787",
---	"SPELL_AURA_REMOVED",
---	"SPELL_PERIODIC_DAMAGE",
---	"SPELL_PERIODIC_MISSED",
---	"UNIT_DIED",
-	"UNIT_SPELLCAST_SUCCEEDED boss1"
+	"SPELL_AURA_APPLIED_DOSE 267787"
 )
 
---TODO, cleansing purge timer/countdown
---TODO, Review Depleted Energy
---TODO, add flame vents, wind tunnels, and surgical beams. They have too many spellIds/scripts and adding wrong ones could be spammy nightmare
+--TODO, if tanks are still able to dodge scalpel in next test, refactor warning to be a shockwave/dodge instead of tank swap mechanic
 --local warnXorothPortal				= mod:NewSpellAnnounce(244318, 2, nil, nil, nil, nil, nil, 7)
+local warnSunderingScalpelCast			= mod:NewCastAnnounce(267787, 2, nil, "Tank")
 local warnSunderingScalpel				= mod:NewStackAnnounce(267787, 2, nil, "Tank")
+local warnWindTunnel					= mod:NewSpellAnnounce(267945, 2)
 local warnDepletedEnergy				= mod:NewSpellAnnounce(274205, 1)
+local warnCleansingPurgeFinish			= mod:NewTargetNoFilterAnnounce(268089, 4)
 
 local specWarnSunderingScalpel			= mod:NewSpecialWarningStack(267787, nil, 2, nil, nil, 1, 6)
 local specWarnSunderingScalpelOther		= mod:NewSpecialWarningTaunt(267787, nil, nil, nil, 1, 2)
+local specWarnPurifyingFlame			= mod:NewSpecialWarningDodge(267787, nil, nil, nil, 2, 2)
 local specWarnClingingCorruption		= mod:NewSpecialWarningInterrupt(268198, "HasInterrupt", nil, nil, 1, 2)
---local yellFelSilkWrap					= mod:NewYell(244949)
---local specWarnGTFO					= mod:NewSpecialWarningGTFO(238028, nil, nil, nil, 1, 2)
+local specWarnSurgicalBeam				= mod:NewSpecialWarningDodge(269827, nil, nil, nil, 3, 2)
 
 --mod:AddTimerLine(Nexus)
-local timerSunderingScalpelCD			= mod:NewAITimer(12.1, 267787, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerSunderingScalpelCD			= mod:NewNextTimer(23.1, 267787, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerPurifyingFlameCD				= mod:NewNextTimer(23.1, 267795, nil, nil, nil, 3, nil, DBM_CORE_DEADLY_ICON)
+local timerWindTunnelCD					= mod:NewNextTimer(40.1, 267945, nil, nil, nil, 2)
+local timerSurgicalBeamCD				= mod:NewNextTimer(30, 269827, nil, nil, nil, 3, nil, DBM_CORE_DEADLY_ICON)
+local timerCleansingFlameCD				= mod:NewCastSourceTimer(180, 268089, nil, nil, nil, 6)
 
 --local berserkTimer					= mod:NewBerserkTimer(600)
 
---local countdownCollapsingWorld			= mod:NewCountdown(50, 243983, true, 3, 3)
---local countdownSunderingScalpel				= mod:NewCountdown("Alt12", 244016, false, 2, 3)
---local countdownFelstormBarrage			= mod:NewCountdown("AltTwo32", 244000, nil, nil, 3)
+local countdownPurifyingFlame			= mod:NewCountdown(50, 267795, true, 3, 3)
+local countdownSunderingScalpel			= mod:NewCountdown("Alt23", 267787, "Tank", 2, 3)
+local countdownSurgicalBeam				= mod:NewCountdown("AltTwo30", 269827, nil, nil, 4)
 
---mod:AddSetIconOption("SetIconGift", 255594, true)
---mod:AddRangeFrameOption("8/10")
---mod:AddBoolOption("ShowAllPlatforms", false)
---mod:AddInfoFrameOption(258040, true)
+mod:AddInfoFrameOption(268095, true)
 
 function mod:OnCombatStart(delay)
-	timerSunderingScalpelCD:Start(1-delay)
+	timerSunderingScalpelCD:Start(5.9-delay)
+	countdownSunderingScalpel:Start(5.9-delay)
+	timerPurifyingFlameCD:Start(10.8-delay)
+	countdownPurifyingFlame:Start(10.8-delay)
+	timerWindTunnelCD:Start(20.6-delay)
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:Show(4, "enemypower", 1)
+	end
 end
 
 function mod:OnCombatEnd()
---	if self.Options.RangeFrame then
---		DBM.RangeCheck:Hide()
---	end
---	if self.Options.InfoFrame then
---		DBM.InfoFrame:Hide()
---	end
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:Hide()
+	end
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 267787 then
+		warnSunderingScalpelCast:Show()
 		timerSunderingScalpelCD:Start()
+		countdownSunderingScalpel:Start()
 	elseif spellId == 268198 and self:CheckInterruptFilter(args.sourceGUID) then
 		specWarnClingingCorruption:Show(args.sourceName)
 		specWarnClingingCorruption:Play("kickcast")
 	end
 end
 
---[[
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 245050 then--Delusions
-
+	if spellId == 267795 then
+		specWarnPurifyingFlame:Show()
+		specWarnPurifyingFlame:Play("watchstep")
+		timerPurifyingFlameCD:Start()
+		countdownPurifyingFlame:Start()
+	elseif spellId == 267878 then--Directional IDs for winds (Coming from east blowing to west?)
+		--warnWindTunnel:Show()
+		DBM:Debug("what way is wind blowing for spellId :"..spellId)
+	elseif spellId == 267885 then--Directional IDs for winds (Coming from west blowing to east?)
+		--warnWindTunnel:Show()
+		DBM:Debug("what way is wind blowing for spellId :"..spellId)
+	elseif spellId == 267945 then--Global Id for winds
+		warnWindTunnel:Show()
+		timerWindTunnelCD:Show()
+	elseif spellId == 269827 then
+		specWarnSurgicalBeam:Show()
+		specWarnSurgicalBeam:Play("laserrun")
+		specWarnSurgicalBeam:ScheduleVoice(1.5, "keepmove")
+		timerSurgicalBeamCD:Start()
+		countdownSurgicalBeam:Start()
+	elseif spellId == 269051 then--Begin Cast of Cleansing Purge
+		--136429 Chamber 01, 137022 Chamber 02, 137023 Chamber 03
+		local cid = self:GetCIDFromGUID(args.destGUID)
+		if cid == 136429 then
+			timerCleansingFlameCD:Start(180, 1)
+		elseif cid == 137022 then
+			timerCleansingFlameCD:Start(180, 2)
+		else
+			timerCleansingFlameCD:Start(180, 3)
+		end
+	elseif spellId == 268089 then--End Cast of Cleansing Purge
+		warnCleansingPurgeFinish:Show(args.sourceName)
 	end
 end
---]]
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
@@ -117,33 +145,3 @@ function mod:SPELL_AURA_APPLIED(args)
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
-
---[[
-function mod:SPELL_AURA_REMOVED(args)
-	local spellId = args.spellId
-	if spellId == 244383 then
-
-	end
-end
-
-function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 228007 and destGUID == UnitGUID("player") and self:AntiSpam(2, 4) then
-		specWarnGTFO:Show()
-		specWarnGTFO:Play("runaway")
-	end
-end
-mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
-
-function mod:UNIT_DIED(args)
-	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 136315 then--Remnant of Corruption
-
-	end
-end
-
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
-	if spellId == 257939 then
-
-	end
-end
---]]
