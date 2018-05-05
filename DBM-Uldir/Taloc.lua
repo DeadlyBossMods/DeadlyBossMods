17c2167de6788cd6c131be904b98f3f539752cbc
Adam <MysticalOS@users.noreply.github.com>
2018-05-05 01:49:34 +0000

    Taloc Update

--- DBM-Uldir/Taloc.lua
+++ DBM-Uldir/Taloc.lua
@@ -2,11 +2,9 @@ local mod	= DBM:NewMod(2168, "DBM-Uldir", nil, 1031)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(134924)--Taloc 122276 or 134924
+mod:SetCreatureID(137119)--Taloc
 mod:SetEncounterID(2144)
---mod:DisableESCombatDetection()
 mod:SetZone()
---mod:SetBossHPInfoToHighest()
 --mod:SetUsedIcons(1, 2, 3)
 --mod:SetHotfixNoticeRev(16950)
 --mod:SetMinSyncRevision(16950)
@@ -15,23 +13,20 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 271296 271728",
-	"SPELL_AURA_APPLIED 271224 271965",
+	"SPELL_CAST_START 271296 271728 271895",
+	"SPELL_AURA_APPLIED 271224 271965 275270",
 --	"SPELL_AURA_APPLIED_DOSE",
 	"SPELL_AURA_REMOVED 271225 271965",
 	"SPELL_PERIODIC_DAMAGE 270290",
 	"SPELL_PERIODIC_MISSED 270290",
 --	"UNIT_DIED",
-	"UNIT_SPELLCAST_START boss1"
+	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---TODO, add icon setting later when a good cleanup method is detectable.
---TODO, way to detect Volatile Droplet fixates? Maybe scan all nearby nameplates for Status 3 threat?
---TODO, detect Blood Spawns
---TODO, verify/fix SanguineStatic
---TODO, verify GTFO
 local warnPoweringDown					= mod:NewSpellAnnounce(271965, 2, nil, nil, nil, nil, nil, 2)
 local warnPlastmaDischarge				= mod:NewTargetAnnounce(271225, 2)
+local warnPoweringDownOver				= mod:NewEndAnnounce(271965, 2, nil, nil, nil, nil, nil, 2)
+local warnFixate						= mod:NewTargetAnnounce(275270, 2)
 
 local specWarnPlasmaDischarge			= mod:NewSpecialWarningMoveAway(271225, nil, nil, nil, 3, 2)
 local yellPlasmaDischarge				= mod:NewYell(271225)
@@ -39,14 +34,15 @@ local specWarnCudgelofGore				= mod:NewSpecialWarningMoveTo(271296, nil, nil, ni
 local specWarnCudgelofGoreEveryone		= mod:NewSpecialWarningRun(271296, nil, nil, nil, 4, 2)
 local specWarnRetrieveCudgel			= mod:NewSpecialWarningDodge(271728, nil, nil, nil, 2, 2)
 local specWarnSanguineStatic			= mod:NewSpecialWarningDodge(272582, nil, nil, nil, 2, 2)
+local specWarnFixate					= mod:NewSpecialWarningYou(275270, nil, nil, nil, 1, 2)
 local specWarnGTFO						= mod:NewSpecialWarningGTFO(270290, nil, nil, nil, 1, 2)
 
 mod:AddTimerLine(BOSS)
-local timerPlasmaDischargeCD			= mod:NewAITimer(12.1, 271225, nil, nil, nil, 3)--Change to count later
-local timerCudgelOfGoreCD				= mod:NewAITimer(12.1, 271296, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)
-local timerSanguineStaticCD				= mod:NewAITimer(12.1, 272582, nil, nil, nil, 3)
+local timerPlasmaDischargeCD			= mod:NewCDTimer(30.4, 271225, nil, nil, nil, 3)--30.4-42
+local timerCudgelOfGoreCD				= mod:NewCDTimer(57.4, 271296, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)--57.4-63
+local timerSanguineStaticCD				= mod:NewCDTimer(57.4, 272582, nil, nil, nil, 3)--57.4-63
 mod:AddTimerLine(DBM:GetSpellInfo(271965))
-local timerPoweredDown					= mod:NewBuffActiveTimer(90, 271965, nil, nil, nil, 6)
+local timerPoweredDown					= mod:NewBuffActiveTimer(88.6, 271965, nil, nil, nil, 6)
 
 --local berserkTimer					= mod:NewBerserkTimer(600)
 
@@ -57,14 +53,16 @@ local timerPoweredDown					= mod:NewBuffActiveTimer(90, 271965, nil, nil, nil, 6
 mod:AddSetIconOption("SetIconPlasmaDischarge", 271225, true)
 --mod:AddRangeFrameOption("8/10")
 --mod:AddBoolOption("ShowAllPlatforms", false)
-mod:AddInfoFrameOption(272584, true)
+mod:AddInfoFrameOption(275270, true)
 
 local bloodStorm = DBM:GetSpellInfo(270290)
+local ignoreGTFO = false
 
 function mod:OnCombatStart(delay)
-	timerPlasmaDischargeCD:Start(1-delay)
-	timerCudgelOfGoreCD:Start(1-delay)
-	timerSanguineStaticCD:Start(1-delay)
+	timerPlasmaDischargeCD:Start(5.9-delay)
+	timerCudgelOfGoreCD:Start(31.6-delay)
+	timerSanguineStaticCD:Start(20.6-delay)
+	ignoreGTFO = false
 end
 
 function mod:OnCombatEnd()
@@ -88,9 +86,17 @@ function mod:SPELL_CAST_START(args)
 			specWarnCudgelofGoreEveryone:Show()
 			specWarnCudgelofGoreEveryone:Play("justrun")
 		end
+		if self:IsTank() then
+			ignoreGTFO = true
+		end
 	elseif spellId == 271728 then
+		ignoreGTFO = false
 		specWarnRetrieveCudgel:Show()
 		specWarnRetrieveCudgel:Play("chargemove")
+	elseif spellId == 271895 then--Sanguine Static
+		specWarnSanguineStatic:Show()
+		specWarnSanguineStatic:Play("watchwave")
+		timerSanguineStaticCD:Start()
 	end
 end
 
@@ -104,20 +110,24 @@ function mod:SPELL_AURA_APPLIED(args)
 			specWarnPlasmaDischarge:ScheduleVoice(1.5, "keepmove")
 			yellPlasmaDischarge:Yell()
 		end
-		if self:AntiSpam(8, 1) then--Temp location hopefully
-			timerPlasmaDischargeCD:Start()
-		end
 	elseif spellId == 271965 then
+		ignoreGTFO = false
 		warnPoweringDown:Show()
 		warnPoweringDown:Play("phasechange")
 		timerPoweredDown:Start()
 		timerPlasmaDischargeCD:Stop()
 		timerCudgelOfGoreCD:Stop()
 		timerSanguineStaticCD:Stop()
-		if self.Options.AggroFrame then--Show aggro frame regardless if health frame is still up, it should be more important than health frame at this point. Shouldn't be blowing up traps while elementals are up.
-			DBM.InfoFrame:SetHeader(L.Aggro)
-			--Smart into frame that shows all players that have aggro that aren't tanks (if aggro i detectable on invisible stalkers anyways)
-			DBM.InfoFrame:Show(5, "playeraggro", 2, true)
+		if self.Options.InfoFrame then
+			DBM.InfoFrame:SetHeader(DBM:GetSpellInfo(275270))
+			DBM.InfoFrame:Show(5, "playerbaddebuff", 275270)
+		end
+	elseif spellId == 275270 then
+		if args:IsPlayer() then
+			specWarnFixate:Show()
+			specWarnFixate:Play("targetyou")
+		else
+			warnFixate:Show(args.destName)
 		end
 	end
 end
@@ -128,10 +138,12 @@ function mod:SPELL_AURA_REMOVED(args)
 	if spellId == 271225 then--Used later with icon feature
 
 	elseif spellId == 271965 then
+		warnPoweringDownOver:Show()
+		warnPoweringDownOver:Play("phasechange")
 		timerPoweredDown:Stop()
-		timerPlasmaDischargeCD:Start(2)
-		timerCudgelOfGoreCD:Start(2)
-		timerSanguineStaticCD:Start(2)
+		timerPlasmaDischargeCD:Start(6)
+		timerSanguineStaticCD:Start(20.7)
+		timerCudgelOfGoreCD:Start(30.5)
 		if self.Options.InfoFrame then
 			DBM.InfoFrame:Hide()
 		end
@@ -139,7 +151,7 @@ function mod:SPELL_AURA_REMOVED(args)
 end
 
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 270290 and destGUID == UnitGUID("player") and self:AntiSpam(2, 2) then
+	if spellId == 270290 and destGUID == UnitGUID("player") and self:AntiSpam(2, 2) and not ignoreGTFO then
 		specWarnGTFO:Show()
 		specWarnGTFO:Play("runaway")
 	end
@@ -155,11 +167,9 @@ function mod:UNIT_DIED(args)
 end
 --]]
 
-function mod:UNIT_SPELLCAST_START(uId, _, spellId)
-	if spellId == 271895 then--Sanguine Static
-		specWarnSanguineStatic:Show()
-		specWarnSanguineStatic:Play("watchwave")
-		timerSanguineStaticCD:Start()
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
+	if spellId == 271222 then--Plastma Discharge
+		timerPlasmaDischargeCD:Start()
 	end
 end
 
