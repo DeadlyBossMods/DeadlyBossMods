671ee4bf6c18bde791ba36686527072834b7a152
Adam <MysticalOS@users.noreply.github.com>
2018-05-12 06:37:50 +0000

    CN update

---
+++ DBM-Zandalar/localization.cn.lua
@@ -0,0 +1,17 @@
+-- Mini Dragon(projecteurs@gmail.com)
+-- 夏一可
+-- Blizzard Entertainment
+-- Last update: 2018/05/02
+
+if GetLocale() ~= "zhCN" then return end
+local L
+
+-----------------------
+-- T'zane --
+-----------------------
+L= DBM:GetModLocalization(2139)
+
+-----------------------
+-- Ji'arak --
+-----------------------
+L= DBM:GetModLocalization(2141)
