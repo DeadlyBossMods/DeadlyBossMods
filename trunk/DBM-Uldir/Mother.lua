a5fa96ab253ce4926f330906cbdf8e69705b236d
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-05-18 00:00:54 +0000

    Taloc and mother updates

--- trunk/DBM-Uldir/Mother.lua
+++ trunk/DBM-Uldir/Mother.lua
@@ -13,11 +13,12 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 267787 268198",
-	"SPELL_CAST_SUCCESS 267795 267945 267885 267878 269827 269051 268089",
-	"SPELL_AURA_APPLIED 267787 274205",
+	"SPELL_CAST_SUCCESS 267795 267945 267885 267878 269827 268089 277973 277961",
+	"SPELL_AURA_APPLIED 267787 274205 269051",
 	"SPELL_AURA_APPLIED_DOSE 267787"
 )
 
+--More mythic timer work
 --local warnXorothPortal				= mod:NewSpellAnnounce(244318, 2, nil, nil, nil, nil, nil, 7)
 --local warnSunderingScalpelCast			= mod:NewCastAnnounce(267787, 2, nil, nil, "Tank")
 local warnSunderingScalpel				= mod:NewStackAnnounce(267787, 3, nil, "Tank")
@@ -26,17 +27,15 @@ local warnDepletedEnergy				= mod:NewSpellAnnounce(274205, 1)
 local warnCleansingPurgeFinish			= mod:NewTargetNoFilterAnnounce(268095, 4)
 
 local specWarnSunderingScalpel			= mod:NewSpecialWarningDodge(267787, nil, nil, nil, 1, 2)
---local specWarnSunderingScalpel			= mod:NewSpecialWarningStack(267787, nil, 2, nil, nil, 1, 6)
---local specWarnSunderingScalpelOther		= mod:NewSpecialWarningTaunt(267787, nil, nil, nil, 1, 2)
 local specWarnPurifyingFlame			= mod:NewSpecialWarningDodge(267787, nil, nil, nil, 2, 2)
 local specWarnClingingCorruption		= mod:NewSpecialWarningInterrupt(268198, "HasInterrupt", nil, nil, 1, 2)
-local specWarnSurgicalBeam				= mod:NewSpecialWarningDodge(269827, nil, nil, nil, 3, 2)
+local specWarnSurgicalBeam				= mod:NewSpecialWarningDodgeLoc(269827, nil, nil, nil, 3, 2)
 
 --mod:AddTimerLine(Nexus)
 local timerSunderingScalpelCD			= mod:NewNextTimer(23.1, 267787, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
 local timerPurifyingFlameCD				= mod:NewNextTimer(23.1, 267795, nil, nil, nil, 3, nil, DBM_CORE_DEADLY_ICON)
-local timerWindTunnelCD					= mod:NewNextTimer(40.1, 267945, nil, nil, nil, 2)
-local timerSurgicalBeamCD				= mod:NewNextTimer(30, 269827, nil, nil, nil, 3, nil, DBM_CORE_DEADLY_ICON)
+local timerWindTunnelCD					= mod:NewNextTimer(39.8, 267945, nil, nil, nil, 2)
+local timerSurgicalBeamCD				= mod:NewCDSourceTimer(30, 269827, nil, nil, nil, 3, nil, DBM_CORE_DEADLY_ICON)
 local timerCleansingFlameCD				= mod:NewCastSourceTimer(180, 268095, nil, nil, nil, 6)
 
 --local berserkTimer					= mod:NewBerserkTimer(600)
@@ -71,11 +70,11 @@ do
 			end
 		end
 		--Player personal checks
-		local spellName, _, _, _, _, expireTime = DBM:UnitDebuff("player", 267821)
-		if spellName and expireTime then--Personal Defense Grid. Same spellId is used for going through and lingering, but expire time will only exist for lingering
-			local remaining = expireTime-GetTime()
-			addLine(spellName, remaining)
-		end
+		--local spellName, _, _, _, _, expireTime = DBM:UnitDebuff("player", 267821)
+		--if spellName and expireTime then--Personal Defense Grid. Same spellId is used for going through and lingering, but expire time will only exist for lingering
+			--local remaining = expireTime-GetTime()
+			--addLine(spellName, remaining)
+		--end
 		--TODO, player tracking per chamber if possible
 		return lines, sortedLines
 	end
@@ -130,23 +129,21 @@ function mod:SPELL_CAST_SUCCESS(args)
 		DBM:Debug("what way is wind blowing for spellId :"..spellId)
 	elseif spellId == 267945 then--Global Id for winds
 		warnWindTunnel:Show()
-		timerWindTunnelCD:Show()
-	elseif spellId == 269827 then
-		specWarnSurgicalBeam:Show()
-		specWarnSurgicalBeam:Play("laserrun")
-		specWarnSurgicalBeam:ScheduleVoice(1.5, "keepmove")
-		timerSurgicalBeamCD:Start()
-		countdownSurgicalBeam:Start()
-	elseif spellId == 269051 then--Begin Cast of Cleansing Purge
-		--136429 Chamber 01, 137022 Chamber 02, 137023 Chamber 03
-		local cid = self:GetCIDFromGUID(args.sourceGUID)
-		if cid == 136429 then
-			timerCleansingFlameCD:Start(180, 1)
-		elseif cid == 137022 then
-			timerCleansingFlameCD:Start(180, 2)
-		elseif cid == 137023 then
-			timerCleansingFlameCD:Start(180, 3)
+		timerWindTunnelCD:Show()--40-47
+	elseif spellId == 269827 or spellId == 277973 or spellId == 277961 then
+		if spellId == 277961 then--Top
+			specWarnSurgicalBeam:Show(DBM_CORE_TOP)
+			timerSurgicalBeamCD:Start(40, DBM_CORE_TOP)--40-47
+		elseif spellId == 277973 then--Sides
+			specWarnSurgicalBeam:Show(DBM_CORE_SIDE)
+			timerSurgicalBeamCD:Start(40, DBM_CORE_SIDE)--40-47
+		else--Middle (chamber 3)
+			specWarnSurgicalBeam:Show(DBM_CORE_MIDDLE)
+			timerSurgicalBeamCD:Start(30, DBM_CORE_MIDDLE)--30-?
+			countdownSurgicalBeam:Start()
+			specWarnSurgicalBeam:ScheduleVoice(1.5, "keepmove")
 		end
+		specWarnSurgicalBeam:Play("laserrun")
 	elseif spellId == 268089 then--End Cast of Cleansing Purge
 		warnCleansingPurgeFinish:Show(args.sourceName)
 	end
@@ -159,29 +156,24 @@ function mod:SPELL_AURA_APPLIED(args)
 		if self:IsTanking(uId) then
 			local amount = args.amount or 1
 			warnSunderingScalpel:Show(args.destName, amount)
-			--[[if amount >= 2 then
-				if args:IsPlayer() then
-					specWarnSunderingScalpel:Show(amount)
-					specWarnSunderingScalpel:Play("stackhigh")
-				else
-					local _, _, _, _, _, expireTime = DBM:UnitDebuff("player", spellId)
-					local remaining
-					if expireTime then
-						remaining = expireTime-GetTime()
-					end
-					if not UnitIsDeadOrGhost("player") and (not remaining or remaining and remaining < 30) then--30 is placeholder until CD is known
-						specWarnSunderingScalpelOther:Show(args.destName)
-						specWarnSunderingScalpelOther:Play("tauntboss")
-					else
-						warnSunderingScalpel:Show(args.destName, amount)
-					end
-				end
-			else
-				warnSunderingScalpel:Show(args.destName, amount)
-			end--]]
 		end
 	elseif spellId == 274205 then
 		warnDepletedEnergy:Show()
+	elseif spellId == 269051 then--Begin Cast of Cleansing Purge
+		--136429 Chamber 01, 137022 Chamber 02, 137023 Chamber 03
+		local cid = self:GetCIDFromGUID(args.destGUID)
+		local time = self:IsMythic() and 123 or 180
+		if cid == 136429 then
+			timerCleansingFlameCD:Start(time, 1)
+		elseif cid == 137022 then
+			timerCleansingFlameCD:Start(time, 2)
+			--if self:IsMythic() then
+			--	timerSurgicalBeamCD:Start(10, DBM_CORE_SIDE)--10-18, need more work to get this better if possible
+			--	timerSurgicalBeamCD:Start(33, DBM_CORE_TOP)--33-41
+			--end
+		elseif cid == 137023 then
+			timerCleansingFlameCD:Start(time, 3)
+		end
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
