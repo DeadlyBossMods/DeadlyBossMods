ee31aea30db8775c418e4f5ecd1c85ae046eedcd
Adam <MysticalOS@users.noreply.github.com>
2018-05-25 19:56:28 +0000

    Some timer fixes

--- DBM-Brawlers/Rank7.lua
+++ DBM-Brawlers/Rank7.lua
@@ -22,7 +22,7 @@ local specWarnBlueCrush			= mod:NewSpecialWarningInterrupt(133262)--Epicus Maxim
 local specWarnDestructolaser	= mod:NewSpecialWarningMove(133250)--Epicus Maximus
 
 local timerSpitAcidCD			= mod:NewNextTimer(20, 141013)--Nibbleh
-local timerBlueCrushCD			= mod:NewNextTimer(28.2, 133262, nil, nil, nil, 4)--Epicus Maximus
+local timerBlueCrushCD			= mod:NewCDTimer(21.9, 133262, nil, nil, nil, 4, nil, DBM_CORE_INTERRUPT_ICON)--Epicus Maximus
 local timerDestructolaserCD		= mod:NewNextTimer(30, 133250, nil, nil, nil, 3)--Epicus Maximus
 
 local brawlersMod = DBM:GetModByName("Brawlers")
