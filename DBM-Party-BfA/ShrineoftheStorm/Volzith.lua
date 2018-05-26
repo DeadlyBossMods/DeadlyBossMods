d8f9f6f6059bc598c31e48f98a860a1ad8acd8f1
Adam <MysticalOS@users.noreply.github.com>
2018-05-26 06:53:53 +0000

    antispam

--- DBM-Party-BfA/ShrineoftheStorm/Volzith.lua
+++ DBM-Party-BfA/ShrineoftheStorm/Volzith.lua
@@ -72,7 +72,7 @@ function mod:SPELL_CAST_START(args)
 		specWarnCalltheAbyss:Show()
 		specWarnCalltheAbyss:Play("moveboss")
 		timerCalltheAbyssCD:Start()
-	elseif spellId == 267385 then
+	elseif spellId == 267385 and self:AntiSpam(1.5, 2) then
 		warnTentacleSlam:Show()
 	elseif spellId == 267360 then
 		specWarnGrasp:Show()
