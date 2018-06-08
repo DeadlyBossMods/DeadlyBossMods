a18662bdbc103ef8704acadea4db58badd928c17
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-06-08 19:56:43 +0000

    Follow similar naming scheme

--- trunk/DBM-Core/DBM-Core.lua
+++ trunk/DBM-Core/DBM-Core.lua
@@ -418,6 +418,8 @@ local bannedMods = { -- a list of "banned" (meaning they are replaced by another
 	"DBM-ProvingGrounds",--Renamed to DBM-Challenges going forward to include proving grounds and any new single player challendges of similar design such as mage tower artifact quests
 	"DBM-VPKiwiBeta",--Renamed to DBM-VPKiwi in final version.
 	"DBM-Suramar",--Renamed to DBM-Nighthold
+	"DBM-KulTiras",--Merged to DBM-Azeroth-BfA
+	"DBM-Zandalar",--Merged to DBM-Azeroth-BfA
 --	"DBM-PvP",--Discontinued do to inability to maintain such large scale external projects with limitted time/resources
 }
 
