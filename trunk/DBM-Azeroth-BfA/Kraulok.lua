27cc54ee0b59431adbbc195607d40db3ce6e8bdf
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-06-08 20:15:58 +0000

    fixes

--- trunk/DBM-Azeroth-BfA/Kraulok.lua
+++ trunk/DBM-Azeroth-BfA/Kraulok.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod(2210, "DBM-Azeroth-BfA", nil, 1028)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 17462 $"):sub(12, -3))
+mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(138794)
 --mod:SetEncounterID(1880)
 mod:SetReCombatTime(20)
