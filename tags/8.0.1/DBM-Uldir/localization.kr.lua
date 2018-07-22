9ffca2efd15e39fa77d3cbbc0f5d4a6a9ad1664b
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-07-22 20:14:43 +0000

    tag as 8.0.1

---
+++ tags/8.0.1/DBM-Uldir/localization.kr.lua
@@ -0,0 +1,73 @@
+if GetLocale() ~= "koKR" then return end
+local L
+
+---------------------------
+-- Taloc the Corrupted --
+---------------------------
+L= DBM:GetModLocalization(2168)
+
+L:SetMiscLocalization({
+	Aggro	 =	"어그로 수치"
+})
+
+---------------------------
+-- MOTHER --
+---------------------------
+L= DBM:GetModLocalization(2167)
+
+---------------------------
+-- Fetid Devourer --
+---------------------------
+L= DBM:GetModLocalization(2146)
+
+---------------------------
+-- Zek'vhozj --
+---------------------------
+L= DBM:GetModLocalization(2169)
+
+L:SetMiscLocalization({
+	CThunDisc	 =	"Disc accessed. C'thun data loading.",
+	YoggDisc	 =	"Disc accessed. Yogg-Saron data loading.",
+	CorruptedDisc =	"Disc accessed. Corrupted data loading."
+})
+
+---------------------------
+-- Vectis --
+---------------------------
+L= DBM:GetModLocalization(2166)
+
+L:SetOptionLocalization({
+	ShowHighestFirst	 =	"중첩이 높은 순서대로 정보 프레임에 끈질긴 감염 대상자 정렬 (중첩 낮은순 대신)"
+})
+
+---------------
+-- Mythrax the Unraveler --
+---------------
+L= DBM:GetModLocalization(2194)
+
+---------------------------
+-- Zul --
+---------------------------
+L= DBM:GetModLocalization(2195)
+
+L:SetTimerLocalization({
+	timerAddIncoming		= DBM_INCOMING
+})
+
+L:SetOptionLocalization({
+	timerAddIncoming		= "쫄이 공격 가능해지면 타이머 바 보기"
+})
+
+------------------
+-- G'huun --
+------------------
+L= DBM:GetModLocalization(2147)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("UldirTrash")
+
+L:SetGeneralLocalization({
+	name =	"울디르 일반몹"
+})
---
+++ tags/8.0.1/DBM-WorldEvents/localization.es.lua
@@ -0,0 +1,156 @@
+if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
+local L
+
+-------------
+-- Augurio --
+-------------
+L = DBM:GetModLocalization("Omen")
+
+L:SetGeneralLocalization({
+	name = "Augurio"
+})
+
+------------------------------
+-- Químicos La Corona, S.L. --
+------------------------------
+L = DBM:GetModLocalization("d288")
+
+L:SetTimerLocalization{
+	HummelActive		= "Hummel entra en combate",
+	BaxterActive		= "Baxter entra en combate",
+	FryeActive			= "Frye entra en combate"
+}
+
+L:SetOptionLocalization({
+	TrioActiveTimer		= "Mostrar temporizadores para cuando los apotecarios entren en combate"
+})
+
+L:SetMiscLocalization({
+	SayCombatStart		= "¿Se han molestado en decirte quién soy y por qué estoy haciendo esto?"
+})
+
+-----------
+-- Ahune --
+-----------
+L = DBM:GetModLocalization("d286")
+
+L:SetWarningLocalization({
+	Emerged			= "Ahune vuelve a la superficie",
+	specWarnAttack	= "Ahune es vulnerable - ¡Ataca ahora!"
+})
+
+L:SetTimerLocalization{
+	SubmergTimer	= "Sumergir",
+	EmergeTimer		= "Volver a la superficie"
+}
+
+L:SetOptionLocalization({
+	Emerged			= "Mostrar aviso cuando Ahune vuelva a la superficie",
+	specWarnAttack	= "Mostrar aviso especial cuando Ahune se vuelva vulnerable",
+	SubmergTimer	= "Mostrar temporizador para cuando Ahune se sumerja",
+	EmergeTimer		= "Mostrar temporizador para cuando Ahune vuelva a la superficie"
+})
+
+L:SetMiscLocalization({
+	Pull			= "¡La piedra de hielo se ha derretido!"
+})
+
+---------------------------
+-- Coren Cerveza Temible --
+---------------------------
+L = DBM:GetModLocalization("d287")
+
+L:SetWarningLocalization({
+	specWarnBrew		= "¡Bebe la cerveza antes de que te lance otra!",
+	specWarnBrewStun	= "SUGERENCIA: ¡Te han dado! ¡No te olvides de beber la cerveza!"
+})
+
+L:SetOptionLocalization({
+	specWarnBrew		= "Mostrar aviso especial para $spell:47376",
+	specWarnBrewStun	= "Mostrar aviso especial para $spell:47340"
+})
+
+L:SetMiscLocalization({
+	YellBarrel			= "¡Tengo el barril!"
+})
+
+--------------------------
+-- Fiesta de la Cerveza --
+--------------------------
+L = DBM:GetModLocalization("Brew")
+
+L:SetGeneralLocalization({
+	name = "Fiesta de la Cerveza"
+})
+
+L:SetOptionLocalization({
+	NormalizeVolume			= "Normalizar automáticamente el volumen del canal de sonido de diálogo con el de música cuando estés en una zona de la Fiesta de la Cerveza para que el sonido no sea demasiado molesto"
+})
+
+--------------------------
+-- El Jinete decapitado --
+--------------------------
+L = DBM:GetModLocalization("d285")
+
+L:SetWarningLocalization({
+	WarnPhase				= "Fase %d",
+	warnHorsemanSoldiers	= "Calabazas con pulso",
+	warnHorsemanHead		= "Cabeza de El jinete decapitado"
+})
+
+L:SetOptionLocalization({
+	WarnPhase				= "Mostrar aviso para cambios de fase",
+	warnHorsemanSoldiers	= "Mostrar aviso cuando aparezcan Cabalazas con pulso",
+	warnHorsemanHead		= "Mostrar aviso cuando aparezca la Cabeza de El jinete decapitado"
+})
+
+L:SetMiscLocalization({
+	HorsemanSummon			= "Jinete álzate...",
+	HorsemanSoldiers		= "Soldados, alzaos y luchad, tomad vuestro acero. Dad la victoria a este deshonrado caballero."
+})
+
+--------------------------
+-- Grinch el Abominable --
+--------------------------
+L = DBM:GetModLocalization("Greench")
+
+L:SetGeneralLocalization({
+	name = "Grinch el Abominable"
+})
+
+-------------------------------------
+-- Flores de paz contra necrófagos --
+-------------------------------------
+L = DBM:GetModLocalization("PlantsVsZombies")
+
+L:SetGeneralLocalization({
+	name = "Flores de paz contra necrófagos"
+})
+
+L:SetWarningLocalization({
+	warnTotalAdds	= "Total de zombis desde la última oleada gigantesca: %d",
+	specWarnWave	= "¡Oleada gigantesca!"
+})
+
+L:SetTimerLocalization{
+	timerWave		= "Siguiente oleada gigantesca"
+}
+
+L:SetOptionLocalization({
+	warnTotalAdds	= "Anunciar total de zombis entre oleadas gigantescas",
+	specWarnWave	= "Mostrar aviso especial cuando comience una oleada gigantesca",
+	timerWave		= "Mostrar temporizador para las siguientes oleadas gigantescas"
+})
+
+L:SetMiscLocalization({
+	MassiveWave		= "¡Se acerca una gigantesca oleada de zombis!"
+})
+
+---------------------------
+-- Invasiones demoníacas --
+---------------------------
+L = DBM:GetModLocalization("DemonInvasions")
+
+L:SetGeneralLocalization({
+	name = "Invasiones demoníacas"
+})
