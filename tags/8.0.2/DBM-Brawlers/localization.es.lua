3674ebb87dfba1491eea74a0fabced6a427aace9
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-07-22 20:12:30 +0000

    tag as 8.0.2

---
+++ tags/8.0.2/DBM-Brawlers/localization.es.lua
@@ -0,0 +1,181 @@
+if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
+local L
+
+-----------------
+-- Camorristas --
+-----------------
+L= DBM:GetModLocalization("Brawlers")
+
+L:SetGeneralLocalization({
+	name = "Opciones generales"
+})
+
+L:SetWarningLocalization({
+	warnQueuePosition2	= "Estás en el puesto %d de la cola",
+	specWarnYourNext	= "¡Te toca el siguiente!",
+	specWarnYourTurn	= "¡Te toca!"
+})
+
+L:SetOptionLocalization({
+	warnQueuePosition2	= "Anunciar tu puesto en la cola cada vez que cambie",
+	specWarnYourNext	= "Mostrar aviso especial cuando seas el primero en cola",
+	specWarnYourTurn	= "Mostrar aviso especial cuando sea tu turno",
+	SpectatorMode		= "Mostrar avisos y temporizadores al espectar combates (tus avisos especiales personales no se mostrarán a los espectadores)",
+	SpeakOutQueue		= "Anunciar tu número en la cola cuando esta se actualice",
+	NormalizeVolume		= "Normalizar automáticamente el volumen del canal de sonido de diálogos para que coincida con el del canal de efectos de sonido mientras estés en la arena, de forma que los ánimos y aplausos no suenen tan alto."
+})
+
+L:SetMiscLocalization({
+	Bizmo			= "Bizmo",--Alliance
+	Bazzelflange	= "Jefa Zumbarriel",--Horde
+	--I wish there was a better way to do this....so much localizing. :(
+	Rank1			= "Rango 1",
+	Rank2			= "Rango 2",
+	Rank3			= "Rango 3",
+	Rank4			= "Rango 4",
+	Rank5			= "Rango 5",
+	Rank6			= "Rango 6",
+	Rank7			= "Rango 7",
+	Rank8			= "Rango 8",
+	Rank9			= "Rango 9",
+	Rank10			= "Rango 10",
+	Proboskus		= "Oh, no... Lo siento, pero parece que te va a tocar luchar contra Proboskus.",--Alliance
+	Proboskus2		= "¡Ja, ja, ja! ¡Qué mala suerte tienes! ¡Es Proboskus! ¡Ja, ja, ja, ja! Me apuesto veinticinco monedas de oro a que mueres en el fuego."--Horde
+})
+
+-------------
+-- Rango 1 --
+-------------
+L= DBM:GetModLocalization("BrawlRank1")
+
+L:SetGeneralLocalization({
+	name = "Rango 1"
+})
+
+-------------
+-- Rango 2 --
+-------------
+L= DBM:GetModLocalization("BrawlRank2")
+
+L:SetGeneralLocalization({
+	name = "Rango 2"
+})
+
+-------------
+-- Rango 3 --
+-------------
+L= DBM:GetModLocalization("BrawlRank3")
+
+L:SetGeneralLocalization({
+	name = "Rango 3"
+})
+
+L:SetOptionLocalization({
+	SetIconOnBlat	= "Poner icono (calavera) en el verdadero Blat"
+})
+
+-------------
+-- Rango 4 --
+-------------
+L= DBM:GetModLocalization("BrawlRank4")
+
+L:SetGeneralLocalization({
+	name = "Rango 4"
+})
+
+L:SetOptionLocalization({
+	SetIconOnDominika	= "Poner icono (calavera) en la verdadera Dominika la Ilusionista"
+})
+
+-------------
+-- Rango 5 --
+-------------
+L= DBM:GetModLocalization("BrawlRank5")
+
+L:SetGeneralLocalization({
+	name = "Rango 5"
+})
+
+-------------
+-- Rango 6 --
+-------------
+L= DBM:GetModLocalization("BrawlRank6")
+
+L:SetGeneralLocalization({
+	name = "Rango 6"
+})
+
+-------------
+-- Rango 7 --
+-------------
+L= DBM:GetModLocalization("BrawlRank7")
+
+L:SetGeneralLocalization({
+	name = "Rango 7"
+})
+
+--[[
+-------------
+-- Rango 8 --
+-------------
+L= DBM:GetModLocalization("BrawlRank8")
+
+L:SetGeneralLocalization({
+	name = "Rango 8"
+})
+
+-------------
+-- Rango 9 --
+-------------
+L= DBM:GetModLocalization("BrawlRank9")
+
+L:SetGeneralLocalization({
+	name = "Rango 9"
+})
+--]]
+
+------------
+-- Legado --
+------------
+L= DBM:GetModLocalization("BrawlLegacy")
+
+L:SetGeneralLocalization({
+	name = "Desafíos de legado"
+})
+
+L:SetOptionLocalization({
+	SpeakOutStrikes		= "Anunciar número de $spell:141190"
+})
+
+----------------
+-- Especiales --
+----------------
+L= DBM:GetModLocalization("BrawlChallenges")
+
+L:SetGeneralLocalization({
+	name = "Desafíos especiales"
+})
+
+L:SetWarningLocalization({
+	specWarnRPS			= "¡Usa %s!"
+})
+
+L:SetOptionLocalization({
+	ArrowOnBoxing		= "Mostrar flecha para $spell:140868, $spell:140862 y $spell:140886",
+	specWarnRPS			= "Mostrar aviso especial con qué usar para $spell:141206"
+})
+
+L:SetMiscLocalization({
+	rock			= "Piedra",
+	paper			= "Papel",
+	scissors		= "Tijeras"
+})
+
+--------------
+-- Reyertas --
+--------------
+L= DBM:GetModLocalization("BrawlRumble")
+
+L:SetGeneralLocalization({
+	name = "Reyertas"
+})
---
+++ tags/8.0.2/DBM-Party-Legion/localization.de.lua
@@ -0,0 +1,606 @@
+﻿if GetLocale() ~= "deDE" then return end
+local L
+
+-----------------------
+-- <<<Black Rook Hold>>> --
+-----------------------
+-----------------------
+-- The Amalgam of Souls --
+-----------------------
+L= DBM:GetModLocalization(1518)
+
+-----------------------
+-- Illysanna Ravencrest --
+-----------------------
+L= DBM:GetModLocalization(1653)
+
+-----------------------
+-- Smashspite the Hateful --
+-----------------------
+L= DBM:GetModLocalization(1664)
+
+-----------------------
+-- Lord Kur'talos Ravencrest --
+-----------------------
+L= DBM:GetModLocalization(1672)
+
+-----------------------
+--Black Rook Hold Trash
+-----------------------
+L = DBM:GetModLocalization("BRHTrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash der Rabenwehr"
+})
+
+-----------------------
+-- <<<Darkheart Thicket>>> --
+-----------------------
+-----------------------
+-- Arch-Druid Glaidalis --
+-----------------------
+L= DBM:GetModLocalization(1654)
+
+-----------------------
+-- Oakheart --
+-----------------------
+L= DBM:GetModLocalization(1655)
+
+-----------------------
+-- Dresaron --
+-----------------------
+L= DBM:GetModLocalization(1656)
+
+-----------------------
+-- Shade of Xavius --
+-----------------------
+L= DBM:GetModLocalization(1657)
+
+-----------------------
+--Darkheart Thicket Trash
+-----------------------
+L = DBM:GetModLocalization("DHTTrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash des Finsterherzdickichts"
+})
+
+
+-----------------------
+-- <<<Eye of Azshara>>> --
+-----------------------
+-----------------------
+-- Warlord Parjesh --
+-----------------------
+L= DBM:GetModLocalization(1480)
+
+-----------------------
+-- Lady Hatecoil --
+-----------------------
+L= DBM:GetModLocalization(1490)
+
+L:SetWarningLocalization({
+	specWarnStaticNova			= "Statische Nova - geh auf Sanddüne",
+	specWarnFocusedLightning	= "Gebündelter Blitz - geh ins Wasser"
+})
+
+-----------------------
+-- King Deepbeard --
+-----------------------
+L= DBM:GetModLocalization(1491)
+
+-----------------------
+-- Serpentrix --
+-----------------------
+L= DBM:GetModLocalization(1479)
+
+-----------------------
+-- Wrath of Azshara --
+-----------------------
+L= DBM:GetModLocalization(1492)
+
+-----------------------
+--Eye of Azshara Trash
+-----------------------
+L = DBM:GetModLocalization("EoATrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash des Auge Azsharas"
+})
+
+-----------------------
+-- <<<Halls of Valor>>> --
+-----------------------
+-----------------------
+-- Hymdall --
+-----------------------
+L= DBM:GetModLocalization(1485)
+
+-----------------------
+-- Hyrja --
+-----------------------
+L= DBM:GetModLocalization(1486)
+
+-----------------------
+-- Fenryr --
+-----------------------
+L= DBM:GetModLocalization(1487)
+
+-----------------------
+-- God-King Skovald --
+-----------------------
+L= DBM:GetModLocalization(1488)
+
+L:SetMiscLocalization({
+	SkovaldRP		= "Nein! Auch ich habe mich bewiesen, Odyn. Ich bin Gottkönig Skovald! Diese Sterblichen werden mir die Aegis nicht streitig machen!"
+})
+
+-----------------------
+-- Odyn --
+-----------------------
+L= DBM:GetModLocalization(1489)
+
+L:SetMiscLocalization({
+	tempestModeMessage		=	"Nicht Orkanabfolge: %s. Prüfe erneut in 8 Sekunden."
+})
+
+-----------------------
+--Halls of Valor Trash
+-----------------------
+L = DBM:GetModLocalization("HoVTrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash der Hallen der Tapferkeit"
+})
+
+-----------------------
+-- <<<Neltharion's Lair>>> --
+-----------------------
+-----------------------
+-- Rokmora --
+-----------------------
+L= DBM:GetModLocalization(1662)
+
+-----------------------
+-- Ularogg Cragshaper --
+-----------------------
+L= DBM:GetModLocalization(1665)
+
+-----------------------
+-- Naraxas --
+-----------------------
+L= DBM:GetModLocalization(1673)
+
+-----------------------
+-- Dargrul the Underking --
+-----------------------
+L= DBM:GetModLocalization(1687)
+
+-----------------------
+--Neltharion's Lair Trash
+-----------------------
+L = DBM:GetModLocalization("NLTrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash des Neltharions Hort"
+})
+
+-----------------------
+-- <<<The Arcway>>> --
+-----------------------
+-----------------------
+-- Ivanyr --
+-----------------------
+L= DBM:GetModLocalization(1497)
+
+-----------------------
+-- Nightwell Sentry --
+-----------------------
+L= DBM:GetModLocalization(1498)
+
+-----------------------
+-- General Xakal --
+-----------------------
+L= DBM:GetModLocalization(1499)
+
+L:SetMiscLocalization({
+	batSpawn		=	"Reinforcements to me! NOW!"--translate (trigger)
+})
+
+-----------------------
+-- Nal'tira --
+-----------------------
+L= DBM:GetModLocalization(1500)
+
+-----------------------
+-- Advisor Vandros --
+-----------------------
+L= DBM:GetModLocalization(1501)
+
+-----------------------
+--The Arcway Trash
+-----------------------
+L = DBM:GetModLocalization("ArcwayTrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash des Arkus"
+})
+
+-----------------------
+-- <<<Court of Stars>>> --
+-----------------------
+-----------------------
+-- Patrol Captain Gerdo --
+-----------------------
+L= DBM:GetModLocalization(1718)
+
+-----------------------
+-- Talixae Flamewreath --
+-----------------------
+L= DBM:GetModLocalization(1719)
+
+-----------------------
+-- Advisor Melandrus --
+-----------------------
+L= DBM:GetModLocalization(1720)
+
+-----------------------
+--Court of Stars Trash
+-----------------------
+L = DBM:GetModLocalization("CoSTrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash des Hofs der Sterne"
+})
+
+L:SetOptionLocalization({
+	SpyHelper	= "Hilf bei der Erkennung des Spions"
+})
+
+L:SetMiscLocalization({
+	Gloves1			= "Einem Gerücht zufolge trägt der Spion immer Handschuhe.",
+	Gloves2			= "Wie ich hörte, verbirgt der Spion sorgfältig die Hände.",
+	Gloves3			= "Ich hörte, dass der Spion immer Handschuhe anlegt.",
+	Gloves4			= "Jemand behauptete, dass der Spion Handschuhe trägt, um sichtbare Narben zu verbergen.",
+	NoGloves1		= "Es gibt Gerüchte, dass der Spion niemals Handschuhe trägt.",
+	NoGloves2		= "Wisst Ihr... Ich habe ein zusätzliches Paar Handschuhe im Hinterzimmer gefunden. Wahrscheinlich ist der Spion hier irgendwo mit bloßen Händen unterwegs.",
+	NoGloves3		= "Mir ist zu Ohren gekommen, dass der Spion ungern Handschuhe trägt.",
+	NoGloves4		= "Ich hörte, dass der Spion es vermeidet, Handschuhe zu tragen, falls er schnell handeln muss.",
+	Cape1			= "Jemand erwähnte, dass der Spion vorhin hier hereinkam und einen Umhang trug.",
+	Cape2			= "Mir ist zu Ohren gekommen, dass der Spion gerne Umhänge trägt.",
+	NoCape1			= "Ich hörte, dass der Spion keine Umhänge mag und sich weigert, einen zu tragen.",
+	NoCape2			= "Ich hörte, dass der Spion seinen Umhang im Palast gelassen hat, bevor er hierhergekommen ist.",
+	LightVest1		= "Der Spion bevorzugt auf jeden Fall Westen mit hellen Farben.",
+	LightVest2		= "Wie ich hörte, trägt der Spion auf der Party heute Abend eine helle Weste.",
+	LightVest3		= "Die Leute sagen, dass der Spion heute Abend keine dunkle Weste trägt.",
+	DarkVest1		= "Der Spion bevorzugt auf alle Fälle dunkle Kleidung.",
+	DarkVest2		= "Ich hörte, dass die Weste des Spions heute Abend von dunkler, kräftiger Farbe ist.",
+	DarkVest3		= "Dem Spion gefallen Westen mit dunklen Farben... dunkel wie die Nacht.",
+	DarkVest4		= "Gerüchten zufolge vermeidet der Spion es, helle Kleidung zu tragen, damit er nicht so auffällt.",
+	Female1			= "Ein Gast hat beobachtet, wie sie und Elisande vorhin gemeinsam eingetroffen sind.",
+	Female2			= "Wie ich höre, hat eine Frau sich ständig nach diesem Bezirk erkundigt...",
+	Female3			= "Jemand hat behauptet, dass unser neuester Gast nicht männlich ist.",
+	Female4			= "Man sagt, die Spionin wäre hier und sie wäre eine wahre Augenweide.",
+	Male1			= "Irgendwo habe ich gehört, dass der Spion nicht weiblich ist.",
+	Male2			= "Ich hörte, dass der Spion ein äußerst gutaussehender Herr ist.",
+	Male3			= "Ein Gast sagte, sie sah, wie ein Herr an der Seite der Großmagistrix das Anwesen betreten hat.",
+	Male4			= "Einer der Musiker sagte, er stellte unablässig Fragen über den Bezirk.",
+	ShortSleeve1	= "Mir ist zu Ohren gekommen, dass der Spion kurze Ärmel trägt, damit er seine Arme ungehindert bewegen kann.",
+	ShortSleeve2	= "Jemand sagte mir, dass der Spion lange Ärmel hasst.",
+	ShortSleeve3	= "Eine meiner Freundinnen sagte, dass sie die Kleidung des Spions gesehen hat. Er trägt keine langen Ärmel.",
+	ShortSleeve4	= "Man hat mir zugetragen, dass der Spion die kühle Luft mag und deshalb heute Abend keine langen Ärmel trägt.",
+	LongSleeve1		= "Wie ich hörte, trägt der Spion heute Abend Kleidung mit langen Ärmeln.",
+	LongSleeve2		= "Jemand sagte, dass der Spion heute Abend seine Arme mit langen Ärmeln bedeckt.",
+	LongSleeve3		= "Ich habe am frühen Abend einen kurzen Blick auf die langen Ärmel des Spions erhascht.",
+	LongSleeve4		= "Einer meiner Freunde erwähnte, dass der Spion lange Ärmel trägt.",
+	Potions1		= "Ich hörte, dass der Spion Tränke mitgebracht hat... Ich frage mich wieso?",
+	Potions2		= "Von mir habt Ihr das nicht... aber der Spion verkleidet sich als Alchemist und trägt Tränke an seinem Gürtel.",
+	Potions3		= "Ich bin mir ziemlich sicher, dass der Spion Tränke am Gürtel trägt.",
+	Potions4		= "Wie ich hörte, hat der Spion einige Tränke mitgebracht... für alle Fälle.",
+	NoPotions1		= "Wie ich hörte, hat der Spion keine Tränke bei sich.",
+	NoPotions2		= "Eine Musikerin erzählte mir, dass sie gesehen hat, wie der Spion seinen letzten Trank wegwarf und jetzt keinen mehr übrig hat.",
+	Book1			= "Ich hörte, dass der Spion immer ein Buch mit niedergeschriebenen Geheimnissen am Gürtel trägt.",
+	Book2			= "Gerüchten zufolge liest der Spion gerne und trägt immer mindestens ein Buch bei sich.",
+	Pouch1			= "Ich hörte, dass der Spion immer einen magischen Beutel mit sich herumträgt.",
+	Pouch2			= "Ein Freund behauptet, dass der Spion Gold liebt und einen Gürtelbeutel voll davon hat.",
+	Pouch3			= "Ich hörte, dass der Gürtelbeutel des Spions mit ausgefallenem Garn gesäumt wurde.",
+	Pouch4			= "Mir ist zu Ohren gekommen, dass der Gürtelbeutel des Spions mit Gold gefüllt ist, um besonders extravagant zu erscheinen.",
+	Found			= "Now now, let's not be hasty",--translate (trigger)
+	--
+	Gloves			= "Handschuhe",
+	NoGloves		= "keine Handschuhe",
+	Cape			= "Umhang",
+	Nocape			= "kein Umhang",
+	LightVest		= "helle Weste",
+	DarkVest		= "dunkle Weste",
+	Female			= "weiblich",
+	Male			= "männlich",
+	ShortSleeve		= "kurze Ärmel",
+	LongSleeve		= "lange Ärmel",
+	Potions			= "Tränke",
+	NoPotions		= "keine Tränke",
+	Book			= "Buch",
+	Pouch			= "Beutel"
+})
+
+
+-----------------------
+-- <<<The Maw of Souls>>> --
+-----------------------
+-----------------------
+-- Ymiron, the Fallen King --
+-----------------------
+L= DBM:GetModLocalization(1502)
+
+-----------------------
+-- Harbaron --
+-----------------------
+L= DBM:GetModLocalization(1512)
+
+-----------------------
+-- Helya --
+-----------------------
+L= DBM:GetModLocalization(1663)
+
+-----------------------
+--Maw of Souls Trash
+-----------------------
+L = DBM:GetModLocalization("MawTrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash des Seelenschlundes"
+})
+
+-----------------------
+-- <<<Assault Violet Hold>>> --
+-----------------------
+-----------------------
+-- Mindflayer Kaahrj --
+-----------------------
+L= DBM:GetModLocalization(1686)
+
+-----------------------
+-- Millificent Manastorm --
+-----------------------
+L= DBM:GetModLocalization(1688)
+
+-----------------------
+-- Festerface --
+-----------------------
+L= DBM:GetModLocalization(1693)
+
+-----------------------
+-- Shivermaw --
+-----------------------
+L= DBM:GetModLocalization(1694)
+
+-----------------------
+-- Blood-Princess Thal'ena --
+-----------------------
+L= DBM:GetModLocalization(1702)
+
+-----------------------
+-- Anub'esset --
+-----------------------
+L= DBM:GetModLocalization(1696)
+
+-----------------------
+-- Sael'orn --
+-----------------------
+L= DBM:GetModLocalization(1697)
+
+-----------------------
+-- Fel Lord Betrug --
+-----------------------
+L= DBM:GetModLocalization(1711)
+
+-----------------------
+--Assault Violet Hold Trash
+-----------------------
+L = DBM:GetModLocalization("AVHTrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash des Sturms auf die VF"
+})
+
+L:SetWarningLocalization({
+	WarningPortalSoon	= "Neues Portal bald",
+	WarningPortalNow	= "Portal %d",
+	WarningBossNow		= "Boss kommt"
+})
+
+L:SetTimerLocalization({
+	TimerPortal			= "Portal CD"
+})
+
+L:SetOptionLocalization({
+	WarningPortalNow		= "Zeige Warnung für neues Portal",
+	WarningPortalSoon		= "Zeige Vorwarnung für neues Portal",
+	WarningBossNow			= "Zeige Warnung für neuen Boss",
+	TimerPortal				= "Zeige Timer für nächstes Portal (nach einem Boss)"
+})
+
+L:SetMiscLocalization({
+	Malgath		=	"Lord Malgath"
+})
+
+-----------------------
+-- <<<Vault of the Wardens>>> --
+-----------------------
+-----------------------
+-- Tirathon Saltheril --
+-----------------------
+L= DBM:GetModLocalization(1467)
+
+-----------------------
+-- Inquisitor Tormentorum --
+-----------------------
+L= DBM:GetModLocalization(1695)
+
+-----------------------
+-- Ash'golm --
+-----------------------
+L= DBM:GetModLocalization(1468)
+
+-----------------------
+-- Glazer --
+-----------------------
+L= DBM:GetModLocalization(1469)
+
+-----------------------
+-- Cordana --
+-----------------------
+L= DBM:GetModLocalization(1470)
+
+-----------------------
+--Vault of Wardens Trash
+-----------------------
+L = DBM:GetModLocalization("VoWTrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash des Verlieses der Wächterinnen"
+})
+
+-----------------------
+-- <<<Return To Karazhan>>> --
+-----------------------
+-----------------------
+-- Maiden of Virtue --
+-----------------------
+L= DBM:GetModLocalization(1825)
+
+-----------------------
+-- Opera Hall: Wikket  --
+-----------------------
+L= DBM:GetModLocalization(1820)
+
+-----------------------
+-- Opera Hall: Westfall Story --
+-----------------------
+L= DBM:GetModLocalization(1826)
+
+-----------------------
+-- Opera Hall: Beautiful Beast  --
+-----------------------
+L= DBM:GetModLocalization(1827)
+
+-----------------------
+-- Attumen the Huntsman --
+-----------------------
+L= DBM:GetModLocalization(1835)
+
+-----------------------
+-- Moroes --
+-----------------------
+L= DBM:GetModLocalization(1837)
+
+-----------------------
+-- The Curator --
+-----------------------
+L= DBM:GetModLocalization(1836)
+
+-----------------------
+-- Shade of Medivh --
+-----------------------
+L= DBM:GetModLocalization(1817)
+
+-----------------------
+-- Mana Devourer --
+-----------------------
+L= DBM:GetModLocalization(1818)
+
+-----------------------
+-- Viz'aduum the Watcher --
+-----------------------
+L= DBM:GetModLocalization(1838)
+
+-----------------------
+--Nightbane
+-----------------------
+L = DBM:GetModLocalization("Nightbane")
+
+L:SetGeneralLocalization({
+	name =	"Schrecken der Nacht"
+})
+
+-----------------------
+--Return To Karazhan Trash
+-----------------------
+L = DBM:GetModLocalization("RTKTrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash der Rückkehr nach Karazhan"
+})
+
+L:SetMiscLocalization({
+	speedRun		=	"Die seltsame Kühle einer dunklen Präsenz durchweht die Luft..."
+})
+
+-----------------------
+-- <<<Cathedral of Eternal Night >>> --
+-----------------------
+-----------------------
+-- Agronox --
+-----------------------
+L= DBM:GetModLocalization(1905)
+
+-----------------------
+-- Trashbite the Scornful  --
+-----------------------
+L= DBM:GetModLocalization(1906)
+
+L:SetMiscLocalization({
+	bookCase	=	"hinter Bücherregal"
+})
+
+-----------------------
+-- Domatrax --
+-----------------------
+L= DBM:GetModLocalization(1904)
+
+-----------------------
+-- Mephistroth  --
+-----------------------
+L= DBM:GetModLocalization(1878)
+
+-----------------------
+--Cathedral of Eternal Night Trash
+-----------------------
+L = DBM:GetModLocalization("CoENTrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash der Kathedr. d. ew. Nacht"
+})
+
+-----------------------
+-- <<<Seat of Triumvirate >>> --
+-----------------------
+-----------------------
+-- Zuraal --
+-----------------------
+L= DBM:GetModLocalization(1979)
+
+-----------------------
+-- Saprish  --
+-----------------------
+L= DBM:GetModLocalization(1980)
+
+-----------------------
+-- Viceroy Nezhar --
+-----------------------
+L= DBM:GetModLocalization(1981)
+
+-----------------------
+-- L'ura  --
+-----------------------
+L= DBM:GetModLocalization(1982)
+
+-----------------------
+--Seat of Triumvirate Trash
+-----------------------
+L = DBM:GetModLocalization("SoTTrash")
+
+L:SetGeneralLocalization({
+	name =	"Trash des Sitzes des Triumvirats"
+})
+
