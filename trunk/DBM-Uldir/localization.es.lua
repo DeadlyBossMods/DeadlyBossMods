520c2eabeb3081a73946f08b6ddf00a2999d0bed
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-09-02 22:08:23 +0000

    *ES update *Added yells to Fetid Devourer and optimized UnitDebuff check some.

--- trunk/DBM-Uldir/localization.es.lua
+++ trunk/DBM-Uldir/localization.es.lua
@@ -1,10 +1,9 @@
 if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
-
 local L
 
------------------------
--- Taloc el Corrupto --
------------------------
+------------
+-- Taloc  --
+------------
 L= DBM:GetModLocalization(2168)
 
 L:SetMiscLocalization({
@@ -27,11 +26,10 @@ L= DBM:GetModLocalization(2146)
 L= DBM:GetModLocalization(2169)
 
 L:SetMiscLocalization({
-	CThunDisc	 =	"Disc accessed. C'thun data loading.",
-	YoggDisc	 =	"Disc accessed. Yogg-Saron data loading.",
-	CorruptedDisc =	"Disc accessed. Corrupted data loading."
+	CThunDisc	 =	"Acceso al disco. Cargando datos de C'Thun.",
+	YoggDisc	 =	"Acceso al disco. Cargando datos de Yogg-Saron.",
+	CorruptedDisc =	"Acceso al disco. Cargando datos corruptos."
 })
---WIP until I get actual footage of the fight in European Spanish, or proper datamining
 
 ------------
 -- Vectis --
@@ -39,8 +37,9 @@ L:SetMiscLocalization({
 L= DBM:GetModLocalization(2166)
 
 L:SetOptionLocalization({
-	ShowHighestFirst	 =	"Ordenar marco de información de Infección persistente de mayor a menor cantidad de acumulaciones (en lugar de menor a mayor)"
+	ShowHighestFirst	 =	"Ordenar marco de información de $spell:265127 de mayor a menor cantidad de acumulaciones (en lugar de menor a mayor)"
 })
+--Mostrar spellid en lugar de texto, pendiente de prueba
 
 ------------------------------
 -- Mythrax el Desintegrador --
@@ -53,11 +52,20 @@ L= DBM:GetModLocalization(2194)
 L= DBM:GetModLocalization(2195)
 
 L:SetTimerLocalization({
+	timerCallofCrawgCD		= "Tragadones (%s)",
+	timerCallofHexerCD 		= "Aojasangres (%s)",
+	timerCallofCrusherCD	= "Trituradora (%s)",
 	timerAddIncoming		= DBM_INCOMING
 })
 
 L:SetOptionLocalization({
 	timerAddIncoming		= "Mostrar temporizador para cuando los esbirros se vuelvan atacables"
+})
+
+L:SetMiscLocalization({
+	Crusher			=	"Trituradora",
+	Bloodhexer		=	"Aojasangre",
+	Crawg			=	"Tragadón"
 })
 
 ------------
