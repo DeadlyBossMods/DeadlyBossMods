1b1bb7988685c91d6ed78915262d90a40a1c7b8b
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-09-03 00:46:37 +0000

    Fix stormsong RP timer, wrong event

--- trunk/DBM-Party-BfA/Freehold/RingofBooty.lua
+++ trunk/DBM-Party-BfA/Freehold/RingofBooty.lua
@@ -91,7 +91,7 @@ function mod:SPELL_AURA_REMOVED_DOSE(args)
 		--"<78.80 02:52:31> [CLEU] SPELL_AURA_REMOVED#Creature-0-2084-1754-9152-130099-00007D20E9#Lightning#Creature-0-2084-1754-9152-130099-00007D20E9#Lightning#257829#Greasy#BUFF#nil", -- [62]
 		--"<104.47 02:52:56> [IsEncounterInProgress()] true", -- [69]
 		if amount == 0 then
-			timerRP:Start(25.5)
+			timerRP:Start(25)
 		end
 	end
 end
@@ -102,7 +102,7 @@ mod.SPELL_AURA_REMOVED = mod.SPELL_AURA_REMOVED_DOSE
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 129699 then--Ludwig Von Tortollen
-		timerRP:Start(35.5)
+		timerRP:Start(35)
 	end
 end
 
