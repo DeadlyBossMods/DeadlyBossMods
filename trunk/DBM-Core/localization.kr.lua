b6ecb21bebc1e6f08a38ad82c5a132fa37f43a7a
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-09-16 07:42:51 +0000

    Kr update

--- trunk/DBM-Core/localization.kr.lua
+++ trunk/DBM-Core/localization.kr.lua
@@ -56,6 +56,8 @@ DBM_CORE_TRANSCRIPTOR_LOG_START		= "Transcriptor 기록이 시작됐습니다."
 DBM_CORE_TRANSCRIPTOR_LOG_END		= "Transcriptor 기록이 종료됐습니다."
 
 DBM_CORE_MOVIE_SKIPPED				= "DBM이 동영상 자동 생략 기능을 작동시켰습니다."
+DBM_CORE_BONUS_SKIPPED				= "DBM이 자동으로 추가 전리품 주사위 굴림 창을 닫았습니다. 창을 열고 싶으면 3분 안에 /dbmbonusroll 명령어를 입력하세요"
+DBM_CORE_BONUS_EXPIRED				= "/dbmbonusroll 명령어를 사용해 추가 전리품 주사위 굴림 창을 열려고 했으나 현재 굴릴 수 있는 추가 전리품 주사위가 없습니다."
 
 DBM_CORE_AFK_WARNING				= "자리 비움 상태에서 전투에 돌입하여 (남은 생명력 %d퍼센트) 경고음을 재생중입니다. 자리를 비우고 있는게 아니라면 자리 비움 상태를 해제하거나 '기타 기능' 항목에서 해당 설정을 비활성화 해주세요."
 
@@ -247,6 +249,7 @@ DBM_COOLDOWN						= "%s 쿨타임"
 DBM_CORE_UNKNOWN					= "알 수 없음"--UNKNOWN
 DBM_CORE_LEFT						= "왼쪽"
 DBM_CORE_RIGHT						= "오른쪽"
+DBM_CORE_BOTH						= "양쪽"
 DBM_CORE_BACK						= "뒤쪽"--BACK
 DBM_CORE_SIDE						= "옆쪽"
 DBM_CORE_TOP						= "위쪽"
