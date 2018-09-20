dac9ff6e3c99e1a29b58370589061e51f77fe8a7
tandanu <tandanu@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-09-20 15:10:55 +0000

    tag as 8.0.10

---
+++ tags/8.0.10/DBM-Azeroth-BfA/Azurethos.lua
@@ -0,0 +1,84 @@
+local mod	= DBM:NewMod(2199, "DBM-Azeroth-BfA", nil, 1028)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision$"):sub(12, -3))
+mod:SetCreatureID(136385)
+--mod:SetEncounterID(1880)
+mod:SetReCombatTime(20)
+mod:SetZone()
+--mod:SetMinSyncRevision(11969)
+
+mod:RegisterCombat("combat")
+
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_START 274839 274829 274832"
+)
+
+--TODO, see if can detect gale force teleport target
+--local warnMothersEmbrace			= mod:NewTargetAnnounce(219045, 3)
+
+local specWarnAzurethosFury			= mod:NewSpecialWarningDodge(274839, nil, nil, nil, 2, 2)
+local specWarnGaleForce				= mod:NewSpecialWarningDodge(274829, nil, nil, nil, 2, 2)
+local specWarnWingBuffet			= mod:NewSpecialWarningDodge(274832, nil, nil, nil, 1, 2)
+
+local timerAzurethosFuryCD			= mod:NewCDTimer(46.8, 274839, nil, nil, nil, 2)
+--"Gale Force-274829-npc:136385 = pull:40.7, 47.1, 19.8, 33.7, 47.5, 19.6, 31.9, 18.4, 32.2", --
+--local timerGaleForceCD			= mod:NewAITimer(16, 274829, nil, nil, nil, 3)--2 timers alternating, problem is, joining mid fight you don't know what count you're on
+--"Wing Buffet-274832-npc:136385 = pull:2.8, 42.1, 11.2, 35.9, 11.1, 42.8, 11.5, 35.8, 11.4, 39.8, 10.7, 40.0"
+--local timerWingBuffetCD				= mod:NewAITimer(16, 274832, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)--Also alternating timers. on a world boss this just won't work
+
+--mod:AddRangeFrameOption(5, 194966)
+--mod:AddReadyCheckOption(37460, false)
+
+function mod:OnCombatStart(delay, yellTriggered)
+	if yellTriggered then
+		--timerAzurethosFuryCD:Start(1-delay)
+		--timerGaleForceCD:Start(1-delay)
+		--timerWingBuffetCD:Start(1-delay)
+	end
+end
+
+function mod:OnCombatEnd()
+--	if self.Options.RangeFrame then
+--		DBM.RangeCheck:Hide()
+--	end
+end
+
+function mod:SPELL_CAST_START(args)
+	local spellId = args.spellId
+	if spellId == 274839 then
+		specWarnAzurethosFury:Show()
+		specWarnAzurethosFury:Play("watchstep")
+		timerAzurethosFuryCD:Start()
+	elseif spellId == 274829 then
+		specWarnGaleForce:Show()
+		specWarnGaleForce:Play("shockwave")
+		--timerGaleForceCD:Start()
+	elseif spellId == 274832 then
+		specWarnWingBuffet:Show()
+		specWarnWingBuffet:Play("shockwave")
+		--timerWingBuffetCD:Start()
+	end
+end
+
+--[[
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
+	if spellId == 228007 and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) then
+		specWarnGTFO:Show()
+		specWarnGTFO:Play("runaway")
+	end
+end
+mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
+
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 124396 then
+		
+	end
+end
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
+	if spellId == 257939 then
+	end
+end
+--]]
---
+++ tags/8.0.10/DBM-Uldir/localization.es.lua
@@ -0,0 +1,83 @@
+if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
+local L
+
+------------
+-- Taloc  --
+------------
+L= DBM:GetModLocalization(2168)
+
+L:SetMiscLocalization({
+	Aggro	 =	"tiene amenaza"
+})
+
+-----------
+-- MADRE --
+-----------
+L= DBM:GetModLocalization(2167)
+
+----------------------
+-- Devorador fétido --
+----------------------
+L= DBM:GetModLocalization(2146)
+
+--------------------------------
+-- Zek'voz, Heraldo de N'Zoth --
+--------------------------------
+L= DBM:GetModLocalization(2169)
+
+L:SetMiscLocalization({
+	CThunDisc	 =	"Acceso al disco. Cargando datos de C'Thun.",
+	YoggDisc	 =	"Acceso al disco. Cargando datos de Yogg-Saron.",
+	CorruptedDisc =	"Acceso al disco. Cargando datos corruptos."
+})
+
+------------
+-- Vectis --
+------------
+L= DBM:GetModLocalization(2166)
+
+L:SetOptionLocalization({
+	ShowHighestFirst2	 =	"Ordenar marco de información de $spell:265127 de mayor a menor cantidad de acumulaciones (en lugar de menor a mayor)"
+})
+--Mostrar spellid en lugar de texto, pendiente de prueba
+
+------------------------------
+-- Mythrax el Desintegrador --
+------------------------------
+L= DBM:GetModLocalization(2194)
+
+---------------------
+-- Zul el Renacido --
+---------------------
+L= DBM:GetModLocalization(2195)
+
+L:SetTimerLocalization({
+	timerCallofCrawgCD		= "Tragadones (%s)",
+	timerCallofHexerCD 		= "Aojasangres (%s)",
+	timerCallofCrusherCD	= "Trituradora (%s)",
+	timerAddIncoming		= DBM_INCOMING
+})
+
+L:SetOptionLocalization({
+	timerAddIncoming		= "Mostrar temporizador para cuando los esbirros se vuelvan atacables"
+})
+
+L:SetMiscLocalization({
+	Crusher			=	"Trituradora",
+	Bloodhexer		=	"Aojasangre",
+	Crawg			=	"Tragadón"
+})
+
+------------
+-- G'huun --
+------------
+L= DBM:GetModLocalization(2147)
+
+----------------------
+-- Enemigos menores --
+----------------------
+L = DBM:GetModLocalization("UldirTrash")
+
+L:SetGeneralLocalization({
+	name =	"Enemigos menores"
+})
