74cfdba45e4f7f3a4206b62bad51ddbbeae444ec
ebmor <ebmor@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-09-22 22:59:16 +0000

    deDE update

--- trunk/DBM-Uldir/localization.de.lua
+++ trunk/DBM-Uldir/localization.de.lua
@@ -25,6 +25,14 @@ L= DBM:GetModLocalization(2146)
 ---------------------------
 L= DBM:GetModLocalization(2169)
 
+L:SetTimerLocalization({
+	timerOrbLands	= "Kugel (%s) landet"
+})
+
+L:SetOptionLocalization({
+	timerOrbLands	 =	"Zeige Zeit bis Kugel der Verderbnis landet"
+})
+
 L:SetMiscLocalization({
 	CThunDisc	 =	"Disc accessed. C'thun data loading.",--translate (trigger)
 	YoggDisc	 =	"Disc accessed. Yogg-Saron data loading.",--translate (trigger)
@@ -37,7 +45,12 @@ L:SetMiscLocalization({
 L= DBM:GetModLocalization(2166)
 
 L:SetOptionLocalization({
-	ShowHighestFirst2	 =	"Sortiere Infofenster nach dem höchsten Stapel von \"Anhaltende Infektion\" (statt dem niedrigsten Stapel)"
+	ShowHighestFirst2	 =	"Sortiere Infofenster nach dem höchsten Stapel von \"Anhaltende Infektion\" (statt dem niedrigsten Stapel)",
+	ShowOnlyParty		 =	"Zeige Anhaltende Infektion nur für deine Gruppe"
+})
+
+L:SetMiscLocalization({
+	BWIconMsg			 =	"Um Konfikte zu vermeiden, hat DBM das Setzen von Schlachtzugzeichen BW-Nutzern mit \"Assistent\"-Rechten überlassen. Stelle sicher, dass diese die Zeichensetzung aktiviert haben oder entziehe ihnen die \"Assistent\"-Rechte um die Zeichensetzung durch DBM zu aktivieren."
 })
 
 ---------------
@@ -57,7 +70,14 @@ L:SetTimerLocalization({
 })
 
 L:SetOptionLocalization({
-	timerAddIncoming		= "Zeit bis erscheinende Adds angreifbar sind anzeigen"
+	timerCallofCrawgCD		= "Zeige Zeit bis Krogg Pfützen entstehen",
+	timerCallofHexerCD 		= "Zeige Zeit bis Hexer Pfützen entstehen",
+	timerCallofCrusherCD	= "Zeige Zeit bis Zermalmer Pfützen entstehen",
+	timerAddIncoming		= "Zeit bis erscheinende Adds angreifbar sind anzeigen",
+	TauntBehavior			= "Setze Spottverhalten für Tankwechsel",
+	TwoHardThreeEasy		= "Wechsel bei 2 Stacks (heroisch/mythisch) bzw. 3 Stacks (andere Schwierigkeitsgrade)",
+	TwoAlways				= "Wechsel immer bei 2 Stacks (unabhängig vom Schwierigkeitsgrad)",
+	ThreeAlways				= "Wechsel immer bei 3 Stacks (unabhängig vom Schwierigkeitsgrad)"
 })
 
 L:SetMiscLocalization({
@@ -70,6 +90,21 @@ L:SetMiscLocalization({
 -- G'huun --
 ------------------
 L= DBM:GetModLocalization(2147)
+
+L:SetWarningLocalization({
+	warnMatrixFail		= "Energiematrix fallen gelassen"
+})
+
+L:SetOptionLocalization({
+	warnMatrixFail		= "Zeige Warnung, wenn eine Energiematrix fallen gelassen wird"
+})
+
+L:SetMiscLocalization({
+	CurrentMatrix		=	"Jetzige Matrix:",
+	NextMatrix			=	"Nächste Matrix:",
+	CurrentMatrixLong	=	"Jetzige Matrix (%s):",
+	NextMatrixLong		=	"Nächste Matrix (%s):"
+})
 
 -------------
 --  Trash  --
