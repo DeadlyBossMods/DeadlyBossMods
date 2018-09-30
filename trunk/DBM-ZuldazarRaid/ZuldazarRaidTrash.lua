efa1a7d12a7720bb747ad1eedf5ba40fc355f3f0
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-09-30 23:39:33 +0000

    Fix a couple bugs

--- trunk/DBM-ZuldazarRaid/ZuldazarRaidTrash.lua
+++ trunk/DBM-ZuldazarRaid/ZuldazarRaidTrash.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod("ZuldazarRaidTrash", "DBM-ZuldazarRaid")
+local mod	= DBM:NewMod("ZuldazarRaidTrash", "DBM-ZuldazarRaid", 1)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
