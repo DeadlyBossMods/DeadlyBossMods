f1988700f951efedd7f08b0805a3b2969a255af2
Adam <MysticalOS@users.noreply.github.com>
2018-10-04 05:32:12 +0000

    Flag grong as shared while at it

--- DBM-ZuldazarRaid/GrongtheRevenant.lua
+++ DBM-ZuldazarRaid/GrongtheRevenant.lua
@@ -1,5 +1,5 @@
 if UnitFactionGroup("player") ~= "Alliance" then return end--Don't load this version of fight on horde side
-local mod	= DBM:NewMod(2340, "DBM-ZuldazarRaid", 2, 1176)
+local mod	= DBM:NewMod(2340, "DBM-ZuldazarRaid", 1, 1176)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
