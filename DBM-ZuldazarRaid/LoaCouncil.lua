8578e275cf0391736eac559287c377d109fecd45
Adam <MysticalOS@users.noreply.github.com>
2018-10-04 15:36:42 +0000

    Change flags on these to what is assumed to make sense

--- DBM-ZuldazarRaid/LoaCouncil.lua
+++ DBM-ZuldazarRaid/LoaCouncil.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod(2330, "DBM-ZuldazarRaid", 1, 1176)
+local mod	= DBM:NewMod(2330, "DBM-ZuldazarRaid", 2, 1176)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
