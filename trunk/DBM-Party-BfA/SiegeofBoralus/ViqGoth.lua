a6d135c9fe1cb8b67ebb23a6be141c707e38833d
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-10-08 17:04:54 +0000

    Antispam slam in case you have too many tentacles up

--- trunk/DBM-Party-BfA/SiegeofBoralus/ViqGoth.lua
+++ trunk/DBM-Party-BfA/SiegeofBoralus/ViqGoth.lua
@@ -69,7 +69,7 @@ function mod:SPELL_CAST_START(args)
 		specWarnCalloftheDeep:Show()
 		specWarnCalloftheDeep:Play("watchstep")
 		--timerCalloftheDeepCD:Start()
-	elseif spellId == 269266 then
+	elseif spellId == 269266 and self:AntiSpam(2.5, 1) then
 		specWarnSlam:Show()
 		specWarnSlam:Play("shockwave")
 		timerSlamCD:Start(nil, args.sourceGUID)
