7c989e56dabd938b377ef385532afc47eedd2bf0
Adam <MysticalOS@users.noreply.github.com>
2018-10-17 01:38:04 +0000

    Duo drycode Also, since it was required by now two mods in zuldazar raid, DBM-Nameplate now supports auto hide if a duration is specified. Useful when using nameplate auras for SPELL_CAST_START events

--- DBM-ZuldazarRaid/Frida_Kanae.lua
+++ DBM-ZuldazarRaid/Frida_Kanae.lua
@@ -265,9 +265,12 @@ mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 147895 or cid == 145898 then--Rezani Disciple/Anointed Disciple
-	
+
 	elseif cid == 147896 or cid == 145903 then--Zandalari Crusader/Darkforged Crusader
 		timerBlindingFaithCD:Stop(args.destGUID)
+		if self.Options.NPAuraOnBlindingFaith then
+			DBM.Nameplate:Hide(true, args.destGUID, 283650)
+		end
 	end
 end
 
