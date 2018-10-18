316522a8f9e377ad96e757c3f6a5ef792392ee61
Adam <MysticalOS@users.noreply.github.com>
2018-10-18 22:55:44 +0000

    Updates to Duo to fix many items not in combat log as well as add a few helpful things like ember icons

--- DBM-ZuldazarRaid/FlameDuo.lua
+++ DBM-ZuldazarRaid/FlameDuo.lua
@@ -13,7 +13,7 @@ mod:SetEncounterID(2266, 2285)--2266 horde, 2285 Alliance
 --mod:DisableESCombatDetection()
 mod:SetZone()
 --mod:SetBossHPInfoToHighest()
---mod:SetUsedIcons(1, 2, 8)
+mod:SetUsedIcons(1, 2, 3)
 --mod:SetHotfixNoticeRev(17775)
 --mod:SetMinSyncRevision(16950)
 --mod.respawnTime = 35
@@ -21,9 +21,8 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 282030 284399",
-	"SPELL_CAST_SUCCESS 286988",
-	"SPELL_SUMMON 285647",
+	"SPELL_CAST_START 284399 285428",
+	"SPELL_CAST_SUCCESS 282030",
 	"SPELL_AURA_APPLIED 282037 285632 286425 286988",
 	"SPELL_AURA_APPLIED_DOSE 282037",
 	"SPELL_AURA_REMOVED 282037 285632 286425 286988",
@@ -31,10 +30,9 @@ mod:RegisterEventsInCombat(
 	"UNIT_SPELLCAST_SUCCEEDED boss1 boss2"
 )
 
---TODO, infoframe for showing balance of Harmonios Spiriits?
---TODO, Mestrah, the Illuminated CID might be wrong, there are 3 of them to choose from
+--TODO, infoframe for showing balance of Harmonios Spirits?
 --TODO, add Spinning Crane Kick? seems like an anti kite/no tank mechanic for most part
---TODO, Multi Sided Strike has MANY spell IDs, figure out right events and IDs for warning, including counting each strike
+--TODO, Multi Sided Strike, figure out how to warn each image?
 --TODO, improve spirits of Xuen warning when see how many go out, who has to switch, who should switch
 --TODO, figure out optimal lowest stacks possible for swapping rising flames
 --TODO, obviously boss swaps and stuff
@@ -43,13 +41,13 @@ mod:RegisterEventsInCombat(
 --local warnXorothPortal				= mod:NewSpellAnnounce(244318, 2, nil, nil, nil, nil, nil, 7)
 --Monk
 local warnSpiritsofXuen					= mod:NewSpellAnnounce(285647, 2)
---local warnActiveDecay					= mod:NewTargetNoFilterAnnounce(276434, 1)
 --Mage
 local warnRisingFlames					= mod:NewStackAnnounce(282037, 2, nil, "Tank")
-local warnMagmaTrap						= mod:NewCountAnnounce(284374, 4)
+local warnShield						= mod:NewTargetNoFilterAnnounce(286425, 4)
+--local warnMagmaTrap						= mod:NewCountAnnounce(284374, 4)
 --Team Attacks
 
-local specWarnMultiSidedStrike			= mod:NewSpecialWarningDefensive(282030, nil, nil, nil, 1, 2)
+local specWarnMultiSidedStrike			= mod:NewSpecialWarningYou(282030, nil, nil, nil, 3, 2)
 local specWarnStalking					= mod:NewSpecialWarningYou(285632, nil, nil, nil, 1, 2)
 local yellStalking						= mod:NewYell(285632)
 --Mage
@@ -57,24 +55,24 @@ local specWarnRisingFlames				= mod:NewSpecialWarningStack(282037, nil, 2, nil, 
 local specWarnRisingFlamesOther			= mod:NewSpecialWarningTaunt(282037, nil, nil, nil, 1, 2)
 --local yellDarkRevolation				= mod:NewPosYell(273365)
 local yellRisingFlamesFades				= mod:NewShortFadesYell(282037)
-local specWarnShield					= mod:NewSpecialWarningSwitch(286425, "Dps", nil, nil, 3, 2)
+local specWarnShield					= mod:NewSpecialWarningTargetChange(286425, false, nil, nil, 1, 2)
 local specWarnPyroblast					= mod:NewSpecialWarningInterrupt(286379, "HasInterrupt", nil, nil, 1, 2)
 local specWarnSearingEmbers				= mod:NewSpecialWarningYou(286988, nil, nil, nil, 1, 2)
-local yellSearingEmbers					= mod:NewYell(286988, nil, false)
+local yellSearingEmbers					= mod:NewYell(286988)
 local yellSearingEmbersFades			= mod:NewShortFadesYell(286988)
 --local specWarnBloodshard				= mod:NewSpecialWarningInterrupt(273350, false, nil, 4, 1, 2)
 --local specWarnGTFO					= mod:NewSpecialWarningGTFO(238028, nil, nil, nil, 1, 2)
 --Team Attacks
-local specWarnChiJisSong				= mod:NewSpecialWarningSwitch(284453, nil, nil, nil, 2, 2)
+local specWarnFirefromMist				= mod:NewSpecialWarningSwitch(285428, nil, nil, nil, 2, 2)
 
 --mod:AddTimerLine(DBM:EJ_GetSectionInfo(18527))
 --local timerDarkRevolationCD			= mod:NewCDCountTimer(55, 273365, nil, nil, nil, 3)
-local timerMultiSidedStrikeCD			= mod:NewAITimer(14.1, 282030, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
-local timerSpiritsofXuenCD				= mod:NewAITimer(55, 285645, nil, nil, nil, 1, nil, DBM_CORE_HEROIC_ICON)
+local timerMultiSidedStrikeCD			= mod:NewCDTimer(35.2, 282030, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerSpiritsofXuenCD				= mod:NewCDTimer(40.1, 285645, nil, nil, nil, 1, nil, DBM_CORE_HEROIC_ICON)
 --Mage
-local timerShieldCD						= mod:NewAITimer(55, 286425, nil, nil, nil, 4, nil, DBM_CORE_DAMAGE_ICON..DBM_CORE_INTERRUPT_ICON)
-local timerSearingEmbersCD				= mod:NewAITimer(55, 286988, nil, nil, nil, 3, nil, DBM_CORE_MAGIC_ICON..DBM_CORE_HEALER_ICON)
-local timerMagmaTrapCD					= mod:NewAITimer(55, 284374, nil, nil, nil, 5)
+local timerShieldCD						= mod:NewCDTimer(13.4, 286425, nil, nil, nil, 4, nil, DBM_CORE_DAMAGE_ICON..DBM_CORE_INTERRUPT_ICON)
+local timerSearingEmbersCD				= mod:NewCDTimer(15.8, 286988, nil, nil, nil, 3, nil, DBM_CORE_MAGIC_ICON..DBM_CORE_HEALER_ICON)--15.8-29.2?
+--local timerMagmaTrapCD					= mod:NewAITimer(55, 284374, nil, nil, nil, 5)
 --Team Attacks
 
 --local berserkTimer					= mod:NewBerserkTimer(600)
@@ -83,7 +81,7 @@ local timerMagmaTrapCD					= mod:NewAITimer(55, 284374, nil, nil, nil, 5)
 --local countdownRupturingBlood				= mod:NewCountdown("Alt12", 244016, false, 2, 3)
 --local countdownFelstormBarrage			= mod:NewCountdown("AltTwo32", 244000, nil, nil, 3)
 
---mod:AddSetIconOption("SetIconGift", 255594, true)
+mod:AddSetIconOption("SetIconEmbers", 286988, true)
 --mod:AddRangeFrameOption("8/10")
 mod:AddInfoFrameOption(281959, true)
 mod:AddNamePlateOption("NPAuraOnFixate", 268074)
@@ -91,15 +89,19 @@ mod:AddNamePlateOption("NPAuraOnExplosion", 284399)
 --mod:AddSetIconOption("SetIconDarkRev", 273365, true)
 
 mod.vb.phase = 1
-mod.vb.shieldActive = false
-mod.vb.magmaTrapCount = 0
+mod.vb.shieldsActive = 0
+mod.vb.embersIcon = 0
+--mod.vb.magmaTrapCount = 0
 
 function mod:OnCombatStart(delay)
 	self.vb.phase = 1
-	self.vb.shieldActive = false
-	self.vb.magmaTrapCount = 0
-	timerMultiSidedStrikeCD:Start(1-delay)
-	timerSpiritsofXuenCD:Start(1-delay)
+	self.vb.shieldsActive = 0
+	self.vb.embersIcon = 0
+	--self.vb.magmaTrapCount = 0
+	timerSearingEmbersCD:Start(9.5)
+	timerShieldCD:Start(10.5-delay)
+	timerMultiSidedStrikeCD:Start(15.6-delay)
+	timerSpiritsofXuenCD:Start(25.5-delay)
 	if self.Options.NPAuraOnFixate or self.Options.NPAuraOnExplosion then
 		DBM:FireEvent("BossMod_EnableHostileNameplates")
 	end
@@ -123,20 +125,13 @@ end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 282030 then
-		for i = 1, 2 do
-			local bossUnitID = "boss"..i
-			if UnitExists(bossUnitID) and UnitGUID(bossUnitID) == args.sourceGUID and UnitDetailedThreatSituation("player", bossUnitID) then--We are highest threat target
-				specWarnMultiSidedStrike:Show()--So show tank warning
-				specWarnMultiSidedStrike:Play("defensive")
-				break
-			end
-		end
-		timerMultiSidedStrikeCD:Start()
-	elseif spellId == 284399 then
+	if spellId == 284399 then
 		if self.Options.NPAuraOnExplosion then
 			DBM.Nameplate:Show(true, args.sourceGUID, spellId, nil, 10)
 		end
+	elseif spellId == 285428 then
+		specWarnFirefromMist:Show()
+		specWarnFirefromMist:Play("attbomb")
 --	elseif spellId == 273350 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
 --		specWarnBloodshard:Show(args.sourceName)
 --		specWarnBloodshard:Play("kickcast")
@@ -145,16 +140,12 @@ end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 286988 then
-		timerSearingEmbersCD:Start()
-	end
-end
-
-function mod:SPELL_SUMMON(args)
-	local spellId = args.spellId
-	if spellId == 285647 and self:AntiSpam(5, 1) then
-		warnSpiritsofXuen:Show()
-		timerSpiritsofXuenCD:Start()
+	if spellId == 282030 then
+		if args:IsPlayer() then
+			specWarnMultiSidedStrike:Show()--So show tank warning
+			specWarnMultiSidedStrike:Play("targetyou")
+		end
+		timerMultiSidedStrikeCD:Start()
 	end
 end
 
@@ -192,11 +183,15 @@ function mod:SPELL_AURA_APPLIED(args)
 			end
 		end
 	elseif spellId == 286425 then
-		self.vb.shieldActive = true
-		specWarnShield:Show()
-		specWarnShield:Play("targetchange")
+		self.vb.shieldsActive = self.vb.shieldsActive + 1
+		if self.Options.SpecWarn286425switch2 then
+			specWarnShield:Show(args.destName)
+			specWarnShield:Play("targetchange")
+		else
+			warnShield:Show(args.destName)
+		end
 		timerShieldCD:Start()
-		if self.Options.InfoFrame then
+		if self.Options.InfoFrame and self.vb.shieldsActive == 1 then
 			for i = 1, 2 do
 				local bossUnitID = "boss"..i
 				if UnitGUID(bossUnitID) == args.sourceGUID then--Identify correct unit ID
@@ -207,12 +202,15 @@ function mod:SPELL_AURA_APPLIED(args)
 			end
 		end
 	elseif spellId == 286988 then
-		
+		self.vb.embersIcon = self.vb.embersIcon + 1
 		if args:IsPlayer() then
 			specWarnSearingEmbers:Show()
 			specWarnSearingEmbers:Play("targetyou")
 			yellSearingEmbers:Yell()
 			yellSearingEmbersFades:Countdown(10)
+		end
+		if self.Options.SetIconEmbers then
+			self:SetIcon(args.destName, self.vb.embersIcon)
 		end
 	end
 end
@@ -229,12 +227,18 @@ function mod:SPELL_AURA_REMOVED(args)
 			DBM.Nameplate:Hide(true, args.sourceGUID, spellId)
 		end
 	elseif spellId == 286425 then
-		self.vb.shieldActive = false
+		self.vb.shieldsActive = self.vb.shieldsActive - 1
 		specWarnPyroblast:Show(args.destName)
 		specWarnPyroblast:Play("kickcast")
+		if self.Options.InfoFrame and self.vb.shieldsActive == 0 then
+			DBM.InfoFrame:Hide()
+		end
 	elseif spellId == 286988 then
 		if args:IsPlayer() then
 			yellSearingEmbersFades:Cancel()
+		end
+		if self.Options.SetIconEmbers then
+			self:SetIcon(args.destName, 0)
 		end
 	end
 end
@@ -262,11 +266,14 @@ end
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
 	if spellId == 284373 then--Magma Trap
-		self.vb.magmaTrapCount = self.vb.magmaTrapCount + 1
-		warnMagmaTrap:Show(self.vb.magmaTrapCount)
-		timerMagmaTrapCD:Start()
-	elseif spellId == 286370 or spellId == 286368 then--Chi-Ji's Song
-		specWarnChiJisSong:Show()
-		specWarnChiJisSong:Play("attbomb")
+		--self.vb.magmaTrapCount = self.vb.magmaTrapCount + 1
+		--warnMagmaTrap:Show(self.vb.magmaTrapCount)
+		--timerMagmaTrapCD:Start()
+	elseif spellId == 285645 then
+		warnSpiritsofXuen:Show()
+		timerSpiritsofXuenCD:Start()
+	elseif spellId == 286987 then--Searing Embers
+		self.vb.embersIcon = 0
+		timerSearingEmbersCD:Start()
 	end
 end
