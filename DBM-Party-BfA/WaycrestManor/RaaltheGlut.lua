b9bc4aa79c064c936156af07bc5cbb1a6f529850
Adam <MysticalOS@users.noreply.github.com>
2018-10-26 04:34:15 +0000

    Raal will no longer warn to switch to adds if raal is below 10% health when they come out voice for tenderize on raal now uses shockwave, which fits better than watch step

--- DBM-Party-BfA/WaycrestManor/RaaltheGlut.lua
+++ DBM-Party-BfA/WaycrestManor/RaaltheGlut.lua
@@ -47,14 +47,17 @@ end
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 264931 then
-		specWarnServant:Show()
-		specWarnServant:Play("killmob")
+		local bossHealth = self:GetBossHPByGUID(args.sourceGUID)
+		if bossHealth and bossHealth >= 10 then--Only warn to switch to add if boss above 10%, else ignore them
+			specWarnServant:Show()
+			specWarnServant:Play("killmob")
+		end
 		--timerServantCD:Start()
 	elseif spellId == 264923 then
 		self.vb.tenderizeCount = self.vb.tenderizeCount + 1
 		if self.vb.tenderizeCount == 1 then
 			specWarnTenderize:Show()
-			specWarnTenderize:Play("watchstep")
+			specWarnTenderize:Play("shockwave")
 			--timerTenderizeCD:Start()
 		else
 			warnTenderize:Show(self.vb.tenderizeCount)
