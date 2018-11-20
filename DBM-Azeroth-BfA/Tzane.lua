af27bfbe500c334010e2dee7c57d4024da90acb1
Adam <MysticalOS@users.noreply.github.com>
2018-11-20 19:42:35 +0000

    Minor timer tweaks

--- DBM-Azeroth-BfA/Tzane.lua
+++ DBM-Azeroth-BfA/Tzane.lua
@@ -28,10 +28,10 @@ local specWarnTerrorWall			= mod:NewSpecialWarningDodge(261552, nil, nil, nil, 3
 
 --local specWarnGTFO				= mod:NewSpecialWarningGTFO(238028, nil, nil, nil, 1, 8)
 
-local timerCrushingSlamCD			= mod:NewCDTimer(24.8, 262004, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)--24.8-31?
-local timerCoalescedEssenceCD		= mod:NewCDTimer(25.5, 261600, nil, nil, nil, 3)--25-28?
+local timerCrushingSlamCD			= mod:NewCDTimer(23.2, 262004, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)--24.8-31?
+local timerCoalescedEssenceCD		= mod:NewCDTimer(23.6, 261600, nil, nil, nil, 3)--25-28?
 local timerConsumingSpiritsCD		= mod:NewCDTimer(21.9, 261605, nil, nil, nil, 3)--21-35?
-local timerTerrorWallCD				= mod:NewCDTimer(24.9, 261552, nil, nil, nil, 3)--24-29?
+local timerTerrorWallCD				= mod:NewCDTimer(23.2, 261552, nil, nil, nil, 3)--24-29?
 
 mod:AddRangeFrameOption(8, 261605)
 --mod:AddReadyCheckOption(37460, false)
