11def86cec03654b71e23b234712295030af10bf
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2018-12-22 18:00:24 +0000

    Kr Update

--- trunk/DBM-GUI/localization.kr.lua
+++ trunk/DBM-GUI/localization.kr.lua
@@ -273,9 +273,9 @@ L.Area_PullTimer			= "풀링, 휴식, 전투, 사용자 지정 바 필터 설정
 L.DontShowPTNoID			= "같은 지역에 없는 사용자가 보낸 풀링 타이머 차단"
 L.DontShowPT				= "풀링/휴식 타이머 표시 안함"
 L.DontShowPTText			= "풀링/휴식 알림 텍스트 표시 안함"
-L.DontPlayPTCountdown		= "풀링/휴식/전투/사용자 지정 초읽기 음성 재생 안함"
 L.DontShowPTCountdownText	= "풀링 초읽기 숫자 표시 안함"
-L.PT_Threshold				= "휴식/전투/사용자 지정 타이머 초읽기 숫자 표시 안함: %d초 까지"
+L.DontPlayPTCountdown		= "풀링/휴식/전투/사용자 지정 초읽기 전구간 음성 재생 안함"
+L.PT_Threshold				= "휴식/전투/사용자 지정 타이머 초읽기 음성 재생 안함: %d초 까지"
 
 L.Panel_HideBlizzard		= "블리자드 기능 끄기 및 숨김"
 L.Area_HideBlizzard			= "블리자드 기능 끄기 및 숨김 설정"
