a82042638e3bda64de3fdc2bf80e8e81d885b7d6
Omega <Omega@a6602f14-c1bc-49d1-8d65-da3458cc0765>
2019-01-31 23:40:47 +0000

    Spanish update

---
+++ trunk/DBM-ZuldazarRaid/localization.es.lua
@@ -0,0 +1,89 @@
+if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
+local L
+
+------------------------
+-- Campeona de la Luz --
+------------------------
+L= DBM:GetModLocalization(2344)--Ra'wani Kanae (Alianza)
+
+L= DBM:GetModLocalization(2333)--Frida Fuelleférreo (Horda)
+
+-----------
+-- Grong --
+-----------
+L= DBM:GetModLocalization(2325)--Rey Grong (Horda)
+
+L= DBM:GetModLocalization(2340)--Grong el Aparecido (Alianza)
+
+----------------------------
+-- Maestros Fuego de Jade --
+----------------------------
+L= DBM:GetModLocalization(2323)--Ma'ra Colmillosiniestro y Anathos Clamafuegos (Allianza)
+
+L= DBM:GetModLocalization(2341)--Manceroy Puñoígneo y Mestrah la Iluminada (Horda)
+
+---------------
+-- Opulencia --
+---------------
+L= DBM:GetModLocalization(2342)
+
+L:SetWarningLocalization({
+
+})
+
+L:SetTimerLocalization({
+
+})
+
+L:SetOptionLocalization({
+
+})
+
+L:SetMiscLocalization({
+	Bulwark =	"Baluarte",
+	Hand	=	"Mano"
+})
+
+------------------------------
+-- Cónclave de los Elegidos --
+------------------------------
+L= DBM:GetModLocalization(2330)
+
+L:SetMiscLocalization({
+	BwonsamdiWrath =	"Well, if ya so eager for death, ya shoulda come see me sooner!",--Need footage in EU Spanish
+	BwonsamdiWrath2 =	"Sooner or later... everybody serves me!",--Need footage in EU Spanish
+	Bird			 =	"Pa'ku"
+})
+
+-------------------
+-- Rey Rastakhan --
+-------------------
+L= DBM:GetModLocalization(2335)
+
+L:SetOptionLocalization({
+	AnnounceAlternatePhase	= "Mostrar avisos generales del reino de la muerte si te encuentras en el de los vivos, y viceversa (esta opción no afecta a los temporizadores, los cuales se muestran siempre)"
+})
+
+-------------------------------
+-- Manitas mayor Mekkatorque --
+-------------------------------
+L= DBM:GetModLocalization(2332)
+
+----------------------------
+-- Bloqueo de la tormenta --
+----------------------------
+L= DBM:GetModLocalization(2337)
+
+-------------------------
+-- Lady Jaina Valiente --
+-------------------------
+L= DBM:GetModLocalization(2343)
+
+----------------------
+-- Enemigos menores --
+----------------------
+L = DBM:GetModLocalization("ZuldazarRaidTrash")
+
+L:SetGeneralLocalization({
+	name =	"Enemigos menores"
+})
