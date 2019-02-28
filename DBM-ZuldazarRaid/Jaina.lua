778059f1e83d3ee3652fd253e7475126ff346530
Adam <MysticalOS@users.noreply.github.com>
2019-02-28 03:52:25 +0000

    Make this on by default. because it’s kinda sickening to add something, then instead of users enabling it, they install a weak aura that does same thing.
    It’s clear that if something is off by default they assume that DBM doesn’t do it and find something else that does :\

--- DBM-ZuldazarRaid/Jaina.lua
+++ DBM-ZuldazarRaid/Jaina.lua
@@ -71,7 +71,7 @@ local warnCrystalDust					= mod:NewCountAnnounce(289940, 3)
 
 --General
 local specWarnFreezingBlood				= mod:NewSpecialWarningMoveTo(289387, false, nil, 3, 1, 2)
-local yellFreezingBlood					= mod:NewFadesYell(289387, L.Freezing, false, nil, "YELL")
+local yellFreezingBlood					= mod:NewFadesYell(289387, L.Freezing, true, 2, "YELL")
 local specWarnChillingStack				= mod:NewSpecialWarningStack(287993, nil, 2, nil, nil, 1, 6)
 --Stage One: Burning Seas
 local specWarnIceShard					= mod:NewSpecialWarningTaunt(285253, false, nil, nil, 1, 2)
