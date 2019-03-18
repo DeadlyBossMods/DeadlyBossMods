44b9c9f2ee958c49529e6b72aa8d598745d34c41
Adam <MysticalOS@users.noreply.github.com>
2019-03-18 18:26:34 +0000

    Added pyroblast interrupt warning back in from earlier seasons, now that it has smarter check code it can use in core Added dodge warning and timer to dark outpour as well

--- DBM-Brawlers/Rank4.lua
+++ DBM-Brawlers/Rank4.lua
@@ -7,7 +7,7 @@ mod:SetZone()
 mod:SetUsedIcons(8)
 
 mod:RegisterEvents(
-	"SPELL_CAST_START 33975 136334 132666"
+	"SPELL_CAST_START 33975 136334 132666 291394"
 --	"SPELL_AURA_APPLIED 236155",
 --	"SPELL_AURA_APPLIED_DOSE 236155",
 --	"SPELL_AURA_REMOVED 228981"
@@ -17,10 +17,14 @@ mod:RegisterEvents(
 --TODO, stickes stack warning for Aura of Rot
 local warnPyroblast				= mod:NewCastAnnounce(33975, 3)--Sanoriak
 local warnFireWall				= mod:NewSpellAnnounce(132666, 4)--Sanoriak
+local warnDarkOutpour			= mod:NewSpellAnnounce(291394, 4)--Ouroboros
 
 local specWarnFireWall			= mod:NewSpecialWarningDodge(132666, nil, nil, nil, 2, 2)--Sanoriak
+local specWarnPyroblast			= mod:NewSpecialWarningInterrupt(33975, nil, nil, nil, 1, 2)--Sanoriak
+local specWarnDarkOutpour		= mod:NewSpecialWarningDodge(291394, nil, nil, nil, 2, 2)--Ouroboros
 
 --local timerFirewallCD			= mod:NewCDTimer(17, 132666, nil, nil, nil, 3)--Sanoriak
+local timerDarkOutpourCD		= mod:NewCDTimer(43.5, 291394, nil, nil, nil, 3)--Ouroboros
 
 local brawlersMod = DBM:GetModByName("Brawlers")
 --local DominikaGUID = 0
@@ -28,7 +32,12 @@ local brawlersMod = DBM:GetModByName("Brawlers")
 function mod:SPELL_CAST_START(args)
 	if not brawlersMod.Options.SpectatorMode and not brawlersMod:PlayerFighting() then return end--Spectator mode is disabled, do nothing.
 	if args:IsSpellID(33975, 136334) then
-		warnPyroblast:Show()
+		if brawlersMod:PlayerFighting() and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+			specWarnPyroblast:Show(args.sourceName)
+			specWarnPyroblast:Play("kickcast")
+		else
+			warnPyroblast:Show()
+		end
 	elseif args.spellId == 132666 then
 		--timerFirewallCD:Start()--First one is 5 seconds after combat start
 		if brawlersMod:PlayerFighting() then
@@ -36,6 +45,14 @@ function mod:SPELL_CAST_START(args)
 			specWarnFireWall:Play("watchstep")
 		else
 			warnFireWall:Show()
+		end
+	elseif args.spellId == 291394 then
+		timerDarkOutpourCD:Start()
+		if brawlersMod:PlayerFighting() then
+			specWarnDarkOutpour:Show()
+			specWarnDarkOutpour:Play("justrun")
+		else
+			warnDarkOutpour:Show()
 		end
 	end
 end
