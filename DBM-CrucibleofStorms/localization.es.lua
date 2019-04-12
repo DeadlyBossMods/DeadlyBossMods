4d4edb53d7808e181e68b2a939a3af2bbacb1e1d
Adam <MysticalOS@users.noreply.github.com>
2019-04-12 16:38:40 +0000

    spanish update

---
+++ DBM-CrucibleofStorms/localization.es.lua
@@ -0,0 +1,42 @@
+if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
+local L
+
+---------------------------
+-- La Cábala Infatigable --
+---------------------------
+L= DBM:GetModLocalization(2328)
+
+L:SetMiscLocalization({
+	Zaxasj = "Zaxasj",
+	Fathuul = "Fa'thuul"
+})
+
+----------------------------------
+-- Uu'nat, Presagista del Vacío --
+----------------------------------
+L= DBM:GetModLocalization(2332)
+
+L:SetWarningLocalization({
+
+})
+
+L:SetTimerLocalization({
+
+})
+
+L:SetOptionLocalization({
+
+})
+
+L:SetMiscLocalization({
+
+})
+
+----------------------
+-- Enemigos menores --
+----------------------
+L = DBM:GetModLocalization("CrucibleofStormsTrash")
+
+L:SetGeneralLocalization({
+	name =	"Enemigos menores"
+})
