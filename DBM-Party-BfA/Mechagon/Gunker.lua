d0acb26dea2373e06c365364545fb196618394a1
MysticalOS <mysticalosx@gmail.com>
2019-04-17 21:37:09 -0400

    Mods for first two bosses of Mechagon

--- DBM-Party-BfA/Mechagon/Gunker.lua
+++ DBM-Party-BfA/Mechagon/Gunker.lua
@@ -2,32 +2,42 @@ local mod	= DBM:NewMod(2358, "DBM-Party-BfA", 11, 1178)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
---mod:SetCreatureID(127484)
+mod:SetCreatureID(150222)
 mod:SetEncounterID(2292)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
---	"SPELL_AURA_APPLIED",
---	"SPELL_AURA_REMOVED",
---	"SPELL_CAST_START",
---	"SPELL_CAST_SUCCESS"
+	"SPELL_AURA_APPLIED 298259 297913",
+	"SPELL_AURA_REMOVED 298259",
+	"SPELL_CAST_START 297834 297835",
+	"SPELL_CAST_SUCCESS 297821 297985"
 )
 
---local warnSmokePowder				= mod:NewSpellAnnounce(257793, 2)
+local warnGooped					= mod:NewTargetNoFilterAnnounce(298259, 2)
+local warnSplatter					= mod:NewSpellAnnounce(297985, 2)
 
---local specWarnHowlingFear			= mod:NewSpecialWarningInterrupt(257791, "HasInterrupt", nil, nil, 1, 2)
---local yellSwirlingScythe			= mod:NewYell(195254)
+local specWarnToxicWave				= mod:NewSpecialWarningSpell(297834, nil, nil, nil, 2, 2)
+local specWarnGooped				= mod:NewSpecialWarningYou(298259, nil, nil, nil, 1, 2)
+local yellGooped					= mod:NewYell(298259)
+local specWarnGoopedDispel			= mod:NewSpecialWarningDispel(298259, "RemoveDisease", nil, nil, 1, 2)
+local specWarnToxicGoopDispel		= mod:NewSpecialWarningDispel(297913, false, nil, nil, 1, 2)
+local specWarnCoalesce				= mod:NewSpecialWarningDodge(297835, nil, nil, nil, 2, 2)
 --local specWarnGTFO				= mod:NewSpecialWarningGTFO(238028, nil, nil, nil, 1, 8)
 
---local timerHowlingFearCD			= mod:NewAITimer(13.4, 257791, nil, "HasInterrupt", nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
---local timerFlashingDaggerCD			= mod:NewAITimer(31.6, 257785, nil, nil, nil, 2, nil, DBM_CORE_DEADLY_ICON)
+local timerToxicGoopCD				= mod:NewAITimer(31.6, 297913, nil, nil, nil, 3, nil, DBM_CORE_DISEASE_ICON)
+local timerToxicWaveCD				= mod:NewAITimer(31.6, 297834, nil, nil, nil, 2)
+local timerSplatterCD				= mod:NewAITimer(31.6, 297985, nil, nil, nil, 3)
+local timerCoalesceCD				= mod:NewAITimer(31.6, 297835, nil, nil, nil, 3)
 
 --mod:AddRangeFrameOption(5, 194966)
---[[
-function mod:OnCombatStart(delay)
 
+function mod:OnCombatStart(delay)
+	timerToxicGoopCD:Start(1-delay)
+	timerToxicWaveCD:Start(1-delay)
+	timerSplatterCD:Start(1-delay)
+	timerCoalesceCD:Start(1-delay)
 end
 
 function mod:OnCombatEnd()
@@ -39,7 +49,23 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
 	if spellId == 257777 then
-
+		if args:IsPlayer() then
+			specWarnGooped:Show()
+			specWarnGooped:Play("targetyou")
+		elseif self.Options.SpecWarn298259dispel and self:CheckDispelFilter() then
+			specWarnGoopedDispel:Show(args.destName)
+			specWarnGoopedDispel:Play("helpdispel")
+		else
+			warnGooped:CombinedShow(1, args.destName)
+		end
+	elseif spellId == 297913 then
+		if args:IsPlayer() then
+			specWarnGooped:Show()
+			specWarnGooped:Play("targetyou")
+		elseif self.Options.SpecWarn297913dispel and self:CheckDispelFilter() then
+			specWarnToxicGoopDispel:Show(args.destName)
+			specWarnToxicGoopDispel:Play("helpdispel")
+		end
 	end
 end
 --mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
@@ -53,19 +79,28 @@ end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 257791 then
-
+	if spellId == 297834 then
+		specWarnToxicWave:Show()
+		specWarnToxicWave:Play("specialsoon")--watchwave (if dodgable)
+		timerToxicWaveCD:Start()
+	elseif spellId == 297835 then
+		specWarnCoalesce:Show()
+		specWarnCoalesce:Play("watchstep")
+		timerCoalesceCD:Start()
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 257777 then
-
+	if spellId == 297821 then
+		timerToxicGoopCD:Start()
+	elseif spellId == 297985 then
+		warnSplatter:Show()
+		timerSplatterCD:Start()
 	end
 end
 
-
+--[[
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 	if spellId == 228007 and destGUID == UnitGUID("player") and self:AntiSpam(2, 4) then
 		specWarnGTFO:Show()
--- DBM-Party-BfA/Mechagon/KingGobbamak.lua
+++ DBM-Party-BfA/Mechagon/KingGobbamak.lua
@@ -2,32 +2,42 @@ local mod	= DBM:NewMod(2357, "DBM-Party-BfA", 11, 1178)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
---mod:SetCreatureID(127484)
+mod:SetCreatureID(150159)
 mod:SetEncounterID(2290)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
---	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_APPLIED 297257 297261",
 --	"SPELL_AURA_REMOVED",
---	"SPELL_CAST_START",
---	"SPELL_CAST_SUCCESS"
+	"SPELL_CAST_START 297254",
+	"SPELL_CAST_SUCCESS 297465"
 )
 
---local warnSmokePowder				= mod:NewSpellAnnounce(257793, 2)
+--TODO: Get right event for Rumble
+--TODO, add nameplate aura for https://ptr.wowhead.com/spell=297318/powered-up if it's on a unit with nameplate
+local warnElectricalCharge			= mod:NewTargetAnnounce(297257, 2)
+local warnGetEm						= mod:NewSpellAnnounce(297465, 2)
 
+local specWarnChargedSmash			= mod:NewSpecialWarningSpell(297254, nil, nil, nil, 2, 2)
 --local specWarnHowlingFear			= mod:NewSpecialWarningInterrupt(257791, "HasInterrupt", nil, nil, 1, 2)
---local yellSwirlingScythe			= mod:NewYell(195254)
+local specWarnElectricalCharge		= mod:NewSpecialWarningYou(297257, nil, nil, nil, 1, 2)
+local yellElectricalCharge			= mod:NewYell(297257, nil, false)
+local specWarnRumble				= mod:NewSpecialWarningSpell(297261, nil, nil, nil, 2, 2)
 --local specWarnGTFO				= mod:NewSpecialWarningGTFO(238028, nil, nil, nil, 1, 8)
 
 --local timerHowlingFearCD			= mod:NewAITimer(13.4, 257791, nil, "HasInterrupt", nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
---local timerFlashingDaggerCD			= mod:NewAITimer(31.6, 257785, nil, nil, nil, 2, nil, DBM_CORE_DEADLY_ICON)
+local timerChargedSmashCD			= mod:NewAITimer(31.6, 297254, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerRumbleCD					= mod:NewAITimer(31.6, 297261, nil, nil, nil, 2, nil, DBM_CORE_HEALER_ICON)
+local timerGetEmCD					= mod:NewAITimer(31.6, 297465, nil, nil, nil, 1)
 
 --mod:AddRangeFrameOption(5, 194966)
---[[
-function mod:OnCombatStart(delay)
 
+function mod:OnCombatStart(delay)
+	timerChargedSmashCD:Start(1-delay)
+	timerRumbleCD:Start(1-delay)
+	timerGetEmCD:Start(1-delay)
 end
 
 function mod:OnCombatEnd()
@@ -38,34 +48,49 @@ end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 257777 then
-
+	if spellId == 297257 then
+		if args:IsPlayer() then
+			specWarnElectricalCharge:Show()
+			specWarnElectricalCharge:Play("targetyou")
+			yellElectricalCharge:Yell()
+		else
+			warnElectricalCharge:Show(args.destName)
+		end
+	elseif spellId == 297261 then
+		specWarnRumble:Show()
+		specWarnRumble:Play("aesoon")
+		timerRumbleCD:Start()
 	end
 end
 --mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
+--[[
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 257827 then
+	if spellId == 297257 then
 
 	end
 end
+--]]
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 257791 then
-
+	if spellId == 297254 then
+		specWarnChargedSmash:Show()
+		specWarnChargedSmash:Play("helpsoak")
+		timerChargedSmashCD:Start()
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 257777 then
-
+	if spellId == 297465 then
+		warnGetEm:Show()
+		timerGetEmCD:Start()
 	end
 end
 
-
+--[[
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
 	if spellId == 228007 and destGUID == UnitGUID("player") and self:AntiSpam(2, 4) then
 		specWarnGTFO:Show()
