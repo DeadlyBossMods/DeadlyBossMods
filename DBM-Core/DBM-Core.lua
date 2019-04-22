5ab2f7e64d771badf9b7f2befc4f24c90b844d33
MysticalOS <mysticalosx@gmail.com>
2019-04-22 01:39:27 -0400

    Prep A release Version

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -61,8 +61,8 @@ end
 
 DBM = {
 	Revision = parseCurseDate("@file-date-integer@"),
-	DisplayVersion = "8.1.19 alpha", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2019, 4, 17) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DisplayVersion = "8.1.19", -- the string that is shown as version
+	ReleaseRevision = releaseDate(2019, 4, 21) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
