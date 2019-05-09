671f0c326eb7ac2627172d6bdf4ef3f9437b0362
MysticalOS <mysticalosx@gmail.com>
2019-05-08 21:02:05 -0400

    Push 2 new boss mods for Eternal Palace

--- DBM-EternalPalace/RageofAzshara.lua
+++ DBM-EternalPalace/RageofAzshara.lua
@@ -12,23 +12,47 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
---	"SPELL_CAST_START",
---	"SPELL_CAST_SUCCESS",
---	"SPELL_AURA_APPLIED",
---	"SPELL_AURA_REMOVED",
+	"SPELL_CAST_START 296546 296459 296894 302465 295916 296701",
+	"SPELL_CAST_SUCCESS 296430 296737",
+	"SPELL_AURA_APPLIED 296566 296737",
+	"SPELL_AURA_REMOVED 296737",
 --	"SPELL_PERIODIC_DAMAGE",
---	"SPELL_PERIODIC_MISSED"
+--	"SPELL_PERIODIC_MISSED",
+	"UNIT_DIED"
 )
 
---local warnPoweringDown					= mod:NewSpellAnnounce(271965, 2, nil, nil, nil, nil, nil, 2
+--TODO, verify arcanado burst ID/event
+--TODO, verify target scanning squall trap, or use an alternate method if possible
+--TODO, verify correct unshackled power spellid
+--TODO, right voie for gale buffet
+local warnArcanadoBurst					= mod:NewSpellAnnounce(296430, 2)
+local warnSquallTrap					= mod:NewTargetNoFilterAnnounce(296459, 4)
+local warnArcaneBomb					= mod:NewTargetNoFilterAnnounce(296737, 4)
 
---local specWarnPlasmaDischarge			= mod:NewSpecialWarningMoveAway(271225, nil, nil, nil, 3, 2)
+--Rising Fury
+local specWarnTideFistCast				= mod:NewSpecialWarningDefensive(296566, nil, nil, nil, 1, 2)
+local specWarnTideFist					= mod:NewSpecialWarningTaunt(296566, nil, nil, nil, 1, 2)
+local specWarnSquallTrap				= mod:NewSpecialWarningMoveAway(296459, nil, nil, nil, 1, 2)
+local yellSquallTrap					= mod:NewYell(296459)
+local specWarnCrushingNear				= mod:NewSpecialWarningClose(296459, nil, nil, nil, 1, 2)
+local specWarnArcaneBomb				= mod:NewSpecialWarningMoveAway(296737, nil, nil, nil, 1, 2)
+local yellArcaneBomb					= mod:NewYell(296737)
+local yellArcaneBombFades				= mod:NewShortFadesYell(296737)
+local specWarnUnshackledPower			= mod:NewSpecialWarningCount(296894, nil, nil, nil, 2, 2)
+--Raging Storm
+local specWarnAncientTempest			= mod:NewSpecialWarningSpell(295916, nil, nil, nil, 2, 2)
+local specWarnGaleBuffet				= mod:NewSpecialWarningDodge(296701, nil, nil, nil, 2, 2)
 --local specWarnGTFO						= mod:NewSpecialWarningGTFO(270290, nil, nil, nil, 1, 8)
 --local specWarnEntropicBlast				= mod:NewSpecialWarningInterrupt(270620, "HasInterrupt", nil, nil, 1, 2)
 
 --mod:AddTimerLine(BOSS)
 --local timerPlasmaDischargeCD			= mod:NewAITimer(30.4, 271225, nil, nil, nil, 3)--30.4-42
---local timerCudgelOfGoreCD				= mod:NewAITimer(58.2, 271296, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerTideFistCD					= mod:NewAITimer(58.2, 296546, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerArcanadoBurstCD				= mod:NewAITimer(58.2, 296430, nil, nil, nil, 3)
+local timerArcaneBombCD					= mod:NewAITimer(58.2, 296737, nil, nil, nil, 3)
+local timerUnshacklingPowerCD			= mod:NewAITimer(58.2, 296894, nil, nil, nil, 2)
+--Raging Storm
+local timerGaleBuffetCD					= mod:NewAITimer(58.2, 296701, nil, nil, nil, 3)
 
 --local berserkTimer					= mod:NewBerserkTimer(600)
 
@@ -40,9 +64,30 @@ mod:RegisterEventsInCombat(
 --mod:AddSetIconOption("SetIconOnEyeBeam", 264382, true)
 
 --mod.vb.phase = 1
+mod.vb.unshackledCount = 0
+
+function mod:CrushingTarget(targetname, uId)
+	if not targetname then return end
+	if self:AntiSpam(4, targetname) then
+		if targetname == UnitName("player") then
+			specWarnSquallTrap:Show()
+			specWarnSquallTrap:Play("runout")
+			yellSquallTrap:Yell()
+		elseif self:CheckNearby(12, targetname) then
+			specWarnCrushingNear:Show(targetname)
+			specWarnCrushingNear:Play("runaway")
+		else
+			warnSquallTrap:Show(targetname)
+		end
+	end
+end
 
 function mod:OnCombatStart(delay)
-
+	self.vb.unshackledCount = 0
+	timerTideFistCD:Start(1-delay)
+	timerArcanadoBurstCD:Start(1-delay)
+	timerArcaneBombCD:Start(1-delay)
+	timerUnshacklingPowerCD:Start(1-delay)
 end
 
 function mod:OnCombatEnd()
@@ -56,35 +101,76 @@ end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 271296 then
-
-	elseif spellId == 267180 then
-		if self:CheckInterruptFilter(args.sourceGUID, false, true) then
+	if spellId == 296546 then
+		if UnitDetailedThreatSituation("player", "boss1") then
+			specWarnTideFistCast:Show()
+			specWarnTideFistCast:Play("defensive")
+		end
+		timerTideFistCD:Start()
+	elseif spellId == 296459 then
+		self:ScheduleMethod(0.2, "BossTargetScanner", args.sourceGUID, "CrushingTarget", 0.1, 16, true, nil, nil, nil, false)--Does this target tank? if not, change false to true
+	elseif spellId == 296894 or spellId == 302465 then
+		self.vb.unshackledCount = self.vb.unshackledCount + 1
+		specWarnUnshackledPower:Show(self.vb.unshackledCount)
+		specWarnUnshackledPower:Play("aesoon")
+		timerUnshacklingPowerCD:Start()
+	elseif spellId == 295916 then
+		timerTideFistCD:Stop()
+		timerArcanadoBurstCD:Stop()
+		timerArcaneBombCD:Stop()
+		timerUnshacklingPowerCD:Stop()
+		specWarnAncientTempest:Show()
+		specWarnAncientTempest:Play("phasechange")
+		timerGaleBuffetCD:Start(2)
+		timerArcaneBombCD:Start(2)
+	elseif spellId == 296701 then
+		specWarnGaleBuffet:Show()
+		specWarnGaleBuffet:Play("watchstep")
+		timerGaleBuffetCD:Start()
+--	elseif spellId == 267180 then
+--		if self:CheckInterruptFilter(args.sourceGUID, false, true) then
 		--	specWarnVoidbolt:Show(args.sourceName)
 			--specWarnVoidbolt:Play("kickcast")
-		end
+--		end
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 271224 then
-
+	if spellId == 296430 and self:AntiSpam(3, 1) then--Remove antispam if only one event
+		warnArcanadoBurst:Show()
+		timerArcanadoBurstCD:Start()
+	elseif spellId == 296737 then
+		timerArcaneBombCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 271224 then
-
+	if spellId == 296566 then
+		if not args:IsPlayer() then
+			specWarnTideFist:Show(args.destName)
+			specWarnTideFist:Play("tauntboss")
+		end
+	elseif spellId == 296737 then
+		if args:IsPlayer() then
+			specWarnArcaneBomb:Show()
+			specWarnArcaneBomb:Play("runout")
+			yellArcaneBomb:Yell()
+			yellArcaneBombFades:Countdown(10)
+		else
+			warnArcaneBomb:Show(args.destName)
+		end
 	end
 end
 --mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 271965 then
-
+	if spellId == 296737 then
+		if args:IsPlayer() then
+			yellArcaneBombFades:Cancel()
+		end
 	end
 end
 
@@ -96,14 +182,23 @@ function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spell
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
+--]]
 
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 135824 then
+	if cid == 152512 then--Stormwraith
+		timerGaleBuffetCD:Stop()
+		timerArcaneBombCD:Stop()
+		timerTideFistCD:Start(2)
+		timerArcanadoBurstCD:Start(2)
+		timerArcaneBombCD:Start(3)
+		timerUnshacklingPowerCD:Start(2)
+	--elseif cid == 152816 then--Stormling
 
 	end
 end
 
+--[[
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
 	if spellId == 266913 then
 
--- DBM-EternalPalace/TheHatchery.lua
+++ DBM-EternalPalace/TheHatchery.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(2351, "DBM-EternalPalace", nil, 1179)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
---mod:SetCreatureID(153941)--Zoatroid 153941 Orgozoa main boss ???
+mod:SetCreatureID(152128)--Zoatroid 153941 Orgozoa 152128
 mod:SetEncounterID(2303)
 mod:SetZone()
 --mod:SetHotfixNoticeRev(16950)
@@ -12,23 +12,57 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
---	"SPELL_CAST_START",
---	"SPELL_CAST_SUCCESS",
---	"SPELL_AURA_APPLIED",
---	"SPELL_AURA_REMOVED",
+	"SPELL_CAST_START 298242 298413 298548 295818 295822 296691",
+	"SPELL_CAST_SUCCESS 299095 298156 298548",
+	"SPELL_AURA_APPLIED 299095 298156 298306 296914 295779",
+	"SPELL_AURA_APPLIED_DOSE 298156",
+	"SPELL_AURA_REMOVED 298306 296914",
 --	"SPELL_PERIODIC_DAMAGE",
---	"SPELL_PERIODIC_MISSED"
+--	"SPELL_PERIODIC_MISSED",
+	"SPELL_INTERRUPT",
+	"UNIT_DIED",
+	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---local warnPoweringDown					= mod:NewSpellAnnounce(271965, 2, nil, nil, nil, nil, nil, 2
+--TODO, correct ichor event
+--TODO, more with incubation fluid?
+--TODO, Pervasive Shock?
+--TODO, review phase change methods
+--TODO, skewer or not to skewer
+--TODO, verify aqua lance. if works, nameplate aura with line between casting mob and player
+--TODO, raging-rapids?
+--TODO, shocking lightning warnings/timer defaults?
+--TODO, do more with powerful stomp?
+local warnDesensitizingSting				= mod:NewStackAnnounce(298156, 2, nil, "Tank")
+local warnIncubationFluid					= mod:NewStackAnnounce(298242, 2, nil, "Tank")
+----Adds
+local warnAquaLance							= mod:NewTargetAnnounce(295779, 2)
+local warnShockingLightning					= mod:NewSpellAnnounce(295818, 2, nil, false)
+local warnPowerfulStomp						= mod:NewCountAnnounce(296691, 2)
 
---local specWarnPlasmaDischarge			= mod:NewSpecialWarningMoveAway(271225, nil, nil, nil, 3, 2)
+local specWarnDesensitizingSting			= mod:NewSpecialWarningStack(298156, nil, 9, nil, nil, 1, 6)
+local specWarnDesensitizingStingTaunt		= mod:NewSpecialWarningTaunt(298156, nil, nil, nil, 1, 2)
+local specWarnDribblingIchor				= mod:NewSpecialWarningSwitchCount(298103, nil, nil, nil, 1, 2)
+local specWarnIncubationFluid				= mod:NewSpecialWarningMoveAway(298306, nil, nil, nil, 1, 2)
+local specWarnArcingCurrent					= mod:NewSpecialWarningSpell(295825, nil, nil, nil, 2, 2)
+local yellArcingCurrent						= mod:NewYell(295825)
+----Adds
+local specWarnAquaLance						= mod:NewSpecialWarningMoveAway(295779, nil, nil, nil, 1, 2)
+local yellAquaLance							= mod:NewYell(295779)
 --local specWarnGTFO						= mod:NewSpecialWarningGTFO(270290, nil, nil, nil, 1, 8)
---local specWarnEntropicBlast				= mod:NewSpecialWarningInterrupt(270620, "HasInterrupt", nil, nil, 1, 2)
+local specWarnConductivePulse				= mod:NewSpecialWarningInterrupt(295822, "HasInterrupt", nil, nil, 3, 2)
 
 --mod:AddTimerLine(BOSS)
---local timerPlasmaDischargeCD			= mod:NewAITimer(30.4, 271225, nil, nil, nil, 3)--30.4-42
---local timerCudgelOfGoreCD				= mod:NewAITimer(58.2, 271296, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerDesensitizingStingCD				= mod:NewAITimer(58.2, 298156, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerDribblingIchorCD					= mod:NewAITimer(30.4, 298103, nil, nil, nil, 1)--30.4-42
+local timerIncubationFluidCD				= mod:NewAITimer(30.4, 298242, nil, nil, nil, 3)
+local timerArcingCurrentCD					= mod:NewAITimer(30.4, 295825, nil, nil, nil, 3)
+local timerMassiveIncubator					= mod:NewCastTimer(45, 298548, nil, nil, nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
+----Adds
+local timerAquaLanceCD						= mod:NewAITimer(30.4, 295779, nil, nil, nil, 3)
+local timerShockingLightningCD				= mod:NewAITimer(30.4, 295818, nil, false, nil, 3)
+local timerConductivePulseCD				= mod:NewAITimer(45, 295822, nil, nil, nil, 4, nil, DBM_CORE_INTERRUPT_ICON)
+local timerPowerfulStompCD					= mod:NewAITimer(45, 296691, nil, nil, nil, 2, nil, DBM_CORE_HEALER_ICON)
 
 --local berserkTimer					= mod:NewBerserkTimer(600)
 
@@ -38,11 +72,25 @@ mod:RegisterEventsInCombat(
 --mod:AddRangeFrameOption(6, 264382)
 --mod:AddInfoFrameOption(275270, true)
 --mod:AddSetIconOption("SetIconOnEyeBeam", 264382, true)
+mod:AddNamePlateOption("NPAuraOnChaoticGrowth", 296914)
 
---mod.vb.phase = 1
+mod.vb.phase = 1
+mod.vb.addCount = 0
+local playerHasIncubation = false
+local castsPerGUID = {}
 
 function mod:OnCombatStart(delay)
-
+	self.vb.phase = 1
+	self.vb.addCount = 0
+	playerHasIncubation = false
+	table.wipe(castsPerGUID)
+	timerDesensitizingStingCD:Start(1-delay)
+	timerDribblingIchorCD:Start(1-delay)
+	timerIncubationFluidCD:Start(1-delay)
+	timerArcingCurrentCD:Start(1-delay)
+	if self.Options.NPAuraOnChaoticGrowth then
+		DBM:FireEvent("BossMod_EnableHostileNameplates")
+	end
 end
 
 function mod:OnCombatEnd()
@@ -52,39 +100,127 @@ function mod:OnCombatEnd()
 --	if self.Options.RangeFrame then
 --		DBM.RangeCheck:Hide()
 --	end
+	if self.Options.NPAuraOnChaoticGrowth then
+		DBM.Nameplate:Hide(true, nil, nil, nil, true, true)
+	end
+end
+
+function mod:OnTimerRecovery()
+	if DBM:UnitDebuff("player", 298306) then
+		playerHasIncubation = true
+	end
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 271296 then
-
-	elseif spellId == 267180 then
-		if self:CheckInterruptFilter(args.sourceGUID, false, true) then
-		--	specWarnVoidbolt:Show(args.sourceName)
-			--specWarnVoidbolt:Play("kickcast")
+	if spellId == 298242 then
+		warnIncubationFluid:Show()
+		timerIncubationFluidCD:Start()
+	elseif spellId == 298413 then
+		specWarnArcingCurrent:Show()
+		timerArcingCurrentCD:Start()
+		if playerHasIncubation then
+			yellArcingCurrent:Yell()
+			specWarnArcingCurrent:Play("targetyou")
+		else
+			specWarnArcingCurrent:Play("farfromline")
 		end
+	elseif spellId == 298548 then
+		timerMassiveIncubator:Start(45)
+	elseif spellId == 295818 then
+		warnShockingLightning:Show()
+		timerShockingLightningCD:Start(nil, args.sourceGUID)
+	elseif spellId == 295822 then
+		timerConductivePulseCD:Start(nil, args.sourceGUID)
+		if self:CheckInterruptFilter(args.sourceGUID, false, true) then
+			specWarnConductivePulse:Show(args.sourceName)
+			specWarnConductivePulse:Play("kickcast")
+		end
+	elseif spellId == 296691 then
+		if not castsPerGUID[args.sourceGUID] then
+			castsPerGUID[args.sourceGUID] = 0
+		end
+		castsPerGUID[args.sourceGUID] = castsPerGUID[args.sourceGUID] + 1
+		warnPowerfulStomp:Show(castsPerGUID[args.sourceGUID])
+		timerPowerfulStompCD:Start(nil, args.sourceGUID)
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 271224 then
-
+	if spellId == 298103 and self:AntiSpam(5, 1) then--Dribbling Ichor
+		self.vb.addCount = self.vb.addCount + 1
+		specWarnDribblingIchor:Show(self.vb.addCount)
+		specWarnDribblingIchor:Play("mobsoon")
+		timerDribblingIchorCD:Start()
+	elseif spellId == 298156 then
+		timerDesensitizingStingCD:Start()
+	elseif spellId == 298548 then--Massive Incubator
+		timerDribblingIchorCD:Start(2)
+		timerDesensitizingStingCD:Start(2)
+		timerIncubationFluidCD:Start(2)
+		timerArcingCurrentCD:Start(2)
+	elseif spellId == 295779 then
+		timerAquaLanceCD:Start(nil, args.sourceGUID)
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 271224 then
-
+	if spellId == 299095 and self:AntiSpam(5, 1) then--Dribbling Ichor
+		self.vb.addCount = self.vb.addCount + 1
+		specWarnDribblingIchor:Show(self.vb.addCount)
+		specWarnDribblingIchor:Play("mobsoon")
+		timerDribblingIchorCD:Start()
+	elseif spellId == 298156 then
+		local amount = args.amount or 1
+		if amount >= 9 then
+			if args:IsPlayer() then
+				specWarnDesensitizingSting:Show(amount)
+				specWarnDesensitizingSting:Play("stackhigh")
+			else
+				if not UnitIsDeadOrGhost("player") and not DBM:UnitDebuff("player", spellId) then--Can't taunt less you've dropped yours off, period.
+					specWarnDesensitizingStingTaunt:Show(args.destName)
+					specWarnDesensitizingStingTaunt:Play("tauntboss")
+				else
+					warnDesensitizingSting:Show(args.destName, amount)
+				end
+			end
+		else
+			warnDesensitizingSting:Show(args.destName, amount)
+		end
+	elseif spellId == 298306 then
+		if args:IsPlayer() then
+			specWarnIncubationFluid:Show()
+			specWarnIncubationFluid:Play("targetyou")
+			playerHasIncubation = true
+		end
+	elseif spellId == 296914 then--Sanguine Presence
+		if self.Options.NPAuraOnChaoticGrowth then
+			DBM.Nameplate:Show(true, args.destGUID, spellId)
+		end
+	elseif spellId == 295779 then
+		if args:IsPlayer() then
+			specWarnAquaLance:Show()
+			specWarnAquaLance:Play("targetyou")
+			yellAquaLance:Yell()
+		else
+			warnAquaLance:Show(args.destName)
+		end
 	end
 end
---mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 271965 then
-
+	if spellId == 298306 then--Incubation Fluid
+		if args:IsPlayer() then
+			playerHasIncubation = false
+		end
+	elseif spellId == 296914 then--Chaotic Growth
+		if self.Options.NPAuraOnChaoticGrowth then
+			DBM.Nameplate:Hide(true, args.destGUID, spellId)
+		end
 	end
 end
 
@@ -96,17 +232,46 @@ function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spell
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
+--]]
+
+function mod:SPELL_INTERRUPT(args)
+	if type(args.extraSpellId) == "number" and args.extraSpellId == 298548 then
+		timerMassiveIncubator:Stop()
+		timerDribblingIchorCD:Start(2)
+		timerDesensitizingStingCD:Start(2)
+		timerIncubationFluidCD:Start(2)
+		timerArcingCurrentCD:Start(2)
+	end
+end
 
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 135824 then
+	if cid == 153941 then--Zoatroid
 
+	elseif cid == 152311 then--hatchery-warrior
+		timerAquaLanceCD:Stop(args.destGUID)
+	elseif cid == 152312 then--hatchery-witch
+		timerShockingLightningCD:Stop(args.destGUID)
+		timerConductivePulseCD:Stop(args.destGUID)
+	elseif cid == 152313 then--hatchery-brute
+		castsPerGUID[args.destGUID] = nil
+		timerPowerfulStompCD:Stop(args.destGUID)
 	end
 end
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
-	if spellId == 266913 then
-
+	if spellId == 298077 and self:AntiSpam(5, 1) then--Dribling Ichor
+		self.vb.addCount = self.vb.addCount + 1
+		specWarnDribblingIchor:Show(self.vb.addCount)
+		specWarnDribblingIchor:Play("mobsoon")
+		if self.vb.addCount == 3 then
+			self.vb.phase = 2
+			timerDribblingIchorCD:Stop()
+			timerDesensitizingStingCD:Stop()
+			timerIncubationFluidCD:Stop()
+			timerArcingCurrentCD:Stop()
+		else
+			timerDribblingIchorCD:Start()
+		end
 	end
 end
---]]
