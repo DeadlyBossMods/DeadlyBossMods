061d91d1e077df5b12af7fae20fc0ed604549624
Elnarfim <37562642+Elnarfim@users.noreply.github.com>
2019-07-15 01:13:19 +0900

    KR Update (#38)
    * KR Update
    
    * KR Update
    
    * KR Update
    
    * KR Update
    
    * KR Update
    
    * KR Update
    
    * KR Update
    
    * KR Update

--- DBM-EternalPalace/localization.kr.lua
+++ DBM-EternalPalace/localization.kr.lua
@@ -65,20 +65,25 @@ L:SetMiscLocalization({
 ---------------------------
 L= DBM:GetModLocalization(2361)
 
+L:SetOptionLocalization({
+	SortDesc 			= "정보 창에 $spell:298569 디버프 중첩을 높은 순으로 정렬합니다. (낮은순 대신)",
+	ShowTimeNotStacks	= "정보 창에 $spell:298569 디버프 중첩 대신 남은 시간을 표시합니다."
+})
+
 L:SetMiscLocalization({
-	SoakOrb =	"구슬 맞기",
-	AvoidOrb =	"구슬 피하기",
+	SoakOrb =	"구슬 먹기",
+	AvoidOrb =	"구슬 안먹기",
 	GroupUp =	"뭉치기",
 	Spread =	"산개",
 	Move	 =	"계속 이동",
 	DontMove =	"이동 중지",
 	--For Yells
-	HelpSoakMove	= "{rt3}같이 맞기 이동{rt3}",--Purple Diamond
-	HelpSoakStay	= "{rt6}같이 맞기 제자리{rt6}",--Blue Square
-	HelpSoak		= "{rt3}같이 맞기{rt3}",--Purple Diamond
+	HelpSoakMove	= "{rt3}같이 먹기 이동{rt3}",--Purple Diamond
+	HelpSoakStay	= "{rt6}같이 먹기 제자리{rt6}",--Blue Square
+	HelpSoak		= "{rt3}같이 먹기{rt3}",--Purple Diamond
 	HelpMove		= "{rt4}같이 이동{rt4}",--Green Triangle
-	HelpStay		= "{rt7}같이 제자리{rt7}",--Red X
-	SoloSoak 		= "혼자 맞기",
+	HelpStay		= "{rt7}같이 멈춰{rt7}",--Red X
+	SoloSoak 		= "혼자 먹기",
 	Solo =			"혼자",
 	--Not currently used Yells
 	SoloMoving		= "혼자 이동",
