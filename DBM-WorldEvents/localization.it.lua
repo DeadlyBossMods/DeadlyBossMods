0c55e4210fb27b46cc7df374a6b8e359776b80a5
Mattia <oriomatt@gmail.com>
2019-08-20 17:38:52 +0200

    Updated/Added itIT Translations (#54)

---
+++ DBM-WorldEvents/localization.it.lua
@@ -0,0 +1,156 @@
+if GetLocale() ~= "itIT" then return end
+local L
+
+------------
+--  Omen  --
+------------
+L = DBM:GetModLocalization("Omen")
+
+L:SetGeneralLocalization({
+	name = "Bifido"
+})
+
+------------------------------
+--  The Crown Chemical Co.  --
+------------------------------
+L = DBM:GetModLocalization("d288")
+
+L:SetTimerLocalization{
+	HummelActive		= "Hummel becomes active",
+	BaxterActive		= "Baxter becomes active",
+	FryeActive			= "Frye becomes active"
+}
+
+L:SetOptionLocalization({
+	TrioActiveTimer		= "Show timers for when Apothecary Trio becomes active"
+})
+
+L:SetMiscLocalization({
+	SayCombatStart		= "Did they bother to tell you who I am and why I am doing this?"
+})
+
+----------------------------
+--  The Frost Lord Ahune  --
+----------------------------
+L = DBM:GetModLocalization("d286")
+
+L:SetWarningLocalization({
+	Emerged			= "Emersione",
+	specWarnAttack	= "Ahune è vulnerabile - Attaccalo!"
+})
+
+L:SetTimerLocalization{
+	SubmergTimer	= "Immersione",
+	EmergeTimer		= "Emersione"
+}
+
+L:SetOptionLocalization({
+	Emerged			= "Mostra avviso all'emersione di Ahune",
+	specWarnAttack	= "Mostra avviso speciale quando Ahune diventa vulnerabile",
+	SubmergTimer	= "Mostra temporizzatore immersione",
+	EmergeTimer		= "Mostra temporizzatore emersione"
+})
+
+L:SetMiscLocalization({
+	Pull			= "The Ice Stone has melted!" -- TODO
+})
+
+----------------------
+--  Coren Direbrew  --
+----------------------
+L = DBM:GetModLocalization("d287")
+
+L:SetWarningLocalization({
+	specWarnBrew		= "Sbarazzati della botte prima che te ne lanci un altro!",
+	specWarnBrewStun	= "SUGGERIMENTO: Sei stato colpito, ricordati di bere la birra la prossima volta!"
+})
+
+L:SetOptionLocalization({
+	specWarnBrew		= "Mostra avviso speciale per $spell:47376",
+	specWarnBrewStun	= "Mostra avviso speciale per $spell:47340"
+})
+
+L:SetMiscLocalization({
+	YellBarrel			= "Botte su di me!"
+})
+
+----------------
+--  Brewfest  --
+----------------
+L = DBM:GetModLocalization("Brew")
+
+L:SetGeneralLocalization({
+	name = "Festa della Birra"
+})
+
+L:SetOptionLocalization({
+	NormalizeVolume			= "Normalizza automaticamente il volume del canale sonoro DIALOGHI per corrispondere a quello degli effetti quando nella zona Festa della Birra per ridurre il disturbo. (Se il volume della musica non è impostato, verrà silenziato.)"
+})
+
+-----------------------------
+--  The Headless Horseman  --
+-----------------------------
+L = DBM:GetModLocalization("d285")
+
+L:SetWarningLocalization({
+	WarnPhase				= "Fase %d",
+	warnHorsemanSoldiers	= "Evocazione Zucche Pulsanti",
+	warnHorsemanHead		= "Testa del Cavaliere Attiva"
+})
+
+L:SetOptionLocalization({
+	WarnPhase				= "Mostra avviso per ogni cambio fase",
+	warnHorsemanSoldiers	= "Mostra avviso evocazione Zucche Pulsanti",
+	warnHorsemanHead		= "Mostra avviso evocazione Testa del Cavaliere"
+})
+
+L:SetMiscLocalization({
+	HorsemanSummon			= "Horseman rise...", -- TODO
+	HorsemanSoldiers		= "Soldiers arise, stand and fight! Bring victory at last to this fallen knight!" -- TODO
+})
+
+------------------------------
+--  The Abominable Greench  --
+------------------------------
+L = DBM:GetModLocalization("Greench")
+
+L:SetGeneralLocalization({
+	name = "Abominevole Greench"
+})
+
+--------------------------
+--  Plants Vs. Zombies  --
+--------------------------
+L = DBM:GetModLocalization("PlantsVsZombies")
+
+L:SetGeneralLocalization({
+	name = "Piante Vs. Zombie"
+})
+
+L:SetWarningLocalization({
+	warnTotalAdds	= "Zombi evocati dall'ultima ondata massiva: %d",
+	specWarnWave	= "Ondata Massiva!"
+})
+
+L:SetTimerLocalization{
+	timerWave		= "Prossima Ondata Massiva"
+}
+
+L:SetOptionLocalization({
+	warnTotalAdds	= "Annuncia conteggio totale evocazioni tra ondate massive",
+	specWarnWave	= "Mostra avviso speciale a inizio Ondata Massiva",
+	timerWave		= "Mostra temposizzatore prossima Ondata Massiva"
+})
+
+L:SetMiscLocalization({
+	MassiveWave		= "A Massive Wave of Zombies is Approaching!" -- TODO
+})
+
+--------------------------
+--  Demonic Invasions  --
+--------------------------
+L = DBM:GetModLocalization("DemonInvasions")
+
+L:SetGeneralLocalization({
+	name = "Invasioni Demoniache"
+})
---
+++ DBM-ZuldazarRaid/localization.it.lua
@@ -0,0 +1,106 @@
+if GetLocale() ~= "itIT" then return end
+local L
+
+---------------------------
+--  Ra'wani Kanae/Frida Ironbellows (Both) --
+-- Same exact enoucnter, diff names
+---------------------------
+L= DBM:GetModLocalization(2344)--Ra'wani Kanae (Alliance)
+
+L= DBM:GetModLocalization(2333)--Frida Ironbellows (Horde)
+
+---------------------------
+--  King Grong/Grong the Revenant (Both) --
+---------------------------
+L= DBM:GetModLocalization(2325)--King Grong (Horde)
+
+L= DBM:GetModLocalization(2340)--Grong the Revenant (Alliance)
+
+---------------------------
+--  Grimfang and Firecaller/Flamefist and the Illuminated (Both) --
+-- Same exact enoucnter, diff names
+---------------------------
+L= DBM:GetModLocalization(2323)--Grimfang and Firecaller (Alliance)
+
+L= DBM:GetModLocalization(2341)--Flamefist and the Illuminated (Horde)
+
+---------------------------
+--  Opulence (Alliance) --
+---------------------------
+L= DBM:GetModLocalization(2342)
+
+L:SetWarningLocalization({
+
+})
+
+L:SetTimerLocalization({
+
+})
+
+L:SetOptionLocalization({
+
+})
+
+L:SetMiscLocalization({
+	Bulwark =	"Protettore",
+	Hand	=	"Mano"
+})
+
+---------------------------
+--  Loa Council (Alliance) --
+---------------------------
+L= DBM:GetModLocalization(2330)
+
+L:SetMiscLocalization({
+	BwonsamdiWrath =	"Well, if ya so eager for death, ya shoulda come see me sooner!", -- TODO
+	BwonsamdiWrath2 =	"Sooner or later... everybody serves me!", -- TODO
+	Bird			 =	"Uccello" -- TODO
+})
+
+---------------------------
+--  King Rastakhan (Alliance) --
+---------------------------
+L= DBM:GetModLocalization(2335)
+
+L:SetOptionLocalization({
+	AnnounceAlternatePhase	= "Mostra avvisi generali per le fasi in cui non ti trovi (i temporizzatori verranno comunque sempre mostrati)"
+})
+
+---------------------------
+-- High Tinker Mekkatorgue (Horde) --
+---------------------------
+L= DBM:GetModLocalization(2332)
+
+---------------------------
+--  Sea Priest Blockade (Horde) --
+---------------------------
+L= DBM:GetModLocalization(2337)
+
+---------------------------
+--  Jaina Proudmoore (Horde) --
+---------------------------
+L= DBM:GetModLocalization(2343)
+
+L:SetOptionLocalization({
+	ShowOnlySummary2	= "Nascondi giocatori nel distanziometro inverso e mostra solo info sommarie (totale giocatori vicini)",
+	InterruptBehavior	= "Imposta comportamento interruzione elementale (Sovrascriverà ogni impostazione altrui se capogruppo)",
+	Three				= "rotazione a 3 persone ",--Default
+	Four				= "rotazione a 4 persone ",
+	Five				= "rotazione a 5 persone",
+	SetWeather			= "Imposta densità effetti climatici al minimo all'ingaggio del boss e ripristina al termine",
+})
+
+L:SetMiscLocalization({
+	Port			=	"lato porto",
+	Starboard		=	"lato tribordo",
+	Freezing		=	"Congelamento tra %s"
+})
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("ZuldazarRaidTrash")
+
+L:SetGeneralLocalization({
+	name =	"Scartini Dazar'alor"
+})
