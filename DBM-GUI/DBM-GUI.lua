b31bdc6698765229b7db527ef40bf4aa39c00025
Kris <cblair91@gmail.com>
2019-09-11 01:26:16 +0100

    Some more GUI work.. Because whoever wrote it should feel bad.
    * Added the ability to display edit boxes defined by mods
    * Added the ability to display sliders (because AddSliderOption has existed for ages, but never actually injected anything... WHY?)
    * Little bit of cleanup, prevent duplicating work.

--- DBM-GUI/DBM-GUI.lua
+++ DBM-GUI/DBM-GUI.lua
@@ -4717,46 +4717,52 @@ do
 				for _, v in ipairs(category) do
 					if v == DBM_OPTION_SPACER then
 						addSpacer = true
-					elseif v.line then
+					else
 						lastButton = button
-						button = catpanel:CreateLine(v.text)
-					elseif type(mod.Options[v]) == "boolean" then
-						lastButton = button
-						if mod.Options[v .. "TColor"] then
-							button = catpanel:CreateCheckButton(mod.localization.options[v], true, nil, nil, nil, mod, v, nil, true)
-						elseif mod.Options[v .. "SWSound"] then
-							button = catpanel:CreateCheckButton(mod.localization.options[v], true, nil, nil, nil, mod, v)
-						else
-							button = catpanel:CreateCheckButton(mod.localization.options[v], true)
+						if v.line then
+							button = catpanel:CreateLine(v.text)
+						elseif type(mod.Options[v]) == "boolean" then
+							if mod.Options[v .. "TColor"] then
+								button = catpanel:CreateCheckButton(mod.localization.options[v], true, nil, nil, nil, mod, v, nil, true)
+							elseif mod.Options[v .. "SWSound"] then
+								button = catpanel:CreateCheckButton(mod.localization.options[v], true, nil, nil, nil, mod, v)
+							else
+								button = catpanel:CreateCheckButton(mod.localization.options[v], true)
+							end
+							button:SetScript("OnShow", function(self)
+								self:SetChecked(mod.Options[v])
+							end)
+							button:SetScript("OnClick", function(self)
+								mod.Options[v] = not mod.Options[v]
+								if mod.optionFuncs and mod.optionFuncs[v] then mod.optionFuncs[v]() end
+							end)
+						elseif mod.editboxes and mod.editboxes[v] then
+							local editBox = mod.editboxes[v]
+							button = catpanel:CreateEditBox(mod.localization.options[v], mod.Options[v], editBox.width, editBox.height)
+						elseif mod.sliders and mod.sliders[v] then
+							local slider = mod.sliders[v]
+							button = catpanel:CreateSlider(mod.localization.options[v], slider.minValue, slider.maxValue, slider.valueStep)
+							button:SetScript("OnShow", function(self)
+								self:SetValue(mod.Options[v])
+							end)
+						elseif mod.dropdowns and mod.dropdowns[v] then
+							local dropdownOptions = {}
+							for i, v in ipairs(mod.dropdowns[v]) do
+								dropdownOptions[#dropdownOptions + 1] = { text = mod.localization.options[v], value = v }
+							end
+							button = catpanel:CreateDropdown(mod.localization.options[v], dropdownOptions, mod, v, function(value) mod.Options[v] = value end, nil, 32)
+							button:SetScript("OnShow", function(self)
+								self:SetSelectedValue(mod.Options[v])
+							end)
+							if not addSpacer then
+								hasDropdowns = hasDropdowns + 7--Add 7 extra pixels per dropdown, because autodims is only reserving 25 per line, and dropdowns are 32
+								button:SetPoint("TOPLEFT", lastButton, "BOTTOMLEFT", 0, -10)
+							end
 						end
 						if addSpacer then
 							button:SetPoint("TOPLEFT", lastButton, "BOTTOMLEFT", 0, -6)
 							addSpacer = false
 						end
-						button:SetScript("OnShow",  function(self)
-							self:SetChecked(mod.Options[v])
-						end)
-						button:SetScript("OnClick", function(self)
-							mod.Options[v] = not mod.Options[v]
-							if mod.optionFuncs and mod.optionFuncs[v] then mod.optionFuncs[v]() end
-						end)
-					elseif mod.dropdowns and mod.dropdowns[v] then
-						lastButton = button
-						local dropdownOptions = {}
-						for i, v in ipairs(mod.dropdowns[v]) do
-							dropdownOptions[#dropdownOptions + 1] = { text = mod.localization.options[v], value = v }
-						end
-						button = catpanel:CreateDropdown(mod.localization.options[v], dropdownOptions, mod, v, function(value) mod.Options[v] = value end, nil, 32)
-						if addSpacer then
-							button:SetPoint("TOPLEFT", lastButton, "BOTTOMLEFT", 0, -6)
-							addSpacer = false
-						else
-							hasDropdowns = hasDropdowns + 7--Add 7 extra pixels per dropdown, because autodims is only reserving 25 per line, and dropdowns are 32
-							button:SetPoint("TOPLEFT", lastButton, "BOTTOMLEFT", 0, -10)
-						end
-						button:SetScript("OnShow", function(self)
-							self:SetSelectedValue(mod.Options[v])
-						end)
 					end
 				end
 				catpanel:AutoSetDimension(hasDropdowns)
