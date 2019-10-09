64c73f3cc490eb4b17672487bbb1f65de4f07113
Elnarfim <37562642+Elnarfim@users.noreply.github.com>
2019-10-09 22:30:33 +0900

    KR Update w/ Ny'alotha (#77)
    KR Update  w/ N'yalotha

---
+++ DBM-Nyalotha/localization.kr.lua
@@ -0,0 +1,87 @@
+if GetLocale() ~= "koKR" then return end
+local L
+
+---------------------------
+--  Wrathion, the Black Emperor --
+---------------------------
+L= DBM:GetModLocalization(2368)
+
+L:SetWarningLocalization({
+
+})
+
+L:SetTimerLocalization({
+
+})
+
+L:SetOptionLocalization({
+
+})
+
+L:SetMiscLocalization({
+})
+
+---------------------------
+--  Maut --
+---------------------------
+L= DBM:GetModLocalization(2365)
+
+---------------------------
+--  The Prophet Skitra --
+---------------------------
+L= DBM:GetModLocalization(2369)
+
+---------------------------
+--  Dark Inquisitor Xanesh --
+---------------------------
+L= DBM:GetModLocalization(2377)
+
+---------------------------
+--  The Hivemind --
+---------------------------
+L= DBM:GetModLocalization(2372)
+
+---------------------------
+--  Shad'har the Insatiable --
+---------------------------
+L= DBM:GetModLocalization(2367)
+
+---------------------------
+-- Drest'agath --
+---------------------------
+L= DBM:GetModLocalization(2373)
+
+---------------------------
+--  Vexiona --
+---------------------------
+L= DBM:GetModLocalization(2370)
+
+---------------------------
+--  Ra-den the Despoiled --
+---------------------------
+L= DBM:GetModLocalization(2364)
+
+---------------------------
+--  Il'gynoth, Corruption Reborn --
+---------------------------
+L= DBM:GetModLocalization(2374)
+
+---------------------------
+--  Carapace of N'Zoth --
+---------------------------
+L= DBM:GetModLocalization(2366)
+
+---------------------------
+--  N'Zoth, the Corruptor --
+---------------------------
+L= DBM:GetModLocalization(2375)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("NyalothaTrash")
+
+L:SetGeneralLocalization({
+	name =	"나이알로사 일반몹"
+})
+
