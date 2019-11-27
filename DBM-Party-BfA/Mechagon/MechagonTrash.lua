5588f4113f7aca4f8cb5bac2cc8c819652cebfc6
MysticalOS <mysticalosx@gmail.com>
2019-11-27 17:08:10 -0500

    Begin work on Mechagon trash mod. This mod was low priority among other things because the zone had no M+ mode. However, with 8.3 raid work wrapped up, and mechagon getting upgrade to M+ eligable zone in 8.3, work on this dungeon is now resuming. This first update contains notable trash warnings before first boss. I'll continue pushing additional notable trash warnings in coming days/weeks up to release of 8.3 to ensure mod is ready for M+

--- DBM-Party-BfA/Mechagon/MechagonTrash.lua
+++ DBM-Party-BfA/Mechagon/MechagonTrash.lua
@@ -8,44 +8,59 @@ mod:SetZone()
 mod.isTrashMod = true
 
 mod:RegisterEvents(
---	"SPELL_CAST_START",
---	"SPELL_AURA_APPLIED"
---	"SPELL_CAST_SUCCESS"
+	"SPELL_CAST_START 300687 300764 300777 300650",
+	"SPELL_AURA_APPLIED 300650",
+--	"SPELL_CAST_SUCCESS",
+	"UNIT_DIED"
 )
 
+--https://www.wowhead.com/guides/operation-mechagon-megadungeon-strategy-guide
 --local warnRiotShield				= mod:NewSpellAnnounce(258317, 4)
 
 --local yellArrowBarrage				= mod:NewYell(200343)
---local specWarnLockdown				= mod:NewSpecialWarningDodge(259711, nil, nil, nil, 2, 2)
+local specWarnSlimewave				= mod:NewSpecialWarningDodge(300777, nil, nil, nil, 2, 2)
 --local specWarnRiotShieldMove		= mod:NewSpecialWarningMove(258317, nil, nil, nil, 1, 2)
---local specWarnHeavilyArmed			= mod:NewSpecialWarningRun(259188, "Tank", nil, nil, 4, 2)
---local specWarnDebilitatingShout		= mod:NewSpecialWarningInterrupt(258128, "HasInterrupt", nil, nil, 1, 2)
---local specWarnWateryDome			= mod:NewSpecialWarningDispel(258153, "MagicDispeller", nil, nil, 1, 2)
---local specWarnTorchStrike			= mod:NewSpecialWarningDispel(265889, "Healer", nil, nil, 1, 2)
+local specWarnConsume				= mod:NewSpecialWarningRun(300687, nil, nil, nil, 4, 2)
+local specWarnSlimeBolt				= mod:NewSpecialWarningInterrupt(300764, "HasInterrupt", nil, nil, 1, 2)
+local specWarnSuffocatingSmog		= mod:NewSpecialWarningInterrupt(300650, "HasInterrupt", nil, nil, 1, 2)
+local specWarnSuffocatingSmogDispel	= mod:NewSpecialWarningDispel(300650, "RemoveDisease", nil, nil, 1, 2)
 --local specWarnRiotShield			= mod:NewSpecialWarningReflect(258317, "CasterDps", nil, nil, 1, 2)
 
---[[
+
+local timerConsume					= mod:NewCDTimer(20, 300687, nil, nil, nil, 2)
+
+--Antispam IDs for this mod: 1 run away, 2 dodges, 3 dispels
 function mod:SPELL_CAST_START(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
-	if spellId == 259711 and self:IsValidWarning(args.sourceGUID) and self:AntiSpam(3, 6) then
-
-	elseif spellId == 258153 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
-		specWarnHandcuff:Show(args.sourceName)
-		specWarnHandcuff:Play("kickcast")
+	if spellId == 300687 and self:IsValidWarning(args.sourceGUID) then
+		if self:AntiSpam(3, 1) then
+			specWarnConsume:Show()
+			specWarnConsume:Play("justrun")
+		end
+		timerConsume:Start(20, args.sourceGUID)
+	elseif spellId == 300777 and self:IsValidWarning(args.sourceGUID) and self:AntiSpam(3, 2) then
+		specWarnSlimewave:Show()
+		specWarnSlimewave:Play("chargemove")
+	elseif spellId == 300764 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+		specWarnSlimeBolt:Show(args.sourceName)
+		specWarnSlimeBolt:Play("kickcast")
+	elseif spellId == 300650 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+		specWarnSuffocatingSmog:Show(args.sourceName)
+		specWarnSuffocatingSmog:Play("kickcast")
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
-	if spellId == 258153 and not args:IsDestTypePlayer() and self:AntiSpam(5, 1) then
-
-	spellId == 265889 and args:IsDestTypePlayer() and self:CheckDispelFilter() and self:AntiSpam(5, 2) then
-
+	if spellId == 300650 and args:IsDestTypePlayer() and self:CheckDispelFilter() and self:AntiSpam(5, 3) then
+		specWarnSuffocatingSmogDispel:Show()
+		specWarnSuffocatingSmogDispel:Play("helpdispel")
 	end
 end
 
+--[[
 function mod:SPELL_CAST_SUCCESS(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
@@ -54,3 +69,10 @@ function mod:SPELL_CAST_SUCCESS(args)
 	end
 end
 --]]
+
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 154744 or cid == 154758 or cid == 150168 then--Toxic Monstrosity
+		timerConsume:Stop(args.destGUID)
+	end
+end
