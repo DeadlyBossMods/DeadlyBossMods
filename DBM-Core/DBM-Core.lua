5350f5a8735a9734287388ecb10ee206bf8c7550
MysticalOS <mysticalosx@gmail.com>
2020-01-10 09:18:37 -0500

    Post tier cleanup. Removed all unused commented code from Eternal Palace that will likely never be used now that we're moving into 8.3

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -69,7 +69,7 @@ end
 
 DBM = {
 	Revision = parseCurseDate("@project-date-integer@"),
-	DisplayVersion = "8.2.32 alpha", -- the string that is shown as version
+	DisplayVersion = "8.3.0 alpha", -- the string that is shown as version
 	ReleaseRevision = releaseDate(2019, 12, 19) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
--- DBM-EternalPalace/BlackwaterBehemoth.lua
+++ DBM-EternalPalace/BlackwaterBehemoth.lua
@@ -18,8 +18,6 @@ mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED_DOSE 289699",
 	"SPELL_AURA_REMOVED 292133 292138 298595 301494",
 	"SPELL_INTERRUPT",
---	"SPELL_PERIODIC_DAMAGE",
---	"SPELL_PERIODIC_MISSED",
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
@@ -249,25 +247,6 @@ function mod:SPELL_INTERRUPT(args)
 		end
 	end
 end
-
---[[
-function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
-	if spellId == 270290 and destGUID == UnitGUID("player") and self:AntiSpam(2, 2) then
-		specWarnGTFO:Show(spellName)
-		specWarnGTFO:Play("watchfeet")
-	end
-end
-mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
-
-function mod:UNIT_DIED(args)
-	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 150773 then--Shimmerskin Pufferfish
-
-	elseif cid == 153779 then--Darkwater Jellyfish
-
-	end
-end
---]]
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
 	if spellId == 292252 and self:AntiSpam(5, 3) then--Power Drain [DNT]
