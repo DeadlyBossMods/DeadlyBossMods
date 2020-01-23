59ecf978534eb0e7e8116255c312ac4022353778
MysticalOS <mysticalosx@gmail.com>
2020-01-22 22:53:10 -0500

    Bump version and prepare a tag, all the fixes thus far should be pushed out even if only 11/12 completed with live data. Don't worry though, There will be another update as soon as I can get hands on enough Nzoth data (heroic and normal)

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -69,8 +69,8 @@ end
 
 DBM = {
 	Revision = parseCurseDate("@project-date-integer@"),
-	DisplayVersion = "8.3.3 alpha", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2020, 1, 21) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DisplayVersion = "8.3.3", -- the string that is shown as version
+	ReleaseRevision = releaseDate(2020, 1, 22) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
