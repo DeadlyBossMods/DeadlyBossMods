ae09acabf8d7f13103f73ea17274374bb018a277
MysticalOS <mysticalosx@gmail.com>
2020-04-10 22:15:50 -0400

    Fix bug with assignment of shadowlands party mods that caused them to appear in wrong mod cat. Added drycodes to first two bosses of Plaguefall dungeon.

--- DBM-Party-Shadowlands/Plaguefall/DoctorIckus.lua
+++ DBM-Party-Shadowlands/Plaguefall/DoctorIckus.lua
@@ -9,61 +9,135 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
---	"SPELL_AURA_APPLIED",
---	"SPELL_CAST_START",
---	"SPELL_CAST_SUCCESS",
---	"SPELL_PERIODIC_DAMAGE",
---	"SPELL_PERIODIC_MISSED",
+	"SPELL_CAST_START 321166 321321 321406",
+	"SPELL_CAST_SUCCESS 321384",
+	"SPELL_AURA_APPLIED 321522 321268 321384 322410 322174",
+	"SPELL_AURA_REMOVED 321522 321268 321384 322174",
+	"SPELL_PERIODIC_DAMAGE 322356",
+	"SPELL_PERIODIC_MISSED 322356"
 --	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---local warnBlackPowder				= mod:NewTargetAnnounce(257314, 4)
+--TODO, maybe warn when oozes spawn after the slams
+local warnBurningContagion			= mod:NewTargetNoFilterAnnounce(321522, 2)
+local warnCongealedContagion		= mod:NewTargetNoFilterAnnounce(321268, 2)
+local warnFocusedGaze				= mod:NewTargetNoFilterAnnounce(321384, 4)
+local warnEngulf					= mod:NewTargetNoFilterAnnounce(322174, 4)
 
---local specWarnBlackPowder			= mod:NewSpecialWarningRun(257314, nil, nil, nil, 4, 2)
---local yellBlackPowder				= mod:NewYell(257314)
---local specWarnHealingBalm			= mod:NewSpecialWarningInterrupt(257397, "HasInterrupt", nil, nil, 1, 2)
---local specWarnGTFO					= mod:NewSpecialWarningGTFO(257274, nil, nil, nil, 1, 8)
+--General
+local specWarnSurgingSlam			= mod:NewSpecialWarningDefensive(321166, "Tank", nil, nil, 1, 2)
+local specWarnVirulentExplosion		= mod:NewSpecialWarningSpell(321406, nil, nil, nil, 2, 2)
+local specWarnGTFO					= mod:NewSpecialWarningGTFO(322356, nil, nil, nil, 1, 8)
+--Burning Contagion Abilities
+local specWarnPestilenceSlam		= mod:NewSpecialWarningDefensive(321321, "Tank", nil, nil, 1, 2)
+local specWarnWitheringFilth		= mod:NewSpecialWarningDispel(322410, "RemoveMagic", nil, nil, 1, 2)
+--Congealed Contagion Abilities
+local specWarnCongealedSlam			= mod:NewSpecialWarningDefensive(321285, "Tank", nil, nil, 1, 2)
+local specWarnFocusedGaze			= mod:NewSpecialWarningYou(321384, nil, nil, nil, 3, 2)
+local yellFocusedGaze				= mod:NewYell(321384)
+local yellFocusedGazeFades			= mod:NewShortFadesYell(321384)
+local specWarnEngulf				= mod:NewSpecialWarningDispel(322174, "RemoveDisease", nil, nil, 1, 2)
 
---local timerAvastyeCD				= mod:NewCDTimer(13, 257316, nil, nil, nil, 1, nil, DBM_CORE_DAMAGE_ICON)
---local timerSwiftwindSaberCD			= mod:NewCDTimer(15.8, 257316, nil, nil, nil, 3)
+local timerSurgingSlamCD			= mod:NewAITimer(13, 321166, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)
+--Burning Contagion Abilities
+local timerPestilenceSlamCD			= mod:NewAITimer(13, 321321, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerVirulentExplosionCD		= mod:NewAITimer(13, 321406, nil, nil, nil, 2, nil, DBM_CORE_DEADLY_ICON)
+--Congealed Contagion Abilities
+local timerCongealedSlamCD			= mod:NewAITimer(13, 321285, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)
+local timerFocusedGazeCD			= mod:NewAITimer(20, 321384, nil, nil, nil, 3)
+local timerEngulf					= mod:NewTargetTimer(20, 322174, nil, nil, nil, 3)
 
 function mod:OnCombatStart(delay)
-
+	timerSurgingSlamCD:Start(1-delay)
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 257402 then
-
---	elseif spellId == 257397 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
---		specWarnHealingBalm:Show(args.sourceName)
---		specWarnHealingBalm:Play("kickcast")
+	if spellId == 321166 then
+		specWarnSurgingSlam:Show()
+		specWarnSurgingSlam:Play("defensive")
+		timerSurgingSlamCD:Start()
+	elseif spellId == 321321 then
+		specWarnPestilenceSlam:Show()
+		specWarnPestilenceSlam:Play("defensive")
+		timerPestilenceSlamCD:Start()
+	elseif spellId == 321285 then
+		specWarnPestilenceSlam:Show()
+		specWarnPestilenceSlam:Play("defensive")
+		timerCongealedSlamCD:Start()
+	elseif spellId == 321406 then
+		specWarnVirulentExplosion:Show()
+		specWarnVirulentExplosion:Play("aesoon")
+		timerVirulentExplosionCD:Start()
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 257316 then
-
+	if spellId == 321384 then
+		timerFocusedGazeCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 194966 then
-
+	if spellId == 321522 then--Burning Contagion
+		warnBurningContagion:Show(args.destName)
+		timerSurgingSlamCD:Stop()
+		timerPestilenceSlamCD:Start(1)
+		timerVirulentExplosionCD:Start(1)
+	elseif spellId == 321268 then--Congealed Contagion
+		warnCongealedContagion:Show(args.destName)
+		timerSurgingSlamCD:Stop()
+		timerCongealedSlamCD:Start(1)
+		timerFocusedGazeCD:Start(1)
+	elseif spellId == 321384 then
+		if args:IsPlayer() then
+			specWarnFocusedGaze:Show()
+			specWarnFocusedGaze:Play("targetyou")
+			yellFocusedGaze:Yell()
+			yellFocusedGazeFades:Countdown(spellId)
+		else
+			warnFocusedGaze:Show(args.destName)
+		end
+	elseif spellId == 322410 and self:CheckDispelFilter() then
+		specWarnWitheringFilth:CombinedShow(0.3, args.destName)
+		specWarnWitheringFilth:ScheduleVoice(0.3, args.destName)
+	elseif spellId == 322174 then
+		timerEngulf:Start(20, args.destName)
+		if self.Options.SpecWarn322174dispel and self:CheckDispelFilter() then
+			specWarnEngulf:Show(args.destName)
+			specWarnEngulf:Play(args.destName)
+		else
+			warnEngulf:Show(args.destName)
+		end
 	end
 end
 
---[[
+function mod:SPELL_AURA_REMOVED(args)
+	local spellId = args.spellId
+	if spellId == 321522 then--Burning Contagion
+		timerPestilenceSlamCD:Stop()
+		timerVirulentExplosionCD:Stop()
+	elseif spellId == 321268 then--Congealed Contagion
+		timerCongealedSlamCD:Stop()
+		timerFocusedGazeCD:Stop()
+	elseif spellId == 321384 and args:IsPlayer() then
+		yellFocusedGazeFades:Cancel()
+	elseif spellId == 322174 then
+		timerEngulf:Stop(args.destName)
+	end
+end
+
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
-	if spellId == 309991 and destGUID == UnitGUID("player") and self:AntiSpam(2, 2) then
+	if spellId == 322356 and destGUID == UnitGUID("player") and self:AntiSpam(2, 2) then
 		specWarnGTFO:Show(spellName)
 		specWarnGTFO:Play("watchfeet")
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
+--[[
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
 	if spellId == 257453  then
 
--- DBM-Party-Shadowlands/Plaguefall/Globgrog.lua
+++ DBM-Party-Shadowlands/Plaguefall/Globgrog.lua
@@ -9,49 +9,67 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
---	"SPELL_AURA_APPLIED",
---	"SPELL_CAST_START",
+	"SPELL_AURA_APPLIED 324652",
+	"SPELL_CAST_START 324527 327608 327597 327584 327581 327232 327062 324667"
 --	"SPELL_CAST_SUCCESS",
 --	"SPELL_PERIODIC_DAMAGE",
 --	"SPELL_PERIODIC_MISSED",
 --	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---local warnBlackPowder				= mod:NewTargetAnnounce(257314, 4)
+--TODO, MANY Beckon SLime spellIds, which is right?
+local warnPlaguestomp				= mod:NewCastAnnounce(324527, 2)
+local warnSlimeWave					= mod:NewCastAnnounce(324667, 2)
 
---local specWarnBlackPowder			= mod:NewSpecialWarningRun(257314, nil, nil, nil, 4, 2)
+local specWarnDebilitatingPlague	= mod:NewSpecialWarningDispel(324652, "RemoveDisease", nil, nil, 1, 2)
+local specWarnSummonSlime			= mod:NewSpecialWarningSwitch(327608, "-Healer", nil, nil, 1, 2)
 --local yellBlackPowder				= mod:NewYell(257314)
 --local specWarnHealingBalm			= mod:NewSpecialWarningInterrupt(257397, "HasInterrupt", nil, nil, 1, 2)
 --local specWarnGTFO					= mod:NewSpecialWarningGTFO(257274, nil, nil, nil, 1, 8)
 
---local timerAvastyeCD				= mod:NewCDTimer(13, 257316, nil, nil, nil, 1, nil, DBM_CORE_DAMAGE_ICON)
---local timerSwiftwindSaberCD			= mod:NewCDTimer(15.8, 257316, nil, nil, nil, 3)
+local timerPlaguestompCD			= mod:NewAITimer(15.8, 324527, nil, nil, nil, 3)
+local timerSummonSlimeCD			= mod:NewCDTimer(13, 327608, nil, nil, nil, 1, nil, DBM_CORE_DAMAGE_ICON)
+local timerSlimeWaveCD				= mod:NewCDTimer(13, 324667, nil, "Tank", nil, 5, nil, DBM_CORE_TANK_ICON)
 
 function mod:OnCombatStart(delay)
-
+	timerPlaguestompCD:Start(1-delay)
+	timerSummonSlimeCD:Start(1-delay)
+	timerSlimeWaveCD:Start(1-delay)
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 257402 then
-
---	elseif spellId == 257397 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
---		specWarnHealingBalm:Show(args.sourceName)
---		specWarnHealingBalm:Play("kickcast")
+	if spellId == 324527 then
+		warnPlaguestomp:Show()
+		timerPlaguestompCD:Start()
+	elseif spellId == 327608 or spellId == 327597 or spellId == 327584 or spellId == 327581 or spellId == 327232 or spellId == 327062 then
+		specWarnSummonSlime:Show()
+		if self:IsTank() then
+			specWarnSummonSlime:Play("moveboss")
+		else
+			specWarnSummonSlime:Play("killmob")
+		end
+		timerSummonSlimeCD:Start()
+	elseif spellId == 324667 then
+		warnSlimeWave:Show()
+		timerSlimeWaveCD:Start()
 	end
 end
 
+--[[
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
 	if spellId == 257316 then
 
 	end
 end
+--]]
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 194966 then
-
+	if spellId == 324652 and self:CheckDispelFilter() then
+		specWarnDebilitatingPlague:CombinedShow(0.3, args.destName)
+		specWarnDebilitatingPlague:ScheduleVoice(0.3, args.destName)
 	end
 end
 
