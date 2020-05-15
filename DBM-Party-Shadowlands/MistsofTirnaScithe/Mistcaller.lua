a43307652e755774bb95a54bf09efff02ef49dba
MysticalOS <mysticalosx@gmail.com>
2020-05-14 21:21:26 -0400

    Added Tredova drycode, now that journal is more complete with latest build. blizz sure waited til last minute for that (literally only adding journal for it, in the patch that opened instance up) Added Patty Cakes to Mistcaller, since this mechanic was also added in most recent build. Implimented a scaling throttle to Guild comms to DBM-Core based on number of online guildies, to make DBM gradually increase anti spam throttle the more guildies are online. The intent is to not break guild version checks but ensure that in VERY large guilds they don't generate exponentially increasing addon comms

--- DBM-Party-Shadowlands/MistsofTirnaScithe/Mistcaller.lua
+++ DBM-Party-Shadowlands/MistsofTirnaScithe/Mistcaller.lua
@@ -9,8 +9,8 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 321471 321834 321873",
-	"SPELL_AURA_APPLIED 321891",
+	"SPELL_CAST_START 321471 321834 321873 321828",
+	"SPELL_AURA_APPLIED 321891 321828",
 	"SPELL_AURA_REMOVED 321891"
 --	"SPELL_CAST_SUCCESS",
 --	"SPELL_PERIODIC_DAMAGE",
@@ -27,11 +27,13 @@ local warnFixate					= mod:NewTargetNoFIlterAnnounce(321891, 3)
 local specWarnDodgeBall				= mod:NewSpecialWarningDodge(321834, nil, nil, nil, 2, 2)
 local yellDodgeBall					= mod:NewYell(321834)
 local specWarnFixate				= mod:NewSpecialWarningRun(321891, nil, nil, nil, 4, 2)
+local specWarnPattyCakes			= mod:NewSpecialWarningYou(321828, nil, nil, nil, 1, 2)
 --local specWarnGTFO					= mod:NewSpecialWarningGTFO(257274, nil, nil, nil, 1, 8)
 
 local timerGuessingGameCD			= mod:NewAITimer(13, 321471, nil, nil, nil, 6)
 local timerDodgeBallCD				= mod:NewAITimer(13, 321834, nil, nil, nil, 3)
 local timerFreezeTagCD				= mod:NewAITimer(13, 321873, nil, nil, nil, 3)
+local timerPattyCakesCD				= mod:NewAITimer(13, 321828, nil, nil, nil, 3)
 
 mod:AddNamePlateOption("NPAuraOnFixate", 321891)
 
@@ -47,6 +49,7 @@ function mod:OnCombatStart(delay)
 	timerGuessingGameCD:Start(1-delay)
 	timerDodgeBallCD:Start(1-delay)
 	timerFreezeTagCD:Start(1-delay)
+	timerPattyCakesCD:Start(1-delay)(
 	if self.Options.NPAuraOnFixate then
 		DBM:FireEvent("BossMod_EnableHostileNameplates")
 	end
@@ -71,6 +74,8 @@ function mod:SPELL_CAST_START(args)
 	elseif spellId == 321873 then
 		warnFreezeTag:Show()
 		timerFreezeTagCD:Start()
+	elseif spellId == 321828 then
+		timerPattyCakesCD:Start()
 	end
 end
 
@@ -94,6 +99,11 @@ function mod:SPELL_AURA_APPLIED(args)
 			end
 		else
 			warnFixate:Show(args.destName)
+		end
+	elseif spellId == 321828 then
+		if args:IsPlayer() then
+			specWarnPattyCakes:Show()
+			specWarnPattyCakes:Play("targetyou")
 		end
 	end
 end
