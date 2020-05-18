021144e45add7ff143aed86f7e842efbbf584f83
QartemisT <QartemisT@gmail.com>
2020-05-18 07:11:23 +0100

    Pretty much an entire rewrite of the GUI system

--- DBM-GUI/DBM-GUI_DropDown.lua
+++ DBM-GUI/DBM-GUI_DropDown.lua
@@ -1,52 +1,13 @@
--- *********************************************************
--- **               Deadly Boss Mods - GUI                **
--- **            http://www.deadlybossmods.com            **
--- *********************************************************
---
--- This addon is written and copyrighted by:
---    * Paul Emmerich (Tandanu @ EU-Aegwynn) (DBM-Core)
---    * Martin Verges (Nitram @ EU-Azshara) (DBM-GUI)
---
--- The localizations are written by:
---    * enGB/enUS: Tandanu				http://www.deadlybossmods.com
---    * deDE: Tandanu					http://www.deadlybossmods.com
---    * zhCN: Diablohu					http://wow.gamespot.com.cn
---    * ruRU: BootWin					bootwin@gmail.com
---    * zhTW: Hman						herman_c1@hotmail.com
---    * zhTW: Azael/kc10577				kc10577@hotmail.com
---    * koKR: BlueNyx/nBlueWiz			bluenyx@gmail.com / everfinale@gmail.com
---    * esES: Interplay/1nn7erpLaY      http://www.1nn7erpLaY.com
---
--- Special thanks to:
---    * Arta (DBM-Party)
---    * Omegal @ US-Whisperwind (some patches, and DBM-Party updates)
---    * Tennberg (a lot of fixes in the enGB/enUS localization)
---
---
--- The code of this addon is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 License. (see license.txt)
--- All included textures and sounds are copyrighted by their respective owners, license information for these media files can be found in the modules that make use of them.
---
---
---  You are free:
---    * to Share - to copy, distribute, display, and perform the work
---    * to Remix - to make derivative works
---  Under the following conditions:
---    * Attribution. You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work). (A link to http://www.deadlybossmods.com is sufficient)
---    * Noncommercial. You may not use this work for commercial purposes.
---    * Share Alike. If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one.
---
-
 do
 	local MAX_BUTTONS = 10
-	local BackDropTable = { bgFile = "" }
 	local L = DBM_GUI_Translations
 
 	local TabFrame1 = CreateFrame("Frame", "DBM_GUI_DropDown", UIParent, "DBM_GUI_DropDownMenu")
 	local ClickFrame = CreateFrame("Button", nil, UIParent)
 
 	TabFrame1.backdropInfo = {
-		bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",--131071
-		edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",--131072
+		bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background", -- 131071
+		edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border", -- 131072
 		tile = 1,
 		tileSize = 32,
 		edgeSize = 32,
@@ -68,21 +29,21 @@ do
 	TabFrame1:SetScript("OnMouseWheel", TabFrame1.OnMouseWheel)
 
 	TabFrame1:Hide()
-	TabFrame1:SetParent( DBM_GUI_OptionsFrame )
+	TabFrame1:SetParent(DBM_GUI_OptionsFrame)
 	TabFrame1:SetFrameStrata("TOOLTIP")
 
 	TabFrame1.offset = 0
 
 	TabFrame1.buttons = {}
 	TabFrame1.fontbuttons = {}
-	local buttonTable = {"buttons", "fontbuttons"}
-	for i=1, MAX_BUTTONS, 1 do
+	local buttonTable = { "buttons", "fontbuttons" }
+	for i=1, MAX_BUTTONS do
 		for _, buttonName in ipairs(buttonTable) do
-			TabFrame1[buttonName][i] = CreateFrame("Button", TabFrame1:GetName().."Button"..buttonName..i, TabFrame1, "DBM_GUI_DropDownMenuButtonTemplate")
+			TabFrame1[buttonName][i] = CreateFrame("Button", TabFrame1:GetName() .. "Button" .. buttonName .. i, TabFrame1, "DBM_GUI_DropDownMenuButtonTemplate")
 			if i == 1 then
 				TabFrame1[buttonName][i]:SetPoint("TOPLEFT", TabFrame1, "TOPLEFT", 11, -13)
 			else
-				TabFrame1[buttonName][i]:SetPoint("TOPLEFT", TabFrame1[buttonName][i-1], "BOTTOMLEFT", 0,0)
+				TabFrame1[buttonName][i]:SetPoint("TOPLEFT", TabFrame1[buttonName][i - 1], "BOTTOMLEFT")
 			end
 			TabFrame1[buttonName][i]:SetScript("OnClick", function(self)
 				self:GetParent():HideMenu()
@@ -97,49 +58,49 @@ do
 				if self:GetParent().dropdown.callfunc then
 					self:GetParent().dropdown.callfunc(self.entry.value)
 				end
-				_G[self:GetParent().dropdown:GetName().."Text"]:SetText(self.entry.text)--Menu refresh
+				_G[self:GetParent().dropdown:GetName() .. "Text"]:SetText(self.entry.text) -- Menu refresh
 			end)
 		end
 	end
-	local default_button_width = TabFrame1.buttons[1]:GetWidth() + 16--Adding pixels for scrollbar
-	TabFrame1:SetWidth(default_button_width+22)
-	TabFrame1:SetHeight(MAX_BUTTONS*TabFrame1.buttons[1]:GetHeight()+24)
+	local default_button_width = TabFrame1.buttons[1]:GetWidth() + 16 -- Adding pixels for scrollbar
+	TabFrame1:SetWidth(default_button_width + 22)
+	TabFrame1:SetHeight(MAX_BUTTONS * TabFrame1.buttons[1]:GetHeight() + 24)
 
-	TabFrame1.text = TabFrame1:CreateFontString(TabFrame1:GetName().."Text", 'BACKGROUND')
-	TabFrame1.text:SetPoint('CENTER', TabFrame1, 'BOTTOM', 0, 0)
+	TabFrame1.text = TabFrame1:CreateFontString(TabFrame1:GetName() .. "Text", 'BACKGROUND')
+	TabFrame1.text:SetPoint('CENTER', TabFrame1, 'BOTTOM')
 	TabFrame1.text:SetFontObject('GameFontNormalSmall')
 	TabFrame1.text:SetText("scroll with mouse")
 	TabFrame1.text:Hide()
 
 	function TabFrame1:ShowMenu(values)
 		self:Show()
-		if self.offset > #values-MAX_BUTTONS then self.offset = #values-MAX_BUTTONS end
+		if self.offset > #values - MAX_BUTTONS then self.offset = #values - MAX_BUTTONS end
 		if self.offset < 0 then self.offset = 0 end
 		if #values > MAX_BUTTONS then
-			self:SetHeight(MAX_BUTTONS*self.buttons[1]:GetHeight()+24)
+			self:SetHeight(MAX_BUTTONS * self.buttons[1]:GetHeight() + 24)
 			self.text:Show()
 		elseif #values == MAX_BUTTONS then
-			self:SetHeight(MAX_BUTTONS*self.buttons[1]:GetHeight()+24)
+			self:SetHeight(MAX_BUTTONS * self.buttons[1]:GetHeight() + 24)
 			self.text:Hide()
 		elseif #values < MAX_BUTTONS then
-			self:SetHeight( #values * self.buttons[1]:GetHeight() + 24)
+			self:SetHeight(#values * self.buttons[1]:GetHeight() + 24)
 			self.text:Hide()
 		end
-		for i=1, MAX_BUTTONS, 1 do
+		for i = 1, MAX_BUTTONS do
 			if i + self.offset <= #values then
 				local ind = "   "
-				if values[i+self.offset].value == self.dropdown.value then
-				  ind = "|TInterface\\Buttons\\UI-CheckBox-Check:0|t"
+				if values[i + self.offset].value == self.dropdown.value then
+					ind = "|TInterface\\Buttons\\UI-CheckBox-Check:0|t"
 				end
-				_G[self.buttons[i]:GetName().."NormalText"]:SetFontObject(GameFontHighlightSmall)
-				self.buttons[i]:SetText(ind..values[i+self.offset].text)
-				self.buttons[i].entry = values[i+self.offset]
-				if values[i+self.offset].texture then
+				_G[self.buttons[i]:GetName() .. "NormalText"]:SetFontObject(GameFontHighlightSmall)
+				self.buttons[i]:SetText(ind .. values[i + self.offset].text)
+				self.buttons[i].entry = values[i + self.offset]
+				if values[i + self.offset].texture then
 					self.buttons[i].backdropInfo = {
 						bgFile = values[i + self.offset].texture
 					}
 					if not DBM:IsAlpha() then
-						self.buttons[i]:SetBackdrop(BackDropTable)
+						self.buttons[i]:SetBackdrop(self.buttons[i].backdropInfo)
 					else
 						self.buttons[i]:ApplyBackdrop()
 					end
@@ -152,9 +113,9 @@ do
 		local width = self.buttons[1]:GetWidth()
 		local bwidth = 0
 		for k, button in pairs(self.buttons) do
-			bwidth = button:GetTextWidth() + 16--Adding pixels for scrollbar
+			bwidth = button:GetTextWidth() + 16 -- Adding pixels for scrollbar
 			if bwidth > width then
-				TabFrame1:SetWidth(bwidth+32)
+				TabFrame1:SetWidth(bwidth + 32)
 				width = bwidth
 			end
 		end
@@ -166,27 +127,31 @@ do
 
 	function TabFrame1:ShowFontMenu(values)
 		self:Show()
-		if self.offset > #values-MAX_BUTTONS then self.offset = #values-MAX_BUTTONS end
-		if self.offset < 0 then self.offset = 0 end
+		if self.offset > #values - MAX_BUTTONS then
+			self.offset = #values - MAX_BUTTONS
+		end
+		if self.offset < 0 then
+			self.offset = 0
+		end
 		if #values > MAX_BUTTONS then
-			self:SetHeight(MAX_BUTTONS*self.fontbuttons[1]:GetHeight()+24)
+			self:SetHeight(MAX_BUTTONS * self.fontbuttons[1]:GetHeight() + 24)
 			self.text:Show()
 		elseif #values == MAX_BUTTONS then
-			self:SetHeight(MAX_BUTTONS*self.fontbuttons[1]:GetHeight()+24)
+			self:SetHeight(MAX_BUTTONS * self.fontbuttons[1]:GetHeight() + 24)
 			self.text:Hide()
 		elseif #values < MAX_BUTTONS then
-			self:SetHeight( #values * self.fontbuttons[1]:GetHeight() + 24)
+			self:SetHeight(#values * self.fontbuttons[1]:GetHeight() + 24)
 			self.text:Hide()
 		end
-		for i=1, MAX_BUTTONS, 1 do
+		for i = 1, MAX_BUTTONS do
 			if i + self.offset <= #values then
 				local ind = "   "
-				if values[i+self.offset].value == self.dropdown.value then
-				  ind = "|TInterface\\Buttons\\UI-CheckBox-Check:0|t"
+				if values[i + self.offset].value == self.dropdown.value then
+					ind = "|TInterface\\Buttons\\UI-CheckBox-Check:0|t"
 				end
-				_G[self.fontbuttons[i]:GetName().."NormalText"]:SetFont(values[i+self.offset].font, values[i+self.offset].fontsize or 14)
-				self.fontbuttons[i]:SetText(ind..values[i+self.offset].text)
-				self.fontbuttons[i].entry = values[i+self.offset]
+				_G[self.fontbuttons[i]:GetName() .. "NormalText"]:SetFont(values[i + self.offset].font, values[i + self.offset].fontsize or 14)
+				self.fontbuttons[i]:SetText(ind .. values[i + self.offset].text)
+				self.fontbuttons[i].entry = values[i + self.offset]
 				self.fontbuttons[i]:Show()
 			else
 				self.fontbuttons[i]:Hide()
@@ -195,7 +160,7 @@ do
 		local width = self.fontbuttons[1]:GetWidth()
 		local bwidth = 0
 		for k, button in pairs(self.fontbuttons) do
-			bwidth = button:GetTextWidth() + 16--Adding pixels for scrollbar
+			bwidth = button:GetTextWidth() + 16 -- Adding pixels for scrollbar
 			if bwidth > width then
 				self:SetWidth(bwidth+32)
 				width = bwidth
@@ -208,7 +173,7 @@ do
 	end
 
 	function TabFrame1:HideMenu()
-		for i = 1, MAX_BUTTONS, 1 do
+		for i = 1, MAX_BUTTONS do
 			self.buttons[i]:Hide()
 			if not DBM:IsAlpha() then
 				self.buttons[i]:SetBackdrop(nil)
@@ -216,7 +181,7 @@ do
 				self.buttons[i]:ClearBackdrop()
 			end
 			self.buttons[i]:SetWidth(default_button_width)
-			_G[self.buttons[i]:GetName().."NormalText"]:SetFontObject(GameFontHighlightSmall)
+			_G[self.buttons[i]:GetName() .. "NormalText"]:SetFontObject(GameFontHighlightSmall)
 			self.fontbuttons[i]:Hide()
 			self.fontbuttons[i]:SetWidth(default_button_width)
 		end
@@ -240,12 +205,12 @@ do
 			self:ShowMenu(self.dropdown.values)
 		end
 		if #self.dropdown.values > MAX_BUTTONS then
-			_G[self:GetName().."List"]:Show()
-			_G[self:GetName().."ListScrollBar"]:SetMinMaxValues(0, valuesWOButtons)
-			_G[self:GetName().."ListScrollBar"]:SetValueStep(1)
+			_G[self:GetName() .. "List"]:Show()
+			_G[self:GetName() .. "ListScrollBar"]:SetMinMaxValues(0, valuesWOButtons)
+			_G[self:GetName() .. "ListScrollBar"]:SetValueStep(1)
 		else
-			_G[self:GetName().."ListScrollBar"]:SetValue(0)
-			_G[self:GetName().."List"]:Hide()
+			_G[self:GetName() .. "ListScrollBar"]:SetValue(0)
+			_G[self:GetName() .. "List"]:Hide()
 		end
 	end
 
@@ -265,7 +230,7 @@ do
 		if selected and self.values and type(self.values) == "table" then
 			for k,v in next, self.values do
 				if v.value ~= nil and v.value == selected or v.text == selected then
-					_G[self:GetName().."Text"]:SetText(v.text)
+					_G[self:GetName() .. "Text"]:SetText(v.text)
 					self.value = v.value
 					self.text = v.text
 				end
@@ -274,35 +239,34 @@ do
 	end
 
 	function DBM_GUI:CreateDropdown(title, values, vartype, var, callfunc, width, height, parent)
-		local FrameTitle = "DBM_GUI_DropDown"
 		-- Check Values
 		if type(values) == "table" then
-			for _,entry in next,values do
+			for _, entry in next, values do
 				entry.text = entry.text or "Missing entry.text"
 				entry.value = entry.value or entry.text
 			end
 		end
 
 		-- Create the Dropdown Frame
-		local dropdown = CreateFrame("Frame", FrameTitle..self:GetNewID(), parent or self.frame, "DBM_GUI_DropDownMenuTemplate")
+		local dropdown = CreateFrame("Frame", "DBM_GUI_DropDown" .. self:GetNewID(), parent or self.frame, "DBM_GUI_DropDownMenuTemplate")
 		dropdown.creator = self
 		dropdown.values = values
 		dropdown.callfunc = callfunc
 		if not width then
 			width = 120 -- minimum size
-			if title ~= L.Warn_FontType and title ~= L.Warn_FontStyle and title ~= L.Bar_Font then--Force font menus to always be fixed 120 width
+			if title ~= L.Warn_FontType and title ~= L.Warn_FontStyle and title ~= L.Bar_Font then -- Force font menus to always be fixed 120 width
 				for i, v in ipairs(values) do
-					_G[dropdown:GetName().."Text"]:SetText(v.text)
-					width = math.max(width, _G[dropdown:GetName().."Text"]:GetStringWidth())
+					_G[dropdown:GetName() .. "Text"]:SetText(v.text)
+					width = math.max(width, _G[dropdown:GetName() .. "Text"]:GetStringWidth())
 				end
 			end
 		end
-		dropdown:SetWidth(width + 30)	-- required to fix some setpoint problems
+		dropdown:SetWidth(width + 30) -- Required to fix some setpoint problems
 		dropdown:SetHeight(height or 32)
-		_G[dropdown:GetName().."Text"]:SetWidth(width + 30)
-		_G[dropdown:GetName().."Text"]:SetJustifyH("LEFT")
-		_G[dropdown:GetName().."Middle"]:SetWidth(width + 30)
-		_G[dropdown:GetName().."Button"]:SetScript("OnClick", function(self)
+		_G[dropdown:GetName() .. "Text"]:SetWidth(width + 30)
+		_G[dropdown:GetName() .. "Text"]:SetJustifyH("LEFT")
+		_G[dropdown:GetName() .. "Middle"]:SetWidth(width + 30)
+		_G[dropdown:GetName() .. "Button"]:SetScript("OnClick", function(self)
 			DBM:PlaySound(856)
 			if TabFrame1:IsShown() then
 				TabFrame1:HideMenu()
@@ -315,14 +279,16 @@ do
 			end
 		end)
 
-		if not (not title or title == "") then
-			dropdown.titletext = dropdown:CreateFontString(FrameTitle..self:GetCurrentID().."Text", 'BACKGROUND')
+		if title ~= nil and title ~= "" then
+			dropdown.titletext = dropdown:CreateFontString(dropdown:GetName() .. "Text", 'BACKGROUND')
 			dropdown.titletext:SetPoint('BOTTOMLEFT', dropdown, 'TOPLEFT', 21, 1)
 			dropdown.titletext:SetFontObject('GameFontNormalSmall')
 			dropdown.titletext:SetText(title)
 		end
 
-		local obj = setmetatable(dropdown, {__index = dropdownPrototype})
+		local obj = setmetatable(dropdown, {
+			__index = dropdownPrototype
+		})
 
 		if vartype and vartype == "DBM" and DBM.Options[var] ~= nil then
 			dropdown:SetScript("OnShow", function() dropdown:SetSelectedValue(DBM.Options[var]) end)
@@ -330,10 +296,10 @@ do
 			dropdown:SetScript("OnShow", function() dropdown:SetSelectedValue(DBM.Bars:GetOption(var)) end)
 		elseif vartype then
 			dropdown:SetScript("OnShow", function() dropdown:SetSelectedValue(vartype.Options[var]) end)
-		else--For external modules like DBM-RaidLeadTools
-			for k,v in next, dropdown.values do
+		else -- For external modules like DBM-RaidLeadTools
+			for k, v in next, dropdown.values do
 				if v.value ~= nil and v.value == var or v.text == var then
-					_G[dropdown:GetName().."Text"]:SetText(v.text)
+					_G[dropdown:GetName() .. "Text"]:SetText(v.text)
 					dropdown.value = v.value
 					dropdown.text = v.text
 				end
---
+++ DBM-GUI/modules/MainFrame.lua
@@ -0,0 +1,203 @@
+local frame = DBM_GUI_OptionsFrame
+table.insert(_G["UISpecialFrames"], frame:GetName())
+frame:SetSize(800, 600)
+frame:SetFrameStrata("DIALOG")
+frame:SetPoint("CENTER")
+frame:SetMovable(true)
+frame:EnableMouse(true)
+frame:SetUserPlaced(true)
+frame:RegisterForDrag("LeftButton")
+frame:SetFrameLevel(frame:GetFrameLevel() + 4)
+frame:Hide()
+frame.backdropInfo = {
+	bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background", -- 131071
+	edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border", -- 131072
+	tile = true,
+	tileSize = 32,
+	edgeSize = 32,
+	insets = { left = 11, right = 12, top = 12, bottom = 11 }
+}
+if DBM:IsAlpha() then
+	frame:ApplyBackdrop()
+else
+	frame:SetBackdrop(frame.backdropInfo)
+end
+frame.firstshow = true
+frame:SetScript("OnShow", function(self)
+	if self.firstshow then
+		self.firstshow = false
+		self:ShowTab(1)
+		self:UpdateMenuFrame()
+	end
+end)
+frame:SetScript("OnDragStart", frame.StartMoving)
+frame:SetScript("OnDragStop", frame.StopMovingOrSizing)
+frame.tabs = {}
+
+local frameHeader = frame:CreateTexture("$parentHeader", "ARTWORK")
+frameHeader:SetPoint("TOP", 0, 12)
+frameHeader:SetTexture(131080) -- "Interface\\DialogFrame\\UI-DialogBox-Header"
+frameHeader:SetSize(300, 68)
+
+local frameHeaderText = frame:CreateFontString("$parentHeaderText", "ARTWORK", "GameFontNormal")
+frameHeaderText:SetPoint("TOP", frameHeader:GetName(), 0, -14)
+frameHeaderText:SetText("Deadly Boss Mods")
+
+local frameRevision = frame:CreateFontString("$parentRevision", "ARTWORK", "GameFontDisableSmall")
+frameRevision:SetPoint("BOTTOMLEFT", frame:GetName(), "BOTTOMLEFT", 20, 18)
+if DBM.NewerVersion then
+	frameRevision:SetText(DBM_DEADLY_BOSS_MODS.. " " .. DBM.DisplayVersion.. " (" .. DBM:ShowRealDate(DBM.Revision) .. "). |cffff0000Version " .. DBM.NewerVersion.. " is available.|r")
+else
+	frameRevision:SetText(DBM_DEADLY_BOSS_MODS.. " " .. DBM.DisplayVersion.. " (" .. DBM:ShowRealDate(DBM.Revision) .. ")")
+end
+
+local frameTranslation = frame:CreateFontString("$parentTranslation", "ARTWORK", "GameFontDisableSmall")
+frameTranslation:SetPoint("LEFT", frameRevision:GetName(), "RIGHT", 20, 0)
+if DBM_GUI_Translations.TranslationBy then
+	frameTranslation:SetText(DBM_GUI_Translations.TranslationByPrefix .. DBM_GUI_Translations.TranslationBy)
+end
+
+local frameWebsite = frame:CreateFontString("$parentWebsite", "ARTWORK", "GameFontNormal")
+frameWebsite:SetPoint("BOTTOMLEFT", frameRevision:GetName(), "TOPLEFT", 0, 15)
+frameWebsite:SetText(DBM_GUI_Translations.Website)
+
+local frameWebsiteButtonA = CreateFrame("Frame", nil, frame)
+frameWebsiteButtonA:SetAllPoints(frameWebsite)
+frameWebsiteButtonA:SetScript("OnMouseUp", function()
+	DBM:ShowUpdateReminder(nil, nil, DBM_COPY_URL_DIALOG, "https://discord.gg/deadlybossmods")
+end)
+
+local frameOkay = CreateFrame("Button", "$parentOkay", frame, "UIPanelButtonTemplate")
+frameOkay:SetSize(96, 22)
+frameOkay:SetPoint("BOTTOMRIGHT", -16, 14)
+frameOkay:SetText(CLOSE)
+frameOkay:SetScript("OnClick", function()
+	frame:Hide()
+	if DBM.Options.tempMusicSetting then
+		SetCVar("Sound_EnableMusic", DBM.Options.tempMusicSetting)
+		DBM.Options.tempMusicSetting = nil
+	end
+	if DBM.Options.musicPlaying then
+		StopMusic()
+		DBM.Options.musicPlaying = nil
+	end
+end)
+
+local frameWebsiteButton = CreateFrame("Button", "$parentWebsiteButton", frame, "UIPanelButtonTemplate")
+frameWebsiteButton:SetSize(96, 22)
+frameWebsiteButton:SetPoint("BOTTOMRIGHT", frameOkay:GetName(), "BOTTOMLEFT", -20, 0)
+frameWebsiteButton:SetText(DBM_GUI_Translations.WebsiteButton)
+frameWebsiteButton:SetScript("OnClick", function()
+	DBM:ShowUpdateReminder(nil, nil, DBM_COPY_URL_DIALOG)
+end)
+
+local bossMods = CreateFrame("Frame", "$parentBossMods", frame)
+bossMods.name = DBM_GUI_Translations.OTabBosses
+frame:CreateTab(bossMods)
+
+local DBMOptions = CreateFrame("Frame", "$parentDBMOptions", frame)
+DBMOptions.name = DBM_GUI_Translations.OTabOptions
+frame:CreateTab(DBMOptions)
+
+local frameList = CreateFrame("Frame", "$parentList", frame, "OptionsFrameListTemplate")
+frameList:SetSize(205, 499)
+frameList:SetPoint("TOPLEFT", 22, -40)
+frameList:SetScript("OnShow", function()
+	frame:UpdateMenuFrame()
+end)
+frameList:SetBackdropBorderColor(0.6, 0.6, 0.6, 1)
+frameList.buttons = {}
+for i = 1, (frameList:GetHeight() - 8) / 18 do
+	local button = CreateFrame("Button", frameList:GetName() .. "Button" .. i, frameList)
+	button:SetHeight(18)
+	button.text = button:CreateFontString(button:GetName() .. "Text", "ARTWORK", "GameFontNormalSmall")
+	button:RegisterForClicks("LeftButtonUp")
+	button:SetScript("OnClick", function(self)
+		frame:ClearSelection()
+		frame:SelectButton(button)
+		DBM_GUI.currentViewing = self.element
+		frame:DisplayFrame(self.element)
+	end)
+	if i == 1 then
+		button:SetPoint("TOPLEFT", frameList:GetName(), 0, -8)
+	else
+		button:SetPoint("TOPLEFT", frameList.buttons[i - 1]:GetName(), "BOTTOMLEFT")
+	end
+	local buttonHighlight = button:CreateTexture("$parentHighlight")
+	buttonHighlight:SetTexture(136809) -- "Interface\\QuestFrame\\UI-QuestLogTitleHighlight"
+	buttonHighlight:SetBlendMode("ADD")
+	buttonHighlight:SetPoint("TOPLEFT", 0, 1)
+	buttonHighlight:SetPoint("BOTTOMRIGHT", 0, 1)
+	buttonHighlight:SetVertexColor(0.196, 0.388, 0.8)
+	button:SetHighlightTexture(buttonHighlight)
+	frameList.buttons[i] = button
+	local buttonToggle = CreateFrame("Button", "$parentToggle", button, "UIPanelButtonTemplate")
+	button.toggle = buttonToggle
+	buttonToggle:SetSize(14, 14)
+	buttonToggle:SetPoint("TOPLEFT", button:GetName(), "TOPLEFT", 5, -1)
+	buttonToggle:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+	buttonToggle:SetScript("OnClick", function()
+		button.element.showSub = not button.element.showSub
+		frame:UpdateMenuFrame()
+	end)
+end
+local frameListList = _G[frameList:GetName() .. "List"]
+frameListList:SetBackdropBorderColor(0.6, 0.6, 0.6, 0.6)
+frameListList.offset = 0
+_G[frameListList:GetName() .. "ScrollBarScrollUpButton"]:Disable()
+_G[frameListList:GetName() .. "ScrollBarScrollDownButton"]:Disable()
+local frameListScrollBar = _G[frameListList:GetName() .. "ScrollBar"]
+frameListScrollBar:SetMinMaxValues(0, 11)
+frameListScrollBar:SetValue(0)
+frameList.scrollBar = frameListScrollBar
+frameListList:SetScript("OnVerticalScroll", function()
+	frameListScrollBar:SetValue(offset)
+	self.offset = math.floor((offset / 18) + 0.5)
+	frame:UpdateMenuFrame(self:GetParent())
+end)
+frameList:SetScript("OnMouseWheel", function(self, delta)
+	if delta > 0 then
+		frameListScrollBar:SetValue(frameListScrollBar:GetValue() - (frameListScrollBar:GetHeight() / 2))
+	else
+		frameListScrollBar:SetValue(frameListScrollBar:GetValue() + (frameListScrollBar:GetHeight() / 2))
+	end
+	frame:UpdateMenuFrame()
+end)
+
+local frameContainer = CreateFrame("Frame", "$parentPanelContainer", frame, DBM:IsAlpha() and "BackdropTemplate")
+frameContainer:SetPoint("TOPLEFT", frameList:GetName(), "TOPRIGHT", 16, 0)
+frameContainer:SetPoint("BOTTOMLEFT", frameList:GetName(), "BOTTOMRIGHT", 16, 1)
+frameContainer:SetPoint("RIGHT", -22, 0)
+frameContainer:SetBackdropBorderColor(0.6, 0.6, 0.6, 1)
+frameContainer.backdropInfo = {
+	edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+	tile = true,
+	tileSize = 16,
+	edgeSize = 16,
+	insets = { left = 5, right = 5, top = 5, bottom = 5 }
+}
+if DBM:IsAlpha() then
+	frameContainer:ApplyBackdrop()
+else
+	frameContainer:SetBackdrop(frameContainer.backdropInfo)
+end
+
+local frameContainerHeaderText = frameContainer:CreateFontString("$parentHeaderText", "BACKGROUND", "GameFontHighlightSmall")
+frameContainerHeaderText:SetPoint("BOTTOMLEFT", frame:GetName(), "TOPLEFT", 10, 1);
+
+local frameContainerFOV = CreateFrame("ScrollFrame", "$parentFOV", frameContainer)
+frameContainerFOV:SetPoint("TOPLEFT", frameContainer:GetName(), "TOPLEFT", 5, -5)
+frameContainerFOV:SetPoint("BOTTOMRIGHT", frameContainer:GetName(), "BOTTOMRIGHT", -20, 7)
+frameContainerFOV:SetScript("OnMouseWheel", function(self, delta)
+	local scrollBar = _G[self:GetName() .. "ScrollBar"]
+	if delta > 0 then
+		scrollBar:SetValue(scrollBar:GetValue() - (scrollBar:GetHeight() / 2))
+	else
+		scrollBar:SetValue(scrollBar:GetValue() + (scrollBar:GetHeight() / 2))
+	end
+end)
+
+-- TODO: Deprecate XML usage inheritance
+local frameContainerFOVScrollBar = CreateFrame("Slider", "$parentScrollBar", frameContainerFOV, "DBM_GUI_PanelScrollBarTemplate")
+frameContainerFOVScrollBar:SetPoint("TOPRIGHT", 15, -15)
+frameContainerFOVScrollBar:SetPoint("BOTTOMRIGHT", 15, 13)
