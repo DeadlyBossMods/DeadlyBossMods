6763bcb64b8ecf224a02cf469dc802282c2c78d7
QartemisT <QartemisT@gmail.com>
2020-05-24 12:04:13 +0100

    Fix event firing with isTrashMod variable undefined.
    This was for some reason, causing events to not fire properly if the variable was undefined (which in a lot of cases, it was)

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -798,7 +798,7 @@ do
 			local zones = v.zones
 			local handler = v[event]
 			local modEvents = v.registeredUnitEvents
-			if handler and (not isUnitEvent or not modEvents or modEvents[event .. ...])  and (not zones or zones[LastInstanceMapID]) and not (v.isTrashMod and #inCombat > 0) then
+			if handler and (not isUnitEvent or not modEvents or modEvents[event .. ...]) and (not zones or zones[LastInstanceMapID]) and not (not v.isTrashMod or v.isTrashMod and #inCombat > 0) then
 				handler(v, ...)
 			end
 		end
