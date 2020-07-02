d0de2364308a4abdbc6afd654020523e2642e8f2
MysticalOS <mysticalosx@gmail.com>
2020-07-01 20:28:30 -0400

    Amarth update:    - BoneSpikes was deleted from game, replaced with Necrotic breath, so updated mod accordingly.    - A new interrupt warning was also added

--- DBM-Party-Shadowlands/TheNecroticWake/Amarth.lua
+++ DBM-Party-Shadowlands/TheNecroticWake/Amarth.lua
@@ -10,33 +10,33 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED 320012",
-	"SPELL_CAST_START 322493 321247 322519",
-	"SPELL_CAST_SUCCESS 321226"
+	"SPELL_CAST_START 322493 321247 320170",
+	"SPELL_CAST_SUCCESS 321226 333493"
 --	"SPELL_PERIODIC_DAMAGE",
 --	"SPELL_PERIODIC_MISSED",
 --	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---local warnBlackPowder				= mod:NewTargetAnnounce(257314, 4)
-
+--TODO, which interrupt is priority?
 local specWarnLandoftheDead			= mod:NewSpecialWarningSwitch(321226, "-Healer", nil, nil, 1, 2)
 local specWarnFinalHarvest			= mod:NewSpecialWarningDodge(321247, nil, nil, nil, 2, 2)
-local specWarnBoneSpikes			= mod:NewSpecialWarningDodge(322519, nil, nil, nil, 2, 2)
+--local specWarnNecroticBreath		= mod:NewSpecialWarningDodge(333493, nil, nil, nil, 2, 2)
 --local yellBlackPowder				= mod:NewYell(257314)
 local specWarnFrostboltVolley		= mod:NewSpecialWarningInterrupt(322493, "HasInterrupt", nil, nil, 1, 2)
+local specWarnNecroticBolt			= mod:NewSpecialWarningInterrupt(320170, "HasInterrupt", nil, nil, 1, 2)
 local specWarnUnholyFrenzy			= mod:NewSpecialWarningDispel(320012, "RemoveEnrage", nil, nil, 1, 2)
 local specWarnUnholyFrenzyTank		= mod:NewSpecialWarningDefensive(320012, "Tank", nil, nil, 1, 2)
 --local specWarnGTFO					= mod:NewSpecialWarningGTFO(257274, nil, nil, nil, 1, 8)
 
 local timerLandoftheDeadCD			= mod:NewAITimer(13, 321226, nil, nil, nil, 1, nil, DBM_CORE_L.DAMAGE_ICON)
 local timerFinalHarvestCD			= mod:NewAITimer(15.8, 321247, nil, nil, nil, 2)
-local timerBoneSpikesCD				= mod:NewAITimer(15.8, 322519, nil, nil, nil, 3)
+local timerNecroticBreathCD			= mod:NewAITimer(15.8, 333493, nil, nil, nil, 3)
 local timerUnholyFrenzyCD			= mod:NewAITimer(13, 320012, nil, nil, nil, 5, nil, DBM_CORE_L.ENRAGE_ICON..DBM_CORE_L.TANK_ICON)
 
 function mod:OnCombatStart(delay)
 	timerLandoftheDeadCD:Start(1-delay)--SUCCESS
 	timerFinalHarvestCD:Start(1-delay)
-	timerBoneSpikesCD:Start(1-delay)
+	timerNecroticBreathCD:Start(1-delay)
 	timerUnholyFrenzyCD:Start(1-delay)--SUCCESS
 end
 
@@ -45,14 +45,13 @@ function mod:SPELL_CAST_START(args)
 	if spellId == 322493 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
 		specWarnFrostboltVolley:Show(args.sourceName)
 		specWarnFrostboltVolley:Play("kickcast")
+	elseif spellId == 320170 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+		specWarnNecroticBolt:Show(args.sourceName)
+		specWarnNecroticBolt:Play("kickcast")
 	elseif spellId == 321247 then
 		specWarnFinalHarvest:Show()
 		specWarnFinalHarvest:Play("watchstep")
 		timerFinalHarvestCD:Start()
-	elseif spellId == 322519 then
-		specWarnBoneSpikes:Show()
-		specWarnBoneSpikes:Play("watchstep")
-		timerBoneSpikesCD:Start()
 	end
 end
 
@@ -64,6 +63,10 @@ function mod:SPELL_CAST_SUCCESS(args)
 		timerLandoftheDeadCD:Start()
 	elseif spellId == 320012 then
 		timerUnholyFrenzyCD:Start()
+	elseif spellId == 333493 then
+--		specWarnNecroticBreath:Show()
+--		specWarnNecroticBreath:Play("watchstep")
+		timerNecroticBreathCD:Start()
 	end
 end
 
