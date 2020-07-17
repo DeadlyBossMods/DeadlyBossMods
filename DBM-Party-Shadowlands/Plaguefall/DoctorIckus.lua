882aac437b7f0fe667e1add03c69cdc41feaae30
MysticalOS <mysticalosx@gmail.com>
2020-07-16 21:48:11 -0400

    Completely redid doctor Ickus mod since blizzard completely threw old fight out window and redid it

--- DBM-Party-Shadowlands/Plaguefall/DoctorIckus.lua
+++ DBM-Party-Shadowlands/Plaguefall/DoctorIckus.lua
@@ -9,129 +9,149 @@ mod:SetZone()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 321166 321321 321406",
-	"SPELL_CAST_SUCCESS 321384",
-	"SPELL_AURA_APPLIED 321522 321268 321384 322410 322174",
-	"SPELL_AURA_REMOVED 321522 321268 321384 322174",
-	"SPELL_PERIODIC_DAMAGE 322356",
-	"SPELL_PERIODIC_MISSED 322356"
+	"SPELL_CAST_START 329110 332617 321406",
+	"SPELL_CAST_SUCCESS 329217",
+	"SPELL_AURA_APPLIED 322410 319070 329110",
+	"SPELL_AURA_APPLIED_DOSE 329110"
+--	"SPELL_AURA_REMOVED 331967 328175 322410"
+--	"SPELL_PERIODIC_DAMAGE 322356",
+--	"SPELL_PERIODIC_MISSED 322356"
 --	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---TODO, maybe warn when oozes spawn after the slams
---TODO, probably half the boss, blizzard fucked journal up so badly for boss that it's unclear how accurate this mod even is anymore
---TODO, fight so broken not even WCL can parse it correctly, so it literally can't be fixed right now
+--TODO, maybe warn when oozes spawn after the dispel
+--TODO, optimize witchering filt range finder to only be visible of oozes that cast it are up
+--TODO, determine if more than tank need to step on the Slithering Oozes and if it deserves a warning or not
+--TODO, maybe nameplate icon for https://shadowlands.wowhead.com/spell=320103/metamorphosis Or bar if DBM ever catches up to BW in that regard
+--TODO, https://shadowlands.wowhead.com/spell=328662/slime-coated GTFO, get right event
+--TODO, https://shadowlands.wowhead.com/spell=321521/congealed-bile infoframe during virulent explosion?
 --[[
-(ability.id = 321166 or ability.id = 321321 or ability.id = 321285 or ability.id = 321406) and type = "begincast"
- or ability.id = 321384 and type = "cast"
+(ability.id = 329110 or ability.id = 332617 or ability.id = 321406) and type = "begincast"
+ or ability.id = 329217 and type = "cast"
 --]]
-local warnBurningContagion			= mod:NewTargetNoFilterAnnounce(321522, 2)
-local warnCongealedContagion		= mod:NewTargetNoFilterAnnounce(321268, 2)
-local warnFocusedGaze				= mod:NewTargetNoFilterAnnounce(321384, 4)
-local warnEngulf					= mod:NewTargetNoFilterAnnounce(322174, 4)
+local warnSlimeInjection			= mod:NewStackAnnounce(329110, 2, nil, "Tank|Healer")
+local warnSlimeLunge				= mod:NewCountAnnounce(329217, 3)
+--Oozes
+local warnCorrosiveGunk				= mod:NewTargetAnnounce(319070, 3)
+local warnWitheringFilth			= mod:NewTargetNoFilterAnnounce(322410, 3, nil, "RemoveMagic")--Not special warning, because it's not as urgent to remove as tank debuff (same dispel type)
 
 --General
-local specWarnSurgingSlam			= mod:NewSpecialWarningDefensive(321166, "Tank", nil, nil, 1, 2)
-local specWarnVirulentExplosion		= mod:NewSpecialWarningSpell(321406, nil, nil, nil, 2, 2)
-local specWarnGTFO					= mod:NewSpecialWarningGTFO(322356, nil, nil, nil, 1, 8)
---Burning Contagion Abilities
-local specWarnPestilenceSlam		= mod:NewSpecialWarningDefensive(321321, "Tank", nil, nil, 1, 2)
-local specWarnWitheringFilth		= mod:NewSpecialWarningDispel(322410, "RemoveMagic", nil, nil, 1, 2)
---Congealed Contagion Abilities
-local specWarnCongealedSlam			= mod:NewSpecialWarningDefensive(321285, "Tank", nil, nil, 1, 2)
-local specWarnFocusedGaze			= mod:NewSpecialWarningYou(321384, nil, nil, nil, 3, 2)
-local yellFocusedGaze				= mod:NewYell(321384)
-local yellFocusedGazeFades			= mod:NewShortFadesYell(321384)
-local specWarnEngulf				= mod:NewSpecialWarningDispel(322174, "RemoveDisease", nil, nil, 1, 2)
+local specWarnSlimeLunge			= mod:NewSpecialWarningSpell(321406, nil, nil, nil, 2, 2)
+local specWarnSlimeInjection		= mod:NewSpecialWarningDispel(329110, "RemoveMagic", nil, nil, 1, 2)
+--local specWarnSlitheringOoze		= mod:NewSpecialWarningMoveTo(334579, nil, nil, nil, 1, 2)
+local specWarnVirulentExplosion		= mod:NewSpecialWarningSpell(321406, nil, nil, nil, 2, 2)--Change to MoveTo warning for Congealed Bile?
+local specWarnPestilenceSurge		= mod:NewSpecialWarningSwitch(332617, "Dps", nil, nil, 1, 2)
+--local specWarnGTFO				= mod:NewSpecialWarningGTFO(322356, nil, nil, nil, 1, 8)
+--Ooze
+local specWarnCorrosiveGunk			= mod:NewSpecialWarningDispel(319070, "RemoveDisease", nil, nil, 1, 2)
 
-local timerSurgingSlamCD			= mod:NewAITimer(13, 321166, nil, nil, nil, 5, nil, DBM_CORE_L.TANK_ICON)
---Burning Contagion Abilities
-local timerPestilenceSlamCD			= mod:NewAITimer(13, 321321, nil, nil, nil, 5, nil, DBM_CORE_L.TANK_ICON)
-local timerVirulentExplosionCD		= mod:NewAITimer(13, 321406, nil, nil, nil, 2, nil, DBM_CORE_L.DEADLY_ICON)
---Congealed Contagion Abilities
-local timerCongealedSlamCD			= mod:NewAITimer(13, 321285, nil, nil, nil, 5, nil, DBM_CORE_L.TANK_ICON)
-local timerFocusedGazeCD			= mod:NewAITimer(20, 321384, nil, nil, nil, 3)
-local timerEngulf					= mod:NewTargetTimer(20, 322174, nil, nil, nil, 3)
+local timerSlimeLungeCD				= mod:NewCDTimer(40, 329217, nil, nil, nil, 3)
+local timerSlimeInjectionCD			= mod:NewCDTimer(44.5, 329110, nil, nil, nil, 5, nil, DBM_CORE_L.TANK_ICON)
+local timerPestilenceSurgeCD		= mod:NewCDTimer(44.9, 332617, nil, nil, nil, 1, nil, DBM_CORE_L.DAMAGE_ICON)
+local timerVirulentExplosion		= mod:NewCastTimer(30, 321406, nil, nil, nil, 2, nil, DBM_CORE_L.DEADLY_ICON)--no CD, health based trigger
+
+mod:AddRangeFrameOption(5, 321935)
+
+mod.vb.lungeCount = 0
 
 function mod:OnCombatStart(delay)
-	timerSurgingSlamCD:Start(1-delay)
+	self.vb.lungeCount = 0
+	--TODO, fine tune start timers, they are approximations using first MELEE swing of boss since WCL lacked proper start event for encounter
+	timerSlimeInjectionCD:Start(9-delay)
+	timerPestilenceSurgeCD:Start(34-delay)
+	timerSlimeLungeCD:Start(44-delay)
+	if self.Options.RangeFrame then
+		DBM.RangeCheck:Show(5)
+	end
+--	if self.Options.NPAuraOnRapidInfection or self.Options.NPAuraOnCongealing then
+--		DBM:FireEvent("BossMod_EnableHostileNameplates")
+--	end
+end
+
+function mod:OnCombatEnd()
+	if self.Options.RangeFrame then
+		DBM.RangeCheck:Hide()
+	end
+--	if self.Options.NPAuraOnRapidInfection or self.Options.NPAuraOnCongealing then
+--		DBM.Nameplate:Hide(true, nil, nil, nil, true, true)
+--	end
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 321166 then
-		specWarnSurgingSlam:Show()
-		specWarnSurgingSlam:Play("defensive")
-		timerSurgingSlamCD:Start()
-	elseif spellId == 321321 then
-		specWarnPestilenceSlam:Show()
-		specWarnPestilenceSlam:Play("defensive")
-		timerPestilenceSlamCD:Start()
-	elseif spellId == 321285 then
-		specWarnPestilenceSlam:Show()
-		specWarnPestilenceSlam:Play("defensive")
-		timerCongealedSlamCD:Start()
+	if spellId == 329110 then
+		timerSlimeInjectionCD:Start()
+	elseif spellId == 332617 then
+		specWarnPestilenceSurge:Show()
+		specWarnPestilenceSurge:Play("killmob")
+		timerPestilenceSurgeCD:Start()
 	elseif spellId == 321406 then
 		specWarnVirulentExplosion:Show()
 		specWarnVirulentExplosion:Play("aesoon")
-		timerVirulentExplosionCD:Start()
+		timerVirulentExplosion:Start()
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
-	if spellId == 321384 then
-		timerFocusedGazeCD:Start()
+	if spellId == 329217 then
+		self.vb.lungeCount = self.vb.lungeCount + 1
+		--Comes in sets of 3, special warn for sequence beginning but general announce lunge 2 and 3
+		if self.vb.lungeCount == 1 then
+			specWarnSlimeLunge:Show()
+			specWarnSlimeLunge:Play("watchstep")
+			timerSlimeLungeCD:Start(40)
+		else
+			warnSlimeLunge:Show(self.vb.lungeCount)
+			if self.vb.lungeCount == 3 then self.vb.lungeCount = 0 end
+		end
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 321522 then--Burning Contagion
-		warnBurningContagion:Show(args.destName)
-		timerSurgingSlamCD:Stop()
-		timerPestilenceSlamCD:Start(1)
-		timerVirulentExplosionCD:Start(1)
-	elseif spellId == 321268 then--Congealed Contagion
-		warnCongealedContagion:Show(args.destName)
-		timerSurgingSlamCD:Stop()
-		timerCongealedSlamCD:Start(1)
-		timerFocusedGazeCD:Start(1)
-	elseif spellId == 321384 then
-		if args:IsPlayer() then
-			specWarnFocusedGaze:Show()
-			specWarnFocusedGaze:Play("targetyou")
-			yellFocusedGaze:Yell()
-			yellFocusedGazeFades:Countdown(spellId)
+	if spellId == 322410 and self:CheckDispelFilter() then
+		warnWitheringFilth:CombinedShow(0.3, args.destName)
+	elseif spellId == 319070 then
+		if self.Options.SpecWarn319070dispel and self:CheckDispelFilter() then
+			specWarnCorrosiveGunk:Show(args.destName)
+			specWarnCorrosiveGunk:Play(args.destName)
 		else
-			warnFocusedGaze:Show(args.destName)
+			warnCorrosiveGunk:Show(args.destName)
 		end
-	elseif spellId == 322410 and self:CheckDispelFilter() then
-		specWarnWitheringFilth:CombinedShow(0.3, args.destName)
-		specWarnWitheringFilth:ScheduleVoice(0.3, args.destName)
-	elseif spellId == 322174 then
-		timerEngulf:Start(20, args.destName)
-		if self.Options.SpecWarn322174dispel and self:CheckDispelFilter() then
-			specWarnEngulf:Show(args.destName)
-			specWarnEngulf:Play(args.destName)
+	elseif spellId == 329110 then
+		if self.Options.SpecWarn329110dispel then
+			specWarnSlimeInjection:Show(args.destName)
+			specWarnSlimeInjection:Play("helpdispel")
 		else
-			warnEngulf:Show(args.destName)
+			warnSlimeInjection:Show(args.destName, args.amount or 1)
 		end
+--	elseif spellId == 331967 then--Rapid Infection Contagion
+--		if self.Options.NPAuraOnRapidInfection then
+--			DBM.Nameplate:Show(true, args.destGUID, spellId)
+--		end
+--	elseif spellId == 328175 then--Congealed Contagion
+--		if self.Options.NPAuraOnCongealing then
+--			DBM.Nameplate:Show(true, args.destGUID, spellId)
+--		end
 	end
 end
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
+--[[
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
-	if spellId == 321522 then--Burning Contagion
-		timerPestilenceSlamCD:Stop()
-		timerVirulentExplosionCD:Stop()
-	elseif spellId == 321268 then--Congealed Contagion
-		timerCongealedSlamCD:Stop()
-		timerFocusedGazeCD:Stop()
-	elseif spellId == 321384 and args:IsPlayer() then
-		yellFocusedGazeFades:Cancel()
-	elseif spellId == 322174 then
-		timerEngulf:Stop(args.destName)
+	if spellId == 331967 then--Rapid Infection Contagion
+--		if self.Options.NPAuraOnRapidInfection then
+--			DBM.Nameplate:Hide(true, args.destGUID, spellId)
+--		end
+	elseif spellId == 328175 then--Congealed Contagion
+--		if self.Options.NPAuraOnCongealing then
+--			DBM.Nameplate:Hide(true, args.destGUID, spellId)
+--		end
+--	elseif spellId == 329110 then
+--		specWarnSlitheringOoze:Show()
+--		specWarnSlitheringOoze:Play("")
 	end
 end
 
@@ -143,7 +163,6 @@ function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spell
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
---[[
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
 	if spellId == 257453  then
 
