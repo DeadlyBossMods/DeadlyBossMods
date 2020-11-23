068c5a9f3f71571be7c880d1ee7bdfc2f356b358
MysticalOS <mysticalosx@gmail.com>
2020-11-23 14:11:07 -0500

    FFFFFFFFFFF Full power.......Dungeon mods are at.......FULL POWER!

--- DBM-Party-Shadowlands/TheaterofPain/Gorechop.lua
+++ DBM-Party-Shadowlands/TheaterofPain/Gorechop.lua
@@ -27,12 +27,12 @@ local specWarnHatefulStrike			= mod:NewSpecialWarningDefensive(323515, "Tank", n
 local specWarnGTFO					= mod:NewSpecialWarningGTFO(323130, nil, nil, nil, 1, 8)
 
 local timerMeatHooksCD				= mod:NewNextTimer(20.6, 322795, nil, nil, nil, 1)
-local timerTenderizingSmashCD		= mod:NewCDTimer(19.5, 318406, nil, nil, nil, 3, nil, DBM_CORE_L.DEADLY_ICON)
-local timerHatefulStrikeCD			= mod:NewCDTimer(13.7, 323515, nil, "Tank|Healer", nil, 5, nil, DBM_CORE_L.TANK_ICON)
+local timerTenderizingSmashCD		= mod:NewCDTimer(19.4, 318406, nil, nil, nil, 3, nil, DBM_CORE_L.DEADLY_ICON)
+local timerHatefulStrikeCD			= mod:NewCDTimer(14.6, 323515, nil, "Tank|Healer", nil, 5, nil, DBM_CORE_L.TANK_ICON)
 
 function mod:OnCombatStart(delay)
 	timerMeatHooksCD:Start(5.8-delay)
---	timerHatefulStrikeCD:Start(1-delay)--Literally 1 second
+	timerHatefulStrikeCD:Start(9.7-delay)
 	timerTenderizingSmashCD:Start(14.5-delay)
 end
 
