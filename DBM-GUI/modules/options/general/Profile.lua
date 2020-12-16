a4454e44dd48b8011ed1566e33caee6aecef3e68
QartemisT <QartemisT@gmail.com>
2020-12-16 17:22:39 +0000

    Validate sound settings on import, and have actual core (DBM + DBT) imports

--- DBM-GUI/modules/options/general/Profile.lua
+++ DBM-GUI/modules/options/general/Profile.lua
@@ -1,5 +1,8 @@
 local L = DBM_GUI_L
 
+local DBM = DBM
+local type, ipairs, tinsert = type, ipairs, table.insert
+
 local Create, Refresh
 local profileDropdown = {}
 
@@ -62,15 +65,67 @@ dualProfile:SetScript("OnClick", function()
 end)
 dualProfile:SetChecked(DBM_UseDualProfile)
 
+local function actuallyImport(importTable)
+	DBM.Options = importTable.DBM
+	DBT_AllPersistentOptions[_G["DBM_UsedProfile"]] = importTable.DBT
+end
+
+StaticPopupDialogs["IMPORTPROFILE_ERROR"] = {
+	text = "There are one or more errors importing this profile. Please see the chat for more information. Would you like to continue and reset found errors to default?",
+	button1 = "Import and fix",
+	button2 = "No",
+	OnAccept = function(self)
+		for _, soundSetting in ipairs(self.errors) do
+			self.importTable.DBM[soundSetting] = DBM.DefaultOptions[soundSetting]
+		end
+		actuallyImport(self.importTable)
+	end,
+	timeout = 0,
+	whileDead = true,
+	hideOnEscape = true,
+	preferredIndex = 3,
+}
+
 local importExportProfilesArea = profilePanel:CreateArea(L.Area_ImportExportProfile)
 importExportProfilesArea:CreateText(L.ImportExportInfo, nil, true)
 local exportProfile = importExportProfilesArea:CreateButton(L.ButtonExportProfile, 120, 20, function()
-	DBM_GUI:CreateExportProfile(DBM.Options)
+	DBM_GUI:CreateExportProfile({
+		DBM = DBM.Options,
+		DBT = DBT_AllPersistentOptions[_G["DBM_UsedProfile"]]
+	})
 end)
 exportProfile:SetPoint("TOPLEFT", 12, -20)
 local importProfile = importExportProfilesArea:CreateButton(L.ButtonImportProfile, 120, 20, function()
 	DBM_GUI:CreateImportProfile(function(importTable)
-		print("We got an import table.")
+		local errors = {}
+		-- Check if voice pack missing
+		local activeVP = importTable.DBM.ChosenVoicePack
+		if activeVP ~= "None" then
+			if not DBM.VoiceVersions[activeVP] or (DBM.VoiceVersions[activeVP] and DBM.VoiceVersions[activeVP] == 0) then
+				DBM:AddMsg(L.VOICE_MISSING)
+				tinsert(errors, "ChosenVoicePack")
+			end
+		end
+		-- Check if sound packs are missing
+		for _, soundSetting in ipairs({
+			"RaidWarningSound", "SpecialWarningSound", "SpecialWarningSound3", "SpecialWarningSound4", "SpecialWarningSound5", "EventSoundVictory2",
+			"EventSoundWipe", "EventSoundEngage2", "EventSoundMusic", "EventSoundDungeonBGM", "RangeFrameSound1", "RangeFrameSound2"
+		}) do
+			local activeSound = importTable.DBM[soundSetting]
+			if type(activeSound) == "string" and activeSound ~= "None" and activeSound ~= "none" and not DBM:ValidateSound(activeSound, true) then
+				tinsert(errors, soundSetting)
+			end
+		end
+		-- Create popup confirming if they wish to continue (and therefor resetting to default)
+		if #errors > 0 then
+			local popup = StaticPopup_Show("IMPORTPROFILE_ERROR")
+			if popup then
+				popup.importTable = importTable
+				popup.errors = errors
+			end
+		else
+			actuallyImport(importTable)
+		end
 	end)
 end)
 importProfile.myheight = 0
