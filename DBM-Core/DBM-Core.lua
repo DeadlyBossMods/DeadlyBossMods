bc4ada72fe2a2825108af60859d614e415b74c1a
MysticalOS <mysticalosx@gmail.com>
2021-03-23 13:01:49 -0400

    Bump alpha revision

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -71,7 +71,7 @@ end
 
 DBM = {
 	Revision = parseCurseDate("@project-date-integer@"),
-	DisplayVersion = "9.0.24", -- the string that is shown as version
+	DisplayVersion = "9.0.25 alpha", -- the string that is shown as version
 	ReleaseRevision = releaseDate(2021, 3, 23) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
