19b8ffb25b06efdecc1a59d83d46a6a41749691f
QartemisT <QartemisT@gmail.com>
2021-03-24 16:43:43 +0000

    Update DBT.lua

--- DBM-StatusBarTimers/DBT.lua
+++ DBM-StatusBarTimers/DBT.lua
@@ -590,19 +590,6 @@ function DBT:SetAnnounceHook(f)
 	self.announceHook = f
 end
 
-local function dump(o)
-	if type(o) == 'table' then
-		local s = '{ '
-		for k,v in pairs(o) do
-			if type(k) ~= 'number' then k = '"'..k..'"' end
-			s = s .. '['..k..'] = ' .. dump(v) .. ','
-		end
-		return s .. '} '
-	else
-		return tostring(o)
-	end
-end
-
 function DBT:UpdateBars(sortBars)
 	if sortBars and self.Options.Sort ~= "None" then
 		tsort(largeBars, function(x, y)
