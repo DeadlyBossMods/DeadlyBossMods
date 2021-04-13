4e5f12a76f2ab5777e6512fac6813709e89d95d1
MysticalOS <mysticalosx@gmail.com>
2021-04-13 19:35:15 -0400

    Chains of domination templating

---
+++ DBM-SantumOfDomination/localization.en.lua
@@ -0,0 +1,72 @@
+local L
+
+---------------------------
+--  The Tarragrue --
+---------------------------
+--L= DBM:GetModLocalization(2435)
+
+--L:SetTimerLocalization{
+
+--}
+
+--L:SetOptionLocalization({
+
+--})
+
+--L:SetMiscLocalization({
+
+--})
+
+---------------------------
+--  The Eye of the Jailer --
+---------------------------
+--L= DBM:GetModLocalization(2442)
+
+---------------------------
+--  The Nine --
+---------------------------
+--L= DBM:GetModLocalization(2439)
+
+---------------------------
+--  Remnant of Ner'zhul --
+---------------------------
+--L= DBM:GetModLocalization(2444)
+
+---------------------------
+--  Soulrender Dormazain --
+---------------------------
+--L= DBM:GetModLocalization(2445)
+
+---------------------------
+--  Painsmith Raznal --
+---------------------------
+--L= DBM:GetModLocalization(2443)
+
+---------------------------
+--  Guardian of the First Ones --
+---------------------------
+--L= DBM:GetModLocalization(2446)
+
+---------------------------
+--  Fatescribe Roh-Kalo --
+---------------------------
+--L= DBM:GetModLocalization(2447)
+
+---------------------------
+--  Kel'Thuzad --
+---------------------------
+--L= DBM:GetModLocalization(2440)
+
+---------------------------
+--  Sylvanas Windrunner --
+---------------------------
+--L= DBM:GetModLocalization(2441)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("SanctumofDomTrash")
+
+L:SetGeneralLocalization({
+	name =	"Sanctum of Domination Trash"
+})
