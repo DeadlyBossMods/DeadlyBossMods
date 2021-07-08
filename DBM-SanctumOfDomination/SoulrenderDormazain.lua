f2468bb19c23695dc69f68c5be2eba98c9672eb4
MysticalOS <mysticalosx@gmail.com>
2021-07-07 21:48:04 -0400

    Small tweaks to guardian, ultimately though the timers on this fight are going to suck really bad. the only way to map them out correctly is to do the impossible, get a long pull where boss has infinite energy and doesn't have timers screwed with by phasing. Since that won't happen, there is insufficient data to determine how to properly handle phasing interaction on timers.. Can't solve for C if A or B are missing.

--- DBM-SanctumOfDomination/SoulrenderDormazain.lua
+++ DBM-SanctumOfDomination/SoulrenderDormazain.lua
@@ -115,7 +115,7 @@ function mod:OnCombatStart(delay)
 	if self.Options.NPAuraOnDefiance or self.Options.NPAuraOnTormented then
 		DBM:FireEvent("BossMod_EnableHostileNameplates")
 	end
-	DBM:AddMsg("There is no Hellscream/shackles timer because it wouldn't be accurate. If blizz scripts fight better I'll add a timer")
+	DBM:AddMsg("There is no Hellscream/shackles timer because it wouldn't be accurate. If blizz scripts fight better, I'll add a timer")
 end
 
 function mod:OnCombatEnd()
