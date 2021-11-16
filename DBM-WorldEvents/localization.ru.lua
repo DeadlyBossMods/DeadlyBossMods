4b85373cea6d9a1e5cbe5d78bffcb2315352c585
Artemis <QartemisT@gmail.com>
2021-11-16 17:26:38 +0000

    Update localization.ru.lua

--- DBM-WorldEvents/localization.ru.lua
+++ DBM-WorldEvents/localization.ru.lua
@@ -89,7 +89,7 @@ L:SetWarningLocalization({
 L:SetOptionLocalization({
 	WarnPhase				= "Предупреждение о смене фаз",
 	warnHorsemanSoldiers	= "Предупреждать о призыве Пульсирующих тыкв",
-	warnHorsemanHead		= "СпецпредупрежДение о появлении головы всадника"
+	warnHorsemanHead		= "Спецпредупреждение о появлении головы всадника"
 })
 
 L:SetMiscLocalization({
