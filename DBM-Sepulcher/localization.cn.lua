bdfa7ad07e8d20d06ab552fcef8725ad09c63db1
Mini Dragon <projecteurs@gmail.com>
2021-11-22 16:29:04 +0800

    Create localization.cn.lua

---
+++ DBM-Sepulcher/localization.cn.lua
@@ -0,0 +1,76 @@
+--Mini Dragon <流浪者酒馆-Brilla@金色平原> 20211122
+
+if GetLocale() ~= "zhCN" then return end
+local L
+
+---------------------------
+--  Solitary Guardian -- 孤生护卫
+---------------------------
+--L= DBM:GetModLocalization(2458)
+
+--L:SetOptionLocalization({
+
+--})
+
+--L:SetMiscLocalization({
+
+--})
+
+---------------------------
+--  Dausegne, the Fallen Oracle -- 道茜歌妮，堕落先知
+---------------------------
+--L= DBM:GetModLocalization(2459)
+
+---------------------------
+--  Artificer Xy'mox -- 圣物匠赛·墨克斯
+---------------------------
+--L= DBM:GetModLocalization(2470)
+
+---------------------------
+--  Prototype Pantheon -- 万神殿原型
+---------------------------
+--L= DBM:GetModLocalization(2460)
+
+---------------------------
+--  Lihuvim, Principal Architect -- 首席建筑师利胡威姆
+---------------------------
+--L= DBM:GetModLocalization(2461)
+
+---------------------------
+--  Skolex, the Insatiable Ravener -- 司垢莱克斯，无穷噬灭者
+---------------------------
+--L= DBM:GetModLocalization(2465)
+
+---------------------------
+--  Halondrus the Reclaimer -- 回收者黑伦度斯
+---------------------------
+--L= DBM:GetModLocalization(2463)
+
+---------------------------
+--  Anduin Wrynn -- 安度因·乌瑞恩
+---------------------------
+--L= DBM:GetModLocalization(2469)
+
+---------------------------
+--  Lords of Dread -- 恐惧魔王
+---------------------------
+--L= DBM:GetModLocalization(2457)
+
+---------------------------
+--  Rygelon -- 莱葛隆
+---------------------------
+--L= DBM:GetModLocalization(2467)
+
+---------------------------
+--  The Jailer -- 典狱长
+---------------------------
+--L= DBM:GetModLocalization(2464)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("SepulcherTrash")
+
+L:SetGeneralLocalization({
+	name =	"初诞者圣墓小怪"
+})
