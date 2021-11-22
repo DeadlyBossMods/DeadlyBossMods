e74d9cee5ede72d7c54e5f9e6fcbe6f1eb486e48
Mini Dragon <projecteurs@gmail.com>
2021-11-22 20:03:52 +0800

    Add zhCN (#693)
    * Create localization.cn.lua
    
    * Update localization.kr.lua
    
    Fix GetLocale() ~= "koKR"
    
    * Update DBM-Sepulcher.toc

--- DBM-Sepulcher/localization.cn.lua
+++ DBM-Sepulcher/localization.cn.lua
@@ -1,8 +1,10 @@
-if GetLocale() ~= "koKR" then return end
+--Mini Dragon <流浪者酒馆-Brilla@金色平原> 20211122
+
+if GetLocale() ~= "zhCN" then return end
 local L
 
 ---------------------------
---  Solitary Guardian --
+--  Solitary Guardian -- 孤生护卫
 ---------------------------
 --L= DBM:GetModLocalization(2458)
 
@@ -15,52 +17,52 @@ local L
 --})
 
 ---------------------------
---  Dausegne, the Fallen Oracle --
+--  Dausegne, the Fallen Oracle -- 道茜歌妮，堕落先知
 ---------------------------
 --L= DBM:GetModLocalization(2459)
 
 ---------------------------
---  Artificer Xy'mox --
+--  Artificer Xy'mox -- 圣物匠赛·墨克斯
 ---------------------------
 --L= DBM:GetModLocalization(2470)
 
 ---------------------------
---  Prototype Pantheon --
+--  Prototype Pantheon -- 万神殿原型
 ---------------------------
 --L= DBM:GetModLocalization(2460)
 
 ---------------------------
---  Lihuvim, Principal Architect --
+--  Lihuvim, Principal Architect -- 首席建筑师利胡威姆
 ---------------------------
 --L= DBM:GetModLocalization(2461)
 
 ---------------------------
---  Skolex, the Insatiable Ravener --
+--  Skolex, the Insatiable Ravener -- 司垢莱克斯，无穷噬灭者
 ---------------------------
 --L= DBM:GetModLocalization(2465)
 
 ---------------------------
---  Halondrus the Reclaimer --
+--  Halondrus the Reclaimer -- 回收者黑伦度斯
 ---------------------------
 --L= DBM:GetModLocalization(2463)
 
 ---------------------------
---  Anduin Wrynn --
+--  Anduin Wrynn -- 安度因·乌瑞恩
 ---------------------------
 --L= DBM:GetModLocalization(2469)
 
 ---------------------------
---  Lords of Dread --
+--  Lords of Dread -- 恐惧魔王
 ---------------------------
 --L= DBM:GetModLocalization(2457)
 
 ---------------------------
---  Rygelon --
+--  Rygelon -- 莱葛隆
 ---------------------------
 --L= DBM:GetModLocalization(2467)
 
 ---------------------------
---  The Jailer --
+--  The Jailer -- 典狱长
 ---------------------------
 --L= DBM:GetModLocalization(2464)
 
@@ -70,5 +72,5 @@ local L
 L = DBM:GetModLocalization("SepulcherTrash")
 
 L:SetGeneralLocalization({
-	name =	"매장터 일반몹"
+	name =	"初诞者圣墓小怪"
 })
