90ed8f92c00d050250193a66dee7dd0187e3a5fd
Elnarfim <37562642+Elnarfim@users.noreply.github.com>
2021-12-04 07:50:02 +0900

    Update koKR (#697)

---
+++ DBM-Shadowlands/localization.kr.lua
@@ -0,0 +1,36 @@
+if GetLocale() ~= "koKR" then return end
+local L
+
+-----------------------
+-- Valinor, the Light of Eons --
+-----------------------
+--L= DBM:GetModLocalization(2430)
+
+-----------------------
+-- Mortanis --
+-----------------------
+--L= DBM:GetModLocalization(2431)
+
+-----------------------
+-- Oranomonos the Everbranching --
+-----------------------
+--L= DBM:GetModLocalization(2432)
+
+-----------------------
+-- Nurgash Muckformed --
+-----------------------
+--L= DBM:GetModLocalization(2433)
+
+-----------------------
+-- Mor'geth, Tormentor of the Damned --
+-----------------------
+L= DBM:GetModLocalization(2456)
+
+L:SetMiscLocalization({
+	Pull	= "너희 목숨을 거두는 건 나다!"
+})
+
+-----------------------
+-- Antros --
+-----------------------
+--L= DBM:GetModLocalization(2468)
