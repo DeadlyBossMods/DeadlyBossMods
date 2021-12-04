93df1d7e5b9b3a0543e2c7ddebfb7ecd81ce901c
MysticalOS <mysticalosx@gmail.com>
2021-12-03 21:21:29 -0500

    Push finished Dausegne mod

--- DBM-Sepulcher/Dausegne.lua
+++ DBM-Sepulcher/Dausegne.lua
@@ -2,39 +2,36 @@ local mod	= DBM:NewMod(2459, "DBM-Sepulcher", nil, 1195)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
-mod:SetCreatureID(181224)--or 183042
+mod:SetCreatureID(181224)
 mod:SetEncounterID(2540)
 mod:SetUsedIcons(1, 2, 3)
---mod:SetHotfixNoticeRev(20210902000000)
---mod:SetMinSyncRevision(20210706000000)
+mod:SetHotfixNoticeRev(20211203000000)
+mod:SetMinSyncRevision(20211203000000)
 --mod.respawnTime = 29
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 359483 363607 361513 361630 365418",
-	"SPELL_CAST_SUCCESS 361018 361214 361751 365373",
+	"SPELL_CAST_START 359483 363607 361513 361630 365418 360960",
 	"SPELL_AURA_APPLIED 361966 361018 361651",
 	"SPELL_AURA_APPLIED_DOSE 361966",
-	"SPELL_AURA_REMOVED 361966 361018 361651"
+	"SPELL_AURA_REMOVED 361966 361018 361651",
 --	"SPELL_PERIODIC_DAMAGE",
 --	"SPELL_PERIODIC_MISSED",
---	"UNIT_DIED",
+	"CHAT_MSG_RAID_BOSS_EMOTE"
 --	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
 --TODO, exact stack count optimal of tanks swaps of 361966, for now most warnings are silent or way overtuned
---TODO, Staggering Barrage tracking https://ptr.wowhead.com/spell=364289/staggering-barrage
---TODO, use https://ptr.wowhead.com/spell=359481/domination-core for auto marking domination ores maybe, if more than 1 to mark
---TODO, valid Encroaching Dominion cast and maybe count timer if it's not spam cast. also GTFO
---TODO, verify target scanning of arc
---TODO, likely spellids for Disintegration Halo are completely wrong. It has too many scripts to accurately guess correct cast trigger
---TODO, verify more phasing stuff
+--TODO, use https://ptr.wowhead.com/spell=359481/domination-core for auto marking domination ores maybe, if more than 1 to mark on mythic
+--[[
+(ability.id = 359483 or ability.id = 361513 or ability.id = 361630 or ability.id = 365418 or ability.id = 360960) and type = "begincast"
+ or ability.id = 361651 and (type = "applybuff" or type = "removebuff")
+--]]
 --The Fallen Oracle
 local warnInfusedStrikes						= mod:NewStackAnnounce(361966, 2, nil, "Tank|Healer")
 local warnStaggeringBarrage						= mod:NewTargetNoFilterAnnounce(361018, 3)
-local warnEncroachingDominion					= mod:NewSpellAnnounce(361214, 2)
-local warnObliterationArc						= mod:NewTargetNoFilterAnnounce(361513, 3)
+local warnDominationCore						= mod:NewCountAnnounce(359483, 3)
 --Inevitable Dominion
 local warnSiphonReservoir						= mod:NewCountAnnounce(361643, 2)
 
@@ -47,59 +44,50 @@ local yellStaggeringBarrage						= mod:NewShortPosYell(361018)
 local yellStaggeringBarrageFades				= mod:NewIconFadesYell(361018)
 local specWarnStaggeringBarrageTarget			= mod:NewSpecialWarningTarget(361018, false, nil, nil, 1, 2, 3)--Optional Soak special warning that auto checks no soak debuff
 local specWarnDominationBolt					= mod:NewSpecialWarningInterruptCount(363607, "HasInterrupt", nil, nil, 1, 2)
-local specWarnObliterationArc					= mod:NewSpecialWarningDodge(361513, nil, nil, nil, 2, 2)
-local yellObliterationArc						= mod:NewYell(361513)
-local specWarnDisintegrationHalo				= mod:NewSpecialWarningDodge(365373, nil, nil, nil, 2, 2)
+local specWarnObliterationArc					= mod:NewSpecialWarningDodgeCount(361513, nil, nil, nil, 2, 2)
+local specWarnDisintegrationHalo				= mod:NewSpecialWarningCount(365373, nil, nil, nil, 2, 2)
 --local specWarnGTFO							= mod:NewSpecialWarningGTFO(340324, nil, nil, nil, 1, 8)
 --Inevitable Dominion
 local specWarnTotalDominion						= mod:NewSpecialWarningSpell(365418, nil, nil, nil, 3, 2)--Basically soft enrage/wipe mechanic
 
 --mod:AddTimerLine(BOSS)
 --The Fallen Oracle
-local timerStaggeringBarrageCD					= mod:NewAITimer(28.8, 361018, nil, nil, nil, 3)
-local timerDominationCoreCD						= mod:NewAITimer(28.8, 359483, nil, nil, nil, 1, nil, DBM_COMMON_L.DAMAGE_ICON)
---local timerEncroachingDominionCD				= mod:NewAITimer(28.8, 361214, nil, nil, nil, 3)
-local timerObliterationArcCD					= mod:NewAITimer(28.8, 361513, nil, nil, nil, 3)
-local timerDisintegrationHaloCD					= mod:NewAITimer(28.8, 365373, nil, nil, nil, 3)
+local timerStaggeringBarrageCD					= mod:NewCDCountTimer(35, 361018, nil, nil, nil, 3)
+local timerDominationCoreCD						= mod:NewCDCountTimer(33.5, 359483, nil, nil, nil, 1, nil, DBM_COMMON_L.DAMAGE_ICON)
+local timerObliterationArcCD					= mod:NewCDCountTimer(35, 361513, nil, nil, nil, 3)
+local timerDisintegrationHaloCD					= mod:NewCDCountTimer(70, 365373, nil, nil, nil, 3)
 --Inevitable Dominion
-local timerSiphonReservoirCD					= mod:NewAITimer(28.8, 361643, nil, nil, nil, 6)
+local timerSiphonReservoirCD					= mod:NewCDCountTimer(28.8, 361643, nil, nil, nil, 6)
 
 --local berserkTimer							= mod:NewBerserkTimer(600)
 
 --mod:AddRangeFrameOption("8")
 mod:AddInfoFrameOption(361651, true)
-mod:AddSetIconOption("SetIconOnStaggeringBarrage", 361018, true, false, {1, 2, 3})
+mod:AddSetIconOption("SetIconOnStaggeringBarrage", 361018, true, false, {1, 2, 3})--Only one was happening on heroic, is 3 mythic only?
 --mod:AddNamePlateOption("NPAuraOnBurdenofDestiny", 353432, true)
 
 mod.vb.DebuffIcon = 1
+mod.vb.barrageCount = 0
 mod.vb.ReservoirCount = 0
+mod.vb.arcCount = 0
+mod.vb.coreCount = 0
+mod.vb.haloCount = 0
 mod.vb.softEnrage = false
 local castsPerGUID = {}
 
-function mod:ArcTarget(targetname, uId)
-	if not targetname then return end
-	if targetname == UnitName("player") then
---		specWarnObliterationArc:Show()
---		specWarnObliterationArc:Play("targetyou")
-		yellObliterationArc:Yell()
-	else
-		warnObliterationArc:Show(targetname)
-	end
-end
-
 function mod:OnCombatStart(delay)
 	self.vb.DebuffIcon = 1
+	self.vb.barrageCount = 0
 	self.vb.ReservoirCount = 0
+	self.vb.arcCount = 0
+	self.vb.coreCount = 0
+	self.vb.haloCount = 0
 	self.vb.softEnrage = false
-	timerStaggeringBarrageCD:Start(1-delay)
-	timerDominationCoreCD:Start(1-delay)
-	timerObliterationArcCD:Start(1-delay)
-	timerDisintegrationHaloCD:Start(1-delay)
-	timerSiphonReservoirCD:Start(1-delay)
---	if self.Options.InfoFrame then
---		DBM.InfoFrame:SetHeader(DBM:GetSpellInfo(328897))
---		DBM.InfoFrame:Show(10, "table", ExsanguinatedStacks, 1)
---	end
+	timerDisintegrationHaloCD:Start(5.2-delay, 1)
+	timerDominationCoreCD:Start(6.5-delay, 1)
+	timerObliterationArcCD:Start(15-delay, 1)
+	timerStaggeringBarrageCD:Start(29-delay, 1)
+	timerSiphonReservoirCD:Start(72.8-delay, 1)
 --	if self.Options.NPAuraOnBurdenofDestiny then
 --		DBM:FireEvent("BossMod_EnableHostileNameplates")
 --	end
@@ -124,8 +112,9 @@ end
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 359483 then
-
-		timerDominationCoreCD:Start()
+		self.vb.coreCount = self.vb.coreCount + 1
+		warnDominationCore:Show(self.vb.coreCount)
+		timerDominationCoreCD:Start(nil, self.vb.coreCount+1)
 	elseif spellId == 363607 then
 		if not castsPerGUID[args.sourceGUID] then
 			castsPerGUID[args.sourceGUID] = 0
@@ -149,10 +138,10 @@ function mod:SPELL_CAST_START(args)
 			end
 		end
 	elseif spellId == 361513 then
-		specWarnObliterationArc:Show()
+		self.vb.arcCount = self.vb.arcCount + 1
+		specWarnObliterationArc:Show(self.vb.arcCount)
 		specWarnObliterationArc:Play("shockwave")
-		timerObliterationArcCD:Start()
-		self:BossTargetScanner(args.sourceGUID, "ArcTarget", 0.1, 12)
+		timerObliterationArcCD:Start(nil, self.vb.arcCount+1)
 	elseif spellId == 361630 then--Teleport
 		self.vb.ReservoirCount = self.vb.ReservoirCount + 1
 		warnSiphonReservoir:Show(self.vb.ReservoirCount)
@@ -164,23 +153,10 @@ function mod:SPELL_CAST_START(args)
 		self.vb.softEnrage = true
 		specWarnTotalDominion:Show()
 		specWarnTotalDominion:Play("stilldanger")
-	end
-end
-
-function mod:SPELL_CAST_SUCCESS(args)
-	local spellId = args.spellId
-	if spellId == 361018 then
+	elseif spellId == 360960 then
 		self.vb.DebuffIcon = 1
-		timerStaggeringBarrageCD:Start()
-	elseif spellId == 361214 then
-		warnEncroachingDominion:Show()
-		--timerEncroachingDominionCD:Start(nil, args.sourceGUID)
-	elseif (spellId == 361751 or spellId == 365373) and self:AntiSpam(5, 1) then
-		specWarnDisintegrationHalo:Show()
-		specWarnDisintegrationHalo:Play("watchwave")
-		if not self.vb.softEnrage then--Disable timer if boss is spamming halos
-			timerDisintegrationHaloCD:Start()
-		end
+		self.vb.barrageCount = self.vb.barrageCount + 1
+		timerStaggeringBarrageCD:Start(nil, self.vb.barrageCount+1)
 	end
 end
 
@@ -191,20 +167,22 @@ function mod:SPELL_AURA_APPLIED(args)
 		if args:IsPlayer() then
 			yellInfusedStrikes:Cancel()
 			yellInfusedStrikes:Countdown(spellId, 5)
-			if amount % 3 == 0 and amount >= 9 then
-				specWarnInfusedStrikes:Show(amount)
-				specWarnInfusedStrikes:Play("stackhigh")
+			if amount % 3 == 0 then
+				if amount >= 9 then
+					specWarnInfusedStrikes:Show(amount)
+					specWarnInfusedStrikes:Play("stackhigh")
+				else
+					warnInfusedStrikes:Show(args.destName, amount)
+				end
 			end
 		else
-			if amount % 3 == 0 and amount >= 9 then
-				if not UnitIsDeadOrGhost("player") and not DBM:UnitDebuff("player", 362008) then--Not dead and not 100% vulnerable
+			if amount % 3 == 0 then
+				if (amount >= 9) and not UnitIsDeadOrGhost("player") and not DBM:UnitDebuff("player", 362008) then
 					specWarnInfusedStrikesTaunt:Show(args.destName)
 					specWarnInfusedStrikesTaunt:Play("tauntboss")
 				else
 					warnInfusedStrikes:Show(args.destName, amount)
 				end
-			else
-				warnInfusedStrikes:Show(args.destName, amount)
 			end
 		end
 	elseif spellId == 361018 then
@@ -251,29 +229,30 @@ function mod:SPELL_AURA_REMOVED(args)
 		if args:IsPlayer() then
 			yellStaggeringBarrageFades:Cancel()
 		end
-	elseif spellId == 361651 then
+	elseif spellId == 361651 then--Siphoned Barrier
 		if self.Options.InfoFrame then
 			DBM.InfoFrame:Hide()
 		end
-		timerSiphonReservoirCD:Start(2)
-		--Do timers reset this way?
-		timerStaggeringBarrageCD:Start(2)
-		timerDominationCoreCD:Start(2)
-		timerObliterationArcCD:Start(2)
+		timerDominationCoreCD:Start(7.6, timerDominationCoreCD+1)
+		timerObliterationArcCD:Start(16.1, self.vb.arcCount+1)
+		timerStaggeringBarrageCD:Start(30, self.vb.barrageCount+1)
+		timerSiphonReservoirCD:Start(108, self.vb.ReservoirCount+1)--108-110, closer here than teleport to teleport.
 		if not self.vb.softEnrage then
-			timerDisintegrationHaloCD:Start(2)
+			timerDisintegrationHaloCD:Start(6.3, self.vb.haloCount+1)
 		end
 	end
 end
 
---[[
-function mod:UNIT_DIED(args)
-	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 181244 then--Domination Core
-		timerEncroachingDominionCD:Stop(args.destGUID)
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
+	if msg:find("spell:365373") then
+		self.vb.haloCount = self.vb.haloCount + 1
+		specWarnDisintegrationHalo:Show(self.vb.haloCount)
+		specWarnDisintegrationHalo:Play("watchwave")
+		if not self.vb.softEnrage then
+			timerDisintegrationHaloCD:Start(70, self.vb.haloCount+1)
+		end
 	end
 end
--]]
 
 --[[
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
