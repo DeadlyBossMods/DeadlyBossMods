7e81fae4ccf39a954216a23bd362c35498bb48e6
MysticalOS <mysticalosx@gmail.com>
2021-12-08 18:44:22 -0500

    Fixed bug with sepulcher trash mod being assigned to incorrect parent mod Improved jailer mod considerbly by making options panel for it a hell of a lot more readable and organized.

--- DBM-Sepulcher/SepulcherTrash.lua
+++ DBM-Sepulcher/SepulcherTrash.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod("SanctumofDomTrash", "DBM-SanctumOfDomination", 1)
+local mod	= DBM:NewMod("SepulcherTrash", "DBM-Sepulcher", 1)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
