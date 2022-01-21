0a6d439ba08d909282854843c38c64feed11d3f3
Elnarfim <37562642+Elnarfim@users.noreply.github.com>
2022-01-21 18:42:32 +0900

    Update koKR (#716)

--- DBM-Sepulcher/localization.kr.lua
+++ DBM-Sepulcher/localization.kr.lua
@@ -30,7 +30,7 @@ local L
 L= DBM:GetModLocalization(2460)
 
 L:SetOptionLocalization({
-	RitualistIconSetting	= "의식술사 징표 설정 방식 (공대장일 경우 공격대 전체 적용)",
+	RitualistIconSetting	= "의식술사 징표 방식을 설정합니다. 공대장이 DBM을 사용한다면 공대장의 설정을 사용합니다",
 	SetOne					= "씨앗/밤의 사냥꾼 각자 다르게 (충돌 없음) |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:0:16:16:32|t |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:16:32:16:32|t |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:32:48:16:32|t |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:48:64:16:32|t",--5-8 (Default)
 	SetTwo					= "씨앗/밤의 사냥꾼 동일하게 (씨앗과 의식술사가 같이 나올시 충돌) |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:0:16:0:16|t |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:16:32:0:16|t |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:32:48:0:16|t |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:48:64:0:16|t",-- 1-4
 	SetThree				= "씨앗/밤의 사냥꾼 동일하게 (충돌은 없으나 모든 징표를 보려면 확장 공격대 징표 텍스쳐를 설치해야 함) |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:0:16:32:48|t |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:16:32:32:48|t |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:32:48:32:48|t |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:48:64:32:48|t"--9-12
@@ -71,7 +71,7 @@ L:SetOptionLocalization({
 L= DBM:GetModLocalization(2469)
 
 L:SetOptionLocalization({
-	PairingBehavior		= "신성 모독의 모드 작동 방식을 설정합니다. DBM 사용시 공대장의 설정이 적용됩니다",
+	PairingBehavior		= "신성 모독의 모드 작동 방식을 설정합니다. 공대장이 DBM을 사용한다면 공대장의 설정을 사용합니다",
 	Auto				= "'당신이 걸림' 경고시 자동으로 짝이 지정됩니다. 같은 짝끼리 고유 상징이 말풍선으로 표시됩니다",
 	Generic				= "'당신이 걸림' 경고시 짝을 지정하지 않습니다. 2종류 디버프에 각각 별도의 상징이 말풍선으로 표시됩니다",--Default
 	None				= "'당신이 걸림' 경고시 짝을 지정하지 않습니다. 말풍선도 나오지 않습니다"
