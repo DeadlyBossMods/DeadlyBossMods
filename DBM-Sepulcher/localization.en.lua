9e7ceef5aa218dc0aedf2b147ea7f1160b12c2a9
MysticalOS <mysticalosx@gmail.com>
2022-03-29 18:28:04 -0400

    fix stupid

--- DBM-Sepulcher/localization.en.lua
+++ DBM-Sepulcher/localization.en.lua
@@ -109,7 +109,7 @@ L:SetTimerLocalization{
 L:SetOptionLocalization({
 	timerPits			= "Show timer for when the floor pits open up and expose holes you can fall into.",
 	warnHealAzeroth		= "Show warning for when you need to heal azeroth (via fight mechanics) on mythic difficulty, based on Echo's strategy",
-	warnDispel			= "Show warning for when you need to dispel Death Sentence on mythic difficulty, based on Echo's strategy"
+	warnDispel			= "Show warning for when you need to dispel Death Sentence on mythic difficulty, based on Echo's strategy",
 	timerHealAzeroth	= "Show timer for when you need to heal azeroth (via fight mechanics) on mythic difficulty, based on Echo's strategy",
 	timerDispels		= "Show timer for when you need to dispel Death Sentence on mythic difficulty, based on Echo's strategy"
 })
