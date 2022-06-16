ad3ae8129fb06177e4a11329612b3363c22cc525
MysticalOS <mysticalosx@gmail.com>
2022-06-16 15:08:32 -0400

    Fixed a bug where Tears of the forrest timer and alert never worked on Ingra Molach (the entirety of the expansion, too bad it wasn't reported sooner) Added Spear flurry warning to Tirna Scithe trash mod Added RP timer for Soazmi Added Spawn timeers for Both Achillite and Venza to The Grand Menagerie encounter

--- DBM-Party-Shadowlands/localization.en.lua
+++ DBM-Party-Shadowlands/localization.en.lua
@@ -305,7 +305,12 @@ L:SetGeneralLocalization({
 -----------------------
 -- The Menagerie --
 -----------------------
---L= DBM:GetModLocalization(2454)
+L= DBM:GetModLocalization(2454)
+
+L:SetMiscLocalization({
+	AchilliteRPTrigger	= "Are rampaging beasts ruining your day? We have the solution!",
+	VenzaRPTrigger		= "Now's my chance! That axe is mine!"
+})
 
 -----------------------
 -- Mailroom Mayhem --
@@ -320,7 +325,11 @@ L:SetGeneralLocalization({
 -----------------------
 -- So'azmi --
 -----------------------
---L= DBM:GetModLocalization(2451)
+L= DBM:GetModLocalization(2451)
+
+L:SetMiscLocalization({
+	RPTrigger	= "Excuse our intrusion, So'leah. I hope we caught you at an inconvenient time."
+})
 
 -----------------------
 -- Hylbrande --
