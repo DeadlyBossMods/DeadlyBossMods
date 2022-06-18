bb04b32273f6139f9009cb762328a7eb17668c45
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2022-06-19 00:58:58 +0500

    Update localization.ru.lua

--- DBM-Party-Shadowlands/localization.ru.lua
+++ DBM-Party-Shadowlands/localization.ru.lua
@@ -306,12 +306,12 @@ L:SetGeneralLocalization({
 -----------------------
 -- The Menagerie --
 -----------------------
--- L= DBM:GetModLocalization(2454)
+L= DBM:GetModLocalization(2454)
 
--- L:SetMiscLocalization({
--- 	AchilliteRPTrigger	= "Are rampaging beasts ruining your day? We have the solution!",
--- 	VenzaRPTrigger		= "Now's my chance! That axe is mine!"
--- })
+L:SetMiscLocalization({
+ 	AchilliteRPTrigger	= "Докучают разъяренные звери? У нас есть решение!",
+ 	VenzaRPTrigger		= "Вот мой шанс! Топор будет моим!"
+ })
 
 -----------------------
 -- Mailroom Mayhem --
@@ -326,11 +326,11 @@ L:SetGeneralLocalization({
 -----------------------
 -- So'azmi --
 -----------------------
--- L= DBM:GetModLocalization(2451)
+ L= DBM:GetModLocalization(2451)
 
--- L:SetMiscLocalization({
--- 	RPTrigger	= "Excuse our intrusion, So'leah. I hope we caught you at an inconvenient time."
--- })
+ L:SetMiscLocalization({
+ 	RPTrigger	= "Прости нас за вторжение, Со'лея. Кажется, сейчас не самое подходящее время."
+ })
 
 -----------------------
 -- Hylbrande --
