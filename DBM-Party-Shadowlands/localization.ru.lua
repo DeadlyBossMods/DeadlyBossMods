8637772897e7e37f4f129bb9195238a016f2e496
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2022-07-02 20:30:03 +0500

    Update localization.ru.lua (#770)
    * Update localization.ru.lua
    
    Add phrases. Translation later.
    
    * Update localization.ru.lua
    
    * Update localization.ru.lua
    
    * Update localization.ru.lua
    
    * Update localization.ru.lua

--- DBM-Party-Shadowlands/localization.ru.lua
+++ DBM-Party-Shadowlands/localization.ru.lua
@@ -306,7 +306,12 @@ L:SetGeneralLocalization({
 -----------------------
 -- The Menagerie --
 -----------------------
---L= DBM:GetModLocalization(2454)
+L= DBM:GetModLocalization(2454)
+
+L:SetMiscLocalization({
+	AchilliteRPTrigger	= "Докучают разъяренные звери? У нас есть решение!",
+	VenzaRPTrigger		= "Вот мой шанс! Топор будет моим!"
+})
 
 -----------------------
 -- Mailroom Mayhem --
@@ -321,7 +326,11 @@ L:SetGeneralLocalization({
 -----------------------
 -- So'azmi --
 -----------------------
---L= DBM:GetModLocalization(2451)
+L= DBM:GetModLocalization(2451)
+
+L:SetMiscLocalization({
+	RPTrigger	= "Прости нас за вторжение, Со'лея. Кажется, сейчас не самое подходящее время."
+})
 
 -----------------------
 -- Hylbrande --
