bd816358146d0ec6f86e4f02a5908657d9465203
MysticalOS <mysticalosx@gmail.com>
2022-07-15 23:56:03 -0400

    fucking bad template

--- DBM-Party-Dragonflight/BrackenhideHollow/Treemouth.lua
+++ DBM-Party-Dragonflight/BrackenhideHollow/Treemouth.lua
@@ -131,7 +131,7 @@ function mod:SPELL_AURA_REMOVED(args)
 end
 
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
-	if spellId == 378054 and destGUID == UnitGUID("player") and not playerDebuff and self:AntiSpam(2, 2) then
+	if spellId == 378054 and destGUID == UnitGUID("player") and self:AntiSpam(2, 2) then
 		specWarnGTFO:Show(spellName)
 		specWarnGTFO:Play("watchfeet")
 	end
