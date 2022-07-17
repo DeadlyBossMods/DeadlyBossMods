1378dd72853691927a28a9f70f6e8ea4e1a9d6e7
Elnarfim <37562642+Elnarfim@users.noreply.github.com>
2022-07-18 03:45:48 +0900

    Added KR locales for Dragonflight mods

---
+++ DBM-Party-Dragonflight/localization.kr.lua
@@ -0,0 +1,268 @@
+if GetLocale() ~= "koKR" then return end
+local L
+
+-----------------------
+-- <<<Global (M+ Affixes) >>> --
+-----------------------
+L = DBM:GetModLocalization("MPlusAffixesDF")
+
+L:SetGeneralLocalization({
+	name =	"신화+ 어픽스: 용군단"
+})
+
+-----------------------
+-- <<<Brackenhide Hollow >>> --
+-----------------------
+-----------------------
+-- Hackclaw's War-Band --
+-----------------------
+--L= DBM:GetModLocalization(2471)
+
+-----------------------
+-- Treemouth  --
+-----------------------
+--L= DBM:GetModLocalization(2473)
+
+-----------------------
+-- Gutshot --
+-----------------------
+--L= DBM:GetModLocalization(2472)
+
+-----------------------
+-- Decatriarch Wratheye --
+-----------------------
+--L= DBM:GetModLocalization(2474)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("BrackenhideHollowTrash")
+
+L:SetGeneralLocalization({
+	name =	"Brackenhide Hollow Trash"
+})
+
+-----------------------
+-- <<<Uldaman: Legacy of Tyr >>> --
+-----------------------
+-----------------------
+-- The Lost Dwarves --
+-----------------------
+--L= DBM:GetModLocalization(2475)
+
+-----------------------
+-- Bromach --
+-----------------------
+--L= DBM:GetModLocalization(2487)
+
+-----------------------
+-- Sentinel Talondras --
+-----------------------
+--L= DBM:GetModLocalization(2484)
+
+-----------------------
+-- Emberon --
+-----------------------
+--L= DBM:GetModLocalization(2476)
+
+-----------------------
+-- Chrono-Lord Deios --
+-----------------------
+--L= DBM:GetModLocalization(2479)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("UldamanLegacyofTyrTrash")
+
+L:SetGeneralLocalization({
+	name =	"울다만: 유산 일반몹"
+})
+
+-----------------------
+-- <<<The Nokhud Offensive >>> --
+-----------------------
+-----------------------
+-- Granyth --
+-----------------------
+--L= DBM:GetModLocalization(2498)
+
+-----------------------
+-- The Raging Tempest --
+-----------------------
+--L= DBM:GetModLocalization(2497)
+
+-----------------------
+-- Teera and Maruuk --
+-----------------------
+--L= DBM:GetModLocalization(2478)
+
+-----------------------
+-- Balakar Khan --
+-----------------------
+--L= DBM:GetModLocalization(2477)
+
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("TheNokhudOffensiveTrash")
+
+L:SetGeneralLocalization({
+	name =	"Nokhud Offensive Trash"
+})
+
+-----------------------
+-- <<<Neltharus >>> --
+-----------------------
+-----------------------
+-- Chargath, Bane of Scales --
+-----------------------
+--L= DBM:GetModLocalization(2490)
+
+-----------------------
+-- The Scorching Forge --
+-----------------------
+--L= DBM:GetModLocalization(2489)
+
+-----------------------
+-- Magmatusk --
+-----------------------
+--L= DBM:GetModLocalization(2494)
+
+-----------------------
+-- Warlord Sargha --
+-----------------------
+--L= DBM:GetModLocalization(2501)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("NeltharusTrash")
+
+L:SetGeneralLocalization({
+	name =	"넬타루스 일반몹"
+})
+
+-----------------------
+-- <<<Taz'algeth Academy >>> --
+-----------------------
+-----------------------
+-- Crawth --
+-----------------------
+--L= DBM:GetModLocalization(2495)
+
+-----------------------
+-- Vexamus --
+-----------------------
+--L= DBM:GetModLocalization(9999)
+
+-----------------------
+-- Accidental Amalgamation --
+-----------------------
+--L= DBM:GetModLocalization(9999)
+
+-----------------------
+-- Headmaster --
+-----------------------
+--L= DBM:GetModLocalization(9999)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("TazalgethAcademyTrash")
+
+L:SetGeneralLocalization({
+	name =	"Taz'algeth Academy Trash"
+})
+
+-----------------------
+-- <<<The Azure Vault>>> --
+-----------------------
+-----------------------
+-- Leymor --
+-----------------------
+--L= DBM:GetModLocalization(2492)
+
+-----------------------
+-- Talash Greywing --
+-----------------------
+--L= DBM:GetModLocalization(9999)
+
+-----------------------
+-- Umbrelskul --
+-----------------------
+--L= DBM:GetModLocalization(9999)
+
+-----------------------
+-- Azureblade --
+-----------------------
+--L= DBM:GetModLocalization(9999)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("TheAzurevaultTrash")
+
+L:SetGeneralLocalization({
+	name =	"Azure Vault Trash"
+})
+
+-----------------------
+-- <<<Ruby Life Pools>>> --
+-----------------------
+-----------------------
+-- Melidrussa Chillworn --
+-----------------------
+--L= DBM:GetModLocalization(2488)
+
+-----------------------
+-- Kokia Blazehoof --
+-----------------------
+--L= DBM:GetModLocalization(2485)
+
+-----------------------
+-- Kyrakka and Erkhart Stormvein --
+-----------------------
+--L= DBM:GetModLocalization(2503)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("RubyLifePoolsTrash")
+
+L:SetGeneralLocalization({
+	name =	"루비 생명의 연못 일반몹"
+})
+
+-----------------------
+-- <<<Halls of Infusion>>> --
+-----------------------
+-----------------------
+-- Watcher Irideus --
+-----------------------
+--L= DBM:GetModLocalization(9999)
+
+-----------------------
+-- Gulping Goliath --
+-----------------------
+--L= DBM:GetModLocalization(9999)
+
+-----------------------
+-- Khajin the Unyielding --
+-----------------------
+--L= DBM:GetModLocalization(9999)
+
+-----------------------
+-- Primal Tsunami --
+-----------------------
+--L= DBM:GetModLocalization(9999)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("HallsofInfusionTrash")
+
+L:SetGeneralLocalization({
+	name =	"Halls of Infusion Trash"
+})
