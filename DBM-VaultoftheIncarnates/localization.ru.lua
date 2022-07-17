e667d865b5643f23a46e69c41ad2050bdadb9785
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2022-07-18 00:12:41 +0500

    Create localization.ru.lua (#778)

---
+++ DBM-VaultoftheIncarnates/localization.ru.lua
@@ -0,0 +1,63 @@
+if GetLocale() ~= "ruRU" then return end
+local L
+
+---------------------------
+--  Eranog --
+---------------------------
+--L= DBM:GetModLocalization(2480)
+
+--L:SetWarningLocalization({
+--})
+--
+--L:SetTimerLocalization{
+--}
+--
+--L:SetOptionLocalization({
+--})
+--
+--L:SetMiscLocalization({
+--})
+
+---------------------------
+--  Terros --
+---------------------------
+--L= DBM:GetModLocalization(2500)
+
+---------------------------
+--  The Primalist Council --
+---------------------------
+--L= DBM:GetModLocalization(2486)
+
+---------------------------
+--  Sennarth, The Cold Breath --
+---------------------------
+--L= DBM:GetModLocalization(2482)
+
+---------------------------
+--  Dathea, Ascended --
+---------------------------
+--L= DBM:GetModLocalization(2502)
+
+---------------------------
+--  Kurog Grimtotem --
+---------------------------
+--L= DBM:GetModLocalization(2491)
+
+---------------------------
+--  Broodkeeper Diurna --
+---------------------------
+--L= DBM:GetModLocalization(2493)
+
+---------------------------
+--  Raszageth the Storm-Eater --
+---------------------------
+--L= DBM:GetModLocalization(2499)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("VaultoftheIncarnatesTrash")
+
+L:SetGeneralLocalization({
+	name =	"Трэш мобы Хранилище Воплощений"
+})
