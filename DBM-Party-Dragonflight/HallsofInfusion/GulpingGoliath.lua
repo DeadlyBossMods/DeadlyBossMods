0dc0293cbf15c2a4235e18fd1f5b98f21276c89f
MysticalOS <mysticalosx@gmail.com>
2022-07-26 18:51:15 -0400

    Enable 3 more dungeon mods now that they exist in journal

--- DBM-Party-Dragonflight/HallsofInfusion/GulpingGoliath.lua
+++ DBM-Party-Dragonflight/HallsofInfusion/GulpingGoliath.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod(9999, "DBM-Party-Dragonflight", 8, 1204)
+local mod	= DBM:NewMod(2507, "DBM-Party-Dragonflight", 8, 1204)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
