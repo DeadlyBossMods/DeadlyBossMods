9045b940b7bd173679b914e1198df086f8428bc1
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2022-07-29 15:41:26 +0500

    Create localization.ru.lua
    Add localization.ru.lua

---
+++ DBM-Affixes/localization.ru.lua
@@ -0,0 +1,20 @@
+if GetLocale() ~= "ruRU" then return end
+local L
+
+-----------------------
+-- <<< M+ Affixes >>> --
+-----------------------
+L = DBM:GetModLocalization("MPlusAffixes")
+
+L:SetGeneralLocalization({
+	name =	"М+ аффиксы"
+})
+
+-----------------------
+-- <<< Fated Raid Affixes >>> --
+-----------------------
+L = DBM:GetModLocalization("FatedAffixes")
+
+L:SetGeneralLocalization({
+	name =	"Судьбоносные аффиксы"
+})
