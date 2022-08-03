9de6fe2a57ca756731d3a31632320c70c6cfb212
Elnarfim <37562642+Elnarfim@users.noreply.github.com>
2022-08-04 05:03:50 +0900

    Update koKR (DF) (#797)

---
+++ DBM-DragonIsles/localization.kr.lua
@@ -0,0 +1,11 @@
+if GetLocale() ~= "koKR" then return end
+local L
+
+-----------------------
+-- Basrikron, The Shale Wing --
+-----------------------
+--L= DBM:GetModLocalization(2506)
+
+--L:SetMiscLocalization({
+--	Pull	= ""
+--})
