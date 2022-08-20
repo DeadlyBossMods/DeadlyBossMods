c969cfcbf56f437f5f6acc802da750b9059e841b
MysticalOS <mysticalosx@gmail.com>
2022-08-19 22:03:18 -0400

    How did these not push earlier?

--- DBM-VaultoftheIncarnates/ThePrimalistCouncil.lua
+++ DBM-VaultoftheIncarnates/ThePrimalistCouncil.lua
@@ -46,14 +46,14 @@ function mod:OnCombatStart(delay)
 
 end
 
-function mod:OnCombatEnd()
+--function mod:OnCombatEnd()
 --	if self.Options.RangeFrame then
 --		DBM.RangeCheck:Hide()
 --	end
 --	if self.Options.InfoFrame then
 --		DBM.InfoFrame:Hide()
 --	end
-end
+--end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
