3f7b535b292e4fcadb2c54b13e4f26c861efd1b9
BlueNightSky <afan1128@hotmail.com>
2023-03-16 19:45:37 +0800

    Create localization.tw.lua (#873)

---
+++ DBM-Aberrus/localization.tw.lua
@@ -0,0 +1,71 @@
+local L
+
+---------------------------
+--  Kazzara --
+---------------------------
+--L= DBM:GetModLocalization(2522)
+
+--L:SetWarningLocalization({
+--})
+--
+--L:SetTimerLocalization{
+--}
+--
+--L:SetOptionLocalization({
+--})
+--
+--L:SetMiscLocalization({
+--})
+
+---------------------------
+--  The Amalgamation Chamber --
+---------------------------
+L= DBM:GetModLocalization(2529)
+
+L:SetOptionLocalization({
+	AdvancedBossFiltering	= "在第1階段中積極掃描與每個首領的距離，並自動隱藏您非接近首領的某些警報和淡出的計時器（超過43距離）"
+})
+
+---------------------------
+--  The Forgotten Experiments --
+---------------------------
+--L= DBM:GetModLocalization(2530)
+
+---------------------------
+--  Assault of the Zaqali --
+---------------------------
+--L= DBM:GetModLocalization(2524)
+
+---------------------------
+--  Rashok --
+---------------------------
+--L= DBM:GetModLocalization(2525)
+
+---------------------------
+--  The Vigilant Steward, Zskarn --
+---------------------------
+--L= DBM:GetModLocalization(2532)
+
+---------------------------
+--  Magmorax --
+---------------------------
+--L= DBM:GetModLocalization(2527)
+
+---------------------------
+--  Echo of Neltharion --
+---------------------------
+--L= DBM:GetModLocalization(2523)
+
+---------------------------
+--  Scalecommander Sarkareth --
+---------------------------
+--L= DBM:GetModLocalization(2520)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("AberrusTrash")
+
+L:SetGeneralLocalization({
+	name =	"亞貝魯斯小怪"
+})
