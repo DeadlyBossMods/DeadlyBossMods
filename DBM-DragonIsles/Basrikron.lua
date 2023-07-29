a8864928dc15aed29a2b104f0e6e260a757b9779
MysticalOS <mysticalosx@gmail.com>
2023-07-28 22:03:10 -0400

    Some basrikron fixes while at it since I had to solo it earlier and had about 15 minutes worth of data to look at.

--- DBM-DragonIsles/Basrikron.lua
+++ DBM-DragonIsles/Basrikron.lua
@@ -14,16 +14,8 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 385652 385137 386059 386259",
 	"SPELL_CAST_SUCCESS 386680"
---	"SPELL_AURA_APPLIED_DOSE",
---	"SPELL_AURA_REMOVED",
---	"SPELL_PERIODIC_DAMAGE",
---	"SPELL_PERIODIC_MISSED"
 )
 
---TODO, Awaken Crag trigger correct?
---TODO, fracturing tremor correct?
---local warnFuriousSlam					= mod:NewTargetNoFilterAnnounce(361209, 2)
---local warnDarkDeterrence				= mod:NewStackAnnounce(361390, 2, nil, "Tank|Healer")
 local warnAwakenCrag					= mod:NewSpellAnnounce(385506, 2)--Bigger alert if needed
 local warnFracturingTremor				= mod:NewSpellAnnounce(385270, 2)--Bigger alert if needed
 local warnShaleBeath					= mod:NewSpellAnnounce(385137, 3, nil, "Tank|Healer", nil, nil, nil, 2)
@@ -31,12 +23,10 @@ local warnShaleBeath					= mod:NewSpellAnnounce(385137, 3, nil, "Tank|Healer", n
 local specWarnSundneringCrash			= mod:NewSpecialWarningDodge(386259, nil, nil, nil, 2, 2)
 local specWarnEarthBolt					= mod:NewSpecialWarningInterrupt(385652, "HasInterrupt", nil, nil, 1, 2)
 
-local timerSunderingCrashCD				= mod:NewAITimer(74.7, 386259, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON)
-local timerAwakenCragCD					= mod:NewCDTimer(36.6, 385506, nil, nil, nil, 1, nil, DBM_COMMON_L.DAMAGE_ICON)--Wild Variation, but also world lag
+local timerSunderingCrashCD				= mod:NewCDTimer(65, 386259, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON)
+local timerAwakenCragCD					= mod:NewCDTimer(31.6, 385506, nil, nil, nil, 1, nil, DBM_COMMON_L.DAMAGE_ICON)--Wild Variation, but also world lag
 local timerFracturingTremorCD			= mod:NewCDTimer(13.9, 385270, nil, nil, nil, 3)
 local timerShaleBreathCD				= mod:NewCDTimer(15.9, 385137, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
-
---mod:AddRangeFrameOption(5, 361632)
 
 function mod:OnCombatStart(delay, yellTriggered)
 --	if yellTriggered then
@@ -84,20 +74,3 @@ function mod:SPELL_CAST_SUCCESS(args)
 		timerFracturingTremorCD:Start()
 	end
 end
-
---[[
-function mod:SPELL_AURA_REMOVED(args)
-	local spellId = args.spellId
-	if spellId == 361632 then
-
-	end
-end
-
-function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
-	if spellId == 361335 and destGUID == UnitGUID("player") and self:AntiSpam(2, 4) then
-		specWarnGTFO:Show(spellName)
-		specWarnGTFO:Play("watchfeet")
-	end
-end
-mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
---]]
