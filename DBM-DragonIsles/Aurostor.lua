245270c48baece8e90e1c466d9671e7c86f181ee
MysticalOS <mysticalosx@gmail.com>
2024-01-19 01:53:17 -0500

    Aurostor Update:  - Added cranky tantrum counter  - Changed recombat time from 20 to 30 to avoid false engages that could occur -Council of Dreams Update:  - Special warning for Barreling charge now always shows, instead of only if you have debuff  - In addition, special warning for barreling charge will now announce which soak group it is via spoken alerts (ie "group 1 share", "group 2 share") if you do not have debuff. It'll continue to say "charge move" if you do.  - Song of the Dragon "take damage" alert will now wait until absorb effect is up instead of saying it immediately on song being cast, so it's not telling you to take damage too early  - In addition, Song of the Dragon take damage alert will repeat a 2nd time if you have not yet cleared the full absorb after 6 seconds.  - Then finally, a new alert will show that song of the dragon has successfully faded.  - GTFO warning will no longer alert you are afflicted with song of the dragon absorb

--- DBM-DragonIsles/Aurostor.lua
+++ DBM-DragonIsles/Aurostor.lua
@@ -4,27 +4,33 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision("@file-date-integer@")
 mod:SetCreatureID(209574)
 mod:SetEncounterID(2828)
-mod:SetReCombatTime(20)
+mod:SetReCombatTime(30)
 mod:EnableWBEngageSync()--Enable syncing engage in outdoors
-mod:SetHotfixNoticeRev(20231129000000)
---mod:SetMinSyncRevision(11969)
+mod:SetHotfixNoticeRev(20240119000000)
+mod:SetMinSyncRevision(20240119000000)
 
 mod:RegisterCombat("combat")
 --mod:RegisterCombat("combat_yell", L.Pull)
 mod:RegisterKill("yell", L.Win)
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 420895 420925 421260",
+	"SPELL_CAST_START 420895 420925 421260 421059",
+	"SPELL_CAST_SUCCESS 421006",
 	"SPELL_AURA_APPLIED 421260 181089"
 )
+
+local warnCrankyTantrum				= mod:NewCountAnnounce(421059, 3)
 
 local specWarnGroggyBash				= mod:NewSpecialWarningYou(420895, nil, nil, nil, 1, 2)
 local specWarnPulverizingOutburst		= mod:NewSpecialWarningDodge(420925, nil, nil, nil, 1, 2)
 local specWarnRoarDebuff				= mod:NewSpecialWarningJump(421260, nil, nil, nil, 1, 6)
 
-local timerGroggyBashCD					= mod:NewCDTimer(33, 420895, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
---local timerPulverizingOutburstCD		= mod:NewAITimer(15.9, 420925, nil, nil, nil, 3)--Health based? 15-59 is too much variation for a CD timer
+local timerGroggyBashCD					= mod:NewCDTimer(32.7, 420895, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
+--local timerPulverizingOutburstCD		= mod:NewAITimer(15.7, 420925, nil, nil, nil, 3)--15-59 is too much variation, would need spell queuing and cast priority to be sorted out
 local timerSlumberingRoarCD				= mod:NewCDTimer(70.9, 421260, nil, nil, nil, 2)--Small sample
+local timerCrankyTantrumCD				= mod:NewCDTimer(27.9, 421059, nil, nil, nil, 3)--27.9-43.8
+
+mod.vb.tantrumCount = 0
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
@@ -40,6 +46,17 @@ function mod:SPELL_CAST_START(args)
 --		timerPulverizingOutburstCD:Start()
 	elseif spellId == 421260 then
 		timerSlumberingRoarCD:Start()
+	elseif spellId == 421059 then
+		self.vb.tantrumCount = self.vb.tantrumCount + 1
+		warnCrankyTantrum:Show(self.vb.tantrumCount)
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+	if spellId == 421006 then
+		self.vb.tantrumCount = 0
+		timerCrankyTantrumCD:Start()
 	end
 end
 
