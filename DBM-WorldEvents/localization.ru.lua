9ad4107416f5d1465e7fe87f145bb931a12428f2
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2024-02-11 15:32:14 +0500

    Update localization.ru.lua (#991)

--- DBM-WorldEvents/localization.ru.lua
+++ DBM-WorldEvents/localization.ru.lua
@@ -23,7 +23,8 @@ L:SetTimerLocalization{
 }
 
 L:SetOptionLocalization({
-	TrioActiveTimer		= "Отсчет времени до вступления Троих аптекарей в бой"
+	TrioActiveTimer		= "Отсчет времени до вступления Троих аптекарей в бой",
+	AGBoss				= "Автоматический выбор диалога для вызова Троих аптекарей"
 })
 
 L:SetMiscLocalization({
