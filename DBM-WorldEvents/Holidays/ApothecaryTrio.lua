0d7ceafbd58352ba9946f30095be7098c1b7d51c
MysticalOS <mysticalosx@gmail.com>
2024-02-11 04:45:31 -0500

    scope trio to just run gossip checks in shadowfang keep

--- DBM-WorldEvents/Holidays/ApothecaryTrio.lua
+++ DBM-WorldEvents/Holidays/ApothecaryTrio.lua
@@ -2,6 +2,7 @@ local mod	= DBM:NewMod("d288", "DBM-WorldEvents", 1)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
+mod:SetZone(33)
 mod:SetCreatureID(36272, 36296, 36565)
 mod:SetModelID(16176)
 
