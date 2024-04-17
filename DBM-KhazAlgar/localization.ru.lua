34462ebeada28a9143a1e310937ba16569cf07ea
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2024-04-18 00:28:40 +0500

    Create localization.ru.lua

---
+++ DBM-KhazAlgar/localization.ru.lua
@@ -0,0 +1,11 @@
+if GetLocale() ~= "ruRU" then return end
+local L
+
+-----------------------
+-- Орта, Разбитая гора --
+-----------------------
+--L= DBM:GetModLocalization(2625)
+
+--L:SetMiscLocalization({
+--	Pull	= ""
+--})
