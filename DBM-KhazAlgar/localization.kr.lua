c8b69a3b5ee33e9ede8f0bd536f73ab8b49a9102
Elnarfim <37562642+Elnarfim@users.noreply.github.com>
2024-04-18 04:54:49 +0900

    koKR locale for warwithin prep

---
+++ DBM-KhazAlgar/localization.kr.lua
@@ -0,0 +1,11 @@
+if GetLocale() ~= "koKR" then return end
+local L
+
+-----------------------
+-- Orta, the Broken Mountain --
+-----------------------
+--L= DBM:GetModLocalization(2625)
+
+--L:SetMiscLocalization({
+--	Pull	= ""
+--})
