DeadlyBossMods/DeadlyBossMods

commit 59d2cd7b94737d8d3c99f730a9028d0f51b1df5a

Oops, it actually changed timer of all 4 abilities, not just 3 of them. darkness also increased due to the change

DBM-Raids-Dragonflight/Aberrus/EchoofNeltharion.lua
@@ -250,7 +250,7 @@ function mod:SPELL_CAST_START(args)
 		if self:GetStage(3) and (self.vb.RushingDarknessCount == 1) and not self.vb.skippedDarkness then
 			timerRushingDarknessCD:Start(17, self.vb.RushingDarknessCount+1)
 		else
-			timerRushingDarknessCD:Start(self:GetStage(1) and 35.9 or 27.9, self.vb.RushingDarknessCount+1)--27.9-29.2, almost always 29 but sometimes 28 :\
+			timerRushingDarknessCD:Start(self:GetStage(1) and 35.9 or 35.2, self.vb.RushingDarknessCount+1)--27.9-29.2, almost always 29 but sometimes 28 :\
 		end
 		if self:IsMythic() and self:GetStage(1) then--Mythic P1 only wall breaker strat used by all top guilds (which means everyone else will use it too and expect it in DBM)
 			self:BossTargetScanner(args.sourceGUID, "RushingDarknessTarget", 0.2, 8, true, nil, nil, nil, true)

commit 3e8445d9af337cf8cd0a0a077707cae2f1737bed

Fix timer regression from https://github.com/DeadlyBossMods/DeadlyBossMods/commit/59d2cd7b94737d8d3c99f730a9028d0f51b1df5a . Didn't mean to change P2's timer. That's unchanged

DBM-Raids-Dragonflight/Aberrus/EchoofNeltharion.lua
@@ -250,7 +250,7 @@ function mod:SPELL_CAST_START(args)
 		if self:GetStage(3) and (self.vb.RushingDarknessCount == 1) and not self.vb.skippedDarkness then
 			timerRushingDarknessCD:Start(17, self.vb.RushingDarknessCount+1)
 		else
-			timerRushingDarknessCD:Start(self:GetStage(1) and 35.9 or 35.2, self.vb.RushingDarknessCount+1)--new, now every 35-36 due to inceased cast time of portals
+			timerRushingDarknessCD:Start(self:GetStage(2) and 27.9 or 35.2, self.vb.RushingDarknessCount+1)--new, now every 35-36 due to inceased cast time of portals
 		end
 		if self:IsMythic() and self:GetStage(1) then--Mythic P1 only wall breaker strat used by all top guilds (which means everyone else will use it too and expect it in DBM)
 			self:BossTargetScanner(args.sourceGUID, "RushingDarknessTarget", 0.2, 8, true, nil, nil, nil, true)