4f7ab28fd73f2ae39aa5f514f08d09523319c216
MysticalOS <mysticalosx@gmail.com>
2024-05-11 00:48:40 -0400

    Push drycodes for both War within world bosses

--- DBM-KhazAlgar/localization.kr.lua
+++ DBM-KhazAlgar/localization.kr.lua
@@ -2,6 +2,15 @@ if GetLocale() ~= "koKR" then return end
 local L
 
 -----------------------
+-- Aggregation of Horrors --
+-----------------------
+--L= DBM:GetModLocalization(2635)
+
+--L:SetMiscLocalization({
+--	Pull	= ""
+--})
+
+-----------------------
 -- Orta, the Broken Mountain --
 -----------------------
 --L= DBM:GetModLocalization(2625)
