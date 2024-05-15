ca817ff8a7180557a249baaf0a520f1afb9a3980
MysticalOS <mysticalosx@gmail.com>
2024-05-15 00:48:32 -0400

    Preliminary delve work

---
+++ DBM-Delves/TheSpiralWeave.lua
@@ -0,0 +1,135 @@
+local mod	= DBM:NewMod("z2688", "DBM-Delves")
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision("@file-date-integer@")
+
+mod:RegisterCombat("scenario", 2688)
+
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_START 449318 450546 433410 449072 448644 449038",
+	--"SPELL_CAST_SUCCESS",
+	--"SPELL_AURA_APPLIED",
+	--"SPELL_AURA_REMOVED",
+	--"SPELL_PERIODIC_DAMAGE",
+	"UNIT_DIED",
+	"ENCOUNTER_START",
+	"ENCOUNTER_END"
+)
+
+local warnShadowsofStrife					= mod:NewCastAnnounce(449318, 3)--High Prio Interrupt
+local warnWebbedAegis						= mod:NewCastAnnounce(450546, 3)--Can only be CCed
+local warnDrones							= mod:NewSpellAnnounce(449072, 2)
+
+local specWarnFearfulShriek					= mod:NewSpecialWarningDodge(433410, nil, nil, nil, 2, 2)
+local specWarnShadowsofStrife				= mod:NewSpecialWarningInterrupt(449318, "HasInterrupt", nil, nil, 1, 2)--High Prio Interrupt
+local specWarnBurrowingTremors				= mod:NewSpecialWarningRun(448644, nil, nil, nil, 4, 2)--Boss
+local specWarnImpalingStrikes				= mod:NewSpecialWarningDodge(433410, nil, nil, nil, 2, 2)--Boss
+
+local timerShadowsofStrifeCD				= mod:NewCDNPTimer(12.4, 449318, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)--12.4-15.1
+local timerWebbedAegisCD					= mod:NewCDNPTimer(23.1, 450546, nil, nil, nil, 5)
+local timerBurrowingTremorsCD				= mod:NewCDTimer(31.5, 448644, nil, nil, nil, 5)
+local timerCallDronesCD						= mod:NewCDTimer(31.5, 449072, nil, nil, nil, 1)
+local timerImpalingStrikesCD				= mod:NewCDTimer(31.5, 433410, nil, nil, nil, 3)
+
+--Antispam IDs for this mod: 1 run away, 2 dodge, 3 dispel, 4 incoming damage, 5 you/role, 6 misc, 7 off interrupt
+
+mod.vb.impalingCount = 0
+
+function mod:SPELL_CAST_START(args)
+	if args.spellId == 449318 then
+		timerShadowsofStrifeCD:Start(nil, args.sourceGUID)
+		if self.Options.SpecWarn449318interrupt and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+			specWarnShadowsofStrife:Show(args.sourceName)
+			specWarnShadowsofStrife:Play("kickcast")
+		elseif self:AntiSpam(3, 7) then
+			warnShadowsofStrife:Show()
+		end
+	elseif args.spellId == 450546 then
+		timerWebbedAegisCD:Start(nil, args.sourceGUID)
+		if self:AntiSpam(3, 6) then
+			warnWebbedAegis:Show()
+			warnWebbedAegis:Play("crowdcontrol")
+		end
+	elseif args.spellId == 433410 then
+		if self:AntiSpam(3, 2) then
+			specWarnFearfulShriek:Show()
+			specWarnFearfulShriek:Play("watchstep")
+		end
+	elseif args.spellId == 449072 then
+		warnDrones:Show()
+		timerCallDronesCD:Start()
+	elseif args.spellId == 448644 then
+		specWarnBurrowingTremors:Show()
+		specWarnBurrowingTremors:Play("justrun")
+		specWarnBurrowingTremors:ScheduleVoice(1.5, "keepmove")
+		timerBurrowingTremorsCD:Start()
+	elseif args.spellId == 449038 then
+		self.vb.impalingCount = self.vb.impalingCount + 1
+		specWarnImpalingStrikes:Show()
+		specWarnImpalingStrikes:Play("watchstep")
+		if self.vb.impalingCount < 3 then
+			timerImpalingStrikesCD:Start(25.1)
+		else
+			timerImpalingStrikesCD:Start(31.5)
+		end
+	end
+end
+
+--[[
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 138680 then
+
+	end
+end
+--]]
+
+--[[
+function mod:SPELL_AURA_APPLIED(args)
+	if args.spellId == 1098 then
+
+	end
+end
+--]]
+
+--[[
+function mod:SPELL_AURA_REMOVED(args)
+	if args.spellId == 1098 then
+
+	end
+end
+--]]
+
+--[[
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
+	if spellId == 138561 and destGUID == UnitGUID("player") and self:AntiSpam() then
+
+	end
+end
+--]]
+
+function mod:UNIT_DIED(args)
+	--if args.destGUID == UnitGUID("player") then--Solo scenario, a player death is a wipe
+
+	--end
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 208242 then--Nerubian Darkcaster
+		timerShadowsofStrifeCD:Stop(args.destGUID)
+	elseif cid == 216584 then--Nerubian Captain
+		timerWebbedAegisCD:Stop(args.destGUID)
+	end
+end
+
+function mod:ENCOUNTER_START(eID)
+	if eID == 2990 then
+		--Start some timers
+		self.vb.impalingCount = 0
+		timerBurrowingTremorsCD:Start(12.5)
+		timerCallDronesCD:Start(27.1)
+	end
+end
+
+function mod:ENCOUNTER_END(eID)
+	if eID == 2990 then
+		DBM:EndCombat(self)
+	end
+end
--- DBM-KhazAlgar/AggregationofHorrors.lua
+++ DBM-KhazAlgar/AggregationofHorrors.lua
@@ -21,35 +21,35 @@ mod:RegisterEventsInCombat(
 --TODO, Skardyn adds?
 --TODO, verify bursting cast event and warning type it should use
 --TODO, does https://www.wowhead.com/beta/spell=453298/retribution-pain stack? if so add stack and taunt warnings
-local warnBurstingRetribution			= mod:NewSpellAnnounce(452210, 2)
-local warnEntropy     		        	= mod:NewSpellAnnounce(453271, 4)--Tooltip bugged? why does boss have two "not in melee range of boss" mechanics?
-local warnRetributionPunch  			= mod:NewSpellAnnounce(453294, 3)
-local warnThrowDarkrock       			= mod:NewSpellAnnounce(455198, 3)--why does boss have two "not in melee range of boss" mechanics?
+local warnCrystallinebarrage			= mod:NewSpellAnnounce(452210, 2)
+local warnDarkAwakening                 = mod:NewSpellAnnounce(453271, 4)--Tooltip bugged? why does boss have two "not in melee range of boss" mechanics?
+local warnCrystalStrike  		    	= mod:NewSpellAnnounce(453294, 3)
+local warnAnnihilationBarrage       	= mod:NewSpellAnnounce(455198, 3)--why does boss have two "not in melee range of boss" mechanics?
 
-local specWarnFuryHorrors				= mod:NewSpecialWarningDodge(452980, nil, nil, nil, 2, 2)
+local specWarnVoidquake				    = mod:NewSpecialWarningDodge(452980, nil, nil, nil, 2, 2)
 
-local timerBurstingRetributionCD		= mod:NewAITimer(32.7, 452210, nil, nil, nil, 1)
-local timerFuryHorrorsCD	        	= mod:NewAITimer(32.7, 452980, nil, nil, nil, 3)
-local timerRetributionPunchCD	       	= mod:NewAITimer(32.7, 453294, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
+local timerCrystallinebarrageCD		    = mod:NewAITimer(32.7, 452210, nil, nil, nil, 1)
+local timerVoidquakeCD	        	    = mod:NewAITimer(32.7, 452980, nil, nil, nil, 3)
+local timerCrystalStrikeCD	          	= mod:NewAITimer(32.7, 453294, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 452205 then
-        warnBurstingRetribution:Show()
-        timerBurstingRetributionCD:Start()
+        warnCrystallinebarrage:Show()
+        timerCrystallinebarrageCD:Start()
     elseif spellId == 453271 then
-        warnEntropy:Show()
+        warnDarkAwakening:Show()
     elseif spellId == 452980 then
-        specWarnFuryHorrors:Show()
-        specWarnFuryHorrors:Play("shockwave")
-        timerFuryHorrorsCD:Start()
+        specWarnVoidquake:Show()
+        specWarnVoidquake:Play("shockwave")
+        timerVoidquakeCD:Start()
     elseif spellId == 453294 then
-        timerRetributionPunchCD:Start()
+        timerCrystalStrikeCD:Start()
         if self:IsTanking("player", nil, nil, true, args.sourceGUID) then
-            warnRetributionPunch:Show()
+            warnCrystalStrike:Show()
         end
     elseif spellId == 455188 then
-        warnThrowDarkrock:Show()
+        warnAnnihilationBarrage:Show()
 	end
 end
 
