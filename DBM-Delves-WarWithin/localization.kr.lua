0220ba52df1eebd8ed133f3fcf1766e5401e1342
Elnarfim <37562642+Elnarfim@users.noreply.github.com>
2024-05-15 16:52:35 +0900

    Delves kr locale initializing (#1087)

---
+++ DBM-Delves-WarWithin/localization.kr.lua
@@ -0,0 +1,19 @@
+if GetLocale() ~= "koKR" then return end
+local L
+
+---------------------
+-- The Spiral Weave --
+---------------------
+L = DBM:GetModLocalization("z2688")
+
+L:SetTimerLocalization({
+
+})
+
+L:SetOptionLocalization({
+
+})
+
+L:SetMiscLocalization({
+
+})
