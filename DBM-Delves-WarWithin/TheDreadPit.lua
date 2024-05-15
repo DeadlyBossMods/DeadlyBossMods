61bdf59fb7a67a8ceae00421a4c50578138d67c6
MysticalOS <mysticalosx@gmail.com>
2024-05-15 16:11:41 -0400

    template remaining delve mods

---
+++ DBM-Delves-WarWithin/TheDreadPit.lua
@@ -0,0 +1,89 @@
+local mod	= DBM:NewMod("z2684", "DBM-Delves")
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision("@file-date-integer@")
+
+mod:RegisterCombat("scenario", 2684)
+
+mod:RegisterEventsInCombat(
+--	"SPELL_CAST_START ",
+--	"SPELL_CAST_SUCCESS",
+--	"SPELL_AURA_APPLIED",
+--	"SPELL_AURA_REMOVED",
+--	"SPELL_PERIODIC_DAMAGE",
+--	"UNIT_DIED",
+	"ENCOUNTER_START",
+	"ENCOUNTER_END"
+)
+
+--local warnDrones							= mod:NewSpellAnnounce(449072, 2)
+
+--local specWarnFearfulShriek				= mod:NewSpecialWarningDodge(433410, nil, nil, nil, 2, 2)
+
+--local timerShadowsofStrifeCD				= mod:NewCDNPTimer(12.4, 449318, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
+--local timerBurrowingTremorsCD				= mod:NewCDTimer(31.5, 448644, nil, nil, nil, 5)
+
+--Antispam IDs for this mod: 1 run away, 2 dodge, 3 dispel, 4 incoming damage, 5 you/role, 6 misc, 7 off interrupt
+
+--[[
+function mod:SPELL_CAST_START(args)
+	if args.spellId == 449318 then
+
+	end
+end
+--]]
+
+--[[
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 138680 then
+
+	end
+end
+--]]
+
+--[[
+function mod:SPELL_AURA_APPLIED(args)
+	if args.spellId == 1098 then
+
+	end
+end
+--]]
+
+--[[
+function mod:SPELL_AURA_REMOVED(args)
+	if args.spellId == 1098 then
+
+	end
+end
+--]]
+
+--[[
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
+	if spellId == 138561 and destGUID == UnitGUID("player") and self:AntiSpam() then
+
+	end
+end
+--]]
+
+--[[
+function mod:UNIT_DIED(args)
+	--if args.destGUID == UnitGUID("player") then--Solo scenario, a player death is a wipe
+
+	--end
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 208242 then
+	end
+end
+--]]
+
+function mod:ENCOUNTER_START(eID)
+	if eID == 2990 then
+		--Start some timers
+	end
+end
+
+function mod:ENCOUNTER_END(eID)
+	if eID == 2990 then
+		DBM:EndCombat(self)
+	end
+end
--- DBM-Delves-WarWithin/localization.en.lua
+++ DBM-Delves-WarWithin/localization.en.lua
@@ -1,9 +1,9 @@
 local L
 
 ---------------------
--- The Spiral Weave --
+-- Fungal Folly --
 ---------------------
-L = DBM:GetModLocalization("z2688")
+L = DBM:GetModLocalization("z2664")
 
 L:SetTimerLocalization({
 
@@ -17,3 +17,62 @@ L:SetMiscLocalization({
 
 })
 
+---------------------
+-- Mycomancer Cavern --
+---------------------
+L = DBM:GetModLocalization("z2679")
+
+---------------------
+-- Earthcrawl Mines --
+---------------------
+L = DBM:GetModLocalization("z2680")
+
+---------------------
+-- Kriegval's Rest --
+---------------------
+L = DBM:GetModLocalization("z2681")
+
+---------------------
+-- Zekvir's Lair --
+---------------------
+L = DBM:GetModLocalization("z2682")
+
+---------------------
+-- The Waterworks --
+---------------------
+L = DBM:GetModLocalization("z2683")
+
+---------------------
+-- The Dread Pit --
+---------------------
+L = DBM:GetModLocalization("z2684")
+
+---------------------
+-- Skittering Breach --
+---------------------
+L = DBM:GetModLocalization("z2685")
+
+---------------------
+-- Nightfall Sanctum --
+---------------------
+L = DBM:GetModLocalization("z2686")
+
+---------------------
+-- The Sinkhole --
+---------------------
+L = DBM:GetModLocalization("z2687")
+
+---------------------
+-- The Spiral Weave --
+---------------------
+L = DBM:GetModLocalization("z2688")
+
+---------------------
+-- Tak-Rethan Abyss --
+---------------------
+L = DBM:GetModLocalization("z2689")
+
+---------------------
+-- The Underkeep --
+---------------------
+L = DBM:GetModLocalization("z2690")
