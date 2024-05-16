2946a4f5c9819b677d2d0fbe0f6123363d02f03a
MysticalOS <mysticalosx@gmail.com>
2024-05-16 02:46:09 -0400

    add 3 new trash abilities to delves Added support for FungalFolley's end boss

--- DBM-Delves-WarWithin/TrashCommon.lua
+++ DBM-Delves-WarWithin/TrashCommon.lua
@@ -13,15 +13,21 @@ mod:RegisterEvents(
 )
 
 --TODO Add Void Bolt interrupt. it hits for 1.4 Million on level 2
+--NOTE: Many abilities are shared by mobs that can spawn in ANY delve. However Some warnings/mobs might actually be specific to certain delves
+--for now ALL are being put in common til we have enough data to scope trash abilities to appropriate modules
 local warnShadowsofStrife					= mod:NewCastAnnounce(449318, 3)--High Prio Interrupt
 local warnWebbedAegis						= mod:NewCastAnnounce(450546, 3)--Can only be CCed
+local warnDebilitatingVenom					= mod:NewTargetNoFilterAnnounce(424614, 3)--Brann will dispel this if healer role
 
 local specWarnFearfulShriek					= mod:NewSpecialWarningDodge(433410, nil, nil, nil, 2, 2)
 local specWarnJaggedBarbs					= mod:NewSpecialWarningDodge(450714, nil, nil, nil, 2, 2)
 local specWarnLavablast	    				= mod:NewSpecialWarningDodge(445781, nil, nil, nil, 2, 2)
+local specWarnFungalBreath    				= mod:NewSpecialWarningDodge(415253, nil, nil, nil, 2, 2)
 local specWarnShadowsofStrife				= mod:NewSpecialWarningInterrupt(449318, "HasInterrupt", nil, nil, 1, 2)--High Prio Interrupt
+local specWarnRotWaveVolley					= mod:NewSpecialWarningInterrupt(425040, "HasInterrupt", nil, nil, 1, 2)
 
 local timerShadowsofStrifeCD				= mod:NewCDNPTimer(12.4, 449318, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)--12.4-15.1
+local timerRotWaveVolleyCD					= mod:NewCDNPTimer(12.4, 425040, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)--14.6-17
 local timerWebbedAegisCD					= mod:NewCDNPTimer(23.1, 450546, nil, nil, nil, 5)
 local timerLavablastCD					    = mod:NewCDNPTimer(15.8, 445781, nil, nil, nil, 3)
 
@@ -35,9 +41,9 @@ do
 		if not force and validZones[currentZone] and not eventsRegistered then
 			eventsRegistered = true
 			self:RegisterShortTermEvents(
-                "SPELL_CAST_START 449318 450546 433410 450714 445781",
+                "SPELL_CAST_START 449318 450546 433410 450714 445781 415253 425040",
                 --"SPELL_CAST_SUCCESS",
-                --"SPELL_AURA_APPLIED",
+                "SPELL_AURA_APPLIED 424614",
                 --"SPELL_AURA_REMOVED",
                 --"SPELL_PERIODIC_DAMAGE",
                 "UNIT_DIED"
@@ -68,6 +74,12 @@ function mod:SPELL_CAST_START(args)
 		elseif self:AntiSpam(3, 7) then
 			warnShadowsofStrife:Show()
 		end
+	elseif args.spellId == 425040 then
+		timerRotWaveVolleyCD:Start(nil, args.sourceGUID)
+		if self:CheckInterruptFilter(args.sourceGUID, false, true) then
+			specWarnRotWaveVolley:Show(args.sourceName)
+			specWarnRotWaveVolley:Play("kickcast")
+		end
 	elseif args.spellId == 450546 then
 		timerWebbedAegisCD:Start(nil, args.sourceGUID)
 		if self:AntiSpam(3, 6) then
@@ -90,6 +102,11 @@ function mod:SPELL_CAST_START(args)
 			specWarnLavablast:Show()
 			specWarnLavablast:Play("shockwave")
 		end
+	elseif args.spellId == 415253 then
+		if self:AntiSpam(3, 2) then
+			specWarnFungalBreath:Show()
+			specWarnFungalBreath:Play("shockwave")
+		end
 	end
 end
 
@@ -101,13 +118,13 @@ function mod:SPELL_CAST_SUCCESS(args)
 end
 --]]
 
---[[
 function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 1098 then
-
+	if args.spellId == 424614 and args:IsDestTypePlayer() then
+		if args:IsPlayer() or self:CheckDispelFilter("poison") then
+			warnDebilitatingVenom:Show(args.destName)
+		end
 	end
 end
---]]
 
 --[[
 function mod:SPELL_AURA_REMOVED(args)
@@ -133,5 +150,7 @@ function mod:UNIT_DIED(args)
 		timerWebbedAegisCD:Stop(args.destGUID)
     elseif cid == 223541 then--Stolen Loader
         timerLavablastCD:Stop(args.destGUID)
+	elseif cid == 207460 then--Fungarian Flinger
+		timerRotWaveVolleyCD:Stop(args.destGUID)
 	end
 end
