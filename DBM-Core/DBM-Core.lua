4ac886b8d9054227023603e24f939fc26a1deec5
MysticalOS <mysticalosx@gmail.com>
2024-05-27 21:35:36 -0400

    bump alpha

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -81,7 +81,7 @@ local fakeBWVersion, fakeBWHash = 330, "8c25119"--330.1
 local bwVersionResponseString = "V^%d^%s"
 local PForceDisable
 -- The string that is shown as version
-DBM.DisplayVersion = "10.2.44"--Core version
+DBM.DisplayVersion = "10.2.45 alpha"--Core version
 DBM.classicSubVersion = 0
 DBM.ReleaseRevision = releaseDate(2024, 5, 27) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 PForceDisable = private.isCata and 11 or 10--When this is incremented, trigger force disable regardless of major patch
