1eca7b0c67bb39a672bbdf34e9eab46615d5034f
Paul Emmerich <tandanu@deadlybossmods.com>
2024-06-15 00:15:26 +0200

    Minor test updates (#1117)

--- DBM-Test/Tools/ParseTranscriptor.lua
+++ DBM-Test/Tools/ParseTranscriptor.lua
@@ -38,21 +38,44 @@ end
 
 local playerName = args.player
 
-local function loadTranscriptorDB(fileName)
+local function loadTranscriptorLuaString(code)
 	local env = {}
 	if _VERSION == "Lua 5.1" then
 		print("Lua 5.1 can choke on huge Transcriptor logs, try using a newer version of Lua if you get errors about the constant limit")
-		local f, err = loadfile(fileName)
+		local f, err = loadstring(code)
 		if not f then error(err) end
 		setfenv(f, env)
 		f()
 	else
-		---@diagnostic disable-next-line: redundant-parameter
-		local f, err = loadfile(fileName, nil, env)
+		---@diagnostic disable-next-line: redundant-parameter -- project setup as Lua 5.1 because I want to stay 5.1 compatible
+		local f, err = load(code, nil, nil, env)
 		if not f then error(err) end
 		f()
 	end
 	return env.TranscriptDB
+end
+
+local function jsonToLua(json)
+	-- The secret TWW logs are split with some weird tool that outputs json
+	-- So far this code doesn't have any dependencies, I don't feel like pulling in one just to read json, so this hack is the best I can do
+	local lines = {}
+	for line in json:gmatch("([^\n]*)\n?") do
+		line = line:gsub("^(%s*)\"([^\"]+)\": [%[{]%s*$", "%1[\"%2\"] = {")
+		line = line:gsub("^(%s*)\"([^\"]+)\":", "%1[\"%2\"] = ")
+		line = line:gsub("^(%s*)],?(%s*)$", "%1},%2")
+		lines[#lines + 1] = line
+	end
+	return "TranscriptDB = {jsonLog = " .. table.concat(lines, "\n") .. "}"
+end
+
+local function loadTranscriptorDB(fileName)
+	local file, err = io.open(fileName, "rb")
+	if not file then error(err) end
+	local code = file:read("*a")
+	if fileName:match(".json$") then
+		code = jsonToLua(code)
+	end
+	return loadTranscriptorLuaString(code)
 end
 
 local transcriptorDB = loadTranscriptorDB(args.transcriptor)
@@ -111,7 +134,7 @@ if not firstLog or not lastLog then
 	firstLog = encounterStarts[1].offset
 	lastLog = encounterEnds[1].offset
 	-- BOSS_KILL often triggers after ENCOUNTER_END, we want to include both to test that we don't trigger end multiple times
-	local lastBossKill = bossKills[#bossKills].offset
+	local lastBossKill = #bossKills > 0 and bossKills[#bossKills].offset
 	if lastBossKill and lastBossKill < lastLog + 50 then
 		lastLog = math.max(lastLog, lastBossKill)
 	end
@@ -220,6 +243,7 @@ local function reconstructFlags(name, isPlayer, isPet, isNpc)
 end
 
 local flagWarningShown
+local seenFriendlyCids = {}
 
 local function transcribeCleu(rawParams)
 	local params = {}
@@ -293,6 +317,13 @@ local function transcribeCleu(rawParams)
 	if (event:match("_ENERGIZE$") or event:match("_HEAL$")) and destIsPlayerOrPet then
 		return
 	end
+	if event:match("_HEAL$") and srcIsPlayer and destIsNpc then -- Likely healing summons, opportunity to learn summon creature IDs not yet ignored
+		if destCid then
+			seenFriendlyCids[destCid] = destName
+		end
+		return
+	end
+
 	if (event:match("^SPELL_CAST") or event == "SPELL_EXTRA_ATTACKS") and srcIsPlayerOrPet then
 		return
 	end
@@ -333,7 +364,7 @@ local function transcribeCleu(rawParams)
 end
 
 local function transcribeEvent(event, params)
-	if event:match("^DBM_") or event:match("^NAME_PLATE_UNIT_") then
+	if event:match("^DBM_") or event:match("^NAME_PLATE_UNIT_") or event:match("BigWigs_") or event == "Echo_Log" then
 		return
 	end
 	if event:match("^UNIT_SPELL") then
@@ -439,4 +470,11 @@ if args.noheader then
 	print(generateLogOnly())
 else
 	print(generateTest())
+end
+
+if args["show-ignore-candidates"] then
+	print("Potentially ignoreable creature IDs (healed by players)")
+	for cid, name in pairs(seenFriendlyCids) do
+		print(cid .. ", -- " .. name)
+	end
 end
--- DBM-Test/Tools/Transcriptor-Filter.lua
+++ DBM-Test/Tools/Transcriptor-Filter.lua
@@ -9,18 +9,20 @@ local function set(tbl)
 end
 
 -- Very incomplete list of spammy events we don't care about.
--- Quick way to find IDs to add here:
--- lua ParseTranscriptor.lua --transcriptor log.lua | grep "SPELL_CAST" | cut -d, -f 6-8 | sort | uniq -c | sort -k1 -g -r
--- lua ParseTranscriptor.lua --transcriptor log.lua | grep "SPELL_AURA" | cut -d, -f 14-16 | sort | uniq -c | sort -k1 -g -r
--- lua ParseTranscriptor.lua --transcriptor log.lua | grep "UNIT_DESTROYED" | cut -d, -f 8-9 | sort | uniq -c | sort -k1 -g -r
+-- Quick way to find potential IDs to add here:
+-- lua ParseTranscriptor.lua --transcriptor log.lua --show-ignore-candidates
+-- lua ParseTranscriptor.lua --transcriptor log.lua | grep "SPELL_CAST" | cut -d, -f 4-6 | sort | uniq -c | sort -k1 -g -r
+-- lua ParseTranscriptor.lua --transcriptor log.lua | grep "SPELL_AURA" | cut -d, -f 12-14 | sort | uniq -c | sort -k1 -g -r
+-- lua ParseTranscriptor.lua --transcriptor log.lua | grep "UNIT_DESTROYED" | cut -d, -f 6-7 | sort | uniq -c | sort -k1 -g -r
 
 filter.ignoredSpellIds = set{
 	448995, -- Rune Scrying, barriers in SoD Sunken Temple
 	401962, -- SW: D
+	458016, -- Oversized Totems
 }
 
 filter.ignoredCreatureIds = set{
-	3904, 7400, -- Searing Totem
+	2523, 3904, 7400, -- Searing Totem
 	7465, -- Magma Totem
 	7844, -- Fire Nova Totem
 	7468, -- Nature Resistance Totem
@@ -32,13 +34,39 @@ filter.ignoredCreatureIds = set{
 	11100, -- Mana Tide Totem
 	3908, -- Healing Stream Totem
 	7415, -- Mana Spring Totem
+	225409, -- Surging Totem
 	202079, -- Shadowfiend
 	202390, 202391, 202392, -- Homunculus
+	224466, -- Voidwraith
+	217429, -- Overfiend
 	202387, -- Eye of the void
 	164874, -- Immolation Trap
 	164879, -- Explosive Trap
 	164639, -- Frost Trap
 	164876, -- Freezing Trap
+	69792, -- Earth Spirit
+	95072, -- Greater Earth Elemental
+	31216, -- Mirror Image
+	98035, -- Dreadstalker
+	100820, -- Spirit Wolf
+	224764, -- Brightstone
+	224756, -- Boulderbane
+	165374, -- Yu'lon
+	17252, -- Felguard
+	89, -- Infernal
+	103673, -- Darkglare
+	63508, -- Xuen
+	228108, -- Beast
+	226269, -- Charhound
+	103822, -- Treant
+	69791, -- Fire Spirit
+	416, -- Imp
+	55659, 143622, -- Wild Imp
+	135002, -- Demonic Tyrant
+	192337, -- Void Tendril
+	17252, -- Felguard
+	1863, -- Succubus
+	198489, -- Denizen of the Dream
 }
 
 
