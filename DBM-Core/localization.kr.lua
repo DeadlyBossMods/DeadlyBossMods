9a30993b12813a2d111bafe0645e2c0e57f86629
Elnarfim <37562642+Elnarfim@users.noreply.github.com>
2024-06-14 17:17:14 +0900

    Update koKR (#1113)

--- DBM-Core/localization.kr.lua
+++ DBM-Core/localization.kr.lua
@@ -279,6 +279,7 @@ L.TIMER_USAGE	= {
 }
 
 L.ERROR_NO_PERMISSION				= "이 명령어를 실행하기 위한 권한을 가지고 있지 않습니다."
+L.ERROR_NO_PERMISSION_COMBAT			= "보스 전투가 진행중인 동안에는 풀링/휴식 타이머를 보낼 수 없습니다"
 L.PULL_TIME_TOO_SHORT					= "풀링 타이머는 3초 이상으로 설정해야 합니다."
 L.PULL_TIME_TOO_LONG							= "풀링 타이머는 60초 이상 설정할 수 없습니다."
 
