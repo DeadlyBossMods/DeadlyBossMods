12422cbc2aaee77f51d91b7c02eea5de453d7ff1
MysticalOS <mysticalosx@gmail.com>
2024-06-22 16:30:12 -0400

    prep new release

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -77,13 +77,13 @@ local DBM = private:GetPrototype("DBM")
 _G.DBM = DBM
 DBM.Revision = parseCurseDate("@project-date-integer@")
 
-local fakeBWVersion, fakeBWHash = 330, "8c25119"--330.1
+local fakeBWVersion, fakeBWHash = 337, "848363e"--337.4
 local bwVersionResponseString = "V^%d^%s"
 local PForceDisable
 -- The string that is shown as version
-DBM.DisplayVersion = "10.2.49 alpha"--Core version
+DBM.DisplayVersion = "10.2.49"--Core version
 DBM.classicSubVersion = 0
-DBM.ReleaseRevision = releaseDate(2024, 6, 14) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+DBM.ReleaseRevision = releaseDate(2024, 6, 22) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 PForceDisable = 12--When this is incremented, trigger force disable regardless of major patch
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
