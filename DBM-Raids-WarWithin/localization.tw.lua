a9aa5a8fb3ec05f3badab6a9c0dea2c8b7b1068e
BlueNightSky <afan1128@hotmail.com>
2024-07-17 08:48:42 +0800

    Create localization.tw.lua (#1146)

---
+++ DBM-Raids-WarWithin/localization.tw.lua
@@ -0,0 +1,64 @@
+if GetLocale() ~= "zhTW" then return end
+local L
+
+----<<<<奈幽巴宮殿>>>>----
+---------------------------
+--  Ulgrax the Devourer (2902) --
+---------------------------
+--L= DBM:GetModLocalization(2607)
+
+--L:SetWarningLocalization({
+--})
+--
+--L:SetTimerLocalization{
+--}
+--
+--L:SetOptionLocalization({
+--})
+--
+--L:SetMiscLocalization({
+--})
+
+---------------------------
+--  The Bloodbound Horror (2917) --
+---------------------------
+--L= DBM:GetModLocalization(2611)
+
+---------------------------
+--  Sikran, Captain of the Sureki (2898) --
+---------------------------
+--L= DBM:GetModLocalization(2599)
+
+---------------------------
+--  Rasha'nan (2918) --
+---------------------------
+--L= DBM:GetModLocalization(2609)
+
+---------------------------
+--  Eggtender Ovi'nax (2919) --
+---------------------------
+--L= DBM:GetModLocalization(2612)
+
+---------------------------
+--  Nexus-Princess Ky'veza(2920) --
+---------------------------
+--L= DBM:GetModLocalization(2601)
+
+---------------------------
+--  The Silken Court (2921) --
+---------------------------
+--L= DBM:GetModLocalization(2608)
+
+---------------------------
+--  Queen Ansurek (2922) --
+---------------------------
+--L= DBM:GetModLocalization(2602)
+
+-------------
+--  Trash  --
+-------------
+L = DBM:GetModLocalization("NerubarPalaceTrash")
+
+L:SetGeneralLocalization({
+	name =	"奈幽巴宮殿小怪"
+})
