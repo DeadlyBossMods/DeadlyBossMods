a8f87ec23de04f3b6a6ea87d9197063d5a38f18c
Paul Emmerich <tandanu@deadlybossmods.com>
2024-08-25 12:43:39 +0200

    Tests: Pass the reporter object to the TestStop callback instead of the report itself
    I initially didn't do this because I didn't want callbacks to potentially mess with the report, but it seems useful, especially as we might add different report formats

--- DBM-Test/Public/Callbacks.lua
+++ DBM-Test/Public/Callbacks.lua
@@ -48,7 +48,7 @@ local Start = {
 local Stop = {
 	Name		= nil, ---@type string
 	-- Will be nil if the test got stopped by the user
-	Report		= nil, ---@type string?
+	Reporter	= nil, ---@type DBMTestReporterPublic?
 	-- Early test cancelation by the user
 	Canceled	= nil, ---@type boolean
 }
@@ -87,3 +87,23 @@ local Timewarper = {}
 -- Register a frame with an OnUpdate handler for timewarping
 ---@param frame Frame
 function Timewarper:RegisterFrame(frame) end
+
+---@class DBMTestReporterPublic
+local TestReporter = {}
+
+---@return "Success"|"Failure"
+function TestReporter:GetResult() end
+
+-- True if the mod threw any errors during the test, also returns true for errors that were allowed to propagate during the test run.
+---@return boolean
+function TestReporter:HasErrors() end
+
+-- Pass on any errors that happened to the default error handler.
+function TestReporter:ReportErrors() end
+
+-- Generate a neatly formatted test report.
+---@return string
+function TestReporter:Report() end
+
+-- Show the report as created by :Report() in a popup.
+function TestReporter:ShowReport() end
