87edb9c4d4fcf6a3717edcf44a956550f2331971
MysticalOS <mysticalosx@gmail.com>
2024-09-12 03:06:41 -0400

    Preliminary warnings for notable trash abilities in Nerubar Palace

--- DBM-Raids-WarWithin/NerubarPalace/NerubarPalaceTrash.lua
+++ DBM-Raids-WarWithin/NerubarPalace/NerubarPalaceTrash.lua
@@ -6,46 +6,85 @@ mod:SetRevision("@file-date-integer@")
 mod.isTrashMod = true
 
 mod:RegisterEvents(
---	"SPELL_CAST_START",
---	"SPELL_AURA_APPLIED",
+	"SPELL_CAST_START 439873 459952 463104 441747 443138 436679 440184 441097 463176",
+	"SPELL_AURA_APPLIED 445553",
 --	"SPELL_AURA_APPLIED_DOSE",
 --	"SPELL_AURA_REMOVED",
---	"CHAT_MSG_MONSTER_YELL"
+	"UNIT_DIED"
 )
 
+--TODO, add https://www.wowhead.com/spell=446760/slobbering-grasp in some capacity?
 --local warnShadowflameBomb					= mod:NewTargetAnnounce(425300, 3)
 
---local specWarnShadowflameBomb				= mod:NewSpecialWarningMoveAway(425300, nil, nil, nil, 1, 2)
+local specWarnStagFlip						= mod:NewSpecialWarningDefensive(463176, nil, nil, nil, 3, 2)
+local specWarnFixate						= mod:NewSpecialWarningYou(445553, nil, nil, nil, 1, 2)
+local specWarnImpale						= mod:NewSpecialWarningDodge(459952, nil, nil, nil, 2, 2)
+local specWarnBlackCleave					= mod:NewSpecialWarningDodge(440184, nil, nil, nil, 2, 2)
+local specWarnHeavingRetch					= mod:NewSpecialWarningDodge(441097, nil, nil, nil, 2, 2)
 --local yellShadowflameBomb					= mod:NewYell(425300)
 --local yellShadowflameBombFades			= mod:NewShortFadesYell(425300)
---local specWarnTranquility					= mod:NewSpecialWarningInterrupt(425995, "HasInterrupt", nil, nil, 1, 2)
+local specWarnDeafeningRoar					= mod:NewSpecialWarningCast(436679, "SpellCaster", nil, nil, 1, 2)
+local specWarnPsychicScream					= mod:NewSpecialWarningInterrupt(439873, "HasInterrupt", nil, nil, 1, 2)
+local specWarnVoidBoltVolley				= mod:NewSpecialWarningInterrupt(463104, "HasInterrupt", nil, nil, 1, 2)--No real cd, even when kicked, just spammed
+local specWarnDarkMending					= mod:NewSpecialWarningInterrupt(441747, "HasInterrupt", nil, nil, 1, 2)--CD possibly 14-15 seconds, but not confirmed
+local specWarnEnshroudingPulse				= mod:NewSpecialWarningInterrupt(443138, "HasInterrupt", nil, nil, 1, 2)
 
---local timerFeatherBombCD					= mod:NewAITimer(22.9, 428765, nil, nil, nil, 3)
+local timerImpaleCD							= mod:NewCDNPTimer(17, 459952, nil, nil, nil, 3)--17-20
 
 --local playerName = UnitName("player")
 
 --Antispam IDs for this mod: 1 run away, 2 dodge, 3 dispel, 4 incoming damage, 5 you/role, 6 misc
---[[
+
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 425062 and self:AntiSpam(5, 1) then
-
+	if spellId == 439873 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+		specWarnPsychicScream:Show(args.sourceName)
+		specWarnPsychicScream:Play("kickcast")
+	elseif spellId == 459952 then
+		if self:AntiSpam(3, 2) then
+			specWarnImpale:Show()
+			specWarnImpale:Play("shockwave")
+		end
+		timerImpaleCD:Start(nil, args.sourceGUID)
+	elseif spellId == 440184 then
+		if self:AntiSpam(3, 2) then
+			specWarnBlackCleave:Show()
+			specWarnBlackCleave:Play("shockwave")
+		end
+	elseif spellId == 441097 then
+		if self:AntiSpam(3, 2) then
+			specWarnHeavingRetch:Show()
+			specWarnHeavingRetch:Play("shockwave")
+		end
+	elseif spellId == 463104 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+		specWarnVoidBoltVolley:Show(args.sourceName)
+		specWarnVoidBoltVolley:Play("kickcast")
+	elseif spellId == 441747 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+		specWarnDarkMending:Show(args.sourceName)
+		specWarnDarkMending:Play("kickcast")
+	elseif spellId == 443138 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+		specWarnEnshroudingPulse:Show(args.sourceName)
+		specWarnEnshroudingPulse:Play("kickcast")
+	elseif spellId == 436679 then
+		specWarnDeafeningRoar:Show()
+		specWarnDeafeningRoar:Play("stopcast")
+	elseif spellId == 463176 then
+		if self:IsTanking("player", nil, nil, true, args.sourceGUID) then
+			specWarnStagFlip:Show()
+			specWarnStagFlip:Play("carefly")
+		end
 	end
 end
---]]
 
---[[
 function mod:SPELL_AURA_APPLIED(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
-	if spellId == 428765 then
-
---	elseif spellId == 425381 and self:CheckInterruptFilter(args.sourceGUID, false, true) then
-
+	if spellId == 445553 and args:IsPlayer() and self:AntiSpam(3, 1) then
+		specWarnFixate:Show()
+		specWarnFixate:Play("targetyou")
 	end
 end
 --mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
---]]
 
 --[[
 function mod:SPELL_AURA_REMOVED(args)
@@ -55,3 +94,10 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end
 --]]
+
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 222305 then--Palace Guardian
+		timerImpaleCD:Stop(args.destGUID)
+	end
+end
