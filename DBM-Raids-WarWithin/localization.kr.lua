3fa28ab8c8b7a57d422e87864f2078733d419029
Elnarfim <37562642+Elnarfim@users.noreply.github.com>
2024-09-20 15:54:00 +0900

    Update koKR (#1249)

--- DBM-Raids-WarWithin/localization.kr.lua
+++ DBM-Raids-WarWithin/localization.kr.lua
@@ -43,7 +43,7 @@ L:SetOptionLocalization({
 	MatchBW				= "|TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:16:32:16:32|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:48:64:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:32:48:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:32:48:16:32|t 징표를 근딜 > 원거리 > 힐러순으로 (Bigwigs 작동 방식에 맞춤)",--Default
 	UseAllAscending		= "|TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:0:16:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:16:32:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:32:48:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:48:64:0:16|t, 기타 징표를 근딜 > 원거리 > 힐러순으로",
 	DisableIconsForRaid	= "대상에 징표를 설정하지 않고 징표 없는 말풍선 알림만 표시",
-	DisableAllForRaid	= "대상에 징표 설정과 말풍선 알림 모두 끄기"
+	DisableAllForRaid	= "징표 설정과 말풍선 알림 모두 끄기"
 })
 
 ---------------------------
@@ -59,7 +59,15 @@ L:SetOptionLocalization({
 ---------------------------
 --  Queen Ansurek (2922) --
 ---------------------------
---L= DBM:GetModLocalization(2602)
+L= DBM:GetModLocalization(2602)
+
+L:SetOptionLocalization({
+	ToxinBehavior		= "반응성 독소 작동 방식 설정 (공대장이면 이 설정을 다른 사람의 설정보다 우선 적용)",
+	MatchBW				= "|TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:16:32:16:32|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:48:64:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:32:48:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:32:48:16:32|t 징표를 근딜 > 원거리 > 힐러순으로 (Bigwigs 작동 방식에 맞춤)",--Default
+	UseAllAscending		= "|TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:0:16:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:16:32:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:32:48:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:48:64:0:16|t, 기타 징표를 근딜 > 원거리 > 힐러순으로",
+	DisableIconsForRaid	= "대상에 징표를 설정하지 않고 징표 없는 말풍선 알림만 표시",
+	DisableAllForRaid	= "징표 설정과 말풍선 알림 모두 끄기"
+})
 
 -------------
 --  Trash  --
