885c235abdffe730cced2d02c2bece036f50b2c6
BlueNightSky <afan1128@hotmail.com>
2024-09-25 09:02:41 +0800

    Update localization.tw.lua (#1272)

--- DBM-Raids-WarWithin/localization.tw.lua
+++ DBM-Raids-WarWithin/localization.tw.lua
@@ -32,20 +32,21 @@ local L
 ---------------------------
 --  Rasha'nan (2918) --
 ---------------------------
+--L= DBM:GetModLocalization(2609)
+
+---------------------------
+--  Bloodtwister Ovi'nax (2919) --
+---------------------------
 L= DBM:GetModLocalization(2612)
 
 L:SetOptionLocalization({
 	EggBreakerBehavior	= "設定破蛋的行為 (如果您是團隊的領隊，將覆蓋每個人的設置)",
-	MatchBW				= "|TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:16:32:16:32|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:48:64:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:32:48:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:32:48:16:32|t 使用近戰 > 遠程 > 治療者 的優先級 (匹配 Bigwigs 的行為)",--Default
+	MatchBW				= DBM_COMMON_L.SQUARE_ICON_SMALL ..", ".. DBM_COMMON_L.TRIANGLE_ICON_SMALL ..", ".. DBM_COMMON_L.DIAMOND_ICON_SMALL ..", ".. DBM_COMMON_L.CROSS_ICON_SMALL .. " (所有難度) 使用近戰 > 遠程 > 治療者 的優先級 (匹配 Bigwigs 的行為)",--Default
+	MatchEW				= DBM_COMMON_L.SQUARE_ICON_SMALL ..", ".. DBM_COMMON_L.TRIANGLE_ICON_SMALL ..", ".. DBM_COMMON_L.DIAMOND_ICON_SMALL ..", ".. DBM_COMMON_L.CROSS_ICON_SMALL .. " (非傳奇) ".. DBM_COMMON_L.SQUARE_ICON_SMALL ..", ".. DBM_COMMON_L.TRIANGLE_ICON_SMALL ..", ".. DBM_COMMON_L.STAR_ICON_SMALL ..", ".. DBM_COMMON_L.CIRCLE_ICON_SMALL .. " (傳奇) 使用近戰 > 遠程 > 治療者 的優先級 (匹配 Echowigs 的行為)",
 	UseAllAscending		= "|TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:0:16:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:16:32:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:32:48:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:48:64:0:16|t, 其他使用近戰 > 遠程 > 治療者 的優先級",
 	DisableIconsForRaid	= "停用設置標記並顯示沒有標記的大喊",
 	DisableAllForRaid	= "完全停用設置標記和大喊"
 })
-
----------------------------
---  Eggtender Ovi'nax (2919) --
----------------------------
---L= DBM:GetModLocalization(2612)
 
 ---------------------------
 --  Nexus-Princess Ky'veza(2920) --
@@ -55,12 +56,25 @@ L:SetOptionLocalization({
 ---------------------------
 --  The Silken Court (2921) --
 ---------------------------
---L= DBM:GetModLocalization(2608)
+L= DBM:GetModLocalization(2608)
+
+L:SetMiscLocalization({
+	Red		= " [紅]",--Skeinspinner Takazj
+	Blue	= " [藍]"--Anub'arash
+})
 
 ---------------------------
 --  Queen Ansurek (2922) --
 ---------------------------
---L= DBM:GetModLocalization(2602)
+L= DBM:GetModLocalization(2602)
+
+L:SetOptionLocalization({
+	ToxinBehavior		= "設定反應泡沫的行為 (如果您是團隊的領隊，將覆蓋每個人的設置))",
+	MatchBW				= DBM_COMMON_L.SQUARE_ICON_SMALL ..", ".. DBM_COMMON_L.TRIANGLE_ICON_SMALL ..", ".. DBM_COMMON_L.DIAMOND_ICON_SMALL ..", ".. DBM_COMMON_L.STAR_ICON_SMALL ..", ".. DBM_COMMON_L.CIRCLE_ICON_SMALL .. "(所有難度) 使用近戰 > 遠程 > 治療者 的優先級 (匹配 Bigwigs 的行為)",--Default
+	UseAllAscending		= DBM_COMMON_L.STAR_ICON_SMALL ..", ".. DBM_COMMON_L.CIRCLE_ICON_SMALL ..", ".. DBM_COMMON_L.DIAMOND_ICON_SMALL ..", ".. DBM_COMMON_L.TRIANGLE_ICON_SMALL ..", ".. DBM_COMMON_L.MOON_ICON_SMALL .. " (所有難度) 使用近戰 > 遠程 > 治療者 的優先級",
+	DisableIconsForRaid	= "停用設置標記並顯示沒有標記的大喊",
+	DisableAllForRaid	= "完全停用設置標記和大喊"
+})
 
 -------------
 --  Trash  --
