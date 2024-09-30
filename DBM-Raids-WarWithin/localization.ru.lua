72709a52eabe1e77d652083ddb040eb910cb6d35
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2024-09-30 13:30:59 +0500

    Update localization.ru.lua

--- DBM-Raids-WarWithin/localization.ru.lua
+++ DBM-Raids-WarWithin/localization.ru.lua
@@ -34,9 +34,18 @@ local L
 --L= DBM:GetModLocalization(2609)
 
 ---------------------------
---  Eggtender Ovi'nax (2919) --
+--  Bloodtwister Ovi'nax (2919) --
 ---------------------------
---L= DBM:GetModLocalization(2612)
+L= DBM:GetModLocalization(2612)
+
+L:SetOptionLocalization({
+	EggBreakerBehavior	= "Установить режим разбивания яиц (перезапишет настройки всех остальных, если Вы являетесь лидером рейда)",
+	MatchBW				= DBM_COMMON_L.SQUARE_ICON_SMALL ..", ".. DBM_COMMON_L.TRIANGLE_ICON_SMALL ..", ".. DBM_COMMON_L.DIAMOND_ICON_SMALL ..", ".. DBM_COMMON_L.CROSS_ICON_SMALL .. " (все сложности) с приоритетом ближнего боя > дальнего боя > целителя (соответствует режиму Bigwigs)",--По умолчанию
+	MatchEW				= DBM_COMMON_L.SQUARE_ICON_SMALL ..", ".. DBM_COMMON_L.TRIANGLE_ICON_SMALL ..", ".. DBM_COMMON_L.DIAMOND_ICON_SMALL ..", ".. DBM_COMMON_L.CROSS_ICON_SMALL .. " (НЕ эпохальная сложность) ".. DBM_COMMON_L.SQUARE_ICON_SMALL ..", ".. DBM_COMMON_L.TRIANGLE_ICON_SMALL ..", ".. DBM_COMMON_L.STAR_ICON_SMALL ..", ".. DBM_COMMON_L.CIRCLE_ICON_SMALL .. " (эпохальная сложность) с приоритетом ближнего боя > дальнего боя > целителя (соответствует режиму Echowigs)",
+	UseAllAscending		= "|TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:0:16:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:16:32:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:32:48:0:16|t, |TInterface\\TargetingFrame\\UI-RaidTargetingIcons.blp:13:13:0:0:64:64:48:64:0:16|t и т.д. с приоритетом ближнего боя > дальнего боя > целителя",
+	DisableIconsForRaid	= "Отключить иконки и показывать крики без иконок",
+	DisableAllForRaid	= "Полностью отключить иконки и крики"
+})
 
 ---------------------------
 --  Nexus-Princess Ky'veza(2920) --
@@ -46,12 +55,25 @@ local L
 ---------------------------
 --  The Silken Court (2921) --
 ---------------------------
---L= DBM:GetModLocalization(2608)
+L= DBM:GetModLocalization(2608)
+
+L:SetMiscLocalization({
+	Red		= " [R-Таказдж]",--Нитемот Таказдж
+	Blue	= " [B-Анубараш]"--Ануб'араш
+})
 
 ---------------------------
 --  Queen Ansurek (2922) --
 ---------------------------
---L= DBM:GetModLocalization(2602)
+L= DBM:GetModLocalization(2602)
+
+L:SetOptionLocalization({
+	ToxinBehavior		= "Установить режим Реактивного токсина (перезапишет настройки всех остальных, если Вы лидер рейда)",
+	MatchBW				= DBM_COMMON_L.SQUARE_ICON_SMALL ..", ".. DBM_COMMON_L.TRIANGLE_ICON_SMALL ..", ".. DBM_COMMON_L.DIAMOND_ICON_SMALL ..", ".. DBM_COMMON_L.STAR_ICON_SMALL ..", ".. DBM_COMMON_L.CIRCLE_ICON_SMALL .. " (все сложности) с приоритетом ближнего боя > дальнего боя > целителя (соответствует режиму Bigwigs)",--По умолчанию
+	UseAllAscending		= DBM_COMMON_L.STAR_ICON_SMALL ..", ".. DBM_COMMON_L.CIRCLE_ICON_SMALL ..", ".. DBM_COMMON_L.DIAMOND_ICON_SMALL ..", ".. DBM_COMMON_L.TRIANGLE_ICON_SMALL ..", ".. DBM_COMMON_L.MOON_ICON_SMALL .. " (все сложности) с приоритетом ближнего боя > дальнего боя > целителя",
+	DisableIconsForRaid	= "Отключить иконки и показывать крики без иконок",
+	DisableAllForRaid	= "Полностью отключить иконки и крики"
+})
 
 -------------
 --  Trash  --
