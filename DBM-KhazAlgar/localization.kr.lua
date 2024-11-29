f5879d19032eb41155d14c65a6b8fec3e8597363
Elnarfim <37562642+Elnarfim@users.noreply.github.com>
2024-11-30 01:39:40 +0900

    Update koKR

--- DBM-KhazAlgar/localization.kr.lua
+++ DBM-KhazAlgar/localization.kr.lua
@@ -2,9 +2,27 @@ if GetLocale() ~= "koKR" then return end
 local L
 
 -----------------------
+-- Kordac, the Dormant Protector --
+-----------------------
+L= DBM:GetModLocalization(2637)
+
+L:SetMiscLocalization({
+	Win	= "심각... 상황... 비상..."
+})
+
+-----------------------
 -- Aggregation of Horrors --
 -----------------------
 --L= DBM:GetModLocalization(2635)
+
+--L:SetMiscLocalization({
+--	Pull	= ""
+--})
+
+-----------------------
+-- Shurrai, Atrocity of the Undersea --
+-----------------------
+--L= DBM:GetModLocalization(2636)
 
 --L:SetMiscLocalization({
 --	Pull	= ""
