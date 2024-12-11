445580d2632824a399a265cbe46d3b197388f5fd
MysticalOS <mysticalosx@gmail.com>
2024-12-10 19:38:29 -0500

    bump alpha

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -81,7 +81,7 @@ DBM.TaintedByTests = false -- Tests may mess with some internal state, you proba
 local fakeBWVersion, fakeBWHash = 368, "fc06f51"--368.0
 local PForceDisable
 -- The string that is shown as version
-DBM.DisplayVersion = "11.0.35"--Core version
+DBM.DisplayVersion = "11.0.36 alpha"--Core version
 DBM.classicSubVersion = 0
 DBM.ReleaseRevision = releaseDate(2024, 12, 10) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 PForceDisable = 15--When this is incremented, trigger force disable regardless of major patch
