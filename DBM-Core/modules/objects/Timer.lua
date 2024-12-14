06c7dcb59bcb8238c492035277a33151b223b861
MysticalOS <mysticalosx@gmail.com>
2024-12-14 00:04:00 -0500

    auto set "keep" on variance timers in debugmode

--- DBM-Core/modules/objects/Timer.lua
+++ DBM-Core/modules/objects/Timer.lua
@@ -207,6 +207,9 @@ function timerPrototype:Start(timer, ...)
 		hasVariance = true
 		timerStringWithVariance = timer -- cache timer string
 		timer, minTimer = parseVarianceFromTimer(timer) -- use highest possible value as the actual End timer
+		if DBM.Options.DebugMode then
+			self.keep = true -- keep variance timers for debug purposes
+		end
 	end
 	if timer and type(timer) ~= "number" then
 		return self:Start(nil, timer, ...) -- first argument is optional!
