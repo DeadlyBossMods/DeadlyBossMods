690e794f69e3c7fe97c196ef43d773ebf605fea0
MysticalOS <mysticalosx@gmail.com>
2025-03-17 19:07:52 -0400

    Fix a bug causing bad initials stage numbers

--- DBM-Raids-WarWithin/Undermine/ChromeKingGallywix.lua
+++ DBM-Raids-WarWithin/Undermine/ChromeKingGallywix.lua
@@ -384,7 +384,6 @@ local allTimers = {
 }
 
 function mod:OnCombatStart(delay)
-	self:SetStage(1)
 	table.wipe(castsPerGUID)
 	table.wipe(addUsedMarks)
 	self.vb.canisterCount = 0
@@ -404,6 +403,8 @@ function mod:OnCombatStart(delay)
 	if self:IsMythic() then
 		savedDifficulty = "mythic"
 		self:SetStage(0.5)
+	else
+		self:SetStage(1)
 	end
 	if not self:IsStory() and not self:IsMythic() then
 		if self:IsHeroic() then
