6a2dad1ea480c6cab10f12080c4249d337956276
MysticalOS <mysticalosx@gmail.com>
2025-04-04 08:12:20 -0400

    bump alpha

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,7 +82,7 @@ DBM.TaintedByTests = false -- Tests may mess with some internal state, you proba
 local fakeBWVersion, fakeBWHash = 378, "fc82835"--378.7
 local PForceDisable
 -- The string that is shown as version
-DBM.DisplayVersion = "11.1.14"--Core version
+DBM.DisplayVersion = "11.1.15 alpha"--Core version
 DBM.classicSubVersion = 0
 DBM.dungeonSubVersion = 0
 DBM.ReleaseRevision = releaseDate(2025, 4, 4) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
