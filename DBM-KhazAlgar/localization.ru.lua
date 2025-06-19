6a12d67b60cf0ee7bbc7601144513000d8693a1c
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-06-20 01:55:20 +0500

    Update RU locales (#1675)

--- DBM-KhazAlgar/localization.ru.lua
+++ DBM-KhazAlgar/localization.ru.lua
@@ -2,6 +2,15 @@ if GetLocale() ~= "ruRU" then return end
 local L
 
 -----------------------
+-- Kordac, the Dormant Protector --
+-----------------------
+L= DBM:GetModLocalization(2637)
+
+L:SetMiscLocalization({
+	Win	= "Состоя... Крити... Трево..."
+})
+
+-----------------------
 -- Aggregation of Horrors --
 -----------------------
 --L= DBM:GetModLocalization(2635)
@@ -11,9 +20,37 @@ local L
 --})
 
 -----------------------
--- Орта, Разбитая гора --
+-- Shurrai, Atrocity of the Undersea --
+-----------------------
+--L= DBM:GetModLocalization(2636)
+
+--L:SetMiscLocalization({
+--	Pull	= ""
+--})
+
+
+-----------------------
+-- Orta, the Broken Mountain --
 -----------------------
 --L= DBM:GetModLocalization(2625)
+
+--L:SetMiscLocalization({
+--	Pull	= ""
+--})
+
+-----------------------
+-- The Gobfather --
+-----------------------
+--L= DBM:GetModLocalization(2683)
+
+--L:SetMiscLocalization({
+--	Pull	= ""
+--})
+
+-----------------------
+-- Reshanor, The Untethered --
+-----------------------
+--L= DBM:GetModLocalization(2762)
 
 --L:SetMiscLocalization({
 --	Pull	= ""
