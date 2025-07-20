2afda977c05a45759667fd33491e100d1fdd691c
MysticalOS <mysticalosx@gmail.com>
2025-07-20 18:58:37 -0400

    prep new tag and bump force update to force library updates (so users avoid deprecated errors if other addons update library first. although caveat, this means users of OTHER addons will get deprecated errors from those addons until they update. but better to be first than last)

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,11 +82,11 @@ DBM.TaintedByTests = false -- Tests may mess with some internal state, you proba
 local fakeBWVersion, fakeBWHash = 391, "1adddef"--391.1
 local PForceDisable
 -- The string that is shown as version
-DBM.DisplayVersion = "11.2.4 alpha"--Core version
+DBM.DisplayVersion = "11.2.4"--Core version
 DBM.classicSubVersion = 0
 DBM.dungeonSubVersion = 0
-DBM.ReleaseRevision = releaseDate(2025, 7, 18) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
-PForceDisable = 18--When this is incremented, trigger force disable regardless of major patch
+DBM.ReleaseRevision = releaseDate(2025, 7, 20) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+PForceDisable = 19--When this is incremented, trigger force disable regardless of major patch
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
 -- support for github downloads, which doesn't support curse keyword expansion
