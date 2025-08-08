c75cdb8b8f736f32dd72b82a1f2d34ae16207252
MysticalOS <mysticalosx@gmail.com>
2025-08-07 21:56:20 -0400

    Fix loading of reshanor and add basic alerts

--- DBM-KhazAlgar/Reshanor.lua
+++ DBM-KhazAlgar/Reshanor.lua
@@ -2,37 +2,43 @@ local mod	= DBM:NewMod(2762, "DBM-KhazAlgar", nil, 1278)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
---mod:SetCreatureID(231821)
+mod:SetCreatureID(238319)
 mod:SetEncounterID(3184)
 --mod:SetReCombatTime(30)
 mod:EnableWBEngageSync()--Enable syncing engage in outdoors
 --mod:SetHotfixNoticeRev(20240119000000)
 --mod:SetMinSyncRevision(20240119000000)
-mod:SetZone(2774)--Possibly incorrect
+mod:SetZone(2738)
 
 mod:RegisterCombat("combat")
 --mod:RegisterKill("yell", L.Win)
 
 mod:RegisterEventsInCombat(
---	"SPELL_CAST_START"
+	"SPELL_CAST_START 1237905 1237261 1237893"
 --	"SPELL_CAST_SUCCESS",
 --	"SPELL_AURA_APPLIED"
 )
 
---local warnBombfield				= mod:NewSpellAnnounce(1216505, 3)
+local warnUntetheredRetreat			= mod:NewSpellAnnounce(1237261, 3)
 
---local specWarnDeathFromAbove	= mod:NewSpecialWarningDodge(1216709, nil, nil, nil, 2, 2)
+local specWarnTwilightBreath		= mod:NewSpecialWarningDodge(1237905, nil, nil, nil, 2, 2)
+local specWarnVeilshatterRoar		= mod:NewSpecialWarningCast(1237893, "SpellCaster", nil, nil, 2, 2)
 
---local timerBombfieldCD			= mod:NewAITimer(32.7, 1216505, nil, nil, nil, 1)
+local timerTwilightBreathCD			= mod:NewAITimer(32.7, 1237905, nil, nil, nil, 3)
 
---[[
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 1216505 then
-
+	if spellId == 1237905 then
+		specWarnTwilightBreath:Show()
+		specWarnTwilightBreath:Play("breathsoon")
+		timerTwilightBreathCD:Start()
+	elseif spellId == 1237261 then
+		warnUntetheredRetreat:Show()
+	elseif spellId == 1237893 then
+		specWarnVeilshatterRoar:Show()
+		specWarnVeilshatterRoar:Play("stopcast")
 	end
 end
---]]
 
 --[[
 function mod:SPELL_CAST_SUCCESS(args)
