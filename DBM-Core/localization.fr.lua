300bdc09eeac13129c2917e1551fd0fff0a92908
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2025-09-16 08:30:58 -0700

    Update localization.fr.lua (#1762)

--- DBM-Core/localization.fr.lua
+++ DBM-Core/localization.fr.lua
@@ -296,9 +296,9 @@ L.PULL_TIME_TOO_SHORT			= "Le chronomètre de pull doit durer plus de 3 secondes
 L.PULL_TIME_TOO_LONG			= "Le chronomètre de pull ne peut pas dépasser 60 secondes."
 
 L.BREAK_USAGE				= "Les chronomètres de pause ne peuvent pas durer plus de 60 minutes. Assurez vous de mettre le temps en minutes et pas secondes."
-L.BREAK_START				= "La pause commence maintenant -- vous avez %s minute(s) !"
-L.BREAK_MIN					= "Fin de la pause dans %s minute(s) !"
-L.BREAK_SEC					= "Fin de la pause dans %s secondes !"
+L.BREAK_START				= "La pause commence maintenant -- vous avez %s !"
+L.BREAK_MIN					= "Fin de la pause dans %s |4minute:minutes !"
+L.BREAK_SEC					= "Fin de la pause dans %s |4seconde:secondes !"
 L.TIMER_BREAK				= "Pause !"
 L.ANNOUNCE_BREAK_OVER		= "La pause est terminée"
 
