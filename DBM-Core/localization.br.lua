362aa49ebafd4eec9a774229c5adef81f66f28bf
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2025-10-18 02:41:57 -0700

    Update localization.br.lua (#1780)
    * Update localization.br.lua
    
    * Update localization.br.lua
    
    * Update WORLDBUFF_STARTED localization string

--- DBM-Core/localization.br.lua
+++ DBM-Core/localization.br.lua
@@ -129,7 +129,7 @@ L.MOD_DEFAULT_LOADED				= "Foram carregadas opções padrão para esta luta."
 
 L.WORLDBOSS_ENGAGED					= "%s foi possivelmente puxado no seu reino %s por cento de vida. (Enviado por %s)"
 L.WORLDBOSS_DEFEATED				= "%s foi possivelmente derrotado no seu reino (Enviado por %s)."
-L.WORLDBUFF_STARTED					= "%s buff começou em seu reino para a facção da %s (Enviado por %s)."
+L.WORLDBUFF_STARTED					= "%s começou em seu reino para a facção da %s (Enviado por %s)."
 
 L.TIMER_FORMAT_SECS					= "%.2f |4segundo:segundos;"
 L.TIMER_FORMAT_MINS					= "%d |4minuto:minutos;"
@@ -299,8 +299,8 @@ L.PULL_TIME_TOO_LONG				= "O cronômetro de puxada não pode ser maior que que 6
 
 L.BREAK_USAGE 						= "O cronômetro de pausa não pode ser maior que 60 minutos. Certifique-se de inserir o tempo em minutos e não em segundos."
 L.BREAK_START						= "Pausa começando agora - você tem %s!"
-L.BREAK_MIN							= "Pausa encerra-se em %s |4minuto:minutos!"
-L.BREAK_SEC							= "Pausa encerra-se em %s |4segundo:segundos!"
+L.BREAK_MIN							= "Pausa encerra-se em %s |4minuto:minutos;!"
+L.BREAK_SEC							= "Pausa encerra-se em %s |4segundo:segundos;!"
 L.TIMER_BREAK						= "Pausa!"
 L.ANNOUNCE_BREAK_OVER				= "A pausa acabou"
 
