df1bff1c5fca1c4a4830f11507827e29a77ca06c
MysticalOS <mysticalosx@gmail.com>
2025-10-26 11:49:47 -0400

    Remove 11.2 toc

--- DBM-Raids-Midnight/TheDreamrift/ChimaerustheUndreamtGod.lua
+++ DBM-Raids-Midnight/TheDreamrift/ChimaerustheUndreamtGod.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(2795, "DBM-Raids-Midnight", 2, 1314)
 --local L		= mod:GetLocalizedStrings()--Nothing to localize for blank mods
 
 mod:SetRevision("@file-date-integer@")
---mod:SetCreatureID()--Yes, they're all used
+--mod:SetCreatureID()
 mod:SetEncounterID(3306)
 --mod:SetHotfixNoticeRev(20250823000000)
 --mod:SetMinSyncRevision(20250823000000)
