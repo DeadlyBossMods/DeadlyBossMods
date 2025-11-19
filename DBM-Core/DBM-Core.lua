49afb8086352f3e0f52f401ee5c6d15ac3ff53b2
MysticalOS <mysticalosx@gmail.com>
2025-11-19 15:19:13 -0500

    blizzard includes respawn timer in timeline so no need to start our own

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -5738,7 +5738,7 @@ do
 			local v = inCombat[i]
 			if not v.combatInfo then return end
 			if v.noEEDetection then return end
-			if v.respawnTime and success == 0 then--No special hacks needed for bad wrath ENCOUNTER_END. Only mods that define respawnTime have a timer, since variable per boss.
+			if not self:IsPostMidnight() and v.respawnTime and success == 0 then--No special hacks needed for bad wrath ENCOUNTER_END. Only mods that define respawnTime have a timer, since variable per boss.
 				local timerEnabled = self.Options.ShowRespawn and not self.Options.DontShowEventTimers
 				name = string.split(",", name)
 				if timerEnabled then
