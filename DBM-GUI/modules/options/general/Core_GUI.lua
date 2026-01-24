a4157534dd3bc6e99ef241c57bbec6562b5903b7
MysticalOS <mysticalosx@gmail.com>
2026-01-23 20:39:54 -0500

    auto expand options by default in midnight

--- DBM-GUI/modules/options/general/Core_GUI.lua
+++ DBM-GUI/modules/options/general/Core_GUI.lua
@@ -195,7 +195,7 @@ optionsFrame:HookScript("OnSizeChanged", function(self)
 end)
 
 local UIGroupingOptions = coreoptions:CreateArea(L.UIGroupingOptions)
-UIGroupingOptions:CreateCheckButton(L.AutoExpandSpellGroups, true, nil, "AutoExpandSpellGroups")
+UIGroupingOptions:CreateCheckButton(L.AutoExpandSpellGroups2, true, nil, "AutoExpandSpellGroups2")
 if not DBM:IsPostMidnight() then
 	UIGroupingOptions:CreateCheckButton(L.GroupOptionsExcludePrivateAura, true, nil, "GroupOptionsExcludePA")
 	UIGroupingOptions:CreateCheckButton(L.GroupOptionsExcludeIcon, true, nil, "GroupOptionsExcludeIcon")
