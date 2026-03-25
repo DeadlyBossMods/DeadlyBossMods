92d53a5a867f65f84627f02a67c01ace83684865
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2026-03-25 14:23:31 +0500

    Upd RU locale (#1984)
    * Create localization.ru.lua
    
    * Update DBM-Raids-Midnight_Mainline.toc
    
    * Update commonlocal.ru.lua

---
+++ DBM-Raids-Midnight/localization.ru.lua
@@ -0,0 +1,12 @@
+if GetLocale() ~= "ruRU" then return end
+local L
+
+---------------------------
+--  LightblindedVanguard (3180) --
+---------------------------
+L= DBM:GetModLocalization(2737)
+
+L:SetMiscLocalization({
+	JudgementShield	= "Правосудие - Щит праведника)",
+	JudgementFV		= "Правосудие - Окончательный приговор)"
+})
