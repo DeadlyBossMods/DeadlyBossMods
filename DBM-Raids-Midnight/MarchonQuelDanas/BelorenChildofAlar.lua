f0eacabf6b5aecb66d0075f20562f843aa18c7bb
MysticalOS <mysticalosx@gmail.com>
2026-03-29 03:37:22 -0400

    Update builtin voice pack sounds make March of Quel danas modules public

--- DBM-Raids-Midnight/MarchonQuelDanas/BelorenChildofAlar.lua
+++ DBM-Raids-Midnight/MarchonQuelDanas/BelorenChildofAlar.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(2739, "DBM-Raids-Midnight", 1, 1308)
 --local L		= mod:GetLocalizedStrings()--Nothing to localize for blank mods
 
 mod:SetRevision("@file-date-integer@")
---mod:SetCreatureID()--No Data Yet?
+--mod:SetCreatureID()--No Data Yet, has 4 CIDs
 mod:SetEncounterID(3182)
 --mod:SetHotfixNoticeRev(20250823000000)
 --mod:SetMinSyncRevision(20250823000000)
@@ -10,8 +10,109 @@ mod:SetZone(2913)
 
 mod:RegisterCombat("combat")
 
---mod:RegisterEventsInCombat(
+--NOTES: Light quill has encounter event if private aura gets removed. https://www.wowhead.com/spell=1241992/light-quill 384
+--https://www.wowhead.com/spell=1242091/void-quill is 385
+--Twilight Seal is a mechanic not in journal but has both private auras and encounter events 417 and 418
+--Stage 1
+--local warnVoidlightConveergence			= mod:NewCountAnnounce(1242515, 3)--Hardcode only?
 
---)
+local specWarnEmbersofBeloren			= mod:NewSpecialWarningCount(1241282, nil, nil, DBM_COMMON_L.ADDS, 1, 2)
+local specWarnRadiantEchoes				= mod:NewSpecialWarningCount(1242981, nil, nil, DBM_COMMON_L.ORBS, 2, 2)
+local specWarnGuardiansEdict			= mod:NewSpecialWarningCount(1260763, nil, nil, DBM_COMMON_L.TANKCOMBO, 1, 2)
+--local specWarnVoidlightConveergence		= mod:NewSpecialWarningCount(1242515, nil, nil, nil, 2, 2)--No PA to detect color, can only just warn to check color
+local specWarnLightFeather				= mod:NewSpecialWarningYou(1241162, nil, nil, nil, 1, 2)--Untested
+local specWarnVoidFeather				= mod:NewSpecialWarningYou(1241163, nil, nil, nil, 1, 2)--Untested
+--mod:GroupSpells(1242515, 1241162, 1241163)--Uncomment group when hardcode enables parent warning
+local specWarnDeathDrop					= mod:NewSpecialWarningCount(1246709, nil, nil, nil, 2, 2)
+--Adds
+local specWarnLightDiver				= mod:NewSpecialWarningYou(1241292, nil, nil, DBM_COMMON_L.GROUPSOAK, 1, 2)
+local specWarnVoidDiver					= mod:NewSpecialWarningYou(1241339, nil, nil, DBM_COMMON_L.GROUPSOAK, 1, 2)
 
---TODO. Not a damn thing
+local timerEmbersofBelorenCD			= mod:NewCDCountTimer(20.5, 1241282, DBM_COMMON_L.ADDS.." (%s)", nil, nil, 1)
+local timerRadiantEchoesCD				= mod:NewCDCountTimer(20.5, 1242981, DBM_COMMON_L.ORBS.." (%s)", nil, nil, 5)
+local timerGuardiansEdictCD				= mod:NewCDCountTimer(20.5, 1260763, DBM_COMMON_L.TANKCOMBO.." (%s)", "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK)
+local timerEternalBurnsCD				= mod:NewCDCountTimer(20.5, 1244344, nil, nil, nil, 3)
+local timerInfusedQuillsCD				= mod:NewCDCountTimer(20.5, 1242260, nil, nil, nil, 3)
+local timerVoidlightConvergenceCD		= mod:NewCDCountTimer(20.5, 1242515, nil, nil, nil, 2)
+local timerDeathDropCD					= mod:NewCDCountTimer(20.5, 1246709, nil, nil, nil, 6)--Stage bar, unless fight actually fires a diff stage bar then we'll use that
+local timerBerserkCD					= mod:NewBerserkTimer(600)
+
+mod:AddPrivateAuraSoundOption(1244348, true, 1244344, 1, 3, "absorbyou", 19)--Light Burn (sub spell of Eternal Burns)
+mod:AddPrivateAuraSoundOption(1266404, true, 1244344, 1, 3, "absorbyou", 19)--Void Burn (sub spell of Eternal Burns)
+mod:AddPrivateAuraSoundOption(1241992, true, 1242260, 1, 1, "lineyou", 17)--Light Quill (sub spell of Infused Quills)
+mod:AddPrivateAuraSoundOption(1242091, true, 1242260, 1, 1, "lineyou", 17)--Void Quill (sub spell of Infused Quills)
+mod:AddPrivateAuraSoundOption(1241840, true, 1241292, 1, 2, "watchfeet", 8)--Light Patch (dropped by Light Dive)
+mod:AddPrivateAuraSoundOption(1241841, true, 1241339, 1, 2, "watchfeet", 8)--Void Patch (dropped by Void Dive)
+--Stage 2
+local specWarnIncubationofFlames		= mod:NewSpecialWarningCount(1242792, nil, nil, nil, 2, 2)
+local specWarnRebirth					= mod:NewSpecialWarningCount(1241313, nil, nil, nil, 1, 2)
+
+local timerIncubationofFlamesCD			= mod:NewCDCountTimer(20.5, 1242792, nil, nil, nil, 3)--Might not even have a timer, if not kill object
+local timerRebirthCD					= mod:NewCDCountTimer(20.5, 1241313, nil, nil, nil, 6)--Iffy
+
+mod:AddPrivateAuraSoundOption(1242803, true, 1242792, 1, 2, "watchfeet", 8)--Light Flames (dropped by Incubation of Flames)
+mod:AddPrivateAuraSoundOption(1242815, true, 1242792, 1, 2, "watchfeet", 8)--Void Flames (dropped by Incubation of Flames)
+
+mod.vb.embersCount = 0
+mod.vb.echoesCount = 0
+mod.vb.edictCount = 0
+mod.vb.burnsCount = 0
+mod.vb.quillsCount = 0
+mod.vb.convergenceCount = 0
+mod.vb.deathDropCount = 0
+mod.vb.incubationCount = 0
+--local badStateDetected = false
+
+---@param self DBMMod
+local function setFallback(self)
+	--Blizz API fallbacks
+	specWarnEmbersofBeloren:SetAlert(128, "mobsoon", 2, 3)
+	timerEmbersofBelorenCD:SetTimeline(128)
+	specWarnRadiantEchoes:SetAlert(130, "orbsincoming", 19, 3)
+	timerRadiantEchoesCD:SetTimeline(130)
+	if self:IsTank() or self:IsHealer() then
+		specWarnGuardiansEdict:SetAlert(134, "tankcombo", 19, 3)
+	end
+	timerGuardiansEdictCD:SetTimeline(134)
+	timerEternalBurnsCD:SetTimeline(138)
+	timerInfusedQuillsCD:SetTimeline(161)
+	--warnVoidlightConveergence:SetAlert(218, "colorchange", 19, 3)
+	timerVoidlightConvergenceCD:SetTimeline(218)
+	specWarnDeathDrop:SetAlert(272, "justrun", 2, 3)
+	timerDeathDropCD:SetTimeline(272)
+	specWarnIncubationofFlames:SetAlert(273, "watchstep", 2, 3)
+	timerIncubationofFlamesCD:SetTimeline(273)
+	specWarnLightFeather:SetAlert(482, "lightyou", 19, 3, 0)
+	specWarnVoidFeather:SetAlert(483, "voidyou", 19, 3, 0)
+	specWarnLightDiver:SetAlert(494, "lightsoak", 19, 3, 0)
+	specWarnVoidDiver:SetAlert(495, "voidsoak", 19, 3, 0)
+	specWarnRebirth:SetAlert(497, "dpshard", 16, 3)
+	timerRebirthCD:SetTimeline(497)
+	timerBerserkCD:SetTimeline(500)
+end
+
+function mod:OnLimitedCombatStart(delay)
+	self.vb.embersCount = 0
+	self.vb.echoesCount = 0
+	self.vb.edictCount = 0
+	self.vb.burnsCount = 0
+	self.vb.quillsCount = 0
+	self.vb.convergenceCount = 0
+	self.vb.deathDropCount = 0
+	self.vb.incubationCount = 0
+	--Hardcode features first
+	--if DBM.Options.HardcodedTimer and self:IsEasy() and not badStateDetected then
+	--	self:IgnoreBlizzardAPI()
+	--	self:RegisterShortTermEvents(
+	--		"ENCOUNTER_TIMELINE_EVENT_ADDED",
+	--		"ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED"
+	--	)
+	--else
+		setFallback(self)
+	--end
+end
+
+--function mod:OnCombatEnd()
+--	self:TLCountReset()
+--	self:UnregisterShortTermEvents()
+--end
--- DBM-Raids-Midnight/MarchonQuelDanas/MidnightFalls.lua
+++ DBM-Raids-Midnight/MarchonQuelDanas/MidnightFalls.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod(2740, "DBM-Raids-Midnight", 1, 1308)
 --local L		= mod:GetLocalizedStrings()--Nothing to localize for blank mods
 
 mod:SetRevision("@file-date-integer@")
---mod:SetCreatureID()--No Data Yet?
+mod:SetCreatureID(214650)
 mod:SetEncounterID(3183)
 --mod:SetHotfixNoticeRev(20250823000000)
 --mod:SetMinSyncRevision(20250823000000)
@@ -10,8 +10,157 @@ mod:SetZone(2913)
 
 mod:RegisterCombat("combat")
 
---mod:RegisterEventsInCombat(
+--TODO, each rune might be a specific private aura. Although I suspect if we solve that, blizzard will disable PAs for it near immediately
+--TODO, appropriate glaive audio
+--TODO, midnight has an eventID but it seems a bit more passive (https://www.wowhead.com/spell=1266622/midnight 260)
+--TODO, https://www.wowhead.com/spell=1284525/galvanize has event of 632 but I'm pretty sure it's just to flag personal announce (which alreaedy has PA)
+--TODO, switch to https://www.wowhead.com/spell=1249609/dark-rune event if PAs get disabled (and they probably will be once players figure out which is which) ID 650
+--TODO, add PAs for https://www.wowhead.com/beta/spell=1286294/grim-symphony and https://www.wowhead.com/beta/spell=1284984/grim-symphony ? they seem to overlap with dark runes though
+--TODO, figure out which torchbearer is which. One is for player holding it and one is for standing near them i'm pretty sure
+--TODO, do a thorough audit of private auras, timers, and alerts and remove what isn't actually used and eliminate redundancies or fix anything missing
+local warnTotalEclipse				= mod:NewSpellAnnounce(1261871, 2)--Intermission 1.5 Start
+local warnHeavensLance				= mod:NewCountAnnounce(1267049, 2)--Stage 1 tank ability
 
---)
+local specWarnDeathsDirge			= mod:NewSpecialWarningCount(1249620, nil, nil, nil, 2, 2)
+local specWarnHeavensGlaives		= mod:NewSpecialWarningCount(1253915, nil, nil, nil, 2, 2)
+local specWarnSafeguaredPrism		= mod:NewSpecialWarningSwitchCount(1251386, nil, nil, nil, 1, 2)
+local specWarnShatteredSky			= mod:NewSpecialWarningCount(1249796, nil, nil, nil, 2, 2)
+local specWarnLightSiphon			= mod:NewSpecialWarningCount(1266897, nil, nil, nil, 2, 2)--Stage 3 ability
+local specWarnDarkConstellation		= mod:NewSpecialWarningCount(1266388, nil, nil, nil, 2, 2)--Stage 3 ability
+local specWarnDarkArchangel			= mod:NewSpecialWarningCount(1250898, nil, nil, nil, 3, 2)--Stage 3 ability
+local specWarnDeathsRequiem			= mod:NewSpecialWarningCount(1249619, nil, nil, nil, 2, 2)--Stage 3 ability
+local specWarnSeverance				= mod:NewSpecialWarningCount(1276202, nil, nil, nil, 2, 2, 4)--Stage 3 mythic ability
+local specWarnIntoDarkwell			= mod:NewSpecialWarningCount(1282047, nil, nil, nil, 2, 2)--Stage 2 Start
+local specWarnCosmicFission			= mod:NewSpecialWarningCount(1282249, nil, nil, nil, 2, 2)--Stage 2 triggered ability (not timer one)
+local specWarnCoreHarvest			= mod:NewSpecialWarningCount(1282412, nil, nil, nil, 2, 2)--Stage 2 ability
+local specWarnDarkMeltdown			= mod:NewSpecialWarningCount(1281194, nil, nil, nil, 2, 2)--Stage 2 ability
+local specWarnTerminationPrism		= mod:NewSpecialWarningSwitchCount(1284931, nil, nil, nil, 2, 2, 4)--Stage 1 Mythic version of Safeguarded Prism
+local specWarnGrimSymphony			= mod:NewSpecialWarningCount(1284980, nil, nil, nil, 2, 2, 4)--Stage 1 Mythic version of DeathsDirge
+local specWarnDarkQuasar			= mod:NewSpecialWarningCount(1279420, nil, nil, nil, 2, 2)--Stage 1 ability
 
---TODO. Not a damn thing
+local timerDeathsDirgeCD			= mod:NewCDCountTimer(20.5, 1249620, nil, nil, nil, 5)
+local timerheavensGlaivesCD			= mod:NewCDCountTimer(20.5, 1253915, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON)
+local timerSafeguaredPrismCD		= mod:NewCDCountTimer(20.5, 1251386, nil, nil, nil, 1)
+local timerShatteredSkyCD			= mod:NewCDCountTimer(20.5, 1249796, nil, nil, nil, 2)
+local timerTotalEclipseCD			= mod:NewCDTimer(60, 1261871, nil, nil, nil, 6)--Stage 1.5 intermisison start
+local timerMidnightCD				= mod:NewCDCountTimer(60, 1263514, nil, nil, nil, 3)--iffy, Stage 3
+local timerLightSiphonCD			= mod:NewCDCountTimer(20.5, 1266897, nil, nil, nil, 5)--Stage 3
+local timerDarkConstellationCD		= mod:NewCDCountTimer(20.5, 1266388, nil, nil, nil, 3)--Stage 3
+local timerDarkArchangelCD			= mod:NewCDCountTimer(20.5, 1250898, nil, nil, nil, 2)--Stage 3
+local timerDeathsRequiemCD			= mod:NewCDCountTimer(20.5, 1249619, nil, nil, nil, 3)--Stage 3
+local timerSeveranceCD				= mod:NewCDCountTimer(20.5, 1276202, nil, nil, nil, 2, nil, DBM_COMMON_L.MYTHIC_ICON)--Stage 3 mythic
+local timerHeavensLanceCD			= mod:NewCDCountTimer(20.5, 1267049, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
+local timerIntoDarkwellCD			= mod:NewCDTimer(60, 1282047, nil, nil, nil, 6)--Stage 2 Start
+local timerCoreHarvestCD			= mod:NewCDCountTimer(20.5, 1282412, nil, nil, nil, 2, nil, DBM_COMMON_L.HEALER_ICON)--Stage 2
+local timerDarkMeltdownCD			= mod:NewCDCountTimer(20.5, 1281194, nil, nil, nil, 2)--Stage 2
+local timerStarSplinterCD			= mod:NewCDCountTimer(20.5, 1282441, nil, nil, nil, 3)--Stage 1.5 intermission
+local timerGalvanizeCD				= mod:NewCDCountTimer(20.5, 1284525, nil, nil, nil, 2)--Stage 2 Core ability (so maybe no timer?)
+local timerTerminationPrismCD		= mod:NewCDCountTimer(20.5, 1284931, nil, nil, nil, 1, nil, DBM_COMMON_L.MYTHIC_ICON)--Stage 1 mythic
+local timerGrimSymphonyCD			= mod:NewCDCountTimer(20.5, 1284980, nil, nil, nil, 5, nil, DBM_COMMON_L.MYTHIC_ICON)--Stage 1 mythic version of DeathsDirge
+local timerDarkQuasarCD				= mod:NewCDCountTimer(20.5, 1279420, nil, nil, nil, 3)--Stage 1
+
+mod:AddPrivateAuraSoundOption({1249609,1249565,1249566,1273133,1249550,1249558,1249562}, true, 1249620, 1, 2, "runeyou", 19)--Dark Rune (sub spell of Death's Dirge & Death's Requiem)
+mod:AddPrivateAuraSoundOption(1263514, false, 1253915, 1, 2, "watchfeet", 8)--Midnight (could be spammy if group is poor and not using lightbearers well)
+mod:AddPrivateAuraSoundOption(1284527, true, 1284525, 1, 1, "beamyou", 19)--Galvanize
+mod:AddPrivateAuraSoundOption(1281184, true, 1284525, 1, 1, "scatter", 2)--Criticality (mythic Galvonize tertiary affect)
+mod:AddPrivateAuraSoundOption({1279512,1285510}, true, 1282441, 1, 1, "runout", 2)--Starsplinter
+mod:AddPrivateAuraSoundOption(1282470, true, 1279420, 1, 2, "watchfeet", 8)--Dark Quasar
+mod:AddPrivateAuraSoundOption(1253031, true, 1253031, 1, 1, "dawncrystal", 19)--Glimmering (Holding Dawn Crystal)
+mod:AddPrivateAuraSoundOption(1253770, true, 1250898, 1, 3, "safenow", 2)--Dawnlight barrier (needed to survive Archangel))
+mod:AddPrivateAuraSoundOption(1262055, false, 1261871, 1, 3, "absorbyou", 19)--Eclipsed (Total Eclipse debuff)
+mod:AddPrivateAuraSoundOption(1275429, true, 1276202, 1, 1, "moveright", 2)--Severance (right)
+mod:AddPrivateAuraSoundOption(1266946, true, 1276202, 1, 1, "moveleft", 2)--Severance (left?)
+--mod:AddPrivateAuraSoundOption({1266113,1266627{, true, 1266113, 1, 1, "torchyou", 2)--Torchbearer (Reundant with glimmering? or maybe buff players who get near person holding it get?)
+
+mod.vb.deathCount = 0--Used for both Dirge and requiem
+mod.vb.glaivesCount = 0
+mod.vb.prismCount = 0
+mod.vb.shatteredSkyCount = 0
+mod.vb.midnightCount = 0
+mod.vb.lightSiphonCount = 0
+mod.vb.darkConstellationCount = 0
+mod.vb.darkArchangelCount = 0
+mod.vb.severanceCount = 0
+mod.vb.heavensLanceCount = 0
+mod.vb.harvestCount = 0
+mod.vb.meltdownCount = 0
+mod.vb.starSplinterCount = 0
+mod.vb.galvanizeCount = 0
+mod.vb.terminationPrismCount = 0
+mod.vb.grimSymphonyCount = 0--Mythic version of Deaths Dirge, combine count if this is confirmed
+mod.vb.darkQuasarCount = 0
+
+---@param self DBMMod
+local function setFallback(self)
+	specWarnDeathsDirge:SetAlert(255, "runesincoming", 19, 4)
+	timerDeathsDirgeCD:SetTimeline(255)
+	specWarnHeavensGlaives:SetAlert(256, "watchstep", 2, 3)
+	timerheavensGlaivesCD:SetTimeline(256)
+	specWarnSafeguaredPrism:SetAlert(257, "targetchange", 2, 3)
+	timerSafeguaredPrismCD:SetTimeline(257)
+	specWarnShatteredSky:SetAlert(258, "aesoon", 2, 2)
+	timerShatteredSkyCD:SetTimeline(258)
+	warnTotalEclipse:SetAlert(259, "phasechange", 2, 1)
+	timerTotalEclipseCD:SetTimeline(259)
+	timerMidnightCD:SetTimeline(260)
+	specWarnLightSiphon:SetAlert(261, "lightrifts", 19, 3)
+	timerLightSiphonCD:SetTimeline(261)
+	specWarnDarkConstellation:SetAlert(262, "watchstep", 2, 3)
+	timerDarkConstellationCD:SetTimeline(262)
+	specWarnDarkArchangel:SetAlert(263, "findshield", 2, 2)
+	timerDarkArchangelCD:SetTimeline(263)
+	specWarnDeathsRequiem:SetAlert(362, "runesincoming", 19, 4)
+	timerDeathsRequiemCD:SetTimeline(362)
+	specWarnSeverance:SetAlert(363, "raidsplit", 19, 4)
+	timerSeveranceCD:SetTimeline(363)
+	if self:IsTank() then
+		warnHeavensLance:SetAlert(364, "defensive", 2, 2)
+	end
+	timerHeavensLanceCD:SetTimeline(364)
+	specWarnIntoDarkwell:SetAlert(433, "pullin", 12, 2)
+	timerIntoDarkwellCD:SetTimeline(433)
+	specWarnCosmicFission:SetAlert(434, "pullin", 12, 2, 0)--Guessed. it's not on a timer it's triggered by players hitting cores
+	specWarnCoreHarvest:SetAlert(435, "aesoon", 2, 2)
+	timerCoreHarvestCD:SetTimeline(435)
+	specWarnDarkMeltdown:SetAlert(436, "energyhigh", 2, 2)
+	timerDarkMeltdownCD:SetTimeline(436)
+	timerStarSplinterCD:SetTimeline(437)
+	timerGalvanizeCD:SetTimeline(632)
+	specWarnTerminationPrism:SetAlert(636, "targetchange", 2, 3)
+	timerTerminationPrismCD:SetTimeline(636)
+	specWarnGrimSymphony:SetAlert(644, "runesincoming", 19, 4)
+	timerGrimSymphonyCD:SetTimeline(644)
+	specWarnDarkQuasar:SetAlert(649, "watchstep", 2, 3)
+	timerDarkQuasarCD:SetTimeline(649)
+end
+
+function mod:OnLimitedCombatStart(delay)
+	self.vb.deathCount = 0
+	self.vb.glaivesCount = 0
+	self.vb.prismCount = 0
+	self.vb.shatteredSkyCount = 0
+	self.vb.midnightCount = 0
+	self.vb.lightSiphonCount = 0
+	self.vb.darkConstellationCount = 0
+	self.vb.darkArchangelCount = 0
+	self.vb.severanceCount = 0
+	self.vb.heavensLanceCount = 0
+	self.vb.harvestCount = 0
+	self.vb.meltdownCount = 0
+	self.vb.starSplinterCount = 0
+	self.vb.galvanizeCount = 0
+	self.vb.terminationPrismCount = 0
+	self.vb.grimSymphonyCount = 0
+	self.vb.darkQuasarCount = 0
+	--Hardcode features first
+	--if DBM.Options.HardcodedTimer and self:IsEasy() and not badStateDetected then
+	--	self:IgnoreBlizzardAPI()
+	--	self:RegisterShortTermEvents(
+	--		"ENCOUNTER_TIMELINE_EVENT_ADDED",
+	--		"ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED"
+	--	)
+	--else
+		setFallback(self)
+	--end
+	DBM:AddMsg("This boss was untested on beta and much is guessed based on datamined info and journal. A full complete mod will be made after release")
+end
