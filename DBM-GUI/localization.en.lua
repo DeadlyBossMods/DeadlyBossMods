5621528ba2b1ca5f6f8637dc22d8cc5d82eb1b1f
MysticalOS <mysticalosx@gmail.com>
2026-03-29 21:55:56 -0400

    Bugfixes and GUi tweaks

--- DBM-GUI/localization.en.lua
+++ DBM-GUI/localization.en.lua
@@ -31,7 +31,6 @@ L.Tools_PizzaTimer					= "Start Generic Timer"
 
 L.Tools_BrezArea					= "Battle Res Timer"
 L.Tools_ShowBrezFrame				= "Show Battle Res charges frame during boss encounters and Mythic+ dungeons"
-L.Tools_BrezPreview					= "Preview / Move Frame"
 
 L.Tools_Duration					= "Duration"
 L.Tools_Message						= "Timer Message"
