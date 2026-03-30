02bcbacd68fac970269532300537a042f426772e
MysticalOS <mysticalosx@gmail.com>
2026-03-30 17:11:01 -0400

    prevent skipping of crown cinematic when played manually by using recent kill logic fyrak had

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -7015,7 +7015,9 @@ do
 		[490] = true, -- Unknown, currently encrypted
 	}
 	local requiresRecentKill = {
-		[2238] = 2519--Fyrakk in Amirdrassil
+		[2238] = 2519,--Fyrakk in Amirdrassil
+		[2529] = 3181,--Crown of the Cosmos
+		[1049] = 3181--Crown of the Cosmos
 	}
 	---@param self DBM
 	local function checkOptions(self, id, mapID)
@@ -7055,7 +7057,8 @@ do
 		self:TransitionToDungeonBGM(false, true)
 		if id and not neverFilter[id] then
 			self:Debug("PLAY_MOVIE fired for ID: " .. id, 2, nil, nil, true)
-			if checkOptions(self, id) then
+			local currentMapID = C_Map.GetBestMapForUnit("player")
+			if checkOptions(self, id, currentMapID) then
 				MovieFrame:Hide()--can only just hide movie frame safely now, which means can't stop audio anymore :\
 				self:AddMsg(L.MOVIE_SKIPPED)
 			end
