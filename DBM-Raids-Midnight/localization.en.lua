884da0becbf00a5650e4f3fa6c9e11bfde7c6d4f
MysticalOS <mysticalosx@gmail.com>
2026-04-06 02:20:15 -0400

    Push beloren hardcode

--- DBM-Raids-Midnight/localization.en.lua
+++ DBM-Raids-Midnight/localization.en.lua
@@ -9,3 +9,22 @@ L:SetMiscLocalization({
 	JudgementShield	= "Judgment-SotR)",
 	JudgementFV		= "Judgment-FV)"
 })
+
+---------------------------
+--  Beloren, Child of Alar (3182) --
+---------------------------
+L= DBM:GetModLocalization(2739)
+
+L:SetMiscLocalization({
+	ColorSwap		= "Color Swap"
+})
+
+---------------------------
+--  Midnight Falls (3183) --
+---------------------------
+
+L= DBM:GetModLocalization(2740)
+
+L:SetMiscLocalization({
+	MemoryGame		= "Memory Game"
+})
