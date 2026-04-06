5634b394bcacc2eef93bc775ef96839069993067
Elnarfim <37562642+Elnarfim@users.noreply.github.com>
2026-04-06 18:52:43 +0900

    Add koKR locale

---
+++ DBM-Raids-Midnight/localization.kr.lua
@@ -0,0 +1,31 @@
+if GetLocale() ~= "koKR" then return end
+local L
+
+---------------------------
+--  LightblindedVanguard (3180) --
+---------------------------
+L= DBM:GetModLocalization(2737)
+
+L:SetMiscLocalization({
+	JudgementShield	= "심판-응방)",
+	JudgementFV		= "심판-선고)"
+})
+
+---------------------------
+--  Beloren, Child of Alar (3182) --
+---------------------------
+L= DBM:GetModLocalization(2739)
+
+L:SetMiscLocalization({
+	ColorSwap		= "색 교체"
+})
+
+---------------------------
+--  Midnight Falls (3183) --
+---------------------------
+
+L= DBM:GetModLocalization(2740)
+
+L:SetMiscLocalization({
+	MemoryGame		= "메모리 게임"
+})
