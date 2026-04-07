5c516527897a0bb884a7e5c9e292ebf34f485d80
MysticalOS <mysticalosx@gmail.com>
2026-04-07 19:16:06 -0400

    Fix DBM not loading on Predaxas

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -83,7 +83,7 @@ DBM.TaintedByTests = false -- Tests may mess with some internal state, you proba
 private.fakeBWVersion, private.fakeBWHash = 412, "5f04367"--412.7
 
 -- The string that is shown as version
-DBM.DisplayVersion = "12.0.37"--Core version
+DBM.DisplayVersion = "12.0.38 alpha"--Core version
 DBM.classicSubVersion = 0
 DBM.dungeonSubVersion = 0
 DBM.ReleaseRevision = releaseDate(2026, 4, 6) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
