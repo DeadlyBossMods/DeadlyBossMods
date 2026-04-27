16f74d3db9877a18eda525ddedf3917e6ba93cc1
MysticalOS <mysticalosx@gmail.com>
2026-04-27 04:46:37 -0400

    double number of lines

--- DBM-Core/modules/DevTools.lua
+++ DBM-Core/modules/DevTools.lua
@@ -20,7 +20,7 @@ local mfloor, mmax, mceil = math.floor, math.max, math.ceil
 do
 	local debugLogFrame, debugLogViewport, debugLogContent, clearButton
 	local debugLogLineFrames = {}
-	local maxDebugLogEntries = 1000
+	local maxDebugLogEntries = 2000
 	local debugLogSoftClosed = true
 	local lineHeight = 14
 	local debugLogLineCount = 0
