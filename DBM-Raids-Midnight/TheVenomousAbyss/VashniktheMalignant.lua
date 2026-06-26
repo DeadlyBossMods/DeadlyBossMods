ad3e0b1dbe44022ea54a4961906847ea224f1bad
MysticalOS <mysticalosx@gmail.com>
2026-06-26 15:35:52 -0500

    Vashnik hardcode and notes update

--- DBM-Raids-Midnight/TheVenomousAbyss/VashniktheMalignant.lua
+++ DBM-Raids-Midnight/TheVenomousAbyss/VashniktheMalignant.lua
@@ -11,14 +11,12 @@ mod:SetZone(3004)
 
 mod:RegisterCombat("combat")
 
---TODO, verify theory on adaptive infection (theory that main ID is only for timer and sub IDs for specific type of infection gained)
---TODO, find way to improve personal alerts
+--TODO, find way to improve personal alerts, might not be able to detect which choice player did to disabiguate WHICH BlizzYou warning to schedule
 --TODO, https://www.wowhead.com/ptr/spell=1280881/toxic-outpouring and https://www.wowhead.com/ptr/spell=1280871/toxic-outpouring exists with ID of 758/769 but isn't in journal
 --TODO, https://www.wowhead.com/ptr/spell=1296335/desquamating-venom exists with ID of 766 but isn't in journal
---TODO, a lot of figuring out which abiliteis have timers and which ones don't. which ones are repeating during each infection and which ones are passive
 DBM:RegisterAltSpellName(1281907, DBM_COMMON_L.DEBUFFS)--Plague Froth --> Debuffs
 local warnAdaptiveInfection				= mod:NewCountAnnounce(1282114, 2)--Hardcode only
-local warnToxicOutpouring				= mod:NewCountAnnounce(1280881, 2)--Hardcode only, likely not used
+--local warnToxicOutpouring				= mod:NewCountAnnounce(1280881, 2)--Hardcode only, likely not used
 local warnImbibeToxin					= mod:NewCountAnnounce(1283164, 2)--Hardcode only
 
 local specWarnDrippingFangs				= mod:NewSpecialWarningCount(1280935, nil, nil, nil, 1, 2, nil, nil, "defensive")
@@ -33,13 +31,14 @@ local timerDrippingFangsCD				= mod:NewCDCountTimer(20.5, 1280935, nil, "Tank|He
 local timerAdaptiveInfectionCD			= mod:NewCDCountTimer(20.5, 1282114, nil, nil, nil, 6)
 local timerMalignantCatalystCD			= mod:NewCDCountTimer(20.5, 1282509, nil, nil, nil, 5, nil, DBM_COMMON_L.HEROIC_ICON)
 local timerPlagueFrothCD				= mod:NewCDCountTimer(20.5, 1281907, nil, nil, nil, 3)
-local timerToxicOutpouringCD			= mod:NewCDCountTimer(20.5, 1280881, nil, nil, nil, 5)--likely not used
+--local timerToxicOutpouringCD			= mod:NewCDCountTimer(20.5, 1280881, nil, nil, nil, 5)--likely not used
 local timerImbibeToxinCD				= mod:NewCDCountTimer(20.5, 1283164, nil, nil, nil, 5)
-local timerDesquamatingVenomCD			= mod:NewCDCountTimer(20.5, 1296335, nil, nil, nil, 3)--likely not used
+--local timerDesquamatingVenomCD			= mod:NewCDCountTimer(20.5, 1296335, nil, nil, nil, 3)--likely not used
 local timerStygianBurstCD				= mod:NewCDCountTimer(20.5, 1302489, nil, nil, nil, 3)
 --local timerBerserkCD					= mod:NewBerserkTimer(600)--Unending Tides
 
---local badStateDetected = false--Used to track if hardcode features have failed and we need to fall back to blizz API
+local badStateDetected = false--Used to track if hardcode features have failed and we need to fall back to blizz API
+local next30Event = "dripping"
 
 mod.vb.DrippingFangsCount = 0
 mod.vb.AdaptiveInfectionCount = 0
@@ -70,14 +69,15 @@ local function setFallback(self, dontSetAlerts)
 	timerAdaptiveInfectionCD:SetTimeline(755, onlyColor)
 	timerMalignantCatalystCD:SetTimeline(756, onlyColor)
 	timerPlagueFrothCD:SetTimeline(757, onlyColor)
-	timerToxicOutpouringCD:SetTimeline({758, 769}, onlyColor)
+--	timerToxicOutpouringCD:SetTimeline({758, 769}, onlyColor)
 	timerImbibeToxinCD:SetTimeline(759, onlyColor)
-	timerDesquamatingVenomCD:SetTimeline(766, onlyColor)
+--	timerDesquamatingVenomCD:SetTimeline(766, onlyColor)
 	timerStygianBurstCD:SetTimeline(775, onlyColor)
 end
 
 function mod:OnLimitedCombatStart()
 	self:TLCountReset()
+	next30Event = "dripping"
 	self.vb.DrippingFangsCount = 1
 	self.vb.AdaptiveInfectionCount = 1
 	self.vb.MalignantCatalystCount = 1
@@ -88,38 +88,61 @@ function mod:OnLimitedCombatStart()
 	self.vb.StygianBurstCount = 1
 
 	--Hardcode features first
-	--if DBM.Options.HardcodedTimer and not badStateDetected then
-	--	--self:SetStage(1)
-	--	self:IgnoreBlizzardAPI()
-	--	self:RegisterShortTermEvents(
-	--		"ENCOUNTER_TIMELINE_EVENT_ADDED",
-	--		"ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED"
-	--	)
-	--	if DBM.Options.HideDBMBars then
-	--		setFallback(self, true)
-	--	end
-	--else
+	if DBM.Options.HardcodedTimer and self:IsHeroic() and not badStateDetected then
+		self:IgnoreBlizzardAPI()
+		self:RegisterShortTermEvents(
+			"ENCOUNTER_TIMELINE_EVENT_ADDED",
+			"ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED"
+		)
+		setFallback(self, true)
+		--These 3 still need to use blizz api for now even in hardcode, we can't disambiguate which on to schedule on phase change yet
+		--TODO, possibly just change these 3 to non hardcoded api
+		specWarnStygianInfection:SetAlert({770, 774}, "watchstep", 2, 2, 0)
+		specWarnSiphoningInfection:SetAlert(771, "gathershare", 2, 2, 0)
+		specWarnExplodingInfection:SetAlert({772, 773}, "runout", 2, 2, 0)
+	else
 		setFallback(self)
-	--end
+	end
 end
 
 
 function mod:OnCombatEnd()
 	self:TLCountReset()
+	next30Event = "dripping"
 	self:UnregisterShortTermEvents()
 end
 
---[[
 do
 	---@param self DBMMod
 	---@param timer number
 	---@param timerExact number
 	---@param eventID number
-	local function timersAll(self, timer, timerExact, eventID)
+	local function timersHeroic(self, timer, timerExact, eventID)
 		local handled
-		if timer == 114 then
+		if timer == 8 or timer == 11 or timer == 22 then
 			handled = true
-			timerWaterJetCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "waterjet", "tankWaterCount"))
+			timerDrippingFangsCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "dripping", "DrippingFangsCount"))
+		elseif timer == 10 or timer == 16 or timer == 21 or timer == 31 then
+			handled = true
+			timerPlagueFrothCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "plague", "PlagueFrothCount"))
+		elseif timer == 20 or timer == 80 then
+			handled = true
+			timerImbibeToxinCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "imbibe", "ImbibeToxinCount"))
+		elseif timer == 6 or timer == 44 then
+			handled = true
+			timerMalignantCatalystCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "catalyst", "MalignantCatalystCount"))
+		elseif timer == 23 or timer == 24 then
+			handled = true
+			timerAdaptiveInfectionCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "adaptive", "AdaptiveInfectionCount"))
+		elseif timer == 30 then
+			handled = true
+			if next30Event == "dripping" then
+				timerDrippingFangsCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "dripping", "DrippingFangsCount"))
+				next30Event = "adaptive"
+			else
+				timerAdaptiveInfectionCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "adaptive", "AdaptiveInfectionCount"))
+				next30Event = "dripping"
+			end
 		end
 
 		if not handled then--Reached end of chain without finding a valid timer, this means hardcode mod has failed, so we need to disable hardcoded features and fall back to blizz API
@@ -135,11 +158,12 @@ do
 
 	function mod:ENCOUNTER_TIMELINE_EVENT_ADDED(eventInfo)
 		if eventInfo.source ~= 0 then return end
+		if not self:IsHeroic() then return end--Hardcoded routing currently Heroic-only
 		local eventID = eventInfo.id
 		local timerExact = eventInfo.duration
 		local timer = math.floor(timerExact + 0.5)
 		if not badStateDetected then
-			timersAll(self, timer, timerExact, eventID)
+			timersHeroic(self, timer, timerExact, eventID)
 		end
 	end
 
@@ -148,14 +172,21 @@ do
 		if not eventID or not eventState then return end
 		if eventState == 2 then
 			local eventType, eventCount = self:TLCountFinish(eventID)
-			if not eventType then return end
-			if not eventCount then return end
-			if eventType == "waterjet" then
-				--specWarnWaterJet:Show(eventCount, "lineyou")
+			if eventType and eventCount then
+				if eventType == "dripping" then
+					specWarnDrippingFangs:Show(eventCount)
+					specWarnDrippingFangs:Play("defensive")
+				elseif eventType == "catalyst" then
+					specWarnMalignantCatalyst:Show(eventCount)
+					specWarnMalignantCatalyst:Play("helpsoak")
+				elseif eventType == "adaptive" then
+					warnAdaptiveInfection:Show(eventCount)
+				elseif eventType == "imbibe" then
+					warnImbibeToxin:Show(eventCount)
+				end
 			end
 		elseif eventState == 3 then
 			self:TLCountCancel(eventID)
 		end
 	end
 end
---]]
