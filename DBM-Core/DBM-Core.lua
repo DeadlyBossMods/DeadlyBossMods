977cde4dbb1082dd07d1e085e85dd66387b7a5de
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-07-24 14:14:15 -0700

    Remove SoD raids from Era on the Raid list (#2139)

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -2345,6 +2345,18 @@ do
 							})
 							if self.AddOns[#self.AddOns].subTabs then
 								local subTabs = self.AddOns[#self.AddOns].subTabs
+								-- Remove SoD-only raid subtabs on Classic Era
+								if private.isClassic and not private.currentSeason then
+									local sodOnlyRaids = {[48]=true, [90]=true, [109]=true, [2856]=true}
+									local sodOnlyDungeons = {[2784]=true, [2875]=true}
+									local filter = addonName == "DBM-Raids-Vanilla" and sodOnlyRaids or sodOnlyDungeons
+									for k = #subTabs, 1, -1 do
+										local id = tonumber(subTabs[k])
+										if id and filter[id] then
+											tremove(subTabs, k)
+										end
+									end
+								end
 								for k, _ in ipairs(subTabs) do
 									--Ugly hack to inject custom string text into auto localized zone name sub cats
 									if subTabs[k]:find("|") then
