2a06c69a2104da6b2d25be922aa0ca931b40dee6
Mini Dragon <projecteurs@gmail.com>
2026-08-07 21:20:48 +0800

    Add zhCN

---
+++ DBM-Raids-Midnight/localization.cn.lua
@@ -0,0 +1,31 @@
+if GetLocale() ~= "zhCN" then return end
+local L
+
+---------------------------
+--  LightblindedVanguard (3180) --
+---------------------------
+L= DBM:GetModLocalization(2737)
+
+L:SetMiscLocalization({
+	JudgementShield	= "正义盾击",
+	JudgementFV		= "最终审判"
+})
+
+---------------------------
+--  Beloren, Child of Alar (3182) --
+---------------------------
+L= DBM:GetModLocalization(2739)
+
+L:SetMiscLocalization({
+	ColorSwap		= "交换颜色"
+})
+
+---------------------------
+--  Midnight Falls (3183) --
+---------------------------
+
+L= DBM:GetModLocalization(2740)
+
+L:SetMiscLocalization({
+	MemoryGame		= "记忆游戏"
+})
